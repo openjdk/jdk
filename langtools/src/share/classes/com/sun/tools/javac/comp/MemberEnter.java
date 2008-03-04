2898557469e8a08c4a36cda72caa6001fa74d6ef
Maurizio Cimadamore <mcimadamore@openjdk.java.net>
2008-03-04 15:45:20 +0000

    6663588: Compiler goes into infinite loop for Cyclic Inheritance test case
    Interplay between cyclic inheritance and tvar bounds hangs javac
    
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/javac/comp/MemberEnter.java
+++ langtools/src/share/classes/com/sun/tools/javac/comp/MemberEnter.java
@@ -901,9 +901,9 @@ public class MemberEnter extends JCTree.Visitor implements Completer {
                 c.flags_field |= DEPRECATED;
             annotateLater(tree.mods.annotations, baseEnv, c);
 
-            attr.attribTypeVariables(tree.typarams, baseEnv);
-
             chk.checkNonCyclic(tree.pos(), c.type);
+
+            attr.attribTypeVariables(tree.typarams, baseEnv);
 
             // Add default constructor if needed.
             if ((c.flags() & INTERFACE) == 0 &&
