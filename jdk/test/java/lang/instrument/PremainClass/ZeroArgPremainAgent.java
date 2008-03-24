0ba12de49f8f35da0e8c1651932f9db3a412297e
Daniel D. Daugherty <dcubed@openjdk.java.net>
2008-03-24 15:20:29 -0700

    6289149: 4/4 Java Agent will pick wrong execution path while attaching
    Check for a declared premain() or agentmain() method before an inherited one
    
    Reviewed-by: sspitsyn

--- jdk/test/java/lang/instrument/PremainClass/ZeroArgPremainAgent.java
+++ jdk/test/java/lang/instrument/PremainClass/ZeroArgPremainAgent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,11 +21,12 @@
  * have any questions.
  */
 
-/*
- * dummy "Hello World"ish application for "premain" tests
- */
-public class DummyMain {
-    public static void main(String[] args) {
-        System.out.println("Hello from DummyMain!");
+import java.lang.instrument.*;
+
+class ZeroArgPremainAgent {
+
+    // This agent has a zero arg premain() function.
+    public static void premain () {
+        System.out.println("Hello from ZeroArgInheritAgent!");
     }
 }
