ce2770ad0d8529829e2cc311f3d06aa02bc366d4
Anton Tarasov <ant@openjdk.java.net>
2008-03-26 16:56:40 +0300

    6609607: test/closed/java/awt/Focus/AppletInitialFocusTest should be rewritten
    Using test.java.awt.regtesthelpers.Util. Refactoring.
    
    Reviewed-by: volk

--- jdk/test/java/awt/Focus/AppletInitialFocusTest/AppletInitialFocusTest1.java
+++ jdk/test/java/awt/Focus/AppletInitialFocusTest/AppletInitialFocusTest1.java
@@ -1,32 +1,9 @@
 /*
- * Copyright 2008 Sun Microsystems, Inc.  All Rights Reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Sun Microsystems, Inc., 4150 Network Circle, Santa Clara,
- * CA 95054 USA or visit www.sun.com if you need additional information or
- * have any questions.
- */
-
-/*
-  test
+  test %W% %E%
   @bug 4411534 4517274
   @summary ensures that user's requestFocus() during applet initialization
            is not ignored.
-  @author  prs area=appletviewer
+  @author  prs@sparc.spb.su area=appletviewer
   @run shell AppletInitialFocusTest1.sh
 */
 
