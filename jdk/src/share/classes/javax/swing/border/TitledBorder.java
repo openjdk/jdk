a43301e185258384ab7fea408c70a45498a53038
Sergey Malenkov <malenkov@openjdk.java.net>
2008-05-07 16:08:31 +0400

    6625450: javax.swing.border.TitledBorder.getBaseline() doesn't throw IAE when width is < 0
    Necessary check is added
    
    Reviewed-by: peterz, alexp

--- jdk/src/share/classes/javax/swing/border/TitledBorder.java
+++ jdk/src/share/classes/javax/swing/border/TitledBorder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 1997-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -641,6 +641,9 @@ public class TitledBorder extends AbstractBorder
     public int getBaseline(Component c, int width, int height) {
         if (c == null) {
             throw new NullPointerException("Must supply non-null component");
+        }
+        if (width < 0) {
+            throw new IllegalArgumentException("Width must be >= 0");
         }
         if (height < 0) {
             throw new IllegalArgumentException("Height must be >= 0");
