4c95421013701dca4e79a34f5ef5de7883d71c5b
Jim Holmlund <jjh@openjdk.org>
2008-07-09 13:43:26 -0700

    6700889: Thread resume invalidates all stack frames, even from other threads 6701700: MonitorInfo objects aren't invalidated when the owning thread is resumed
    Don't inform ThreadListeners for thread T1 when some other thread is resumed, and MonitoryIfoImpl must add itself as a ThreadListener
    
    Reviewed-by: dcubed

--- jdk/test/com/sun/jdi/MonitorFrameInfo.java
+++ jdk/test/com/sun/jdi/MonitorFrameInfo.java
@@ -25,7 +25,8 @@
  *  @test
  *  @bug 6230699
  *  @summary Test ThreadReference.ownedMonitorsAndFrames()
- *
+ *  @bug 6701700
+ *  @summary MonitorInfo objects aren't invalidated when the owning thread is resumed
  *  @author Swamy Venkataramanappa
  *
  *  @run build TestScaffold VMConnection TargetListener TargetAdapter
@@ -100,15 +101,15 @@ public class MonitorFrameInfo extends TestScaffold {
 
         if (!mainThread.frame(0).location().method().name()
                         .equals("foo3")) {
-            failure("frame failed");
+            failure("FAILED: frame failed");
         }
 
         if (mainThread.frames().size() != (initialSize + 3)) {
-            failure("frames size failed");
+            failure("FAILED: frames size failed");
         }
 
         if (mainThread.frames().size() != mainThread.frameCount()) {
-            failure("frames size not equal to frameCount");
+            failure("FAILED: frames size not equal to frameCount");
         }
 
         /* Test monitor frame info.
@@ -119,12 +120,31 @@ public class MonitorFrameInfo extends TestScaffold {
             if (monitors.size() != expectedCount) {
                 failure("monitors count is not equal to expected count");
             }
+            MonitorInfo mon = null;
             for (int j=0; j < monitors.size(); j++) {
-                MonitorInfo mon  = (MonitorInfo)monitors.get(j);
+                mon  = (MonitorInfo)monitors.get(j);
                 System.out.println("Monitor obj " + mon.monitor() + "depth =" +mon.stackDepth());
                 if (mon.stackDepth() != expectedDepth[j]) {
-                    failure("monitor stack depth is not equal to expected depth");
+                    failure("FAILED: monitor stack depth is not equal to expected depth");
                 }
+            }
+
+            // The last gotten monInfo is in mon.   When we resume the thread,
+            // it should become invalid.  We will step out of the top frame
+            // so that the frame depth in this mon object will no longer be correct.
+            // That is why the monInfo's have to become invalid when the thread is
+            // resumed.
+            stepOut(mainThread);
+            boolean ok = false;
+            try {
+                System.out.println("*** Saved Monitor obj " + mon.monitor() + "depth =" +mon.stackDepth());
+            } catch(InvalidStackFrameException ee) {
+                // ok
+                ok = true;
+                System.out.println("Got expected InvalidStackFrameException after a resume");
+            }
+            if (!ok) {
+                failure("FAILED: MonitorInfo object was not invalidated by a resume");
             }
         } else {
             System.out.println("can not get monitors frame info");
