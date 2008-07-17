1193f0f9dbe9e365e86f1865d828a86bccbc39ce
Igor Veresov <iveresov@openjdk.org>
2008-07-17 10:26:33 -0700

    6723228: NUMA allocator: assert(lgrp_id != -1, "No lgrp_id set") 6723229: NUMA allocator: assert(lgrp_num > 0, "There should be at least one locality group")
    The fix takes care of the assertion triggered during TLAB resizing after reconfiguration. Also it now handles a defect in the topology graph, in which a single leaf node doesn't have memory.
    
    Reviewed-by: jmasa

--- hotspot/src/share/vm/gc_implementation/shared/mutableNUMASpace.hpp
+++ hotspot/src/share/vm/gc_implementation/shared/mutableNUMASpace.hpp
@@ -112,6 +112,7 @@ class MutableNUMASpace : public MutableSpace {
     int lgrp_id() const                             { return _lgrp_id;             }
     MutableSpace* space() const                     { return _space;               }
     AdaptiveWeightedAverage* alloc_rate() const     { return _alloc_rate;          }
+    void clear_alloc_rate()                         { _alloc_rate->clear();        }
     SpaceStats* space_stats()                       { return &_space_stats;        }
     void clear_space_stats()                        { _space_stats = SpaceStats(); }
 
