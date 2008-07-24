95dc5f84736124acfafdcb7e8671c3ef2b1858ed
Sergey Malenkov <malenkov@openjdk.org>
2008-07-24 17:30:59 +0400

    6707406: BasicColorChooserUI tests throw NPE while getColorSelectionModel if isPropertyChanged() returns true
    Reviewed-by: peterz, rupashka

--- jdk/src/share/classes/com/sun/java/swing/plaf/gtk/GTKColorChooserPanel.java
+++ jdk/src/share/classes/com/sun/java/swing/plaf/gtk/GTKColorChooserPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2004 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2002-2008 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -442,7 +442,10 @@ class GTKColorChooserPanel extends AbstractColorChooserPanel implements
         }
 
         if (updateModel) {
-            getColorSelectionModel().setSelectedColor(color);
+            ColorSelectionModel model = getColorSelectionModel();
+            if (model != null) {
+                model.setSelectedColor(color);
+            }
         }
 
         triangle.setColor(hue, saturation, brightness);
