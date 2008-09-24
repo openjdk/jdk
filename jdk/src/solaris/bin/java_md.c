d799afdc047fc665d4bade4bf7bdeb6c3c6bfb51
Kumar Srinivasan <ksrini@openjdk.org>
2008-09-24 15:07:41 -0700

    6742159: (launcher) improve the java launching mechanism
    Improves launching, also addresses 6694671 and 6749732
    
    Reviewed-by: darcy, dholmes

--- jdk/src/solaris/bin/java_md.c
+++ jdk/src/solaris/bin/java_md.c
@@ -1312,3 +1312,24 @@ InitLauncher(jboolean javaw)
 {
     JLI_SetTraceLauncher();
 }
+
+/*
+ * The implementation for finding classes from the bootstrap
+ * class loader, refer to java.h
+ */
+static FindClassFromBootLoader_t *findBootClass = NULL;
+
+jclass
+FindBootStrapClass(JNIEnv *env, const char* classname)
+{
+   if (findBootClass == NULL) {
+       findBootClass = (FindClassFromBootLoader_t *)dlsym(RTLD_DEFAULT,
+          "JVM_FindClassFromBootLoader");
+       if (findBootClass == NULL) {
+           JLI_ReportErrorMessage(DLL_ERROR4,
+               "JVM_FindClassFromBootLoader");
+           return NULL;
+       }
+   }
+   return findBootClass(env, classname, JNI_FALSE);
+}
