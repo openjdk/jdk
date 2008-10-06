1668ffd84e27bb430996faefb4e2781b77e8f0cb
Jonathan Gibbons <jjg@openjdk.java.net>
2008-10-06 16:57:15 -0700

    6748546: javadoc API should be classloader-friendly
    Reviewed-by: bpatel

--- langtools/src/share/classes/com/sun/tools/javadoc/Main.java
+++ langtools/src/share/classes/com/sun/tools/javadoc/Main.java
@@ -61,12 +61,43 @@ public class Main {
 
     /**
      * Programmatic interface.
+     * @param args   The command line parameters.
+     * @param docletParentClassLoader The parent class loader used when
+     *  creating the doclet classloader. If null, the class loader used
+     *  to instantiate doclets will be created without specifying a parent
+     *  class loader.
+     * @return The return code.
+     * @since 1.7
+     */
+    public static int execute(ClassLoader docletParentClassLoader, String... args) {
+        Start jdoc = new Start(docletParentClassLoader);
+        return jdoc.begin(args);
+    }
+
+    /**
+     * Programmatic interface.
      * @param programName  Name of the program (for error messages).
      * @param args   The command line parameters.
      * @return The return code.
      */
     public static int execute(String programName, String... args) {
         Start jdoc = new Start(programName);
+        return jdoc.begin(args);
+    }
+
+    /**
+     * Programmatic interface.
+     * @param programName  Name of the program (for error messages).
+     * @param args   The command line parameters.
+     * @param docletParentClassLoader The parent class loader used when
+     *  creating the doclet classloader. If null, the class loader used
+     *  to instantiate doclets will be created without specifying a parent
+     *  class loader.
+     * @return The return code.
+     * @since 1.7
+     */
+    public static int execute(String programName, ClassLoader docletParentClassLoader, String... args) {
+        Start jdoc = new Start(programName, docletParentClassLoader);
         return jdoc.begin(args);
     }
 
@@ -81,6 +112,26 @@ public class Main {
                               String defaultDocletClassName,
                               String... args) {
         Start jdoc = new Start(programName, defaultDocletClassName);
+        return jdoc.begin(args);
+    }
+
+    /**
+     * Programmatic interface.
+     * @param programName  Name of the program (for error messages).
+     * @param defaultDocletClassName  Fully qualified class name.
+     * @param docletParentClassLoader The parent class loader used when
+     *  creating the doclet classloader. If null, the class loader used
+     *  to instantiate doclets will be created without specifying a parent
+     *  class loader.
+     * @param args   The command line parameters.
+     * @return The return code.
+     * @since 1.7
+     */
+    public static int execute(String programName,
+                              String defaultDocletClassName,
+                              ClassLoader docletParentClassLoader,
+                              String... args) {
+        Start jdoc = new Start(programName, defaultDocletClassName, docletParentClassLoader);
         return jdoc.begin(args);
     }
 
@@ -103,6 +154,35 @@ public class Main {
         Start jdoc = new Start(programName,
                                errWriter, warnWriter, noticeWriter,
                                defaultDocletClassName);
+        return jdoc.begin(args);
+    }
+
+    /**
+     * Programmatic interface.
+     * @param programName  Name of the program (for error messages).
+     * @param errWriter    PrintWriter to receive error messages.
+     * @param warnWriter    PrintWriter to receive error messages.
+     * @param noticeWriter    PrintWriter to receive error messages.
+     * @param defaultDocletClassName  Fully qualified class name.
+     * @param docletParentClassLoader The parent class loader used when
+     *  creating the doclet classloader. If null, the class loader used
+     *  to instantiate doclets will be created without specifying a parent
+     *  class loader.
+     * @param args   The command line parameters.
+     * @return The return code.
+     * @since 1.7
+     */
+    public static int execute(String programName,
+                              PrintWriter errWriter,
+                              PrintWriter warnWriter,
+                              PrintWriter noticeWriter,
+                              String defaultDocletClassName,
+                              ClassLoader docletParentClassLoader,
+                              String... args) {
+        Start jdoc = new Start(programName,
+                               errWriter, warnWriter, noticeWriter,
+                               defaultDocletClassName,
+                               docletParentClassLoader);
         return jdoc.begin(args);
     }
 }
