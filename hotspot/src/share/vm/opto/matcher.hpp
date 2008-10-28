429a95440a995e537dd8c93c6f4f60d8f6445574
Tom Rodriguez <never@openjdk.org>
2008-10-28 09:31:30 -0700

    6754519: don't emit flag fixup for NaN when condition being tested doesn't need it
    Reviewed-by: kvn, rasbold

--- hotspot/src/share/vm/opto/matcher.hpp
+++ hotspot/src/share/vm/opto/matcher.hpp
@@ -324,7 +324,7 @@ public:
   virtual int      regnum_to_fpu_offset(int regnum);
 
   // Is this branch offset small enough to be addressed by a short branch?
-  bool is_short_branch_offset(int offset);
+  bool is_short_branch_offset(int rule, int offset);
 
   // Optional scaling for the parameter to the ClearArray/CopyArray node.
   static const bool init_array_count_is_in_bytes;
