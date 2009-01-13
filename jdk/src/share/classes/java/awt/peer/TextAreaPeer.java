69da36915edb9fa742c73929b88cfb9a5027e8e8
Roman Kennke <rkennke@openjdk.java.net>
2009-01-13 20:04:05 +0100

    6792515: Specify awt peer's API
    Document AWT peer API.
    
    Reviewed-by: art, dav

--- jdk/src/share/classes/java/awt/peer/TextAreaPeer.java
+++ jdk/src/share/classes/java/awt/peer/TextAreaPeer.java
@@ -25,8 +25,11 @@
 package java.awt.peer;
 
 import java.awt.Dimension;
+import java.awt.TextArea;
 
 /**
+ * The peer interface for {@link TexTArea}.
+ *
  * The peer interfaces are intended only for use in porting
  * the AWT. They are not intended for use by application
  * developers, and developers should not implement peers
@@ -34,9 +37,52 @@ import java.awt.Dimension;
  * instances.
  */
 public interface TextAreaPeer extends TextComponentPeer {
+
+    /**
+     * Inserts the specified text at the specified position in the document.
+     *
+     * @param text the text to insert
+     * @param pos the position to insert
+     *
+     * @see TextArea#insert(String, int)
+     */
     void insert(String text, int pos);
+
+    /**
+     * Replaces a range of text by the specified string
+     *
+     * @param text the replacement string
+     * @param start the begin of the range to replace
+     * @param end the end of the range to replace
+     *
+     * @see TextArea#replaceRange(String, int, int)
+     */
     void replaceRange(String text, int start, int end);
+
+    /**
+     * Returns the preferred size of a textarea with the specified number of
+     * columns and rows.
+     *
+     * @param rows the number of rows
+     * @param columns the number of columns
+     *
+     * @return the preferred size of a textarea
+     *
+     * @see TextArea#getPreferredSize(int, int)
+     */
     Dimension getPreferredSize(int rows, int columns);
+
+    /**
+     * Returns the minimum size of a textarea with the specified number of
+     * columns and rows.
+     *
+     * @param rows the number of rows
+     * @param columns the number of columns
+     *
+     * @return the minimum size of a textarea
+     *
+     * @see TextArea#getMinimumSize(int, int)
+     */
     Dimension getMinimumSize(int rows, int columns);
 
 }
