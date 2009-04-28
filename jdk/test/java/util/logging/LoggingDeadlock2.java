2756e993160f12f0bd5c0297a4a8eb2a83e6bd50
Kelly O'Hair <ohair@openjdk.org>
2009-04-28 14:43:44 -0700

    6835241: Annotate some tests with @ignore that have shown to be unpredictable
    Reviewed-by: tbell

--- jdk/test/java/util/logging/LoggingDeadlock2.java
+++ jdk/test/java/util/logging/LoggingDeadlock2.java
@@ -24,6 +24,7 @@
 /*
  * @test
  * @bug     6467152 6716076 6829503
+ * @ignore Until made more stable, see 6829636.
  * @summary deadlock occurs in LogManager initialization and JVM termination
  * @author  Serguei Spitsyn / Hitachi / Martin Buchholz
  *
