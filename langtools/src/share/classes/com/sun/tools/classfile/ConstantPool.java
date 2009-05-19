f05e74203e2d4fb0668c59d724f52c706a62e0ec
Jonathan Gibbons <jjg@openjdk.org>
2009-05-19 11:43:50 -0700

    6841419: classfile: add constant pool iterator
    Reviewed-by: mcimadamore

--- langtools/src/share/classes/com/sun/tools/classfile/ConstantPool.java
+++ langtools/src/share/classes/com/sun/tools/classfile/ConstantPool.java
@@ -26,6 +26,7 @@
 package com.sun.tools.classfile;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * See JVMS3, section 4.5.
@@ -223,6 +224,40 @@ public class ConstantPool {
         throw new EntryNotFound(value);
     }
 
+    public Iterable<CPInfo> entries() {
+        return new Iterable<CPInfo>() {
+            public Iterator<CPInfo> iterator() {
+                return new Iterator<CPInfo>() {
+
+                    public boolean hasNext() {
+                        return next < pool.length;
+                    }
+
+                    public CPInfo next() {
+                        current = pool[next];
+                        switch (current.getTag()) {
+                            case CONSTANT_Double:
+                            case CONSTANT_Long:
+                                next += 2;
+                                break;
+                            default:
+                                next += 1;
+                        }
+                        return current;
+                    }
+
+                    public void remove() {
+                        throw new UnsupportedOperationException();
+                    }
+
+                    private CPInfo current;
+                    private int next = 1;
+
+                };
+            }
+        };
+    }
+
     private CPInfo[] pool;
 
     public interface Visitor<R,P> {
@@ -249,6 +284,12 @@ public class ConstantPool {
         }
 
         public abstract int getTag();
+
+        /** The number of slots in the constant pool used by this entry.
+         * 2 for CONSTANT_Double and CONSTANT_Long; 1 for everything else. */
+        public int size() {
+            return 1;
+        }
 
         public abstract <R,D> R accept(Visitor<R,D> visitor, D data);
 
@@ -347,6 +388,11 @@ public class ConstantPool {
 
         public int getTag() {
             return CONSTANT_Double;
+        }
+
+        @Override
+        public int size() {
+            return 2;
         }
 
         @Override
@@ -460,6 +506,11 @@ public class ConstantPool {
 
         public int getTag() {
             return CONSTANT_Long;
+        }
+
+        @Override
+        public int size() {
+            return 2;
         }
 
         @Override
