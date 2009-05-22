ccd5be0fb808cddb175603fad13dccde903de54f
Xueming Shen <sherman@openjdk.java.net>
2009-05-21 23:32:46 -0700

    6843578: Re-implement IBM doublebyte charsets 6639450: IBM949C encoder modifies state of IBM949 encoder 6569191: Cp943 io converter returns U+0000 and U+FFFD for unconvertable character 6577466: Character encoder IBM970 throws a BufferOverflowException 5065777: CharsetEncoder canEncode() methods often incorrectly return false
    Re-write 11 IBM doublebyte charsets. Thanks Ulf.Zibis for the codereview!
    
    Reviewed-by: martin

--- jdk/src/share/classes/sun/io/CharToByteCp948.java
+++ jdk/src/share/classes/sun/io/CharToByteCp948.java
@@ -22,36 +22,19 @@
  * CA 95054 USA or visit www.sun.com if you need additional information or
  * have any questions.
  */
+
 package sun.io;
 
-import sun.nio.cs.ext.IBM948;
+import sun.nio.cs.ext.*;
 
-/**
-* Tables and data to convert Unicode to Cp948
-*
-* @author Malcolm Ayres, assisted by UniMap program
-*/
-public class CharToByteCp948
-        extends CharToByteDBCS_ASCII
+public class CharToByteCp948 extends CharToByteDBCS_ASCII {
 
-{
-        private static IBM948 nioCoder = new IBM948();
+    // Return the character set id
+    public String getCharacterEncoding() {
+        return "Cp948";
+    }
 
-        // Return the character set id
-        public String getCharacterEncoding()
-        {
-                return "Cp948";
-        }
-
-
-        public CharToByteCp948()
-        {
-                super();
-                super.mask1 = 0xFFC0;
-                super.mask2 = 0x003F;
-                super.shift = 6;
-                super.index1 = nioCoder.getEncoderIndex1();
-                super.index2 = nioCoder.getEncoderIndex2();
-                super.index2a = nioCoder.getEncoderIndex2a();
-        }
+    public CharToByteCp948() {
+        super((DoubleByte.Encoder)new IBM948().newEncoder());
+    }
 }
