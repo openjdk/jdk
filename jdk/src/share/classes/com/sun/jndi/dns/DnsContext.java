1919e3cbbc6dfdf60b6a29651a9a00e2bb6d30c1
Vinnie Ryan <vinnie@openjdk.java.net>
2009-07-17 20:29:41 +0100

    6657619: DnsContext.debug is public static mutable (findbugs)
    Reviewed-by: alanb

--- jdk/src/share/classes/com/sun/jndi/dns/DnsContext.java
+++ jdk/src/share/classes/com/sun/jndi/dns/DnsContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2007 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2000-2009 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -922,7 +922,7 @@ public class DnsContext extends ComponentDirContext {
 
     //---------- Debugging
 
-    public static boolean debug = false;
+    private static final boolean debug = false;
 
     private static final void dprint(String msg) {
         if (debug) {
