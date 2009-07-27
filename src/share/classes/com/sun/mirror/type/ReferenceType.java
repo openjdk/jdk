56e24998be5aaceb397e68f84b70e4bc14a3e438
darcy <none@none>
2009-07-26 21:27:11 -0700

    6381698: Warn of decommissioning of apt Reviewed-by: jjg

--- src/share/classes/com/sun/mirror/type/ReferenceType.java
+++ src/share/classes/com/sun/mirror/type/ReferenceType.java
@@ -30,10 +30,16 @@ package com.sun.mirror.type;
  * Represents a reference type.
  * These include class and interface types, array types, and type variables.
  *
+ * @deprecated All components of this API have been superseded by the
+ * standardized annotation processing API.  The replacement for the
+ * functionality of this interface is {@link
+ * javax.lang.model.type.ReferenceType}.
+ *
  * @author Joseph D. Darcy
  * @author Scott Seligman
  * @since 1.5
  */
-
+@Deprecated
+@SuppressWarnings("deprecation")
 public interface ReferenceType extends TypeMirror {
 }
--- src/share/classes/com/sun/mirror/util/SimpleTypeVisitor.java
+++ src/share/classes/com/sun/mirror/util/SimpleTypeVisitor.java
@@ -37,11 +37,17 @@ import com.sun.mirror.type.*;
  * methods that correspond to the kinds of types on which it will
  * operate.
  *
+ * @deprecated All components of this API have been superseded by the
+ * standardized annotation processing API.  The replacement for the
+ * functionality of this class is {@link
+ * javax.lang.model.util.SimpleTypeVisitor6}.
+ *
  * @author Joseph D. Darcy
  * @author Scott Seligman
  * @since 1.5
  */
-
+@Deprecated
+@SuppressWarnings("deprecation")
 public class SimpleTypeVisitor implements TypeVisitor {
 
     /**
