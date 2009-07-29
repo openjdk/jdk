c114fed1b050d6a76432e4c167bf5f486237b89d
Martin Buchholz <martin@openjdk.java.net>
2009-07-29 13:56:15 -0700

    6866554: Misc. javadoc warnings
    Reviewed-by: alanb

--- jdk/src/share/classes/java/util/concurrent/LinkedBlockingDeque.java
+++ jdk/src/share/classes/java/util/concurrent/LinkedBlockingDeque.java
@@ -988,7 +988,8 @@ public class LinkedBlockingDeque<E>
      * Returns an iterator over the elements in this deque in proper sequence.
      * The elements will be returned in order from first (head) to last (tail).
      * The returned {@code Iterator} is a "weakly consistent" iterator that
-     * will never throw {@link ConcurrentModificationException},
+     * will never throw {@link java.util.ConcurrentModificationException
+     * ConcurrentModificationException},
      * and guarantees to traverse elements as they existed upon
      * construction of the iterator, and may (but is not guaranteed to)
      * reflect any modifications subsequent to construction.
@@ -1004,7 +1005,8 @@ public class LinkedBlockingDeque<E>
      * sequential order.  The elements will be returned in order from
      * last (tail) to first (head).
      * The returned {@code Iterator} is a "weakly consistent" iterator that
-     * will never throw {@link ConcurrentModificationException},
+     * will never throw {@link java.util.ConcurrentModificationException
+     * ConcurrentModificationException},
      * and guarantees to traverse elements as they existed upon
      * construction of the iterator, and may (but is not guaranteed to)
      * reflect any modifications subsequent to construction.
