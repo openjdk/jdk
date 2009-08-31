f30afff296b65edf63f50cb05d20968a561e36f7
Andrey Petrusenko <apetrusenko@openjdk.java.net>
2009-08-31 05:27:29 -0700

    6841313: G1: dirty cards of survivor regions in parallel
    Reviewed-by: tonyp, iveresov

--- hotspot/src/share/vm/gc_implementation/g1/g1_globals.hpp
+++ hotspot/src/share/vm/gc_implementation/g1/g1_globals.hpp
@@ -260,6 +260,9 @@
                                                                             \
   develop(intx, G1CardCountCacheExpandThreshold, 16,                        \
           "Expand the card count cache if the number of collisions for "    \
-          "a particular entry exceeds this value.")
+          "a particular entry exceeds this value.")                         \
+                                                                            \
+  develop(bool, G1VerifyCTCleanup, false,                                   \
+          "Verify card table cleanup.")
 
 G1_FLAGS(DECLARE_DEVELOPER_FLAG, DECLARE_PD_DEVELOPER_FLAG, DECLARE_PRODUCT_FLAG, DECLARE_PD_PRODUCT_FLAG, DECLARE_DIAGNOSTIC_FLAG, DECLARE_EXPERIMENTAL_FLAG, DECLARE_NOTPRODUCT_FLAG, DECLARE_MANAGEABLE_FLAG, DECLARE_PRODUCT_RW_FLAG)
