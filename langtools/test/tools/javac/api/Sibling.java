b2eaa8339f8e5409f56577b74b0c949a5f9c4c19
Jonathan Gibbons <jjg@openjdk.org>
2009-09-08 11:12:13 -0700

    6419701: DefaultFileManager clean up: URI.create 6483788: DefaultFileManager.ZipFileObject.toUri() fails to escape space characters 6501502: JSR 199: FileObject.toUri should return file:///c:/ or file:/c:/ not file://c:/ 6877206: JavaFileObject.toUri returns bogus URI (win) 6877223: tests @ignored because of issues with File.toURI on Windows
    Reviewed-by: mcimadamore, alanb

--- langtools/test/tools/javac/api/Sibling.java
+++ langtools/test/tools/javac/api/Sibling.java
@@ -26,7 +26,6 @@
  * @bug     6399602
  * @summary Verify that files are created relative to sibling
  * @author  Peter von der Ah\u00e9
- * @ignore 6877223 test ignored because of issues with File.toUri on Windows (6877206)
  */
 
 import java.io.File;
@@ -48,10 +47,9 @@ public class Sibling {
                                                             "foo.bar.baz.Test",
                                                             CLASS,
                                                             sibling);
-        String name =
-            new File("Test.class").getAbsolutePath().replace(File.separatorChar, '/');
-        if (!classFile.toUri().getPath().equals(name))
-            throw new AssertionError("Expected " + name + ", got " +
-                                     classFile.toUri().getPath());
+        File file = new File("Test.class").getAbsoluteFile();
+        if (!classFile.toUri().equals(file.toURI()))
+            throw new AssertionError("Expected " + file.toURI() + ", got " +
+                                     classFile.toUri());
     }
 }
