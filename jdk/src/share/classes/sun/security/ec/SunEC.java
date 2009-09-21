f13c1a7ce95dffc4e285fd75594e19ef70abb53f
Vinnie Ryan <vinnie@openjdk.org>
2009-09-21 23:01:42 +0100

    6884175: CR cleanup for 6840752: Provide out-of-the-box support for ECC algorithms
    Reviewed-by: wetmore

--- jdk/src/share/classes/sun/security/ec/SunEC.java
+++ jdk/src/share/classes/sun/security/ec/SunEC.java
@@ -39,13 +39,32 @@ import sun.security.action.PutAllAction;
  * via JNI to a C++ wrapper class which in turn calls C functions.
  * The Java classes are packaged into the signed sunec.jar in the JRE
  * extensions directory and the C++ and C functions are packaged into
- * libsunecc.so or sunecc.dll in the JRE native libraries directory.
+ * libsunec.so or sunec.dll in the JRE native libraries directory.
+ * If the native library is not present then this provider is registered
+ * with support for fewer ECC algorithms (KeyPairGenerator, Signature and
+ * KeyAgreement are omitted).
  *
  * @since   1.7
  */
 public final class SunEC extends Provider {
 
     private static final long serialVersionUID = -2279741672933606418L;
+
+    // flag indicating whether the full EC implementation is present
+    // (when native library is absent then fewer EC algorithms are available)
+    private static boolean useFullImplementation = true;
+    static {
+        try {
+            AccessController.doPrivileged(new PrivilegedAction<Void>() {
+                public Void run() {
+                    System.loadLibrary("sunec"); // check for native library
+                    return null;
+                }
+            });
+        } catch (UnsatisfiedLinkError e) {
+            useFullImplementation = false;
+        }
+    }
 
     public SunEC() {
         super("SunEC", 1.7d, "Sun Elliptic Curve provider (EC, ECDSA, ECDH)");
@@ -54,10 +73,10 @@ public final class SunEC extends Provider {
         // the provider. Otherwise, create a temporary map and use a
         // doPrivileged() call at the end to transfer the contents
         if (System.getSecurityManager() == null) {
-            SunECEntries.putEntries(this);
+            SunECEntries.putEntries(this, useFullImplementation);
         } else {
             Map<Object, Object> map = new HashMap<Object, Object>();
-            SunECEntries.putEntries(map);
+            SunECEntries.putEntries(map, useFullImplementation);
             AccessController.doPrivileged(new PutAllAction(this, map));
         }
     }
