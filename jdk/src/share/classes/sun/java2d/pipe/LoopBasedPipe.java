041e6cb96718723f4d8389b7f029cf5322e5738a
Mario Torre <neugens@openjdk.org>
2009-10-30 19:19:35 +0100

    6896068: SunGraphics2D exposes a reference to itself while non fully initialised
    Introduce a new Interface to mark the Loops based pipes and initialise the loops accordingly.
    
    Reviewed-by: flar, rkennke

--- jdk/src/share/classes/sun/java2d/pipe/LoopBasedPipe.java
+++ jdk/src/share/classes/sun/java2d/pipe/LoopBasedPipe.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2005 Sun Microsystems, Inc.  All Rights Reserved.
+ * Copyright 2009 Sun Microsystems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,21 +25,13 @@
 
 package sun.java2d.pipe;
 
-import java.awt.font.GlyphVector;
-import sun.java2d.SunGraphics2D;
-import sun.font.GlyphList;
-
 /**
- * A delegate pipe of SG2D for drawing anti-aliased text with
- * a solid source colour to an opaque destination.
+ * This is a marker interface used by Pipes that need RenderLoops.
+ * RenderLoops are validated in SurfaceData when a pipe is recognised to
+ * implement this interface.
+ *
+ * @author Mario Torre <neugens@aicas.com>
  */
-
-public class AATextRenderer extends GlyphListLoopPipe
-    implements LoopBasedPipe
-{
-   protected void drawGlyphList(SunGraphics2D sg2d, GlyphList gl) {
-       sg2d.loops.drawGlyphListAALoop.DrawGlyphListAA(sg2d, sg2d.surfaceData,
-                                                      gl);
-   }
+public interface LoopBasedPipe {
 
 }
