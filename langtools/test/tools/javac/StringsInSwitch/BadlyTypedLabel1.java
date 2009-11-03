5979378e3234cc6e66e5f6051acf88df8ec317aa
Joe Darcy <darcy@openjdk.java.net>
2009-11-02 21:36:59 -0800

    6827009: Project Coin: Strings in Switch
    Reviewed-by: jjg, mcimadamore

---
+++ langtools/test/tools/javac/StringsInSwitch/BadlyTypedLabel1.java
@@ -0,0 +1,17 @@
+/*
+ * @test  /nodynamiccopyright/
+ * @bug 6827009
+ * @summary Check for case labels of different types.
+ * @compile/fail -source 6 BadlyTypedLabel1.java
+ * @compile/fail/ref=BadlyTypedLabel1.out -XDstdout -XDrawDiagnostics BadlyTypedLabel1.java
+ */
+class BadlyTypedLabel1 {
+    String m(String s) {
+        switch(s) {
+        case "Hello World":
+            return(s);
+        case 42:
+            return ("Don't forget your towel!");
+        }
+    }
+}
