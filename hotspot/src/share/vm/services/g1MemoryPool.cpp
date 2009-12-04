911a80e5d516bb78426997b6533ee7272e62a0f2
Antonios Printezis <tonyp@openjdk.org>
2009-12-04 07:44:41 -0500

    6906565: G1: deal with compilation warning in g1MemoryPool.hpp
    Size_t max_size() hides size_t max_size() const.
    
    Reviewed-by: jmasa, ysr

--- hotspot/src/share/vm/services/g1MemoryPool.cpp
+++ hotspot/src/share/vm/services/g1MemoryPool.cpp
@@ -116,7 +116,7 @@ MemoryUsage G1EdenPool::get_memory_usage() {
   size_t initial_sz = initial_size();
   size_t max_sz     = max_size();
   size_t used       = used_in_bytes();
-  size_t committed  = eden_space_committed();
+  size_t committed  = eden_space_committed(_g1h);
 
   return MemoryUsage(initial_sz, used, committed, max_sz);
 }
@@ -133,7 +133,7 @@ MemoryUsage G1SurvivorPool::get_memory_usage() {
   size_t initial_sz = initial_size();
   size_t max_sz     = max_size();
   size_t used       = used_in_bytes();
-  size_t committed  = survivor_space_committed();
+  size_t committed  = survivor_space_committed(_g1h);
 
   return MemoryUsage(initial_sz, used, committed, max_sz);
 }
@@ -150,7 +150,7 @@ MemoryUsage G1OldGenPool::get_memory_usage() {
   size_t initial_sz = initial_size();
   size_t max_sz     = max_size();
   size_t used       = used_in_bytes();
-  size_t committed  = old_space_committed();
+  size_t committed  = old_space_committed(_g1h);
 
   return MemoryUsage(initial_sz, used, committed, max_sz);
 }
