8969366ae21a7d7e18625adf01497747d52addae
Peter Zhelezniakov <peterz@openjdk.java.net>
2009-12-10 12:08:58 +0300

    6827653: Make Synth UI classes public
    Reviewed-by: alexp

--- jdk/src/share/classes/javax/swing/plaf/synth/SynthDesktopIconUI.java
+++ jdk/src/share/classes/javax/swing/plaf/synth/SynthDesktopIconUI.java
@@ -28,36 +28,44 @@ package javax.swing.plaf.synth;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.border.*;
 import javax.swing.plaf.*;
 import javax.swing.plaf.basic.BasicDesktopIconUI;
 import java.beans.*;
-import java.io.Serializable;
-import sun.swing.plaf.synth.SynthUI;
 
 
 /**
- * Synth L&F for a minimized window on a desktop.
+ * Provides the Synth L&F UI delegate for a minimized internal frame on a desktop.
  *
  * @author Joshua Outwater
+ * @since 1.7
  */
-class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
-                                        ActionListener, PropertyChangeListener {
+public class SynthDesktopIconUI extends BasicDesktopIconUI
+                                implements SynthUI, PropertyChangeListener {
     private SynthStyle style;
+    private Handler handler = new Handler();
 
+    /**
+     * Creates a new UI object for the given component.
+     *
+     * @param c component to create UI object for
+     * @return the UI object
+     */
     public static ComponentUI createUI(JComponent c)    {
         return new SynthDesktopIconUI();
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     protected void installComponents() {
         if (UIManager.getBoolean("InternalFrame.useTaskBar")) {
             iconPane = new JToggleButton(frame.getTitle(), frame.getFrameIcon()) {
-                public String getToolTipText() {
+                @Override public String getToolTipText() {
                     return getText();
                 }
 
-                public JPopupMenu getComponentPopupMenu() {
+                @Override public JPopupMenu getComponentPopupMenu() {
                     return frame.getComponentPopupMenu();
                 }
             };
@@ -73,24 +81,37 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         desktopIcon.add(iconPane, BorderLayout.CENTER);
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     protected void installListeners() {
         super.installListeners();
         desktopIcon.addPropertyChangeListener(this);
 
         if (iconPane instanceof JToggleButton) {
             frame.addPropertyChangeListener(this);
-            ((JToggleButton)iconPane).addActionListener(this);
+            ((JToggleButton)iconPane).addActionListener(handler);
         }
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     protected void uninstallListeners() {
         if (iconPane instanceof JToggleButton) {
+            ((JToggleButton)iconPane).removeActionListener(handler);
             frame.removePropertyChangeListener(this);
         }
         desktopIcon.removePropertyChangeListener(this);
         super.uninstallListeners();
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     protected void installDefaults() {
         updateStyle(desktopIcon);
     }
@@ -101,6 +122,10 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         context.dispose();
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     protected void uninstallDefaults() {
         SynthContext context = getContext(desktopIcon, ENABLED);
         style.uninstallDefaults(context);
@@ -108,12 +133,16 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         style = null;
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public SynthContext getContext(JComponent c) {
         return getContext(c, getComponentState(c));
     }
 
     private SynthContext getContext(JComponent c, int state) {
-        Region region = getRegion(c);
+        Region region = SynthLookAndFeel.getRegion(c);
         return SynthContext.getContext(SynthContext.class, c, region,
                                        style, state);
     }
@@ -122,10 +151,10 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         return SynthLookAndFeel.getComponentState(c);
     }
 
-    Region getRegion(JComponent c) {
-        return SynthLookAndFeel.getRegion(c);
-    }
-
+    /**
+     * @inheritDoc
+     */
+    @Override
     public void update(Graphics g, JComponent c) {
         SynthContext context = getContext(c);
 
@@ -136,6 +165,10 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         context.dispose();
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public void paint(Graphics g, JComponent c) {
         SynthContext context = getContext(c);
 
@@ -143,31 +176,22 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
         context.dispose();
     }
 
+    /**
+     * Paints the specified component. This implementation does nothing.
+     *
+     * @param context context for the component being painted
+     * @param g {@code Graphics} object used for painting
+     */
     protected void paint(SynthContext context, Graphics g) {
     }
 
+    /**
+     * @inheritDoc
+     */
+    @Override
     public void paintBorder(SynthContext context, Graphics g, int x,
                             int y, int w, int h) {
         context.getPainter().paintDesktopIconBorder(context, g, x, y, w, h);
-    }
-
-    public void actionPerformed(ActionEvent evt) {
-        if (evt.getSource() instanceof JToggleButton) {
-            // Either iconify the frame or deiconify and activate it.
-            JToggleButton button = (JToggleButton)evt.getSource();
-            try {
-                boolean selected = button.isSelected();
-                if (!selected && !frame.isIconifiable()) {
-                    button.setSelected(true);
-                } else {
-                    frame.setIcon(!selected);
-                    if (selected) {
-                        frame.setSelected(true);
-                    }
-                }
-            } catch (PropertyVetoException e2) {
-            }
-        }
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
@@ -187,6 +211,27 @@ class SynthDesktopIconUI extends BasicDesktopIconUI implements SynthUI,
                 } else if (prop == JInternalFrame.IS_ICON_PROPERTY ||
                            prop == JInternalFrame.IS_SELECTED_PROPERTY) {
                     button.setSelected(!frame.isIcon() && frame.isSelected());
+                }
+            }
+        }
+    }
+
+    private final class Handler implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+            if (evt.getSource() instanceof JToggleButton) {
+                // Either iconify the frame or deiconify and activate it.
+                JToggleButton button = (JToggleButton)evt.getSource();
+                try {
+                    boolean selected = button.isSelected();
+                    if (!selected && !frame.isIconifiable()) {
+                        button.setSelected(true);
+                    } else {
+                        frame.setIcon(!selected);
+                        if (selected) {
+                            frame.setSelected(true);
+                        }
+                    }
+                } catch (PropertyVetoException e2) {
                 }
             }
         }
