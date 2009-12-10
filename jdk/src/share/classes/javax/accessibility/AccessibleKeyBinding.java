b94d2e92bb8625231fa66c6c5184c119136d92a6
Joe Darcy <darcy@openjdk.java.net>
2009-12-10 13:28:07 -0800

    4891262: API spec, javax/accessibility: few invalid javadoc tags
    Reviewed-by: jjg

--- jdk/src/share/classes/javax/accessibility/AccessibleKeyBinding.java
+++ jdk/src/share/classes/javax/accessibility/AccessibleKeyBinding.java
@@ -32,16 +32,11 @@ package javax.accessibility;
  * the standard mechanism for an assistive technology to determine the
  * key bindings which exist for this object.
  * Any object that has such key bindings should support this
- * interface.  Applications can determine if an object supports the
- * AccessibleKeyBinding interface by first obtaining its AccessibleContext
- * (see @link Accessible} and then calling the
- * {@link AccessibleContext#getAccessibleKeyBinding} method.  If the return
- * value is not null, the object supports this interface.
+ * interface.
  *
  * @see Accessible
  * @see Accessible#getAccessibleContext
  * @see AccessibleContext
- * @see AccessibleContext#getAccessibleKeyBinding
  *
  * @author      Lynn Monsanto
  * @since 1.4
@@ -58,21 +53,7 @@ public interface AccessibleKeyBinding {
     /**
      * Returns a key binding for this object.  The value returned is an
      * java.lang.Object which must be cast to appropriate type depending
-     * on the underlying implementation of the key.  For example, if the
-     * Object returned is a javax.swing.KeyStroke, the user of this
-     * method should do the following:
-     * <nf><code>
-     * Component c = <get the component that has the key bindings>
-     * AccessibleContext ac = c.getAccessibleContext();
-     * AccessibleKeyBinding akb = ac.getAccessibleKeyBinding();
-     * for (int i = 0; i < akb.getAccessibleKeyBindingCount(); i++) {
-     *     Object o = akb.getAccessibleKeyBinding(i);
-     *     if (o instanceof javax.swing.KeyStroke) {
-     *         javax.swing.KeyStroke keyStroke = (javax.swing.KeyStroke)o;
-     *         <do something with the key binding>
-     *     }
-     * }
-     * </code></nf>
+     * on the underlying implementation of the key.
      *
      * @param i zero-based index of the key bindings
      * @return a javax.lang.Object which specifies the key binding
