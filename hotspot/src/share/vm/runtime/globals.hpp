04d77ad78eed3bdef7842e36f193856485b7c959
Antonios Printezis <tonyp@openjdk.org>
2010-01-13 15:46:13 -0500

    6916652: G1: remove +UseG1GC from under experimental options
    What the title says.
    
    Reviewed-by: ysr, jmasa

--- hotspot/src/share/vm/runtime/globals.hpp
+++ hotspot/src/share/vm/runtime/globals.hpp
@@ -1204,7 +1204,7 @@ class CommandLineFlags {
   product(bool, UseSerialGC, false,                                         \
           "Use the serial garbage collector")                               \
                                                                             \
-  experimental(bool, UseG1GC, false,                                        \
+  product(bool, UseG1GC, false,                                             \
           "Use the Garbage-First garbage collector")                        \
                                                                             \
   product(bool, UseParallelGC, false,                                       \
