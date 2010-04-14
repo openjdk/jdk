965595d38b48f88a78184b5e8d18638574d9b1cb
Maurizio Cimadamore <mcimadamore@openjdk.org>
2010-04-14 12:23:29 +0100

    6939618: Revert 'simple' diamond implementation
    Backout changeset for 6840638
    
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/javac/code/Source.java
+++ langtools/src/share/classes/com/sun/tools/javac/code/Source.java
@@ -122,9 +122,6 @@ public enum Source {
     public boolean allowGenerics() {
         return compareTo(JDK1_5) >= 0;
     }
-    public boolean allowDiamond() {
-        return compareTo(JDK1_7) >= 0;
-    }
     public boolean allowEnums() {
         return compareTo(JDK1_5) >= 0;
     }
