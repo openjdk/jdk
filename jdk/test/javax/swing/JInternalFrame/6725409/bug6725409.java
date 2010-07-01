798039b168a210cde9bc404d01052b967f5523fa
Sergey Malenkov <malenkov@openjdk.java.net>
2010-07-01 18:47:56 +0400

    6959266: test javax/swing/JInternalFrame/6725409/bug6725409.java should be modified
    Reviewed-by: alexp

--- jdk/test/javax/swing/JInternalFrame/6725409/bug6725409.java
+++ jdk/test/javax/swing/JInternalFrame/6725409/bug6725409.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,7 +43,7 @@ public class bug6725409 {
                     new com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel());
         } catch(UnsupportedLookAndFeelException e) {
             System.out.println("The test is for Windows LaF only");
-            System.exit(0);
+            return;
         }
 
         final bug6725409 bug6725409 = new bug6725409();
