19f23653ef8bc6494dd10db57f48e9bf460b7817
Maurizio Cimadamore <mcimadamore@openjdk.java.net>
2010-08-16 14:58:10 +0100

    6369605: Unconstrained type variables fails to include bounds
    Unconstrained type-variables with recursive bounds are not inferred properly
    
    Reviewed-by: jjg

--- langtools/test/tools/javac/generics/inference/6369605/T6369605a.java
+++ langtools/test/tools/javac/generics/inference/6369605/T6369605a.java
@@ -21,18 +21,30 @@
  * questions.
  */
 
-// key: compiler.err.invalid.inferred.types
-// key: compiler.misc.inferred.do.not.conform.to.bounds
+/**
+ * @test
+ * @bug 6369605
+ * @summary Unconstrained type variables fails to include bounds
+ * @author mcimadamore
+ * @compile T6369605a.java
+ */
+import java.util.List;
 
-import java.util.*;
-
-class InvalidInferredTypes {
-
-    <T extends List<? super T>> T makeList() {
+class T6369605a {
+    static <T extends List<T>> T m1() {
         return null;
     }
 
-    public void test() {
-        List<? super String> l = makeList();
+    static <T extends List<U>, U extends List<T>> T m2() {
+        return null;
     }
+
+    static <T extends List<U>, U extends List<V>, V extends List<T>> T m3() {
+        return null;
+    }
+
+    List<?> l1 = m1();
+    List<?> l2 = m2();
+    List<?> l3 = m3();
 }
+
