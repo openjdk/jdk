a3319eba8ab8544c61e6e1e6881729fb8024eb04
Abhijit Saha <asaha@openjdk.org>
2010-08-17 22:52:50 -0700

    6977952: Test: Sync missing tests from hs16.3 to hs17.x
    Reviewed-by: wrockett

---
+++ hotspot/test/runtime/6626217/You_Have_Been_P0wned.java
@@ -0,0 +1,11 @@
+
+// I can cast any old thing I want to this type object:
+public class You_Have_Been_P0wned {
+  // Make a bunch of int-fields so I can peek & poke it
+  int _a;
+  int _b;
+  int _c;
+  int _d;
+
+}
+
