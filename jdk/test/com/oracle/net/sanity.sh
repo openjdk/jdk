2fdbbbae4e3d2dca9b786a506269ee14bde2f6f9
Alan Bateman <alanb@openjdk.org>
2010-09-03 13:11:54 +0100

    6965072: Need API to create SDP sockets
    Reviewed-by: michaelm

--- jdk/test/com/oracle/net/sanity.sh
+++ jdk/test/com/oracle/net/sanity.sh
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2009, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -22,28 +22,34 @@
 #
 
 # @test
-# @bug 4890703
-# @summary Unit test for Solaris SDP support
-# @build ProbeIB Sanity
+# @bug 6965072
+# @summary Unit test for SDP support
+# @build  Sanity
 # @run shell sanity.sh
 
-# Check we are on Solaris and that SDP is enabled
-OS=`uname -s`
-if [ "$OS" != "SunOS" ]; then
-    echo "This is a Solaris-only test"
-    exit 0
-fi
-
 IB_LINKS=ib.links
-IB_ADDRS=ib.addrs
 
-# Display IB partition link information
-# (requires Solaris 11, will fail on Solaris 10)
-/usr/sbin/dladm show-part -o LINK -p > ${IB_LINKS}
-if [ $? != 0 ]; then
-    echo "Unable to get IB parition link information"
-    exit 0
-fi
+OS=`uname -s`
+case "$OS" in
+    SunOS )
+        /usr/sbin/dladm show-part -o LINK -p > ${IB_LINKS}
+        if [ $? != 0 ]; then
+            echo "Unable to get InfiniBand parition link information"
+            exit 0
+        fi
+        ;;
+    Linux )
+        if [ ! -f /proc/net/sdp ]; then
+            echo "InfiniBand SDP module not installed"
+            exit 0
+        fi
+        egrep "^[ \t]+ib" /proc/net/dev|cut -d':' -f1|tr -d '\t ' > ${IB_LINKS}
+        ;; 
+    * )
+        echo "This test only runs on Solaris or Linux"
+        exit 0
+        ;;
+esac
 
 if [ -z "$TESTJAVA" ]; then
     JAVA=java
@@ -56,18 +62,5 @@ fi
 CLASSPATH=${TESTCLASSES}:${TESTSRC}
 export CLASSPATH
 
-# Probe for IP addresses plumbed to IB interfaces
-$JAVA -Djava.net.preferIPv4Stack=true ProbeIB ${IB_LINKS} > ${IB_ADDRS}
-
-# Create sdp.conf
-SDPCONF=sdp.conf
-rm ${SDPCONF}
-touch ${SDPCONF}
-cat ${IB_ADDRS} | while read ADDR
-do
-   echo "bind ${ADDR} *" > ${SDPCONF}
-   echo "connect ${ADDR} *" >> ${SDPCONF}
-done
-
-# Sanity check
-$JAVA -Djava.net.preferIPv4Stack=true -Dcom.sun.sdp.conf=${SDPCONF} -Dcom.sun.sdp.debug Sanity
+# Run sanity test (IPv4-only for now)
+$JAVA -Djava.net.preferIPv4Stack=true Sanity ${IB_LINKS}
