41cc1e2f4a94c476d05cd9bda6ef1e183013b33c
Alan Bateman <alanb@openjdk.org>
2010-10-04 14:17:36 +0100

    6989190: SO_SNDBUF/SO_RCVBUF limits should only be checked when setsockopt fails (sol)
    Reviewed-by: chegar, michaelm

--- jdk/src/solaris/native/java/net/net_util_md.c
+++ jdk/src/solaris/native/java/net/net_util_md.c
@@ -363,17 +363,20 @@ void ThrowUnknownHostExceptionWithGaiError(JNIEnv *env,
                                            const char* hostname,
                                            int gai_error)
 {
+    int size;
+    char *buf;
     const char *format = "%s: %s";
     const char *error_string =
         (gai_strerror_ptr == NULL) ? NULL : (*gai_strerror_ptr)(gai_error);
     if (error_string == NULL)
         error_string = "unknown error";
 
-    int size = strlen(format) + strlen(hostname) + strlen(error_string) + 2;
-    char *buf = (char *) malloc(size);
+    size = strlen(format) + strlen(hostname) + strlen(error_string) + 2;
+    buf = (char *) malloc(size);
     if (buf) {
+        jstring s;
         sprintf(buf, format, hostname, error_string);
-        jstring s = JNU_NewStringPlatform(env, buf);
+        s = JNU_NewStringPlatform(env, buf);
         if (s != NULL) {
             jobject x = JNU_NewObjectByName(env,
                                             "java/net/UnknownHostException",
@@ -1203,19 +1206,26 @@ NET_SetSockOpt(int fd, int level, int  opt, const void *arg,
     }
 
     /*
-     * SOL_SOCKET/{SO_SNDBUF,SO_RCVBUF} - On Solaris need to
-     * ensure that value is <= max_buf as otherwise we get
-     * an invalid argument.
+     * SOL_SOCKET/{SO_SNDBUF,SO_RCVBUF} - On Solaris we may need to clamp
+     * the value when it exceeds the system limit.
      */
 #ifdef __solaris__
     if (level == SOL_SOCKET) {
         if (opt == SO_SNDBUF || opt == SO_RCVBUF) {
             int sotype, arglen;
             int *bufsize, maxbuf;
+            int ret;
+
+            /* Attempt with the original size */
+            ret = setsockopt(fd, level, opt, arg, len);
+            if ((ret == 0) || (ret == -1 && errno != ENOBUFS))
+                return ret;
+
+            /* Exceeded system limit so clamp and retry */
 
             if (!init_max_buf) {
-                tcp_max_buf = getParam("/dev/tcp", "tcp_max_buf", 64*1024);
-                udp_max_buf = getParam("/dev/udp", "udp_max_buf", 64*1024);
+                tcp_max_buf = getParam("/dev/tcp", "tcp_max_buf", 1024*1024);
+                udp_max_buf = getParam("/dev/udp", "udp_max_buf", 2048*1024);
                 init_max_buf = 1;
             }
 
