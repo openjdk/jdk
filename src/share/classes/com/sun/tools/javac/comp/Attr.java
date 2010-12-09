9ae9c8ff2d3016f65df9f0262bf8e82cdfdce43b
mcimadamore <none@none>
2010-12-09 15:50:34 +0000

    6476118: compiler bug causes runtime ClassCastException for generics overloading Summary: compiler allows bridge methods to override unrelated method Reviewed-by: jjg

--- src/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -676,6 +676,7 @@ public class Attr extends JCTree.Visitor {
 
             // If we override any other methods, check that we do so properly.
             // JLS ???
+            chk.checkClashes(tree.pos(), env.enclClass.type, m);
             chk.checkOverride(tree, m);
 
             // Create a new environment with local scope
