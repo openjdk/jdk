f2bb972af477629013f279a355c1ef33923b8df0
Masayoshi Okutsu <okutsu@openjdk.org>
2010-12-27 14:13:52 +0900

    7003643: [Fmt-Me] MessageFormat.toPattern produces wrong quoted string and subformat modifiers 7008195: [Fmt-Me] Improve MessageFormat.applyPattern performance
    Reviewed-by: naoto, peytoia

--- jdk/test/java/util/PluggableLocale/NumberFormatProviderTest.sh
+++ jdk/test/java/util/PluggableLocale/NumberFormatProviderTest.sh
@@ -1,5 +1,5 @@
 # 
-# Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2007, 2010, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 # 
 # This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,6 @@
 #!/bin/sh
 #
 # @test
-# @bug 4052440
+# @bug 4052440 7003643
 # @summary NumberFormatProvider tests
 # @run shell ExecTest.sh foo NumberFormatProviderTest true
