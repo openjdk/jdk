a89cd04fd1a331f3e5eae83a766ebb431c65a0e9
Joe Darcy <darcy@openjdk.java.net>
2011-01-25 17:02:56 -0800

    7013420: Project Coin: remove general expression support from try-with-resources statement
    Reviewed-by: mcimadamore, jjg

--- langtools/src/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ langtools/src/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -142,7 +142,7 @@ public class JavacParser implements Parser {
      */
     boolean allowAnnotations;
 
-    /** Switch: should we recognize automatic resource management?
+    /** Switch: should we recognize try-with-resources?
      */
     boolean allowTWR;
 
@@ -2184,29 +2184,23 @@ public class JavacParser implements Parser {
         while (S.token() == SEMI) {
             // All but last of multiple declarators subsume a semicolon
             storeEnd(defs.elems.last(), S.endPos());
+            int semiColonPos = S.pos();
             S.nextToken();
+            if (S.token() == RPAREN) { // Illegal trailing semicolon
+                                       // after last resource
+                error(semiColonPos, "try.resource.trailing.semi");
+                break;
+            }
             defs.append(resource());
         }
         return defs.toList();
     }
 
-    /** Resource =
-     *    VariableModifiers Type VariableDeclaratorId = Expression
-     *  | Expression
+    /** Resource = VariableModifiersOpt Type VariableDeclaratorId = Expression
      */
     JCTree resource() {
-        int pos = S.pos();
-        if (S.token() == FINAL || S.token() == MONKEYS_AT) {
-            return variableDeclaratorRest(pos, optFinal(0), parseType(),
-                                          ident(), true, null);
-        } else {
-            JCExpression t = term(EXPR | TYPE);
-            if ((lastmode & TYPE) != 0 && S.token() == IDENTIFIER)
-                return variableDeclaratorRest(pos, toP(F.at(pos).Modifiers(Flags.FINAL)), t,
-                                              ident(), true, null);
-            else
-                return t;
-        }
+        return variableDeclaratorRest(S.pos(), optFinal(Flags.FINAL),
+                                      parseType(), ident(), true, null);
     }
 
     /** CompilationUnit = [ { "@" Annotation } PACKAGE Qualident ";"] {ImportDeclaration} {TypeDeclaration}
