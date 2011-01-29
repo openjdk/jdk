9b7612b045741fd8478f7e50090c035534796fd6
Joe Darcy <darcy@openjdk.org>
2011-01-28 17:09:49 -0800

    7015156: Remove JSR 308 changes from core libraries
    Reviewed-by: mduigou

--- jdk/src/share/classes/java/lang/SuppressWarnings.java
+++ jdk/src/share/classes/java/lang/SuppressWarnings.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -44,7 +44,7 @@ import static java.lang.annotation.ElementType.*;
  * @since 1.5
  * @author Josh Bloch
  */
-@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE, TYPE_PARAMETER})
+@Target({TYPE, FIELD, METHOD, PARAMETER, CONSTRUCTOR, LOCAL_VARIABLE})
 @Retention(RetentionPolicy.SOURCE)
 public @interface SuppressWarnings {
     /**
