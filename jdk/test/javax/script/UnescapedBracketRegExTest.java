932380b9e07d8ffdc6c85b63887b50261fd79778
Athijegannathan Sundararajan <sundar@openjdk.org>
2011-02-01 21:00:09 +0530

    7015908: 3 javax.script tests fail with openjdk build
    Reviewed-by: alanb

--- jdk/test/javax/script/UnescapedBracketRegExTest.java
+++ jdk/test/javax/script/UnescapedBracketRegExTest.java
@@ -34,6 +34,10 @@ public class UnescapedBracketRegExTest {
     public static void main(String[] args) throws ScriptException {
         ScriptEngineManager sem = new ScriptEngineManager();
         ScriptEngine engine = sem.getEngineByName("js");
+        if (engine == null) {
+            System.out.println("Warning: No js engine found; test vacuously passes.");
+            return;
+        }
         // the following throws exception
         engine.eval("var x = /[a-zA-Z+/=]/;");
     }
