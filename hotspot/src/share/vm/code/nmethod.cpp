dcfa5e385aa0386332764d33ac9c0b2534a9781d
Tom Rodriguez <never@openjdk.org>
2011-03-04 20:01:48 -0800

    7024866: #  assert(limit == NULL || limit <= nm->code_end()) failed: in bounds
    Reviewed-by: kvn, iveresov

--- hotspot/src/share/vm/code/nmethod.cpp
+++ hotspot/src/share/vm/code/nmethod.cpp
@@ -1881,7 +1881,7 @@ void nmethod::preserve_callee_argument_oops(frame fr, const RegisterMap *reg_map
 
 
 oop nmethod::embeddedOop_at(u_char* p) {
-  RelocIterator iter(this, p, p + oopSize);
+  RelocIterator iter(this, p, p + 1);
   while (iter.next())
     if (iter.type() == relocInfo::oop_type) {
       return iter.oop_reloc()->oop_value();
