680370af5b0b872b27f5311b9ea529e260608c80
Weijun Wang <weijun@openjdk.org>
2011-03-28 18:04:17 +0800

    7031536: test/sun/security/krb5/auto/HttpNegotiateServer.java should not use static ports
    Reviewed-by: xuelei

--- jdk/test/sun/security/krb5/DnsFallback.java
+++ jdk/test/sun/security/krb5/DnsFallback.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
  * @test
  * @bug 6673164
  * @bug 6552334
+ * @run main/othervm DnsFallback
  * @summary fix dns_fallback parse error, and use dns by default
  */
 
