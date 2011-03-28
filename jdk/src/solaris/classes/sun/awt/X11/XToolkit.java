100e409a4adb2889141e5dc826059f419cc73cb2
Andrei Dmitriev <dav@openjdk.java.net>
2011-03-28 15:25:24 +0400

    7023011: Toolkit.getPrintJob(Frame,String,Properties) throws HE instead of specified NPE
    Reviewed-by: dcherepanov, art

--- jdk/src/solaris/classes/sun/awt/X11/XToolkit.java
+++ jdk/src/solaris/classes/sun/awt/X11/XToolkit.java
@@ -1222,8 +1222,8 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
     public PrintJob getPrintJob(final Frame frame, final String doctitle,
                                 final Properties props) {
 
-        if (GraphicsEnvironment.isHeadless()) {
-            throw new IllegalArgumentException();
+        if (frame == null) {
+            throw new NullPointerException("frame must not be null");
         }
 
         PrintJob2D printJob = new PrintJob2D(frame, doctitle, props);
@@ -1236,11 +1236,10 @@ public final class XToolkit extends UNIXToolkit implements Runnable {
 
     public PrintJob getPrintJob(final Frame frame, final String doctitle,
                 final JobAttributes jobAttributes,
-                final PageAttributes pageAttributes) {
-
-
-        if (GraphicsEnvironment.isHeadless()) {
-            throw new IllegalArgumentException();
+                final PageAttributes pageAttributes)
+    {
+        if (frame == null) {
+            throw new NullPointerException("frame must not be null");
         }
 
         PrintJob2D printJob = new PrintJob2D(frame, doctitle,
