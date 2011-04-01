cf3120dddc9728fabfa00d1e1ec8365ec07be04a
Andrew John Hughes <andrew@openjdk.org>
2011-04-01 15:15:37 -0700

    7010849: 5/5 Extraneous javac source/target options when building sa-jdi
    Make code changes necessary to get rid of the '-source 1.4 -target 1.4' options.
    
    Reviewed-by: dholmes, dcubed

--- hotspot/agent/src/share/classes/sun/jvm/hotspot/HelloWorld.java
+++ hotspot/agent/src/share/classes/sun/jvm/hotspot/HelloWorld.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2001, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -55,7 +55,7 @@ public class HelloWorld {
     synchronized(lock) {
       if (useMethodInvoke) {
         try {
-          Method method = HelloWorld.class.getMethod("e", null);
+          Method method = HelloWorld.class.getMethod("e");
           Integer result = (Integer) method.invoke(null, new Object[0]);
           return result.intValue();
         }
