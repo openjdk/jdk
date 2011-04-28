50ceca76455de916caff4ef09ffac0225320619a
Max Weijun Wang <weijun@openjdk.java.net>
2011-04-28 20:34:53 +0800

    7037201: regression: invalid signed jar file not detected
    Reviewed-by: mullan

--- jdk/test/java/util/jar/JarInputStream/TestIndexedJarWithBadSignature.java
+++ jdk/test/java/util/jar/JarInputStream/TestIndexedJarWithBadSignature.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,7 +37,7 @@ public class TestIndexedJarWithBadSignature {
 
     public static void main(String...args) throws Throwable {
         try (JarInputStream jis = new JarInputStream(
-                 new FileInputStream(System.getProperty("tst.src", ".") +
+                 new FileInputStream(System.getProperty("test.src", ".") +
                                      System.getProperty("file.separator") +
                                      "BadSignedJar.jar")))
         {
