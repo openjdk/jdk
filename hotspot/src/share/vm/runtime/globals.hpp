782c74cddc333435bd6dadcc0797e5376df5850d
Bengt Rutisson <brutisso@openjdk.java.net>
2011-04-29 09:11:03 +0200

    7040068: CMS: Possibly unsafe initialization of BlockOffsetArrayUseUnallocatedBlock
    BlockOffsetArrayUseUnallocatedBlock was intended to be turned off as part of BUG 6948538 but a code line in collectorPolicy.cpp actually kept it turned on.
    
    Reviewed-by: jwilhelm, ysr

--- hotspot/src/share/vm/runtime/globals.hpp
+++ hotspot/src/share/vm/runtime/globals.hpp
@@ -1824,7 +1824,7 @@ class CommandLineFlags {
   develop(bool, VerifyBlockOffsetArray, false,                              \
           "Do (expensive!) block offset array verification")                \
                                                                             \
-  product(bool, BlockOffsetArrayUseUnallocatedBlock, false,                 \
+  diagnostic(bool, BlockOffsetArrayUseUnallocatedBlock, false,              \
           "Maintain _unallocated_block in BlockOffsetArray"                 \
           " (currently applicable only to CMS collector)")                  \
                                                                             \
