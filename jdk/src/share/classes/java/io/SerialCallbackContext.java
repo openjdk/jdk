d4af21159476630ef3bb6f917c817c2eec2d9a2e
David Katleman <katleman@openjdk.java.net>
2011-05-25 13:32:36 -0700

    7044486: open jdk repos have files with incorrect copyright headers, which can end up in src bundles
    Reviewed-by: ohair, trims

--- jdk/src/share/classes/java/io/SerialCallbackContext.java
+++ jdk/src/share/classes/java/io/SerialCallbackContext.java
@@ -1,7 +1,5 @@
   /*
-   * %W% %E%
-   *
-   * Copyright (c) 2006, 2010  Oracle and/or its affiliates. All rights reserved.
+   * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
    * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
    */
 
