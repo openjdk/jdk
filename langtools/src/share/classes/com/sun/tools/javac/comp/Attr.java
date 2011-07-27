02802c9449b7e0bf6496423f4c2f87de3cc44b7c
Maurizio Cimadamore <mcimadamore@openjdk.org>
2011-07-27 19:01:08 +0100

    7046778: Project Coin: problem with diamond and member inner classes
    Diamond inference generates spurious error messages when target type is a member inner class
    
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/javac/comp/Attr.java
+++ langtools/src/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -1837,7 +1837,7 @@ public class Attr extends JCTree.Visitor {
         try {
             constructor = rs.resolveDiamond(tree.pos(),
                     localEnv,
-                    clazztype.tsym.type,
+                    clazztype,
                     argtypes,
                     typeargtypes);
         } finally {
@@ -2872,8 +2872,10 @@ public class Attr extends JCTree.Visitor {
 
         if (clazztype.tag == CLASS) {
             List<Type> formals = clazztype.tsym.type.getTypeArguments();
+            if (actuals.isEmpty()) //diamond
+                actuals = formals;
 
-            if (actuals.length() == formals.length() || actuals.length() == 0) {
+            if (actuals.length() == formals.length()) {
                 List<Type> a = actuals;
                 List<Type> f = formals;
                 while (a.nonEmpty()) {
