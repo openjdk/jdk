c4933101c199c4d4755118cc2fe0acb20e5fe591
Xue-Lei Andrew Fan <xuelei@openjdk.java.net>
2011-07-29 02:50:58 -0700

    7068662: Reserve and restore the default locale
    Reviewed-by: alanb, weijun

--- jdk/test/java/util/ResourceBundle/Test4318520.java
+++ jdk/test/java/util/ResourceBundle/Test4318520.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,8 +36,14 @@ import java.util.Locale;
 public class Test4318520 {
 
     public static void main(String[] args) {
-        test(Locale.GERMAN);
-        test(Locale.ENGLISH);
+        Locale reservedLocale = Locale.getDefault();
+        try {
+            test(Locale.GERMAN);
+            test(Locale.ENGLISH);
+        } finally {
+            // restore the reserved locale
+            Locale.setDefault(reservedLocale);
+        }
     }
 
     private static void test(Locale locale) {
