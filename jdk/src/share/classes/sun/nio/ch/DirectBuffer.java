c26005b40ee5b2187301308984ffcf32d93062ce
Sean Coffey <coffeys@openjdk.org>
2011-08-11 12:40:24 +0100

    7047325: Internal API to improve management of direct buffers
    Reviewed-by: alanb, mduigou

--- jdk/src/share/classes/sun/nio/ch/DirectBuffer.java
+++ jdk/src/share/classes/sun/nio/ch/DirectBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2003, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,7 @@ public interface DirectBuffer {
 
     public long address();
 
-    public Object viewedBuffer();
+    public Object attachment();
 
     public Cleaner cleaner();
 
