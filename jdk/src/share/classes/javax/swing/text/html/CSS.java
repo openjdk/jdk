dcceb86a30ef82a5a23afd1ad34c6ee5169417dd
Pavel Porvatov <rupashka@openjdk.java.net>
2011-08-29 16:25:04 +0400

    7030332: Default borders in tables looks incorrect JEditorPane
    Reviewed-by: peterz

--- jdk/src/share/classes/javax/swing/text/html/CSS.java
+++ jdk/src/share/classes/javax/swing/text/html/CSS.java
@@ -806,15 +806,11 @@ public class CSS implements Serializable {
             // translate border width into the cells, if it has non-zero value.
             AttributeSet tableAttr = elem.getParentElement().
                                      getParentElement().getAttributes();
-            int borderWidth;
-            try {
-                borderWidth = Integer.parseInt(
-                    (String) tableAttr.getAttribute(HTML.Attribute.BORDER));
-            } catch (NumberFormatException e) {
-                borderWidth = 0;
-            }
+
+            int borderWidth = getTableBorder(tableAttr);
             if (borderWidth > 0) {
-                translateAttribute(HTML.Attribute.BORDER, tableAttr, cssAttrSet);
+                // If table contains the BORDER attribute cells should have border width equals 1
+                translateAttribute(HTML.Attribute.BORDER, "1", cssAttrSet);
             }
             String pad = (String)tableAttr.getAttribute(HTML.Attribute.CELLPADDING);
             if (pad != null) {
@@ -848,6 +844,21 @@ public class CSS implements Serializable {
             }
         }
         return cssAttrSet;
+    }
+
+    private static int getTableBorder(AttributeSet tableAttr) {
+        String borderValue = (String) tableAttr.getAttribute(HTML.Attribute.BORDER);
+
+        if (borderValue == HTML.NULL_ATTRIBUTE_VALUE || "".equals(borderValue)) {
+            // Some browsers accept <TABLE BORDER> and <TABLE BORDER=""> with the same semantics as BORDER=1
+            return 1;
+        }
+
+        try {
+            return Integer.parseInt(borderValue);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 
     private static final Hashtable<String, Attribute> attributeMap = new Hashtable<String, Attribute>();
@@ -1400,17 +1411,20 @@ public class CSS implements Serializable {
                         }
                     }
                 } else {
-
-                    /*
-                     * The html size attribute has a mapping in the CSS world only
-                     * if it is par of a font or base font tag.
-                     */
-
                     if (key == HTML.Attribute.SIZE && !isHTMLFontTag(tag)) {
-                        continue;
-                    }
+                        /*
+                         * The html size attribute has a mapping in the CSS world only
+                         * if it is par of a font or base font tag.
+                         */
+                    } else if (tag == HTML.Tag.TABLE && key == HTML.Attribute.BORDER) {
+                        int borderWidth = getTableBorder(htmlAttrSet);
 
-                    translateAttribute(key, htmlAttrSet, cssAttrSet);
+                        if (borderWidth > 0) {
+                            translateAttribute(HTML.Attribute.BORDER, Integer.toString(borderWidth), cssAttrSet);
+                        }
+                    } else {
+                        translateAttribute(key, (String) htmlAttrSet.getAttribute(key), cssAttrSet);
+                    }
                 }
             } else if (name instanceof CSS.Attribute) {
                 cssAttrSet.addAttribute(name, htmlAttrSet.getAttribute(name));
@@ -1419,15 +1433,13 @@ public class CSS implements Serializable {
     }
 
     private void translateAttribute(HTML.Attribute key,
-                                           AttributeSet htmlAttrSet,
+                                           String htmlAttrValue,
                                            MutableAttributeSet cssAttrSet) {
         /*
          * In the case of all remaining HTML.Attribute's they
          * map to 1 or more CCS.Attribute.
          */
         CSS.Attribute[] cssAttrList = getCssAttribute(key);
-
-        String htmlAttrValue = (String)htmlAttrSet.getAttribute(key);
 
         if (cssAttrList == null || htmlAttrValue == null) {
             return;
