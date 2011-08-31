05b60b3662da8a4c71ac942508248aa09502e039
Christian Thalinger <twisti@openjdk.org>
2011-08-31 01:40:45 -0700

    7078382: JSR 292: don't count method handle adapters against inlining budgets
    Reviewed-by: kvn, never

--- hotspot/src/share/vm/ci/ciMethod.hpp
+++ hotspot/src/share/vm/ci/ciMethod.hpp
@@ -157,6 +157,9 @@ class ciMethod : public ciObject {
   int interpreter_invocation_count() const       { check_is_loaded(); return _interpreter_invocation_count; }
   int interpreter_throwout_count() const         { check_is_loaded(); return _interpreter_throwout_count; }
 
+  // Code size for inlining decisions.
+  int code_size_for_inlining();
+
   int comp_level();
   int highest_osr_comp_level();
 
