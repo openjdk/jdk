b37a843c75b0d6a9043fd7ca99010da92400608a
Roland Westrelin <roland@openjdk.java.net>
2011-09-07 14:15:07 +0200

    7085012: ARM: com/sun/jdi/PopSynchronousTest.java still fails
    InterpreterRuntime::popframe_move_outgoing_args() is required for the ARM interpreter.
    
    Reviewed-by: kvn, twisti

--- hotspot/src/share/vm/interpreter/interpreterRuntime.hpp
+++ hotspot/src/share/vm/interpreter/interpreterRuntime.hpp
@@ -141,8 +141,8 @@ class InterpreterRuntime: AllStatic {
                                         methodOopDesc* method,
                                         intptr_t* from, intptr_t* to);
 
-#if defined(IA32) || defined(AMD64)
-  // Popframe support (only needed on x86 and AMD64)
+#if defined(IA32) || defined(AMD64) || defined(ARM)
+  // Popframe support (only needed on x86, AMD64 and ARM)
   static void popframe_move_outgoing_args(JavaThread* thread, void* src_address, void* dest_address);
 #endif
 
