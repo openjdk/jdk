3cd849d50a75a32fa9ed4dd8db889afdf72406dc
Michael McMahon <michaelm@openjdk.java.net>
2011-09-19 15:14:17 +0100

    7091369: DatagramSocket/Limit.java failing on 8 and 7u2
    Reviewed-by: chegar, alanb

--- jdk/src/windows/classes/java/net/TwoStacksPlainDatagramSocketImpl.java
+++ jdk/src/windows/classes/java/net/TwoStacksPlainDatagramSocketImpl.java
@@ -68,7 +68,12 @@ class TwoStacksPlainDatagramSocketImpl extends AbstractPlainDatagramSocketImpl
 
     protected synchronized void create() throws SocketException {
         fd1 = new FileDescriptor();
-        super.create();
+        try {
+            super.create();
+        } catch (IOException e) {
+            fd1 = null;
+            throw e;
+        }
     }
 
     protected synchronized void bind(int lport, InetAddress laddr)
