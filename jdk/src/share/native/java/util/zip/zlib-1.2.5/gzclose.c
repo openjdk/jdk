21852d76f6d6aabe517d09eb1f9c16e62c55ee07
Xueming Shen <sherman@openjdk.java.net>
2011-11-29 11:39:59 -0800

    7110149: Update the JDK8 bundled zlib library to the latest version 1.2.5
    Updated to zlib-1.2.5
    
    Reviewed-by: alanb

--- jdk/src/share/native/java/util/zip/zlib-1.2.5/gzclose.c
+++ jdk/src/share/native/java/util/zip/zlib-1.2.5/gzclose.c
@@ -22,14 +22,28 @@
  * questions.
  */
 
-/* inffast.h -- header to use inffast.c
- * Copyright (C) 1995-2003 Mark Adler
+/* gzclose.c -- zlib gzclose() function
+ * Copyright (C) 2004, 2010 Mark Adler
  * For conditions of distribution and use, see copyright notice in zlib.h
  */
 
-/* WARNING: this file should *not* be used by applications. It is
-   part of the implementation of the compression library and is
-   subject to change. Applications should only use zlib.h.
- */
+#include "gzguts.h"
 
-void inflate_fast OF((z_streamp strm, unsigned start));
+/* gzclose() is in a separate file so that it is linked in only if it is used.
+   That way the other gzclose functions can be used instead to avoid linking in
+   unneeded compression or decompression routines. */
+int ZEXPORT gzclose(file)
+    gzFile file;
+{
+#ifndef NO_GZCOMPRESS
+    gz_statep state;
+
+    if (file == NULL)
+        return Z_STREAM_ERROR;
+    state = (gz_statep)file;
+
+    return state->mode == GZ_READ ? gzclose_r(file) : gzclose_w(file);
+#else
+    return gzclose_r(file);
+#endif
+}
