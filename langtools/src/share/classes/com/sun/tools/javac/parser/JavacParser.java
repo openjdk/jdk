51677ae75b5adc27d18ea33d02cdbc8024c955a4
Maurizio Cimadamore <mcimadamore@openjdk.java.net>
2011-12-11 17:48:25 +0000

    7120266: javac fails to compile hotspot code
    Parser changes for method references cause bad intercation with method call syntax
    
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ langtools/src/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -1143,49 +1143,49 @@ public class JavacParser implements Parser {
                         // typeArgs saved for next loop iteration.
                         t = toP(F.at(pos).Select(t, ident()));
                         break;
-                    case LT:
-                        if ((mode & (TYPE | NOPARAMS)) == 0) {
-                            //could be an unbound method reference whose qualifier
-                            //is a generic type i.e. A<S>#m
-                            mode = EXPR | TYPE;
-                            JCTree.Tag op = JCTree.Tag.LT;
-                            int pos1 = token.pos;
-                            nextToken();
-                            mode |= EXPR | TYPE | TYPEARG;
-                            JCExpression t1 = term3();
-                            if ((mode & TYPE) != 0 &&
-                                (token.kind == COMMA || token.kind == GT)) {
-                                mode = TYPE;
-                                ListBuffer<JCExpression> args = new ListBuffer<JCExpression>();
-                                args.append(t1);
-                                while (token.kind == COMMA) {
-                                    nextToken();
-                                    args.append(typeArgument());
-                                }
-                                accept(GT);
-                                t = toP(F.at(pos1).TypeApply(t, args.toList()));
-                                checkGenerics();
-                                while (token.kind == DOT) {
-                                    nextToken();
-                                    mode = TYPE;
-                                    t = toP(F.at(token.pos).Select(t, ident()));
-                                    t = typeArgumentsOpt(t);
-                                }
-                                if (token.kind != HASH) {
-                                    //method reference expected here
-                                    t = illegal();
-                                }
-                                mode = EXPR;
-                                break;
-                            } else if ((mode & EXPR) != 0) {
-                                //rollback - it was a binary expression
-                                mode = EXPR;
-                                JCExpression e = term2Rest(t1, TreeInfo.shiftPrec);
-                                t = F.at(pos1).Binary(op, t, e);
-                                t = termRest(term1Rest(term2Rest(t, TreeInfo.orPrec)));
-                            }
-                        }
-                        break loop;
+//                    case LT:
+//                        if ((mode & (TYPE | NOPARAMS)) == 0) {
+//                            //could be an unbound method reference whose qualifier
+//                            //is a generic type i.e. A<S>#m
+//                            mode = EXPR | TYPE;
+//                            JCTree.Tag op = JCTree.Tag.LT;
+//                            int pos1 = token.pos;
+//                            nextToken();
+//                            mode |= EXPR | TYPE | TYPEARG;
+//                            JCExpression t1 = term3();
+//                            if ((mode & TYPE) != 0 &&
+//                                (token.kind == COMMA || token.kind == GT)) {
+//                                mode = TYPE;
+//                                ListBuffer<JCExpression> args = new ListBuffer<JCExpression>();
+//                                args.append(t1);
+//                                while (token.kind == COMMA) {
+//                                    nextToken();
+//                                    args.append(typeArgument());
+//                                }
+//                                accept(GT);
+//                                t = toP(F.at(pos1).TypeApply(t, args.toList()));
+//                                checkGenerics();
+//                                while (token.kind == DOT) {
+//                                    nextToken();
+//                                    mode = TYPE;
+//                                    t = toP(F.at(token.pos).Select(t, ident()));
+//                                    t = typeArgumentsOpt(t);
+//                                }
+//                                if (token.kind != HASH) {
+//                                    //method reference expected here
+//                                    t = illegal();
+//                                }
+//                                mode = EXPR;
+//                                break;
+//                            } else if ((mode & EXPR) != 0) {
+//                                //rollback - it was a binary expression
+//                                mode = EXPR;
+//                                JCExpression e = term2Rest(t1, TreeInfo.shiftPrec);
+//                                t = F.at(pos1).Binary(op, t, e);
+//                                t = termRest(term1Rest(term2Rest(t, TreeInfo.orPrec)));
+//                            }
+//                        }
+//                        break loop;
                     default:
                         break loop;
                     }
