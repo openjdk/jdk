56e931bd3a9ecbb7cc7e4eb80dd8e17a8c1a96fb
Joe Darcy <darcy@openjdk.java.net>
2012-02-07 17:39:13 -0800

    7143629: JDK jar/zip code should use unsigned library support
    Reviewed-by: sherman

--- jdk/src/share/classes/java/util/jar/JarOutputStream.java
+++ jdk/src/share/classes/java/util/jar/JarOutputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2006, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -135,7 +135,7 @@ class JarOutputStream extends ZipOutputStream {
      * The bytes are assumed to be in Intel (little-endian) byte order.
      */
     private static int get16(byte[] b, int off) {
-        return (b[off] & 0xff) | ((b[off+1] & 0xff) << 8);
+        return Byte.toUnsignedInt(b[off]) | ( Byte.toUnsignedInt(b[off+1]) << 8);
     }
 
     /*
