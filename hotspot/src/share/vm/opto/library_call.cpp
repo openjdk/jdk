7258e87f0bb7da4145dc08107658ccb914796d77
Vladimir Kozlov <kvn@openjdk.java.net>
2012-02-07 16:33:43 -0800

    7143491: G1 C2 CTW: assert(p2x->outcnt() == 2) failed: expects 2 users: Xor and URShift nodes
    Adjust the assert and code in eliminate_card_mark() method for case when stored value is NULL.
    
    Reviewed-by: iveresov, never

--- hotspot/src/share/vm/opto/library_call.cpp
+++ hotspot/src/share/vm/opto/library_call.cpp
@@ -2678,7 +2678,13 @@ bool LibraryCallKit::inline_unsafe_CAS(BasicType type) {
     cas = _gvn.transform(new (C, 5) CompareAndSwapLNode(control(), mem, adr, newval, oldval));
     break;
   case T_OBJECT:
-     // reference stores need a store barrier.
+    // Transformation of a value which could be NULL pointer (CastPP #NULL)
+    // could be delayed during Parse (for example, in adjust_map_after_if()).
+    // Execute transformation here to avoid barrier generation in such case.
+    if (_gvn.type(newval) == TypePtr::NULL_PTR)
+      newval = _gvn.makecon(TypePtr::NULL_PTR);
+
+    // Reference stores need a store barrier.
     // (They don't if CAS fails, but it isn't worth checking.)
     pre_barrier(true /* do_load*/,
                 control(), base, adr, alias_idx, newval, value_type->make_oopptr(),
