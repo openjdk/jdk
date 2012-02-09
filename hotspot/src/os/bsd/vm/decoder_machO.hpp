90f83928e2cf3079e92898f2d6599aec4738ac00
Zhengyu Gu <zgu@openjdk.java.net>
2012-02-09 10:16:26 -0500

    7141259: Native stack is missing in hs_err
    Code cleanup and creating a private decoder for error handler, since it can be triggered from in signal handler, where no lock can be taken
    
    Reviewed-by: dholmes, kamg, acorn, coleenp

--- hotspot/src/os/bsd/vm/decoder_machO.hpp
+++ hotspot/src/os/bsd/vm/decoder_machO.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,8 +29,9 @@
 
 #include "utilities/decoder.hpp"
 
-// Just a placehold for now
-class MachODecoder: public NullDecoder {
+// Just a placehold for now, a real implementation should derive
+// from AbstractDecoder
+class MachODecoder : public NullDecoder {
 public:
   MachODecoder() { }
   ~MachODecoder() { }
