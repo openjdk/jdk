cfdca5342c7209e2ab85b720629baa6a821a5af2
Coleen Phillimore <coleenp@openjdk.org>
2012-02-13 12:30:47 -0500

    7059899: Stack overflows in Java code cause 64-bit JVMs to exit due to SIGSEGV
    Increase StackShadowPages to accomodate the JDK changes to increase buffer size in socketWrite
    
    Reviewed-by: acorn, phh

--- hotspot/src/cpu/x86/vm/globals_x86.hpp
+++ hotspot/src/cpu/x86/vm/globals_x86.hpp
@@ -60,7 +60,7 @@ define_pd_global(intx, StackRedPages, 1);
 #ifdef AMD64
 // Very large C++ stack frames using solaris-amd64 optimized builds
 // due to lack of optimization caused by C++ compiler bugs
-define_pd_global(intx, StackShadowPages, SOLARIS_ONLY(20) NOT_SOLARIS(6) DEBUG_ONLY(+2));
+define_pd_global(intx, StackShadowPages, NOT_WIN64(20) WIN64_ONLY(6) DEBUG_ONLY(+2));
 #else
 define_pd_global(intx, StackShadowPages, 3 DEBUG_ONLY(+5));
 #endif // AMD64
