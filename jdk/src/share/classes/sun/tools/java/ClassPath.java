468b58467e071562650d1748e3295a3e0fb37ede
Deven You <youdwei@openjdk.java.net>
2012-04-12 15:04:39 +0800

    6610897: New constructor in sun.tools.java.ClassPath builds a path using File.separator instead of File.pathSeparator
    Reviewed-by: alanb

--- jdk/src/share/classes/sun/tools/java/ClassPath.java
+++ jdk/src/share/classes/sun/tools/java/ClassPath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -141,7 +141,7 @@ class ClassPath {
         } else {
             StringBuilder sb = new StringBuilder(patharray[0]);
             for (int i = 1; i < patharray.length; i++) {
-                sb.append(File.separator);
+                sb.append(File.pathSeparatorChar);
                 sb.append(patharray[i]);
             }
             this.pathstr = sb.toString();
