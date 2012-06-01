826e3d559f5642829ae4755baae3af7bfcbe08ed
Mike Duigou <mduigou@openjdk.java.net>
2012-06-01 00:05:21 -0700

    7173432: Handle null key at HashMap resize
    If the key to be inserted into a HashMap is null and the table needs to be resized as part of the insertion then addEntry will try to recalculate the hash of a null key. This will fail with an NPE.
    
    Reviewed-by: darcy

--- jdk/src/share/classes/java/util/HashMap.java
+++ jdk/src/share/classes/java/util/HashMap.java
@@ -784,7 +784,7 @@ public class HashMap<K,V>
     void addEntry(int hash, K key, V value, int bucketIndex) {
         if ((size >= threshold) && (null != table[bucketIndex])) {
             resize(2 * table.length);
-            hash = hash(key);
+            hash = (null != key) ? hash(key) : 0;
             bucketIndex = indexFor(hash, table.length);
         }
 
