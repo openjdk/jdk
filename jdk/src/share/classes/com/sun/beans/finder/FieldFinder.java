9ec1d58234403fc740370f4142d029f8e0818b6c
Sergey Malenkov <malenkov@openjdk.org>
2012-06-15 21:01:55 +0400

    7162473: ConstructorFinder/FieldFinder/MethodFinder gives access to restricted classes
    Reviewed-by: art, ahgross

--- jdk/src/share/classes/com/sun/beans/finder/FieldFinder.java
+++ jdk/src/share/classes/com/sun/beans/finder/FieldFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,8 @@ package com.sun.beans.finder;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+
+import static sun.reflect.misc.ReflectUtil.isPackageAccessible;
 
 /**
  * This utility class provides {@code static} methods
@@ -56,7 +58,8 @@ public final class FieldFinder {
         if (!Modifier.isPublic(field.getModifiers())) {
             throw new NoSuchFieldException("Field '" + name + "' is not public");
         }
-        if (!Modifier.isPublic(field.getDeclaringClass().getModifiers())) {
+        type = field.getDeclaringClass();
+        if (!Modifier.isPublic(type.getModifiers()) || !isPackageAccessible(type)) {
             throw new NoSuchFieldException("Field '" + name + "' is not accessible");
         }
         return field;
