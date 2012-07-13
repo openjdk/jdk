29739d16eeb75dea3b62821b7c3db6be3a44d1bf
Kurchi Subhra Hazra <khazra@openjdk.org>
2012-07-13 16:02:26 -0700

    7160252: (prefs) NodeAddedEvent was not delivered when new node add when new Node
    Change native code to convey to Java code whether a new node was added
    
    Reviewed-by: alanb, chegar

--- jdk/src/macosx/classes/java/util/prefs/MacOSXPreferencesFile.java
+++ jdk/src/macosx/classes/java/util/prefs/MacOSXPreferencesFile.java
@@ -360,11 +360,11 @@ class MacOSXPreferencesFile {
         }
     }
 
-    void addChildToNode(String path, String child)
+    boolean addChildToNode(String path, String child)
     {
         synchronized(MacOSXPreferencesFile.class) {
             markChanged();
-            addChildToNode(path, child+"/", appName, user, host);
+            return addChildToNode(path, child+"/", appName, user, host);
         }
     }
 
@@ -433,7 +433,7 @@ class MacOSXPreferencesFile {
         addNode(String path, String name, long user, long host);
     private static final native void
         removeNode(String path, String name, long user, long host);
-    private static final native void
+    private static final native boolean
         addChildToNode(String path, String child,
                        String name, long user, long host);
     private static final native void
