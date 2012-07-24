b9fa2e4fb93023ae2faaaced7212eee67bedb4e7
Xueming Shen <sherman@openjdk.org>
2012-07-24 12:17:39 -0700

    6653797: Reimplement JDK charset repository charsets.jar
    Migrated all jis based charsets to new implementation
    
    Reviewed-by: okutsu

--- jdk/test/sun/nio/cs/OLD/JIS_X_0212_OLD.java
+++ jdk/test/sun/nio/cs/OLD/JIS_X_0212_OLD.java
@@ -26,22 +26,21 @@
 /*
  */
 
-package sun.nio.cs.ext;
 
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 
-public class JIS_X_0208
+public class JIS_X_0212_OLD
     extends Charset
 {
 
-    public JIS_X_0208() {
-        super("x-JIS0208", ExtendedCharsets.aliasesFor("x-JIS0208"));
+    public JIS_X_0212_OLD() {
+        super("JIS_X0212-1990_OLD", null);
     }
 
     public boolean contains(Charset cs) {
-        return (cs instanceof JIS_X_0208);
+        return (cs instanceof JIS_X_0212_OLD);
     }
 
     public CharsetDecoder newDecoder() {
@@ -49,10 +48,10 @@ public class JIS_X_0208
     }
 
     public CharsetEncoder newEncoder() {
-        return new JIS_X_0208_Encoder(this);
+        return new JIS_X_0212_Encoder(this);
     }
 
-    private static class Decoder extends JIS_X_0208_Decoder {
+    private static class Decoder extends JIS_X_0212_Decoder {
         protected char decodeSingle(int b) {
             return DoubleByteDecoder.REPLACE_CHAR;
         }
