0250f893469e54156511bf1ebd475c2d2fe2b139
Zhengyu Gu <zgu@openjdk.java.net>
2012-07-30 10:25:52 -0400

    7186778: MachO decoder implementation for MacOSX
    Implementation of decoder for Apple's MacOSX. The implementation is based on the patch provided by Kevin Walls.
    
    Reviewed-by: coleenp, kamg, kevinw

--- hotspot/src/os/bsd/vm/decoder_machO.hpp
+++ hotspot/src/os/bsd/vm/decoder_machO.hpp
@@ -31,10 +31,25 @@
 
 // Just a placehold for now, a real implementation should derive
 // from AbstractDecoder
-class MachODecoder : public NullDecoder {
-public:
+class MachODecoder : public AbstractDecoder {
+ public:
   MachODecoder() { }
   ~MachODecoder() { }
+  virtual bool can_decode_C_frame_in_vm() const {
+    return true;
+  }
+  virtual bool demangle(const char* symbol, char* buf, int buflen);
+  virtual bool decode(address pc, char* buf, int buflen, int* offset,
+                      const void* base);
+  virtual bool decode(address pc, char* buf, int buflen, int* offset,
+                      const char* module_path = NULL) {
+    ShouldNotReachHere();
+    return false;
+  }
+
+ private:
+  void * mach_find_command(struct mach_header_64 * mach_base, uint32_t command_wanted);
+  char * mach_find_in_stringtable(char *strtab, uint32_t tablesize, int strx_wanted);
 };
 
 #endif
