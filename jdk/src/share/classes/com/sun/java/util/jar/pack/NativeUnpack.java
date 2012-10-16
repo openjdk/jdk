3d51231f1a5c4c45e4511a6f167979413a4e0056
Kumar Srinivasan <ksrini@openjdk.java.net>
2012-10-16 12:38:29 -0700

    7186946: Refine unpacker resource usage
    Reviewed-by: jrose, jjh, mschoene

--- jdk/src/share/classes/com/sun/java/util/jar/pack/NativeUnpack.java
+++ jdk/src/share/classes/com/sun/java/util/jar/pack/NativeUnpack.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -107,6 +107,10 @@ class NativeUnpack {
     static private Object currentInstance() {
         UnpackerImpl p200 = (UnpackerImpl) Utils.getTLGlobals();
         return (p200 == null)? null: p200._nunp;
+    }
+
+    private synchronized long getUnpackerPtr() {
+        return unpackerPtr;
     }
 
     // Callback from the unpacker engine to get more data.
