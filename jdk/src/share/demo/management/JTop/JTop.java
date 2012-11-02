430592a30ee623ebee5d4593942263e2a61ed6c9
Alan Bateman <alanb@openjdk.org>
2012-11-02 15:50:11 +0000

    7197491: update copyright year to match last edit in jdk8 jdk repository
    Reviewed-by: chegar, ksrini

--- jdk/src/share/demo/management/JTop/JTop.java
+++ jdk/src/share/demo/management/JTop/JTop.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2006, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2011, Oracle and/or its affiliates. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
