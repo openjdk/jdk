8c32bcb315072658a2a6c34b34b888ee9fb558ac
Mikael Vidstedt <mikael@openjdk.org>
2012-11-27 07:57:57 -0800

    8003879: Duplicate definitions in vmStructs
    Removed duplicate entries
    
    Reviewed-by: dholmes, sspitsyn

--- hotspot/src/share/vm/prims/jni.cpp
+++ hotspot/src/share/vm/prims/jni.cpp
@@ -5044,6 +5044,9 @@ _JNI_IMPORT_OR_EXPORT_ jint JNICALL JNI_GetDefaultJavaVMInitArgs(void *args_) {
 
 #include "gc_interface/collectedHeap.hpp"
 #include "utilities/quickSort.hpp"
+#if INCLUDE_VM_STRUCTS
+#include "runtime/vmStructs.hpp"
+#endif
 
 #define run_unit_test(unit_test_function_call)              \
   tty->print_cr("Running test: " #unit_test_function_call); \
@@ -5056,6 +5059,9 @@ void execute_internal_vm_tests() {
     run_unit_test(CollectedHeap::test_is_in());
     run_unit_test(QuickSort::test_quick_sort());
     run_unit_test(AltHashing::test_alt_hash());
+#if INCLUDE_VM_STRUCTS
+    run_unit_test(VMStructs::test());
+#endif
     tty->print_cr("All internal VM tests passed");
   }
 }
