306cab1006b4ac8e7256801d97576ddb54c42c47
David Katleman <katleman@openjdk.org>
2012-12-20 16:24:50 -0800

    8004982: JDK8 source with GPL header errors
    Reviewed-by: ohair

--- jdk/src/share/classes/sun/tools/jcmd/JCmd.java
+++ jdk/src/share/classes/sun/tools/jcmd/JCmd.java
@@ -1,8 +1,7 @@
 /*
  * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *com.sun.tools.attach.AttachNotSupportedException
-
+ *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.  Oracle designates this
