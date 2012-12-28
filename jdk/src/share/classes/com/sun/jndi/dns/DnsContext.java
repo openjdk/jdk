6ded31648025206d0d717e22f5fe25e2eead96b3
Jim Gish <jgish@openjdk.java.net>
2012-12-28 18:32:26 -0500

    8005594: Fix to 8003265 breaks build
    Backout changeset d05f028770b9
    
    Reviewed-by: smarks, wetmore

--- jdk/src/share/classes/com/sun/jndi/dns/DnsContext.java
+++ jdk/src/share/classes/com/sun/jndi/dns/DnsContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -102,8 +102,8 @@ public class DnsContext extends ComponentDirContext {
         this.domain = new DnsName(domain.endsWith(".")
                                   ? domain
                                   : domain + ".");
-        this.servers = servers.clone();
-        this.environment = (Hashtable<Object,Object>)environment.clone();
+        this.servers = servers;
+        this.environment = (Hashtable<Object,Object>) environment.clone();
         envShared = false;
         parentIsDns = false;
         resolver = null;
@@ -129,11 +129,11 @@ public class DnsContext extends ComponentDirContext {
      * no conflict.
      */
     private DnsContext(DnsContext ctx) {
-        environment = (Hashtable<Object,Object>)environment.clone();
+        environment = ctx.environment;
         envShared = ctx.envShared = true;
         parentIsDns = ctx.parentIsDns;
         domain = ctx.domain;
-        servers = ctx.servers.clone();
+        servers = ctx.servers;
         resolver = ctx.resolver;
         authoritative = ctx.authoritative;
         recursion = ctx.recursion;
