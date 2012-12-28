2b7071376d900a85fa567192bdc20791d72242ed
Erik Joelsson <erikj@openjdk.java.net>
2012-12-28 09:51:15 +0100

    8005549: build-infra: Merge NewMakefile.gmk and common/makefiles/Makefile
    Reviewed-by: ohair, tbell

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -3679,7 +3679,7 @@ fi
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1356635654
+DATE_WHEN_GENERATED=1356638459
 
 ###############################################################################
 #
