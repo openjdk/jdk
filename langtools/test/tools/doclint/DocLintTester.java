39734d27f0e2600e329a00489178bc28950ce274
Jonathan Gibbons <jjg@openjdk.org>
2012-12-30 06:17:36 -0800

    8005195: Doclint regression tests fail on windows
    Reviewed-by: mcimadamore

--- langtools/test/tools/doclint/DocLintTester.java
+++ langtools/test/tools/doclint/DocLintTester.java
@@ -107,7 +107,7 @@ public class DocLintTester {
 
     private static final Pattern dirFileLine = Pattern.compile(
             "(?m)"                          // multi-line mode
-            + "^([^: ]+?)"                  // directory part of file name
+            + "^(.*?)"                      // directory part of file name
             + "([A-Za-z0-9.]+:[0-9]+:)");   // file name and line number
 
     String removeFileNames(String s) {
