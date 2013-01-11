7518dede81f02eeff80f686d7e5784935b5bc3fd
Jonathan Gibbons <jjg@openjdk.org>
2013-01-10 19:38:57 -0800

    8004834: Add doclint support into javadoc
    Reviewed-by: darcy

--- langtools/test/tools/javadoc/6964914/TestStdDoclet.java
+++ langtools/test/tools/javadoc/6964914/TestStdDoclet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -57,6 +57,7 @@ public class TestStdDoclet {
         Process p = new ProcessBuilder()
             .command(javadoc.getPath(),
                 "-J-Xbootclasspath:" + System.getProperty("sun.boot.class.path"),
+                "-Xdoclint:none",
                 "-package",
                 new File(testSrc, thisClassName + ".java").getPath())
             .redirectErrorStream(true)
