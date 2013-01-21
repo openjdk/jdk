4e1409effea0603c98aac75ac9ca12cba40849b7
Masayoshi Okutsu <okutsu@openjdk.java.net>
2013-01-21 15:41:30 +0900

    8004489: Add support for Minguo and Hijrah calendars to CalendarNameProvider SPI 8006509: Add more calendar symbol names from CLDR
    Reviewed-by: peytoia

--- jdk/src/share/classes/sun/util/locale/provider/LocaleResources.java
+++ jdk/src/share/classes/sun/util/locale/provider/LocaleResources.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -385,6 +385,15 @@ public class LocaleResources {
         }
 
         return numberPatterns;
+    }
+
+    /**
+     * Returns the FormatData resource bundle of this LocaleResources.
+     * The FormatData should be used only for accessing extra
+     * resources required by JSR 310.
+     */
+    public ResourceBundle getFormatData() {
+        return localeData.getDateFormatData(locale);
     }
 
     private String getDateTimePattern(String key, int styleIndex, String calendarType) {
