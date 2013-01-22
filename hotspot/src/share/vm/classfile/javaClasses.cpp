98c357abf8931cb58432469a238e0d4d2da1b811
Dave Dice <dave.dice@oracle.com>
2013-01-22 05:56:42 -0800

    8004902: correctness fixes motivated by contended locking work (6607129)
    Misc correctness fixes
    
    Reviewed-by: acorn, dholmes, dice, sspitsyn

--- hotspot/src/share/vm/classfile/javaClasses.cpp
+++ hotspot/src/share/vm/classfile/javaClasses.cpp
@@ -911,7 +911,6 @@ jlong java_lang_Thread::stackSize(oop java_thread) {
 // Write the thread status value to threadStatus field in java.lang.Thread java class.
 void java_lang_Thread::set_thread_status(oop java_thread,
                                          java_lang_Thread::ThreadStatus status) {
-  assert(JavaThread::current()->thread_state() == _thread_in_vm, "Java Thread is not running in vm");
   // The threadStatus is only present starting in 1.5
   if (_thread_status_offset > 0) {
     java_thread->int_field_put(_thread_status_offset, status);
