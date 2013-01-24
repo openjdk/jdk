f76ce3727464e53e9dd7052ffeb9ecd0d152655f
Joe Darcy <darcy@openjdk.java.net>
2013-01-23 20:11:07 -0800

    8006264: Add explanation of why default methods cannot be used in JDK 8 javax.lang.model
    Reviewed-by: jjg

--- langtools/src/share/classes/javax/lang/model/util/AbstractTypeVisitor6.java
+++ langtools/src/share/classes/javax/lang/model/util/AbstractTypeVisitor6.java
@@ -49,6 +49,15 @@ import javax.lang.model.type.*;
  * visit method in question.  When the new visitor is introduced, all
  * or portions of this visitor may be deprecated.
  *
+ * <p>Note that adding a default implementation of a new visit method
+ * in a visitor class will occur instead of adding a <em>default
+ * method</em> directly in the visitor interface since a Java SE 8
+ * language feature cannot be used to this version of the API since
+ * this version is required to be runnable on Java SE 7
+ * implementations.  Future versions of the API that are only required
+ * to run on Java SE 8 and later may take advantage of default methods
+ * in this situation.
+ *
  * @param <R> the return type of this visitor's methods.  Use {@link
  *            Void} for visitors that do not need to return results.
  * @param <P> the type of the additional parameter to this visitor's
