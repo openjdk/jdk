55733f385c5a9602d320527c5025b3739fbcbbc2
jjg <none@none>
2013-01-30 09:40:54 -0800

    8007096: DocLint parsing problems with some comments Reviewed-by: mcimadamore

---
+++ test/tools/doclint/UnfinishedInlineTagTest.java
@@ -0,0 +1,17 @@
+/*
+ * @test /nodynamiccopyright/
+ * @bug 8007096
+ * @summary DocLint parsing problems with some comments
+ * @build DocLintTester
+ * @run main DocLintTester -Xmsgs:-html UnfinishedInlineTagTest.java
+ * @run main DocLintTester -Xmsgs -ref UnfinishedInlineTagTest.out UnfinishedInlineTagTest.java
+ * @author jlahoda
+ */
+
+import java.util.List;
+
+/**{@link List
+ */
+public class UnfinishedInlineTagTest {
+}
+
