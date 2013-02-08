c5672879b1b65897bec45d98e6c7179845f15a6b
vromero <none@none>
2013-02-08 09:15:27 +0000

    8005931: javac doesn't set ACC_STRICT for classes with package access Reviewed-by: mcimadamore

--- src/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ src/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -3022,7 +3022,7 @@ public class JavacParser implements Parser {
         boolean checkForImports = true;
         boolean firstTypeDecl = true;
         while (token.kind != EOF) {
-            if (token.pos <= endPosTable.errorEndPos) {
+            if (token.pos > 0 && token.pos <= endPosTable.errorEndPos) {
                 // error recovery
                 skip(checkForImports, false, false, false);
                 if (token.kind == EOF)
