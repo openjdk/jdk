4e7de85e4d6522f40ef392874e9e79173df3f457
Zhengyu Gu <zgu@openjdk.org>
2013-02-12 14:47:36 -0500

    8006691: Remove jvm_version_info->is_kernel_jvm field
    Remove is_kernel_jvm field in jvm_version_info structure, as kernel VM has been deprecated
    
    Reviewed-by: mchung

--- jdk/src/share/javavm/export/jvm.h
+++ jdk/src/share/javavm/export/jvm.h
@@ -1401,8 +1401,7 @@ typedef struct {
      * the new bit is also added in the main/baseline.
      */
     unsigned int is_attach_supported : 1;
-    unsigned int is_kernel_jvm : 1;
-    unsigned int : 30;
+    unsigned int : 31;
     unsigned int : 32;
     unsigned int : 32;
 } jvm_version_info;
