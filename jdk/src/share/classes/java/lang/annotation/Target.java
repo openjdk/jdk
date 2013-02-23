f90ddeac399aaca6a85fad16c6408b845c8d3261
Joe Darcy <darcy@openjdk.java.net>
2013-02-23 13:32:32 -0800

    6556996: (ann spec) SuppressWarnings strings should be documented
    Reviewed-by: mduigou, chegar, abuckley

--- jdk/src/share/classes/java/lang/annotation/Target.java
+++ jdk/src/share/classes/java/lang/annotation/Target.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2004, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -59,6 +59,9 @@ package java.lang.annotation;
  *        ...
  *    }
  * </pre>
+ *
+ * @since 1.5
+ * @jls 9.6.3.1 @Target
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
