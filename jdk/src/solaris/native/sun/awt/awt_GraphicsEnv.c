9602ed03251c3db20f5ecbc84dd7f2cb19bb6828
Andrew Brygin <bae@openjdk.java.net>
2013-02-27 12:10:59 +0400

    8001034: Memory management improvements
    Reviewed-by: mschoene, prr, jgodinez

--- jdk/src/solaris/native/sun/awt/awt_GraphicsEnv.c
+++ jdk/src/solaris/native/sun/awt/awt_GraphicsEnv.c
@@ -906,6 +906,8 @@ static jint canUseShmExt = UNSET_MITSHM;
 static jint canUseShmExtPixmaps = UNSET_MITSHM;
 static jboolean xshmAttachFailed = JNI_FALSE;
 
+extern int mitShmPermissionMask;
+
 int J2DXErrHandler(Display *display, XErrorEvent *xerr) {
     int ret = 0;
     if (xerr->minor_code == X_ShmAttach) {
@@ -944,7 +946,8 @@ void TryInitMITShm(JNIEnv *env, jint *shmExt, jint *shmPixmaps) {
      * we need to test that we can actually do XShmAttach.
      */
     if (XShmQueryExtension(awt_display)) {
-        shminfo.shmid = shmget(IPC_PRIVATE, 0x10000, IPC_CREAT|0777);
+        shminfo.shmid = shmget(IPC_PRIVATE, 0x10000,
+                               IPC_CREAT|mitShmPermissionMask);
         if (shminfo.shmid < 0) {
             AWT_UNLOCK();
             J2dRlsTraceLn1(J2D_TRACE_ERROR,
