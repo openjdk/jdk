9e7f6fe31f0d42ec33fbc709a109da5fd4272d04
Shanliang Jiang <sjiang@openjdk.java.net>
2013-04-02 10:38:51 +0200

    8007467: Better JMX type conversion
    Reviewed-by: dfuchs, mchung, skoivu

--- jdk/src/share/classes/com/sun/jmx/mbeanserver/ConvertingMethod.java
+++ jdk/src/share/classes/com/sun/jmx/mbeanserver/ConvertingMethod.java
@@ -33,6 +33,7 @@ import javax.management.Descriptor;
 import javax.management.MBeanException;
 import javax.management.openmbean.OpenDataException;
 import javax.management.openmbean.OpenType;
+import sun.reflect.misc.MethodUtil;
 
 final class ConvertingMethod {
     static ConvertingMethod from(Method m) {
@@ -189,7 +190,7 @@ final class ConvertingMethod {
                 "from open values: " + e;
             throw new MBeanException(e, msg);
         }
-        final Object javaReturn = method.invoke(obj, javaParams);
+        final Object javaReturn = MethodUtil.invoke(method, obj, javaParams);
         try {
             return returnMapping.toOpenValue(javaReturn);
         } catch (OpenDataException e) {
