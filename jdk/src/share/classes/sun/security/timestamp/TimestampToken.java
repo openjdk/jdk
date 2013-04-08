60bb493c66c5b223d74ebca3674e7333b6bed61a
Vinnie Ryan <vinnie@openjdk.org>
2013-04-08 21:12:28 +0100

    8009235: Improve handling of TSA data
    Reviewed-by: ahgross, mullan

--- jdk/src/share/classes/sun/security/timestamp/TimestampToken.java
+++ jdk/src/share/classes/sun/security/timestamp/TimestampToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -113,6 +113,10 @@ public class TimestampToken {
 
     public BigInteger getNonce() {
         return nonce;
+    }
+
+    public BigInteger getSerialNumber() {
+        return serialNumber;
     }
 
     /*
