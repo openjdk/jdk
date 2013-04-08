0bc5f86c054e86044abd5bbcb1a47302c5e2b706
mcimadamore <none@none>
2013-04-08 15:59:29 +0100

    8010822: Intersection type cast for functional expressions does not follow spec EDR Summary: Remove support for marker interfaces; redefine intersection type casts to be order-independent Reviewed-by: jjg

--- test/tools/javac/diags/examples/NotAnInterfaceComponent.java
+++ test/tools/javac/diags/examples/NotAnInterfaceComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,8 +22,9 @@
  */
 
 // key: compiler.err.prob.found.req
-// key: compiler.misc.secondary.bound.must.be.marker.intf
+// key: compiler.misc.bad.intersection.target.for.functional.expr
+// key: compiler.misc.not.an.intf.component
 
-class SecondaryBoundMustBeMarkerInterface {
-    Runnable r = (Runnable & Comparable<?>)()->{};
+class NotAnInterfaceComponent {
+    Object o = (Object & Runnable) ()-> { };
 }
