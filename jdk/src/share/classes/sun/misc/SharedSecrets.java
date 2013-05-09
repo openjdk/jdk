ceb03179807ffc1ade7dd4d85d7432bb97f89297
Sean Coffey <coffeys@openjdk.org>
2013-05-09 20:31:47 +0100

    8013196: TimeZone.getDefault() throws NPE due to sun.awt.AppContext.getAppContext()
    Reviewed-by: mchung, okutsu

--- jdk/src/share/classes/sun/misc/SharedSecrets.java
+++ jdk/src/share/classes/sun/misc/SharedSecrets.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -170,6 +170,9 @@ public class SharedSecrets {
     public static JavaAWTAccess getJavaAWTAccess() {
         // this may return null in which case calling code needs to
         // provision for.
+        if (javaAWTAccess == null || javaAWTAccess.getContext() == null) {
+            return null;
+        }
         return javaAWTAccess;
     }
 }
