28c4dfd9c721e9542cebb1b28027fd7bda994e44
jjg <none@none>
2013-05-14 10:14:54 -0700

    8012176: reduce use of TagletOutputImpl.toString Reviewed-by: darcy

--- src/share/classes/com/sun/tools/doclets/formats/html/TagletOutputImpl.java
+++ src/share/classes/com/sun/tools/doclets/formats/html/TagletOutputImpl.java
@@ -59,6 +59,10 @@ public class TagletOutputImpl implements TagletOutput {
         setOutput(c);
     }
 
+    Content getContent() {
+        return content;
+    }
+
     /**
      * {@inheritDoc}
      */
