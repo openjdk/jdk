b4238545fd32ad292899553650c027e552fdb904
Andreas Eriksson <aeriksso@openjdk.java.net>
2013-05-17 17:24:20 +0200

    7176220: 'Full GC' events miss date stamp information occasionally
    Move date stamp logic into GCTraceTime
    
    Reviewed-by: brutisso, tschatzl

--- hotspot/src/share/vm/gc_implementation/g1/vm_operations_g1.cpp
+++ hotspot/src/share/vm/gc_implementation/g1/vm_operations_g1.cpp
@@ -230,7 +230,6 @@ void VM_CGC_Operation::release_and_notify_pending_list_lock() {
 }
 
 void VM_CGC_Operation::doit() {
-  gclog_or_tty->date_stamp(G1Log::fine() && PrintGCDateStamps);
   TraceCPUTime tcpu(G1Log::finer(), true, gclog_or_tty);
   GCTraceTime t(_printGCMessage, G1Log::fine(), true, G1CollectedHeap::heap()->gc_timer_cm(), G1CollectedHeap::heap()->concurrent_mark()->concurrent_gc_id());
   SharedHeap* sh = SharedHeap::heap();
