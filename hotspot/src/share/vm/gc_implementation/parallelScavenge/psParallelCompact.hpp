04b5063d0689d75b0e3f5729d56163e98763953a
Tao Mao <tao.mao@oracle.com>
2013-05-20 10:44:33 -0700

    7186737: Unable to allocate bit maps or card tables for parallel gc for the requested heap
    Print helpful error message when VM aborts due to inability of allocating bit maps or card tables
    
    Reviewed-by: jmasa, stefank

--- hotspot/src/share/vm/gc_implementation/parallelScavenge/psParallelCompact.hpp
+++ hotspot/src/share/vm/gc_implementation/parallelScavenge/psParallelCompact.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -347,6 +347,7 @@ public:
   bool initialize(MemRegion covered_region);
 
   size_t region_count() const { return _region_count; }
+  size_t reserved_byte_size() const { return _reserved_byte_size; }
 
   // Convert region indices to/from RegionData pointers.
   inline RegionData* region(size_t region_idx) const;
@@ -420,6 +421,7 @@ private:
 #endif  // #ifdef ASSERT
 
   PSVirtualSpace* _region_vspace;
+  size_t          _reserved_byte_size;
   RegionData*     _region_data;
   size_t          _region_count;
 };
