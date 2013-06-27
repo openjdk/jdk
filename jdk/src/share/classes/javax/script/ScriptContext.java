ff500d62010352b7d306aa3e3c9d48290bbb4cea
Joe Darcy <darcy@openjdk.org>
2013-06-27 14:11:25 -0700

    8019320: Fix doclint issues in javax.script
    Reviewed-by: lancea

--- jdk/src/share/classes/javax/script/ScriptContext.java
+++ jdk/src/share/classes/javax/script/ScriptContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -78,6 +78,7 @@ public interface ScriptContext {
      * @return The associated <code>Bindings</code>.  Returns <code>null</code> if it has not
      * been set.
      *
+     * @param scope The scope
      * @throws IllegalArgumentException If no <code>Bindings</code> is defined for the
      * specified scope value in <code>ScriptContext</code> of this type.
      */
