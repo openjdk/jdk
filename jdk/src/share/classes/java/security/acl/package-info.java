16848e3385f29751c00780041b2a61ce0b8b5cf2
Jason Uh <juh@openjdk.java.net>
2013-06-28 10:48:02 -0700

    8019360: Cleanup of the javadoc <code> tag in java.security.*
    Convert to {@code ...} tags. convert package.html to package-info.java.
    
    Reviewed-by: darcy

--- jdk/src/share/classes/java/security/acl/package-info.java
+++ jdk/src/share/classes/java/security/acl/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,36 +23,11 @@
  * questions.
  */
 
-package java.security.interfaces;
-
-import java.math.BigInteger;
-
 /**
- * The standard interface to a DSA private key. DSA (Digital Signature
- * Algorithm) is defined in NIST's FIPS-186.
+ * The classes and interfaces in this package have been
+ * superseded by classes in the java.security package.
+ * See that package and, for example, java.security.Permission for details.
  *
- * @see java.security.Key
- * @see java.security.Signature
- * @see DSAKey
- * @see DSAPublicKey
- *
- * @author Benjamin Renaud
+ * @since JDK1.1
  */
-public interface DSAPrivateKey extends DSAKey, java.security.PrivateKey {
-
-    // Declare serialVersionUID to be compatible with JDK1.1
-
-   /**
-    * The class fingerprint that is set to indicate
-    * serialization compatibility with a previous
-    * version of the class.
-    */
-    static final long serialVersionUID = 7776497482533790279L;
-
-    /**
-     * Returns the value of the private key, {@code x}.
-     *
-     * @return the value of the private key, {@code x}.
-     */
-    public BigInteger getX();
-}
+package java.security.acl;
