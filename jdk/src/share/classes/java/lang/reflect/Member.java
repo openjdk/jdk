5a360a7579c6a46f7a0ed76e861fff704f2f62d8
Mandy Chung <mchung@openjdk.org>
2013-07-02 15:58:09 -0700

    8007035: deprecate public void SecurityManager.checkMemberAccess(Class<?> clazz, int which)
    Reviewed-by: jrose, alanb, dfuchs

--- jdk/src/share/classes/java/lang/reflect/Member.java
+++ jdk/src/share/classes/java/lang/reflect/Member.java
@@ -42,14 +42,12 @@ interface Member {
     /**
      * Identifies the set of all public members of a class or interface,
      * including inherited members.
-     * @see java.lang.SecurityManager#checkMemberAccess
      */
     public static final int PUBLIC = 0;
 
     /**
      * Identifies the set of declared members of a class or interface.
      * Inherited members are not included.
-     * @see java.lang.SecurityManager#checkMemberAccess
      */
     public static final int DECLARED = 1;
 
