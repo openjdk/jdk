7a6be8a426db8dc98bcff563a1c487f7313fe3db
Sean Mullan <mullan@openjdk.java.net>
2013-07-05 15:54:42 -0400

    8011547: Update XML Signature implementation to Apache Santuario 1.5.4
    Reviewed-by: xuelei

--- jdk/src/share/classes/com/sun/org/apache/xml/internal/security/encryption/EncryptionProperties.java
+++ jdk/src/share/classes/com/sun/org/apache/xml/internal/security/encryption/EncryptionProperties.java
@@ -2,27 +2,27 @@
  * reserved comment block
  * DO NOT REMOVE OR ALTER!
  */
-/*
- * Copyright  2003-2004 The Apache Software Foundation.
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package com.sun.org.apache.xml.internal.security.encryption;
 
-
 import java.util.Iterator;
-
 
 /**
  * <code>EncryptionProperties</code> can hold additional information concerning
@@ -46,6 +46,7 @@ import java.util.Iterator;
  * @author Axl Mattheus
  */
 public interface EncryptionProperties {
+
     /**
      * Returns the <code>EncryptionProperties</code>' id.
      *
@@ -72,14 +73,15 @@ public interface EncryptionProperties {
     /**
      * Adds an <code>EncryptionProperty</code>.
      *
-     * @param property.
+     * @param property
      */
     void addEncryptionProperty(EncryptionProperty property);
 
     /**
      * Removes the specified <code>EncryptionProperty</code>.
      *
-     * @param property.
+     * @param property
      */
     void removeEncryptionProperty(EncryptionProperty property);
 }
+
