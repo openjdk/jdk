aa8ae5a10251496d4029e4d2ec7adce519537a66
Brian Goetz <brian.goetz@oracle.com>
2013-07-23 13:20:11 -0700

    8019840: Spec updates for java.util.function
    Reviewed-by: mduigou, chegar

--- jdk/src/share/classes/java/util/function/LongUnaryOperator.java
+++ jdk/src/share/classes/java/util/function/LongUnaryOperator.java
@@ -27,9 +27,12 @@ package java.util.function;
 import java.util.Objects;
 
 /**
- * An operation on a single {@code long} operand yielding a {@code long} result.
- * This is the primitive type specialization of {@link UnaryOperator} for
- * {@code long}.
+ * Represents an operation on a single {@code long}-valued operand that produces
+ * a {@code long}-valued result.  This is the primitive type specialization of
+ * {@link UnaryOperator} for {@code long}.
+ *
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #applyAsLong(long)}.
  *
  * @see UnaryOperator
  * @since 1.8
@@ -38,24 +41,25 @@ import java.util.Objects;
 public interface LongUnaryOperator {
 
     /**
-     * Returns the {@code long} result of the operation upon the {@code long}
-     * operand.
+     * Applies this operator to the given operand.
      *
-     * @param operand the operand value
-     * @return the operation result value
+     * @param operand the operand
+     * @return the operator result
      */
     long applyAsLong(long operand);
 
     /**
-     * Compose a new function which applies the provided function followed by
-     * this function.  If either function throws an exception, it is relayed
-     * to the caller.
+     * Returns a composed operator that first applies the {@code before}
+     * operator to its input, and then applies this operator to the result.
+     * If evaluation of either operator throws an exception, it is relayed to
+     * the caller of the composed operator.
      *
-     * @param before An additional function to be applied before this function
-     * is applied
-     * @return A function which performs the provided function followed by this
-     * function
+     * @param before the operator to apply before this operator is applied
+     * @return a composed operator that first applies the {@code before}
+     * operator and then applies this operator
      * @throws NullPointerException if before is null
+     *
+     * @see #andThen(LongUnaryOperator)
      */
     default LongUnaryOperator compose(LongUnaryOperator before) {
         Objects.requireNonNull(before);
@@ -63,15 +67,17 @@ public interface LongUnaryOperator {
     }
 
     /**
-     * Compose a new function which applies this function followed by the
-     * provided function.  If either function throws an exception, it is relayed
-     * to the caller.
+     * Returns a composed operator that first applies this operator to
+     * its input, and then applies the {@code after} operator to the result.
+     * If evaluation of either operator throws an exception, it is relayed to
+     * the caller of the composed operator.
      *
-     * @param after An additional function to be applied after this function is
-     * applied
-     * @return A function which performs this function followed by the provided
-     * function followed
+     * @param after the operator to apply after this operator is applied
+     * @return a composed operator that first applies this operator and then
+     * applies the {@code after} operator
      * @throws NullPointerException if after is null
+     *
+     * @see #compose(LongUnaryOperator)
      */
     default LongUnaryOperator andThen(LongUnaryOperator after) {
         Objects.requireNonNull(after);
@@ -79,9 +85,9 @@ public interface LongUnaryOperator {
     }
 
     /**
-     * Returns a unary operator that provides its input value as the result.
+     * Returns a unary operator that always returns its input argument.
      *
-     * @return a unary operator that provides its input value as the result
+     * @return a unary operator that always returns its input argument
      */
     static LongUnaryOperator identity() {
         return t -> t;
