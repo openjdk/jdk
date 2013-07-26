d656c7e2b89d2f9d145ffb056844bf63eccebb31
Volker Simonis <simonis@openjdk.org>
2013-07-26 10:12:15 +0200

    8019926: PPC64 (part 106): Make hsdis build and work on Linux/PPC64
    Make hsdis work on Linux/PPC64 and AIX/PPC64
    
    Reviewed-by: kvn

--- hotspot/src/share/tools/hsdis/hsdis.c
+++ hotspot/src/share/tools/hsdis/hsdis.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -307,7 +307,8 @@ static void setup_app_data(struct hsdis_app_data* app_data,
                                  app_data->printf_stream,
                                  app_data->printf_callback,
                                  native_bfd,
-                                 app_data->insn_options);
+                                 /* On PowerPC we get warnings, if we pass empty options */
+                                 (caller_options == NULL) ? NULL : app_data->insn_options);
 
   /* Finish linking together the various callback blocks. */
   app_data->dinfo.application_data = (void*) app_data;
@@ -458,6 +459,9 @@ static const char* native_arch_name() {
 #endif
 #ifdef LIBARCH_sparcv9
   res = "sparc:v9b";
+#endif
+#ifdef LIBARCH_ppc64
+  res = "powerpc:common64";
 #endif
   if (res == NULL)
     res = "architecture not set in Makefile!";
