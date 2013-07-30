174533b45e976c3bd12d8e716cdc7bea41118e6c
Henry Jen <henryjen@openjdk.java.net>
2013-07-30 15:47:35 -0700

    8020977: StringJoiner merges with itself not as expected
    Reviewed-by: psandoz, chegar, mduigou, smarks

--- jdk/src/share/classes/java/util/StringJoiner.java
+++ jdk/src/share/classes/java/util/StringJoiner.java
@@ -206,11 +206,12 @@ public final class StringJoiner {
     public StringJoiner merge(StringJoiner other) {
         Objects.requireNonNull(other);
         if (other.value != null) {
+            final int length = other.value.length();
+            // lock the length so that we can seize the data to be appended
+            // before initiate copying to avoid interference, especially when
+            // merge 'this'
             StringBuilder builder = prepareBuilder();
-            StringBuilder otherBuilder = other.value;
-            if (other.prefix.length() < otherBuilder.length()) {
-                builder.append(otherBuilder, other.prefix.length(), otherBuilder.length());
-            }
+            builder.append(other.value, other.prefix.length(), length);
         }
         return this;
     }
