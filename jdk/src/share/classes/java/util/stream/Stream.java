2be7768ab06be1db080133019c6aeecbdcb712f7
Joe Darcy <darcy@openjdk.org>
2013-08-05 07:50:16 -0700

    8022190: Fix varargs lint warnings in the JDK
    Reviewed-by: alanb, lancea, alexsch

--- jdk/src/share/classes/java/util/stream/Stream.java
+++ jdk/src/share/classes/java/util/stream/Stream.java
@@ -827,6 +827,7 @@ public interface Stream<T> extends BaseStream<T, Stream<T>> {
      * @return the new stream
      */
     @SafeVarargs
+    @SuppressWarnings("varargs") // Creating a stream from an array is safe
     public static<T> Stream<T> of(T... values) {
         return Arrays.stream(values);
     }
