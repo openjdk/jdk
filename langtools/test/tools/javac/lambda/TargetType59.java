714b4473249776259fe4f343a31a5a05271e8a76
Vicente Romero <vromero@openjdk.java.net>
2013-08-10 16:26:50 +0100

    8022622: javac, two tests are failing with compile time error after class Collector was modified
    Reviewed-by: mcimadamore

--- langtools/test/tools/javac/lambda/TargetType59.java
+++ langtools/test/tools/javac/lambda/TargetType59.java
@@ -34,15 +34,15 @@ import java.util.stream.*;
 
 class TargetType59 {
 
-    <T, R> Collector<T, R> m(Supplier<? extends R> supplier, BiConsumer<R, T> accumulator) {
+    <T, R> Collector<T, String, R> m(Supplier<? extends R> supplier, BiConsumer<R, T> accumulator) {
         return null;
     }
 
-    <T, C extends Collection<T>> Collector<T,C> test1(Supplier<C> collectionFactory) {
+    <T, C extends Collection<T>> Collector<T, String, C> test1(Supplier<C> collectionFactory) {
         return m(collectionFactory, Collection::add);
     }
 
-    Collector<String, StringBuilder> test2(Supplier<StringBuilder> sb) {
+    Collector<String, String, StringBuilder> test2(Supplier<StringBuilder> sb) {
         return m(sb, StringBuilder::append);
     }
 }
