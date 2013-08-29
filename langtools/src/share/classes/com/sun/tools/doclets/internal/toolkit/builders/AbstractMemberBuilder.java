12da62f6a8c3f5beb9b907c2ce844226a27542b9
Jonathan Gibbons <jjg@openjdk.java.net>
2013-08-29 11:41:20 -0700

    8001669: javadoc internal DocletAbortException should set cause when appropriate
    Reviewed-by: darcy

--- langtools/src/share/classes/com/sun/tools/doclets/internal/toolkit/builders/AbstractMemberBuilder.java
+++ langtools/src/share/classes/com/sun/tools/doclets/internal/toolkit/builders/AbstractMemberBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -62,7 +62,7 @@ public abstract class AbstractMemberBuilder extends AbstractBuilder {
      */
     public void build() throws DocletAbortException {
         //You may not call the build method in a subbuilder.
-        throw new DocletAbortException();
+        throw new DocletAbortException("not supported");
     }
 
 
