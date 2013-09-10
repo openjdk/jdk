183d54ce3d4702c0800bf01862c7b7e228334f6e
jfranck <none@none>
2013-09-10 13:47:51 +0200

    8005222: Fixed bugs should have tests with bugid in @bug tag Reviewed-by: jfranck, jjg Contributed-by: Andreas Lundblad <andreas.lundblad@oracle.com>

--- test/tools/javac/defaultMethods/Pos10.java
+++ test/tools/javac/defaultMethods/Pos10.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 
 /*
  * @test
+ * @bug 7192246
  * @summary check that type-variables in generic extension decl can be accessed from default impl
  * @author  Maurizio Cimadamore
  * @compile Pos10.java
