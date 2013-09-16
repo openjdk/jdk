77bd2934be3c0b862ab3df4892f019d27a1a5f6a
Yumin Qi <yumin.qi@oracle.com>
2013-09-16 15:35:04 -0700

    7164841: Improvements to the GC log file rotation
    Made changes to easily identify current log file in rotation. Parameterize the input with %t for time replacement in file name.
    
    Reviewed-by: ccheung, tschatzl, tamao, zgu

--- hotspot/src/share/vm/prims/jni.cpp
+++ hotspot/src/share/vm/prims/jni.cpp
@@ -5037,6 +5037,7 @@ _JNI_IMPORT_OR_EXPORT_ jint JNICALL JNI_GetDefaultJavaVMInitArgs(void *args_) {
 #include "gc_implementation/g1/heapRegionRemSet.hpp"
 #endif
 #include "utilities/quickSort.hpp"
+#include "utilities/ostream.hpp"
 #if INCLUDE_VM_STRUCTS
 #include "runtime/vmStructs.hpp"
 #endif
@@ -5060,6 +5061,7 @@ void execute_internal_vm_tests() {
     run_unit_test(CollectedHeap::test_is_in());
     run_unit_test(QuickSort::test_quick_sort());
     run_unit_test(AltHashing::test_alt_hash());
+    run_unit_test(test_loggc_filename());
 #if INCLUDE_VM_STRUCTS
     run_unit_test(VMStructs::test());
 #endif
