52d45755b4f41a69715d17ab365334ce89221a0c
Jonathan Gibbons <jjg@openjdk.java.net>
2013-09-25 14:04:24 -0700

    8025412: Add legal header and comments to test/tools/doclint/tidy/util/Main.java
    Reviewed-by: bpatel

--- langtools/test/tools/doclint/tidy/util/tidy.sh
+++ langtools/test/tools/doclint/tidy/util/tidy.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #
-# Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 #
 
 # Run the "tidy" program over the files in a directory.
+# The "tidy" program must be on your PATH.
 #
 # Usage:
 #   sh tidy.sh <dir>
