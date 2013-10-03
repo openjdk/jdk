3692c02c341a422ed7d5b54d8cdd230738ab03ec
Oleg Pekhovskiy <bagiras@openjdk.java.net>
2013-10-03 16:51:56 +0400

    8013553: [macosx] java.awt.FileDialog removes file extensions
    Reviewed-by: leonidr, serb

--- jdk/src/macosx/native/sun/awt/CFileDialog.m
+++ jdk/src/macosx/native/sun/awt/CFileDialog.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -92,6 +92,14 @@ canChooseDirectories:(BOOL)inChooseDirectories
 
 - (void)safeSaveOrLoad {
     NSSavePanel *thePanel = nil;
+
+    /* 
+     * 8013553: turns off extension hiding for the native file dialog.
+     * This way is used because setExtensionHidden(NO) doesn't work
+     * as expected.
+     */
+    NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
+    [defaults setBool:NO forKey:@"NSNavLastUserSetHideExtensionButtonState"];
 
     if (fMode == java_awt_FileDialog_SAVE) {
         thePanel = [NSSavePanel savePanel];
