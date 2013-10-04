ab3ef4ac0764c79fc3fb8a9ebcea1f9ab8cc5eaf
Daniel Fuchs <dfuchs@openjdk.java.net>
2013-10-04 19:15:10 +0200

    8025745: Clarify API documentation of JAXP factories
    Clarifies usage of ServiceLoader in JAXP factories.
    
    Reviewed-by: alanb, joehw, psandoz

--- jaxp/src/javax/xml/stream/XMLEventFactory.java
+++ jaxp/src/javax/xml/stream/XMLEventFactory.java
@@ -84,7 +84,13 @@ public abstract class XMLEventFactory {
    * <li>
    *   Use the service-provider loading facilities, defined by the
    *   {@link java.util.ServiceLoader} class, to attempt to locate and load an
-   *   implementation of the service.
+   *   implementation of the service using the {@linkplain
+   *   java.util.ServiceLoader#load(java.lang.Class) default loading mechanism}:
+   *   the service-provider loading facility will use the {@linkplain
+   *   java.lang.Thread#getContextClassLoader() current thread's context class loader}
+   *   to attempt to load the service. If the context class
+   *   loader is null, the {@linkplain
+   *   ClassLoader#getSystemClassLoader() system class loader} will be used.
    * </li>
    * <li>
    *   Otherwise, the system-default implementation is returned.
@@ -152,7 +158,13 @@ public abstract class XMLEventFactory {
    *   If {@code factoryId} is "javax.xml.stream.XMLEventFactory",
    *   use the service-provider loading facilities, defined by the
    *   {@link java.util.ServiceLoader} class, to attempt to locate and load an
-   *   implementation of the service.
+   *   implementation of the service using the {@linkplain
+   *   java.util.ServiceLoader#load(java.lang.Class) default loading mechanism}:
+   *   the service-provider loading facility will use the {@linkplain
+   *   java.lang.Thread#getContextClassLoader() current thread's context class loader}
+   *   to attempt to load the service. If the context class
+   *   loader is null, the {@linkplain
+   *   ClassLoader#getSystemClassLoader() system class loader} will be used.
    * </li>
    * <li>
    *   Otherwise, throws a {@link FactoryConfigurationError}.
