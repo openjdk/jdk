31fdd1b27caffd81e5d58f7d9f37e37508ca4243
Alan Bateman <alanb@openjdk.org>
2013-10-08 10:49:09 +0100

    8024788: (fs) Files.readAllBytes uses FileChannel which may not be supported by all providers
    Reviewed-by: chegar

--- jdk/src/share/classes/java/nio/file/Files.java
+++ jdk/src/share/classes/java/nio/file/Files.java
@@ -3082,13 +3082,13 @@ public final class Files {
      *          method is invoked to check read access to the file.
      */
     public static byte[] readAllBytes(Path path) throws IOException {
-        try (FileChannel fc = FileChannel.open(path);
-             InputStream is = Channels.newInputStream(fc)) {
-            long size = fc.size();
+        try (SeekableByteChannel sbc = Files.newByteChannel(path);
+             InputStream in = Channels.newInputStream(sbc)) {
+            long size = sbc.size();
             if (size > (long)MAX_BUFFER_SIZE)
                 throw new OutOfMemoryError("Required array size too large");
 
-            return read(is, (int)size);
+            return read(in, (int)size);
         }
     }
 
