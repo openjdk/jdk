cbd0e9bf96e7f4ce5ae532f81160263a92b59f2e
Roland Westrelin <roland@openjdk.org>
2013-10-09 16:32:21 +0200

    8023657: New type profiling points: arguments to call
    X86 interpreter and c1 type profiling for arguments at calls
    
    Reviewed-by: kvn, twisti

--- hotspot/src/cpu/x86/vm/globals_x86.hpp
+++ hotspot/src/cpu/x86/vm/globals_x86.hpp
@@ -79,6 +79,8 @@ define_pd_global(bool, UseMembar,            false);
 // GC Ergo Flags
 define_pd_global(uintx, CMSYoungGenPerWorker, 64*M);  // default max size of CMS young gen, per GC worker thread
 
+define_pd_global(uintx, TypeProfileLevel, 1);
+
 #define ARCH_FLAGS(develop, product, diagnostic, experimental, notproduct) \
                                                                             \
   develop(bool, IEEEPrecision, true,                                        \
