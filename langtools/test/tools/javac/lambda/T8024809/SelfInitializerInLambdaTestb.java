027b935c1b780731322112ba7d458235e9fd273a
Vicente Romero <vromero@openjdk.org>
2013-10-19 17:43:09 +0100

    8024809: javac, some lambda programs are rejected by flow analysis
    Reviewed-by: jjg, dlsmith

--- langtools/test/tools/javac/lambda/T8024809/SelfInitializerInLambdaTestb.java
+++ langtools/test/tools/javac/lambda/T8024809/SelfInitializerInLambdaTestb.java
@@ -23,17 +23,18 @@
 
 /*
  * @test
- * @bug 8016081
- * @summary field initialized with lambda in annotation types doesn't compile
- * @compile T8016081.java
+ * @bug 8024809
+ * @summary javac, some lambda programs are rejected by flow analysis
+ * @compile/fail/ref=SelfInitializerInLambdaTestb.out -XDrawDiagnostics SelfInitializerInLambdaTestb.java
  */
 
-class T8016081 {
-    interface fint { int get(); }
+public class SelfInitializerInLambdaTestb {
 
-    @interface atype {
-        fint fld = ()->1;
+    final Runnable r1;
+
+    final Runnable r2 = ()-> System.out.println(r1);
+
+    SelfInitializerInLambdaTestb() {
+        r1 = ()->System.out.println(r1);
     }
-
-    @atype class T {}
 }
