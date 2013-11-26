c1f01ee7e836574fc56a8a30842344618440271c
Michael McMahon <michaelm@openjdk.java.net>
2013-11-26 10:06:57 +0000

    8028293: Check local configuration for actual ephemeral port range
    Reviewed-by: alanb, chegar, smarks

--- jdk/src/share/classes/java/net/SocketPermission.java
+++ jdk/src/share/classes/java/net/SocketPermission.java
@@ -44,6 +44,7 @@ import java.io.ObjectInputStream;
 import java.io.IOException;
 import sun.net.util.IPAddressUtil;
 import sun.net.RegisteredDomain;
+import sun.net.PortConfig;
 import sun.security.util.SecurityConstants;
 import sun.security.util.Debug;
 
@@ -1217,17 +1218,9 @@ public final class SocketPermission extends Permission
                     if (val != -1) {
                         return val;
                     } else {
-                        String prop = Security.getProperty(
-                            "network.ephemeralPortRange."+suffix
-                        );
-                        try {
-                                val = Integer.parseInt(prop);
-                        } catch (NumberFormatException e) {
-                            // shouldn't happen
-                            return defval;
-                        }
+                        return suffix.equals("low") ?
+                            PortConfig.getLower() : PortConfig.getUpper();
                     }
-                    return val;
                 }
             }
         );
