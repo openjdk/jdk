81ef12b24a81df0f3b43cd331ef742df9afef18d
Bhavesh Patel <bpatel@openjdk.org>
2013-12-03 14:21:45 -0800

    8025416: doclet not substituting {@docRoot} in some cases
    Reviewed-by: jjg

--- langtools/test/com/sun/javadoc/testDocRootLink/pkg1/C1.java
+++ langtools/test/com/sun/javadoc/testDocRootLink/pkg1/C1.java
@@ -25,7 +25,12 @@ package pkg1;
 
 /**
  * Class 1. This is a test.
- * Refer <a href="{@docRoot}/../technotes/guides/index.html">Here</a>. Lets see if this works
- * or not.
+ * Refer <a href="{@docRoot}/../technotes/guides/index.html">Here</a>. This link should
+ * not be replaced with an absolute link.
+ * This <a href="{@docRoot}/pkg2/C2.html">Here</a> should not be replaced
+ * with an absolute link.
+ * Testing <a href="{@docRoot}/technotes/guides/index.html">Link 1</a> and
+ * <a href="{@docRoot}/pkg2/C2.html">Link 2</a>. 2 back-to-back links using
+ * docroot. These should not be replaced with an absolute link.
  */
 public class C1 {}
