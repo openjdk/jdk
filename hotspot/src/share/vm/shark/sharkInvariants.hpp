a0da47fd66549fb7484d8ea36d070027924f6576
Mikael Vidstedt <mikael@openjdk.org>
2013-12-24 11:48:39 -0800

    8029233: Update copyright year to match last edit in jdk8 hotspot repository for 2013
    Copyright year updated for files modified during 2013
    
    Reviewed-by: twisti, iveresov

--- hotspot/src/share/vm/shark/sharkInvariants.hpp
+++ hotspot/src/share/vm/shark/sharkInvariants.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2013, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2008, 2009 Red Hat, Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
