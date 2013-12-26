657b9db1ba9be6520dbd1086702e64746feafd34
Lana Steuck <lana@openjdk.org>
2013-12-26 12:04:16 -0800

    8029235: Update copyright year to match last edit in jdk8 jdk repository for 2013
    Updated files with 2011, 2012 and 2013 years according to the file's last updated date
    
    Reviewed-by: tbell, lancea, chegar

--- jdk/src/share/classes/org/jcp/xml/dsig/internal/dom/DOMPGPData.java
+++ jdk/src/share/classes/org/jcp/xml/dsig/internal/dom/DOMPGPData.java
@@ -21,7 +21,7 @@
  * under the License.
  */
 /*
- * Copyright (c) 2005, 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
  */
 /*
  * $Id: DOMPGPData.java 1203846 2011-11-18 21:18:17Z mullan $
--- jdk/src/solaris/classes/sun/awt/X11/keysym2ucs.h
+++ jdk/src/solaris/classes/sun/awt/X11/keysym2ucs.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2012, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,7 +39,7 @@
  */
 
 tojava /*
-tojava  * Copyright (c) 2005, 2009, Oracle and/or its affiliates. All rights reserved.
+tojava  * Copyright (c) 2005, 2012, Oracle and/or its affiliates. All rights reserved.
 tojava  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 tojava  *
 tojava  * This code is free software; you can redistribute it and/or modify it
