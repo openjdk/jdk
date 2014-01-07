af5201e37a5a58103294a8bd177ce9d2ac6a2a18
Joe Darcy <darcy@openjdk.java.net>
2014-01-07 13:47:49 -0800

    8031360: Update langtools code base to use RELEASE_9
    Reviewed-by: jjg

--- langtools/src/share/classes/com/sun/tools/javac/code/Source.java
+++ langtools/src/share/classes/com/sun/tools/javac/code/Source.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -251,7 +251,7 @@ public enum Source {
         case JDK1_8:
             return RELEASE_8;
         case JDK1_9:
-            return RELEASE_8; // Adjust once RELEASE_9 exists
+            return RELEASE_9;
         default:
             return null;
         }
