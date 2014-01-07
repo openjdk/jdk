091f9435984665fbee8c45edbc6a7319bf6d4283
Joe Darcy <darcy@openjdk.org>
2014-01-07 09:09:30 -0800

    8027063: SecurityManger.getClassContext returns a raw type
    Reviewed-by: lancea, alanb, xuelei

--- jdk/src/share/classes/java/lang/SecurityManager.java
+++ jdk/src/share/classes/java/lang/SecurityManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -307,7 +307,7 @@ class SecurityManager {
      *
      * @return  the execution stack.
      */
-    protected native Class[] getClassContext();
+    protected native Class<?>[] getClassContext();
 
     /**
      * Returns the class loader of the most recently executing method from
