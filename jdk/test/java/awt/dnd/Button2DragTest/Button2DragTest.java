90db59d13e3c1817556ba948e7bc88df56bf8022
Petr Pchelko <pchelko@openjdk.org>
2014-01-20 14:23:51 +0400

    8032058: [TEST_BUG] [macosx] java/awt/dnd/Button2DragTest sometimes fail
    Reviewed-by: anthony, serb

--- jdk/test/java/awt/dnd/Button2DragTest/Button2DragTest.java
+++ jdk/test/java/awt/dnd/Button2DragTest/Button2DragTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -112,6 +112,7 @@ public class Button2DragTest extends Applet {
         Util.drag(robot, startPoint, endPoint, InputEvent.BUTTON2_MASK);
 
         Util.waitForIdle(robot);
+        robot.delay(500);
 
         if (dropSuccess) {
             System.err.println("test passed");
