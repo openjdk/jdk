58eccf40a6a3d1dcaeb90a04b48b801b02bad060
Roger Riggs <rriggs@openjdk.java.net>
2014-01-22 17:40:58 -0500

    8032502: java.time add @param tags to readObject
    Add @param tags
    
    Reviewed-by: lancea, darcy

--- jdk/src/share/classes/java/time/zone/ZoneRules.java
+++ jdk/src/share/classes/java/time/zone/ZoneRules.java
@@ -319,6 +319,7 @@ public final class ZoneRules implements Serializable {
     /**
      * Defend against malicious streams.
      *
+     * @param s the stream to read
      * @throws InvalidObjectException always
      */
     private void readObject(ObjectInputStream s) throws InvalidObjectException {
