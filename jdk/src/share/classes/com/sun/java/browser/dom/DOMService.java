3a096e4753986caa8e47250583baca08455b948c
Joe Darcy <darcy@openjdk.java.net>
2014-01-22 23:20:58 -0800

    8032047: Fix static lint warnings in client libraries 8032048: Add static lint warning to build of jdk repository
    Reviewed-by: pchelko, serb, erikj

--- jdk/src/share/classes/com/sun/java/browser/dom/DOMService.java
+++ jdk/src/share/classes/com/sun/java/browser/dom/DOMService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2001, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -46,7 +46,7 @@ public abstract class DOMService
             String provider = (String) java.security.AccessController.doPrivileged(
                    new sun.security.action.GetPropertyAction("com.sun.java.browser.dom.DOMServiceProvider"));
 
-            Class clazz = DOMService.class.forName("sun.plugin.dom.DOMService");
+            Class clazz = Class.forName("sun.plugin.dom.DOMService");
 
             return (DOMService) clazz.newInstance();
         }
--- jdk/src/share/classes/sun/font/ExtendedTextSourceLabel.java
+++ jdk/src/share/classes/sun/font/ExtendedTextSourceLabel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2005, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -894,12 +894,12 @@ class ExtendedTextSourceLabel extends ExtendedTextLabel implements Decoration.La
 
   public String toString() {
     if (true) {
-        return source.toString(source.WITHOUT_CONTEXT);
+        return source.toString(TextSource.WITHOUT_CONTEXT);
     }
     StringBuffer buf = new StringBuffer();
     buf.append(super.toString());
     buf.append("[source:");
-    buf.append(source.toString(source.WITHOUT_CONTEXT));
+    buf.append(source.toString(TextSource.WITHOUT_CONTEXT));
     buf.append(", lb:");
     buf.append(lb);
     buf.append(", ab:");
