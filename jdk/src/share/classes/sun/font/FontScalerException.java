c3b3f9ecc45df04ccb1a01020b935612cd5dedbc
Joe Darcy <darcy@openjdk.java.net>
2014-02-01 10:27:55 -0800

    8033223: Fix serial lint warnings in sun.{font, java2d[.*]}
    Reviewed-by: bae

--- jdk/src/share/classes/sun/font/FontScalerException.java
+++ jdk/src/share/classes/sun/font/FontScalerException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@
 
 package sun.font;
 
+@SuppressWarnings("serial") // JDK-implementation class
 public class FontScalerException extends Exception {
     public FontScalerException() {
       super("Font scaler encountered runtime problem.");
