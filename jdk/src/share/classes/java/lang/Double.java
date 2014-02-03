9268c02fc004fea4612c42a07e509425b20ccb7c
Joe Darcy <darcy@openjdk.java.net>
2014-02-03 09:52:36 -0800

    8033416: Remove sun.misc.FpUtils
    Reviewed-by: alanb, bpb, psandoz

--- jdk/src/share/classes/java/lang/Double.java
+++ jdk/src/share/classes/java/lang/Double.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,7 +26,6 @@
 package java.lang;
 
 import sun.misc.FloatingDecimal;
-import sun.misc.FpUtils;
 import sun.misc.DoubleConsts;
 
 /**
