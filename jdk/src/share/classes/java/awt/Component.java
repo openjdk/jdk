25b763a4436c4d7af3b511136a2b55fb538bae66
Alexander Scherbatiy <alexsch@openjdk.java.net>
2014-02-17 13:41:50 +0400

    8029960: Remove reflection from Swing classes
    Reviewed-by: serb, pchelko

--- jdk/src/share/classes/java/awt/Component.java
+++ jdk/src/share/classes/java/awt/Component.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -975,6 +975,17 @@ public abstract class Component implements ImageObserver, MenuContainer,
 
             public void revalidateSynchronously(Component comp) {
                 comp.revalidateSynchronously();
+            }
+
+            @Override
+            public void createBufferStrategy(Component comp, int numBuffers,
+                    BufferCapabilities caps) throws AWTException {
+                comp.createBufferStrategy(numBuffers, caps);
+            }
+
+            @Override
+            public BufferStrategy getBufferStrategy(Component comp) {
+                return comp.getBufferStrategy();
             }
         });
     }
