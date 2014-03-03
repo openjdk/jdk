bd9b40bbfb53164abbac90ad982636fdc76ae10d
Pavel Punegov <pavel.punegov@oracle.com>
2014-03-03 18:17:00 +0400

    6946101: ShellScaffold.sh based tests can issue "write error: Broken pipe" messages
    Reviewed-by: dcubed, sla

--- jdk/test/com/sun/jdi/NullLocalVariable.sh
+++ jdk/test/com/sun/jdi/NullLocalVariable.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Copyright (c) 2002, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2002, 2014 Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -59,7 +59,7 @@ dojdbCmds()
    cmd next			; $sleepcmd
    cmd next			; $sleepcmd
    cmd locals			; $sleepcmd
-   cmd cont
+   cmd allowExit cont
 }
 
 mysetup()
