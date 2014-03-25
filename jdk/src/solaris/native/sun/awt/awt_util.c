3e818dfe5b151d698962a4466d5ae78b2e7c2151
Petr Pchelko <pchelko@openjdk.java.net>
2014-03-25 16:31:02 +0400

    8031001: [Parfait] warnings from b121 for jdk/src/solaris/native/sun/awt: JNI-related warnings
    Reviewed-by: serb, anthony

--- jdk/src/solaris/native/sun/awt/awt_util.c
+++ jdk/src/solaris/native/sun/awt/awt_util.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -76,6 +76,7 @@ awtJNI_ThreadYield(JNIEnv *env) {
         Boolean err = FALSE;
         if (threadClass == NULL) {
             jclass tc = (*env)->FindClass(env, "java/lang/Thread");
+            CHECK_NULL(tc);
             threadClass = (*env)->NewGlobalRef(env, tc);
             (*env)->DeleteLocalRef(env, tc);
             if (threadClass != NULL) {
