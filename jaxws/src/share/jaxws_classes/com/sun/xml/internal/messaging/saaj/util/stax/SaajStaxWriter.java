d724c56d2c1425d6206782c65215eeee82745faa
Miroslav Kos <mkos@openjdk.java.net>
2014-04-28 14:05:34 -0700

    8040754: Break the circular dependency between SAAJ and JAXB
    Reviewed-by: chegar, mchung

--- jaxws/src/share/jaxws_classes/com/sun/xml/internal/messaging/saaj/util/stax/SaajStaxWriter.java
+++ jaxws/src/share/jaxws_classes/com/sun/xml/internal/messaging/saaj/util/stax/SaajStaxWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
  * questions.
  */
 
-package com.sun.xml.internal.ws.api.message.saaj;
+package com.sun.xml.internal.messaging.saaj.util.stax;
 
 import java.util.Arrays;
 import java.util.Iterator;
