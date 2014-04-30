c0f886ec6561a76039029a28c00d0b7d7def7548
Shrinivas Joshi <shrinivas.joshi@oracle.com>
2014-04-30 14:14:01 -0700

    8035936: SIGBUS in StubRoutines::aesencryptBlock, solaris-sparc
    Fix the arbitrary alignment issue in SPARC AES crypto stub routines.
    
    Reviewed-by: kvn, iveresov

--- hotspot/src/share/vm/opto/runtime.cpp
+++ hotspot/src/share/vm/opto/runtime.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -868,7 +868,7 @@ const TypeFunc* OptoRuntime::updateBytesCRC32_Type() {
   return TypeFunc::make(domain, range);
 }
 
-// for cipherBlockChaining calls of aescrypt encrypt/decrypt, four pointers and a length, returning void
+// for cipherBlockChaining calls of aescrypt encrypt/decrypt, four pointers and a length, returning int
 const TypeFunc* OptoRuntime::cipherBlockChaining_aescrypt_Type() {
   // create input type (domain)
   int num_args      = 5;
