7f721c111bc24eb6d3a2a682fb46fc59c67f64b4
Staffan Larsen <sla@openjdk.org>
2014-05-20 20:35:39 +0200

    8043314: Fix for JDK-8041934 causes assert(is_interpreted_frame()) failed: interpreted frame expected
    Back out fix for JDK-8041934
    
    Reviewed-by: coleenp, sspitsyn

--- hotspot/src/share/vm/runtime/sharedRuntime.hpp
+++ hotspot/src/share/vm/runtime/sharedRuntime.hpp
@@ -263,9 +263,6 @@ class SharedRuntime: AllStatic {
   static int dtrace_method_entry(JavaThread* thread, Method* m);
   static int dtrace_method_exit(JavaThread* thread, Method* m);
 
-  // jvmti notification
-  static int jvmti_method_exit(JavaThread* thread, Method* m);
-
   // Utility method for retrieving the Java thread id, returns 0 if the
   // thread is not a well formed Java thread.
   static jlong get_java_tid(Thread* thread);
