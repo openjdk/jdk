d3217c7b396e9733804ac842b7fbad0a477ae78a
Alexander Stepanov <alexander.v.stepanov@oracle.com>
2014-06-03 17:55:58 +0400

    8040081: Tidy warnings cleanup for java.applet
    Reviewed-by: pchelko

--- jdk/src/share/classes/java/applet/Applet.java
+++ jdk/src/share/classes/java/applet/Applet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -86,6 +86,7 @@ public class Applet extends Panel {
 
     /**
      * Read an applet from an object input stream.
+     * @param  s  an object input stream.
      * @exception HeadlessException if
      * <code>GraphicsEnvironment.isHeadless()</code> returns
      * <code>true</code>
