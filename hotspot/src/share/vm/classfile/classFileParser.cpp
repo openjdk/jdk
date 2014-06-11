b6a2f946f6c80c2dc54a3d315ea3dae5d30b6829
Harold Seigel <hseigel@openjdk.org>
2014-06-11 09:58:23 -0400

    8031819: Remove legacy jdk checks and code
    Delete old jdk specific code.
    
    Reviewed-by: dholmes, coleenp, zgu, lfoltan

--- hotspot/src/share/vm/classfile/classFileParser.cpp
+++ hotspot/src/share/vm/classfile/classFileParser.cpp
@@ -4667,9 +4667,7 @@ bool ClassFileParser::has_illegal_visibility(jint flags) {
 }
 
 bool ClassFileParser::is_supported_version(u2 major, u2 minor) {
-  u2 max_version =
-    JDK_Version::is_gte_jdk17x_version() ? JAVA_MAX_SUPPORTED_VERSION :
-    (JDK_Version::is_gte_jdk16x_version() ? JAVA_6_VERSION : JAVA_1_5_VERSION);
+  u2 max_version = JAVA_MAX_SUPPORTED_VERSION;
   return (major >= JAVA_MIN_SUPPORTED_VERSION) &&
          (major <= max_version) &&
          ((major != max_version) ||
