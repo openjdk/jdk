a657a5dc8e25ce9535c4711d2ad1bf595d5a3964
Mikhail Cherkasov <mcherkas@openjdk.java.net>
2014-07-10 18:46:40 +0400

    4991647: PNGMetadata.getAsTree() sets bitDepth to invalid value
    Reviewed-by: prr, bae

--- jdk/src/share/classes/com/sun/imageio/plugins/png/PNGMetadata.java
+++ jdk/src/share/classes/com/sun/imageio/plugins/png/PNGMetadata.java
@@ -1254,8 +1254,11 @@ public class PNGMetadata extends IIOMetadata implements Cloneable {
             if (name.equals("IHDR")) {
                 IHDR_width = getIntAttribute(node, "width");
                 IHDR_height = getIntAttribute(node, "height");
-                IHDR_bitDepth = getEnumeratedAttribute(node, "bitDepth",
-                                                       IHDR_bitDepths);
+                IHDR_bitDepth =
+                        Integer.valueOf(IHDR_bitDepths[
+                                getEnumeratedAttribute(node,
+                                                    "bitDepth",
+                                                    IHDR_bitDepths)]);
                 IHDR_colorType = getEnumeratedAttribute(node, "colorType",
                                                         IHDR_colorTypeNames);
                 IHDR_compressionMethod =
