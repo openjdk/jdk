ce9bf10f69c6b677bc9635314b9b4267d4f46387
Valerie Peng <valeriep@openjdk.java.net>
2014-07-23 21:50:21 +0000

    8035166: Remove dependency on EC classes from pkcs11 provider
    Move the sun.security.ec.ECParameters and its related classes to sun.security.util package.
    
    Reviewed-by: mullan, vinnie

--- jdk/src/share/classes/sun/security/util/CurveDB.java
+++ jdk/src/share/classes/sun/security/util/CurveDB.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
  * questions.
  */
 
-package sun.security.ec;
+package sun.security.util;
 
 import java.math.BigInteger;
 
@@ -54,10 +54,10 @@ public class CurveDB {
 
     private static Collection<? extends NamedCurve> specCollection;
 
-    static final String SPLIT_PATTERN = ",|\\[|\\]";
+    public static final String SPLIT_PATTERN = ",|\\[|\\]";
 
     // Used by SunECEntries
-    static Collection<? extends NamedCurve>getSupportedCurves() {
+    public static Collection<? extends NamedCurve>getSupportedCurves() {
         return specCollection;
     }
 
