7c8053cd971a28b8d6016c7507652a13be5546a0
Michael McMahon <michaelm@openjdk.org>
2014-08-26 10:10:58 +0100

    8055747: Move SimpleSSLContext to jdk/testlibrary
    Reviewed-by: chegar

--- jdk/test/com/sun/net/httpserver/Test6a.java
+++ jdk/test/com/sun/net/httpserver/Test6a.java
@@ -24,6 +24,8 @@
 /**
  * @test
  * @bug 6270015
+ * @library /lib/testlibrary/
+ * @build jdk.testlibrary.SimpleSSLContext
  * @run main/othervm Test6a
  * @summary  Light weight HTTP server
  */
@@ -34,6 +36,7 @@ import java.util.concurrent.*;
 import java.io.*;
 import java.net.*;
 import javax.net.ssl.*;
+import jdk.testlibrary.SimpleSSLContext;
 
 /**
  * Test https POST large file via chunked encoding (unusually small chunks)
@@ -47,7 +50,7 @@ public class Test6a extends Test {
         HttpsServer server = HttpsServer.create (addr, 0);
         HttpContext ctx = server.createContext ("/test", handler);
         ExecutorService executor = Executors.newCachedThreadPool();
-        SSLContext ssl = new SimpleSSLContext(System.getProperty("test.src")).get();
+        SSLContext ssl = new SimpleSSLContext().get();
         server.setExecutor (executor);
         server.setHttpsConfigurator(new HttpsConfigurator (ssl));
         server.start ();
