88eb5fb4d65e76e96769c85e66b398870d04ca70
Bill Pittore <bill.pittore@oracle.com>
2014-09-02 21:27:08 -0400

    8046210: Missing memory barrier when reading init_lock
    Reviewed-by: fparain, dcubed, mdoerr

--- hotspot/src/share/vm/oops/instanceKlass.cpp
+++ hotspot/src/share/vm/oops/instanceKlass.cpp
@@ -460,6 +460,8 @@ objArrayOop InstanceKlass::signers() const {
 oop InstanceKlass::init_lock() const {
   // return the init lock from the mirror
   oop lock = java_lang_Class::init_lock(java_mirror());
+  // Prevent reordering with any access of initialization state
+  OrderAccess::loadload();
   assert((oop)lock != NULL || !is_not_initialized(), // initialized or in_error state
          "only fully initialized state can have a null lock");
   return lock;
