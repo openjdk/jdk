b4401ebed904c49d6de675d9a6cd3b9275f3e69c
Eric McCorkle <emc@openjdk.org>
2014-09-22 17:09:33 -0400

    8048614: Add TypeMetadata to contain type annotations and other type information
    Implement general framework for metadata on types
    
    Reviewed-by: mcimadamore

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Infer.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Infer.java
@@ -418,7 +418,7 @@ public class Infer {
             List<Type> upperBounds = uv.getBounds(InferenceBound.UPPER);
             if (Type.containsAny(upperBounds, vars)) {
                 TypeSymbol fresh_tvar = new TypeVariableSymbol(Flags.SYNTHETIC, uv.qtype.tsym.name, null, uv.qtype.tsym.owner);
-                fresh_tvar.type = new TypeVar(fresh_tvar, types.makeCompoundType(uv.getBounds(InferenceBound.UPPER)), null, Type.noAnnotations);
+                fresh_tvar.type = new TypeVar(fresh_tvar, types.makeCompoundType(uv.getBounds(InferenceBound.UPPER)), null);
                 todo.append(uv);
                 uv.inst = fresh_tvar.type;
             } else if (upperBounds.nonEmpty()) {
@@ -1562,8 +1562,7 @@ public class Infer {
                         infer.syms.botType;
                 CapturedType prevCaptured = (CapturedType)uv.qtype;
                 return new CapturedType(prevCaptured.tsym.name, prevCaptured.tsym.owner,
-                                        upper, lower, prevCaptured.wildcard,
-                                        Type.noAnnotations);
+                                        upper, lower, prevCaptured.wildcard);
             }
         };
 
