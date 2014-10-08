23f4c4d5abe9cc527bd9ac6f6859b87c6f0f9a64
Lance Andersen <lancea@openjdk.java.net>
2014-10-08 11:51:36 -0400

    8059870: RowsetFactoryTests & RowSetProviderTests failing
    Reviewed-by: dfuchs

--- jdk/test/javax/sql/testng/test/rowset/RowSetFactoryTests.java
+++ jdk/test/javax/sql/testng/test/rowset/RowSetFactoryTests.java
@@ -50,7 +50,7 @@ public class RowSetFactoryTests extends BaseTest {
      * Validate that the RowSetFactory returned by RowSetProvider.newFactory()
      * returns the correct RowSet implementations
      */
-    @Test(dataProvider = "RowSetValues", enabled = false)
+    @Test(dataProvider = "RowSetValues", enabled = true)
     public void test(RowSetFactory rsf, String impl) throws SQLException {
         validateRowSetImpl(rsf, impl);
     }
