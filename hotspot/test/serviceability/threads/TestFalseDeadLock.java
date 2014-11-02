7d40eb095f3ffd942e7c7db23400ea3f79ab04eb
Sergei Kovalev <sergei.kovalev@oracle.com>
2014-11-02 18:43:38 +0300

    8044186: Introduce a reproducible random generator
    Reviewed-by: kvn, iveresov, iignatyev

--- hotspot/test/serviceability/threads/TestFalseDeadLock.java
+++ hotspot/test/serviceability/threads/TestFalseDeadLock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,6 +21,7 @@
  * questions.
  */
 
+import com.oracle.java.testlibrary.Utils;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.util.Random;
@@ -29,6 +30,7 @@ import java.util.Random;
  * @test
  * @bug 8016304
  * @summary Make sure no deadlock is reported for this program which has no deadlocks.
+ * @library /testlibrary
  * @run main/othervm TestFalseDeadLock
  */
 
@@ -65,7 +67,7 @@ public class TestFalseDeadLock {
 
     public static class Test implements Runnable {
         public void run() {
-            Random r = new Random();
+            Random r = Utils.getRandomInstance();
             while (running) {
                 try {
                     synchronized (this) {
