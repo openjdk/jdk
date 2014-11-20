9ca1379f55163dc57468e944fa4ff8dfdb7483de
Vicente Romero <vromero@openjdk.java.net>
2014-11-20 14:05:39 -0800

    8063052: Inference chokes on wildcard derived from method reference
    Reviewed-by: mcimadamore

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Resolve.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Resolve.java
@@ -3172,7 +3172,7 @@ public class Resolve {
             super(referenceTree, name, site, argtypes.tail, typeargtypes, maxPhase);
             if (site.isRaw() && !argtypes.head.hasTag(NONE)) {
                 Type asSuperSite = types.asSuper(argtypes.head, site.tsym);
-                this.site = asSuperSite;
+                this.site = types.capture(asSuperSite);
             }
         }
 
