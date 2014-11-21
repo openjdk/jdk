ca85e583e9c19d810645213b5cbb809768253bc7
Sean Mullan <mullan@openjdk.org>
2014-11-21 15:23:36 -0500

    8046949: Generify the javax.xml.crypto API
    Reviewed-by: xuelei

--- jdk/src/java.xml.crypto/share/classes/javax/xml/crypto/dsig/keyinfo/RetrievalMethod.java
+++ jdk/src/java.xml.crypto/share/classes/javax/xml/crypto/dsig/keyinfo/RetrievalMethod.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -80,8 +80,7 @@ public interface RetrievalMethod extends URIReference, XMLStructure {
      * @return an unmodifiable list of <code>Transform</code> objects (may be
      *    empty but never <code>null</code>).
      */
-    @SuppressWarnings("rawtypes")
-    List getTransforms();
+    List<Transform> getTransforms();
 
     /**
      * Returns the URI of the referenced <code>KeyInfo</code> information.
