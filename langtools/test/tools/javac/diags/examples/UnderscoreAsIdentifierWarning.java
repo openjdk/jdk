889ccc6260b9015328a958de15eacc4e3848e33e
Jan Lahoda <jlahoda@openjdk.java.net>
2014-12-08 18:02:07 +0100

    8061549: Disallow _ as a one-character identifier
    Underscore is no longer a one-charater identifier with -source 9
    
    Reviewed-by: mcimadamore, jjg

--- langtools/test/tools/javac/diags/examples/UnderscoreAsIdentifierWarning.java
+++ langtools/test/tools/javac/diags/examples/UnderscoreAsIdentifierWarning.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2014, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,7 +22,8 @@
  */
 
 // key: compiler.warn.underscore.as.identifier
+// options: -source 8 -Xlint:-options
 
-class UnderscoreAsIdentifier {
+class UnderscoreAsIdentifierWarning {
     String _ = null;
 }
