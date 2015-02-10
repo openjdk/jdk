3bf803f0c91e899cf6fb218e4b96fcfa2b105a4d
Amy Lu <amlu@openjdk.org>
2015-02-10 12:28:02 -0500

    8069255: Suppress deprecation warnings in jdk.rmic module (jdk repo)
    Reviewed-by: rriggs

--- jdk/src/jdk.rmic/share/classes/sun/rmi/rmic/BatchEnvironment.java
+++ jdk/src/jdk.rmic/share/classes/sun/rmi/rmic/BatchEnvironment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,6 +61,7 @@ import sun.tools.java.ClassPath;
  * they are subject to change or removal without notice.
  */
 
+@SuppressWarnings("deprecation")
 public class BatchEnvironment extends sun.tools.javac.BatchEnvironment {
 
     /** instance of Main which created this environment */
