51ced1478809b8f3aee2edcf9ed068bab70eca45
Stefan Karlsson <stefank@openjdk.java.net>
2015-02-18 10:28:27 +0100

    8073389: Remove the include of resourceArea.hpp from classFileParser.hpp
    Reviewed-by: coleenp, dholmes

--- hotspot/src/share/vm/code/dependencies.hpp
+++ hotspot/src/share/vm/code/dependencies.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,6 +31,7 @@
 #include "classfile/systemDictionary.hpp"
 #include "code/compressedStream.hpp"
 #include "code/nmethod.hpp"
+#include "memory/resourceArea.hpp"
 #include "utilities/growableArray.hpp"
 #include "utilities/hashtable.hpp"
 
