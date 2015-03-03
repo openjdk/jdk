33505a8b5216bf2670d31d3f005e4a869615e46f
Jason Uh <juh@openjdk.org>
2015-03-03 14:16:49 -0800

    8054037: Improve tracing for java.security.debug=certpath 8055207: keystore and truststore debug output could be much better
    Reviewed-by: mullan, coffeys, jnimeh

--- jdk/src/java.base/share/classes/sun/security/ssl/ClientHandshaker.java
+++ jdk/src/java.base/share/classes/sun/security/ssl/ClientHandshaker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -817,6 +817,11 @@ final class ClientHandshaker extends Handshaker {
                     m1 = new CertificateMsg(new X509Certificate [0]);
                 } else {
                     warningSE(Alerts.alert_no_certificate);
+                }
+                if (debug != null && Debug.isOn("handshake")) {
+                    System.out.println(
+                        "Warning: no suitable certificate found - " +
+                        "continuing without client authentication");
                 }
             }
 
