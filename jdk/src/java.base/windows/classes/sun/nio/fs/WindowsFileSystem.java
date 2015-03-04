9b84b7d367c1ab6343030699639c595d61b0accc
Brian Burkhalter <bpb@openjdk.java.net>
2015-03-04 15:05:41 -0800

    8073445: (fs) FileSystem.getPathMatcher(...) should check syntax component without regard to case
    Change String equals() to equalsIgnoreCase() where needed.
    
    Reviewed-by: alanb

--- jdk/src/java.base/windows/classes/sun/nio/fs/WindowsFileSystem.java
+++ jdk/src/java.base/windows/classes/sun/nio/fs/WindowsFileSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -290,10 +290,10 @@ class WindowsFileSystem
         String input = syntaxAndInput.substring(pos+1);
 
         String expr;
-        if (syntax.equals(GLOB_SYNTAX)) {
+        if (syntax.equalsIgnoreCase(GLOB_SYNTAX)) {
             expr = Globs.toWindowsRegexPattern(input);
         } else {
-            if (syntax.equals(REGEX_SYNTAX)) {
+            if (syntax.equalsIgnoreCase(REGEX_SYNTAX)) {
                 expr = input;
             } else {
                 throw new UnsupportedOperationException("Syntax '" + syntax +
