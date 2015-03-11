bff9aa6c0f7ac2951a7927cb40ecccd519ec964d
Chris Plummer <cjplummer@openjdk.java.net>
2015-03-10 19:56:18 -0700

    8069111: Investigate NMT detail tracking support for 32bit ARM
    Fix native stack walking issues arm arm32. Disable on thumb2. Removed support for NMT without NMT detail.
    
    Reviewed-by: dholmes, bdelsart

--- test/lib/sun/hotspot/WhiteBox.java
+++ test/lib/sun/hotspot/WhiteBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -106,7 +106,6 @@ public class WhiteBox {
   public native void NMTUncommitMemory(long addr, long size);
   public native void NMTReleaseMemory(long addr, long size);
   public native long NMTMallocWithPseudoStack(long size, int index);
-  public native boolean NMTIsDetailSupported();
   public native boolean NMTChangeTrackingLevel();
   public native int NMTGetHashSize();
 
