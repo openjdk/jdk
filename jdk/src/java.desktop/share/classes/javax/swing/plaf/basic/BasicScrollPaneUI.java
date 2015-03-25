a76b85f2bbd5c00a3771549483e5a28efe02902d
Alexander Scherbatiy <alexsch@openjdk.org>
2015-03-25 16:31:05 +0400

    8033000: No Horizontal Mouse Wheel Support In
    Reviewed-by: serb, azvegint

--- jdk/src/java.desktop/share/classes/javax/swing/plaf/basic/BasicScrollPaneUI.java
+++ jdk/src/java.desktop/share/classes/javax/swing/plaf/basic/BasicScrollPaneUI.java
@@ -962,6 +962,12 @@ public class BasicScrollPaneUI
                         return;
                     }
                     orientation = SwingConstants.HORIZONTAL;
+                } else if(e.isShiftDown()){
+                    JScrollBar hScroll = scrollpane.getHorizontalScrollBar();
+                    if (hScroll != null && hScroll.isVisible()) {
+                        toScroll = hScroll;
+                        orientation = SwingConstants.HORIZONTAL;
+                    }
                 }
 
                 e.consume();
