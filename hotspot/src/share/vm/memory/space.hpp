602b7d79e988f22e274b19ec3fd29782609907e8
Stefan Karlsson <stefank@openjdk.org>
2015-03-25 10:13:56 +0100

    8075829: Move CSpaceCounters implementation to cSpaceCounters.cpp
    Reviewed-by: jwilhelm, brutisso

--- hotspot/src/share/vm/memory/space.hpp
+++ hotspot/src/share/vm/memory/space.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -195,7 +195,7 @@ class Space: public CHeapObj<mtGC> {
   // structure supporting these calls, possibly speeding up future calls.
   // The default implementation, however, is simply to call the const
   // version.
-  inline virtual HeapWord* block_start(const void* p);
+  virtual HeapWord* block_start(const void* p);
 
   // Requires "addr" to be the start of a chunk, and returns its size.
   // "addr + size" is required to be the start of a new chunk, or the end
