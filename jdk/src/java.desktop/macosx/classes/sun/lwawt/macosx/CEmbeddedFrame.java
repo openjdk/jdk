795c4bd7363a3850099f8e5f615cce2fc9ce52dd
Anton Nashatyrev <anashaty@openjdk.org>
2015-03-30 17:46:58 +0300

    8074481: [macosx] Menu items are appearing on top of other windows
    Reviewed-by: ant, serb

--- jdk/src/java.desktop/macosx/classes/sun/lwawt/macosx/CEmbeddedFrame.java
+++ jdk/src/java.desktop/macosx/classes/sun/lwawt/macosx/CEmbeddedFrame.java
@@ -164,7 +164,7 @@ public class CEmbeddedFrame extends EmbeddedFrame {
         }
         // ignore focus "lost" native request as it may mistakenly
         // deactivate active window (see 8001161)
-        if (globalFocusedWindow == this && parentWindowActive) {
+        if (globalFocusedWindow == this) {
             responder.handleWindowFocusEvent(parentWindowActive, null);
         }
     }
