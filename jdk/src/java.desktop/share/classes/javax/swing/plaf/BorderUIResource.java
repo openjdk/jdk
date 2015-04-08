2bc44815c561b2beed24c8098f2e5379d4ca4a3f
Joe Darcy <darcy@openjdk.org>
2015-04-08 10:50:50 -0700

    8077094: Fix missing doclint warnings in the javax.swing.plaf package
    Reviewed-by: serb

--- jdk/src/java.desktop/share/classes/javax/swing/plaf/BorderUIResource.java
+++ jdk/src/java.desktop/share/classes/javax/swing/plaf/BorderUIResource.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -38,7 +38,7 @@ import javax.swing.Icon;
 import javax.swing.plaf.UIResource;
 
 
-/*
+/**
  * A Border wrapper class which implements UIResource.  UI
  * classes which set border properties should use this class
  * to wrap any borders specified as defaults.
@@ -67,6 +67,10 @@ public class BorderUIResource implements Border, UIResource, Serializable
     static Border raisedBevel;
     static Border blackLine;
 
+    /**
+     * Returns a etched border UI resource.
+     * @return a etched border UI resource
+     */
     public static Border getEtchedBorderUIResource() {
         if (etched == null) {
             etched = new EtchedBorderUIResource();
@@ -74,6 +78,10 @@ public class BorderUIResource implements Border, UIResource, Serializable
         return etched;
     }
 
+    /**
+     * Returns a lowered bevel border UI resource.
+     * @return a lowered bevel border UI resource
+     */
     public static Border getLoweredBevelBorderUIResource() {
         if (loweredBevel == null) {
             loweredBevel = new BevelBorderUIResource(BevelBorder.LOWERED);
@@ -81,6 +89,10 @@ public class BorderUIResource implements Border, UIResource, Serializable
         return loweredBevel;
     }
 
+    /**
+     * Returns a raised bevel border UI resource.
+     * @return a raised bevel border UI resource
+     */
     public static Border getRaisedBevelBorderUIResource() {
         if (raisedBevel == null) {
             raisedBevel = new BevelBorderUIResource(BevelBorder.RAISED);
@@ -88,6 +100,10 @@ public class BorderUIResource implements Border, UIResource, Serializable
         return raisedBevel;
     }
 
+    /**
+     * Returns a black line border UI resource.
+     * @return a black line border UI resource
+     */
     public static Border getBlackLineBorderUIResource() {
         if (blackLine == null) {
             blackLine = new LineBorderUIResource(Color.black);
@@ -122,7 +138,15 @@ public class BorderUIResource implements Border, UIResource, Serializable
         return delegate.isBorderOpaque();
     }
 
+    /**
+     * A compound border UI resource.
+     */
     public static class CompoundBorderUIResource extends CompoundBorder implements UIResource {
+        /**
+         * Constructs a {@code CompoundBorderUIResource}.
+         * @param outsideBorder the outside border
+         * @param insideBorder the inside border
+         */
         @ConstructorProperties({"outsideBorder", "insideBorder"})
         public CompoundBorderUIResource(Border outsideBorder, Border insideBorder) {
             super(outsideBorder, insideBorder);
@@ -130,23 +154,49 @@ public class BorderUIResource implements Border, UIResource, Serializable
 
     }
 
+    /**
+     * An empty border UI resource.
+     */
     public static class EmptyBorderUIResource extends EmptyBorder implements UIResource {
 
+        /**
+         * Constructs a {@code EmptyBorderUIResource}.
+         * @param top the top inset of the border
+         * @param left the left inset of the border
+         * @param bottom the bottom inset of the border
+         * @param right the right inset of the border
+         */
         public EmptyBorderUIResource(int top, int left, int bottom, int right)   {
             super(top, left, bottom, right);
         }
+        /**
+         * Constructs a {@code EmptyBorderUIResource}.
+         * @param insets the insets of the border
+         */
         @ConstructorProperties({"borderInsets"})
         public EmptyBorderUIResource(Insets insets) {
             super(insets);
         }
     }
 
+    /**
+     * A line border UI resource.
+     */
     public static class LineBorderUIResource extends LineBorder implements UIResource {
 
+        /**
+         * Constructs a {@code LineBorderUIResource}.
+         * @param color the color for the border
+         */
         public LineBorderUIResource(Color color) {
             super(color);
         }
 
+        /**
+         * Constructs a {@code LineBorderUIResource}.
+         * @param color the color for the border
+         * @param thickness the thickness of the border
+         */
         @ConstructorProperties({"lineColor", "thickness"})
         public LineBorderUIResource(Color color, int thickness)  {
             super(color, thickness);
@@ -154,16 +204,37 @@ public class BorderUIResource implements Border, UIResource, Serializable
     }
 
 
+    /**
+     * A bevel border UI resource.
+     */
     public static class BevelBorderUIResource extends BevelBorder implements UIResource {
 
+        /**
+         * Constructs a {@code BevelBorderUIResource}.
+         * @param bevelType the type of bevel for the border
+         */
         public BevelBorderUIResource(int bevelType) {
             super(bevelType);
         }
 
+        /**
+         * Constructs a {@code BevelBorderUIResource}.
+         * @param bevelType the type of bevel for the border
+         * @param highlight the color to use for the bevel highlight
+         * @param shadow the color to use for the bevel shadow
+         */
         public BevelBorderUIResource(int bevelType, Color highlight, Color shadow) {
             super(bevelType, highlight, shadow);
         }
 
+        /**
+         * Constructs a {@code BevelBorderUIResource}.
+         * @param bevelType the type of bevel for the border
+         * @param highlightOuter the color to use for the bevel outer highlight
+         * @param highlightInner the color to use for the bevel inner highlight
+         * @param shadowOuter the color to use for the bevel outer shadow
+         * @param shadowInner the color to use for the bevel inner shadow
+         */
         @ConstructorProperties({"bevelType", "highlightOuterColor", "highlightInnerColor", "shadowOuterColor", "shadowInnerColor"})
         public BevelBorderUIResource(int bevelType,
                                      Color highlightOuter, Color highlightInner,
@@ -172,57 +243,124 @@ public class BorderUIResource implements Border, UIResource, Serializable
         }
     }
 
+    /**
+     * An etched border UI resource.
+     */
     public static class EtchedBorderUIResource extends EtchedBorder implements UIResource {
 
+        /**
+         * Constructs an {@code EtchedBorderUIResource}.
+         */
         public EtchedBorderUIResource()    {
             super();
         }
 
+        /**
+         * Constructs an {@code EtchedBorderUIResource}.
+         * @param etchType the type of etch to be drawn by the border
+         */
         public EtchedBorderUIResource(int etchType)    {
             super(etchType);
         }
 
+        /**
+         * Constructs an {@code EtchedBorderUIResource}.
+         * @param highlight the color to use for the etched highlight
+         * @param shadow the color to use for the etched shadow
+         */
         public EtchedBorderUIResource(Color highlight, Color shadow)    {
             super(highlight, shadow);
         }
 
+        /**
+         * Constructs an {@code EtchedBorderUIResource}.
+         * @param etchType the type of etch to be drawn by the border
+         * @param highlight the color to use for the etched highlight
+         * @param shadow the color to use for the etched shadow
+         */
         @ConstructorProperties({"etchType", "highlightColor", "shadowColor"})
         public EtchedBorderUIResource(int etchType, Color highlight, Color shadow)    {
             super(etchType, highlight, shadow);
         }
     }
 
+    /**
+     * A matte border UI resource.
+     */
     public static class MatteBorderUIResource extends MatteBorder implements UIResource {
 
+        /**
+         * Constructs a {@code MatteBorderUIResource}.
+         * @param top the top inset of the border
+         * @param left the left inset of the border
+         * @param bottom the bottom inset of the border
+         * @param right the right inset of the border
+         * @param color the color rendered for the border
+         */
         public MatteBorderUIResource(int top, int left, int bottom, int right,
                                      Color color)   {
             super(top, left, bottom, right, color);
         }
 
+        /**
+         * Constructs a {@code MatteBorderUIResource}.
+         * @param top the top inset of the border
+         * @param left the left inset of the border
+         * @param bottom the bottom inset of the border
+         * @param right the right inset of the border
+         * @param tileIcon the icon to be used for tiling the border
+         */
         public MatteBorderUIResource(int top, int left, int bottom, int right,
                                      Icon tileIcon)   {
             super(top, left, bottom, right, tileIcon);
         }
 
+        /**
+         * Constructs a {@code MatteBorderUIResource}.
+         * @param tileIcon the icon to be used for tiling the border
+         */
         public MatteBorderUIResource(Icon tileIcon)   {
             super(tileIcon);
         }
     }
 
+    /**
+     * A titled border UI resource.
+     */
     public static class TitledBorderUIResource extends TitledBorder implements UIResource {
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param title the title the border should display
+         */
         public TitledBorderUIResource(String title)     {
             super(title);
         }
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param border the border
+         */
         public TitledBorderUIResource(Border border)       {
             super(border);
         }
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param border the border
+         * @param title the title the border should display
+         */
         public TitledBorderUIResource(Border border, String title) {
             super(border, title);
         }
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param border the border
+         * @param title the title the border should display
+         * @param titleJustification the justification fro the title
+         * @param titlePosition the position for the title
+         */
         public TitledBorderUIResource(Border border,
                         String title,
                         int titleJustification,
@@ -230,6 +368,14 @@ public class BorderUIResource implements Border, UIResource, Serializable
             super(border, title, titleJustification, titlePosition);
         }
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param border the border
+         * @param title the title the border should display
+         * @param titleJustification the justification fro the title
+         * @param titlePosition the position for the title
+         * @param titleFont the font for rendering the title
+         */
         public TitledBorderUIResource(Border border,
                         String title,
                         int titleJustification,
@@ -238,6 +384,15 @@ public class BorderUIResource implements Border, UIResource, Serializable
             super(border, title, titleJustification, titlePosition, titleFont);
         }
 
+        /**
+         * Constructs a {@code TitledBorderUIResource}.
+         * @param border the border
+         * @param title the title the border should display
+         * @param titleJustification the justification fro the title
+         * @param titlePosition the position for the title
+         * @param titleFont the font for rendering the title
+         * @param titleColor the color of the title
+         */
         @ConstructorProperties({"border", "title", "titleJustification", "titlePosition", "titleFont", "titleColor"})
         public TitledBorderUIResource(Border border,
                         String title,
