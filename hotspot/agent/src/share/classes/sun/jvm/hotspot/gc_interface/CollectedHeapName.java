1ffb3549f470bd27e203f73f12b273b354876d87
Per Liden <pliden@openjdk.java.net>
2015-04-08 15:19:49 +0200

    8076534: CollectedHeapName in SA agent incorrect
    Reviewed-by: dsamersoff, sla, tschatzl

--- hotspot/agent/src/share/classes/sun/jvm/hotspot/gc_interface/CollectedHeapName.java
+++ hotspot/agent/src/share/classes/sun/jvm/hotspot/gc_interface/CollectedHeapName.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,6 @@ public class CollectedHeapName {
 
   private CollectedHeapName(String name) { this.name = name; }
 
-  public static final CollectedHeapName ABSTRACT = new CollectedHeapName("abstract");
   public static final CollectedHeapName GEN_COLLECTED_HEAP = new CollectedHeapName("GenCollectedHeap");
   public static final CollectedHeapName G1_COLLECTED_HEAP = new CollectedHeapName("G1CollectedHeap");
   public static final CollectedHeapName PARALLEL_SCAVENGE_HEAP = new CollectedHeapName("ParallelScavengeHeap");
