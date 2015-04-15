72c7f51e1ac15abe96c34253e85232c7bf91c6e1
Kim Barrett <kbarrett@openjdk.java.net>
2015-04-15 16:37:57 -0400

    8075466: SATB queue pre-filter verify found reclaimed humongous object
    Removed pre-filter verify, and made filtering more careful.
    
    Reviewed-by: brutisso, tschatzl

--- hotspot/src/share/vm/gc_implementation/g1/satbQueue.hpp
+++ hotspot/src/share/vm/gc_implementation/g1/satbQueue.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -72,8 +72,6 @@ public:
   void print(const char* name);
   static void print(const char* name, void** buf, size_t index, size_t sz);
 #endif // PRODUCT
-
-  void verify_oops_in_buffer() NOT_DEBUG_RETURN;
 };
 
 class SATBMarkQueueSet: public PtrQueueSet {
