183dd96318e633e0c7d78a1451e5c33c2609c93d
Jaroslav Bachorik <jbachorik@openjdk.org>
2015-05-26 11:57:51 +0200

    8080663: Use sun.misc.SharedSecrets to allow access from java.management to @ConstructorProperties
    Reviewed-by: alanb, mchung, dfuchs

--- jdk/src/java.base/share/classes/sun/misc/JavaBeansAccess.java
+++ jdk/src/java.base/share/classes/sun/misc/JavaBeansAccess.java
@@ -25,8 +25,26 @@
 
 package sun.misc;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
-public interface JavaBeansIntrospectorAccess {
+public interface JavaBeansAccess {
+    /**
+     * Returns the getter method for a property of the given name
+     * @param clazz The JavaBeans class
+     * @param property The property name
+     * @return The resolved property getter method
+     * @throws Exception
+     */
     Method getReadMethod(Class<?> clazz, String property) throws Exception;
+
+    /**
+     * Return the <b>value</b> attribute of the associated
+     * <code>@ConstructorProperties</code> annotation if that is present.
+     * @param ctr The constructor to extract the annotation value from
+     * @return The {@code value} attribute of the <code>@ConstructorProperties</code>
+     *         annotation or {@code null} if the constructor is not annotated by
+     *         this annotation or the annotation is not accessible.
+     */
+    String[] getConstructorPropertiesValue(Constructor<?> ctr);
 }
