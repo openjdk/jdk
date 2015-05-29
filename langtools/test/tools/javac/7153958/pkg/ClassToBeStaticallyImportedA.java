783a654b1781d2f2e143b8c291cf1ef96f6e693c
Vicente Romero <vromero@openjdk.org>
2015-05-29 14:04:57 -0700

    8073372: Redundant CONSTANT_Class entry not generated for inlined constant
    Reviewed-by: jjg

--- langtools/test/tools/javac/7153958/pkg/ClassToBeStaticallyImportedA.java
+++ langtools/test/tools/javac/7153958/pkg/ClassToBeStaticallyImportedA.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,6 @@
  */
 package pkg;
 
-public class ClassToBeStaticallyImported {
-    public static final int staticField = 1;
+public class ClassToBeStaticallyImportedA {
+    public static final int staticFieldA = 1;
 }
