07c1aa7e07dbc1148e593b1c970d099eaecfe683
Michael Haupt <mhaupt@openjdk.java.net>
2015-06-02 10:40:10 +0200

    8081603: erroneous dot file generated from Nashorn --print-code
    Emit a dot label string-conformant line break instead of a hard one to avoid strings ranging across an EOL.
    
    Reviewed-by: attila, lagergren, sundar

--- nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/ir/debug/NashornTextifier.java
+++ nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/ir/debug/NashornTextifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1109,7 +1109,7 @@ public final class NashornTextifier extends Printer {
                 }
                 final String ex = catches.get(node);
                 if (ex != null) {
-                    sb.append("*** CATCH: ").append(ex).append(" ***\n");
+                    sb.append("*** CATCH: ").append(ex).append(" ***\\l");
                 }
                 sb.append(c);
                 sb.append("\"]\n");
