44033ae9bdd847f6cec17d410aa40fd1d9ea8138
Sergey Bylokhov <serb@openjdk.org>
2015-06-03 23:17:52 +0300

    8076312: The behavior of the javax.swing.SwingContainer.delegate contradicts spec
    Reviewed-by: alexsch, malenkov

--- jdk/test/java/beans/Introspector/4058433/TestSwingContainer.java
+++ jdk/test/java/beans/Introspector/4058433/TestSwingContainer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,24 +20,31 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 import java.beans.BeanDescriptor;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.util.Objects;
 import javax.swing.SwingContainer;
-/*
+
+/**
  * @test
  * @bug 4058433
  * @summary Tests the SwingContainer annotation
  * @author Sergey Malenkov
  */
 public class TestSwingContainer {
+
     public static void main(String[] args) throws Exception {
         test(X.class, null, null);
-        test(T.class, true, null);
-        test(D.class, true, "method");
-        test(F.class, false, null);
-        test(A.class, false, "method");
+        test(H.class, true, "");
+        test(G.class, true, "");
+        test(F.class, true, "method");
+        test(E.class, false, "");
+        test(D.class, false, "");
+        test(C.class, true, "");
+        test(B.class, false, "method");
+        test(A.class, true, "method");
     }
 
     private static void test(Class<?> type, Object iC, Object cD) throws Exception {
@@ -50,7 +57,7 @@ public class TestSwingContainer {
 
     private static void test(BeanDescriptor bd, String name, Object expected) {
         Object value = bd.getValue(name);
-        System.out.println(name + " = " + value);
+        System.out.println("\t" + name + " = " + value);
         if (!Objects.equals(value, expected)) {
             throw new Error(name + ": expected = " + expected + "; actual = " + value);
         }
@@ -60,18 +67,34 @@ public class TestSwingContainer {
     }
 
     @SwingContainer()
-    public static class T {
+    public static class H {
+    }
+
+    @SwingContainer(delegate = "")
+    public static class G {
     }
 
     @SwingContainer(delegate = "method")
-    public static class D {
-    }
-
-    @SwingContainer(false)
     public static class F {
     }
 
+    @SwingContainer(false)
+    public static class E {
+    }
+
+    @SwingContainer(value = false, delegate = "")
+    public static class D {
+    }
+
+    @SwingContainer(value = true, delegate = "")
+    public static class C {
+    }
+
     @SwingContainer(value = false, delegate = "method")
+    public static class B {
+    }
+
+    @SwingContainer(value = true, delegate = "method")
     public static class A {
     }
 }
