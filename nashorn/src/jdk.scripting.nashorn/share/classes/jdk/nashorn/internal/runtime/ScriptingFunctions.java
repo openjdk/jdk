d00cfaa9cbafbdfaf109958c94111cffe03bc367
Michael Haupt <mhaupt@openjdk.java.net>
2015-07-01 16:26:25 +0200

    8130127: streamline input parameter of Nashorn scripting $EXEC function
    Handle null and undefined correctly, do not coerce them to strings
    
    Reviewed-by: lagergren, sundar

--- nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/ScriptingFunctions.java
+++ nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/ScriptingFunctions.java
@@ -128,9 +128,9 @@ public final class ScriptingFunctions {
      * Nashorn extension: exec a string in a separate process.
      *
      * @param self   self reference
-     * @param args   string to execute, input and additional arguments, to be appended to {@code string}. Additional arguments can be passed as
-     *               either one JavaScript array, whose elements will be converted to strings; or as a sequence of
-     *               varargs, each of which will be converted to a string.
+     * @param args   string to execute, input and additional arguments, to be appended to {@code string}. Additional
+     *               arguments can be passed as either one JavaScript array, whose elements will be converted to
+     *               strings; or as a sequence of varargs, each of which will be converted to a string.
      *
      * @return output string from the request
      *
@@ -215,13 +215,13 @@ public final class ScriptingFunctions {
         errThread.start();
 
         // If input is present, pass on to process.
-        try (OutputStreamWriter outputStream = new OutputStreamWriter(process.getOutputStream())) {
-            if (input != UNDEFINED) {
+        if (!JSType.nullOrUndefined(input)) {
+            try (OutputStreamWriter outputStream = new OutputStreamWriter(process.getOutputStream())) {
                 final String in = JSType.toString(input);
                 outputStream.write(in, 0, in.length());
+            } catch (final IOException ex) {
+                // Process was not expecting input.  May be normal state of affairs.
             }
-        } catch (final IOException ex) {
-            // Process was not expecting input.  May be normal state of affairs.
         }
 
         // Wait for the process to complete.
