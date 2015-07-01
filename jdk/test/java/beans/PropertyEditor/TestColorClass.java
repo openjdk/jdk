efa84f658a465bfe648c345ee3100bc78606ece4
Joe Darcy <darcy@openjdk.org>
2015-06-30 17:48:06 -0700

    8081547: Prepare client libs regression tests for running in a concurrent, headless jtreg environment
    Reviewed-by: alanb

--- jdk/test/java/beans/PropertyEditor/TestColorClass.java
+++ jdk/test/java/beans/PropertyEditor/TestColorClass.java
@@ -26,6 +26,7 @@
  * @bug 4506596 6258510
  * @summary Tests PropertyEditor for value of type Color with security manager
  * @author Sergey Malenkov
+ * @key headful
  * @modules java.compiler
  *          java.desktop
  *          jdk.compiler
