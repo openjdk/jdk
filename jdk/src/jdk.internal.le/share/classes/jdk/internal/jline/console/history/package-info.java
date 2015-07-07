7e863752edaeb3bcf8539511be9c44e2c86bf3b1
Jan Lahoda <jlahoda@openjdk.java.net>
2015-07-07 13:17:53 +0200

    8080679: Include jline in JDK for Java and JavaScript REPLs
    Reviewed-by: alanb, erikj, forax, iris, sundar

---
+++ jdk/src/jdk.internal.le/share/classes/jdk/internal/jline/console/history/package-info.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2002-2012, the original author or authors.
+ *
+ * This software is distributable under the BSD license. See the terms of the
+ * BSD license in the documentation provided with this software.
+ *
+ * http://www.opensource.org/licenses/bsd-license.php
+ */
+/**
+ * Console history support.
+ *
+ * @since 2.0
+ */
+package jdk.internal.jline.console.history;
