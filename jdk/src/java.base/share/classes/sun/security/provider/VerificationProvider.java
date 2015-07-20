3542628ec712671cfc4ebc6512bc78b167116798
Iris Clark <iris@openjdk.org>
2015-07-20 09:40:49 -0700

    8130696: Security Providers need to have their version numbers updated for JDK 9
    Reviewed-by: ascarpino, mullan, valeriep, weijun, wetmore

--- jdk/src/java.base/share/classes/sun/security/provider/VerificationProvider.java
+++ jdk/src/java.base/share/classes/sun/security/provider/VerificationProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,7 +61,7 @@ public final class VerificationProvider extends Provider {
     }
 
     public VerificationProvider() {
-        super("SunJarVerification", 1.9d, "Jar Verification Provider");
+        super("SunJarVerification", 9.0d, "Jar Verification Provider");
         // register all algorithms normally registered by the Sun and SunRsaSign
         // providers, but only if they are missing
         if (ACTIVE == false) {
