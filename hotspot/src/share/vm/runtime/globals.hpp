99f005b648c3fde7ca01c978447f8782011ebf21
Daniel D. Daugherty <dcubed@openjdk.org>
2015-07-23 14:48:23 -0700

    8132286: change 'InlineNotify' flag option from "product" to "diagnostic"
    Reviewed-by: coleenp

--- hotspot/src/share/vm/runtime/globals.hpp
+++ hotspot/src/share/vm/runtime/globals.hpp
@@ -1286,7 +1286,7 @@ public:
                                                                             \
   experimental(intx, SyncVerbose, 0, "(Unstable)")                          \
                                                                             \
-  product(bool, InlineNotify, true, "intrinsify subset of notify" )         \
+  diagnostic(bool, InlineNotify, true, "intrinsify subset of notify")       \
                                                                             \
   experimental(intx, ClearFPUAtPark, 0, "(Unsafe, Unstable)")               \
                                                                             \
