325d83e6893f70c1b04f19166664dfab185673f9
Ioi Lam <iklam@openjdk.org>
2015-07-22 20:14:16 -0700

    8140802: Clean up and refactor of class loading code for CDS
    Reviewed-by: jiangli, acorn

--- test/lib/sun/hotspot/WhiteBox.java
+++ test/lib/sun/hotspot/WhiteBox.java
@@ -407,6 +407,7 @@ public class WhiteBox {
   public native void assertMatchingSafepointCalls(boolean mutexSafepointValue, boolean attemptedNoSafepointValue);
 
   // Sharing
+  public native boolean isSharedClass(Class<?> c);
   public native boolean isShared(Object o);
   public native boolean areSharedStringsIgnored();
 }
