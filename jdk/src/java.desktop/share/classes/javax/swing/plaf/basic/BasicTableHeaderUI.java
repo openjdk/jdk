36dd7094393c74cbf34eccf0bfef3d1739aa2810
Sergey Bylokhov <serb@openjdk.org>
2015-09-07 23:57:21 +0300

    8134947: [macosx] Various memory leaks in Aqua look and feel
    Reviewed-by: azvegint, alexsch

--- jdk/src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTableHeaderUI.java
+++ jdk/src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTableHeaderUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -403,6 +403,7 @@ public class BasicTableHeaderUI extends TableHeaderUI {
     protected void uninstallListeners() {
         header.removeMouseListener(mouseInputListener);
         header.removeMouseMotionListener(mouseInputListener);
+        header.removeFocusListener(focusListener);
 
         mouseInputListener = null;
     }
