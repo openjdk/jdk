2486adcdd36bb44c39a9fcafc946424c620e9e55
Andrew Haley <aph@openjdk.java.net>
2015-09-24 12:04:57 +0200

    8135018: AARCH64: Missing memory barriers for CMS collector
    Add StoreStore barrier when CMS needs them
    
    Reviewed-by: tschatzl

--- hotspot/src/cpu/aarch64/vm/macroAssembler_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/macroAssembler_aarch64.cpp
@@ -3063,11 +3063,15 @@ void MacroAssembler::store_check(Register obj) {
 
   if (UseCondCardMark) {
     Label L_already_dirty;
+    membar(StoreLoad);
     ldrb(rscratch2,  Address(obj, rscratch1));
     cbz(rscratch2, L_already_dirty);
     strb(zr, Address(obj, rscratch1));
     bind(L_already_dirty);
   } else {
+    if (UseConcMarkSweepGC && CMSPrecleaningEnabled) {
+      membar(StoreStore);
+    }
     strb(zr, Address(obj, rscratch1));
   }
 }
