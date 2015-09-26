8bdfaa57ae29383fb2087feae3cc5f01a0a31be7
Sergey Bylokhov <serb@openjdk.java.net>
2015-09-26 22:38:36 +0300

    8079965: Stop ignoring warnings for libawt_lwawt
    Reviewed-by: prr, ihse

--- jdk/src/java.desktop/macosx/native/libawt_lwawt/awt/JavaComponentAccessibility.h
+++ jdk/src/java.desktop/macosx/native/libawt_lwawt/awt/JavaComponentAccessibility.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,9 +23,9 @@
  * questions.
  */
 
-#import <AppKit/AppKit.h>
-#import <JavaNativeFoundation/JavaNativeFoundation.h>
+#include "jni.h"
 
+#import <AppKit/AppKit.h>
 
 //#define JAVA_AX_DEBUG 1
 //#define JAVA_AX_NO_IGNORES 1
