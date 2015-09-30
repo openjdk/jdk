65cf3f8eed30ccd52964b73b2d58aa4fca73f001
Felix Yang <felix.yang@linaro.org>
2015-09-30 13:23:46 +0000

    8138641: Disable C2 peephole by default for aarch64
    Reviewed-by: roland

--- hotspot/src/cpu/aarch64/vm/c2_globals_aarch64.hpp
+++ hotspot/src/cpu/aarch64/vm/c2_globals_aarch64.hpp
@@ -68,7 +68,7 @@ define_pd_global(intx, RegisterCostAreaRatio,        16000);
 
 // Peephole and CISC spilling both break the graph, and so makes the
 // scheduler sick.
-define_pd_global(bool, OptoPeephole,                 true);
+define_pd_global(bool, OptoPeephole,                 false);
 define_pd_global(bool, UseCISCSpill,                 true);
 define_pd_global(bool, OptoScheduling,               false);
 define_pd_global(bool, OptoBundling,                 false);
