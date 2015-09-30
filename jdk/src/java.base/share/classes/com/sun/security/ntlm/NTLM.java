1020874afb091cd818a76b06d2fe454e94a3e576
Artem Smotrakov <asmotrak@openjdk.java.net>
2015-09-30 15:30:50 +0300

    8137174: NTLM impl should use doPrivileged when it reads system properties
    Reviewed-by: chegar

--- jdk/src/java.base/share/classes/com/sun/security/ntlm/NTLM.java
+++ jdk/src/java.base/share/classes/com/sun/security/ntlm/NTLM.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -56,7 +56,9 @@ class NTLM {
     private final Mac hmac;
     private final MessageDigest md5;
     private static final boolean DEBUG =
-            System.getProperty("ntlm.debug") != null;
+            java.security.AccessController.doPrivileged(
+                    new sun.security.action.GetBooleanAction("ntlm.debug"))
+                        .booleanValue();
 
     final Version v;
 
