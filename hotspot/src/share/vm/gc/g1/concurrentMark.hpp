d0ae130dd5b7a41e339068d5163331d209b3b64d
Kim Barrett <kbarrett@openjdk.org>
2015-10-05 21:17:11 -0400

    8138833: Remove CMMarkStack::drain
    Remove unused functions and data members.
    
    Reviewed-by: pliden, brutisso

--- hotspot/src/share/vm/gc/g1/concurrentMark.hpp
+++ hotspot/src/share/vm/gc/g1/concurrentMark.hpp
@@ -182,15 +182,6 @@ class CMMarkStack VALUE_OBJ_CLASS_SPEC {
 
   bool  _overflow;
   bool  _should_expand;
-  DEBUG_ONLY(bool _drain_in_progress;)
-  DEBUG_ONLY(bool _drain_in_progress_yields;)
-
-  oop pop() {
-    if (!isEmpty()) {
-      return _base[--_index] ;
-    }
-    return NULL;
-  }
 
  public:
   CMMarkStack(ConcurrentMark* cm);
@@ -211,17 +202,6 @@ class CMMarkStack VALUE_OBJ_CLASS_SPEC {
   // concurrency is allowed only with "par_push_arr" and/or "par_pop_arr"
   // operations, which use the same locking strategy.
   bool par_pop_arr(oop* ptr_arr, int max, int* n);
-
-  // Drain the mark stack, applying the given closure to all fields of
-  // objects on the stack.  (That is, continue until the stack is empty,
-  // even if closure applications add entries to the stack.)  The "bm"
-  // argument, if non-null, may be used to verify that only marked objects
-  // are on the mark stack.  If "yield_after" is "true", then the
-  // concurrent marker performing the drain offers to yield after
-  // processing each object.  If a yield occurs, stops the drain operation
-  // and returns false.  Otherwise, returns true.
-  template<class OopClosureClass>
-  bool drain(OopClosureClass* cl, CMBitMap* bm, bool yield_after = false);
 
   bool isEmpty()    { return _index == 0; }
   int  maxElems()   { return _capacity; }
