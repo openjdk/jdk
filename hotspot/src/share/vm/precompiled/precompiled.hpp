77d59149014687b1636a316b1dc4029aa633fb63
David Lindholm <david@openjdk.java.net>
2015-10-13 14:07:05 +0200

    8139506: Remove the WaterMark class
    Reviewed-by: stefank, mgerdin

--- hotspot/src/share/vm/precompiled/precompiled.hpp
+++ hotspot/src/share/vm/precompiled/precompiled.hpp
@@ -111,7 +111,6 @@
 # include "gc/shared/spaceDecorator.hpp"
 # include "gc/shared/taskqueue.hpp"
 # include "gc/shared/threadLocalAllocBuffer.hpp"
-# include "gc/shared/watermark.hpp"
 # include "gc/shared/workgroup.hpp"
 # include "interpreter/abstractInterpreter.hpp"
 # include "interpreter/bytecode.hpp"
