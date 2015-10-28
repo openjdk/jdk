c95fc22507ac9be41a2216c0eb8377639eeacaa5
Pavel Punegov <ppunegov@openjdk.org>
2015-10-28 16:26:42 +0300

    8140453: compiler control test failed with RuntimeException: CompileCommand: nonexistent missing
    Replace incorrect check for validity of method pattern with full command check
    
    Reviewed-by: kvn

--- hotspot/test/compiler/compilercontrol/share/processors/QuietProcessor.java
+++ hotspot/test/compiler/compilercontrol/share/processors/QuietProcessor.java
@@ -23,8 +23,6 @@
 
 package compiler.compilercontrol.share.processors;
 
-import compiler.compilercontrol.share.method.MethodDescriptor;
-import compiler.compilercontrol.share.scenario.Command;
 import compiler.compilercontrol.share.scenario.CompileCommand;
 import jdk.test.lib.OutputAnalyzer;
 
@@ -41,12 +39,10 @@ public class QuietProcessor implements Consumer<OutputAnalyzer> {
     @Override
     public void accept(OutputAnalyzer outputAnalyzer) {
         for (CompileCommand command : commands) {
-            MethodDescriptor methodDescriptor = command.methodDescriptor;
-            if (methodDescriptor.isValid()) {
-                Command cmd = command.command;
-                String method = methodDescriptor.getCanonicalString();
+            if (command.isValid()) {
                 outputAnalyzer.shouldNotContain("CompileCommand: "
-                        + cmd.name + " " + method);
+                        + command.command.name + " "
+                        + command.methodDescriptor.getCanonicalString());
                 outputAnalyzer.shouldNotContain("CompileCommand: An error "
                         + "occurred during parsing");
             } else {
