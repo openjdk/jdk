df9103d724e8eab23c27c21fa989322f42c03141
Prasanta Sadhukhan <psadhukhan@openjdk.java.net>
2015-11-05 14:08:51 +0300

    8020448: Test closed/java/awt/font/JNICheck/JNICheck.sh fails on Solaris 11 since 7 FCS
    Reviewed-by: azvegint, rchamyal

--- jdk/src/java.desktop/unix/native/common/awt/fontpath.c
+++ jdk/src/java.desktop/unix/native/common/awt/fontpath.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -178,6 +178,7 @@ jboolean isDisplayLocal(JNIEnv *env) {
                                                        "()Z");
         JNU_CHECK_EXCEPTION_RETURN(env, JNI_FALSE);
         isLocal = (*env)->CallBooleanMethod(env, ge, isDisplayLocal);
+        JNU_CHECK_EXCEPTION_RETURN(env, JNI_FALSE);
       } else {
         isLocal = True;
       }
