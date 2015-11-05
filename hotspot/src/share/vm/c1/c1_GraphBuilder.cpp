dfb1ed8a19c79c951115168af51fdc8dd6f39210
Aleksey Shipilev <shade@openjdk.org>
2015-11-05 13:33:18 +0300

    8141044: C1 should fold (this == null) to false
    Reviewed-by: jrose, roland

--- hotspot/src/share/vm/c1/c1_GraphBuilder.cpp
+++ hotspot/src/share/vm/c1/c1_GraphBuilder.cpp
@@ -3089,7 +3089,7 @@ ValueStack* GraphBuilder::state_at_entry() {
   int idx = 0;
   if (!method()->is_static()) {
     // we should always see the receiver
-    state->store_local(idx, new Local(method()->holder(), objectType, idx));
+    state->store_local(idx, new Local(method()->holder(), objectType, idx, true));
     idx = 1;
   }
 
@@ -3101,7 +3101,7 @@ ValueStack* GraphBuilder::state_at_entry() {
     // don't allow T_ARRAY to propagate into locals types
     if (basic_type == T_ARRAY) basic_type = T_OBJECT;
     ValueType* vt = as_ValueType(basic_type);
-    state->store_local(idx, new Local(type, vt, idx));
+    state->store_local(idx, new Local(type, vt, idx, false));
     idx += type->size();
   }
 
