07c222d2d571f4ed6547327282af78b9bafd0a89
Tom Rodriguez <never@openjdk.java.net>
2015-11-10 11:49:42 -1000

    8142435: [JVMCI] restore missing InstalledCode.version increment
    Reviewed-by: twisti

--- hotspot/src/share/vm/jvmci/jvmciCompilerToVM.cpp
+++ hotspot/src/share/vm/jvmci/jvmciCompilerToVM.cpp
@@ -690,6 +690,7 @@ C2V_VMENTRY(jint, installCode, (JNIEnv *jniEnv, jobject, jobject target, jobject
       assert(installed_code_handle->is_a(InstalledCode::klass()), "wrong type");
       CompilerToVM::invalidate_installed_code(installed_code_handle, CHECK_0);
       InstalledCode::set_address(installed_code_handle, (jlong) cb);
+      InstalledCode::set_version(installed_code_handle, InstalledCode::version(installed_code_handle) + 1);
       if (cb->is_nmethod()) {
         InstalledCode::set_entryPoint(installed_code_handle, (jlong) cb->as_nmethod_or_null()->verified_entry_point());
       } else {
