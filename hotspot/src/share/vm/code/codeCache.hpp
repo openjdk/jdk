8ac86c17f800295ac87b41441c84feebbab33d75
Tobias Hartmann <thartmann@openjdk.org>
2015-11-16 15:35:43 +0100

    8061436: Processing of options related to segmented code cache should be enhanced
    Allow user to set code heap sizes individually and enhance checking of code cache flags.
    
    Reviewed-by: kvn, roland

--- hotspot/src/share/vm/code/codeCache.hpp
+++ hotspot/src/share/vm/code/codeCache.hpp
@@ -94,6 +94,8 @@ class CodeCache : AllStatic {
 
   // CodeHeap management
   static void initialize_heaps();                             // Initializes the CodeHeaps
+  // Check the code heap sizes set by the user via command line
+  static void check_heap_sizes(size_t non_nmethod_size, size_t profiled_size, size_t non_profiled_size, size_t cache_size, bool all_set);
   // Creates a new heap with the given name and size, containing CodeBlobs of the given type
   static void add_heap(ReservedSpace rs, const char* name, int code_blob_type);
   static CodeHeap* get_code_heap(const CodeBlob* cb);         // Returns the CodeHeap for the given CodeBlob
