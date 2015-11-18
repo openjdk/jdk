cd715bbcb222bd0656368adbaa872637a7c11a57
Christoph Langer <clanger@openjdk.org>
2015-11-18 08:43:52 +0800

    8139436: sun.security.mscapi.KeyStore might load incomplete data
    Reviewed-by: vinnie, weijun

--- jdk/test/sun/security/mscapi/SignUsingNONEwithRSA.sh
+++ jdk/test/sun/security/mscapi/SignUsingNONEwithRSA.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,7 @@
 
 # @test
 # @bug 6578658
+# @requires os.family == "windows"
 # @run shell SignUsingNONEwithRSA.sh
 # @summary Sign using the NONEwithRSA signature algorithm from SunMSCAPI
 # @key intermittent
