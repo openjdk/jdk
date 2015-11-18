267d7007df618762a618a65c3a6c363c8c99bf41
Sergey Bylokhov <serb@openjdk.org>
2015-11-19 01:52:52 +0300

    8143256: The build is broken after JDK-8081411
    Reviewed-by: omajid

--- jdk/src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKIconFactory.java
+++ jdk/src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKIconFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -150,8 +150,7 @@ class GTKIconFactory {
         }
     }
 
-    private static class DelegatingIcon extends SynthIcon implements
-                                   UIResource {
+    private static class DelegatingIcon implements UIResource, SynthIcon {
         private static final Class<?>[] PARAM_TYPES = new Class<?>[] {
             SynthContext.class, Graphics.class, int.class,
             int.class, int.class, int.class, int.class
