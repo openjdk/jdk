83c1d51a410417600d021c4ae7cf57b5a5495652
Sergey Bylokhov <serb@openjdk.java.net>
2015-11-19 01:52:52 +0300

    8143256: The build is broken after JDK-8081411
    Reviewed-by: omajid

--- jdk/src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKStyle.java
+++ jdk/src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKStyle.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -992,7 +992,7 @@ class GTKStyle extends SynthStyle implements GTKConstants {
     /**
      * An Icon that is fetched using getStockIcon.
      */
-    private static class GTKStockIcon extends SynthIcon {
+    private static class GTKStockIcon implements SynthIcon {
         private String key;
         private int size;
         private boolean loadedLTR;
