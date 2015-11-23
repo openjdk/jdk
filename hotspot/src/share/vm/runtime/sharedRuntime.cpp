2952cd0bde5f9d6220323bcbc5526272d7565e81
Martin Doerr <mdoerr@openjdk.org>
2015-11-23 11:06:14 +0100

    8138952: C1: Distinguish between PPC32 and PPC64
    Reviewed-by: twisti, goetz, vlivanov

--- hotspot/src/share/vm/runtime/sharedRuntime.cpp
+++ hotspot/src/share/vm/runtime/sharedRuntime.cpp
@@ -386,7 +386,7 @@ double SharedRuntime::dabs(double f)  {
 
 #endif
 
-#if defined(__SOFTFP__) || defined(PPC32)
+#if defined(__SOFTFP__) || defined(PPC)
 double SharedRuntime::dsqrt(double f) {
   return sqrt(f);
 }
