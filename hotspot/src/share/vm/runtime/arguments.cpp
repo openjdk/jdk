49fdc35f97a62bdbe7cafccc792ee76b5424cbc5
Calvin Cheung <ccheung@openjdk.java.net>
2015-11-24 16:58:45 -0800

    8143963: improve ClassLoader::trace_class_path to accept an additional outputStream* arg
    For fixing a truncation problem on the output from -XX:+TraceClassPaths
    
    Reviewed-by: coleenp, jiangli, cjplummer, minqi

--- hotspot/src/share/vm/runtime/arguments.cpp
+++ hotspot/src/share/vm/runtime/arguments.cpp
@@ -3425,7 +3425,7 @@ void Arguments::fix_appclasspath() {
   }
 
   if (!PrintSharedArchiveAndExit) {
-    ClassLoader::trace_class_path("[classpath: ", _java_class_path->value());
+    ClassLoader::trace_class_path(tty, "[classpath: ", _java_class_path->value());
   }
 }
 
