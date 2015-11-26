68126c8ee9a16b1e65e9217fbfd9b87835e19edd
Srikanth Adayapalam <sadayapalam@openjdk.org>
2015-11-26 07:44:23 +0530

    8143647: Javac compiles method reference that allows results in an IllegalAccessError
    Lambda implementation method synthesized by javac should not mention inaccessible types.
    
    Reviewed-by: mcimadamore

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/LambdaToMethod.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/comp/LambdaToMethod.java
@@ -931,7 +931,7 @@ public class LambdaToMethod extends TreeTranslator {
         private JCExpression makeReceiver(VarSymbol rcvr) {
             if (rcvr == null) return null;
             JCExpression rcvrExpr = make.Ident(rcvr);
-            Type rcvrType = tree.sym.enclClass().type;
+            Type rcvrType = tree.ownerAccessible ? tree.sym.enclClass().type : tree.expr.type;
             if (rcvrType == syms.arrayClass.type) {
                 // Map the receiver type to the actually type, not just "array"
                 rcvrType = tree.getQualifierExpression().type;
