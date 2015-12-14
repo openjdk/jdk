ecc19111612c80afd4d4ef9b1bf657737522495c
Hannes Wallnofer <hannesw@openjdk.java.net>
2015-12-14 14:27:46 +0100

    8144020: Remove long as an internal numeric type
    Reviewed-by: attila, sundar, lagergren

--- nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/arrays/ArrayFilter.java
+++ nashorn/src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/arrays/ArrayFilter.java
@@ -109,13 +109,6 @@ abstract class ArrayFilter extends ArrayData {
     }
 
     @Override
-    public ArrayData set(final int index, final long value, final boolean strict) {
-        underlying = underlying.set(index, value, strict);
-        setLength(underlying.length());
-        return this;
-    }
-
-    @Override
     public ArrayData set(final int index, final double value, final boolean strict) {
         underlying = underlying.set(index, value, strict);
         setLength(underlying.length());
@@ -147,16 +140,6 @@ abstract class ArrayFilter extends ArrayData {
     @Override
     public int getIntOptimistic(final int index, final int programPoint) {
         return underlying.getIntOptimistic(index, programPoint);
-    }
-
-    @Override
-    public long getLong(final int index) {
-        return underlying.getLong(index);
-    }
-
-    @Override
-    public long getLongOptimistic(final int index, final int programPoint) {
-        return underlying.getLongOptimistic(index, programPoint);
     }
 
     @Override
