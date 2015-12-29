a2e3a92f2bb295f1228b833304cfe7eb4c2ead5b
Ed Nevill <enevill@openjdk.java.net>
2015-12-29 16:47:34 +0000

    8146286: aarch64: guarantee failures with large code cache sizes on jtreg test java/lang/invoke/LFCaching/LFMultiThreadCachingTest.java
    Patch trampoline calls with special case bl to itself which does not cause guarantee failure
    
    Reviewed-by: aph

--- hotspot/src/cpu/aarch64/vm/relocInfo_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/relocInfo_aarch64.cpp
@@ -66,7 +66,13 @@ address Relocation::pd_call_destination(address orig_addr) {
     }
   }
   if (orig_addr != NULL) {
-    return MacroAssembler::pd_call_destination(orig_addr);
+    address new_addr = MacroAssembler::pd_call_destination(orig_addr);
+    // If call is branch to self, don't try to relocate it, just leave it
+    // as branch to self. This happens during code generation if the code
+    // buffer expands. It will be relocated to the trampoline above once
+    // code generation is complete.
+    new_addr = (new_addr == orig_addr) ? addr() : new_addr;
+    return new_addr;
   }
   return MacroAssembler::pd_call_destination(addr());
 }
