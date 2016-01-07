0cc00c446534b90d1633b965003c15a8cc7bebea
Andreas Lundblad <alundblad@openjdk.org>
2016-01-07 11:50:48 +0100

    8145557: Unused method in JavacState should be removed
    Unused method removed.
    
    Reviewed-by: jlahoda

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/sjavac/JavacState.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/sjavac/JavacState.java
@@ -120,13 +120,6 @@ public class JavacState {
     // ones that -sourcepath is allowed to see.
     Set<URI> visibleSrcs;
 
-    // Visible classes for linking. These are the only
-    // ones that -classpath is allowed to see.
-    // It maps from a classpath root to the set of visible classes for that root.
-    // If the set is empty, then all classes are visible for that root.
-    // It can also map from a jar file to the set of visible classes for that jar file.
-    Map<URI,Set<String>> visibleClasses;
-
     // Setup transform that always exist.
     private CompileJavaPackages compileJavaPackages = new CompileJavaPackages();
 
@@ -213,16 +206,6 @@ public class JavacState {
         }
     }
 
-    /**
-     * Specify which classes are visible to the compiler through -classpath.
-     */
-    public void setVisibleClasses(Map<String,Source> vs) {
-        visibleSrcs = new HashSet<>();
-        for (String s : vs.keySet()) {
-            Source src = vs.get(s);
-            visibleSrcs.add(src.file().toURI());
-        }
-    }
     /**
      * Returns true if this is an incremental build.
      */
@@ -820,7 +803,6 @@ public class JavacState {
             boolean r = t.transform(sjavac,
                                     srcs,
                                     visibleSrcs,
-                                    visibleClasses,
                                     prev.dependents(),
                                     outputDir.toURI(),
                                     packageArtifacts,
