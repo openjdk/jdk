aeccbee0e752c5048c0b503a3587001dd01ce42a
Alexander Scherbatiy <alexsch@openjdk.org>
2016-01-11 06:10:32 -0800

    8143316: Crash Trend in 1.9.0-ea-b93 (sun.awt.DefaultMouseInfoPeer.fillPointWithCoords)
    Reviewed-by: serb

--- jdk/src/java.desktop/windows/classes/sun/awt/windows/WMouseInfoPeer.java
+++ jdk/src/java.desktop/windows/classes/sun/awt/windows/WMouseInfoPeer.java
@@ -23,18 +23,24 @@
  * questions.
  */
 
-package sun.awt;
+package sun.awt.windows;
 
+import java.awt.GraphicsEnvironment;
 import java.awt.Point;
 import java.awt.Window;
 import java.awt.peer.MouseInfoPeer;
 
-public final class DefaultMouseInfoPeer implements MouseInfoPeer {
+public final class WMouseInfoPeer implements MouseInfoPeer {
+
+    static {
+        // initialize screen devices for the mouse coordinates scaling
+        GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+    }
 
     /**
      * Package-private constructor to prevent instantiation.
      */
-    DefaultMouseInfoPeer() {
+    WMouseInfoPeer() {
     }
 
     public native int fillPointWithCoords(Point point);
