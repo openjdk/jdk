60015e143fa83c75aad627140acd91991bacbed6
Michael Haupt <mhaupt@openjdk.org>
2016-01-11 17:19:16 +0100

    8146786: [TESTBUG] straighten out testability for several issues
    Reviewed-by: psandoz

--- jdk/test/java/lang/invoke/T8139885.java
+++ jdk/test/java/lang/invoke/T8139885.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,8 @@
  */
 
 /* @test
+ * @bug 8139885
+ * @bug 8143798
  * @run testng/othervm -ea -esa test.java.lang.invoke.T8139885
  */
 
