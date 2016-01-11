13c9cbfb57454e4b9b6812a9e4adcb6f2a6dc13b
Roger Riggs <rriggs@openjdk.org>
2016-01-11 10:24:20 -0500

    8146567: Remove dead code finalizer methods
    Reviewed-by: dfuchs, alanb

--- jdk/src/java.base/share/classes/sun/net/www/protocol/jar/URLJarFile.java
+++ jdk/src/java.base/share/classes/sun/net/www/protocol/jar/URLJarFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -106,13 +106,6 @@ public class URLJarFile extends JarFile {
                 return true;
         }
         return false;
-    }
-
-    /*
-     * close the jar file.
-     */
-    protected void finalize() throws IOException {
-        close();
     }
 
     /**
