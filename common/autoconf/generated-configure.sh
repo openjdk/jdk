cb002b17c792b8b98839664fd45bf19368ae7b4d
Erik Joelsson <erikj@openjdk.java.net>
2016-01-16 13:01:43 +0100

    8146403: Windows build can be faster
    Reviewed-by: ihse

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -4839,7 +4839,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1452780299
+DATE_WHEN_GENERATED=1452935762
 
 ###############################################################################
 #
