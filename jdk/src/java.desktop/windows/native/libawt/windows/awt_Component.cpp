e3c4a8683a3548f9e23d75bf2d3a1f1d9d7d85be
Sergey Bylokhov <serb@openjdk.org>
2016-01-20 15:10:25 +0300

    8080492: [Parfait] Uninitialised variable in jdk/src/java/desktop/windows/native/libawt/
    Reviewed-by: prr, vadim

--- jdk/src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
+++ jdk/src/java.desktop/windows/native/libawt/windows/awt_Component.cpp
@@ -5234,6 +5234,8 @@ void AwtComponent::SynthesizeMouseMessage(JNIEnv *env, jobject mouseEvent)
                 message = WM_MBUTTONDOWN; break;
             case java_awt_event_MouseEvent_BUTTON2:
                 message = WM_RBUTTONDOWN; break;
+            default:
+                return;
           }
           break;
       }
@@ -5245,6 +5247,8 @@ void AwtComponent::SynthesizeMouseMessage(JNIEnv *env, jobject mouseEvent)
                 message = WM_MBUTTONUP; break;
             case java_awt_event_MouseEvent_BUTTON2:
                 message = WM_RBUTTONUP; break;
+            default:
+                return;
           }
           break;
       }
