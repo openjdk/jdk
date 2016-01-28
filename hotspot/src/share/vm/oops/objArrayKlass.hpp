e59f3371f14c6436eb24abd849267d1f2345c218
Ray Alex <sky1young@gmail.com>
2016-01-28 13:30:12 +0100

    8146987: Improve Parallel GC Full GC by caching results of live_words_in_range()
    A large part of time in the parallel scavenge collector is spent finding out the amount of live words within memory ranges to find out where to move an object to. Try to incrementally calculate this value.
    
    Reviewed-by: tschatzl, mgerdin, jmasa

--- hotspot/src/share/vm/oops/objArrayKlass.hpp
+++ hotspot/src/share/vm/oops/objArrayKlass.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -116,7 +116,7 @@ class ObjArrayKlass : public ArrayKlass {
   void oop_ps_push_contents(  oop obj, PSPromotionManager* pm);
   // Parallel Compact
   void oop_pc_follow_contents(oop obj, ParCompactionManager* cm);
-  void oop_pc_update_pointers(oop obj);
+  void oop_pc_update_pointers(oop obj, ParCompactionManager* cm);
 #endif
 
   // Oop fields (and metadata) iterators
