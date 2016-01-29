ffbb39518cb3dd96bb1703b34c992c3eb7792743
Igor Ignatyev <iignatyev@openjdk.org>
2016-01-29 03:11:16 +0300

    8148012: get rid of slash-dot-dot in @library directives
    Reviewed-by: dholmes, dfazunen, cjplummer

--- hotspot/test/compiler/compilercontrol/commandfile/LogTest.java
+++ hotspot/test/compiler/compilercontrol/commandfile/LogTest.java
@@ -25,7 +25,7 @@
  * @test
  * @bug 8137167
  * @summary Tests CompileCommand=log
- * @library /testlibrary /../../test/lib /compiler/testlibrary ../share /
+ * @library /testlibrary /test/lib /compiler/testlibrary ../share /
  * @build compiler.compilercontrol.commandfile.LogTest
  *        pool.sub.* pool.subpack.* sun.hotspot.WhiteBox
  *        compiler.testlibrary.CompilerUtils compiler.compilercontrol.share.actions.*
