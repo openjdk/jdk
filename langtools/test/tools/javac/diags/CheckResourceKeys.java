0fdfffaac8697a908495e79a3f5e68b9e9d54b8b
Jonathan Gibbons <jjg@openjdk.java.net>
2016-02-09 14:07:23 -0800

    8147801: java.nio.file.ClosedFileSystemException when using Javadoc API's in JDK9
    Reviewed-by: jlahoda

--- langtools/test/tools/javac/diags/CheckResourceKeys.java
+++ langtools/test/tools/javac/diags/CheckResourceKeys.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -289,6 +289,7 @@ public class CheckResourceKeys {
             // -XD option names
             "process.packages",
             "ignore.symbol.file",
+            "fileManager.deferClose",
             // prefix/embedded strings
             "compiler.",
             "compiler.misc.",
