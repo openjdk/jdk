63056ffbcc7aeab26a40e22cce3a740a30e80d41
Thomas Schatzl <tschatzl@openjdk.java.net>
2016-02-10 12:08:37 +0100

    8136854: G1 ConcurrentG1RefineThread::stop delays JVM shutdown for >150ms
    Decrease the default wait delay for mark thread initialization to accomodate very short running applications.
    
    Reviewed-by: tbenson, mgerdin

--- hotspot/src/share/vm/runtime/init.cpp
+++ hotspot/src/share/vm/runtime/init.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -181,8 +181,7 @@ void exit_globals() {
   }
 }
 
-
-static bool _init_completed = false;
+static volatile bool _init_completed = false;
 
 bool is_init_completed() {
   return _init_completed;
