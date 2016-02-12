6fa45cc06191f49d9d9e18c5030f14019dbb78d7
Iris Clark <iris@openjdk.java.net>
2016-02-12 09:50:02 -0800

    8149601: Update references from "1.9" to "9"
    Reviewed-by: chegar, joehw

--- jaxp/src/java.xml/share/classes/com/sun/org/apache/xml/internal/serialize/EncodingInfo.java
+++ jaxp/src/java.xml/share/classes/com/sun/org/apache/xml/internal/serialize/EncodingInfo.java
@@ -32,7 +32,7 @@ import java.nio.charset.CharsetEncoder;
 /**
  * This class represents an encoding.
  *
- * @deprecated As of JDK 1.9, Xerces 2.9.0, Xerces DOM L3 Serializer implementation
+ * @deprecated As of JDK 9, Xerces 2.9.0, Xerces DOM L3 Serializer implementation
  * is replaced by that of Xalan. Main class
  * {@link com.sun.org.apache.xml.internal.serialize.DOMSerializerImpl} is replaced
  * by {@link com.sun.org.apache.xml.internal.serializer.dom3.LSSerializerImpl}.
