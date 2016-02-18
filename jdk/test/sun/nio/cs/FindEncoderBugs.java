2d281f78c404ea4597818aafb590f2f8d10f955b
Amy Lu <amlu@openjdk.org>
2016-02-18 09:50:55 +0800

    8149920: Remove intermittent key from jdk_core tests
    Reviewed-by: darcy

--- jdk/test/sun/nio/cs/FindEncoderBugs.java
+++ jdk/test/sun/nio/cs/FindEncoderBugs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,7 +29,7 @@
  * @build jdk.testlibrary.*
  * @run main/timeout=1200 FindEncoderBugs
  * @author Martin Buchholz
- * @key randomness intermittent
+ * @key randomness
  */
 
 import java.util.*;
