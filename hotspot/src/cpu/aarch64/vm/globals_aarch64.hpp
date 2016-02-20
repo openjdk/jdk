5f3739f293a12342e4bdde33719451d51972ef60
Ed Nevill <enevill@openjdk.org>
2016-02-20 15:15:35 +0000

    8150313: aarch64: optimise array copy using SIMD instructions
    Reviewed-by: aph

--- hotspot/src/cpu/aarch64/vm/globals_aarch64.hpp
+++ hotspot/src/cpu/aarch64/vm/globals_aarch64.hpp
@@ -109,6 +109,7 @@ define_pd_global(intx, InlineSmallCode,          1000);
 
 // Don't attempt to use Neon on builtin sim until builtin sim supports it
 #define UseCRC32 false
+#define UseSIMDForMemoryOps    false
 
 #else
 #define UseBuiltinSim           false
@@ -126,6 +127,8 @@ define_pd_global(intx, InlineSmallCode,          1000);
           "Use Neon for CRC32 computation")                             \
   product(bool, UseCRC32, false,                                        \
           "Use CRC32 instructions for CRC32 computation")               \
+  product(bool, UseSIMDForMemoryOps, false,                             \
+          "Use SIMD instructions in generated memory move code")        \
   product(bool, UseLSE, false,                                          \
           "Use LSE instructions")                                       \
   product(bool, TraceTraps, false, "Trace all traps the signal handler")
