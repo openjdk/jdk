2ce28397e90be21da4c50e47bb676443dd14ecf8
Nils Eliasson <neliasso@openjdk.java.net>
2016-03-16 10:45:43 +0100

    8150054: Make compilercontrol test ignore xcomp
    Add -Xmixed and use jtreg driver
    
    Reviewed-by: kvn

--- hotspot/test/compiler/compilercontrol/jcmd/AddExcludeTest.java
+++ hotspot/test/compiler/compilercontrol/jcmd/AddExcludeTest.java
@@ -29,9 +29,9 @@
  * @build compiler.compilercontrol.jcmd.AddExcludeTest
  *        pool.sub.* pool.subpack.* sun.hotspot.WhiteBox
  *        compiler.testlibrary.CompilerUtils compiler.compilercontrol.share.actions.*
- * @run main ClassFileInstaller sun.hotspot.WhiteBox
+ * @run driver ClassFileInstaller sun.hotspot.WhiteBox
  *                              sun.hotspot.WhiteBox$WhiteBoxPermission
- * @run main/othervm compiler.compilercontrol.jcmd.AddExcludeTest
+ * @run driver compiler.compilercontrol.jcmd.AddExcludeTest
  */
 
 package compiler.compilercontrol.jcmd;
