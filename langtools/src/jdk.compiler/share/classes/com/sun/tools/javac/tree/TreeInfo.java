e42ca207864e76d58a8cd1c81eba825f28225b05
Vicente Romero <vromero@openjdk.java.net>
2016-03-31 17:25:58 -0400

    8150733: NPE when compiling module-info.java with a class declaration in a non-module mode
    Reviewed-by: jjg

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/tree/TreeInfo.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/tree/TreeInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1152,7 +1152,8 @@ public class TreeInfo {
     }
 
     public static boolean isModuleInfo(JCCompilationUnit tree) {
-        return tree.sourcefile.isNameCompatible("module-info", JavaFileObject.Kind.SOURCE);
+        return tree.sourcefile.isNameCompatible("module-info", JavaFileObject.Kind.SOURCE)
+                && tree.defs.nonEmpty() && tree.defs.head.hasTag(MODULEDEF);
     }
 
     public static JCModuleDecl getModule(JCCompilationUnit t) {
