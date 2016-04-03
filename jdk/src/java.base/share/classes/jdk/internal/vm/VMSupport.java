42fa4e4cc33d09f7e1001cdc89d24cacaa8a95dc
Chris Hegarty <chegar@openjdk.org>
2016-04-03 16:28:41 +0100

    8153181: Move sun.misc.VMSupport to an internal package
    Reviewed-by: alanb, mchung

--- jdk/src/java.base/share/classes/jdk/internal/vm/VMSupport.java
+++ jdk/src/java.base/share/classes/jdk/internal/vm/VMSupport.java
@@ -22,7 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package sun.misc;
+package jdk.internal.vm;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
