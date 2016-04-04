316c739b222e86150846cd042bc6e95ae1e14529
Zoltan Majo <zmajo@openjdk.java.net>
2016-04-04 12:52:11 +0200

    8072422: Cleanup: Remove some unused flags/code in loop optimizations
    Remove unused flags, change test using them.
    
    Reviewed-by: kvn, twisti

--- hotspot/src/share/vm/opto/c2_globals.hpp
+++ hotspot/src/share/vm/opto/c2_globals.hpp
@@ -229,20 +229,11 @@
   develop(bool, TraceLoopOpts, false,                                       \
           "Trace executed loop optimizations")                              \
                                                                             \
-  diagnostic(bool, LoopLimitCheck, true,                                    \
-          "Generate a loop limits check for overflow")                      \
-                                                                            \
   develop(bool, TraceLoopLimitCheck, false,                                 \
           "Trace generation of loop limits checks")                         \
                                                                             \
-  diagnostic(bool, RangeLimitCheck, true,                                   \
-          "Additional overflow checks during range check elimination")      \
-                                                                            \
   develop(bool, TraceRangeLimitCheck, false,                                \
           "Trace additional overflow checks in RCE")                        \
-                                                                            \
-  diagnostic(bool, UnrollLimitCheck, true,                                  \
-          "Additional overflow checks during loop unroll")                  \
                                                                             \
   /* OptimizeFill not yet supported on PowerPC. */                          \
   product(bool, OptimizeFill, true PPC64_ONLY(&& false),                    \
