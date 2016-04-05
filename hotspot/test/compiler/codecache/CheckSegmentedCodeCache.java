07299d44289470e009404c496c75b390ec170418
Per Lidén <pliden@openjdk.org>
2016-04-05 09:05:19 +0200

    8152929: Remove UNSUPPORTED_GC_OPTION, use UNSUPPORTED_OPTION instead
    Reviewed-by: brutisso, jmasa

--- hotspot/test/compiler/codecache/CheckSegmentedCodeCache.java
+++ hotspot/test/compiler/codecache/CheckSegmentedCodeCache.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -52,7 +52,7 @@ public class CheckSegmentedCodeCache {
         out.shouldContain(NON_METHOD);
       } catch (RuntimeException e) {
         // Check if TieredCompilation is disabled (in a client VM)
-        if(!out.getOutput().contains("TieredCompilation is disabled in this release.")) {
+        if(!out.getOutput().contains("-XX:+TieredCompilation not supported in this VM")) {
           // Code cache is not segmented
           throw new RuntimeException("No code cache segmentation.");
         }
