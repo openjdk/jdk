0050625f1e5444f2b3881a5de69fc3ce969840b6
Rachel Protacio <rprotacio@openjdk.java.net>
2016-04-07 13:11:15 -0400

    8152896: Convert PrintCompressedOopsMode to Unified Logging
    Converted -XX:+PrintCompressedOopsMode to -Xlog:gc+heap+coops and aliased old option
    
    Reviewed-by: coleenp, sjohanss, kvn, hseigel

--- hotspot/test/runtime/CompressedOops/CompressedClassPointers.java
+++ hotspot/test/runtime/CompressedOops/CompressedClassPointers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -85,8 +85,7 @@ public class CompressedClassPointers {
     public static void heapBaseMinAddressTest() throws Exception {
         ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(
             "-XX:HeapBaseMinAddress=1m",
-            "-XX:+UnlockDiagnosticVMOptions",
-            "-XX:+PrintCompressedOopsMode",
+            "-Xlog:gc+heap+coops=debug",
             "-version");
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
         output.shouldContain("HeapBaseMinAddress must be at least");
