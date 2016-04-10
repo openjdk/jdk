acf90220b228c3c31e24f2ad91408a7db91bef90
Kumar Srinivasan <ksrini@openjdk.org>
2016-04-10 08:41:00 -0700

    8073100: [javadoc] Provide an ability to suppress document generation for specific elements
    Reviewed-by: jjg

--- langtools/src/jdk.compiler/share/classes/com/sun/source/doctree/DocTree.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/source/doctree/DocTree.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -102,13 +102,19 @@ public interface DocTree {
         EXCEPTION("exception"),
 
         /**
+         * Used for instances of {@link HiddenTree}
+         * representing an @hidden tag.
+         */
+        HIDDEN("hidden"),
+
+        /**
          * Used for instances of {@link IdentifierTree}
          * representing an identifier.
          */
         IDENTIFIER,
 
         /**
-         * Used for instances of {@index term optional-descr}
+         * Used for instances of {@link IndexTree}
          * representing a search term.
          */
         INDEX("index"),
--- langtools/src/jdk.compiler/share/classes/com/sun/source/doctree/DocTreeVisitor.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/source/doctree/DocTreeVisitor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -127,6 +127,14 @@ public interface DocTreeVisitor<R,P> {
      * @return a result value
      */
     R visitErroneous(ErroneousTree node, P p);
+
+    /**
+     * Visits a HiddenTree node.
+     * @param node the node being visited
+     * @param p a parameter value
+     * @return a result value
+     */
+    R visitHidden(HiddenTree node, P p);
 
     /**
      * Visits an IdentifierTree node.
