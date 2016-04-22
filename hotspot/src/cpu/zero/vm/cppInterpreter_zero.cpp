8998c19e7784e506208922ed2cf35445f50b5755
Andrew Haley <aph@openjdk.java.net>
2016-04-22 14:30:58 +0200

    8154210: Zero: Better byte behaviour
    Complete support for 8132051 on Zero and fix failure on 64-bit big-endian systems
    
    Reviewed-by: andrew, chrisphi, coleenp

--- hotspot/src/cpu/zero/vm/cppInterpreter_zero.cpp
+++ hotspot/src/cpu/zero/vm/cppInterpreter_zero.cpp
@@ -221,9 +221,16 @@ void CppInterpreter::main_loop(int recurse, TRAPS) {
   // Push our result
   for (int i = 0; i < result_slots; i++) {
     // Adjust result to smaller
-    intptr_t res = result[-i];
+    union {
+      intptr_t res;
+      jint res_jint;
+    };
+    res = result[-i];
     if (result_slots == 1) {
-      res = narrow(method->result_type(), res);
+      BasicType t = method->result_type();
+      if (is_subword_type(t)) {
+        res_jint = (jint)narrow(t, res_jint);
+      }
     }
     stack->push(res);
   }
