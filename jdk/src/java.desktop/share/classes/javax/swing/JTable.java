3f778b5b22c3d45fc4d0553f52b11a7d5fc546ff
Phil Race <prr@openjdk.org>
2016-04-25 15:09:45 -0700

    8154213: clean up uses of boxed primitive constructors in the java.desktop module
    Reviewed-by: serb, psadhukhan

--- jdk/src/java.desktop/share/classes/javax/swing/JTable.java
+++ jdk/src/java.desktop/share/classes/javax/swing/JTable.java
@@ -81,7 +81,7 @@ import sun.swing.PrintingStatus;
  *      TableModel dataModel = new AbstractTableModel() {
  *          public int getColumnCount() { return 10; }
  *          public int getRowCount() { return 10;}
- *          public Object getValueAt(int row, int col) { return new Integer(row*col); }
+ *          public Object getValueAt(int row, int col) { return Integer.valueOf(row*col); }
  *      };
  *      JTable table = new JTable(dataModel);
  *      JScrollPane scrollpane = new JScrollPane(table);
