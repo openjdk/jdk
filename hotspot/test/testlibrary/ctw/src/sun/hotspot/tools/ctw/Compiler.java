9c04e65bdacd138766daeba56bdb74a1c8cb1a54
Pavel Punegov <ppunegov@openjdk.org>
2016-04-28 17:58:18 +0300

    8155034: [TESTBUG] ctw tests fail to compile: module reads package sun.reflect from both jdk.unsupported and java.base
    Replace sun.reflect with jdk.internal.reflect
    
    Reviewed-by: iignatyev, ddmitriev

--- hotspot/test/testlibrary/ctw/src/sun/hotspot/tools/ctw/Compiler.java
+++ hotspot/test/testlibrary/ctw/src/sun/hotspot/tools/ctw/Compiler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@ package sun.hotspot.tools.ctw;
 
 import sun.hotspot.WhiteBox;
 import jdk.internal.misc.SharedSecrets;
-import sun.reflect.ConstantPool;
+import jdk.internal.reflect.ConstantPool;
 
 import java.lang.reflect.Executable;
 
