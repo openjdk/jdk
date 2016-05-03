7c8674903be2d07d93b1e4f39e07f31825e582e4
Alan Bateman <alanb@openjdk.org>
2016-05-03 09:09:07 +0100

    8154956: Module system implementation refresh (4/2016)
    Co-authored-by: Mandy Chung <mandy.chung@oracle.com>
    Co-authored-by: Erik Joelsson <erik.joelsson@oracle.com>
    Reviewed-by: alanb, mchung, chegar, redestad

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -5070,7 +5070,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1460963400
+DATE_WHEN_GENERATED=1461307675
 
 ###############################################################################
 #
