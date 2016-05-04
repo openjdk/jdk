7057abda2966b6f679c457873bbe4b5e1cd5dd28
Ahmed Khawaja <ahmed.khawaja@oracle.com>
2016-05-04 15:30:21 -0700

    8155162: java.util.zip.CRC32C Interpreter/C1 intrinsics support on SPARC
    Reviewed-by: kvn

--- hotspot/src/cpu/aarch64/vm/c1_LIRGenerator_aarch64.cpp
+++ hotspot/src/cpu/aarch64/vm/c1_LIRGenerator_aarch64.cpp
@@ -1029,6 +1029,10 @@ void LIRGenerator::do_update_CRC32(Intrinsic* x) {
   }
 }
 
+void LIRGenerator::do_update_CRC32C(Intrinsic* x) {
+  Unimplemented();
+}
+
 // _i2l, _i2f, _i2d, _l2i, _l2f, _l2d, _f2i, _f2l, _f2d, _d2i, _d2l, _d2f
 // _i2b, _i2c, _i2s
 void LIRGenerator::do_Convert(Convert* x) {
