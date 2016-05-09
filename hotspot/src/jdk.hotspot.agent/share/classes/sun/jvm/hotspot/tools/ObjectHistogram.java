f4ffadd18be7dc49cd2fed3dc100f6abc885069d
Dmitry Samersoff <dsamersoff@openjdk.org>
2016-05-09 23:41:41 +0300

    8155091: Remove SA related functions from tmtools
    Remove options that enables support for non-cooperative mode
    
    Reviewed-by: alanb, mchung, sla

--- hotspot/src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/ObjectHistogram.java
+++ hotspot/src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/ObjectHistogram.java
@@ -27,52 +27,46 @@ package sun.jvm.hotspot.tools;
 import sun.jvm.hotspot.debugger.*;
 import sun.jvm.hotspot.oops.*;
 import sun.jvm.hotspot.runtime.*;
-import jdk.internal.vm.agent.spi.ToolProvider;
 
 import java.io.PrintStream;
 
 /** A sample tool which uses the Serviceability Agent's APIs to obtain
     an object histogram from a remote or crashed VM. */
-public class ObjectHistogram extends Tool implements ToolProvider {
+public class ObjectHistogram extends Tool {
 
     public ObjectHistogram() {
-       super();
+        super();
     }
 
     public ObjectHistogram(JVMDebugger d) {
-       super(d);
+        super(d);
     }
 
     @Override
     public String getName() {
-       return "objectHistogram";
+        return "objectHistogram";
     }
 
-    @Override
-    public void run(String... arguments) {
-        execute(arguments);
+    public void run() {
+        run(System.out, System.err);
     }
 
-   public void run() {
-      run(System.out, System.err);
-   }
-
-   public void run(PrintStream out, PrintStream err) {
-      // Ready to go with the database...
-      ObjectHeap heap = VM.getVM().getObjectHeap();
-      sun.jvm.hotspot.oops.ObjectHistogram histogram =
+    public void run(PrintStream out, PrintStream err) {
+        // Ready to go with the database...
+        ObjectHeap heap = VM.getVM().getObjectHeap();
+        sun.jvm.hotspot.oops.ObjectHistogram histogram =
         new sun.jvm.hotspot.oops.ObjectHistogram();
-      err.println("Iterating over heap. This may take a while...");
-      long startTime = System.currentTimeMillis();
-      heap.iterate(histogram);
-      long endTime = System.currentTimeMillis();
-      histogram.printOn(out);
-      float secs = (float) (endTime - startTime) / 1000.0f;
-      err.println("Heap traversal took " + secs + " seconds.");
-   }
+        err.println("Iterating over heap. This may take a while...");
+        long startTime = System.currentTimeMillis();
+        heap.iterate(histogram);
+        long endTime = System.currentTimeMillis();
+        histogram.printOn(out);
+        float secs = (float) (endTime - startTime) / 1000.0f;
+        err.println("Heap traversal took " + secs + " seconds.");
+    }
 
-   public static void main(String[] args) {
-      ObjectHistogram oh = new ObjectHistogram();
-      oh.execute(args);
-   }
+    public static void main(String[] args) {
+        ObjectHistogram oh = new ObjectHistogram();
+        oh.execute(args);
+    }
 }
