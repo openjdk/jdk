c2c042898038e0bc692cd2fbd6022ceaffd3eaca
Jesper Wilhelmsson <jwilhelm@openjdk.java.net>
2016-05-11 23:33:24 +0200

    8156800: Convert QuickSort_test to GTest
    Reviewed-by: rehn, kzhaldyb, rprotacio, mlarsson

--- hotspot/src/share/vm/utilities/internalVMTests.cpp
+++ hotspot/src/share/vm/utilities/internalVMTests.cpp
@@ -52,7 +52,6 @@ void InternalVMTests::run() {
   run_unit_test(TestGlobalDefinitions_test);
   run_unit_test(GCTimer_test);
   run_unit_test(CollectedHeap_test);
-  run_unit_test(QuickSort_test);
   run_unit_test(TestBitMap_test);
   run_unit_test(ObjectMonitor_test);
   run_unit_test(DirectivesParser_test);
