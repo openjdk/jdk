2deba17efe3c91bb67c70a4547ce93d345cf6c63
Sergey Bylokhov <serb@openjdk.java.net>
2016-05-18 20:40:17 +0300

    8156169: Some sound tests rarely hangs because of incorrect synchronization
    Reviewed-by: prr, amenkov

--- jdk/src/java.desktop/share/classes/com/sun/media/sound/MidiInDevice.java
+++ jdk/src/java.desktop/share/classes/com/sun/media/sound/MidiInDevice.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -38,7 +38,7 @@ import javax.sound.midi.*;
  */
 final class MidiInDevice extends AbstractMidiDevice implements Runnable {
 
-    private Thread midiInThread = null;
+    private volatile Thread midiInThread;
 
     // CONSTRUCTOR
 
