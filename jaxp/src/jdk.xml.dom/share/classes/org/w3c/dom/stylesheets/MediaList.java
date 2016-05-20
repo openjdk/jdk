f56536a6bf679352c5ecc51d7f0eca36a7f97768
Joe Wang <joehw@openjdk.org>
2016-05-20 13:47:36 -0700

    8054632: [since-tag]: javadoc for xml classes has invalid @since tag
    Reviewed-by: lancea

--- jaxp/src/jdk.xml.dom/share/classes/org/w3c/dom/stylesheets/MediaList.java
+++ jaxp/src/jdk.xml.dom/share/classes/org/w3c/dom/stylesheets/MediaList.java
@@ -51,7 +51,7 @@ import org.w3c.dom.DOMException;
  * <p> The items in the <code>MediaList</code> are accessible via an integral
  * index, starting from 0.
  * <p>See also the <a href='http://www.w3.org/TR/2000/REC-DOM-Level-2-Style-20001113'>Document Object Model (DOM) Level 2 Style Specification</a>.
- * @since DOM Level 2
+ * @since 1.4, DOM Level 2
  */
 public interface MediaList {
     /**
