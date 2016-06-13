5b68b54f76e1b1ad69514ad02942c04354445e11
Erik Helin <ehelin@openjdk.java.net>
2016-06-13 13:54:46 +0200

    8159365: assert is not defined for unit tests
    Reviewed-by: jwilhelm, coleenp

--- hotspot/test/native/unittest.hpp
+++ hotspot/test/native/unittest.hpp
@@ -27,8 +27,17 @@
 
 #define GTEST_DONT_DEFINE_TEST 1
 #include "gtest/gtest.h"
+
+// gtest/gtest.h includes assert.h which will define the assert macro, but hotspot has its
+// own standards incompatible assert macro that takes two parameters.
+// The workaround is to undef assert and then re-define it. The re-definition
+// must unfortunately be copied since debug.hpp might already have been
+// included and a second include wouldn't work due to the header guards in debug.hpp.
 #ifdef assert
   #undef assert
+  #ifdef vmassert
+    #define assert(p, ...) vmassert(p, __VA_ARGS__)
+  #endif
 #endif
 
 #define CONCAT(a, b) a ## b
