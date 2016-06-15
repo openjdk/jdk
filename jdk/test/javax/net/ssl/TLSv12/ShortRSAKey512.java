65f21ed614e1bc8b20b25d8fe909c63e4ee3d3b1
Joe Darcy <darcy@openjdk.java.net>
2016-06-14 23:02:43 -0700

    8159502: Mark ShortRSAKey512.java as intermittently failing
    Reviewed-by: xuelei

--- jdk/test/javax/net/ssl/TLSv12/ShortRSAKey512.java
+++ jdk/test/javax/net/ssl/TLSv12/ShortRSAKey512.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,6 +33,7 @@
  *
  *     SunJSSE does not support dynamic system properties, no way to re-use
  *     system properties in samevm/agentvm mode.
+ * @key intermittent
  * @run main/othervm ShortRSAKey512 PKIX
  * @run main/othervm ShortRSAKey512 SunX509
  */
