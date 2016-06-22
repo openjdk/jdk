695d93c6b2e86b5cecfb3cb6f373bc01d71d17a0
Vyom Tewari <vtewari@openjdk.java.net>
2016-06-22 09:01:34 +0100

    8071660: URLPermission not handling empty method lists correctly
    Reviewed-by: chegar, dfuchs, prappo, rriggs

--- jdk/src/java.base/share/classes/java/net/URLPermission.java
+++ jdk/src/java.base/share/classes/java/net/URLPermission.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -265,8 +265,14 @@ public final class URLPermission extends Permission {
 
         URLPermission that = (URLPermission)p;
 
-        if (!this.methods.get(0).equals("*") &&
-                Collections.indexOfSubList(this.methods, that.methods) == -1) {
+        if (this.methods.isEmpty() && !that.methods.isEmpty()) {
+            return false;
+        }
+
+        if (!this.methods.isEmpty() &&
+            !this.methods.get(0).equals("*") &&
+            Collections.indexOfSubList(this.methods,
+                                       that.methods) == -1) {
             return false;
         }
 
