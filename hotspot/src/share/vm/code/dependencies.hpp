4abb5e4aafe05871a07d510f002a38298fe4dbb2
Alex Henrie <alexhenrie24@gmail.com>
2016-06-23 16:46:41 -0400

    8157758: JDK9 does not compile on Linux with GCC 6.1 because left-shifting a negative number has undefined behavior
    Replace shifts of -1 with shifts of ~0u.
    
    Reviewed-by: kbarrett, mockner

--- hotspot/src/share/vm/code/dependencies.hpp
+++ hotspot/src/share/vm/code/dependencies.hpp
@@ -168,7 +168,7 @@ class Dependencies: public ResourceObj {
     LG2_TYPE_LIMIT = 4,  // assert(TYPE_LIMIT <= (1<<LG2_TYPE_LIMIT))
 
     // handy categorizations of dependency types:
-    all_types           = ((1 << TYPE_LIMIT) - 1) & ((-1) << FIRST_TYPE),
+    all_types           = ((1 << TYPE_LIMIT) - 1) & ((~0u) << FIRST_TYPE),
 
     non_klass_types     = (1 << call_site_target_value),
     klass_types         = all_types & ~non_klass_types,
