11ecd00506b533138b58139180efede314904aa0
Sibabrata Sahoo <ssahoo@openjdk.org>
2016-07-05 10:04:16 -0700

    8160624: sun/security/tools/keytool/printssl.sh failed with "Socket closed"
    Reviewed-by: weijun

--- jdk/test/sun/security/tools/keytool/PrintSSL.java
+++ jdk/test/sun/security/tools/keytool/PrintSSL.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,36 +21,74 @@
  * questions.
  */
 
-// Read printssl.sh, this Java program starts an SSL server
+/*
+ * @test
+ * @bug 6480981 8160624
+ * @modules java.base/sun.security.tools.keytool
+ * @summary keytool should be able to import certificates from remote SSL server
+ * @run main/othervm PrintSSL
+ */
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.CountDownLatch;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
 public class PrintSSL {
+
     public static void main(String[] args) throws Exception {
-        System.setProperty("javax.net.ssl.keyStorePassword", "passphrase");
-        System.setProperty("javax.net.ssl.keyStore",
-                           System.getProperty("test.src", "./")
-                               + "/../../../../javax/net/ssl/etc/keystore");
-        SSLServerSocketFactory sslssf =
-                (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-        final ServerSocket server = sslssf.createServerSocket(0);
-        System.out.println(server.getLocalPort());
-        System.out.flush();
-        Thread t = new Thread() {
-            public void run() {
-                try {
-                    Thread.sleep(30000);
-                    server.close();
-                } catch (Exception e) {
-                    ;
-                }
-                throw new RuntimeException("Timeout");
-            }
-        };
-        t.setDaemon(true);
-        t.start();
-        ((SSLSocket)server.accept()).startHandshake();
+
+        int port = new Server().start();
+        if(port == -1) {
+            throw new RuntimeException("Unable start ssl server.");
+        }
+        String vmOpt = System.getProperty("TESTTOOLVMOPTS");
+        String cmd = String.format(
+                "-debug %s -printcert -sslserver localhost:%s",
+                ((vmOpt == null) ? "" : vmOpt ), port);
+        sun.security.tools.keytool.Main.main(cmd.split("\\s+"));
     }
+
+    private static class Server implements Runnable {
+
+        private volatile int serverPort = -1;
+        private final CountDownLatch untilServerReady = new CountDownLatch(1);
+
+        public int start() throws InterruptedException {
+
+            Thread server = new Thread(this);
+            server.setDaemon(true);
+            server.start();
+            untilServerReady.await();
+            return this.getServerPort();
+        }
+
+        @Override
+        public void run() {
+
+            System.setProperty("javax.net.ssl.keyStorePassword", "passphrase");
+            System.setProperty("javax.net.ssl.keyStore",
+                    System.getProperty("test.src", "./")
+                    + "/../../../../javax/net/ssl/etc/keystore");
+            SSLServerSocketFactory sslssf =
+                    (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+            try (ServerSocket server = sslssf.createServerSocket(0)) {
+                this.serverPort = server.getLocalPort();
+                System.out.printf("%nServer started on: %s%n", getServerPort());
+                untilServerReady.countDown();
+                ((SSLSocket)server.accept()).startHandshake();
+            } catch (Throwable e) {
+                e.printStackTrace(System.out);
+                untilServerReady.countDown();
+            }
+
+        }
+
+        public int getServerPort() {
+            return this.serverPort;
+        }
+
+    }
+
 }
