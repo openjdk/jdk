a0fd3a2bbdb7c2004a298415181fcbf3022376fa
Semyon Sadetsky <ssadetsky@openjdk.java.net>
2016-07-11 21:46:07 +0300

    8161342: Service Menu services
    Reviewed-by: prr, mschoene

--- jdk/src/java.desktop/windows/classes/sun/awt/windows/WMenuItemPeer.java
+++ jdk/src/java.desktop/windows/classes/sun/awt/windows/WMenuItemPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -107,6 +107,7 @@ class WMenuItemPeer extends WObjectPeer implements MenuItemPeer {
         this.target = target;
         this.parent = (WMenuPeer) WToolkit.targetToPeer(target.getParent());
         this.isCheckbox = isCheckbox;
+        parent.addChildPeer(this);
         create(parent);
         // fix for 5088782: check if menu object is created successfully
         checkMenuCreation();
