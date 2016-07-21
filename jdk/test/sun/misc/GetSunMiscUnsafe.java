043af61997b5d633632282b284bf749c21432912
Paul Sandoz <psandoz@openjdk.org>
2016-07-21 16:29:17 +0200

    8161947: runtime/Unsafe/GetUnsafe.java is failing on jdk9/dev
    Reviewed-by: alanb, dholmes

--- jdk/test/sun/misc/GetSunMiscUnsafe.java
+++ jdk/test/sun/misc/GetSunMiscUnsafe.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,12 +29,12 @@
  */
 
 
-public class Safe {
+public class GetSunMiscUnsafe {
 
     public static void main(String[] args) throws Exception {
         try {
             sun.misc.Unsafe.getUnsafe();
-        } catch (Exception x) {
+        } catch (SecurityException x) {
             System.err.println("Thrown as expected: " + x);
             return;
         }
