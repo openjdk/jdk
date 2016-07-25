a91bd52b1ba97cc77a995775933c883d90f100f0
Dmitrij Pochepko <dpochepk@openjdk.java.net>
2016-07-25 21:02:51 +0300

    8162376: TestSHA512Intrinsics.java failed with Unexpected count of intrinsic _sha5_implCompress is expected
    Reviewed-by: kvn

--- hotspot/test/compiler/intrinsics/sha/sanity/TestSHA512MultiBlockIntrinsics.java
+++ hotspot/test/compiler/intrinsics/sha/sanity/TestSHA512MultiBlockIntrinsics.java
@@ -38,7 +38,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=positive_384.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2
+ *                   -XX:CompileOnly=sun/security/provider/SHA5
  *                   -XX:+UseSHA512Intrinsics -XX:-UseSHA1Intrinsics
  *                   -XX:-UseSHA256Intrinsics
  *                   -Dalgorithm=SHA-384
@@ -48,7 +48,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=positive_384_def.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2
+ *                   -XX:CompileOnly=sun/security/provider/SHA5
  *                   -XX:+UseSHA512Intrinsics -Dalgorithm=SHA-384
  *                   compiler.intrinsics.sha.sanity.TestSHA512MultiBlockIntrinsics
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions
@@ -56,7 +56,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=negative_384.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2 -XX:-UseSHA
+ *                   -XX:CompileOnly=sun/security/provider/SHA5 -XX:-UseSHA
  *                   -Dalgorithm=SHA-384
  *                   compiler.intrinsics.sha.sanity.TestSHA1Intrinsics
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions
@@ -64,7 +64,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=positive_512.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2
+ *                   -XX:CompileOnly=sun/security/provider/SHA5
  *                   -XX:+UseSHA512Intrinsics -XX:-UseSHA1Intrinsics
  *                   -XX:-UseSHA256Intrinsics
  *                   -Dalgorithm=SHA-512
@@ -74,7 +74,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=positive_512_def.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2
+ *                   -XX:CompileOnly=sun/security/provider/SHA5
  *                   -XX:+UseSHA512Intrinsics -Dalgorithm=SHA-512
  *                   compiler.intrinsics.sha.sanity.TestSHA512MultiBlockIntrinsics
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions
@@ -82,7 +82,7 @@
  *                   -XX:Tier4InvocationThreshold=500
  *                   -XX:+LogCompilation -XX:LogFile=negative_512.log
  *                   -XX:CompileOnly=sun/security/provider/DigestBase
- *                   -XX:CompileOnly=sun/security/provider/SHA2 -XX:-UseSHA
+ *                   -XX:CompileOnly=sun/security/provider/SHA5 -XX:-UseSHA
  *                   -Dalgorithm=SHA-512
  *                   compiler.intrinsics.sha.sanity.TestSHA512MultiBlockIntrinsics
  * @run main/othervm -DverificationStrategy=VERIFY_INTRINSIC_USAGE
