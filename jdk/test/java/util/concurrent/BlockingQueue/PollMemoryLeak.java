63c49140bcd8b968a9d47f8f3d36dd27f7a4d04e
Doug Lea <dl@openjdk.java.net>
2016-07-26 10:06:19 -0700

    8161591: Miscellaneous changes imported from jsr166 CVS 2016-07
    Reviewed-by: martin, psandoz, plevart

--- jdk/test/java/util/concurrent/BlockingQueue/PollMemoryLeak.java
+++ jdk/test/java/util/concurrent/BlockingQueue/PollMemoryLeak.java
@@ -42,6 +42,7 @@
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.SynchronousQueue;
@@ -50,9 +51,11 @@ import java.util.concurrent.TimeUnit;
 public class PollMemoryLeak {
     public static void main(String[] args) throws InterruptedException {
         final BlockingQueue[] qs = {
+            new LinkedBlockingDeque(10),
             new LinkedBlockingQueue(10),
             new LinkedTransferQueue(),
             new ArrayBlockingQueue(10),
+            new ArrayBlockingQueue(10, true),
             new SynchronousQueue(),
             new SynchronousQueue(true),
         };
