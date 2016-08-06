6ca8a98449488d6ab9528049357597854f09432a
Igor Veresov <iveresov@openjdk.org>
2016-08-06 11:52:37 -0700

    8163313: assert(comp != __null) failed: compiler not available
    Constrain compLevel parameter in WB_IsIntrinsicAvailable() to supported levels
    
    Reviewed-by: kvn

--- hotspot/src/share/vm/prims/whitebox.cpp
+++ hotspot/src/share/vm/prims/whitebox.cpp
@@ -679,7 +679,7 @@ WB_ENTRY(jboolean, WB_IsMethodQueuedForCompilation(JNIEnv* env, jobject o, jobje
 WB_END
 
 WB_ENTRY(jboolean, WB_IsIntrinsicAvailable(JNIEnv* env, jobject o, jobject method, jobject compilation_context, jint compLevel))
-  if (compLevel < CompLevel_none || compLevel > TieredStopAtLevel) {
+  if (compLevel < CompLevel_none || compLevel > MIN2((CompLevel) TieredStopAtLevel, CompLevel_highest_tier)) {
     return false; // Intrinsic is not available on a non-existent compilation level.
   }
   jmethodID method_id, compilation_context_id;
