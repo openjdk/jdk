22d45dde02b7691a2ee4b0e1e8bb0681341c3296
Rajeev Chamyal <rajeev.chamyal@oracle.com>
2016-08-17 14:48:13 +0530

    8161913: [PIT] java/awt/Window/8159168/SetShapeTest.java mostly fails
    Reviewed-by: alexsch, aniyogi

--- jdk/test/java/awt/Window/8159168/SetShapeTest.java
+++ jdk/test/java/awt/Window/8159168/SetShapeTest.java
@@ -1,29 +1,30 @@
 /*
-* Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
-* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
-*
-* This code is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License version 2 only, as
-* published by the Free Software Foundation.
-*
-* This code is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-* version 2 for more details (a copy is included in the LICENSE file that
-* accompanied this code).
-*
-* You should have received a copy of the GNU General Public License version
-* 2 along with this work; if not, write to the Free Software Foundation,
-* Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
-* or visit www.oracle.com if you need additional information or have any
-* questions.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 
 /**
  * @test
- * @bug 8159168
+ * @key headful
+ * @bug 8159168 8161913
  * @summary [hidpi] Window.setShape() works incorrectly on HiDPI
  * @run main/othervm -Dsun.java2d.uiScale=2 SetShapeTest
  */
@@ -50,6 +51,7 @@ public class SetShapeTest {
         Rectangle rect = window.getBounds();
         rect.x += rect.width - 10;
         rect.y += rect.height - 10;
+        robot.delay(1000);
         Color c = robot.getPixelColor(rect.x, rect.y);
         try {
             if (!c.equals(Color.RED)) {
