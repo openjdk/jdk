6c2a33a67bcaec69b766b5ae2b8000266702e819
Srikanth Adayapalam <sadayapalam@openjdk.org>
2016-08-17 10:34:48 +0530

    8162576: Missing doclint check missing for modules
    Co-authored-by: Lance Andersen <lance.andersen@oracle.com>
    Reviewed-by: jjg, ksrini

--- langtools/test/tools/doclint/moduleTests/good/module-info.java
+++ langtools/test/tools/doclint/moduleTests/good/module-info.java
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -23,22 +21,17 @@
  * questions.
  */
 
- /**
-  * Defines the Language Model, Annotation Processing, and Java Compiler APIs.
-  * <P>
-  * These APIs model declarations and types of the Java programming language,
-  * and define interfaces for tools such as compilers which can be invoked
-  * from a program.
-  */
-module java.compiler {
-    exports javax.annotation.processing;
-    exports javax.lang.model;
-    exports javax.lang.model.element;
-    exports javax.lang.model.type;
-    exports javax.lang.model.util;
-    exports javax.tools;
+/*
+ * @test
+ * @bug 8162576
+ * @summary Missing doclint check missing for modules
+ * @library ../..
+ * @modules jdk.compiler/com.sun.tools.doclint
+ * @build DocLintTester
+ * @run main DocLintTester module-info.java
+ * @compile -Xdoclint:all -Werror module-info.java
+ */
 
-    uses javax.tools.DocumentationTool;
-    uses javax.tools.JavaCompiler;
+/** good module */
+module good {
 }
-
