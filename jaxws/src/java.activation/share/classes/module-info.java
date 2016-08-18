dafc506739c126710c310b1c42a4beb3acbbdd10
Lance Andersen <lancea@openjdk.java.net>
2016-08-18 12:24:30 -0400

    8161965: Create initial javadoc description for modules
    Reviewed-by: mchung, alanb, abuckley

--- jaxws/src/java.activation/share/classes/module-info.java
+++ jaxws/src/java.activation/share/classes/module-info.java
@@ -23,6 +23,9 @@
  * questions.
  */
 
+/**
+ * Defines the JavaBeans Activation Framework (JAF) API.
+ */
 module java.activation {
     requires public java.datatransfer;
     // dependence on java.beans.Beans to be eliminated
