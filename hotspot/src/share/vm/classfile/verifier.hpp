49ff5c45619b358e79cb7f3b5404173f25dbf1b8
Rachel Protacio <rprotacio@openjdk.java.net>
2016-08-25 09:23:45 -0400

    8148854: Class names "SomeClass" and "LSomeClass;" treated by JVM as an equivalent
    Added default format checking of class names loaded by the app class loader
    
    Reviewed-by: coleenp, lfoltan

--- hotspot/src/share/vm/classfile/verifier.hpp
+++ hotspot/src/share/vm/classfile/verifier.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -58,8 +58,8 @@ class Verifier : AllStatic {
   // -Xverify:all/none override this value
   static bool should_verify_for(oop class_loader, bool should_verify_class);
 
-  // Relax certain verifier checks to enable some broken 1.1 apps to run on 1.2.
-  static bool relax_verify_for(oop class_loader);
+  // Relax certain access checks to enable some broken 1.1 apps to run on 1.2.
+  static bool relax_access_for(oop class_loader);
 
   // Print output for class+resolve
   static void trace_class_resolution(Klass* resolve_class, InstanceKlass* verify_class);
