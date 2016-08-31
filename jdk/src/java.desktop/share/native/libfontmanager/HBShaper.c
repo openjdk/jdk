0d1ad07556ac5e366a439bccd6dd0d0568cffce2
Phil Race <prr@openjdk.org>
2016-08-31 13:40:06 -0700

    8164818: Reg. test java/awt/font/TextLayout/VisibleAdvance.java fails
    Reviewed-by: serb, psadhukhan

--- jdk/src/java.desktop/share/native/libfontmanager/HBShaper.c
+++ jdk/src/java.desktop/share/native/libfontmanager/HBShaper.c
@@ -78,7 +78,7 @@ int storeGVData(JNIEnv* env,
     int i;
     float x=0, y=0;
     float startX, startY;
-    float scale = 1.0f/64.0f/devScale;
+    float scale = 1.0f / FloatToFixedScale / devScale;
     unsigned int* glyphs;
     float* positions;
     int initialCount, glyphArrayLen, posArrayLen, maxGlyphs, storeadv;
