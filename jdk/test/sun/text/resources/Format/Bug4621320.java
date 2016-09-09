44d69dd31b88625886e431c8378a8c5f9a18052b
Sergei Kovalev <skovalev@openjdk.org>
2016-09-09 10:58:05 -0700

    8165592: Fix module dependencies for sun/text/* tests
    Reviewed-by: naoto

--- jdk/test/sun/text/resources/Format/Bug4621320.java
+++ jdk/test/sun/text/resources/Format/Bug4621320.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,10 +24,12 @@
 /*
  * @test
  * @bug 4621320
+ * @modules jdk.localedata
  * @summary Verify that Ukrainian month name is correct.
  */
-import java.text.*;
-import java.util.*;
+
+import java.text.DateFormatSymbols;
+import java.util.Locale;
 
 public class Bug4621320 {
 
