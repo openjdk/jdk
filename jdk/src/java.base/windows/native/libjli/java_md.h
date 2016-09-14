a7b5545cb80ded72472e2596a808e9460a1f71b0
Chris Bensen <chris.bensen@oracle.com>
2016-09-14 06:46:19 -0700

    8165524: Better detect JRE that Linux JLI will be using
    Reviewed-by: ksrini

--- jdk/src/java.base/windows/native/libjli/java_md.h
+++ jdk/src/java.base/windows/native/libjli/java_md.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -53,8 +53,5 @@ extern jlong Counter2Micros(jlong counts);
  */
 
 int UnsetEnv(char *name);
-
-jboolean
-GetApplicationHomeFromDll(char *buf, jint bufsize);
 
 #endif /* JAVA_MD_H */
