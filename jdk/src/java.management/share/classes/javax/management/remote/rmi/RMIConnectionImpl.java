86b4b4b7edf710d92cbce1f0203e3454e70d76fb
Harsha Wardhana B <hb@openjdk.java.net>
2016-09-15 13:24:24 +0530

    8165230: RMIConnection addNotificationListeners failing with specific inputs
    Reviewed-by: dfuchs, skoivu

--- jdk/src/java.management/share/classes/javax/management/remote/rmi/RMIConnectionImpl.java
+++ jdk/src/java.management/share/classes/javax/management/remote/rmi/RMIConnectionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -990,7 +990,7 @@ public class RMIConnectionImpl implements RMIConnection, Unreferenced {
 
                 filterValues[i] =
                     unwrap(filters[i], targetCl, defaultClassLoader,
-                           NotificationFilter.class, delegationSubjects[i]);
+                           NotificationFilter.class, sbjs[i]);
 
                 if (debug) logger.debug("addNotificationListener"+
                                         "(ObjectName,NotificationFilter)",
