7aed968c103d02cfe67d337f342e892463b4e06d
Goetz Lindenmaier <goetz@openjdk.org>
2016-09-22 18:23:15 +0200

    8166560: [s390] Basic enablement of s390 port
    Also fix problem with ARM Elf configuration.
    
    Reviewed-by: dholmes, coleenp

--- hotspot/src/cpu/aarch64/vm/globals_aarch64.hpp
+++ hotspot/src/cpu/aarch64/vm/globals_aarch64.hpp
@@ -39,6 +39,7 @@ define_pd_global(bool, ImplicitNullChecks,       true);  // Generate code for im
 define_pd_global(bool, TrapBasedNullChecks,  false);
 define_pd_global(bool, UncommonNullCast,         true);  // Uncommon-trap NULLs past to check cast
 
+define_pd_global(uintx, CodeCacheSegmentSize,    64 TIERED_ONLY(+64)); // Tiered compilation has large code-entry alignment.
 define_pd_global(intx, CodeEntryAlignment,       64);
 define_pd_global(intx, OptoLoopAlignment,        16);
 define_pd_global(intx, InlineFrequencyCount,     100);
