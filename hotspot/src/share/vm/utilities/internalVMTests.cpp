e67d5a890c995e1d3cc6ffea9d0246733bc21d4e
Kirill Zhaldybin <kzhaldyb@openjdk.org>
2016-09-28 18:40:50 +0300

    8166462: Convert TestResourcehash_test to Gtest
    Reviewed-by: mgerdin, iignatyev

--- hotspot/src/share/vm/utilities/internalVMTests.cpp
+++ hotspot/src/share/vm/utilities/internalVMTests.cpp
@@ -58,7 +58,6 @@ void InternalVMTests::run() {
   run_unit_test(TestNewSize_test);
   run_unit_test(TestOldSize_test);
   run_unit_test(TestBitMap_test);
-  run_unit_test(TestResourcehash_test);
   run_unit_test(ObjectMonitor_test);
   run_unit_test(Test_log_tag_combinations_limit);
   run_unit_test(Test_logtarget);
