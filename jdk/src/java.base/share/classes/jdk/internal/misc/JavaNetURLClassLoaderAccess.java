b27d8f6dd9d864f531d7e924b812adc2da28ea39
David Dehaven <ddehaven@openjdk.org>
2016-10-12 08:47:53 -0700

    8165271: Fix use of reflection to gain access to private fields
    Reviewed-by: mchung

--- jdk/src/java.base/share/classes/jdk/internal/misc/JavaNetURLClassLoaderAccess.java
+++ jdk/src/java.base/share/classes/jdk/internal/misc/JavaNetURLClassLoaderAccess.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,18 +25,9 @@
 
 package jdk.internal.misc;
 
+import java.net.URLClassLoader;
 import java.security.AccessControlContext;
-import java.security.PrivilegedAction;
-import java.security.ProtectionDomain;
 
-public interface JavaSecurityAccess {
-
-    <T> T doIntersectionPrivilege(PrivilegedAction<T> action,
-                                  AccessControlContext stack,
-                                  AccessControlContext context);
-
-    <T> T doIntersectionPrivilege(PrivilegedAction<T> action,
-                                  AccessControlContext context);
-
-    ProtectionDomain[] getProtectDomains(AccessControlContext context);
+public interface JavaNetURLClassLoaderAccess {
+    AccessControlContext getAccessControlContext(URLClassLoader u);;
 }
