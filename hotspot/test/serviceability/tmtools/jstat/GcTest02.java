eb2c2bc3c61a6ee556af7ad8753556ea4a26bd73
Michail Chernov <mchernov@openjdk.java.net>
2016-10-31 18:18:05 +0300

    8168535: Quarantine GcCauseTest02 and GcTest02
    Reviewed-by: jwilhelm, sjohanss

--- hotspot/test/serviceability/tmtools/jstat/GcTest02.java
+++ hotspot/test/serviceability/tmtools/jstat/GcTest02.java
@@ -31,6 +31,7 @@ import utils.*;
  * @modules java.base/jdk.internal.misc
  * @library /test/lib
  * @library ../share
+ * @ignore 8168396
  * @run main/othervm -XX:+UsePerfData -Xmx128M -XX:MaxMetaspaceSize=128M GcTest02
  */
 
