14b963cf37bce9f2d2af24c087281b9a015eb769
Amy Lu <amlu@openjdk.java.net>
2016-11-03 09:44:43 +0800

    8151511: Test case in CollectionAndMapModifyStreamTest for LinkedHashMap overrides that for HashMap
    Reviewed-by: chegar, psandoz

--- jdk/test/java/util/stream/bootlib/java.base/java/util/stream/ThrowableHelper.java
+++ jdk/test/java/util/stream/bootlib/java.base/java/util/stream/ThrowableHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@ package java.util.stream;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 
-public final class ThowableHelper {
+public final class ThrowableHelper {
 
     public static void checkException(Class<? extends Exception> ce, Runnable r) {
         Exception caught = null;
