bf128c46abf7f951e193e1c9cf69cc673e16cfec
Maurizio Cimadamore <mcimadamore@openjdk.java.net>
2016-11-07 11:16:23 +0000

    8168854: javac erroneously reject a a service interface inner class in a provides clause
    Java SE 9 proposed spec changes do not explicitly forbid a service interface to be an inner class - javac should follow
    
    Reviewed-by: jjg

--- langtools/test/tools/javac/modules/T8168854/pack1/Outer1.java
+++ langtools/test/tools/javac/modules/T8168854/pack1/Outer1.java
@@ -4,7 +4,9 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -21,7 +23,12 @@
  * questions.
  */
 
-module m {
-    provides p1.C1.InnerDefinition with p2.C2;
-    exports p1;
+package pack1;
+
+public class Outer1 {
+    public static class Implem extends Outer.Inter {
+        public Implem () {
+            new Outer().super();
+        }
+    }
 }
