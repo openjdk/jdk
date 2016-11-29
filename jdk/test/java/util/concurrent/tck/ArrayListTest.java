3d0d86185e189b84e4ee2802314e1596ad66a779
Doug Lea <dl@openjdk.org>
2016-11-28 23:36:11 -0800

    8143577: optimize ArrayList.removeIf 8169679: ArrayList.subList().iterator().forEachRemaining() off-by-one-error 8167202: ArrayDeque improvements 8164793: new ArrayDeque(2**N) allocates backing array of size 2**(N+1) 8169739: LinkedBlockingDeque spliterator needs to support node self-linking 8169738: CopyOnWriteArrayList subList needs more synchronization
    Reviewed-by: martin, smarks, psandoz, forax

--- jdk/test/java/util/concurrent/tck/ArrayListTest.java
+++ jdk/test/java/util/concurrent/tck/ArrayListTest.java
@@ -32,31 +32,35 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import junit.framework.Test;
+import junit.framework.TestSuite;
 
-/**
- * Contains tests applicable to all Collection implementations.
- */
-public class CollectionTest extends JSR166TestCase {
-    final CollectionImplementation impl;
-
-    /** Tests are parameterized by a Collection implementation. */
-    CollectionTest(CollectionImplementation impl, String methodName) {
-        super(methodName);
-        this.impl = impl;
+public class ArrayListTest extends JSR166TestCase {
+    public static void main(String[] args) {
+        main(suite(), args);
     }
 
-    public static Test testSuite(CollectionImplementation impl) {
-        return newTestSuite
-            (parameterizedTestSuite(CollectionTest.class,
-                                    CollectionImplementation.class,
-                                    impl),
-             jdk8ParameterizedTestSuite(CollectionTest.class,
-                                        CollectionImplementation.class,
-                                        impl));
+    public static Test suite() {
+        class Implementation implements CollectionImplementation {
+            public Class<?> klazz() { return ArrayList.class; }
+            public List emptyCollection() { return new ArrayList(); }
+            public Object makeElement(int i) { return i; }
+            public boolean isConcurrent() { return false; }
+            public boolean permitsNulls() { return true; }
+        }
+        class SubListImplementation extends Implementation {
+            public List emptyCollection() {
+                return super.emptyCollection().subList(0, 0);
+            }
+        }
+        return newTestSuite(
+                // ArrayListTest.class,
+                CollectionTest.testSuite(new Implementation()),
+                CollectionTest.testSuite(new SubListImplementation()));
     }
 
-//     public void testCollectionDebugFail() {
-//         fail(impl.klazz().getSimpleName());
-//     }
 }
