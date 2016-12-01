18a725ff0c27147fd6348bb2a8abb1d102ced5dd
Srinivas Dama <srinivas.dama@oracle.com>
2016-12-01 08:12:06 -0800

    8130351: JDK-8130127.js fails under cygwin: cygwin path pased to Java
    Jjs uses jline which expects newline character to terminate interactive input
    
    Reviewed-by: sundar, jlaskey

--- nashorn/test/script/nosecurity/JDK-8130127.js
+++ nashorn/test/script/nosecurity/JDK-8130127.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -51,5 +51,5 @@ print($EXEC(jjsCmd, null))
 print($EXEC(jjsCmd, undefined))
 print($EXEC(jjsCmd, ""))
 
-print($EXEC(jjs, "print('hello')"))
+print($EXEC(jjs, "print('hello')\n"))
 
