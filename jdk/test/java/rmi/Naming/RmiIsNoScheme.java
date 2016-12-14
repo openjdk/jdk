7f53f178e9f719e4de3d5174ab72030a5d647bff
Hamlin Li <mli@openjdk.java.net>
2016-12-13 18:47:23 -0800

    8171076: improve rmi tests by replacing TestLibrary.createRegistryOnUnusedPort, getUnusedRandomPort
    Reviewed-by: rriggs

--- jdk/test/java/rmi/Naming/RmiIsNoScheme.java
+++ jdk/test/java/rmi/Naming/RmiIsNoScheme.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -49,7 +49,7 @@ public class RmiIsNoScheme implements Remote, Serializable {
         System.err.println("\nRegression test for bug 4626311\n");
 
         try {
-            Registry registry = TestLibrary.createRegistryOnUnusedPort();
+            Registry registry = TestLibrary.createRegistryOnEphemeralPort();
             int registryPort = TestLibrary.getRegistryPort(registry);
             Naming.rebind("//:" + registryPort + "/RmiIsNoScheme",
                           new RmiIsNoScheme());
