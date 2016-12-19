12aa0a8788a7351cbce172f878aaad4214f94dd3
Goetz Lindenmaier <goetz@openjdk.org>
2016-12-19 15:48:57 -0800

    8170655: [posix] Fix minimum stack size computations
    Reviewed-by: dcubed, coleenp

--- hotspot/src/os_cpu/linux_aarch64/vm/os_linux_aarch64.cpp
+++ hotspot/src/os_cpu/linux_aarch64/vm/os_linux_aarch64.cpp
@@ -473,8 +473,10 @@ bool os::is_allocatable(size_t bytes) {
 ////////////////////////////////////////////////////////////////////////////////
 // thread stack
 
-size_t os::Posix::_compiler_thread_min_stack_allowed = 64 * K;
-size_t os::Posix::_java_thread_min_stack_allowed = 64 * K;
+// Minimum usable stack sizes required to get to user code. Space for
+// HotSpot guard pages is added later.
+size_t os::Posix::_compiler_thread_min_stack_allowed = 32 * K;
+size_t os::Posix::_java_thread_min_stack_allowed = 32 * K;
 size_t os::Posix::_vm_internal_thread_min_stack_allowed = 64 * K;
 
 // return default stack size for thr_type
--- hotspot/src/os_cpu/linux_s390/vm/os_linux_s390.cpp
+++ hotspot/src/os_cpu/linux_s390/vm/os_linux_s390.cpp
@@ -473,11 +473,11 @@ void os::Linux::set_fpu_control_word(int fpu_control) {
 ////////////////////////////////////////////////////////////////////////////////
 // thread stack
 
-// These sizes exclude libc stack guard pages, but include
-// the HotSpot guard pages.
-size_t os::Posix::_compiler_thread_min_stack_allowed = 128 * K;
-size_t os::Posix::_java_thread_min_stack_allowed = 236 * K;
-size_t os::Posix::_vm_internal_thread_min_stack_allowed = 128 * K;
+// Minimum usable stack sizes required to get to user code. Space for
+// HotSpot guard pages is added later.
+size_t os::Posix::_compiler_thread_min_stack_allowed = (52 DEBUG_ONLY(+ 32)) * K;
+size_t os::Posix::_java_thread_min_stack_allowed = (32 DEBUG_ONLY(+ 8)) * K;
+size_t os::Posix::_vm_internal_thread_min_stack_allowed = 32 * K;
 
 // Return default stack size for thr_type.
 size_t os::Posix::default_stack_size(os::ThreadType thr_type) {
