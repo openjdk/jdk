3dc48a94fc281aa908761f3c06d4730787044aaa
Bernard Blaser <bsrbnd@gmail.com>
2017-01-05 11:16:39 -0800

    8148100: Convert lambda most specific positive tests to check runtime behavior
    Reviewed-by: mcimadamore

--- langtools/test/tools/javac/lambda/MostSpecific18.java
+++ langtools/test/tools/javac/lambda/MostSpecific18.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,13 +25,18 @@
  * @test
  * @bug 8143852
  * @summary Test that generic function interface method bounds are the same
- * @compile MostSpecific18.java
  */
-class MostSpecific18 {
+public class MostSpecific18 {
+    public static void main(String[] args) {
+        new MostSpecific18().test();
+    }
+
     interface F1 { <X extends Number> Object apply(X arg); }
     interface F2 { <Y extends Number> String apply(Y arg); }
 
-    static void m1(F1 f) {}
+    static void m1(F1 f) {
+        throw new AssertionError("Less-specific method invocation.");
+    }
     static void m1(F2 f) {}
 
     static String foo(Object in) { return "a"; }
@@ -39,5 +44,4 @@ class MostSpecific18 {
     void test() {
         m1(MostSpecific18::foo);
     }
-
 }
