05ea6306d3524f2ca3ac1ce8b43bff28ecaea430
Prem Balakrishnan <pkbalakr@openjdk.java.net>
2017-01-12 12:21:47 +0530

    8172559: [PIT][TEST_BUG] Move @test to be 1st annotation in java/awt/image/Raster/TestChildRasterOp.java
    Reviewed-by: aghaisas, psadhukhan

--- jdk/test/java/awt/image/Raster/TestChildRasterOp.java
+++ jdk/test/java/awt/image/Raster/TestChildRasterOp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,8 +22,8 @@
  */
 
 /**
- * @bug 8130737
  * @test
+ * @bug 8130737 8172559
  * @summary test no exception rasterop for child raster with non-zero offset
  */
 
