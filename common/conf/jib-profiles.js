2f0c7dee31f5066c0eaebb30568f4b2738a9a4f4
Staffan Larsen <sla@openjdk.org>
2017-01-12 14:26:57 +0100

    8172709: Upgrade to jtreg 4.2 b05
    Reviewed-by: erikj, chegar

--- common/conf/jib-profiles.js
+++ common/conf/jib-profiles.js
@@ -870,7 +870,7 @@ var getJibProfilesDependencies = function (input, common) {
         jtreg: {
             server: "javare",
             revision: "4.2",
-            build_number: "b04",
+            build_number: "b05",
             checksum_file: "MD5_VALUES",
             file: "jtreg_bin-4.2.zip",
             environment_name: "JT_HOME",
