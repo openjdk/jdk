04405c6abc39622d7c64f669746560099f2a24f5
Jonathan Gibbons <jjg@openjdk.org>
2017-01-19 19:14:51 -0800

    8147414: java.nio.file.ClosedFileSystemException in javadoc
    Reviewed-by: vromero

--- langtools/test/tools/javadoc/8147801/jarsrc/lib/Lib2.java
+++ langtools/test/tools/javadoc/8147801/jarsrc/lib/Lib2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,5 +24,5 @@
 package lib;
 
 public class Lib2 {
-    int i;
+    public int i;
 }
