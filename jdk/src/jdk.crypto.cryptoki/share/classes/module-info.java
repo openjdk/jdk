980617d2f235c2eb3bd9882a7118987b09b39207
Anthony Scarpino <ascarpino@openjdk.java.net>
2017-01-23 11:49:01 -0800

    8172527: Rename jdk.crypto.token to jdk.crypto.cryptoki
    Reviewed-by: mchung, wetmore

--- jdk/src/jdk.crypto.cryptoki/share/classes/module-info.java
+++ jdk/src/jdk.crypto.cryptoki/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
  * questions.
  */
 
-module jdk.crypto.token {
+module jdk.crypto.cryptoki {
     // Depends on SunEC provider for EC related functionality
     requires jdk.crypto.ec;
     provides java.security.Provider with sun.security.pkcs11.SunPKCS11;
