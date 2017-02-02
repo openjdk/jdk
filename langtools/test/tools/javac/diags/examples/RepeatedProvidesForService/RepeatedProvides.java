02431ac603914e06ff9d4acc862601b20369f221
Jonathan Gibbons <jjg@openjdk.org>
2017-02-02 14:34:21 -0800

    8172807: Javac doesn't report errors on duplicate provides with different service implementations
    Reviewed-by: jlahoda

--- langtools/test/tools/javac/diags/examples/RepeatedProvidesForService/RepeatedProvides.java
+++ langtools/test/tools/javac/diags/examples/RepeatedProvidesForService/RepeatedProvides.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,10 +21,5 @@
  * questions.
  */
 
-// key: compiler.err.duplicate.provides
+//key: compiler.err.repeated.provides.for.service
 
-module DuplicateExports {
-     exports exported;
-     provides exported.Service
-        with impl.ServiceImplementation, impl.ServiceImplementation;
-}
