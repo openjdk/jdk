5f5028bec091a0b5406f47dbc5512c34aa51cc16
Lance Andersen <lancea@openjdk.org>
2017-02-04 14:04:28 -0500

    8173604: Rename module 8173604 java.annotations.common to java.xml.ws.annoations
    Reviewed-by: dfuchs, alanb, psandoz, mchung

--- jdk/make/src/classes/build/tools/jigsaw/GenGraphs.java
+++ jdk/make/src/classes/build/tools/jigsaw/GenGraphs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -126,7 +126,7 @@ public class GenGraphs {
         ranks.add(Set.of("java.compiler", "java.instrument"));
         ranks.add(Set.of("java.desktop", "java.management"));
         ranks.add(Set.of("java.corba", "java.xml.ws"));
-        ranks.add(Set.of("java.xml.bind", "java.annotations.common"));
+        ranks.add(Set.of("java.xml.bind", "java.xml.ws.annotation"));
 
     }
 
