845a29eefecd9a4789626221b2674723a68ecd3a
Lance Andersen <lancea@openjdk.org>
2017-02-04 14:03:15 -0500

    8173604: Rename module 8173604 java.annotations.common to java.xml.ws.annoations
    Reviewed-by: dfuchs, alanb, psandoz, mchung

--- jaxws/src/java.xml.ws.annotation/share/classes/module-info.java
+++ jaxws/src/java.xml.ws.annotation/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,7 +27,7 @@
  * Defines a subset of the Common Annotations API to support programs running
  * on the Java SE Platform.
  */
-module java.annotations.common {
+module java.xml.ws.annotation {
     exports javax.annotation;
 }
 
