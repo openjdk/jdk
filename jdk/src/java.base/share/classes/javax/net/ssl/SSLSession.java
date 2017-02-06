779401fbbff093e3878f8df1c1880f627f14f2a5
Sean Mullan <mullan@openjdk.org>
2017-02-06 08:59:00 -0500

    8173827: Remove forRemoval=true from several deprecated security APIs
    Reviewed-by: redestad, xuelei

--- jdk/src/java.base/share/classes/javax/net/ssl/SSLSession.java
+++ jdk/src/java.base/share/classes/javax/net/ssl/SSLSession.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -279,11 +279,10 @@ public interface SSLSession {
      *          has not been verified
      * @see #getPeerPrincipal()
      * @deprecated The {@link #getPeerCertificates()} method that returns an
-     *          array of {@code java.security.cert.Certificate} should
-     *          be used instead.  This method is subject to removal in
-     *          a future version of Java SE.
+     *               array of {@code java.security.cert.Certificate} should
+     *               be used instead.
      */
-    @Deprecated(since="9", forRemoval=true)
+    @Deprecated(since="9")
     public javax.security.cert.X509Certificate [] getPeerCertificateChain()
             throws SSLPeerUnverifiedException;
 
