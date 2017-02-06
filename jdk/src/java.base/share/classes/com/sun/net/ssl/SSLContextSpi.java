80d5bf1d442568f1b61b6d1b7898b9a3ef88f92d
Sean Mullan <mullan@openjdk.java.net>
2017-02-06 08:59:00 -0500

    8173827: Remove forRemoval=true from several deprecated security APIs
    Reviewed-by: redestad, xuelei

--- jdk/src/java.base/share/classes/com/sun/net/ssl/SSLContextSpi.java
+++ jdk/src/java.base/share/classes/com/sun/net/ssl/SSLContextSpi.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,9 +43,8 @@ import javax.net.ssl.*;
  *
  * @deprecated As of JDK 1.4, this implementation-specific class was
  *      replaced by {@link javax.net.ssl.SSLContextSpi}.
- *      This class is subject to removal in a future version of JDK.
  */
-@Deprecated(since="1.4", forRemoval=true)
+@Deprecated(since="1.4")
 public abstract class SSLContextSpi {
     /**
      * Initializes this context.
