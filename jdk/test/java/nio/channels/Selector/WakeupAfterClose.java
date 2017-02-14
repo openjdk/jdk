52bf1f47f23e967b1cca8bb9439dc9d2ee42dcbc
Joe Darcy <darcy@openjdk.org>
2017-02-14 13:08:33 -0800

    8174960: Mark WakeupAfterClose.java as failing intermittentl
    Reviewed-by: rriggs, bpb

--- jdk/test/java/nio/channels/Selector/WakeupAfterClose.java
+++ jdk/test/java/nio/channels/Selector/WakeupAfterClose.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
 /* @test
  * @bug 6524172
  * @summary Invoking wakeup on closed Selector can throw NPE if close resets interrupt status
+ * @key intermittent
  */
 
 import java.nio.channels.Selector;
