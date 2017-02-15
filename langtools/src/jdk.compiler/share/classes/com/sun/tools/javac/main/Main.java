6873ceb82cd97ae5e27350d3604bd1615e7f9f5e
Jonathan Gibbons <jjg@openjdk.org>
2017-02-15 14:12:29 -0800

    8173596: JavaCompiler.CompilationTask should support addModules
    Reviewed-by: ksrini, jlahoda

--- langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/main/Main.java
+++ langtools/src/jdk.compiler/share/classes/com/sun/tools/javac/main/Main.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -304,7 +304,7 @@ public class Main {
         }
 
         try {
-            comp.compile(args.getFileObjects(), args.getClassNames(), null);
+            comp.compile(args.getFileObjects(), args.getClassNames(), null, List.nil());
 
             if (log.expectDiagKeys != null) {
                 if (log.expectDiagKeys.isEmpty()) {
