32e70396f43716669917c622d63ed95d96ac9b55
Mandy Chung <mchung@openjdk.org>
2017-02-24 09:26:59 -0800

    8175193: jlink and `requires static`
    Reviewed-by: alanb, forax

--- jdk/test/tools/jlink/plugins/SystemModuleDescriptors/src/m5/module-info.java
+++ jdk/test/tools/jlink/plugins/SystemModuleDescriptors/src/m5/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,7 +21,7 @@
  * questions.
  */
 
-module m3 {
-    requires m4;
-    exports p3;
+module m5 {
+    requires static m3;
+    exports p5;
 }
