99d6ff5c7688d191d61b85f1813625971a5ef13a
Thomas Stuefe <stuefe@openjdk.org>
2017-03-23 17:45:13 +0100

    8177458: [s390] (jdk10) build broken after 8155672
    Reviewed-by: simonis

--- hotspot/src/cpu/s390/vm/methodHandles_s390.cpp
+++ hotspot/src/cpu/s390/vm/methodHandles_s390.cpp
@@ -73,7 +73,7 @@ void MethodHandles::verify_klass(MacroAssembler* _masm,
                                  const char* error_message) {
 
   InstanceKlass** klass_addr = SystemDictionary::well_known_klass_addr(klass_id);
-  KLass* klass = SystemDictionary::well_known_klass(klass_id);
+  Klass* klass = SystemDictionary::well_known_klass(klass_id);
 
   assert(temp_reg != Z_R0 && // Is used as base register!
          temp_reg != noreg && temp2_reg != noreg, "need valid registers!");
