700a717639b10b1acf50c038b078ad2237d73d30
Mandy Chung <mchung@openjdk.java.net>
2017-03-29 09:41:36 -0700

    8173303: Add module-subgraph images to main platform documentation
    Co-authored-by: Jonathan Gibbons <jonathan.gibbons@oracle.com>
    Reviewed-by: alanb, chegar, erikj, ihse, lancea

--- langtools/src/jdk.jdeps/share/classes/module-info.java
+++ langtools/src/jdk.jdeps/share/classes/module-info.java
@@ -26,6 +26,7 @@
 /** Defines tools for analysing dependencies in Java libraries and programs, including
  *  the <em>jdeps</em> and <em>javap</em> tools.
  *
+ *  @moduleGraph
  *  @since 9
  */
 module jdk.jdeps {
