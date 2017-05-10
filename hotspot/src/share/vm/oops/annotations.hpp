ca7bc471dd30e6e3cd5c5639019474fb85da2ebd
Harold Seigel <hseigel@openjdk.org>
2017-05-10 08:37:17 -0400

    8153646: Move vm/utilities/array.hpp to vm/oops
    Move the header file and update the appropriate #include statements.
    
    Reviewed-by: sspitsyn, dholmes

--- hotspot/src/share/vm/oops/annotations.hpp
+++ hotspot/src/share/vm/oops/annotations.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,9 +25,9 @@
 #ifndef SHARE_VM_OOPS_ANNOTATIONS_HPP
 #define SHARE_VM_OOPS_ANNOTATIONS_HPP
 
+#include "oops/array.hpp"
 #include "oops/metadata.hpp"
 #include "runtime/handles.hpp"
-#include "utilities/array.hpp"
 #include "utilities/exceptions.hpp"
 #include "utilities/globalDefinitions.hpp"
 
--- hotspot/src/share/vm/oops/array.hpp
+++ hotspot/src/share/vm/oops/array.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,19 +22,13 @@
  *
  */
 
-#ifndef SHARE_VM_UTILITIES_ARRAY_HPP
-#define SHARE_VM_UTILITIES_ARRAY_HPP
+#ifndef SHARE_VM_OOPS_ARRAY_HPP
+#define SHARE_VM_OOPS_ARRAY_HPP
 
 #include "memory/allocation.hpp"
 #include "memory/allocation.inline.hpp"
 #include "memory/metaspace.hpp"
 #include "runtime/orderAccess.hpp"
-
-// Arrays for basic types
-typedef GrowableArray<int> intArray;
-typedef GrowableArray<int> intStack;
-typedef GrowableArray<bool> boolArray;
-typedef GrowableArray<bool> boolStack;
 
 // Array for metadata allocation
 
@@ -155,4 +149,4 @@ protected:
 };
 
 
-#endif // SHARE_VM_UTILITIES_ARRAY_HPP
+#endif // SHARE_VM_OOPS_ARRAY_HPP
