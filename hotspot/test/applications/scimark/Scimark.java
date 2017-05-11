8ea9daf94a574c48549ee3d08cc0586da2de6c89
Igor Ignatyev <iignatyev@openjdk.org>
2017-05-11 16:35:02 -0700

    8179930: jdk.test.lib.artifacts.ArtifactResolver::resolve should return Map instead of HashMap
    Reviewed-by: mseledtsov, gtriantafill, vlivanov

--- hotspot/test/applications/scimark/Scimark.java
+++ hotspot/test/applications/scimark/Scimark.java
@@ -32,12 +32,12 @@ import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.artifacts.Artifact;
 import jdk.test.lib.artifacts.ArtifactResolver;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.Map;
 
 @Artifact(organization = "gov.nist.math", name = "scimark", revision = "2.0", extension = "zip")
 public class Scimark {
     public static void main(String... args) throws Exception {
-        HashMap<String, Path> artifacts = ArtifactResolver.resolve(Scimark.class);
+        Map<String, Path> artifacts = ArtifactResolver.resolve(Scimark.class);
 
         OutputAnalyzer output = new OutputAnalyzer(ProcessTools.createJavaProcessBuilder(
             "-cp", artifacts.get("gov.nist.math.scimark-2.0").toString(),
