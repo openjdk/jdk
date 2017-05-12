86fd2cf12d68f61c048d2ac13e22f4cbfd091ded
Magnus Ihse Bursie <ihse@openjdk.java.net>
2017-05-12 19:09:50 +0200

    8175825: Stop including pubs repo
    Reviewed-by: erikj

--- common/autoconf/generated-configure.sh
+++ common/autoconf/generated-configure.sh
@@ -5183,7 +5183,7 @@ VS_SDK_PLATFORM_NAME_2013=
 #CUSTOM_AUTOCONF_INCLUDE
 
 # Do not change or remove the following line, it is needed for consistency checks:
-DATE_WHEN_GENERATED=1493884285
+DATE_WHEN_GENERATED=1494608938
 
 ###############################################################################
 #
