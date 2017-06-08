a944319c4e28029eea584fbf4b9c9cac3c647e8f
Jonathan Gibbons <jjg@openjdk.org>
2017-06-08 15:50:22 -0700

    8180296: Move Javadoc: doclet, taglet specs to specs directory
    Reviewed-by: ksrini

--- langtools/src/jdk.javadoc/share/classes/module-info.java
+++ langtools/src/jdk.javadoc/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,9 @@
 /** Defines the implementation of the
  *  {@link javax.tools.ToolProvider#getSystemDocumentationTool system documentation tool}
  *  and its command line equivalent, <em>javadoc</em>.
+ *
+ *  @see <a href="{@docRoot}/../specs/doc-comment-spec.html">
+ *      Documentation Comment Specification for the Standard Doclet</a>
  *
  *  @moduleGraph
  *  @since 9
