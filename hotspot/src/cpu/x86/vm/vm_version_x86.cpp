02c87ce163328e8728c84833384926ca793867a8
Vladimir Kozlov <kvn@openjdk.java.net>
2017-06-16 12:06:31 -0700

    8182114: assert(VM_Version::supports_sse()) failed
    Restore UseSSE check for FMA
    
    Reviewed-by: dcubed

--- hotspot/src/cpu/x86/vm/vm_version_x86.cpp
+++ hotspot/src/cpu/x86/vm/vm_version_x86.cpp
@@ -812,7 +812,7 @@ void VM_Version::get_processor_features() {
     FLAG_SET_DEFAULT(UseGHASHIntrinsics, false);
   }
 
-  if (supports_fma()) {
+  if (supports_fma() && UseSSE >= 2) { // Check UseSSE since FMA code uses SSE instructions
     if (FLAG_IS_DEFAULT(UseFMA)) {
       UseFMA = true;
     }
