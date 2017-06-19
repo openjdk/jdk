157b885e4f3ea06b1526884ebfd9d909182f2116
Weijun Wang <weijun@openjdk.org>
2017-06-19 22:54:09 +0800

    8182118: Package summary is missing in jdk.security.auth module
    Reviewed-by: mullan, mchung

--- jdk/src/jdk.security.auth/share/classes/com/sun/security/auth/module/package-info.java
+++ jdk/src/jdk.security.auth/share/classes/com/sun/security/auth/module/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,4 +23,9 @@
  * questions.
  */
 
+/**
+ * Provides implementations of {@link javax.security.auth.spi.LoginModule}.
+ *
+ * @since 1.4
+ */
 package com.sun.security.auth.module;
