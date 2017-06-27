74e522e2d451383613e1425722864c3050b6b6cf
Patric Hedlin <patric.hedlin@oracle.com>
2017-06-27 15:27:54 +0200

    8172231: SPARC ISA/CPU feature detection is broken/insufficient (on Solaris)
    Update for new platforms
    
    Reviewed-by: kvn

--- hotspot/src/jdk.internal.vm.ci/share/classes/jdk.vm.ci.hotspot.sparc/src/jdk/vm/ci/hotspot/sparc/SPARCHotSpotJVMCIBackendFactory.java
+++ hotspot/src/jdk.internal.vm.ci/share/classes/jdk.vm.ci.hotspot.sparc/src/jdk/vm/ci/hotspot/sparc/SPARCHotSpotJVMCIBackendFactory.java
@@ -57,72 +57,120 @@ public class SPARCHotSpotJVMCIBackendFactory implements HotSpotJVMCIBackendFacto
 
     protected EnumSet<CPUFeature> computeFeatures(SPARCHotSpotVMConfig config) {
         EnumSet<CPUFeature> features = EnumSet.noneOf(CPUFeature.class);
-        if ((config.vmVersionFeatures & config.sparcVis1Instructions) != 0) {
-            features.add(CPUFeature.VIS1);
+
+        if ((config.vmVersionFeatures & 1L << config.sparc_ADI) != 0) {
+            features.add(CPUFeature.ADI);
         }
-        if ((config.vmVersionFeatures & config.sparcVis2Instructions) != 0) {
-            features.add(CPUFeature.VIS2);
+        if ((config.vmVersionFeatures & 1L << config.sparc_AES) != 0) {
+            features.add(CPUFeature.AES);
         }
-        if ((config.vmVersionFeatures & config.sparcVis3Instructions) != 0) {
-            features.add(CPUFeature.VIS3);
+        if ((config.vmVersionFeatures & 1L << config.sparc_BLK_INIT) != 0) {
+            features.add(CPUFeature.BLK_INIT);
         }
-        if ((config.vmVersionFeatures & config.sparcCbcondInstructions) != 0) {
+        if ((config.vmVersionFeatures & 1L << config.sparc_CAMELLIA) != 0) {
+            features.add(CPUFeature.CAMELLIA);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_CBCOND) != 0) {
             features.add(CPUFeature.CBCOND);
         }
-        if ((config.vmVersionFeatures & config.sparcV8Instructions) != 0) {
-            features.add(CPUFeature.V8);
+        if ((config.vmVersionFeatures & 1L << config.sparc_CRC32C) != 0) {
+            features.add(CPUFeature.CRC32C);
         }
-        if ((config.vmVersionFeatures & config.sparcHardwareMul32) != 0) {
-            features.add(CPUFeature.HARDWARE_MUL32);
+        if ((config.vmVersionFeatures & 1L << config.sparc_DES) != 0) {
+            features.add(CPUFeature.DES);
         }
-        if ((config.vmVersionFeatures & config.sparcHardwareDiv32) != 0) {
-            features.add(CPUFeature.HARDWARE_DIV32);
-        }
-        if ((config.vmVersionFeatures & config.sparcHardwareFsmuld) != 0) {
-            features.add(CPUFeature.HARDWARE_FSMULD);
-        }
-        if ((config.vmVersionFeatures & config.sparcHardwarePopc) != 0) {
-            features.add(CPUFeature.HARDWARE_POPC);
-        }
-        if ((config.vmVersionFeatures & config.sparcV9Instructions) != 0) {
-            features.add(CPUFeature.V9);
-        }
-        if ((config.vmVersionFeatures & config.sparcSun4v) != 0) {
-            features.add(CPUFeature.SUN4V);
-        }
-        if ((config.vmVersionFeatures & config.sparcBlkInitInstructions) != 0) {
-            features.add(CPUFeature.BLK_INIT_INSTRUCTIONS);
-        }
-        if ((config.vmVersionFeatures & config.sparcFmafInstructions) != 0) {
+        if ((config.vmVersionFeatures & 1L << config.sparc_FMAF) != 0) {
             features.add(CPUFeature.FMAF);
         }
-        if ((config.vmVersionFeatures & config.sparcSparc64Family) != 0) {
-            features.add(CPUFeature.SPARC64_FAMILY);
+        if ((config.vmVersionFeatures & 1L << config.sparc_HPC) != 0) {
+            features.add(CPUFeature.HPC);
         }
-        if ((config.vmVersionFeatures & config.sparcMFamily) != 0) {
-            features.add(CPUFeature.M_FAMILY);
+        if ((config.vmVersionFeatures & 1L << config.sparc_IMA) != 0) {
+            features.add(CPUFeature.IMA);
         }
-        if ((config.vmVersionFeatures & config.sparcTFamily) != 0) {
-            features.add(CPUFeature.T_FAMILY);
+        if ((config.vmVersionFeatures & 1L << config.sparc_KASUMI) != 0) {
+            features.add(CPUFeature.KASUMI);
         }
-        if ((config.vmVersionFeatures & config.sparcT1Model) != 0) {
-            features.add(CPUFeature.T1_MODEL);
+        if ((config.vmVersionFeatures & 1L << config.sparc_MD5) != 0) {
+            features.add(CPUFeature.MD5);
         }
-        if ((config.vmVersionFeatures & config.sparcSparc5Instructions) != 0) {
-            features.add(CPUFeature.SPARC5);
+        if ((config.vmVersionFeatures & 1L << config.sparc_MONT) != 0) {
+            features.add(CPUFeature.MONT);
         }
-        if ((config.vmVersionFeatures & config.sparcAesInstructions) != 0) {
-            features.add(CPUFeature.SPARC64_FAMILY);
+        if ((config.vmVersionFeatures & 1L << config.sparc_MPMUL) != 0) {
+            features.add(CPUFeature.MPMUL);
         }
-        if ((config.vmVersionFeatures & config.sparcSha1Instruction) != 0) {
+        if ((config.vmVersionFeatures & 1L << config.sparc_MWAIT) != 0) {
+            features.add(CPUFeature.MWAIT);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_PAUSE) != 0) {
+            features.add(CPUFeature.PAUSE);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_PAUSE_NSEC) != 0) {
+            features.add(CPUFeature.PAUSE_NSEC);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_POPC) != 0) {
+            features.add(CPUFeature.POPC);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_SHA1) != 0) {
             features.add(CPUFeature.SHA1);
         }
-        if ((config.vmVersionFeatures & config.sparcSha256Instruction) != 0) {
+        if ((config.vmVersionFeatures & 1L << config.sparc_SHA256) != 0) {
             features.add(CPUFeature.SHA256);
         }
-        if ((config.vmVersionFeatures & config.sparcSha512Instruction) != 0) {
+        if ((config.vmVersionFeatures & 1L << config.sparc_SHA512) != 0) {
             features.add(CPUFeature.SHA512);
         }
+        if ((config.vmVersionFeatures & 1L << config.sparc_SPARC5) != 0) {
+            features.add(CPUFeature.SPARC5);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_V9) != 0) {
+            features.add(CPUFeature.V9);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_VAMASK) != 0) {
+            features.add(CPUFeature.VAMASK);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_VIS1) != 0) {
+            features.add(CPUFeature.VIS1);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_VIS2) != 0) {
+            features.add(CPUFeature.VIS2);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_VIS3) != 0) {
+            features.add(CPUFeature.VIS3);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_VIS3B) != 0) {
+            features.add(CPUFeature.VIS3B);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_XMONT) != 0) {
+            features.add(CPUFeature.XMONT);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_XMPMUL) != 0) {
+            features.add(CPUFeature.XMPMUL);
+        }
+
+        if ((config.vmVersionFeatures & 1L << config.sparc_BLK_ZEROING) != 0) {
+            features.add(CPUFeature.BLK_ZEROING);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_BIS) != 0) {
+            features.add(CPUFeature.FAST_BIS);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_CMOVE) != 0) {
+            features.add(CPUFeature.FAST_CMOVE);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_IDIV) != 0) {
+            features.add(CPUFeature.FAST_IDIV);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_IND_BR) != 0) {
+            features.add(CPUFeature.FAST_IND_BR);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_LD) != 0) {
+            features.add(CPUFeature.FAST_LD);
+        }
+        if ((config.vmVersionFeatures & 1L << config.sparc_FAST_RDPC) != 0) {
+            features.add(CPUFeature.FAST_RDPC);
+        }
+
         return features;
     }
 
