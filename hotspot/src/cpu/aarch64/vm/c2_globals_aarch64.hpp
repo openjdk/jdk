fe5bc09ad0e32d933d6cd6e731ce15ba5d799d55
Roland Westrelin <roland@openjdk.org>
2017-07-04 15:58:15 +0200

    8183543: Aarch64: C2 compilation often fails with "failed spill-split-recycle sanity check"
    Reviewed-by: aph

--- hotspot/src/cpu/aarch64/vm/c2_globals_aarch64.hpp
+++ hotspot/src/cpu/aarch64/vm/c2_globals_aarch64.hpp
@@ -50,7 +50,7 @@ define_pd_global(intx, ConditionalMoveLimit,         3);
 define_pd_global(intx, FLOATPRESSURE,                64);
 define_pd_global(intx, FreqInlineSize,               325);
 define_pd_global(intx, MinJumpTableSize,             10);
-define_pd_global(intx, INTPRESSURE,                  25);
+define_pd_global(intx, INTPRESSURE,                  24);
 define_pd_global(intx, InteriorEntryAlignment,       16);
 define_pd_global(intx, NewSizeThreadIncrease, ScaleForWordSize(4*K));
 define_pd_global(intx, LoopUnrollLimit,              60);
