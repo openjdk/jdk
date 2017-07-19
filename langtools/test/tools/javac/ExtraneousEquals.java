c31576193ac0bb9b2022151094847f642bf2b680
Priya Lakshmi Muthuswamy <pmuthuswamy@openjdk.org>
2017-07-19 11:44:08 +0530

    8057647: javac parser needs updates to have better error recovery for error cases of new array creation with dimensions
    Reviewed-by: jlahoda

--- langtools/test/tools/javac/ExtraneousEquals.java
+++ langtools/test/tools/javac/ExtraneousEquals.java
@@ -1,6 +1,6 @@
 /*
  * @test /nodynamiccopyright/
- * @bug 5019614
+ * @bug 5019614 8057647
  * @summary variance prototype syntax leftover
  *
  * @compile/fail/ref=ExtraneousEquals.out -XDrawDiagnostics ExtraneousEquals.java
