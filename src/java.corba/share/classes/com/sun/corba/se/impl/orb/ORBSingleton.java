e118d9e9b534201d6456971bdea90709153c75d7
Mark Sheppard <msheppar@openjdk.java.net>
2017-09-03 16:08:13 +0100

    8160104: CORBA communication improvements
    Reviewed-by: rriggs, dfuchs

--- src/java.corba/share/classes/com/sun/corba/se/impl/orb/ORBSingleton.java
+++ src/java.corba/share/classes/com/sun/corba/se/impl/orb/ORBSingleton.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -768,6 +768,13 @@ public class ORBSingleton extends ORB
     public CopierManager getCopierManager() {
         return null ;
     }
+
+    @Override
+    public void validateIORClass(String iorClassName) {
+        getFullORB().validateIORClass(iorClassName);
+
+    }
+
 }
 
 // End of file.
