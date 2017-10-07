17b98d7be82cf52d2f346ffed033f0bc9e57d240
Yasumasa Suenaga <ysuenaga@openjdk.java.net>
2017-10-07 22:45:12 +0900

    8187403: [Unknown generation] is shown in Stack Memory on HSDB
    Reviewed-by: sspitsyn, jgeorge

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/g1/HeapRegionType.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/g1/HeapRegionType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,69 +24,69 @@
 
 package sun.jvm.hotspot.gc.g1;
 
-import java.util.Iterator;
 import java.util.Observable;
 import java.util.Observer;
-
 import sun.jvm.hotspot.debugger.Address;
 import sun.jvm.hotspot.runtime.VM;
 import sun.jvm.hotspot.runtime.VMObject;
-import sun.jvm.hotspot.runtime.VMObjectFactory;
-import sun.jvm.hotspot.types.AddressField;
 import sun.jvm.hotspot.types.CIntegerField;
 import sun.jvm.hotspot.types.Type;
 import sun.jvm.hotspot.types.TypeDataBase;
 
-// Mirror class for HeapRegionManager.
+// Mirror class for HeapRegionType. Currently we don't actually include
+// any of its fields but only iterate over it.
 
-public class HeapRegionManager extends VMObject {
-    // G1HeapRegionTable _regions
-    static private long regionsFieldOffset;
-    // uint _committed_length
-    static private CIntegerField numCommittedField;
+public class HeapRegionType extends VMObject {
+
+    private static int freeTag;
+    private static int youngMask;
+    private static int humongousMask;
+    private static int pinnedMask;
+    private static int oldMask;
+    private static CIntegerField tagField;
+    private int tag;
 
     static {
         VM.registerVMInitializedObserver(new Observer() {
                 public void update(Observable o, Object data) {
                     initialize(VM.getVM().getTypeDataBase());
                 }
-            });
+        });
     }
 
-    static private synchronized void initialize(TypeDataBase db) {
-        Type type = db.lookupType("HeapRegionManager");
+    private static synchronized void initialize(TypeDataBase db) {
+        Type type = db.lookupType("HeapRegionType");
 
-        regionsFieldOffset = type.getField("_regions").getOffset();
-        numCommittedField = type.getCIntegerField("_num_committed");
+        tagField = type.getCIntegerField("_tag");
+
+        freeTag = db.lookupIntConstant("HeapRegionType::FreeTag");
+        youngMask = db.lookupIntConstant("HeapRegionType::YoungMask");
+        humongousMask = db.lookupIntConstant("HeapRegionType::HumongousMask");
+        pinnedMask = db.lookupIntConstant("HeapRegionType::PinnedMask");
+        oldMask = db.lookupIntConstant("HeapRegionType::OldMask");
     }
 
-    private G1HeapRegionTable regions() {
-        Address regionsAddr = addr.addOffsetTo(regionsFieldOffset);
-        return (G1HeapRegionTable) VMObjectFactory.newObject(G1HeapRegionTable.class,
-                                                             regionsAddr);
+    public boolean isFree() {
+        return tagField.getValue(addr) == freeTag;
     }
 
-    public long capacity() {
-        return length() * HeapRegion.grainBytes();
+    public boolean isYoung() {
+        return (tagField.getValue(addr) & youngMask) != 0;
     }
 
-    public long length() {
-        return regions().length();
+    public boolean isHumongous() {
+        return (tagField.getValue(addr) & humongousMask) != 0;
     }
 
-    public long committedLength() {
-        return numCommittedField.getValue(addr);
+    public boolean isPinned() {
+        return (tagField.getValue(addr) & pinnedMask) != 0;
     }
 
-    public Iterator<HeapRegion> heapRegionIterator() {
-        return regions().heapRegionIterator(length());
+    public boolean isOld() {
+        return (tagField.getValue(addr) & oldMask) != 0;
     }
 
-    public HeapRegionManager(Address addr) {
+    public HeapRegionType(Address addr) {
         super(addr);
-    }
-
-    public HeapRegion getByAddress(Address addr) {
-      return regions().getByAddress(addr);
     }
 }
