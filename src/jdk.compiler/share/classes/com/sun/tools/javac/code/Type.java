8315ac39ccc3c16373a447d823d397c52e5ddb03
Vicente Romero <vromero@openjdk.org>
2017-10-17 07:11:05 -0700

    8187624: NPE in Type.UndetVar.debugString()
    Reviewed-by: mcimadamore

--- src/jdk.compiler/share/classes/com/sun/tools/javac/code/Type.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/code/Type.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1976,7 +1976,7 @@ public abstract class Type extends AnnoConstruct implements TypeMirror {
             }
             for (InferenceBound bound: InferenceBound.values()) {
                 List<Type> aboundList = bounds.get(bound);
-                if (aboundList.size() > 0) {
+                if (aboundList != null && aboundList.size() > 0) {
                     result += bound + " = " + aboundList + '\n';
                 }
             }
