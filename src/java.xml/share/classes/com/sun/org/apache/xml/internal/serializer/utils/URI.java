abd084a8d7dd0aedd132dbfbc7a0f50655086161
Joe Wang <joehw@openjdk.java.net>
2017-10-24 19:05:29 -0700

    8176891: Fix lint warnings in JAXP repo: serial
    Reviewed-by: lancea

--- src/java.xml/share/classes/com/sun/org/apache/xml/internal/serializer/utils/URI.java
+++ src/java.xml/share/classes/com/sun/org/apache/xml/internal/serializer/utils/URI.java
@@ -1,6 +1,6 @@
 /*
- * reserved comment block
- * DO NOT REMOVE OR ALTER!
+ * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * @LastModified: Oct 2017
  */
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -73,6 +73,7 @@ final class URI
    */
   public static class MalformedURIException extends IOException
   {
+    private static final long serialVersionUID = 4651455286983598951L;
 
     /**
      * Constructs a <code>MalformedURIException</code> with no specified
--- src/java.xml/share/classes/javax/xml/parsers/ParserConfigurationException.java
+++ src/java.xml/share/classes/javax/xml/parsers/ParserConfigurationException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2005, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,7 +33,7 @@ package javax.xml.parsers;
  */
 
 public class ParserConfigurationException extends Exception {
-
+    private static final long serialVersionUID = -3688849216575373917L;
     /**
      * Create a new <code>ParserConfigurationException</code> with no
      * detail message.
