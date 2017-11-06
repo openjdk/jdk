9167c2a7f0ad6e808304964e2e3a01e82c58b56e
Michael McMahon <michaelm@openjdk.java.net>
2017-11-06 16:32:00 +0000

    8190793: Httpserver does not detect truncated request body
    Reviewed-by: chegar, dfuchs

--- src/jdk.httpserver/share/classes/sun/net/httpserver/FixedLengthInputStream.java
+++ src/jdk.httpserver/share/classes/sun/net/httpserver/FixedLengthInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2017, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -60,6 +60,8 @@ class FixedLengthInputStream extends LeftOverInputStream {
                 t.getServerImpl().requestCompleted (t.getConnection());
             }
         }
+        if (n < 0 && !eof)
+            throw new IOException("connection closed before all data received");
         return n;
     }
 
