f988f60dc873d5c5b3331cc5223343c0872b4ab2
Harold Seigel <harold.seigel@oracle.com>
2017-11-07 16:07:36 -0500

    8189123: More consistent classloading
    Reviewed-by: acorn, ahgross, rhalade

--- src/hotspot/share/classfile/verifier.cpp
+++ src/hotspot/share/classfile/verifier.cpp
@@ -2013,9 +2013,12 @@ Klass* ClassVerifier::load_class(Symbol* name, TRAPS) {
     name, Handle(THREAD, loader), Handle(THREAD, protection_domain),
     true, THREAD);
 
-  if (log_is_enabled(Debug, class, resolve)) {
-    InstanceKlass* cur_class = InstanceKlass::cast(current_class());
-    Verifier::trace_class_resolution(kls, cur_class);
+  if (kls != NULL) {
+    current_class()->class_loader_data()->record_dependency(kls);
+    if (log_is_enabled(Debug, class, resolve)) {
+      InstanceKlass* cur_class = InstanceKlass::cast(current_class());
+      Verifier::trace_class_resolution(kls, cur_class);
+    }
   }
   return kls;
 }
