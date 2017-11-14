5509aec55dbf16ad79d70924967644f29e58b42d
Sean Mullan <mullan@openjdk.java.net>
2017-11-14 09:15:12 -0500

    8175094: Mark the deprecated java.security.acl APIs with forRemoval=true
    Reviewed-by: weijun

--- src/java.base/share/classes/java/security/acl/package-info.java
+++ src/java.base/share/classes/java/security/acl/package-info.java
@@ -27,7 +27,8 @@
  * The classes and interfaces in this package have been deprecated. New
  * classes should not be added to this package. The {@code java.security}
  * package contains suitable replacements. See {@link java.security.Policy}
- * and related classes for details.
+ * and related classes for details. This package is subject to removal in a
+ * future version of Java SE.
  *
  * @since 1.1
  */
