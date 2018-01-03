0e5ad26ad4623f93ee2faf959f91b9fbb94c42ac
Sharath Ballal <sballal@openjdk.java.net>
2018-01-03 11:14:04 +0530

    8194058: [TESTBUG] serviceability/sa/ClhsdbWhere.java fails to find method 'sleep' in output
    Reviewed-by: dholmes, cjplummer

--- test/hotspot/jtreg/serviceability/sa/ClhsdbWhere.java
+++ test/hotspot/jtreg/serviceability/sa/ClhsdbWhere.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -60,7 +60,7 @@ public class ClhsdbWhere {
                     "private static void processPendingReferences",
                     "private static native void waitForReferencePendingList",
                     "Java Stack Trace for main",
-                    "public static native void sleep"));
+                    "public static void main"));
 
             test.run(theApp.getPid(), cmds, expStrMap, null);
         } catch (Exception ex) {
