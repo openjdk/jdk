f42b8e4a00ec28334c79af239520611075241a33
Mandy Chung <mchung@openjdk.org>
2018-02-20 11:28:32 -0800

    8198441: Replace native Runtime::runFinalization0 method with shared secrets
    Reviewed-by: martin

--- src/java.base/share/classes/java/lang/ref/Reference.java
+++ src/java.base/share/classes/java/lang/ref/Reference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -234,6 +234,11 @@ public abstract class Reference<T> {
                 throws InterruptedException
             {
                 return Reference.waitForReferenceProcessing();
+            }
+
+            @Override
+            public void runFinalization() {
+                Finalizer.runFinalization();
             }
         });
     }
