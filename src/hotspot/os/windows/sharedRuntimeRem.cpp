56d737d8c7b5e646ae8fd32abb1e1796e4f0ee2d
Thomas Stuefe <stuefe@openjdk.org>
2018-02-22 16:29:10 +0100

    8198558: Windows does not build without precompiled headers
    Reviewed-by: coleenp, mlarsson, lfoltan

--- src/hotspot/os/windows/sharedRuntimeRem.cpp
+++ src/hotspot/os/windows/sharedRuntimeRem.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+* Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 *
 * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 */
 
 #include "precompiled.hpp"
+#include "runtime/sharedRuntime.hpp"
 
 #ifdef _WIN64
 // These are copied defines from fdlibm.h, this allows us to keep the code
