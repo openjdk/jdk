a131e1668fdf18ac0f91c2ff22964e694209bbdb
Brian Burkhalter <bpb@openjdk.org>
2018-03-05 08:27:42 -0800

    8198834: (ch) Enable java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java on linux-x64
    Move to using centralized native build and remove obsolete .so files
    
    Reviewed-by: alanb, erikj

--- test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java
+++ test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,7 +34,7 @@
  *        jdk.test.lib.Platform
  *        jdk.test.lib.process.*
  *        StateTest StateTestService EchoTest EchoService CloseTest Launcher Util
- * @run testng/othervm InheritedChannelTest
+ * @run testng/othervm/native InheritedChannelTest
  * @key intermittent
  */
 
@@ -68,9 +68,7 @@ public class InheritedChannelTest {
     private static final String OS_ARCH = ARCH.equals("i386") ? "i586" : ARCH;
 
     private static final Path LD_LIBRARY_PATH
-            = Paths.get(TEST_SRC, "lib", OS_NAME + "-" + OS_ARCH);
-
-    private static final Path LAUNCHERLIB = LD_LIBRARY_PATH.resolve("libLauncher.so");
+            = Paths.get(System.getProperty("java.library.path"));
 
     @DataProvider
     public Object[][] testCases() {
@@ -99,11 +97,6 @@ public class InheritedChannelTest {
 
     @Test(dataProvider = "testCases")
     public void test(String desc, List<String> opts) throws Throwable {
-        if (!Files.exists(LAUNCHERLIB)) {
-            System.out.println("Cannot find " + LAUNCHERLIB
-                    + " - library not available for this system");
-            return;
-        }
         System.out.println("LD_LIBRARY_PATH=" + LD_LIBRARY_PATH);
 
         List<String> args = new ArrayList<>();
