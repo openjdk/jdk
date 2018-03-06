5b02a98b210e5d6fbf6807835ae13158b5a55a28
Sean Coffey <coffeys@openjdk.org>
2018-03-06 10:31:14 +0000

    8199018: Test crypto provider not registering
    Reviewed-by: wetmore

--- test/jdk/sun/security/ssl/HandshakeHash/MyProvider.java
+++ test/jdk/sun/security/ssl/HandshakeHash/MyProvider.java
@@ -28,8 +28,8 @@ public final class MyProvider extends Provider {
     public MyProvider() {
         super("MyProvider", "1.0",
                 "Test Provider: SHA1/MD5/SHA256 exhaustion testing");
-        put("MessageDigest.SHA", "DigestBase.SHADigest");
-        put("MessageDigest.MD5", "DigestBase.MD5Digest");
-        put("MessageDigest.SHA-256", "DigestBase.SHA256Digest");
+        put("MessageDigest.SHA", "DigestBase$SHA");
+        put("MessageDigest.MD5", "DigestBase$MD5");
+        put("MessageDigest.SHA-256", "DigestBase$SHA256");
     }
 }
