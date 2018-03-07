e7d78f6eb4a6c134c883118af4a9286914a708ca
Harold Seigel <hseigel@openjdk.java.net>
2018-03-07 09:32:46 -0500

    8159850: Remove unneeded parsing of optional-size when parsing array types
    Remove skip_optional_size() methods and calls to them.
    
    Reviewed-by: coleenp, iklam

--- src/hotspot/share/runtime/fieldType.hpp
+++ src/hotspot/share/runtime/fieldType.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -51,7 +51,6 @@ class FieldArrayInfo : public StackObj {
 
 class FieldType: public AllStatic {
  private:
-  static void skip_optional_size(Symbol* signature, int* index);
   static bool is_valid_array_signature(Symbol* signature);
  public:
 
