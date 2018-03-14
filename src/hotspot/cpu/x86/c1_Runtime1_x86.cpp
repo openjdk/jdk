cb1ebc46374aff9ca8404eced95f997a0b7d0054
Aleksey Shipilev <shade@openjdk.java.net>
2018-03-14 10:38:02 +0100

    8199511: Do not generate g1_{pre|post}_barrier_slow_id without CardTable-enabled barrier set
    Reviewed-by: rkennke, kvn, pliden, stefank

--- src/hotspot/cpu/x86/c1_Runtime1_x86.cpp
+++ src/hotspot/cpu/x86/c1_Runtime1_x86.cpp
@@ -1632,6 +1632,13 @@ OopMapSet* Runtime1::generate_code_for(StubID id, StubAssembler* sasm) {
       {
         StubFrame f(sasm, "g1_post_barrier", dont_gc_arguments);
 
+        BarrierSet* bs = Universe::heap()->barrier_set();
+        if (bs->kind() != BarrierSet::G1BarrierSet) {
+          __ movptr(rax, (int)id);
+          __ call_RT(noreg, noreg, CAST_FROM_FN_PTR(address, unimplemented_entry), rax);
+          __ should_not_reach_here();
+          break;
+        }
 
         // arg0: store_address
         Address store_addr(rbp, 2*BytesPerWord);
