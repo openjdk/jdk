d267620ffc7db3a08db07040d79d8e9778a1d7a2
Ivan Gerasimov <igerasim@openjdk.java.net>
2018-03-26 17:30:14 -0700

    8199843: Optimize Integer/Long.highestOneBit()
    Reviewed-by: redestad, plevart

--- src/java.base/share/classes/java/lang/Integer.java
+++ src/java.base/share/classes/java/lang/Integer.java
@@ -1574,13 +1574,7 @@ public final class Integer extends Number implements Comparable<Integer> {
      * @since 1.5
      */
     public static int highestOneBit(int i) {
-        // HD, Figure 3-1
-        i |= (i >>  1);
-        i |= (i >>  2);
-        i |= (i >>  4);
-        i |= (i >>  8);
-        i |= (i >> 16);
-        return i - (i >>> 1);
+        return i & (MIN_VALUE >>> numberOfLeadingZeros(i));
     }
 
     /**
