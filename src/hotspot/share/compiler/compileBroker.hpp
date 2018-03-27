c530e2724d46d4ed8f882ab4392d0147a181730a
Tobias Hartmann <thartmann@openjdk.org>
2018-03-27 11:51:39 +0200

    8200230: [Graal] Compilations should not be enqueued before Graal is initialized
    Split compiler initialization into two phases to avoid compilations being enqueued before Graal is initialized.
    
    Reviewed-by: kvn, dnsimon

--- src/hotspot/share/compiler/compileBroker.hpp
+++ src/hotspot/share/compiler/compileBroker.hpp
@@ -282,7 +282,8 @@ public:
     CompileQueue *q = compile_queue(comp_level);
     return q != NULL ? q->size() : 0;
   }
-  static void compilation_init(TRAPS);
+  static void compilation_init_phase1(TRAPS);
+  static void compilation_init_phase2();
   static void init_compiler_thread_log();
   static nmethod* compile_method(const methodHandle& method,
                                  int osr_bci,
