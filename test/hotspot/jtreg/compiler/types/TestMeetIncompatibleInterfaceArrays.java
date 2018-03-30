6bdeaad21ae7217c7f62222f4d86bd7e59e2a6c4
Vladimir Kozlov <kvn@openjdk.org>
2018-03-30 07:47:20 -0700

    8200461: MeetIncompatibleInterfaceArrays test fails with -Xcomp
    Add requires to run test only in Xmixed mode.
    
    Reviewed-by: simonis

--- test/hotspot/jtreg/compiler/types/TestMeetIncompatibleInterfaceArrays.java
+++ test/hotspot/jtreg/compiler/types/TestMeetIncompatibleInterfaceArrays.java
@@ -25,6 +25,7 @@
  * @test
  * @bug 8141551
  * @summary C2 can not handle returns with inccompatible interface arrays
+ * @requires vm.compMode == "Xmixed" & vm.flavor == "server"
  * @modules java.base/jdk.internal.org.objectweb.asm
  *          java.base/jdk.internal.misc
  * @library /test/lib /
