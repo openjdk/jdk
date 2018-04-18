4dd97e1df04602d3b96f8c22987f4722c4ecf374
Felix Yang <xiaofeya@openjdk.java.net>
2018-04-17 23:27:41 -0700

    8194260: Point-to-point interface should be excluded from java/net/ipv6tests/*
    Reviewed-by: chegar, vtewari

--- test/jdk/java/net/ipv6tests/TcpTest.java
+++ test/jdk/java/net/ipv6tests/TcpTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,9 @@
  * @bug 4868820
  * @key intermittent
  * @summary IPv6 support for Windows XP and 2003 server
+ * @library /test/lib
+ * @build jdk.test.lib.NetworkConfiguration
+ *        jdk.test.lib.Platform
  * @run main TcpTest -d
  */
 
