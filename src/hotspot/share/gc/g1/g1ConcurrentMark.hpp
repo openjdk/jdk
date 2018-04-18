270f8c2d7a20142e7cd56615044b6429dead6def
Thomas Schatzl <tschatzl@openjdk.java.net>
2018-04-18 11:36:48 +0200

    8178105: Switch mark bitmaps during Remark
    Reviewed-by: sjohanss, sangheki

--- src/hotspot/share/gc/g1/g1ConcurrentMark.hpp
+++ src/hotspot/share/gc/g1/g1ConcurrentMark.hpp
@@ -367,7 +367,7 @@ class G1ConcurrentMark : public CHeapObj<mtGC> {
   void weak_refs_work_parallel_part(BoolObjectClosure* is_alive, bool purged_classes);
   void weak_refs_work(bool clear_all_soft_refs);
 
-  void report_object_count();
+  void report_object_count(bool mark_completed);
 
   void swap_mark_bitmaps();
 
