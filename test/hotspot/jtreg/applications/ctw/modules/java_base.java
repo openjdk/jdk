e14f48de6d4f4639a6e27442f0b15339ee8cb5c2
Mikael Vidstedt <mikael@openjdk.java.net>
2018-04-24 21:40:10 -0700

    8202169: Reduce ctw_2 duration by parallelizing CtwRunner invocations
    Reviewed-by: kvn, iignatyev

--- test/hotspot/jtreg/applications/ctw/modules/java_base.java
+++ test/hotspot/jtreg/applications/ctw/modules/java_base.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @summary run CTW for all classes from java.base module
+ * @summary run CTW for some classes from java.base module
  *
  * @library /test/lib / /testlibrary/ctw/src
  * @modules java.base/jdk.internal.jimage
@@ -34,5 +34,5 @@
  * @build sun.hotspot.WhiteBox
  * @run driver ClassFileInstaller sun.hotspot.WhiteBox
  *                                sun.hotspot.WhiteBox$WhiteBoxPermission
- * @run main/timeout=7200 sun.hotspot.tools.ctw.CtwRunner modules:java.base
+ * @run main/timeout=7200 sun.hotspot.tools.ctw.CtwRunner modules:java.base 0% 50%
  */
