cf20cdd06c0173a98f775bedca405cb56d95b918
Jonathan Gibbons <jjg@openjdk.org>
2018-05-03 12:56:53 -0700

    8194968: problem list actions for tools/javac/jvm/VerboseOutTest
    Reviewed-by: vromero

--- src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/main/JavaCompiler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -784,7 +784,7 @@ public class JavaCompiler {
             try (BufferedWriter out = new BufferedWriter(outFile.openWriter())) {
                 new Pretty(out, true).printUnit(env.toplevel, cdef);
                 if (verbose)
-                    log.printVerbose("wrote.file", outFile);
+                    log.printVerbose("wrote.file", outFile.getName());
             }
             return outFile;
         }
