4e738c3ae0de0094128439b22a355f1e1895a092
Joe Darcy <darcy@openjdk.java.net>
2018-05-05 09:34:01 -0700

    8202599: Mark intermittently failing jshell tests
    Reviewed-by: rfield

--- test/langtools/jdk/jshell/JdiHangingListenExecutionControlTest.java
+++ test/langtools/jdk/jshell/JdiHangingListenExecutionControlTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,6 +28,7 @@
  * @modules jdk.jshell/jdk.jshell jdk.jshell/jdk.jshell.spi jdk.jshell/jdk.jshell.execution
  * @build HangingRemoteAgent
  * @run testng JdiHangingListenExecutionControlTest
+ * @key intermittent
  */
 
 import org.testng.annotations.Test;
