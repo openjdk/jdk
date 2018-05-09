8b1470f8e7ad0a9560bdb7e87502936922507555
Weijun Wang <weijun@openjdk.org>
2018-05-09 09:50:25 +0800

    8202816: Update JarSigning.keystore
    Reviewed-by: xuelei

--- test/jdk/sun/security/tools/jarsigner/JarSigningNonAscii.java
+++ test/jdk/sun/security/tools/jarsigner/JarSigningNonAscii.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 4924188
+ * @bug 4924188 8202816
  * @summary sign a JAR file that has entry names with non-ASCII characters.
  * @modules jdk.jartool/sun.security.tools.jarsigner
  * @run main/othervm JarSigningNonAscii
@@ -42,7 +42,6 @@ public class JarSigningNonAscii {
     private static String keystore;
 
     public static void main(String[] args) throws Exception {
-        Security.setProperty("jdk.jar.disabledAlgorithms", "");
 
         String srcDir = System.getProperty("test.src", ".");
         String destDir = System.getProperty("test.classes", ".");
