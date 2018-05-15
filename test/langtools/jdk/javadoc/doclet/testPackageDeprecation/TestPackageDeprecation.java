2612bef51e2b7ac961e2c4a0d7c4eac66465583e
Jonathan Gibbons <jjg@openjdk.org>
2018-05-15 14:41:06 -0700

    8202614: Add ability to validate links in JavadocTester
    Reviewed-by: ksrini

--- test/langtools/jdk/javadoc/doclet/testPackageDeprecation/TestPackageDeprecation.java
+++ test/langtools/jdk/javadoc/doclet/testPackageDeprecation/TestPackageDeprecation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,6 +36,7 @@ public class TestPackageDeprecation extends JavadocTester {
 
     public static void main(String... args) throws Exception {
         TestPackageDeprecation tester = new TestPackageDeprecation();
+        tester.setAutomaticCheckLinks(false); // @ignore JDK-8202626
         tester.runTests();
     }
 
