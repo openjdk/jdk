41e15e635225bcab0a06ccd8db159dcb976b4475
Coleen Phillimore <coleenp@openjdk.java.net>
2018-05-21 17:17:24 -0400

    8202853: NotifyCount is not initialized
    Remove NotifyCount
    
    Reviewed-by: jiangli

--- src/hotspot/share/runtime/mutex.cpp
+++ src/hotspot/share/runtime/mutex.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -681,7 +681,6 @@ bool Monitor::notify() {
   assert(_owner == Thread::current(), "invariant");
   assert(ILocked(), "invariant");
   if (_WaitSet == NULL) return true;
-  NotifyCount++;
 
   // Transfer one thread from the WaitSet to the EntryList or cxq.
   // Currently we just unlink the head of the WaitSet and prepend to the cxq.
