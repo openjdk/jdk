6ee0c207fc9d81b325c22fcd153a2aca9b049366
Maurizio Cimadamore <mcimadamore@openjdk.java.net>
2018-05-22 13:37:05 +0100

    8203436: javac should fail early when emitting illegal signature attributes
    Check that signature attributes do not contain non-denotable types
    
    Reviewed-by: vromero

--- test/langtools/tools/javac/diags/examples/IllegalSignature.java
+++ test/langtools/tools/javac/diags/examples/IllegalSignature.java
@@ -1,10 +1,12 @@
 /*
- * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -22,13 +24,12 @@
  */
 
 // key: compiler.err.cannot.generate.class
-// key: compiler.misc.synthetic.name.conflict
+// key: compiler.misc.illegal.signature
 
-class ErrSyntheticNameConflict {
+class IllegalSignature<X> {
+    class Inner { }
 
-    static class Outer {
-        ErrSyntheticNameConflict this$0 = null;
+    void test(IllegalSignature<?> outer) {
+        outer.new Inner() { };
     }
-
-    public class Inner extends Outer { }
 }
