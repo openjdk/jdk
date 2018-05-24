a35e8c9b868074e76677c8044b87f2c5fe6cf285
John Jiang <jjiang@openjdk.java.net>
2018-05-23 18:18:33 -0700

    8203686: Add JDK 11 major version to javax/net/ssl/compatibility/JdkRelease.java
    Javax/net/ssl/compatibility/JdkRelease.java adds major version JDK 11
    
    Reviewed-by: xuelei

--- test/jdk/javax/net/ssl/compatibility/JdkRelease.java
+++ test/jdk/javax/net/ssl/compatibility/JdkRelease.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,7 +30,8 @@ public enum JdkRelease {
     JDK7(7, "1.7"),
     JDK8(8, "1.8"),
     JDK9(9, "9"),
-    JDK10(10, "10");
+    JDK10(10, "10"),
+    JDK11(11, "11");
 
     public final int sequence;
     public final String release;
@@ -51,6 +52,8 @@ public enum JdkRelease {
             return JDK9;
         } else if (jdkVersion.startsWith(JDK10.release)) {
             return JDK10;
+        } else if (jdkVersion.startsWith(JDK11.release)) {
+            return JDK11;
         }
 
         return null;
