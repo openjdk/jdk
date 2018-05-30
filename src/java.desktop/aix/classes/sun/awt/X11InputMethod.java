498875bcac5666ca6ba6bfe2ef89854d7e8bc2b4
Christoph Langer <clanger@openjdk.java.net>
2018-05-30 08:19:59 +0200

    8201429: Support AIX Input Method Editor (IME) for AWT Input Method Framework (IMF)
    Co-authored-by: Ichiroh Takiguchi <takiguc@linux.vnet.ibm.com>
    Reviewed-by: prr

--- src/java.desktop/aix/classes/sun/awt/X11InputMethod.java
+++ src/java.desktop/aix/classes/sun/awt/X11InputMethod.java
@@ -26,6 +26,7 @@
 package sun.awt;
 
 import java.awt.AWTException;
+import java.awt.EventQueue;
 import java.awt.event.InputMethodEvent;
 import java.awt.font.TextAttribute;
 import java.awt.font.TextHitInfo;
@@ -35,11 +36,14 @@ import java.text.AttributedString;
 import sun.util.logging.PlatformLogger;
 
 /**
- * Input Method Adapter for XIM
+ * Input Method Adapter for XIM for AIX
  *
  * @author JavaSoft International
  */
 public abstract class X11InputMethod extends X11InputMethodBase {
+
+    // to keep the instance of activating if IM resumed
+    static protected X11InputMethod activatedInstance = null;
 
     /**
      * Constructs an X11InputMethod instance. It initializes the XIM
@@ -55,7 +59,7 @@ public abstract class X11InputMethod extends X11InputMethodBase {
      * Reset the composition state to the current composition state.
      */
     protected void resetCompositionState() {
-        if (compositionEnableSupported) {
+        if (compositionEnableSupported && haveActiveClient()) {
             try {
                 /* Restore the composition mode to the last saved composition
                    mode. */
@@ -70,6 +74,7 @@ public abstract class X11InputMethod extends X11InputMethodBase {
      * Activate input method.
      */
     public synchronized void activate() {
+        activatedInstance = this;
         clientComponentWindow = getClientComponentWindow();
         if (clientComponentWindow == null)
             return;
@@ -79,6 +84,13 @@ public abstract class X11InputMethod extends X11InputMethodBase {
                 log.fine("XICFocused {0}, AWTFocused {1}",
                          lastXICFocussedComponent, awtFocussedComponent);
             }
+            if (lastXICFocussedComponent != awtFocussedComponent) {
+                ComponentPeer lastXICFocussedComponentPeer = getPeer(lastXICFocussedComponent);
+                if (lastXICFocussedComponentPeer != null) {
+                    setXICFocus(lastXICFocussedComponentPeer, false, isLastXICActive);
+                }
+            }
+            lastXICFocussedComponent = null;
         }
 
         if (pData == 0) {
@@ -91,30 +103,18 @@ public abstract class X11InputMethod extends X11InputMethodBase {
         /*  reset input context if necessary and set the XIC focus
         */
         resetXICifneeded();
-        ComponentPeer lastXICFocussedComponentPeer = null;
         ComponentPeer awtFocussedComponentPeer = getPeer(awtFocussedComponent);
+        setStatusAreaVisible(true, pData);
 
-        if (lastXICFocussedComponent != null) {
-           lastXICFocussedComponentPeer = getPeer(lastXICFocussedComponent);
+        if (awtFocussedComponentPeer != null) {
+            setXICFocus(awtFocussedComponentPeer, true, haveActiveClient());
         }
-
-        /* If the last XIC focussed component has a different peer as the
-           current focussed component, change the XIC focus to the newly
-           focussed component.
-        */
-        if (isLastTemporary || lastXICFocussedComponentPeer != awtFocussedComponentPeer ||
-            isLastXICActive != haveActiveClient()) {
-            if (lastXICFocussedComponentPeer != null) {
-                setXICFocus(lastXICFocussedComponentPeer, false, isLastXICActive);
-            }
-            if (awtFocussedComponentPeer != null) {
-                setXICFocus(awtFocussedComponentPeer, true, haveActiveClient());
-            }
-            lastXICFocussedComponent = awtFocussedComponent;
-            isLastXICActive = haveActiveClient();
-        }
-        resetCompositionState();
+        lastXICFocussedComponent = awtFocussedComponent;
+        isLastXICActive = haveActiveClient();
         isActive = true;
+        if (savedCompositionState) {
+            resetCompositionState();
+        }
     }
 
     /**
@@ -140,25 +140,38 @@ public abstract class X11InputMethod extends X11InputMethodBase {
            Last note, getCompositionState should be called before setXICFocus since
            setXICFocus here sets the XIC to 0.
         */
+        activatedInstance = null;
         savedCompositionState = getCompositionState();
 
         if (isTemporary) {
             //turn the status window off...
             turnoffStatusWindow();
+            /* Delay resetting the XIC focus until activate is called and the newly
+             * Focused component has a different peer as the last focused component.
+             */
+            lastXICFocussedComponent = awtFocussedComponent;
+        } else {
+            if (awtFocussedComponent != null ) {
+                ComponentPeer awtFocussedComponentPeer = getPeer(awtFocussedComponent);
+                if (awtFocussedComponentPeer != null) {
+                    setXICFocus(awtFocussedComponentPeer, false, isAc);
+                }
+            }
+            lastXICFocussedComponent = null;
         }
 
-        /* Delay resetting the XIC focus until activate is called and the newly
-         * Focused component has a different peer as the last focused component.
-         */
-        lastXICFocussedComponent = awtFocussedComponent;
         isLastXICActive = isAc;
         isLastTemporary = isTemporary;
         isActive = false;
+        setStatusAreaVisible(false, pData);
     }
 
     // implements java.awt.im.spi.InputMethod.hideWindows
     public void hideWindows() {
-        // ??? need real implementation
+        if (pData != 0) {
+            setStatusAreaVisible(false, pData);
+            turnoffStatusWindow();
+        }
     }
 
     /**
@@ -194,6 +207,41 @@ public abstract class X11InputMethod extends X11InputMethodBase {
             && committedText == null)
             return;
 
+        // Recalculate chgOffset and chgLength for supplementary char
+        if (composedText != null) {
+            int tmpChgOffset=chgOffset;
+            int tmpChgLength=chgLength;
+            int index = 0;
+            for (int i=0;i < tmpChgOffset; i++,index++){
+                if (index < composedText.length()
+                    && Character.charCount(composedText.codePointAt(index))==2){
+                    index++;
+                    chgOffset++;
+                }
+            }
+            // The index keeps value
+            for (int i=0;i < tmpChgLength; i++,index++){
+                if (index < composedText.length()
+                    && Character.charCount(composedText.codePointAt(index))==2){
+                    index++;
+                    chgLength++;
+                }
+            }
+        }
+
+        // Replace control character with a square box
+        if (chgText != null) {
+            StringBuffer newChgText = new StringBuffer();
+            for (int i=0; i < chgText.length(); i++){
+                char c = chgText.charAt(i);
+                if (Character.isISOControl(c)){
+                    c = '\u25A1';
+                }
+                newChgText.append(c);
+            }
+            chgText = new String(newChgText);
+        }
+
         if (composedText == null) {
             // TODO: avoid reallocation of those buffers
             composedText = new StringBuffer(INITIAL_SIZE);
@@ -228,8 +276,47 @@ public abstract class X11InputMethod extends X11InputMethodBase {
         }
         if (chgText != null) {
             composedText.insert(chgOffset, chgText);
-            if (chgStyles != null)
+            if (chgStyles != null) {
+                // Recalculate chgStyles for supplementary char
+                if (chgText.length() > chgStyles.length){
+                    int index=0;
+                    int[] newStyles = new int[chgText.length()];
+                    for (int i=0; i < chgStyles.length; i++, index++){
+                        newStyles[index]=chgStyles[i];
+                        if (index < chgText.length()
+                            && Character.charCount(chgText.codePointAt(index))==2){
+                            newStyles[++index]=chgStyles[i];
+                        }
+                    }
+                    chgStyles=newStyles;
+                }
                 rawFeedbacks.insert(chgOffset, chgStyles);
+            }
+
+        }
+
+        else if (chgStyles != null) {
+            // Recalculate chgStyles to support supplementary char
+            int count=0;
+            for (int i=0; i < chgStyles.length; i++){
+                if (composedText.length() > chgOffset+i+count
+                    && Character.charCount(composedText.codePointAt(chgOffset+i+count))==2){
+                    count++;
+                }
+            }
+            if (count>0){
+                int index=0;
+                int[] newStyles = new int[chgStyles.length+count];
+                for (int i=0; i < chgStyles.length; i++, index++){
+                    newStyles[index]=chgStyles[i];
+                    if (composedText.length() > chgOffset+index
+                        && Character.charCount(composedText.codePointAt(chgOffset+index))==2){
+                        newStyles[++index]=chgStyles[i];
+                    }
+                }
+                chgStyles=newStyles;
+            }
+            rawFeedbacks.replace(chgOffset, chgStyles);
         }
 
         if (composedText.length() == 0) {
@@ -255,6 +342,15 @@ public abstract class X11InputMethod extends X11InputMethodBase {
                                  when);
 
             return;
+        }
+
+        // Adjust caretPosition for supplementary char
+        for (int i=0; i< caretPosition; i++){
+            if (i < composedText.length()
+                && Character.charCount(composedText.codePointAt(i))==2){
+                caretPosition++;
+                i++;
+            }
         }
 
         // Now sending the composed text to the client
@@ -322,6 +418,25 @@ public abstract class X11InputMethod extends X11InputMethodBase {
                              when);
     }
 
+    /* Some IMs need forced Text clear */
+    void clearComposedText(long when) {
+        composedText = null;
+        postInputMethodEvent(InputMethodEvent.INPUT_METHOD_TEXT_CHANGED,
+                             null, 0, null, null,
+                             when);
+        if (committedText != null && committedText.length() > 0) {
+            dispatchCommittedText(committedText, when);
+        }
+        committedText = null;
+        rawFeedbacks = null;
+    }
+
+    void clearComposedText() {
+        if (EventQueue.isDispatchThread()) {
+            clearComposedText(EventQueue.getMostRecentEventTime());
+        }
+    }
+
     /*
      * Subclasses should override disposeImpl() instead of dispose(). Client
      * code should always invoke dispose(), never disposeImpl().
@@ -329,12 +444,17 @@ public abstract class X11InputMethod extends X11InputMethodBase {
     protected synchronized void disposeImpl() {
         disposeXIC();
         awtLock();
-        composedText = null;
-        committedText = null;
-        rawFeedbacks = null;
-        awtUnlock();
+        try {
+            clearComposedText();
+        } finally {
+            // Put awtUnlock into finally block in case an exception is thrown in clearComposedText.
+            awtUnlock();
+        }
         awtFocussedComponent = null;
         lastXICFocussedComponent = null;
+        needResetXIC = false;
+        savedCompositionState = false;
+        compositionEnableSupported = true;
     }
 
     /**
@@ -349,8 +469,22 @@ public abstract class X11InputMethod extends X11InputMethodBase {
            Use package private mthod 'resetCompositionState' if you want the
            exception to be caught.
         */
+        boolean pre, post;
+        pre=getCompositionState();
+
         if (setCompositionEnabledNative(enable)) {
             savedCompositionState = enable;
         }
+
+        post=getCompositionState();
+        if (pre != post && post == enable){
+            if (enable == false) flushText();
+            if (awtFocussedComponent != null && isActive){
+                setXICFocus(getPeer(awtFocussedComponent),
+                            true, haveActiveClient());
+            }
+        }
     }
+
+    private native void setStatusAreaVisible(boolean value, long data);
 }
