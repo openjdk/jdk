aae426f3e71e7a925996a9630d51fcd8729cbc67
Sergey Bylokhov <serb@openjdk.java.net>
2018-06-06 15:37:10 -0700

    8203224: java.awt.desktop.*Event classes could not be instantiated if Desktop feature is not supported
    Reviewed-by: prr, kaddepalli

--- src/java.desktop/share/classes/java/awt/desktop/AppEvent.java
+++ src/java.desktop/share/classes/java/awt/desktop/AppEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,11 +26,13 @@
 package java.awt.desktop;
 
 import java.awt.Desktop;
+import java.awt.GraphicsEnvironment;
+import java.awt.HeadlessException;
 import java.util.EventObject;
 
 /**
  * AppEvents are sent to listeners and handlers installed on the
- * {@link java.awt.Desktop}.
+ * {@link java.awt.Desktop} instance of the current desktop context.
  *
  * @since 9
  */
@@ -38,8 +40,17 @@ public class AppEvent extends EventObject {
 
     private static final long serialVersionUID = -5958503993556009432L;
 
+    /**
+     * Constructs an {@code AppEvent}.
+     *
+     * @throws HeadlessException if {@link GraphicsEnvironment#isHeadless()}
+     *         returns {@code true}
+     * @throws UnsupportedOperationException if Desktop API is not supported on
+     *         the current platform
+     * @see Desktop#isDesktopSupported()
+     * @see java.awt.GraphicsEnvironment#isHeadless
+     */
     AppEvent() {
         super(Desktop.getDesktop());
     }
-
 }
