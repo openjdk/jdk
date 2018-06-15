2f16dd96dba5f8d0eed48b366056402fbc5f6ff8
Per Lidén <pliden@openjdk.org>
2018-06-15 13:31:27 +0200

    8205022: ZGC: SoftReferences not always cleared before throwing OOME
    Reviewed-by: stefank, eosterlund

--- src/hotspot/share/gc/z/zHeap.hpp
+++ src/hotspot/share/gc/z/zHeap.hpp
@@ -124,6 +124,7 @@ public:
   uintptr_t alloc_object(size_t size);
   uintptr_t alloc_object_for_relocation(size_t size);
   void undo_alloc_object_for_relocation(uintptr_t addr, size_t size);
+  bool is_alloc_stalled() const;
   void check_out_of_memory();
 
   // Marking
