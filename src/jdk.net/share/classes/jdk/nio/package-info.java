d7719cc590f9eecf709d387be837abf4bdc07e1c
Alan Bateman <alanb@openjdk.java.net>
2018-06-23 09:34:09 +0100

    8198372: API to create a SelectableChannel to a FileDescriptor
    Reviewed-by: chegar, bpb

--- src/jdk.net/share/classes/jdk/nio/package-info.java
+++ src/jdk.net/share/classes/jdk/nio/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,13 +24,9 @@
  */
 
 /**
- * Defines the JDK-specific Networking API.
+ * Defines JDK-specific {@link java.nio.channels.Channel channel} APIs.
  *
- * @moduleGraph
- * @since 9
+ * @since 11
  */
-module jdk.net {
-    exports jdk.net;
-    exports jdk.nio;
-}
 
+package jdk.nio;
