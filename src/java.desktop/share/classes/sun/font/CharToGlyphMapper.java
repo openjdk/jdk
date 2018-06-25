d6c849a82457e342f8516818d45b7be2341e3b85
Toshio Nakamura <toshiona@jp.ibm.com>
2018-06-25 11:40:46 -0700

    8187100: Support Unicode Variation Selectors
    Support Unicode Variation Selectors
    
    Reviewed-by: prr, srl

--- src/java.desktop/share/classes/sun/font/CharToGlyphMapper.java
+++ src/java.desktop/share/classes/sun/font/CharToGlyphMapper.java
@@ -36,6 +36,10 @@ public abstract class CharToGlyphMapper {
     public static final int HI_SURROGATE_END = 0xDBFF;
     public static final int LO_SURROGATE_START = 0xDC00;
     public static final int LO_SURROGATE_END = 0xDFFF;
+    public static final int VS_START = 0xFE00;
+    public static final int VS_END = 0xFE0F;
+    public static final int VSS_START = 0xE0100;
+    public static final int VSS_END = 0xE01FF;
 
     public static final int UNINITIALIZED_GLYPH = -1;
     public static final int INVISIBLE_GLYPH_ID = 0xffff;
@@ -77,6 +81,11 @@ public abstract class CharToGlyphMapper {
         return glyphs[0];
     }
 
+    public int charToVariationGlyph(int unicode, int variationSelector) {
+        // Override this if variation selector is supported.
+        return charToGlyph(unicode);
+    }
+
     public abstract int getNumGlyphs();
 
     public abstract void charsToGlyphs(int count,
@@ -87,5 +96,10 @@ public abstract class CharToGlyphMapper {
 
     public abstract void charsToGlyphs(int count,
                                        int[] unicodes, int[] glyphs);
+
+    public static boolean isVariationSelector(int charCode) {
+        return ((charCode >= VSS_START && charCode <= VSS_END) ||
+                (charCode >= VS_START && charCode <= VS_END));
+    }
 
 }
