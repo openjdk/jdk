a0e9bf42802d2ce87a8d68921db41f4c0c81bffa
Paul Sandoz <psandoz@openjdk.org>
2018-07-02 10:09:01 -0700

    8202769: jck test fails with C2: vm/jvmti/FollowReferences/fref001/fref00113/fref00113.html
    Reviewed-by: kvn, coleenp

--- src/hotspot/share/ci/ciStreams.cpp
+++ src/hotspot/share/ci/ciStreams.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -255,8 +255,7 @@ ciConstant ciBytecodeStream::get_constant() {
 // constant.
 constantTag ciBytecodeStream::get_constant_pool_tag(int index) const {
   VM_ENTRY_MARK;
-  BasicType bt = _method->get_Method()->constants()->basic_type_for_constant_at(index);
-  return constantTag::ofBasicType(bt);
+  return _method->get_Method()->constants()->constant_tag_at(index);
 }
 
 // ------------------------------------------------------------------
