4dedba9ebe11750f4b39c41feb4a4314ccdb3a08
John Jiang <jjiang@openjdk.org>
2018-07-03 09:27:41 +0800

    8205984: javax/net/ssl/compatibility/Compatibility.java failed to access port log file
    Release resource after reading port log file
    
    Reviewed-by: xuelei

--- test/jdk/javax/net/ssl/compatibility/Server.java
+++ test/jdk/javax/net/ssl/compatibility/Server.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -146,10 +146,9 @@ public class Server {
                 server.close();
             }
 
-            // Cleanups port.log.
-            File file = new File(Utils.PORT_LOG);
-            if (file.exists()) {
-                file.delete();
+            // Cleanups port log.
+            if (!new File(Utils.PORT_LOG).delete()) {
+                throw new RuntimeException("Cannot delete port log");
             }
         }
 
