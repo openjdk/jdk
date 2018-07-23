39cd4bdd52bf239ad5eabdfec996222aff61f41a
Jean Christophe Beyler <jcbeyler@google.com>
2018-07-22 20:00:39 -0700

    8207252: C1 still does eden allocations when TLAB is enabled
    Only do eden allocations when TLAB is disabled
    
    Reviewed-by: kbarrett, jrose, tschatzl, iveresov

--- src/hotspot/cpu/sparc/c1_Runtime1_sparc.cpp
+++ src/hotspot/cpu/sparc/c1_Runtime1_sparc.cpp
@@ -407,8 +407,11 @@ OopMapSet* Runtime1::generate_code_for(StubID id, StubAssembler* sasm) {
           __ set_info("fast new_instance init check", dont_gc_arguments);
         }
 
+        // If TLAB is disabled, see if there is support for inlining contiguous
+        // allocations.
+        // Otherwise, just go to the slow path.
         if ((id == fast_new_instance_id || id == fast_new_instance_init_check_id) &&
-            UseTLAB && Universe::heap()->supports_inline_contig_alloc()) {
+            !UseTLAB && Universe::heap()->supports_inline_contig_alloc()) {
           Label slow_path;
           Register G1_obj_size = G1;
           Register G3_t1 = G3;
