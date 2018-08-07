896daa1bbab22807cce6546197a23b8f16684b22
Sean Mullan <mullan@openjdk.java.net>
2018-08-07 10:29:01 -0400

    8208691: Tighten up jdk.includeInExceptions security property
    Add stronger warning on usage and add test to check that property is not set
    
    Reviewed-by: alanb, rriggs

--- test/jdk/java/net/Socket/ExceptionText.java
+++ test/jdk/java/net/Socket/ExceptionText.java
@@ -25,8 +25,10 @@
  * @test
  * @library /test/lib
  * @build jdk.test.lib.Utils
- * @bug 8204233 8207846
+ * @bug 8204233 8207846 8208691
  * @summary Add configurable option for enhanced socket IOException messages
+ * @run main/othervm
+ *       ExceptionText
  * @run main/othervm
  *       ExceptionText
  *       WITHOUT_Enhanced_Text
@@ -62,6 +64,7 @@ import java.net.Socket;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.ClosedChannelException;
 import java.nio.channels.SocketChannel;
+import java.security.Security;
 import java.util.concurrent.ExecutionException;
 import jdk.test.lib.Utils;
 
@@ -70,19 +73,32 @@ public class ExceptionText {
     enum TestTarget {SOCKET, CHANNEL, ASYNC_CHANNEL};
 
     public static void main(String args[]) throws Exception {
-        String passOrFail = args[0];
-        boolean expectEnhancedText;
-        if (passOrFail.equals("expectEnhancedText")) {
-            expectEnhancedText = true;
+        if (args.length == 0) {
+            testSecProp();
         } else {
-            expectEnhancedText = false;
+            String passOrFail = args[0];
+            boolean expectEnhancedText;
+            if (passOrFail.equals("expectEnhancedText")) {
+                expectEnhancedText = true;
+            } else {
+                expectEnhancedText = false;
+            }
+            test(expectEnhancedText);
         }
-        test(expectEnhancedText);
     }
 
     static final InetSocketAddress dest  = Utils.refusingEndpoint();
     static final String PORT = ":" + Integer.toString(dest.getPort());
     static final String HOST = dest.getHostString();
+
+    static void testSecProp() {
+        String incInExc = Security.getProperty("jdk.includeInExceptions");
+        if (incInExc != null) {
+            throw new RuntimeException("Test failed: default value of " +
+                "jdk.includeInExceptions security property is not null: " +
+                incInExc);
+        }
+    }
 
     static void test(boolean withProperty) {
         // Socket
