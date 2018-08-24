347b95d5007ca0711d35ba8d04d9b26c77f1ac81
Dean Long <dlong@openjdk.org>
2018-08-24 11:56:14 -0700

    8209825: guarantee(false) failed: wrong number of expression stack elements during deopt
    Reviewed-by: kvn, thartmann

--- test/hotspot/jtreg/compiler/interpreter/TestVerifyStackAfterDeopt.java
+++ test/hotspot/jtreg/compiler/interpreter/TestVerifyStackAfterDeopt.java
@@ -25,8 +25,10 @@
 /*
  * @test TestVerifyStackAfterDeopt
  * @bug 8148871
+ * @bug 8209825
  * @summary Checks VerifyStack after deoptimization of array allocation slow call
  * @run main/othervm -XX:+IgnoreUnrecognizedVMOptions -XX:TieredStopAtLevel=1
+ *                   -XX:MinTLABSize=1k -XX:TLABSize=1k
  *                   -XX:+DeoptimizeALot -XX:+VerifyStack
  *                   compiler.interpreter.TestVerifyStackAfterDeopt
  */
@@ -35,9 +37,11 @@ package compiler.interpreter;
 
 public class TestVerifyStackAfterDeopt {
 
-    private void method(Object[] a) {
-
+    private long method(long l1, long l2, Object[] a) {
+        return l1 + l2;
     }
+
+    private long result[] = new long[1];
 
     private void test() {
         // For the array allocation, C1 emits a slow call into the runtime
@@ -45,7 +49,7 @@ public class TestVerifyStackAfterDeopt {
         // The VerifyStack code then gets confused because the following
         // bytecode instruction is an invoke and the interpreter oop map
         // generator reports the oop map after execution of that invoke.
-        method(new Object[0]);
+        this.result[0] = method(1L, 2L, new Object[0]);
     }
 
     public static void main(String[] args) {
