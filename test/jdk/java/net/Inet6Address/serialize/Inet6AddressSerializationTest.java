334d64e9a02d48809fefb10e1d57deba5edabf07
Chris Yin <xyin@openjdk.org>
2018-09-07 09:09:31 +0800

    8042902: Test java/net/Inet6Address/serialize/Inet6AddressSerializationTest.java fails intermittently
    Reviewed-by: chegar

--- test/jdk/java/net/Inet6Address/serialize/Inet6AddressSerializationTest.java
+++ test/jdk/java/net/Inet6Address/serialize/Inet6AddressSerializationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -192,6 +192,7 @@ public class Inet6AddressSerializationTest {
                     System.err.println("Testing with " + iadr);
                     System.err.println(" scoped iface: "
                             + i6adr.getScopedInterface());
+                    System.err.println(" hostname: " + i6adr.getHostName());
                     testInet6AddressSerialization(i6adr, null);
                 }
             }
