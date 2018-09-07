23ec584fda723c948c51b65c13b875ee62710719
Xueming Shen <sherman@openjdk.org>
2018-09-07 10:17:49 -0700

    8210345: The Japanese message of FileNotFoundException garbled
    Reviewed-by: alanb

--- src/java.base/share/native/libjava/io_util.c
+++ src/java.base/share/native/libjava/io_util.c
@@ -211,11 +211,7 @@ throwFileNotFoundException(JNIEnv *env, jstring path)
 
     n = getLastErrorString(buf, sizeof(buf));
     if (n > 0) {
-#ifdef WIN32
-        why = (*env)->NewStringUTF(env, buf);
-#else
         why = JNU_NewStringPlatform(env, buf);
-#endif
         CHECK_NULL(why);
     }
     x = JNU_NewObjectByName(env,
