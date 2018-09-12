2065ebd890454f646ffd5f04fe8201411bd8e093
Jim Laskey <jlaskey@openjdk.org>
2018-09-12 14:19:36 -0300

    8206981: Compiler support for Raw String Literals
    Reviewed-by: mcimadamore, briangoetz, abuckley, jjg, vromero, jlahoda

--- src/jdk.compiler/share/classes/com/sun/tools/javac/parser/UnicodeReader.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/parser/UnicodeReader.java
@@ -64,6 +64,10 @@ public class UnicodeReader {
      */
     protected int unicodeConversionBp = -1;
 
+    /** Control conversion of unicode characters
+     */
+    protected boolean unicodeConversion = true;
+
     protected Log log;
     protected Names names;
 
@@ -154,11 +158,17 @@ public class UnicodeReader {
         return new String(sbuf, 0, sp);
     }
 
+    protected boolean setUnicodeConversion(boolean newState) {
+        boolean oldState = unicodeConversion;
+        unicodeConversion = newState;
+        return oldState;
+    }
+
     /** Convert unicode escape; bp points to initial '\' character
      *  (Spec 3.3).
      */
     protected void convertUnicode() {
-        if (ch == '\\' && unicodeConversionBp != bp) {
+        if (ch == '\\' && unicodeConversion && unicodeConversionBp != bp ) {
             bp++; ch = buf[bp];
             if (ch == 'u') {
                 do {
@@ -252,6 +262,24 @@ public class UnicodeReader {
 
     protected char peekChar() {
         return buf[bp + 1];
+    }
+
+    protected char peekBack() {
+        return buf[bp];
+    }
+
+    /**
+     * Skips consecutive occurrences of the current character, leaving bp positioned
+     * at the last occurrence. Returns the occurrence count.
+     */
+    protected int skipRepeats() {
+        int start = bp;
+        while (bp < buflen) {
+            if (buf[bp] != buf[bp + 1])
+                break;
+            bp++;
+        }
+        return bp - start;
     }
 
     /**
