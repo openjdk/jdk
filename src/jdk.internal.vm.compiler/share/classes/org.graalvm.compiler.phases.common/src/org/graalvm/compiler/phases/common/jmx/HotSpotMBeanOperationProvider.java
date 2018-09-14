acb7acc64ffdc6a6b61774f1b1f7d3f9a7a4e1fb
Igor Veresov <iveresov@openjdk.org>
2018-09-13 22:45:12 -0700

    8210478: Update Graal
    Reviewed-by: kvn

--- src/jdk.internal.vm.compiler/share/classes/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/jmx/HotSpotMBeanOperationProvider.java
+++ src/jdk.internal.vm.compiler/share/classes/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/jmx/HotSpotMBeanOperationProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,26 +22,12 @@
  */
 
 
-package org.graalvm.compiler.hotspot.stubs;
+package org.graalvm.compiler.phases.common.jmx;
 
-import org.graalvm.compiler.options.Option;
-import org.graalvm.compiler.options.OptionKey;
-import org.graalvm.compiler.options.OptionType;
+import java.util.Collection;
 
-//JaCoCo Exclude
+public interface HotSpotMBeanOperationProvider {
+    <T> void registerOperations(Class<T> clazz, Collection<? super T> ops);
 
-/**
- * Options related to HotSpot Graal-generated stubs.
- *
- * Note: This must be a top level class to work around for
- * <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=477597">Eclipse bug 477597</a>.
- */
-public class StubOptions {
-    // @formatter:off
-    @Option(help = "Trace execution of stub used to handle an exception thrown by a callee.", type = OptionType.Debug)
-    static final OptionKey<Boolean> TraceExceptionHandlerStub = new OptionKey<>(false);
-
-    @Option(help = "Trace execution of the stub that routes an exception to a handler in the calling frame.", type = OptionType.Debug)
-    static final OptionKey<Boolean> TraceUnwindStub = new OptionKey<>(false);
-    //@formatter:on
+    Object invoke(String actionName, Object[] params, String[] signature);
 }
