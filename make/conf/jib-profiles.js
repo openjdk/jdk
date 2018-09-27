9d846aae1fa23d5d946e91685b4dc55e8b2a5fe4
Erik Joelsson <erikj@openjdk.org>
2018-09-27 08:49:12 -0700

    8211130: Change to Oracle Developer Studio 12.6 for building on Solaris at Oracle
    Reviewed-by: tbell, prr, ihse

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -840,7 +840,7 @@ var getJibProfilesDependencies = function (input, common) {
         linux_x64: "gcc7.3.0-OEL6.4+1.0",
         macosx_x64: "Xcode9.4-MacOSX10.13+1.0",
         solaris_x64: "SS12u4-Solaris11u1+1.0",
-        solaris_sparcv9: "SS12u4-Solaris11u1+1.1",
+        solaris_sparcv9: "SS12u6-Solaris11u3+1.0",
         windows_x64: "VS2017-15.5.5+1.0",
         linux_aarch64: (input.profile != null && input.profile.indexOf("arm64") >= 0
                     ? "gcc-linaro-aarch64-linux-gnu-4.8-2013.11_linux+1.0"
