51be7db96f3fc32a7ddb24f8af19fb4fc0577aaf
Kim Barrett <kbarrett@openjdk.org>
2018-10-08 20:01:39 -0400

    8211804: Constant AO_UNUSED_MBZ uses left shift of negative value
    Use unsigned shift.
    
    Reviewed-by: alanb

--- src/jdk.pack/share/native/common-unpack/constants.h
+++ src/jdk.pack/share/native/common-unpack/constants.h
@@ -203,7 +203,7 @@ enum {
     AO_HAVE_FIELD_FLAGS_HI    = 1<<10,
     AO_HAVE_METHOD_FLAGS_HI   = 1<<11,
     AO_HAVE_CODE_FLAGS_HI     = 1<<12,
-    AO_UNUSED_MBZ             = (-1)<<13, // options bits reserved for future use.
+    AO_UNUSED_MBZ             = (int)((~0U)<<13), // options bits reserved for future use.
 
 #define ARCHIVE_BIT_DO(F) \
          F(AO_HAVE_SPECIAL_FORMATS) \
