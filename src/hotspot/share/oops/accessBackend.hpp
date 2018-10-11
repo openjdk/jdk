7ea2bd922bbf1acb8ee9376ca2d12f8982dbc08d
Roman Kennke <rkennke@openjdk.org>
2018-10-11 23:48:55 +0200

    8212054: Boilerplate to bind oopDesc::equals_raw() to actual raw implementation
    Reviewed-by: shade, eosterlund

--- src/hotspot/share/oops/accessBackend.hpp
+++ src/hotspot/share/oops/accessBackend.hpp
@@ -998,7 +998,7 @@ namespace AccessInternal {
 
     template <DecoratorSet decorators>
     inline static typename EnableIf<
-      HasDecorator<decorators, INTERNAL_BT_TO_SPACE_INVARIANT>::value, bool>::type
+      HasDecorator<decorators, AS_RAW>::value || HasDecorator<decorators, INTERNAL_BT_TO_SPACE_INVARIANT>::value, bool>::type
     equals(oop o1, oop o2) {
       typedef RawAccessBarrier<decorators & RAW_DECORATOR_MASK> Raw;
       return Raw::equals(o1, o2);
@@ -1006,7 +1006,7 @@ namespace AccessInternal {
 
     template <DecoratorSet decorators>
     inline static typename EnableIf<
-      !HasDecorator<decorators, INTERNAL_BT_TO_SPACE_INVARIANT>::value, bool>::type
+      !HasDecorator<decorators, AS_RAW>::value && !HasDecorator<decorators, INTERNAL_BT_TO_SPACE_INVARIANT>::value, bool>::type
     equals(oop o1, oop o2) {
       return RuntimeDispatch<decorators, oop, BARRIER_EQUALS>::equals(o1, o2);
     }
