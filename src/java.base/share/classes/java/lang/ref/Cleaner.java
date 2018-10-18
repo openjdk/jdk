d54fa9e93d786eb264d431621f7065cf4a4970e5
Andrew Luo <andrewluotechnologies@outlook.com>
2018-10-18 10:45:05 -0400

    8212662: Fix javadoc typo in java.lang.ref.Cleaner
    Reviewed-by: lancea, rriggs

--- src/java.base/share/classes/java/lang/ref/Cleaner.java
+++ src/java.base/share/classes/java/lang/ref/Cleaner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -100,8 +100,8 @@ import java.util.function.Function;
  *            }
  *        }
  *
- *        private final State;
- *        private final Cleaner.Cleanable cleanable
+ *        private final State state;
+ *        private final Cleaner.Cleanable cleanable;
  *
  *        public CleaningExample() {
  *            this.state = new State(...);
