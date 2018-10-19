3ee3988bfd723bc83ed16bfa09f8dc2143accec4
Ichiroh Takiguchi <itakiguchi@openjdk.org>
2018-10-19 15:26:50 +0100

    8211393: Memory leak issue on awt_InputMethod.c
    Reviewed-by: naoto, clanger

--- src/java.desktop/aix/native/libawt_xawt/awt/awt_InputMethod.c
+++ src/java.desktop/aix/native/libawt_xawt/awt/awt_InputMethod.c
@@ -1148,6 +1148,7 @@ PreeditDrawCallback(XIC ic, XPointer client_data,
                         tmpstyle[cnt] = text->feedback[cnt];
                 (*env)->SetIntArrayRegion(env, style, 0,
                                           text->length, (jint *)tmpstyle);
+                free(tmpstyle);
             }
         }
     }
