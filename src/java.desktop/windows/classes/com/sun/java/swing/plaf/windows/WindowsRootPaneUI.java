b64e7fc2bd8cd175ce0e969bcf7de68546233fad
Prasanta Sadhukhan <psadhukhan@openjdk.org>
2018-10-19 12:50:25 +0530

    8211987: Menu bar gets input focus even if Alt-released event is consumed
    Reviewed-by: serb

--- src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsRootPaneUI.java
+++ src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsRootPaneUI.java
@@ -165,11 +165,13 @@ public class WindowsRootPaneUI extends BasicRootPaneUI {
         }
 
         public boolean postProcessKeyEvent(KeyEvent ev) {
-            if(ev.isConsumed() && ev.getKeyCode() != KeyEvent.VK_ALT) {
-                // mnemonic combination, it's consumed, but we need
-                // set altKeyPressed to false, otherwise after selection
-                // component by mnemonic combination a menu will be open
-                altKeyPressed = false;
+            if (ev.isConsumed()) {
+                if (ev.getKeyCode() != KeyEvent.VK_ALT) {
+                    // mnemonic combination, it's consumed, but we need
+                    // set altKeyPressed to false, otherwise after selection
+                    // component by mnemonic combination a menu will be open
+                    altKeyPressed = false;
+                }
                 return false;
             }
             if (ev.getKeyCode() == KeyEvent.VK_ALT) {
