6a9e3b9d296acfcc4340a8604e739d3ec74b6eaf
Daniel D. Daugherty <dcubed@openjdk.org>
2018-11-08 20:54:30 -0500

    8204529: gc/TestAllocateHeapAtMultiple.java fail with Agent 7 timed out
    Increase default timeout to 360 seconds.
    
    Reviewed-by: erikj, dholmes

--- test/hotspot/jtreg/gc/TestAllocateHeapAtMultiple.java
+++ test/hotspot/jtreg/gc/TestAllocateHeapAtMultiple.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,7 @@
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
  * @requires vm.bits == "64" & vm.gc != "Z" & os.family != "aix"
+ * @run main/timeout=360 TestAllocateHeapAtMultiple
  */
 
 import jdk.test.lib.JDKToolFinder;
