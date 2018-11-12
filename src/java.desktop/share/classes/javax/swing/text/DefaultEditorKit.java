8a5f5e0f34f8260bc8a882347f9a3624823c3043
Anton Litvinov <alitvinov@openjdk.org>
2018-11-12 22:28:08 +0000

    8187364: Unable to enter zero width non-joiner (ZWNJ) symbol in Swing text component
    Reviewed-by: serb, dmarkov

--- src/java.desktop/share/classes/javax/swing/text/DefaultEditorKit.java
+++ src/java.desktop/share/classes/javax/swing/text/DefaultEditorKit.java
@@ -878,11 +878,10 @@ public class DefaultEditorKit extends EditorKit {
                         isPrintableMask = ((SunToolkit)tk).isPrintableCharacterModifiersMask(mod);
                     }
 
-                    if (isPrintableMask) {
-                        char c = content.charAt(0);
-                        if ((c >= 0x20) && (c != 0x7F)) {
-                            target.replaceSelection(content);
-                        }
+                    char c = content.charAt(0);
+                    if ((isPrintableMask && (c >= 0x20) && (c != 0x7F)) ||
+                        (!isPrintableMask && (c >= 0x200C) && (c <= 0x200D))) {
+                        target.replaceSelection(content);
                     }
                 }
             }
