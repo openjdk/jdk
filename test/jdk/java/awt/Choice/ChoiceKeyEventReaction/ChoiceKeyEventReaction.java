1b012e213d31b184236a56121f7f1548d1a6fe05
Shashidhara Veerabhadraiah <sveerabhadra@openjdk.org>
2018-11-15 11:25:13 +0530

    6849922: java/awt/Choice/ChoiceKeyEventReaction/ChoiceKeyEventReaction.html fails
    Reviewed-by: kaddepalli, serb

--- test/jdk/java/awt/Choice/ChoiceKeyEventReaction/ChoiceKeyEventReaction.java
+++ test/jdk/java/awt/Choice/ChoiceKeyEventReaction/ChoiceKeyEventReaction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2006, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2018, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,121 +22,126 @@
  */
 
 /*
-  test
-  @bug 6252982
-  @summary PIT: Keyboard FocusTraversal not working when choice's drop-down is visible, on XToolkit
-  @author andrei.dmitriev : area=awt.choice
-  @run applet ChoiceKeyEventReaction.html
-*/
+ * @test
+ * @bug 6252982
+ * @key headful
+ * @summary PIT: Keyboard FocusTraversal not working when choice's drop-down is visible, on XToolkit
+ * @author andrei.dmitriev : area=awt.choice
+ * @run main ChoiceKeyEventReaction
+ */
 
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
-import test.java.awt.regtesthelpers.Util;
+import java.awt.Robot;
+import java.awt.Choice;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.TextField;
+import java.awt.FlowLayout;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.ItemListener;
+import java.awt.Frame;
 
-public class ChoiceKeyEventReaction extends Applet
+public class ChoiceKeyEventReaction
 {
-    Robot robot;
-    Choice choice1 = new Choice();
-    Point pt;
-    TextField tf = new TextField("Hi");
+    private static Robot robot;
+    private static Choice choice1 = new Choice();
+    private static Point pt;
+    private static TextField tf = new TextField("Hi");
+    private static boolean keyTypedOnTextField = false;
+    private static boolean itemChanged = false;
+    private static Frame frame;
+    private static String toolkit;
 
-    boolean keyTypedOnTextField = false;
-    boolean itemChanged = false;
-    String toolkit;
+    public static void main(String[] args) {
+        createAndShowGUI();
 
-    public void init()
-    {
+        try {
+            robot = new Robot();
+            robot.setAutoDelay(100);
+
+            moveFocusToTextField();
+            testKeyOnChoice(InputEvent.BUTTON1_MASK, KeyEvent.VK_UP);
+        } catch (Exception e) {
+            throw new RuntimeException("Test failed. Exception thrown: "+e);
+        } finally {
+            if (frame != null) {
+                frame.dispose();
+            }
+        }
+    }
+
+    private static void createAndShowGUI() {
+        frame = new Frame();
         toolkit = Toolkit.getDefaultToolkit().getClass().getName();
         System.out.println("Current toolkit is :" +toolkit);
         for (int i = 1; i<20; i++){
             choice1.add("item-0"+i);
         }
-        tf.addKeyListener(new KeyAdapter(){
-                public void keyPressed(KeyEvent ke) {
-                    keyTypedOnTextField = true;
-                    System.out.println(ke);
-                }
-            });
 
+        tf.addKeyListener(new KeyAdapter(){
+            public void keyPressed(KeyEvent ke) {
+                keyTypedOnTextField = true;
+                System.out.println(ke);
+            }
+        });
 
         choice1.addItemListener(new ItemListener() {
-                public void itemStateChanged(ItemEvent e) {
-                    itemChanged = true;
-                    System.out.println(e);
-                }
-            });
-
+            public void itemStateChanged(ItemEvent e) {
+                itemChanged = true;
+                System.out.println(e);
+            }
+        });
         choice1.setFocusable(false);
-        add(tf);
-        add(choice1);
-        setLayout (new FlowLayout());
-    }//End  init()
 
-    public void start ()
-    {
-        setSize (200,200);
-        setVisible(true);
-        validate();
-        try{
-            robot = new Robot();
-            Util.waitForIdle(robot);
-            moveFocusToTextField();
-            testKeyOnChoice(InputEvent.BUTTON1_MASK, KeyEvent.VK_UP);
-        } catch (Throwable e) {
-            throw new RuntimeException("Test failed. Exception thrown: "+e);
-        }
-    }// start()
+        frame.add(tf);
+        frame.add(choice1);
+        frame.setLayout (new FlowLayout());
+        frame.setSize (200,200);
+        frame.setVisible(true);
+    }
 
-    public void testKeyOnChoice(int button, int key){
+    private static void testKeyOnChoice(int button, int key) {
         pt = choice1.getLocationOnScreen();
         robot.mouseMove(pt.x + choice1.getWidth()/2, pt.y + choice1.getHeight()/2);
-        Util.waitForIdle(robot);
+
         robot.mousePress(button);
-        robot.delay(10);
         robot.mouseRelease(button);
-        Util.waitForIdle(robot);
 
         robot.keyPress(key);
         robot.keyRelease(key);
 
-        Util.waitForIdle(robot);
-
         System.out.println("keyTypedOnTextField = "+keyTypedOnTextField +": itemChanged = " + itemChanged);
-
-        if (itemChanged){
-                throw new RuntimeException("Test failed. ItemChanged event occur on Choice.");
+        if (itemChanged) {
+            throw new RuntimeException("Test failed. ItemChanged event occur on Choice.");
         }
 
-        // We may just write
-        // if (toolkit.equals("sun.awt.windows.WToolkit") == keyTypedOnTextField) {fail;}
-        // but  must report differently in these cases so put two separate if statements for simplicity.
-        if (toolkit.equals("sun.awt.windows.WToolkit") &&
-            !keyTypedOnTextField)
-        {
-            throw new RuntimeException("Test failed. (Win32) KeyEvent wasn't addressed to TextField. ");
-        }
+       // We may just write
+       // if (toolkit.equals("sun.awt.windows.WToolkit") == keyTypedOnTextField) {fail;}
+       // but  must report differently in these cases so put two separate if statements for simplicity.
+       if (toolkit.equals("sun.awt.windows.WToolkit") &&
+           !keyTypedOnTextField) {
+           throw new RuntimeException("Test failed. (Win32) KeyEvent wasn't addressed to TextField. ");
+       }
 
-        if (!toolkit.equals("sun.awt.windows.WToolkit") &&
-            keyTypedOnTextField)
-        {
-            throw new RuntimeException("Test failed. (XToolkit/MToolkit). KeyEvent was addressed to TextField.");
+       if (!toolkit.equals("sun.awt.windows.WToolkit") &&
+          keyTypedOnTextField) {
+           throw new RuntimeException("Test failed. (XToolkit/MToolkit). KeyEvent was addressed to TextField.");
         }
 
         System.out.println("Test passed. Unfocusable Choice doesn't react on keys.");
+
         //close opened choice
         robot.keyPress(KeyEvent.VK_ESCAPE);
         robot.keyRelease(KeyEvent.VK_ESCAPE);
-        Util.waitForIdle(robot);
     }
 
-    public void moveFocusToTextField(){
+    public static void moveFocusToTextField() {
         pt = tf.getLocationOnScreen();
         robot.mouseMove(pt.x + tf.getWidth()/2, pt.y + tf.getHeight()/2);
-        Util.waitForIdle(robot);
+
         robot.mousePress(InputEvent.BUTTON1_MASK);
-        robot.delay(10);
         robot.mouseRelease(InputEvent.BUTTON1_MASK);
-        Util.waitForIdle(robot);
     }
-}//:~
+}
