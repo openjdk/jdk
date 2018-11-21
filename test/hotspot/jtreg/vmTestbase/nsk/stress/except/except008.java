5639c70032f50b2c805a1b267d54af0bebf49d7b
Jean Christophe Beyler <jcbeyler@openjdk.org>
2018-11-21 09:38:27 -0800

    8213721: [Graal] Tests vmTestbase/nsk/stress/except/except* may be encountering SEGV during out-of-memory conditions
    Add the exception handler only for the current thread
    
    Reviewed-by: dholmes, sspitsyn, thartmann

--- test/hotspot/jtreg/vmTestbase/nsk/stress/except/except008.java
+++ test/hotspot/jtreg/vmTestbase/nsk/stress/except/except008.java
@@ -333,7 +333,7 @@ public class except008 {
      * @see #run(String[], PrintStream)
      */
     public static void main(String args[]) {
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+        Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             // Last try. If there is some exception outside the code, test should end correctly
             @Override
             public void uncaughtException(Thread t, Throwable e) {
