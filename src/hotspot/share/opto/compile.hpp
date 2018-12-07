b68257174d497a955184aa5fc59b610b1d673ce0
Roland Westrelin <roland@openjdk.org>
2018-12-07 17:56:51 +0100

    8214862: assert(proj != __null) at compile.cpp:3251
    Reviewed-by: kvn, thartmann

--- src/hotspot/share/opto/compile.hpp
+++ src/hotspot/share/opto/compile.hpp
@@ -1088,6 +1088,7 @@ class Compile : public Phase {
   void inline_string_calls(bool parse_time);
   void inline_boxing_calls(PhaseIterGVN& igvn);
   bool optimize_loops(PhaseIterGVN& igvn, LoopOptsMode mode);
+  void remove_root_to_sfpts_edges();
 
   // Matching, CFG layout, allocation, code generation
   PhaseCFG*         cfg()                       { return _cfg; }
