c04c838b875501a57a281da3b6ef93b55b9fdcf8
Erik Joelsson <erikj@openjdk.org>
2018-12-10 09:51:23 -0800

    8215030: Disable shenandoah in Oracle builds
    Reviewed-by: kbarrett

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -248,6 +248,7 @@ var getJibProfilesCommon = function (input, data) {
         configure_args: concat(["--enable-jtreg-failure-handler"],
             "--with-exclude-translations=de,es,fr,it,ko,pt_BR,sv,ca,tr,cs,sk,ja_JP_A,ja_JP_HA,ja_JP_HI,ja_JP_I,zh_TW,zh_HK",
             "--disable-manpages",
+            "--with-jvm-features=-shenandoahgc",
             versionArgs(input, common))
     };
     // Extra settings for debug profiles
