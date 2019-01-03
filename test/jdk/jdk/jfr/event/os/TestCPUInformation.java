97c13d5ade9da33d25166a052e5dec0a7239485e
Matthias Baesken <mbaesken@openjdk.org>
2019-01-03 16:14:40 +0100

    8215961: jdk/jfr/event/os/TestCPUInformation.java fails on AArch64
    Reviewed-by: aph, goetz, lucy

--- test/jdk/jdk/jfr/event/os/TestCPUInformation.java
+++ test/jdk/jdk/jfr/event/os/TestCPUInformation.java
@@ -55,7 +55,7 @@ public class TestCPUInformation {
             Events.assertField(event, "cores").atLeast(1);
             Events.assertField(event, "sockets").atLeast(1);
             Events.assertField(event, "cpu").containsAny("Intel", "AMD", "Unknown x86", "sparc", "ARM", "PPC", "PowerPC", "AArch64", "s390");
-            Events.assertField(event, "description").containsAny("Intel", "AMD", "Unknown x86", "SPARC", "ARM", "PPC", "PowerPC", "AArch64", "zArch");
+            Events.assertField(event, "description").containsAny("Intel", "AMD", "Unknown x86", "SPARC", "ARM", "PPC", "PowerPC", "AArch64", "s390");
         }
     }
 }
