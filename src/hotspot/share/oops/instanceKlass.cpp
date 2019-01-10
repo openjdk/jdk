f08eeac278b0785c0bdff0bddf46b66f65822357
Erik Österlund <eosterlund@openjdk.org>
2019-01-10 18:10:15 +0100

    8215889: assert(!_unloading) failed: This oop is not available to unloading class loader data with ZGC
    Reviewed-by: coleenp, neliasso

--- src/hotspot/share/oops/instanceKlass.cpp
+++ src/hotspot/share/oops/instanceKlass.cpp
@@ -2186,6 +2186,7 @@ void InstanceKlass::clean_method_data() {
   for (int m = 0; m < methods()->length(); m++) {
     MethodData* mdo = methods()->at(m)->method_data();
     if (mdo != NULL) {
+      MutexLockerEx ml(SafepointSynchronize::is_at_safepoint() ? NULL : mdo->extra_data_lock());
       mdo->clean_method_data(/*always_clean*/false);
     }
   }
