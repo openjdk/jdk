0032475ef1cd1a800a31fdee1f0e609d6cfbce99
Vicente Romero <vromero@openjdk.org>
2019-01-11 09:02:44 -0500

    8215648: remove equals and hashCode implementations from j.l.i.VarHandle
    Reviewed-by: mchung

--- test/jdk/java/lang/constant/CondyDescTest.java
+++ test/jdk/java/lang/constant/CondyDescTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -67,7 +67,6 @@ public class CondyDescTest extends SymbolicDescTest {
 
     private void testVarHandleDesc(DynamicConstantDesc<VarHandle> r, VarHandle vh) throws ReflectiveOperationException  {
         testSymbolicDesc(r);
-        assertEquals(r.resolveConstantDesc(LOOKUP), vh);
         assertEquals(vh.describeConstable().orElseThrow(), r);
     }
 
