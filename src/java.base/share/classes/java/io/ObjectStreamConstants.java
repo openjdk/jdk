0ec2218c50a24cc18974f09c8242b2646d6b3569
Roger Riggs <rriggs@openjdk.org>
2019-01-15 15:56:41 -0500

    8202675: Replace process-wide terminology in serial filtering to be consistent
    Reviewed-by: alanb, lancea

--- src/java.base/share/classes/java/io/ObjectStreamConstants.java
+++ src/java.base/share/classes/java/io/ObjectStreamConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -201,7 +201,7 @@ public interface ObjectStreamConstants {
                     new SerializablePermission("enableSubclassImplementation");
 
     /**
-     * Enable setting the process-wide serial filter.
+     * Enable setting the system-wide serial filter.
      *
      * @see java.io.ObjectInputFilter.Config#setSerialFilter(ObjectInputFilter)
      * @since 9
