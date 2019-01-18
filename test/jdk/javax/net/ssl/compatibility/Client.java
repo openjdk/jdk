365de9f2bc28d761af84596b5e3dd4fa74f78945
John Jiang <jjiang@openjdk.org>
2019-01-18 14:25:34 +0800

    8203687: javax/net/ssl/compatibility/Compatibility.java supports TLS 1.3
    Reviewed-by: xuelei

--- test/jdk/javax/net/ssl/compatibility/Client.java
+++ test/jdk/javax/net/ssl/compatibility/Client.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -137,7 +137,7 @@ public class Client {
         Status status = Status.SUCCESS;
         Client client = null;
         try {
-            client = new Client(Cert.getCerts(cipherSuite));
+            client = new Client(Cert.getCerts(CipherSuite.cipherSuite(cipherSuite)));
             client.setEnabledProtocols(protocol);
             client.setEnabledCipherSuites(cipherSuite);
 
