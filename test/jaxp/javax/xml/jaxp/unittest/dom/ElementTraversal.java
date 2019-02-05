b63c4ce810601449297be1e070ac7125baaaf292
Joe Wang <joehw@openjdk.org>
2019-02-05 09:57:35 -0800

    8206132: DOM parser does not honor DocumentBuilderFactory.setExpandEntityReferences(false)
    Reviewed-by: rriggs, lancea

--- test/jaxp/javax/xml/jaxp/unittest/dom/ElementTraversal.java
+++ test/jaxp/javax/xml/jaxp/unittest/dom/ElementTraversal.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -125,7 +125,6 @@ public class ElementTraversal {
         Document doc = null;
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setExpandEntityReferences(false);
             DocumentBuilder db = dbf.newDocumentBuilder();
             doc = db.parse(xmlFile);
         } catch (ParserConfigurationException | SAXException | IOException e) {
