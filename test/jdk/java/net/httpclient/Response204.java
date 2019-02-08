fb05ae21bb1dff2c9b9a1a6c2f125f2eba531532
Chris Hegarty <chegar@openjdk.org>
2019-02-08 12:23:16 +0000

    8218662: Allow 204 responses with Content-Length:0
    Reviewed-by: michaelm

--- test/jdk/java/net/httpclient/Response204.java
+++ test/jdk/java/net/httpclient/Response204.java
@@ -23,7 +23,7 @@
 
 /**
  * @test
- * @bug 8211437
+ * @bug 8211437 8218662
  * @run main/othervm -Djdk.httpclient.HttpClient.log=headers,requests Response204
  * @summary
  */
@@ -33,11 +33,13 @@ import com.sun.net.httpserver.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.logging.*;
 import java.io.*;
 import java.net.*;
+import static java.net.http.HttpClient.Builder.NO_PROXY;
 
 /**
  * Verify that a 204 response code with no content-length is handled correctly
@@ -54,6 +56,7 @@ public class Response204 {
         InetSocketAddress addr = new InetSocketAddress (0);
         HttpServer server = HttpServer.create (addr, 0);
         HttpContext ctx = server.createContext ("/test", handler);
+        server.createContext ("/zero", new ZeroContentLengthHandler());
         ExecutorService executor = Executors.newCachedThreadPool();
         server.setExecutor (executor);
         server.start ();
@@ -80,10 +83,29 @@ public class Response204 {
             } catch (IOException ioe) {
                 System.out.println("OK 2");
             }
+
+            // Test 3
+            testZeroContentLength(uri.resolve("/zero/xxyy"));
+            System.out.println ("OK 3");
         } finally {
             server.stop(2);
             executor.shutdown();
         }
+    }
+
+    static void testZeroContentLength(URI uri) throws Exception {
+        System.out.println("--- testZeroContentLength ---");
+        HttpClient client = HttpClient.newBuilder().proxy(NO_PROXY).build();
+        HttpRequest request = HttpRequest.newBuilder(uri).build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        System.out.println("Received response:" + response);
+        System.out.println("Received headers:" + response.headers());
+        if (response.statusCode() != 204)
+            throw new RuntimeException("Expected 204, got:" + response.statusCode());
+        if (response.body() != null && !response.body().equals(""))
+            throw new RuntimeException("Expected empty response, got: " + response.body());
+        if (response.headers().firstValueAsLong("Content-Length").orElse(-1L) != 0L)
+            throw new RuntimeException("Expected Content-Length:0, in: " + response.headers());
     }
 
     public static boolean error = false;
@@ -102,6 +124,18 @@ public class Response204 {
                 // pretend there is a body
                 rmap.set("Content-length", "10");
             }
+            t.sendResponseHeaders(204, -1);
+            t.close();
+        }
+    }
+
+    // A handler that returns a 204 with a `Content-Length: 0` header/value
+    static class ZeroContentLengthHandler implements HttpHandler {
+        public void handle(HttpExchange t) throws IOException {
+            try (InputStream is = t.getRequestBody()) {
+                is.readAllBytes();
+            }
+            t.getResponseHeaders().set("Content-length", "0");
             t.sendResponseHeaders(204, -1);
             t.close();
         }
