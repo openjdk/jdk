e4dc17c3548aa1277b82f98327742b50b8a4c28e
Chris Plummer <cjplummer@openjdk.org>
2019-02-15 12:33:11 -0800

    8218941: jdb should support a dbgtrace command that acts the same as the dbgtrace command line option
    Added dbgtrace command.
    
    Reviewed-by: sspitsyn, amenkov, gadams

--- src/jdk.jdi/share/classes/com/sun/tools/example/debug/tty/Env.java
+++ src/jdk.jdi/share/classes/com/sun/tools/example/debug/tty/Env.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -62,6 +62,10 @@ class Env {
         if (!connection.isLaunch() || openNow) {
             connection.open();
         }
+    }
+
+    static void setTraceFlags(int flags) {
+        connection.setTraceFlags(flags);
     }
 
     static VMConnection connection() {
