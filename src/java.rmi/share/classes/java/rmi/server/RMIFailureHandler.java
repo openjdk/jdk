145c79b8c96c833ef1d5715ee37d98591a953415
Ivan Gerasimov <igerasim@openjdk.org>
2019-02-19 09:06:44 -0800

    4887513: Typo in RMIFailureHandler interface doc page
    Co-authored-by: Andrey Turbanov <turbanoff@gmail.com>
    Reviewed-by: dfuchs

--- src/java.rmi/share/classes/java/rmi/server/RMIFailureHandler.java
+++ src/java.rmi/share/classes/java/rmi/server/RMIFailureHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 1998, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,13 +26,13 @@
 package java.rmi.server;
 
 /**
- * An <code>RMIFailureHandler</code> can be registered via the
- * <code>RMISocketFactory.setFailureHandler</code> call. The
- * <code>failure</code> method of the handler is invoked when the RMI
- * runtime is unable to create a <code>ServerSocket</code> to listen
- * for incoming calls. The <code>failure</code> method returns a boolean
+ * An {@code RMIFailureHandler} can be registered via the
+ * {@code RMISocketFactory.setFailureHandler} call. The
+ * {@code failure} method of the handler is invoked when the RMI
+ * runtime is unable to create a {@code ServerSocket} to listen
+ * for incoming calls. The {@code failure} method returns a boolean
  * indicating whether the runtime should attempt to re-create the
- * <code>ServerSocket</code>.
+ * {@code ServerSocket}.
  *
  * @author      Ann Wollrath
  * @since       1.1
@@ -40,18 +40,18 @@ package java.rmi.server;
 public interface RMIFailureHandler {
 
     /**
-     * The <code>failure</code> callback is invoked when the RMI
-     * runtime is unable to create a <code>ServerSocket</code> via the
-     * <code>RMISocketFactory</code>. An <code>RMIFailureHandler</code>
+     * The {@code failure} callback is invoked when the RMI
+     * runtime is unable to create a {@code ServerSocket} via the
+     * {@code RMISocketFactory}. An {@code RMIFailureHandler}
      * is registered via a call to
-     * <code>RMISocketFacotry.setFailureHandler</code>.  If no failure
+     * {@code RMISocketFactory.setFailureHandler}.  If no failure
      * handler is installed, the default behavior is to attempt to
      * re-create the ServerSocket.
      *
-     * @param ex the exception that occurred during <code>ServerSocket</code>
+     * @param ex the exception that occurred during {@code ServerSocket}
      *           creation
      * @return if true, the RMI runtime attempts to retry
-     * <code>ServerSocket</code> creation
+     * {@code ServerSocket} creation
      * @see java.rmi.server.RMISocketFactory#setFailureHandler(RMIFailureHandler)
      * @since 1.1
      */
