7325fc635139090a45ed121e17b8ea3a5fa70b0f
Joe Darcy <darcy@openjdk.org>
2019-02-27 11:33:57 -0800

    8219805: Cross-link javax.lang.model.{type, element} packages to utility interfaces
    Reviewed-by: vromero

--- src/java.compiler/share/classes/javax/lang/model/element/package-info.java
+++ src/java.compiler/share/classes/javax/lang/model/element/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -107,6 +107,7 @@
  * @author Joseph D. Darcy
  * @author Scott Seligman
  * @author Peter von der Ah&eacute;
+ * @see javax.lang.model.util.Elements
  * @since 1.6
  */
 package javax.lang.model.element;
