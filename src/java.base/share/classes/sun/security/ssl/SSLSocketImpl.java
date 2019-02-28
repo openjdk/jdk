8f84ae56843f10810ba3d256cf9e766181736f4b
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2019-02-28 10:04:27 -0800

    8219658: Deadlock in sun.security.ssl.SSLSocketImpl
    Reviewed-by: jnimeh

--- src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -916,8 +916,12 @@ public final class SSLSocketImpl
         /**
          * Try the best to use up the input records so as to close the
          * socket gracefully, without impact the performance too much.
+         *
+         * Note: please don't synchronize this method as the read() method
+         * may hold the lock. A race should be fine as this method is
+         * designed for cleanup only.
          */
-        private synchronized void deplete() {
+        private void deplete() {
             if (!conContext.isInboundClosed()) {
                 if (!(conContext.inputRecord instanceof SSLSocketInputRecord)) {
                     return;
