6fd9b75e55cf6d6e06f8781ee648ed5b0bb1be4a
Chris Plummer <cjplummer@openjdk.org>
2019-03-13 19:42:23 -0700

    8220352: Crash with assert(external_guard || result != __null) failed: Invalid JNI handle
    Don't delete globalrefs that might still be referenced
    
    Reviewed-by: gadams, jcbeyler, sspitsyn

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/MonitorWaited/monitorwaited001/monitorwaited001.cpp
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/MonitorWaited/monitorwaited001/monitorwaited001.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -147,10 +147,6 @@ static int clean() {
     if (!NSK_JVMTI_VERIFY(
             jvmti->SetEventNotificationMode(JVMTI_DISABLE, JVMTI_EVENT_MONITOR_WAITED, NULL)))
         nsk_jvmti_setFailStatus();
-
-    /* dispose global references */
-    jni->DeleteGlobalRef(object);
-    jni->DeleteGlobalRef(thread);
 
     return NSK_TRUE;
 }
