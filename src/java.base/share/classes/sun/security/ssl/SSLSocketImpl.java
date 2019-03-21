901e797c0c8c29c7496b797e8b2ad45b4017b577
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2019-03-21 13:32:08 -0700

    8221270: Duplicated synchronized keywords in SSLSocketImpl
    Reviewed-by: mullan

--- src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -342,15 +342,8 @@ public final class SSLSocketImpl
 
     @Override
     public synchronized SSLSession getHandshakeSession() {
-        if (conContext.handshakeContext != null) {
-            synchronized (this) {
-                if (conContext.handshakeContext != null) {
-                    return conContext.handshakeContext.handshakeSession;
-                }
-            }
-        }
-
-        return null;
+        return conContext.handshakeContext == null ?
+                null : conContext.handshakeContext.handshakeSession;
     }
 
     @Override
