57bf6a180050629b08e2ec81fd6d1e68d4decdca
Daniil Titov <dtitov@openjdk.org>
2019-03-26 04:19:39 +0000

    8217827: [Graal] Some vmTestbase/nsk/jvmti/ResourceExhausted tests failing
    Reviewed-by: cjplummer, jcbeyler, dholmes

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/ResourceExhausted/resexhausted002/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/ResourceExhausted/resexhausted002/TestDescription.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,6 +36,7 @@
  *
  * @library /vmTestbase
  *          /test/lib
+ * @requires !vm.graal.enabled
  * @run driver jdk.test.lib.FileInstaller . .
  * @run main/othervm/native
  *      -agentlib:resexhausted=-waittime=5
