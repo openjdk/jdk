21d2f5139ddb5a2e3e749038808a6a37b4025c56
Sergey Bylokhov <serb@openjdk.org>
2019-03-29 23:14:37 -0700

    8221436: Incorrect check of package in Line.Info.toString()
    Reviewed-by: prr

--- src/java.desktop/share/classes/javax/sound/sampled/Line.java
+++ src/java.desktop/share/classes/javax/sound/sampled/Line.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -324,20 +324,11 @@ public interface Line extends AutoCloseable {
          */
         @Override
         public String toString() {
-
-            String fullPackagePath = "javax.sound.sampled.";
-            String initialString = new String(getLineClass().toString());
-            String finalString;
-
-            int index = initialString.indexOf(fullPackagePath);
-
-            if (index != -1) {
-                finalString = initialString.substring(0, index) + initialString.substring( (index + fullPackagePath.length()), initialString.length() );
-            } else {
-                finalString = initialString;
+            final String str = getLineClass().toString();
+            if (getLineClass().getPackage() == Line.class.getPackage()) {
+                return str.replace("javax.sound.sampled.", "");
             }
-
-            return finalString;
+            return str;
         }
     }
 }
