cd6079acdd018cd714e840f29d943c215c655fb2
Stefan Karlsson <stefank@openjdk.org>
2019-05-09 14:26:03 +0200

    8223620: Move IsGCActiveMark implementation out of header
    Reviewed-by: coleenp, lkorinth

--- src/hotspot/share/gc/shared/isGCActiveMark.cpp
+++ src/hotspot/share/gc/shared/isGCActiveMark.cpp
@@ -22,21 +22,23 @@
  *
  */
 
-#ifndef SHARE_GC_SHARED_ISGCACTIVEMARK_HPP
-#define SHARE_GC_SHARED_ISGCACTIVEMARK_HPP
-
+#include "precompiled.hpp"
 #include "gc/shared/collectedHeap.hpp"
-#include "memory/allocation.hpp"
+#include "gc/shared/isGCActiveMark.hpp"
 #include "memory/universe.hpp"
 #include "utilities/debug.hpp"
 
 // This class provides a method for block structured setting of the
 // _is_gc_active state without requiring accessors in CollectedHeap
 
-class IsGCActiveMark : public StackObj {
- public:
-  IsGCActiveMark();
-  ~IsGCActiveMark();
-};
+IsGCActiveMark::IsGCActiveMark() {
+  CollectedHeap* heap = Universe::heap();
+  assert(!heap->is_gc_active(), "Not reentrant");
+  heap->_is_gc_active = true;
+}
 
-#endif // SHARE_GC_SHARED_ISGCACTIVEMARK_HPP
+IsGCActiveMark::~IsGCActiveMark() {
+  CollectedHeap* heap = Universe::heap();
+  assert(heap->is_gc_active(), "Sanity");
+  heap->_is_gc_active = false;
+}
