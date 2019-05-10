11d3a2ce355a40ca8ea68a02505b8f823107041e
Bob Vandette <bobv@openjdk.org>
2019-05-10 11:15:34 -0400

    8221340: [TESTBUG] TestCgroupMetrics.java fails after fix for JDK-8219562
    Reviewed-by: mseledtsov, rriggs

--- src/java.base/linux/classes/jdk/internal/platform/cgroupv1/SubSystem.java
+++ src/java.base/linux/classes/jdk/internal/platform/cgroupv1/SubSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,7 +61,7 @@ public class SubSystem {
                     path = mountPoint;
                 }
                 else {
-                    if (root.indexOf(cgroupPath) == 0) {
+                    if (cgroupPath.startsWith(root)) {
                         if (cgroupPath.length() > root.length()) {
                             String cgroupSubstr = cgroupPath.substring(root.length());
                             path = mountPoint + cgroupSubstr;
