8de6f58988e4c8387d91857078896b10bd4be55f
Doug Simon <dnsimon@openjdk.org>
2019-05-10 10:30:06 -0700

    8223531: [Graal] assert(type() == T_INT) failed: type check
    Reviewed-by: dlong, thartmann, never, kvn

--- src/hotspot/share/jvmci/vmStructs_jvmci.cpp
+++ src/hotspot/share/jvmci/vmStructs_jvmci.cpp
@@ -162,6 +162,11 @@
   volatile_nonstatic_field(JavaFrameAnchor,    _last_Java_sp,                                 intptr_t*)                             \
   volatile_nonstatic_field(JavaFrameAnchor,    _last_Java_pc,                                 address)                               \
                                                                                                                                      \
+  nonstatic_field(JVMCICompileState,           _jvmti_can_hotswap_or_post_breakpoint,         jbyte)                                 \
+  nonstatic_field(JVMCICompileState,           _jvmti_can_access_local_variables,             jbyte)                                 \
+  nonstatic_field(JVMCICompileState,           _jvmti_can_post_on_exceptions,                 jbyte)                                 \
+  nonstatic_field(JVMCICompileState,           _jvmti_can_pop_frame,                          jbyte)                                 \
+                                                                                                                                     \
   nonstatic_field(JavaThread,                  _threadObj,                                    oop)                                   \
   nonstatic_field(JavaThread,                  _anchor,                                       JavaFrameAnchor)                       \
   nonstatic_field(JavaThread,                  _vm_result,                                    oop)                                   \
