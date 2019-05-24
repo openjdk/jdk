409113f576bad7c2269117f6a0f17a133359426d
Hannes Wallnöfer <hannesw@openjdk.org>
2019-05-24 16:53:44 +0200

    8223518: Unexpected exception in jjs
    Reviewed-by: sundar, mschoene, rhalade, jlaskey

--- src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/regexp/JdkRegExp.java
+++ src/jdk.scripting.nashorn/share/classes/jdk/nashorn/internal/runtime/regexp/JdkRegExp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,6 +82,8 @@ public class JdkRegExp extends RegExp {
             }
         } catch (final PatternSyntaxException e2) {
             throwParserException("syntax", e2.getMessage());
+        } catch (StackOverflowError e3) {
+            throw new RuntimeException(e3);
         }
     }
 
