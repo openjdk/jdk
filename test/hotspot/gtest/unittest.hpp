99a18f12492a40eee3755e88c841fa0291bcddce
Igor Ignatyev <iignatyev@openjdk.org>
2019-05-27 19:46:34 -0700

    8222414: bring googlemock v1.8.1
    Reviewed-by: erikj, rwestberg

--- test/hotspot/gtest/unittest.hpp
+++ test/hotspot/gtest/unittest.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,23 @@
 #include <stdio.h>
 
 #define GTEST_DONT_DEFINE_TEST 1
+
+// googlemock has ::testing::internal::Log function, so we need to temporary
+// undefine 'Log' from logging/log.hpp and define it back after gmock header
+// file is included. As SS compiler doesn't have push_/pop_macro pragmas and
+// log.hpp might have been already included, we have to copy-paste macro definition.
+#ifdef Log
+  #define UNDEFINED_Log
+  #undef Log
+#endif
+
+#include "gmock/gmock.h"
 #include "gtest/gtest.h"
+
+#ifdef UNDEFINED_Log
+  #define Log(...)  LogImpl<LOG_TAGS(__VA_ARGS__)> // copied from logging/log.hpp
+  #undef UNDEFINED_Log
+#endif
 
 // gtest/gtest.h includes assert.h which will define the assert macro, but hotspot has its
 // own standards incompatible assert macro that takes two parameters.
