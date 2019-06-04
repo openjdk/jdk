b8e5aefce8c523957762129b8ae0e975253bae7f
Ekaterina Pavlova <epavlova@openjdk.org>
2019-06-03 17:14:23 -0700

    8225199: [Graal] compiler/jvmci/compilerToVM/IsMatureVsReprofileTest.java fails with -XX:CompileThresholdScaling=0.1
    Reviewed-by: kvn

--- test/hotspot/jtreg/compiler/jvmci/compilerToVM/IsMatureVsReprofileTest.java
+++ test/hotspot/jtreg/compiler/jvmci/compilerToVM/IsMatureVsReprofileTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,7 +37,7 @@
  * @run driver ClassFileInstaller sun.hotspot.WhiteBox
  *                                sun.hotspot.WhiteBox$WhiteBoxPermission
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions
- *     -XX:+WhiteBoxAPI -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Xbatch
+ *     -XX:+WhiteBoxAPI -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI -Xbatch -XX:CompileThresholdScaling=1.0
  *     compiler.jvmci.compilerToVM.IsMatureVsReprofileTest
  */
 
