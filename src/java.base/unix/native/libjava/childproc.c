d45aa9c1a943b73f83ef3dd32f04cbbd2a9713bd
Thomas Stuefe <stuefe@openjdk.org>
2019-06-05 09:12:45 +0200

    8223777: In posix_spawn mode, failing to exec() jspawnhelper does not result in an error
    Reviewed-by: rriggs, martin, fweimer

--- src/java.base/unix/native/libjava/childproc.c
+++ src/java.base/unix/native/libjava/childproc.c
@@ -315,6 +315,13 @@ childProcess(void *arg)
     const ChildStuff* p = (const ChildStuff*) arg;
     int fail_pipe_fd = p->fail[1];
 
+    if (p->sendAlivePing) {
+        /* Child shall signal aliveness to parent at the very first
+         * moment. */
+        int code = CHILD_IS_ALIVE;
+        restartableWrite(fail_pipe_fd, &code, sizeof(code));
+    }
+
     /* Close the parent sides of the pipes.
        Closing pipe fds here is redundant, since closeDescriptors()
        would do it anyways, but a little paranoia is a good thing. */
