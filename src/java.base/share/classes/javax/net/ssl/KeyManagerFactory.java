6ad9eba4cd71caf04697be75df4d7f67a93843ae
Sean Mullan <mullan@openjdk.org>
2019-06-24 10:11:17 -0400

    8180005: Provide specific links in KeyManagerFactory and TrustManagerFactory to the Standard Algorithm Names Specification
    Reviewed-by: ascarpino

--- src/java.base/share/classes/javax/net/ssl/KeyManagerFactory.java
+++ src/java.base/share/classes/javax/net/ssl/KeyManagerFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -127,9 +127,10 @@ public class KeyManagerFactory {
      *
      * @param algorithm the standard name of the requested algorithm.
      *          See the <a href=
-     *          "{@docRoot}/../specs/security/standard-names.html">
-     *          Java Security Standard Algorithm Names</a> document
-     *          for information about standard algorithm names.
+     *          "{@docRoot}/../specs/security/standard-names.html#keymanagerfactory-algorithms">
+     *          KeyManagerFactory section</a> in the Java Security Standard
+     *          Algorithm Names Specification for information about standard
+     *          algorithm names.
      *
      * @return the new {@code KeyManagerFactory} object
      *
@@ -165,9 +166,10 @@ public class KeyManagerFactory {
 
      * @param algorithm the standard name of the requested algorithm.
      *          See the <a href=
-     *          "{@docRoot}/../specs/security/standard-names.html">
-     *          Java Security Standard Algorithm Names</a> document
-     *          for information about standard algorithm names.
+     *          "{@docRoot}/../specs/security/standard-names.html#keymanagerfactory-algorithms">
+     *          KeyManagerFactory section</a> in the Java Security Standard
+     *          Algorithm Names Specification for information about standard
+     *          algorithm names.
      *
      * @param provider the name of the provider.
      *
@@ -209,9 +211,10 @@ public class KeyManagerFactory {
      *
      * @param algorithm the standard name of the requested algorithm.
      *          See the <a href=
-     *          "{@docRoot}/../specs/security/standard-names.html">
-     *          Java Security Standard Algorithm Names</a> document
-     *          for information about standard algorithm names.
+     *          "{@docRoot}/../specs/security/standard-names.html#keymanagerfactory-algorithms">
+     *          KeyManagerFactory section</a> in the Java Security Standard
+     *          Algorithm Names Specification for information about standard
+     *          algorithm names.
      *
      * @param provider an instance of the provider.
      *
