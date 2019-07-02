605b3f051b6bc7cfbe524b459f2c08956987ddda
Stefan Karlsson <stefank@openjdk.org>
2019-07-02 12:28:41 +0200

    8227086: Use AS_NO_KEEPALIVE loads in HeapDumper
    Reviewed-by: kbarrett, sspitsyn

--- src/hotspot/share/services/heapDumper.cpp
+++ src/hotspot/share/services/heapDumper.cpp
@@ -760,7 +760,7 @@ void DumperSupport::dump_field_value(DumpWriter* writer, char type, oop obj, int
   switch (type) {
     case JVM_SIGNATURE_CLASS :
     case JVM_SIGNATURE_ARRAY : {
-      oop o = obj->obj_field_access<ON_UNKNOWN_OOP_REF>(offset);
+      oop o = obj->obj_field_access<ON_UNKNOWN_OOP_REF | AS_NO_KEEPALIVE>(offset);
       assert(oopDesc::is_oop_or_null(o), "Expected an oop or NULL at " PTR_FORMAT, p2i(o));
       writer->write_objectID(o);
       break;
