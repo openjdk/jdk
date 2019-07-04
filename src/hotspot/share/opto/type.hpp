e844e34e81284b0a9617097fc5c0003d71106c14
Tobias Hartmann <thartmann@openjdk.org>
2019-07-04 09:12:21 +0200

    8227173: Minor cleanup of unused code in compile.hpp
    Removed unused code.
    
    Reviewed-by: kvn

--- src/hotspot/share/opto/type.hpp
+++ src/hotspot/share/opto/type.hpp
@@ -184,9 +184,7 @@ public:
   inline void* operator new( size_t x ) throw() {
     Compile* compile = Compile::current();
     compile->set_type_last_size(x);
-    void *temp = compile->type_arena()->Amalloc_D(x);
-    compile->set_type_hwm(temp);
-    return temp;
+    return compile->type_arena()->Amalloc_D(x);
   }
   inline void operator delete( void* ptr ) {
     Compile* compile = Compile::current();
