a7016e3b5dc46f7423c7a71dc82757fe813830ee
Brian Burkhalter <bpb@openjdk.org>
2019-07-17 14:24:37 -0700

    8067801: Enforce null check for underlying I/O streams
    Reviewed-by: lancea

--- src/java.base/share/classes/java/io/FilterOutputStream.java
+++ src/java.base/share/classes/java/io/FilterOutputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -67,6 +67,10 @@ public class FilterOutputStream extends OutputStream {
      *                created without an underlying stream.
      */
     public FilterOutputStream(OutputStream out) {
+        if (out == null) {
+            throw new NullPointerException();
+        }
+
         this.out = out;
     }
 
