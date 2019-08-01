66dc6cf25b5c1a70ae493b614e29b8dd1b1b35dd
Ivan Gerasimov <igerasim@openjdk.org>
2019-08-01 07:14:03 -0700

    8228352: CANON_EQ breaks when pattern contains supplementary codepoint
    Reviewed-by: naoto

--- test/jdk/java/util/regex/RegExTest.java
+++ test/jdk/java/util/regex/RegExTest.java
@@ -35,7 +35,7 @@
  * 8027645 8035076 8039124 8035975 8074678 6854417 8143854 8147531 7071819
  * 8151481 4867170 7080302 6728861 6995635 6736245 4916384 6328855 6192895
  * 6345469 6988218 6693451 7006761 8140212 8143282 8158482 8176029 8184706
- * 8194667 8197462 8184692 8221431 8224789
+ * 8194667 8197462 8184692 8221431 8224789 8228352
  *
  * @library /test/lib
  * @library /lib/testlibrary/java/lang
@@ -185,6 +185,7 @@ public class RegExTest {
         expoBacktracking();
         invalidGroupName();
         illegalRepetitionRange();
+        surrogatePairWithCanonEq();
 
         if (failure) {
             throw new
@@ -4972,5 +4973,15 @@ public class RegExTest {
             }
         }
         report("illegalRepetitionRange");
+    }
+
+    private static void surrogatePairWithCanonEq() {
+        try {
+            Pattern.compile("\ud834\udd21", Pattern.CANON_EQ);
+        } catch (Throwable t) {
+            failCount++;
+            System.out.println("Unexpected exception: " + t);
+        }
+        report("surrogatePairWithCanonEq");
     }
 }
