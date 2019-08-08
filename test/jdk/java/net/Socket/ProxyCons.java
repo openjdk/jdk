4312f54e51f68d57b90b4e092f67434099ece57e
Aleksei Efimov <aefimov@openjdk.org>
2019-08-08 21:58:11 +0100

    8225430: Replace wildcard address with loopback or local host in tests - part 14
    Reviewed-by: dfuchs, chegar, vtewari

--- test/jdk/java/net/Socket/ProxyCons.java
+++ test/jdk/java/net/Socket/ProxyCons.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -64,12 +64,14 @@ public class ProxyCons {
     }
 
     void test() throws Exception {
-        ServerSocket ss = new ServerSocket(0);
+        InetAddress localHost = InetAddress.getLocalHost();
+        ServerSocket ss = new ServerSocket();
+        ss.bind(new InetSocketAddress(localHost, 0));
         try {
             Server s = new Server(ss);
             s.start();
             Socket sock = new Socket(Proxy.NO_PROXY);
-            sock.connect(new InetSocketAddress("localhost", ss.getLocalPort()));
+            sock.connect(new InetSocketAddress(localHost, ss.getLocalPort()));
             s.done();
             sock.close();
         } catch (java.io.IOException e) {
