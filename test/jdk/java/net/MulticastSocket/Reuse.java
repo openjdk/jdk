a9f92bec7f3acd333742cb726e80bd487a0cdb0d
Patrick Concannon <patrick.concannon@oracle.com>
2019-08-22 18:54:56 +0100

    8078219: Verify lack of @test tag in files in java/net test directory
    Reviewed-by: alanb

--- test/jdk/java/net/MulticastSocket/Reuse.java
+++ test/jdk/java/net/MulticastSocket/Reuse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,11 @@
 
 import java.net.MulticastSocket;
 import java.net.BindException;
+
+/*
+ * @test
+ * @summary Check if MulticastSocket sets SO_REUSEADDR
+ */
 
 public class Reuse {
     public static void main(String[] args) throws Exception {
