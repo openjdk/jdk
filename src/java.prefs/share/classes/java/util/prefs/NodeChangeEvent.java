b03fbff86666867e76fb523418a9e82e8c7f4fd3
Joe Darcy <darcy@openjdk.org>
2019-09-26 12:13:57 -0700

    8231546: Suppress warnings on non-serializable instance fields in java.prefs module
    Reviewed-by: rriggs

--- src/java.prefs/share/classes/java/util/prefs/NodeChangeEvent.java
+++ src/java.prefs/share/classes/java/util/prefs/NodeChangeEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2003, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -49,7 +49,7 @@ public class NodeChangeEvent extends java.util.EventObject {
      *
      * @serial
      */
-    private Preferences child;
+    private transient Preferences child;
 
     /**
      * Constructs a new {@code NodeChangeEvent} instance.
