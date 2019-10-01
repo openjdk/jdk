17c057bd7677f9d72d53fbfb40fa9062c9906e91
Brian Burkhalter <bpb@openjdk.org>
2019-10-01 07:37:39 -0700

    8229333: java/io/File/SetLastModified.java timed out
    Reviewed-by: lancea, rriggs

--- test/jdk/java/io/File/SetLastModified.java
+++ test/jdk/java/io/File/SetLastModified.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,6 +23,7 @@
 
 /* @test
    @bug 4091757 6652379 8177809
+   @requires os.maxMemory >= 16G
    @summary Basic test for setLastModified method
  */
 
