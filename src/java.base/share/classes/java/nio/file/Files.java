dcc9cc3fdd3362d917596ed19ac28b7bbe30fbe4
Severin Gehwolf <sgehwolf@openjdk.org>
2019-10-08 15:30:46 +0200

    8232003: (fs) Files.write can leak file descriptor in the exception case
    Be sure to close the leaked OutputStream in all cases
    
    Reviewed-by: alanb, bpb, clanger

--- src/java.base/share/classes/java/nio/file/Files.java
+++ src/java.base/share/classes/java/nio/file/Files.java
@@ -3550,8 +3550,8 @@ public final class Files {
         // ensure lines is not null before opening file
         Objects.requireNonNull(lines);
         CharsetEncoder encoder = cs.newEncoder();
-        OutputStream out = newOutputStream(path, options);
-        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, encoder))) {
+        try (OutputStream out = newOutputStream(path, options);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, encoder))) {
             for (CharSequence line: lines) {
                 writer.append(line);
                 writer.newLine();
