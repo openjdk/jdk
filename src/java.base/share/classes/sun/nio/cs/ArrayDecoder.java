0dbfc97c05218ffd10242901d73c0715ccb53bf3
Andrew Leonard <aleonard@openjdk.org>
2019-10-10 10:28:55 +0100

    8231717: Improve performance of charset decoding when charset is always compactable
    Reviewed-by: rriggs, redestad, alanb

--- src/java.base/share/classes/sun/nio/cs/ArrayDecoder.java
+++ src/java.base/share/classes/sun/nio/cs/ArrayDecoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,6 +28,9 @@ package sun.nio.cs;
 /*
  * FastPath byte[]->char[] decoder, REPLACE on malformed or
  * unmappable input.
+ *
+ * FastPath encoded byte[]-> "String Latin1 coding" byte[] decoder for use when
+ * charset is always decodable to the internal String Latin1 coding byte[], ie. all mappings <=0xff
  */
 
 public interface ArrayDecoder {
@@ -35,5 +38,15 @@ public interface ArrayDecoder {
 
     default boolean isASCIICompatible() {
         return false;
+    }
+
+    // Is always decodable to internal String Latin1 coding, ie. all mappings <= 0xff
+    default boolean isLatin1Decodable() {
+        return false;
+    }
+
+    // Decode to internal String Latin1 coding byte[] fastpath for when isLatin1Decodable == true
+    default int decodeToLatin1(byte[] src, int sp, int len, byte[] dst) {
+        return 0;
     }
 }
