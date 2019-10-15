b5d427b0bc9fcc9c1c7b58694c8d65401a95dd1c
Joe Darcy <darcy@openjdk.org>
2019-10-15 13:25:57 -0700

    8232234: Suppress warnings on non-serializable non-transient instance fields in java.rmi
    Reviewed-by: rriggs

--- src/java.rmi/share/classes/sun/rmi/server/ActivationGroupImpl.java
+++ src/java.rmi/share/classes/sun/rmi/server/ActivationGroupImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -69,6 +69,7 @@ public class ActivationGroupImpl extends ActivationGroup {
         new Hashtable<>();
     private boolean groupInactive = false;
     private final ActivationGroupID groupID;
+    @SuppressWarnings("serial")  // Conditionally serializable
     private final List<ActivationID> lockedIDs = new ArrayList<>();
 
     /**
