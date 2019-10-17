bd96098ec2d92e8b62fb51932a633ceccdf0bce2
Joe Darcy <darcy@openjdk.org>
2019-10-17 09:38:32 -0700

    8232448: Suppress warnings on non-serializable non-transient instance fields in jdk.jdi
    Reviewed-by: sspitsyn

--- src/jdk.jdi/share/classes/com/sun/jdi/connect/IllegalConnectorArgumentsException.java
+++ src/jdk.jdi/share/classes/com/sun/jdi/connect/IllegalConnectorArgumentsException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -40,6 +40,7 @@ public class IllegalConnectorArgumentsException extends Exception {
 
     private static final long serialVersionUID = -3042212603611350941L;
 
+    @SuppressWarnings("serial") // Conditionally serializable
     List<String> names;
 
     /**
