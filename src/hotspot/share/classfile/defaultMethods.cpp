21ce9772a7073babdffc25a3ac7dbfeb339594cd
Calvin Cheung <ccheung@openjdk.org>
2019-10-18 09:17:38 -0700

    8232030: HelloDynamic.java fails with latest Graal
    Include the dynamic dumping condition when setting up the merged_ordering array in merge_in_new_methods() of defaultMethods.cpp.
    
    Reviewed-by: iklam, coleenp

--- src/hotspot/share/classfile/defaultMethods.cpp
+++ src/hotspot/share/classfile/defaultMethods.cpp
@@ -1012,7 +1012,7 @@ static void merge_in_new_methods(InstanceKlass* klass,
       klass->class_loader_data(), new_size, NULL, CHECK);
 
   // original_ordering might be empty if this class has no methods of its own
-  if (JvmtiExport::can_maintain_original_method_order() || DumpSharedSpaces) {
+  if (JvmtiExport::can_maintain_original_method_order() || Arguments::is_dumping_archive()) {
     merged_ordering = MetadataFactory::new_array<int>(
         klass->class_loader_data(), new_size, CHECK);
   }
