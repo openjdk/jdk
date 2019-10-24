31ab60e21107caeaa92bd1706f5b618a5d1ffbd4
Christoph Goettschkes <christoph.goettschkes@microdoc.com>
2019-10-24 08:49:21 -0400

    8231953: Wrong assumption in assertion in oop::register_oop
    On ARM32 thumb mode, the pc of the current frame is always zero
    
    Reviewed-by: coleenp, dcubed

--- src/hotspot/share/oops/oopsHierarchy.cpp
+++ src/hotspot/share/oops/oopsHierarchy.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,8 +37,6 @@ void oop::register_oop() {
   Thread* t = Thread::current_or_null();
   if (t != NULL && t->is_Java_thread()) {
      frame fr = os::current_frame();
-     // This points to the oop creator, I guess current frame points to caller
-     assert (fr.pc(), "should point to a vm frame");
      t->unhandled_oops()->register_unhandled_oop(this, fr.pc());
   }
 }
