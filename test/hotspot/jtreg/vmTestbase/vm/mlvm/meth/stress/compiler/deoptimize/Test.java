db24eb1e6a0b777dc44f44ae3a1d75ad8d23d6d0
Ekaterina Pavlova <epavlova@openjdk.org>
2019-10-25 07:15:09 -0700

    8233000: Mark vmTestbase/vm/mlvm/meth/stress/compiler/deoptimize test as stress test
    Reviewed-by: iveresov, lmesnik

--- test/hotspot/jtreg/vmTestbase/vm/mlvm/meth/stress/compiler/deoptimize/Test.java
+++ test/hotspot/jtreg/vmTestbase/vm/mlvm/meth/stress/compiler/deoptimize/Test.java
@@ -24,6 +24,7 @@
 
 /*
  * @test
+ * @key stress
  *
  * @summary converted from VM Testbase vm/mlvm/meth/stress/compiler/deoptimize.
  * VM Testbase keywords: [feature_mlvm, nonconcurrent, quarantine]
@@ -54,6 +55,7 @@
 
 /*
  * @test
+ * @key stress
  *
  * @library /vmTestbase
  *          /test/lib
