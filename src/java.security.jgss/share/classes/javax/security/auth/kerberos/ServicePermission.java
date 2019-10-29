026a05d0c607ec7ebc3549870f5c7d88e0177395
Weijun Wang <weijun@openjdk.org>
2019-10-29 09:34:23 +0800

    8231365: ServicePermission::equals doesn't comply to the spec 8231196: DelegationPermission allows to create an instance that thows NPE on ::equals call
    Reviewed-by: mullan

--- src/java.security.jgss/share/classes/javax/security/auth/kerberos/ServicePermission.java
+++ src/java.security.jgss/share/classes/javax/security/auth/kerberos/ServicePermission.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -238,7 +238,7 @@ public final class ServicePermission extends Permission
             return false;
 
         ServicePermission that = (ServicePermission) obj;
-        return ((this.mask & that.mask) == that.mask) &&
+        return (this.mask == that.mask) &&
             this.getName().equals(that.getName());
 
 
