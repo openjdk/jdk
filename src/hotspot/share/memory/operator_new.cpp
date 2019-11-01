5dafc279a7c602e8621485bfeccd622c1a29aa74
Kim Barrett <kbarrett@openjdk.org>
2019-11-01 16:21:17 -0400

    8233359: Add global sized operator delete definitions
    Added new definitions.
    
    Reviewed-by: dholmes

--- src/hotspot/share/memory/operator_new.cpp
+++ src/hotspot/share/memory/operator_new.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -87,6 +87,14 @@ void operator delete(void* p) throw() {
 
 void operator delete [](void* p) throw() {
   fatal("Should not call global delete []");
+}
+
+void operator delete(void* p, size_t size) throw() {
+  fatal("Should not call global sized delete");
+}
+
+void operator delete [](void* p, size_t size) throw() {
+  fatal("Should not call global sized delete []");
 }
 
 #endif // Non-product
