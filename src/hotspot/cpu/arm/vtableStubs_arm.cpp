8ca82e314797f6823474ac5c0dedd5b41297ace5
Markus Knetschke <markus.knetschke@gmail.com>
2019-11-05 18:42:10 -0500

    8233599: ARM32 Build failed due to 8232050 missing an include
    Reviewed-by: dholmes

--- src/hotspot/cpu/arm/vtableStubs_arm.cpp
+++ src/hotspot/cpu/arm/vtableStubs_arm.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,6 +32,7 @@
 #include "oops/compiledICHolder.hpp"
 #include "oops/instanceKlass.hpp"
 #include "oops/klassVtable.hpp"
+#include "oops/klass.inline.hpp"
 #include "runtime/sharedRuntime.hpp"
 #include "vmreg_arm.inline.hpp"
 #ifdef COMPILER2
