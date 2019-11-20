d5b423d17cfb081b380dc0a7cb8b64a5f471d354
Weijun Wang <weijun@openjdk.org>
2019-11-20 08:12:14 +0800

    8234377: new BitArray(0).toString() throws ArrayIndexOutOfBoundsException
    Reviewed-by: mullan, wetmore

--- src/java.base/share/classes/sun/security/util/BitArray.java
+++ src/java.base/share/classes/sun/security/util/BitArray.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2006, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -241,6 +241,10 @@ public class BitArray {
      *  Returns a string representation of this BitArray.
      */
     public String toString() {
+        if (length == 0) {
+            return "";
+        }
+
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
         for (int i = 0; i < repn.length - 1; i++) {
