bd1d7356a356faf6ac3fcc085ae24a1ca8063317
Roman Kennke <rkennke@openjdk.org>
2019-12-10 20:06:06 +0100

    8235598: Shenandoah: Update copyrights
    Reviewed-by: zgu

--- src/hotspot/share/gc/shenandoah/shenandoahSharedVariables.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahSharedVariables.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Red Hat, Inc. All rights reserved.
+ * Copyright (c) 2017, 2019, Red Hat, Inc. All rights reserved.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
