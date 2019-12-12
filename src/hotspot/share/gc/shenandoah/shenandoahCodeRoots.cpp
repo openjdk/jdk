8a54d97a4d78f7b9e2ab942159c8b1913957d905
Roman Kennke <rkennke@openjdk.org>
2019-12-12 18:34:30 +0100

    8234974: Shenandoah: Do concurrent roots even when no evacuation is necessary
    Reviewed-by: zgu

--- src/hotspot/share/gc/shenandoah/shenandoahCodeRoots.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahCodeRoots.cpp
@@ -155,7 +155,7 @@ void ShenandoahCodeRoots::flush_nmethod(nmethod* nm) {
   }
 }
 
-void ShenandoahCodeRoots::prepare_concurrent_unloading() {
+void ShenandoahCodeRoots::arm_nmethods() {
   assert(SafepointSynchronize::is_at_safepoint(), "Must be at a safepoint");
   _disarmed_value ++;
   // 0 is reserved for new nmethod
@@ -224,7 +224,9 @@ public:
 
     // Heal oops and disarm
     ShenandoahEvacOOMScope evac_scope;
-    ShenandoahNMethod::heal_nmethod(nm);
+    if (_heap->is_evacuation_in_progress()) {
+      ShenandoahNMethod::heal_nmethod(nm);
+    }
     ShenandoahNMethod::disarm_nmethod(nm);
 
     // Clear compiled ICs and exception caches
