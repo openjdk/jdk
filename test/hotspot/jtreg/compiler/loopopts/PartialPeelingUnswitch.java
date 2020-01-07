3e9a17c53e078ed199e78bfbbb61bf5e5ccd9172
Christian Hagedorn <chagedorn@openjdk.org>
2020-01-07 17:06:10 +0100

    8235984: C2: assert(out->in(PhiNode::Region) == head || out->in(PhiNode::Region) == slow_head) failed: phi must be either part of the slow or the fast loop
    Bailout from loop unswitching if loop predicates have a control dependency to partially peeled statements.
    
    Reviewed-by: neliasso, thartmann

--- test/hotspot/jtreg/compiler/loopopts/PartialPeelingUnswitch.java
+++ test/hotspot/jtreg/compiler/loopopts/PartialPeelingUnswitch.java
@@ -23,11 +23,12 @@
 
 /*
  * @test
- * @bug 8233033
- * @summary Tests if partially peeled statements are not executed before the loop predicates of the unswitched fast loop.
+ * @bug 8233033 8235984
+ * @summary Tests if partially peeled statements are not executed before the loop predicates by bailing out of loop unswitching.
  *
- * @run main/othervm -Xbatch -XX:-TieredCompilation
+ * @run main/othervm -Xbatch
  *      -XX:CompileCommand=compileonly,compiler.loopopts.PartialPeelingUnswitch::test*
+ *      -XX:CompileCommand=dontinline,compiler.loopopts.PartialPeelingUnswitch::dontInline
  *      compiler.loopopts.PartialPeelingUnswitch
  */
 
@@ -38,6 +39,7 @@ public class PartialPeelingUnswitch {
     public static int iFld;
     public static int x = 42;
     public static int y = 31;
+    public static int z = 22;
     public static int[] iArr = new int[10];
 
     public int test() {
@@ -46,8 +48,9 @@ public class PartialPeelingUnswitch {
          * of the cloned loop predicates for the fast loop (set up at unswitching stage). The only partially peeled
          * statement "iFld += 7" was wrongly executed before the predicates (and before the loop itself).
          * When hitting the uncommon trap, "iFld >>= 1" was not yet executed. As a result, the interpreter directly
-         * reexecuted "iFld += 7" again. This resulted in a wrong result for "iFld". The fix makes peeled statements
-         * control dependant on the cloned loop predicates such that they are executed after them.
+         * reexecuted "iFld += 7" again. This resulted in a wrong result for "iFld". The fix in 8233033 makes peeled
+         * statements control dependant on the cloned loop predicates such that they are executed after them. However,
+         * some cases are not handled properly. For now, the new fix in 8235984 just bails out of loop unswitching.
          */
         iFld = 13;
         for (int i = 0; i < 8; i++) {
@@ -103,16 +106,162 @@ public class PartialPeelingUnswitch {
         return iFld + k;
     }
 
+    public int test3() {
+        iFld = 13;
+        if (z < 34) {
+            z = 34;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            int j = 10;
+            while (--j > 0) {
+                iFld += -7;
+                iArr[5] = 8;
+                x = iArr[6];
+                y = x;
+                for (int k = 50; k < 51; k++) {
+                    x = iArr[7];
+                }
+                switch ((i * 5) + 102) {
+                case 120:
+                    return iFld;
+                case 103:
+                    break;
+                case 116:
+                    break;
+                default:
+                    if (iFld == -7) {
+                        return iFld;
+                    }
+                    z = iArr[5];
+                    iFld >>= 1;
+                }
+            }
+            iArr[5] = 34;
+            dontInline(iArr[5]);
+        }
+        return iFld;
+    }
+
+    public int test4() {
+        iFld = 13;
+        if (z < 34) {
+            z = 34;
+        }
+
+        for (int i = 0; i < 8; i++) {
+            int j = 10;
+            while (--j > 0) {
+                iFld += -7;
+                iArr[5] = 8;
+                x = iArr[6];
+                y = x;
+                for (int k = 50; k < 51; k++) {
+                    x = iArr[7];
+                }
+                switch ((i * 5) + 102) {
+                case 120:
+                    return iFld;
+                case 103:
+                    break;
+                case 116:
+                    break;
+                default:
+                    if (iFld == -7) {
+                        return iFld;
+                    }
+                    z = iArr[5];
+                    iFld >>= 1;
+                }
+            }
+            iArr[5] = 34;
+        }
+        return iFld;
+    }
+
+    public int test5() {
+        iFld = 13;
+        for (int i = 0; i < 8; i++) {
+            int j = 10;
+            while (--j > 0) {
+                iFld += -7;
+                iArr[5] = 8;
+                x = iArr[6];
+                y = x;
+                for (int k = 50; k < 51; k++) {
+                    x = iArr[7];
+                }
+                switch ((i * 5) + 102) {
+                case 120:
+                    return iFld;
+                case 103:
+                    break;
+                case 116:
+                    break;
+                default:
+                    iFld >>= 1;
+                }
+            }
+        }
+        return iFld;
+    }
+
+    public int test6() {
+        iFld = 13;
+        for (int i = 0; i < 8; i++) {
+            int j = 10;
+            while (--j > 0) {
+                iFld += -7;
+                iArr[5] = 8;
+                x = iArr[6];
+                y = x;
+                switch ((i * 5) + 102) {
+                case 120:
+                    return iFld;
+                case 103:
+                    break;
+                case 116:
+                    break;
+                default:
+                    iFld >>= 1;
+                }
+            }
+        }
+        return iFld;
+    }
+
+    public int test7() {
+        iFld = 13;
+        for (int i = 0; i < 8; i++) {
+            int j = 10;
+            while (--j > 0) {
+                iFld += -7;
+                iArr[5] = 8;
+                switch ((i * 5) + 102) {
+                case 120:
+                    return iFld;
+                case 103:
+                    break;
+                case 116:
+                    break;
+                default:
+                    iFld >>= 1;
+                }
+            }
+        }
+        return iFld;
+    }
+
     public static void main(String[] strArr) {
         PartialPeelingUnswitch _instance = new PartialPeelingUnswitch();
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 2000; i++) {
             int result = _instance.test();
             if (result != -7) {
                 throw new RuntimeException("Result should always be -7 but was " + result);
             }
         }
 
-        for (int i = 0; i < 200; i++) {
+        for (int i = 0; i < 2000; i++) {
             int result = _instance.test2();
             check(-1, result);
             check(-7, iFld);
@@ -129,6 +278,22 @@ public class PartialPeelingUnswitch {
             x = 42;
             y = 31;
         }
+
+        for (int i = 0; i < 2000; i++) {
+            _instance.test3();
+            _instance.test4();
+            _instance.test5();
+            _instance.test6();
+            _instance.test7();
+        }
+
+        for (int i = 0; i < 2000; i++) {
+            if (i % 2 == 0) {
+                z = 23;
+            }
+            _instance.test3();
+            _instance.test4();
+        }
     }
 
     public static void check(int expected, int actual) {
@@ -136,4 +301,6 @@ public class PartialPeelingUnswitch {
             throw new RuntimeException("Wrong result, expected: " + expected + ", actual: " + actual);
         }
     }
+
+    public void dontInline(int i) { }
 }
