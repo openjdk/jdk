89f2d145181880b2d168c81b68c9e02db3328d62
Thomas Schatzl <tschatzl@openjdk.org>
2020-01-08 14:36:48 +0100

    8235934: gc/g1/TestGCLogMessages.java fails with 'DerivedPointerTable Update' found
    Fix determining whether C2 or JVMCI are enabled in the test.
    
    Reviewed-by: sjohanss, kbarrett

--- test/lib/sun/hotspot/WhiteBox.java
+++ test/lib/sun/hotspot/WhiteBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -227,6 +227,8 @@ public class WhiteBox {
   public native void NMTArenaMalloc(long arena, long size);
 
   // Compiler
+  public native boolean isC2OrJVMCIIncludedInVmBuild();
+
   public native int     matchesMethod(Executable method, String pattern);
   public native int     matchesInline(Executable method, String pattern);
   public native boolean shouldPrintAssembly(Executable method, int comp_level);
