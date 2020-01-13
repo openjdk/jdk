b79d75d03376f3b7fb399aedd5a636bdc7db9e70
Alexandre Iline <shurailine@openjdk.org>
2020-01-13 14:09:30 -0800

    8237042: Switch to JCov build which supports byte code version 59
    Reviewed-by: erikj

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -1066,7 +1066,7 @@ var getJibProfilesDependencies = function (input, common) {
             // build_number: "b07",
             // file: "bundles/jcov-3_0.zip",
             organization: common.organization,
-            revision: "3.0-58-support+1.0",
+            revision: "3.0-59-support+1.0",
             ext: "zip",
             environment_name: "JCOV_HOME",
         },
