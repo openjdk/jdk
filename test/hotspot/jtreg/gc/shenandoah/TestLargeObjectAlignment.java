b63b9a2edb955cb754433c14872071d54582bc6e
Mikael Vidstedt <mikael@openjdk.org>
2020-01-14 19:27:03 -0800

    8237182: Update copyright header for shenandoah and epsilon files
    Reviewed-by: iignatyev, zgu

--- test/hotspot/jtreg/gc/shenandoah/TestLargeObjectAlignment.java
+++ test/hotspot/jtreg/gc/shenandoah/TestLargeObjectAlignment.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2016, 2018, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
