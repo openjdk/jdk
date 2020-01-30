83911960d41da124f7896a35be6acee7aeb09306
Per Lidén <pliden@openjdk.org>
2020-01-30 12:41:59 +0100

    8237884: ZGC: Use clamp() instead of MIN2(MAX2())
    Reviewed-by: kbarrett, smonteith

--- src/hotspot/share/gc/z/zHeuristics.cpp
+++ src/hotspot/share/gc/z/zHeuristics.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,6 +28,7 @@
 #include "logging/log.hpp"
 #include "runtime/globals.hpp"
 #include "runtime/os.hpp"
+#include "utilities/globalDefinitions.hpp"
 #include "utilities/powerOfTwo.hpp"
 
 void ZHeuristics::set_medium_page_size() {
@@ -38,7 +39,7 @@ void ZHeuristics::set_medium_page_size() {
   const size_t min = ZGranuleSize;
   const size_t max = ZGranuleSize * 16;
   const size_t unclamped = MaxHeapSize * 0.03125;
-  const size_t clamped = MIN2(MAX2(min, unclamped), max);
+  const size_t clamped = clamp(unclamped, min, max);
   const size_t size = round_down_power_of_2(clamped);
 
   if (size > ZPageSizeSmall) {
