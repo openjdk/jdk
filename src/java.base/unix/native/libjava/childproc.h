a59e8a7f6fed2accd1034eebf16545ef78c3ea1b
Patrick Zhang <patrick@os.amperecomputing.com>
2020-02-04 21:27:10 +0800

    8238380: java.base/unix/native/libjava/childproc.c "multiple definition" link errors with GCC10
    Reviewed-by: stuefe, clanger, rriggs

--- src/java.base/unix/native/libjava/childproc.h
+++ src/java.base/unix/native/libjava/childproc.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -126,7 +126,7 @@ typedef struct _SpawnInfo {
  * The cached and split version of the JDK's effective PATH.
  * (We don't support putenv("PATH=...") in native code)
  */
-const char * const *parentPathv;
+extern const char * const *parentPathv;
 
 ssize_t restartableWrite(int fd, const void *buf, size_t count);
 int restartableDup2(int fd_from, int fd_to);
