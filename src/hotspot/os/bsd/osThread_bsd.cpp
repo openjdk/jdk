080c67f0960fb43b15a2c6b476cff8573d5778ac
Gerard Ziemski <gziemski@openjdk.org>
2020-02-10 11:41:55 -0600

    8235962: os::current_thread_id() is not signal safe on macOS
    Use mach_thread_self instead of pthread_mach_thread_np
    
    Reviewed-by: dholmes, cjplummer

--- src/hotspot/os/bsd/osThread_bsd.cpp
+++ src/hotspot/os/bsd/osThread_bsd.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -36,17 +36,34 @@ void OSThread::pd_initialize() {
 #else
   _thread_id        = NULL;
 #endif
+  _unique_thread_id = 0;
   _pthread_id       = NULL;
-  _siginfo = NULL;
-  _ucontext = NULL;
-  _expanding_stack = 0;
-  _alt_sig_stack = NULL;
+  _siginfo          = NULL;
+  _ucontext         = NULL;
+  _expanding_stack  = 0;
+  _alt_sig_stack    = NULL;
 
   sigemptyset(&_caller_sigmask);
 
   _startThread_lock = new Monitor(Mutex::event, "startThread_lock", true,
                                   Monitor::_safepoint_check_never);
   assert(_startThread_lock !=NULL, "check");
+}
+
+// Additional thread_id used to correlate threads in SA
+void OSThread::set_unique_thread_id() {
+#ifdef __APPLE__
+  thread_identifier_info_data_t m_ident_info;
+  mach_msg_type_number_t count = THREAD_IDENTIFIER_INFO_COUNT;
+
+  mach_port_t mach_thread_port = mach_thread_self();
+  guarantee(mach_thread_port != 0, "just checking");
+  thread_info(mach_thread_port, THREAD_IDENTIFIER_INFO,
+              (thread_info_t) &m_ident_info, &count);
+  mach_port_deallocate(mach_task_self(), mach_thread_port);
+
+  _unique_thread_id = m_ident_info.thread_id;
+#endif
 }
 
 void OSThread::pd_destroy() {
