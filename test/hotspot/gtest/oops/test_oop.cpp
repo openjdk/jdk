96b61b8f4bcd493b56009efc4018b92f437db8c6
Kim Barrett <kbarrett@openjdk.org>
2020-03-02 14:45:52 -0500

    8240246: Avoid cast_to_oop from char*
    Change type of gtest object from char[] to unsigned char[].
    
    Reviewed-by: dholmes

--- test/hotspot/gtest/oops/test_oop.cpp
+++ test/hotspot/gtest/oops/test_oop.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,7 +26,7 @@
 #include "unittest.hpp"
 #include "utilities/globalDefinitions.hpp"
 
-static char memory[32];
+static unsigned char memory[32];
 
 oop fake_object() {
   return cast_to_oop(memory);
