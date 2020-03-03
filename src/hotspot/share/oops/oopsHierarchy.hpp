3ca275fb9182d507081196b6ae352d48066417f4
Coleen Phillimore <coleenp@openjdk.org>
2020-03-03 11:19:35 -0500

    8225760: oop::raw_set_obj isn't needed
    Reviewed-by: hseigel, rkennke

--- src/hotspot/share/oops/oopsHierarchy.hpp
+++ src/hotspot/share/oops/oopsHierarchy.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,10 +82,9 @@ class oop {
 
 public:
   void set_obj(const void* p)         {
-    raw_set_obj(p);
+    _o = (oopDesc*)p;
     if (CheckUnhandledOops) register_oop();
   }
-  void raw_set_obj(const void* p)     { _o = (oopDesc*)p; }
 
   oop()                               { set_obj(NULL); }
   oop(const oop& o)                   { set_obj(o.obj()); }
