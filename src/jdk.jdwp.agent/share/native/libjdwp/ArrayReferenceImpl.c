3adad5a6451e185df59ce6ebd381300da056e757
Martin Doerr <mdoerr@openjdk.org>
2020-03-06 11:04:31 +0100

    8239856: [ntintel] asserts about copying unaligned array element
    Reviewed-by: stuefe, sspitsyn

--- src/jdk.jdwp.agent/share/native/libjdwp/ArrayReferenceImpl.c
+++ src/jdk.jdwp.agent/share/native/libjdwp/ArrayReferenceImpl.c
@@ -413,6 +413,9 @@ readLongComponents(JNIEnv *env, PacketInputStream *in,
                    jarray array, int index, int length)
 {
     int i;
+#if defined (_WIN32) && defined (_MSC_VER)
+    __declspec(align(8))
+#endif
     jlong component;
 
     for (i = 0; (i < length) && !inStream_error(in); i++) {
@@ -441,6 +444,9 @@ readDoubleComponents(JNIEnv *env, PacketInputStream *in,
                    jarray array, int index, int length)
 {
     int i;
+#if defined (_WIN32) && defined (_MSC_VER)
+    __declspec(align(8))
+#endif
     jdouble component;
 
     for (i = 0; (i < length) && !inStream_error(in); i++) {
