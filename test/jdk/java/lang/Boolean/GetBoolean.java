d7780973716618b7e88e864c9a04fb262c7a44c2
Vipin Sharma <vipinsharma85@gmail.com>
2020-03-11 13:50:13 +0100

    8240524: Remove explicit type argument in test jdk/java/lang/Boolean/MakeBooleanComparable.java
    Reviewed-by: clanger, vtewari

--- test/jdk/java/lang/Boolean/GetBoolean.java
+++ test/jdk/java/lang/Boolean/GetBoolean.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,7 @@
  */
 
 public class GetBoolean {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Boolean.getBoolean("");
         Boolean.getBoolean(null);
     }
