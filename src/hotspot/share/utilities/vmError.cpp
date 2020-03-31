fc806b67063fb23c7cd2b3ecdf85d13b42e45448
Matthias Baesken <mbaesken@openjdk.org>
2020-03-31 17:11:30 +0200

    8241948: enhance list of environment variables printed in hs_err file
    Reviewed-by: dholmes, hseigel

--- src/hotspot/share/utilities/vmError.cpp
+++ src/hotspot/share/utilities/vmError.cpp
@@ -86,6 +86,7 @@ const char *env_list[] = {
   // Env variables that are defined on Solaris/Linux/BSD
   "LD_LIBRARY_PATH", "LD_PRELOAD", "SHELL", "DISPLAY",
   "HOSTTYPE", "OSTYPE", "ARCH", "MACHTYPE",
+  "LANG", "LC_ALL", "LC_CTYPE", "TZ",
 
   // defined on AIX
   "LIBPATH", "LDR_PRELOAD", "LDR_PRELOAD64",
