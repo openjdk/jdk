3ae56fdf103cc044d6b28b2f485beed3a500472c
Tom Rodriguez <never@openjdk.org>
2020-04-02 11:20:53 -0700

    8191930: [Graal] emits unparseable XML into compile log
    Reviewed-by: kvn

--- src/hotspot/share/compiler/compileTask.cpp
+++ src/hotspot/share/compiler/compileTask.cpp
@@ -393,7 +393,10 @@ void CompileTask::log_task_done(CompileLog* log) {
   if (!_is_success) {
     assert(_failure_reason != NULL, "missing");
     const char* reason = _failure_reason != NULL ? _failure_reason : "unknown";
-    log->elem("failure reason='%s'", reason);
+    log->begin_elem("failure reason='");
+    log->text("%s", reason);
+    log->print("'");
+    log->end_elem();
   }
 
   // <task_done ... stamp='1.234'>  </task>
