b7d5172b8941cca226d09ee69fbbaaf68a750219
Jim Laskey <jlaskey@openjdk.org>
2020-04-09 16:32:22 -0300

    8242472: Comment for unused PreviewFeature.Feature.TEXT_BLOCKS enum
    Reviewed-by: abuckley

--- src/java.base/share/classes/jdk/internal/PreviewFeature.java
+++ src/java.base/share/classes/jdk/internal/PreviewFeature.java
@@ -55,7 +55,13 @@ public @interface PreviewFeature {
 
     public enum Feature {
         PATTERN_MATCHING_IN_INSTANCEOF,
-        TEXT_BLOCKS, // 8242284: needs to be removed after JDK 15
+        // 8242284:
+        // The TEXT_BLOCKS enum constant is not used in the JDK 15 codebase, but
+        // exists to support the bootcycle build of JDK 15. The bootcycle build
+        // of JDK 15 is performed with JDK 14 and the PreviewFeature type from
+        // JDK 15. Since the JDK 14 codebase uses the enum constant, it is
+        // necessary for PreviewFeature in JDK 15 to declare the enum constant.
+        TEXT_BLOCKS,
         RECORDS,
         ;
     }
