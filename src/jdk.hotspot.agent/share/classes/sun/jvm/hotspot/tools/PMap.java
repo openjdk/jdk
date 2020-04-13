4a09f31eff40884b5e8aa5fabf9ad4f55cd8ed6f
Chris Plummer <cjplummer@openjdk.org>
2020-04-13 16:30:11 -0700

    8237250: pmap and pstack should do a better of making it clear that they are not supported on Mac OS X
    Reviewed-by: amenkov, sspitsyn

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/PMap.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/PMap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,6 +28,7 @@ import java.io.*;
 import java.util.*;
 import sun.jvm.hotspot.debugger.*;
 import sun.jvm.hotspot.debugger.cdbg.*;
+import sun.jvm.hotspot.utilities.PlatformInfo;
 
 public class PMap extends Tool {
 
@@ -53,6 +54,11 @@ public class PMap extends Tool {
    }
 
    public void run(PrintStream out, Debugger dbg) {
+      if (PlatformInfo.getOS().equals("darwin")) {
+        out.println("Not available on Mac OS X");
+        return;
+      }
+
       CDebugger cdbg = dbg.getCDebugger();
       if (cdbg != null) {
          List l = cdbg.getLoadObjectList();
--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/PStack.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/PStack.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -60,7 +60,7 @@ public class PStack extends Tool {
 
    public void run(PrintStream out, Debugger dbg) {
       if (PlatformInfo.getOS().equals("darwin")) {
-        out.println("Not available on Darwin");
+        out.println("Not available on Mac OS X");
         return;
       }
 
