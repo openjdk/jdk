efa570126b64a5a662d57458bd7845dd4c56bf64
Lois Foltan <lfoltan@openjdk.org>
2020-04-21 17:20:08 +0000

    8243045: AOTCompiledMethod::print_on triggers assertion after JDK-8210012
    Fix outputStream time stamp issue when AOT and -Xlog:all=trace are specified together.
    
    Reviewed-by: coleenp, hseigel, kvn

--- src/hotspot/share/aot/aotCompiledMethod.cpp
+++ src/hotspot/share/aot/aotCompiledMethod.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -313,7 +313,7 @@ void AOTCompiledMethod::print_on(outputStream* st) const {
 void AOTCompiledMethod::print_on(outputStream* st, const char* msg) const {
   if (st != NULL) {
     ttyLocker ttyl;
-    st->print("%7d ", (int) st->time_stamp().milliseconds());
+    st->print("%7d ", (int) tty->time_stamp().milliseconds());
     st->print("%4d ", _aot_id);    // print compilation number
     st->print("    aot[%2d]", _heap->dso_id());
     // Stubs have _method == NULL
