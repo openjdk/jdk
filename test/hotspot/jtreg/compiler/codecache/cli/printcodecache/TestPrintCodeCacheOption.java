9687723c819828c2de69b4d4be67ea59f17608b1
Igor Ignatyev <iignatyev@openjdk.org>
2020-04-28 11:33:37 -0700

    8243932: compiler/codecache/cli/printcodecache/TestPrintCodeCacheOption.java test can use driver mode
    Reviewed-by: thartmann

--- test/hotspot/jtreg/compiler/codecache/cli/printcodecache/TestPrintCodeCacheOption.java
+++ test/hotspot/jtreg/compiler/codecache/cli/printcodecache/TestPrintCodeCacheOption.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,7 @@
  *          java.management
  *          jdk.internal.jvmstat/sun.jvmstat.monitor
  *
- * @run main/timeout=240 compiler.codecache.cli.printcodecache.TestPrintCodeCacheOption
+ * @run driver/timeout=240 compiler.codecache.cli.printcodecache.TestPrintCodeCacheOption
  */
 
 package compiler.codecache.cli.printcodecache;
