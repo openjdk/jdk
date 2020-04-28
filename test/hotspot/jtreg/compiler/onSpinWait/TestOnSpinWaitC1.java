a740f83da3f3cd9238727496fc9e110c2c8733da
Igor Ignatyev <iignatyev@openjdk.org>
2020-04-27 17:58:27 -0700

    8243930: update copyright years
    Reviewed-by: dholmes

--- test/hotspot/jtreg/compiler/onSpinWait/TestOnSpinWaitC1.java
+++ test/hotspot/jtreg/compiler/onSpinWait/TestOnSpinWaitC1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2016 Azul Systems, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
