0efacb3eedf79dceb6aa185057df2658ce899723
Alan Bateman <alanb@openjdk.org>
2020-05-04 19:07:41 +0100

    8244292: Headful clients failing with --illegal-access=deny
    Reviewed-by: prr

--- test/jdk/java/awt/Toolkit/DisplayChangesException/DisplayChangesException.java
+++ test/jdk/java/awt/Toolkit/DisplayChangesException/DisplayChangesException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,6 +39,7 @@ import sun.awt.SunToolkit;
  * @bug 8207070
  * @modules java.desktop/sun.java2d
  *          java.desktop/sun.awt
+ *          java.desktop/sun.awt.windows:open
  */
 public final class DisplayChangesException {
 
