ba59fe95037a9f98d66d53b1372e5857e57ba48d
Roman Kennke <rkennke@openjdk.org>
2020-05-12 16:12:37 +0200

    8244813: [BACKOUT] 8244523: Shenandoah: Remove null-handling in LRB expansion
    Reviewed-by: shade

--- src/hotspot/share/gc/shenandoah/c2/shenandoahSupport.hpp
+++ src/hotspot/share/gc/shenandoah/c2/shenandoahSupport.hpp
@@ -63,6 +63,9 @@ private:
   static void call_lrb_stub(Node*& ctrl, Node*& val, Node* load_addr, Node*& result_mem, Node* raw_mem, bool is_native, PhaseIdealLoop* phase);
   static void test_in_cset(Node*& ctrl, Node*& not_cset_ctrl, Node* val, Node* raw_mem, PhaseIdealLoop* phase);
   static void move_gc_state_test_out_of_loop(IfNode* iff, PhaseIdealLoop* phase);
+  static Node* clone_null_check(Node*& c, Node* val, Node* unc_ctrl, PhaseIdealLoop* phase);
+  static void fix_null_check(Node* unc, Node* unc_ctrl, Node* new_unc_ctrl, Unique_Node_List& uses,
+                             PhaseIdealLoop* phase);
   static void merge_back_to_back_tests(Node* n, PhaseIdealLoop* phase);
   static bool identical_backtoback_ifs(Node *n, PhaseIdealLoop* phase);
   static void fix_ctrl(Node* barrier, Node* region, const MemoryGraphFixer& fixer, Unique_Node_List& uses, Unique_Node_List& uses_to_ignore, uint last, PhaseIdealLoop* phase);
@@ -251,6 +254,7 @@ public:
   virtual bool cmp( const Node &n ) const;
 
   bool is_redundant();
+  CallStaticJavaNode* pin_and_expand_null_check(PhaseIterGVN& igvn);
 
 private:
   bool needs_barrier(PhaseGVN* phase, Node* n);
