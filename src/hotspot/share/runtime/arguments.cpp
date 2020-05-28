919f10c4076b7bf4d0d4ef717649aafae8155336
Mikael Vidstedt <mikael@openjdk.org>
2020-05-28 09:08:53 -0700

    8245864: Obsolete BranchOnRegister
    Reviewed-by: thartmann, kvn

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -564,6 +564,7 @@ static SpecialFlag const special_jvm_flags[] = {
   { "UseBarriersForVolatile",        JDK_Version::undefined(), JDK_Version::jdk(15), JDK_Version::jdk(16) },
 #endif
   { "UseLWPSynchronization",         JDK_Version::undefined(), JDK_Version::jdk(15), JDK_Version::jdk(16) },
+  { "BranchOnRegister",              JDK_Version::undefined(), JDK_Version::jdk(15), JDK_Version::jdk(16) },
 
 #ifdef TEST_VERIFY_SPECIAL_JVM_FLAGS
   // These entries will generate build errors.  Their purpose is to test the macros.
