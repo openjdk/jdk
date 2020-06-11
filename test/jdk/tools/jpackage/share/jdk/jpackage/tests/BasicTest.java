259662fe77a8a3c4a90f3489046dc84696c88c29
Alexey Semenyuk <asemenyuk@openjdk.org>
2020-06-10 20:45:28 -0400

    8247353: jtreg tests minor issues clean up
    Reviewed-by: herrick, almatvee

--- test/jdk/tools/jpackage/share/jdk/jpackage/tests/BasicTest.java
+++ test/jdk/tools/jpackage/share/jdk/jpackage/tests/BasicTest.java
@@ -227,7 +227,8 @@ public final class BasicTest {
     @Parameter({ "java.desktop", "jdk.jartool" })
     public void testAddModules(String... addModulesArg) {
         JPackageCommand cmd = JPackageCommand
-                .helloAppImage("goodbye.jar:com.other/com.other.Hello");
+                .helloAppImage("goodbye.jar:com.other/com.other.Hello")
+                .ignoreDefaultRuntime(true); // because of --add-modules
         Stream.of(addModulesArg).map(v -> Stream.of("--add-modules", v)).flatMap(
                 s -> s).forEachOrdered(cmd::addArgument);
         cmd.executeAndAssertHelloAppImageCreated();
@@ -270,10 +271,6 @@ public final class BasicTest {
         .run(PackageTest.Action.CREATE);
 
         createTest.get()
-        .addInitializer(cmd -> {
-            // Clean output from the previus jpackage run.
-            Files.delete(cmd.outputBundle());
-        })
         // Temporary directory should not be empty,
         // jpackage should exit with error.
         .setExpectedExitCode(1)
