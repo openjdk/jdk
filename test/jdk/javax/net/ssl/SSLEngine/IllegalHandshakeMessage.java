1faed205dfbc47def8fa9eb5db4dc337db53b9dc
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2020-06-12 15:55:08 -0700

    8247510: typo in IllegalHandshakeMessage
    Reviewed-by: wetmore

--- test/jdk/javax/net/ssl/SSLEngine/IllegalHandshakeMessage.java
+++ test/jdk/javax/net/ssl/SSLEngine/IllegalHandshakeMessage.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,7 +70,7 @@ public class IllegalHandshakeMessage {
             cliToSrv.put(7, (byte)0x80);    // use illegal message length
         } else {
             // unlikely
-            throw new Exception("No handshage message generated.");
+            throw new Exception("No handshake message is generated.");
         }
 
         try {
