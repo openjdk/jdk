fc82a465d3138b395ed18dff7f3ecc83dbc0cf62
Lance Andersen <lancea@openjdk.org>
2020-06-26 13:37:43 -0400

    8248412: test/jdk/java/sql/testng/test/sql/DriverManagerPermissionsTests.java can fail
    Reviewed-by: naoto, dfuchs

--- test/jdk/java/sql/testng/test/sql/othervm/DriverManagerPermissionsTests.java
+++ test/jdk/java/sql/testng/test/sql/othervm/DriverManagerPermissionsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package test.sql;
+package test.sql.othervm;
 
 import java.security.AccessControlException;
 import java.security.Policy;
