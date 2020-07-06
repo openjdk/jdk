45fce6baf9da7698e1dc540cda1ce704bf2cf2df
John Jiang <jjiang@openjdk.org>
2020-07-07 06:06:18 +0800

    8242538: java/security/SecureRandom/ThreadSafe.java failed on windows
    Reviewed-by: mullan

--- test/jdk/java/security/SecureRandom/ThreadSafe.java
+++ test/jdk/java/security/SecureRandom/ThreadSafe.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -87,7 +87,7 @@ public class ThreadSafe {
             }
             inCall = true;
             try {
-                Thread.sleep(100);
+                Thread.sleep(500);
             } catch (Exception e) {
                 // OK
             }
