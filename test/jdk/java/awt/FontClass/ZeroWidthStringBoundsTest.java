3ec5f6b3741c9229d133ff9978586788765f8b8f
Phil Race <prr@openjdk.org>
2020-07-20 08:13:02 -0700

    8249725: testbug: ZeroWithStringBoundsTest.java needs update to copyright header
    Reviewed-by: jdv

--- test/jdk/java/awt/FontClass/ZeroWidthStringBoundsTest.java
+++ test/jdk/java/awt/FontClass/ZeroWidthStringBoundsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates.
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8245159
+ * @bug 8245159 8239725
  * @summary Ensure no exception getting bounds of an empty string when the font has
  * layout attributes.
  * @run main/othervm ZeroWidthStringBoundsTest
