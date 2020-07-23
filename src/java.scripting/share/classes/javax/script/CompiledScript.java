5c8a154e93c5afc3563a235b6b6a5b148eb5dc4a
Joe Darcy <darcy@openjdk.org>
2020-07-23 15:13:08 -0700

    8250237: Address use of default constructors in the javax.script package
    Reviewed-by: lancea, psandoz

--- src/java.scripting/share/classes/javax/script/CompiledScript.java
+++ src/java.scripting/share/classes/javax/script/CompiledScript.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -42,6 +42,10 @@ import java.util.Map;
  * @since 1.6
  */
 public abstract class CompiledScript {
+    /**
+     * Constructor for subclasses to call.
+     */
+    public CompiledScript() {}
 
     /**
      * Executes the program stored in this <code>CompiledScript</code> object.
