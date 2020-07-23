1b1c1cd7b94e21bbcfcf2cd1b930c7ec02781413
David Holmes <dholmes@openjdk.org>
2020-07-23 00:47:02 -0400

    8249940: Remove unnecessary includes of jni_util.h in native tests
    Reviewed-by: iignatyev, mchung

--- test/hotspot/jtreg/runtime/jni/FindClass/libbootLoaderTest.c
+++ test/hotspot/jtreg/runtime/jni/FindClass/libbootLoaderTest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,6 @@
 #include <stdlib.h>
 
 #include "jni.h"
-#include "jni_util.h"
 
 JNIEXPORT jclass JNICALL
 Java_java_lang_BootNativeLibrary_findClass
