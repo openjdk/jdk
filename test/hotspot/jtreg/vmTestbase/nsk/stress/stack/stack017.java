9b73fb125d710815187860ecea20fc1e77588a3e
Harold Seigel <hseigel@openjdk.org>
2020-07-24 18:44:27 +0000

    8250557: Rename vmTestbase/nsk shared timeout handler package to Terminator.java
    Rename the class and fix the tests that use it.
    
    Reviewed-by: dcubed

--- test/hotspot/jtreg/vmTestbase/nsk/stress/stack/stack017.java
+++ test/hotspot/jtreg/vmTestbase/nsk/stress/stack/stack017.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -50,7 +50,7 @@
 package nsk.stress.stack;
 
 
-import nsk.share.Harakiri;
+import nsk.share.Terminator;
 
 import java.io.PrintStream;
 
@@ -73,7 +73,7 @@ public class stack017 extends Thread {
             else if (args[i].toLowerCase().equals("-eager"))
                 eager = true;
         if (!eager)
-            Harakiri.appoint(Harakiri.parseAppointment(args));
+            Terminator.appoint(Terminator.parseAppointment(args));
         stack017.out = out;
         stack017 test = new stack017();
         return test.doRun();
