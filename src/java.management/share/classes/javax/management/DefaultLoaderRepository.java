dfd3080c056370724a8cecc257af6ea6c9272042
Joe Darcy <darcy@openjdk.org>
2020-07-25 12:49:09 -0700

    8250583: Address reliance on default constructors in java.management
    Reviewed-by: alanb

--- src/java.management/share/classes/javax/management/DefaultLoaderRepository.java
+++ src/java.management/share/classes/javax/management/DefaultLoaderRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -50,6 +50,11 @@ import javax.management.loading.ClassLoaderRepository;
  */
 @Deprecated
 public class DefaultLoaderRepository {
+    /**
+     * Constructs an {@code DefaultLoaderRepository}.
+     */
+    public DefaultLoaderRepository() {}
+
     /**
      * Go through the list of class loaders and try to load the requested class.
      * The method will stop as soon as the class is found. If the class
