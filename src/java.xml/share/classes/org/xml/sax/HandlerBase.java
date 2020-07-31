024fa0969a85b180cc7460a4c181bde7ee6cca24
Joe Wang <joehw@openjdk.org>
2020-07-31 18:25:12 +0000

    8250638: Address reliance on default constructors in java.xml
    Reviewed-by: darcy, lancea

--- src/java.xml/share/classes/org/xml/sax/HandlerBase.java
+++ src/java.xml/share/classes/org/xml/sax/HandlerBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -58,7 +58,10 @@ package org.xml.sax;
 public class HandlerBase
     implements EntityResolver, DTDHandler, DocumentHandler, ErrorHandler
 {
-
+    /**
+     * Constructs a {@code HandlerBase}.
+     */
+    public HandlerBase() {}
 
     ////////////////////////////////////////////////////////////////////
     // Default implementation of the EntityResolver interface.
