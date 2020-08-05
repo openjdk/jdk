3a0741afa1062982f660916d41a36ed43b48b236
Joe Darcy <darcy@openjdk.org>
2020-08-04 20:31:57 -0700

    8250920: Increase @jls usage in core reflection
    Reviewed-by: mchung

--- src/java.base/share/classes/java/lang/reflect/AnnotatedWildcardType.java
+++ src/java.base/share/classes/java/lang/reflect/AnnotatedWildcardType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,6 +30,7 @@ package java.lang.reflect;
  * wildcard type argument, whose upper or lower bounds may themselves represent
  * annotated uses of types.
  *
+ * @jls 4.5.1 Type Arguments of Parameterized Types
  * @since 1.8
  */
 public interface AnnotatedWildcardType extends AnnotatedType {
--- src/java.base/share/classes/java/lang/reflect/WildcardType.java
+++ src/java.base/share/classes/java/lang/reflect/WildcardType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,6 +29,7 @@ package java.lang.reflect;
  * WildcardType represents a wildcard type expression, such as
  * {@code ?}, {@code ? extends Number}, or {@code ? super Integer}.
  *
+ * @jls 4.5.1 Type Arguments of Parameterized Types
  * @since 1.5
  */
 public interface WildcardType extends Type {
