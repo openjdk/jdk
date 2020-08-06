cfb02d4854038776f48f351a290099eb3afe11e3
Tobias Hartmann <thartmann@openjdk.org>
2020-08-06 08:10:56 +0200

    8250861: Crash in MinINode::Ideal(PhaseGVN*, bool)
    Added missing NULL checks.
    
    Reviewed-by: kvn, chagedorn

--- src/hotspot/share/opto/addnode.cpp
+++ src/hotspot/share/opto/addnode.cpp
@@ -1078,7 +1078,7 @@ Node *MinINode::Ideal(PhaseGVN *phase, bool can_reshape) {
 
     // Transform MIN2(x + c0, MIN2(x + c1, z)) into MIN2(x + MIN2(c0, c1), z)
     // if x == y and the additions can't overflow.
-    if (phase->eqv(x,y) &&
+    if (phase->eqv(x,y) && tx != NULL &&
         !can_overflow(tx, x_off) &&
         !can_overflow(tx, y_off)) {
       return new MinINode(phase->transform(new AddINode(x, phase->intcon(MIN2(x_off, y_off)))), r->in(2));
@@ -1086,7 +1086,7 @@ Node *MinINode::Ideal(PhaseGVN *phase, bool can_reshape) {
   } else {
     // Transform MIN2(x + c0, y + c1) into x + MIN2(c0, c1)
     // if x == y and the additions can't overflow.
-    if (phase->eqv(x,y) &&
+    if (phase->eqv(x,y) && tx != NULL &&
         !can_overflow(tx, x_off) &&
         !can_overflow(tx, y_off)) {
       return new AddINode(x,phase->intcon(MIN2(x_off,y_off)));
