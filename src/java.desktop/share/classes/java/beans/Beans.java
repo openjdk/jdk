31480720790cc68cd975de79f0c98ae5ca45c7b7
Sergey Bylokhov <serb@openjdk.org>
2020-08-11 05:34:33 +0100

    8250857: Address reliance on default constructors in the Java Beans API
    Reviewed-by: prr

--- src/java.desktop/share/classes/java/beans/Beans.java
+++ src/java.desktop/share/classes/java/beans/Beans.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -58,6 +58,11 @@ import java.util.Vector;
  */
 
 public class Beans {
+
+    /**
+     * Constructs a {@code Beans}.
+     */
+    public Beans() {}
 
     /**
      * <p>
