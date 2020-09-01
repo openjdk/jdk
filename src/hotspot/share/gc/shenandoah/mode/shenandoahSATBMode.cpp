4000d5975c487fa0c2c76747696b8f9b37e4e12c
Aleksey Shipilev <shade@openjdk.org>
2020-09-01 07:06:18 +0200

    8252592: Non-PCH build is broken after JDK-8251560
    Reviewed-by: zgu, iklam

--- src/hotspot/share/gc/shenandoah/mode/shenandoahSATBMode.cpp
+++ src/hotspot/share/gc/shenandoah/mode/shenandoahSATBMode.cpp
@@ -31,6 +31,7 @@
 #include "gc/shenandoah/mode/shenandoahSATBMode.hpp"
 #include "logging/log.hpp"
 #include "logging/logTag.hpp"
+#include "runtime/java.hpp"
 
 void ShenandoahSATBMode::initialize_flags() const {
   if (ShenandoahConcurrentRoots::can_do_concurrent_class_unloading()) {
