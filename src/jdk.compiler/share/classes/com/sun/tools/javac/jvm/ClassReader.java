e19022e515c2e07cb6aa4d356a1195f0b1176af1
Vicente Romero <vromero@openjdk.org>
2020-09-01 14:31:18 -0400

    8230776: Javac throws AssertionError in jvm.Gen.visitExec 8231311: javac throws NPE in TransTypes.retype 8230964: Javac throws AssertionError in PoolReader.resolve 8230963: javac throws j.l.CCE in PoolReader.getName 8230919: javac throws j.l.CCE in ClassReader.readClass
    Reviewed-by: jlahoda

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/ClassReader.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/ClassReader.java
@@ -2214,6 +2214,7 @@ public class ClassReader {
                                    Integer.toString(minorVersion));
             }
         }
+        validateMethodType(name, type);
         if (name == names.init && currentOwner.hasOuterInstance()) {
             // Sometimes anonymous classes don't have an outer
             // instance, however, there is no reliable way to tell so
@@ -2241,6 +2242,7 @@ public class ClassReader {
         } finally {
             currentOwner = prevOwner;
         }
+        validateMethodType(name, m.type);
         setParameters(m, type);
 
         if ((flags & VARARGS) != 0) {
@@ -2252,6 +2254,13 @@ public class ClassReader {
         }
 
         return m;
+    }
+
+    void validateMethodType(Name name, Type t) {
+        if ((!t.hasTag(TypeTag.METHOD) && !t.hasTag(TypeTag.FORALL)) ||
+            (name == names.init && !t.getReturnType().hasTag(TypeTag.VOID))) {
+            throw badClassFile("method.descriptor.invalid", name);
+        }
     }
 
     private List<Type> adjustMethodParams(long flags, List<Type> args) {
@@ -2478,6 +2487,9 @@ public class ClassReader {
                         Integer.toString(minorVersion));
             }
             c.flags_field = flags;
+            if (c.owner.kind != MDL) {
+                throw badClassFile("module.info.definition.expected");
+            }
             currentModule = (ModuleSymbol) c.owner;
             int this_class = nextChar();
             // temp, no check on this_class
