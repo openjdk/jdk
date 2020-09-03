e3a3ea4bb7b7f77d1f08ac2ec2778d8cb2919359
Jan Lahoda <jlahoda@openjdk.org>
2020-09-03 11:13:46 +0200

    8160601: unexpected error compiling @Deprecated package
    Reviewed-by: jlaskey

--- src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -3489,7 +3489,7 @@ public class JavacParser implements Parser {
             List<JCAnnotation> annotations = List.nil();
             seenPackage = true;
             if (mods != null) {
-                checkNoMods(mods.flags);
+                checkNoMods(mods.flags & ~Flags.DEPRECATED);
                 annotations = mods.annotations;
                 mods = null;
             }
