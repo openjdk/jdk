35ea00085f8d72aa312cc84b4446a9b8705590c8
Dmitry Chuyko <dchuyko@openjdk.org>
2020-09-03 14:54:43 +0300

    8251525: AARCH64: Faster Math.signum(fp)
    Reviewed-by: aph, vlivanov, adinn

--- src/hotspot/share/runtime/vmStructs.cpp
+++ src/hotspot/share/runtime/vmStructs.cpp
@@ -1842,6 +1842,10 @@ typedef HashtableEntry<InstanceKlass*, mtClass>  KlassHashtableEntry;
   declare_c2_type(OverflowMulLNode, OverflowLNode)                        \
   declare_c2_type(FmaDNode, Node)                                         \
   declare_c2_type(FmaFNode, Node)                                         \
+  declare_c2_type(CopySignDNode, Node)                                    \
+  declare_c2_type(CopySignFNode, Node)                                    \
+  declare_c2_type(SignumDNode, Node)                                      \
+  declare_c2_type(SignumFNode, Node)                                      \
                                                                           \
   /*********************/                                                 \
   /* Adapter Blob Entries */                                              \
