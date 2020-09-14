d3256553428a22af02b1a52685bdf0e3fd310b42
Conor Cleary <conor.cleary@oracle.com>
2020-09-14 15:16:32 +0100

    8250855: Address reliance on default constructors in the Java 2D APIs

--- src/java.desktop/share/classes/java/awt/font/LineMetrics.java
+++ src/java.desktop/share/classes/java/awt/font/LineMetrics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2003, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,6 +43,10 @@ package java.awt.font;
 
 public abstract class LineMetrics {
 
+    /**
+     * Constructor for subclasses to call.
+     */
+    protected LineMetrics() {}
 
     /**
      * Returns the number of characters ({@code char} values) in the text whose
