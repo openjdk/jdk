12a244ffdfa81a234a5e8458204a5affc4196876
Ian Graves <ian.graves@oracle.com>
2020-09-15 08:35:31 -0500

    Comparator cleanup

--- src/jdk.jlink/share/classes/jdk/tools/jlink/internal/ImageFileCreator.java
+++ src/jdk.jlink/share/classes/jdk/tools/jlink/internal/ImageFileCreator.java
@@ -38,7 +38,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.Comparator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
