cd2bfb684c6457ae5c32a9b808286a29ad117272
Corey Ashford <cjashfor@linux.vnet.ibm.com>
2020-09-14 20:16:34 -0500

    Expand the Base64 intrinsic regression test to cover decodeBlock
    This patch makes four significant changes:
    
    1) The Power implementation of the decodeBlock intrinsic, at least,
    requires a decode length of at least 128 bytes, but the existing test cases
    are much shorter, maxing out at 111 bytes.  So the patch adds a new input
    data file which has longer test cases in it.
    
    2) The original test cases only covers the encoding of just the printable
    subset of the 7-bit ASCII characters.  However, Base64 encoding requires
    being able to encode arbitrary binary data, i.e. it must handle all 256
    8-bit byte encodings.  To remedy this, but keep the original line-oriented
    style of the input data, I added another input file type that uses a simple
    ASCII hexadecimal encoding - two ASCII hex characters per 8-bit byte.  When
    test0 is called, a new parameter is passed that specifies the type of the
    input file, which is either the original ASCII type or the hexadecimal
    format.  So to test both longer input data and arbitrary 8-bit data, the
    newly added input test file has test cases which are both longer and
    encoded in ASCII hex so as to give full 8-bit capability.  When reading
    this type of file, test0 calls a newly-added function to translate the
    ASCII hex to binary data.  Except for the first line of input data, which
    contains all possible 8-bit values sequentially, the input data was
    generated using a random length (between 111 and 520 bytes) buffer filled
    with random 8-bit data, which should give adequate coverage.
    
    3) The original test did not test that the decoder detects illegal Base64
    bytes.  This change chooses a random location in the encoded data to
    corrupt with a randomly-chosen byte which is illegal for the specific
    Base64 encoding that is chosen (i.e. standard or URLsafe).  It then calls
    the decode function to verify that the illegal byte is detected and the
    proper exception is thrown.
    
    4) The test iteration count was originally 100K, but that is far more than
    enough iterations to test the intrinsic.  It takes 20K iterations on each
    instrinsic for HotSpot C2 to begin calling it.  The test originally had
    three types of encodings to test and called the encode intrinsic four times
    for each iteration, which works out to 100K * 3 * 4 = 1.2M calls just to
    encode.  Decode was called four times as well (now five because of the
    illegal byte test).  I believe this is excessive and with the extra test
    data I have added, the test was timing out after ten minutes of execution.
    It appears that it is timing out, not because the intrinsics take a long
    time to run, but because test0 generates an enormous number of discarded
    data buffers for the GC system to recover (the test runs at about 39GB of
    virtual memory on my test machine).  To remedy the timeout problem, I have
    changed the code so that a warmup function of 20K repetitions is performed
    on a fixed buffer, to activate the instrinsic(s).  After the warmup, I have
    reduced the number of iterations to 5K on each test0 call.  This should
    give adequate coverage.

--- test/hotspot/jtreg/compiler/intrinsics/base64/TestBase64.java
+++ test/hotspot/jtreg/compiler/intrinsics/base64/TestBase64.java
@@ -47,6 +47,7 @@ import java.util.Base64;
 import java.util.Base64.Decoder;
 import java.util.Base64.Encoder;
 import java.util.Objects;
+import java.util.Random;
 
 import compiler.whitebox.CompilerWhiteBoxTest;
 import sun.hotspot.code.Compiler;
@@ -59,15 +60,37 @@ public class TestBase64 {
         if (!Compiler.isIntrinsicAvailable(CompilerWhiteBoxTest.COMP_LEVEL_FULL_OPTIMIZATION, "java.util.Base64$Encoder", "encodeBlock", byte[].class, int.class, int.class, byte[].class, int.class, boolean.class)) {
             throw new SkippedException("Base64 intrinsic is not available");
         }
-        int iters = (args.length > 0 ? Integer.valueOf(args[0]) : 100000);
+        int iters = (args.length > 0 ? Integer.valueOf(args[0]) : 5_000);
         System.out.println(iters + " iterations");
 
-        test0(Base64Type.BASIC, Base64.getEncoder(), Base64.getDecoder(),"plain.txt", "baseEncode.txt", iters);
-        test0(Base64Type.URLSAFE, Base64.getUrlEncoder(), Base64.getUrlDecoder(),"plain.txt", "urlEncode.txt", iters);
-        test0(Base64Type.MIME, Base64.getMimeEncoder(), Base64.getMimeDecoder(),"plain.txt", "mimeEncode.txt", iters);
+        warmup();
+
+        test0(FileType.ASCII, Base64Type.BASIC, Base64.getEncoder(), Base64.getDecoder(),"plain.txt", "baseEncode.txt", iters);
+        test0(FileType.ASCII, Base64Type.URLSAFE, Base64.getUrlEncoder(), Base64.getUrlDecoder(),"plain.txt", "urlEncode.txt", iters);
+        test0(FileType.ASCII, Base64Type.MIME, Base64.getMimeEncoder(), Base64.getMimeDecoder(),"plain.txt", "mimeEncode.txt", iters);
+
+        test0(FileType.HEXASCII, Base64Type.BASIC, Base64.getEncoder(), Base64.getDecoder(),"longLineHEX.txt", "longLineBaseEncode.txt", iters);
+        test0(FileType.HEXASCII, Base64Type.URLSAFE, Base64.getUrlEncoder(), Base64.getUrlDecoder(),"longLineHEX.txt", "longLineUrlEncode.txt", iters);
+        test0(FileType.HEXASCII, Base64Type.MIME, Base64.getMimeEncoder(), Base64.getMimeDecoder(),"longLineHEX.txt", "longLineMimeEncode.txt", iters);
     }
 
-    public static void test0(Base64Type type, Encoder encoder, Decoder decoder, String srcFile, String encodedFile, int numIterations) throws Exception {
+    private static void warmup() {
+        final int warmupCount = 20_000;
+        final int bufSize = 60;
+        byte[] srcBuf = new byte[bufSize];
+        byte[] encBuf = new byte[(bufSize / 3) * 4];
+        byte[] decBuf = new byte[bufSize];
+
+        ran.nextBytes(srcBuf);
+
+        // This should be enough to get both the decoder and encoder intrinsic loaded up and running.
+        for (int i = 0; i < warmupCount; i++) {
+            Base64.getEncoder().encode(srcBuf, encBuf);
+            Base64.getDecoder().decode(encBuf, decBuf);
+        }
+    }
+
+    public static void test0(FileType inputFileType, Base64Type type, Encoder encoder, Decoder decoder, String srcFile, String encodedFile, int numIterations) throws Exception {
 
         String[] srcLns = Files.readAllLines(Paths.get(SRCDIR, srcFile), DEF_CHARSET)
                                .toArray(new String[0]);
@@ -96,7 +119,18 @@ public class TestBase64 {
                     }
                 }
 
-                byte[] srcArr = srcStr.getBytes(DEF_CHARSET);
+                byte[] srcArr;
+                switch (inputFileType) {
+                case ASCII:
+                    srcArr = srcStr.getBytes(DEF_CHARSET);
+                    break;
+                case HEXASCII:
+                    srcArr = hexStringToByteArray(srcStr);
+                    break;
+                default:
+                    throw new IllegalStateException();
+                }
+
                 byte[] encodedArr = encodedStr.getBytes(DEF_CHARSET);
 
                 ByteBuffer srcBuf = ByteBuffer.wrap(srcArr);
@@ -134,6 +168,25 @@ public class TestBase64 {
                 resArr = decoder.decode(encodedArr);
                 assertEqual(resArr, srcArr);
 
+                // test that an illegal Base64 character is detected
+                if ((type != Base64Type.MIME) && (encodedArr.length > 0)) {
+                    int bytePosToCorrupt = ran.nextInt(encodedArr.length);
+                    byte orig = encodedArr[bytePosToCorrupt];
+                    encodedArr[bytePosToCorrupt] = getBadBase64Char(type);
+                    boolean caught = false;
+                    try {
+                        // resArr is already allocated
+                        len = decoder.decode(encodedArr, resArr);
+                    } catch (IllegalArgumentException e) {
+                        caught = true;
+                    }
+                    if (!caught) {
+                        throw new RuntimeException(String.format("Decoder did not catch an illegal base64 character: 0x%02x at position: %d in encoded buffer of length %d",
+                             encodedArr[bytePosToCorrupt], bytePosToCorrupt, encodedArr.length));
+                    }
+                    encodedArr[bytePosToCorrupt] = orig;
+                }
+
                 // test ByteBuffer decode(ByteBuffer)
                 limit = encodedBuf.limit();
                 resBuf = decoder.decode(encodedBuf);
@@ -150,6 +203,11 @@ public class TestBase64 {
         }
     }
 
+    // Data type in the input file
+    enum FileType {
+        ASCII, HEXASCII
+    }
+
     // helper
     enum Base64Type {
         BASIC, URLSAFE, MIME
@@ -160,6 +218,7 @@ public class TestBase64 {
     private static final String DEF_EXCEPTION_MSG =
         "Assertion failed! The result is not same as expected\n";
     private static final String DEFAULT_CRLF = "\r\n";
+    private static final Random ran = new Random(1000); // Constant seed for repeatability
 
     private static void assertEqual(Object result, Object expect) {
         if (checkOutput) {
@@ -175,6 +234,141 @@ public class TestBase64 {
                 throw new RuntimeException(DEF_EXCEPTION_MSG +
                     " result: " + resultStr + " expected: " + expectStr);
             }
+        }
+    }
+
+    private static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        if (len % 2 != 0) {
+                throw new InternalError("Internal test error: HEXASCII source file line doesn't contain an even number of characters: " + Integer.toString(len));
+        }
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            byte upperNibble = (byte)Character.digit(s.charAt(i), 16);
+            byte lowerNibble = (byte)Character.digit(s.charAt(i + 1), 16);
+            if (upperNibble == -1) {
+                throw new InternalError("Internal test error: HEXASCII source file contains non-hex character: " + s.charAt(i) + " at pos: " + Integer.toString(i));
+            }
+            if (lowerNibble == -1) {
+                throw new InternalError("Internal test error: HEXASCII source file contains non-hex character: " + s.charAt(i) + " at pos: " + Integer.toString(i + 1));
+            }
+            data[i / 2] = (byte)((upperNibble << 4) + lowerNibble);
+        }
+        return data;
+    }
+
+    private static final byte[] nonBase64 = {
+        (byte)0x00, (byte)0x01, (byte)0x02, (byte)0x03,
+        (byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07,
+        (byte)0x08, (byte)0x09, (byte)0x0a, (byte)0x0b,
+        (byte)0x0c, (byte)0x0d, (byte)0x0e, (byte)0x0f,
+        (byte)0x10, (byte)0x11, (byte)0x12, (byte)0x13,
+        (byte)0x14, (byte)0x15, (byte)0x16, (byte)0x17,
+        (byte)0x18, (byte)0x19, (byte)0x1a, (byte)0x1b,
+        (byte)0x1c, (byte)0x1d, (byte)0x1e, (byte)0x1f,
+        (byte)0x20, (byte)0x21, (byte)0x22, (byte)0x23,
+        (byte)0x24, (byte)0x25, (byte)0x26, (byte)0x27,
+        (byte)0x28, (byte)0x29, (byte)0x2a, (byte)0x2c,
+        (byte)0x2d, (byte)0x2e, (byte)0x3a, (byte)0x3b,
+        (byte)0x3c, (byte)0x3e, (byte)0x3f, (byte)0x40,
+        (byte)0x5b, (byte)0x5c, (byte)0x5d, (byte)0x5e,
+        (byte)0x5f, (byte)0x60, (byte)0x7b, (byte)0x7c,
+        (byte)0x7d, (byte)0x7e, (byte)0x7f, (byte)0x80,
+        (byte)0x81, (byte)0x82, (byte)0x83, (byte)0x84,
+        (byte)0x85, (byte)0x86, (byte)0x87, (byte)0x88,
+        (byte)0x89, (byte)0x8a, (byte)0x8b, (byte)0x8c,
+        (byte)0x8d, (byte)0x8e, (byte)0x8f, (byte)0x90,
+        (byte)0x91, (byte)0x92, (byte)0x93, (byte)0x94,
+        (byte)0x95, (byte)0x96, (byte)0x97, (byte)0x98,
+        (byte)0x99, (byte)0x9a, (byte)0x9b, (byte)0x9c,
+        (byte)0x9d, (byte)0x9e, (byte)0x9f, (byte)0xa0,
+        (byte)0xa1, (byte)0xa2, (byte)0xa3, (byte)0xa4,
+        (byte)0xa5, (byte)0xa6, (byte)0xa7, (byte)0xa8,
+        (byte)0xa9, (byte)0xaa, (byte)0xab, (byte)0xac,
+        (byte)0xad, (byte)0xae, (byte)0xaf, (byte)0xb0,
+        (byte)0xb1, (byte)0xb2, (byte)0xb3, (byte)0xb4,
+        (byte)0xb5, (byte)0xb6, (byte)0xb7, (byte)0xb8,
+        (byte)0xb9, (byte)0xba, (byte)0xbb, (byte)0xbc,
+        (byte)0xbd, (byte)0xbe, (byte)0xbf, (byte)0xc0,
+        (byte)0xc1, (byte)0xc2, (byte)0xc3, (byte)0xc4,
+        (byte)0xc5, (byte)0xc6, (byte)0xc7, (byte)0xc8,
+        (byte)0xc9, (byte)0xca, (byte)0xcb, (byte)0xcc,
+        (byte)0xcd, (byte)0xce, (byte)0xcf, (byte)0xd0,
+        (byte)0xd1, (byte)0xd2, (byte)0xd3, (byte)0xd4,
+        (byte)0xd5, (byte)0xd6, (byte)0xd7, (byte)0xd8,
+        (byte)0xd9, (byte)0xda, (byte)0xdb, (byte)0xdc,
+        (byte)0xdd, (byte)0xde, (byte)0xdf, (byte)0xe0,
+        (byte)0xe1, (byte)0xe2, (byte)0xe3, (byte)0xe4,
+        (byte)0xe5, (byte)0xe6, (byte)0xe7, (byte)0xe8,
+        (byte)0xe9, (byte)0xea, (byte)0xeb, (byte)0xec,
+        (byte)0xed, (byte)0xee, (byte)0xef, (byte)0xf0,
+        (byte)0xf1, (byte)0xf2, (byte)0xf3, (byte)0xf4,
+        (byte)0xf5, (byte)0xf6, (byte)0xf7, (byte)0xf8,
+        (byte)0xf9, (byte)0xfa, (byte)0xfb, (byte)0xfc,
+        (byte)0xfd, (byte)0xfe, (byte)0xff
+    };
+
+    private static final byte[] nonBase64URL = {
+        (byte)0x00, (byte)0x01, (byte)0x02, (byte)0x03,
+        (byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07,
+        (byte)0x08, (byte)0x09, (byte)0x0a, (byte)0x0b,
+        (byte)0x0c, (byte)0x0d, (byte)0x0e, (byte)0x0f,
+        (byte)0x10, (byte)0x11, (byte)0x12, (byte)0x13,
+        (byte)0x14, (byte)0x15, (byte)0x16, (byte)0x17,
+        (byte)0x18, (byte)0x19, (byte)0x1a, (byte)0x1b,
+        (byte)0x1c, (byte)0x1d, (byte)0x1e, (byte)0x1f,
+        (byte)0x20, (byte)0x21, (byte)0x22, (byte)0x23,
+        (byte)0x24, (byte)0x25, (byte)0x26, (byte)0x27,
+        (byte)0x28, (byte)0x29, (byte)0x2a, (byte)0x2b,
+        (byte)0x2c, (byte)0x2e, (byte)0x2f, (byte)0x3a,
+        (byte)0x3b, (byte)0x3c, (byte)0x3e, (byte)0x3f,
+        (byte)0x40, (byte)0x5b, (byte)0x5c, (byte)0x5d,
+        (byte)0x5e, (byte)0x60, (byte)0x7b, (byte)0x7c,
+        (byte)0x7d, (byte)0x7e, (byte)0x7f, (byte)0x80,
+        (byte)0x81, (byte)0x82, (byte)0x83, (byte)0x84,
+        (byte)0x85, (byte)0x86, (byte)0x87, (byte)0x88,
+        (byte)0x89, (byte)0x8a, (byte)0x8b, (byte)0x8c,
+        (byte)0x8d, (byte)0x8e, (byte)0x8f, (byte)0x90,
+        (byte)0x91, (byte)0x92, (byte)0x93, (byte)0x94,
+        (byte)0x95, (byte)0x96, (byte)0x97, (byte)0x98,
+        (byte)0x99, (byte)0x9a, (byte)0x9b, (byte)0x9c,
+        (byte)0x9d, (byte)0x9e, (byte)0x9f, (byte)0xa0,
+        (byte)0xa1, (byte)0xa2, (byte)0xa3, (byte)0xa4,
+        (byte)0xa5, (byte)0xa6, (byte)0xa7, (byte)0xa8,
+        (byte)0xa9, (byte)0xaa, (byte)0xab, (byte)0xac,
+        (byte)0xad, (byte)0xae, (byte)0xaf, (byte)0xb0,
+        (byte)0xb1, (byte)0xb2, (byte)0xb3, (byte)0xb4,
+        (byte)0xb5, (byte)0xb6, (byte)0xb7, (byte)0xb8,
+        (byte)0xb9, (byte)0xba, (byte)0xbb, (byte)0xbc,
+        (byte)0xbd, (byte)0xbe, (byte)0xbf, (byte)0xc0,
+        (byte)0xc1, (byte)0xc2, (byte)0xc3, (byte)0xc4,
+        (byte)0xc5, (byte)0xc6, (byte)0xc7, (byte)0xc8,
+        (byte)0xc9, (byte)0xca, (byte)0xcb, (byte)0xcc,
+        (byte)0xcd, (byte)0xce, (byte)0xcf, (byte)0xd0,
+        (byte)0xd1, (byte)0xd2, (byte)0xd3, (byte)0xd4,
+        (byte)0xd5, (byte)0xd6, (byte)0xd7, (byte)0xd8,
+        (byte)0xd9, (byte)0xda, (byte)0xdb, (byte)0xdc,
+        (byte)0xdd, (byte)0xde, (byte)0xdf, (byte)0xe0,
+        (byte)0xe1, (byte)0xe2, (byte)0xe3, (byte)0xe4,
+        (byte)0xe5, (byte)0xe6, (byte)0xe7, (byte)0xe8,
+        (byte)0xe9, (byte)0xea, (byte)0xeb, (byte)0xec,
+        (byte)0xed, (byte)0xee, (byte)0xef, (byte)0xf0,
+        (byte)0xf1, (byte)0xf2, (byte)0xf3, (byte)0xf4,
+        (byte)0xf5, (byte)0xf6, (byte)0xf7, (byte)0xf8,
+        (byte)0xf9, (byte)0xfa, (byte)0xfb, (byte)0xfc,
+        (byte)0xfd, (byte)0xfe, (byte)0xff
+    };
+
+    private static byte getBadBase64Char(Base64Type b64Type) {
+        int ch = ran.nextInt(256 - 65); // 64 base64 characters, and one for the '=' padding character
+        switch (b64Type) {
+        case MIME:
+        case BASIC:
+            return nonBase64[ch];
+        case URLSAFE:
+            return nonBase64URL[ch];
+        default:
+            throw new InternalError("Internal test error: getBadBase64Char called with unknown Base64Type value");
         }
     }
 }
