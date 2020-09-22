a37d9d28f6860421599f0d1a43d2c8d53342d028
Lance Andersen <lance.andersen@oracle.com>
2020-09-22 16:53:22 -0400

    Add additional offset test cases in DeflaterDictionaryTests.java

--- test/jdk/java/util/zip/DeflaterDictionaryTests.java
+++ test/jdk/java/util/zip/DeflaterDictionaryTests.java
@@ -22,6 +22,7 @@
  */
 
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.nio.ByteBuffer;
@@ -30,6 +31,7 @@ import java.util.zip.Deflater;
 import java.util.zip.Inflater;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.testng.Assert.assertThrows;
 
 /**
  * @test
@@ -48,19 +50,48 @@ public class DeflaterDictionaryTests {
     private static final int DICTIONARY_LENGTH = 3;
 
     /**
+     * DataProvider with offsets which should be valid for setDictionary
+     *
+     * @return valid offset values
+     */
+    @DataProvider(name = "validDictionaryOffsets")
+    protected Object[][] validDictionaryOffsets() {
+        return new Object[][]{
+                {0},
+                {DICTIONARY_OFFSET},
+                {DICTIONARY_LENGTH}
+        };
+    }
+
+    /**
+     * DataProvider with  invalid offsets for setDictionary
+     *
+     * @return invalid offset values
+     */
+    @DataProvider(name = "invalidDictionaryOffsets")
+    protected Object[][] invalidDictionaryOffsets() {
+        return new Object[][]{
+                {-1},
+                {DICTIONARY_LENGTH + 2},
+                {DICTIONARY.length()}
+        };
+    }
+
+    /**
      * Validate that an offset can be used with Deflater::setDictionary
      *
+     * @param dictionary_offset offset value to be used
      * @throws Exception if an error occurs
      */
-    @Test
-    public void testByteArray() throws Exception {
+    @Test(dataProvider = "validDictionaryOffsets")
+    public void testByteArray(int dictionary_offset) throws Exception {
         byte[] input = SRC_DATA.getBytes(UTF_8);
         byte[] output = new byte[RESULT_SIZE];
         Deflater deflater = new Deflater();
         Inflater inflater = new Inflater();
         try {
             // Compress the bytes
-            deflater.setDictionary(DICTIONARY.getBytes(UTF_8), DICTIONARY_OFFSET, DICTIONARY_LENGTH);
+            deflater.setDictionary(DICTIONARY.getBytes(UTF_8), dictionary_offset, DICTIONARY_LENGTH);
             deflater.setInput(input);
             deflater.finish();
             int compressedDataLength = deflater.deflate(output, 0, output.length, Deflater.NO_FLUSH);
@@ -75,7 +106,7 @@ public class DeflaterDictionaryTests {
             int resultLength = inflater.inflate(result);
             if (inflater.needsDictionary()) {
                 System.out.println("Specifying Dictionary");
-                inflater.setDictionary(DICTIONARY.getBytes(UTF_8), DICTIONARY_OFFSET, DICTIONARY_LENGTH);
+                inflater.setDictionary(DICTIONARY.getBytes(UTF_8), dictionary_offset, DICTIONARY_LENGTH);
                 resultLength = inflater.inflate(result);
             } else {
                 System.out.println("Did not need to use a Dictionary");
@@ -188,6 +219,30 @@ public class DeflaterDictionaryTests {
 
             Assert.assertEquals(SRC_DATA.length(), resultLength);
             Assert.assertEquals(input, Arrays.copyOf(result, resultLength));
+        } finally {
+            // Release Resources
+            deflater.end();
+            inflater.end();
+        }
+    }
+
+    /**
+     * Validate that an invalid offset used with setDictionary will
+     * throw an Exception
+     *
+     * @param dictionary_offset offset value to be used
+     */
+    @Test(dataProvider = "invalidDictionaryOffsets")
+    public void testInvalidOffsets(int dictionary_offset) {
+        byte[] dictionary = DICTIONARY.getBytes(UTF_8);
+
+        Deflater deflater = new Deflater();
+        Inflater inflater = new Inflater();
+        try {
+            assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+                    deflater.setDictionary(dictionary, dictionary_offset, DICTIONARY_LENGTH));
+            assertThrows(ArrayIndexOutOfBoundsException.class, () ->
+                    inflater.setDictionary(dictionary, dictionary_offset, DICTIONARY_LENGTH));
         } finally {
             // Release Resources
             deflater.end();
