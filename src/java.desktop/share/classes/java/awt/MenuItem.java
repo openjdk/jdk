33e7d4a047f398f774d8633c0f2fa2fcad55f94f
Sergey Bylokhov <Sergey.Bylokhov@oracle.com>
2020-09-22 22:37:59 -0700

    Update the JDK-8252195

--- src/java.desktop/share/classes/java/awt/MenuItem.java
+++ src/java.desktop/share/classes/java/awt/MenuItem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -824,7 +824,7 @@ public class MenuItem extends MenuComponent implements Accessible {
         private static final long serialVersionUID = -217847831945965825L;
 
         /**
-         * Constructs an {@code AccessibleAWTMenuItem}
+         * Constructs an {@code AccessibleAWTMenuItem}.
          */
         protected AccessibleAWTMenuItem() {}
 
