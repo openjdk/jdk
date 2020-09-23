5ef82794b95efa3be2f4ae625c85ae11ab3f6145
Sergey Bylokhov <Sergey.Bylokhov@oracle.com>
2020-09-22 22:25:51 -0700

    update the JDK-8252721

--- src/java.desktop/share/classes/javax/swing/JEditorPane.java
+++ src/java.desktop/share/classes/javax/swing/JEditorPane.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1660,7 +1660,7 @@ public class JEditorPane extends JTextComponent {
     protected class AccessibleJEditorPane extends AccessibleJTextComponent {
 
         /**
-         * Constructor for subclasses to call.
+         * Constructs an {@code AccessibleJEditorPane}.
          */
         protected AccessibleJEditorPane() {}
 
