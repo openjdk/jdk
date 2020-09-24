76e3d2788f7dd192ab5854fb5dcce7368c256939
Vicente Romero <vicente.romero@oracle.com>
2020-09-24 19:17:41 -0400

    adding missing changes to some tests

--- test/langtools/tools/javac/IllegalAnnotation.java
+++ test/langtools/tools/javac/IllegalAnnotation.java
@@ -4,7 +4,6 @@
  * @summary javac crash when declare an annotation type illegally
  *
  * @compile/fail/ref=IllegalAnnotation.out -XDrawDiagnostics IllegalAnnotation.java
- * @compile/fail/ref=IllegalAnnotation.out -XDrawDiagnostics --enable-preview -source ${jdk.version} IllegalAnnotation.java
  */
 class IllegalAnnotation {
     {
