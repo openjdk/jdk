8239b67d4f1dcbd063a78286a55aafc7c80aeb4f
Sergey Bylokhov <serb@openjdk.org>
2020-09-24 22:48:28 +0000

    8253322: Update the specification in the newly added constructors
    Reviewed-by: psadhukhan, prr

--- src/java.desktop/share/classes/javax/swing/JPasswordField.java
+++ src/java.desktop/share/classes/javax/swing/JPasswordField.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -407,7 +407,7 @@ public class JPasswordField extends JTextField {
     protected class AccessibleJPasswordField extends AccessibleJTextField {
 
         /**
-         * Constructor for subclasses to call.
+         * Constructs an {@code AccessibleJPasswordField}.
          */
         protected AccessibleJPasswordField() {}
 
--- src/java.desktop/share/classes/javax/swing/JToolTip.java
+++ src/java.desktop/share/classes/javax/swing/JToolTip.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -262,7 +262,7 @@ public class JToolTip extends JComponent implements Accessible {
     protected class AccessibleJToolTip extends AccessibleJComponent {
 
         /**
-         * Constructor for subclasses to call.
+         * Constructs an {@code AccessibleJToolTip}.
          */
         protected AccessibleJToolTip() {}
 
