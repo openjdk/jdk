853ec00e2b6dff2be3f6a781763c5e908f547274
Corey Ashford <cjashfor@linux.vnet.ibm.com>
2020-09-28 18:20:09 -0500

    TestBase64.java: Changes from PR review
     * Use Utils.toByteArrays() method instead of a locally-defined method
     * Generate the two non-Base64 tables dynamically rather than use static initialization
     * Added comments describing the two above-mentioned arrays

--- test/hotspot/jtreg/compiler/intrinsics/base64/TestBase64.java
+++ test/hotspot/jtreg/compiler/intrinsics/base64/TestBase64.java
@@ -26,7 +26,7 @@
  * @author Eric Wang <yiming.wang@oracle.com>
  * @summary tests java.util.Base64
  * @library /test/lib /
- * @build sun.hotspot.WhiteBox
+ * @build sun.hotspot.WhiteBox jdk.test.lib.Utils
  * @run driver ClassFileInstaller sun.hotspot.WhiteBox
  *
  * @run main/othervm/timeout=600 -Xbatch -DcheckOutput=true
@@ -52,6 +52,7 @@ import java.util.Random;
 import compiler.whitebox.CompilerWhiteBoxTest;
 import sun.hotspot.code.Compiler;
 import jtreg.SkippedException;
+import jdk.test.lib.Utils;
 
 public class TestBase64 {
     static boolean checkOutput = Boolean.getBoolean("checkOutput");
@@ -62,6 +63,8 @@ public class TestBase64 {
         }
         int iters = (args.length > 0 ? Integer.valueOf(args[0]) : 5_000);
         System.out.println(iters + " iterations");
+
+        initNonBase64Arrays();
 
         warmup();
 
@@ -125,7 +128,7 @@ public class TestBase64 {
                     srcArr = srcStr.getBytes(DEF_CHARSET);
                     break;
                 case HEXASCII:
-                    srcArr = hexStringToByteArray(srcStr);
+                    srcArr = Utils.toByteArray(srcStr);
                     break;
                 default:
                     throw new IllegalStateException();
@@ -237,129 +240,52 @@ public class TestBase64 {
         }
     }
 
-    private static byte[] hexStringToByteArray(String s) {
-        int len = s.length();
-        if (len % 2 != 0) {
-                throw new InternalError("Internal test error: HEXASCII source file line doesn't contain an even number of characters: " + Integer.toString(len));
-        }
-        byte[] data = new byte[len / 2];
-        for (int i = 0; i < len; i += 2) {
-            byte upperNibble = (byte)Character.digit(s.charAt(i), 16);
-            byte lowerNibble = (byte)Character.digit(s.charAt(i + 1), 16);
-            if (upperNibble == -1) {
-                throw new InternalError("Internal test error: HEXASCII source file contains non-hex character: " + s.charAt(i) + " at pos: " + Integer.toString(i));
+    // This array will contain all possible 8-bit values *except* those
+    // that are legal Base64 characters: A-Z a-z 0-9 + / =
+    private static final byte[] nonBase64 = new byte[256 - 65];
+
+    // This array will contain all possible 8-bit values *except* those
+    // that are legal URL-safe Base64 characters: A-Z a-z 0-9 - _ =
+    private static final byte[] nonBase64URL = new byte[256 - 65];
+
+    private static final byte[] legalBase64 = new byte[] {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        '+', '/', '=' };
+
+    private static final byte[] legalBase64URL = new byte[] {
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+        '-', '_', '=' };
+
+    private static final boolean contains(byte[] ary, byte b) {
+        for (int i = 0; i < ary.length; i++) {
+            if (ary[i] == b) {
+                return true;
             }
-            if (lowerNibble == -1) {
-                throw new InternalError("Internal test error: HEXASCII source file contains non-hex character: " + s.charAt(i) + " at pos: " + Integer.toString(i + 1));
-            }
-            data[i / 2] = (byte)((upperNibble << 4) + lowerNibble);
         }
-        return data;
+        return false;
     }
 
-    private static final byte[] nonBase64 = {
-        (byte)0x00, (byte)0x01, (byte)0x02, (byte)0x03,
-        (byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07,
-        (byte)0x08, (byte)0x09, (byte)0x0a, (byte)0x0b,
-        (byte)0x0c, (byte)0x0d, (byte)0x0e, (byte)0x0f,
-        (byte)0x10, (byte)0x11, (byte)0x12, (byte)0x13,
-        (byte)0x14, (byte)0x15, (byte)0x16, (byte)0x17,
-        (byte)0x18, (byte)0x19, (byte)0x1a, (byte)0x1b,
-        (byte)0x1c, (byte)0x1d, (byte)0x1e, (byte)0x1f,
-        (byte)0x20, (byte)0x21, (byte)0x22, (byte)0x23,
-        (byte)0x24, (byte)0x25, (byte)0x26, (byte)0x27,
-        (byte)0x28, (byte)0x29, (byte)0x2a, (byte)0x2c,
-        (byte)0x2d, (byte)0x2e, (byte)0x3a, (byte)0x3b,
-        (byte)0x3c, (byte)0x3e, (byte)0x3f, (byte)0x40,
-        (byte)0x5b, (byte)0x5c, (byte)0x5d, (byte)0x5e,
-        (byte)0x5f, (byte)0x60, (byte)0x7b, (byte)0x7c,
-        (byte)0x7d, (byte)0x7e, (byte)0x7f, (byte)0x80,
-        (byte)0x81, (byte)0x82, (byte)0x83, (byte)0x84,
-        (byte)0x85, (byte)0x86, (byte)0x87, (byte)0x88,
-        (byte)0x89, (byte)0x8a, (byte)0x8b, (byte)0x8c,
-        (byte)0x8d, (byte)0x8e, (byte)0x8f, (byte)0x90,
-        (byte)0x91, (byte)0x92, (byte)0x93, (byte)0x94,
-        (byte)0x95, (byte)0x96, (byte)0x97, (byte)0x98,
-        (byte)0x99, (byte)0x9a, (byte)0x9b, (byte)0x9c,
-        (byte)0x9d, (byte)0x9e, (byte)0x9f, (byte)0xa0,
-        (byte)0xa1, (byte)0xa2, (byte)0xa3, (byte)0xa4,
-        (byte)0xa5, (byte)0xa6, (byte)0xa7, (byte)0xa8,
-        (byte)0xa9, (byte)0xaa, (byte)0xab, (byte)0xac,
-        (byte)0xad, (byte)0xae, (byte)0xaf, (byte)0xb0,
-        (byte)0xb1, (byte)0xb2, (byte)0xb3, (byte)0xb4,
-        (byte)0xb5, (byte)0xb6, (byte)0xb7, (byte)0xb8,
-        (byte)0xb9, (byte)0xba, (byte)0xbb, (byte)0xbc,
-        (byte)0xbd, (byte)0xbe, (byte)0xbf, (byte)0xc0,
-        (byte)0xc1, (byte)0xc2, (byte)0xc3, (byte)0xc4,
-        (byte)0xc5, (byte)0xc6, (byte)0xc7, (byte)0xc8,
-        (byte)0xc9, (byte)0xca, (byte)0xcb, (byte)0xcc,
-        (byte)0xcd, (byte)0xce, (byte)0xcf, (byte)0xd0,
-        (byte)0xd1, (byte)0xd2, (byte)0xd3, (byte)0xd4,
-        (byte)0xd5, (byte)0xd6, (byte)0xd7, (byte)0xd8,
-        (byte)0xd9, (byte)0xda, (byte)0xdb, (byte)0xdc,
-        (byte)0xdd, (byte)0xde, (byte)0xdf, (byte)0xe0,
-        (byte)0xe1, (byte)0xe2, (byte)0xe3, (byte)0xe4,
-        (byte)0xe5, (byte)0xe6, (byte)0xe7, (byte)0xe8,
-        (byte)0xe9, (byte)0xea, (byte)0xeb, (byte)0xec,
-        (byte)0xed, (byte)0xee, (byte)0xef, (byte)0xf0,
-        (byte)0xf1, (byte)0xf2, (byte)0xf3, (byte)0xf4,
-        (byte)0xf5, (byte)0xf6, (byte)0xf7, (byte)0xf8,
-        (byte)0xf9, (byte)0xfa, (byte)0xfb, (byte)0xfc,
-        (byte)0xfd, (byte)0xfe, (byte)0xff
-    };
+    private static final void initNonBase64Arrays() {
+        int i0 = 0, i1 = 0;
+        for (int val = 0; val < 256; val++) {
+            if (! contains(legalBase64, (byte)val)) {
+                nonBase64[i0++] = (byte)val;
+            }
+            if (! contains(legalBase64URL, (byte)val)) {
+                nonBase64URL[i1++] = (byte)val;
+            }
+        }
+    }
 
-    private static final byte[] nonBase64URL = {
-        (byte)0x00, (byte)0x01, (byte)0x02, (byte)0x03,
-        (byte)0x04, (byte)0x05, (byte)0x06, (byte)0x07,
-        (byte)0x08, (byte)0x09, (byte)0x0a, (byte)0x0b,
-        (byte)0x0c, (byte)0x0d, (byte)0x0e, (byte)0x0f,
-        (byte)0x10, (byte)0x11, (byte)0x12, (byte)0x13,
-        (byte)0x14, (byte)0x15, (byte)0x16, (byte)0x17,
-        (byte)0x18, (byte)0x19, (byte)0x1a, (byte)0x1b,
-        (byte)0x1c, (byte)0x1d, (byte)0x1e, (byte)0x1f,
-        (byte)0x20, (byte)0x21, (byte)0x22, (byte)0x23,
-        (byte)0x24, (byte)0x25, (byte)0x26, (byte)0x27,
-        (byte)0x28, (byte)0x29, (byte)0x2a, (byte)0x2b,
-        (byte)0x2c, (byte)0x2e, (byte)0x2f, (byte)0x3a,
-        (byte)0x3b, (byte)0x3c, (byte)0x3e, (byte)0x3f,
-        (byte)0x40, (byte)0x5b, (byte)0x5c, (byte)0x5d,
-        (byte)0x5e, (byte)0x60, (byte)0x7b, (byte)0x7c,
-        (byte)0x7d, (byte)0x7e, (byte)0x7f, (byte)0x80,
-        (byte)0x81, (byte)0x82, (byte)0x83, (byte)0x84,
-        (byte)0x85, (byte)0x86, (byte)0x87, (byte)0x88,
-        (byte)0x89, (byte)0x8a, (byte)0x8b, (byte)0x8c,
-        (byte)0x8d, (byte)0x8e, (byte)0x8f, (byte)0x90,
-        (byte)0x91, (byte)0x92, (byte)0x93, (byte)0x94,
-        (byte)0x95, (byte)0x96, (byte)0x97, (byte)0x98,
-        (byte)0x99, (byte)0x9a, (byte)0x9b, (byte)0x9c,
-        (byte)0x9d, (byte)0x9e, (byte)0x9f, (byte)0xa0,
-        (byte)0xa1, (byte)0xa2, (byte)0xa3, (byte)0xa4,
-        (byte)0xa5, (byte)0xa6, (byte)0xa7, (byte)0xa8,
-        (byte)0xa9, (byte)0xaa, (byte)0xab, (byte)0xac,
-        (byte)0xad, (byte)0xae, (byte)0xaf, (byte)0xb0,
-        (byte)0xb1, (byte)0xb2, (byte)0xb3, (byte)0xb4,
-        (byte)0xb5, (byte)0xb6, (byte)0xb7, (byte)0xb8,
-        (byte)0xb9, (byte)0xba, (byte)0xbb, (byte)0xbc,
-        (byte)0xbd, (byte)0xbe, (byte)0xbf, (byte)0xc0,
-        (byte)0xc1, (byte)0xc2, (byte)0xc3, (byte)0xc4,
-        (byte)0xc5, (byte)0xc6, (byte)0xc7, (byte)0xc8,
-        (byte)0xc9, (byte)0xca, (byte)0xcb, (byte)0xcc,
-        (byte)0xcd, (byte)0xce, (byte)0xcf, (byte)0xd0,
-        (byte)0xd1, (byte)0xd2, (byte)0xd3, (byte)0xd4,
-        (byte)0xd5, (byte)0xd6, (byte)0xd7, (byte)0xd8,
-        (byte)0xd9, (byte)0xda, (byte)0xdb, (byte)0xdc,
-        (byte)0xdd, (byte)0xde, (byte)0xdf, (byte)0xe0,
-        (byte)0xe1, (byte)0xe2, (byte)0xe3, (byte)0xe4,
-        (byte)0xe5, (byte)0xe6, (byte)0xe7, (byte)0xe8,
-        (byte)0xe9, (byte)0xea, (byte)0xeb, (byte)0xec,
-        (byte)0xed, (byte)0xee, (byte)0xef, (byte)0xf0,
-        (byte)0xf1, (byte)0xf2, (byte)0xf3, (byte)0xf4,
-        (byte)0xf5, (byte)0xf6, (byte)0xf7, (byte)0xf8,
-        (byte)0xf9, (byte)0xfa, (byte)0xfb, (byte)0xfc,
-        (byte)0xfd, (byte)0xfe, (byte)0xff
-    };
-
-    private static byte getBadBase64Char(Base64Type b64Type) {
+    private static final byte getBadBase64Char(Base64Type b64Type) {
         int ch = ran.nextInt(256 - 65); // 64 base64 characters, and one for the '=' padding character
         switch (b64Type) {
         case MIME:
