bd194ef6861f169ba9dd1f2f459e1007650c204c
John Tortugo <divcesar@gmail.com>
2020-09-28 10:00:06 -0700

    Address PR feedback. Formatting issues.

--- src/hotspot/share/gc/g1/g1MMUTracker.hpp
+++ src/hotspot/share/gc/g1/g1MMUTracker.hpp
@@ -51,11 +51,9 @@ public:
 };
 
 
-// This implementation uses a fixed-size queue to keep track of all
-// recent pause times. Two major user controls over G1 behavior are
-// setting a pause time goal (MaxGCPauseMillis), over a time slice
-// (GCPauseIntervalMillis). This defines the Minimum Mutator
-// Utilisation (MMU) goal.
+// Two major user controls over G1 behavior are setting a pause 
+// time goal (MaxGCPauseMillis), over a time slice (GCPauseIntervalMillis). 
+// This defines the Minimum Mutator Utilisation (MMU) goal.
 //
 // * Definitions *
 // Mutator Utilisation:
@@ -66,6 +64,10 @@ public:
 // Minimum Mutator Utilisation (MMU):
 // - the worst mutator utilisation across all time slices.
 //
+// The G1MMUTracker uses a fixed-size queue to keep track of all 
+// recent pause times. The pause time data is used to avoid 
+// breaking the MMU.
+//
 // ***** ALL TIMES ARE IN SECS!!!!!!! *****
 class G1MMUTracker: public CHeapObj<mtGC> {
 private:
@@ -73,8 +75,8 @@ private:
     QueueLength = 64
   };
 
-  double                _time_slice;
-  double                _max_gc_time; // this is per time slice
+  double              _time_slice;
+  double              _max_gc_time; // this is per time slice
 
   // The array keeps track of all the pauses that fall within a time
   // slice (the last time slice during which pauses took place).
@@ -85,13 +87,13 @@ private:
   // If the array is full, an easy fix is to look for the pauses with
   // the shortest gap between them and consolidate them.
   // For now, we have taken the expedient alternative of forgetting
-  // the oldest entry in the event that +G1UseFixedWindowMMUTracker, thus
-  // potentially violating MMU specs for some time thereafter.
+  // the oldest entry, thus potentially violating MMU specs for 
+  // some time thereafter.
 
-  G1MMUTrackerElem _array[QueueLength];
-  int                   _head_index;
-  int                   _tail_index;
-  int                   _no_entries;
+  G1MMUTrackerElem    _array[QueueLength];
+  int                 _head_index;
+  int                 _tail_index;
+  int                 _no_entries;
 
   inline int trim_index(int index) {
     return (index + QueueLength) % QueueLength;
@@ -111,7 +113,7 @@ public:
     return _max_gc_time;
   }
 
-  inline double when_max_gc_sec(double current_time) {
+  double when_max_gc_sec(double current_time) {
     return when_sec(current_time, max_gc_time());
   }
 };
