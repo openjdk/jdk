5dd16f592145b64dcf111413cd3d061dfa9101b0
Zhengyu Gu <zgu@redhat.com>
2020-10-07 12:53:22 -0400

    Update

--- src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
@@ -446,12 +446,11 @@ void ShenandoahControlThread::service_concurrent_normal_cycle(GCCause::Cause cau
 
     // Perform update-refs phase.
     heap->vmop_entry_init_updaterefs();
+    heap->entry_updaterefs();
+    if (check_cancellation_or_degen(ShenandoahHeap::_degenerated_updaterefs)) return;
 
     // Concurrent update thread roots
     heap->entry_update_thread_roots();
-    if (check_cancellation_or_degen(ShenandoahHeap::_degenerated_updaterefs)) return;
-
-    heap->entry_updaterefs();
     if (check_cancellation_or_degen(ShenandoahHeap::_degenerated_updaterefs)) return;
 
     heap->vmop_entry_final_updaterefs();
