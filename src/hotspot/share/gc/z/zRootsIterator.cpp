fa2ff601deacd5dc58da7b982c74f3e0f1b06280
Per Liden <pliden@openjdk.org>
2020-10-13 12:16:11 +0200

    8254672: ZGC: ZParallelOopsDo/ZSerialWeakOopsDo should use atomic load/store

--- src/hotspot/share/gc/z/zRootsIterator.cpp
+++ src/hotspot/share/gc/z/zRootsIterator.cpp
@@ -68,10 +68,10 @@ ZParallelOopsDo<T, F>::ZParallelOopsDo(T* iter) :
 
 template <typename T, void (T::*F)(ZRootsIteratorClosure*)>
 void ZParallelOopsDo<T, F>::oops_do(ZRootsIteratorClosure* cl) {
-  if (!_completed) {
+  if (!Atomic::load(&_completed)) {
     (_iter->*F)(cl);
-    if (!_completed) {
-      _completed = true;
+    if (!Atomic::load(&_completed)) {
+      Atomic::store(&_completed, true);
     }
   }
 }
@@ -83,7 +83,7 @@ ZSerialWeakOopsDo<T, F>::ZSerialWeakOopsDo(T* iter) :
 
 template <typename T, void (T::*F)(BoolObjectClosure*, ZRootsIteratorClosure*)>
 void ZSerialWeakOopsDo<T, F>::weak_oops_do(BoolObjectClosure* is_alive, ZRootsIteratorClosure* cl) {
-  if (!_claimed && Atomic::cmpxchg(&_claimed, false, true) == false) {
+  if (!Atomic::load(&_claimed) && Atomic::cmpxchg(&_claimed, false, true) == false) {
     (_iter->*F)(is_alive, cl);
   }
 }
