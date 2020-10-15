05c55047b4b16dd765c11e18fc9b4ba55345a2eb
Christian Hagedorn <christian.hagedorn@oracle.com>
2020-10-15 10:03:02 +0200

    Do some more cleanups and small fixes

--- src/hotspot/share/utilities/decoder_elf.hpp
+++ src/hotspot/share/utilities/decoder_elf.hpp
@@ -45,9 +45,7 @@ public:
     ShouldNotReachHere();
     return false;
   }
-
   bool get_source_info(address pc, char* buf, size_t buflen, int* line);
-
 
 private:
   ElfFile*         get_elf_file(const char* filepath);
