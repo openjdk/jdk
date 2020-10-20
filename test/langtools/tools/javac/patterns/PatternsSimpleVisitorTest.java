5978bca0cdb7b18e060b7735b8656a1852d45af1
Jan Lahoda <jlahoda@openjdk.org>
2020-10-20 14:00:05 +0200

    Cleanup: using a null instead of List.of() as a parameter to JavaCompiler.getTask

--- test/langtools/tools/javac/patterns/PatternsSimpleVisitorTest.java
+++ test/langtools/tools/javac/patterns/PatternsSimpleVisitorTest.java
@@ -105,7 +105,7 @@ public class PatternsSimpleVisitorTest {
 
         StringWriter out = new StringWriter();
         JavacTask ct = (JavacTask) tool.getTask(out, null, noErrors,
-            List.of(), null,
+            null, null,
             Arrays.asList(new MyFileObject(code)));
         return ct.parse().iterator().next();
     }
