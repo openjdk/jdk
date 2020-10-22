7675bbcd6997cdd0a91869830291f183d440776f
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2020-10-22 17:29:26 +0100

    Fix CLinker javadoc

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/CLinker.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/CLinker.java
@@ -137,7 +137,7 @@ public interface CLinker {
      * passed to other foreign functions (as a function pointer); calling such a function pointer
      * from native code will result in the execution of the provided method handle.
      *
-     * <p>The returned segment is <em>not</em> thread-confined, and it only features
+     * <p>The returned segment is <a href=MemorySegment.html#thread-confinement>shared</a>, and it only features
      * the {@link MemorySegment#CLOSE} access mode. When the returned segment is closed,
      * the corresponding native stub will be deallocated.</p>
      *
