695be54615731801cad296a06b66a431c594c656
Christian Hagedorn <christian.hagedorn@oracle.com>
2020-10-23 14:31:53 +0200

    8255245: C1: Fix output of -XX:+PrintCFGToFile to open it with visualizer

--- src/hotspot/share/c1/c1_LinearScan.hpp
+++ src/hotspot/share/c1/c1_LinearScan.hpp
@@ -369,6 +369,7 @@ class LinearScan : public CompilationResourceObj {
   void        print_lir(int level, const char* label, bool hir_valid = true);
   static void print_reg_num(int reg_num) { print_reg_num(tty, reg_num); }
   static void print_reg_num(outputStream* out, int reg_num);
+  static LIR_Opr get_operand(int reg_num);
 #endif
 
 #ifdef ASSERT
@@ -634,6 +635,8 @@ class Interval : public CompilationResourceObj {
 #ifndef PRODUCT
   void print() const { print_on(tty); }
   void print_on(outputStream* out) const;
+  // Special version for compatibility with C1 Visualizer.
+  void print_on(outputStream* out, bool is_cfg_printer) const;
 
   // Used for debugging
   void print_parent() const;
