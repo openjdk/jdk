e51fb4300439eb1c73730f36ec1ef2f8f73cb621
Roman Kennke <rkennke@redhat.com>
2020-10-28 12:11:11 +0100

    Make heap local variable const for consistency

--- src/hotspot/share/gc/shenandoah/shenandoahRootVerifier.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahRootVerifier.cpp
@@ -43,7 +43,7 @@
 ShenandoahGCStateResetter::ShenandoahGCStateResetter() :
   _gc_state(ShenandoahHeap::heap()->gc_state()),
   _concurrent_weak_root_in_progress(ShenandoahHeap::heap()->is_concurrent_weak_root_in_progress()) {
-  ShenandoahHeap* heap = ShenandoahHeap::heap();
+  ShenandoahHeap* const heap = ShenandoahHeap::heap();
   heap->_gc_state.clear();
   heap->set_concurrent_weak_root_in_progress(false);
 }
