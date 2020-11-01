e8c4622535269c89679f270c5b60b78f708db75f
Pankaj Bansal <pankaj.b.bansal@oracle.com>
2020-11-01 22:38:19 +0530

    8251177: [macosx] The text big is truncated

--- test/jdk/javax/swing/JTabbedPane/4209065/bug4209065.java
+++ test/jdk/javax/swing/JTabbedPane/4209065/bug4209065.java
@@ -49,7 +49,9 @@ public final class bug4209065 extends JApplet {
         JTabbedPane tp = new JTabbedPane();
         getContentPane().add(tp);
         String text = "<html><center>If the style of the text on the tabs matches"
-                      + "<br>the descriptions, press <em><b>PASS</b></em></center></html>";
+                      + "<br>the descriptions, press <em><b>PASS</b></em>"
+                      + "<br><b>Note:</b> On Mac, text \"big\" may be truncated,"
+                      + " it should be ignored</center></html>";
         tp.addTab("<html><center><font size=+3>big</font></center></html>", new JLabel(text));
         tp.addTab("<html><center><font color=red>red</font></center></html>", new JLabel(text));
         tp.addTab("<html><center><em><b>Bold Italic!</b></em></center></html>", new JLabel(text));
