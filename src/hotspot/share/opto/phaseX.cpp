e8899406c5ce5fae0f9c641ebe433fceb9c9b60b
Tobias Hartmann <tobias.hartmann@oracle.com>
2020-11-02 09:33:27 +0100

    Restored handling of constant nodes

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -1069,7 +1069,7 @@ void PhaseIterGVN::init_verifyPhaseIterGVN() {
   Unique_Node_List* modified_list = C->modified_nodes();
   while (modified_list != NULL && modified_list->size()) {
     Node* n = modified_list->pop();
-    if (!_worklist.member(n)) {
+    if (!n->is_Con() && !_worklist.member(n)) {
       n->dump();
       fatal("modified node is not on IGVN._worklist");
     }
@@ -1083,8 +1083,10 @@ void PhaseIterGVN::verify_PhaseIterGVN() {
   Unique_Node_List* modified_list = C->modified_nodes();
   while (modified_list != NULL && modified_list->size()) {
     Node* n = modified_list->pop();
-    n->dump();
-    fatal("modified node was not processed by IGVN.transform_old()");
+    if (!n->is_Con()) { // skip Con nodes
+      n->dump();
+      fatal("modified node was not processed by IGVN.transform_old()");
+    }
   }
 #endif
 
