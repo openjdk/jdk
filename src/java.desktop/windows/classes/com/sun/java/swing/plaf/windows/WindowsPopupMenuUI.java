ff35c56a1f7bce62e72962d835a471642d7200a9
Alexander Zuev <alexander.zuev@oracle.com>
2020-11-03 02:25:59 -0800

    4907798: MEMORY LEAK: javax.swing.plaf.basic.BasicPopupMenuUI$MenuKeyboardHelper
    Cleared references on listener deactivation
    Added regression test

--- src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsPopupMenuUI.java
+++ src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsPopupMenuUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -108,6 +108,7 @@ public class WindowsPopupMenuUI extends BasicPopupMenuUI {
                         WindowsGraphicsUtils.repaintMnemonicsInWindow(win);
                     }
                 }
+                repaintRoot = null;
             } else {
                 Component c = (Component)path[0];
                 if (c instanceof JPopupMenu) c = ((JPopupMenu)c).getInvoker();
