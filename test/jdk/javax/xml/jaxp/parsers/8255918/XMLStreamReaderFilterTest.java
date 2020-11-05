30ed16f0d6aa49ffc3721910da5a35c38b060489
Michael Edgar <michael@xlate.io>
2020-11-05 05:31:01 -0500

    8255918: Throw XMLStreamExceptions from XMLStreamFilterImpl constructor

---
+++ test/jdk/javax/xml/jaxp/parsers/8255918/XMLStreamReaderFilterTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Objects;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+/**
+ * JDK-8255918
+ */
+public class XMLStreamReaderFilterTest {
+
+    static final String XMLSOURCE1 = "<root>\n"
+            + "  <element1>\n"
+            + "    <element2>\n" // Unclosed element2
+            + "  </element1>\n"
+            + "  <element3>\n"
+            + "  </element3>\n"
+            + "</root>";
+
+    static final String EXPECTED_MESSAGE1 = ""
+            + "ParseError at [row,col]:[4,5]\n"
+            + "Message: The element type \"element2\" must be terminated by the matching end-tag \"</element2>\".";
+
+    public static void main(String[] args) {
+        testXMLStreamReaderExceptionThrownByConstructor();
+    }
+
+    static void testXMLStreamReaderExceptionThrownByConstructor() {
+        XMLInputFactory factory = XMLInputFactory.newInstance();
+        Throwable thrown = null;
+
+        try (Reader source = new StringReader(XMLSOURCE1)) {
+            XMLStreamReader reader = factory.createXMLStreamReader(source);
+            factory.createFilteredReader(reader, r ->
+                r.getEventType() == XMLStreamConstants.START_ELEMENT
+                        && r.getLocalName().equals("element3"));
+        } catch (Exception e) {
+            thrown = e;
+        }
+
+        if (!(thrown instanceof XMLStreamException)) {
+            throw new RuntimeException("Missing or unexpected exception type: " + String.valueOf(thrown));
+        }
+
+        if (!Objects.equals(EXPECTED_MESSAGE1, thrown.getMessage())) {
+            throw new RuntimeException("Unexpected exception message: " + thrown.getMessage());
+        }
+    }
+
+}
