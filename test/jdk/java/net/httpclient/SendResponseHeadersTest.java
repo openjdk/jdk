8ff6d9b4fcbc0f57f136ff0e7df0d371664badb2
Patrick Concannon <patrick.concannon@oracle.com>
2020-11-06 11:45:55 +0000

    8253005: Restructured test to use @BeforeTest/@AfterTest

--- test/jdk/java/net/httpclient/SendResponseHeadersTest.java
+++ test/jdk/java/net/httpclient/SendResponseHeadersTest.java
@@ -32,6 +32,8 @@
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -40,46 +42,56 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static org.testng.Assert.expectThrows;
 import static org.testng.Assert.fail;
 
-
 public class SendResponseHeadersTest {
+    URI uri;
+    HttpServer server;
+    ExecutorService executor;
+
+    @BeforeTest
+    public void setUp() throws IOException, URISyntaxException {
+        var loopback = InetAddress.getLoopbackAddress();
+        var addr = new InetSocketAddress(loopback, 0);
+        var path = "/test/foo.html";
+        server = HttpServer.create(addr, 0);
+        server.createContext("/test", new TestHandler());
+        executor = Executors.newCachedThreadPool();
+        server.setExecutor(executor);
+        server.start();
+
+        uri = new URI("http", null,
+                loopback.getHostName(),
+                server.getAddress().getPort(),
+                path, null, null);
+    }
 
     @Test
     public void testSend() throws Exception {
-        var addr = new InetSocketAddress(InetAddress.getLoopbackAddress(), 0);
-        var server = HttpServer.create(addr, 0);
-        var path = "/test/foo.html";
-        server.createContext("/test", new TestHandler());
-        ExecutorService executor = Executors.newCachedThreadPool();
-        server.setExecutor (executor);
-        server.start();
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(uri)
+                .GET()
+                .build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        // verify empty response received, otherwise an error has occurred
+        if (!response.body().isEmpty())
+            fail(response.body());
+    }
 
-        URI uri = new URI("http", null,
-                server.getAddress().getHostString(),
-                server.getAddress().getPort(),
-                path, null, null);
-
-        try {
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder(uri)
-                    .GET()
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            // verify empty response received, otherwise an error has occurred
-            if (!response.body().isEmpty())
-                fail(response.body());
-        } finally {
-            server.stop(2);
-            executor.shutdown();
-        }
+    @AfterTest
+    public void tearDown() {
+        server.stop(2);
+        executor.shutdown();
     }
 
     static class TestHandler implements HttpHandler {
@@ -91,12 +103,12 @@ public class SendResponseHeadersTest {
                 exchange.sendResponseHeaders(200, 0);
                 try {
                     IOException io = expectThrows(IOException.class,
-                            () -> exchange.sendResponseHeaders(200, "failMsg".getBytes().length));
+                            () -> exchange.sendResponseHeaders(200, 0));
                     System.out.println("Got expected exception: " + io);
                 } catch (Throwable t) {
-                    // unexpected exception thrown, return error to client
+                    // expectThrows triggered an assertion, return error message to the client
                     t.printStackTrace();
-                    os.write(("Unexpected error: " + t).getBytes());
+                    os.write(("Unexpected error: " + t).getBytes(StandardCharsets.UTF_8));
                 }
             }
         }
