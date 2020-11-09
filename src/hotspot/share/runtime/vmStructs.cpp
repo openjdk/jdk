b4b24792e979420061ad6be9f3cab95ad184bc4f
Frederic Parain <frederic.parain@oracle.com>
2020-11-09 14:46:51 -0500

    Remove unused symbol from vmStruct

--- src/hotspot/share/runtime/vmStructs.cpp
+++ src/hotspot/share/runtime/vmStructs.cpp
@@ -2258,7 +2258,6 @@ typedef HashtableEntry<InstanceKlass*, mtClass>  KlassHashtableEntry;
                                                                           \
   declare_preprocessor_constant("FIELDINFO_TAG_SIZE", FIELDINFO_TAG_SIZE) \
   declare_preprocessor_constant("FIELDINFO_TAG_OFFSET", FIELDINFO_TAG_OFFSET) \
-  declare_preprocessor_constant("FIELDINFO_TAG_CONTENDED", FIELDINFO_TAG_CONTENDED) \
                                                                           \
   /************************************************/                      \
   /* InstanceKlass InnerClassAttributeOffset enum */                      \
