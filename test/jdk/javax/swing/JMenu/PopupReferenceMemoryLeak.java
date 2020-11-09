31cfa27c91dfb4d317c0120eb2d633bbee685a64
Alexander Zuev <alexander.zuev@oracle.com>
2020-11-09 03:52:15 -0800

    Test case made multiplatform and testing all the LaF's

--- test/jdk/javax/swing/JMenu/PopupReferenceMemoryLeak.java
+++ test/jdk/javax/swing/JMenu/PopupReferenceMemoryLeak.java
@@ -25,9 +25,8 @@
  * @test
  * @bug 4907798
  * @key headful
- * @requires (os.family == "windows")
  * @summary Check for memory leak in menu subsystem
- * @run main/othervm PopupReferenceMemoryLeak
+ * @run main/othervm -Xmx8m PopupReferenceMemoryLeak
  */
 
 import javax.swing.AbstractAction;
@@ -37,6 +36,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
 import java.awt.BorderLayout;
 import java.awt.Robot;
@@ -44,89 +44,123 @@ import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+
+import static javax.swing.UIManager.getInstalledLookAndFeels;
 
 public class PopupReferenceMemoryLeak {
     static volatile WeakReference referenceToFrame1;
     static JFrame frame1, frame2;
 
-    public static void main(String[] args) throws InvocationTargetException, InterruptedException {
-        try {
-            // Set system look and feel
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ignore) {
-            throw new RuntimeException("Can not install system look and feel");
-        }
-
-        PopupReferenceMemoryLeak newTest = new PopupReferenceMemoryLeak();
-        SwingUtilities.invokeAndWait(newTest::createUI);
-        try {
-            Robot robot = new Robot();
-            robot.setAutoDelay(200);
-            robot.waitForIdle();
-            Thread.sleep(2000);
-            robot.mouseMove(200, 200);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-            robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_F);
-            robot.keyRelease(KeyEvent.VK_F);
-            robot.keyRelease(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_C);
-            robot.keyRelease(KeyEvent.VK_C);
-            robot.waitForIdle();
-            Thread.sleep(2000);
-            robot.mouseMove(600, 200);
-            robot.waitForIdle();
-            Thread.sleep(2000);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-            // Force GC three times to see if it clears the old frame
-            for(int i=0; i<3; i++) {
-                try {
-                    ArrayList gc = new ArrayList();
-                    while (true) {gc.add(new int[100000]);}
-                } catch (Throwable ignore){}
-                robot.waitForIdle();
-                Thread.sleep(1000);
-                if(referenceToFrame1.get() == null) {
-                    // Frame was released
-                    return;
-                }
+    public static void main(String[] args) throws Exception {
+        Robot robot = new Robot();
+        robot.setAutoDelay(200);
+        for (UIManager.LookAndFeelInfo laf : getInstalledLookAndFeels()) {
+            String lafName = laf.getName();
+            System.out.println("Testing LaF: " + lafName);
+            if (lafName == null || lafName.startsWith("Mac OS X")) {
+                // Aqua Look and Feel uses system menus we can't really test it
+                continue;
             }
-            robot.waitForIdle();
-            robot.keyPress(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_T);
-            robot.keyRelease(KeyEvent.VK_T);
-            robot.keyRelease(KeyEvent.VK_ALT);
-            robot.keyPress(KeyEvent.VK_M);
-            robot.keyRelease(KeyEvent.VK_M);
-            robot.waitForIdle();
-            Thread.sleep(2000);
-            for(int i=0; i<3; i++) {
-                try {
-                    ArrayList gc = new ArrayList();
-                    while (true) {gc.add(new int[100000]);}
-                } catch (Throwable ignore){}
+            setLookAndFeel(laf);
+            PopupReferenceMemoryLeak newTest = new PopupReferenceMemoryLeak();
+            SwingUtilities.invokeAndWait(newTest::createUI);
+            try {
+                boolean passed = false;
                 robot.waitForIdle();
+                Thread.sleep(2000);
+                robot.mouseMove(200, 200);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                robot.keyPress(KeyEvent.VK_F10);
+                robot.keyRelease(KeyEvent.VK_F10);
+                robot.keyPress(KeyEvent.VK_F);
+                robot.keyRelease(KeyEvent.VK_F);
+                robot.keyPress(KeyEvent.VK_C);
+                robot.keyRelease(KeyEvent.VK_C);
+                robot.waitForIdle();
+                Thread.sleep(2000);
+                robot.mouseMove(600, 200);
+                robot.waitForIdle();
+                Thread.sleep(2000);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+                // Workaround for Linux issues when sometimes there
+                // is a ref to last opened frame from native code
+                JFrame frame3 = new JFrame("Workaround");
+                frame3.setSize(100, 100);
+                frame3.setLocation(0,0);
+                frame3.setVisible(true);
                 Thread.sleep(1000);
-                if(referenceToFrame1.get() == null) {
-                    // Frame was released
-                    throw new RuntimeException("Frame cleared only after menu activated on frame2");
+                frame3.setVisible(false);
+                frame3.dispose();
+
+                // Force GC three times to see if it clears the old frame
+                for (int i = 0; i < 3; i++) {
+                    try {
+                        ArrayList gc = new ArrayList();
+                        while (true) {
+                            gc.add(new int[100000]);
+                        }
+                    } catch (Throwable ignore) {
+                    }
+                    robot.waitForIdle();
+                    Thread.sleep(1000);
+                    if (referenceToFrame1.get() == null) {
+                        // Frame was released
+                        passed = true;
+                        break;
+                    }
                 }
-            }
-            throw new RuntimeException("Test finished but menu has not cleared the reference!");
-        } catch (Exception re) {
-            throw new RuntimeException(re.getLocalizedMessage());
-        } finally {
-            if (frame2 != null) {
-                frame2.setVisible(false);
-                frame2.dispose();
+                if (!passed) {
+                    robot.waitForIdle();
+                    robot.keyPress(KeyEvent.VK_F10);
+                    robot.keyRelease(KeyEvent.VK_F10);
+                    robot.keyPress(KeyEvent.VK_T);
+                    robot.keyRelease(KeyEvent.VK_T);
+                    robot.keyPress(KeyEvent.VK_M);
+                    robot.keyRelease(KeyEvent.VK_M);
+                    robot.waitForIdle();
+                    Thread.sleep(2000);
+                    for (int i = 0; i < 5; i++) {
+                        try {
+                            ArrayList gc = new ArrayList();
+                            while (true) {
+                                gc.add(new int[100000]);
+                            }
+                        } catch (Throwable ignore) {
+                        }
+                        robot.waitForIdle();
+                        Thread.sleep(1000);
+                        if (referenceToFrame1.get() == null) {
+                            // Frame was released
+                            throw new RuntimeException("Frame cleared only after menu activated on frame2");
+                        }
+                    }
+                    throw new RuntimeException("Test finished but menu has not cleared the reference!");
+                }
+            } catch (Exception re) {
+                throw new RuntimeException(re.getLocalizedMessage());
+            } finally {
+                if (frame2 != null) {
+                    frame2.setVisible(false);
+                    frame2.dispose();
+                }
             }
         }
     }
+
+    private static void setLookAndFeel(UIManager.LookAndFeelInfo laf) {
+        try {
+            UIManager.setLookAndFeel(laf.getClassName());
+        } catch (UnsupportedLookAndFeelException ignored) {
+            System.out.println("Unsupported LookAndFeel: " + laf.getClassName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public void createUI() {
         frame1 = new JFrame("Main test window");
