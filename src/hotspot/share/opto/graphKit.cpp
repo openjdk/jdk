a836cc324916d7c6f0e4c9a8c0501d25e3d70c81
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2020-11-16 15:30:39 +0000

    Fix aarch64 test failure

--- src/hotspot/share/opto/graphKit.cpp
+++ src/hotspot/share/opto/graphKit.cpp
@@ -2629,7 +2629,7 @@ Node* GraphKit::make_native_call(const TypeFunc* call_type, uint nargs, ciNative
     BufferBlob* invoker = SharedRuntime::make_native_invoker(call_addr,
                                                              nep->shadow_space(),
                                                              arg_regs, ret_regs);
-    if (call_addr == NULL) {
+    if (invoker == NULL) {
       C->record_failure("native invoker not implemented on this platform");
       return NULL;
     }
