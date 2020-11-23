53f38353e0d07b8b4435ed435390ed01e9b24d59
Per Liden <pliden@openjdk.org>
2020-11-23 10:38:38 +0000

    8256419: ZGC: Remove "Heap Used Before/After Mark/Relocation" samplers
    Reviewed-by: eosterlund, stefank

--- src/hotspot/share/gc/z/zHeap.cpp
+++ src/hotspot/share/gc/z/zHeap.cpp
@@ -48,10 +48,6 @@
 #include "runtime/thread.hpp"
 #include "utilities/debug.hpp"
 
-static const ZStatSampler ZSamplerHeapUsedBeforeMark("Memory", "Heap Used Before Mark", ZStatUnitBytes);
-static const ZStatSampler ZSamplerHeapUsedAfterMark("Memory", "Heap Used After Mark", ZStatUnitBytes);
-static const ZStatSampler ZSamplerHeapUsedBeforeRelocation("Memory", "Heap Used Before Relocation", ZStatUnitBytes);
-static const ZStatSampler ZSamplerHeapUsedAfterRelocation("Memory", "Heap Used After Relocation", ZStatUnitBytes);
 static const ZStatCounter ZCounterUndoPageAllocation("Memory", "Undo Page Allocation", ZStatUnitOpsPerSecond);
 static const ZStatCounter ZCounterOutOfMemory("Memory", "Out Of Memory", ZStatUnitOpsPerSecond);
 
@@ -246,9 +242,6 @@ void ZHeap::flip_to_remapped() {
 void ZHeap::mark_start() {
   assert(SafepointSynchronize::is_at_safepoint(), "Should be at safepoint");
 
-  // Update statistics
-  ZStatSample(ZSamplerHeapUsedBeforeMark, used());
-
   // Flip address view
   flip_to_marked();
 
@@ -295,7 +288,6 @@ bool ZHeap::mark_end() {
   ZVerify::after_mark();
 
   // Update statistics
-  ZStatSample(ZSamplerHeapUsedAfterMark, used());
   ZStatHeap::set_at_mark_end(capacity(), allocated(), used());
 
   // Block resurrection of weak/phantom references
@@ -444,7 +436,6 @@ void ZHeap::relocate_start() {
   ZGlobalPhase = ZPhaseRelocate;
 
   // Update statistics
-  ZStatSample(ZSamplerHeapUsedBeforeRelocation, used());
   ZStatHeap::set_at_relocate_start(capacity(), allocated(), used());
 
   // Notify JVMTI
@@ -456,7 +447,6 @@ void ZHeap::relocate() {
   const bool success = _relocate.relocate(&_relocation_set);
 
   // Update statistics
-  ZStatSample(ZSamplerHeapUsedAfterRelocation, used());
   ZStatRelocation::set_at_relocate_end(success);
   ZStatHeap::set_at_relocate_end(capacity(), allocated(), reclaimed(),
                                  used(), used_high(), used_low());
