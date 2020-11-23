18e85064125aa6b933f538422fcebfc8ec4a6c8d
Aleksey Shipilev <shade@openjdk.org>
2020-11-23 17:55:20 +0000

    8256860: S390 builds broken after JDK-8254231
    Reviewed-by: stuefe

--- src/hotspot/cpu/s390/universalUpcallHandle_s390.cpp
+++ src/hotspot/cpu/s390/universalUpcallHandle_s390.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2016 SAP SE. All rights reserved.
+ * Copyright (c) 2020, Red Hat, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,35 +19,13 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- *
  */
 
 #include "precompiled.hpp"
-#include "asm/assembler.hpp"
-#include "code/vmreg.hpp"
+#include "prims/universalUpcallHandler.hpp"
+#include "utilities/debug.hpp"
 
-void VMRegImpl::set_regName() {
-  // Not clear why we have this duplication (triplication?)
-  Register reg = ::as_Register(0);
-  int i;
-  for (i = 0; i < ConcreteRegisterImpl::max_gpr;) {
-    regName[i++] = reg->name();
-    regName[i++] = reg->name();
-    reg = reg->successor();
-  }
-
-  FloatRegister freg = ::as_FloatRegister(0);
-  for (; i < ConcreteRegisterImpl::max_fpr;) {
-    regName[i++] = freg->name();
-    regName[i++] = freg->name();
-    freg = freg->successor();
-  }
-  for (; i < ConcreteRegisterImpl::number_of_registers; i ++) {
-    regName[i] = "NON-GPR-XMM";
-  }
-}
-
-VMReg VMRegImpl::vmStorageToVMReg(int type, int index) {
+address ProgrammableUpcallHandler::generate_upcall_stub(jobject rec, jobject jabi, jobject jlayout) {
   Unimplemented();
-  return VMRegImpl::Bad();
+  return nullptr;
 }
