6c25c7390061b0ba9083f2281d3858be0e33701a
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2020-11-24 15:50:29 +0000

    Fix use of star import in LibrariesHelper

--- src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/LibrariesHelper.java
+++ src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/LibrariesHelper.java
@@ -35,7 +35,11 @@ import jdk.internal.ref.CleanerFactory;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Arrays;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
 
