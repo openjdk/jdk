9dbc045f839323ea20ac5819911f3d9ec24bd6e2
Aleksey Shipilev <shade@redhat.com>
2020-11-24 09:48:24 +0100

    Just disable the workaround for Zero

--- src/hotspot/os_cpu/linux_zero/os_linux_zero.hpp
+++ src/hotspot/os_cpu/linux_zero/os_linux_zero.hpp
@@ -34,7 +34,6 @@
   // Note: Currently only used in 64 bit Windows implementations
   static bool register_code_area(char *low, char *high) { return true; }
 
-#ifdef IA32
   /*
    * Work-around for broken NX emulation using CS limit, Red Hat patch "Exec-Shield"
    * (IA32 only).
@@ -47,7 +46,6 @@
    * @see JDK-8023956
    */
   static void workaround_expand_exec_shield_cs_limit();
-#endif
 
   // Atomically copy 64 bits of data
   static void atomic_copy64(const volatile void *src, volatile void *dst) {
