8f7caa435359052a845c7ca28fd7a7814f71a84a
Aleksey Shipilev <shade@openjdk.org>
2020-11-24 11:02:13 +0000

    8256857: ARM32 builds broken after JDK-8254231
    Reviewed-by: jvernee, stuefe

--- src/hotspot/cpu/arm/foreign_globals_arm.cpp
+++ src/hotspot/cpu/arm/foreign_globals_arm.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Red Hat, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -19,38 +19,18 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- *
  */
 
-
 #include "precompiled.hpp"
-#include "asm/assembler.hpp"
-#include "code/vmreg.hpp"
+#include "prims/foreign_globals.hpp"
+#include "utilities/debug.hpp"
 
-void VMRegImpl::set_regName() {
-  Register reg = ::as_Register(0);
-  int i;
-  for (i = 0; i < ConcreteRegisterImpl::max_gpr; reg = reg->successor()) {
-    for (int j = 0; j < (1 << ConcreteRegisterImpl::log_vmregs_per_gpr); j++) {
-      regName[i++] = reg->name();
-    }
-  }
-#ifndef __SOFTFP__
-  FloatRegister freg = ::as_FloatRegister(0);
-  for ( ; i < ConcreteRegisterImpl::max_fpr ; ) {
-    for (int j = 0; j < (1 << ConcreteRegisterImpl::log_vmregs_per_fpr); j++) {
-      regName[i++] = freg->name();
-    }
-    freg = freg->successor();
-  }
-#endif
-
-  for ( ; i < ConcreteRegisterImpl::number_of_registers ; i ++ ) {
-    regName[i] = "NON-GPR-FPR";
-  }
+const ABIDescriptor ForeignGlobals::parse_abi_descriptor_impl(jobject jabi) const {
+  Unimplemented();
+  return {};
 }
 
-VMReg VMRegImpl::vmStorageToVMReg(int type, int index) {
+const BufferLayout ForeignGlobals::parse_buffer_layout_impl(jobject jlayout) const {
   Unimplemented();
-  return VMRegImpl::Bad();
+  return {};
 }
