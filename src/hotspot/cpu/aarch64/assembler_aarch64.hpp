a40c0d882b782ff198f043dcefb97ded4d0eac77
Hao Sun <hao.sun@arm.com>
2020-11-24 03:40:32 +0000

    8257137: Revise smov and umov in aarch64 assembler.
    1. Both smov and umov lack of checking the register type validity.
       Register type must be 'B', 'H' or 'S' for smov [1].
       Register type can NOT be 'Q' for umov [2].
       Such checks are added.
    
    2. smov and umov have different explanations on 'Q' field, i.e. bit 30
    of the insturction, but current assembler implementation mixed it up.
       For umov, 'Q' field can only be set when register type 'D' is given
       [2]. However, this field of smov must be set for register type 'S'
       [1], that is, 'Q' field can be optional for register type 'B' or 'H'.
    
       Current implementation only took the umov scenario into account. As a
       result, runtime error ILL_ILLOPN would occur if 'smov(Register,
    		   FloatRegister, S, index)' is used.
    
       We put them into two separate functions and make 'Q' field always set
       for smov. That means 'SMOVX' (64-bit register variant) is generated
       for all cases since it's compatible with our current usages of 'SMOVW'.
       Existing usages of smov have been double checked and this patch does
       not affect them.
    
    3. Smoke tests are also added.
    
    [1]. https://developer.arm.com/docs/ddi0602/f/simd-and-floating-point-instructions-alphabetic-order/smov-signed-move-vector-element-to-general-purpose-register
    [2]. https://developer.arm.com/docs/ddi0602/f/simd-and-floating-point-instructions-alphabetic-order/umov-unsigned-move-vector-element-to-general-purpose-register

--- src/hotspot/cpu/aarch64/assembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/assembler_aarch64.hpp
@@ -2652,17 +2652,21 @@ public:
     f(sidx<<(int)T, 14, 11), f(1, 10), rf(Vn, 5), rf(Vd, 0);
   }
 
-#define INSN(NAME, op)                                                     \
-  void NAME(Register Rd, FloatRegister Vn, SIMD_RegVariant T, int idx) {   \
-    starti;                                                                \
-    f(0, 31), f(T==D ? 1:0, 30), f(0b001110000, 29, 21);                   \
-    f(((idx<<1)|1)<<(int)T, 20, 16), f(op, 15, 10);                        \
-    rf(Vn, 5), rf(Rd, 0);                                                  \
+  void umov(Register Rd, FloatRegister Vn, SIMD_RegVariant T, int idx) {
+    starti;
+    assert(T != Q, "invalid register variant");
+    f(0, 31), f(T==D ? 1:0, 30), f(0b001110000, 29, 21);
+    f(((idx<<1)|1)<<(int)T, 20, 16), f(0b001111, 15, 10);
+    rf(Vn, 5), rf(Rd, 0);
   }
 
-  INSN(umov, 0b001111);
-  INSN(smov, 0b001011);
-#undef INSN
+  void smov(Register Rd, FloatRegister Vn, SIMD_RegVariant T, int idx) {
+    starti;
+    assert(T < D, "invalid register variant");
+    f(0, 31), f(1, 30), f(0b001110000, 29, 21);
+    f(((idx<<1)|1)<<(int)T, 20, 16), f(0b001011, 15, 10);
+    rf(Vn, 5), rf(Rd, 0);
+  }
 
 #define INSN(NAME, opc, opc2, isSHR)                                    \
   void NAME(FloatRegister Vd, SIMD_Arrangement T, FloatRegister Vn, int shift){ \
