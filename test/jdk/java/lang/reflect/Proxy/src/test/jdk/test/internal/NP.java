56b15fbbcc7c04252f2712d859ff7b820b7c79ad
Mandy Chung <mchung@openjdk.org>
2020-12-01 17:23:07 +0000

    8159746: (proxy) Support for default methods
    Co-authored-by: Peter Levart <plevart@openjdk.org>
    Reviewed-by: darcy, alanb, plevart

--- test/jdk/java/lang/reflect/Proxy/src/test/jdk/test/internal/NP.java
+++ test/jdk/java/lang/reflect/Proxy/src/test/jdk/test/internal/NP.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,10 +21,13 @@
  * questions.
  */
 
-package p.one;
+package jdk.test.internal;
 
-public interface I {
-    void run();
-
-    default int m() { return 1; }
+/*
+ * Non-public interface
+ */
+interface NP {
+    default int m() {
+        throw new UnsupportedOperationException("non-public interface: " + NP.class.getName());
+    }
 }
