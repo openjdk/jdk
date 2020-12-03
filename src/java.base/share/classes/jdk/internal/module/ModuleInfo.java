f15dbb1b9467252436fd12b593f1f5c56e35e984
Alan Bateman <alan.bateman@oracle.com>
2020-12-03 09:52:02 +0000

    Restructure check to make it more obvious that it doesn't overflow

--- src/java.base/share/classes/jdk/internal/module/ModuleInfo.java
+++ src/java.base/share/classes/jdk/internal/module/ModuleInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -285,7 +285,7 @@ public final class ModuleInfo {
             }
 
             long newPosition = in.count();
-            if (newPosition != (initialPosition + length)) {
+            if ((newPosition - initialPosition) != length) {
                 // attribute length does not match actual attribute size
                 throw invalidModuleDescriptor("Attribute " + attribute_name
                         + " does not match its expected length");
