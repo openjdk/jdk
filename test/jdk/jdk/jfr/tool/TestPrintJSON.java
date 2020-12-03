053e3b4cd1228e3ef6ecdde53878344fb374f160
Erik Gahlin <erik.gahlin@gmail.com>
2020-12-03 12:17:07 +0100

    Remove unused imports

--- test/jdk/jdk/jfr/tool/TestPrintJSON.java
+++ test/jdk/jdk/jfr/tool/TestPrintJSON.java
@@ -25,11 +25,9 @@
 
 package jdk.jfr.tool;
 
-import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.time.OffsetDateTime;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
