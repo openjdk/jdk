97b67e94b4bd63b23228e2fbd623ba04f5ec917f
Daniel Fuchs <daniel.fuchs@oracle.com>
2020-12-04 17:37:42 +0000

    8236413: AbstractConnectTimeout should tolerate both NoRouteToHostException and UnresolvedAddressException
    The timeout tests extending AbstractConnectTimeout have been relaxed to accept a
    ConnectException wrapping NoRouteToHostException.
    These test have been observed failing intermittently with ConnectException wrapping
    java.nio.channels.UnresolvedAddressException instead - presumably a transient network
    failure caused this other exception to be thrown.
    
    The test logic should be relaxed to accept UnresolvedAddressException as possible
    cause in ConnectException too.

--- test/jdk/java/net/httpclient/AbstractConnectTimeout.java
+++ test/jdk/java/net/httpclient/AbstractConnectTimeout.java
@@ -33,6 +33,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.channels.UnresolvedAddressException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -142,11 +143,11 @@ public abstract class AbstractConnectTimeout {
                 long elapsedTime = NANOSECONDS.toMillis(System.nanoTime() - startTime);
                 out.printf("Client: received in %d millis%n", elapsedTime);
                 Throwable t = e.getCause().getCause();  // blocking thread-specific exception
-                if (!(t instanceof NoRouteToHostException)) { // tolerate only NRTHE
+                if (!isAcceptableCause(t)) { // tolerate only NRTHE or UAE
                     e.printStackTrace(out);
                     fail("Unexpected exception:" + e);
                 } else {
-                    out.printf("Caught ConnectException with NoRouteToHostException"
+                    out.printf("Caught ConnectException with "
                             + " cause: %s - skipping%n", t.getCause());
                 }
             }
@@ -194,15 +195,21 @@ public abstract class AbstractConnectTimeout {
                 long elapsedTime = NANOSECONDS.toMillis(System.nanoTime() - startTime);
                 out.printf("Client: received in %d millis%n", elapsedTime);
                 Throwable t = e.getCause();
-                if (t instanceof ConnectException &&
-                        t.getCause() instanceof NoRouteToHostException) { // tolerate only NRTHE
-                    out.printf("Caught ConnectException with NoRouteToHostException"
-                            + " cause: %s - skipping%n", t.getCause());
+                if (t instanceof ConnectException && isAcceptableCause(t.getCause())) {
+                    // tolerate only NRTHE and UAE
+                    out.printf("Caught ConnectException with "
+                            + "cause: %s - skipping%n", t.getCause());
                 } else {
                     assertExceptionTypeAndCause(t);
                 }
             }
         }
+    }
+
+    static boolean isAcceptableCause(Throwable cause) {
+        if (cause instanceof NoRouteToHostException) return true;
+        if (cause instanceof UnresolvedAddressException) return true;
+        return false;
     }
 
     static HttpClient newClient(Duration connectTimeout, ProxySelector proxy) {
