877e4b82b74ee61bb957fabb3575644bdc100074
Guoxiong Li <lgxbslgx@gmail.com>
2020-12-05 00:02:33 +0800

    8231622: SuppressWarning(serial) ignored on field serialVersionUID

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -5338,7 +5338,7 @@ public class Attr extends JCTree.Visitor {
                 && isSerializable(c.type)
                 && (c.flags() & (Flags.ENUM | Flags.INTERFACE)) == 0
                 && !c.isAnonymous()) {
-            checkSerialVersionUID(tree, c);
+            checkSerialVersionUID(tree, c, env);
         }
         if (allowTypeAnnos) {
             // Correctly organize the positions of the type annotations
@@ -5371,7 +5371,7 @@ public class Attr extends JCTree.Visitor {
         }
 
         /** Check that an appropriate serialVersionUID member is defined. */
-        private void checkSerialVersionUID(JCClassDecl tree, ClassSymbol c) {
+        private void checkSerialVersionUID(JCClassDecl tree, ClassSymbol c, Env<AttrContext> env) {
 
             // check for presence of serialVersionUID
             VarSymbol svuid = null;
@@ -5385,6 +5385,13 @@ public class Attr extends JCTree.Visitor {
             if (svuid == null) {
                 if (!c.isRecord())
                     log.warning(LintCategory.SERIAL, tree.pos(), Warnings.MissingSVUID(c));
+                return;
+            }
+
+            // Check if @SuppressWarnings("serial") is an annotation of serialVersionUID.
+            // See JDK-8231622 for more information.
+            Lint lint = env.info.lint.augment(svuid);
+            if (lint.isSuppressed(LintCategory.SERIAL)) {
                 return;
             }
 
