7c4d53bf7b464990fd65e5f252e7ab6b721ab9d3
Magnus Ihse Bursie <ihse@openjdk.org>
2020-12-07 15:55:53 +0000

    8257679: Improved unix compatibility layer in Windows build (winenv)
    Reviewed-by: erikj, jvernee, burban

--- make/scripts/extract-vs-env.cmd
+++ make/scripts/extract-vs-env.cmd
@@ -1,6 +1,6 @@
 @echo off
 REM
-REM Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+REM Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
 REM DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 REM
 REM This code is free software; you can redistribute it and/or modify it
@@ -21,4 +21,27 @@ REM Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
 REM or visit www.oracle.com if you need additional information or have any
 REM questions.
 REM
-if '%1' NEQ '' echo %~s1
+
+set vcvarscmd=%1
+set output=%2
+if not "%3" == "auto" set version=-vcvars_ver=%3
+
+set PATH_BEFORE=%PATH%
+
+call %vcvarscmd% %version% %4 %5 %6 %7 %8 %9
+if exist %output% del %output%
+
+call :extract "%PATH_BEFORE%", PATH_BEFORE
+call :extract "%PATH%", PATH_AFTER
+call :extract "%INCLUDE%", VS_INCLUDE
+call :extract "%LIB%", VS_LIB
+call :extract "%VCINSTALLDIR%", VCINSTALLDIR
+call :extract "%VCToolsRedistDir%", VCToolsRedistDir
+call :extract "%WindowsSdkDir%", WindowsSdkDir
+call :extract "%WINDOWSSDKDIR%", WINDOWSSDKDIR
+
+exit /b 0
+
+:extract
+echo %~2=$($BASH $TOPDIR/make/scripts/fixpath.sh -i import '%~1 ') >> %output%
+exit /b 0
