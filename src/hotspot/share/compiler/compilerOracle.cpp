cbc07869e3fd2455a9504f0478774ab0cd56ea92
Aleksey Shipilev <shade@redhat.com>
2020-12-07 20:42:28 +0100

    Check blackhole unlocking at startup

--- src/hotspot/share/compiler/compilerOracle.cpp
+++ src/hotspot/share/compiler/compilerOracle.cpp
@@ -286,6 +286,11 @@ static void register_command(TypedMethodOptionMatcher* matcher,
   }
   assert(CompilerOracle::option_matches_type(option, value), "Value must match option type");
 
+  if (option == CompileCommand::Blackhole && !UnlockDiagnosticVMOptions) {
+    warning("Blackhole compile option is diagnostic and must be enabled via -XX:+UnlockDiagnosticVMOptions");
+    return;
+  }
+
   matcher->init(option, option_list);
   matcher->set_value<T>(value);
   option_list = matcher;
@@ -413,15 +418,12 @@ bool CompilerOracle::should_blackhole(const methodHandle& method) {
   if (!check_predicate(CompileCommand::Blackhole, method)) {
     return false;
   }
-  if (!UnlockDiagnosticVMOptions) {
-    warning("Blackhole compile option is diagnostic and must be enabled via -XX:+UnlockDiagnosticVMOptions");
-    return false;
-  }
   if (method->result_type() != T_VOID) {
     warning("Blackhole compile option only works for methods with void type: %s",
             method->name_and_sig_as_C_string());
     return false;
   }
+  guarantee(UnlockDiagnosticVMOptions, "Checked during initial parsing");
   return true;
 }
 
