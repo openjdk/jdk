85f689a8fd82bc2f65545290c0515c5cade709df
Michael-Mc-Mahon <michael.x.mcmahon@oracle.com>
2020-12-10 13:07:41 +0000

    removed unneeded import

--- test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/StateTestService.java
+++ test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/StateTestService.java
@@ -51,7 +51,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static java.nio.file.StandardOpenOption.APPEND;
-import static java.nio.file.StandardOpenOption.WRITE;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 public class StateTestService {
