9208263f04805d26d961aa98f4dd9a85776e5149
hltj <jiaywe@gmail.com>
2020-12-14 20:35:52 +0800

    merge test cases & introduce more cases with anonymous cases enabled

---
+++ test/langtools/tools/javac/lambda/localClass/CaptureVariables.java
@@ -0,0 +1,65 @@
+import java.util.function.Supplier;
+
+/**
+ * @test
+ */
+public class CaptureVariables {
+    static Supplier<Integer> supplier1 = () -> {
+        boolean b0 = false;
+        String s0 = "hello";
+        boolean b1 = false;
+        int i0 = 6;
+
+        class Local {
+            int i = s0.length() + i0;
+        }
+
+        return ((Supplier<Integer>) () -> new Local().i).get();
+    };
+
+    static Supplier<Integer> supplier2 = () -> {
+        boolean b0 = false;
+        String s0 = "hello";
+        boolean b1 = false;
+        int i0 = 6;
+
+        class Local {
+            int i = s0.length() + i0;
+        }
+
+        return ((Supplier<Integer>) () -> ((Supplier<Integer>) () -> new Local().i).get()).get();
+    };
+
+    Supplier<Integer> supplier3 = () -> {
+        boolean b0 = false;
+        String s0 = "hello";
+        boolean b1 = false;
+        int i0 = 6;
+
+        class Local {
+            int i = s0.length() + i0;
+        }
+
+        return ((Supplier<Integer>) () -> new Local().i).get();
+    };
+
+    Supplier<Integer> supplier4 = () -> {
+        boolean b0 = false;
+        String s0 = "hello";
+        boolean b1 = false;
+        int i0 = 6;
+
+        class Local {
+            int i = s0.length() + i0;
+        }
+
+        return ((Supplier<Integer>) () -> ((Supplier<Integer>) () -> new Local().i).get()).get();
+    };
+
+    public static void main(String[] args) {
+        assert supplier1.get() == 11;
+        assert supplier2.get() == 11;
+        assert new CaptureVariables().supplier3.get() == 11;
+        assert new CaptureVariables().supplier4.get() == 11;
+    }
+}
