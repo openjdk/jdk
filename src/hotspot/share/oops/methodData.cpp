7ff9c85639ace351b6d99c9916e3afe46ac9c69d
Maurizio Cimadamore <mcimadamore@openjdk.org>
2020-12-15 13:50:40 +0000

    8258242: Type profile pollution occurs when memory segments of different kinds are used
    Reviewed-by: vlivanov, redestad

--- src/hotspot/share/oops/methodData.cpp
+++ src/hotspot/share/oops/methodData.cpp
@@ -1600,6 +1600,18 @@ bool MethodData::profile_unsafe(const methodHandle& m, int bci) {
   return false;
 }
 
+bool MethodData::profile_memory_access(const methodHandle& m, int bci) {
+  Bytecode_invoke inv(m , bci);
+  if (inv.is_invokestatic()) {
+    if (inv.klass() == vmSymbols::jdk_incubator_foreign_MemoryAccess()) {
+      if (inv.name()->starts_with("get") || inv.name()->starts_with("set")) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
 int MethodData::profile_arguments_flag() {
   return TypeProfileLevel % 10;
 }
@@ -1626,6 +1638,10 @@ bool MethodData::profile_arguments_for_invoke(const methodHandle& m, int bci) {
   }
 
   if (profile_unsafe(m, bci)) {
+    return true;
+  }
+
+  if (profile_memory_access(m, bci)) {
     return true;
   }
 
