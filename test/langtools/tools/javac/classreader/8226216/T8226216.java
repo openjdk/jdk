a074937f8628276f7d7474009410c9219d0f03f1
Guoxiong Li <lgxbslgx@gmail.com>
2020-12-25 15:20:55 +0800

    Modify legal header. Fix typo.

--- test/langtools/tools/javac/classreader/8226216/T8226216.java
+++ test/langtools/tools/javac/classreader/8226216/T8226216.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -120,7 +118,7 @@ public class T8226216 extends TestRunner {
     }
 
     @Test
-    public void testParameterModifiersNotVisiable() throws Exception {
+    public void testParameterModifiersNotVisible() throws Exception {
         Path base = Path.of(".");
         Path pluginPath = base.resolve("plugin");
         Path testPath = base.resolve("test");
