94f6f859e1a91adb7fb32bbabb1fc1423dae2cbd
Alexander Zuev <alexander.zuev@oracle.com>
2021-01-04 01:36:12 -0800

    8258643: javax/swing/JComponent/7154030/bug7154030.java failed with "Exception: Failed to hide opaque button"
    By taking into acount frame insets the instability caused by the
    variations in window border painting is eliminated.

--- test/jdk/javax/swing/JComponent/7154030/bug7154030.java
+++ test/jdk/javax/swing/JComponent/7154030/bug7154030.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -98,10 +98,11 @@ public class bug7154030 {
             robot.waitForIdle(1000);
 
             Rectangle bounds = frame.getBounds();
-            locx = bounds.x;
-            locy = bounds.y;
-            frw = bounds.width;
-            frh = bounds.height;
+            Insets insets = frame.getInsets();
+            locx = bounds.x + insets.left;
+            locy = bounds.y + insets.top;
+            frw = bounds.width - insets.left - insets.right;
+            frh = bounds.height - insets.top - insets.bottom;
 
             imageInit = robot.createScreenCapture(new Rectangle(locx, locy, frw, frh));
 
