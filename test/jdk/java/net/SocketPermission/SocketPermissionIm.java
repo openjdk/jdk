5ec81526f0e783aedad4f909dd60d20de5f704b2
Jayashree S Kumar <jayashreesk@in.ibm.com>
2021-01-05 14:45:28 +0530

    Added testcase and corrected WhiteSpace tab error

---
+++ test/jdk/java/net/SocketPermission/SocketPermissionIm.java
@@ -0,0 +1,51 @@
+/*
+ * @test
+ * @bug 8243376
+ * @summary SocketPermission implies(Permission p) spec allows, If the object was initialized with a single IP address and one of p's IP addresses is equal to this object's IP address. 
+ * @run java -Dsun.net.inetaddr.ttl=0 SocketPermissionIm
+ */
+
+import java.net.SocketPermission;
+import java.net.InetAddress;
+import java.io.*;
+
+public class SocketPermissionIm {
+           
+    
+    public static void main(String[] args) throws Exception {
+	     
+	     String hostname = "www.exmp.com";
+             String hostsFileName = System.getProperty("test.src", ".") + "/Host.txt";
+	     System.setProperty("jdk.net.hosts.file", hostsFileName); 
+
+             int testPass = 0;
+
+             SocketPermission sp = new SocketPermission(hostname, "connect,resolve");
+	     	     
+	     do{
+		     if (!sp.implies(new SocketPermission(hostname, "connect,resolve"))) {
+                              System.out.println("Expected true, returned false");
+                              System.exit(0);
+                     }
+
+                     addIpToHostsFile(hostname, "1.2.3."+testPass, hostsFileName, true); 
+                     Thread.sleep(1000);
+
+		     testPass++;
+         
+	      }while(testPass <= 2);
+    }
+
+    private  static void addIpToHostsFile( String host,
+                                               String addr,
+                                               String hostsFileName,
+                                               boolean append)
+                                               throws Exception {
+        String mapping = addr + " " + host;
+        RandomAccessFile f = new RandomAccessFile(new File(hostsFileName), "rw");
+            f.seek(0);
+            f.write(mapping.getBytes());
+	    f.close();
+    }
+
+}
