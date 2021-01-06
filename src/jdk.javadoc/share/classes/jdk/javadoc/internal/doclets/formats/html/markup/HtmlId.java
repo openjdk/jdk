e8a1e0747eca4d0c85c28d0d952a162bf64779fc
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2021-01-05 17:13:52 -0800

    fix whitespace; add doc comments

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/markup/HtmlId.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/markup/HtmlId.java
@@ -29,13 +29,22 @@ package jdk.javadoc.internal.doclets.formats.html.markup;
  * A type-safe wrapper around a {@code String}, for use as an "id"
  * in {@code HtmlTree} objects.
  *
- * @see HtmlTree#setId(HtmlId) 
+ * @see HtmlTree#setId(HtmlId)
  */
 public interface HtmlId {
+    /**
+     * Creates an id with the given name.
+     *
+     * @param name the name
+     * @return the id
+     */
     static HtmlId of(String name) {
         assert name.indexOf(' ') == -1;
         return () -> name;
     }
 
+    /**
+     * {@return the name of the id}
+     */
     String name();
 }
