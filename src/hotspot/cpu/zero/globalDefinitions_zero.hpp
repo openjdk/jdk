6a6a6d3a0a2b182a578c780ec1b63c19fe133144
Aleksey Shipilev <shade@redhat.com>
2021-01-07 16:33:12 +0100

    Update copyright year

--- src/hotspot/cpu/zero/globalDefinitions_zero.hpp
+++ src/hotspot/cpu/zero/globalDefinitions_zero.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
- * Copyright 2009, 2015, Red Hat, Inc.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2009, 2021, Red Hat, Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
