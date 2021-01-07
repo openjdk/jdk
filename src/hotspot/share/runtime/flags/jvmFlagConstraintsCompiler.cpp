8d3d75ca98f46a502ebacd4d24b18625e9d3a8ff
Igor Veresov <igor.veresov@oracle.com>
2021-01-07 11:35:23 -0800

    Fix copyright years

--- src/hotspot/share/runtime/flags/jvmFlagConstraintsCompiler.cpp
+++ src/hotspot/share/runtime/flags/jvmFlagConstraintsCompiler.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -410,3 +410,4 @@ JVMFlag::Error ControlIntrinsicConstraintFunc(ccstrlist value, bool verbose) {
 
   return JVMFlag::SUCCESS;
 }
+
