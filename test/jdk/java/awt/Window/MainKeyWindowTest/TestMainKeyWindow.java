620977af3cdcb2a2ee3245531e8b71721e065098
mrserb <14138494+mrserb@users.noreply.github.com>
2021-01-06 18:30:13 -0800

    Update TestMainKeyWindow.java

--- test/jdk/java/awt/Window/MainKeyWindowTest/TestMainKeyWindow.java
+++ test/jdk/java/awt/Window/MainKeyWindowTest/TestMainKeyWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -389,7 +389,6 @@ public class TestMainKeyWindow
             mb.add(new Menu("Hello"));
             frame.setMenuBar(mb);
             frame.setBounds(400, 180, 1068, 821);
-            frame.setLocationRelativeTo(null);
             frame.setVisible(true);
             frame.toFront();
             Thread.sleep(20_000);
