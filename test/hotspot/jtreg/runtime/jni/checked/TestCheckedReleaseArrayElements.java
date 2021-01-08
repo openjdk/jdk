93b6f54808f15f4bc47fd1b279850f974c772cbc
David Holmes <david.holmes@oracle.com>
2021-01-08 17:14:14 -0500

    Add comment as requested by Dan

--- test/hotspot/jtreg/runtime/jni/checked/TestCheckedReleaseArrayElements.java
+++ test/hotspot/jtreg/runtime/jni/checked/TestCheckedReleaseArrayElements.java
@@ -44,6 +44,8 @@ public class TestCheckedReleaseArrayElements {
         if (args == null || args.length == 0) {
             test();
         } else {
+            // Uses executeProcess() instead of executeTestJvm() to avoid passing options
+            // that might generate output on stderr (which should be empty for this test).
             ProcessBuilder pb =
                 ProcessTools.createJavaProcessBuilder("-Xcheck:jni",
                                                       "-Djava.library.path=" + Utils.TEST_NATIVE_PATH,
