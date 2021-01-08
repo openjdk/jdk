40aee0c44e7e425faa21ffc57c7b51db342319fd
Johannes Kuhn <info@j-kuhn.de>
2021-01-08 10:46:24 +0100

    * Fix Whitespace V2

--- test/jdk/jdk/modules/scenarios/automaticmodules/src/somelibTest/somelib/test/TestMain.java
+++ test/jdk/jdk/modules/scenarios/automaticmodules/src/somelibTest/somelib/test/TestMain.java
@@ -34,14 +34,14 @@ import java.lang.module.ModuleDescriptor;
 public class TestMain {
     public static void main(String[] args) {
         ModuleDescriptor ownDesc = TestMain.class.getModule().getDescriptor();
-        
-		assertThat(ownDesc.isAutomatic(), "Expected to be executed in an automatic module");
+
+        assertThat(ownDesc.isAutomatic(), "Expected to be executed in an automatic module");
         assertThat(ownDesc.requires().stream().anyMatch(
                 r -> r.name().equals("java.base") && r.modifiers().contains(ModuleDescriptor.Requires.Modifier.MANDATED)),
                 "requires mandated java.base");
         assertThat(Dummy.returnTrue(), "Dummy.returnTrue returns true");
-	}
-    
+    }
+
     private static void assertThat(boolean expected, String message) {
         if (!expected)
             throw new AssertionError(message);
