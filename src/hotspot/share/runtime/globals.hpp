be57cf149e334fa468d6944820b5d86f35d21d2c
Daniel D. Daugherty <dcubed@openjdk.org>
2021-01-14 14:23:27 +0000

    8226416: MonitorUsedDeflationThreshold can cause repeated async deflation requests
    Reviewed-by: dholmes, coleenp

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -733,6 +733,11 @@ const intx ObjectAlignmentInBytes = 8;
           "or AsyncDeflationInterval.")                                     \
           range(0, 100)                                                     \
                                                                             \
+  product(uintx, NoAsyncDeflationProgressMax, 3, DIAGNOSTIC,                \
+          "Max number of no progress async deflation attempts to tolerate " \
+          "before adjusting the in_use_list_ceiling up (0 is off).")        \
+          range(0, max_uintx)                                               \
+                                                                            \
   product(intx, hashCode, 5, EXPERIMENTAL,                                  \
                "(Unstable) select hashCode generation algorithm")           \
                                                                             \
