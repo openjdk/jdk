4c8eacd1d78cec422ca236c0f1f6aafbe3bc11c5
Claes Redestad <claes.redestad@oracle.com>
2021-01-16 02:14:23 +0100

    Add missing import (who needs IDEs?)

--- src/java.base/share/classes/java/lang/StringCoding.java
+++ src/java.base/share/classes/java/lang/StringCoding.java
@@ -30,6 +30,7 @@ import java.lang.ref.SoftReference;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.CoderResult;
