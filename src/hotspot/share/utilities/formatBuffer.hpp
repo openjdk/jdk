f3cce1ba6d4b8534b114faec48fc00dd26f84d3d
iklam <ilam3388@gmail.com>
2021-01-19 20:00:42 -0800

    8259894: refactor parts of jvm.h into jvm_io.h and jvm_constants.h

--- src/hotspot/share/utilities/formatBuffer.hpp
+++ src/hotspot/share/utilities/formatBuffer.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@
 #ifndef SHARE_UTILITIES_FORMATBUFFER_HPP
 #define SHARE_UTILITIES_FORMATBUFFER_HPP
 
-#include "jvm.h"
+#include "jvm_io.h"
 #include "utilities/globalDefinitions.hpp"
 #include <stdarg.h>
 
