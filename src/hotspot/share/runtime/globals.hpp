21e64e92e7979c1c2dd8909c30f3d20ac46f7c77
Ralf Schmelter <ralf.schmelter@sap.com>
2021-01-25 13:31:05 +0000

    Added option to compress the heap dump created by -XX:+HeapDumpOnOutOfMemoryError

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -554,6 +554,12 @@ const intx ObjectAlignmentInBytes = 8;
           "directory) of the dump file (defaults to java_pid<pid>.hprof "   \
           "in the working directory)")                                      \
                                                                             \
+  product(intx, HeapDumpGzipLevel, -1, MANAGEABLE,                          \
+          "When HeapDumpOnOutOfMemoryError is on, the gzip compression "    \
+          "level of the dump file. -1 disables gzip compression. "          \
+          "Otherwise the level must be between 0 and 10.")                  \
+          range(-1, 10)                                                     \
+                                                                            \
   product(ccstr, NativeMemoryTracking, "off",                               \
           "Native memory tracking options")                                 \
                                                                             \
