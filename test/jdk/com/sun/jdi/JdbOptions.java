ac0b08b04752f9104dd55332bf5b75066bd5361d
Alex Menkov <alexey.menkov@oracle.com>
2021-01-29 15:59:58 -0800

    Redirect debuggee output to file

--- test/jdk/com/sun/jdi/JdbOptions.java
+++ test/jdk/com/sun/jdi/JdbOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,9 +34,17 @@ import lib.jdb.Jdb;
 import lib.jdb.JdbCommand;
 import jdk.test.lib.process.OutputAnalyzer;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.lang.management.ManagementFactory;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 class JbdOptionsTarg {
     static final String OK_MSG = "JbdOptionsTarg: OK";
@@ -49,22 +57,32 @@ class JbdOptionsTarg {
         return "prop[" + name + "] = >" + value + "<";
     }
 
-    public static void main(String[] args) {
-        System.out.println(OK_MSG);
-        // print all args
-        List<String> vmArgs = ManagementFactory.getRuntimeMXBean().getInputArguments();
-        for (String s: vmArgs) {
-            System.out.println(argString(s));
-        }
-        // print requested sys.props
-        for (String p: args) {
-            System.out.println(propString(p, System.getProperty(p)));
+    /**
+     * 1st argument is a filename to redirect application output,
+     * the rest are names of the properties to dump.
+     */
+    public static void main(String[] args) throws IOException {
+        String outFile = args[0];
+        try (PrintStream out = new PrintStream(outFile, StandardCharsets.UTF_8)) {
+            out.println(OK_MSG);
+            // print all args
+            List<String> vmArgs = ManagementFactory.getRuntimeMXBean().getInputArguments();
+            for (String s : vmArgs) {
+                out.println(argString(s));
+            }
+            // print requested sys.props (skip 1st arg which is output filename)
+            for (int i=1; i < args.length; i++) {
+                String p = args[i];
+                out.println(propString(p, System.getProperty(p)));
+            }
         }
     }
 }
 
 public class JdbOptions {
-    private static final String targ = JbdOptionsTarg.class.getName();
+    private static final String outFilename = UUID.randomUUID().toString() + ".out";
+    private static final Path outPath = Paths.get(outFilename);
+    private static final String targ = JbdOptionsTarg.class.getName() + " " + outFilename;
 
     public static void main(String[] args) throws Exception {
         // the simplest case
@@ -151,12 +169,18 @@ public class JdbOptions {
                     .map(s -> s.replace("\"", "\\\""))
                     .toArray(String[]::new);
         }
+
         try (Jdb jdb = new Jdb(args)) {
             jdb.waitForSimplePrompt(1024, true); // 1024 lines should be enough
             jdb.command(JdbCommand.run().allowExit());
-            OutputAnalyzer out = new OutputAnalyzer(jdb.getJdbOutput());
-            out.shouldContain(JbdOptionsTarg.OK_MSG);
-            return new TestResult(out);
         }
+        String output = Files.readAllLines(outPath, StandardCharsets.UTF_8).stream()
+                .collect(Collectors.joining(System.getProperty("line.separator")));
+        Files.deleteIfExists(outPath);
+        System.out.println("Debuggee output: [");
+        System.out.println(output);
+        System.out.println("]");
+        OutputAnalyzer out = new OutputAnalyzer(output);
+        return new TestResult(out);
     }
 }
