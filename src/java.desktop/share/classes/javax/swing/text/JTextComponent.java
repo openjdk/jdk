7bbd27ae6c29677c4f423c54fbabf42c65df3fca
Denis Konoplev <denis.konoplev@jetbrains.com>
2021-02-01 21:12:22 +0300

    8260719: Input method character moves with caret

--- src/java.desktop/share/classes/javax/swing/text/JTextComponent.java
+++ src/java.desktop/share/classes/javax/swing/text/JTextComponent.java
@@ -4786,6 +4786,9 @@ public abstract class JTextComponent extends JComponent implements Scrollable, A
         AttributedCharacterIterator text = e.getText();
         int composedTextIndex;
 
+        boolean isCaretMoved = false;
+        int caretPositionToRestore = 0;
+
         // old composed text deletion
         Document doc = getDocument();
         if (composedTextExists()) {
@@ -4793,6 +4796,15 @@ public abstract class JTextComponent extends JComponent implements Scrollable, A
                 doc.remove(composedTextStart.getOffset(),
                            composedTextEnd.getOffset() -
                            composedTextStart.getOffset());
+                isCaretMoved = caret.getDot() != composedTextStart.getOffset();
+                if (isCaretMoved) {
+                    caretPositionToRestore = caret.getDot();
+                    // if caret set furter in the doc, we should add commitCount
+                    if (caretPositionToRestore > composedTextStart.getOffset()) {
+                        caretPositionToRestore += commitCount;
+                    }
+                    caret.setDot(composedTextStart.getOffset());
+                }
             } catch (BadLocationException ble) {}
             composedTextStart = composedTextEnd = null;
             composedTextAttribute = null;
@@ -4867,6 +4879,10 @@ public abstract class JTextComponent extends JComponent implements Scrollable, A
                 latestCommittedTextStart =
                     latestCommittedTextEnd = null;
             }
+        }
+
+        if (isCaretMoved) {
+            caret.setDot(caretPositionToRestore);
         }
     }
 
