7cffd436265bd82f3ec06c91a914fea914977e53
iklam <ilam3388@gmail.com>
2021-02-02 08:44:09 -0800

    updated copyright

--- src/hotspot/cpu/ppc/runtime_ppc.cpp
+++ src/hotspot/cpu/ppc/runtime_ppc.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012, 2018 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
