612ee6cdf3835fc81fa7bf47a7e5ab9d751f907b
denghui.ddh <denghui.ddh@alibaba-inc.com>
2021-02-03 21:46:06 +0800

    improve test

--- test/jdk/jdk/jfr/api/consumer/TestChunkInputStreamAvailable.java
+++ test/jdk/jdk/jfr/api/consumer/TestChunkInputStreamAvailable.java
@@ -28,47 +28,29 @@
  */
 package jdk.jfr.api.consumer;
 
-import java.io.File;
 import java.io.InputStream;
-import java.nio.file.Path;
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
-import jdk.jfr.FlightRecorder;
 import jdk.jfr.Recording;
 import jdk.test.lib.Asserts;
-import jdk.test.lib.jfr.EventNames;
 
 public class TestChunkInputStreamAvailable {
 
     public static void main(String[] args) throws Exception {
         try (Recording r = new Recording()) {
-            r.enable(EventNames.JavaThreadStatistics)
-             .withPeriod(Duration.ofMillis(100));
             r.start();
-            File repository = Path.of(System.getProperty("jdk.jfr.repository")).toFile();
-            for (int i = 0; i < 5; i++) {
-                TimeUnit.SECONDS.sleep(1);
-                try (Recording snapshot = FlightRecorder.getFlightRecorder().takeSnapshot()) {
-                    InputStream stream = snapshot.getStream(null, null);
-                    File[] jfrs = repository.listFiles(f -> f.getName().endsWith(".jfr"));
-                    Arrays.sort(jfrs);
-
-                    int size = 0;
-                    // the last file isn't counted in
-                    for (int j = 0; j < jfrs.length - 1; j++) {
-                        size += (int)jfrs[j].length();
-                    }
-                    Asserts.assertEquals(stream.available(), size);
-                    int rc = new Random().nextInt(size);
-                    int left = size - rc;
-                    while (rc-- > 0) {
-                         stream.read();
-                    }
-                    Asserts.assertEquals(stream.available(), left);
+            try (Recording s = new Recording()) {
+                s.start();
+                s.stop();
+            }
+            r.stop();
+            try (InputStream stream = r.getStream(null, null)) {
+                int left = stream.available();
+                Asserts.assertEquals(r.getSize(), (long) left);
+                while (stream.read() != -1) {
+                    left--;
+                    Asserts.assertEquals(left, stream.available());
                 }
+                Asserts.assertEquals(0, left);
             }
         }
     }
