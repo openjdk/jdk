b9d4211bc1aa92e257ddfe86c7a2b4e4e60598a0
Ioi Lam <iklam@openjdk.org>
2021-02-03 03:56:35 +0000

    8260193: Remove JVM_GetInterfaceVersion() and JVM_DTraceXXX
    Reviewed-by: alanb, lfoltan, gziemski, ihse

--- src/hotspot/share/prims/jvm.cpp
+++ src/hotspot/share/prims/jvm.cpp
@@ -94,7 +94,6 @@
 #include "services/threadService.hpp"
 #include "utilities/copy.hpp"
 #include "utilities/defaultStream.hpp"
-#include "utilities/dtrace.hpp"
 #include "utilities/events.hpp"
 #include "utilities/macros.hpp"
 #include "utilities/utf8.hpp"
@@ -227,15 +226,6 @@ void trace_class_resolution(Klass* to_class) {
     CLEAR_PENDING_EXCEPTION;
   }
 }
-
-
-// Interface version /////////////////////////////////////////////////////////////////////
-
-
-JVM_LEAF(jint, JVM_GetInterfaceVersion())
-  return JVM_INTERFACE_VERSION;
-JVM_END
-
 
 // java.lang.System //////////////////////////////////////////////////////////////////////
 
