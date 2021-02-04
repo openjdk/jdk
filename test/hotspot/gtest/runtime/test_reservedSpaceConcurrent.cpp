d4942c27b6b9d18daf209a8292175ce2ab4ed0bf
M. Seledtsov <mikhailo.seledtsov@oracle.com>
2021-02-03 20:13:26 -0800

    Convert ReservedSpaceConcurrent test to GTest - initial drop

---
+++ test/hotspot/gtest/runtime/test_reservedSpaceConcurrent.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#include "precompiled.hpp"
+#include "runtime/interfaceSupport.inline.hpp"
+#include "threadHelper.inline.hpp"
+#include "unittest.hpp"
+
+
+class UnitTestThread : public JavaTestThread {
+  public:
+  UnitTestThread(Semaphore* post) : JavaTestThread(post) {}
+  virtual ~UnitTestThread() {}
+  void main_run() {
+    for (int i = 0; i < 10; i++) {
+      // TODO: invoke the actual unit test code here
+      // os::snprintf(variable_name, LENGTH, "some_variable %d", i);
+    }
+  }
+};
+
+#define TEST_THREAD_COUNT 5
+
+class DriverThread : public JavaTestThread {
+public:
+  Semaphore _done;
+  DriverThread(Semaphore* post) : JavaTestThread(post) { };
+  virtual ~DriverThread(){}
+
+  void main_run() {
+    Semaphore done(0);
+
+
+    UnitTestThread* st[TEST_THREAD_COUNT];
+    for (int i = 0; i < TEST_THREAD_COUNT; i++) {
+      st[i] = new UnitTestThread(&done);
+      st[i]->doit();
+    }
+
+    for (int i = 0; i < TEST_THREAD_COUNT; i++) {
+      done.wait();
+    }
+  }
+};
+
+TEST_VM(ReservedSpaceConcurrent, test_concurrent_threads) {
+  mt_test_doer<DriverThread>();
+}
