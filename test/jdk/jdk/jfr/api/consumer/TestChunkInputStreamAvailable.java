c56e21f865ad58b2c732da14a242ee27e905aec4
Daniel D. Daugherty <daniel.daugherty@oracle.com>
2021-02-04 17:37:20 -0500

    8261190: restore original Alibaba copyright line in two files

--- test/jdk/jdk/jfr/api/consumer/TestChunkInputStreamAvailable.java
+++ test/jdk/jdk/jfr/api/consumer/TestChunkInputStreamAvailable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Alibaba Group Holding Limited. All rights reserved.
+ * Copyright (c) 2021, Alibaba Group Holding Limited. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
