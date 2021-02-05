3923fde37d65b3141e32a5db9c6159dce61cdf9e
Harold Seigel <harold.seigel@oracle.com>
2021-02-05 14:38:12 +0000

    8261161: Clean up warnings in hotspot/jtreg/vmTestbase tests

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/AttachOnDemand/attach001/attach001TestRunner.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/AttachOnDemand/attach001/attach001TestRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -80,7 +80,7 @@ public class attach001TestRunner extends AODTestRunner {
             try {
                 new TargetApplicationWaitingAgents().runTargetApplication(new String[] {
                         "-" + AODTargetArgParser.agentsNumberParam,
-                        new Integer(argParser.getAgents().size()).toString() });
+                        Integer.valueOf(argParser.getAgents().size()).toString() });
 
                 finishedSuccessfully = true;
             } catch (Throwable t) {
