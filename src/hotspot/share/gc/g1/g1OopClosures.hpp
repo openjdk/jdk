946660de937f7affde72db58dc667cc34496a46b
Albert Yang <albert.m.yang@oracle.com>
2021-02-08 12:09:04 +0100

    bool

--- src/hotspot/share/gc/g1/g1OopClosures.hpp
+++ src/hotspot/share/gc/g1/g1OopClosures.hpp
@@ -156,13 +156,7 @@ enum G1Barrier {
   G1BarrierNoOptRoots  // Do not collect optional roots.
 };
 
-enum G1Mark {
-  G1MarkNone,
-  G1MarkFromRoot,
-  G1MarkPromotedFromRoot
-};
-
-template <G1Barrier barrier, G1Mark do_mark_object>
+template <G1Barrier barrier, bool should_mark>
 class G1ParCopyClosure : public G1ParCopyHelper {
 public:
   G1ParCopyClosure(G1CollectedHeap* g1h, G1ParScanThreadState* par_scan_state) :
