826c5fb8d7772892fa6d55b0d9adc0e835e3ff5a
ganjianxuan <ganjianxuan@oppo.com>
2021-02-09 18:37:38 +0800

    src/jdk.management/linux/native/libmanagement_ext/UnixOperatingSystem.c
    Fix the dead loop
    Summary: If the /proc/stat mount point is changed in container environment, the while loop may lead to 100% cpu usage.

--- src/jdk.management/linux/native/libmanagement_ext/UnixOperatingSystem.c
+++ src/jdk.management/linux/native/libmanagement_ext/UnixOperatingSystem.c
@@ -63,7 +63,10 @@ static struct perfbuf {
 #define DEC_64 "%"SCNd64
 
 static void next_line(FILE *f) {
-    while (fgetc(f) != '\n');
+    int c;
+    do {
+        c = fgetc(f);
+    } while (c != '\n' && c != EOF);
 }
 
 /**
