b38d5be8878127aa609adda50a8e7a375f6805dc
Harold Seigel <hseigel@openjdk.org>
2021-02-09 13:16:26 +0000

    8261340: Fix 'deprecated' warnings in the vmTestbase/nsk tests
    Reviewed-by: lfoltan, sspitsyn

--- test/hotspot/jtreg/vmTestbase/nsk/share/jdi/Binder.java
+++ test/hotspot/jtreg/vmTestbase/nsk/share/jdi/Binder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -93,6 +93,7 @@ public class Binder extends DebugeeBinder {
      *              <code>Binder(ArgumentHandler,Log)</code>
      *              constructor.
      */
+    @Deprecated
     public Binder (String args[]) {
         this(args, new Log(System.err));
     }
@@ -105,6 +106,7 @@ public class Binder extends DebugeeBinder {
      *              <code>Binder(ArgumentHandler,Log)</code>
      *              constructor.
      */
+    @Deprecated
     public Binder (String args[], Log log) {
         this(new ArgumentHandler(args), log);
     }
