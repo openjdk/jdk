67fb9553332913f171eb6ecdd9e28469e7bd0d76
Aleksey Shipilev <shade@redhat.com>
2021-02-10 11:12:37 +0100

    8261496: Shenandoah: reconsider pacing updates memory ordering

--- src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
@@ -100,7 +100,7 @@ void ShenandoahControlThread::run_service() {
     bool implicit_gc_requested = _gc_requested.is_set() && !is_explicit_gc(_requested_gc_cause);
 
     // This control loop iteration have seen this much allocations.
-    size_t allocs_seen = Atomic::xchg(&_allocs_seen, (size_t)0);
+    size_t allocs_seen = Atomic::xchg(&_allocs_seen, (size_t)0, memory_order_relaxed);
 
     // Check if we have seen a new target for soft max heap size.
     bool soft_max_changed = check_soft_max_changed();
@@ -595,7 +595,7 @@ void ShenandoahControlThread::notify_heap_changed() {
 
 void ShenandoahControlThread::pacing_notify_alloc(size_t words) {
   assert(ShenandoahPacing, "should only call when pacing is enabled");
-  Atomic::add(&_allocs_seen, words);
+  Atomic::add(&_allocs_seen, words, memory_order_relaxed);
 }
 
 void ShenandoahControlThread::set_forced_counters_update(bool value) {
