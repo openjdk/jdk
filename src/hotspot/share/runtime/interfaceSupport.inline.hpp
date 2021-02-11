01633cda2ddb30d879e3abd5f9f5065cafcf9c75
iklam <ilam3388@gmail.com>
2021-02-10 19:53:22 -0800

    interfaceSupport.inline.hpp needs VM_Exit from vmOperations.hpp for JVM_LEAF macro

--- src/hotspot/share/runtime/interfaceSupport.inline.hpp
+++ src/hotspot/share/runtime/interfaceSupport.inline.hpp
@@ -33,6 +33,7 @@
 #include "runtime/safepointMechanism.inline.hpp"
 #include "runtime/safepointVerifiers.hpp"
 #include "runtime/thread.hpp"
+#include "runtime/vmOperations.hpp"
 #include "utilities/globalDefinitions.hpp"
 #include "utilities/macros.hpp"
 #include "utilities/preserveException.hpp"
