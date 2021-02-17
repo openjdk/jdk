0f220ee3e6cfeed7c773d6306273faa0c03e58cb
Lutz Schmidt <Lutz.Schmidt@sap.com>
2021-02-17 19:17:35 +0100

    update copyright year

--- src/hotspot/cpu/ppc/vtableStubs_ppc_64.cpp
+++ src/hotspot/cpu/ppc/vtableStubs_ppc_64.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1997, 2018, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012, 2018 SAP SE. All rights reserved.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
