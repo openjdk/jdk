5f308291a0c8b3439fe1ef2d72aa61394cef58cc
Calvin Cheung <ccheung@openjdk.org>
2021-02-18 02:51:24 +0000

    8202750: Reduce the use of get_canonical_path() in CDS
    Reviewed-by: minqi, iklam

--- src/hotspot/os/windows/os_windows.cpp
+++ src/hotspot/os/windows/os_windows.cpp
@@ -4481,8 +4481,18 @@ bool os::same_files(const char* file1, const char* file2) {
     return true;
   }
 
-  HANDLE handle1 = create_read_only_file_handle(file1);
-  HANDLE handle2 = create_read_only_file_handle(file2);
+  char* native_file1 = os::strdup(file1);
+  native_file1 = os::native_path(native_file1);
+  char* native_file2 = os::strdup(file2);
+  native_file2 = os::native_path(native_file2);
+  if (strcmp(native_file1, native_file2) == 0) {
+    os::free(native_file1);
+    os::free(native_file2);
+    return true;
+  }
+
+  HANDLE handle1 = create_read_only_file_handle(native_file1);
+  HANDLE handle2 = create_read_only_file_handle(native_file2);
   bool result = false;
 
   // if we could open both paths...
@@ -4508,6 +4518,9 @@ bool os::same_files(const char* file1, const char* file2) {
   if (handle2 != INVALID_HANDLE_VALUE) {
     ::CloseHandle(handle2);
   }
+
+  os::free(native_file1);
+  os::free(native_file2);
 
   return result;
 }
