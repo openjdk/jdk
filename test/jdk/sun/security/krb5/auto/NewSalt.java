1b2764fd1b0055550c731ce69c173c5a09578693
Weijun Wang <weijun.wang@oracle.com>
2021-02-23 16:09:30 -0500

    8139348: Deprecate 3DES and RC4 in Kerberos
    8139348: Deprecate 3DES and RC4 in Kerberos

--- test/jdk/sun/security/krb5/auto/NewSalt.java
+++ test/jdk/sun/security/krb5/auto/NewSalt.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 6960894 8194486
+ * @bug 6960894 8194486 8139348
  * @summary Better AS-REQ creation and processing
  * @library /test/lib
  * @run main jdk.test.lib.FileInstaller TestHosts TestHosts
@@ -45,7 +45,7 @@ public class NewSalt {
         KDC kdc = new OneKDC(null);
         if (System.getProperty("onlyonepreauth") != null) {
             KDC.saveConfig(OneKDC.KRB5_CONF, kdc,
-                    "default_tgs_enctypes=des3-cbc-sha1");
+                    "default_tgs_enctypes=aes128-sha1");
             Config.refresh();
             kdc.setOption(KDC.Option.ONLY_ONE_PREAUTH, true);
         }
