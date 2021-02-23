8b106a14eb782903a58d812ed317729abd08d521
MBaesken <matthias.baesken@sap.com>
2021-02-23 15:28:07 +0100

    Small changes

--- src/java.base/share/native/libjli/args.c
+++ src/java.base/share/native/libjli/args.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -359,6 +359,7 @@ static void reportFcloseExit(FILE *fptr, const char* fmt, const char* arg) {
     if (fptr != NULL) fclose(fptr);
     exit(1);
 }
+
 /*
  * if the arg represent a file, that is, prefix with a single '@',
  * return a list of arguments from the file.
