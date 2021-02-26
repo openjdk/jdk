aa475b0aa36ed1ec4b6509bdf44536fe80b5994a
Jie Fu <jiefu@openjdk.org>
2021-02-26 10:17:19 +0800

    Update the jtreg test

--- test/jdk/jdk/incubator/vector/PreferredSpeciesTest.java
+++ test/jdk/jdk/incubator/vector/PreferredSpeciesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,6 +31,16 @@ import org.testng.annotations.Test;
  * @test
  * @modules jdk.incubator.vector java.base/jdk.internal.vm.vector
  * @run testng PreferredSpeciesTest
+ */
+
+/*
+ * @test
+ * @bug 8262096
+ * @summary Test the initialization of vector shapes
+ * @modules jdk.incubator.vector java.base/jdk.internal.vm.vector
+ * @run testng/othervm -XX:MaxVectorSize=8 PreferredSpeciesTest
+ * @run testng/othervm -XX:MaxVectorSize=4 PreferredSpeciesTest
+ * @run testng/othervm -XX:TieredStopAtLevel=3 PreferredSpeciesTest
  */
 
 public class PreferredSpeciesTest {
