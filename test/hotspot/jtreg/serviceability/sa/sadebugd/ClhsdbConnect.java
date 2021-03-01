7e86add8803501c1ad53011fb006bc4e12d428be
Yasumasa Suenaga <suenaga@oss.nttdata.com>
2021-03-01 21:45:13 +0900

    Skip ClhsdbConnect.java if it runs on OS X and the user is not root

--- test/hotspot/jtreg/serviceability/sa/sadebugd/ClhsdbConnect.java
+++ test/hotspot/jtreg/serviceability/sa/sadebugd/ClhsdbConnect.java
@@ -45,6 +45,19 @@ public class ClhsdbConnect {
 
     public static void main(String[] args) throws Exception {
         SATestUtils.skipIfCannotAttach(); // throws SkippedException if attach not expected to work.
+
+        if (SATestUtils.needsPrivileges()) {
+            // This tests has issues if you try adding privileges on OSX. The debugd process cannot
+            // be killed if you do this (because it is a root process and the test is not), so the destroy()
+            // call fails to do anything, and then waitFor() will time out. If you try to manually kill it with
+            // a "sudo kill" command, that seems to work, but then leaves the LingeredApp it was
+            // attached to in a stuck state for some unknown reason, causing the stopApp() call
+            // to timeout. For that reason we don't run this test when privileges are needed. Note
+            // it does appear to run fine as root, so we still allow it to run on OSX when privileges
+            // are not required.
+            throw new SkippedException("Cannot run this test on OSX if adding privileges is required.");
+        }
+
         System.out.println("Starting ClhsdbConnect test");
 
         LingeredApp theApp = null;
