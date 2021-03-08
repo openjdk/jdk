c2da64562b21997175a087c8461133a6c4cf9fb0
Yi Yang <qingfeng.yy@alibaba-inc.com>
2021-03-08 14:03:39 +0800

    one-line fix for runtime/cds/appcds/OldClassTest.java

--- test/hotspot/jtreg/runtime/cds/appcds/OldClassTest.java
+++ test/hotspot/jtreg/runtime/cds/appcds/OldClassTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,7 +30,7 @@
  * @modules java.base/jdk.internal.org.objectweb.asm
  * @compile test-classes/Hello.java
  * @run build TestCommon JarBuilder
- * @run driver OldClassTest
+ * @run main/othervm OldClassTest
  */
 
 import java.io.File;
