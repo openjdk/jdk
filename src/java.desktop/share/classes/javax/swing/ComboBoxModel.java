68b4fbe66af0c3754b21a5d204bb4aef1e4303c2
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2021-03-09 04:06:55 +0000

    8263170: ComboBoxModel documentation refers to a nonexistant type

--- src/java.desktop/share/classes/javax/swing/ComboBoxModel.java
+++ src/java.desktop/share/classes/javax/swing/ComboBoxModel.java
@@ -25,7 +25,7 @@
 package javax.swing;
 
 /**
- * A data model for a combo box. This interface extends <code>ListDataModel</code>
+ * A data model for a combo box. This interface extends <code>ListModel</code>
  * and adds the concept of a <i>selected item</i>. The selected item is generally
  * the item which is visible in the combo box display area.
  * <p>
