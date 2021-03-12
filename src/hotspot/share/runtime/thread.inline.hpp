29991c922a83cdf11bc8717b4c8a6723a254f367
Anton Kozlov <akozlov@azul.com>
2021-03-12 01:47:29 -0800

    Add Azul copyright

--- src/hotspot/share/runtime/thread.inline.hpp
+++ src/hotspot/share/runtime/thread.inline.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Azul Systems, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
