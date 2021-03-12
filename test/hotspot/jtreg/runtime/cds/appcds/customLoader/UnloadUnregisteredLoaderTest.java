b932a62c441c8ffd0967782a7faed8071855233f
Coleen Phillimore <coleenp@openjdk.org>
2021-03-12 20:48:51 +0000

    8263470: Consolidate copies of getClassBytes in various tests
    Reviewed-by: hseigel, stuefe, mseledtsov

--- test/hotspot/jtreg/runtime/cds/appcds/customLoader/UnloadUnregisteredLoaderTest.java
+++ test/hotspot/jtreg/runtime/cds/appcds/customLoader/UnloadUnregisteredLoaderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,13 +29,13 @@
  * @requires vm.cds
  * @requires vm.cds.custom.loaders
  * @requires vm.opt.final.ClassUnloading
- * @library /test/lib /test/hotspot/jtreg/runtime/cds/appcds /test/hotspot/jtreg/runtime/testlibrary
- * @build sun.hotspot.WhiteBox ClassUnloadCommon
+ * @library /test/lib /test/hotspot/jtreg/runtime/cds/appcds
+ * @build sun.hotspot.WhiteBox jdk.test.lib.classloader.ClassUnloadCommon
  * @compile test-classes/UnloadUnregisteredLoader.java test-classes/CustomLoadee.java
  * @run driver ClassFileInstaller sun.hotspot.WhiteBox
- *                                ClassUnloadCommon
- *                                ClassUnloadCommon$1
- *                                ClassUnloadCommon$TestFailure
+ *                 jdk.test.lib.classloader.ClassUnloadCommon
+ *                 jdk.test.lib.classloader.ClassUnloadCommon$1
+ *                 jdk.test.lib.classloader.ClassUnloadCommon$TestFailure
  * @run driver UnloadUnregisteredLoaderTest
  */
 
@@ -46,7 +46,9 @@ public class UnloadUnregisteredLoaderTest {
     public static void main(String[] args) throws Exception {
         String appJar1 = JarBuilder.build("UnloadUnregisteredLoader_app1", "UnloadUnregisteredLoader");
         String appJar2 = JarBuilder.build(true, "UnloadUnregisteredLoader_app2",
-                                          "ClassUnloadCommon", "ClassUnloadCommon$1", "ClassUnloadCommon$TestFailure");
+                                          "jdk/test/lib/classloader/ClassUnloadCommon",
+                                          "jdk/test/lib/classloader/ClassUnloadCommon$1",
+                                          "jdk/test/lib/classloader/ClassUnloadCommon$TestFailure");
         String customJarPath = JarBuilder.build("UnloadUnregisteredLoader_custom", "CustomLoadee");
         String wbJar = JarBuilder.build(true, "WhiteBox", "sun/hotspot/WhiteBox");
         String use_whitebox_jar = "-Xbootclasspath/a:" + wbJar;
@@ -54,9 +56,9 @@ public class UnloadUnregisteredLoaderTest {
         String classpath = TestCommon.concatPaths(appJar1, appJar2);
         String classlist[] = new String[] {
             "UnloadUnregisteredLoader",
-            "ClassUnloadCommon",
-            "ClassUnloadCommon$1",
-            "ClassUnloadCommon$TestFailure",
+            "jdk/test/lib/classloader/ClassUnloadCommon",
+            "jdk/test/lib/classloader/ClassUnloadCommon$1",
+            "jdk/test/lib/classloader/ClassUnloadCommon$TestFailure",
             "java/lang/Object id: 1",
             "CustomLoadee id: 2 super: 1 source: " + customJarPath,
         };
