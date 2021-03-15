1fadb3117d83bc1bad6938f92bc85a2c96e5b5db
Yasumasa Suenaga <suenaga@oss.nttdata.com>
2021-03-15 10:58:20 +0900

    8263565: NPE was thrown when sun.jvm.hotspot.rmi.serverNamePrefix was set

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/RMIHelper.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/RMIHelper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -54,8 +54,7 @@ public class RMIHelper {
             }
         }
 
-        tmp = System.getProperty("sun.jvm.hotspot.rmi.serverNamePrefix");
-        serverNamePrefix = (tmp != null)? serverNamePrefix : "SARemoteDebugger";
+        serverNamePrefix = System.getProperty("sun.jvm.hotspot.rmi.serverNamePrefix", "SARemoteDebugger");
     }
 
     public static void rebind(String uniqueID, Remote object) throws DebuggerException {
