b440af33aaa4eb420090afbfe9586118ba3df72d
Jonathan Dowland <jdowland@redhat.com>
2021-03-16 13:48:55 +0000

    Correct test invocation metadata
    -Xcheck:jni prior to the main class declaration; /othervm suffix now
    that we are specifying JVM options. I now see the local ref warnings
    in the jtreg output (JTwork, not JTreport).
    
    Thanks Chris Hegarty.

--- test/jdk/java/net/NetworkInterface/Test.java
+++ test/jdk/java/net/NetworkInterface/Test.java
@@ -24,8 +24,8 @@
 /* @test
  * @bug 4405354 6594296 8058216
  * @library /test/lib
- * @run main Test -Xcheck:jni
- * @run main/othervm -Djava.net.preferIPv4Stack=true Test -Xcheck:jni
+ * @run main/othervm -Xcheck:jni Test
+ * @run main/othervm -Xcheck:jni -Djava.net.preferIPv4Stack=true Test
  * @summary Basic tests for NetworkInterface
  */
 import java.net.NetworkInterface;
