57149ae1f861cc73604b168a4f1de6d4d7ca5d6d
Yasumasa Suenaga <suenaga@oss.nttdata.com>
2021-03-18 14:12:42 +0900

    Update testcases

--- test/hotspot/jtreg/serviceability/sa/sadebugd/PmapOnDebugdTest.java
+++ test/hotspot/jtreg/serviceability/sa/sadebugd/PmapOnDebugdTest.java
@@ -33,7 +33,7 @@ import jtreg.SkippedException;
  * @test
  * @bug 8263670
  * @requires vm.hasSA
- * @requires os.family != "windows"
+ * @requires (os.family != "windows") & (os.family != "mac")
  * @library /test/lib
  * @run main/othervm PmapOnDebugdTest
  */
@@ -42,18 +42,7 @@ public class PmapOnDebugdTest {
 
     public static void main(String[] args) throws Exception {
         SATestUtils.skipIfCannotAttach(); // throws SkippedException if attach not expected to work.
-
-        if (SATestUtils.needsPrivileges()) {
-            // This tests has issues if you try adding privileges on OSX. The debugd process cannot
-            // be killed if you do this (because it is a root process and the test is not), so the destroy()
-            // call fails to do anything, and then waitFor() will time out. If you try to manually kill it with
-            // a "sudo kill" command, that seems to work, but then leaves the LingeredApp it was
-            // attached to in a stuck state for some unknown reason, causing the stopApp() call
-            // to timeout. For that reason we don't run this test when privileges are needed. Note
-            // it does appear to run fine as root, so we still allow it to run on OSX when privileges
-            // are not required.
-            throw new SkippedException("Cannot run this test on OSX if adding privileges is required.");
-        }
+        SATestUtils.validateSADebugDPrivileges();
 
         LingeredApp theApp = null;
         DebugdUtils debugd = null;
@@ -76,7 +65,7 @@ public class PmapOnDebugdTest {
             System.err.println(out.getStderr());
 
             out.stderrShouldBeEmptyIgnoreDeprecatedWarnings();
-            out.shouldMatch("^0x[0-9a-f]+.+jvm\\.(dll|dylib|so)$"); // Find libjvm from output
+            out.shouldMatch("^0x[0-9a-f]+.+libjvm\\.so$"); // Find libjvm from output
             out.shouldHaveExitValue(0);
 
             // This will detect most SA failures, including during the attach.
