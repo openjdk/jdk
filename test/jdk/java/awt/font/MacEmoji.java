a9b2e9c3e470f8e2211a92fa9a0e65c0f2a68620
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2021-03-19 14:15:39 +0300

    8263583: Emoji rendering on macOS
    extended test case to cover different types of target surfaces

--- test/jdk/java/awt/font/MacEmoji.java
+++ test/jdk/java/awt/font/MacEmoji.java
@@ -23,33 +23,48 @@
 
 /*
  * @test
+ * @key headful
  * @bug 8263583
- * @summary Checks that emoji characters are rendered
+ * @summary Checks that emoji character has a non-empty and identical
+ *          representation when rendered to different types of images,
+ *          including an accelerated (OpenGL or Metal) surface.
  * @requires (os.family == "mac")
+ * @run main/othervm -Dsun.java2d.uiScale=1 MacEmoji
  */
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.VolatileImage;
+import java.util.List;
 
 public class MacEmoji {
     private static final int IMG_WIDTH = 20;
     private static final int IMG_HEIGHT = 20;
 
     public static void main(String[] args) {
-        BufferedImage img = new BufferedImage(IMG_WIDTH, IMG_HEIGHT,
-                                              BufferedImage.TYPE_INT_RGB);
+        GraphicsConfiguration cfg
+                = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                .getDefaultScreenDevice().getDefaultConfiguration();
 
-        Graphics2D g = img.createGraphics();
-        g.setColor(Color.white);
-        g.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
-        g.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
-        g.drawString("\uD83D\uDE00" /* U+1F600 'GRINNING FACE' */, 2, 15);
-        g.dispose();
+        VolatileImage vImg = cfg.createCompatibleVolatileImage(IMG_WIDTH,
+                                                               IMG_HEIGHT);
+        BufferedImage refImg;
+        int attempt = 0;
+        do {
+            if (++attempt > 10) {
+                throw new RuntimeException("Failed to render to VolatileImage");
+            }
+            if (vImg.validate(cfg) == VolatileImage.IMAGE_INCOMPATIBLE) {
+                throw new RuntimeException("Unexpected validation failure");
+            }
+            drawEmoji(vImg);
+            refImg = vImg.getSnapshot();
+        } while (vImg.contentsLost());
 
         boolean rendered = false;
         for (int x = 0; x < IMG_WIDTH; x++) {
             for (int y = 0; y < IMG_HEIGHT; y++) {
-                if (img.getRGB(x, y) != 0xFFFFFFFF) {
+                if (refImg.getRGB(x, y) != 0xFFFFFFFF) {
                     rendered = true;
                     break;
                 }
@@ -58,5 +73,36 @@ public class MacEmoji {
         if (!rendered) {
             throw new RuntimeException("Emoji character wasn't rendered");
         }
+
+        List<Integer> imageTypes = List.of(
+                BufferedImage.TYPE_INT_RGB,
+                BufferedImage.TYPE_INT_ARGB,
+                BufferedImage.TYPE_INT_ARGB_PRE,
+                BufferedImage.TYPE_INT_BGR,
+                BufferedImage.TYPE_3BYTE_BGR,
+                BufferedImage.TYPE_4BYTE_ABGR,
+                BufferedImage.TYPE_4BYTE_ABGR_PRE
+        );
+        for (Integer type : imageTypes) {
+            BufferedImage img = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+            drawEmoji(img);
+            for (int x = 0; x < IMG_WIDTH; x++) {
+                for (int y = 0; y < IMG_HEIGHT; y++) {
+                    if (refImg.getRGB(x, y) != img.getRGB(x, y)) {
+                        throw new RuntimeException(
+                                "Rendering differs for image type " + type);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void drawEmoji(Image img) {
+        Graphics g = img.getGraphics();
+        g.setColor(Color.white);
+        g.fillRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+        g.setFont(new Font(Font.DIALOG, Font.PLAIN, 12));
+        g.drawString("\uD83D\uDE00" /* U+1F600 'GRINNING FACE' */, 2, 15);
+        g.dispose();
     }
 }
