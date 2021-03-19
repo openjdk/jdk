cfe460c22420bd60688948b33ba815708ccdea2c
Alexey Ivanov <alexey.ivanov@oracle.com>
2021-03-19 20:15:57 +0000

    8263894: Convert defaultPrinter and printers fields to local variables

--- src/java.desktop/windows/classes/sun/print/PrintServiceLookupProvider.java
+++ src/java.desktop/windows/classes/sun/print/PrintServiceLookupProvider.java
@@ -43,10 +43,8 @@ import javax.print.attribute.standard.PrinterName;
 
 public class PrintServiceLookupProvider extends PrintServiceLookup {
 
-    private String defaultPrinter;
     private PrintService defaultPrintService;
-    private String[] printers; /* excludes the default printer */
-    private PrintService[] printServices; /* includes the default printer */
+    private PrintService[] printServices;
 
     static {
         java.security.AccessController.doPrivileged(
@@ -114,7 +112,7 @@ public class PrintServiceLookupProvider extends PrintServiceLookup {
     }
 
     private synchronized void refreshServices() {
-        printers = getAllPrinterNames();
+        String[] printers = getAllPrinterNames();
         if (printers == null) {
             // In Windows it is safe to assume no default if printers == null so we
             // don't get the default.
@@ -279,7 +277,7 @@ public class PrintServiceLookupProvider extends PrintServiceLookup {
 
         // Windows does not have notification for a change in default
         // so we always get the latest.
-        defaultPrinter = getDefaultPrinterName();
+        String defaultPrinter = getDefaultPrinterName();
         if (defaultPrinter == null) {
             return null;
         }
