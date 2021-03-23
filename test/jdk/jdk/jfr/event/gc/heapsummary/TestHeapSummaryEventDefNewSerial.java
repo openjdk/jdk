10c9ab14aef972333bc606a7f075420f80206a75
Jie Fu <jiefu@openjdk.org>
2021-03-24 07:23:24 +0800

    Exclude 32-bit vm

--- test/jdk/jdk/jfr/event/gc/heapsummary/TestHeapSummaryEventDefNewSerial.java
+++ test/jdk/jdk/jfr/event/gc/heapsummary/TestHeapSummaryEventDefNewSerial.java
@@ -39,10 +39,10 @@ import jdk.test.lib.jfr.GCHelper;
  * @test
  * @bug 8264008
  * @key jfr
- * @requires vm.hasJFR
+ * @requires vm.hasJFR & vm.bits == 64
  * @requires vm.gc == "Serial" | vm.gc == null
  * @library /test/lib /test/jdk
- * @run main/othervm -XX:+IgnoreUnrecognizedVMOptions -XX:+UseSerialGC -XX:-UseCompressedClassPointers
+ * @run main/othervm -XX:+UseSerialGC -XX:-UseCompressedClassPointers
  *                   jdk.jfr.event.gc.heapsummary.TestHeapSummaryEventDefNewSerial
  */
 public class TestHeapSummaryEventDefNewSerial {
