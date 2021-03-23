19d7168ee5aaed3b4e96cd9bcc39ab052012de23
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2021-03-23 09:04:22 -0700

    Address review comments: * Use InetAddress.getLoopbackAddress * Suppress irrelevant doclint warnings * Check if testURL host is resolveable * Report duration if test connection fails
    (Duration is ~75 seconds on local machine.)

--- test/langtools/jdk/javadoc/doclet/testLinkOption/TestRedirectLinks.java
+++ test/langtools/jdk/javadoc/doclet/testLinkOption/TestRedirectLinks.java
@@ -43,9 +43,12 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.UnknownHostException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyStore;
+import java.time.Duration;
+import java.time.Instant;
 
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
@@ -85,8 +88,19 @@ public class TestRedirectLinks extends JavadocTester {
     @Test
     public void testRedirects() throws Exception {
         // first, test to see if access to external URLs is available
+
         URL testURL = new URL("http://docs.oracle.com/en/java/javase/11/docs/api/element-list");
+        String testURLHost = testURL.getHost();
+        try {
+            InetAddress testAddr = InetAddress.getByName(testURLHost);
+            out.println("Found " + testURLHost + ": " + testAddr);
+        } catch (UnknownHostException e) {
+            out.println("Setup failed (" + testURLHost + " not found); this test skipped");
+            return;
+        }
+
         boolean haveRedirectURL = false;
+        Instant start = Instant.now();
         try {
             URLConnection conn = testURL.openConnection();
             conn.connect();
@@ -107,10 +121,12 @@ public class TestRedirectLinks extends JavadocTester {
             }
         } catch (Exception e) {
             out.println("Exception occurred: " + e);
+            Instant now = Instant.now();
+            out.println("Attempt took " + Duration.between(start, now).toSeconds() + " seconds");
         }
 
         if (!haveRedirectURL) {
-            out.println("Setup failed; this test skipped");
+            out.println("Setup failed (no redirect URL); this test skipped");
             return;
         }
 
@@ -119,6 +135,7 @@ public class TestRedirectLinks extends JavadocTester {
         javadoc("-d", outRedirect,
                 "-sourcepath", testSrc,
                 "-link", apiURL,
+                "-Xdoclint:none",
                 "pkg");
         checkExit(Exit.OK);
         checkOutput("pkg/B.html", true,
@@ -157,17 +174,19 @@ public class TestRedirectLinks extends JavadocTester {
         new JavacTask(tb)
                 .outdir(libModules)
                 .options("--module-source-path", libSrc.toString(),
-                        "--module", "mA,mB")
+                        "--module", "mA,mB",
+                        "-Xdoclint:none")
                 .run()
                 .writeAll();
 
         javadoc("-d", libApi.toString(),
                 "--module-source-path", libSrc.toString(),
-                "--module", "mA,mB" );
+                "--module", "mA,mB",
+                "-Xdoclint:none" );
 
         // start web servers
-        // use explicit 127.0.0.1 to avoid any issues if proxy is in use
-        InetAddress localHost = InetAddress.getByAddress("localhost", new byte[] { 127, 0, 0, 1});
+        // use loopback address to avoid any issues if proxy is in use
+        InetAddress localHost = InetAddress.getLoopbackAddress();
         try {
             oldServer = HttpServer.create(new InetSocketAddress(localHost, 0), 0);
             String oldURL = "http:/" + oldServer.getAddress();
@@ -202,7 +221,8 @@ public class TestRedirectLinks extends JavadocTester {
                         "--module-source-path", src.toString(),
                         "--module-path", libModules.toString(),
                         "-link", "http:/" + oldServer.getAddress(),
-                        "--module", "mC" );
+                        "--module", "mC",
+                        "-Xdoclint:none");
 
             } finally {
                 HttpsURLConnection.setDefaultHostnameVerifier(prevHostNameVerifier);
