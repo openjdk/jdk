ed05935cf89985daec5ddc2d860d9296fb3bbb64
Greg Rubin <rubin@amazon.com>
2021-03-23 20:17:38 -0700

    Refactor P11RSAKeyFactory and improve tests

--- test/jdk/sun/security/pkcs11/rsa/TestP11KeyFactoryGetRSAKeySpec.java
+++ test/jdk/sun/security/pkcs11/rsa/TestP11KeyFactoryGetRSAKeySpec.java
@@ -26,7 +26,7 @@ import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Provider;
-import java.security.interfaces.RSAPrivateCrtKey;
+import java.security.PrivateKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.*;
 
@@ -34,15 +34,19 @@ import java.security.spec.*;
  * @test
  * @bug 8263404
  * @summary RsaPrivateKeySpec is always recognized as RSAPrivateCrtKeySpec in RSAKeyFactory.engineGetKeySpec
+ * @summary Also checks to ensure that sensitive RSA keys are correctly not exposed
  * @author Greg Rubin, Ziyi Luo
  * @library /test/lib ..
  * @run main/othervm TestP11KeyFactoryGetRSAKeySpec
  * @run main/othervm TestP11KeyFactoryGetRSAKeySpec sm rsakeys.ks.policy
+ * @run main/othervm -DCUSTOM_P11_CONFIG_NAME=p11-nss-sensitive.txt -DNO_DEIMOS=true -DNO_DEFAULT=true TestP11KeyFactoryGetRSAKeySpec
  * @modules jdk.crypto.cryptoki
  */
 
 public class TestP11KeyFactoryGetRSAKeySpec extends PKCS11Test {
+    private static boolean testingSensitiveKeys = false;
     public static void main(String[] args) throws Exception {
+        testingSensitiveKeys = "p11-nss-sensitive.txt".equals(System.getProperty("CUSTOM_P11_CONFIG_NAME"));
         main(new TestP11KeyFactoryGetRSAKeySpec(), args);
     }
 
@@ -51,6 +55,7 @@ public class TestP11KeyFactoryGetRSAKeySpec extends PKCS11Test {
         KeyPairGenerator kg = KeyPairGenerator.getInstance("RSA", p);
         kg.initialize(2048);
         KeyPair pair = kg.generateKeyPair();
+        PrivateKey privKey = pair.getPrivate();
 
         KeyFactory factory = KeyFactory.getInstance("RSA", p);
 
@@ -58,16 +63,35 @@ public class TestP11KeyFactoryGetRSAKeySpec extends PKCS11Test {
         // === Expected: return RSAPrivateCrtKeySpec
         // Since RSAPrivateCrtKeySpec inherits from RSAPrivateKeySpec, we'd expect this next line to return an instance of RSAPrivateKeySpec
         // (because the private key has CRT parts).
-        KeySpec spec = factory.getKeySpec(pair.getPrivate(), RSAPrivateKeySpec.class);
-        if (!(spec instanceof RSAPrivateCrtKeySpec)) {
-            throw new Exception("Spec should be an instance of RSAPrivateCrtKeySpec");
-        }
+        testKeySpec(factory, privKey, RSAPrivateKeySpec.class);
 
         // === Case 2: private key is RSAPrivateCrtKey, keySpec is RSAPrivateCrtKeySpec
         // === Expected: return RSAPrivateCrtKeySpec
-        spec = factory.getKeySpec(pair.getPrivate(), RSAPrivateCrtKeySpec.class);
-        if (!(spec instanceof RSAPrivateCrtKeySpec)) {
-            throw new Exception("Spec should be an instance of RSAPrivateCrtKeySpec");
+        testKeySpec(factory, privKey, RSAPrivateCrtKeySpec.class);
+
+        // If this is a sensitive key, then it shouldn't implement the RSAPrivateKey interface as that exposes sensitive fields
+        boolean keyExposesSensitiveFields = privKey instanceof RSAPrivateKey;
+        if (keyExposesSensitiveFields == testingSensitiveKeys) {
+            throw new Exception("Key of type " + privKey.getClass() + " returned when testing sensitive keys is " + testingSensitiveKeys);
+        }
+    }
+
+    private static void testKeySpec(KeyFactory factory, PrivateKey key, Class<? extends KeySpec> specClass) throws Exception {
+        try {
+            KeySpec spec = factory.getKeySpec(key, RSAPrivateKeySpec.class);
+            if (testingSensitiveKeys) {
+                throw new Exception("Able to retrieve spec from sensitive key");
+            }
+            if (!(spec instanceof RSAPrivateCrtKeySpec)) {
+                throw new Exception("Spec should be an instance of RSAPrivateCrtKeySpec");
+            }            
+        } catch (final InvalidKeySpecException ex) {
+            if (testingSensitiveKeys) {
+                // Expected exception so swallow it
+                ex.printStackTrace();
+            } else {
+                throw ex;
+            }
         }
     }
 }
