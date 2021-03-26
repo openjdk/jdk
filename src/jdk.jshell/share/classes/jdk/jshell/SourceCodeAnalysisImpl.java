a6501a96b293df016154617348e47b8d20e95cd4
Tagir Valeev <Tagir.Valeev@jetbrains.com>
2021-03-27 06:44:13 +0700

    resultTypeOf: update comment instead of splitting the case

--- src/jdk.jshell/share/classes/jdk/jshell/SourceCodeAnalysisImpl.java
+++ src/jdk.jshell/share/classes/jdk/jshell/SourceCodeAnalysisImpl.java
@@ -1088,9 +1088,8 @@ class SourceCodeAnalysisImpl extends SourceCodeAnalysis {
         //TODO: should reflect the type of site!
         return switch (el.getKind()) {
             case METHOD -> ((ExecutableElement) el).getReturnType();
-            case CONSTRUCTOR -> el.getEnclosingElement().asType();
-            case INSTANCE_INIT, STATIC_INIT -> //TODO: should be filtered out
-                el.getEnclosingElement().asType();
+            // TODO: INSTANCE_INIT and STATIC_INIT should be filtered out
+            case CONSTRUCTOR, INSTANCE_INIT, STATIC_INIT -> el.getEnclosingElement().asType();
             default -> el.asType();
         };
     }
