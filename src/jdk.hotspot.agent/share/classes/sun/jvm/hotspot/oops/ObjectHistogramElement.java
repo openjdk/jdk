39f0b27a127e65985df0899ff881dc99f313dd16
Koichi Sakata <ksakata@openjdk.org>
2021-04-01 04:10:15 +0000

    8176026: SA: Huge heap sizes cause a negative value to be displayed in the jhisto heap total
    Reviewed-by: cjplummer, kevinw, ysuenaga

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/ObjectHistogramElement.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/ObjectHistogramElement.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -47,7 +47,7 @@ public class ObjectHistogramElement {
   }
 
   public int compare(ObjectHistogramElement other) {
-    return (int) (other.size - size);
+    return Long.compare(other.size, size);
   }
 
   /** Klass for this ObjectHistogramElement */
