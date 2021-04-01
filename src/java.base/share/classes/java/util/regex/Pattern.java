2a783a1e65f2fc21187407adc83592a7fe9d3554
Ian Graves <ian.graves@oracle.com>
2021-04-01 16:05:12 -0500

    Fixing missing space

--- src/java.base/share/classes/java/util/regex/Pattern.java
+++ src/java.base/share/classes/java/util/regex/Pattern.java
@@ -2663,7 +2663,7 @@ loop:   for(int x=0, offset=0; x<nCodePoints; x++, offset+=len) {
                                     right = right.union(clazz(true));
                             } else { // abc&&def
                                 unread();
-                                if(right == null) {
+                                if (right == null) {
                                     right = clazz(false);
                                 } else {
                                     right = right.union(clazz(false));
