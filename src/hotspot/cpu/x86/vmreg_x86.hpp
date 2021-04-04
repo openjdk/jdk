f084bd2f619f39fab0ef8cffc02a4bfd05fd7d5f
Jatin Bhateja <jbhateja@openjdk.org>
2021-04-04 17:47:23 +0000

    8262355: Support for AVX-512 opmask register allocation.
    Reviewed-by: vlivanov, njian, kvn

--- src/hotspot/cpu/x86/vmreg_x86.hpp
+++ src/hotspot/cpu/x86/vmreg_x86.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,7 +82,7 @@ inline XMMRegister as_XMMRegister() {
 inline KRegister as_KRegister() {
   assert(is_KRegister(), "must be");
   // Yuk
-  return ::as_KRegister((value() - ConcreteRegisterImpl::max_xmm));
+  return ::as_KRegister((value() - ConcreteRegisterImpl::max_xmm) >> 1);
 }
 
 inline   bool is_concrete() {
