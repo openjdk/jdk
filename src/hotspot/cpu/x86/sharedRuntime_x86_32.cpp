a756d8d7ef01b2eb990b6e84bdb45aaa4a7a96b3
Aleksey Shipilev <shade@openjdk.org>
2021-04-06 18:14:15 +0000

    8264759: x86_32 Minimal VM build failure after JDK-8262355
    Reviewed-by: thartmann, kvn

--- src/hotspot/cpu/x86/sharedRuntime_x86_32.cpp
+++ src/hotspot/cpu/x86/sharedRuntime_x86_32.cpp
@@ -219,6 +219,7 @@ OopMap* RegisterSaver::save_live_registers(MacroAssembler* masm, int additional_
     }
   }
 
+#ifdef COMPILER2
   if (save_vectors) {
     __ subptr(rsp, ymm_bytes);
     // Save upper half of YMM registers
@@ -238,6 +239,10 @@ OopMap* RegisterSaver::save_live_registers(MacroAssembler* masm, int additional_
       }
     }
   }
+#else
+  assert(!save_vectors, "vectors are generated only by C2");
+#endif
+
   __ vzeroupper();
 
   // Set an oopmap for the call site.  This oopmap will map all
