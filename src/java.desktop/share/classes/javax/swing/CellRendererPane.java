c74fbd1be743aa09c33967224d3f8252cd95b44f
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2021-04-07 04:13:57 +0100

    javadoc change

--- src/java.desktop/share/classes/javax/swing/CellRendererPane.java
+++ src/java.desktop/share/classes/javax/swing/CellRendererPane.java
@@ -200,8 +200,8 @@ public class CellRendererPane extends Container implements Accessible
 
 
     /**
-     * Calls this.paintComponent(g, c, p, x, y, w, h) with the input parameter
-     * {@code Rectangle} x,y,width,height fields.
+     * Calls this.paintComponent(g, c, p, r.x, r.y, r.w, r.h) where
+     * {@code r} is the input {@code Rectangle} parameter.
      *
      * @param g  the {@code Graphics} object to draw on
      * @param c  the {@code Component} to draw
