a1b94754d60582119ea30bae2931e8231bd2acdd
Yasumasa Suenaga <suenaga@oss.nttdata.com>
2021-04-08 10:53:29 +0900

    check the contents whether it is just "\n"

--- src/hotspot/os_cpu/linux_aarch64/vm_version_linux_aarch64.cpp
+++ src/hotspot/os_cpu/linux_aarch64/vm_version_linux_aarch64.cpp
@@ -171,16 +171,16 @@ void VM_Version::get_os_cpu_info() {
 
 static bool read_fully(const char *fname, char *buf, size_t buflen) {
   assert(buf != NULL, "invalid argument");
-  assert(buflen >= 1, "invalid argument");
+  assert(buflen >= 2, "invalid argument");
   int fd = os::open(fname, O_RDONLY, 0);
   if (fd != -1) {
     ssize_t read_sz = os::read(fd, buf, buflen);
     os::close(fd);
 
-    // Skip if the contents starts with '\n' because some machine only sets
+    // Skip if the contents is just "\n" because some machine only sets
     // '\n' to the board name.
     // (e.g. /sys/devices/virtual/dmi/id/board_name)
-    if ((read_sz > 0) && (*buf != '\n')) {
+    if ((read_sz > 0) && (strncmp(buf, "\n", 2) != 0)) {
       // Replace '\0' to ' '
       for (char *ch = buf; ch < buf + read_sz - 1; ch++) {
         if (*ch == '\0') {
