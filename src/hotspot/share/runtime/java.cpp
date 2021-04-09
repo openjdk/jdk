666fd62ee8c5a4fff65a92840269bee98205d574
Yi Yang <yyang@openjdk.org>
2021-04-09 06:23:12 +0000

    8264881: Remove the old development option MemProfiling
    Reviewed-by: dholmes, coleenp, shade

--- src/hotspot/share/runtime/java.cpp
+++ src/hotspot/share/runtime/java.cpp
@@ -64,7 +64,6 @@
 #include "runtime/init.hpp"
 #include "runtime/interfaceSupport.inline.hpp"
 #include "runtime/java.hpp"
-#include "runtime/memprofiler.hpp"
 #include "runtime/sharedRuntime.hpp"
 #include "runtime/statSampler.hpp"
 #include "runtime/stubRoutines.hpp"
@@ -224,10 +223,6 @@ void print_bytecode_count() {
 
 // General statistics printing (profiling ...)
 void print_statistics() {
-  if (MemProfiling) {
-    MemProfiler::disengage();
-  }
-
   if (CITime) {
     CompileBroker::print_times();
   }
