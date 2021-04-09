0f23157de4203bd53f472efa2ff0a969d141acd3
erikj <erik.joelsson@oracle.com>
2021-04-09 13:09:19 -0700

    Enable cross building from aarch64 to x64

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -419,9 +419,11 @@ var getJibProfilesProfiles = function (input, common, data) {
         "linux-x64": {
             target_os: "linux",
             target_cpu: "x64",
-            dependencies: ["devkit", "gtest", "graphviz", "pandoc"],
-            configure_args: concat(common.configure_args_64bit,
+            dependencies: ["devkit", "gtest", "build_devkit", "graphviz", "pandoc"],
+            configure_args: concat(
+                (input.build_cpu == "x64" ? common.configure_args_64bit : "--openjdk-target=x86_64-linux-gnu"),
                 "--with-zlib=system", "--disable-dtrace",
+                (input.build_cpu != "x64" ? "--openjdk-target=x86_64-linux-gnu" : null),
                 (isWsl(input) ? [ "--host=x86_64-unknown-linux-gnu",
                     "--build=x86_64-unknown-linux-gnu" ] : [])),
         },
@@ -1092,7 +1094,10 @@ var getJibProfilesDependencies = function (input, common) {
             organization: common.organization,
             ext: "tar.gz",
             module: "devkit-" + input.build_platform,
-            revision: devkit_platform_revisions[input.build_platform]
+            revision: devkit_platform_revisions[input.build_platform],
+            // Only set --with-build-devkit when cross compiling.
+            configure_args: (input.build_arch == input.target_arch ? false 
+                : "--with-build-devkit=" + input.get("build_devkit", "home_path"))
         },
 
         cups: {
