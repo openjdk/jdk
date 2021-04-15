e4a5c572d4f06a4737f64014e670fd7432890fbb
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2021-04-15 14:38:10 +0200

    Set group's parent when a group is added to the graph document

--- src/utils/IdealGraphVisualizer/Coordinator/src/main/java/com/sun/hotspot/igv/coordinator/OutlineTopComponent.java
+++ src/utils/IdealGraphVisualizer/Coordinator/src/main/java/com/sun/hotspot/igv/coordinator/OutlineTopComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -114,6 +114,7 @@ public final class OutlineTopComponent extends TopComponent implements ExplorerM
             public void started(Group g) {
                 synchronized(OutlineTopComponent.this) {
                     getDocument().addElement(g);
+                    g.setParent(getDocument());
                 }
             }
         };
