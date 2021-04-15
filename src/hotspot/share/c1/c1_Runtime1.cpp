593194864a4b340db4aeb45d0eabb70020c4721d
David Holmes <dholmes@openjdk.org>
2021-04-15 03:01:49 +0000

    8265246: Fix macos-Aarch64 build after JDK-8263709
    Reviewed-by: mikael

--- src/hotspot/share/c1/c1_Runtime1.cpp
+++ src/hotspot/share/c1/c1_Runtime1.cpp
@@ -1265,7 +1265,7 @@ void Runtime1::patch_code(JavaThread* current, Runtime1::StubID stub_id) {
 
   // Enable WXWrite: the function is called by c1 stub as a runtime function
   // (see another implementation above).
-  MACOS_AARCH64_ONLY(ThreadWXEnable wx(WXWrite, thread));
+  MACOS_AARCH64_ONLY(ThreadWXEnable wx(WXWrite, current));
 
   if (TracePatching) {
     tty->print_cr("Deoptimizing because patch is needed");
