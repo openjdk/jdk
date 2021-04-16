913267863001d4439de6e50d7811b96f36ef4162
Naoto Sato <naoto.sato@oracle.com>
2021-04-16 13:48:41 -0700

    8265375: Bootcycle builds fail with StackOverflowError in cldrconverter

--- make/jdk/src/classes/build/tools/cldrconverter/CLDRConverter.java
+++ make/jdk/src/classes/build/tools/cldrconverter/CLDRConverter.java
@@ -1038,6 +1038,10 @@ public class CLDRConverter {
                 Objects.nonNull(p) &&
                 !candidates.get(i+1).equals(p)) {
                 List<Locale> applied = candidates.subList(0, i+1);
+                if (applied.contains(p)) {
+                    // avoid circular recursion (could happen with nb/no case)
+                    continue;
+                }
                 applied.addAll(applyParentLocales(baseName, defCon.getCandidateLocales(baseName, p)));
                 return applied;
             }
