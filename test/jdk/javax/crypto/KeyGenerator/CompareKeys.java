be5d9a6247cd749aaf8a4dbfdd294a824d200605
Sibabrata Sahoo <69787900+sisahoo@users.noreply.github.com>
2021-04-16 13:59:56 +0530

    Update CompareKeys.java

--- test/jdk/javax/crypto/KeyGenerator/CompareKeys.java
+++ test/jdk/javax/crypto/KeyGenerator/CompareKeys.java
@@ -76,7 +76,7 @@ public class CompareKeys {
     }
 
     /**
-     * Compare original KeyPair with another copy.
+     * Compare original Key with another copy.
      */
     private static void checkKeyEquality(Key origKey, Key copyKey) {
 
