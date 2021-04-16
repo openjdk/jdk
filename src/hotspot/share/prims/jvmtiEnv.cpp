d6e091daef322d572bb82cd7812c8da8560b804e
Robbin Ehn <robbin.ehn@oracle.com>
2021-04-16 08:35:34 +0200

    Fixed nits

--- src/hotspot/share/prims/jvmtiEnv.cpp
+++ src/hotspot/share/prims/jvmtiEnv.cpp
@@ -949,7 +949,7 @@ JvmtiEnv::SuspendThread(JavaThread* java_thread) {
   }
   if (!JvmtiSuspendControl::suspend(java_thread)) {
     // Either the thread is already suspended or
-    // it was in process of exiting.
+    // it was in the process of exiting.
     if (java_thread->is_exiting()) {
       return JVMTI_ERROR_THREAD_NOT_ALIVE;
     }
@@ -990,7 +990,7 @@ JvmtiEnv::SuspendThreadList(jint request_count, const jthread* request_list, jvm
     }
     if (!JvmtiSuspendControl::suspend(java_thread)) {
       // Either the thread is already suspended or
-      // it was in process of exiting.
+      // it was in the process of exiting.
       if (java_thread->is_exiting()) {
         results[i] = JVMTI_ERROR_THREAD_NOT_ALIVE;
         continue;
@@ -1003,7 +1003,7 @@ JvmtiEnv::SuspendThreadList(jint request_count, const jthread* request_list, jvm
   if (self_index >= 0) {
     if (!JvmtiSuspendControl::suspend(current)) {
       // Either the thread is already suspended or
-      // it was in process of exiting.
+      // it was in the process of exiting.
       if (current->is_exiting()) {
         results[self_index] = JVMTI_ERROR_THREAD_NOT_ALIVE;
       } else {
