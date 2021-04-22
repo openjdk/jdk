1088911157020711ddb1d5474381a76f8a933b7e
Jesper Steen Møller <jesper@selskabet.org>
2021-04-22 23:00:26 +0200

    Fixes filename, description, and javac options.

--- test/langtools/tools/javac/8264258/MisleadingNonExistentPathError.java
+++ test/langtools/tools/javac/8264258/MisleadingNonExistentPathError.java
@@ -1,12 +1,12 @@
 /*
  * @test /nodynamiccopyright/
  * @bug 8264258
- * @summary unknown.Class.default gives misleading compilation error
- * @compile/fail/ref=MisleadingTypeNotFound.out -Xlint:all -Werror -XDrawDiagnostics -XDdev MisleadingTypeNotFound.java
+ * @summary Unknown lookups in the java package give misleading compilation errors
+ * @compile/fail/ref=MisleadingNonExistentPathError.out -XDrawDiagnostics MisleadingNonExistentPathError.java
  */
 package knownpkg;
 
-public class MisleadingTypeNotFound {
+public class MisleadingNonExistentPathError {
 
     void classNotFound() {
         // Not found, but in an existing package
@@ -20,5 +20,8 @@ public class MisleadingTypeNotFound {
 
         // Not found, but in the 'java' package which is in scope as per JLS 6.3 and observable as per JLS 7.4.3
         Class<?> c4 = java.NotFound.class;
+        
+        // Not found, but in a (system) package which exists and is in scope
+        Object c5 = new java.lang();
     }
 }
