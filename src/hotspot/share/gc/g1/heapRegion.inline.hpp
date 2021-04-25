7a4276963835d7731d2daa5e96288e12b3905d15
Hamlin-Li <leeming1509@outlook.com>
2021-04-25 17:18:50 +0800

    JDK-8265394: G1: Improve assert in HeapRegion::reset_not_compacted_after_full_gc

--- src/hotspot/share/gc/g1/heapRegion.inline.hpp
+++ src/hotspot/share/gc/g1/heapRegion.inline.hpp
@@ -199,7 +199,7 @@ inline void HeapRegion::reset_compacted_after_full_gc() {
 }
 
 inline void HeapRegion::reset_not_compacted_after_full_gc() {
-  assert(!is_free(), "should not have compacted free region");
+  assert(!is_free(), "must be");
 
   assert(compaction_top() == bottom(),
          "region %u compaction_top " PTR_FORMAT " must not be different from bottom " PTR_FORMAT,
