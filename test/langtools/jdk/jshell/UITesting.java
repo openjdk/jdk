d7eacd6a8bbd535a352f73fffc579041af0b630f
Jan Lahoda <jlahoda@openjdk.org>
2021-04-26 13:22:59 +0200

    Restoring timeout.

--- test/langtools/jdk/jshell/UITesting.java
+++ test/langtools/jdk/jshell/UITesting.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -142,7 +142,7 @@ public class UITesting {
         } catch (NumberFormatException ex) {
             factor = 1;
         }
-        TIMEOUT = 1_000 * factor;
+        TIMEOUT = 60_000 * factor;
     }
 
     protected void waitOutput(StringBuilder out, String expected) {
