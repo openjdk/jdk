1cf7d1c2fb8bcd44d670ae99a4985879c3d1d337
aghaisas <ajitgh@gmail.com>
2021-04-27 20:25:55 +0530

    cleanup unused import

--- src/java.desktop/macosx/classes/sun/java2d/metal/MTLContext.java
+++ src/java.desktop/macosx/classes/sun/java2d/metal/MTLContext.java
@@ -32,7 +32,6 @@ import sun.java2d.pipe.hw.ContextCapabilities;
 
 import java.lang.annotation.Native;
 
-import static sun.java2d.pipe.BufferedOpCodes.INVALIDATE_CONTEXT;
 import static sun.java2d.pipe.BufferedOpCodes.SET_SCRATCH_SURFACE;
 
 /**
