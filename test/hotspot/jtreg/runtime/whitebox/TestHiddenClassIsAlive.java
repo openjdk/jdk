36739c57d26b09876f6acb88cad8f4040e558578
Harold Seigel <harold.seigel@oracle.com>
2021-05-05 18:51:38 +0000

    Update test

--- test/hotspot/jtreg/runtime/whitebox/TestHiddenClassIsAlive.java
+++ test/hotspot/jtreg/runtime/whitebox/TestHiddenClassIsAlive.java
@@ -21,68 +21,53 @@
  * questions.
  */
 
-/* @test
+/*
+ * @test
  * @bug 8266496
  * @summary Test that Whitebox.isClassAlive() works with hidden classes.
- * @modules java.base/jdk.internal.misc java.base/jdk.internal.org.objectweb.asm
  * @library /test/lib
  * @build sun.hotspot.WhiteBox
  * @run driver jdk.test.lib.helpers.ClassFileInstaller sun.hotspot.WhiteBox
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI TestHiddenClassIsAlive
  */
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodHandles.Lookup;
 import java.lang.invoke.MethodType;
-import jdk.internal.org.objectweb.asm.*;
-import static jdk.internal.org.objectweb.asm.Opcodes.*;
 
 import sun.hotspot.WhiteBox;
 
+final class MyClass {
+    MyClass() {}
+    public Object get(int i) { return null; }
+}
+
 public class TestHiddenClassIsAlive {
 
-    static final String CLASS_NAME = "MyClass";
 
     public static void main(String[] args) throws Throwable {
-        byte[] classBytes = dumpClass();
+        byte[] classBytes = readClassFile("MyClass.class");
         Lookup lookup = MethodHandles.lookup();
         Class<?> c = lookup.defineHiddenClass(classBytes, true).lookupClass();
-        if (!WhiteBox.getWhiteBox().isClassAlive(CLASS_NAME)) {
+        if (!WhiteBox.getWhiteBox().isClassAlive("MyClass")) {
             throw new RuntimeException("Hidden class should be alive");
         }
     }
 
-    /*
-     * Constructs bytecode for the following class:
-     * public class MyClass {
-     *     MyClass() {}
-     *     public Object get(int i) { return null; }
-     * }
-     */
-    public static byte[] dumpClass() {
-        ClassWriter cw = new ClassWriter(0);
-        MethodVisitor mv;
-
-        cw.visit(52, ACC_SUPER | ACC_PUBLIC, CLASS_NAME, null, "java/lang/Object", null);
+    static byte[] readClassFile(String classFileName) throws Exception {
+        File classFile = new File(System.getProperty("test.classes") + File.separator + classFileName);
+        try (FileInputStream in = new FileInputStream(classFile);
+             ByteArrayOutputStream out = new ByteArrayOutputStream())
         {
-            mv = cw.visitMethod(0, "<init>", "()V", null, null);
-            mv.visitCode();
-            mv.visitVarInsn(ALOAD, 0);
-            mv.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
-            mv.visitInsn(RETURN);
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            return out.toByteArray();
         }
-        {
-            mv = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "get", "(I)Ljava/lang/Object;", null, null);
-            mv.visitCode();
-            mv.visitInsn(ACONST_NULL);
-            mv.visitInsn(ARETURN);
-            mv.visitMaxs(1, 1);
-            mv.visitEnd();
-        }
-        cw.visitEnd();
-        return cw.toByteArray();
     }
 }
