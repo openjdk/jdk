4e21791e71a5b86c377a01bc2a1f9716f7eb5f68
MBaesken <matthias.baesken@sap.com>
2021-05-05 16:06:13 +0200

    adjust absorbweight check

--- src/java.desktop/share/classes/java/awt/font/TextJustifier.java
+++ src/java.desktop/share/classes/java/awt/font/TextJustifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 1999, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -161,7 +161,7 @@ class TextJustifier {
             }
 
             float weightedAbsorb = 0;
-            if (hitLimit && absorbweight > 0) {
+            if (hitLimit && absorbweight != 0) {
                 weightedAbsorb = (delta - gslimit) / absorbweight;
             }
 
