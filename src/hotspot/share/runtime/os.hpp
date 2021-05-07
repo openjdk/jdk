94c6177f246fc569b416f85f1411f7fe031f7aaf
Thomas Stuefe <stuefe@openjdk.org>
2021-05-07 09:47:46 +0000

    8266536: Provide a variant of os::iso8601_time which works with arbitrary timestamps
    Reviewed-by: xliu, simonis, ysuenaga

--- src/hotspot/share/runtime/os.hpp
+++ src/hotspot/share/runtime/os.hpp
@@ -224,6 +224,16 @@ class os: AllStatic {
   static char*      local_time_string(char *buf, size_t buflen);
   static struct tm* localtime_pd     (const time_t* clock, struct tm*  res);
   static struct tm* gmtime_pd        (const time_t* clock, struct tm*  res);
+
+  // "YYYY-MM-DDThh:mm:ss.mmm+zzzz" incl. terminating zero
+  static const size_t iso8601_timestamp_size = 29;
+
+  // Fill in buffer with an ISO-8601 string corresponding to the given javaTimeMillis value
+  // E.g., YYYY-MM-DDThh:mm:ss.mmm+zzzz.
+  // Returns buffer, or NULL if it failed.
+  static char* iso8601_time(jlong milliseconds_since_19700101, char* buffer,
+                            size_t buffer_length, bool utc = false);
+
   // Fill in buffer with current local time as an ISO-8601 string.
   // E.g., YYYY-MM-DDThh:mm:ss.mmm+zzzz.
   // Returns buffer, or NULL if it failed.
