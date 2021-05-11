9e9600674e8a2f535eda8698b1c16aa32ca611df
Jan Lahoda <jlahoda@openjdk.org>
2021-05-11 10:06:04 +0000

    8263452: Javac slow compilation due to algorithmic complexity
    Reviewed-by: vromero, jfranck

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -2247,7 +2247,7 @@ public class Check {
 
     class CycleChecker extends TreeScanner {
 
-        List<Symbol> seenClasses = List.nil();
+        Set<Symbol> seenClasses = new HashSet<>();
         boolean errorFound = false;
         boolean partialCheck = false;
 
@@ -2266,7 +2266,7 @@ public class Check {
                 } else if (sym.kind == TYP) {
                     checkClass(pos, sym, List.nil());
                 }
-            } else {
+            } else if (sym == null || sym.kind != PCK) {
                 //not completed yet
                 partialCheck = true;
             }
@@ -2315,7 +2315,7 @@ public class Check {
                 noteCyclic(pos, (ClassSymbol)c);
             } else if (!c.type.isErroneous()) {
                 try {
-                    seenClasses = seenClasses.prepend(c);
+                    seenClasses.add(c);
                     if (c.type.hasTag(CLASS)) {
                         if (supertypes.nonEmpty()) {
                             scan(supertypes);
@@ -2338,7 +2338,7 @@ public class Check {
                         }
                     }
                 } finally {
-                    seenClasses = seenClasses.tail;
+                    seenClasses.remove(c);
                 }
             }
         }
