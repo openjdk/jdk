5246dd76ba94bd8172cd4fa20ba5e7b8fe9fec77
Harold Seigel <harold.seigel@oracle.com>
2021-05-12 16:02:27 +0000

    test changes and small fixes

--- src/hotspot/share/oops/instanceMirrorKlass.inline.hpp
+++ src/hotspot/share/oops/instanceMirrorKlass.inline.hpp
@@ -62,7 +62,7 @@ void InstanceMirrorKlass::oop_oop_iterate(oop obj, OopClosureType* closure) {
         // A non-strong hidden class doesn't have its own class loader,
         // so when handling the java mirror for the class we need to make sure its class
         // loader data is claimed, this is done by calling do_cld explicitly.
-        // For non-string hidden classes the call to do_cld is made when the class
+        // For non-strong hidden classes the call to do_cld is made when the class
         // loader itself is handled.
         Devirtualizer::do_cld(closure, klass->class_loader_data());
       } else {
