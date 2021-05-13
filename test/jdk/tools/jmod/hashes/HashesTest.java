16ec525bad6c730bc59a12fa4f1aed5abe299f09
Lance Andersen <lance.andersen@oracle.com>
2021-05-13 06:41:44 -0400

    Update HashesTest for TestNG 7.4

--- test/jdk/tools/jmod/hashes/HashesTest.java
+++ test/jdk/tools/jmod/hashes/HashesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -86,6 +86,13 @@ public class HashesTest {
     private final Path lib;
     private final ModuleInfoMaker builder;
 
+    public HashesTest() {
+        mods= null;
+        srcDir=null;
+        lib= null;
+        builder=null;
+    }
+    
     HashesTest(Path dest) throws IOException {
         if (Files.exists(dest)) {
             deleteDirectory(dest);
