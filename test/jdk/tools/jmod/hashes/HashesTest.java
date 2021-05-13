a36e9f515a9ed3c5a1309b49e0e4ee29c9cc8a63
Lance Andersen <lance.andersen@oracle.com>
2021-05-13 14:33:33 -0400

    Update copyright typo

--- test/jdk/tools/jmod/hashes/HashesTest.java
+++ test/jdk/tools/jmod/hashes/HashesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyrig(c) 2015, 2021, Oracle and/or its affiliates. All rig reserved.
+ * Copyright(c) 2015, 2021, Oracle and/or its affiliates. All rig reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
