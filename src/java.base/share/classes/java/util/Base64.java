147aed330d7634d9a0210ce007d1d76ef6de9450
asgibbons <scott.gibbons@intel.com>
2021-05-17 12:45:45 -0700

    Fixing last block

--- src/java.base/share/classes/java/util/Base64.java
+++ src/java.base/share/classes/java/util/Base64.java
@@ -815,7 +815,7 @@ public class Base64 {
                      * Calculate how many characters were processed by how many
                      * bytes of data were returned.
                      */
-                    int chars_decoded = (dl / 3) * 4;
+                    int chars_decoded = (dl * 4) / 3;
 
                     sp += chars_decoded;
                     dp += dl;
