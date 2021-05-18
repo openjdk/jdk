21475defb2e3f7d1c9a1339ee7b479219d7503fb
Jie Fu <jiefu@openjdk.org>
2021-05-18 17:45:52 +0800

    8267311: vmTestbase/gc/gctests/StringInternGC/StringInternGC.java fails when JTREG_JOBS > 25

--- test/hotspot/jtreg/vmTestbase/gc/gctests/StringInternGC/StringInternGC.java
+++ test/hotspot/jtreg/vmTestbase/gc/gctests/StringInternGC/StringInternGC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,7 @@
  *
  * @library /vmTestbase
  *          /test/lib
- * @run main/othervm gc.gctests.StringInternGC.StringInternGC
+ * @run main/othervm -Xmx1g gc.gctests.StringInternGC.StringInternGC
  */
 
 package gc.gctests.StringInternGC;
