cd99dc495ad25249ce124a1856bc15e9e9886a83
Patrick Concannon <patrick.concannon@oracle.com>
2021-05-19 15:04:02 +0100

    8267110: Reverted changes in java/util/Formatter as primitive to boxed types may have semantic/performance implications

--- src/java.base/share/classes/java/util/Formatter.java
+++ src/java.base/share/classes/java/util/Formatter.java
@@ -3018,17 +3018,20 @@ public final class Formatter implements Closeable, Flushable {
             String s = null;
             if (arg instanceof Character) {
                 s = ((Character)arg).toString();
-            } else if (arg instanceof Byte i) {
+            } else if (arg instanceof Byte) {
+                byte i = (Byte) arg;
                 if (Character.isValidCodePoint(i))
                     s = new String(Character.toChars(i));
                 else
                     throw new IllegalFormatCodePointException(i);
-            } else if (arg instanceof Short i) {
+            } else if (arg instanceof Short) {
+                short i = (Short) arg;
                 if (Character.isValidCodePoint(i))
                     s = new String(Character.toChars(i));
                 else
                     throw new IllegalFormatCodePointException(i);
-            } else if (arg instanceof Integer i) {
+            } else if (arg instanceof Integer) {
+                int i = (Integer) arg;
                 if (Character.isValidCodePoint(i))
                     s = new String(Character.toChars(i));
                 else
