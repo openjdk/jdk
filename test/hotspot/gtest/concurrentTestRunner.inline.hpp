fd87020aacafe51c21535134f27cf72a6d88e0d8
tstuefe <thomas.stuefe@gmail.com>
2021-05-19 16:29:18 +0200

    5 threads 3 secs

--- test/hotspot/gtest/concurrentTestRunner.inline.hpp
+++ test/hotspot/gtest/concurrentTestRunner.inline.hpp
@@ -64,7 +64,7 @@ public:
   // runnableArg - what to run
   // nrOfThreadsArg - how many threads to use concurrently
   // testDurationMillisArg - duration for each test run
-  ConcurrentTestRunner(TestRunnable* const runnableArg, int nrOfThreadsArg, long testDurationMillisArg = 5000) :
+  ConcurrentTestRunner(TestRunnable* const runnableArg, int nrOfThreadsArg, long testDurationMillisArg) :
     unitTestRunnable(runnableArg),
     nrOfThreads(nrOfThreadsArg),
     testDurationMillis(testDurationMillisArg) {}
