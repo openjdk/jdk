392dc8b617b4676fcdfa6eff5285e5e7b8db3900
Fernando Guallini <fernando.guallini@oracle.com>
2021-05-24 15:18:27 +0100

    SocketExceptions is not wrapped into SSLExceptions

--- test/jdk/sun/security/ssl/SSLSocketImpl/SSLSocketImplThrowsWrongExceptions.java
+++ test/jdk/sun/security/ssl/SSLSocketImpl/SSLSocketImplThrowsWrongExceptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -95,12 +95,12 @@ public class SSLSocketImplThrowsWrongExceptions {
             System.out.println("Server starting handshake");
             sslSocket.startHandshake();
             throw new Exception("Handshake was successful");
-        } catch (SSLException e) {
+        } catch (SSLException | SocketException se){
             /*
-             * Caught the right Exeption.  Swallow it.
+             * Caught the right Exception.  Swallow it.
              */
             System.out.println("Server reported the right exception");
-            System.out.println(e.toString());
+            System.out.println(se);
         } catch (Exception e) {
             /*
              * Caught the wrong exception.  Rethrow it.
