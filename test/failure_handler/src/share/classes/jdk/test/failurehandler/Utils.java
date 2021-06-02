57d761631b17358de562af4d3a348b245d4c7205
lmesnik <leonid.mesnik@oracle.com>
2021-06-01 17:39:55 -0700

    remove unused code.

--- test/failure_handler/src/share/classes/jdk/test/failurehandler/Utils.java
+++ test/failure_handler/src/share/classes/jdk/test/failurehandler/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,14 +23,11 @@
 
 package jdk.test.failurehandler;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.Reader;
 import java.io.Writer;
-import java.nio.file.Path;
 import java.util.Properties;
 
 public final class Utils {
