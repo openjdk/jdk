bd64a33e01d7821298b1d42260235ac969b0e83f
Andy Herrick <herrick@openjdk.org>
2021-06-03 19:02:11 +0000

    8267598: jpackage removes system libraries from java.library.path
    Reviewed-by: almatvee, asemenyuk

--- src/jdk.jpackage/share/native/common/SysInfo.h
+++ src/jdk.jpackage/share/native/common/SysInfo.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -84,9 +84,15 @@ namespace SysInfo {
             const tstring& defValue=tstring());
 
     /**
+     * Sets the value of environment variable with the given name to the given value.
+     */
+    void setEnvVariable(const tstring& name, const tstring& value);
+
+    /**
      * Returns 'true' if environment variable with the given name is set.
      */
     bool isEnvVariableSet(const tstring& name);
+
 }
 
 #endif // SYSINFO_H
