8fe172a7f79c280c75968d67017ceb8881e639d3
Yasumasa Suenaga <ysuenaga@openjdk.org>
2021-06-05 00:07:04 +0000

    8266957: SA has not followed JDK-8220587 and JDK-8224965
    Reviewed-by: cjplummer, sspitsyn

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/z/ZGranuleMapForForwarding.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/z/ZGranuleMapForForwarding.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, NTT DATA.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,34 +28,63 @@ package sun.jvm.hotspot.gc.z;
 import sun.jvm.hotspot.debugger.Address;
 import sun.jvm.hotspot.runtime.VM;
 import sun.jvm.hotspot.runtime.VMObject;
-import sun.jvm.hotspot.runtime.VMObjectFactory;
 import sun.jvm.hotspot.types.AddressField;
-import sun.jvm.hotspot.types.CIntegerField;
 import sun.jvm.hotspot.types.Type;
 import sun.jvm.hotspot.types.TypeDataBase;
 
-public class ZForwardingTable extends VMObject {
-    private static long mapFieldOffset;
+public class ZGranuleMapForForwarding  extends VMObject {
+    private static AddressField mapField;
 
     static {
         VM.registerVMInitializedObserver((o, d) -> initialize(VM.getVM().getTypeDataBase()));
     }
 
     static private synchronized void initialize(TypeDataBase db) {
-        Type type = db.lookupType("ZForwardingTable");
+        Type type = db.lookupType("ZGranuleMapForForwarding");
 
-        mapFieldOffset = type.getAddressField("_map").getOffset();
+        mapField = type.getAddressField("_map");
     }
 
-    public ZForwardingTable(Address addr) {
+    public ZGranuleMapForForwarding(Address addr) {
         super(addr);
     }
 
-    private ZGranuleMapForForwarding map() {
-        return VMObjectFactory.newObject(ZGranuleMapForForwarding.class, addr.addOffsetTo(mapFieldOffset));
+    private Address map() {
+        return mapField.getValue(addr);
     }
 
-    public ZForwarding get(Address o) {
-        return VMObjectFactory.newObject(ZForwarding.class, map().get(ZAddress.offset(o)));
+    public long size() {
+        return ZGlobals.ZAddressOffsetMax >> ZGlobals.ZGranuleSizeShift;
+    }
+
+    private long index_for_offset(long offset) {
+        long index = offset >>> ZGlobals.ZGranuleSizeShift;
+
+        return index;
+    }
+
+    Address at(long index) {
+        return map().getAddressAt(index * VM.getVM().getAddressSize());
+    }
+
+    Address get(long offset) {
+        long index = index_for_offset(offset);
+        return at(index);
+    }
+
+    public class Iterator {
+        private long next = 0;
+
+        boolean hasNext() {
+            return next < size();
+        }
+
+        Address next() {
+            if (next >= size()) {
+                throw new RuntimeException("OOIBE");
+            }
+
+            return at(next++);
+        }
     }
 }
