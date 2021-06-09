eb46926706ed2043dbc783bb9a8afa72b48f459e
Yi Yang <qingfeng.yy@alibaba-inc.com>
2021-06-09 09:45:53 +0800

    print tid directly

--- src/hotspot/share/runtime/osThread.cpp
+++ src/hotspot/share/runtime/osThread.cpp
@@ -38,7 +38,7 @@ OSThread::~OSThread() {
 
 // Printing
 void OSThread::print_on(outputStream *st) const {
-  st->print("nid=0x%x ", thread_id());
+  st->print("nid=%d ", thread_id());
   switch (_state) {
     case ALLOCATED:               st->print("allocated ");                 break;
     case INITIALIZED:             st->print("initialized ");               break;
@@ -46,10 +46,10 @@ void OSThread::print_on(outputStream *st) const {
     case MONITOR_WAIT:            st->print("waiting for monitor entry "); break;
     case CONDVAR_WAIT:            st->print("waiting on condition ");      break;
     case OBJECT_WAIT:             st->print("in Object.wait() ");          break;
-    case BREAKPOINTED:            st->print("at breakpoint");               break;
-    case SLEEPING:                st->print("sleeping");                    break;
-    case ZOMBIE:                  st->print("zombie");                      break;
-    default:                      st->print("unknown state %d", _state); break;
+    case BREAKPOINTED:            st->print("at breakpoint ");             break;
+    case SLEEPING:                st->print("sleeping ");                  break;
+    case ZOMBIE:                  st->print("zombie ");                    break;
+    default:                      st->print("unknown state %d ", _state);  break;
   }
 }
 
