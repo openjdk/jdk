78cb6776b6d43b67457993a109719b36ee892d60
Igor Ignatyev <iignatyev@openjdk.org>
2021-06-10 17:06:19 +0000

    8268539: several serviceability/sa tests should be run in driver mode
    Reviewed-by: sspitsyn

--- test/hotspot/jtreg/serviceability/sa/TestJhsdbJstackLineNumbers.java
+++ test/hotspot/jtreg/serviceability/sa/TestJhsdbJstackLineNumbers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,7 +39,7 @@ import jdk.test.lib.SA.SATestUtils;
  * @requires os.arch=="amd64" | os.arch=="x86_64"
  * @requires os.family=="windows" | os.family == "linux" | os.family == "mac"
  * @library /test/lib
- * @run main/othervm TestJhsdbJstackLineNumbers
+ * @run driver TestJhsdbJstackLineNumbers
  */
 
 /*
