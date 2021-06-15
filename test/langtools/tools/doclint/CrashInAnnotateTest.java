8c387d1272b598d44a5d384ff985af7b00ae351d
Joel Borggrén-Franck <jfranck@openjdk.org>
2021-06-15 13:32:46 +0200

    8266082: Use nodynamiccopyright and add test cases

--- test/langtools/tools/doclint/CrashInAnnotateTest.java
+++ test/langtools/tools/doclint/CrashInAnnotateTest.java
@@ -1,41 +1,27 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
-
-/*
- * @test
+ * @test /nodynamiccopyright/
  * @bug 8266082
  * @summary javac should not crash when seeing type annotations in links
  * @compile/fail/ref=CrashInAnnotateTest.out -Xdoclint -XDrawDiagnostics CrashInAnnotateTest.java
  */
 
+import java.util.List;
+
 /** {@link #equals(@Deprecated Object)} */
 class CrashInAnnotateTest {
 }
 
-/** {@link #compare(Object, @Deprecated Object)} */
+/** {@link #compare(Object, List<List<@Deprecated Object>>)} */
 class CrashInAnnotateTest2 {
+    void compare(Object o, List<List<Object>> l) {}
 }
 
-/** {@link #compare(Object, List<List<@Deprecated Object>>)} */
-class CrashInAnnotateTest3 {
+/** {@link java.lang.@Deprecated Object#hashCode()} */
+class CrashInAnnotateTest3 { }
+
+/** {@link CrashInAnnotateTest4.@Deprecated Inner#aField} */
+class CrashInAnnotateTest4 {
+    class Inner {
+        Object aField;
+    }
 }
