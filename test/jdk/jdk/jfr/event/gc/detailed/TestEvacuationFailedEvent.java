fcf922c0283ffff46366e69f4bdb136bc36940d7
Thomas Schatzl <thomas.schatzl@oracle.com>
2021-06-21 16:40:46 +0200

    Remove whitespace

--- test/jdk/jdk/jfr/event/gc/detailed/TestEvacuationFailedEvent.java
+++ test/jdk/jdk/jfr/event/gc/detailed/TestEvacuationFailedEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,9 +70,8 @@ public class TestEvacuationFailedEvent {
         WhiteBox.getWhiteBox().youngGC();
         // Keep alive data.
         System.out.println(data);
-        
-        recording.stop();
 
+        recording.stop();
 
         // Verify recording
         List<RecordedEvent> events = Events.fromRecording(recording);
