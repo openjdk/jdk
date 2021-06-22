36cdd465d5628157dc544f35f1c53ea2095365e0
Alex Menkov <69548902+alexmenkov@users.noreply.github.com>
2021-06-22 13:51:36 -0700

    JDK-8260540

--- test/hotspot/jtreg/serviceability/jdwp/AllModulesCommandTest.java
+++ test/hotspot/jtreg/serviceability/jdwp/AllModulesCommandTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -82,7 +82,7 @@ public class AllModulesCommandTest implements DebuggeeLauncher.Listener {
         try {
             // Establish JDWP socket connection
             channel = new JdwpChannel();
-            channel.connect();
+            channel.connect(launcher.getJdwpPort());
             // Send out ALLMODULES JDWP command
             // and verify the reply
             JdwpAllModulesReply reply = new JdwpAllModulesCmd().send(channel);
