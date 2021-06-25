a38ec48f260a3a671b9a29bb52b6b260c6b06dd9
theRealAph <aph@redhat.com>
2021-06-25 16:26:00 +0100

    Remove dead code

--- src/hotspot/share/asm/register.hpp
+++ src/hotspot/share/asm/register.hpp
@@ -47,16 +47,6 @@ class AbstractRegisterImpl {
 
 #define AS_REGISTER(type,name)         ((type)name##_##type##EnumValue)
 
-#ifndef USE_EXTERN_CONST_FOR_REGISTER_DEFINITIONS
-
-// JDK-8269122: The use of "extern const" for Register definitions
-// generates poor code.
-//
-// The use of "extern const" for Register definitions is a historical
-// artefact that leads to excessive code size for class Assembler. The
-// old definitions for CONSTANT_REGISTER_DECLARATION et al are left
-// here in case some platform needs them.
-
 #define CONSTANT_REGISTER_DECLARATION(type, name, value)        \
 const type name = ((type)value);                                \
 enum { name##_##type##EnumValue = (value) }
@@ -65,56 +55,6 @@ enum { name##_##type##EnumValue = (value) }
 const type name = ((type)value)
 
 #define REGISTER_DEFINITION(type, name)
-
-#else
-
-// Macros for use in defining Register instances.  We'd like to be
-// able to simply define const instances of the RegisterImpl* for each
-// of the registers needed on a system in a header file.  However many
-// compilers don't handle this very well and end up producing a
-// private definition in every file which includes the header file.
-// Along with the static constructors necessary for initialization it
-// can consume a significant amount of space in the result library.
-//
-// The following macros allow us to declare the instance in a .hpp and
-// produce an enumeration value which has the same number.  Then in a
-// .cpp the the register instance can be defined using the enumeration
-// value.  This avoids the use of static constructors and multiple
-// definitions per .cpp.  In addition #defines for the register can be
-// produced so that the constant registers can be inlined.  These
-// macros should not be used inside other macros, because you may get
-// multiple evaluations of the macros which can give bad results.
-//
-// Here are some example uses and expansions.  Note that the macro
-// invocation is terminated with a ;.
-//
-// CONSTANT_REGISTER_DECLARATION(Register, G0, 0);
-//
-// extern const Register G0 ;
-// enum { G0_RegisterEnumValue = 0 } ;
-//
-// REGISTER_DECLARATION(Register, Gmethod, G5);
-//
-// extern const Register Gmethod ;
-// enum { Gmethod_RegisterEnumValue = G5_RegisterEnumValue } ;
-//
-// REGISTER_DEFINITION(Register, G0);
-//
-// const Register G0 = ( ( Register ) G0_RegisterEnumValue ) ;
-//
-
-#define CONSTANT_REGISTER_DECLARATION(type, name, value) \
-extern const type name;                                  \
-enum { name##_##type##EnumValue = (value) }
-
-#define REGISTER_DECLARATION(type, name, value) \
-extern const type name;                         \
-enum { name##_##type##EnumValue = value##_##type##EnumValue }
-
-#define REGISTER_DEFINITION(type, name) \
-const type name = ((type)name##_##type##EnumValue)
-
-#endif // USE_EXTERN_CONST_FOR_REGISTER_DEFINITIONS
 
 #include CPU_HEADER(register)
 
