fc0fd96a5f8faf09264edf4fbc94bf1d8ac7b3a2
Thomas Schatzl <tschatzl@openjdk.org>
2021-06-29 08:34:55 +0000

    8269126: Rename G1AllowPreventiveGC option to G1UsePreventiveGC
    Reviewed-by: kbarrett, iwalulya

--- test/hotspot/jtreg/gc/g1/TestGCLogMessages.java
+++ test/hotspot/jtreg/gc/g1/TestGCLogMessages.java
@@ -242,7 +242,7 @@ public class TestGCLogMessages {
                                                                   "-XX:G1EvacuationFailureALotCount=100",
                                                                   "-XX:G1EvacuationFailureALotInterval=1",
                                                                   "-XX:+UnlockDiagnosticVMOptions",
-                                                                  "-XX:-G1AllowPreventiveGC",
+                                                                  "-XX:-G1UsePreventiveGC",
                                                                   "-Xlog:gc+phases=debug",
                                                                   GCTestWithEvacuationFailure.class.getName());
 
@@ -255,7 +255,7 @@ public class TestGCLogMessages {
                                                    "-Xmn16M",
                                                    "-Xms32M",
                                                    "-XX:+UnlockDiagnosticVMOptions",
-                                                   "-XX:-G1AllowPreventiveGC",
+                                                   "-XX:-G1UsePreventiveGC",
                                                    "-Xlog:gc+phases=trace",
                                                    GCTestWithEvacuationFailure.class.getName());
 
