1f859463172394752d128f28b368fd102690b4ca
Vicente Romero <vicente.romero@oracle.com>
2021-07-01 11:34:57 -0400

    addressing review comments

--- test/langtools/tools/javac/generics/diamond/protectedConstructor/ProtectedConstructorTest.java
+++ test/langtools/tools/javac/generics/diamond/protectedConstructor/ProtectedConstructorTest.java
@@ -29,11 +29,15 @@
  */
 
 import pkg.Bar;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.function.Supplier;
 
 class ProtectedConstructorTest {
     public void foo() {
         supply(getSupplier(new Bar<>(){}));
+        CompletableFuture<List<String>> completableFuture = getCompletableFuture(getSupplier(new Bar<>(){}));
+        completableFuture = getCompletableFuture(() -> getList(null, new Bar<>() {}));
     }
 
     static <U> Supplier<U> getSupplier(Bar<U> t) {
@@ -41,4 +45,6 @@ class ProtectedConstructorTest {
     }
 
     static <U> void supply(Supplier<U> supplier) {}
+    static <U> CompletableFuture<U> getCompletableFuture(Supplier<U> supplier) { return null; }
+    <T> List<T> getList(final Supplier<List<T>> supplier, Bar<T> t) { return null; }
 }
