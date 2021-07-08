3a2288dac4352562ad8c9f12523893f224be2964
matias9927 <matias@udel.edu>
2021-07-08 08:12:58 -0700

    Added Test java files

---
+++ test/hotspot/jtreg/runtime/Preverifier/recursiveJSRTest.java
@@ -0,0 +1,84 @@
+ /*
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @library /test/lib
+ * @summary Check that hello world with JSR and RET runs the same after instructions are replaced.
+ * @modules java.base/jdk.internal.misc
+ *          java.management
+ *          java.base/jdk.internal.vm
+ * @compile recursiveJSR.jasm
+            testPatch.java
+ * @run main/othervm -Xverify:all helloworldjsrTest
+ */
+
+import java.lang.reflect.Method;
+import java.io.File;
+import jdk.test.lib.process.ProcessTools;
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.internal.vm.Preverifier;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.io.IOException;
+
+public class recursiveJSRTest {
+	public static void main(String[] args) throws Throwable {
+        String fileName = "recursiveJSR";
+		Class<?> newClass;
+        try {
+            newClass = Class.forName(fileName);
+            Method m = newClass.getMethod("test");
+            m.invoke(newClass.newInstance());
+            byte[] newClassBytes = Preverifier.patch(
+                new String[]{newClass.getProtectionDomain()
+                .getCodeSource().getLocation().getPath() + fileName}
+            );
+            try {
+                Path tmpDir;
+                if (!Files.exists(Path.of("/tmp/preverifier/"))) {
+                    tmpDir = Files.createDirectory(Path.of("/tmp/preverifier/"));   
+                    try {
+                        ProcessBuilder pb = ProcessTools.createJavaProcessBuilder(
+                            "/tmp/preverifier/" + fileName);
+                        throw new RuntimeException(
+                            "StackMapCheck failed, BadMap did not throw VerifyError");
+                    } catch (Exception e) {
+                        System.out.println("recursiveJSR passed, error thrown");
+                    }
+                }
+                else {
+                    tmpDir = Path.of("/tmp/preverifier/");
+                }
+                Path tmpFile = Path.of(tmpDir.toString() + fileName + ".class");
+                Files.write(tmpFile, newClassBytes, 
+                    StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+            } catch (IOException e) {
+                throw new Error("Cannot write file", e);
+            }
+        } catch (Exception e) {
+            System.out.println("Class not found");
+        }
+	}
+}
