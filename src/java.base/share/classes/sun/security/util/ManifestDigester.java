b3054c8168f132ffa91b72b80264986776701a38
Kiran Sidhartha Ravikumar <kravikumar@openjdk.org>
2021-07-08 10:42:58 +0000

    8268506: More Manifest Digests
    Backport-of: 306a51b13f5307b0de9d2f04daa983ccd8f92284

--- src/java.base/share/classes/sun/security/util/ManifestDigester.java
+++ src/java.base/share/classes/sun/security/util/ManifestDigester.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -331,8 +331,12 @@ public class ManifestDigester {
      * @see #MF_MAIN_ATTRS
      */
     public Entry getMainAttsEntry(boolean oldStyle) {
-        mainAttsEntry.oldStyle = oldStyle;
-        return mainAttsEntry;
+        if (mainAttsEntry != null) {
+            mainAttsEntry.oldStyle = oldStyle;
+            return mainAttsEntry;
+        } else {
+            return null;
+        }
     }
 
     public Entry get(String name) {
