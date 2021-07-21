a6eaf58607c556c646f0f7a3b684d6c716859df5
tstuefe <thomas.stuefe@gmail.com>
2021-07-21 17:15:11 +0200

    kbarret feedback

--- src/hotspot/share/memory/arena.hpp
+++ src/hotspot/share/memory/arena.hpp
@@ -138,8 +138,10 @@ protected:
     // allocation was AmallocWords. Note though that padding plays havoc with arenas holding packed arrays,
     // like HandleAreas. Those areas should never mix Amalloc.. calls with differing alignment.
 #ifndef LP64 // Since this is a hot path, and on 64-bit Amalloc and AmallocWords are identical, restrict this alignment to 32-bit.
-    _hwm = ARENA_ALIGN(_hwm);
-    _hwm = MIN2(_hwm, _max); // _max is not guaranteed to be 64 bit aligned.
+    if (x > 0) {
+      _hwm = ARENA_ALIGN(_hwm);
+      _hwm = MIN2(_hwm, _max); // _max is not guaranteed to be 64 bit aligned.
+    }
 #endif // !LP64
     return internal_amalloc(x, alloc_failmode);
   }
