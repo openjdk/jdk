153e75ea1f2a607d3390e28f7b53fcb4212a310e
Andy Herrick <andy.herrick@oracle.com>
2021-08-04 16:34:47 -0400

    JDK-8271868: Warn user when using mac-sign option with unsigned app-image.

--- src/jdk.jpackage/macosx/classes/jdk/jpackage/internal/MacAppImageBuilder.java
+++ src/jdk.jpackage/macosx/classes/jdk/jpackage/internal/MacAppImageBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -879,6 +879,5 @@ public class MacAppImageBuilder extends AbstractAppImageBuilder {
              return false;
          }
          return true;
-     } 
-
+     }
 }
