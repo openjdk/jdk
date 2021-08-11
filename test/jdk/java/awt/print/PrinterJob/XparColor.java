00999140eb17f14514359eeace4f7a48015845e3
lawrence.andrews <lawrence.andrews@oracle.com>
2021-08-11 10:29:25 -0500

    Rewrote the code for better starting the test and timeout

--- test/jdk/java/awt/print/PrinterJob/XparColor.java
+++ test/jdk/java/awt/print/PrinterJob/XparColor.java
@@ -31,35 +31,60 @@
  * @run main/manual/othervm -Dsun.java2d.metal=true XparColor
  */
 
-import java.awt.Dialog;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Dimension;
 import java.awt.Color;
-import java.awt.Frame;
-import java.awt.Button;
-import java.awt.TextArea;
-import java.awt.Panel;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.print.Printable;
 import java.awt.print.PrinterJob;
 import java.awt.print.PageFormat;
 import java.awt.print.PrinterException;
 import java.awt.geom.Ellipse2D;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.Timer;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 public class XparColor implements Printable {
+    private static final CountDownLatch countDownLatch = new CountDownLatch(1);
+    private static final int testTimeout = 300000;
+    private static volatile String testFailureMsg;
+    private static volatile boolean testPassed;
+    private static volatile boolean testFinished;
 
-    private static void init() {
-        String[] instructions =
-                {
-                        "This testcase will be launched twice, once for opengl and once for metal.",
-                        "This test verify that the BullsEye rings are printed correctly.",
-                        "The printout should show transparent rings with increasing darkness toward the center"
-                };
-        Sysout.createDialog();
-        Sysout.printInstructions(instructions);
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> createAndShowTestDialog());
 
+        try {
+            if (!countDownLatch.await(testTimeout, TimeUnit.MILLISECONDS)) {
+                throw new RuntimeException(String.format("Test timeout '%d ms' elapsed.", testTimeout));
+            }
+            if (!testPassed) {
+                String failureMsg = testFailureMsg;
+                if ((failureMsg != null) && (!failureMsg.trim().isEmpty())) {
+                    throw new RuntimeException(failureMsg);
+                } else {
+                    throw new RuntimeException("Test failed.");
+                }
+            }
+        } catch (InterruptedException ie) {
+            throw new RuntimeException(ie);
+        } finally {
+            testFinished = true;
+        }
+    }
+
+    private static void doTest() {
         XparColor xc = new XparColor();
         PrinterJob printJob = PrinterJob.getPrinterJob();
         printJob.setPrintable(xc);
@@ -70,6 +95,106 @@ public class XparColor implements Printable {
                 ex.printStackTrace();
             }
         }
+    }
+
+    private static void pass() {
+        testPassed = true;
+        countDownLatch.countDown();
+    }
+
+    private static void fail(String failureMsg) {
+        testFailureMsg = failureMsg;
+        testPassed = false;
+        countDownLatch.countDown();
+    }
+
+    private static String convertMillisToTimeStr(int millis) {
+        if (millis < 0) {
+            return "00:00:00";
+        }
+        int hours = millis / 3600000;
+        int minutes = (millis - hours * 3600000) / 60000;
+        int seconds = (millis - hours * 3600000 - minutes * 60000) / 1000;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    private static void createAndShowTestDialog() {
+        String testInstruction = "This testcase will be launched twice, once for opengl and once for metal.\n" +
+                "This test verify that the BullsEye rings are printed correctly.\n" +
+                "The printout should show transparent rings with increasing darkness toward the center";
+
+        final JDialog dialog = new JDialog();
+        dialog.setTitle("SaveFileWithoutPrinter");
+        dialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        dialog.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                dialog.dispose();
+                fail("Main dialog was closed.");
+            }
+        });
+
+        final JLabel testTimeoutLabel = new JLabel(String.format("Test timeout: %s", convertMillisToTimeStr(testTimeout)));
+        final long startTime = System.currentTimeMillis();
+        final Timer timer = new Timer(0, null);
+        timer.setDelay(1000);
+        timer.addActionListener((e) -> {
+            int leftTime = testTimeout - (int) (System.currentTimeMillis() - startTime);
+            if ((leftTime < 0) || testFinished) {
+                timer.stop();
+                dialog.dispose();
+            }
+            testTimeoutLabel.setText(String.format("Test timeout: %s", convertMillisToTimeStr(leftTime)));
+        });
+        timer.start();
+
+        JTextArea textArea = new JTextArea(testInstruction);
+        textArea.setEditable(false);
+
+        final JButton startTestButton = new JButton("Start Test");
+        final JButton passButton = new JButton("PASS");
+        final JButton failButton = new JButton("FAIL");
+        startTestButton.addActionListener((e) -> {
+            startTestButton.setEnabled(false);
+            new Thread(() -> {
+                try {
+                    doTest();
+
+                    SwingUtilities.invokeLater(() -> {
+                        passButton.setEnabled(true);
+                        failButton.setEnabled(true);
+                    });
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                    dialog.dispose();
+                    fail("Exception occurred in a thread executing the test.");
+                }
+            }).start();
+        });
+        passButton.setEnabled(false);
+        passButton.addActionListener((e) -> {
+            dialog.dispose();
+            pass();
+        });
+        failButton.setEnabled(false);
+        failButton.addActionListener((e) -> {
+            dialog.dispose();
+            fail("PrinterException thrown.");
+        });
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel labelPanel = new JPanel(new FlowLayout());
+        labelPanel.add(testTimeoutLabel);
+        mainPanel.add(labelPanel, BorderLayout.NORTH);
+        mainPanel.add(textArea, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel(new FlowLayout());
+        buttonPanel.add(startTestButton);
+        buttonPanel.add(passButton);
+        buttonPanel.add(failButton);
+        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        dialog.add(mainPanel);
+        dialog.pack();
+        dialog.setVisible(true);
     }
 
     public int print(Graphics g, PageFormat pf, int pi) throws PrinterException {
@@ -111,250 +236,5 @@ public class XparColor implements Printable {
             g2.fill(new Ellipse2D.Float(x, y, ew, eh));
         }
     }
-
-
-    /*****************************************************
-     Standard Test Machinery Section
-     DO NOT modify anything in this section -- it's a
-     standard chunk of code which has all of the
-     synchronisation necessary for the test harness.
-     By keeping it the same in all tests, it is easier
-     to read and understand someone else's test, as
-     well as insuring that all tests behave correctly
-     with the test harness.
-     There is a section following this for test-defined
-     classes
-     ******************************************************/
-    private static boolean theTestPassed = false;
-    private static boolean testGeneratedInterrupt = false;
-    private static String failureMessage = "";
-
-    private static Thread mainThread = null;
-
-    private static int sleepTime = 300000;
-
-    public static void main(String args[]) throws InterruptedException {
-        mainThread = Thread.currentThread();
-        try {
-            init();
-        } catch (TestPassedException e) {
-            //The test passed, so just return from main and harness will interpret this return as a pass
-            return;
-        }
-
-        /*
-            At this point, neither test passed nor test failed has been
-            called -- either would have thrown an exception and ended the
-            test, so we know we have multiple threads.
-            Test involves other threads, so sleep and wait for them to
-            called pass() or fail()
-         */
-        try {
-            Thread.sleep(sleepTime);
-            //Timed out, so fail the test
-            throw new RuntimeException("Timed out after " + sleepTime / 1000 + " seconds");
-        } catch (InterruptedException e) {
-            if (!testGeneratedInterrupt) throw e;
-
-            //reset flag in case hit this code more than once for some reason (just safety)
-            testGeneratedInterrupt = false;
-            if (theTestPassed == false) {
-                throw new RuntimeException(failureMessage);
-            }
-        }
-
-    }
-
-    public static synchronized void setTimeoutTo(int seconds) {
-        sleepTime = seconds * 1000;
-    }
-
-    public static synchronized void pass() {
-        Sysout.println("The test passed.");
-        Sysout.println("The test is over, hit  Ctl-C to stop Java VM");
-
-        // first check if this is executing in main thread
-        if (mainThread == Thread.currentThread()) {
-            /*
-             * Still in the main thread, so set the flag just for kicks,
-             * and throw a test passed exception which will be caught
-             * and end the test.
-             */
-            theTestPassed = true;
-            throw new TestPassedException();
-        }
-        /*
-         * pass was called from a different thread, so set the flag and interrupt the main thead.
-         */
-        theTestPassed = true;
-        testGeneratedInterrupt = true;
-        mainThread.interrupt();
-    }
-
-    public static synchronized void fail() {
-        // test writer didn't specify why test failed, so give generic
-        fail("it just plain failed! :-)");
-    }
-
-    public static synchronized void fail(String whyFailed) {
-        Sysout.println("The test failed: " + whyFailed);
-        Sysout.println("The test is over, hit  Ctl-C to stop Java VM");
-        //check if this called from main thread
-        if (mainThread == Thread.currentThread()) {
-            //If main thread, fail now 'cause not sleeping
-            throw new RuntimeException(whyFailed);
-        }
-        theTestPassed = false;
-        testGeneratedInterrupt = true;
-        failureMessage = whyFailed;
-        mainThread.interrupt();
-    }
-
 }
 
-/**
- * This exception is used to exit from any level of call nesting
- * when it's determined that the test has passed, and immediately
- * end the test.
- */
-class TestPassedException extends RuntimeException {
-}
-
-
-/****************************************************
- Standard Test Machinery
- DO NOT modify anything below -- it's a standard
- chunk of code whose purpose is to make user
- interaction uniform, and thereby make it simpler
- to read and understand someone else's test.
- ****************************************************/
-
-/**
- * This is part of the standard test machinery.
- * It creates a dialog (with the instructions), and is the interface
- * for sending text messages to the user.
- * To print the instructions, send an array of strings to Sysout.createDialog
- * WithInstructions method.  Put one line of instructions per array entry.
- * To display a message for the tester to see, simply call Sysout.println
- * with the string to be displayed.
- * This mimics System.out.println but works within the test harness as well
- * as standalone.
- */
-
-class Sysout {
-    private static TestDialog dialog;
-
-    public static void createDialogWithInstructions(String[] instructions) {
-        dialog = new TestDialog(new Frame(), "Instructions");
-        dialog.printInstructions(instructions);
-        dialog.show();
-        println("Any messages for the tester will display here.");
-    }
-
-    public static void createDialog() {
-        dialog = new TestDialog(new Frame(), "Instructions");
-        String[] defInstr = {"Instructions will appear here. ", ""};
-        dialog.printInstructions(defInstr);
-        dialog.show();
-        println("Any messages for the tester will display here.");
-    }
-
-    public static void printInstructions(String[] instructions) {
-        dialog.printInstructions(instructions);
-    }
-
-    public static void println(String messageIn) {
-        dialog.displayMessage(messageIn);
-    }
-
-}
-
-/**
- * This is part of the standard test machinery.  It provides a place for the
- * test instructions to be displayed, and a place for interactive messages
- * to the user to be displayed.
- * To have the test instructions displayed, see Sysout.
- * To have a message to the user be displayed, see Sysout.
- * Do not call anything in this dialog directly.
- */
-class TestDialog extends Dialog implements ActionListener {
-
-    TextArea instructionsText;
-    TextArea messageText;
-    int maxStringLength = 80;
-    Panel buttonP = new Panel();
-    Button passB = new Button("pass");
-    Button failB = new Button("fail");
-
-    //DO NOT call this directly, go through Sysout
-    public TestDialog(Frame frame, String name) {
-        super(frame, name);
-        int scrollBoth = TextArea.SCROLLBARS_BOTH;
-        instructionsText = new TextArea("", 15, maxStringLength, scrollBoth);
-        add("North", instructionsText);
-
-        messageText = new TextArea("", 5, maxStringLength, scrollBoth);
-        add("Center", messageText);
-
-        passB = new Button("pass");
-        passB.setActionCommand("pass");
-        passB.addActionListener(this);
-        buttonP.add("East", passB);
-
-        failB = new Button("fail");
-        failB.setActionCommand("fail");
-        failB.addActionListener(this);
-        buttonP.add("West", failB);
-
-        add("South", buttonP);
-        pack();
-        setVisible(true);
-    }
-
-    //DO NOT call this directly, go through Sysout
-    public void printInstructions(String[] instructions) {
-        instructionsText.setText("");
-
-        String printStr, remainingStr;
-        for (int i = 0; i < instructions.length; i++) {
-            //chop up each into pieces maxSringLength long
-            remainingStr = instructions[i];
-            while (remainingStr.length() > 0) {
-                //if longer than max then chop off first max chars to print
-                if (remainingStr.length() >= maxStringLength) {
-                    //Try to chop on a word boundary
-                    int posOfSpace = remainingStr.
-                            lastIndexOf(' ', maxStringLength - 1);
-
-                    if (posOfSpace <= 0) posOfSpace = maxStringLength - 1;
-
-                    printStr = remainingStr.substring(0, posOfSpace + 1);
-                    remainingStr = remainingStr.substring(posOfSpace + 1);
-                } else {
-                    printStr = remainingStr;
-                    remainingStr = "";
-                }
-
-                instructionsText.append(printStr + "\n");
-            }
-        }
-    }
-
-    //DO NOT call this directly, go through Sysout
-    public void displayMessage(String messageIn) {
-        messageText.append(messageIn + "\n");
-    }
-
-    /**
-     * Catch presses of the passed and failed buttons. Wimply call the standard pass() or fail()
-     * static methods of XparColor
-     */
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand() == "pass") {
-            XparColor.pass();
-        } else {
-            XparColor.fail();
-        }
-    }
-
-}
