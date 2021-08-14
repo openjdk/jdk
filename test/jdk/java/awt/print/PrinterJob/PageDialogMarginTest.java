2c181a8aef975369f982e78aac0da89540a304c5
Rajat Mahajan <rajat.m.mahajan@oracle.com>
2021-08-14 02:09:08 +0100

    Fix IDE warnings, added Default Locale as US, fixed Copyright to include current year.

--- test/jdk/java/awt/print/PrinterJob/PageDialogMarginTest.java
+++ test/jdk/java/awt/print/PrinterJob/PageDialogMarginTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2021,  Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,29 +33,33 @@ import java.awt.print.PageFormat;
 import java.awt.print.PrinterJob;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
+import javax.print.attribute.standard.DialogTypeSelection;
 import javax.print.attribute.standard.MediaPrintableArea;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import java.text.MessageFormat;
+import java.util.Locale;
 
 public class PageDialogMarginTest {
 
-    public static void main(String args[]) throws Exception {
+    public static void main(String[] args) throws Exception {
+
+        Locale.setDefault(Locale.US);
         String[] instructions
                 = {
                     "Page Dialog will be shown.",
                     "Change top(in) margin value from 1.0 to 2.0",
                     "Then select OK."
                 };
-        SwingUtilities.invokeAndWait(() -> {
-            JOptionPane.showMessageDialog((Component) null,
-                    instructions, "Instructions",
-                    JOptionPane.INFORMATION_MESSAGE);
-        });
+        SwingUtilities.invokeAndWait(() -> JOptionPane.showMessageDialog(null,
+                instructions, "Instructions",
+                JOptionPane.INFORMATION_MESSAGE));
         PrinterJob pj = PrinterJob.getPrinterJob();
 
         HashPrintRequestAttributeSet aset = new HashPrintRequestAttributeSet();
-        PageFormat pf;
-        pf = pj.pageDialog(aset);
+
+        PageFormat pf = pj.pageDialog(aset);
+
         double left = pf.getImageableX();
         double top = pf.getImageableY();
         System.out.println("pageDialog - left/top from pageFormat: " + left / 72
