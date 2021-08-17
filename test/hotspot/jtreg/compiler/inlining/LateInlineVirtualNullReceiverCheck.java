ad3585345ff2e5416045113dcd56cad265156acb
Vladimir Ivanov <vladimir.x.ivanov@oracle.com>
2021-08-17 20:24:52 +0300

    Whitespaces

--- test/hotspot/jtreg/compiler/inlining/LateInlineVirtualNullReceiverCheck.java
+++ test/hotspot/jtreg/compiler/inlining/LateInlineVirtualNullReceiverCheck.java
@@ -28,23 +28,23 @@
  */
 package compiler.inlining;
 
-import java.util.regex.Pattern; 
+import java.util.regex.Pattern;
 
-public class LateInlineVirtualNullReceiverCheck { 
-    static final Pattern pattern = Pattern.compile(""); 
+public class LateInlineVirtualNullReceiverCheck {
+    static final Pattern pattern = Pattern.compile("");
 
-    public static void test(String s) { 
-        pattern.matcher(s); 
-    } 
+    public static void test(String s) {
+        pattern.matcher(s);
+    }
 
-    public static void main(String[] args) { 
-        for (int i = 0; i < 10_000; ++i) { 
-            try { 
-                test(null); 
-            } catch (NullPointerException npe) { 
+    public static void main(String[] args) {
+        for (int i = 0; i < 10_000; ++i) {
+            try {
+                test(null);
+            } catch (NullPointerException npe) {
                 // ignore
-            } 
-        } 
+            }
+        }
         System.out.println("TEST PASSED");
-    } 
-} 
+    }
+}
