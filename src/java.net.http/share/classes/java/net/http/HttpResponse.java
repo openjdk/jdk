3626552d8d6e5f581581183e0db7f53d223c70aa
Andrey Turbanov <turbanoff@gmail.com>
2021-08-18 10:53:44 +0300

    [PATCH] Cleanup Optional usage in HttpResponse.BodySubscribers.discarding

--- src/java.net.http/share/classes/java/net/http/HttpResponse.java
+++ src/java.net.http/share/classes/java/net/http/HttpResponse.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1273,7 +1273,7 @@ public interface HttpResponse<T> {
          * @return a response body subscriber
          */
         public static BodySubscriber<Void> discarding() {
-            return new ResponseSubscribers.NullSubscriber<>(Optional.ofNullable(null));
+            return new ResponseSubscribers.NullSubscriber<>(Optional.empty());
         }
 
         /**
