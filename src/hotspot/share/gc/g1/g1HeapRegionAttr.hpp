64d072f66086977c70da8b9c91d0c4cbf4481ad6
Albert Yang <albert.m.yang@oracle.com>
2021-08-20 13:07:05 +0200

    Revert "boolean"
    This reverts commit 2a23beb0549a928ae3876070dbdcf5a153c0703d.

--- src/hotspot/share/gc/g1/g1HeapRegionAttr.hpp
+++ src/hotspot/share/gc/g1/g1HeapRegionAttr.hpp
@@ -39,9 +39,10 @@ public:
 #else
   typedef int8_t region_type_t;
 #endif
+  typedef uint8_t needs_remset_update_t;
 
 private:
-  bool _needs_remset_update;
+  needs_remset_update_t _needs_remset_update;
   region_type_t _type;
 
 public:
@@ -80,14 +81,14 @@ public:
     }
   }
 
-  bool needs_remset_update() const     { return _needs_remset_update; }
+  bool needs_remset_update() const     { return _needs_remset_update != 0; }
 
   void set_old()                       { _type = Old; }
   void clear_humongous()               {
     assert(is_humongous() || !is_in_cset(), "must be");
     _type = NotInCSet;
   }
-  void set_has_remset(bool value)      { _needs_remset_update = value; }
+  void set_has_remset(bool value)      { _needs_remset_update = value ? 1 : 0; }
 
   bool is_in_cset_or_humongous() const { return is_in_cset() || is_humongous(); }
   bool is_in_cset() const              { return type() >= Young; }
