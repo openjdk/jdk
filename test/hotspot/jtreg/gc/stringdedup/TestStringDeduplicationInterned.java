c1f12158b3afc706e84a0508cf12597af846960c
Denghui Dong <ddong@openjdk.org>
2021-08-23 06:12:01 +0000

    8272609: Add string deduplication support to SerialGC
    Reviewed-by: kbarrett, iwalulya

--- test/hotspot/jtreg/gc/stringdedup/TestStringDeduplicationInterned.java
+++ test/hotspot/jtreg/gc/stringdedup/TestStringDeduplicationInterned.java
@@ -27,6 +27,19 @@ package gc.stringdedup;
  * @test TestStringDeduplicationInterned
  * @summary Test string deduplication of interned strings
  * @bug 8029075
+ * @requires vm.gc.Serial
+ * @library /test/lib
+ * @library /
+ * @modules java.base/jdk.internal.misc:open
+ * @modules java.base/java.lang:open
+ *          java.management
+ * @run driver gc.stringdedup.TestStringDeduplicationInterned Serial
+ */
+
+/*
+ * @test TestStringDeduplicationInterned
+ * @summary Test string deduplication of interned strings
+ * @bug 8029075
  * @requires vm.gc.G1
  * @library /test/lib
  * @library /
