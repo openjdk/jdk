6ae1b554c754b0cca4e46db965a14fbea523da8b
Bin Liao <buddyliao@tencent.com>
2021-08-30 19:28:27 +0800

    8272611: Use parallel heap inspection when print classhisto info in gc log

--- src/hotspot/share/gc/shared/collectedHeap.cpp
+++ src/hotspot/share/gc/shared/collectedHeap.cpp
@@ -548,8 +548,8 @@ void CollectedHeap::full_gc_dump(GCTimer* timer, bool before) {
     GCTraceTime(Trace, gc, classhisto) tm(before ? "Class Histogram (before full gc)" : "Class Histogram (after full gc)", timer);
     ResourceMark rm;
     LogStream ls(lt);
-    uint parallel_thread_num = MAX2<uint>(1, (uint)os::initial_active_processor_count() * 3 / 8);
-    VM_GC_HeapInspection inspector(&ls, false /* ! full gc */, parallel_thread_num);
+    uint parallel_thread_num = safepoint_workers()->active_workers();
+    VM_GC_HeapInspection inspector(&ls, false /* request full gc */, parallel_thread_num);
     inspector.doit();
   }
 }
