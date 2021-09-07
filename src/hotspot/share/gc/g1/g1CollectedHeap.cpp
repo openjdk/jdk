bab3b91eea674653b754a9f9d9d569e7ed3103cc
Thomas Schatzl <thomas.schatzl@oracle.com>
2021-09-07 19:28:50 +0200

    Rename atomic to concurrent/stw

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -1835,7 +1835,7 @@ void G1CollectedHeap::ref_processing_init() {
                            // thread counts must be considered for discovery.
                            (ParallelGCThreads > 1) || (ConcGCThreads > 1), // mt discovery
                            MAX2(ParallelGCThreads, ConcGCThreads),         // degree of mt discovery
-                           false,                                          // Reference discovery is not atomic
+                           true,                                           // Reference discovery is concurrent
                            &_is_alive_closure_cm);                         // is alive closure
 
   // STW ref processor
@@ -1844,7 +1844,7 @@ void G1CollectedHeap::ref_processing_init() {
                            ParallelGCThreads,                    // degree of mt processing
                            (ParallelGCThreads > 1),              // mt discovery
                            ParallelGCThreads,                    // degree of mt discovery
-                           true,                                 // Reference discovery is atomic
+                           false,                                // Reference discovery is not concurrent
                            &_is_alive_closure_stw);              // is alive closure
 }
 
