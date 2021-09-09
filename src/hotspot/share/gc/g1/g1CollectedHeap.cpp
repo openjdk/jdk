9690df7fb9843bdc4775a34d94b2ca81f40aea0a
Hamlin Li <mli@openjdk.org>
2021-09-09 10:41:27 +0000

    8273476: G1: refine G1CollectedHeap::par_iterate_regions_array_part_from
    Reviewed-by: ayang, tschatzl

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -2326,12 +2326,11 @@ void G1CollectedHeap::collection_set_iterate_increment_from(HeapRegionClosure *c
   _collection_set.iterate_incremental_part_from(cl, hr_claimer, worker_id);
 }
 
-void G1CollectedHeap::par_iterate_regions_array_part_from(HeapRegionClosure* cl,
-                                                          HeapRegionClaimer* hr_claimer,
-                                                          const uint* regions,
-                                                          size_t offset,
-                                                          size_t length,
-                                                          uint worker_id) const {
+void G1CollectedHeap::par_iterate_regions_array(HeapRegionClosure* cl,
+                                                HeapRegionClaimer* hr_claimer,
+                                                const uint regions[],
+                                                size_t length,
+                                                uint worker_id) const {
   assert_at_safepoint();
   if (length == 0) {
     return;
@@ -2342,7 +2341,7 @@ void G1CollectedHeap::par_iterate_regions_array_part_from(HeapRegionClosure* cl,
   size_t cur_pos = start_pos;
 
   do {
-    uint region_idx = regions[cur_pos + offset];
+    uint region_idx = regions[cur_pos];
     if (hr_claimer == NULL || hr_claimer->claim_region(region_idx)) {
       HeapRegion* r = region_at(region_idx);
       bool result = cl->do_heap_region(r);
