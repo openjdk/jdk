5ed6cf04c0fe0017070cd8c79e902c310a29d104
lmesnik <leonid.mesnik@oracle.com>
2021-09-15 19:30:48 -0700

    ObjectReference added

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/ObjectReference/getValue/getvalue001a.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/ObjectReference/getValue/getvalue001a.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -94,8 +94,8 @@ public class getvalue001a {
     //------------------------------------------------------  section tested
 
                 case 0:
-                         Threadgetvalue001a test_thread =
-                             new Threadgetvalue001a("testedThread");
+                         Thread test_thread =
+                                 JDIThreadFactory.newThread(new Threadgetvalue001a("testedThread"));
                          log1("       thread2 is created");
 
                          label:
@@ -145,7 +145,7 @@ public class getvalue001a {
     }
 }
 
-class Threadgetvalue001a extends Thread {
+class Threadgetvalue001a extends JDITask {
 
     public Threadgetvalue001a(String threadName) {
         super(threadName);
