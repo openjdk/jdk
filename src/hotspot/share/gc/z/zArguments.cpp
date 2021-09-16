a76baace7e1b4931a1067fe1bcd60158dfac076e
Per Liden <pliden@openjdk.org>
2021-09-16 14:30:47 +0200

    Fix comment

--- src/hotspot/share/gc/z/zArguments.cpp
+++ src/hotspot/share/gc/z/zArguments.cpp
@@ -72,7 +72,7 @@ void ZArguments::initialize() {
     vm_exit_during_initialization("The flag -XX:+UseZGC can not be combined with -XX:ConcGCThreads=0");
   }
 
-  // Only 2M large pages are supported
+  // Large page size must match granule size
   if (!FLAG_IS_DEFAULT(LargePageSizeInBytes) && LargePageSizeInBytes != ZGranuleSize) {
     vm_exit_during_initialization(err_msg("Incompatible -XX:LargePageSizeInBytes, only "
                                           SIZE_FORMAT "M large pages are supported by ZGC",
