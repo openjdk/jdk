337ad5bb7d20d35dbbca9390ad190ca33648eb7a
AlexanderScherbatiy <alexander.scherbatiy@bell-sw.com>
2021-09-17 12:57:26 +0300

    Remove @SuppressWarnings(removal)

--- src/java.desktop/share/classes/sun/font/FontManagerFactory.java
+++ src/java.desktop/share/classes/sun/font/FontManagerFactory.java
@@ -51,7 +51,6 @@ public final class FontManagerFactory {
      *
      * @return a valid FontManager instance for the current platform
      */
-    @SuppressWarnings("removal")
     public static synchronized FontManager getInstance() {
 
         if (instance == null) {
