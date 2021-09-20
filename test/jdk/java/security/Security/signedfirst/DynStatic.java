ee2d308088bd3a4c9d66efebb3ec261bd54218aa
Sean Mullan <sean.mullan@oracle.com>
2021-09-20 11:28:02 -0400

    Remove trailing whitespace.

--- test/jdk/java/security/Security/signedfirst/DynStatic.java
+++ test/jdk/java/security/Security/signedfirst/DynStatic.java
@@ -1,11 +1,11 @@
 /*
  * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- * 
+ *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.
- * 
+ *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
@@ -19,7 +19,7 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- */ 
+ */
 /*
  * @test
  * @bug 4504355 4744260
