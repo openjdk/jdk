6440a8c410cf1ddcb881733b6c0d7ddc705920cc
Coleen Phillimore <coleen.phillimore@oracle.com>
2021-09-21 19:41:52 -0400

    Remove special comment.

--- src/hotspot/share/runtime/mutexLocker.hpp
+++ src/hotspot/share/runtime/mutexLocker.hpp
@@ -54,7 +54,7 @@ extern Mutex*   VtableStubs_lock;                // a lock on the VtableStubs
 extern Mutex*   SymbolArena_lock;                // a lock on the symbol table arena
 extern Monitor* StringDedup_lock;                // a lock on the string deduplication facility
 extern Mutex*   StringDedupIntern_lock;          // a lock on StringTable notification of StringDedup
-extern Monitor* CodeCache_lock;                  // a lock on the CodeCache, rank is special
+extern Monitor* CodeCache_lock;                  // a lock on the CodeCache
 extern Monitor* CodeSweeper_lock;                // a lock used by the sweeper only for wait notify
 extern Mutex*   MethodData_lock;                 // a lock on installation of method data
 extern Mutex*   TouchedMethodLog_lock;           // a lock on allocation of LogExecutedMethods info
