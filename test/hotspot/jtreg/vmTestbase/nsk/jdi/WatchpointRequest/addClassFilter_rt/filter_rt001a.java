a72c8aa6ad58bad57ddbd65139bcb88b6d8aa5db
Leonid Mesnik <lmesnik@openjdk.org>
2021-09-21 23:36:16 +0000

    8273921: Refactor NSK/JDI tests to create thread using factory
    Reviewed-by: cjplummer, sspitsyn, amenkov

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/WatchpointRequest/addClassFilter_rt/filter_rt001a.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/WatchpointRequest/addClassFilter_rt/filter_rt001a.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,7 +24,6 @@
 package nsk.jdi.WatchpointRequest.addClassFilter_rt;
 
 import nsk.share.*;
-import nsk.share.jpda.*;
 import nsk.share.jdi.*;
 
 /**
@@ -54,8 +53,8 @@ public class filter_rt001a {
 
     //====================================================== test program
 
-    static Thread1filter_rt001a thread1 = null;
-    static Thread2filter_rt001a thread2 = null;
+    static Thread thread1 = null;
+    static Thread thread2 = null;
 
     static TestClass10 obj10 = new TestClass10();
     static TestClass11 obj11 = new TestClass11();
@@ -108,11 +107,11 @@ public class filter_rt001a {
     //------------------------------------------------------  section tested
 
                     case 0:
-                            thread1 = new Thread1filter_rt001a("thread1");
+                            thread1 = JDIThreadFactory.newThread(new Thread1filter_rt001a("thread1"));
                             log1("run1(thread1);");
                             run1(thread1);
 
-                            thread2 = new Thread2filter_rt001a("thread2");
+                            thread2 = JDIThreadFactory.newThread(new Thread2filter_rt001a("thread2"));
                             log1("run1(thread2);");
                             run1(thread2);
 
@@ -182,44 +181,38 @@ class TestClass11 extends TestClass10 {
     }
 }
 
-class Thread1filter_rt001a extends Thread {
-
-    String tName = null;
+class Thread1filter_rt001a extends NamedTask {
 
     public Thread1filter_rt001a(String threadName) {
         super(threadName);
-        tName = threadName;
     }
 
     public void run() {
-        filter_rt001a.log1("  'run': enter  :: threadName == " + tName);
+        filter_rt001a.log1("  'run': enter  :: threadName == " + getName());
         synchronized (filter_rt001a.waitnotifyObj) {
            filter_rt001a.waitnotifyObj.notify();
         }
             TestClass10.method();
             TestClass11.method();
-        filter_rt001a.log1("  'run': exit   :: threadName == " + tName);
+        filter_rt001a.log1("  'run': exit   :: threadName == " + getName());
         return;
     }
 }
 
-class Thread2filter_rt001a extends Thread {
-
-    String tName = null;
+class Thread2filter_rt001a extends NamedTask {
 
     public Thread2filter_rt001a(String threadName) {
         super(threadName);
-        tName = threadName;
     }
 
     public void run() {
-        filter_rt001a.log1("  'run': enter  :: threadName == " + tName);
+        filter_rt001a.log1("  'run': enter  :: threadName == " + getName());
         synchronized (filter_rt001a.waitnotifyObj) {
             filter_rt001a.waitnotifyObj.notify();
         }
             TestClass20.method();
             TestClass21.method();
-        filter_rt001a.log1("  'run': exit   :: threadName == " + tName);
+        filter_rt001a.log1("  'run': exit   :: threadName == " + getName());
         return;
     }
 
