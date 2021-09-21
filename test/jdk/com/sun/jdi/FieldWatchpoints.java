81e45ca783b7be7894b2b420e693f5058273ecbe
fmatte <fairoz.matte@oracle.com>
2021-09-21 07:38:29 +0000

    8206438: com/sun/jdi/FieldWatchpoints.java timeout intermittently

--- test/jdk/com/sun/jdi/FieldWatchpoints.java
+++ test/jdk/com/sun/jdi/FieldWatchpoints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -29,7 +29,7 @@
  *
  * @run build TestScaffold VMConnection TargetListener TargetAdapter
  * @run compile -g FieldWatchpoints.java
- * @run driver FieldWatchpoints
+ * @run main/othervm/timeout=240 FieldWatchpoints
  */
 import com.sun.jdi.*;
 import com.sun.jdi.event.*;
