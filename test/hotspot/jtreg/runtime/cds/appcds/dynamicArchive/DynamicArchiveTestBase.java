806ae76265e1118d57998c1ec53237135d1df74d
iklam <ioi.lam@oracle.com>
2021-09-22 08:58:32 -0700

    fixed typo; removed fix for handling of CDSUtils::getRuntimePrefix() to be done in a separate bug

--- test/hotspot/jtreg/runtime/cds/appcds/dynamicArchive/DynamicArchiveTestBase.java
+++ test/hotspot/jtreg/runtime/cds/appcds/dynamicArchive/DynamicArchiveTestBase.java
@@ -182,9 +182,7 @@ class DynamicArchiveTestBase {
         String archiveFiles = (baseArchiveName == null) ? topArchiveName :
             (topArchiveName == null) ? baseArchiveName :
             baseArchiveName + File.pathSeparator + topArchiveName;
-        CDSOptions opts = new CDSOptions();
         String[] cmdLine = TestCommon.concat(
-            opts.getRuntimePrefix(),
             "-Xshare:on",
             "-XX:SharedArchiveFile=" + archiveFiles);
         cmdLine = TestCommon.concat(cmdLine, cmdLineSuffix);
@@ -203,9 +201,7 @@ class DynamicArchiveTestBase {
         String archiveFiles = (baseArchiveName == null) ? topArchiveName :
             (topArchiveName == null) ? baseArchiveName :
             baseArchiveName + File.pathSeparator + topArchiveName;
-        CDSOptions opts = new CDSOptions();
         String[] cmdLine = TestCommon.concat(
-            opts.getRuntimePrefix(),
             "-Xshare:on",
             "-XX:SharedArchiveFile=" + archiveFiles);
         cmdLine = TestCommon.concat(cmdLine, cmdLineSuffix);
