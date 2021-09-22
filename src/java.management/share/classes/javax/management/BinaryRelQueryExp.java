ce8f707e7cae5bbead349835b6165ebd01888c1b
Andrey Turbanov <turbanoff@gmail.com>
2021-09-22 23:28:23 +0300

    Avoid String.compareTo == 0 to check String equality

--- src/java.management/share/classes/javax/management/BinaryRelQueryExp.java
+++ src/java.management/share/classes/javax/management/BinaryRelQueryExp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -177,7 +177,7 @@ class BinaryRelQueryExp extends QueryEval implements QueryExp {
             case Query.LE:
                 return sval1.compareTo(sval2) <= 0;
             case Query.EQ:
-                return sval1.compareTo(sval2) == 0;
+                return sval1.equals(sval2);
             }
         }
 
