f8b15c13a3146061e12960c43baa3e84fd411e5a
Christian Stein <sormuras@gmail.com>
2021-09-23 16:49:28 +0200

    Add JBS issue `8262944` to the `@bug` list and update copyright year

--- test/jdk/java/lang/module/AutomaticModulesTest.java
+++ test/jdk/java/lang/module/AutomaticModulesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /**
  * @test
- * @bug 8142968 8253751
+ * @bug 8142968 8253751 8262944
  * @library /test/lib
  * @build AutomaticModulesTest
  *        jdk.test.lib.util.JarUtils
