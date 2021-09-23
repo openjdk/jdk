a8fd00b65d494c7e17b3a14eee95d133347bd81d
Pavel Rappo <prappo@openjdk.org>
2021-09-23 13:00:22 +0100

    Initial commit

--- src/java.base/share/classes/java/nio/file/DirectoryStream.java
+++ src/java.base/share/classes/java/nio/file/DirectoryStream.java
@@ -65,7 +65,7 @@ import java.io.IOException;
  * using the {@code Iterator}, behaves as if the end of stream has been reached.
  * Due to read-ahead, the {@code Iterator} may return one or more elements
  * after the directory stream has been closed. Once these buffered elements
- * have been read, then subsequent calls to the {@code hasNext} method returns
+ * have been read, then subsequent calls to the {@code hasNext} method return
  * {@code false}, and subsequent calls to the {@code next} method will throw
  * {@code NoSuchElementException}.
  *
