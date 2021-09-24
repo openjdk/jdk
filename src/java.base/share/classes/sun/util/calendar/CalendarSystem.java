8855f91021fe896b5b7f098797ddb8fe1c950c1c
jpai <jaikiran.pai@gmail.com>
2021-09-24 19:37:30 +0530

    8273790: Potential cyclic dependencies between Gregorian and CalendarSystem

--- src/java.base/share/classes/sun/util/calendar/CalendarSystem.java
+++ src/java.base/share/classes/sun/util/calendar/CalendarSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -111,7 +111,7 @@ public abstract class CalendarSystem {
         }
     }
 
-    private static final Gregorian GREGORIAN_INSTANCE = new Gregorian();
+    private static volatile Gregorian GREGORIAN_INSTANCE;
 
     /**
      * Returns the singleton instance of the <code>Gregorian</code>
@@ -120,7 +120,17 @@ public abstract class CalendarSystem {
      * @return the <code>Gregorian</code> instance
      */
     public static Gregorian getGregorianCalendar() {
-        return GREGORIAN_INSTANCE;
+        var gCal = GREGORIAN_INSTANCE;
+        if (gCal != null) {
+            return gCal;
+        }
+        synchronized (CalendarSystem.class) {
+            gCal = GREGORIAN_INSTANCE;
+            if (gCal == null) {
+                gCal = GREGORIAN_INSTANCE = new Gregorian();
+            }
+        }
+        return gCal;
     }
 
     /**
@@ -135,7 +145,7 @@ public abstract class CalendarSystem {
      */
     public static CalendarSystem forName(String calendarName) {
         if ("gregorian".equals(calendarName)) {
-            return GREGORIAN_INSTANCE;
+            return getGregorianCalendar();
         }
 
         if (!initialized) {
