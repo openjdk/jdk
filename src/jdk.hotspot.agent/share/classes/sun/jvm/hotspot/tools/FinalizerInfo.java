0abf9527f0a35e4d70945e47f94aa4eb0b8aa1ff
Andrey Turbanov <turbanoff@gmail.com>
2021-09-25 14:14:46 +0300

    8272992: Replace usages of Collections.sort with List.sort call in jdk.hotspot.agent

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/FinalizerInfo.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/tools/FinalizerInfo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,7 +30,6 @@ import sun.jvm.hotspot.oops.*;
 import sun.jvm.hotspot.utilities.SystemDictionaryHelper;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 
@@ -126,11 +125,7 @@ public class FinalizerInfo extends Tool {
              */
             ArrayList<ObjectHistogramElement> list = new ArrayList<>();
             list.addAll(map.values());
-            Collections.sort(list, new Comparator<>() {
-              public int compare(ObjectHistogramElement o1, ObjectHistogramElement o2) {
-                  return o1.compare(o2);
-              }
-            });
+            list.sort(ObjectHistogramElement::compare);
 
             /*
              * Print summary of objects in queue
