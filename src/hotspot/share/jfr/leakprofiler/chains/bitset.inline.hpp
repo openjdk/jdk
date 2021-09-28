3fd8ccec25ec91eb2336a3b54fec69530d619523
kvergizova <katya@azul.com>
2021-09-28 13:46:15 +0300

    JDK-8274435: EXCEPTION_ACCESS_VIOLATION in BFSClosure::closure_impl

--- src/hotspot/share/jfr/leakprofiler/chains/bitset.inline.hpp
+++ src/hotspot/share/jfr/leakprofiler/chains/bitset.inline.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -68,7 +68,7 @@ inline BitMap::idx_t BitSet::addr_to_bit(uintptr_t addr) const {
 
 inline CHeapBitMap* BitSet::get_fragment_bits(uintptr_t addr) {
   uintptr_t granule = addr >> _bitmap_granularity_shift;
-  if (granule == _last_fragment_granule) {
+  if (granule == _last_fragment_granule && _last_fragment_bits != NULL) {
     return _last_fragment_bits;
   }
   CHeapBitMap* bits = NULL;
