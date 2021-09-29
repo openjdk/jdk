3f7f3045463a8ef072ee8da28961d0543d773185
Aleksei Efimov <aleksej.efimov@oracle.com>
2021-09-29 13:21:29 +0100

    8274227: Seal java.net.InetAddressImpl

--- src/java.base/share/classes/java/net/Inet4AddressImpl.java
+++ src/java.base/share/classes/java/net/Inet4AddressImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,7 +30,7 @@ import java.io.IOException;
  *
  * @since 1.4
  */
-class Inet4AddressImpl implements InetAddressImpl {
+final class Inet4AddressImpl implements InetAddressImpl {
     public native String getLocalHostName() throws UnknownHostException;
     public native InetAddress[]
         lookupAllHostAddr(String hostname) throws UnknownHostException;
--- src/java.base/share/classes/java/net/Inet6AddressImpl.java
+++ src/java.base/share/classes/java/net/Inet6AddressImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -44,7 +44,7 @@ import static java.net.InetAddress.PREFER_SYSTEM_VALUE;
  *
  * @since 1.4
  */
-class Inet6AddressImpl implements InetAddressImpl {
+final class Inet6AddressImpl implements InetAddressImpl {
 
     public native String getLocalHostName() throws UnknownHostException;
 
