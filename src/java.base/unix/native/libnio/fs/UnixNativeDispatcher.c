511823876f2ec51d2deb466a1b7adb252210e27b
Daniel Mor <daniel.mor@ibm.com>
2021-09-30 01:32:54 -0300

    JDK-8274562: Fix fast-fail detection of extended user attributes determining a filesystem does not support user attributes when it does
    JDK-8262957 introduced a fast-fail mechanism determining if a filesystem
    supports extended attributes based on if _SYS_XATTR_H_ is defined. The
    problem with this approach is that the way these variables are defined
    differ between MacOS and Linux.
    
    MacOS: _SYS_XATTR_H_
    Linux: _SYS_XATTR_H
    
    This will result in the following line returning false even though the
    filesystem supports UserDefinedFileAttributeView(s)
    
    Files.getFileStore(Paths.get( "/tmp/testfile" )).supportsFileAttributeView(UserDefinedFileAttributeView.class)
    
    This is fixed by alternatively using XATTR_CREATE which is defined in
    both MacOS and Linux.

--- src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
+++ src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
@@ -317,7 +317,7 @@ Java_sun_nio_fs_UnixNativeDispatcher_init(JNIEnv* env, jclass this)
 
     /* supports extended attributes */
 
-#ifdef _SYS_XATTR_H_
+#ifdef XATTR_CREATE
     capabilities |= sun_nio_fs_UnixNativeDispatcher_SUPPORTS_XATTR;
 #endif
 
