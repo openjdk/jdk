82cae46785c98f1dc8bc80f8217b4a245de7dd35
Joe Darcy <joe.darcy@oracle.com>
2021-10-05 16:53:51 -0700

    Fix whitespace.

--- test/langtools/tools/javac/warnings/Serial/ImproperReturnTypes.java
+++ test/langtools/tools/javac/warnings/Serial/ImproperReturnTypes.java
@@ -8,7 +8,7 @@ import java.io.*;
 
 class ImproperReturnTypes implements Serializable {
     private static final long serialVersionUID = 42;
-    
+
     /*
      * Serialization-related methods return either void or Object:
      *
