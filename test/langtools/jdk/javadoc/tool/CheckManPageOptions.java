81315dc25e49fb7515d2abb23d008fe7963650c9
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2021-10-05 20:06:57 -0700

    fix copyright and trailing whitespace

--- test/langtools/jdk/javadoc/tool/CheckManPageOptions.java
+++ test/langtools/jdk/javadoc/tool/CheckManPageOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -57,7 +57,7 @@ public class CheckManPageOptions {
     public static void main(String... args) throws Exception {
         new CheckManPageOptions().run(args);
     }
-    
+
     static final PrintStream out = System.err;
 
     // FIXME: JDK-8274295
