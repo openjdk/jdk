423c3069d788ecf32fa6f1104f57cb9ad2c3e626
Andrey Turbanov <turbanoff@gmail.com>
2021-10-06 21:42:45 +0300

    8274809: Update java.base classes to use try-with-resources update copyright year

--- src/java.base/share/classes/sun/net/www/MimeTable.java
+++ src/java.base/share/classes/sun/net/www/MimeTable.java
@@ -24,6 +24,7 @@
  */
 
 package sun.net.www;
+
 import jdk.internal.util.StaticProperty;
 
 import java.io.*;
--- src/java.base/unix/classes/sun/net/sdp/SdpProvider.java
+++ src/java.base/unix/classes/sun/net/sdp/SdpProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,7 +35,6 @@ import java.io.FileDescriptor;
 import java.io.IOException;
 import java.io.PrintStream;
 
-import sun.net.sdp.SdpSupport;
 import sun.security.action.GetPropertyAction;
 
 /**
