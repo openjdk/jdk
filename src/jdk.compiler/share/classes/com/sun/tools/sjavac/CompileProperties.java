f14eac32ff6da7481f31fce5d109b3fb54635303
Andrey Turbanov <turbanoff@gmail.com>
2021-10-07 10:59:09 +0300

    [PATCH] Update jdk.compiler classes to use try-with-resources update copyright year

--- src/jdk.compiler/share/classes/com/sun/tools/sjavac/CompileProperties.java
+++ src/jdk.compiler/share/classes/com/sun/tools/sjavac/CompileProperties.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.PrintStream;
 import java.io.Writer;
 import java.net.URI;
 import java.text.MessageFormat;
