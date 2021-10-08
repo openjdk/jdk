3cb9724ed94a7267853c5cddb18b4ae868ecf2e9
Coleen Phillimore <coleenp@openjdk.org>
2021-10-08 21:10:42 +0000

    8274934: Attempting to acquire lock JNICritical_lock/41 out of order with lock MultiArray_lock/41
    Reviewed-by: dcubed, iklam

--- src/hotspot/share/runtime/mutexLocker.cpp
+++ src/hotspot/share/runtime/mutexLocker.cpp
@@ -275,7 +275,6 @@ void mutex_init() {
   def(Terminator_lock              , PaddedMonitor, safepoint,      true);
   def(InitCompleted_lock           , PaddedMonitor, nosafepoint,    true);
   def(Notify_lock                  , PaddedMonitor, safepoint,      true);
-  def(JNICritical_lock             , PaddedMonitor, safepoint,      true); // used for JNI critical regions
   def(AdapterHandlerLibrary_lock   , PaddedMutex  , safepoint,      true);
 
   def(Heap_lock                    , PaddedMonitor, safepoint,      false); // Doesn't safepoint check during termination.
@@ -365,6 +364,7 @@ void mutex_init() {
   defl(OopMapCacheAlloc_lock       , PaddedMutex ,  Threads_lock,              true);
   defl(Module_lock                 , PaddedMutex ,  ClassLoaderDataGraph_lock, false);
   defl(SystemDictionary_lock       , PaddedMonitor, Module_lock,               true);
+  defl(JNICritical_lock            , PaddedMonitor, MultiArray_lock,           true); // used for JNI critical regions
 
 #if INCLUDE_JFR
   defl(JfrMsg_lock                 , PaddedMonitor, Module_lock,               true);
