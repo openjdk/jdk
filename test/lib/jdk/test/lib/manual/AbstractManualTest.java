49ad12ec1d62d44f9ed9dfb692b09532808553a0
Amresh Sahu <72060147+amresh-sahu@users.noreply.github.com>
2021-10-08 20:15:26 +0530

    White space removed.

--- test/lib/jdk/test/lib/manual/AbstractManualTest.java
+++ test/lib/jdk/test/lib/manual/AbstractManualTest.java
@@ -14,123 +14,123 @@ import javax.imageio.ImageIO;
 
 public abstract class AbstractManualTest {
 
-	protected static boolean theTestPassed;
+    protected static boolean theTestPassed;
 
-	protected static boolean testGeneratedInterrupt;
+    protected static boolean testGeneratedInterrupt;
 
-	protected static Thread mainThread;
+    protected static Thread mainThread;
 
-	protected static int sleepTime = 600000;
+    protected static int sleepTime = 600000;
 
-	protected Process process = null;
-	
-	protected String testName;
+    protected Process process = null;
 
-	public AbstractManualTest() {
-		process = execute();
-	}
+    protected String testName;
 
-	public void fail() {
-		theTestPassed = false;
-		testGeneratedInterrupt = true;
-	}
+    public AbstractManualTest() {
+        process = execute();
+    }
 
-	public void pass() {
-		theTestPassed = true;
-		testGeneratedInterrupt = true;
+    public void fail() {
+        theTestPassed = false;
+        testGeneratedInterrupt = true;
+    }
 
-	}
+    public void pass() {
+        theTestPassed = true;
+        testGeneratedInterrupt = true;
 
-	public void comment(String text) {
-		if (!theTestPassed) {
-			System.out.println("Failure details : " + text);
-		}
-	}
+    }
 
-	protected Process execute() {
-		String javahome = System.getProperty("java.home");
-		System.out.println("java.home =" + System.getProperty("java.home"));
+    public void comment(String text) {
+        if (!theTestPassed) {
+            System.out.println("Failure details : " + text);
+        }
+    }
 
-		javahome = javahome + File.separator;
-		System.out.println("javahome= " + javahome);
-		String jarFile = javahome + "demo" + File.separator + "jfc" + File.separator + "SwingSet2" + File.separator
-				+ "SwingSet2.jar";
-		java.io.File demoFile = new java.io.File(jarFile);
-		if (!demoFile.exists()) {
-			String errMessage = "ERROR: SwingDemo.jar is not found, \nlocation is " + jarFile
-					+ ". \nPlease install demo into " + javahome + " and rerun test.";
-			javax.swing.JOptionPane.showMessageDialog(null, errMessage, "ERROR", javax.swing.JOptionPane.ERROR_MESSAGE);
-			System.out.println(errMessage);
-			System.out.println("Will exit");
-			System.exit(2);
-		}
-		String command = javahome + "bin" + File.separator + "java -jar " + jarFile;
-		String lafProp = System.getProperty("swing.defaultlaf");
-		if (lafProp == null) {
-			command += " -Dswing.defaultlaf=" + lafProp;
-		}
-		System.out.println("command=" + command);
-		ProcessBuilder pBuilder = new ProcessBuilder();
-		Process p = null;
+    protected Process execute() {
+        String javahome = System.getProperty("java.home");
+        System.out.println("java.home =" + System.getProperty("java.home"));
 
-		try {
-			pBuilder.command(command.split(" "));
-			p = pBuilder.start();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+        javahome = javahome + File.separator;
+        System.out.println("javahome= " + javahome);
+        String jarFile = javahome + "demo" + File.separator + "jfc" + File.separator + "SwingSet2" + File.separator
+                + "SwingSet2.jar";
+        java.io.File demoFile = new java.io.File(jarFile);
+        if (!demoFile.exists()) {
+            String errMessage = "ERROR: SwingDemo.jar is not found, \nlocation is " + jarFile
+                    + ". \nPlease install demo into " + javahome + " and rerun test.";
+            javax.swing.JOptionPane.showMessageDialog(null, errMessage, "ERROR", javax.swing.JOptionPane.ERROR_MESSAGE);
+            System.out.println(errMessage);
+            System.out.println("Will exit");
+            System.exit(2);
+        }
+        String command = javahome + "bin" + File.separator + "java -jar " + jarFile;
+        String lafProp = System.getProperty("swing.defaultlaf");
+        if (lafProp == null) {
+            command += " -Dswing.defaultlaf=" + lafProp;
+        }
+        System.out.println("command=" + command);
+        ProcessBuilder pBuilder = new ProcessBuilder();
+        Process p = null;
 
-		return p;
-	}
+        try {
+            pBuilder.command(command.split(" "));
+            p = pBuilder.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-	protected void captureScreenShot() {
-		try {
-			Thread.sleep(120);
-			Robot robot = new Robot();
-			Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-			BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
-			String filePath = getClass().getResource("").getPath() + File.separator + getTestName() + "_failed"
-					+ ".jpg";
-			File file = new File(filePath);
-			boolean status = ImageIO.write(bufferedImage, "jpg", file);
-			System.out.println("Screen Captured ? " + status + " File Path :- " + file.getAbsolutePath());
-		} catch (AWTException | IOException | InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        return p;
+    }
 
-	public void saveAndClose() {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		int exitCode;
-		String line;
-		try {
-			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-			}
+    protected void captureScreenShot() {
+        try {
+            Thread.sleep(120);
+            Robot robot = new Robot();
+            Rectangle rectangle = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage bufferedImage = robot.createScreenCapture(rectangle);
+            String filePath = getClass().getResource("").getPath() + File.separator + getTestName() + "_failed"
+                    + ".jpg";
+            File file = new File(filePath);
+            boolean status = ImageIO.write(bufferedImage, "jpg", file);
+            System.out.println("Screen Captured ? " + status + " File Path :- " + file.getAbsolutePath());
+        } catch (AWTException | IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-			exitCode = process.waitFor();
-		} catch (InterruptedException | IOException e) {
-			throw new RuntimeException(e);
-		}
+    public void saveAndClose() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        int exitCode;
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
 
-		System.out.println("\nExited with the code : " + exitCode);
-		mainThread.interrupt();
-	}
+            exitCode = process.waitFor();
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
 
-	public void closeSwingSetDemo() {
-		try {
-			if (System.getProperty("os.name").startsWith("Windows")) {
-				Runtime.getRuntime()
-						.exec("wmic Path win32_process Where \"CommandLine Like '%SwingSet2.jar%'\" Call Terminate");
-			} else {
-				Runtime.getRuntime()
-						.exec("ps -ef | grep -v grep | grep  SwingSet2.jar | cut -d' ' -f4 | xargs kill -9");
-			}
+        System.out.println("\nExited with the code : " + exitCode);
+        mainThread.interrupt();
+    }
 
-		} catch (IOException e1) {
-			System.err.print("Error while terminating the SwingSet2.jar " + e1.getMessage());
-		}
-	}
-	
-	abstract protected String getTestName();
+    public void closeSwingSetDemo() {
+        try {
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                Runtime.getRuntime()
+                    .exec("wmic Path win32_process Where \"CommandLine Like '%SwingSet2.jar%'\" Call Terminate");
+            else {
+            Runtime.getRuntime()
+                        .exec("ps -ef | grep -v grep | grep  SwingSet2.jar | cut -d' ' -f4 | xargs kill -9");
+            }
+
+        } catch (IOException e1) {
+            System.err.print("Error while terminating the SwingSet2.jar " + e1.getMessage());
+        }
+    }
+
+    abstract protected String getTestName();
 }
