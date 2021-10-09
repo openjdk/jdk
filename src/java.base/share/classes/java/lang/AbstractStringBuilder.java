d679bd3ab8b41a14359d3bfb9763a1178d02ecb0
Andrey Turbanov <turbanoff@gmail.com>
2021-10-09 20:46:23 +0300

    [PATCH] Remove unused AbstractStringBuilder.MAX_ARRAY_SIZE

--- src/java.base/share/classes/java/lang/AbstractStringBuilder.java
+++ src/java.base/share/classes/java/lang/AbstractStringBuilder.java
@@ -232,14 +232,6 @@ abstract class AbstractStringBuilder implements Appendable, CharSequence {
     }
 
     /**
-     * The maximum size of array to allocate (unless necessary).
-     * Some VMs reserve some header words in an array.
-     * Attempts to allocate larger arrays may result in
-     * OutOfMemoryError: Requested array size exceeds VM limit
-     */
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-
-    /**
      * Returns a capacity at least as large as the given minimum capacity.
      * Returns the current capacity increased by the current length + 2 if
      * that suffices.
