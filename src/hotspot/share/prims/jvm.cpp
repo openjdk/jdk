0d80f6cf82ddab85f2461c7cc663a93bb222e988
Patricio Chilano Mateo <pchilanomate@openjdk.org>
2021-10-11 13:34:42 +0000

    8274379: Allow process of unsafe access errors in check_special_condition_for_native_trans
    Reviewed-by: rehn, dholmes

--- src/hotspot/share/prims/jvm.cpp
+++ src/hotspot/share/prims/jvm.cpp
@@ -2961,7 +2961,7 @@ JVM_ENTRY(void, JVM_StopThread(JNIEnv* env, jobject jthread, jobject throwable))
       THROW_OOP(java_throwable);
     } else {
       // Use a VM_Operation to throw the exception.
-      JavaThread::send_async_exception(java_thread, java_throwable);
+      JavaThread::send_async_exception(receiver, java_throwable);
     }
   } else {
     // Either:
