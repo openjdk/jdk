3c69c904c71313b6fb0910c1af732288107ae7b1
Amresh Sahu <amresh.sahu@oracle.com>
2021-10-11 13:33:26 +0530

    JDK-8275027:Create JDK test library classes for manual tests UI.

--- test/lib/jdk/test/lib/manual/StatusBar.java
+++ test/lib/jdk/test/lib/manual/StatusBar.java
@@ -7,24 +7,24 @@ import javax.swing.JPanel;
 
 public class StatusBar extends JPanel {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static StatusBar sBar = new StatusBar();
+    private static StatusBar sBar = new StatusBar();
 
-	private JLabel lblStatus;
+    private JLabel lblStatus;
 
-	private StatusBar() {
-		setLayout(new GridLayout());
-		lblStatus = new JLabel();
-		lblStatus.setHorizontalAlignment(JLabel.LEFT);
-		add(lblStatus);
-	}
+    private StatusBar() {
+        setLayout(new GridLayout());
+        lblStatus = new JLabel();
+        lblStatus.setHorizontalAlignment(JLabel.LEFT);
+        add(lblStatus);
+    }
 
-	public static StatusBar getStatusBar() {
-		return sBar;
-	}
+    public static StatusBar getStatusBar() {
+        return sBar;
+    }
 
-	public void setStatus(String msg) {
-		lblStatus.setText(msg);
-	}
+    public void setStatus(String msg) {
+        lblStatus.setText(msg);
+    }
 }
--- test/lib/jdk/test/lib/manual/TestInstructionUIHelper.java
+++ test/lib/jdk/test/lib/manual/TestInstructionUIHelper.java
@@ -14,26 +14,26 @@ import java.net.URL;
  */
 
 public class TestInstructionUIHelper {
-	private static TestInstructionUI frame;
+    private static TestInstructionUI frame;
 
-	public static void createDialogWithInstructions(String[] instructions, AbstractManualTest testclass) {
-		frame = new TestInstructionUI("Instructions", testclass);
-		println("Any messages for the tester will display here.");
-	}
+    public static void createDialogWithInstructions(String[] instructions, AbstractManualTest testclass) {
+        frame = new TestInstructionUI("Instructions", testclass);
+        println("Any messages for the tester will display here.");
+    }
 
-	public static void createDialogWithInstructions(URL url, AbstractManualTest testclass) throws IOException {
-		frame = new TestInstructionUI(testclass.getClass().getSimpleName() + " Instructions", testclass);
-		frame.printInstructions(url);
-	}
+    public static void createDialogWithInstructions(URL url, AbstractManualTest testclass) throws IOException {
+        frame = new TestInstructionUI(testclass.getClass().getSimpleName() + " Instructions", testclass);
+        frame.printInstructions(url);
+    }
 
-	public static void println(String messageIn) {
-		frame.displayMessage(messageIn);
-	}
+    public static void println(String messageIn) {
+        frame.displayMessage(messageIn);
+    }
 
-	public static void dispose(AbstractManualTest testClass) {
-		TestInstructionUIHelper.println("Shutting down the Java process..");
-		testClass.closeSwingSetDemo();
-		frame.dispose();
-		frame.dispose();
-	}
+    public static void dispose(AbstractManualTest testClass) {
+        TestInstructionUIHelper.println("Shutting down the Java process..");
+        testClass.closeSwingSetDemo();
+        frame.dispose();
+        frame.dispose();
+    }
 }
