2df965e55fba2b9bfef77705a04040796fffe7c6
sunguoyun <sunguoyun@loongson.cn>
2021-10-19 15:37:19 +0800

    8273317: crash in cmovP_cmpP_zero_zeroNode::bottom_type()

--- src/hotspot/share/adlc/output_h.cpp
+++ src/hotspot/share/adlc/output_h.cpp
@@ -1945,7 +1945,7 @@ void ArchDesc::declareClasses(FILE *fp) {
       int offset = 1;
       // Special special hack to see if the Cmp? has been incorporated in the conditional move
       MatchNode *rl = instr->_matrule->_rChild->_lChild;
-      if( rl && !strcmp(rl->_opType, "Binary") && rl->_rChild && strncmp(rl->_rChild->_opType, "Cmp", 3) == 0) {
+      if (rl && !strcmp(rl->_opType, "Binary") && rl->_rChild && strncmp(rl->_rChild->_opType, "Cmp", 3) == 0) {
         offset = 2;
         fprintf(fp,"  const Type            *bottom_type() const { if (req() == 3) return in(2)->bottom_type();\n\tconst Type *t = in(oper_input_base()+%d)->bottom_type(); return (req() <= oper_input_base()+%d) ? t : t->meet(in(oper_input_base()+%d)->bottom_type()); } // %s\n",
         offset, offset+1, offset+1, instr->_matrule->_rChild->_opType);
