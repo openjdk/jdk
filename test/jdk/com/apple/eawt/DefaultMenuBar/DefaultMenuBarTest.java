5218b7b7fc07da6af1b04c460076b48dbcc145a0
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2021-10-19 16:05:33 +0200

    initial

--- test/jdk/com/apple/eawt/DefaultMenuBar/DefaultMenuBarTest.java
+++ test/jdk/com/apple/eawt/DefaultMenuBar/DefaultMenuBarTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -58,6 +58,7 @@ public class DefaultMenuBarTest {
 
         Robot robot = new Robot();
         robot.setAutoDelay(100);
+        robot.waitForIdle();
 
         robot.keyPress(KeyEvent.VK_META);
         robot.keyPress(ks.getKeyCode());
