313d242b7bab033cbbffd6195f39dd531725b5fb
Patricio Chilano Mateo <patricio.chilano.mateo@oracle.com>
2021-10-26 12:26:32 -0400

    v1

--- src/hotspot/share/runtime/interfaceSupport.inline.hpp
+++ src/hotspot/share/runtime/interfaceSupport.inline.hpp
@@ -229,14 +229,12 @@ class ThreadBlockInVMPreprocess : public ThreadStateTransition {
   ~ThreadBlockInVMPreprocess() {
     assert(_thread->thread_state() == _thread_blocked, "coming from wrong thread state");
     // Change to transition state and ensure it is seen by the VM thread.
-    _thread->set_thread_state_fence(_thread_blocked_trans);
+    _thread->set_thread_state_fence(_thread_in_vm);
 
     if (SafepointMechanism::should_process(_thread, _allow_suspend)) {
       _pr(_thread);
       SafepointMechanism::process_if_requested(_thread, _allow_suspend);
     }
-
-    _thread->set_thread_state(_thread_in_vm);
   }
 
   static void emptyOp(JavaThread* current) {}
