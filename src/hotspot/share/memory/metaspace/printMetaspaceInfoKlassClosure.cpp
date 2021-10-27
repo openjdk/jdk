7bbf1517ff7aa3a06f0ffe1fa372e14c600f177b
Yi Yang <qingfeng.yy@alibaba-inc.com>
2021-10-27 09:22:17 +0800

    typo

--- src/hotspot/share/memory/metaspace/printMetaspaceInfoKlassClosure.cpp
+++ src/hotspot/share/memory/metaspace/printMetaspaceInfoKlassClosure.cpp
@@ -43,7 +43,7 @@ void PrintMetaspaceInfoKlassClosure::do_klass(Klass* k) {
 
   // Print a 's' for shared classes
   _out->put(k->is_shared() ? 's' : ' ');
-  // Print a 'f' for classes that having a non-trivial finalize() method
+  // Print a 'f' for classes that have an non-trivial finalize() method
   _out->put(k->has_finalizer() ? 'f' : ' ');
 
   ResourceMark rm;
