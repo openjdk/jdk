8b5ebfd849756d8ba9ff15f004559ac1032ec2c3
tstuefe <thomas.stuefe@gmail.com>
2021-10-29 10:26:56 +0200

    adapt copyrights

--- test/hotspot/jtreg/runtime/Metaspace/elastic/MetaspaceTestWithThreads.java
+++ test/hotspot/jtreg/runtime/Metaspace/elastic/MetaspaceTestWithThreads.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2020 SAP SE. All rights reserved.
+ * Copyright (c) 2021 SAP SE. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,8 +22,6 @@
  * questions.
  *
  */
-
-import java.util.Set;
 
 public class MetaspaceTestWithThreads {
 
