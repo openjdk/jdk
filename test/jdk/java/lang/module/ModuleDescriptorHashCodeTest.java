9dd2774c71b47b7222a734f17ad61d332559cc4a
jpai <jaikiran.pai@gmail.com>
2021-11-03 08:40:25 +0530

    simplify the test based on review inputs

--- test/jdk/java/lang/module/ModuleDescriptorHashCodeTest.java
+++ test/jdk/java/lang/module/ModuleDescriptorHashCodeTest.java
@@ -21,108 +21,45 @@
  * questions.
  */
 
-import jdk.test.lib.process.OutputAnalyzer;
-import jdk.test.lib.process.ProcessTools;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.module.ModuleDescriptor;
-import java.util.Optional;
 import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotSame;
 
 /**
  * @test
  * @bug 8275509
- * @library /test/lib
- * @run driver ModuleDescriptorHashCodeTest
- * @summary Tests that the ModuleDescriptor.hashCode() method returns the same hash code
- * across multiple JVM runs, for the same module descriptor.
+ * @run testng ModuleDescriptorHashCodeTest
+ * @run testng/othervm -Xshare:off ModuleDescriptorHashCodeTest
+ * @summary Tests the ModuleDescriptor.hashCode() for boot layer modules
  */
 public class ModuleDescriptorHashCodeTest {
 
     /**
-     * Launches multiple instances of a Java program which verifies the hashCode() of a
-     * ModuleDescriptor, which is loaded from the boot layer as well as constructed using
-     * the ModuleDescriptor.Builder. It is expected that every single run of this program
-     * will generate the exact same hash code for the module descriptor of the same module.
+     * Verifies that the ModuleDescriptor.hashCode() returned by a boot layer module is
+     * the same as that returned by a ModuleDescriptor constructed from the ModuleDescriptor.Builder
+     * for the same module.
      */
-    public static void main(String[] args) throws Exception {
+    @Test
+    public void testBootModuleDescriptor() throws Exception {
         Set<Module> bootModules = ModuleLayer.boot().modules();
         for (Module bootModule : bootModules) {
-            ModuleDescriptor md = bootModule.getDescriptor();
-            int expectedHashCode = md.hashCode();
-            System.out.println("Expected ModuleDescriptor.hashCode() of boot module "
-                    + bootModule.getName() + " is " + expectedHashCode);
-            int numProcesses = 2;
-            for (int i = 0; i < numProcesses; i++) {
-                // run some with CDS enabled and some with CDS disabled
-                boolean disableCDS = (i % 2 == 0);
-                String[] processArgs;
-                if (disableCDS) {
-                    processArgs = new String[]{"-Xshare:off",
-                            HashCodeChecker.class.getName(),
-                            bootModule.getName(),
-                            String.valueOf(expectedHashCode)};
-                } else {
-                    processArgs = new String[]{HashCodeChecker.class.getName(),
-                            bootModule.getName(),
-                            String.valueOf(expectedHashCode)};
-                }
-                ProcessBuilder processBuilder = ProcessTools.createJavaProcessBuilder(processArgs);
-                long start = System.currentTimeMillis();
-                OutputAnalyzer outputAnalyzer = ProcessTools.executeProcess(processBuilder);
-                System.out.println("Process " + outputAnalyzer.pid() + " completed in "
-                        + (System.currentTimeMillis() - start) + " milli seconds");
-                outputAnalyzer.shouldHaveExitValue(0);
-            }
-        }
-    }
-
-    /**
-     * Loads the passed module from the boot layer and compares the hashCode of that module's
-     * descriptor with the expected hash code (which is passed as an argument).
-     * Then uses the {@link ModuleDescriptor.Builder} to construct a module descriptor for the
-     * same module and verifies that it too has the same hash code.
-     */
-    private static void assertExpectedHashCode(String moduleName, int expectedHashCode) throws Exception {
-        Optional<Module> bootModule = ModuleLayer.boot().findModule(moduleName);
-        if (bootModule.isEmpty()) {
-            throw new RuntimeException("Boot module " + moduleName + " is missing");
-        }
-        ModuleDescriptor bootMD = bootModule.get().getDescriptor();
-        int actualHashCode = bootMD.hashCode();
-        if (actualHashCode != expectedHashCode) {
-            throw new RuntimeException("Expected hashCode " + expectedHashCode + " but got " + actualHashCode
-                    + " from boot module descriptor " + bootMD);
-        }
-        System.out.println("Got expected hashCode of " + expectedHashCode + " for boot module descriptor " + bootMD);
-        ModuleDescriptor mdFromBuilder = fromModuleInfoClass(bootModule.get());
-        // verify that this object is indeed a different object instance than the boot module descriptor
-        // to prevent any artificial passing of the test
-        if (bootMD == mdFromBuilder) {
-            throw new RuntimeException("ModuleDescriptor loaded from boot layer and " +
-                    "one created from module-info.class unexpectedly returned the same instance: " + bootMD);
-        }
-        int hashCode = mdFromBuilder.hashCode();
-        if (expectedHashCode != hashCode) {
-            throw new RuntimeException("Expected hashCode " + expectedHashCode + " but got " + hashCode
-                    + " from module descriptor " + mdFromBuilder);
-        }
-        // invoke a few times to make sure the hashCode doesn't change within the same JVM run
-        for (int i = 0; i < 5; i++) {
-            int h = mdFromBuilder.hashCode();
-            if (expectedHashCode != h) {
-                throw new RuntimeException("Expected hashCode " + expectedHashCode + " but got " + h
-                        + " from module descriptor " + mdFromBuilder);
-            }
-        }
-    }
-
-    private static class HashCodeChecker {
-        public static void main(String[] args) throws Exception {
-            // args[0] is module name
-            // args[1] is the expected hash code of that module descriptor
-            ModuleDescriptorHashCodeTest.assertExpectedHashCode(args[0], Integer.parseInt(args[1]));
+            System.out.println("Testing module descriptor of boot module " + bootModule);
+            ModuleDescriptor bootMD = bootModule.getDescriptor();
+            ModuleDescriptor mdFromBuilder = fromModuleInfoClass(bootModule);
+            // verify that this object is indeed a different object instance than the boot module descriptor
+            // to prevent any artificial passing of the test
+            assertNotSame(mdFromBuilder, bootMD, "ModuleDescriptor loaded from boot layer and " +
+                    "one created from module-info.class unexpectedly returned the same instance");
+            assertEquals(mdFromBuilder.hashCode(), bootMD.hashCode(),
+                    "Unexpected ModuleDescriptor.hashCode() for " + mdFromBuilder);
+            assertEquals(mdFromBuilder.compareTo(bootMD), 0,
+                    "Unexpected ModuleDescriptor.compareTo() for " + mdFromBuilder);
         }
     }
 
@@ -132,6 +69,7 @@ public class ModuleDescriptorHashCodeTest {
             if (moduleInfo == null) {
                 throw new RuntimeException("Could not locate module-info.class in " + module);
             }
+            // internally calls ModuleDescriptor.Builder
             return ModuleDescriptor.read(moduleInfo);
         }
     }
