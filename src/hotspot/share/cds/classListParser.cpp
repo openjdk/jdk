66f7c4dc1c981b6a7845775787e2ca62d11380f9
Yumin Qi <Yumin.Qi@Oralce.Com>
2021-11-03 13:34:37 -0700

    Revert change for not-custom loader related, fixed file relative path

--- src/hotspot/share/cds/classListParser.cpp
+++ src/hotspot/share/cds/classListParser.cpp
@@ -448,7 +448,7 @@ void ClassListParser::error(const char* msg, ...) {
 InstanceKlass* ClassListParser::load_class_from_source(Symbol* class_name, TRAPS) {
 #if !(defined(_LP64) && (defined(LINUX) || defined(__APPLE__) || defined(_WINDOWS)))
   // The only supported platforms are: (1) Linux/64-bit and (2) Solaris/64-bit and
-  // (3) MacOSX/64-bit
+  // (3) MacOSX/64-bit and (4) Windowss/64-bit
   // This #if condition should be in sync with the areCustomLoadersSupportedForCDS
   // method in test/lib/jdk/test/lib/Platform.java.
   error("AppCDS custom class loaders not supported on this platform");
