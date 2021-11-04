2a87b02fac22b19f71db1caef87ef97ee488ca54
Coleen Phillimore <coleen.phillimore@oracle.com>
2021-11-04 16:48:57 +0000

    remove JNIHandleBlockFreeList_lock

--- src/hotspot/share/runtime/mutexLocker.hpp
+++ src/hotspot/share/runtime/mutexLocker.hpp
@@ -40,7 +40,6 @@ extern Mutex*   Module_lock;                     // a lock on module and package
 extern Mutex*   CompiledIC_lock;                 // a lock used to guard compiled IC patching and access
 extern Mutex*   InlineCacheBuffer_lock;          // a lock used to guard the InlineCacheBuffer
 extern Mutex*   VMStatistic_lock;                // a lock used to guard statistics count increment
-extern Mutex*   JNIHandleBlockFreeList_lock;     // a lock on the JNI handle block free list
 extern Mutex*   JmethodIdCreation_lock;          // a lock on creating JNI method identifiers
 extern Mutex*   JfieldIdCreation_lock;           // a lock on creating JNI static field identifiers
 extern Monitor* JNICritical_lock;                // a lock used while entering and exiting JNI critical regions, allows GC to sometimes get in
