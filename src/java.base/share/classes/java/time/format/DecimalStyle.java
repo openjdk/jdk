5bdd4917cb621cd254a65f8d8ac77a923457c851
Naoto Sato <naoto.sato@oracle.com>
2021-11-10 10:58:53 -0800

    Using @code tag for `Set`.

--- src/java.base/share/classes/java/time/format/DecimalStyle.java
+++ src/java.base/share/classes/java/time/format/DecimalStyle.java
@@ -115,7 +115,7 @@ public final class DecimalStyle {
     /**
      * Lists all the locales that are supported.
      * <p>
-     * At a minimum, the returned Set must contain a {@code Locale} instance equal to
+     * At a minimum, the returned {@code Set} must contain a {@code Locale} instance equal to
      * {@link Locale#ROOT Locale.ROOT} and a {@code Locale} instance equal to
      * {@link Locale#US Locale.US}.
      *
