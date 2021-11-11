89d00e14744d593ecc695d573686562eccd53562
casparcwang <casparcwang@tencent.com>
2021-11-11 10:41:33 +0800

    Add some comment

--- src/hotspot/share/opto/ifnode.cpp
+++ src/hotspot/share/opto/ifnode.cpp
@@ -1722,6 +1722,8 @@ Node* IfProjNode::Identity(PhaseGVN* phase) {
        in(0)->outcnt() == 1))) {
     // IfNode control
     if (in(0)->is_BaseCountedLoopEnd()) {
+      // CountedLoopEndNode may be eliminated by if subsuming, replace CountedLoopNode with LoopNode to
+      // avoid mismatching between CountedLoopNode and CountedLoopEndNode in the following optimization.
       Node* head = unique_ctrl_out();
       if (head != NULL && head->is_BaseCountedLoop() && head->in(LoopNode::LoopBackControl) == this) {
         Node* new_head = new LoopNode(head->in(LoopNode::EntryControl), this);
