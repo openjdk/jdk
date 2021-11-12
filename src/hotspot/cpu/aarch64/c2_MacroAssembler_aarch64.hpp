9cfd1700abae4d69d656875510da725f94bf6021
Ningsheng Jian <ningsheng.jian@arm.com>
2021-11-12 09:12:28 +0800

    8281712: [REDO] AArch64: Implement string_compare intrinsic in SVE
    This is the REDO of JDK-8269559 and JDK-8275448. Those two backouts
    finally turned to be some zlib issue in AArch64 macOS, and is not
    related to the patch itself. See [1][2] for details.
    
    This patch is generally the same as JDK-8275448, which uses SVE to
    optimize string_compare intrinsics for long string comparisons. I did a
    rebase with small tweaks to get better performance for recent Neoverse
    hardware. Test data on systems with different SVE vector sizes:
    
    ```
       case    delta size  128-bits  256-bits  512-bits
    compareToLL  2   24     0.17%     0.58%     0.00%
    compareToLL  2   36     0.00%    2.25%      0.04%
    compareToLL  2   72     -4.40%   3.87%      -12.82%
    compareToLL  2   128    4.55%    58.31%     13.53%
    compareToLL  2   256    19.39%   69.77%     82.03%
    compareToLL  2   512    1.81%    68.38%     170.93%
    compareToLU  2   24     25.57%   46.98%     54.61%
    compareToLU  2   36     36.03%   70.26%     94.33%
    compareToLU  2   72     35.86%   90.58%     146.04%
    compareToLU  2   128    70.82%   119.19%    266.22%
    compareToLU  2   256    80.77%   146.33%    420.01%
    compareToLU  2   512    94.62%   171.72%    530.87%
    compareToUL  2   24     20.82%   34.48%     62.14%
    compareToUL  2   36     39.77%   60.79%     69.77%
    compareToUL  2   72     35.46%   84.34%     121.90%
    compareToUL  2   128    67.77%   110.97%    220.53%
    compareToUL  2   256    77.05%   160.29%    331.30%
    compareToUL  2   512    91.88%   184.57%    524.21%
    compareToUU  2   24     -0.13%   0.40%      0.00%
    compareToUU  2   36     -9.18%   12.84%     -13.93%
    compareToUU  2   72     1.67%    60.61%     6.69%
    compareToUU  2   128    13.51%   60.33%     55.27%
    compareToUU  2   256    2.55%    62.17%     153.26%
    compareToUU  2   512    4.12%    68.62%     201.68%
    ```
    JTreg tests passed on SVE hardware.
    
    [1] https://bugs.openjdk.java.net/browse/JDK-8275448
    [2] https://bugs.openjdk.java.net/browse/JDK-8282954

--- src/hotspot/cpu/aarch64/c2_MacroAssembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/c2_MacroAssembler_aarch64.hpp
@@ -32,7 +32,8 @@
   void string_compare(Register str1, Register str2,
                       Register cnt1, Register cnt2, Register result,
                       Register tmp1, Register tmp2, FloatRegister vtmp1,
-                      FloatRegister vtmp2, FloatRegister vtmp3, int ae);
+                      FloatRegister vtmp2, FloatRegister vtmp3,
+                      PRegister pgtmp1, PRegister pgtmp2, int ae);
 
   void string_indexof(Register str1, Register str2,
                       Register cnt1, Register cnt2,
--- src/hotspot/cpu/aarch64/stubGenerator_aarch64.cpp
+++ src/hotspot/cpu/aarch64/stubGenerator_aarch64.cpp
@@ -5324,7 +5324,120 @@ class StubGenerator: public StubCodeGenerator {
     return entry;
   }
 
+  enum string_compare_mode {
+    LL,
+    LU,
+    UL,
+    UU,
+  };
+
+  // The following registers are declared in aarch64.ad
+  // r0  = result
+  // r1  = str1
+  // r2  = cnt1
+  // r3  = str2
+  // r4  = cnt2
+  // r10 = tmp1
+  // r11 = tmp2
+  // z0  = ztmp1
+  // z1  = ztmp2
+  // p0  = pgtmp1
+  // p1  = pgtmp2
+  address generate_compare_long_string_sve(string_compare_mode mode) {
+    __ align(CodeEntryAlignment);
+    address entry = __ pc();
+    Register result = r0, str1 = r1, cnt1 = r2, str2 = r3, cnt2 = r4,
+             tmp1 = r10, tmp2 = r11;
+
+    Label LOOP, DONE, MISMATCH;
+    Register vec_len = tmp1;
+    Register idx = tmp2;
+    // The minimum of the string lengths has been stored in cnt2.
+    Register cnt = cnt2;
+    FloatRegister ztmp1 = z0, ztmp2 = z1;
+    PRegister pgtmp1 = p0, pgtmp2 = p1;
+
+#define LOAD_PAIR(ztmp1, ztmp2, pgtmp1, src1, src2, idx)                       \
+    switch (mode) {                                                            \
+      case LL:                                                                 \
+        __ sve_ld1b(ztmp1, __ B, pgtmp1, Address(str1, idx));                  \
+        __ sve_ld1b(ztmp2, __ B, pgtmp1, Address(str2, idx));                  \
+        break;                                                                 \
+      case LU:                                                                 \
+        __ sve_ld1b(ztmp1, __ H, pgtmp1, Address(str1, idx));                  \
+        __ sve_ld1h(ztmp2, __ H, pgtmp1, Address(str2, idx, Address::lsl(1))); \
+        break;                                                                 \
+      case UL:                                                                 \
+        __ sve_ld1h(ztmp1, __ H, pgtmp1, Address(str1, idx, Address::lsl(1))); \
+        __ sve_ld1b(ztmp2, __ H, pgtmp1, Address(str2, idx));                  \
+        break;                                                                 \
+      case UU:                                                                 \
+        __ sve_ld1h(ztmp1, __ H, pgtmp1, Address(str1, idx, Address::lsl(1))); \
+        __ sve_ld1h(ztmp2, __ H, pgtmp1, Address(str2, idx, Address::lsl(1))); \
+        break;                                                                 \
+      default:                                                                 \
+        ShouldNotReachHere();                                                  \
+    }
+
+    const char* stubname;
+    switch (mode) {
+      case LL: stubname = "compare_long_string_same_encoding LL";      break;
+      case LU: stubname = "compare_long_string_different_encoding LU"; break;
+      case UL: stubname = "compare_long_string_different_encoding UL"; break;
+      case UU: stubname = "compare_long_string_same_encoding UU";      break;
+      default: ShouldNotReachHere();
+    }
+
+    StubCodeMark mark(this, "StubRoutines", stubname);
+
+    __ mov(idx, 0);
+    __ sve_whilelt(pgtmp1, mode == LL ? __ B : __ H, idx, cnt);
+
+    if (mode == LL) {
+      __ sve_cntb(vec_len);
+    } else {
+      __ sve_cnth(vec_len);
+    }
+
+    __ sub(rscratch1, cnt, vec_len);
+
+    __ bind(LOOP);
+
+      // main loop
+      LOAD_PAIR(ztmp1, ztmp2, pgtmp1, src1, src2, idx);
+      __ add(idx, idx, vec_len);
+      // Compare strings.
+      __ sve_cmp(Assembler::NE, pgtmp2, mode == LL ? __ B : __ H, pgtmp1, ztmp1, ztmp2);
+      __ br(__ NE, MISMATCH);
+      __ cmp(idx, rscratch1);
+      __ br(__ LT, LOOP);
+
+    // post loop, last iteration
+    __ sve_whilelt(pgtmp1, mode == LL ? __ B : __ H, idx, cnt);
+
+    LOAD_PAIR(ztmp1, ztmp2, pgtmp1, src1, src2, idx);
+    __ sve_cmp(Assembler::NE, pgtmp2, mode == LL ? __ B : __ H, pgtmp1, ztmp1, ztmp2);
+    __ br(__ EQ, DONE);
+
+    __ bind(MISMATCH);
+
+    // Crop the vector to find its location.
+    __ sve_brkb(pgtmp2, pgtmp1, pgtmp2, false /* isMerge */);
+    // Extract the first different characters of each string.
+    __ sve_lasta(rscratch1, mode == LL ? __ B : __ H, pgtmp2, ztmp1);
+    __ sve_lasta(rscratch2, mode == LL ? __ B : __ H, pgtmp2, ztmp2);
+
+    // Compute the difference of the first different characters.
+    __ sub(result, rscratch1, rscratch2);
+
+    __ bind(DONE);
+    __ ret(lr);
+#undef LOAD_PAIR
+    return entry;
+  }
+
   void generate_compare_long_strings() {
+    if (UseSVE == 0) {
       StubRoutines::aarch64::_compare_long_string_LL
           = generate_compare_long_string_same_encoding(true);
       StubRoutines::aarch64::_compare_long_string_UU
@@ -5333,6 +5446,16 @@ class StubGenerator: public StubCodeGenerator {
           = generate_compare_long_string_different_encoding(true);
       StubRoutines::aarch64::_compare_long_string_UL
           = generate_compare_long_string_different_encoding(false);
+    } else {
+      StubRoutines::aarch64::_compare_long_string_LL
+          = generate_compare_long_string_sve(LL);
+      StubRoutines::aarch64::_compare_long_string_UU
+          = generate_compare_long_string_sve(UU);
+      StubRoutines::aarch64::_compare_long_string_LU
+          = generate_compare_long_string_sve(LU);
+      StubRoutines::aarch64::_compare_long_string_UL
+          = generate_compare_long_string_sve(UL);
+    }
   }
 
   // R0 = result
