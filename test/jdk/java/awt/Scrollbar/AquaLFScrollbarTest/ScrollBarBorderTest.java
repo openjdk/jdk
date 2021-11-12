883a0ffe4ddbba945470296fc576fc1c10698aa7
Alisen Chung <alisen.chung@oracle.com>
2021-11-12 11:00:56 -0800

    added copyright

--- test/jdk/java/awt/Scrollbar/AquaLFScrollbarTest/ScrollBarBorderTest.java
+++ test/jdk/java/awt/Scrollbar/AquaLFScrollbarTest/ScrollBarBorderTest.java
@@ -1,3 +1,28 @@
+/*
+ * Copyright (c) 2011, 2021, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 import javax.swing.JScrollBar;
 import javax.swing.JPanel;
 import javax.swing.JFrame;
@@ -52,7 +77,7 @@ public class ScrollBarBorderTest implements ActionListener {
     private static boolean testPassed = false;
     private static boolean isInterrupted = false;
     private static final int testTimeOut = 300000;
-    private static String testFailMessage = "testfail";
+    private static String testFailMessage = "Test Failed. Thumb was able to move into border.";
 
 
     public void createAndShowGUI() {
