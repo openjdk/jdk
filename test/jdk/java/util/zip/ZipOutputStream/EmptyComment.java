33617622cd13b1406be86d17c7ac1335878075f4
mrserb <14138494+mrserb@users.noreply.github.com>
2021-11-15 16:12:55 -0800

    Update EmptyComment.java

--- test/jdk/java/util/zip/ZipOutputStream/EmptyComment.java
+++ test/jdk/java/util/zip/ZipOutputStream/EmptyComment.java
@@ -27,51 +27,58 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertThrows;
+
 /**
  * @test
  * @bug 8277087
  * @summary Verifies various use cases when the zip comment should be empty
+ * @run testng EmptyComment
  */
 public final class EmptyComment {
 
-    public static void main(String[] args) throws Exception {
-        // Simple cases where the comment is set to empty text
-        test(zos -> {
-            // do nothing
-        });
-        test(zos -> {
-            zos.setComment(null);
-        });
-        test(zos -> {
-            zos.setComment("");
-        });
-        test(zos -> {
-            zos.setComment("");
-            zos.setComment(null);
-        });
-        test(zos -> {
-            zos.setComment(null);
-            zos.setComment("");
-        });
-        test(zos -> {
-            zos.setComment("Comment");
-            zos.setComment(null);
-        });
-        test(zos -> {
-            zos.setComment("Comment");
-            zos.setComment("");
-        });
+    @DataProvider()
+    Object[][] longLengths() {
+        return new Object[][]{{0xFFFF + 1}, {0xFFFF + 2}, {0xFFFF * 2}};
+    }
 
-        // Overflow, the text is too long to be stored as a comment
-        for (int count : new int[]{0xFFFF + 1, 0xFFFF + 2, 0xFFFF * 2}) {
-            test(zos -> {
-                try {
-                    zos.setComment("X".repeat(count));
-                } catch (IllegalArgumentException ignored) {
-                    // skip
-                }
-            });
-        }
+    /**
+     * Overflow, the text is too long to be stored as a comment.
+     */
+    @Test(dataProvider = "longLengths")
+    void testOverflow(int length) throws Exception {
+        test(zos -> assertThrows(IllegalArgumentException.class, () -> {
+            zos.setComment("X".repeat(length));
+        }));
+    }
+
+    /**
+     * Simple cases where the comment is set to the empty text.
+     */
+    @Test
+    void testSimpleCases() throws Exception {
+        test(zos -> {/* do nothing */});
+        test(zos -> zos.setComment(null));
+        test(zos -> zos.setComment(""));
+        test(zos -> {
+            zos.setComment("");
+            zos.setComment(null);
+        });
+        test(zos -> {
+            zos.setComment(null);
+            zos.setComment("");
+        });
+        test(zos -> {
+            zos.setComment("Comment");
+            zos.setComment(null);
+        });
+        test(zos -> {
+            zos.setComment("Comment");
+            zos.setComment("");
+        });
     }
 
     private static void test(Consumer<ZipOutputStream> test) throws Exception {
@@ -84,8 +91,17 @@ public final class EmptyComment {
             zos.finish();
 
             byte[] data = baos.toByteArray();
+
+            if (data.length > 0xFFFF) { // just in case
+                throw new RuntimeException("data is too big: " + data.length);
+            }
+            int pk = data.length - ZipFile.ENDHDR;
+            if (data[pk] != 'P' || data[pk + 1] != 'K') {
+                throw new RuntimeException("PK is not found");
+            }
             // Since the comment is empty this will be two last bytes
             int pos = data.length - ZipFile.ENDHDR + ZipFile.ENDCOM;
+
             int len = (data[pos] & 0xFF) + ((data[pos + 1] & 0xFF) << 8);
             if (len != 0) {
                 throw new RuntimeException("zip comment is not empty: " + len);
