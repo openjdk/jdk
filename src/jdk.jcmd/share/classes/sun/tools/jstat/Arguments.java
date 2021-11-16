20f3872d1cd6257ab9c76bb998f8dc2d07bc1724
Andrey Turbanov <turbanoff@gmail.com>
2021-11-16 11:13:24 +0000

    8274261: Use enhanced-for instead of plain 'for' in jdk.jcmd
    Reviewed-by: sspitsyn, cjplummer

--- src/jdk.jcmd/share/classes/sun/tools/jstat/Arguments.java
+++ src/jdk.jcmd/share/classes/sun/tools/jstat/Arguments.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -115,8 +115,8 @@ public class Arguments {
         String unitString = null;
         String valueString = s;
 
-        for (int i = 0; i < unitStrings.length; i++) {
-            int index = s.indexOf(unitStrings[i]);
+        for (String unit : unitStrings) {
+            int index = s.indexOf(unit);
             if (index > 0) {
                 unitString = s.substring(index);
                 valueString = s.substring(0, index);
