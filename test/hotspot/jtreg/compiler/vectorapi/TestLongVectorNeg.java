db1e77bd40727b9c50dae4a5ebc8b8010d27be67
Jie Fu <jiefu@openjdk.org>
2021-11-19 10:38:42 +0000

    8277449: compiler/vectorapi/TestLongVectorNeg.java fails with release VMs
    Reviewed-by: thartmann, chagedorn

--- test/hotspot/jtreg/compiler/vectorapi/TestLongVectorNeg.java
+++ test/hotspot/jtreg/compiler/vectorapi/TestLongVectorNeg.java
@@ -30,6 +30,7 @@ import jdk.incubator.vector.LongVector;
  * @bug 8275643
  * @summary Test that LongVector.neg is properly handled by the _VectorUnaryOp C2 intrinsic
  * @modules jdk.incubator.vector
+ * @requires vm.debug
  * @run main/othervm -XX:-TieredCompilation -XX:+AlwaysIncrementalInline -Xbatch
  *                   -XX:CompileCommand=dontinline,compiler.vectorapi.TestLongVectorNeg::test
  *                   compiler.vectorapi.TestLongVectorNeg
