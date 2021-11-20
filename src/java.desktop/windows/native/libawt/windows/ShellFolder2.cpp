16d3eadc74570091a505aa5841f901add63de73a
Alexander Zuev <alexander.zuev@oracle.com>
2021-11-19 20:58:09 -0800

    Added bad_malloc handling Fixed insets Declaration and assignment are now joined

--- src/java.desktop/windows/native/libawt/windows/ShellFolder2.cpp
+++ src/java.desktop/windows/native/libawt/windows/ShellFolder2.cpp
@@ -1056,8 +1056,10 @@ JNIEXPORT jintArray JNICALL Java_sun_awt_shell_Win32ShellFolder2_getIconBits
             bmi.bmiHeader.biCompression = BI_RGB;
             // Extract the color bitmap
             int nBits = iconSize * iconSize;
-            long * colorBits;
-            colorBits = (long*)safe_Malloc(MAX_ICON_SIZE * MAX_ICON_SIZE * sizeof(long));
+
+            TRY;
+
+            long *colorBits = (long*)safe_Malloc(MAX_ICON_SIZE * MAX_ICON_SIZE * sizeof(long));
             GetDIBits(dc, iconInfo.hbmColor, 0, iconSize, colorBits, &bmi, DIB_RGB_COLORS);
             // XP supports alpha in some icons, and depending on device.
             // This should take precedence over the icon mask bits.
@@ -1072,8 +1074,7 @@ JNIEXPORT jintArray JNICALL Java_sun_awt_shell_Win32ShellFolder2_getIconBits
             }
             if (!hasAlpha) {
                 // Extract the mask bitmap
-                long * maskBits;
-                maskBits = (long*)safe_Malloc(MAX_ICON_SIZE * MAX_ICON_SIZE * sizeof(long));
+                long *maskBits = (long*)safe_Malloc(MAX_ICON_SIZE * MAX_ICON_SIZE * sizeof(long));
                 GetDIBits(dc, iconInfo.hbmMask, 0, iconSize, maskBits, &bmi, DIB_RGB_COLORS);
                 // Copy the mask alphas into the color bits
                 for (int i = 0; i < nBits; i++) {
@@ -1088,10 +1089,12 @@ JNIEXPORT jintArray JNICALL Java_sun_awt_shell_Win32ShellFolder2_getIconBits
             // Create java array
             iconBits = env->NewIntArray(nBits);
             if (!(env->ExceptionCheck())) {
-            // Copy values to java array
-            env->SetIntArrayRegion(iconBits, 0, nBits, colorBits);
+                // Copy values to java array
+                env->SetIntArrayRegion(iconBits, 0, nBits, colorBits);
+            }
             free(colorBits);
-        }
+
+            CATCH_BAD_ALLOC_RET(NULL);
         }
         // Fix 4745575 GDI Resource Leak
         // MSDN
