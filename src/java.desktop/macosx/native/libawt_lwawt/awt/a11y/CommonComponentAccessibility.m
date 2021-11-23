7ffac79187c7d90a39413d05a96b5a3d66eaf11b
Artem Semenov <asemenov@openjdk.org>
2021-11-23 08:20:15 +0000

    8277195: missing CAccessibility definition in [CommonComponentAccessibility accessibilityHitTest]
    Reviewed-by: ant, kizune, pbansal

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -1091,6 +1091,7 @@ static jobject sAccessibilityClass = NULL;
     DECLARE_STATIC_METHOD_RETURN(jm_accessibilityHitTest, sjc_CAccessibility, "accessibilityHitTest",
                                  "(Ljava/awt/Container;FF)Ljavax/accessibility/Accessible;", nil);
 
+    GET_CACCESSIBILITY_CLASS_RETURN(nil);
     // Make it into java screen coords
     point.y = [[[[self view] window] screen] frame].size.height - point.y;
 
