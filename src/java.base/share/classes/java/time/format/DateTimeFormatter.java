4e9e061e5d2a18f7c8b022bbda7114597231a8d7
Naoto Sato <naoto.sato@oracle.com>
2021-11-24 14:38:07 -0800

    Refined wording #2

--- src/java.base/share/classes/java/time/format/DateTimeFormatter.java
+++ src/java.base/share/classes/java/time/format/DateTimeFormatter.java
@@ -504,9 +504,8 @@ import sun.util.locale.provider.TimeZoneNameUtility;
  * If a {@code ZoneId} was parsed without an offset then the zone will be
  * combined with the {@code LocalDateTime} to form the instant using the rules
  * of {@link ChronoLocalDateTime#atZone(ZoneId)}. If the {@code ZoneId} was
- * parsed from the zone name that does not imply daylight saving time, then
- * {@link ChronoZonedDateTime#withLaterOffsetAtOverlap()} is issued
- * to use the standard offset at the overlap, before forming the instant.
+ * parsed from the zone name that does not indicate daylight saving time, then
+ * the standard offset will be used at the local time-line overlap.
  * </ol>
  *
  * @implSpec
