002eec1ef3935a018bc005beb377fe682d1a479f
Maurizio Cimadamore <mcimadamore@openjdk.org>
2021-11-24 11:51:16 +0000

    8275063: Implementation of Foreign Function & Memory API (Second incubator)
    Reviewed-by: erikj, psandoz, jvernee, darcy

--- test/jdk/java/foreign/TestSegmentOffset.java
+++ test/jdk/java/foreign/TestSegmentOffset.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ *  Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  *  This code is free software; you can redistribute it and/or modify it
@@ -16,60 +16,58 @@
  *  2 along with this work; if not, write to the Free Software Foundation,
  *  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *   Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ *  Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  *  or visit www.oracle.com if you need additional information or have any
  *  questions.
- *
  */
 
 /*
  * @test
- * @run testng TestRebase
+ * @run testng TestSegmentOffset
  */
 
-import jdk.incubator.foreign.MemoryAccess;
-import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.MemorySegment;
 import jdk.incubator.foreign.ResourceScope;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.IntFunction;
-
+import static java.lang.System.out;
+import static jdk.incubator.foreign.ValueLayout.JAVA_BYTE;
 import static org.testng.Assert.*;
 
-public class TestRebase {
+public class TestSegmentOffset {
 
     @Test(dataProvider = "slices")
-    public void testRebase(SegmentSlice s1, SegmentSlice s2) {
+    public void testOffset(SegmentSlice s1, SegmentSlice s2) {
         if (s1.contains(s2)) {
-            //check that an address and its rebased counterpart point to same element
-            MemoryAddress base = s2.segment.address();
-            long offset = base.segmentOffset(s1.segment);
+            // check that a segment and its overlapping segment point to same elements
+            long offset = s1.segment.segmentOffset(s2.segment);
             for (int i = 0; i < s2.size(); i++) {
-                int expected = MemoryAccess.getByteAtOffset(s2.segment, i);
-                int found = (int)MemoryAccess.getByteAtOffset(s1.segment, i + offset);
+                out.format("testOffset s1:%s, s2:%s, offset:%d, i:%s\n", s1, s2, offset, i);
+                byte expected = s2.segment.get(JAVA_BYTE, i);
+                byte found = s1.segment.get(JAVA_BYTE, i + offset);
                 assertEquals(found, expected);
             }
         } else if (s1.kind != s2.kind) {
-            // check that rebase s1 to s2 fails
+            // check that offset from s1 to s2 fails
             try {
-                s1.segment.address().segmentOffset(s2.segment);
-                fail("Rebase unexpectedly passed!");
-            } catch (IllegalArgumentException ex) {
-                assertTrue(true);
+                long offset = s1.segment.segmentOffset(s2.segment);
+                out.format("testOffset s1:%s, s2:%s, offset:%d\n", s1, s2, offset);
+                fail("offset unexpectedly passed!");
+            } catch (UnsupportedOperationException ex) {
+                assertTrue(ex.getMessage().contains("Cannot compute offset from native to heap (or vice versa)."));
             }
         } else if (!s2.contains(s1)) {
-            //disjoint segments - check that rebased address is out of bounds
-            MemoryAddress base = s2.segment.address();
-            long offset = base.segmentOffset(s1.segment);
+            // disjoint segments - check that offset is out of bounds
+            long offset = s1.segment.segmentOffset(s2.segment);
             for (int i = 0; i < s2.size(); i++) {
-                MemoryAccess.getByteAtOffset(s2.segment, i);
+                out.format("testOffset s1:%s, s2:%s, offset:%d, i:%s\n", s1, s2, offset, i);
+                s2.segment.get(JAVA_BYTE, i);
                 try {
-                    MemoryAccess.getByteAtOffset(s1.segment, i + offset);
-                    fail("Rebased address on a disjoint segment is not out of bounds!");
+                    s1.segment.get(JAVA_BYTE, i + offset);
+                    fail("Offset on a disjoint segment is not out of bounds!");
                 } catch (IndexOutOfBoundsException ex) {
                     assertTrue(true);
                 }
@@ -80,7 +78,7 @@ public class TestRebase {
     static class SegmentSlice {
 
         enum Kind {
-            NATIVE(i -> MemorySegment.allocateNative(i, ResourceScope.newImplicitScope())),
+            NATIVE(i -> MemorySegment.allocateNative(i, ResourceScope.newConfinedScope())),
             ARRAY(i -> MemorySegment.ofArray(new byte[i]));
 
             final IntFunction<MemorySegment> segmentFactory;
@@ -122,12 +120,12 @@ public class TestRebase {
         int[] sizes = { 16, 8, 4, 2, 1 };
         List<SegmentSlice> slices = new ArrayList<>();
         for (SegmentSlice.Kind kind : SegmentSlice.Kind.values()) {
-            //init root segment
+            // init root segment
             MemorySegment segment = kind.makeSegment(16);
             for (int i = 0 ; i < 16 ; i++) {
-                MemoryAccess.setByteAtOffset(segment, i, (byte)i);
+                segment.set(JAVA_BYTE, i, (byte)i);
             }
-            //compute all slices
+            // compute all slices
             for (int size : sizes) {
                 for (int index = 0 ; index < 16 ; index += size) {
                     MemorySegment slice = segment.asSlice(index, size);
