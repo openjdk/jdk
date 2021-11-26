99e4bda303f2c71972a125d0ecaf4cf986c8614a
Roman Kennke <rkennke@openjdk.org>
2021-11-26 09:46:09 +0000

    8277417: C1 LIR instruction for load-klass
    Reviewed-by: iveresov, mdoerr, ngasson, aph

--- src/hotspot/share/c1/c1_LIRAssembler.hpp
+++ src/hotspot/share/c1/c1_LIRAssembler.hpp
@@ -197,6 +197,7 @@ class LIR_Assembler: public CompilationResourceObj {
   void emit_typecheck_helper(LIR_OpTypeCheck *op, Label* success, Label* failure, Label* obj_is_null);
   void emit_compare_and_swap(LIR_OpCompareAndSwap* op);
   void emit_lock(LIR_OpLock* op);
+  void emit_load_klass(LIR_OpLoadKlass* op);
   void emit_call(LIR_OpJavaCall* op);
   void emit_rtcall(LIR_OpRTCall* op);
   void emit_profile_call(LIR_OpProfileCall* op);
