6d41a9ac8f62d0e1d3c71cce1fd0b4eb6151cfc4
Ludvig Janiuk <ludvig.j.janiuk@oracle.com>
2021-11-30 03:54:07 +0100

    Remove null and match checks using successors

--- src/hotspot/share/c1/c1_Optimizer.cpp
+++ src/hotspot/share/c1/c1_Optimizer.cpp
@@ -383,11 +383,8 @@ class BlockMerger: public BlockClosure {
     BlockBegin *receiver = sux;// disconnect this block from all other blocks
     for (int p = 0; p < receiver->number_of_preds(); p++) {
       BlockBegin *receiver1 = receiver->pred_at(p);
-      assert(receiver1->end() != NULL, "End should not be null.");
-      assert(receiver1->successors() == receiver1->end()->sux(), "must match janiuk");
       int idx;
       while ((idx = receiver1->end()->find_sux(receiver)) >= 0) {
-        assert(receiver1->successors() == receiver1->end()->sux(), "must match janiuk");
         receiver1->end()->remove_sux_at(idx);
       }
     }
