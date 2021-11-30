db32350f5392a50859c36f768f227523eb579386
stsypanov <sergei.tsypanov@yandex.ru>
2021-11-30 20:37:52 +0200

    Remove entryName

--- src/java.base/share/classes/sun/net/www/protocol/jar/JarURLConnection.java
+++ src/java.base/share/classes/sun/net/www/protocol/jar/JarURLConnection.java
@@ -31,7 +31,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.security.Permission;
 import java.util.List;
 import java.util.Map;
@@ -47,9 +46,6 @@ public class JarURLConnection extends java.net.JarURLConnection {
     /* the Jar file factory. It handles both retrieval and caching.
      */
     private static final JarFileFactory factory = JarFileFactory.getInstance();
-
-    /* the entry name, if any */
-    private String entryName;
 
     /* the JarEntry */
     private JarEntry jarEntry;
@@ -68,7 +64,6 @@ public class JarURLConnection extends java.net.JarURLConnection {
         // whether, or not, the embedded URL should use the cache will depend
         // on this instance's cache value
         jarFileURLConnection.setUseCaches(useCaches);
-        entryName = getEntryName();
     }
 
     public JarFile getJarFile() throws IOException {
@@ -103,7 +98,7 @@ public class JarURLConnection extends java.net.JarURLConnection {
     public void connect() throws IOException {
         if (!connected) {
             boolean useCaches = getUseCaches();
-            String entryName = this.entryName;
+            String entryName = getEntryName();
 
             /* the factory call will do the security checks */
             URL url = getJarFileURL();
@@ -159,6 +154,7 @@ public class JarURLConnection extends java.net.JarURLConnection {
 
         InputStream result = null;
 
+        String entryName = getEntryName();
         if (entryName == null) {
             throw new IOException("no entry name specified");
         } else {
@@ -199,7 +195,7 @@ public class JarURLConnection extends java.net.JarURLConnection {
         Object result = null;
 
         connect();
-        if (entryName == null) {
+        if (getEntryName() == null) {
             result = jarFile;
         } else {
             result = super.getContent();
@@ -209,6 +205,7 @@ public class JarURLConnection extends java.net.JarURLConnection {
 
     public String getContentType() {
         if (contentType == null) {
+            String entryName = getEntryName();
             if (entryName == null) {
                 contentType = "x-java/jar";
             } else {
