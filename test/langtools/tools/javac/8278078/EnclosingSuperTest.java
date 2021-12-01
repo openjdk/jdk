a3964667c33345f13d31243ca38d391059382997
Adam Sotona <adam.sotona@oracle.com>
2021-12-01 21:30:33 +0100

    fixed typo in test name

--- test/langtools/tools/javac/8278078/EnclosingSuperTest.java
+++ test/langtools/tools/javac/8278078/EnclosingSuperTest.java
@@ -4,10 +4,10 @@
  * @summary error: cannot reference super before supertype constructor has been called
  * @run compile EclosingSuperTest.java
  */
-public class EclosingSuperTest {
+public class EnclosingSuperTest {
     class InnerClass extends Exception {
         InnerClass() {
-            super(EclosingSuperTest.super.toString());
+            super(EnclosingSuperTest.super.toString());
         }
     }
 }
