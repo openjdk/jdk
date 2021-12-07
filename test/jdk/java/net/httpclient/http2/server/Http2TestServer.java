c02ca56590ea704940d6fb34e3787f2e777dfb7a
jpai <jaikiran.pai@gmail.com>
2021-12-07 18:58:11 +0530

    print the stacktrace, to aid debugging, from Http2TestServer when server fails to start

--- test/jdk/java/net/httpclient/http2/server/Http2TestServer.java
+++ test/jdk/java/net/httpclient/http2/server/Http2TestServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -297,6 +297,7 @@ public class Http2TestServer implements AutoCloseable {
                             socket.close();
                         }
                         System.err.println("TestServer: start exception: " + e);
+                        e.printStackTrace();
                     }
                 }
             } catch (SecurityException se) {
