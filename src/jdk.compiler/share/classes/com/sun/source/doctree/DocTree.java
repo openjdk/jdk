1ceb5aad06dc38b3d8acf4aab786cc37a07dc906
Pavel Rappo <prappo@openjdk.org>
2021-12-07 12:30:03 +0000

    Initial commit

--- src/jdk.compiler/share/classes/com/sun/source/doctree/DocTree.java
+++ src/jdk.compiler/share/classes/com/sun/source/doctree/DocTree.java
@@ -38,126 +38,168 @@ public interface DocTree {
         /**
          * Used for instances of {@link AttributeTree}
          * representing an attribute in an HTML element or tag.
+         *
+         * @since 1.8
          */
         ATTRIBUTE,
 
         /**
          * Used for instances of {@link AuthorTree}
          * representing an {@code @author} tag.
+         *
+         * @since 1.8
          */
         AUTHOR("author"),
 
         /**
          * Used for instances of {@link LiteralTree}
          * representing an {@code @code} tag.
+         *
+         * @since 1.8
          */
         CODE("code"),
 
         /**
          * Used for instances of {@link CommentTree}
          * representing an HTML comment.
+         *
+         * @since 1.8
          */
         COMMENT,
 
         /**
          * Used for instances of {@link DeprecatedTree}
          * representing an {@code @deprecated} tag.
+         *
+         * @since 1.8
          */
         DEPRECATED("deprecated"),
 
         /**
          * Used for instances of {@link DocCommentTree}
          * representing a complete doc comment.
+         *
+         * @since 1.8
          */
         DOC_COMMENT,
 
         /**
          * Used for instances of {@link DocRootTree}
          * representing an {@code @docRoot} tag.
+         *
+         * @since 1.8
          */
         DOC_ROOT("docRoot"),
 
         /**
          * Used for instances of {@link DocTypeTree}
          * representing an HTML DocType declaration.
+         *
+         * @since 10
          */
         DOC_TYPE,
 
         /**
          * Used for instances of {@link EndElementTree}
          * representing the end of an HTML element.
+         *
+         * @since 1.8
          */
         END_ELEMENT,
 
         /**
          * Used for instances of {@link EntityTree}
          * representing an HTML entity.
+         *
+         * @since 1.8
          */
         ENTITY,
 
         /**
          * Used for instances of {@link ErroneousTree}
          * representing some invalid text.
+         *
+         * @since 1.8
          */
         ERRONEOUS,
 
         /**
          * Used for instances of {@link ThrowsTree}
          * representing an {@code @exception} tag.
+         *
+         * @since 1.8
          */
         EXCEPTION("exception"),
 
         /**
          * Used for instances of {@link HiddenTree}
          * representing an {@code @hidden} tag.
+         *
+         * @since 1.8
          */
         HIDDEN("hidden"),
 
         /**
          * Used for instances of {@link IdentifierTree}
          * representing an identifier.
+         *
+         * @since 1.8
          */
         IDENTIFIER,
 
         /**
          * Used for instances of {@link IndexTree}
          * representing an {@code @index} tag.
+         *
+         * @since 9
          */
         INDEX("index"),
 
         /**
          * Used for instances of {@link InheritDocTree}
          * representing an {@code @inheritDoc} tag.
+         *
+         * @since 1.8
          */
         INHERIT_DOC("inheritDoc"),
 
         /**
          * Used for instances of {@link LinkTree}
          * representing an {@code @link} tag.
+         *
+         * @since 1.8
          */
         LINK("link"),
 
         /**
          * Used for instances of {@link LinkTree}
          * representing an {@code @linkplain} tag.
+         *
+         * @since 1.8
          */
         LINK_PLAIN("linkplain"),
 
         /**
          * Used for instances of {@link LiteralTree}
          * representing an {@code @literal} tag.
+         *
+         * @since 1.8
          */
         LITERAL("literal"),
 
         /**
          * Used for instances of {@link ParamTree}
          * representing an {@code @param} tag.
+         *
+         * @since 1.8
          */
         PARAM("param"),
 
         /**
          * Used for instances of {@link ProvidesTree}
          * representing an {@code @provides} tag.
+         *
+         * @since 9
          */
         PROVIDES("provides"),
 
@@ -165,114 +207,152 @@ public interface DocTree {
          * Used for instances of {@link ReferenceTree}
          * representing a reference to an element in the
          * Java programming language.
+         *
+         * @since 1.8
          */
         REFERENCE,
 
         /**
          * Used for instances of {@link ReturnTree}
          * representing an {@code @return} tag.
+         *
+         * @since 1.8
          */
         RETURN("return"),
 
         /**
          * Used for instances of {@link SeeTree}
          * representing an {@code @see} tag.
+         *
+         * @since 1.8
          */
         SEE("see"),
 
         /**
          * Used for instances of {@link SerialTree}
          * representing an {@code @serial} tag.
+         *
+         * @since 1.8
          */
         SERIAL("serial"),
 
         /**
          * Used for instances of {@link SerialDataTree}
          * representing an {@code @serialData} tag.
+         *
+         * @since 1.8
          */
         SERIAL_DATA("serialData"),
 
         /**
          * Used for instances of {@link SerialFieldTree}
          * representing an {@code @serialField} tag.
+         *
+         * @since 1.8
          */
         SERIAL_FIELD("serialField"),
 
         /**
          * Used for instances of {@link SinceTree}
          * representing an {@code @since} tag.
+         *
+         * @since 1.8
          */
         SINCE("since"),
 
         /**
          * Used for instances of {@link SnippetTree}
          * representing an {@code @snippet} tag.
+         *
+         * @since 18
          */
         SNIPPET("snippet"),
 
         /**
          * Used for instances of {@link EndElementTree}
          * representing the start of an HTML element.
+         *
+         * @since 1.8
          */
         START_ELEMENT,
 
         /**
          * Used for instances of {@link SystemPropertyTree}
          * representing an {@code @systemProperty} tag.
+         *
+         * @since 12
          */
         SYSTEM_PROPERTY("systemProperty"),
 
         /**
          * Used for instances of {@link SummaryTree}
          * representing an {@code @summary} tag.
+         *
+         * @since 10
          */
         SUMMARY("summary"),
 
         /**
          * Used for instances of {@link TextTree}
          * representing some documentation text.
+         *
+         * @since 1.8
          */
         TEXT,
 
         /**
          * Used for instances of {@link ThrowsTree}
          * representing an {@code @throws} tag.
+         *
+         * @since 1.8
          */
         THROWS("throws"),
 
         /**
          * Used for instances of {@link UnknownBlockTagTree}
          * representing an unknown block tag.
+         *
+         * @since 1.8
          */
         UNKNOWN_BLOCK_TAG,
 
         /**
          * Used for instances of {@link UnknownInlineTagTree}
          * representing an unknown inline tag.
+         *
+         * @since 1.8
          */
         UNKNOWN_INLINE_TAG,
 
         /**
          * Used for instances of {@link UsesTree}
          * representing an {@code @uses} tag.
+         *
+         * @since 9
          */
         USES("uses"),
 
         /**
          * Used for instances of {@link ValueTree}
          * representing an {@code @value} tag.
+         *
+         * @since 1.8
          */
         VALUE("value"),
 
         /**
          * Used for instances of {@link VersionTree}
          * representing an {@code @version} tag.
+         *
+         * @since 1.8
          */
         VERSION("version"),
 
         /**
          * An implementation-reserved node. This is not the node
          * you are looking for.
+         *
+         * @since 1.8
          */
         OTHER;
 
