ba348fa1364d55ffd31965658778d75793148d88
Weijun Wang <weijun.wang@oracle.com>
2021-12-08 10:33:46 -0500

    8278186: throw StringIndexOutOfBoundsException when calling substring method
    8278186: throw StringIndexOutOfBoundsException when calling substring method

--- src/java.xml.crypto/share/classes/org/jcp/xml/dsig/internal/dom/Utils.java
+++ src/java.xml.crypto/share/classes/org/jcp/xml/dsig/internal/dom/Utils.java
@@ -21,7 +21,7 @@
  * under the License.
  */
 /*
- * Copyright (c) 2005, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2021, Oracle and/or its affiliates. All rights reserved.
  */
 package org.jcp.xml.dsig.internal.dom;
 
@@ -94,7 +94,9 @@ public final class Utils {
         if (id.startsWith("xpointer(id(")) {
             int i1 = id.indexOf('\'');
             int i2 = id.indexOf('\'', i1+1);
-            id = id.substring(i1+1, i2);
+            if (i1 >= 0 && i2 >= 0) {
+                id = id.substring(i1 + 1, i2);
+            }
         }
         return id;
     }
