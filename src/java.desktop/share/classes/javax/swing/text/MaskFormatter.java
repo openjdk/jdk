c14fb5f8675b183a509615070929baade256b288
TejeshR13 <tejesh.r@oracle.com>
2021-12-09 18:51:46 +0530

    6462028:MaskFormatter API documentation refers to getDisplayValue

--- src/java.desktop/share/classes/javax/swing/text/MaskFormatter.java
+++ src/java.desktop/share/classes/javax/swing/text/MaskFormatter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2017, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,13 +25,13 @@
 
 package javax.swing.text;
 
-import javax.swing.JFormattedTextField;
-import javax.swing.text.DefaultFormatter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serial;
 import java.text.ParseException;
 import java.util.ArrayList;
+import javax.swing.JFormattedTextField;
+import javax.swing.text.DefaultFormatter;
 
 /**
  * <code>MaskFormatter</code> is used to format and edit strings. The behavior
