63f7515f0f0e7f2afad2792b6c711ae4bc7976df
Alan Hayward <alan.hayward@arm.com>
2021-12-10 11:18:00 +0000

    Remove BSD/Apple specific code

--- src/hotspot/os_cpu/bsd_aarch64/pauth_bsd_aarch64.inline.hpp
+++ src/hotspot/os_cpu/bsd_aarch64/pauth_bsd_aarch64.inline.hpp
@@ -28,71 +28,20 @@
 // OS specific Support for ROP Protection in VM code.
 // For more details on PAC see pauth_aarch64.hpp.
 
-inline bool pauth_ptr_is_raw(address ptr);
-
-#ifdef __APPLE__
-#include <ptrauth.h>
-#endif
-
-// Use only the PAC instructions in the NOP space. This ensures the binaries work on systems
-// without PAC. Write these instructions using their alternate "hint" instructions to ensure older
-// compilers can still be used. For Apple, instead use the recommended pauth interface.
-#define XPACLRI   "hint #0x7;"
-#define PACIA1716 "hint #0x8;"
-#define AUTIA1716 "hint #0xc;"
-
-// Strip an address. Use with caution - only if there is no guaranteed way of authenticating the
-// value.
-//
 inline address pauth_strip_pointer(address ptr) {
-#ifdef __APPLE__
-  return ptrauth_strip(ptr, ptrauth_key_asib);
-#else
-  register address result __asm__("x30") = ptr;
-  asm (XPACLRI : "+r"(result));
-  return result;
-#endif
+  // No PAC support in BSD as of yet.
+  return ptr;
 }
 
-// Sign a return value, using the given modifier.
-//
 inline address pauth_sign_return_address(address ret_addr, address sp) {
-  if (UseROPProtection) {
-    // A pointer cannot be double signed.
-    guarantee(pauth_ptr_is_raw(ret_addr), "Return address is already signed");
-#ifdef __APPLE__
-    ret_addr = ptrauth_sign_unauthenticated(ret_addr, ptrauth_key_asib, sp);
-#else
-    register address r17 __asm("r17") = ret_addr;
-    register address r16 __asm("r16") = sp;
-    asm volatile (PACIA1716 : "+r"(r17) : "r"(r16));
-    ret_addr = r17;
-#endif
-  }
+  // No PAC support in BSD as of yet.
   return ret_addr;
 }
 
-// Authenticate a return value, using the given modifier.
-//
 inline address pauth_authenticate_return_address(address ret_addr, address sp) {
-  if (UseROPProtection) {
-#ifdef __APPLE__
-    ret_addr = ptrauth_auth_data(ret_addr, ptrauth_key_asib, sp);
-#else
-    register address r17 __asm("r17") = ret_addr;
-    register address r16 __asm("r16") = sp;
-    asm volatile (AUTIA1716 : "+r"(r17) : "r"(r16));
-    ret_addr = r17;
-#endif
-    // Ensure that the pointer authenticated.
-    guarantee(pauth_ptr_is_raw(ret_addr), "Return address did not authenticate");
-  }
+  // No PAC support in BSD as of yet.
   return ret_addr;
 }
-
-#undef XPACLRI
-#undef PACIA1716
-#undef AUTIA1716
 
 #endif // OS_CPU_BSD_AARCH64_PAUTH_BSD_AARCH64_INLINE_HPP
 
