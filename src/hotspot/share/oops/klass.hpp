55e718054c3ebcb22a79bd0c7d4384bf5e0fad0c
iklam <ioi.lam@oracle.com>
2021-12-14 20:13:45 -0800

    added comments about the location of vtable_len

--- src/hotspot/share/oops/klass.hpp
+++ src/hotspot/share/oops/klass.hpp
@@ -154,7 +154,10 @@ class Klass : public Metadata {
   // Provide access the corresponding instance java.lang.ClassLoader.
   ClassLoaderData* _class_loader_data;
 
-  int _vtable_len;              // vtable length
+  int _vtable_len;              // vtable length. This field may be read very often when we
+                                // have lots of itable dispatches (e.g., lambdas and streams).
+                                // Keep it away from the beginning of a Klass to avoid cacheline
+                                // contention that may happen when a nearby object is modified.
   AccessFlags _access_flags;    // Access flags. The class/interface distinction is stored here.
 
   JFR_ONLY(DEFINE_TRACE_ID_FIELD;)
