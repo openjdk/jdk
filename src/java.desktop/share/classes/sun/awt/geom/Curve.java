fcf9d3595e280f664f659d5362529d473f0825b6
jeremy <jeremy.wood@mac.com>
2021-12-16 06:19:44 -0500

    8176501: Method Shape.getBounds2D() incorrectly includes Bezier control points in bounding box
    Fixing unit test failures introduced with b3e84a5e4281c2bb9be1c7e1d751ef3593cc387c .
    
    (The likely explanation for why this went unaddressed previously is: I probably didn't correctly execute unit tests after this change. A less likely explanation might be: the Path2D/UnitTest class uses random numbers each session, so it's possible results passed in that session and failed in others.)

--- src/java.desktop/share/classes/sun/awt/geom/Curve.java
+++ src/java.desktop/share/classes/sun/awt/geom/Curve.java
@@ -791,7 +791,9 @@ public abstract class Curve {
      */
     public static void accumulateExtremaBoundsForCubic(double[] bounds, int boundsOffset, double x1, double ctrlX1, double ctrlX2, double x2, double[] coeff, double[] deriv_coeff) {
         if (ctrlX1 < bounds[boundsOffset] ||
-                ctrlX1 > bounds[boundsOffset + 1]) {
+                ctrlX1 > bounds[boundsOffset + 1] ||
+                ctrlX2 < bounds[boundsOffset] ||
+                ctrlX2 > bounds[boundsOffset + 1]) {
             final double dx32 = 3.0 * (ctrlX2 - ctrlX1);
             final double dx21 = 3.0 * (ctrlX1 - x1);
             coeff[3] = (x2 - x1) - dx32;  // A = P3 - P0 - 3 (P2 - P1) = (P3 - P0) + 3 (P1 - P2)
