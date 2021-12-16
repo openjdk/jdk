4b6ab9c742693fb1c23f41dc6a8f9672cd02bc84
liach <liach@users.noreply.github.com>
2021-12-16 15:32:45 -0600

    choose a concise name for the local var

--- src/java.base/share/classes/java/lang/Class.java
+++ src/java.base/share/classes/java/lang/Class.java
@@ -3820,9 +3820,9 @@ public final class Class<T> implements java.io.Serializable,
     // Fetches the factory for reflective objects
     @SuppressWarnings("removal")
     private static ReflectionFactory getReflectionFactory() {
-        ReflectionFactory reflectionFactory = Class.reflectionFactory;
-        if (reflectionFactory == null) {
-            return Class.reflectionFactory =
+        var factory = reflectionFactory;
+        if (factory == null) {
+            return reflectionFactory =
                 java.security.AccessController.doPrivileged
                     (new ReflectionFactory.GetReflectionFactoryAction());
         }
