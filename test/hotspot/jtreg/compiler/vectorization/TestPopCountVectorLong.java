1892c37da286c3739b51cf2862af3977433d00de
Vamsi Parasa <srinivas.vamsi.parasa@intel.com>
2022-01-05 15:17:55 -0800

    Update year in copyright notice. Add avx512dq check for vectorization test

--- test/hotspot/jtreg/compiler/vectorization/TestPopCountVectorLong.java
+++ test/hotspot/jtreg/compiler/vectorization/TestPopCountVectorLong.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
 /**
 * @test
 * @summary Test vectorization of popcount for Long
+* @requires vm.cpu.features ~= ".*avx512dq.*"
 * @library /test/lib /
 * @run driver compiler.vectorization.TestPopCountVectorLong
 */
