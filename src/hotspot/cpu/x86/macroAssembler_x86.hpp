483e8e656f4b832fe36b51b9873f9a6b52e92136
Aleksey Shipilev <shade@redhat.com>
2022-01-10 19:20:26 +0100

    Review comments

--- src/hotspot/cpu/x86/macroAssembler_x86.hpp
+++ src/hotspot/cpu/x86/macroAssembler_x86.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1490,11 +1490,11 @@ public:
 
   // Simple version for AVX2 256bit vectors
   void vpxor(XMMRegister dst, XMMRegister src) {
-    assert(UseAVX >= 2, "Only with AVX2");
+    assert(UseAVX >= 2, "Should be at least AVX2");
     Assembler::vpxor(dst, dst, src, AVX_256bit);
   }
   void vpxor(XMMRegister dst, Address src) {
-    assert(UseAVX >= 2, "Only with AVX2");
+    assert(UseAVX >= 2, "Should be at least AVX2");
     Assembler::vpxor(dst, dst, src, AVX_256bit);
   }
 
