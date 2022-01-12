d9fa3ec8d1f85a890053e0a658edae8dbd1308e2
Hannes Wallnoefer <hannes.wallnoefer@oracle.com>
2022-01-12 09:22:06 +0100

    JDK-8234682: The order of @param in the generated docs should match the method signature

--- test/langtools/jdk/javadoc/doclet/testParamTaglet/pkg/C.java
+++ test/langtools/jdk/javadoc/doclet/testParamTaglet/pkg/C.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -38,4 +38,14 @@ public class C extends Parent {
      */
     public void nonMatchingParams(int param1, int param2) {}
 
+    /**
+     * Overriding method with missing/additional/duplicate param tags in non-declaration order.
+     *
+     * @param x does not exist
+     * @param b a boolean
+     * @param i an int
+     * @param x duplicate
+     * @param b another duplicate
+     */
+    public void unorderedParams(int i, double d, boolean b) {}
 }
