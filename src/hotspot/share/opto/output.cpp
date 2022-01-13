b6c8b7e114e3466964a4f3c45eaee847fcdfe27e
sunguoyun <sunguoyun@loongson.cn>
2022-01-13 19:19:32 +0800

    8279956: Useless method Scheduling::ComputeLocalLatenciesForward( )

--- src/hotspot/share/opto/output.cpp
+++ src/hotspot/share/opto/output.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,12 +20,6 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  *
- */
-
-/*
- * This file has been modified by Loongson Technology in 2022. These
- * modifications are Copyright (c) 2022, Loongson Technology, and are made
- * available on the same license terms set forth above.
  */
 
 #include "precompiled.hpp"
