cb25029885b176be9ebbc84ac1a8ba71be96a6a7
Weijun Wang <weijun@openjdk.org>
2022-01-13 00:42:00 +0000

    8279800: isAssignableFrom checks in AlgorithmParametersSpi.engineGetParameterSpec appear to be backwards
    Reviewed-by: xuelei, valeriep

--- src/java.base/share/classes/sun/security/rsa/PSSParameters.java
+++ src/java.base/share/classes/sun/security/rsa/PSSParameters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -180,7 +180,7 @@ public final class PSSParameters extends AlgorithmParametersSpi {
     protected <T extends AlgorithmParameterSpec>
             T engineGetParameterSpec(Class<T> paramSpec)
             throws InvalidParameterSpecException {
-        if (PSSParameterSpec.class.isAssignableFrom(paramSpec)) {
+        if (paramSpec.isAssignableFrom(PSSParameterSpec.class)) {
             return paramSpec.cast(spec);
         } else {
             throw new InvalidParameterSpecException
