621f1a684724d5625ece93273e7e0cad9971b238
Yano, Masanori <yano-masanori@jp.fujitsu.com>
2022-01-14 19:59:24 +0900

    8272746: ZipFile can't open big file (NegativeArraySizeException)

--- test/jdk/java/util/zip/ZipFile/TestTooManyEntries.java
+++ test/jdk/java/util/zip/ZipFile/TestTooManyEntries.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,8 +25,11 @@
  * @bug 8272746
  * @summary ZipFile can't open big file (NegativeArraySizeException)
  * @requires (sun.arch.data.model == "64" & os.maxMemory > 8g)
- * @run main/othervm/timeout=3600 -Xmx8g TestTooManyEntries
+ * @run testng/manual/othervm -Xmx8g TestTooManyEntries
  */
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.BufferedOutputStream;
@@ -37,41 +40,39 @@ import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 import java.util.UUID;
 
+import static org.testng.Assert.assertThrows;
+
 public class TestTooManyEntries {
-    public static void main(String[] args) throws Exception {
-        boolean passed = false;
-        File hugeZipFile = File.createTempFile("hugeZip", ".zip", new File("."));
+    private static final int DIR_COUNT = 25000;
+    private static final int ENTRIES_IN_DIR = 1000;
+
+    private File hugeZipFile;
+
+    @BeforeTest
+    public void setup() throws Exception {
+        hugeZipFile = File.createTempFile("hugeZip", ".zip", new File("."));
         hugeZipFile.deleteOnExit();
         long startTime = System.currentTimeMillis();
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(hugeZipFile)))) {
-            int dirCount = 25_000;
             long nextLog = System.currentTimeMillis();
-            for (int dirN = 0; dirN < dirCount; dirN++) {
+            for (int dirN = 0; dirN < DIR_COUNT; dirN++) {
                 String dirName = UUID.randomUUID() + "/";
-                for (int fileN = 0; fileN < 1_000; fileN++) {
+                for (int fileN = 0; fileN < ENTRIES_IN_DIR; fileN++) {
                     ZipEntry entry = new ZipEntry(dirName + UUID.randomUUID());
                     zip.putNextEntry(entry);
                     zip.closeEntry(); // all files are empty
                 }
-                if (System.currentTimeMillis() >= nextLog) {
-                    nextLog = 30_000 + System.currentTimeMillis();
-                    System.out.printf("Processed %s%% directories (%s), file size is %sMb (%ss)%n",
-                            dirN * 100 / dirCount, dirN, hugeZipFile.length() / 1024 / 1024,
+                if ((dirN + 1) % 1000 == 0) {
+                    System.out.printf("%s / %s of entries written, file size is %sMb (%ss)%n",
+                            (dirN + 1) * ENTRIES_IN_DIR, DIR_COUNT * ENTRIES_IN_DIR, hugeZipFile.length() / 1024 / 1024,
                             (System.currentTimeMillis() - startTime) / 1000);
                 }
             }
         }
-        System.out.printf("File generated in %ss, file size is %sMb%n",
-                (System.currentTimeMillis() - startTime) / 1000, hugeZipFile.length() / 1024 / 1024);
+    }
 
-        try {
-            ZipFile zip = new ZipFile(hugeZipFile);
-        } catch (ZipException ze) {
-            passed = true;
-        }
-
-        if (!passed) {
-            throw new RuntimeException("Failed.");
-        }
+    @Test
+    public void test() {
+        assertThrows(ZipException.class, () -> new ZipFile(hugeZipFile));
     }
 }
