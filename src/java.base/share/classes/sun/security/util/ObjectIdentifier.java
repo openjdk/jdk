47a269a11ad4a57927418e73e4458fcb2559def7
Aleksei Voitylov <avoitylov@openjdk.org>
2022-01-14 22:11:10 +0000

    8275151: Improved Object Identification
    Reviewed-by: mbaesken
    Backport-of: 4cffe1cc913f895f54281de8e91ca39f4bde4b5f

--- src/java.base/share/classes/sun/security/util/ObjectIdentifier.java
+++ src/java.base/share/classes/sun/security/util/ObjectIdentifier.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -684,6 +684,10 @@ public final class ObjectIdentifier implements Serializable {
     }
 
     private static void checkOidSize(int oidLength) throws IOException {
+        if (oidLength < 0) {
+            throw new IOException("ObjectIdentifier encoded length was " +
+                    "negative: " + oidLength);
+        }
         if (oidLength > MAXIMUM_OID_SIZE) {
             throw new IOException(
                     "ObjectIdentifier encoded length exceeds " +
