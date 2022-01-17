c09e41f658c05025627b11e3d1029dc63b80e172
Erik Gahlin <erik.gahlin@gmail.com>
2022-01-17 02:56:11 +0100

    Update comment and copyright

--- src/jdk.jfr/share/classes/jdk/jfr/internal/consumer/ObjectContext.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/consumer/ObjectContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,8 +61,8 @@ public final class ObjectContext {
         return contextLookup.get(descriptor);
     }
 
-    // Create mapping from ValueDescriptor to ObjectContext for transitive
-    // closure of List<ValueDescriptor> ValueDescriptor::getFields()
+    // Create mapping from ValueDescriptor to ObjectContext for all reachable
+    // ValueDescriptors.
     public Map<ValueDescriptor, ObjectContext> buildContextLookup(List<ValueDescriptor> fields) {
         Map<ValueDescriptor, ObjectContext> lookup = new IdentityHashMap<>();
         ArrayDeque<ValueDescriptor> q = new ArrayDeque<>(fields);
