3ca676cd30746415a39cca41f0921e8dd1a425af
Goetz Lindenmaier <goetz@openjdk.org>
2022-01-19 19:48:46 +0000

    8269032: Stringdedup tests are failing if the ergonomically select GC does not support it
    Backport-of: 3fc761d97d43429d3fdc0646835c462e0fe1bd3e

--- test/hotspot/jtreg/gc/stringdedup/TestStringDeduplicationAgeThreshold.java
+++ test/hotspot/jtreg/gc/stringdedup/TestStringDeduplicationAgeThreshold.java
@@ -27,17 +27,31 @@ package gc.stringdedup;
  * @test TestStringDeduplicationAgeThreshold
  * @summary Test string deduplication age threshold
  * @bug 8029075
- * @requires vm.gc == "null" | vm.gc == "G1" | vm.gc == "Shenandoah"
+ * @requires vm.gc.G1
  * @library /test/lib
  * @library /
  * @modules java.base/jdk.internal.misc:open
  * @modules java.base/java.lang:open
  *          java.management
- * @run driver gc.stringdedup.TestStringDeduplicationAgeThreshold
+ * @run driver gc.stringdedup.TestStringDeduplicationAgeThreshold G1
+ */
+
+/*
+ * @test TestStringDeduplicationAgeThreshold
+ * @summary Test string deduplication age threshold
+ * @bug 8029075
+ * @requires vm.gc.Shenandoah
+ * @library /test/lib
+ * @library /
+ * @modules java.base/jdk.internal.misc:open
+ * @modules java.base/java.lang:open
+ *          java.management
+ * @run driver gc.stringdedup.TestStringDeduplicationAgeThreshold Shenandoah
  */
 
 public class TestStringDeduplicationAgeThreshold {
     public static void main(String[] args) throws Exception {
+        TestStringDeduplicationTools.selectGC(args);
         TestStringDeduplicationTools.testAgeThreshold();
     }
 }
