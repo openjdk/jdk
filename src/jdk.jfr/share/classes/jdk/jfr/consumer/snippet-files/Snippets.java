7942d3de68c7cc4edfbd2818929953844d4f3090
Erik Gahlin <erik.gahlin@gmail.com>
2022-01-20 18:07:56 +0100

    Unique package name

--- src/jdk.jfr/share/classes/jdk/jfr/consumer/snippet-files/Snippets.java
+++ src/jdk.jfr/share/classes/jdk/jfr/consumer/snippet-files/Snippets.java
@@ -1,4 +1,4 @@
-package example;
+package example2;
 
 import java.io.IOException;
 import java.nio.file.Path;
--- src/jdk.jfr/share/classes/jdk/jfr/snippet-files/Snippets.java
+++ src/jdk.jfr/share/classes/jdk/jfr/snippet-files/Snippets.java
@@ -1,4 +1,4 @@
-package example;
+package example1;
 
 import jdk.jfr.AnnotationElement;
 import jdk.jfr.ValueDescriptor;
