2fa41ad283a16c2be3a0655ecf456d653f58e281
Hamlin-Li <leeming1509@outlook.com>
2022-01-21 20:39:19 +0800

    Thomas review

--- src/hotspot/share/gc/g1/g1EvacFailure.cpp
+++ src/hotspot/share/gc/g1/g1EvacFailure.cpp
@@ -104,7 +104,8 @@ public:
   }
 
   // Fill the memory area from start to end with filler objects, and update the BOT
-  // and the mark bitmap accordingly.
+  // accordingly. Since we clear and use the prev bitmap for marking objects that
+  // failed evacuation, there is no work to be done there.
   void zap_dead_objects(HeapWord* start, HeapWord* end) {
     if (start == end) {
       return;
