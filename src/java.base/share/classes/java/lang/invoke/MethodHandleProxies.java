cbe8395ace3230dc599c7f082e3524a861b2da8e
Joe Darcy <darcy@openjdk.org>
2022-01-25 18:15:37 +0000

    8280168: Add Objects.toIdentityString
    Reviewed-by: alanb, mchung, rriggs, smarks

--- src/java.base/share/classes/java/lang/invoke/MethodHandleProxies.java
+++ src/java.base/share/classes/java/lang/invoke/MethodHandleProxies.java
@@ -292,7 +292,7 @@ public class MethodHandleProxies {
     private static Object callObjectMethod(Object self, Method m, Object[] args) {
         assert(isObjectMethod(m)) : m;
         return switch (m.getName()) {
-            case "toString" -> self.getClass().getName() + "@" + Integer.toHexString(self.hashCode());
+            case "toString" -> java.util.Objects.toIdentityString(self);
             case "hashCode" -> System.identityHashCode(self);
             case "equals"   -> (self == args[0]);
             default -> null;
