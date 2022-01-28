c297d42f4505b03780bd9ab2e169546fd6cffcb9
Yasser Bazzi <yassermbb@gmail.com>
2022-01-27 21:23:32 -0300

    make sure setseed its initialized and throw

--- src/java.base/share/classes/jdk/internal/util/random/RandomWrapper.java
+++ src/java.base/share/classes/jdk/internal/util/random/RandomWrapper.java
@@ -39,17 +39,19 @@ import java.util.stream.LongStream;
 @SuppressWarnings("serial")
 public class RandomWrapper extends Random implements RandomGenerator {
     private final RandomGenerator generator;
+    private boolean initialized;
 
     private RandomWrapper(RandomGenerator randomToWrap) {
-	this.generator = randomToWrap;
+        this.generator = randomToWrap;
+        this.initialized = true;
     }
 
     public static Random wrap(RandomGenerator random) {
-	// Check to see if its not wrapping another Random instance
-	if (random instanceof Random rand)
-	    return rand;
+        // Check to see if its not wrapping another Random instance
+        if (random instanceof Random rand)
+            return rand;
 
-	return new RandomWrapper(random);
+        return new RandomWrapper(random);
     }
 
     /**
@@ -58,105 +60,107 @@ public class RandomWrapper extends Random implements RandomGenerator {
      */
     @Override
     public void setSeed(long seed) {
+        if(initialized)
+            throw new UnsupportedOperationException();
     }
 
     @Override
     public void nextBytes(byte[] bytes) {
-	this.generator.nextBytes(bytes);
+        this.generator.nextBytes(bytes);
     }
 
     @Override
     public int nextInt() {
-	return this.generator.nextInt();
+        return this.generator.nextInt();
     }
 
     @Override
     public int nextInt(int bound) {
-	return this.generator.nextInt(bound);
+        return this.generator.nextInt(bound);
     }
 
     @Override
     public long nextLong() {
-	return this.generator.nextLong();
+        return this.generator.nextLong();
     }
 
     @Override
     public boolean nextBoolean() {
-	return this.generator.nextBoolean();
+        return this.generator.nextBoolean();
     }
 
     @Override
     public float nextFloat() {
-	return this.generator.nextFloat();
+        return this.generator.nextFloat();
     }
 
     @Override
     public double nextDouble() {
-	return this.generator.nextDouble();
+        return this.generator.nextDouble();
     }
 
     @Override
     public double nextGaussian() {
-	return this.generator.nextGaussian();
+        return this.generator.nextGaussian();
     }
 
     @Override
     public IntStream ints(long streamSize) {
-	return this.generator.ints(streamSize);
+        return this.generator.ints(streamSize);
     }
 
     @Override
     public IntStream ints() {
-	return this.generator.ints();
+        return this.generator.ints();
     }
 
     @Override
     public IntStream ints(long streamSize, int randomNumberOrigin, int randomNumberBound) {
-	return this.generator.ints(streamSize, randomNumberOrigin, randomNumberBound);
+        return this.generator.ints(streamSize, randomNumberOrigin, randomNumberBound);
     }
 
     @Override
     public IntStream ints(int randomNumberOrigin, int randomNumberBound) {
-	return this.generator.ints(randomNumberOrigin, randomNumberBound);
+        return this.generator.ints(randomNumberOrigin, randomNumberBound);
     }
 
     @Override
     public LongStream longs(long streamSize) {
-	return this.generator.longs(streamSize);
+        return this.generator.longs(streamSize);
     }
 
     @Override
     public LongStream longs() {
-	return this.generator.longs();
+        return this.generator.longs();
     }
 
     @Override
     public LongStream longs(long streamSize, long randomNumberOrigin, long randomNumberBound) {
-	return this.generator.longs(streamSize, randomNumberOrigin, randomNumberBound);
+        return this.generator.longs(streamSize, randomNumberOrigin, randomNumberBound);
     }
 
     @Override
     public LongStream longs(long randomNumberOrigin, long randomNumberBound) {
-	return this.generator.longs(randomNumberOrigin, randomNumberBound);
+        return this.generator.longs(randomNumberOrigin, randomNumberBound);
     }
 
     @Override
     public DoubleStream doubles(long streamSize) {
-	return this.generator.doubles(streamSize);
+        return this.generator.doubles(streamSize);
     }
 
     @Override
     public DoubleStream doubles() {
-	return this.generator.doubles();
+        return this.generator.doubles();
     }
 
     @Override
     public DoubleStream doubles(long streamSize, double randomNumberOrigin, double randomNumberBound) {
-	return this.generator.doubles(streamSize, randomNumberOrigin, randomNumberBound);
+        return this.generator.doubles(streamSize, randomNumberOrigin, randomNumberBound);
     }
 
     @Override
     public DoubleStream doubles(double randomNumberOrigin, double randomNumberBound) {
-	return this.generator.doubles(randomNumberOrigin, randomNumberBound);
+        return this.generator.doubles(randomNumberOrigin, randomNumberBound);
     }
 }
