a888a0fd10bca2760838b0e10468bfa14f622064
rwestrel <rwestrel@redhat.com>
2022-02-02 08:46:09 +0100

    indentation

--- test/hotspot/jtreg/compiler/loopopts/TestCastIIMakesMainLoopPhiDead.java
+++ test/hotspot/jtreg/compiler/loopopts/TestCastIIMakesMainLoopPhiDead.java
@@ -29,25 +29,25 @@
  */
 
 public class TestCastIIMakesMainLoopPhiDead {
-  int iArr[] = new int[0];
+    int iArr[] = new int[0];
 
-  void test() {
-    int x = 8;
-    try {
-      for (int i = 0; i < 8; i++) {
-        iArr[1] = 9;
-        for (int j = -400; 1 > j; j++) {
-          iArr[j] = 4;
-          x -= 2;
+    void test() {
+        int x = 8;
+        try {
+            for (int i = 0; i < 8; i++) {
+                iArr[1] = 9;
+                for (int j = -400; 1 > j; j++) {
+                    iArr[j] = 4;
+                    x -= 2;
+                }
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
         }
-      }
-    } catch (ArrayIndexOutOfBoundsException e) {
     }
-  }
-  public static void main(String[] k) {
-      TestCastIIMakesMainLoopPhiDead t = new TestCastIIMakesMainLoopPhiDead();
-      for (int i = 0; i < 3; i++) {
-        t.test();
-      }
-  }
+    public static void main(String[] k) {
+        TestCastIIMakesMainLoopPhiDead t = new TestCastIIMakesMainLoopPhiDead();
+        for (int i = 0; i < 3; i++) {
+            t.test();
+        }
+    }
 }
