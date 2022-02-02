915aca2613e70cb9a12888ae39c1f761a7ca6f7c
Damon Nguyen <damon.nguyen@oracle.com>
2022-02-01 18:36:51 -0800

    Updated test to include copyright and moved the jtreg tags to the top.

--- test/jdk/javax/swing/JButton/HtmlButtonImageTest/HtmlButtonImageTest.java
+++ test/jdk/javax/swing/JButton/HtmlButtonImageTest/HtmlButtonImageTest.java
@@ -1,3 +1,34 @@
+/*
+ * Copyright 2022 JetBrains s.r.o.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @key headful
+ * @bug 8015854
+ * @summary JButton text set as html image had additional unwanted padding
+ * @run main HtmlButtonImageTest
+ */
+
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Point;
@@ -12,13 +43,6 @@ import javax.swing.UIManager;
 import javax.swing.SwingConstants;
 import javax.swing.UnsupportedLookAndFeelException;
 
-/*
- * @test
- * @key headful
- * @bug 8015854
- * @summary JButton text set as html image had additional unwanted padding
- * @run main HtmlButtonImageTest
- */
 public final class HtmlButtonImageTest {
     private static JFrame frame;
     private static Point point;
