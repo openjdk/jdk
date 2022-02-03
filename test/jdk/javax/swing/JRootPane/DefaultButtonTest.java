c652841de2ad9109e7fdd9e9a69eb300a2a3f3f2
Manukumar V S <manukumar.vs@oracle.com>
2022-02-03 16:46:53 +0530

    8280913: Create a regression test for JRootPane.setDefaultButton() method

--- test/jdk/javax/swing/JRootPane/DefaultButtonTest.java
+++ test/jdk/javax/swing/JRootPane/DefaultButtonTest.java
@@ -21,103 +21,88 @@
  * questions.
  */
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.FlowLayout;
+import java.awt.Robot;
 import java.awt.event.KeyEvent;
-import java.lang.reflect.InvocationTargetException;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /*
  * @test
  * @key headful
  * @bug 8280913
- * @summary Check whether the default button is honoured when <Enter> key is
- * pressed when the focus is on the frame.
+ * @summary Check whether the default button is honored when <Enter> key is
+ * pressed while the focus is on the frame.
  * @run main DefaultButtonTest
  */
 public class DefaultButtonTest {
-    static JFrame frame = new JFrame();
-    static volatile boolean buttonPressed = false;
-    boolean testFailed = false;
-    JButton button1;
-    Robot robot;
-    private JPanel panel;
-    private JButton button2;
+    volatile boolean buttonPressed = false;
+    JFrame frame;
 
     public static void main(String[] s) throws Exception {
         DefaultButtonTest test = new DefaultButtonTest();
         try {
             test.runTest();
         } finally {
-            if (frame != null) {
-                frame.dispose();
-                frame = null;
-            }
+            test.disposeFrame();
         }
 
     }
 
-    private static void setLookAndFeel(UIManager.LookAndFeelInfo laf) {
+    private static void setLookAndFeel(String lafName) {
         try {
-            UIManager.setLookAndFeel(laf.getClassName());
+            UIManager.setLookAndFeel(lafName);
         } catch (UnsupportedLookAndFeelException ignored) {
-            System.out.println("Unsupported L&F: " + laf.getClassName());
+            System.out.println("Ignoring Unsupported L&F: " + lafName);
         } catch (ClassNotFoundException | InstantiationException
                 | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private void disposeFrame() {
+        if (frame != null) {
+            frame.dispose();
+            frame = null;
+        }
+    }
+
     private void createUI() {
-        panel = new JPanel();
+        frame = new JFrame();
+        JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
-        button1 = new JButton("butt1");
-        button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                synchronized (this) {
-                    buttonPressed = true;
-                    notifyAll();
-                }
-            }
-        });
+        JButton button1 = new JButton("button1");
+        button1.addActionListener(e -> buttonPressed = true);
         panel.add(button1);
 
-        button2 = new JButton("butt2");
+        JButton button2 = new JButton("button2");
         panel.add(button2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new FlowLayout());
 
         frame.add(panel);
 
-        frame.setSize(200, 300);
+        frame.setSize(200, 200);
         frame.setLocationRelativeTo(null);
         frame.getRootPane().setDefaultButton(button1);
         frame.setVisible(true);
     }
 
-    public void runTest() throws InvocationTargetException, InterruptedException {
-        try {
-            robot = new Robot();
-        } catch (Exception e) {
-            System.err.print("Error creating robot");
-            e.printStackTrace();
-            System.exit(1);
-        }
+    public void runTest() throws Exception {
+        Robot robot = new Robot();
         for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
             buttonPressed = false;
-            System.out.println("Testing L&F: " + laf);
-            try {
-                SwingUtilities.invokeAndWait(new Runnable() {
-                    public void run() {
-                        setLookAndFeel(laf);
-                        createUI();
-                        frame.getRootPane().requestFocus();
-                    }
-                });
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            String lafName = laf.getClassName();
+            System.out.println("Testing L&F: " + lafName);
+            SwingUtilities.invokeAndWait(() -> {
+                setLookAndFeel(lafName);
+                createUI();
+                frame.getRootPane().requestFocus();
+            });
             robot.waitForIdle();
             robot.keyPress(KeyEvent.VK_ENTER);
             robot.delay(100);
@@ -125,18 +110,12 @@ public class DefaultButtonTest {
             robot.waitForIdle();
 
             if (buttonPressed) {
-                System.out.println("Test Passed for laf " + laf);
+                System.out.println("Test Passed for L&F: " + lafName);
             } else {
-                testFailed = true;
-                System.out.println("Test Failed, button not pressed for laf " + laf);
+                throw new RuntimeException("Test Failed, button not pressed for L&F: " + lafName);
             }
-        }
-        if (testFailed) {
-            throw new RuntimeException("Test Failed, button not pressed in one or more LAFs");
-        } else {
-            System.out.println("Test Passed for all supported LAFs ");
+            disposeFrame();
         }
     }
 
 }
-
