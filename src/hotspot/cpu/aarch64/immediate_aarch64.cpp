e4afa9e6f1bc6721ae4cf3ddf2745862e008b058
Daniel D. Daugherty <daniel.daugherty@oracle.com>
2022-02-03 12:14:52 -0500

    kimbarrett and adinn CR changes

--- src/hotspot/cpu/aarch64/immediate_aarch64.cpp
+++ src/hotspot/cpu/aarch64/immediate_aarch64.cpp
@@ -129,10 +129,13 @@ static inline uint32_t uimm(uint32_t val, int hi, int lo)
 
 uint64_t replicate(uint64_t bits, int nbits, int count)
 {
+  assert(count > 0, "must be");
+  assert(nbits > 0, "must be");
+  assert(count * nbits <= 64, "must be");
+
   // Special case nbits == 64 since the shift below with that nbits value
   // would result in undefined behavior.
   if (nbits == 64) {
-    assert(count <= 1, "must be");
     return bits;
   }
 
