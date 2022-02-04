openjdk/jdk

commit 4e2706d2a5bfc70bcc1c75a898ffedf41acb7d55

Clarify comments for overriddenType
 - Removes confusing part of the comment that claims that overriddenType can return TypeParameterElement (this might be an error in translation from JDK 8: ParameterizedType as TypeParameterElement).
 - Clarifies Utils.isSimpleOverride
 - Downgrades doc comments to block comments

src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/WorkArounds.java
@@ -201,11 +201,11 @@ public class WorkArounds {
     // TODO:  need to re-implement this using j.l.m. correctly!, this has
     //        implications on testInterface, the note here is that javac's supertype
     //        does the right thing returning Parameters in scope.
-    /**
-     * Returns the type containing the method that the specified method overrides.
-     * It may be a <code>TypeElement</code> or a <code>TypeParameterElement</code>.
-     * @param method target
-     * @return a type
+    /*
+     * Returns the type (TypeElement) containing the method that both:
+     *   - overrides the specified method
+     *   - has documentation
+     * If no such method can be found, returns null.
      */
     public TypeMirror overriddenType(ExecutableElement method) {
         if (utils.isStatic(method)) {

src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/Utils.java
@@ -772,9 +772,11 @@ public class Utils {
                !((DeclaredType)e.getEnclosingElement().asType()).getTypeArguments().isEmpty();
     }
 
-    /**
-     * Return the type containing the method that this method overrides.
-     * It may be a {@code TypeElement} or a {@code TypeParameterElement}.
+    /*
+     * Returns the type (TypeElement) containing the method that both:
+     *   - overrides the specified method
+     *   - has documentation
+     * If no such method can be found, returns null.
      */
     public TypeMirror overriddenType(ExecutableElement method) {
         return configuration.workArounds.overriddenType(method);
@@ -1470,10 +1472,10 @@ public class Utils {
         return hasBlockTag(e, DocTree.Kind.HIDDEN);
     }
 
-    /**
-     * Returns true if the method has no comments, or a lone &commat;inheritDoc.
-     * @param m a method
-     * @return true if there are no comments, false otherwise
+    /*
+     * Returns true if the specified method does not change its parent specification.
+     * If the specified method is not deprecated and has either no comment or a lone
+     * {@inheritDoc}, this method returns true; otherwise this method returns false.
      */
     public boolean isSimpleOverride(ExecutableElement m) {
         if (!options.summarizeOverriddenMethods() || !isIncluded(m)) {

commit aad7b6c4ab890fe43c164b898b3e7a1b371ace50

Further clarify comments for overriddenType
Also fixes the bugs introduced in an earlier commit (4e2706d2).

src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/WorkArounds.java
@@ -202,10 +202,13 @@ public class WorkArounds {
     //        implications on testInterface, the note here is that javac's supertype
     //        does the right thing returning Parameters in scope.
     /*
-     * Returns the type (TypeElement) containing the method that both:
-     *   - overrides the specified method
-     *   - has documentation
-     * If no such method can be found, returns null.
+     * Returns the closest supertype (TypeElement) that contains a method that
+     * is both:
+     *
+     *   - overridden by the specified method, and
+     *   - is not itself a *simple* override
+     *
+     * If no such type can be found, returns null.
      */
     public TypeMirror overriddenType(ExecutableElement method) {
         if (utils.isStatic(method)) {

src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/Utils.java
@@ -773,10 +773,13 @@ public class Utils {
     }
 
     /*
-     * Returns the type (TypeElement) containing the method that both:
-     *   - overrides the specified method
-     *   - has documentation
-     * If no such method can be found, returns null.
+     * Returns the closest supertype (TypeElement) that contains a method that
+     * is both:
+     *
+     *   - overridden by the specified method, and
+     *   - is not itself a *simple* override
+     *
+     * If no such type can be found, returns null.
      */
     public TypeMirror overriddenType(ExecutableElement method) {
         return configuration.workArounds.overriddenType(method);