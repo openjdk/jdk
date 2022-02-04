04c78bfe4db0f0653aa651e1252d61bab922f854
Tyler Steele <tyler.steele@ibm.com>
2022-02-03 17:17:53 -0700

    Adds Oracle & IBM copyrights as per guidance from IBM legal team.

--- src/hotspot/os/aix/loadlib_aix.cpp
+++ src/hotspot/os/aix/loadlib_aix.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012, 2019 SAP SE. All rights reserved.
+ * Copyright (c) 2022, IBM Corp.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
