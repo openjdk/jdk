3b4ecd782ed5f0a9573aeaa031a813452008e059
Kevin Walls <kevin.walls@oracle.com>
2022-02-07 11:11:35 +0000

    8276680: Malformed Javadoc inline tags in JDK source in javax/management

--- src/java.management/share/classes/javax/management/NumericValueExp.java
+++ src/java.management/share/classes/javax/management/NumericValueExp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -113,7 +113,7 @@ class NumericValueExp extends QueryEval implements ValueExp {
     public NumericValueExp() {
     }
 
-    /** Creates a new NumericValue representing the numeric literal @{code val}.*/
+    /** Creates a new NumericValue representing the numeric literal {@code val}. */
     NumericValueExp(Number val)
     {
       this.val = val;
