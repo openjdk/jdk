76ed2b26d509bd5075b78446743f2b344333ef88
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2022-02-10 13:02:28 -0500

    Better handling of purge action.

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/RunnablePackageTest.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/RunnablePackageTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,6 +41,9 @@ public abstract class RunnablePackageTest {
                     .filter(Predicate.not(Action.INITIALIZE::equals))
                     .filter(Predicate.not(Action.FINALIZE::equals))
                     .collect(Collectors.toList()));
+            if (hasAction(Action.PURGE) && !actionList.contains(Action.PURGE)) {
+                actionList.add(Action.PURGE);
+            }
         }
         actionList.add(Action.FINALIZE);
 
