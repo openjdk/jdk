7aacc083366cfd09387b2b66b5ba3999e1eace3f
Kim Barrett <kim.barrett@oracle.com>
2022-02-10 05:08:14 -0500

    dholmes review

--- src/hotspot/share/utilities/nonblockingQueue.inline.hpp
+++ src/hotspot/share/utilities/nonblockingQueue.inline.hpp
@@ -116,10 +116,13 @@ void NonblockingQueue<T, next_ptr>::append(T& first, T& last) {
     // Successfully extended the queue list from old_tail to first.  No
     // other push/append could have competed with us, because we claimed
     // old_tail for extension.  We won any races with try_pop by changing
-    // away from end-marker.  So we're done.  Note that ABA is possible; a
-    // concurrent try_pop could take old_tail before our update, it gets
-    // recycled and re-added to the end, and then we successfully cmpxchg,
-    // rendering the list in _tail circular.
+    // away from end-marker.  So we're done.
+    //
+    // Note that ABA is possible here.  A concurrent try_pop could take
+    // old_tail before our update of old_tail's next_ptr, old_tail gets
+    // recycled and re-added to the end of this queue, and then we
+    // successfully cmpxchg, making the list in _tail circular.  Callers
+    // must ensure this can't happen.
     return;
   } else {
     // A concurrent try_pop has claimed old_tail, so it is no longer in the
