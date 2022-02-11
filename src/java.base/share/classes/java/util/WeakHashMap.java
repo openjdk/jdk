2a1bf274589622ee8f47e6cb26560b6f1ab20fce
XenoAmess <xenoamess@gmail.com>
2022-02-11 23:41:47 +0800

    9072610: HashMap.putAll can cause redundant space waste

--- src/java.base/share/classes/java/util/WeakHashMap.java
+++ src/java.base/share/classes/java/util/WeakHashMap.java
@@ -240,10 +240,14 @@ public class WeakHashMap<K,V>
 
     /**
      * use for calculate HashMap capacity, using default load factor 0.75
+     *
      * @param size size
      * @return HashMap capacity under default load factor
      */
-    private static int calculateHashMapCapacity(int size){
+    private static int calculateHashMapCapacity(int size) {
+        if (size > (int) (Integer.MAX_VALUE * 0.75)) {
+            return Integer.MAX_VALUE;
+        }
         return size + (size + 2) / 3;
     }
 
