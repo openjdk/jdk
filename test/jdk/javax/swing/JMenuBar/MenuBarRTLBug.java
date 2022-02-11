19b72d40b8bded83e81af1c2041d625046bfe9f7
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-02-11 08:03:47 +0000

    Added L&F iteration

--- test/jdk/javax/swing/JMenuBar/MenuBarRTLBug.java
+++ test/jdk/javax/swing/JMenuBar/MenuBarRTLBug.java
@@ -38,6 +38,8 @@ import javax.swing.event.ChangeListener;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /*
  * @test
@@ -67,95 +69,110 @@ public class MenuBarRTLBug {
 
     static volatile boolean passed = false;
 
+    private static void setLookAndFeel(UIManager.LookAndFeelInfo laf) {
+        try {
+            UIManager.setLookAndFeel(laf.getClassName());
+        } catch (UnsupportedLookAndFeelException ignored) {
+            System.out.println("Unsupported L&F: " + laf.getClassName());
+        } catch (ClassNotFoundException | InstantiationException
+                 | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void main(String[] args) throws  Exception {
 
-        try {
-            SwingUtilities.invokeAndWait(() -> {
-                frame = new JFrame();
-                frame.setLayout(new BorderLayout());
+        for (UIManager.LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+            System.out.println("Testing L&F: " + laf.getClassName());
+            passed = false;
+            SwingUtilities.invokeAndWait(() -> setLookAndFeel(laf));
+            try {
+                SwingUtilities.invokeAndWait(() -> {
+                    frame = new JFrame();
+                    frame.setLayout(new BorderLayout());
 
-                firstMenu = new JMenu("first");
-                a = new JMenuItem("a");
-                b = new JMenuItem("b");
-                firstMenu.add(a);
-                firstMenu.add(b);
+                    firstMenu = new JMenu("first");
+                    a = new JMenuItem("a");
+                    b = new JMenuItem("b");
+                    firstMenu.add(a);
+                    firstMenu.add(b);
 
-                secondMenu = new JMenu("second");
-                c = new JMenuItem("c");
-                d = new JMenuItem("d");
-                secondMenu.add(c);
-                secondMenu.add(d);
-                secondMenu.addMenuListener(new MenuListener() {
-                    @Override
-                    public void menuSelected(MenuEvent e) {
-                        passed = true;
-                    }
-                    @Override
-                    public void menuDeselected(MenuEvent e) {
-                    }
+                    secondMenu = new JMenu("second");
+                    c = new JMenuItem("c");
+                    d = new JMenuItem("d");
+                    secondMenu.add(c);
+                    secondMenu.add(d);
+                    secondMenu.addMenuListener(new MenuListener() {
+                        @Override
+                        public void menuSelected(MenuEvent e) {
+                            passed = true;
+                        }
+                        @Override
+                        public void menuDeselected(MenuEvent e) {
+                        }
 
-                    @Override
-                    public void menuCanceled(MenuEvent e) {
+                        @Override
+                        public void menuCanceled(MenuEvent e) {
+                        }
+                    });
+
+                    thirdMenu = new JMenu("third");
+                    e = new JMenuItem("e");
+                    f = new JMenuItem("f");
+                    thirdMenu.add(e);
+                    thirdMenu.add(f);
+
+                    forthMenu = new JMenu("fourth");
+                    e = new JMenuItem("e");
+                    f = new JMenuItem("f");
+                    forthMenu.add(e);
+                    forthMenu.add(f);
+
+                    fifthMenu = new JMenu("fifth");
+                    e = new JMenuItem("e");
+                    f = new JMenuItem("f");
+                    fifthMenu.add(e);
+                    fifthMenu.add(f);
+
+                    menuBar = new JMenuBar();
+                    menuBar.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+                    menuBar.add(firstMenu);
+                    menuBar.add(secondMenu);
+                    menuBar.add(thirdMenu);
+                    menuBar.add(forthMenu);
+                    menuBar.add(fifthMenu);
+                    frame.setJMenuBar(menuBar);
+
+                    frame.setLocationRelativeTo(null);
+                    frame.pack();
+                    frame.setVisible(true);
+                });
+                Robot robot = new Robot();
+                robot.setAutoDelay(100);
+                robot.waitForIdle();
+                robot.delay(1000);
+                SwingUtilities.invokeAndWait(() -> {
+                    p = thirdMenu.getLocationOnScreen();
+                    width = thirdMenu.getWidth();
+                    height = thirdMenu.getHeight();
+                });
+                robot.mouseMove(p.x + width / 2, p.y + height / 2);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                robot.delay(1000);
+                robot.keyPress(KeyEvent.VK_RIGHT);
+                robot.keyRelease(KeyEvent.VK_RIGHT);
+                robot.delay(1000);
+                if (!passed) {
+                    throw new RuntimeException("Arrow traversal order not correct in RTL orientation");
+                }
+            } finally {
+                SwingUtilities.invokeAndWait(() -> {
+                    if (frame != null) {
+                        frame.dispose();
                     }
                 });
-
-
-                thirdMenu = new JMenu("third");
-                e = new JMenuItem("e");
-                f = new JMenuItem("f");
-                thirdMenu.add(e);
-                thirdMenu.add(f);
-
-                forthMenu = new JMenu("fourth");
-                e = new JMenuItem("e");
-                f = new JMenuItem("f");
-                forthMenu.add(e);
-                forthMenu.add(f);
-
-                fifthMenu = new JMenu("fifth");
-                e = new JMenuItem("e");
-                f = new JMenuItem("f");
-                fifthMenu.add(e);
-                fifthMenu.add(f);
-
-                menuBar = new JMenuBar();
-                menuBar.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
-                menuBar.add(firstMenu);
-                menuBar.add(secondMenu);
-                menuBar.add(thirdMenu);
-                menuBar.add(forthMenu);
-                menuBar.add(fifthMenu);
-                frame.setJMenuBar(menuBar);
-
-                frame.setLocationRelativeTo(null);
-                frame.pack();
-                frame.setVisible(true);
-            });
-            Robot robot = new Robot();
-            robot.setAutoDelay(100);
-            robot.waitForIdle();
-            robot.delay(1000);
-            SwingUtilities.invokeAndWait(() -> {
-                p = thirdMenu.getLocationOnScreen();
-                width = thirdMenu.getWidth();
-                height = thirdMenu.getHeight();
-            });
-            robot.mouseMove(p.x + width / 2, p.y + height / 2);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-            robot.delay(1000);
-            robot.keyPress(KeyEvent.VK_RIGHT);
-            robot.keyRelease(KeyEvent.VK_RIGHT);
-            robot.delay(1000);
-            if (!passed) {
-                throw new RuntimeException("Arrow traversal order not correct in RTL orientation");
             }
-        } finally {
-            SwingUtilities.invokeAndWait(() -> {
-                if (frame != null) {
-                    frame.dispose();
-                }
-            });
-        }
+	}
     }
 }
