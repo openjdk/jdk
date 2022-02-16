847a99b53da6b2c82f7cd5f8634aa7bbae8f445e
Aleksey Shipilev <shade@openjdk.org>
2022-02-16 20:08:27 +0000

    8281822: Test failures on non-DTrace builds due to incomplete DTrace* flags handling
    Reviewed-by: dholmes, kvn

--- test/lib/jdk/test/whitebox/WhiteBox.java
+++ test/lib/jdk/test/whitebox/WhiteBox.java
@@ -634,6 +634,7 @@ public class WhiteBox {
   public native boolean isSharedInternedString(String s);
   public native boolean isCDSIncluded();
   public native boolean isJFRIncluded();
+  public native boolean isDTraceIncluded();
   public native boolean canWriteJavaHeapArchive();
   public native Object  getResolvedReferences(Class<?> c);
   public native void    linkClass(Class<?> c);
