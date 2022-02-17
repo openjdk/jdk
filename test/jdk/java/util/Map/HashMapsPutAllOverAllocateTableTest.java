2998571af915d6e12c4a7a7f9d0e7e9e8647989b
XenoAmess <xenoamess@gmail.com>
2022-02-17 23:25:25 +0800

    fix license year in test

--- test/jdk/java/util/Map/HashMapsPutAllOverAllocateTableTest.java
+++ test/jdk/java/util/Map/HashMapsPutAllOverAllocateTableTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,7 +37,6 @@ import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 
