01cc39cf1b6917c26699005d6d008403584603d1
Claes Redestad <claes.redestad@oracle.com>
2022-02-17 15:26:13 +0100

    Enhance StringDecode/Encode micros

--- test/micro/org/openjdk/bench/java/lang/StringEncode.java
+++ test/micro/org/openjdk/bench/java/lang/StringEncode.java
@@ -36,91 +36,132 @@ import java.util.concurrent.TimeUnit;
 @State(Scope.Thread)
 public class StringEncode {
 
-    @Param({"US-ASCII", "ISO-8859-1", "UTF-8", "MS932", "ISO-8859-6"})
+    @Param({"UTF-8"})
     private String charsetName;
     private Charset charset;
     private String asciiString;
+    private String longAsciiString;
     private String utf16String;
-    private String longUtf16String;
+    private String longUtf16EndString;
     private String longUtf16StartString;
+    private String latin1String;
+    private String longLatin1EndString;
+    private String longLatin1StartString;
+
+    private static final String LOREM = """
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sem eu
+             urna egestas placerat. Etiam finibus ipsum nulla, non mattis dolor cursus a.
+             Nulla nec nisl consectetur, lacinia neque id, accumsan ante. Curabitur et
+             sapien in magna porta ultricies. Sed vel pellentesque nibh. Pellentesque dictum
+             dignissim diam eu ultricies. Class aptent taciti sociosqu ad litora torquent
+             per conubia nostra, per inceptos himenaeos. Suspendisse erat diam, fringilla
+             sed massa sed, posuere viverra orci. Suspendisse tempor libero non gravida
+             efficitur. Vivamus lacinia risus non orci viverra, at consectetur odio laoreet.
+             Suspendisse potenti.""";
+    private static final String UTF16_STRING = "\uFF11".repeat(31);
+    private static final String LATIN1_STRING = "\u00B6".repeat(31);
 
     @Setup
     public void setup() {
         charset = Charset.forName(charsetName);
-        asciiString = "ascii string";
+        asciiString = LOREM.substring(0, 32);
+        longAsciiString = LOREM.repeat(200);
         utf16String = "UTF-\uFF11\uFF16 string";
-        longUtf16String = """
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sem eu
-                 urna egestas placerat. Etiam finibus ipsum nulla, non mattis dolor cursus a.
-                 Nulla nec nisl consectetur, lacinia neque id, accumsan ante. Curabitur et
-                 sapien in magna porta ultricies. Sed vel pellentesque nibh. Pellentesque dictum
-                 dignissim diam eu ultricies. Class aptent taciti sociosqu ad litora torquent
-                 per conubia nostra, per inceptos himenaeos. Suspendisse erat diam, fringilla
-                 sed massa sed, posuere viverra orci. Suspendisse tempor libero non gravida
-                 efficitur. Vivamus lacinia risus non orci viverra, at consectetur odio laoreet.
-                 Suspendisse potenti.
-
-                 Phasellus vel nisi iaculis, accumsan quam sed, bibendum eros. Sed venenatis
-                 nulla tortor, et eleifend urna sodales id. Nullam tempus ac metus sit amet
-                 sollicitudin. Nam sed ex diam. Praesent vitae eros et neque condimentum
-                 consectetur eget non tortor. Praesent bibendum vel felis nec dignissim.
-                 Maecenas a enim diam. Suspendisse quis ligula at nisi accumsan lacinia id
-                 hendrerit sapien. Donec aliquam mattis lectus eu ultrices. Duis eu nisl
-                 euismod, blandit mauris vel, placerat urna. Etiam malesuada enim purus,
-                 tristique mollis odio blandit quis. Vivamus posuere.
-                 \uFF11
-                """;
-        longUtf16StartString = """
-                 \uFF11
-                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac sem eu
-                 urna egestas placerat. Etiam finibus ipsum nulla, non mattis dolor cursus a.
-                 Nulla nec nisl consectetur, lacinia neque id, accumsan ante. Curabitur et
-                 sapien in magna porta ultricies. Sed vel pellentesque nibh. Pellentesque dictum
-                 dignissim diam eu ultricies. Class aptent taciti sociosqu ad litora torquent
-                 per conubia nostra, per inceptos himenaeos. Suspendisse erat diam, fringilla
-                 sed massa sed, posuere viverra orci. Suspendisse tempor libero non gravida
-                 efficitur. Vivamus lacinia risus non orci viverra, at consectetur odio laoreet.
-                 Suspendisse potenti.
-
-                 Phasellus vel nisi iaculis, accumsan quam sed, bibendum eros. Sed venenatis
-                 nulla tortor, et eleifend urna sodales id. Nullam tempus ac metus sit amet
-                 sollicitudin. Nam sed ex diam. Praesent vitae eros et neque condimentum
-                 consectetur eget non tortor. Praesent bibendum vel felis nec dignissim.
-                 Maecenas a enim diam. Suspendisse quis ligula at nisi accumsan lacinia id
-                 hendrerit sapien. Donec aliquam mattis lectus eu ultrices. Duis eu nisl
-                 euismod, blandit mauris vel, placerat urna. Etiam malesuada enim purus,
-                 tristique mollis odio blandit quis. Vivamus posuere.
-                """;
+        longUtf16EndString = LOREM.repeat(2).concat(UTF16_STRING);
+        longUtf16StartString = UTF16_STRING.concat(LOREM.repeat(2));
+        latin1String = LATIN1_STRING;
+        longLatin1EndString = LOREM.repeat(2).concat(LATIN1_STRING);
+        longLatin1StartString = LATIN1_STRING.concat(LOREM.repeat(2));
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public byte[] encodeAsciiCharsetName() throws Exception {
-        return asciiString.getBytes(charset);
+        return asciiString.getBytes(charsetName);
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public byte[] encodeAscii() throws Exception {
         return asciiString.getBytes(charset);
     }
 
     @Benchmark
-    public void encodeMix(Blackhole bh) throws Exception {
-        bh.consume(asciiString.getBytes(charset));
-        bh.consume(utf16String.getBytes(charset));
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public byte[] encodeAsciiLong() throws Exception {
+        return longAsciiString.getBytes(charset);
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public byte[] encodeUTF16LongEnd() throws Exception {
-        return longUtf16String.getBytes(charset);
+        return longUtf16EndString.getBytes(charset);
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public byte[] encodeUTF16LongStart() throws Exception {
         return longUtf16StartString.getBytes(charset);
     }
 
     @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public byte[] encodeUTF16() throws Exception {
         return utf16String.getBytes(charset);
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void encodeUTF16Mixed(Blackhole bh) throws Exception {
+        bh.consume(utf16String.getBytes(charset));
+        bh.consume(longUtf16StartString.getBytes(charset));
+        bh.consume(longUtf16EndString.getBytes(charset));
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public byte[] encodeLatin1Short() throws Exception {
+        return latin1String.getBytes(charset);
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public byte[] encodeLatin1StartLong() throws Exception {
+        return longLatin1StartString.getBytes(charset);
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public byte[] encodeLatin1EndLong() throws Exception {
+        return longLatin1EndString.getBytes(charset);
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void encodeLatin1Mixed(Blackhole bh) throws Exception {
+        bh.consume(longLatin1EndString.getBytes(charset));
+        bh.consume(longLatin1StartString.getBytes(charset));
+        bh.consume(latin1String.getBytes(charset));
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void encodeAllMixed(Blackhole bh) throws Exception {
+        bh.consume(utf16String.getBytes(charset));
+        bh.consume(longUtf16StartString.getBytes(charset));
+        bh.consume(longUtf16EndString.getBytes(charset));
+        bh.consume(longLatin1EndString.getBytes(charset));
+        bh.consume(longLatin1StartString.getBytes(charset));
+        bh.consume(latin1String.getBytes(charset));
+        bh.consume(asciiString.getBytes(charset));
+        bh.consume(longAsciiString.getBytes(charset));
+    }
+
+    @Benchmark
+    @CompilerControl(CompilerControl.Mode.DONT_INLINE)
+    public void encodeShortMixed(Blackhole bh) throws Exception {
+        bh.consume(utf16String.getBytes(charset));
+        bh.consume(latin1String.getBytes(charset));
+        bh.consume(asciiString.getBytes(charset));
     }
 }
