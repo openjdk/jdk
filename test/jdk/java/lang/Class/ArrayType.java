b2a7d90094052525ba4ac98ff5535bded5e0d576
Joe Darcy <joe.darcy@oracle.com>
2022-02-16 21:06:08 -0800

    Respond to review feedback.

--- test/jdk/java/lang/Class/ArrayType.java
+++ test/jdk/java/lang/Class/ArrayType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,8 +39,7 @@ public class ArrayType {
 
     private static void expectException(Supplier<Class<?>> arrayTypeArg) {
         try {
-            Class<?> clazz = arrayTypeArg.get();
-            Class<?> arrayClazz = clazz.arrayType();
+            Class<?> arrayClazz = arrayTypeArg.get().arrayType();
             throw new RuntimeException("Expected exception not thrown: " +
                                        arrayClazz);
         } catch (UnsupportedOperationException uoe) {
