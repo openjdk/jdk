2a8651b8852334f7449e9cfb07d2f6c86244cad3
Tyler Steele <tyler.steele@ibm.com>
2022-02-18 12:45:55 -0700

    Makes sm changes requested by Naoto

--- test/jdk/java/lang/System/FileEncodingTest.java
+++ test/jdk/java/lang/System/FileEncodingTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8260265
+ * @bug 8260265 8282042
  * @summary Test file.encoding system property
  * @library /test/lib
  * @build jdk.test.lib.process.*
