57b1b13a9e3ff52db97845e533b77010d6ae9f55
Jatin Bhateja <jbhateja@openjdk.org>
2022-03-01 19:57:14 +0530

    8279508: Removing +LogCompilation flag.

--- test/jdk/java/lang/Math/RoundTests.java
+++ test/jdk/java/lang/Math/RoundTests.java
@@ -25,7 +25,7 @@
  * @test
  * @bug 6430675 8010430
  * @summary Check for correct implementation of {Math, StrictMath}.round
- * @run main/othervm -XX:+LogCompilation -XX:Tier3CompileThreshold=100 -XX:CompileThresholdScaling=0.01 -XX:+TieredCompilation RoundTests
+ * @run main/othervm -XX:Tier3CompileThreshold=100 -XX:CompileThresholdScaling=0.01 -XX:+TieredCompilation RoundTests
  */
 public class RoundTests {
     public static void main(String... args) {
