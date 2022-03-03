1f4aec9b07813e65881692e9b7090c6c083ba7a4
rwestrel <rwestrel@redhat.com>
2022-03-03 15:29:30 +0100

    more

--- src/hotspot/share/opto/ifg.cpp
+++ src/hotspot/share/opto/ifg.cpp
@@ -877,6 +877,9 @@ uint PhaseChaitin::build_ifg_physical(ResourceArea* a, const Block_List &blocks,
       if (lid) {
         LRG& lrg = lrgs(lid);
         if (lrg._region < region) {
+          if (!n->is_Phi()) {
+            add_input_to_liveout(block, n, &liveout, cost, int_pressure, float_pressure);
+          }
           continue;
         }
 
