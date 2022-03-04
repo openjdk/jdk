8e2e59bb0ad5e9e52e1070b9bb274fe7322fd68d
Ludovic Henry <git@ludovic.dev>
2022-03-04 17:39:45 +0000

    Add UTF-16 benchmarks

--- test/micro/org/openjdk/bench/java/lang/StringHashCode.java
+++ test/micro/org/openjdk/bench/java/lang/StringHashCode.java
@@ -85,85 +85,148 @@ public class StringHashCode {
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
     @State(Scope.Thread)
     public static class Algorithm {
-        private final static byte[] alphabet;
 
-        static {
-            try {
-                alphabet = "abcdefghijklmnopqrstuvwxyz".getBytes("US-ASCII");
-            } catch (UnsupportedEncodingException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        private final static String alphabet = "abcdefghijklmnopqrstuvwxyz";
 
         @Param({"0", "1", "10", "100", "1000", "10000"})
         private int size;
 
-        private byte[] value;
+        private byte[] latin1;
+        private byte[] utf16;
 
         @Setup
         public void setup() throws UnsupportedEncodingException {
-            value = new byte[size];
+            char[] str = new char[size];
             for (int i = 0; i < size; i++) {
-                value[i] = alphabet[ThreadLocalRandom.current().nextInt(alphabet.length)];
+                str[i] = alphabet.charAt(ThreadLocalRandom.current().nextInt(alphabet.length()));
             }
+            latin1 = new String(str).getBytes("US-ASCII");
+            utf16 = new String(str).getBytes("UTF-16");
         }
 
         @Benchmark
-        public int scalar() {
+        public int scalarLatin1() {
             int h = 0;
-            int i = 0;
-            for (; i < value.length; i++) {
-                h = 31 * h + (value[i] & 0xff);
+            int i = 0, len = latin1.length;
+            for (; i < len; i++) {
+                h = 31 * h + (latin1[i] & 0xff);
             }
             return h;
         }
 
         @Benchmark
-        public int scalarUnrolled8() {
+        public int scalarLatin1Unrolled8() {
             int h = 0;
-            int i = 0;
-            for (; i < (value.length & ~(8 - 1)); i += 8) {
+            int i = 0, len = latin1.length;
+            for (; i < (len & ~(8 - 1)); i += 8) {
                 h = -1807454463 * h                   +
-                     1742810335 * (value[i+0] & 0xff) +
-                      887503681 * (value[i+1] & 0xff) +
-                       28629151 * (value[i+2] & 0xff) +
-                         923521 * (value[i+3] & 0xff) +
-                          29791 * (value[i+4] & 0xff) +
-                            961 * (value[i+5] & 0xff) +
-                             31 * (value[i+6] & 0xff) +
-                              1 * (value[i+7] & 0xff);
+                     1742810335 * (latin1[i+0] & 0xff) +
+                      887503681 * (latin1[i+1] & 0xff) +
+                       28629151 * (latin1[i+2] & 0xff) +
+                         923521 * (latin1[i+3] & 0xff) +
+                          29791 * (latin1[i+4] & 0xff) +
+                            961 * (latin1[i+5] & 0xff) +
+                             31 * (latin1[i+6] & 0xff) +
+                              1 * (latin1[i+7] & 0xff);
             }
-            for (; i < value.length; i++) {
-                h = 31 * h + (value[i] & 0xff);
+            for (; i < len; i++) {
+                h = 31 * h + (latin1[i] & 0xff);
             }
             return h;
         }
 
         @Benchmark
-        public int scalarUnrolled16() {
+        public int scalarLatin1Unrolled16() {
             int h = 0;
-            int i = 0;
-            for (; i < (value.length & ~(16 - 1)); i += 16) {
+            int i = 0, len = latin1.length;
+            for (; i < (len & ~(16 - 1)); i += 16) {
                 h =  1353309697 * h                    +
-                     -510534177 * (value[i+ 0] & 0xff) +
-                     1507551809 * (value[i+ 1] & 0xff) +
-                     -505558625 * (value[i+ 2] & 0xff) +
-                     -293403007 * (value[i+ 3] & 0xff) +
-                      129082719 * (value[i+ 4] & 0xff) +
-                    -1796951359 * (value[i+ 5] & 0xff) +
-                     -196513505 * (value[i+ 6] & 0xff) +
-                    -1807454463 * (value[i+ 7] & 0xff) +
-                     1742810335 * (value[i+ 8] & 0xff) +
-                      887503681 * (value[i+ 9] & 0xff) +
-                       28629151 * (value[i+10] & 0xff) +
-                         923521 * (value[i+11] & 0xff) +
-                          29791 * (value[i+12] & 0xff) +
-                            961 * (value[i+13] & 0xff) +
-                             31 * (value[i+14] & 0xff) +
-                              1 * (value[i+15] & 0xff);
+                     -510534177 * (latin1[i+ 0] & 0xff) +
+                     1507551809 * (latin1[i+ 1] & 0xff) +
+                     -505558625 * (latin1[i+ 2] & 0xff) +
+                     -293403007 * (latin1[i+ 3] & 0xff) +
+                      129082719 * (latin1[i+ 4] & 0xff) +
+                    -1796951359 * (latin1[i+ 5] & 0xff) +
+                     -196513505 * (latin1[i+ 6] & 0xff) +
+                    -1807454463 * (latin1[i+ 7] & 0xff) +
+                     1742810335 * (latin1[i+ 8] & 0xff) +
+                      887503681 * (latin1[i+ 9] & 0xff) +
+                       28629151 * (latin1[i+10] & 0xff) +
+                         923521 * (latin1[i+11] & 0xff) +
+                          29791 * (latin1[i+12] & 0xff) +
+                            961 * (latin1[i+13] & 0xff) +
+                             31 * (latin1[i+14] & 0xff) +
+                              1 * (latin1[i+15] & 0xff);
             }
-            for (; i < value.length; i++) {
-                h = 31 * h + (value[i] & 0xff);
+            for (; i < len; i++) {
+                h = 31 * h + (latin1[i] & 0xff);
+            }
+            return h;
+        }
+
+        char getCharUTF16(byte[] value, int index) {
+            index <<= 1;
+            // assuming little endian
+            return (char)(((value[index++] & 0xff) << 0) |
+                          ((value[index]   & 0xff) << 8));
+        }
+
+        @Benchmark
+        public int scalarUTF16() {
+            int h = 0;
+            int i = 0, len = utf16.length / 2;
+            for (; i < len; i++) {
+                h = 31 * h + getCharUTF16(utf16, i);
+            }
+            return h;
+        }
+
+        @Benchmark
+        public int scalarUTF16Unrolled8() {
+            int h = 0;
+            int i = 0, len = utf16.length / 2;
+            for (; i < (len & ~(8 - 1)); i += 8) {
+                h = -1807454463 * h                   +
+                     1742810335 * getCharUTF16(utf16, i+0) +
+                      887503681 * getCharUTF16(utf16, i+1) +
+                       28629151 * getCharUTF16(utf16, i+2) +
+                         923521 * getCharUTF16(utf16, i+3) +
+                          29791 * getCharUTF16(utf16, i+4) +
+                            961 * getCharUTF16(utf16, i+5) +
+                             31 * getCharUTF16(utf16, i+6) +
+                              1 * getCharUTF16(utf16, i+7);
+            }
+            for (; i < len; i++) {
+                h = 31 * h + getCharUTF16(utf16, i);
+            }
+            return h;
+        }
+
+        @Benchmark
+        public int scalarUTF16Unrolled16() {
+            int h = 0;
+            int i = 0, len = utf16.length / 2;
+            for (; i < (len & ~(16 - 1)); i += 16) {
+                h =  1353309697 * h                    +
+                     -510534177 * getCharUTF16(utf16, i+0) +
+                     1507551809 * getCharUTF16(utf16, i+1) +
+                     -505558625 * getCharUTF16(utf16, i+2) +
+                     -293403007 * getCharUTF16(utf16, i+3) +
+                      129082719 * getCharUTF16(utf16, i+4) +
+                    -1796951359 * getCharUTF16(utf16, i+5) +
+                     -196513505 * getCharUTF16(utf16, i+6) +
+                    -1807454463 * getCharUTF16(utf16, i+7) +
+                     1742810335 * getCharUTF16(utf16, i+8) +
+                      887503681 * getCharUTF16(utf16, i+9) +
+                       28629151 * getCharUTF16(utf16, i+10) +
+                         923521 * getCharUTF16(utf16, i+11) +
+                          29791 * getCharUTF16(utf16, i+12) +
+                            961 * getCharUTF16(utf16, i+13) +
+                             31 * getCharUTF16(utf16, i+14) +
+                              1 * getCharUTF16(utf16, i+15);
+            }
+            for (; i < len; i++) {
+                h = 31 * h + getCharUTF16(utf16, i);
             }
             return h;
         }
