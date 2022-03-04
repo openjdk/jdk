c51b82d7101b2090e9101794128f1e88bd8bf276
Alex Menkov <69548902+alexmenkov@users.noreply.github.com>
2022-03-04 20:07:02 +0300

    Reworked the test

--- test/hotspot/jtreg/serviceability/jvmti/RedefineClasses/RedefineGenericSignatureTest.java
+++ test/hotspot/jtreg/serviceability/jvmti/RedefineClasses/RedefineGenericSignatureTest.java
@@ -24,32 +24,34 @@
 /*
  * @test
  * @bug 8282241
- * @summary Verifies class retransformation correctly updates generic_signature and source_file_name attributes
- *
+ * @summary Verifies class redefinition correctly updates generic_signature and source_file_name attributes
+ * @requires vm.jvmti
+ * @modules java.base/jdk.internal.org.objectweb.asm
+ *          java.instrument
  * @library /test/lib
- * @run compile -g RetransformGenericSignatureTest.java
- * @run shell MakeJAR.sh retransformAgent
- *
- * @run main/othervm -javaagent:retransformAgent.jar --add-opens=java.base/java.lang=ALL-UNNAMED RetransformGenericSignatureTest
+ * @run compile -g RedefineGenericSignatureTest.java
+ * @run main RedefineClassHelper
+ * @run main/othervm -javaagent:redefineagent.jar --add-opens=java.base/java.lang=ALL-UNNAMED RedefineGenericSignatureTest
  */
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.lang.instrument.ClassFileTransformer;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.ProtectionDomain;
 import java.util.List;
 
+import jdk.internal.org.objectweb.asm.ClassReader;
+import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.ClassWriter;
+import jdk.internal.org.objectweb.asm.Opcodes;
+import jdk.test.lib.Asserts;
 import jdk.test.lib.JDKToolLauncher;
+import jdk.test.lib.compiler.InMemoryJavaCompiler;
 import jdk.test.lib.process.ProcessTools;
 import jdk.test.lib.process.OutputAnalyzer;
-import jdk.test.lib.util.ClassTransformer;
-
 
 class GenericSignatureTester {
     public GenericSignatureTarget<List<String>> method1() {
@@ -57,42 +59,33 @@ class GenericSignatureTester {
     }
 }
 
-// See ClassTransformer.transform(int) comment for @1 tag explanations.
-// @1 uncomment class GenericSignatureTarget<T> {
-class GenericSignatureTarget<T extends List<?>>  { // @1 commentout
-    public GenericSignatureTarget<T> method1(GenericSignatureTarget<?> a) {
-        // the following line changes CP and this causes mapping during CP merge
-        // @1 uncomment System.out.println(a);
-        return null;
-    }
-
-    public static void throwException() {
-        throw new RuntimeException();
-    }
+class GenericSignatureTarget<T extends List<?>>  {
+    public GenericSignatureTarget<T> foo() { return null; }
+    public static void throwException() { throw new RuntimeException(); }
 }
 
-public class RetransformGenericSignatureTest extends ATransformerManagementTestCase {
-
-    public RetransformGenericSignatureTest() {
-        super("RetransformGenericSignatureTest");
-    }
+public class RedefineGenericSignatureTest {
+    private static final String newTargetClassSource =
+            "class GenericSignatureTarget<T> {\n" +
+            "    public GenericSignatureTarget<T> foo() { return null; }\n" +
+            "    public static void throwException() { throw new RuntimeException(); }\n" +
+            "}\n";
 
     public static void main (String[] args) throws Throwable {
-        ATestCaseScaffold test = new RetransformGenericSignatureTest();
+        RedefineGenericSignatureTest test = new RedefineGenericSignatureTest();
         test.runTest();
     }
 
-    private final Transformer transformer = new Transformer();
-    private final static String sourceFileName = "RetransformGenericSignatureTest.java";
-    private final static String sourceFileNameNew = "RetransformGenericSignatureTestNew.java";
+    private final static String sourceFileName = "RedefineGenericSignatureTest.java";
+    private final static String sourceFileNameNew = "RedefineGenericSignatureTestNew.java";
     // expected signature of GenericSignatureTester.method1 return type
     private final static String expectedRetType = "GenericSignatureTarget<java.util.List<java.lang.String>>";
     // expected generic signature of the original GenericSignatureTarget
     private final static String expectedSigOld = "<T::Ljava/util/List<*>;>Ljava/lang/Object;";
-    // expected generic signature of the retransformed GenericSignatureTarget
+    // expected generic signature of the redefined GenericSignatureTarget
     private final static String expectedSigNew = "<T:Ljava/lang/Object;>Ljava/lang/Object;";
 
-    private void log(Object o) {
+    private static void log(Object o) {
         System.out.println(o);
     }
 
@@ -117,18 +110,6 @@ public class RetransformGenericSignatureTest extends ATransformerManagementTestC
             return ex.getStackTrace()[0].getFileName();
         }
         return "Cannot get source file name";
-    }
-
-    // Retransforms the class using provided class bytes;
-    // Returns class bytes passed to the transformer.
-    private byte[] retransform(Class cls, byte[] classBytes) throws Throwable {
-        transformer.targetClassName = cls.getName();
-        transformer.seenClassBytes = null;
-        transformer.newClassBytes = classBytes;
-        fInst.retransformClasses(cls);
-        assertTrue(transformer.targetClassName + " was not seen by transform()",
-                transformer.seenClassBytes != null);
-        return transformer.seenClassBytes;
     }
 
     // Prints dissassembled class bytes.
@@ -158,33 +139,45 @@ public class RetransformGenericSignatureTest extends ATransformerManagementTestC
         Files.deleteIfExists(f.toPath());
     }
 
-    private void retransformTargetClass() throws Throwable {
-        Class targetClass = GenericSignatureTarget.class;
-        log("Retransforming " + targetClass.getName() + " class");
-        String origSource = Files.readString(Paths.get(System.getProperty("test.src")).resolve(sourceFileName));
-        // replace main class name to avoid compilation errors
-        String newSource = origSource.replaceAll(
-                "RetransformGenericSignatureTest", "RetransformGenericSignatureTestNew");
+    private byte[] getNewClassBytes() {
+        byte[] bytecode = InMemoryJavaCompiler.compile(GenericSignatureTarget.class.getName(), newTargetClassSource, "-g");
 
-        String transformedClassFile = ClassTransformer.fromString(newSource)
-                .setFileName(sourceFileNameNew)
-                .transform(1, targetClass.getName(), "-g");
-        byte[] classBytes = Files.readAllBytes(Paths.get(transformedClassFile));
-        byte[] oldClassBytes = retransform(targetClass, classBytes);
+        ClassWriter cw = new ClassWriter(0);
+        ClassReader cr = new ClassReader(bytecode);
+        cr.accept(new ClassVisitor(Opcodes.ASM7, cw) {
+            private boolean sourceSet = false;
+            @Override
+            public void visitSource(String source, String debug) {
+                sourceSet = true;
+                log("Changing source: \"" + source + "\" -> \"" + sourceFileNameNew + "\"");
+                super.visitSource(sourceFileNameNew, debug);
+            }
 
-        printDisassembled("Old " + targetClass.getName(), targetClass, oldClassBytes);
-        printDisassembled("New " + targetClass.getName(), targetClass, classBytes);
+            @Override
+            public void visitEnd() {
+                if (!sourceSet) {
+                    log("Set source: \"" + sourceFileNameNew + "\"");
+                    super.visitSource(sourceFileNameNew, null);
+                }
+                super.visitEnd();
+            }
+        }, 0);
+        return cw.toByteArray();
     }
 
-    protected final void doRunTest() throws Throwable {
-        beVerbose();
+    private void runTest() throws Throwable {
+        Class targetClass = GenericSignatureTarget.class;
 
         String oldSig = getTargetGenSig();
         log("old target class sig: \"" + oldSig + "\"");
 
-        addTransformerToManager(fInst, transformer, true);
+        byte[] oldClassBytes = targetClass.getResourceAsStream(targetClass.getName() + ".class").readAllBytes();
+        printDisassembled("Old " + targetClass.getName(), targetClass, oldClassBytes);
 
-        retransformTargetClass();
+        log("Redefining " + targetClass.getName() + " class");
+        byte[] newClassBytes = getNewClassBytes();
+        printDisassembled("New " + targetClass.getName(), targetClass, newClassBytes);
+        RedefineClassHelper.redefineClass(targetClass, newClassBytes);
 
         String newSig = getTargetGenSig();
         log("new target class sig: \"" + newSig + "\"");
@@ -195,39 +188,9 @@ public class RetransformGenericSignatureTest extends ATransformerManagementTestC
         String newSrcFileName = getTargetSourceFilename();
         log("new source file name: \"" + newSrcFileName + "\"");
 
-        assertEquals("wrong old generic signature", expectedSigOld, oldSig);
-        assertEquals("wrong new generic signature", expectedSigNew, newSig);
-        assertEquals("wrong ret type", expectedRetType, newRetType);
-        assertEquals("wrong new source file name", sourceFileNameNew, newSrcFileName);
-    }
-
-    public class Transformer implements ClassFileTransformer {
-        public Transformer() {
-        }
-
-        public String toString() {
-            return Transformer.this.getClass().getName();
-        }
-
-        String targetClassName;
-        byte[] seenClassBytes;
-        byte[] newClassBytes;
-
-        public byte[] transform(ClassLoader loader, String className,
-            Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) {
-
-            if (className.equals(targetClassName)) {
-                log(this + ".transform() sees '" + className
-                        + "' of " + classfileBuffer.length + " bytes.");
-                seenClassBytes = classfileBuffer;
-                if (newClassBytes != null) {
-                    log(this + ".transform() sets new classbytes for '" + className
-                            + "' of " + newClassBytes.length + " bytes.");
-                }
-                return newClassBytes;
-            }
-
-            return null;
-        }
+        Asserts.assertStringsEqual(expectedSigOld, oldSig, "wrong old generic signature");
+        Asserts.assertStringsEqual(expectedSigNew, newSig, "wrong new generic signature");
+        Asserts.assertStringsEqual(expectedRetType, newRetType, "wrong ret type");
+        Asserts.assertStringsEqual(sourceFileNameNew, newSrcFileName, "wrong new source file name");
     }
 }
