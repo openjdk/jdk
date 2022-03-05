afc1d0161a23f8c837c831a7ffef13250d83b10b
Joe Darcy <joe.darcy@oracle.com>
2022-03-04 18:46:43 -0800

    JDK-8282696: Add constructors take a cause to InvalidObjectException and InvalidClassException

--- src/java.base/share/classes/java/io/InvalidClassException.java
+++ src/java.base/share/classes/java/io/InvalidClassException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -70,6 +70,30 @@ public class InvalidClassException extends ObjectStreamException {
      */
     public InvalidClassException(String cname, String reason) {
         super(reason);
+        classname = cname;
+    }
+
+    /**
+     * Report an InvalidClassException for the reason and cause specified.
+     *
+     * @param reason  String describing the reason for the exception.
+     * @param cause the cause
+     * @since 19
+     */
+    public InvalidClassException(String reason, Throwable cause) {
+        super(reason, cause);
+    }
+
+    /**
+     * Report an InvalidClassException for the reason and cause specified.
+     *
+     * @param cname   a String naming the invalid class.
+     * @param reason  String describing the reason for the exception.
+     * @param cause the cause
+     * @since 19
+     */
+    public InvalidClassException(String cname, String reason, Throwable cause) {
+        super(reason, cause);
         classname = cname;
     }
 
