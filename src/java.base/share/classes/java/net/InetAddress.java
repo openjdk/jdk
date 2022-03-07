682633d7d7d35ae18e5b9c02738eb2de84ee83e7
Aleksei Efimov <aleksej.efimov@oracle.com>
2022-03-07 19:29:34 +0000

    8282536: java.net.InetAddress should be a sealed class

--- src/java.base/share/classes/java/net/InetAddress.java
+++ src/java.base/share/classes/java/net/InetAddress.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -223,7 +223,7 @@ import static java.net.spi.InetAddressResolver.LookupPolicy.IPV6_FIRST;
  * @see     java.net.InetAddress#getLocalHost()
  * @since 1.0
  */
-public class InetAddress implements java.io.Serializable {
+public sealed class InetAddress implements java.io.Serializable permits Inet4Address, Inet6Address {
 
     /**
      * Specify the address family: Internet Protocol, Version 4
