da86369272de1110d090d2cdb3d92dbd3145c3ff
Joe Darcy <joe.darcy@oracle.com>
2022-03-07 09:49:13 -0800

    Improve test.

--- test/jdk/java/net/SocketException/TestSocketExceptionCtor.java
+++ test/jdk/java/net/SocketException/TestSocketExceptionCtor.java
@@ -24,20 +24,28 @@
 /*
  * @test
  * @bug 8282686
- * @summary Verify cause handling of SocketException
+ * @summary Verify cause and message handling of SocketException
  */
 import java.net.SocketException;
+import java.util.Objects;
 
 public class TestSocketExceptionCtor {
     public static void main(String... args) {
-        String reason = "reason";
+        String message = "message";
         Throwable cause = new RuntimeException();
 
-        testException(new SocketException(cause), cause);
+        testException(new SocketException(cause), cause.toString(), cause);
+        testException(new SocketException(message, cause), message, cause);
     }
 
     private static void testException(SocketException se,
+                                      String expectedMessage,
                                       Throwable expectedCause) {
+        var message = se.getMessage();
+        if (!Objects.equals(message, expectedMessage)) {
+            throw new RuntimeException("Unexpected message " + message);
+        }
+
         var cause = se.getCause();
         if (cause != expectedCause) {
             throw new RuntimeException("Unexpected cause");
