3aa5b5100a92c43ddf311f513718cf5ee4ab7551
Martin Doerr <mdoerr@openjdk.org>
2022-03-07 10:47:13 +0000

    8273804: Platform.isTieredSupported should handle the no-compiler case
    Backport-of: 46af82e5b0d176e340f46125f8a3bb17e56bd7b3

--- test/lib/jdk/test/lib/Platform.java
+++ test/lib/jdk/test/lib/Platform.java
@@ -79,7 +79,7 @@ public class Platform {
     }
 
     public static boolean isTieredSupported() {
-        return compiler.contains("Tiered Compilers");
+        return (compiler != null) && compiler.contains("Tiered Compilers");
     }
 
     public static boolean isInt() {
