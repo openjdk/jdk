0babec73b92f574cce4185853cc74e918862cd43
Xin Liu <xxinliu@amazon.com>
2022-03-07 05:32:35 +0000

    8282715: typo compileony in test Test8005033.java

--- test/hotspot/jtreg/compiler/codegen/Test8005033.java
+++ test/hotspot/jtreg/compiler/codegen/Test8005033.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012 SAP SE. All rights reserved.
+ * Copyright (c) 2012, 2022 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,7 +27,7 @@
  * @summary On sparcv9, C2's intrinsic for Integer.bitCount(OV) returns wrong result if OV is the result of an operation with int overflow.
  *
  * @run main/othervm -Xcomp
- *      -XX:CompileCommand=compileony,compiler.codegen.Test8005033::testBitCount
+ *      -XX:CompileCommand=compileonly,compiler.codegen.Test8005033::testBitCount
  *      compiler.codegen.Test8005033
  * @author Richard Reingruber richard DOT reingruber AT sap DOT com
  */
