2519a58c2527d29f832e7f43ce20475e72c2de1e
Jatin Bhateja <jbhateja@openjdk.org>
2022-03-11 12:19:28 +0530

    8279508: Reducing the invocation count and compile thresholds for RoundTests.java.

--- test/jdk/java/lang/Math/RoundTests.java
+++ test/jdk/java/lang/Math/RoundTests.java
@@ -25,12 +25,12 @@
  * @test
  * @bug 6430675 8010430
  * @summary Check for correct implementation of {Math, StrictMath}.round
- * @run main/othervm -XX:Tier3CompileThreshold=100 -XX:CompileThresholdScaling=0.01 -XX:+TieredCompilation RoundTests
+ * @run main/othervm -XX:Tier3CompileThreshold=50 -XX:CompileThresholdScaling=0.01 -XX:+TieredCompilation RoundTests
  */
 public class RoundTests {
     public static void main(String... args) {
         int failures = 0;
-        for (int i = 0; i < 2000; i++) {
+        for (int i = 0; i < 500; i++) {
             failures += testNearFloatHalfCases();
             failures += testNearDoubleHalfCases();
             failures += testUnityULPCases();
