160231a146212a35cc967637976a831ff0dfecde
Jatin Bhateja <jbhateja@openjdk.org>
2022-03-14 07:11:23 +0000

    8281375: Accelerate bitCount operation for AVX2 and AVX512 target.
    Reviewed-by: sviswanathan, thartmann

--- src/hotspot/cpu/aarch64/matcher_aarch64.hpp
+++ src/hotspot/cpu/aarch64/matcher_aarch64.hpp
@@ -163,4 +163,10 @@
   // Implements a variant of EncodeISOArrayNode that encode ASCII only
   static const bool supports_encode_ascii_array = true;
 
+  // Returns pre-selection estimated size of a vector operation.
+  static int vector_op_pre_select_sz_estimate(int vopc, BasicType ety, int vlen) {
+    return 0;
+  }
+
+
 #endif // CPU_AARCH64_MATCHER_AARCH64_HPP
