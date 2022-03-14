787ad25196bac75827b6bcefc26ba283426b833b
U-LAPTOP-UFCGRAU7\jpg <srinivas.mandalika@oracle.com>
2022-03-14 11:44:43 +0530

    frame dispose call on EDT

--- test/jdk/javax/accessibility/4529616/AccessibleJTableCellTest.java
+++ test/jdk/javax/accessibility/4529616/AccessibleJTableCellTest.java
@@ -62,7 +62,7 @@ public class AccessibleJTableCellTest {
                 }
             }
         } finally {
-            jFrame.dispose();
+            SwingUtilities.invokeAndWait(() -> jFrame.dispose());
         }
     }
 
