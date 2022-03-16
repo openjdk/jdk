f05c8d854404fb75e5e057f61bff69f0eeb9e76a
Tyler Steele <tyler.steele@ibm.com>
2022-03-16 14:58:41 -0600

    Addresses failure in NullClassTest on AIX.
    - Changes malloc(0) call to malloc(1) on AIX.

--- src/java.base/share/native/libjava/ClassLoader.c
+++ src/java.base/share/native/libjava/ClassLoader.c
@@ -99,7 +99,14 @@ Java_java_lang_ClassLoader_defineClass1(JNIEnv *env,
         return 0;
     }
 
+    // On malloc(0), implementators of malloc(3) have the choice to return either
+    // NULL or a unique non-NULL pointer. To unify libc behavior across our platforms
+    // we chose the latter. (see 8283225)
+    #ifdef _AIX
+    body = (jbyte *)malloc(length == 0 ? 1 : length);
+    #else
     body = (jbyte *)malloc(length);
+    #endif
 
     if (body == 0) {
         JNU_ThrowOutOfMemoryError(env, 0);
@@ -239,7 +246,15 @@ Java_java_lang_ClassLoader_defineClass0(JNIEnv *env,
         return 0;
     }
 
+    // On malloc(0), implementators of malloc(3) have the choice to return either
+    // NULL or a unique non-NULL pointer. To unify libc behavior across our platforms
+    // we chose the latter. (see 8283225)
+    #ifdef _AIX
+    body = (jbyte *)malloc(length == 0 ? 1 : length);
+    #else
     body = (jbyte *)malloc(length);
+    #endif
+
     if (body == 0) {
         JNU_ThrowOutOfMemoryError(env, 0);
         return 0;
