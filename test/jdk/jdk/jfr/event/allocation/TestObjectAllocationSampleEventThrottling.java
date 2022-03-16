60d085e6f7ebc92f9ee14cd542c6968d6a125e43
Aleksey Shipilev <shade@openjdk.org>
2022-03-16 09:39:35 +0000

    8281638: jfr/event/allocation tests fail with release VMs after JDK-8281318 due to lack of -XX:+UnlockDiagnosticVMOptions
    Backport-of: d254cf28c5e72bd9b8de863b831015237640ca25

--- test/jdk/jdk/jfr/event/allocation/TestObjectAllocationSampleEventThrottling.java
+++ test/jdk/jdk/jfr/event/allocation/TestObjectAllocationSampleEventThrottling.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -44,7 +44,7 @@ import sun.hotspot.WhiteBox;
  * @build sun.hotspot.WhiteBox
  *
  * @run driver jdk.test.lib.helpers.ClassFileInstaller sun.hotspot.WhiteBox
- * @run main/othervm -XX:+WhiteBoxAPI -Xbootclasspath/a:.
+ * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xbootclasspath/a:.
  *                   -XX:+UseTLAB -XX:TLABSize=2k -XX:-ResizeTLAB
  *                   jdk.jfr.event.allocation.TestObjectAllocationSampleEventThrottling
  */
