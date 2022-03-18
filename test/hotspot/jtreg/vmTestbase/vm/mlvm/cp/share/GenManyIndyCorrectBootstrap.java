1bcd779f677420326bf365c3580ceab5a6b5e3fa
liach <liach@users.noreply.github.com>
2022-03-18 17:08:34 -0500

    Remove homemade callsite from test

--- test/hotspot/jtreg/vmTestbase/vm/mlvm/cp/share/GenManyIndyCorrectBootstrap.java
+++ test/hotspot/jtreg/vmTestbase/vm/mlvm/cp/share/GenManyIndyCorrectBootstrap.java
@@ -45,36 +45,6 @@ public class GenManyIndyCorrectBootstrap extends GenFullCP {
     }
 
     /**
-     * Creates static init method, which constructs a call site object, which refers to the target method
-     * and invokes Dummy.setMH() on this call site
-     * @param cw Class writer object
-     */
-    @Override
-    protected void createClassInitMethod(ClassWriter cw) {
-        MethodVisitor mw = cw.visitMethod(
-                Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC,
-                STATIC_INIT_METHOD_NAME, INIT_METHOD_SIGNATURE,
-                null,
-                new String[0]);
-
-        mw.visitMethodInsn(Opcodes.INVOKESTATIC, JLI_METHODHANDLES, "lookup", "()" + fd(JLI_METHODHANDLES_LOOKUP));
-        mw.visitLdcInsn(Type.getObjectType(fullClassName));
-        mw.visitLdcInsn(TARGET_METHOD_NAME);
-        mw.visitLdcInsn(TARGET_METHOD_SIGNATURE);
-        mw.visitLdcInsn(Type.getObjectType(fullClassName));
-        mw.visitMethodInsn(Opcodes.INVOKEVIRTUAL, JL_CLASS,
-                "getClassLoader", "()" + fd(JL_CLASSLOADER));
-        mw.visitMethodInsn(Opcodes.INVOKESTATIC, JLI_METHODTYPE,
-                "fromMethodDescriptorString", "(" + fd(JL_STRING) + fd(JL_CLASSLOADER) + ")" + fd(JLI_METHODTYPE));
-        mw.visitMethodInsn(Opcodes.INVOKEVIRTUAL, JLI_METHODHANDLES_LOOKUP,
-                "findStatic", "(" + fd(JL_CLASS) + fd(JL_STRING) + fd(JLI_METHODTYPE) + ")" + fd(JLI_METHODHANDLE));
-        mw.visitMethodInsn(Opcodes.INVOKESTATIC, NEW_INVOKE_SPECIAL_CLASS_NAME,
-                "setMH", "(" + fd(JLI_METHODHANDLE) + ")V");
-
-        finishMethodCode(mw);
-    }
-
-    /**
      * Disables invoke dynamic CP entry caching and generate default common data
      * @param cw Class writer object
      */
@@ -92,20 +62,14 @@ public class GenManyIndyCorrectBootstrap extends GenFullCP {
      */
     @Override
     protected void generateCPEntryData(ClassWriter cw, MethodVisitor mw) {
-        Handle bsm;
-        if (Env.getRNG().nextBoolean()) {
-            bsm = new Handle(Opcodes.H_NEWINVOKESPECIAL,
-                    NEW_INVOKE_SPECIAL_CLASS_NAME,
-                    INIT_METHOD_NAME,
-                    NEW_INVOKE_SPECIAL_BOOTSTRAP_METHOD_SIGNATURE);
-        } else {
-            bsm = new Handle(Opcodes.H_INVOKESTATIC,
-                    this.fullClassName,
-                    BOOTSTRAP_METHOD_NAME,
-                    BOOTSTRAP_METHOD_SIGNATURE);
-        }
+        Handle bsm = new Handle(Opcodes.H_INVOKESTATIC,
+                this.fullClassName,
+                BOOTSTRAP_METHOD_NAME,
+                BOOTSTRAP_METHOD_SIGNATURE);
         mw.visitInvokeDynamicInsn(TARGET_METHOD_NAME,
                 TARGET_METHOD_SIGNATURE,
                 bsm);
     }
+
+
 }
