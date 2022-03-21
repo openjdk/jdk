780c07024343be4e240a5e00c55084382a18f05f
Eric Liu <eric.c.liu@arm.com>
2022-03-21 15:22:21 +0000

    AArch64: [vectorapi] Backend implementation of VectorMask.fromLong with SVE2
    This patch implements AArch64 codegen for VectorLongToMask using the
    SVE2 BitPerm feature. With this patch, the final code (generated on an
    SVE vector reg size of 512-bit QEMU emulator) is shown as below:
    
            mov     z17.b, #0
            mov     v17.d[0], x13
            sunpklo z17.h, z17.b
            sunpklo z17.s, z17.h
            sunpklo z17.d, z17.s
            mov     z16.b, #1
            bdep    z17.d, z17.d, z16.d
            cmpne   p0.b, p7/z, z17.b, #0
    
    Change-Id: I9135fce39c8a08c72b757c78b258f5d968baa7ff

--- test/hotspot/gtest/aarch64/asmtest.out.h
+++ test/hotspot/gtest/aarch64/asmtest.out.h
@@ -1184,17 +1184,18 @@
     __ sve_uzp1(z23, __ S, z22, z0);                   //       uzp1    z23.s, z22.s, z0.s
     __ sve_uzp2(z25, __ H, z26, z23);                  //       uzp2    z25.h, z26.h, z23.h
     __ sve_bext(z21, __ B, z21, z1);                   //       bext    z21.b, z21.b, z1.b
+    __ sve_bdep(z10, __ S, z19, z11);                  //       bdep    z10.s, z19.s, z11.s
 
 // SVEReductionOp
-    __ sve_andv(v10, __ S, p5, z11);                   //       andv s10, p5, z11.s
-    __ sve_orv(v23, __ D, p6, z8);                     //       orv d23, p6, z8.d
-    __ sve_eorv(v17, __ S, p5, z19);                   //       eorv s17, p5, z19.s
-    __ sve_smaxv(v4, __ D, p5, z13);                   //       smaxv d4, p5, z13.d
-    __ sve_sminv(v22, __ D, p7, z30);                  //       sminv d22, p7, z30.d
-    __ sve_fminv(v17, __ S, p4, z14);                  //       fminv s17, p4, z14.s
-    __ sve_fmaxv(v12, __ S, p7, z20);                  //       fmaxv s12, p7, z20.s
-    __ sve_fadda(v1, __ S, p3, z13);                   //       fadda s1, p3, s1, z13.s
-    __ sve_uaddv(v7, __ S, p2, z11);                   //       uaddv d7, p2, z11.s
+    __ sve_andv(v23, __ D, p6, z8);                    //       andv d23, p6, z8.d
+    __ sve_orv(v17, __ S, p5, z19);                    //       orv s17, p5, z19.s
+    __ sve_eorv(v4, __ D, p5, z13);                    //       eorv d4, p5, z13.d
+    __ sve_smaxv(v22, __ D, p7, z30);                  //       smaxv d22, p7, z30.d
+    __ sve_sminv(v17, __ H, p4, z14);                  //       sminv h17, p4, z14.h
+    __ sve_fminv(v12, __ S, p7, z20);                  //       fminv s12, p7, z20.s
+    __ sve_fmaxv(v1, __ S, p3, z13);                   //       fmaxv s1, p3, z13.s
+    __ sve_fadda(v7, __ D, p2, z11);                   //       fadda d7, p2, d7, z11.d
+    __ sve_uaddv(v4, __ B, p6, z15);                   //       uaddv d4, p6, z15.b
 
     __ bind(forth);
 
@@ -1213,30 +1214,30 @@
     0x9101a1a0,     0xb10a5cc8,     0xd10810aa,     0xf10fd061,
     0x120cb166,     0x321764bc,     0x52174681,     0x720c0227,
     0x9241018e,     0xb25a2969,     0xd278b411,     0xf26aad01,
-    0x14000000,     0x17ffffd7,     0x140003e5,     0x94000000,
-    0x97ffffd4,     0x940003e2,     0x3400000a,     0x34fffa2a,
-    0x34007bea,     0x35000008,     0x35fff9c8,     0x35007b88,
-    0xb400000b,     0xb4fff96b,     0xb4007b2b,     0xb500001d,
-    0xb5fff91d,     0xb5007add,     0x10000013,     0x10fff8b3,
-    0x10007a73,     0x90000013,     0x36300016,     0x3637f836,
-    0x363079f6,     0x3758000c,     0x375ff7cc,     0x3758798c,
+    0x14000000,     0x17ffffd7,     0x140003e6,     0x94000000,
+    0x97ffffd4,     0x940003e3,     0x3400000a,     0x34fffa2a,
+    0x34007c0a,     0x35000008,     0x35fff9c8,     0x35007ba8,
+    0xb400000b,     0xb4fff96b,     0xb4007b4b,     0xb500001d,
+    0xb5fff91d,     0xb5007afd,     0x10000013,     0x10fff8b3,
+    0x10007a93,     0x90000013,     0x36300016,     0x3637f836,
+    0x36307a16,     0x3758000c,     0x375ff7cc,     0x375879ac,
     0x128313a0,     0x528a32c7,     0x7289173b,     0x92ab3acc,
     0xd2a0bf94,     0xf2c285e8,     0x9358722f,     0x330e652f,
     0x53067f3b,     0x93577c53,     0xb34a1aac,     0xd35a4016,
     0x13946c63,     0x93c3dbc8,     0x54000000,     0x54fff5a0,
-    0x54007760,     0x54000001,     0x54fff541,     0x54007701,
-    0x54000002,     0x54fff4e2,     0x540076a2,     0x54000002,
-    0x54fff482,     0x54007642,     0x54000003,     0x54fff423,
-    0x540075e3,     0x54000003,     0x54fff3c3,     0x54007583,
-    0x54000004,     0x54fff364,     0x54007524,     0x54000005,
-    0x54fff305,     0x540074c5,     0x54000006,     0x54fff2a6,
-    0x54007466,     0x54000007,     0x54fff247,     0x54007407,
-    0x54000008,     0x54fff1e8,     0x540073a8,     0x54000009,
-    0x54fff189,     0x54007349,     0x5400000a,     0x54fff12a,
-    0x540072ea,     0x5400000b,     0x54fff0cb,     0x5400728b,
-    0x5400000c,     0x54fff06c,     0x5400722c,     0x5400000d,
-    0x54fff00d,     0x540071cd,     0x5400000e,     0x54ffefae,
-    0x5400716e,     0x5400000f,     0x54ffef4f,     0x5400710f,
+    0x54007780,     0x54000001,     0x54fff541,     0x54007721,
+    0x54000002,     0x54fff4e2,     0x540076c2,     0x54000002,
+    0x54fff482,     0x54007662,     0x54000003,     0x54fff423,
+    0x54007603,     0x54000003,     0x54fff3c3,     0x540075a3,
+    0x54000004,     0x54fff364,     0x54007544,     0x54000005,
+    0x54fff305,     0x540074e5,     0x54000006,     0x54fff2a6,
+    0x54007486,     0x54000007,     0x54fff247,     0x54007427,
+    0x54000008,     0x54fff1e8,     0x540073c8,     0x54000009,
+    0x54fff189,     0x54007369,     0x5400000a,     0x54fff12a,
+    0x5400730a,     0x5400000b,     0x54fff0cb,     0x540072ab,
+    0x5400000c,     0x54fff06c,     0x5400724c,     0x5400000d,
+    0x54fff00d,     0x540071ed,     0x5400000e,     0x54ffefae,
+    0x5400718e,     0x5400000f,     0x54ffef4f,     0x5400712f,
     0xd40658e1,     0xd4014d22,     0xd4046543,     0xd4273f60,
     0xd44cad80,     0xd503201f,     0xd503203f,     0xd503205f,
     0xd503209f,     0xd50320bf,     0xd503219f,     0xd50323bf,
@@ -1460,8 +1461,9 @@
     0x65f1af0b,     0x65eec9f1,     0x65a7fed6,     0x65aa5f65,
     0x65b47aae,     0x04c55723,     0x0441723d,     0x042d33ae,
     0x04be3051,     0x047d32b6,     0x04e03048,     0x05a06ad7,
-    0x05776f59,     0x4501b2b5,     0x049a356a,     0x04d83917,
-    0x04993671,     0x04c835a4,     0x04ca3fd6,     0x658731d1,
-    0x65863e8c,     0x65982da1,     0x04812967,
+    0x05776f59,     0x4501b2b5,     0x458bb66a,     0x04da3917,
+    0x04983671,     0x04d935a4,     0x04c83fd6,     0x044a31d1,
+    0x65873e8c,     0x65862da1,     0x65d82967,     0x040139e4,
+
   };
 // END  Generated code -- do not edit
