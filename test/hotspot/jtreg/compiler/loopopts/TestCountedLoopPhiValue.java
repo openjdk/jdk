2ec58ba212aa1238096d8fbfd33e87568c192868
rwestrel <rwestrel@redhat.com>
2022-03-23 14:10:41 +0100

    more test

--- test/hotspot/jtreg/compiler/loopopts/TestCountedLoopPhiValue.java
+++ test/hotspot/jtreg/compiler/loopopts/TestCountedLoopPhiValue.java
@@ -31,12 +31,47 @@
 public class TestCountedLoopPhiValue {
     public static void main(String[] args) {
         test1();
+        test2();
     }
 
     private static void test1() {
         for (long l = (Long.MAX_VALUE - 1); l != (Long.MIN_VALUE + 100_000); l++) {
             if (l == 0) {
                 throw new RuntimeException("Test failed");
+            }
+        }
+    }
+
+    private static void test2() {
+        for (int i = 1; i < 10 * 182 * 138; i++) {
+            iMeth(-9, -9);
+        }
+    }
+
+    public static final int N = 400;
+
+    public static long instanceCount=-2L;
+    public static long lArrFld[]=new long[N];
+
+    public static void iMeth(int i6, int i7) {
+
+        double d1;
+        int i8, i10, i11=30785, i12=8;
+        long l2;
+        byte by=58;
+
+        d1 = i7;
+        i8 = 1;
+        while (++i8 < 342) {
+            for (l2 = 1; l2 < 5; l2++) {
+                i6 -= (int)d1;
+                for (i10 = 1; i10 < 2; i10++) {
+                    i7 += (int)l2;
+                    i12 *= i11;
+                    i11 -= i12;
+                    instanceCount += i10;
+                    lArrFld[i8 - 1] = by;
+                }
             }
         }
     }
