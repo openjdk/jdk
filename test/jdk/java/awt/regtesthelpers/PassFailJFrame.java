746ceab6d22c736836fdd8baec3a0e2c79075c95
lawrence.andrews <lawrence.andrews@oracle.com>
2022-03-25 21:07:18 -0500

    Fixed the review comments and fixed the test using PassFailJFrame.java

--- test/jdk/java/awt/regtesthelpers/PassFailJFrame.java
+++ test/jdk/java/awt/regtesthelpers/PassFailJFrame.java
@@ -24,6 +24,7 @@
 import java.awt.BorderLayout;
 import java.awt.Frame;
 import java.awt.HeadlessException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import javax.swing.JButton;
@@ -32,52 +33,81 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
 
 public class PassFailJFrame extends JFrame {
     private final CountDownLatch latch = new CountDownLatch(1);
     private boolean failed = false;
     private String testFailedReason;
     private JTextArea instructionsText;
-    private int maxStringLength = 50;
-    private int timeoutMinutes = 3;
-    private Runnable tearDownRunnable;
+    private final int maxRowLength;
+    private final int maxStringLength;
+    private final int timeoutMinutes;
 
-    public PassFailJFrame(String instructions, String title) throws HeadlessException {
+    /**
+     * Constructs a JFrame with a given title & serves as test instructional
+     * frame where the user follows the specified test instruction in order
+     * to test the test case & mark the test pass or fail. If the expected
+     * result is seen then the user click on the 'Pass' button else click
+     * on the 'Fail' button and the reason for the failure should be
+     * specified in the JDailog JTextArea.
+     * @param title title of the Frame.
+     * @param instructions specified instruction that user should follow.
+     * @param maxRowLength number of visible rows of the JTextArea where the
+     *                     instruction is show.
+     * @param maxStringLength number of columns of the instructional JTextArea
+     * @param timeoutMinutes timeout of the test where time is specified in
+     *                       minutes.
+     * @throws HeadlessException
+     * @throws InterruptedException
+     * @throws InvocationTargetException
+     */
+    public PassFailJFrame(String title, String instructions,
+                          int maxRowLength, int maxStringLength,
+                          int timeoutMinutes) throws HeadlessException,
+            InterruptedException, InvocationTargetException {
         super(title);
+        this.maxRowLength = maxRowLength;
+        this.maxStringLength = maxStringLength;
+        this.timeoutMinutes = timeoutMinutes;
 
-        setLayout(new BorderLayout());
-        instructionsText = new JTextArea(instructions, 20, maxStringLength);
-        instructionsText.setEditable(false);
-        instructionsText.setFocusable(false);
-        add(instructionsText, BorderLayout.NORTH);
+        SwingUtilities.invokeAndWait(() -> {
+            setLayout(new BorderLayout());
+            instructionsText = new JTextArea(instructions, maxRowLength, maxStringLength);
+            instructionsText.setEditable(false);
+            instructionsText.setFocusable(false);
+            add(instructionsText, BorderLayout.NORTH);
 
-        JButton btnPass = new JButton("Pass");
-        btnPass.addActionListener((e) -> latch.countDown());
+            JButton btnPass = new JButton("Pass");
+            btnPass.addActionListener((e) -> latch.countDown());
 
-        JButton btnFail = new JButton("Fail");
-        btnFail.addActionListener((e) -> getFailureReason());
+            JButton btnFail = new JButton("Fail");
+            btnFail.addActionListener((e) -> getFailureReason());
 
-        JPanel buttonsPanel = new JPanel();
-        buttonsPanel.add(btnPass);
-        buttonsPanel.add(btnFail);
+            JPanel buttonsPanel = new JPanel();
+            buttonsPanel.add(btnPass);
+            buttonsPanel.add(btnFail);
 
-        add(buttonsPanel, BorderLayout.SOUTH);
-        pack();
-        setLocationRelativeTo(null);
-        setVisible(true);
+            add(buttonsPanel, BorderLayout.SOUTH);
+            pack();
+	    setLocation(10,10);
+            setVisible(true);
+        });
     }
 
-    public void awaitAndCheck() throws InterruptedException {
-        boolean timeoutHappened = !latch.await(timeoutMinutes, TimeUnit.MINUTES);
+    /**
+     * Wait for the user decision i,e user selects pass or fail button.
+     * If user does not select pass or fail button then the test waits for
+     * the specified timeoutMinutes period and the test gets timeout.
+     * This method should not be invoke with in EDT.
+     * @throws InterruptedException
+     * @throws InvocationTargetException
+     */
+    public void awaitAndCheck() throws InterruptedException, InvocationTargetException {
+        boolean timeoutHappened = !latch.await(this.timeoutMinutes,
+                TimeUnit.MINUTES);
 
-        for (Frame f : JFrame.getFrames()) {
-            System.out.println(f.getTitle() + " is getting disposed");
-            f.dispose();
-        }
-
-        if (tearDownRunnable != null) {
-            tearDownRunnable.run();
-        }
+        SwingUtilities.invokeAndWait(() -> dispose());
 
         if (timeoutHappened) {
             throw new RuntimeException("Test timed out!");
@@ -87,8 +117,13 @@ public class PassFailJFrame extends JFrame {
         }
     }
 
-    protected void tearDownRunnable(Runnable runnable) {
-        tearDownRunnable = runnable;
+    public void tearDown() throws InterruptedException, InvocationTargetException {
+        SwingUtilities.invokeAndWait(() -> {
+            for (Frame f : JFrame.getFrames()) {
+                System.out.println(f.getTitle() + " is getting disposed");
+                f.dispose();
+            }
+        });
     }
 
     public void getFailureReason() {
