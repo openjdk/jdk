fca1eaac7ca9eaaa9952c7b0f35cccc41a299914
Quan Anh Mai <anhmdq99@gmail.com>
2022-03-28 00:27:43 +0800

    inconsistent override

--- src/hotspot/share/adlc/output_h.cpp
+++ src/hotspot/share/adlc/output_h.cpp
@@ -1703,7 +1703,7 @@ void ArchDesc::declareClasses(FILE *fp) {
 
     // If there is an explicit peephole rule, build it
     if ( instr->peepholes() != NULL ) {
-      fprintf(fp,"  virtual int            peephole(Block *block, int block_index, PhaseRegAlloc *ra_) override;\n");
+      fprintf(fp,"  virtual int            peephole(Block *block, int block_index, PhaseRegAlloc *ra_);\n");
     }
 
     // Output the declaration for number of relocation entries
