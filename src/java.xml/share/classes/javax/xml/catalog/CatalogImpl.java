14513d7bd7104ed86f0d25d6823aa577c1e2a54c
JoeWang-Java <huizhe.wang@oracle.com>
2022-03-29 16:30:22 +0000

    8253569: javax.xml.catalog.Catalog.matchURI() implementation should reset state variables

--- src/java.xml/share/classes/javax/xml/catalog/CatalogImpl.java
+++ src/java.xml/share/classes/javax/xml/catalog/CatalogImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,10 +28,8 @@ import com.sun.org.apache.xerces.internal.jaxp.SAXParserFactoryImpl;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -108,6 +106,8 @@ class CatalogImpl extends GroupEntry implements Catalog {
      */
     public CatalogImpl(CatalogImpl parent, CatalogFeatures f, URI... uris) throws CatalogException {
         super(CatalogEntryType.CATALOG, parent);
+        isCatalogInstance = true;
+
         if (f == null) {
             throw new NullPointerException(
                     formatMessage(CatalogMessages.ERR_NULL_ARGUMENT, new Object[]{"CatalogFeatures"}));
