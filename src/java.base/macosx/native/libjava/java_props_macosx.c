21176ba08b55a07bd1afeac6da9c67483542f92b
Roger Riggs <roger.riggs@oracle.com>
2022-03-31 11:24:06 -0400

    8283697: On macOS, initialize the locale from Posix setLocale if available, otherwise use macOs locale

--- src/java.base/macosx/native/libjava/java_props_macosx.c
+++ src/java.base/macosx/native/libjava/java_props_macosx.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -212,14 +212,16 @@ const char * convertToPOSIXLocale(const char* src) {
     return src;
 }
 
+/**
+ * Return locale from Posix if it is defined and non-empty,
+ * with a fallback to macOS language environment.
+ */
 char *setupMacOSXLocale(int cat) {
-    char * ret = getMacOSXLocale(cat);
-
-    if (ret == NULL) {
-        return getPosixLocale(cat);
-    } else {
+    char * ret = getPosixLocale(cat);
+    if (ret != NULL && strlen(ret) > 0) {
         return ret;
     }
+    return getMacOSXLocale(cat);
 }
 
 // 10.9 SDK does not include the NSOperatingSystemVersion struct.
