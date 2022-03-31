77a205aa4cfe4892843f22dce63eb85789b4b321
Jie Fu <jiefu@openjdk.org>
2022-03-31 13:51:12 +0000

    8284090: com/sun/security/auth/module/AllPlatforms.java fails to compile
    Reviewed-by: mullan

--- test/jdk/com/sun/security/auth/module/AllPlatforms.java
+++ test/jdk/com/sun/security/auth/module/AllPlatforms.java
@@ -31,7 +31,7 @@ import javax.security.auth.login.Configuration;
 import javax.security.auth.login.LoginContext;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import javax.security.auth.login.LoginException;
+import javax.security.auth.login.FailedLoginException;
 
 public class AllPlatforms {
 
