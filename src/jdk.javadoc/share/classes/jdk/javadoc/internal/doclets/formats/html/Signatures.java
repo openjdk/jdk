73cb922bfc13a00bd1e07a41c6971ae7e1ac7005
Pavel Rappo <prappo@openjdk.org>
2022-03-31 15:23:40 +0000

    8284026: Use unmodifiable collections where practical
    Reviewed-by: jjg

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/Signatures.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/Signatures.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -46,7 +46,6 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementKindVisitor14;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.SortedSet;
@@ -106,7 +105,7 @@ public class Signatures {
         private final HtmlConfiguration configuration;
         private Content modifiers;
 
-        private static final Set<String> previewModifiers = Collections.emptySet();
+        private static final Set<String> previewModifiers = Set.of();
 
          TypeSignature(TypeElement typeElement, HtmlDocletWriter writer) {
              this.typeElement = typeElement;
