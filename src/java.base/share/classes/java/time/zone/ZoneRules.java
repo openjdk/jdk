1a5f5da0502d056388b8390b008a4ff0d22faf92
Claes Redestad <redestad@openjdk.org>
2022-03-31 08:45:06 +0000

    8283996: Reduce cost of year and month calculations
    Reviewed-by: bpb, scolebourne, naoto, rriggs

--- src/java.base/share/classes/java/time/zone/ZoneRules.java
+++ src/java.base/share/classes/java/time/zone/ZoneRules.java
@@ -974,11 +974,11 @@ public final class ZoneRules implements Serializable {
             doyEst = zeroDay - (365 * yearEst + yearEst / 4 - yearEst / 100 + yearEst / 400);
         }
         yearEst += adjust;  // reset any negative year
-        int marchDoy0 = (int) doyEst;
 
-        // convert march-based values back to january-based
-        int marchMonth0 = (marchDoy0 * 5 + 2) / 153;
-        yearEst += marchMonth0 / 10;
+        // convert march-based values back to january-based, adjust year
+        if (doyEst >= 306) {
+            yearEst++;
+        }
 
         // Cap to the max value
         return (int)Math.min(yearEst, Year.MAX_VALUE);
