bee769870a220d881f112634927482df7f9a7d42
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-04-02 18:28:06 +0100

    8284191: Replace usages of 'an the' in hotspot and java.base

--- src/jdk.sctp/share/classes/com/sun/nio/sctp/ShutdownNotification.java
+++ src/jdk.sctp/share/classes/com/sun/nio/sctp/ShutdownNotification.java
@@ -25,7 +25,7 @@
 package com.sun.nio.sctp;
 
 /**
- * Notification emitted when a peers shutdowns an the association.
+ * Notification emitted when a peers shutdowns the association.
  *
  * <P> When a peer sends a <i>SHUTDOWN</i>, the SCTP stack delivers this
  * notification to inform the application that it should cease sending data.
