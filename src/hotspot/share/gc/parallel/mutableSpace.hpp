5a6b1f1004c9097860545c9264fb93773b9b89d5
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-04-04 11:13:07 +0200

    Initial version

--- src/hotspot/share/gc/parallel/mutableSpace.hpp
+++ src/hotspot/share/gc/parallel/mutableSpace.hpp
@@ -62,10 +62,12 @@ class MutableSpace: public CHeapObj<mtGC> {
 
   MutableSpaceMangler* mangler() { return _mangler; }
 
-  void numa_setup_pages(MemRegion mr, bool clear_space);
+  void numa_setup_pages(MemRegion mr, size_t page_size, bool clear_space);
 
   void set_last_setup_region(MemRegion mr) { _last_setup_region = mr;   }
   MemRegion last_setup_region() const      { return _last_setup_region; }
+
+  size_t used_page_size() const;
 
  public:
   virtual ~MutableSpace();
