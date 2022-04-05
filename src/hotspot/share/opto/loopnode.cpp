500f9a577bd7df1321cb28e69893e84b16857dd3
Dean Long <dlong@openjdk.org>
2022-04-05 21:07:29 +0000

    8283396: Null pointer dereference in loopnode.cpp:2851
    Reviewed-by: chagedorn, kvn

--- src/hotspot/share/opto/loopnode.cpp
+++ src/hotspot/share/opto/loopnode.cpp
@@ -853,6 +853,7 @@ bool PhaseIdealLoop::create_loop_nest(IdealLoopTree* loop, Node_List &old_new) {
   if (bt == T_INT && head->as_CountedLoop()->is_strip_mined()) {
     // Loop is strip mined: use the safepoint of the outer strip mined loop
     OuterStripMinedLoopNode* outer_loop = head->as_CountedLoop()->outer_loop();
+    assert(outer_loop != NULL, "no outer loop");
     safepoint = outer_loop->outer_safepoint();
     outer_loop->transform_to_counted_loop(&_igvn, this);
     exit_test = head->loopexit();
