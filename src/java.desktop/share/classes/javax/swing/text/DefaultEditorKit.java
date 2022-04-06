89935491d30dc89026d641ef533c1ed5a02da7d7
Tejesh R <tejesh.r@oracle.com>
2022-04-06 15:05:42 +0530

    Fix for the Bug - Checking Condition added for \r

--- src/java.desktop/share/classes/javax/swing/text/DefaultEditorKit.java
+++ src/java.desktop/share/classes/javax/swing/text/DefaultEditorKit.java
@@ -339,7 +339,13 @@ public class DefaultEditorKit extends EditorKit {
                 for (int counter = last; counter < maxCounter; counter++) {
                     if (array[counter] == '\n') {
                         if (counter > last) {
-                            out.write(array, last, counter - last);
+                            if(array[counter-1] == '\r') {
+                                out.write(array, last, counter - last - 1);
+                            }
+                            else
+                            {
+                                out.write(array, last, counter - last);
+                            }
                         }
                         out.write(endOfLine);
                         last = counter + 1;
