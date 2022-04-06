a6191579a3c505a9afef55f8d5548a591d6b86fc
Chris Hennick <4961925+Pr0methean@users.noreply.github.com>
2022-04-06 10:46:58 -0700

    Fix rounding error in computeNextExponential; use FMA
    Repeatedly adding DoubleZigguratTables.exponentialX0 to extra causes a rounding error to accumulate at the tail of the distribution; this fixes that by tracking the multiple of exponentialX0 as a long.
    
    Add computeWinsorizedNextExponential for testability

--- src/java.base/share/classes/jdk/internal/util/random/RandomSupport.java
+++ src/java.base/share/classes/jdk/internal/util/random/RandomSupport.java
@@ -1138,6 +1138,11 @@ public class RandomSupport {
     }
 
     /**
+     * Largest value that {@link #computeNextExponential} can ever return.
+     */
+    private static final double MAX_EXPONENTIAL = 0x1.0p63 * DoubleZigguratTables.exponentialX0;
+
+    /**
      * Implementation support for the {@code nextExponential} method of
      * {@link java.util.random.RandomGenerator}.
      *
@@ -1158,6 +1163,16 @@ public class RandomSupport {
      *         from an exponential distribution whose mean is 1
      */
     public static double computeNextExponential(RandomGenerator rng) {
+        return computeWinsorizedNextExponential(rng, MAX_EXPONENTIAL);
+    }
+
+    /**
+     * Equivalent to {@code Math.min(computeNextExponential(rng), maxValue)} except
+     * that the worst-case running time is linear with {@code maxValue} provided that
+     * {@code rng.nextLong()} runs in constant time.
+     * @param maxValue a non-negative limit on the value to return
+     */
+    public static double computeWinsorizedNextExponential(RandomGenerator rng, double maxValue) {
         /*
          * The tables themselves, as well as a number of associated parameters, are
          * defined in class DoubleZigguratTables, which is automatically
@@ -1177,6 +1192,19 @@ public class RandomSupport {
          * (September 1977), 253-256. DOI: https://doi.org/10.1145/355744.355749
          *
          */
+        if (maxValue < 0.0) {
+            throw new IllegalArgumentException("maxValue can't be negative");
+        }
+        if (maxValue == 0.0) {
+            return 0.0;
+        }
+        final long maxExtraMinus1;
+        if (maxValue >= MAX_EXPONENTIAL) {
+            maxExtraMinus1 = Long.MAX_VALUE;
+        } else {
+            // Conversion to long rounds toward zero
+            maxExtraMinus1 = (long) (maxValue / DoubleZigguratTables.exponentialX0);
+        }
         long U1 = rng.nextLong();
         // Experimentation on a variety of machines indicates that it is overall much faster
         // to do the following & and < operations on longs rather than first cast U1 to int
@@ -1184,11 +1212,10 @@ public class RandomSupport {
         long i = U1 & DoubleZigguratTables.exponentialLayerMask;
         if (i < DoubleZigguratTables.exponentialNumberOfLayers) {
             // This is the fast path (occurring more than 98% of the time).  Make an early exit.
-            return DoubleZigguratTables.exponentialX[(int)i] * (U1 >>> 1);
+            return Math.min(maxValue, DoubleZigguratTables.exponentialX[(int)i] * (U1 >>> 1));
         }
         // We didn't use the upper part of U1 after all.  We'll be able to use it later.
-
-        for (double extra = 0.0; ; ) {
+        for (long extra = 0; ; ) {
             // Use Walker's alias method to sample an (unsigned) integer j from a discrete
             // probability distribution that includes the tail and all the ziggurat overhangs;
             // j will be less than DoubleZigguratTables.exponentialNumberOfLayers + 1.
@@ -1219,25 +1246,30 @@ public class RandomSupport {
                     // Compute the actual x-coordinate of the randomly chosen point.
                     double x = (X[j] * 0x1.0p63) + ((X[j-1] - X[j]) * (double)U1);
                     if (Udiff >= DoubleZigguratTables.exponentialConvexMargin) {
-                        return x + extra;   // The chosen point is way below the curve; accept it.
+                        return Math.min(maxValue, Math.fma(extra, DoubleZigguratTables.exponentialX0, x));   // The chosen point is way below the curve; accept it.
                     }
                     // Compute the actual y-coordinate of the randomly chosen point.
                     double y = (Y[j] * 0x1.0p63) + ((Y[j-1] - Y[j]) * (double)U2);
                     // Now see how that y-coordinate compares to the curve
                     if (y <= Math.exp(-x)) {
-                        return x + extra;   // The chosen point is below the curve; accept it.
+                        return Math.min(maxValue, Math.fma(extra, DoubleZigguratTables.exponentialX0, x));   // The chosen point is below the curve; accept it.
                     }
                     // Otherwise, we reject this sample and have to try again.
                 }
             }
+            if (extra == maxExtraMinus1) {
+                // Can't keep track of any larger "extra"
+                return maxValue;
+            }
             // We are now committed to sampling from the tail.  We could do a recursive call
             // and then add X[0], but we save some time and stack space by using an iterative loop.
-            extra += DoubleZigguratTables.exponentialX0;
-            // This is like the first five lines of this method, but if it returns, it first adds "extra".
+            extra++;
+            // This is like the first five lines of this method, but if it returns, it first adds "extra" times X0.
             U1 = rng.nextLong();
             i = U1 & DoubleZigguratTables.exponentialLayerMask;
             if (i < DoubleZigguratTables.exponentialNumberOfLayers) {
-                return DoubleZigguratTables.exponentialX[(int)i] * (U1 >>> 1) + extra;
+                return Math.min(maxValue,
+                        Math.fma(extra, DoubleZigguratTables.exponentialX0, DoubleZigguratTables.exponentialX[(int)i] * (U1 >>> 1)));
             }
         }
     }
@@ -1359,9 +1391,11 @@ public class RandomSupport {
             //    ACM Comput. Surv. 39, 4, Article 11 (November 2007).  See Algorithm 16.
             //    http://doi.org/10.1145/1287620.1287622
             // Compute two separate random exponential samples and then compare them in certain way.
+            double limit;
             do {
                 x = (1.0 / DoubleZigguratTables.normalX0) * computeNextExponential(rng);
-            } while (computeNextExponential(rng) < 0.5*x*x);
+                limit = 0.5*x*x;
+            } while (computeWinsorizedNextExponential(rng, limit) < limit);
             x += DoubleZigguratTables.normalX0;
         } else if (j < DoubleZigguratTables.normalInflectionIndex) {   // Convex overhang
             for (;; U1 = (rng.nextLong() >>> 1)) {
@@ -1392,7 +1426,7 @@ public class RandomSupport {
             for (;; U1 = (rng.nextLong() >>> 1)) {
                 long U2 = (rng.nextLong() >>> 1);
                 // Compute the actual x-coordinate of the randomly chosen point.
-                x = (X[j] * 0x1.0p63) + ((X[j-1] - X[j]) * (double)U1);
+                x = Math.fma(X[j-1] - X[j], (double)U1, X[j] * 0x1.0p63);
                 // Does the point lie below the curve?
                 long Udiff = U2 - U1;
                 if (Udiff >= DoubleZigguratTables.normalConvexMargin) {
@@ -1402,7 +1436,7 @@ public class RandomSupport {
                     continue;   // The chosen point is way above the curve; reject it.
                 }
                 // Compute the actual y-coordinate of the randomly chosen point.
-                double y = (Y[j] * 0x1.0p63) + ((Y[j-1] - Y[j]) * (double)U2);
+                double y = Math.fma(Y[j-1] - Y[j], (double)U2, Y[j] * 0x1.0p63);
                 // Now see how that y-coordinate compares to the curve
                 if (y <= Math.exp(-0.5*x*x)) {
                     break;   // The chosen point is below the curve; accept it.
