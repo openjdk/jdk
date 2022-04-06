bea66c1f1021d4ee67b7f20fc98fc7682ac5eb53
Oaiqiy <84843355+Oaiqiy@users.noreply.github.com>
2022-04-06 14:51:07 +0800

    Improve return speed when the rem is 0
    Improve the return 0 speed when the remaing byte is zero.

--- src/java.base/share/classes/sun/nio/ch/IOUtil.java
+++ src/java.base/share/classes/sun/nio/ch/IOUtil.java
@@ -86,6 +86,8 @@ public class IOUtil {
         int lim = src.limit();
         assert (pos <= lim);
         int rem = (pos <= lim ? lim - pos : 0);
+        if(rem == 0)
+            return 0;
         ByteBuffer bb;
         if (directIO) {
             Util.checkRemainingBufferSizeAligned(rem, alignment);
@@ -126,9 +128,9 @@ public class IOUtil {
             Util.checkRemainingBufferSizeAligned(rem, alignment);
         }
 
-        int written = 0;
         if (rem == 0)
             return 0;
+        int written = 0;
         var handle = acquireScope(bb, async);
         try {
             if (position != -1) {
