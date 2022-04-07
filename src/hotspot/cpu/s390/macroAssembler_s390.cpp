934e71a03063bb08e7e9fee91c88768b5efc9f54
Lutz Schmidt <Lutz.Schmidt@sap.com>
2022-04-07 11:43:24 +0200

    8278757: [s390] Implement AES Counter Mode Intrinsic

--- src/hotspot/cpu/s390/macroAssembler_s390.cpp
+++ src/hotspot/cpu/s390/macroAssembler_s390.cpp
@@ -4667,6 +4667,22 @@ void MacroAssembler::kmc(Register dstBuff, Register srcBuff) {
   Assembler::z_brc(Assembler::bcondOverflow /* CC==3 (iterate) */, retry);
 }
 
+void MacroAssembler::kmctr(Register dstBuff, Register ctrBuff, Register srcBuff) {
+  // DstBuff and srcBuff are allowed to be the same register (encryption in-place).
+  // DstBuff and srcBuff storage must not overlap destructively, and neither must overlap the parameter block.
+  assert(srcBuff->encoding()     != 0, "src buffer address can't be in Z_R0");
+  assert(dstBuff->encoding()     != 0, "dst buffer address can't be in Z_R0");
+  assert(ctrBuff->encoding()     != 0, "ctr buffer address can't be in Z_R0");
+  assert(ctrBuff->encoding() % 2 == 0, "ctr buffer addr must be an even register");
+  assert(dstBuff->encoding() % 2 == 0, "dst buffer addr must be an even register");
+  assert(srcBuff->encoding() % 2 == 0, "src buffer addr/len must be an even/odd register pair");
+
+  Label retry;
+  bind(retry);
+  Assembler::z_kmctr(dstBuff, ctrBuff, srcBuff);
+  Assembler::z_brc(Assembler::bcondOverflow /* CC==3 (iterate) */, retry);
+}
+
 void MacroAssembler::cksm(Register crcBuff, Register srcBuff) {
   assert(srcBuff->encoding() % 2 == 0, "src buffer addr/len must be an even/odd register pair");
 
--- src/hotspot/cpu/s390/macroAssembler_s390.hpp
+++ src/hotspot/cpu/s390/macroAssembler_s390.hpp
@@ -248,8 +248,9 @@ class MacroAssembler: public Assembler {
   // Crypto instructions.
   // Being interruptible, these instructions need a retry-loop.
   void cksm(Register crcBuff, Register srcBuff);
-  void km( Register dstBuff, Register srcBuff);
-  void kmc(Register dstBuff, Register srcBuff);
+  void km(   Register dstBuff, Register srcBuff);
+  void kmc(  Register dstBuff, Register srcBuff);
+  void kmctr(Register dstBuff, Register ctrBuff, Register srcBuff);
   void kimd(Register srcBuff);
   void klmd(Register srcBuff);
   void kmac(Register srcBuff);
