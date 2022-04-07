86943027be9c662d965ead554b5a84e029f1577c
zelva <zelvalea@gmail.com>
2022-04-07 16:17:44 +0300

    unnecessary volatile for int hash field

--- src/java.base/share/classes/sun/util/locale/BaseLocale.java
+++ src/java.base/share/classes/sun/util/locale/BaseLocale.java
@@ -97,7 +97,7 @@ public final class BaseLocale {
     private final String region;
     private final String variant;
 
-    private volatile int hash;
+    private int hash;
 
     /**
      * Boolean for the old ISO language code compatibility.
