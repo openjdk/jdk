2bc1675a9290f03f809bc99249102901d7183da1
Andrey Turbanov <turbanoff@gmail.com>
2022-04-08 09:09:05 +0300

    [PATCH] Collapse identical catch branches in java.base

--- src/java.base/linux/classes/jdk/internal/platform/CgroupSubsystemController.java
+++ src/java.base/linux/classes/jdk/internal/platform/CgroupSubsystemController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Red Hat Inc.
+ * Copyright (c) 2020, 2022, Red Hat Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
