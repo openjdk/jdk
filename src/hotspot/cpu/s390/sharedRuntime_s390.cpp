a445ecd1e9e255e9cd977c727b3dba8f17acf5d9
Stefan Karlsson <stefank@openjdk.org>
2022-04-08 07:53:32 +0000

    8284297: Move FILE_AND_LINE to a platform independent header
    Reviewed-by: tschatzl

--- src/hotspot/cpu/s390/sharedRuntime_s390.cpp
+++ src/hotspot/cpu/s390/sharedRuntime_s390.cpp
@@ -45,6 +45,7 @@
 #include "runtime/stubRoutines.hpp"
 #include "runtime/vframeArray.hpp"
 #include "utilities/align.hpp"
+#include "utilities/macros.hpp"
 #include "vmreg_s390.inline.hpp"
 #ifdef COMPILER1
 #include "c1/c1_Runtime1.hpp"
