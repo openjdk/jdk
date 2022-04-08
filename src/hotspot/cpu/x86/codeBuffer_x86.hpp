975b9c22211094e1a5e961fe1800c5a20be356f9
Evgeny Astigeevich <eastig@amazon.com>
2022-04-08 20:09:45 +0000

    Add x86 implementation

--- src/hotspot/cpu/x86/codeBuffer_x86.hpp
+++ src/hotspot/cpu/x86/codeBuffer_x86.hpp
@@ -31,6 +31,6 @@ private:
 
 public:
   void flush_bundle(bool start_new_bundle) {}
-  static bool supports_shared_stubs() { return false; }
+  static bool supports_shared_stubs() { return true; }
 
 #endif // CPU_X86_CODEBUFFER_X86_HPP
