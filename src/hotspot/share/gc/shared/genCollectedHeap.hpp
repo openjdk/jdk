755bfcb2d46446ab4c1f802852ecac3b127ecc07
Albert Mingkun Yang <ayang@openjdk.org>
2022-04-11 06:25:23 +0000

    8284581: Serial: Remove unused GenCollectedHeap::collect_locked
    Reviewed-by: tschatzl

--- src/hotspot/share/gc/shared/genCollectedHeap.hpp
+++ src/hotspot/share/gc/shared/genCollectedHeap.hpp
@@ -192,9 +192,6 @@ public:
   // supports. Caller does not hold the Heap_lock on entry.
   virtual void collect(GCCause::Cause cause);
 
-  // The same as above but assume that the caller holds the Heap_lock.
-  void collect_locked(GCCause::Cause cause);
-
   // Perform a full collection of generations up to and including max_generation.
   // Mostly used for testing purposes. Caller does not hold the Heap_lock on entry.
   void collect(GCCause::Cause cause, GenerationType max_generation);
