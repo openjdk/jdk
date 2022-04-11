291ca3b7732748a9e7322b2f63e1f660a3bdadb6
Claes Redestad <claes.redestad@oracle.com>
2022-04-11 16:44:35 +0200

    Fix imports

--- test/micro/org/openjdk/bench/java/lang/invoke/Wrappers.java
+++ test/micro/org/openjdk/bench/java/lang/invoke/Wrappers.java
@@ -24,14 +24,17 @@ package org.openjdk.bench.java.lang.invoke;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
+import java.util.concurrent.TimeUnit;
 import sun.invoke.util.Wrapper;
 
 /**
