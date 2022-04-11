d5b1a5756d51e1e35d960c5030872993aa38a442
Pavel Rappo <prappo@openjdk.org>
2022-04-11 22:34:38 +0100

    Reorder checks

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/VisibleMemberTable.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/VisibleMemberTable.java
@@ -463,7 +463,8 @@ public class VisibleMemberTable {
     }
 
     private boolean mustDocument(Element e) {
-        return !utils.hasHiddenTag(e) && utils.shouldDocument(e);
+        // these checks are ordered in a particular way to avoid parsing unless absolutely necessary
+        return utils.shouldDocument(e) && !utils.hasHiddenTag(e);
     }
 
     private boolean allowInheritedMembers(Element e, Kind kind, LocalMemberTable lmt) {
