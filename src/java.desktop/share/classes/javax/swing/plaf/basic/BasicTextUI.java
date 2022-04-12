8a005f4998759d0ed51a136a115ca7c0b461dbb5
Andrey Turbanov <turbanoff@gmail.com>
2022-04-12 22:37:10 +0300

    [PATCH] Unnecessary string length argument for String.substring in java.desktop

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTextUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTextUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -151,7 +151,7 @@ public abstract class BasicTextUI extends TextUI implements ViewFactory {
         String nm = getClass().getName();
         int index = nm.lastIndexOf('.');
         if (index >= 0) {
-            nm = nm.substring(index+1, nm.length());
+            nm = nm.substring(index+1);
         }
         return nm;
     }
--- src/java.desktop/unix/classes/sun/awt/X11/XFileDialogPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XFileDialogPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -728,7 +728,7 @@ class XFileDialogPeer extends XDialogPeer
         int i;
         if ((i=dir.indexOf("~")) != -1) {
 
-            dir = dir.substring(0,i) + System.getProperty("user.home") + dir.substring(i+1,dir.length());
+            dir = dir.substring(0,i) + System.getProperty("user.home") + dir.substring(i+1);
         }
 
         File fe = new File(dir).getAbsoluteFile();
