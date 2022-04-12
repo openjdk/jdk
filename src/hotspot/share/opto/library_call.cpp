0d706db33280053de92b7f7bbf53ff3bee471774
vamsi-parasa <srinivas.vamsi.parasa@intel.com>
2022-04-12 13:39:06 -0700

    fix code style

--- src/hotspot/share/opto/library_call.cpp
+++ src/hotspot/share/opto/library_call.cpp
@@ -2200,27 +2200,43 @@ bool LibraryCallKit::inline_number_methods(vmIntrinsics::ID id) {
 // inline long Long.remainderUnsigned(long, long)
 bool LibraryCallKit::inline_divmod_methods(vmIntrinsics::ID id) {
   Node* n = NULL;
-  switch(id) {
-    case vmIntrinsics::_divideUnsigned_i:
+  switch (id) {
+    case vmIntrinsics::_divideUnsigned_i: {
       zero_check_int(argument(1));
       // Compile-time detect of null-exception
-      if (stopped()) return true; // keep the graph constructed so far
-      n = new UDivINode(control(), argument(0), argument(1)); break;
-    case vmIntrinsics::_divideUnsigned_l:
+      if (stopped()) {
+        return true; // keep the graph constructed so far
+      }
+      n = new UDivINode(control(), argument(0), argument(1));
+      break;
+    }
+    case vmIntrinsics::_divideUnsigned_l: {
       zero_check_long(argument(2));
       // Compile-time detect of null-exception
-      if (stopped()) return true; // keep the graph constructed so far
-      n = new UDivLNode(control(), argument(0), argument(2));  break;
-    case vmIntrinsics::_remainderUnsigned_i:
+      if (stopped()) {
+        return true; // keep the graph constructed so far
+      }
+      n = new UDivLNode(control(), argument(0), argument(2));
+      break;
+    }
+    case vmIntrinsics::_remainderUnsigned_i: {
       zero_check_int(argument(1));
       // Compile-time detect of null-exception
-      if (stopped()) return true; // keep the graph constructed so far
-      n = new UModINode(control(), argument(0), argument(1));  break;
-    case vmIntrinsics::_remainderUnsigned_l:
+      if (stopped()) {
+        return true; // keep the graph constructed so far
+      }
+      n = new UModINode(control(), argument(0), argument(1));
+      break;
+    }
+    case vmIntrinsics::_remainderUnsigned_l: {
       zero_check_long(argument(2));
       // Compile-time detect of null-exception
-      if (stopped()) return true; // keep the graph constructed so far
-      n = new UModLNode(control(), argument(0), argument(2));  break;
+      if (stopped()) {
+        return true; // keep the graph constructed so far
+      }
+      n = new UModLNode(control(), argument(0), argument(2));
+      break;
+    }
     default:  fatal_unexpected_iid(id);  break;
   }
   set_result(_gvn.transform(n));
