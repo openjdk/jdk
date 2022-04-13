f52442b67de1457dee6c25d82c0d984788d85b08
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2022-04-13 16:45:52 +0800

    Fix again: MachNode size mismatch

--- src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -378,7 +378,7 @@ void MacroAssembler::verify_oop(Register reg, const char* s) {
   push_reg(RegSet::of(ra, t0, t1, c_rarg0), sp);
 
   mv(c_rarg0, reg); // c_rarg0 : x10
-  li(t0, (uintptr_t)(address)b);
+  mv(t0, (address)b);
 
   // call indirectly to solve generation ordering problem
   int32_t offset = 0;
@@ -414,7 +414,7 @@ void MacroAssembler::verify_oop_addr(Address addr, const char* s) {
     ld(x10, addr);
   }
 
-  li(t0, (uintptr_t)(address)b);
+  mv(t0, (address)b);
 
   // call indirectly to solve generation ordering problem
   int32_t offset = 0;
