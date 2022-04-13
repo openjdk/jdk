8ee2944cc404d4d53d0f94b56dd52111fd31cc39
Matthias Baesken <mbaesken@openjdk.org>
2022-04-13 11:49:00 +0000

    8284754: print more interesting env variables in hs_err and VM.info
    Reviewed-by: stuefe

--- src/hotspot/share/utilities/vmError.cpp
+++ src/hotspot/share/utilities/vmError.cpp
@@ -101,7 +101,8 @@ static const char* env_list[] = {
   // Env variables that are defined on Linux/BSD
   "LD_LIBRARY_PATH", "LD_PRELOAD", "SHELL", "DISPLAY",
   "HOSTTYPE", "OSTYPE", "ARCH", "MACHTYPE",
-  "LANG", "LC_ALL", "LC_CTYPE", "TZ",
+  "LANG", "LC_ALL", "LC_CTYPE", "LC_NUMERIC", "LC_TIME",
+  "TERM", "TMPDIR", "TZ",
 
   // defined on AIX
   "LIBPATH", "LDR_PRELOAD", "LDR_PRELOAD64",
