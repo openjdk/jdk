aac07eaf5457caea884c2390a4c2c0f3b397d770
tstuefe <thomas.stuefe@gmail.com>
2022-04-14 11:45:25 +0200

    modify comment

--- test/jdk/java/lang/ProcessHandle/OnExitTest.java
+++ test/jdk/java/lang/ProcessHandle/OnExitTest.java
@@ -96,14 +96,15 @@ public class OnExitTest extends ProcessUtil {
     @Test
     public static void test2() {
 
-        // Please note:
+        // Please note (JDK-8284282):
         //
         // On Unix, this test relies on the ability of the system to adopt orphaned processes and
         // reap them in a timely fashion. In other words, the ability to prevent orphans from becoming
         // zombies.
         //
         // Therefore, on misconfigured or broken systems, this test may fail. These failures will manifest
-        // as timeouts.
+        // as timeouts. The failures depend on timing: they may not happen at all, be intermittent or
+        // constant.
         //
         // That will rarely be a problem on bare-metal systems but may be more common when running in
         // Docker. Misconfigured Docker instances may run with an initial process unable to reap. One
