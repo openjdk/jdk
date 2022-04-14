f63c7cfe1ba49cb4ecc60bd067e592ec3fc28966
Magnus Ihse Bursie <ihse@openjdk.org>
2022-04-14 21:41:45 +0000

    8284894: Fix typos in langtools
    Reviewed-by: jjg

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -3337,7 +3337,7 @@ public class Check {
 
     boolean annotationApplicable(JCAnnotation a, Symbol s) {
         Optional<Set<Name>> targets = getApplicableTargets(a, s);
-        /* the optional could be emtpy if the annotation is unknown in that case
+        /* the optional could be empty if the annotation is unknown in that case
          * we return that it is applicable and if it is erroneous that should imply
          * an error at the declaration site
          */
