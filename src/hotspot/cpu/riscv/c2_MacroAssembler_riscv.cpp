b8e5cb0b1dde52b21776d05db678ebd7083f90c0
Feilong Jiang <fjiang@openjdk.org>
2022-04-15 06:12:48 +0000

    8284863: riscv: missing side effect for result in instruct vcount_positives
    Reviewed-by: fyang, shade

--- src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
@@ -1546,6 +1546,8 @@ void C2_MacroAssembler::count_positives_v(Register ary, Register len, Register r
   Label LOOP, SET_RESULT, DONE;
 
   BLOCK_COMMENT("count_positives_v {");
+  assert_different_registers(ary, len, result, tmp);
+
   mv(result, zr);
 
   bind(LOOP);
