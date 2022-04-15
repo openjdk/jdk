7e24327dd7a802ce6cb14c4be43f1929d3901a08
Doug Simon <dnsimon@openjdk.org>
2022-04-15 22:16:47 +0000

    8284921: tier1 test failures after JDK-8284909
    Reviewed-by: kvn

--- src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
+++ src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
@@ -2687,7 +2687,6 @@ C2V_VMENTRY_0(jlong, getThreadLocalLong, (JNIEnv* env, jobject, jint id))
 #define HS_CONSTANT_POOL        "Ljdk/vm/ci/hotspot/HotSpotConstantPool;"
 #define HS_COMPILED_CODE        "Ljdk/vm/ci/hotspot/HotSpotCompiledCode;"
 #define HS_CONFIG               "Ljdk/vm/ci/hotspot/HotSpotVMConfig;"
-#define HS_METADATA             "Ljdk/vm/ci/hotspot/HotSpotMetaData;"
 #define HS_STACK_FRAME_REF      "Ljdk/vm/ci/hotspot/HotSpotStackFrameReference;"
 #define HS_SPECULATION_LOG      "Ljdk/vm/ci/hotspot/HotSpotSpeculationLog;"
 #define METASPACE_OBJECT        "Ljdk/vm/ci/hotspot/MetaspaceObject;"
