cc5b0b5b53c61791fe2c6ef4bfeec7a7876bda3a
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-04-16 18:33:15 +0100

    8284189: Miscellaneous spelling fixes

--- src/java.desktop/share/classes/javax/swing/text/TextLayoutStrategy.java
+++ src/java.desktop/share/classes/javax/swing/text/TextLayoutStrategy.java
@@ -44,7 +44,7 @@ import javax.swing.event.DocumentEvent;
 import sun.font.BidiUtils;
 
 /**
- * A flow strategy that uses java.awt.font.LineBreakMeasureer to
+ * A flow strategy that uses java.awt.font.LineBreakMeasurer to
  * produce java.awt.font.TextLayout for i18n capable rendering.
  * If the child view being placed into the flow is of type
  * GlyphView and can be rendered by TextLayout, a GlyphPainter
@@ -109,7 +109,7 @@ class TextLayoutStrategy extends FlowView.FlowStrategy {
     /**
      * Does a full layout on the given View.  This causes all of
      * the rows (child views) to be rebuilt to match the given
-     * constraints for each row.  This is called by a FlowView.layout
+     * constraints for each row.  This is called by FlowView.layout
      * to update the child views in the flow.
      *
      * @param fv the view to reflow
@@ -128,7 +128,7 @@ class TextLayoutStrategy extends FlowView.FlowStrategy {
      * @param rowIndex the row to fill in with views.  This is assumed
      *   to be empty on entry.
      * @param p0  The current position in the children of
-     *   this views element from which to start.
+     *   this view element from which to start.
      * @return the position to start the next row
      */
     protected int layoutRow(FlowView fv, int rowIndex, int p0) {
@@ -180,7 +180,7 @@ class TextLayoutStrategy extends FlowView.FlowStrategy {
      *
      * @param fv the view holding the flow
      * @param startOffset the start location for the view being created
-     * @param spanLeft the about of span left to fill in the row
+     * @param spanLeft the amount of span left to fill in the row
      * @param rowIndex the row the view will be placed into
      */
     protected View createView(FlowView fv, int startOffset, int spanLeft, int rowIndex) {
