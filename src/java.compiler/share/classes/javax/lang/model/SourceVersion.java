d5bac8c0c444406383c45a12e4c0878da39f90fe
Joe Darcy <darcy@openjdk.org>
2022-04-18 17:17:29 +0000

    8284923: Update description of SourceVersion.RELEASE_18
    Reviewed-by: jjg, iris

--- src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
+++ src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
@@ -63,8 +63,10 @@ public enum SourceVersion {
      *  15: text blocks (sealed classes in preview; records and pattern
      *      matching in second preview)
      *  16: records and pattern matching (sealed classes in second preview)
-     *  17: sealed classes, floating-point always strict
-     *  18: TBD
+     *  17: sealed classes, floating-point always strict (pattern
+     *      matching for switch in preview)
+     *  18: no changes (pattern matching for switch in second preview)
+     *  19: TBD
      */
 
     /**
@@ -246,6 +248,8 @@ public enum SourceVersion {
     /**
      * The version recognized by the Java Platform, Standard Edition
      * 18.
+     *
+     * No major changes from the prior release.
      *
      * @since 18
      */
