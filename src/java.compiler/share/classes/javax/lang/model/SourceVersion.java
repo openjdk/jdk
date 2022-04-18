ffdeb32062d510c291e1a89031bed626ee892537
Joe Darcy <darcy@openjdk.org>
2022-04-18 17:42:23 +0000

    8284928: Add links from SourceVersion to specific JLS versions
    Reviewed-by: iris

--- src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
+++ src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
@@ -129,6 +129,10 @@ public enum SourceVersion {
      * 6.
      *
      * No major changes from {@code RELEASE_5}.
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se6/html/j3TOC.html">
+     * <cite>The Java Language Specification, Third Edition</cite></a>
      */
     RELEASE_6,
 
@@ -140,6 +144,10 @@ public enum SourceVersion {
      * constructors, {@code try}-with-resources, strings in switch,
      * binary literals, and multi-catch.
      * @since 1.7
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se7/html/index.html">
+     * <cite>The Java Language Specification, Java SE 7 Edition</cite></a>
      */
     RELEASE_7,
 
@@ -149,6 +157,10 @@ public enum SourceVersion {
      *
      * Additions in this release include lambda expressions and default methods.
      * @since 1.8
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se8/html/index.html">
+     * <cite>The Java Language Specification, Java SE 8 Edition</cite></a>
      */
     RELEASE_8,
 
@@ -160,6 +172,10 @@ public enum SourceVersion {
      * single underscore from the set of legal identifier names.
      *
      * @since 9
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se9/html/index.html">
+     * <cite>The Java Language Specification, Java SE 9 Edition</cite></a>
      */
      RELEASE_9,
 
@@ -171,6 +187,10 @@ public enum SourceVersion {
      * ({@code var}).
      *
      * @since 10
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se10/html/index.html">
+     * <cite>The Java Language Specification, Java SE 10 Edition</cite></a>
      */
     RELEASE_10,
 
@@ -182,6 +202,10 @@ public enum SourceVersion {
      * lambda parameters.
      *
      * @since 11
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se11/html/index.html">
+     * <cite>The Java Language Specification, Java SE 11 Edition</cite></a>
      */
     RELEASE_11,
 
@@ -191,6 +215,10 @@ public enum SourceVersion {
      * No major changes from the prior release.
      *
      * @since 12
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se12/html/index.html">
+     * <cite>The Java Language Specification, Java SE 12 Edition</cite></a>
      */
     RELEASE_12,
 
@@ -200,6 +228,10 @@ public enum SourceVersion {
      * No major changes from the prior release.
      *
      * @since 13
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se13/html/index.html">
+     * <cite>The Java Language Specification, Java SE 13 Edition</cite></a>
      */
     RELEASE_13,
 
@@ -210,6 +242,10 @@ public enum SourceVersion {
      * Additions in this release include switch expressions.
      *
      * @since 14
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se14/html/index.html">
+     * <cite>The Java Language Specification, Java SE 14 Edition</cite></a>
      */
     RELEASE_14,
 
@@ -220,6 +256,10 @@ public enum SourceVersion {
      * Additions in this release include text blocks.
      *
      * @since 15
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se15/html/index.html">
+     * <cite>The Java Language Specification, Java SE 15 Edition</cite></a>
      */
     RELEASE_15,
 
@@ -231,6 +271,10 @@ public enum SourceVersion {
      * for {@code instanceof}.
      *
      * @since 16
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se16/html/index.html">
+     * <cite>The Java Language Specification, Java SE 16 Edition</cite></a>
      */
     RELEASE_16,
 
@@ -242,6 +286,10 @@ public enum SourceVersion {
      * restoration of always-strict floating-point semantics.
      *
      * @since 17
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se17/html/index.html">
+     * <cite>The Java Language Specification, Java SE 17 Edition</cite></a>
      */
     RELEASE_17,
 
@@ -252,6 +300,10 @@ public enum SourceVersion {
      * No major changes from the prior release.
      *
      * @since 18
+     *
+     * @see <a
+     * href="https://docs.oracle.com/javase/specs/jls/se18/html/index.html">
+     * <cite>The Java Language Specification, Java SE 18 Edition</cite></a>
      */
     RELEASE_18,
 
