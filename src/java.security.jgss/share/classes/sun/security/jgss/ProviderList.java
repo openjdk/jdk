c63fabe3d582ce0828b04b0224cea49aab5fedf3
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2022-04-18 14:07:56 +0000

    8284935: Improve debug in java.security.jgss
    Reviewed-by: mullan

--- src/java.security.jgss/share/classes/sun/security/jgss/ProviderList.java
+++ src/java.security.jgss/share/classes/sun/security/jgss/ProviderList.java
@@ -134,8 +134,10 @@ public final class ProviderList {
                 addProviderAtEnd(prov, null);
             } catch (GSSException ge) {
                 // Move on to the next provider
-                GSSUtil.debug("Error in adding provider " +
-                              prov.getName() + ": " + ge);
+                if (GSSUtil.DEBUG) {
+                    GSSUtil.debug("Error in adding provider " +
+                            prov.getName() + ": " + ge);
+                }
             }
         } // End of for loop
     }
@@ -421,8 +423,10 @@ public final class ProviderList {
                     retVal = true;
                 } catch (GSSException e) {
                     // Skip to next property
-                    GSSUtil.debug("Ignore the invalid property " +
-                                  prop + " from provider " + p.getName());
+                    if (GSSUtil.DEBUG) {
+                        GSSUtil.debug("Ignore the invalid property " +
+                                prop + " from provider " + p.getName());
+                    }
                 }
             } // Processed GSS property
         } // while loop
