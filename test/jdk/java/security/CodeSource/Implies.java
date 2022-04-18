69b79c6ca1ebe8de1b07bde0e69e67390ec5fbd7
Sibabrata Sahoo <sibabrata.sahoo@oracle.com>
2022-04-18 15:13:41 +0530

    8209935: Test to cover CodeSource.getCodeSigners()

--- test/jdk/java/security/CodeSource/Implies.java
+++ test/jdk/java/security/CodeSource/Implies.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,16 +21,16 @@
  * questions.
  */
 
-/*
+ /*
  * @test
  * @bug 4866847 7152564 7155693
  * @summary various CodeSource.implies tests
  */
-
 import java.security.CodeSource;
 import java.net.URL;
 
 public class Implies {
+
     public static void main(String[] args) throws Exception {
         URL thisURL = new URL("http", "localhost", "file");
         URL thatURL = new URL("http", null, "file");
@@ -51,14 +51,16 @@ public class Implies {
     }
 
     private static void testImplies(URL thisURL, URL thatURL, boolean result)
-        throws SecurityException
-    {
-        CodeSource thisCs =
-            new CodeSource(thisURL, (java.security.cert.Certificate[]) null);
-        CodeSource thatCs =
-            new CodeSource(thatURL, (java.security.cert.Certificate[]) null);
+            throws SecurityException {
+        CodeSource thisCs
+                = new CodeSource(thisURL, (java.security.cert.Certificate[]) null);
+        CodeSource thatCs
+                = new CodeSource(thatURL, (java.security.cert.Certificate[]) null);
         if (thisCs.implies(thatCs) != result) {
             throw new SecurityException("test failed");
+        }
+        if (thisCs.getCodeSigners() != null && thatCs.getCodeSigners() != null) {
+            throw new RuntimeException("Both getCodeSigners should be null");
         }
     }
 }
