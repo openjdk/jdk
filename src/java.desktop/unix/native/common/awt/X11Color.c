a4ffbbc31608d726167977238d30aeaca4924d26
Zhengyu Gu <zgu@redhat.com>
2022-04-18 16:00:26 -0400

    v1

--- src/java.desktop/unix/native/common/awt/X11Color.c
+++ src/java.desktop/unix/native/common/awt/X11Color.c
@@ -299,6 +299,27 @@ awt_fill_imgcv(ImgConvertFcn **array, int mask, int value, ImgConvertFcn fcn)
 }
 
 #ifndef HEADLESS
+void free_graphics_config_data_internal(AwtGraphicsConfigDataPtr awt_data) {
+    if (awt_data->awtImage != NULL) {
+        free(awt_data->awtImage);
+        awt_data->awtImage = NULL;
+    }
+
+    if (awt_data->color_data != NULL) {
+        if (awt_data->color_data->img_clr_tbl != NULL) {
+            free(awt_data->color_data->img_clr_tbl);
+        }
+        if (awt_data->color_data->awt_icmLUT2Colors != NULL) {
+            free(awt_data->color_data->awt_icmLUT2Colors);
+        }
+        if (awt_data->color_data->awt_icmLUT != NULL) {
+            free(awt_data->color_data->awt_icmLUT);
+        }
+        free(awt_data->color_data);
+        awt_data->color_data = NULL;
+    }
+}
+
 /*
  * called from X11Server_create() in xlib.c
  */
@@ -320,6 +341,9 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
     char *forcemono;
     char *forcegray;
 
+    // Clean up awt_data for reuse, avoid memory leak
+    free_graphics_config_data_internal(awt_data);
+
     make_uns_ordered_dither_array(img_oda_alpha, 256);
 
 
@@ -336,14 +360,11 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
     depth = awt_data->awt_depth;
     pVI = &awt_data->awt_visInfo;
     awt_data->awt_num_colors = awt_data->awt_visInfo.colormap_size;
+    awt_data->awtImage = (awtImageData *) calloc(1, sizeof(awtImageData));
     if (awt_data->awtImage == NULL) {
-        awt_data->awtImage = (awtImageData *) calloc(1, sizeof(awtImageData));
-        if (awt_data->awtImage == NULL) {
-            return 0;
-        }
-    } else {
-        memset((void*)awt_data->awtImage, 0, sizeof(awtImageData));
+        return 0;
     }
+
     pPFV = XListPixmapFormats(dpy, &numpfv);
     if (pPFV) {
         for (i = 0; i < numpfv; i++) {
@@ -498,7 +519,7 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
                        OrdColorDcmOpqUnsImageConvert);
 #endif /* NEED_IMAGE_CONVERT */
     } else {
-        free (awt_data->awtImage);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
 
@@ -513,14 +534,14 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
     }
 
     if (awt_data->awt_num_colors > paletteSize) {
-        free(awt_data->awtImage);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
 
     /* Allocate ColorData structure */
     awt_data->color_data = ZALLOC (_ColorData);
     if (awt_data->color_data == NULL) {
-        free(awt_data->awtImage);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
 
@@ -541,8 +562,7 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
     awt_data->color_data->awt_Colors =
         (ColorEntry *)calloc(paletteSize, sizeof (ColorEntry));
     if (awt_data->color_data->awt_Colors == NULL) {
-        free(awt_data->awtImage);
-        free(awt_data->color_data);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
 
@@ -619,8 +639,7 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
         awt_data->color_data->img_grays =
             (unsigned char *)calloc(256, sizeof(unsigned char));
         if ( awt_data->color_data->img_grays == NULL) {
-            free(awt_data->awtImage);
-            free(awt_data->color_data);
+            free_graphics_config_data_internal(awt_data);
             return 0;
         }
         for (g = 0; g < 256; g++) {
@@ -764,13 +783,13 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
         k = getVirtCubeSize();
     }
     awt_data->color_data->img_clr_tbl =
-        (unsigned char *)calloc(LOOKUPSIZE * LOOKUPSIZE * LOOKUPSIZE,
-                                sizeof(unsigned char));
+            (unsigned char *) calloc(LOOKUPSIZE * LOOKUPSIZE * LOOKUPSIZE,
+                                     sizeof(unsigned char));
     if (awt_data->color_data->img_clr_tbl == NULL) {
-        free(awt_data->awtImage);
-        free(awt_data->color_data);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
+
     img_makePalette(cmapsize, k, LOOKUPSIZE, 50, 250,
                     allocatedColorsNum, TRUE, reds, greens, blues,
                     awt_data->color_data->img_clr_tbl);
@@ -816,12 +835,14 @@ awt_allocate_colors(AwtGraphicsConfigDataPtr awt_data)
 
     /* Fill in the ICM lut and lut2cmap mapping */
     awt_data->color_data->awt_numICMcolors = 0;
+    if (awt_data->color_data->awt_icmLUT2Colors != NULL) {
+        free(awt_data->color_data->awt_icmLUT2Colors);
+    }
     awt_data->color_data->awt_icmLUT2Colors =
         (unsigned char *)calloc(paletteSize, sizeof (unsigned char));
     awt_data->color_data->awt_icmLUT = (int *)calloc(paletteSize, sizeof(int));
     if (awt_data->color_data->awt_icmLUT2Colors == NULL || awt_data->color_data->awt_icmLUT == NULL) {
-        free(awt_data->awtImage);
-        free(awt_data->color_data);
+        free_graphics_config_data_internal(awt_data);
         return 0;
     }
 
