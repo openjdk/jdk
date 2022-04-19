75fd291b6b0309541d50b4f9a844cc88a1d9bf59
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-04-19 17:06:27 +0200

    do not run the test with ZGC

--- test/hotspot/jtreg/gc/arguments/TestUseCompressedOopsFlagsWithUlimit.java
+++ test/hotspot/jtreg/gc/arguments/TestUseCompressedOopsFlagsWithUlimit.java
@@ -32,6 +32,7 @@ package gc.arguments;
  * @library /
  * @requires vm.bits == "64"
  * @requires os.family != "aix" & os.family != "windows"
+ * @requires vm.gc != "Z"
  * @run driver gc.arguments.TestUseCompressedOopsFlagsWithUlimit
  */
 
