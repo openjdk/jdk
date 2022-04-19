fc03bca0ff5e0ff841d41e6cf1eb4133c9993056
Xue-Lei Andrew Fan <xuelei@openjdk.org>
2022-04-19 21:53:08 +0000

    8284855: Update needed to Cleaners added to jdk.crypto.cryptoki
    Reviewed-by: valeriep

--- src/jdk.crypto.cryptoki/windows/native/libj2pkcs11/p11_md.c
+++ src/jdk.crypto.cryptoki/windows/native/libj2pkcs11/p11_md.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  */
 
 /* Copyright  (c) 2002 Graz University of Technology. All rights reserved.
@@ -275,19 +275,21 @@ cleanup:
 /*
  * Class:     sun_security_pkcs11_wrapper_PKCS11
  * Method:    disconnect
- * Signature: ()V
+ * Signature: (J)V
  */
-JNIEXPORT void JNICALL Java_sun_security_pkcs11_wrapper_PKCS11_disconnect
-    (JNIEnv *env, jobject obj)
-{
-    ModuleData *moduleData;
-    TRACE0("DEBUG: disconnecting module...");
-    moduleData = removeModuleEntry(env, obj);
+JNIEXPORT void JNICALL Java_sun_security_pkcs11_wrapper_PKCS11_disconnect(
+        JNIEnv *env, jclass thisClass, jlong ckpNativeData) {
 
-    if (moduleData != NULL) {
-        FreeLibrary(moduleData->hModule);
+    TRACE0("DEBUG: disconnecting module...");
+    if (ckpNativeData != 0L) {
+        ModuleData *moduleData = jlong_to_ptr(ckpNativeData);
+
+        if (moduleData->hModule != NULL) {
+            FreeLibrary(moduleData->hModule);
+        }
+
+        free(moduleData);
     }
 
-    free(moduleData);
     TRACE0("FINISHED\n");
 }
