d74d78c8151ca67656e5217d3e0c3f6bf8a31139
Claes Redestad <claes.redestad@oracle.com>
2022-04-19 10:36:11 +0200

    Simplify latin1 to latin1 locale-independent comparisons in StringLatin1.regionMatchesCI

--- src/java.base/share/classes/java/lang/StringLatin1.java
+++ src/java.base/share/classes/java/lang/StringLatin1.java
@@ -398,9 +398,6 @@ final class StringLatin1 {
             if (u1 == u2) {
                 continue;
             }
-            if (Character.toLowerCase(u1) == Character.toLowerCase(u2)) {
-                continue;
-            }
             return false;
         }
         return true;
