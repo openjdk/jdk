8f6c6d9c8502f768ff9e1af8f97e9aa3924c4082
Magnus Ihse Bursie <ihse@openjdk.org>
2022-04-19 20:07:57 +0000

    8284893: Fix typos in java.base
    Reviewed-by: iris, wetmore, lancea, mullan, naoto

--- src/java.base/share/classes/java/util/zip/ZipUtils.java
+++ src/java.base/share/classes/java/util/zip/ZipUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -283,7 +283,7 @@ class ZipUtils {
     static final int READBLOCKSZ = 128;
 
     /**
-     * Loads zip native library, if not already laoded
+     * Loads zip native library, if not already loaded
      */
     static void loadLibrary() {
         jdk.internal.loader.BootLoader.loadLibrary("zip");
