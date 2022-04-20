5b5cfce385dea98cf6a4f4c0d5b6d8f274c92dba
Tim Prinzing <tim.prinzing@oracle.com>
2022-04-20 17:02:31 +0000

    8281006: Module::getResourceAsStream should check if the resource is open unconditionally when caller is null
    Reviewed-by: alanb, erikj, mchung

--- src/java.base/share/classes/java/lang/Class.java
+++ src/java.base/share/classes/java/lang/Class.java
@@ -3002,9 +3002,9 @@ public final class Class<T> implements java.io.Serializable,
         if (callerModule != thisModule) {
             String pn = Resources.toPackageName(name);
             if (thisModule.getDescriptor().packages().contains(pn)) {
-                if (callerModule == null && !thisModule.isOpen(pn)) {
-                    // no caller, package not open
-                    return false;
+                if (callerModule == null) {
+                    // no caller, return true if the package is open to all modules
+                    return thisModule.isOpen(pn);
                 }
                 if (!thisModule.isOpen(pn, callerModule)) {
                     // package not open to caller
