83b3bd3509044b5cab91e991e2681fe06a6674e2
Eric Liu <eliu@openjdk.org>
2022-04-20 00:55:56 +0000

    8284563: AArch64: bitperm feature detection for SVE2 on Linux
    Reviewed-by: aph, njian

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
@@ -432,7 +432,7 @@ class MacroAssembler: public Assembler {
 
 #define WRAP(INSN)                                                            \
   void INSN(Register Rd, Register Rn, Register Rm, Register Ra) {             \
-    if ((VM_Version::features() & VM_Version::CPU_A53MAC) && Ra != zr)        \
+    if (VM_Version::supports_a53mac() && Ra != zr)                            \
       nop();                                                                  \
     Assembler::INSN(Rd, Rn, Rm, Ra);                                          \
   }
