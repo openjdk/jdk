4f2d483894742accd01ce5062c853420f09a79d4
XenoAmess <xenoamess@gmail.com>
2022-04-21 03:19:22 +0800

    add more replaces

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Operators.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Operators.java
@@ -74,10 +74,10 @@ public class Operators {
     private final Types types;
 
     /** Unary operators map. */
-    private Map<Name, List<UnaryOperatorHelper>> unaryOperators = HashMap.newHashMap(Tag.getNumberOfOperators());
+    private Map<Name, List<UnaryOperatorHelper>> unaryOperators = new HashMap<>((int) Math.ceil(Tag.getNumberOfOperators() / 0.75));
 
     /** Binary operators map. */
-    private Map<Name, List<BinaryOperatorHelper>> binaryOperators = HashMap.newHashMap(Tag.getNumberOfOperators());
+    private Map<Name, List<BinaryOperatorHelper>> binaryOperators = new HashMap<>((int) Math.ceil(Tag.getNumberOfOperators() / 0.75));
 
     /** The names of all operators. */
     private Name[] opname = new Name[Tag.getNumberOfOperators()];
