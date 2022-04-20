b84ab2a3ed4bfe957a5fb6310762c5b1a2bd39fa
bobpengxie <bobpengxie@tencent.com>
2022-04-21 00:05:39 +0800

    add test

--- test/hotspot/jtreg/containers/docker/TestMemoryAwareness.java
+++ test/hotspot/jtreg/containers/docker/TestMemoryAwareness.java
@@ -61,6 +61,8 @@ public class TestMemoryAwareness {
             testMemorySoftLimit("500m", "524288000");
             testMemorySoftLimit("1g", "1073741824");
 
+            testMemorySwapLimit("100m", "100m");
+
             // Add extra 10 Mb to allocator limit, to be sure to cause OOM
             testOOM("256m", 256 + 10);
 
@@ -173,6 +175,19 @@ public class TestMemoryAwareness {
         } catch(RuntimeException ex) {
             out.shouldMatch("OperatingSystemMXBean\\.getFreeSwapSpaceSize: 0");
         }
+    }
+
+    private static void testMemorySwapLimit(String dockerMemSwapLimit, String initialHeapSize) throws Exception {
+        Common.logNewTestCase("Check initial heap size with memory and swap limit");
+
+        DockerRunOptions opts = Common.newOpts(imageName, "CheckInitialHeapSize")
+            .addDockerOpts("--memory", dockerMemSwapLimit, "--memory-swap", dockerMemSwapLimit)
+            .addJavaOptsAppended("-Xms" + initialHeapSize);
+
+        OutputAnalyzer out = DockerTestUtils.dockerRunJava(opts);
+        out.shouldContain("Error occurred during initialization of VM")
+           .shouldContain("Initial heap size set to a larger value than the container memory & swap limit");
+
     }
 
 }
