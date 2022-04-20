45be1d48b5f986ffc5bb0f2fbcea28b428606ebb
Evgeny Astigeevich <eastig@amazon.com>
2022-04-20 15:09:08 +0000

    Update copyright year and add Unimplemented guards

--- src/hotspot/cpu/arm/codeBuffer_arm.hpp
+++ src/hotspot/cpu/arm/codeBuffer_arm.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,7 +27,12 @@
 
 private:
   void pd_initialize() {}
-  bool pd_finalize_stubs() { return true; }
+  bool pd_finalize_stubs() {
+    if (_finalize_stubs) {
+      Unimplemented();
+    }
+    return true;
+  }
 
 public:
   void flush_bundle(bool start_new_bundle) {}
