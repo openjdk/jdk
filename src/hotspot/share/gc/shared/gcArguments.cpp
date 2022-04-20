ed6862cb7c4e7bc649ea4bf1e485bf11f434eac8
bobpengxie <bobpengxie@tencent.com>
2022-04-20 09:55:51 +0800

    rename memswBytes to limit

--- src/hotspot/share/gc/shared/gcArguments.cpp
+++ src/hotspot/share/gc/shared/gcArguments.cpp
@@ -143,8 +143,8 @@ void GCArguments::initialize_heap_flags_and_sizes() {
 
 #if defined(LINUX)
   if (OSContainer::is_containerized() && FLAG_IS_CMDLINE(InitialHeapSize)) {
-    jlong memswBytes = OSContainer::memory_and_swap_limit_in_bytes();
-    if ((memswBytes > 0) && (InitialHeapSize >= (julong) memswBytes)) {
+    jlong limit = OSContainer::memory_and_swap_limit_in_bytes();
+    if ((limit > 0) && (InitialHeapSize >= (julong) limit)) {
       vm_exit_during_initialization("Initial heap size set to a larger value than the container memory & swap limit");
     }
   }
