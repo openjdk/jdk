40d35c4c445de2f8bb310663bb021fe94b703162
Tobias Holenstein <tobias.holenstein@oracle.com>
2022-04-20 09:41:53 +0000

    8075816: Deprecate AliasLevel flag since it is broken
    Reviewed-by: thartmann, kvn

--- src/hotspot/share/opto/c2_globals.hpp
+++ src/hotspot/share/opto/c2_globals.hpp
@@ -630,7 +630,8 @@
           range(1, max_intx)                                                \
                                                                             \
   product(intx, AliasLevel,     3,                                          \
-          "0 for no aliasing, 1 for oop/field/static/array split, "         \
+          "(Deprecated) 0 for no aliasing, "                                \
+          "1 for oop/field/static/array split, "                            \
           "2 for class split, 3 for unique instances")                      \
           range(0, 3)                                                       \
           constraint(AliasLevelConstraintFunc,AfterErgo)                    \
