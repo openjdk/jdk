9c9645155dd90e8abb80e8e4c48afd42364ba69d
Andrey Turbanov <turbanoff@gmail.com>
2022-04-20 22:14:59 +0300

    [PATCH] Avoid redundant String allocation in WindowsPreferences.toJavaName

--- src/java.prefs/windows/classes/java/util/prefs/WindowsPreferences.java
+++ src/java.prefs/windows/classes/java/util/prefs/WindowsPreferences.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -929,8 +929,7 @@ class WindowsPreferences extends AbstractPreferences {
     private static String toJavaName(byte[] windowsNameArray) {
         String windowsName = byteArrayToString(windowsNameArray);
         // check if Alt64
-        if ((windowsName.length() > 1) &&
-                (windowsName.substring(0, 2).equals("/!"))) {
+        if (windowsName.startsWith("/!")) {
             return toJavaAlt64Name(windowsName);
         }
         StringBuilder javaName = new StringBuilder();
