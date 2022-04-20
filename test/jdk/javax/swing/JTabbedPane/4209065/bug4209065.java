2fd26ad6176bddfa2fb96556dda7463ddf00fa57
Harshitha Onkar <harshitha.onkar@oracle.com>
2022-04-20 16:46:51 -0700

    changed test case to use PassFailJFrame instead of Applet

--- test/jdk/javax/swing/JTabbedPane/4209065/bug4209065.java
+++ test/jdk/javax/swing/JTabbedPane/4209065/bug4209065.java
@@ -21,43 +21,65 @@
  * questions.
  */
 
-import java.awt.EventQueue;
-import java.lang.reflect.InvocationTargetException;
-
-import javax.swing.JApplet;
-import javax.swing.JLabel;
-import javax.swing.JTabbedPane;
-
-/**
+/*
  * @test
  * @bug 4209065
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
  * @summary To test if the style of the text on the tab matches the description.
- * @run applet/manual=yesno bug4209065.html
+ * @run main/manual bug4209065
  */
 
-public final class bug4209065 extends JApplet {
+import java.lang.reflect.InvocationTargetException;
 
-    @Override
-    public void init() {
-        try {
-            EventQueue.invokeAndWait(this::createTabbedPane);
-        } catch (InterruptedException | InvocationTargetException e) {
-            throw new RuntimeException(e);
-        }
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingUtilities;
+
+
+public final class bug4209065 {
+
+    private static JFrame frame;
+    private static final String text = """
+            If the style of the text on the tabs matches the descriptions,
+             press PASS.
+
+            NOTE: where a large font is used, the text may be larger than the
+             tab height but this is OK and NOT a failure.
+            """;
+
+    public static void createAndShowGUI() throws InterruptedException,
+            InvocationTargetException {
+        SwingUtilities.invokeAndWait(() -> {
+            frame = new JFrame("JTabbedPane");
+
+            JTabbedPane tp = new JTabbedPane();
+
+            tp.addTab("<html><center><font size=+3>big</font></center></html>",
+                    new JLabel());
+            tp.addTab("<html><center><font color=red>red</font></center></html>",
+                    new JLabel());
+            tp.addTab("<html><center><em><b>Bold Italic!</b></em></center></html>",
+                    new JLabel());
+
+            frame.getContentPane().add(tp);
+            frame.setSize(400, 400);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+
+
+            PassFailJFrame.addTestFrame(frame);
+            PassFailJFrame.positionTestFrame(frame,
+                    PassFailJFrame.Position.HORIZONTAL);
+        });
     }
 
-    private void createTabbedPane() {
-        JTabbedPane tp = new JTabbedPane();
-        getContentPane().add(tp);
-        String text = "<html><center>If the style of the text on the tabs matches"
-                      + "<br>the descriptions, press <em><b>PASS</b></em><br>"
-                      + "<br> NOTE: the text (tab title) may not always "
-                      + "fit the tab.</center></html>";
-        tp.addTab("<html><center><font size=+3>big</font></center></html>",
-                new JLabel(text));
-        tp.addTab("<html><center><font color=red>red</font></center></html>",
-                new JLabel(text));
-        tp.addTab("<html><center><em><b>Bold Italic!</b></em></center></html>",
-                new JLabel(text));
+    public static void main(String[] args) throws InterruptedException,
+            InvocationTargetException {
+        PassFailJFrame passFailJFrame = new PassFailJFrame("JTabbedPane " +
+                "Test Instructions", text, 5, 19, 35);
+        createAndShowGUI();
+        passFailJFrame.awaitAndCheck();
     }
 }
