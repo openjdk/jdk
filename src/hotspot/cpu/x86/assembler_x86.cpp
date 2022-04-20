b4a85cdae14eee895a0de2f26a2ffdd62b72bebc
Quan Anh Mai <anhmdq99@gmail.com>
2022-04-20 16:17:01 +0000

    8284742: x86: Handle integral division overflow during parsing
    Reviewed-by: kvn, mdoerr

--- src/hotspot/cpu/x86/assembler_x86.cpp
+++ src/hotspot/cpu/x86/assembler_x86.cpp
@@ -2235,6 +2235,13 @@ void Assembler::idivl(Register src) {
   emit_int16((unsigned char)0xF7, (0xF8 | encode));
 }
 
+void Assembler::idivl(Address src) {
+  InstructionMark im(this);
+  prefix(src);
+  emit_int8((unsigned char)0xF7);
+  emit_operand(as_Register(7), src);
+}
+
 void Assembler::divl(Register src) { // Unsigned
   int encode = prefix_and_encode(src->encoding());
   emit_int16((unsigned char)0xF7, (0xF0 | encode));
@@ -12364,6 +12371,13 @@ void Assembler::xsave(Address dst) {
 void Assembler::idivq(Register src) {
   int encode = prefixq_and_encode(src->encoding());
   emit_int16((unsigned char)0xF7, (0xF8 | encode));
+}
+
+void Assembler::idivq(Address src) {
+  InstructionMark im(this);
+  prefixq(src);
+  emit_int8((unsigned char)0xF7);
+  emit_operand(as_Register(7), src);
 }
 
 void Assembler::divq(Register src) {
--- src/hotspot/share/opto/parse2.cpp
+++ src/hotspot/share/opto/parse2.cpp
@@ -1802,7 +1802,7 @@ Node* Parse::optimize_cmp_with_klass(Node* c) {
 
 //------------------------------do_one_bytecode--------------------------------
 // Parse this bytecode, and alter the Parsers JVM->Node mapping
-void Parse::do_one_bytecode() {
+void Parse::do_one_bytecode_common() {
   Node *a, *b, *c, *d;          // Handy temps
   BoolTest::mask btest;
   int i;
@@ -2750,16 +2750,4 @@ void Parse::do_one_bytecode() {
     tty->print("\nUnhandled bytecode %s\n", Bytecodes::name(bc()) );
     ShouldNotReachHere();
   }
-
-#ifndef PRODUCT
-  if (C->should_print_igv(1)) {
-    IdealGraphPrinter* printer = C->igv_printer();
-    char buffer[256];
-    jio_snprintf(buffer, sizeof(buffer), "Bytecode %d: %s", bci(), Bytecodes::name(bc()));
-    bool old = printer->traverse_outs();
-    printer->set_traverse_outs(true);
-    printer->print_method(buffer, 4);
-    printer->set_traverse_outs(old);
-  }
-#endif
 }
