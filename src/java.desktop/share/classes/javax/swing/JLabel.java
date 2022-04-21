ec4fb47b90c9737dfdc285ebe98367a221c90c79
Alexey Ivanov <aivanov@openjdk.org>
2022-04-21 19:12:57 +0000

    8284189: Replace usages of 'a the' in java.desktop
    Reviewed-by: jdv, prr, dmarkov

--- src/java.desktop/share/classes/javax/swing/JLabel.java
+++ src/java.desktop/share/classes/javax/swing/JLabel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -593,7 +593,7 @@ public class JLabel extends JComponent implements SwingConstants, Accessible
      *
      * @param key the property value to check
      * @param message the IllegalArgumentException detail message
-     * @return the key value if {@code key} is a a legal value for the
+     * @return the key value if {@code key} is a legal value for the
      *         horizontalAlignment properties
      * @throws IllegalArgumentException if key isn't LEFT, CENTER, RIGHT,
      * LEADING or TRAILING.
