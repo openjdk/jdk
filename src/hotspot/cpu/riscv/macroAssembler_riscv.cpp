43675ed6b22b8b151943ad4cc22810206c5624dd
yunyao.zxl <yunyao.zxl@alibaba-inc.com>
2022-04-21 15:10:15 +0800

    Fix: broken MacroAssembler::debug64

--- src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -530,7 +530,7 @@ void MacroAssembler::resolve_jobject(Register value, Register thread, Register t
 
 void MacroAssembler::stop(const char* msg) {
   address ip = pc();
-  pusha();
+  push_reg(0xffffffff, sp);
   // The length of the instruction sequence emitted should be independent
   // of the values of msg and ip so that the size of mach nodes for scratch
   // emit and normal emit matches.
--- src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
@@ -578,7 +578,7 @@ class StubGenerator: public StubCodeGenerator {
     __ bind(error);
     __ pop_reg(0x3000, sp);   // pop c_rarg2 and c_rarg3
 
-    __ pusha();
+    __ push_reg(0xffffffff, sp);
     // debug(char* msg, int64_t pc, int64_t regs[])
     __ mv(c_rarg0, t0);             // pass address of error message
     __ mv(c_rarg1, ra);             // pass return address
