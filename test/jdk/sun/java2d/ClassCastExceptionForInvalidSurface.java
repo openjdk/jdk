dd608af5e0dc5b7f08cc5532d01b931c916f8e80
Alexey Ushakov <avu@openjdk.org>
2022-04-21 09:56:39 +0000

    8283794: CCE in XRTextRenderer.drawGlyphList and XRMaskFill.MaskFill
    Reviewed-by: serb, aghaisas

--- test/jdk/sun/java2d/ClassCastExceptionForInvalidSurface.java
+++ test/jdk/sun/java2d/ClassCastExceptionForInvalidSurface.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2022 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,7 +41,7 @@ import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
 /**
  * @test
  * @key headful
- * @bug 8158072 7172749
+ * @bug 8158072 7172749 8283794
  */
 public final class ClassCastExceptionForInvalidSurface {
 
