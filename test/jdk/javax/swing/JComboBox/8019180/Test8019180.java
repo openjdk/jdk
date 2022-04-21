7d49634d1d686599bda81e3c21a660d8ad57a964
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-04-21 05:12:11 +0100

    throw exception in main

--- test/jdk/javax/swing/JComboBox/8019180/Test8019180.java
+++ test/jdk/javax/swing/JComboBox/8019180/Test8019180.java
@@ -38,11 +38,16 @@ public class Test8019180 implements Runnable {
     private static final CountDownLatch LATCH = new CountDownLatch(1);
     private static final String[] ITEMS = {"First", "Second", "Third", "Fourth"};
     private static JFrame frame;
+    private static boolean selectionFail = false;
 
     public static void main(String[] args) throws Exception {
         try {
             SwingUtilities.invokeLater(new Test8019180());
             LATCH.await();
+	    System.out.println("selectionFail " + selectionFail);
+	    if (selectionFail) {
+                throw new RuntimeException("Combobox not selected");
+            }
         } finally {
             SwingUtilities.invokeAndWait(() -> {
                 if (frame != null) {
@@ -56,25 +61,22 @@ public class Test8019180 implements Runnable {
 
     @Override
     public void run() {
-        try {
-            if (this.test == null) {
-                this.test = new JComboBox<>(ITEMS);
-                this.test.addActionListener(this.test);
-                frame = new JFrame();
-                frame.add(test);
-                frame.setLocationRelativeTo(null);
-                frame.pack();
-                frame.setVisible(true);
-                SwingUtilities.invokeLater(this);
-            } else {
-                int index = this.test.getSelectedIndex();
-                this.test.setSelectedIndex(1 + index);
-                if (0 > this.test.getSelectedIndex()) {
-                    System.err.println("ERROR: no selection");
-                    throw new RuntimeException("Combobox not selected");
-                }
+        if (this.test == null) {
+            this.test = new JComboBox<>(ITEMS);
+            this.test.addActionListener(this.test);
+            frame = new JFrame();
+            frame.add(test);
+            frame.setLocationRelativeTo(null);
+            frame.pack();
+            frame.setVisible(true);
+            SwingUtilities.invokeLater(this);
+        } else {
+            int index = this.test.getSelectedIndex();
+            this.test.setSelectedIndex(1 + index);
+            if (0 > this.test.getSelectedIndex()) {
+                System.err.println("ERROR: no selection");
+                selectionFail = true;
             }
-        } finally {
             LATCH.countDown();
         }
     }
