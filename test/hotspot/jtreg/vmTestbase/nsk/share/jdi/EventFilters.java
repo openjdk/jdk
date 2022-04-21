8445f3285dc22091dbb66aa6bc7edfbd4411e62d
Chris Plummer <chris.plummer@oracle.com>
2022-04-21 19:59:46 +0000

    Fix numerous issues with suspendPolicy008 related to unexpected thread creation.

--- test/hotspot/jtreg/vmTestbase/nsk/share/jdi/EventFilters.java
+++ test/hotspot/jtreg/vmTestbase/nsk/share/jdi/EventFilters.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -335,6 +335,9 @@ public class EventFilters
             return true;
 
         if (event.toString().contains("JFR request timer"))
+            return true;
+
+        if (event.toString().contains("ForkJoinPool"))
             return true;
 
         return false;
