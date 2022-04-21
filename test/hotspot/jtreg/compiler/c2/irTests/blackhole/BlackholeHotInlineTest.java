5a78b4451863f208cc87b94892dfc251029003a9
Aleksey Shipilev <shade@redhat.com>
2022-04-21 18:56:21 +0200

    Fix

---
+++ test/hotspot/jtreg/compiler/c2/irTests/blackhole/BlackholeHotInlineTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2022, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8285394
+ * @requires vm.compiler2.enabled
+ * @summary Blackholes should work when hot inlined
+ * @library /test/lib /
+ * @run driver compiler.c2.irTests.blackhole.BlackholeHotInlineTest
+ */
+
+package compiler.c2.irTests.blackhole;
+
+import compiler.lib.ir_framework.*;
+import jdk.test.lib.Asserts;
+
+public class BlackholeHotInlineTest {
+
+    public static void main(String[] args) {
+        TestFramework.runWithFlags(
+            "-XX:+UnlockExperimentalVMOptions",
+            "-XX:CompileThreshold=100",
+            "-XX:-TieredCompilation",
+            "-XX:CompileCommand=blackhole,compiler.c2.irTests.blackhole.BlackholeHotInlineTest::blackhole",
+            "-XX:CompileCommand=dontinline,compiler.c2.irTests.blackhole.BlackholeHotInlineTest::dontinline"
+        );
+    }
+
+    static long x, y;
+
+    /*
+     * Baseline test: check that dontinline method does not allow the elimination.
+     */
+
+    @Test
+    @IR(counts = {IRNode.MUL_L, "1"})
+    static void testDontline() {
+        dontinline(x * y);
+    }
+
+    static void dontinline(long x) {}
+
+    @Run(test = "testDontline")
+    static void runDontinline() {
+        testDontline();
+    }
+
+    /*
+     * Positive test: check that blackhole method does not allow the elimination either.
+     */
+
+    @Test
+    @IR(counts = {IRNode.MUL_L, "1"})
+    static void testBlackholed() {
+        blackhole(x * y);
+    }
+
+    static void blackhole(long x) {}
+
+    @Run(test = "testBlackholed")
+    static void runBlackholed() {
+        testBlackholed();
+    }
+
+}
