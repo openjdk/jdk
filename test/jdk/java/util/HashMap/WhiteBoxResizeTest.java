24ddad35d04fe64dc77bed51ac934b0f98162d29
Stuart Marks <smarks@openjdk.org>
2022-04-21 22:28:14 +0000

    8285386: java/util/HashMap/WhiteBoxResizeTest.java fails in tier7 after JDK-8186958
    Reviewed-by: lancea

--- test/jdk/java/util/HashMap/WhiteBoxResizeTest.java
+++ test/jdk/java/util/HashMap/WhiteBoxResizeTest.java
@@ -48,10 +48,10 @@ import static org.testng.Assert.assertNull;
 
 /*
  * @test
- * @bug 8186958 8210280 8281631
+ * @bug 8186958 8210280 8281631 8285386
  * @modules java.base/java.util:open
  * @summary White box tests for HashMap-related internals around table sizing
- * @run testng WhiteBoxResizeTest
+ * @run testng/othervm -Xmx2g WhiteBoxResizeTest
  */
 public class WhiteBoxResizeTest {
 
