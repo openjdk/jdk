2e892be493caa255884b0f2a6f0bc419eac8b32f
Stuart Marks <stuart.marks@oracle.com>
2022-04-21 15:03:24 -0700

    Add -Xmx2g to ensure JVM has enough heap.

--- test/jdk/java/util/HashMap/WhiteBoxResizeTest.java
+++ test/jdk/java/util/HashMap/WhiteBoxResizeTest.java
@@ -51,7 +51,7 @@ import static org.testng.Assert.assertNull;
  * @bug 8186958 8210280 8281631
  * @modules java.base/java.util:open
  * @summary White box tests for HashMap-related internals around table sizing
- * @run testng WhiteBoxResizeTest
+ * @run testng/othervm -Xmx2g WhiteBoxResizeTest
  */
 public class WhiteBoxResizeTest {
 
