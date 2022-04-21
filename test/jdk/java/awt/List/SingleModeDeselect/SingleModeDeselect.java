f43bb9a258669753d45aef873f94cb067dbb6e87
Alexander Zuev <alexander.zuev@oracle.com>
2022-04-21 15:00:50 -0700

    8196367: java/awt/List/SingleModeDeselect/SingleModeDeselect.java times out
    Clean up imports;
    Close and dispose frame before exiting the test;

--- test/jdk/java/awt/List/SingleModeDeselect/SingleModeDeselect.java
+++ test/jdk/java/awt/List/SingleModeDeselect/SingleModeDeselect.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,16 +26,16 @@
   @key headful
   @bug 6248040
   @summary List.deselect() de-selects the currently selected item regardless of the index, win32
-  @author Dmitry Cherepanov area=awt.list
   @run main SingleModeDeselect
 */
 
-import java.awt.*;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.List;
+import java.awt.Robot;
 
-public class SingleModeDeselect
-{
-    public static final void main(String args[])
-    {
+public class SingleModeDeselect {
+    public static final void main(String args[]) {
         final Frame frame = new Frame();
         final List list = new List();
 
@@ -45,6 +45,7 @@ public class SingleModeDeselect
         frame.add(list);
         frame.setLayout(new FlowLayout());
         frame.setBounds(100,100,300,300);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
 
         list.select(0);
@@ -53,13 +54,18 @@ public class SingleModeDeselect
         try {
             Robot robot = new Robot();
             robot.waitForIdle();
-        }catch(Exception ex) {
+        } catch(Exception ex) {
             ex.printStackTrace();
             throw new RuntimeException("Unexpected failure");
         }
 
-        if (list.getSelectedIndex() != 0){
-            throw new RuntimeException("Test failed: List.getSelectedIndex() returns "+list.getSelectedIndex());
+        try {
+            if (list.getSelectedIndex() != 0) {
+                throw new RuntimeException("Test failed: List.getSelectedIndex() returns "+list.getSelectedIndex());
+            }
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
         }
 
     }//End  init()
