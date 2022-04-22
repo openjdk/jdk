bdbbdc21ba8ff54b1a4724c79109c7b3755ce472
Roberto Castañeda Lozano <rcastanedalo@openjdk.org>
2022-04-22 07:38:01 +0000

    8285369: C2: emit reduction flag value in node and loop dumps
    Reviewed-by: kvn

--- src/hotspot/share/opto/idealGraphPrinter.cpp
+++ src/hotspot/share/opto/idealGraphPrinter.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -455,6 +455,9 @@ void IdealGraphPrinter::visit_node(Node *n, bool edges, VectorSet* temp_set) {
     }
     if (flags & Node::Flag_has_call) {
       print_prop("has_call", "true");
+    }
+    if (flags & Node::Flag_is_reduction) {
+      print_prop("is_reduction", "true");
     }
 
     if (C->matcher() != NULL) {
--- src/hotspot/share/opto/loopnode.cpp
+++ src/hotspot/share/opto/loopnode.cpp
@@ -2233,6 +2233,7 @@ void CountedLoopNode::dump_spec(outputStream *st) const {
   if (is_pre_loop ()) st->print("pre of N%d" , _main_idx);
   if (is_main_loop()) st->print("main of N%d", _idx);
   if (is_post_loop()) st->print("post of N%d", _main_idx);
+  if (is_reduction_loop()) st->print(" reduction");
   if (is_strip_mined()) st->print(" strip mined");
 }
 #endif
@@ -3934,6 +3935,7 @@ void IdealLoopTree::dump_head() const {
     if (cl->is_pre_loop ()) tty->print(" pre" );
     if (cl->is_main_loop()) tty->print(" main");
     if (cl->is_post_loop()) tty->print(" post");
+    if (cl->is_reduction_loop()) tty->print(" reduction");
     if (cl->is_vectorized_loop()) tty->print(" vector");
     if (cl->range_checks_present()) tty->print(" rc ");
     if (cl->is_multiversioned()) tty->print(" multi ");
