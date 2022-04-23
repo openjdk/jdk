684d1528bc3fbb27159247e1400d7ce92afa57cf
XenoAmess <xenoamess@gmail.com>
2022-04-23 22:30:30 +0800

    maintains compatibility with jdk1.4 for TextTests

--- src/demo/share/java2d/J2DBench/src/j2dbench/tests/text/TextTests.java
+++ src/demo/share/java2d/J2DBench/src/j2dbench/tests/text/TextTests.java
@@ -288,7 +288,7 @@ public abstract class TextTests extends Test {
     }
 
 
-    static HashMap strcache = HashMap.newHashMap(tscripts.length);
+    static HashMap strcache = new HashMap((int) (Math.ceil(tscripts.length / 0.75)));
     private static String getSimpleString(Object key) {
         String s = (String)strcache.get(key);
         if (s == null) {
