1ee4074c79333ce297d8af553c703913e47a0629
Joe Darcy <darcy@openjdk.org>
2022-04-25 23:03:11 +0000

    8285614: Fix typo in java.lang.Float
    Reviewed-by: naoto, lancea, iris

--- src/java.base/share/classes/java/lang/Float.java
+++ src/java.base/share/classes/java/lang/Float.java
@@ -57,7 +57,7 @@ import jdk.internal.vm.annotation.IntrinsicCandidate;
  * The class {@code java.lang.Double} has a <a
  * href="Double.html#equivalenceRelation">discussion of equality,
  * equivalence, and comparison of floating-point values</a> that is
- * equality applicable to {@code float} values.
+ * equally applicable to {@code float} values.
  *
  * @author  Lee Boynton
  * @author  Arthur van Hoff
