aa9fd1eb3b44427d3409eb674a312dae5aaf703a
Michael McMahon <michaelm@openjdk.org>
2022-04-26 13:49:58 +0000

    8284890: Support for Do not fragment IP socket options
    Reviewed-by: erikj, ihse, dfuchs

--- src/jdk.net/macosx/classes/jdk/net/MacOSXSocketOptions.java
+++ src/jdk.net/macosx/classes/jdk/net/MacOSXSocketOptions.java
@@ -79,6 +79,16 @@ class MacOSXSocketOptions extends PlatformSocketOptions {
     }
 
     @Override
+    void setIpDontFragment(int fd, final boolean value) throws SocketException {
+        setIpDontFragment0(fd, value);
+    }
+
+    @Override
+    boolean getIpDontFragment(int fd) throws SocketException {
+        return getIpDontFragment0(fd);
+    }
+
+    @Override
     UnixDomainPrincipal getSoPeerCred(int fd) throws SocketException {
         long l = getSoPeerCred0(fd);
         int uid = (int)(l >> 32);
@@ -91,9 +101,11 @@ class MacOSXSocketOptions extends PlatformSocketOptions {
     private static native void setTcpkeepAliveProbes0(int fd, int value) throws SocketException;
     private static native void setTcpKeepAliveTime0(int fd, int value) throws SocketException;
     private static native void setTcpKeepAliveIntvl0(int fd, int value) throws SocketException;
+    private static native void setIpDontFragment0(int fd, boolean value) throws SocketException;
     private static native int getTcpkeepAliveProbes0(int fd) throws SocketException;
     private static native int getTcpKeepAliveTime0(int fd) throws SocketException;
     private static native int getTcpKeepAliveIntvl0(int fd) throws SocketException;
+    private static native boolean getIpDontFragment0(int fd) throws SocketException;
     private static native long getSoPeerCred0(int fd) throws SocketException;
     private static native boolean keepAliveOptionsSupported0();
     static {
