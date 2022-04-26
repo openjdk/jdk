8536a648fdecae4c6811258aac70335cae4da77d
U-LAPTOP-UFCGRAU7\jpg <srinivas.mandalika@oracle.com>
2022-04-26 14:51:34 +0530

    Review Comments Fixed: Simplified test code with a loop over the JComponents being tested.

--- test/jdk/javax/accessibility/4422535/SetCurrentAccessibleValueTest.java
+++ test/jdk/javax/accessibility/4422535/SetCurrentAccessibleValueTest.java
@@ -33,6 +33,7 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JInternalFrame;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollBar;
@@ -43,273 +44,28 @@ import javax.swing.SwingUtilities;
 public class SetCurrentAccessibleValueTest {
 
     public static void doTest() {
+        JComponent[] jComponents =
+        { new JButton(), new JInternalFrame(), new JSplitPane(),
+            new JScrollBar(), new JProgressBar(), new JSlider() };
 
-        JButton jButton = new JButton();
-        JInternalFrame iFrame = new JInternalFrame();
-        JSplitPane jSplitPane = new JSplitPane();
-        JScrollBar jScrollBar = new JScrollBar();
-        JProgressBar jProgressBar = new JProgressBar();
-        JSlider jSlider = new JSlider();
-
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept Short");
+        for (JComponent jComponent : jComponents) {
+            testIt(jComponent, (Float.valueOf(5)));
+            testIt(jComponent, (Double.valueOf(37.266)));
+            testIt(jComponent, (Integer.valueOf(10)));
+            testIt(jComponent, (Long.valueOf(123L)));
+            testIt(jComponent, (Short.valueOf((short) 123)));
+            testIt(jComponent, (BigInteger.ONE));
+            testIt(jComponent, (new BigDecimal(BigInteger.ONE)));
         }
 
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
+    }
 
-        if (!jButton.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("JButton's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("JScrollBar's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!iFrame.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("iFrame's Accessible Value"
-                + " implementation doesn't accept new "
-                +"BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!jSplitPane.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("jSplitPane's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!jScrollBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("jScrollBar's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!jProgressBar.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("jProgressBar's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
-        }
-
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Integer.valueOf(5))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept Integer");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Float.valueOf(5))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept Float");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Double.valueOf(5))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept Double");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Long.valueOf(5))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept Long");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(Short.valueOf((short) 123))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept Short");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(BigInteger.ONE)) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept BigInteger.ONE");
-        }
-        if (!jSlider.getAccessibleContext().getAccessibleValue()
-            .setCurrentAccessibleValue(new BigDecimal(BigInteger.ONE))) {
-            throw new RuntimeException("jSlider's Accessible Value"
-                + " implementation doesn't accept new "
-                + "BigDecimal(BigInteger.ONE)");
+    static void testIt(JComponent jComponent, Number number) {
+        if (!jComponent.getAccessibleContext().getAccessibleValue()
+            .setCurrentAccessibleValue(number)) {
+            throw new RuntimeException(jComponent.getClass().getName()
+                + " Accessible Value implementation doesn't accept "
+                + number.getClass().getName());
         }
     }
 
