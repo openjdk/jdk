625dbd2b3a8fe87d1e37ed70d98fb44884021990
Brian Burkhalter <bpb@openjdk.org>
2022-04-26 15:35:45 +0000

    8284930: Re-examine FilterInputStream mark/reset
    Reviewed-by: alanb, jpai, dfuchs, lancea

--- src/java.base/share/classes/java/io/InputStream.java
+++ src/java.base/share/classes/java/io/InputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -683,7 +683,7 @@ public abstract class InputStream implements Closeable {
      *                      the mark position becomes invalid.
      * @see     java.io.InputStream#reset()
      */
-    public synchronized void mark(int readlimit) {}
+    public void mark(int readlimit) {}
 
     /**
      * Repositions this stream to the position at the time the
@@ -729,7 +729,7 @@ public abstract class InputStream implements Closeable {
      * @see     java.io.InputStream#mark(int)
      * @see     java.io.IOException
      */
-    public synchronized void reset() throws IOException {
+    public void reset() throws IOException {
         throw new IOException("mark/reset not supported");
     }
 
