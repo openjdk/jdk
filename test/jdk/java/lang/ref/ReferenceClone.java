8f86362855e009fdaad6bc82fbeb7f7edb3958b1
Kim Barrett <kim.barrett@oracle.com>
2022-04-27 10:07:04 +0000

    update copyright, @bug list

--- test/jdk/java/lang/ref/ReferenceClone.java
+++ test/jdk/java/lang/ref/ReferenceClone.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8201793
+ * @bug 8201793 8285690
  * @summary Test Reference::clone to throw CloneNotSupportedException
  */
 
