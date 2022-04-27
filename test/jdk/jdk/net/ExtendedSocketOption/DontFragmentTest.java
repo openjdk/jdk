b77a6f90dcb01eb457f330f91bc4fdccce31344d
Michael-Mc-Mahon <michael.x.mcmahon@oracle.com>
2022-04-27 10:16:35 +0100

    initial fix

--- test/jdk/jdk/net/ExtendedSocketOption/DontFragmentTest.java
+++ test/jdk/jdk/net/ExtendedSocketOption/DontFragmentTest.java
@@ -58,6 +58,9 @@ public class DontFragmentTest {
     public static void testDatagramChannel() throws IOException {
         try (DatagramChannel c1 = DatagramChannel.open()) {
 
+            if (!c1.supportedOptions().contains(IP_DONTFRAGMENT)) {
+                return;
+            }
             if (c1.getOption(IP_DONTFRAGMENT)) {
                 throw new RuntimeException("IP_DONTFRAGMENT should not be set");
             }
@@ -75,6 +78,9 @@ public class DontFragmentTest {
     public static void testDatagramChannel(String[] args, ProtocolFamily fam) throws IOException {
         try (DatagramChannel c1 = DatagramChannel.open(fam)) {
 
+            if (!c1.supportedOptions().contains(IP_DONTFRAGMENT)) {
+                return;
+            }
             if (c1.getOption(IP_DONTFRAGMENT)) {
                 throw new RuntimeException("IP_DONTFRAGMENT should not be set");
             }
@@ -90,6 +96,9 @@ public class DontFragmentTest {
     }
 
     public static void testDatagramSocket(DatagramSocket c1) throws IOException {
+        if (!c1.supportedOptions().contains(IP_DONTFRAGMENT)) {
+            return;
+        }
         if (c1.getOption(IP_DONTFRAGMENT)) {
             throw new RuntimeException("IP_DONTFRAGMENT should not be set");
         }
