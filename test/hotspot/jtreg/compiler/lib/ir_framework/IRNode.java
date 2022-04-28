f48096ee501860c05a48acd4e6331a378347636d
Aleksey Shipilev <shade@openjdk.org>
2022-04-28 08:32:44 +0000

    8284848: C2: Compiler blackhole arguments should be treated as globally escaping
    Reviewed-by: kvn, thartmann

--- test/hotspot/jtreg/compiler/lib/ir_framework/IRNode.java
+++ test/hotspot/jtreg/compiler/lib/ir_framework/IRNode.java
@@ -137,6 +137,7 @@ public class IRNode {
 
     public static final String SCOPE_OBJECT = "(.*# ScObj.*" + END;
     public static final String MEMBAR = START + "MemBar" + MID + END;
+    public static final String MEMBAR_STORESTORE = START + "MemBarStoreStore" + MID + END;
     public static final String SAFEPOINT = START + "SafePoint" + MID + END;
 
     public static final String ABS_I = START + "AbsI" + MID + END;
@@ -187,6 +188,9 @@ public class IRNode {
     public static final String VECTOR_UCAST_S2X = START + "VectorUCastS2X" + MID + END;
     public static final String VECTOR_UCAST_I2X = START + "VectorUCastI2X" + MID + END;
     public static final String VECTOR_REINTERPRET = START + "VectorReinterpret" + MID + END;
+
+    public static final String FAST_LOCK   = START + "FastLock" + MID + END;
+    public static final String FAST_UNLOCK = START + "FastUnlock" + MID + END;
 
     /**
      * Called by {@link IRMatcher} to merge special composite nodes together with additional user-defined input.
