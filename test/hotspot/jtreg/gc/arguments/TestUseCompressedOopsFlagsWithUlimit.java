e7b8bc812057a7c06e020b34c7bd5c5957a60d06
Thomas Schatzl <tschatzl@openjdk.org>
2022-04-28 15:49:27 +0000

    8285011: gc/arguments/TestUseCompressedOopsFlagsWithUlimit.java fails after JDK-8280761
    Co-authored-by: Albert Mingkun Yang <ayang@openjdk.org>
    Reviewed-by: ayang, lkorinth

--- test/hotspot/jtreg/gc/arguments/TestUseCompressedOopsFlagsWithUlimit.java
+++ test/hotspot/jtreg/gc/arguments/TestUseCompressedOopsFlagsWithUlimit.java
@@ -31,7 +31,8 @@ package gc.arguments;
  * @library /test/lib
  * @library /
  * @requires vm.bits == "64"
- * @requires os.family != "aix" & os.family != "windows"
+ * @requires os.family == "linux"
+ * @requires vm.gc != "Z"
  * @run driver gc.arguments.TestUseCompressedOopsFlagsWithUlimit
  */
 
