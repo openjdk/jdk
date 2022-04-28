bc059b6ae7dfafe40fddf6df62895673e381fda9
Aleksey Shipilev <shade@redhat.com>
2022-04-28 19:54:05 +0200

    Fix

--- src/hotspot/share/gc/shared/c2/barrierSetC2.cpp
+++ src/hotspot/share/gc/shared/c2/barrierSetC2.cpp
@@ -388,9 +388,6 @@ void C2Access::fixup_decorators() {
 //--------------------------- atomic operations---------------------------------
 
 void BarrierSetC2::pin_atomic_op(C2AtomicParseAccess& access) const {
-  if (!access.needs_pinning()) {
-    return;
-  }
   // SCMemProjNodes represent the memory state of a LoadStore. Their
   // main role is to prevent LoadStore nodes from being optimized away
   // when their results aren't used.
