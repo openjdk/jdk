214eb65f9857c6905c2cbde4cbcfa53e9411ba0a
Jonathan Gibbons <jjg@openjdk.org>
2022-04-29 00:32:56 +0000

    8285496: DocLint does not check for missing `@param` tags for type parameters on classes and interfaces
    Reviewed-by: darcy, iris

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclint/Checker.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclint/Checker.java
@@ -251,7 +251,7 @@ public class Checker extends DocTreePathScanner<Void, Void> {
         // the following checks are made after the scan, which will record @param tags
         if (isDeclaredType()) {
             TypeElement te = (TypeElement) env.currElement;
-            // checkParamsDocumented(te.getTypeParameters()); // See JDK-8285496
+            checkParamsDocumented(te.getTypeParameters());
             checkParamsDocumented(te.getRecordComponents());
         } else if (isExecutable()) {
             if (!isOverridingMethod) {
