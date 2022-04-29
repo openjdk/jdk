461f8a3002ce2718698a43d5bca13fdfbd9d9879
Erik Österlund <eosterlund@openjdk.org>
2022-04-29 12:22:01 +0000

    8284404: Too aggressive sweeping with Loom

--- src/hotspot/share/compiler/compilerDefinitions.cpp
+++ src/hotspot/share/compiler/compilerDefinitions.cpp
@@ -25,6 +25,7 @@
 #include "precompiled.hpp"
 #include "code/codeCache.hpp"
 #include "runtime/arguments.hpp"
+#include "runtime/continuation.hpp"
 #include "runtime/flags/jvmFlag.hpp"
 #include "runtime/flags/jvmFlagAccess.hpp"
 #include "runtime/flags/jvmFlagConstraintsCompiler.hpp"
@@ -587,7 +588,9 @@ void CompilerConfig::ergo_initialize() {
 #endif
 
   if (FLAG_IS_DEFAULT(SweeperThreshold)) {
-    if ((SweeperThreshold * ReservedCodeCacheSize / 100) > (1.2 * M)) {
+    if (Continuations::enabled()) {
+      FLAG_SET_ERGO(SweeperThreshold, SweeperThreshold * 10.0);
+    } else if ((SweeperThreshold * ReservedCodeCacheSize / 100) > (1.2 * M)) {
       // Cap default SweeperThreshold value to an equivalent of 1.2 Mb
       FLAG_SET_ERGO(SweeperThreshold, (1.2 * M * 100) / ReservedCodeCacheSize);
     }
