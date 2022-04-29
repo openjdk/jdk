62d7a60340fede1097296826834d39e46e812b35
Pavel Rappo <prappo@openjdk.org>
2022-04-29 09:49:39 +0100

    Clarify with whitespace tags from 8285676

--- src/java.base/share/classes/java/lang/ref/PhantomReference.java
+++ src/java.base/share/classes/java/lang/ref/PhantomReference.java
@@ -45,7 +45,7 @@ import jdk.internal.vm.annotation.IntrinsicCandidate;
  * phantom reference always returns {@code null}.
  * The {@link #refersTo(Object) refersTo} method can be used to test
  * whether some object is the referent of a phantom reference.
- * @param<T> the type of the referent
+ * @param <T> the type of the referent
  *
  * @author   Mark Reinhold
  * @since    1.2
--- src/java.base/share/classes/java/lang/ref/Reference.java
+++ src/java.base/share/classes/java/lang/ref/Reference.java
@@ -36,7 +36,7 @@ import jdk.internal.ref.Cleaner;
  * operations common to all reference objects.  Because reference objects are
  * implemented in close cooperation with the garbage collector, this class may
  * not be subclassed directly.
- * @param<T> the type of the referent
+ * @param <T> the type of the referent
  *
  * @author   Mark Reinhold
  * @since    1.2
