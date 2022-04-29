b5e29f7e43e18cf96fa54bf1bb85cfb51eb97b55
Dean Long <dlong@openjdk.org>
2022-04-29 19:09:58 +0000

    8284883: JVM crash: guarantee(sect->end() <= sect->limit()) failed: sanity on AVX512
    Reviewed-by: kvn, jbhateja

--- src/hotspot/share/opto/memnode.cpp
+++ src/hotspot/share/opto/memnode.cpp
@@ -3072,7 +3072,7 @@ Node* ClearArrayNode::Identity(PhaseGVN* phase) {
 // Clearing a short array is faster with stores
 Node *ClearArrayNode::Ideal(PhaseGVN *phase, bool can_reshape) {
   // Already know this is a large node, do not try to ideal it
-  if (!IdealizeClearArrayNode || _is_large) return NULL;
+  if (_is_large) return NULL;
 
   const int unit = BytesPerLong;
   const TypeX* t = phase->type(in(2))->isa_intptr_t();
@@ -3093,6 +3093,7 @@ Node *ClearArrayNode::Ideal(PhaseGVN *phase, bool can_reshape) {
   } else if (size > 2 && Matcher::match_rule_supported_vector(Op_ClearArray, 4, T_LONG)) {
     return NULL;
   }
+  if (!IdealizeClearArrayNode) return NULL;
   Node *mem = in(1);
   if( phase->type(mem)==Type::TOP ) return NULL;
   Node *adr = in(3);
