3e92e195c377714f96b4efd446cd28b1239d5ef4
Yadong Wang <yadongwang@openjdk.org>
2022-04-29 03:19:29 +0000

    8285699: riscv: Provide information when hitting a HaltNode
    Reviewed-by: fyang

--- src/hotspot/os_cpu/linux_riscv/os_linux_riscv.cpp
+++ src/hotspot/os_cpu/linux_riscv/os_linux_riscv.cpp
@@ -354,7 +354,7 @@ void os::print_context(outputStream *st, const void *context) {
   // point to garbage if entry point in an nmethod is corrupted. Leave
   // this at the end, and hope for the best.
   address pc = os::Posix::ucontext_get_pc(uc);
-  print_instructions(st, pc, sizeof(char));
+  print_instructions(st, pc, UseRVC ? sizeof(char) : 4/*non-compressed native instruction size*/);
   st->cr();
 }
 
