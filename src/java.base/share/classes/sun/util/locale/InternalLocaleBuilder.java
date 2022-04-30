44a9c72283915806f1e760b4a9276bae90caa338
Andrey Turbanov <turbanoff@gmail.com>
2022-04-30 20:09:44 +0300

    [PATCH] Avoid redundant HashMap.containsKey calls in InternalLocaleBuilder

--- src/java.base/share/classes/sun/util/locale/InternalLocaleBuilder.java
+++ src/java.base/share/classes/sun/util/locale/InternalLocaleBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2011, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -184,7 +184,7 @@ public final class InternalLocaleBuilder {
                     ukeywords.clear();
                 }
             } else {
-                if (extensions != null && extensions.containsKey(key)) {
+                if (extensions != null) {
                     extensions.remove(key);
                 }
             }
