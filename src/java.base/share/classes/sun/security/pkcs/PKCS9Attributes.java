141ee3f14d39d82af812f7685b49abf6fecb5fe4
Andrey Turbanov <turbanoff@gmail.com>
2022-04-30 19:25:22 +0300

    [PATCH] Avoid redundant Hashtable.containsKey call in PKCS9Attributes

--- src/java.base/share/classes/sun/security/pkcs/PKCS9Attributes.java
+++ src/java.base/share/classes/sun/security/pkcs/PKCS9Attributes.java
@@ -158,13 +158,12 @@ public class PKCS9Attributes {
         ObjectIdentifier oid;
         for (int i=0; i < attribs.length; i++) {
             oid = attribs[i].getOID();
-            if (attributes.containsKey(oid))
+            if (attributes.putIfAbsent(oid, attribs[i]) != null)
                 throw new IllegalArgumentException(
                           "PKCSAttribute " + attribs[i].getOID() +
                           " duplicated while constructing " +
                           "PKCS9Attributes.");
 
-            attributes.put(oid, attribs[i]);
         }
         derEncoding = generateDerEncoding();
         permittedAttributes = null;
