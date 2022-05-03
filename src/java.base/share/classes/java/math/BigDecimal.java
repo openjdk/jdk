b11eec2e2deb926b097a1eb77d4208704192e5b9
Joe Darcy <darcy@openjdk.org>
2022-05-03 16:58:19 +0000

    8285977: Add links to IEEE 754 specification
    Reviewed-by: jjg, iris, bpb

--- src/java.base/share/classes/java/math/BigDecimal.java
+++ src/java.base/share/classes/java/math/BigDecimal.java
@@ -293,6 +293,9 @@ import java.util.Objects;
  * @see     RoundingMode
  * @see     java.util.SortedMap
  * @see     java.util.SortedSet
+ * @see <a href="https://standards.ieee.org/ieee/754/6210/">
+ *      <cite>IEEE Standard for Floating-Point Arithmetic</cite></a>
+ *
  * @author  Josh Bloch
  * @author  Mike Cowlishaw
  * @author  Joseph D. Darcy
