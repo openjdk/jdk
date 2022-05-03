4d225d748bac49db10eff1beb2f26b96f69b3512
Daniel D. Daugherty <dcubed@openjdk.org>
2022-05-03 21:26:53 +0000

    8286088: add comment to InstallAsyncExceptionHandshake destructor
    Reviewed-by: dholmes

--- src/hotspot/share/runtime/thread.cpp
+++ src/hotspot/share/runtime/thread.cpp
@@ -1671,6 +1671,7 @@ public:
   InstallAsyncExceptionHandshake(AsyncExceptionHandshake* aeh) :
     HandshakeClosure("InstallAsyncException"), _aeh(aeh) {}
   ~InstallAsyncExceptionHandshake() {
+    // If InstallAsyncExceptionHandshake was never executed we need to clean up _aeh.
     delete _aeh;
   }
   void do_thread(Thread* thr) {
