2613c9bdf16b3aa3d52aa787d23e1aa66cb06102
aph <aph@gcc.gnu.org>
2022-05-04 13:27:07 -0400

    8285923: [REDO] JDK-8285802 AArch64: Consistently handle offsets in MacroAssembler as 64-bit quantities

--- src/hotspot/cpu/aarch64/stubGenerator_aarch64.cpp
+++ src/hotspot/cpu/aarch64/stubGenerator_aarch64.cpp
@@ -6521,7 +6521,7 @@ class StubGenerator: public StubCodeGenerator {
     assert(is_even(framesize/2), "sp not 16-byte aligned");
 
     // lr and fp are already in place
-    __ sub(sp, rfp, ((unsigned)framesize-4) << LogBytesPerInt); // prolog
+    __ sub(sp, rfp, ((uint64_t)framesize-4) << LogBytesPerInt); // prolog
 
     int frame_complete = __ pc() - start;
 
