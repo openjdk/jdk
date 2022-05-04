22284448701b494f71d72777c0f1df529bb05972
Doug Simon <dnsimon@openjdk.org>
2022-05-04 04:55:27 +0000

    8286063: check compiler queue after calling AbstractCompiler::on_empty_queue
    Reviewed-by: kvn

--- src/hotspot/share/compiler/compileBroker.cpp
+++ src/hotspot/share/compiler/compileBroker.cpp
@@ -443,6 +443,11 @@ CompileTask* CompileQueue::get(CompilerThread* thread) {
     AbstractCompiler* compiler = thread->compiler();
     guarantee(compiler != nullptr, "Compiler object must exist");
     compiler->on_empty_queue(this, thread);
+    if (_first != nullptr) {
+      // The call to on_empty_queue may have temporarily unlocked the MCQ lock
+      // so check again whether any tasks were added to the queue.
+      break;
+    }
 
     // If there are no compilation tasks and we can compile new jobs
     // (i.e., there is enough free space in the code cache) there is
