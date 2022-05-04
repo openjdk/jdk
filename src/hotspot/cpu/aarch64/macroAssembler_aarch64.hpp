9e6e89dfca0c2fd300217d89c5ce8dbd83d1caa1
Evgeny Astigeevich <eastig@amazon.com>
2022-05-04 11:51:16 +0000

    8286056: AArch64: clarify uses of MacroAssembler::far_call/MacroAssembler::far_jump

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
@@ -1095,8 +1095,15 @@ public:
     return CodeCache::max_distance_to_non_nmethod() > branch_range;
   }
 
-  // Jumps that can reach anywhere in the code cache.
-  // Trashes tmp.
+  // Far_call and far_jump generate a call of/jump to the provided address.
+  // The address must be inside the code cache.
+  // Supported entry.rspec():
+  // - relocInfo::external_word_type
+  // - relocInfo::runtime_call_type
+  // - relocInfo::none
+  // If the distance to the address can exceed the branch range (128M for the release build)
+  // for direct calls/jumps, an indirect call/jump is generated.
+  // It trashes tmp.
   void far_call(Address entry, CodeBuffer *cbuf = NULL, Register tmp = rscratch1);
   int far_jump(Address entry, CodeBuffer *cbuf = NULL, Register tmp = rscratch1);
 
