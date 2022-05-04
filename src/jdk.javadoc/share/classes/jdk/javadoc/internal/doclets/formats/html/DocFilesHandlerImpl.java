8d8b503696652fadda65dbf6fd2f9c525ed42a43
Pavel Rappo <prappo@openjdk.org>
2022-05-04 20:55:01 +0000

    8285470: Improve handling of @inheritDoc
    Reviewed-by: jjg

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/DocFilesHandlerImpl.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/DocFilesHandlerImpl.java
@@ -199,13 +199,13 @@ public class DocFilesHandlerImpl implements DocFilesHandler {
         HtmlDocletWriter docletWriter = new DocFileWriter(configuration, dfilePath, element, pkg);
 
         List<? extends DocTree> localTags = getLocalHeaderTags(utils.getPreamble(dfElement));
-        Content localTagsContent = docletWriter.commentTagsToContent(null, dfElement, localTags, false);
+        Content localTagsContent = docletWriter.commentTagsToContent(dfElement, localTags, false);
 
         String title = getWindowTitle(docletWriter, dfElement).trim();
         HtmlTree htmlContent = docletWriter.getBody(title);
 
         List<? extends DocTree> fullBody = utils.getFullBody(dfElement);
-        Content pageContent = docletWriter.commentTagsToContent(null, dfElement, fullBody, false);
+        Content pageContent = docletWriter.commentTagsToContent(dfElement, fullBody, false);
         docletWriter.addTagsInfo(dfElement, pageContent);
 
         htmlContent.add(new BodyContents()
