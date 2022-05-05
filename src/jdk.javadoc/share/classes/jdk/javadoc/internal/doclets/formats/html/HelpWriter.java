2e1a63ce8e4e07a9a565901c3e6b06ffcaadff33
Hannes Wallnöfer <hannesw@openjdk.org>
2022-05-05 14:46:43 +0000

    8282559: Allow multiple search terms in javadoc search
    Reviewed-by: jjg

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HelpWriter.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HelpWriter.java
@@ -50,9 +50,9 @@ import jdk.javadoc.internal.doclets.toolkit.util.DocPaths;
 public class HelpWriter extends HtmlDocletWriter {
 
     private final String[][] SEARCH_EXAMPLES = {
-            {"j.l.obj", "\"java.lang.Object\""},
-            {"InpStr", "\"java.io.InputStream\""},
-            {"HM.cK", "\"java.util.HashMap.containsKey(Object)\""}
+            {"\"j.l.obj\"", "\"java.lang.Object\""},
+            {"\"InpStr\"", "\"java.io.InputStream\""},
+            {"\"math exact long\"", "\"java.lang.Math.absExact(long)\""}
     };
 
     Content overviewLink;
