dd06cc638ec54e54cce599f82f37eb56abac2eae
Tobias Holenstein <tobias.holenstein@oracle.com>
2022-05-06 07:08:19 +0000

    8283807: Handle CompileThreshold the same as other thresholds when scaled with -XX:CompileThresholdScaling
    Reviewed-by: kvn, iklam, thartmann

--- test/hotspot/jtreg/runtime/CommandLine/OptionsValidation/TestOptionsWithRanges.java
+++ test/hotspot/jtreg/runtime/CommandLine/OptionsValidation/TestOptionsWithRanges.java
@@ -257,6 +257,14 @@ public class TestOptionsWithRanges {
         excludeTestMaxRange("NonNMethodCodeHeapSize");
         excludeTestMaxRange("CodeCacheExpansionSize");
 
+        /*
+         * Exclude CompileThresholdScaling from max range testing, because
+         * it is expected to print "outside the allowed range" warnings for the
+         * scaled flag and the "outside the allowed range" warning does not
+         * refer to CompileThresholdScaling itself.
+         */
+        excludeTestMaxRange("CompileThresholdScaling");
+
         List<JVMOption> testSubset = getTestSubset(args);
 
         Asserts.assertGT(testSubset.size(), 0, "Options with ranges not found!");
