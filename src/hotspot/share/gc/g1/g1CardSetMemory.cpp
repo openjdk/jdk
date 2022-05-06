98dcb20520b235e983a9efb405761d187bf995cf
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-06 12:10:00 +0200

    initail implementation, waiting for dependent issues

--- src/hotspot/share/gc/g1/g1CardSetMemory.cpp
+++ src/hotspot/share/gc/g1/g1CardSetMemory.cpp
@@ -71,25 +71,6 @@ uint G1CardSetAllocator::num_segments() const {
   return _segmented_array.num_segments();
 }
 
-void G1CardSetAllocator::print(outputStream* os) {
-  uint num_allocated_slots = _segmented_array.num_allocated_slots();
-  uint num_total_slots = _segmented_array.num_total_slots();
-  uint highest = _segmented_array.first_array_segment() != nullptr
-               ? _segmented_array.first_array_segment()->num_slots()
-               : 0;
-  uint num_segments = _segmented_array.num_segments();
-  uint num_pending_slots = (uint)_free_slots_list.pending_count();
-  os->print("MA " PTR_FORMAT ": %u slots pending (allocated %u total %u) used %.3f highest %u segments %u size %zu ",
-            p2i(this),
-            num_pending_slots,
-            num_allocated_slots,
-            num_total_slots,
-            percent_of(num_allocated_slots - num_pending_slots, num_total_slots),
-            highest,
-            num_segments,
-            mem_size());
-}
-
 G1CardSetMemoryManager::G1CardSetMemoryManager(G1CardSetConfiguration* config,
                                                G1CardSetFreePool* free_list_pool) : _config(config) {
 
