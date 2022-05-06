d13f630c4e6f5799f4b89c1f4cb7f84fe2ef5c79
Jonathan Gibbons <jjg@openjdk.org>
2022-05-06 00:16:52 +0000

    8286153: Remove redundant casts and other cleanup
    Reviewed-by: prappo

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDocletWriter.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDocletWriter.java
@@ -2322,7 +2322,7 @@ public class HtmlDocletWriter {
                 case PACKAGE, MODULE ->
                         ((QualifiedNameable) forWhat).getQualifiedName();
                 case CONSTRUCTOR ->
-                        ((TypeElement) forWhat.getEnclosingElement()).getSimpleName();
+                        forWhat.getEnclosingElement().getSimpleName();
                 default -> forWhat.getSimpleName();
             }).toString();
             var nameCode = HtmlTree.CODE(Text.of(name));
