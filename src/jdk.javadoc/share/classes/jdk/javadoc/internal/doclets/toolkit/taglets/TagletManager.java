1e7d2925feb1239e4042e8d90eaddf23d829e13a
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2022-05-06 14:20:19 -0700

    JDK-8286338: suppress warnings about bad @author tags when author info is not generated.

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/TagletManager.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/TagletManager.java
@@ -381,9 +381,15 @@ public class TagletManager {
             final Taglet taglet = allTaglets.get(name);
             // Check and verify tag usage
             if (taglet != null) {
+                if (taglet instanceof SimpleTaglet st && !st.enabled) {
+                    // taglet has been disabled
+                    return;
+                }
+
                 if (inlineTrees && !taglet.isInlineTag()) {
                     printTagMisuseWarn(ch, taglet, tag, "inline");
                 }
+
                 // nothing more to do
                 if (element == null) {
                     return;
