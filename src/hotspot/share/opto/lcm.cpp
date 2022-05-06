20caea7f4715735107bfe9b17edf82b40092d589
Brian J. Stafford <brianjstafford@gmail.com>
2022-05-06 15:45:35 -0700

    This change simplifies the anti-dependence check in PhaseCFG::implicit_null_check(). JDK-8263075

--- src/hotspot/share/opto/lcm.cpp
+++ src/hotspot/share/opto/lcm.cpp
@@ -326,25 +326,28 @@ void PhaseCFG::implicit_null_check(Block* block, Node *proj, Node *val, int allo
     Block *mb = get_block_for_node(mach);
     // Hoisting stores requires more checks for the anti-dependence case.
     // Give up hoisting if we have to move the store past any load.
-    if( was_store ) {
-      Block *b = mb;            // Start searching here for a local load
-      // mach use (faulting) trying to hoist
-      // n might be blocker to hoisting
-      while( b != block ) {
-        uint k;
-        for( k = 1; k < b->number_of_nodes(); k++ ) {
-          Node *n = b->get_node(k);
-          if( n->needs_anti_dependence_check() &&
-              n->in(LoadNode::Memory) == mach->in(StoreNode::Memory) )
-            break;              // Found anti-dependent load
-        }
-        if( k < b->number_of_nodes() )
-          break;                // Found anti-dependent load
-        // Make sure control does not do a merge (would have to check allpaths)
-        if( b->num_preds() != 2 ) break;
-        b = get_block_for_node(b->pred(1)); // Move up to predecessor block
-      }
-      if( b != block ) continue;
+    if (was_store) {
+       // Start searching here for a local load
+       // mach use (faulting) trying to hoist
+       // n might be blocker to hoisting
+       // This assert ensures that the following code should be run
+       assert(mb != block, "mb should be distinct from block"); 
+       uint k;
+       uint num_nodes = mb->number_of_nodes();
+       for (k = 1; k < num_nodes; k++) {
+         Node *n = mb->get_node(k);
+         if (n->needs_anti_dependence_check() &&
+             n->in(LoadNode::Memory) == mach->in(StoreNode::Memory))
+           break;              // Found anti-dependent load
+       }
+       if (k < num_nodes)
+         continue;             // Found anti-dependent load
+
+       // Make sure control does not do a merge (would have to check allpaths)
+       if (mb->num_preds() != 2) continue;
+
+       // This assert ensures that the above code should only be run once
+       assert(get_block_for_node(mb->pred(1)) == block, "Unexpected predecessor block");
     }
 
     // Make sure this memory op is not already being used for a NullCheck
