7e77228de5b902d63a6c98ab3e8b4e4f79b762d3
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-09 11:41:45 +0200

    ayang review

--- src/hotspot/share/gc/g1/g1HeapVerifier.cpp
+++ src/hotspot/share/gc/g1/g1HeapVerifier.cpp
@@ -352,15 +352,13 @@ void G1HeapVerifier::verify_archive_regions() {
 
 class VerifyRegionClosure: public HeapRegionClosure {
 private:
-  bool             _par;
   VerifyOption     _vo;
   bool             _failures;
 public:
   // _vo == UsePrevMarking -> use "prev" marking information,
   // _vo == UseFullMarking -> use "next" marking bitmap but no TAMS
-  VerifyRegionClosure(bool par, VerifyOption vo)
-    : _par(par),
-      _vo(vo),
+  VerifyRegionClosure(VerifyOption vo)
+    : _vo(vo),
       _failures(false) {}
 
   bool failures() {
@@ -415,9 +413,9 @@ public:
   }
 };
 
-// This is the task used for parallel verification of the heap regions
+// This is the task used for verification of the heap regions
 
-class G1ParVerifyTask: public WorkerTask {
+class G1VerifyTask: public WorkerTask {
 private:
   G1CollectedHeap*  _g1h;
   VerifyOption      _vo;
@@ -427,8 +425,8 @@ private:
 public:
   // _vo == UsePrevMarking -> use "prev" marking information,
   // _vo == UseFullMarking -> use "next" marking bitmap but no TAMS
-  G1ParVerifyTask(G1CollectedHeap* g1h, VerifyOption vo) :
-      WorkerTask("Parallel verify task"),
+  G1VerifyTask(G1CollectedHeap* g1h, VerifyOption vo) :
+      WorkerTask("Verify task"),
       _g1h(g1h),
       _vo(vo),
       _failures(false),
@@ -493,7 +491,7 @@ void G1HeapVerifier::verify(VerifyOption vo) {
   log_debug(gc, verify)("HeapRegions");
 
   uint num_workers = GCParallelVerificationEnabled ? _g1h->workers()->active_workers() : 1u;
-  G1ParVerifyTask task(_g1h, vo);
+  G1VerifyTask task(_g1h, vo);
   _g1h->workers()->run_task(&task, num_workers);
   if (task.failures()) {
     failures = true;
