221086e42d1e02f27379e8bf462a08372d620bf7
Erik Österlund <eosterlund@openjdk.org>
2022-05-09 08:39:31 +0000

    8286376: Wrong condition for using non-immediate oops on AArch64

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -4202,7 +4202,7 @@ void MacroAssembler::movoop(Register dst, jobject obj, bool immediate) {
   // ordered with respected to oop accesses.
   // Using immediate literals would necessitate ISBs.
   BarrierSet* bs = BarrierSet::barrier_set();
-  if ((bs->barrier_set_nmethod() != NULL && bs->barrier_set_assembler()->nmethod_code_patching()) || !immediate) {
+  if ((bs->barrier_set_nmethod() != NULL && !bs->barrier_set_assembler()->nmethod_code_patching()) || !immediate) {
     address dummy = address(uintptr_t(pc()) & -wordSize); // A nearby aligned address
     ldr_constant(dst, Address(dummy, rspec));
   } else
