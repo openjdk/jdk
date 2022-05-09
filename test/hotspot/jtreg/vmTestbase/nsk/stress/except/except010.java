35e616602d2735be4e0760419c54e4a29feb112d
lmesnik <leonid.mesnik@oracle.com>
2022-05-09 12:00:31 -0600

    8284980: Test vmTestbase/nsk/stress/except/except010.java times out with -Xcomp -XX:+DeoptimizeALot

--- test/hotspot/jtreg/vmTestbase/nsk/stress/except/except010.java
+++ test/hotspot/jtreg/vmTestbase/nsk/stress/except/except010.java
@@ -58,6 +58,7 @@
  *     JDK 1.3 classic VM for Sparc may crash (core dump) due to the known bug:
  *         #4245057 (P2/S3) VM crashes when heap is exhausted
  *
+ * @requires vm.opt.DeoptimizeALot != true
  * @run main/othervm -Xms50M -Xmx200M nsk.stress.except.except010
  */
 
