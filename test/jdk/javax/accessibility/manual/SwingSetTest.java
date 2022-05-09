7f2dbc181307c4267f04c3947a37c4acec40747e
Daniel D. Daugherty <dcubed@openjdk.org>
2022-05-09 19:22:42 +0000

    8286435: JDK-8284316 caused validate-source to fail in Tier1
    Reviewed-by: mikael

--- test/jdk/javax/accessibility/manual/SwingSetTest.java
+++ test/jdk/javax/accessibility/manual/SwingSetTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 
