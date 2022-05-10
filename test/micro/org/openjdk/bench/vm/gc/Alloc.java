a7930fd52d1503929f756856d6dc20c33957bc40
wanghaomin <wanghaomin@loongson.cn>
2022-05-10 10:58:51 +0800

    8286452: The array length of testSmallConstArray should be small and const

--- test/micro/org/openjdk/bench/vm/gc/Alloc.java
+++ test/micro/org/openjdk/bench/vm/gc/Alloc.java
@@ -38,13 +38,14 @@ import java.util.concurrent.TimeUnit;
 public class Alloc {
 
     public static final int LENGTH = 400;
-    public static final int ARR_LEN = 100;
-    public int largeLen = 100;
-    public int smalllen = 6;
+    public static final int largeConstLen = 100;
+    public static final int smallConstLen = 6;
+    public int largeVariableLen = 100;
+    public int smallVariableLen = 6;
 
     @Benchmark
     public void testLargeConstArray(Blackhole bh) throws Exception {
-        int localArrlen = ARR_LEN;
+        int localArrlen = largeConstLen;
         for (int i = 0; i < LENGTH; i++) {
             Object[] tmp = new Object[localArrlen];
             bh.consume(tmp);
@@ -53,7 +54,7 @@ public class Alloc {
 
     @Benchmark
     public void testLargeVariableArray(Blackhole bh) throws Exception {
-        int localArrlen = largeLen;
+        int localArrlen = largeVariableLen;
         for (int i = 0; i < LENGTH; i++) {
             Object[] tmp = new Object[localArrlen];
             bh.consume(tmp);
@@ -62,7 +63,7 @@ public class Alloc {
 
     @Benchmark
     public void testSmallConstArray(Blackhole bh) throws Exception {
-        int localArrlen = largeLen;
+        int localArrlen = smallConstLen;
         for (int i = 0; i < LENGTH; i++) {
             Object[] tmp = new Object[localArrlen];
             bh.consume(tmp);
@@ -82,7 +83,7 @@ public class Alloc {
 
     @Benchmark
     public void testSmallVariableArray(Blackhole bh) throws Exception {
-        int localArrlen = smalllen;
+        int localArrlen = smallVariableLen;
         for (int i = 0; i < LENGTH; i++) {
             Object[] tmp = new Object[localArrlen];
             bh.consume(tmp);
