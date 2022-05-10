1b74acf41489e6a67d1fbe5734c39e874d0cfca1
Aleksey Shipilev <shade@redhat.com>
2022-05-10 16:52:14 +0200

    Review comments

--- test/jdk/java/lang/reflect/sealed_classes/SealedClassesReflectionTest.java
+++ test/jdk/java/lang/reflect/sealed_classes/SealedClassesReflectionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,7 @@
  * @test
  * @bug 8227046
  * @summary reflection test for sealed classes
- * @compile -source ${jdk.version} SealedClassesReflectionTest.java
+ * @compile SealedClassesReflectionTest.java
  * @run testng/othervm SealedClassesReflectionTest
  */
 
