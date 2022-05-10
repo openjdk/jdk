329bb4f835d2a763e42b36db5cf2afc0035364bc
Lennart Fricke <lennart.fricke@drehpunkt.de>
2022-05-10 09:00:09 +0000

    8286163: micro-optimize Instant.plusSeconds
    Reviewed-by: scolebourne, redestad, naoto

---
+++ test/micro/org/openjdk/bench/java/time/InstantBench.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.bench.java.time;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+/**
+ * Examine Instant.plusSeconds operations
+ */
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(3)
+@State(Scope.Benchmark)
+public class InstantBench {
+
+    private Instant[] INSTANTS;
+    private int[] SECONDS;
+
+    private long[] RESULT;
+
+    @Setup
+    public void createInstants() {
+        // Various instants during the same day
+        final Instant loInstant = Instant.EPOCH.plus(Duration.ofDays(365*50)); // 2020-01-01
+        final Instant hiInstant = loInstant.plus(Duration.ofDays(1));
+        final long maxOffsetNanos = Duration.between(loInstant, hiInstant).toNanos();
+        final Random random = new Random(0);
+        INSTANTS = IntStream
+                .range(0, 1_000)
+                .mapToObj(ignored -> {
+                    final long offsetNanos = (long) Math.floor(random.nextDouble() * maxOffsetNanos);
+                    return loInstant.plus(offsetNanos, ChronoUnit.NANOS);
+                })
+                .toArray(Instant[]::new);
+        SECONDS = IntStream
+                .range(0, INSTANTS.length)
+                .map(ignored -> random.nextInt(1000))
+                .toArray();
+        RESULT = new long[INSTANTS.length];
+    }
+
+    @Benchmark
+    public long[] plusSecondsDropNanos() {
+        for (int i = 0; i < INSTANTS.length; i++) {
+            RESULT[i] = INSTANTS[i].plusSeconds(SECONDS[i]).getEpochSecond();
+        }
+        return RESULT;
+    }
+}
