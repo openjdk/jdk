9af8034c21d79bcbb1d44dc43e2a6e71f0187267
Alexander Zuev <alexander.zuev@oracle.com>
2022-05-10 09:21:36 -0700

    8284690: [macos] VoiceOver : Getting java.lang.IllegalArgumentException: Invalid location on Editable JComboBox
    Moving cache invalidation from the clearCache method to a createRowWithIndex method
    eliminating race condition that causes crash. Now clearCache just nitifies that cache
    is invalid and should be regenerated next time it is being accessed.

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/TableAccessibility.h
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/TableAccessibility.h
@@ -29,6 +29,7 @@
 @interface TableAccessibility : CommonComponentAccessibility <NSAccessibilityTable>
 {
     NSMutableDictionary<NSNumber*, id> *rowCache;
+    BOOL cacheValid;
 }
 
 - (BOOL)isAccessibleChildSelectedFromIndex:(int)index;
--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/TableAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/TableAccessibility.m
@@ -130,6 +130,15 @@ static jmethodID sjm_getAccessibleName = NULL;
     if (rowCache == nil) {
         int rowCount = [self accessibilityRowCount];
         rowCache = [[NSMutableDictionary<NSNumber*, id> dictionaryWithCapacity:rowCount] retain];
+        cacheValid = YES;
+    }
+
+    if (!cacheValid) {
+        for (NSNumber *key in [rowCache allKeys]) {
+            [[rowCache objectForKey:key] release];
+            [rowCache removeObjectForKey:key];
+        }
+        cacheValid = YES;
     }
 
     id row = [rowCache objectForKey:[NSNumber numberWithUnsignedInteger:index]];
@@ -223,11 +232,7 @@ static jmethodID sjm_getAccessibleName = NULL;
 }
 
 - (void)clearCache {
-    for (NSNumber *key in [rowCache allKeys]) {
-        [[rowCache objectForKey:key] release];
-    }
-    [rowCache release];
-    rowCache = nil;
+    cacheValid = NO;
 }
 
 @end
