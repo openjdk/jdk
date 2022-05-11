aeddff62b89d24fdfce1f3f13d35db357981bd72
Adam Sotona <asotona@users.noreply.github.com>
2022-05-11 13:30:01 +0200

    8286391: Address possibly lossy conversions in jdk.compiler

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Code.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/Code.java
@@ -2015,7 +2015,7 @@ public class Code {
             if (localVar != null) {
                 for (LocalVar.Range range: localVar.aliveRanges) {
                     if (range.closed() && range.start_pc + range.length >= oldCP) {
-                        range.length += delta;
+                        range.length += (char)delta;
                     }
                 }
             }
