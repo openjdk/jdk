10feb4e3a9a96502a5a0d388030d704fec6b6610
Albert Mingkun Yang <ayang@openjdk.org>
2022-05-11 15:06:14 +0000

    8286387: Remove unused FreeListAllocator::reduce_free_list
    Reviewed-by: kbarrett, tschatzl

--- src/hotspot/share/gc/shared/ptrQueue.hpp
+++ src/hotspot/share/gc/shared/ptrQueue.hpp
@@ -198,11 +198,6 @@ public:
   // If _free_list has items buffered in the pending list, transfer
   // these to make them available for re-allocation.
   bool flush_free_list() { return _free_list.try_transfer_pending(); }
-
-  // Deallocate some of the available buffers.  remove_goal is the target
-  // number to remove.  Returns the number actually deallocated, which may
-  // be less than the goal if there were fewer available.
-  size_t reduce_free_list(size_t remove_goal);
 };
 
 // A PtrQueueSet represents resources common to a set of pointer queues.
--- test/hotspot/gtest/gc/shared/test_ptrQueueBufferAllocator.cpp
+++ test/hotspot/gtest/gc/shared/test_ptrQueueBufferAllocator.cpp
@@ -85,13 +85,6 @@ TEST_VM(PtrQueueBufferAllocatorTest, test) {
   }
   ASSERT_TRUE(BufferNode::TestSupport::try_transfer_pending(&allocator));
   ASSERT_EQ(node_count, allocator.free_count());
-
-  // Destroy some nodes in the free list.
-  // We don't have a way to verify destruction, but we can at
-  // least verify we don't crash along the way.
-  size_t count = allocator.free_count();
-  ASSERT_EQ(count, allocator.reduce_free_list(count));
-  // destroy allocator.
 }
 
 // Stress test with lock-free allocator and completed buffer list.
