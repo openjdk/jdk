4b5dce96458eae0f60c9e9061ea516de3843fa6f
Coleen Phillimore <coleenp@openjdk.org>
2022-05-11 16:19:49 +0000

    8286556: Remove EagerInitialization develop option
    Reviewed-by: rehn, redestad

--- src/hotspot/share/classfile/systemDictionary.cpp
+++ src/hotspot/share/classfile/systemDictionary.cpp
@@ -1453,7 +1453,6 @@ void SystemDictionary::define_instance_class(InstanceKlass* k, Handle class_load
     // Grabs and releases SystemDictionary_lock
     update_dictionary(name_hash, k, class_loader);
   }
-  k->eager_initialize(THREAD);
 
   // notify jvmti
   if (JvmtiExport::should_post_class_load()) {
