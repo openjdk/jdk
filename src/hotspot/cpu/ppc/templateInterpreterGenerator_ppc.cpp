87f3d2b870a1534183c4a70db4526532bc858d04
Richard Reingruber <rrich@openjdk.org>
2022-05-11 13:21:43 +0000

    8286446: PPC64: fix crashes after JDK-8284161 (virtual threads preview)
    Reviewed-by: mdoerr, coleenp, shade

--- src/hotspot/cpu/ppc/templateInterpreterGenerator_ppc.cpp
+++ src/hotspot/cpu/ppc/templateInterpreterGenerator_ppc.cpp
@@ -477,6 +477,7 @@ address TemplateInterpreterGenerator::generate_abstract_entry(void) {
 }
 
 address TemplateInterpreterGenerator::generate_Continuation_doYield_entry(void) {
+  if (!Continuations::enabled()) return nullptr;
   Unimplemented();
   return NULL;
 }
