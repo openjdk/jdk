3979fc6a612559cd827d785c1ba6e0b7748d08ec
Guoxiong Li <gli@openjdk.org>
2022-05-12 13:05:57 +0000

    8286573: Remove the unnecessary method Attr#attribTopLevel and its usage
    Reviewed-by: vromero

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -5155,27 +5155,11 @@ public class Attr extends JCTree.Visitor {
             case MODULEDEF:
                 attribModule(env.tree.pos(), ((JCModuleDecl)env.tree).sym);
                 break;
-            case TOPLEVEL:
-                attribTopLevel(env);
-                break;
             case PACKAGEDEF:
                 attribPackage(env.tree.pos(), ((JCPackageDecl) env.tree).packge);
                 break;
             default:
                 attribClass(env.tree.pos(), env.enclClass.sym);
-        }
-    }
-
-    /**
-     * Attribute a top level tree. These trees are encountered when the
-     * package declaration has annotations.
-     */
-    public void attribTopLevel(Env<AttrContext> env) {
-        JCCompilationUnit toplevel = env.toplevel;
-        try {
-            annotate.flush();
-        } catch (CompletionFailure ex) {
-            chk.completionError(toplevel.pos(), ex);
         }
     }
 
