9a7bb6bb1e34d6d72b2fc786432efcdf5245d967
Jorn Vernee <jorn.vernee@oracle.com>
2022-05-12 16:14:30 +0200

    Update Oracle copyright years

--- src/hotspot/cpu/zero/vmreg_zero.cpp
+++ src/hotspot/cpu/zero/vmreg_zero.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, 2022, Oracle and/or its affiliates. All rights reserved.
  * Copyright 2007 Red Hat, Inc.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
