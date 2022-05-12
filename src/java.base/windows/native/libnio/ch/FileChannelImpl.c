95f7a0ae3689472d34f24e7e6ca94680954f8b99
Brian Burkhalter <brian.burkhalter@oracle.com>
2022-05-12 11:43:44 -0700

    8286637: (fc) Memory mapped regions bigger than 2GB do not work correctly on Windows

--- src/java.base/windows/native/libnio/ch/FileChannelImpl.c
+++ src/java.base/windows/native/libnio/ch/FileChannelImpl.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -110,7 +110,7 @@ Java_sun_nio_ch_FileChannelImpl_map0(JNIEnv *env, jobject this,
         mapAccess,           /* Read and write access */
         highOffset,          /* High word of offset */
         lowOffset,           /* Low word of offset */
-        (DWORD)len);         /* Number of bytes to map */
+        (SIZE_T)len);        /* Number of bytes to map */
     mapError = GetLastError();
 
     result = CloseHandle(mapping);
---
+++ test/jdk/java/nio/channels/FileChannel/LargeMapTest.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import jdk.incubator.foreign.MemorySegment;
+import jdk.incubator.foreign.ResourceScope;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+
+/*
+ * @test
+ * @bug 8286637
+ * @requires os.family == "windows"
+ * @summary Ensure that memory mapping beyond 32-bit range does not cause an
+ *          EXCEPTION_ACCESS_VIOLATION.
+ */
+public class LargeMapTest {
+    private static final long LENGTH = 8000000000L;
+    private static final long OFFSET = 3704800000L;
+    private static final int  BUFSIZ = 100000;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(System.getProperty("sun.arch.data.model"));
+        System.out.println(System.getProperty("os.arch"));
+        System.out.println(System.getProperty("java.version"));
+
+        Path p = Path.of("test.dat");
+        RandomAccessFile raf = new RandomAccessFile("test.dat", "rw");
+        raf.setLength(LENGTH); //~8gb
+        raf.close();
+        p.toFile().deleteOnExit();
+
+
+        long offset = OFFSET;
+        ByteBuffer bb = ByteBuffer.allocateDirect(BUFSIZ);
+
+        MemorySegment mbb = MemorySegment.ofByteBuffer(bb);
+        MemorySegment mappedMemorySegment = MemorySegment.mapFile(p, 0,
+            p.toFile().length(), FileChannel.MapMode.READ_WRITE,
+            ResourceScope.newSharedScope());
+
+        while (offset < LENGTH) {
+            System.out.println("offset: " + offset);
+            MemorySegment target = mappedMemorySegment.asSlice(offset, BUFSIZ);
+            offset = offset + BUFSIZ;
+            target.copyFrom(mbb);
+        }
+    }
+}
