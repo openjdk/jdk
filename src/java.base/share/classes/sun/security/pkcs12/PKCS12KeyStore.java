386bf57ec6e16f2334b922133462d123fc277049
Weijun Wang <weijun@openjdk.org>
2022-05-12 13:27:45 +0000

    8286422: Add OIDs for RC2 and Blowfish
    Reviewed-by: hchao, ascarpino

--- src/java.base/share/classes/sun/security/pkcs12/PKCS12KeyStore.java
+++ src/java.base/share/classes/sun/security/pkcs12/PKCS12KeyStore.java
@@ -407,6 +407,8 @@ public final class PKCS12KeyStore extends KeyStoreSpi {
                         byte[] keyBytes = in.getOctetString();
                         if (keyAlgo.equals(KnownOIDs.OIW_DES_CBC.stdName())) {
                             keyAlgo = "DES";
+                        } else if (keyAlgo.equals(KnownOIDs.RC2$CBC$PKCS5Padding.stdName())) {
+                            keyAlgo = "RC2";
                         }
                         SecretKeySpec secretKeySpec =
                                 new SecretKeySpec(keyBytes, keyAlgo);
