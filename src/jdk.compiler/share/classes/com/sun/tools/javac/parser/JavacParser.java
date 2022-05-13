bcbafe97231e17a3829709e6279c9564388121cc
Angelos Bimpoudis <angelos.bimpoudis@oracle.com>
2022-05-13 16:16:51 +0200

    Add precise error for enums with empty parameter lists

--- src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/parser/JavacParser.java
@@ -4033,8 +4033,13 @@ public class JavacParser implements Parser {
 
         Name name = typeName();
 
-        if(typeParametersOpt().size() > 0) {
-            log.error(DiagnosticFlag.SYNTAX, S.prevToken().endPos, Errors.EnumCantBeGeneric);
+        int typeNamePos = token.pos;
+        List<JCTypeParameter> typarams = typeParametersOpt(true);
+        if (typarams == null || !typarams.isEmpty()) {
+            int errorPosition = typarams == null
+                    ? typeNamePos
+                    : typarams.head.pos;
+            log.error(DiagnosticFlag.SYNTAX, errorPosition, Errors.EnumCantBeGeneric);
         }
 
         List<JCExpression> implementing = List.nil();
@@ -4539,9 +4544,21 @@ public class JavacParser implements Parser {
      *  }
      */
     protected List<JCTypeParameter> typeParametersOpt() {
+        return typeParametersOpt(false);
+    }
+    /** Parses a potentially empty type parameter list if needed with `allowEmpty`.
+     *  The caller is free to choose the desirable error message in this (erroneous) case.
+     */
+    protected List<JCTypeParameter> typeParametersOpt(boolean parseEmpty) {
         if (token.kind == LT) {
             ListBuffer<JCTypeParameter> typarams = new ListBuffer<>();
             nextToken();
+
+            if (parseEmpty && token.kind == GT) {
+                accept(GT);
+                return null;
+            }
+
             typarams.append(typeParameter());
             while (token.kind == COMMA) {
                 nextToken();
