fab593a3549a9c5673846e7025e13f67e50b0c92
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2022-05-13 14:46:34 -0700

    JDK-8284037: Snippet-files subdirectory not automatically detected when in unnamed package

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/SnippetTaglet.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/SnippetTaglet.java
@@ -38,7 +38,6 @@ import javax.lang.model.element.PackageElement;
 import javax.tools.Diagnostic;
 import javax.tools.DocumentationTool.Location;
 import javax.tools.FileObject;
-import javax.tools.JavaFileManager;
 
 import com.sun.source.doctree.AttributeTree;
 import com.sun.source.doctree.DocTree;
@@ -215,14 +214,14 @@ public class SnippetTaglet extends BaseTaglet {
             // we didn't create JavaFileManager, so we won't close it; even if an error occurs
             var fileManager = writer.configuration().getFileManager();
 
-            // first, look in local snippet-files subdirectory
-            Utils utils = writer.configuration().utils;
-            PackageElement pkg = getPackageElement(holder, utils);
-            JavaFileManager.Location l = utils.getLocationForPackage(pkg);
-            String relativeName = "snippet-files/" + v;
-            String packageName = packageName(pkg, utils);
             try {
-                fileObject = fileManager.getFileForInput(l, packageName, relativeName);
+                // first, look in local snippet-files subdirectory
+                var utils = writer.configuration().utils;
+                var pkg = getPackageElement(holder, utils);
+                var pkgLocn = utils.getLocationForPackage(pkg);
+                var pkgName = pkg.getQualifiedName().toString(); // note: empty string for unnamed package
+                var relativeName = "snippet-files/" + v;
+                fileObject = fileManager.getFileForInput(pkgLocn, pkgName, relativeName);
 
                 // if not found in local snippet-files directory, look on snippet path
                 if (fileObject == null && fileManager.hasLocation(Location.SNIPPET_PATH)) {
