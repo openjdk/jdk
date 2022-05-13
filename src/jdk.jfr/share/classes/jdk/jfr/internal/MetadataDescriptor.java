78ffefb8492eb48a9bd964e7184830d4762e5adb
Erik Gahlin <egahlin@openjdk.org>
2022-05-13 15:33:04 +0000

    8286668: JFR: Cleanup
    Reviewed-by: mgronlun

--- src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataDescriptor.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataDescriptor.java
@@ -41,14 +41,7 @@ import jdk.jfr.internal.consumer.RecordingInput;
  */
 public final class MetadataDescriptor {
 
-    static final class Attribute {
-        final String name;
-        final String value;
-
-        private Attribute(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
+    record Attribute(String name, String value) {
     }
 
     static final class Element {
--- src/jdk.jfr/share/classes/jdk/jfr/internal/consumer/ConstantLookup.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/consumer/ConstantLookup.java
@@ -28,7 +28,7 @@ package jdk.jfr.internal.consumer;
 import jdk.jfr.internal.Type;
 
 final class ConstantLookup {
-    final Type type;
+    private final Type type;
     private ConstantMap current;
     private ConstantMap previous = ConstantMap.EMPTY;
 
