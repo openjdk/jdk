76caeed498d868c7923461fb481349c0a2cbd99d
Magnus Ihse Bursie <ihse@openjdk.org>
2022-05-13 15:17:22 +0000

    8285366: Fix typos in serviceability
    Reviewed-by: kevinw, sspitsyn

--- src/java.management/share/classes/com/sun/jmx/remote/internal/ArrayNotificationBuffer.java
+++ src/java.management/share/classes/com/sun/jmx/remote/internal/ArrayNotificationBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -437,7 +437,7 @@ public class ArrayNotificationBuffer implements NotificationBuffer {
                     if (isDisposed()) {
                         if (logger.debugOn())
                             logger.debug("fetchNotifications",
-                                         "dispose callled, no wait");
+                                         "dispose called, no wait");
                         return new NotificationResult(earliestSequenceNumber(),
                                                   nextSequenceNumber(),
                                                   new TargetedNotification[0]);
--- src/java.management/share/classes/com/sun/jmx/remote/util/EnvHelp.java
+++ src/java.management/share/classes/com/sun/jmx/remote/util/EnvHelp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -267,7 +267,7 @@ public class EnvHelp {
     public static int getNotifBufferSize(Map<String, ?> env) {
         int defaultQueueSize = 1000; // default value
 
-        // keep it for the compability for the fix:
+        // keep it for the compatibility for the fix:
         // 6174229: Environment parameter should be notification.buffer.size
         // instead of buffer.size
         final String oldP = "jmx.remote.x.buffer.size";
