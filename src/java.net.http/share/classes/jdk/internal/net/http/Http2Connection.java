3a3da5434d6bcfca5eb23ae610c2ee5b6a78d3c0
Daniel Fuchs <dfuchs@openjdk.org>
2022-05-13 11:51:42 +0000

    8286194: ExecutorShutdown test fails intermittently
    Reviewed-by: jpai, michaelm

--- src/java.net.http/share/classes/jdk/internal/net/http/Http2Connection.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/Http2Connection.java
@@ -626,11 +626,13 @@ class Http2Connection  {
 
     void close() {
         Log.logTrace("Closing HTTP/2 connection: to {0}", connection.address());
-        GoAwayFrame f = new GoAwayFrame(0,
-                                        ErrorFrame.NO_ERROR,
-                                        "Requested by user".getBytes(UTF_8));
-        // TODO: set last stream. For now zero ok.
-        sendFrame(f);
+        if (connection.channel().isOpen()) {
+            GoAwayFrame f = new GoAwayFrame(0,
+                    ErrorFrame.NO_ERROR,
+                    "Requested by user".getBytes(UTF_8));
+            // TODO: set last stream. For now zero ok.
+            sendFrame(f);
+        }
     }
 
     long count;
