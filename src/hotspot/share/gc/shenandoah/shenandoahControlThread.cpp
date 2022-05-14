9eb15c9b100b87e332c572bbc24a818e1cceb180
Jie Fu <jiefu@openjdk.org>
2022-05-14 10:13:49 +0000

    8286681: ShenandoahControlThread::request_gc misses the case of GCCause::_codecache_GC_threshold
    Reviewed-by: zgu

--- src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahControlThread.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2013, 2021, Red Hat, Inc. All rights reserved.
+ * Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -478,6 +479,7 @@ void ShenandoahControlThread::request_gc(GCCause::Cause cause) {
   assert(GCCause::is_user_requested_gc(cause) ||
          GCCause::is_serviceability_requested_gc(cause) ||
          cause == GCCause::_metadata_GC_clear_soft_refs ||
+         cause == GCCause::_codecache_GC_threshold ||
          cause == GCCause::_full_gc_alot ||
          cause == GCCause::_wb_full_gc ||
          cause == GCCause::_wb_breakpoint ||
