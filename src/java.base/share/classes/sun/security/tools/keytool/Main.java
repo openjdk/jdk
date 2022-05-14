0d9f8d10aa85a89cc7c1e72df14a6ecc7bbf62c2
John Jiang <johnsjiang@tencent.com>
2022-05-14 21:39:04 +0800

    8286773: cleanup @returns in sun.security classes

--- src/java.base/share/classes/sun/security/tools/keytool/Main.java
+++ src/java.base/share/classes/sun/security/tools/keytool/Main.java
@@ -4070,7 +4070,7 @@ public final class Main {
      *                 It must have the same public key as certToVerify
      *                 but cannot be the same cert.
      * @param certToVerify the starting certificate to build the chain
-     * @returns the established chain, might be null if user decides not
+     * @return the established chain, might be null if user decides not
      */
     private Certificate[] establishCertChain(Certificate userCert,
                                              Certificate certToVerify)
