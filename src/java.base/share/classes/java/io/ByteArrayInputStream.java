e6bd4a93ed32c37a1d1b756a23ccc5c93cf64d76
Joe Darcy <joe.darcy@oracle.com>
2022-05-15 11:27:39 -0700

    JDK-8286783: Expand use of @inheritDoc in InputStream and OutputStream subclasses

--- src/java.base/share/classes/java/io/ByteArrayInputStream.java
+++ src/java.base/share/classes/java/io/ByteArrayInputStream.java
@@ -139,9 +139,9 @@ public class ByteArrayInputStream extends InputStream {
      * This {@code read} method
      * cannot block.
      *
-     * @return  the next byte of data, or {@code -1} if the end of the
-     *          stream has been reached.
+     * @return  {@inheritDoc}
      */
+    @Override
     public synchronized int read() {
         return (pos < count) ? (buf[pos++] & 0xff) : -1;
     }
@@ -162,17 +162,14 @@ public class ByteArrayInputStream extends InputStream {
      * <p>
      * This {@code read} method cannot block.
      *
-     * @param   b     the buffer into which the data is read.
-     * @param   off   the start offset in the destination array {@code b}
-     * @param   len   the maximum number of bytes read.
-     * @return  the total number of bytes read into the buffer, or
-     *          {@code -1} if there is no more data because the end of
-     *          the stream has been reached.
-     * @throws  NullPointerException If {@code b} is {@code null}.
-     * @throws  IndexOutOfBoundsException If {@code off} is negative,
-     * {@code len} is negative, or {@code len} is greater than
-     * {@code b.length - off}
+     * @param   b     {@inheritDoc}
+     * @param   off   {@inheritDoc}
+     * @param   len   {@inheritDoc}
+     * @return  {@inheritDoc}
+     * @throws  NullPointerException {@inheritDoc}
+     * @throws  IndexOutOfBoundsException {@inheritDoc}
      */
+    @Override
     public synchronized int read(byte[] b, int off, int len) {
         Objects.checkFromIndexSize(off, len, b.length);
 
@@ -219,9 +216,10 @@ public class ByteArrayInputStream extends InputStream {
      * The value {@code k} is added into {@code pos}
      * and {@code k} is returned.
      *
-     * @param   n   the number of bytes to be skipped.
+     * @param   n   {@inheritDoc}
      * @return  the actual number of bytes skipped.
      */
+    @Override
     public synchronized long skip(long n) {
         long k = count - pos;
         if (n < k) {
@@ -242,17 +240,20 @@ public class ByteArrayInputStream extends InputStream {
      * @return  the number of remaining bytes that can be read (or skipped
      *          over) from this input stream without blocking.
      */
+    @Override
     public synchronized int available() {
         return count - pos;
     }
 
     /**
-     * Tests if this {@code InputStream} supports mark/reset. The
-     * {@code markSupported} method of {@code ByteArrayInputStream}
+     * Tests if this {@code InputStream} supports mark/reset.
+     * @implSpec
+     * The {@code markSupported} method of {@code ByteArrayInputStream}
      * always returns {@code true}.
-     *
+     * @return true
      * @since   1.1
      */
+    @Override
     public boolean markSupported() {
         return true;
     }
@@ -281,6 +282,7 @@ public class ByteArrayInputStream extends InputStream {
      * is 0 unless another position was marked or an offset was specified
      * in the constructor.
      */
+    @Override
     public synchronized void reset() {
         pos = mark;
     }
@@ -290,7 +292,7 @@ public class ByteArrayInputStream extends InputStream {
      * this class can be called after the stream has been closed without
      * generating an {@code IOException}.
      */
+    @Override
     public void close() throws IOException {
     }
-
 }
--- src/java.base/share/classes/java/io/FilterInputStream.java
+++ src/java.base/share/classes/java/io/FilterInputStream.java
@@ -58,20 +58,12 @@ public class FilterInputStream extends InputStream {
     }
 
     /**
-     * Reads the next byte of data from this input stream. The value
-     * byte is returned as an {@code int} in the range
-     * {@code 0} to {@code 255}. If no byte is available
-     * because the end of the stream has been reached, the value
-     * {@code -1} is returned. This method blocks until input data
-     * is available, the end of the stream is detected, or an exception
-     * is thrown.
-     *
+     * {@inheritDoc}
      * @implSpec
      * This method simply performs {@code in.read()} and returns the result.
      *
-     * @return     the next byte of data, or {@code -1} if the end of the
-     *             stream is reached.
-     * @throws     IOException  if an I/O error occurs.
+     * @return     {@inheritDoc}
+     * @throws     IOException  {@inheritDoc}
      * @see        java.io.FilterInputStream#in
      */
     @Override
@@ -93,10 +85,8 @@ public class FilterInputStream extends InputStream {
      * depend on the implementation strategy actually
      * used.
      *
-     * @param      b   the buffer into which the data is read.
-     * @return     the total number of bytes read into the buffer, or
-     *             {@code -1} if there is no more data because the end of
-     *             the stream has been reached.
+     * @param      b   {@inheritDoc}
+     * @return     {@inheritDoc}
      * @throws     IOException  if an I/O error occurs.
      * @see        java.io.FilterInputStream#read(byte[], int, int)
      */
@@ -115,16 +105,12 @@ public class FilterInputStream extends InputStream {
      * This method simply performs {@code in.read(b, off, len)}
      * and returns the result.
      *
-     * @param      b     the buffer into which the data is read.
-     * @param      off   the start offset in the destination array {@code b}
-     * @param      len   the maximum number of bytes read.
-     * @return     the total number of bytes read into the buffer, or
-     *             {@code -1} if there is no more data because the end of
-     *             the stream has been reached.
-     * @throws     NullPointerException If {@code b} is {@code null}.
-     * @throws     IndexOutOfBoundsException If {@code off} is negative,
-     *             {@code len} is negative, or {@code len} is greater than
-     *             {@code b.length - off}
+     * @param      b     {@inheritDoc}
+     * @param      off   {@inheritDoc}
+     * @param      len   {@inheritDoc}
+     * @return     {@inheritDoc}
+     * @throws     NullPointerException {@inheritDoc}
+     * @throws     IndexOutOfBoundsException {@inheritDoc}
      * @throws     IOException  if an I/O error occurs.
      * @see        java.io.FilterInputStream#in
      */
@@ -143,7 +129,7 @@ public class FilterInputStream extends InputStream {
      * @implSpec
      * This method simply performs {@code in.skip(n)} and returns the result.
      *
-     * @param      n   the number of bytes to be skipped.
+     * @param      n   {@inheritDoc}
      * @return     the actual number of bytes skipped.
      * @throws     IOException  if {@code in.skip(n)} throws an IOException.
      */
@@ -164,7 +150,7 @@ public class FilterInputStream extends InputStream {
      *
      * @return     an estimate of the number of bytes that can be read (or
      *             skipped over) from this input stream without blocking.
-     * @throws     IOException  if an I/O error occurs.
+     * @throws     IOException  {@inheritDoc}
      */
     @Override
     public int available() throws IOException {
@@ -172,13 +158,11 @@ public class FilterInputStream extends InputStream {
     }
 
     /**
-     * Closes this input stream and releases any system resources
-     * associated with the stream.
-     *
+     * {@inheritDoc}
      * @implSpec
      * This method simply performs {@code in.close()}.
      *
-     * @throws     IOException  if an I/O error occurs.
+     * @throws     IOException  {@inheritDoc}
      * @see        java.io.FilterInputStream#in
      */
     @Override
@@ -198,8 +182,7 @@ public class FilterInputStream extends InputStream {
      * @implSpec
      * This method simply performs {@code in.mark(readlimit)}.
      *
-     * @param   readlimit   the maximum limit of bytes that can be read before
-     *                      the mark position becomes invalid.
+     * @param   readlimit   {@inheritDoc}
      * @see     java.io.FilterInputStream#in
      * @see     java.io.FilterInputStream#reset()
      */
@@ -224,8 +207,7 @@ public class FilterInputStream extends InputStream {
      * @implSpec
      * This method simply performs {@code in.reset()}.
      *
-     * @throws     IOException  if the stream has not been marked or if the
-     *               mark has been invalidated.
+     * @throws     IOException  {@inheritDoc}
      * @see        java.io.FilterInputStream#in
      * @see        java.io.FilterInputStream#mark(int)
      */
