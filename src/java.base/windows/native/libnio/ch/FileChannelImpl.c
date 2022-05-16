0d53bdb9ab60510bbb13da59d72d82de4bec4d11
Brian Burkhalter <brian.burkhalter@oracle.com>
2022-05-16 13:03:42 -0700

    8286763: [REDO] (fc) Tune FileChannel.transferFrom()

--- src/java.base/windows/native/libnio/ch/FileChannelImpl.c
+++ src/java.base/windows/native/libnio/ch/FileChannelImpl.c
@@ -188,6 +188,13 @@ Java_sun_nio_ch_FileChannelImpl_transferTo0(JNIEnv *env, jobject this,
     return chunkSize;
 }
 
+JNIEXPORT jlong JNICALL
+Java_sun_nio_ch_FileChannelImpl_transferFrom0(JNIEnv *env, jobject this,
+                                              jobject srcFDO, jobject dstFDO,
+                                              jlong position, jlong count)
+{
+    return IOS_UNSUPPORTED;
+}
 
 JNIEXPORT jint JNICALL
 Java_sun_nio_ch_FileChannelImpl_maxDirectTransferSize0(JNIEnv* env, jobject this)
