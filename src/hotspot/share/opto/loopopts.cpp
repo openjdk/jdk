d71f6de44b60a12267b16ace0ecf1d2ba80cd7dc
Martin Doerr <martin.doerr@sap.com>
2022-05-16 14:31:15 +0200

    8286182: C2: crash with SIGFPE when executing compiled code

--- src/hotspot/share/opto/loopopts.cpp
+++ src/hotspot/share/opto/loopopts.cpp
@@ -65,7 +65,8 @@ Node* PhaseIdealLoop::split_thru_phi(Node* n, Node* region, int policy) {
   // phi p of a trip-counted (integer) loop whose inputs could be zero (include zero in their type range). p could have a more precise type
   // range that does not necessarily include all values of its inputs. Since each of these inputs will be a divisor of the newly cloned nodes
   // of 'n', we need to bail out of one of these divisors could be zero (zero in its type range).
-  if ((n->Opcode() == Op_DivI || n->Opcode() == Op_ModI) && n->in(0) == NULL
+  if ((n->Opcode() == Op_DivI || n->Opcode() == Op_NoOvfDivI || n->Opcode() == Op_ModI || n->Opcode() == Op_NoOvfModI)
+      && n->in(0) == NULL
       && region->is_CountedLoop() && n->in(2) == region->as_CountedLoop()->phi()) {
     Node* phi = region->as_CountedLoop()->phi();
     for (uint i = 1; i < phi->req(); i++) {
--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -1689,7 +1689,9 @@ void PhaseIterGVN::remove_speculative_types()  {
 bool PhaseIterGVN::no_dependent_zero_check(Node* n) const {
   switch (n->Opcode()) {
     case Op_DivI:
-    case Op_ModI: {
+    case Op_NoOvfDivI:
+    case Op_ModI:
+    case Op_NoOvfModI: {
       // Type of divisor includes 0?
       if (type(n->in(2)) == Type::TOP) {
         // 'n' is dead. Treat as if zero check is still there to avoid any further optimizations.
@@ -1699,7 +1701,9 @@ bool PhaseIterGVN::no_dependent_zero_check(Node* n) const {
       return (type_divisor->_hi < 0 || type_divisor->_lo > 0);
     }
     case Op_DivL:
-    case Op_ModL: {
+    case Op_NoOvfDivL:
+    case Op_ModL:
+    case Op_NoOvfModL: {
       // Type of divisor includes 0?
       if (type(n->in(2)) == Type::TOP) {
         // 'n' is dead. Treat as if zero check is still there to avoid any further optimizations.
