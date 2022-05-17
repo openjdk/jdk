c565201f5291ecb8bbc561cc04e06ce2ed9c95dc
Zhengyu Gu <zgu@redhat.com>
2022-05-17 13:03:53 -0400

    Add comments

--- src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -2310,10 +2310,13 @@ void ShenandoahHeap::flush_liveness_cache(uint worker_id) {
 bool ShenandoahHeap::requires_barriers(stackChunkOop obj) const {
   if (is_idle()) return false;
 
+  // Objects allocated after marking start are implicitly alive, don't need any barriers during
+  // marking phase.
   if (is_concurrent_mark_in_progress() && marking_context()->allocated_after_mark_start(obj)) {
     return false;
   }
-
+  // Objects allocated after evacuation start are guaranteed in to-space, don't need any barriers
+  // during evacuation/update references phases.
   if (has_forwarded_objects() &&
       cast_from_oop<HeapWord*>(obj) >= heap_region_containing(obj)->get_update_watermark()) {
     return false;
