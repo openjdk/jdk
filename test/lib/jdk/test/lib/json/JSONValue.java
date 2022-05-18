aff547c1bced27e8d1c7a3723a0bad24b7042e44
Erik Gahlin <egahlin@openjdk.org>
2022-05-18 15:07:29 +0000

    8286925: Move JSON parser used in JFR tests to test library
    Reviewed-by: mgronlun, alanb

--- test/lib/jdk/test/lib/json/JSONValue.java
+++ test/lib/jdk/test/lib/json/JSONValue.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package jdk.jfr.tool;
+package jdk.test.lib.json;
 
 import java.util.ArrayList;
 import java.util.HashMap;
