f072d3863ebe62623898061613f0ada1433cece5
Severin Gehwolf <sgehwolf@redhat.com>
2022-05-18 18:51:23 +0200

    Separate into function. Fix comment.

--- src/hotspot/os/linux/cgroupV1Subsystem_linux.cpp
+++ src/hotspot/os/linux/cgroupV1Subsystem_linux.cpp
@@ -32,6 +32,7 @@
 #include "runtime/os.hpp"
 #include "utilities/globalDefinitions.hpp"
 
+
 /*
  * Set directory to subsystem specific files based
  * on the contents of the mountinfo and cgroup files.
@@ -73,7 +74,7 @@ void CgroupV1Controller::set_subsystem_path(char *cgroup_path) {
             _path = os::strdup(buf);
           }
         } else {
-          // Find the longest common substring as namespaces are hierarchical.
+          // Find the longest common prefix as namespaces are hierarchical.
           // If there are no matches between the root and the cgroup path,
           // the controller's mount point will be used, which is a reasonable
           // fallback.
@@ -81,20 +82,30 @@ void CgroupV1Controller::set_subsystem_path(char *cgroup_path) {
           ss.print_raw(_mount_point);
           const char* root_p = _root;
           const char* cgroup_p = cgroup_path;
-          int last_matching_slash_pos = -1;
-          for (int i = 0; *root_p == *cgroup_p && *root_p != 0; i++) {
-            if (*root_p == '/') {
-              last_matching_slash_pos = i;
-            }
-            root_p++; cgroup_p++;
-          }
-          ss.print_raw(_root, last_matching_slash_pos);
+          int last_slash = find_last_slash_pos(root_p, cgroup_p);
+          assert(last_slash >= 0, "not an absolute path?");
+          ss.print_raw(_root, last_slash);
           _path = os::strdup(ss.base());
         }
       }
     }
   }
 }
+
+// Return last index of '/' in s1 and s2 up to which point paths
+// are the same in s1 and s2. -1 otherwise. For example
+// returns 4 for s1 = "/cat/dog" and s2 = "/cat/cow".
+int CgroupV1Controller::find_last_slash_pos(const char* s1, const char* s2) {
+  int last_matching_slash_pos = -1;
+  for (int i = 0; *s1 == *s2 && *s1 != 0; i++) {
+    if (*s1 == '/') {
+      last_matching_slash_pos = i;
+    }
+    s1++; s2++;
+  }
+  return last_matching_slash_pos;
+}
+
 
 /* uses_mem_hierarchy
  *
--- src/hotspot/os/linux/cgroupV1Subsystem_linux.hpp
+++ src/hotspot/os/linux/cgroupV1Subsystem_linux.hpp
@@ -49,6 +49,8 @@ class CgroupV1Controller: public CgroupController {
 
     virtual void set_subsystem_path(char *cgroup_path);
     char *subsystem_path() { return _path; }
+  private:
+    int find_last_slash_pos(const char* s1, const char* s2);
 };
 
 class CgroupV1MemoryController: public CgroupV1Controller {
