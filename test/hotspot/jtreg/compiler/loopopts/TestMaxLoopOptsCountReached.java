73871c70f1d1f517dc5fed8fd713a7ebb29cff74
Tobias Holenstein <tobias.holenstein@oracle.com>
2022-05-18 15:02:02 +0200

    reformat spaces in test

--- test/hotspot/jtreg/compiler/loopopts/TestMaxLoopOptsCountReached.java
+++ test/hotspot/jtreg/compiler/loopopts/TestMaxLoopOptsCountReached.java
@@ -26,95 +26,100 @@
  * @bug 8284944
  * @requires vm.compiler2.enabled
  * @summary triggers the loop optimization phase `LoopOptsCount` many times
- * @run  main/othervm -Xcomp -XX:-PartialPeelLoop -XX:CompileCommand=compileonly,TestMaxLoopOptsCountReached::test TestMaxLoopOptsCountReached
+ * @run main/othervm -Xcomp -XX:-PartialPeelLoop -XX:CompileCommand=compileonly,TestMaxLoopOptsCountReached::test TestMaxLoopOptsCountReached
  */
 
 import java.lang.System.Logger.Level;
 
- public class TestMaxLoopOptsCountReached {
-   static Long a = Long.valueOf(42);
-   class A {
-     static String e(long f, boolean b, String g, Level h, String s,
-                     Object... i) {
-       return "message" + s + new String() + g;
-     }
-   }
-   public static void main(String[] args) {
-     test(null, "", null, null);
-     test(null, "", null, null);
-   }
-   static void test(Integer o, String g, String name, Object obj) {
-     for (Level q : Level.values())
-       for (Level r : Level.values())
-         A.e(a.longValue(), q != Level.OFF, g, null, null);
-     for (Level q : Level.values())
-       for (Level r : Level.values())
-         A.e(a.longValue(), q != Level.OFF, g, null, null);
-     for (Level q : Level.values()) {
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-     }
-     for (Level q : Level.values()) {
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-     }
-     for (Level q : Level.values()) {
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-       for (Level r : Level.values())
-         ;
-     }
-     for (Level q : Level.values()) {
-       for (Level r : Level.values())
-         ;
-       for (Level r : Level.values())
-         ;
-     }
-     for (Level q : Level.values()) {
-       for (Level r : Level.values()) {
-         String msg = q + "message";
-         String val =
-             (q != Level.OFF || name != msg)
-                 ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
-                 : null;
-       }
-       for (Level r : Level.values())
-         ;
-     }
-     for (Level q : Level.values()) {
-       for (Level r : Level.values())
-         ;
-       for (Level r : Level.values())
-         ;
-     }
-   }
- }
+public class TestMaxLoopOptsCountReached {
+
+    static Long a = Long.valueOf(42);
+
+    class A {
+
+        static String e(long f, boolean b, String g, Level h, String s,
+                        Object... i) {
+            return "message" + s + new String() + g;
+        }
+    }
+
+    public static void main(String[] args) {
+        test(null, "", null, null);
+        test(null, "", null, null);
+    }
+
+    static void test(Integer o, String g, String name, Object obj) {
+        for (Level q : Level.values())
+            for (Level r : Level.values())
+                A.e(a.longValue(), q != Level.OFF, g, null, null);
+        for (Level q : Level.values())
+            for (Level r : Level.values())
+                A.e(a.longValue(), q != Level.OFF, g, null, null);
+        for (Level q : Level.values()) {
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+        }
+        for (Level q : Level.values()) {
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+        }
+        for (Level q : Level.values()) {
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+            for (Level r : Level.values())
+                ;
+        }
+        for (Level q : Level.values()) {
+            for (Level r : Level.values())
+                ;
+            for (Level r : Level.values())
+                ;
+        }
+        for (Level q : Level.values()) {
+            for (Level r : Level.values()) {
+                String msg = q + "message";
+                String val =
+                        (q != Level.OFF || name != msg)
+                                ? A.e(a.longValue(), q != Level.OFF, g, null, null, "foo")
+                                : null;
+            }
+            for (Level r : Level.values())
+                ;
+        }
+        for (Level q : Level.values()) {
+            for (Level r : Level.values())
+                ;
+            for (Level r : Level.values())
+                ;
+        }
+    }
+}
