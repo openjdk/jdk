ab81cd02b1a25446b81367ff73e62c49d06188bf
Goetz Lindenmaier <goetz@openjdk.org>
2022-05-18 10:13:53 +0000

    8286293: Tests ShortResponseBody and ShortResponseBodyWithRetry should use less resources
    Backport-of: f143386109bce2a2e7241f685e2df26849a0ad48

--- test/jdk/java/net/httpclient/ShortResponseBodyPostWithRetry.java
+++ test/jdk/java/net/httpclient/ShortResponseBodyPostWithRetry.java
@@ -23,12 +23,13 @@
 
 /*
  * @test
- * @summary Run of ShortResponseBody with -Djdk.httpclient.enableAllMethodRetry
+ * @summary Run of ShortResponseBodyPost with -Djdk.httpclient.enableAllMethodRetry
  * @library /test/lib
  * @build jdk.test.lib.net.SimpleSSLContext
- * @build ShortResponseBody
+ * @build ShortResponseBody ShortResponseBodyPost
  * @run testng/othervm
  *       -Djdk.httpclient.HttpClient.log=headers,errors,channel
  *       -Djdk.httpclient.enableAllMethodRetry
- *       ShortResponseBody
+ *       -Djdk.internal.httpclient.debug=true
+ *       ShortResponseBodyPost
  */
