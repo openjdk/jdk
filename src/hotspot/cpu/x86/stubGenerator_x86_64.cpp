474ff49060e6c2c4545cd7a167c36617a9990d7f
Aleksey Shipilev <shade@redhat.com>
2022-05-18 12:17:43 +0200

    Fix

--- src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
@@ -8229,8 +8229,8 @@ OopMap* continuation_enter_setup(MacroAssembler* masm, int& stack_slots) {
 //
 // Arguments:
 //   rsp: pointer to blank Continuation entry
-//   c_rarg1: pointer to the continuation
-//   c_rarg3: flags
+//   reg_cont_obj: pointer to the continuation
+//   reg_flags: flags
 //
 // Results:
 //   rsp: pointer to filled out ContinuationEntry
@@ -8238,11 +8238,13 @@ OopMap* continuation_enter_setup(MacroAssembler* masm, int& stack_slots) {
 // Kills:
 //   rax
 //
-void fill_continuation_entry(MacroAssembler* masm) {
+void fill_continuation_entry(MacroAssembler* masm, Register reg_cont_obj, Register reg_flags) {
+  assert_different_registers(rax, reg_cont_obj, reg_flags);
+
   DEBUG_ONLY(__ movl(Address(rsp, ContinuationEntry::cookie_offset()), ContinuationEntry::cookie_value());)
 
-  __ movptr(Address(rsp, ContinuationEntry::cont_offset()), c_rarg1);
-  __ movl  (Address(rsp, ContinuationEntry::flags_offset()), c_rarg3);
+  __ movptr(Address(rsp, ContinuationEntry::cont_offset()), reg_cont_obj);
+  __ movl  (Address(rsp, ContinuationEntry::flags_offset()), reg_flags);
   __ movptr(Address(rsp, ContinuationEntry::chunk_offset()), 0);
   __ movl(Address(rsp, ContinuationEntry::argsize_offset()), 0);
   __ movl(Address(rsp, ContinuationEntry::pin_count_offset()), 0);
