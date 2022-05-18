91e955d5ae740c7b4ab0c25ade2d9e44d7fdb5bc
Jaroslav Bachorik <jbachorik@openjdk.org>
2022-05-18 06:45:15 +0000

    8283849: AsyncGetCallTrace may crash JVM on guarantee
    Reviewed-by: dholmes, stuefe

--- src/hotspot/share/runtime/thread.cpp
+++ src/hotspot/share/runtime/thread.cpp
@@ -1011,6 +1011,7 @@ void JavaThread::check_for_valid_safepoint_state() {
 JavaThread::JavaThread() :
   // Initialize fields
 
+  _in_asgct(false),
   _on_thread_list(false),
   DEBUG_ONLY(_java_call_counter(0) COMMA)
   _entry_point(nullptr),
