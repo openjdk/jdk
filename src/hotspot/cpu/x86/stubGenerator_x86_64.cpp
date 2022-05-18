7960e070ef7562f5ad74205706eb4eddf77ff55b
Aleksey Shipilev <shade@redhat.com>
2022-05-18 11:45:07 +0200

    Fix

--- src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
@@ -8199,7 +8199,7 @@ OopMap* continuation_enter_setup(MacroAssembler* masm, int& stack_slots) {
 //          c_rarg3 -- isVirtualThread
 // kills rax
 void fill_continuation_entry(MacroAssembler* masm) {
-  DEBUG_ONLY(__ movl(Address(rsp, ContinuationEntry::cookie_offset()), 0x1234);)
+  DEBUG_ONLY(__ movl(Address(rsp, ContinuationEntry::cookie_offset()), ContinuationEntry::cookie_value());)
 
   __ movptr(Address(rsp, ContinuationEntry::cont_offset()), c_rarg1);
   __ movl  (Address(rsp, ContinuationEntry::flags_offset()), c_rarg3);
