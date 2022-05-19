9f562ef7546c80e6ddce7b932f3f5ca17c741d21
Alexey Ivanov <aivanov@openjdk.org>
2022-05-19 20:04:19 +0000

    8286872: Refactor add/modify notification icon (TrayIcon)
    Reviewed-by: azvegint, kizune, honkar

--- src/java.desktop/windows/native/libawt/windows/awt_TrayIcon.h
+++ src/java.desktop/windows/native/libawt/windows/awt_TrayIcon.h
@@ -52,7 +52,6 @@ public:
 
     virtual void Dispose();
 
-    BOOL SendTrayMessage(DWORD dwMessage);
     void LinkObjects(JNIEnv *env, jobject peer);
     void UnlinkObjects();
 
@@ -153,6 +152,20 @@ private:
         AwtTrayIcon* m_trayIcon;
         TrayIconListItem* m_next;
     };
+
+    BOOL SendTrayMessage(DWORD dwMessage);
+
+    INLINE void AddTrayIcon() {
+        BOOL result = SendTrayMessage(NIM_ADD);
+        // 6270114: Instructs the taskbar to behave according to the Shell version 5.0
+        if (result) {
+            SendTrayMessage(NIM_SETVERSION);
+        }
+    }
+
+    INLINE void ModifyTrayIcon() {
+        SendTrayMessage(NIM_MODIFY);
+    }
 
     static bool m_bDPIChanged;
 
