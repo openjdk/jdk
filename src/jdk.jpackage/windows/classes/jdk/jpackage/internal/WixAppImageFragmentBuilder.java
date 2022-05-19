67a3cea79daa392b76e2364b27c99e630fe11df5
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2022-05-18 23:40:42 -0400

    8281682: Redundant .ico files in Windows app-image cause unnecessary bloat

--- src/jdk.jpackage/windows/classes/jdk/jpackage/internal/WixAppImageFragmentBuilder.java
+++ src/jdk.jpackage/windows/classes/jdk/jpackage/internal/WixAppImageFragmentBuilder.java
@@ -485,17 +485,11 @@ class WixAppImageFragmentBuilder extends WixFragmentBuilder {
 
         Path shortcutPath = folder.getPath(this).resolve(launcherBasename);
         return addComponent(xml, shortcutPath, Component.Shortcut, unused -> {
-            final Path icoFile = IOUtils.addSuffix(
-                    installedAppImage.destktopIntegrationDirectory().resolve(
-                            launcherBasename), ".ico");
-
             xml.writeAttribute("Name", launcherBasename);
             xml.writeAttribute("WorkingDirectory", INSTALLDIR.toString());
             xml.writeAttribute("Advertise", "no");
-            xml.writeAttribute("IconIndex", "0");
             xml.writeAttribute("Target", String.format("[#%s]",
                     Component.File.idOf(launcherPath)));
-            xml.writeAttribute("Icon", Id.Icon.of(icoFile));
         });
     }
 
