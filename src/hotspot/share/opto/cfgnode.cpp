fa1b56ede6eed653f70efbbfff3af5ee6b481ee4
Tobias Hartmann <thartmann@openjdk.org>
2022-05-19 14:52:54 +0000

    8280696: C2 compilation hits assert(is_dominator(c, n_ctrl)) failed
    Reviewed-by: kvn, chagedorn, roland

--- src/hotspot/share/opto/cfgnode.cpp
+++ src/hotspot/share/opto/cfgnode.cpp
@@ -2165,7 +2165,7 @@ Node *PhiNode::Ideal(PhaseGVN *phase, bool can_reshape) {
           doit = false;
           break;
         }
-        if (in(i)->in(AddPNode::Offset) != base) {
+        if (in(i)->in(AddPNode::Base) != base) {
           base = NULL;
         }
         if (in(i)->in(AddPNode::Offset) != offset) {
