6e386669e3832cdf412bc6d485aa358e8811483f
Aleksey Shipilev <shade@openjdk.org>
2022-05-19 09:35:31 +0000

    8286944: Loom: Common ContinuationEntry cookie handling
    Reviewed-by: rbackman, coleenp

--- src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64.cpp
@@ -8239,7 +8239,7 @@ OopMap* continuation_enter_setup(MacroAssembler* masm, int& stack_slots) {
 //   rax
 //
 void fill_continuation_entry(MacroAssembler* masm) {
-  DEBUG_ONLY(__ movl(Address(rsp, ContinuationEntry::cookie_offset()), 0x1234);)
+  DEBUG_ONLY(__ movl(Address(rsp, ContinuationEntry::cookie_offset()), ContinuationEntry::cookie_value());)
 
   __ movptr(Address(rsp, ContinuationEntry::cont_offset()), c_rarg1);
   __ movl  (Address(rsp, ContinuationEntry::flags_offset()), c_rarg3);
