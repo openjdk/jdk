f1893af00da56130921fb72a63b70cc298e70a76
Aleksey Shipilev <shade@redhat.com>
2022-05-19 19:41:02 +0200

    Fix

--- src/hotspot/share/classfile/javaClasses.hpp
+++ src/hotspot/share/classfile/javaClasses.hpp
@@ -1168,8 +1168,8 @@ class jdk_internal_vm_StackChunk: AllStatic {
   static inline int sp(oop chunk);
   static inline void set_sp(oop chunk, int value);
   static inline void set_sp(HeapWord* chunk, int value); // used while allocating
-  static inline intptr_t pc(oop chunk);
-  static inline void set_pc(oop chunk, intptr_t value);
+  static inline address pc(oop chunk);
+  static inline void set_pc(oop chunk, address value);
   static inline int argsize(oop chunk);
   static inline void set_argsize(oop chunk, int value);
   static inline uint8_t flags(oop chunk);
