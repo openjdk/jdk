926f0814f821c2bab56e909bea2a2f547110e666
Alexey Ivanov <alexey.ivanov@oracle.com>
2022-05-19 19:31:51 +0100

    ...and the address of the current archive

--- src/hotspot/share/cds/filemap.cpp
+++ src/hotspot/share/cds/filemap.cpp
@@ -1911,7 +1911,7 @@ bool FileMapInfo::relocate_pointers_in_core_regions(intx addr_delta) {
     address patch_end  = (address)mapped_end();
 
     // the current value of the pointers to be patched must be within this
-    // range (i.e., must be between the requested base address, and the of the current archive).
+    // range (i.e., must be between the requested base address and the address of the current archive).
     // Note: top archive may point to objects in the base archive, but not the other way around.
     address valid_old_base = (address)header()->requested_base_address();
     address valid_old_end  = valid_old_base + mapping_end_offset();
