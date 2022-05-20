f76bd095dca22ac36f01c7ff54b21df896dedb18
Alexander Ilin <alexandre.iline@oracle.com>
2022-05-20 14:14:54 -0700

    1. Wait for ScrollPaneDemo container to be loaded. 2. Wait for the image to be loaded. 3. Save screenshot and XML dump before attempting to use the scrolls.

--- test/jdk/sanity/client/SwingSet/src/ScrollPaneDemoTest.java
+++ test/jdk/sanity/client/SwingSet/src/ScrollPaneDemoTest.java
@@ -21,16 +21,31 @@
  * questions.
  */
 
+import com.sun.swingset3.demos.ResourceManager;
+import org.jemmy2ext.JemmyExt;
 import org.jtregext.GuiTestListener;
 import com.sun.swingset3.demos.scrollpane.ScrollPaneDemo;
 import static com.sun.swingset3.demos.scrollpane.ScrollPaneDemo.DEMO_TITLE;
 import static org.testng.AssertJUnit.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.UIManager;
+
+import org.netbeans.jemmy.ComponentSearcher;
+import org.netbeans.jemmy.operators.ComponentOperator;
+import org.netbeans.jemmy.operators.ContainerOperator;
+import org.netbeans.jemmy.operators.JLabelOperator;
+import org.netbeans.jemmy.util.Dumper;
 import org.testng.annotations.Test;
 import org.netbeans.jemmy.ClassReference;
 import org.netbeans.jemmy.operators.JFrameOperator;
 import org.netbeans.jemmy.operators.JScrollPaneOperator;
 import org.testng.annotations.Listeners;
+
+import java.awt.MediaTracker;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
 
 /*
  * @test
@@ -46,9 +61,13 @@ import org.testng.annotations.Listeners;
  * @build org.jemmy2ext.JemmyExt
  * @build com.sun.swingset3.demos.scrollpane.ScrollPaneDemo
  * @run testng/timeout=600 ScrollPaneDemoTest
+
  */
 @Listeners(GuiTestListener.class)
 public class ScrollPaneDemoTest {
+
+    public static final String IMAGE_DESCRIPTION =
+            new ResourceManager(ScrollPaneDemo.class).getString("ScrollPaneDemo.crayons");
 
     @Test(dataProvider = "availableLookAndFeels", dataProviderClass = TestHelpers.class)
     public void test(String lookAndFeel) throws Exception {
@@ -57,7 +76,9 @@ public class ScrollPaneDemoTest {
         new ClassReference(ScrollPaneDemo.class.getName()).startApplication();
 
         JFrameOperator frame = new JFrameOperator(DEMO_TITLE);
-        JScrollPaneOperator jspo = new JScrollPaneOperator(frame);
+        ContainerOperator<ScrollPaneDemo> scrollPaneDemo =
+                new ContainerOperator<>(frame, c -> c instanceof ScrollPaneDemo);
+        JScrollPaneOperator jspo = new JScrollPaneOperator(scrollPaneDemo);
 
         // Set initial scrollbar positions
         int initialVerticalValue = jspo.getVerticalScrollBar().getValue();
@@ -65,6 +86,25 @@ public class ScrollPaneDemoTest {
 
         System.out.println("Initial Vertical Value = " + jspo.getVerticalScrollBar().getValue());
         System.out.println("Initial HoriZontal Value = " + jspo.getHorizontalScrollBar().getValue());
+
+        JLabelOperator imageLabel = new JLabelOperator(scrollPaneDemo, c -> {
+            if(c instanceof JLabel) {
+                var icon = ((JLabel)c).getIcon();
+                if(icon instanceof ImageIcon) {
+                    return ((ImageIcon)icon).getDescription().equals(IMAGE_DESCRIPTION);
+                }
+            }
+            return false;
+        });
+        imageLabel.waitState(c -> ((ImageIcon)((JLabel)c).getIcon()).getImageLoadStatus() == MediaTracker.COMPLETE);
+
+        //this additional instrumentation is related to JDK-8225013
+        //after the image has been completely loaded, the UI is supposed to be functional
+        //screenshot and dump are created to know what else to wait for, should the test fail again
+        JemmyExt.save(
+                JemmyExt.getRobot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize())),
+                "after-image-load");
+        Dumper.dumpAll("after-image-load.xml");
 
         // Check scroll to Bottom
         {
