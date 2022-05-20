ffe947b88186f37057ec75e963472b5dbb4abc21
Alan Bateman <alan.bateman@oracle.com>
2022-05-20 12:22:08 +0100

    Initial commit

--- src/jdk.management/share/classes/com/sun/management/HotSpotDiagnosticMXBean.java
+++ src/jdk.management/share/classes/com/sun/management/HotSpotDiagnosticMXBean.java
@@ -165,7 +165,13 @@ public interface HotSpotDiagnosticMXBean extends PlatformManagedObject {
          */
         TEXT_PLAIN,
         /**
-         * JSON (JavaScript Object Notation) format.
+         * JSON (JavaScript Object Notation) format
+         * (<a href="https://tools.ietf.org/html/rfc8259">RFC 8259</a>).
+         *
+         * <p> <a href="doc-files/threadDump.schema.json">threadDump.schema.json</a>
+         * describes the thread dump format in draft
+         * <a href="https://tools.ietf.org/html/draft-json-schema-language-02">
+         * JSON Schema Language version 2</a>.
          */
         JSON,
     }
