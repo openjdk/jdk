39aabc1ba472a989216750e7d27b4cb96bb6ff7e
Hannes Wallnoefer <hannes.wallnoefer@oracle.com>
2022-05-20 16:48:05 +0200

    Clean up copy-to-clipboard button styles

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/markup/HtmlStyle.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/markup/HtmlStyle.java
@@ -656,11 +656,6 @@ public enum HtmlStyle {
     // The following constants are used for items in the static and interactive search indexes.
 
     /**
-     * The class for a {@code button} in the search page to copy the search URL to the clipboard.
-     */
-    copyUrl,
-
-    /**
      * The class for a {@code details} element in the search page to show additional information.
      */
     pageSearchDetails,
@@ -914,6 +909,11 @@ public enum HtmlStyle {
      * The class of a {@code ul} element listing classes in the uses page.
      */
     classUses,
+
+    /**
+     * The class for a {@code button} element to copy some page content to the clipboard.
+     */
+    copy,
 
     /**
      * The class of an {@code a} element for a link with an external target.
