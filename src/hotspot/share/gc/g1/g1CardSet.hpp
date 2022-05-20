6569666b050e8b6f3a0aae7d14d96007a1e429b3
Thomas Schatzl <tschatzl@openjdk.org>
2022-05-20 07:39:26 +0000

    8286943: G1: With virtualized remembered sets, maximum number of cards configured is wrong
    Reviewed-by: ayang, iwalulya

--- src/hotspot/share/gc/g1/g1CardSet.hpp
+++ src/hotspot/share/gc/g1/g1CardSet.hpp
@@ -110,7 +110,7 @@ public:
   uint howl_bucket_index(uint card_idx) { return card_idx >> _log2_max_cards_in_howl_bitmap; }
 
   // Full card configuration
-  // Maximum number of cards in a non-full card set for a single region. Card sets
+  // Maximum number of cards in a non-full card set for a single card region. Card sets
   // with more entries per region are coarsened to Full.
   uint max_cards_in_region() const { return _max_cards_in_card_set; }
 
