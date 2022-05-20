cfdd54f3b7a00d38148373e43917e47937a0a849
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-20 15:06:50 +0200

    8189669: Deduplicate VerifyOption documentation

--- src/hotspot/share/gc/g1/heapRegion.hpp
+++ src/hotspot/share/gc/g1/heapRegion.hpp
@@ -581,15 +581,6 @@ public:
   void print() const;
   void print_on(outputStream* st) const;
 
-  // vo == UsePrevMarking -> use "prev" marking information,
-  // vo == UseFullMarking -> use "next" marking bitmap but no TAMS
-  //
-  // NOTE: Only the "prev" marking information is guaranteed to be
-  // consistent most of the time, so most calls to this should use
-  // vo == UsePrevMarking.
-  // Currently there is only one place where this is called with
-  // vo == UseFullMarking, which is to verify the marking during a
-  // full GC.
   void verify(VerifyOption vo, bool *failures) const;
 
   void verify_rem_set(VerifyOption vo, bool *failures) const;
