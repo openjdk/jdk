681a2d977acd9ce7e5e138e19ac2bd3020271d80
lmesnik <leonid.mesnik@oracle.com>
2022-05-21 09:30:22 -0700

    fix

--- test/jdk/java/lang/management/ThreadMXBean/VirtualThreadDeadlocks.java
+++ test/jdk/java/lang/management/ThreadMXBean/VirtualThreadDeadlocks.java
@@ -23,6 +23,7 @@
 
 /**
  * @test
+ * @bug 8284161 8287103
  * @summary Test ThredMXBean.findMonitorDeadlockedThreads with cycles of
  *   platform and virtual threads in deadlock
  * @compile --enable-preview -source ${jdk.version} VirtualThreadDeadlocks.java
@@ -40,7 +41,8 @@ public class VirtualThreadDeadlocks {
     private static final Object LOCK1 = new Object();
     private static final Object LOCK2 = new Object();
 
-    private static volatile boolean lock2Obtained = false;
+    private static volatile boolean lock2Held;
+
     /**
      * PP = test deadlock with two platform threads
      * PV = test deadlock with one platform thread and one virtual thread
@@ -54,7 +56,7 @@ public class VirtualThreadDeadlocks {
                 : Thread.ofVirtual();
         Thread thread1 = builder1.start(() -> {
             synchronized (LOCK1) {
-                while (!lock2Obtained) {
+                while (!lock2Held) {
                     try { Thread.sleep(10); } catch (Exception e) { }
                 }
                 synchronized (LOCK2) { }
@@ -68,7 +70,7 @@ public class VirtualThreadDeadlocks {
                 : Thread.ofVirtual();
         Thread thread2 = builder2.start(() -> {
             synchronized (LOCK2) {
-                lock2Obtained = true;
+                lock2Held = true;
                 synchronized (LOCK1) { }
             }
         });
