50ae8f45640c95ccfec3833ecbbe2863d1b88946
Anthony Vanelverdinghe <dev@anthonyv.be>
2022-05-22 08:22:27 +0200

    Print top-level exception
    Signed-off-by: Anthony Vanelverdinghe <dev@anthonyv.be>

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/SnippetTaglet.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/taglets/SnippetTaglet.java
@@ -230,7 +230,7 @@ public class SnippetTaglet extends BaseTaglet {
                 }
             } catch (IOException | IllegalArgumentException e) { // TODO: test this when JDK-8276892 is integrated
                 // JavaFileManager.getFileForInput can throw IllegalArgumentException in certain cases
-                throw new BadSnippetException(a, "doclet.exception.read.file", v, e.getCause());
+                throw new BadSnippetException(a, "doclet.exception.read.file", v, e);
             }
 
             if (fileObject == null) {
@@ -242,7 +242,7 @@ public class SnippetTaglet extends BaseTaglet {
                 externalContent = fileObject.getCharContent(true).toString();
             } catch (IOException e) {  // TODO: test this when JDK-8276892 is integrated
                 throw new BadSnippetException(a, "doclet.exception.read.file",
-                        fileObject.getName(), e.getCause());
+                        fileObject.getName(), e);
             }
         }
 
