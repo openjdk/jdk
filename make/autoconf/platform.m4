42321984291439ab610a539dacd5de07da7fbaa0
Magnus Ihse Bursie <mag@icus.se>
2022-05-23 12:03:02 +0200

    8287155: Additional make typos

--- make/autoconf/platform.m4
+++ make/autoconf/platform.m4
@@ -384,7 +384,7 @@ AC_DEFUN([PLATFORM_EXTRACT_TARGET_AND_BUILD],
   fi
 ])
 
-# Check if a reduced build (32-bit on 64-bit platforms) is requested, and modify behaviour
+# Check if a reduced build (32-bit on 64-bit platforms) is requested, and modify behavior
 # accordingly. Must be done after setting up build and target system, but before
 # doing anything else with these values.
 AC_DEFUN([PLATFORM_SETUP_TARGET_CPU_BITS],
--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -837,7 +837,7 @@ var getJibProfilesProfiles = function (input, common, data) {
         [ "", common.open_suffix ].forEach(function (suffix) {
             var cmpBaselineName = name + suffix + "-cmp-baseline";
             profiles[cmpBaselineName] = clone(profiles[name + suffix]);
-            // Only compare the images target. This should pressumably be expanded
+            // Only compare the images target. This should presumably be expanded
             // to include more build targets when possible.
             profiles[cmpBaselineName].default_make_targets = [ "images", "test-image" ];
             if (name == "linux-x64") {
