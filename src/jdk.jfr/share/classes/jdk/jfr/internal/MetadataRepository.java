8122466fbb8e3f3450131895551ec5b832845938
Erik Gahlin <egahlin@openjdk.org>
2022-05-23 15:12:21 +0000

    8287113: JFR: Periodic task thread uses period for method sampling events
    Reviewed-by: jbachorik

--- src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
@@ -85,7 +85,7 @@ public final class MetadataRepository {
                 // annotations, such as Period and Threshold.
                 if (pEventType.hasPeriod()) {
                     pEventType.setEventHook(true);
-                    if (!(Type.EVENT_NAME_PREFIX + "ExecutionSample").equals(type.getName())) {
+                    if (!pEventType.isMethodSampling()) {
                         requestHooks.add(new RequestHook(pEventType));
                     }
                 }
