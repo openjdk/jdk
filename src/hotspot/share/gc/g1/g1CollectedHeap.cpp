2214ae829e5e4c8a288c81c6d06c01e79990d1bb
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-23 15:13:20 +0200

    ayang review

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -3317,7 +3317,7 @@ HeapRegion* G1CollectedHeap::alloc_highest_free_region() {
   return NULL;
 }
 
-void G1CollectedHeap::mark_evac_failure_object(oop const obj) const {
+void G1CollectedHeap::mark_evac_failure_object(const oop obj) const {
   // All objects failing evacuation are live. What we'll do is
   // that we'll update the prev marking info so that they are
   // all under PTAMS and explicitly marked.
