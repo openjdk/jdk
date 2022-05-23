602d3f3252cf1096695ab9af41439d7e4f3be3e8
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-23 12:03:18 +0200

    8287153: Whitespace typos in HeapRegion class

--- src/hotspot/share/gc/g1/heapRegion.hpp
+++ src/hotspot/share/gc/g1/heapRegion.hpp
@@ -117,7 +117,7 @@ public:
   // given address.
   bool is_in_reserved(const void* p) const { return _bottom <= p && p < _end; }
 
-  size_t capacity()     const { return byte_size(bottom(), end()); }
+  size_t capacity() const { return byte_size(bottom(), end()); }
   size_t used() const { return byte_size(bottom(), top()); }
   size_t free() const { return byte_size(top(), end()); }
 
