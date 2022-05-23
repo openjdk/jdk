f8284b17b2058c0963a94a39b1c0c4c42766b1b7
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-23 11:27:19 +0200

    8287150: Remove HeapRegion::block_start_const declaration without definition

--- src/hotspot/share/gc/g1/heapRegion.hpp
+++ src/hotspot/share/gc/g1/heapRegion.hpp
@@ -130,8 +130,6 @@ private:
 
   void clear(bool mangle_space);
 
-  HeapWord* block_start_const(const void* p) const;
-
   void mangle_unused_area() PRODUCT_RETURN;
 
   // Try to allocate at least min_word_size and up to desired_size from this region.
