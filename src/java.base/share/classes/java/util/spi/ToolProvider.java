035c1b143709a778620e447d2f088549adfb4f58
Christian Stein <sormuras@gmail.com>
2022-05-23 10:48:24 +0200

    8286815: Add API note to ToolProvider about being reusable/reentrant

--- src/java.base/share/classes/java/util/spi/ToolProvider.java
+++ src/java.base/share/classes/java/util/spi/ToolProvider.java
@@ -50,6 +50,14 @@ import java.util.stream.StreamSupport;
  * arguments that could be provided to the tool when invoking the tool
  * from the command line.
  *
+ * @apiNote
+ * It is recommended that tools implementing this interface are either
+ * reusable and reentrant, or should clearly document any limitations and
+ * restrictions. In this context, reusable means that any one instance of
+ * a tool may be a the target of multiple {@code run} method invocations,
+ * and reentrant means that multiple invocations of {@code run} may occur
+ * concurrently.
+ *
  * @since 9
  */
 public interface ToolProvider {
