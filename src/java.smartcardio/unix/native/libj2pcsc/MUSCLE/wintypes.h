8344ddb5103faf9150133e56fd754e5feee8e755
Valerie Peng <valerie.peng@oracle.com>
2022-05-23 20:57:18 +0000

    Replaced tab with spaces in order to pass jcheck. Undo regression tests since there are no more tabs in the header files.

--- src/java.smartcardio/unix/native/libj2pcsc/MUSCLE/wintypes.h
+++ src/java.smartcardio/unix/native/libj2pcsc/MUSCLE/wintypes.h
@@ -75,36 +75,36 @@ extern "C"
 #else
 
 #ifndef BYTE
-	typedef unsigned char BYTE;
+  typedef unsigned char BYTE;
 #endif
-	typedef unsigned char UCHAR;
-	typedef UCHAR *PUCHAR;
-	typedef unsigned short USHORT;
+  typedef unsigned char UCHAR;
+  typedef UCHAR *PUCHAR;
+  typedef unsigned short USHORT;
 
 #ifndef __COREFOUNDATION_CFPLUGINCOM__
-	typedef unsigned long ULONG;
-	typedef void *LPVOID;
+  typedef unsigned long ULONG;
+  typedef void *LPVOID;
 #endif
 
-	typedef const void *LPCVOID;
-	typedef unsigned long DWORD;
-	typedef DWORD *PDWORD;
-	typedef long LONG;
-	typedef const char *LPCSTR;
-	typedef const BYTE *LPCBYTE;
-	typedef BYTE *LPBYTE;
-	typedef DWORD *LPDWORD;
-	typedef char *LPSTR;
+  typedef const void *LPCVOID;
+  typedef unsigned long DWORD;
+  typedef DWORD *PDWORD;
+  typedef long LONG;
+  typedef const char *LPCSTR;
+  typedef const BYTE *LPCBYTE;
+  typedef BYTE *LPBYTE;
+  typedef DWORD *LPDWORD;
+  typedef char *LPSTR;
 
-	/* these types were deprecated but still used by old drivers and
-	 * applications. So just declare and use them. */
-	typedef LPSTR LPTSTR;
-	typedef LPCSTR LPCTSTR;
+  /* these types were deprecated but still used by old drivers and
+   * applications. So just declare and use them. */
+  typedef LPSTR LPTSTR;
+  typedef LPCSTR LPCTSTR;
 
-	/* types unused by pcsc-lite */
-	typedef short BOOL;
-	typedef unsigned short WORD;
-	typedef ULONG *PULONG;
+  /* types unused by pcsc-lite */
+  typedef short BOOL;
+  typedef unsigned short WORD;
+  typedef ULONG *PULONG;
 
 #endif
 
