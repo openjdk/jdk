a682cc039b81484751c165dd5ede14681f7a0828
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2022-05-24 16:23:56 +0100

    Tweak javadoc for ValueLayout::arrayElementVarHandle

--- src/java.base/share/classes/java/lang/foreign/ValueLayout.java
+++ src/java.base/share/classes/java/lang/foreign/ValueLayout.java
@@ -137,7 +137,7 @@ public sealed class ValueLayout extends AbstractLayout implements MemoryLayout {
      * Can be used to access a multi-dimensional array whose layout is as follows:
      *
      * {@snippet lang=java :
-     * MemoryLayout arrayLayout = MemoryLayout.sequenceLayout(-1,
+     * SequenceLayout arrayLayout = MemoryLayout.sequenceLayout(-1,
      *                                      MemoryLayout.sequenceLayout(10,
      *                                                  MemoryLayout.sequenceLayout(20, ValueLayout.JAVA_INT)));
      * }
@@ -150,6 +150,22 @@ public sealed class ValueLayout extends AbstractLayout implements MemoryLayout {
      * <blockquote><pre>{@code
      * offset = (10 * 20 * 4 * x) + (20 * 4 * y) + (4 * z)
      * }</pre></blockquote>
+     *
+     * Additionally, the values of {@code x}, {@code y} and {@code z} are constrained as follows:
+     * <ul>
+     *     <li>{@code 0 <= x < arrayLayout.elementCount() }</li>
+     *     <li>{@code 0 <= y < 10 }</li>
+     *     <li>{@code 0 <= z < 20 }</li>
+     * </ul>
+     * <p>
+     * Consider the following access expressions:
+     * {@snippet lang=java :
+     * int value1 = arrayHandle.get(10, 2, 4); // ok, accessed offset = 8176
+     * int value2 = arrayHandle.get(0, 0, 30); // out of bounds value for z
+     * }
+     * In the first case, access is well-formed, as the values for {@code x}, {@code y} and {@code z} conform to
+     * the bounds specified above. In the second case, access fails with {@link IndexOutOfBoundsException},
+     * as the value for {@code z} is outside its specified bounds.
      *
      * @param shape the size of each nested array dimension.
      * @return a var handle which can be used to dereference a multi-dimensional array, featuring {@code shape.length + 1}
