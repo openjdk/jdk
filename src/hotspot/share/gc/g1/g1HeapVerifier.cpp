5be4f57a1dc92638e9d53d586a447488f6b0bc2a
Thomas Schatzl <thomas.schatzl@oracle.com>
2022-05-24 10:47:26 +0200

    Removal

--- src/hotspot/share/gc/g1/g1HeapVerifier.cpp
+++ src/hotspot/share/gc/g1/g1HeapVerifier.cpp
@@ -490,14 +490,9 @@ void G1HeapVerifier::verify(VerifyOption vo) {
 
   log_debug(gc, verify)("HeapRegions");
 
-  uint num_workers = UseGCParallelVerification ? _g1h->workers()->active_workers() : 1u;
   G1VerifyTask task(_g1h, vo);
-  _g1h->workers()->run_task(&task, num_workers);
-  if (task.failures()) {
-    failures = true;
-  }
-
-  if (failures) {
+  _g1h->workers()->run_task(&task);
+  if (failures || task.failures()) {
     log_error(gc, verify)("Heap after failed verification (kind %d):", vo);
     // It helps to have the per-region information in the output to
     // help us track down what went wrong. This is why we call
@@ -506,8 +501,9 @@ void G1HeapVerifier::verify(VerifyOption vo) {
     ResourceMark rm;
     LogStream ls(log.error());
     _g1h->print_extended_on(&ls);
+
+    guarantee(false, "there should not have been any failures");
   }
-  guarantee(!failures, "there should not have been any failures");
 }
 
 // Heap region set verification
