194bc08a899d699b34d3d896001af8c642758b1d
Zhengyu Gu <zgu@openjdk.org>
2022-05-24 18:12:03 +0000

    8287249: Zero: Missing BarrierSetNMethod::arm() method
    Reviewed-by: shade

--- src/hotspot/cpu/zero/gc/shared/barrierSetNMethod_zero.cpp
+++ src/hotspot/cpu/zero/gc/shared/barrierSetNMethod_zero.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,10 @@
 #include "utilities/debug.hpp"
 
 void BarrierSetNMethod::deoptimize(nmethod* nm, address* return_address_ptr) {
+  ShouldNotReachHere();
+}
+
+void BarrierSetNMethod::arm(nmethod* nm, int value) {
   ShouldNotReachHere();
 }
 
