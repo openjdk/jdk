5dd935837616e43233991641c67f4c27246432ee
Tyler Steele <tyler.steele@ibm.com>
2022-05-24 08:27:29 -0600

    Remove AIX-specific macro

--- src/hotspot/share/prims/universalUpcallHandler.cpp
+++ src/hotspot/share/prims/universalUpcallHandler.cpp
@@ -46,9 +46,7 @@ extern struct JavaVM_ main_vm;
 struct UpcallContext {
   Thread* attachedThread;
 
-  #ifdef _AIX
-    UpcallContext() {}
-  #endif
+  UpcallContext() {} // Explicit constructor to address XL C compiler bug.
   ~UpcallContext() {
     if (attachedThread != NULL) {
       JavaVM_ *vm = (JavaVM *)(&main_vm);
