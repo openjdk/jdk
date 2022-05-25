ee963c3aa7ccd6074a6f3d5b88a01b619ee29f00
Jaikiran Pai <jaikiran.pai@gmail.com>
2022-05-25 18:42:17 +0530

    8287318: ConcurrentModificationException in sun.net.httpserver.ServerImpl

--- src/jdk.httpserver/share/classes/sun/net/httpserver/ServerImpl.java
+++ src/jdk.httpserver/share/classes/sun/net/httpserver/ServerImpl.java
@@ -374,10 +374,16 @@ class ServerImpl implements TimeSource {
 
                     /* process the selected list now  */
                     Set<SelectionKey> selected = selector.selectedKeys();
-                    Iterator<SelectionKey> iter = selected.iterator();
-                    while (iter.hasNext()) {
-                        SelectionKey key = iter.next();
-                        iter.remove ();
+                    // create a copy of the selected keys so that we can iterate over it
+                    // and at the same time not worry about the underlying Set being
+                    // modified (leading to ConcurrentModificationException) due to
+                    // any subsequent select operations that we invoke on the
+                    // selector (in this same thread).
+                    final Set<SelectionKey> copy = new HashSet<>(selected);
+                    // iterate over the copy
+                    for (final SelectionKey key : copy) {
+                        // remove the key from the original selected keys (live) Set
+                        selected.remove(key);
                         if (key.equals (listenerKey)) {
                             if (terminating) {
                                 continue;
