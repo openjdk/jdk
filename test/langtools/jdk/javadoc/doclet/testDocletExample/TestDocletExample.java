7156f98e324ffd0ab17105b2cb8cb6ce0d718e5b
Jonathan Gibbons <jjg@openjdk.org>
2022-05-25 17:45:02 +0000

    8279513: jdk/javadoc/doclet/testDocletExample/TestDocletExample.java fails after 8278795
    Reviewed-by: prappo

--- test/langtools/jdk/javadoc/doclet/testDocletExample/TestDocletExample.java
+++ test/langtools/jdk/javadoc/doclet/testDocletExample/TestDocletExample.java
@@ -25,6 +25,7 @@
  * @test
  * @bug 8272944
  * @summary Use snippets in jdk.javadoc documentation
+ * @key needs-src needs-src-jdk_javadoc
  * @library /tools/lib ../../lib
  * @modules jdk.compiler/com.sun.tools.javac.api
  *          jdk.compiler/com.sun.tools.javac.main
@@ -53,11 +54,12 @@ public class TestDocletExample extends TestRunner {
         t.runTests(m -> new Object[] { Path.of(m.getName()) });
     }
 
-    SnippetUtils snippets = new SnippetUtils("jdk.javadoc");
+    SnippetUtils snippets;
     ToolBox tb = new ToolBox();
 
-    TestDocletExample() {
+    TestDocletExample() throws SnippetUtils.ConfigurationException {
         super(System.out);
+        snippets = new SnippetUtils("jdk.javadoc");
     }
 
     @Test
@@ -65,6 +67,9 @@ public class TestDocletExample extends TestRunner {
         var docletPkg = snippets.getElements().getPackageElement("jdk.javadoc.doclet");
         var dc = snippets.getDocTrees().getDocCommentTree(docletPkg);
         var entryPointSnippet = snippets.getSnippetById(dc, "entry-point");
+        if (entryPointSnippet == null) {
+            throw new Error("Cannot find snippet \"entry-point\"");
+        }
         var entryPointCode = entryPointSnippet.getBody().getBody();
         var code = """
                 class C {
@@ -89,6 +94,9 @@ public class TestDocletExample extends TestRunner {
         var docletPkg = snippets.getElements().getPackageElement("jdk.javadoc.doclet");
         var dc = snippets.getDocTrees().getDocCommentTree(docletPkg);
         var exampleSnippet = snippets.getSnippetById(dc, "Example.java");
+        if (exampleSnippet == null) {
+            throw new Error("Cannot find snippet \"Example.java\"");
+        }
         var exampleCode = exampleSnippet.getBody().getBody();
 
         // compile it
