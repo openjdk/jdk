e1f140d270cc666d26b888a0a25ca7b02e1239af
Maurizio Cimadamore <mcimadamore@openjdk.org>
2022-05-25 09:50:55 +0000

    8287206: Use WrongThreadException for confinement errors
    Reviewed-by: alanb, darcy, mchung

--- test/jdk/java/foreign/channels/TestSocketChannels.java
+++ test/jdk/java/foreign/channels/TestSocketChannels.java
@@ -54,6 +54,7 @@ import static org.testng.Assert.*;
 public class TestSocketChannels extends AbstractChannelsTest {
 
     static final Class<IllegalStateException> ISE = IllegalStateException.class;
+    static final Class<WrongThreadException> WTE = WrongThreadException.class;
 
     @Test(dataProvider = "closeableSessions")
     public void testBasicIOWithClosedSegment(Supplier<MemorySession> sessionSupplier)
@@ -151,7 +152,7 @@ public class TestSocketChannels extends AbstractChannelsTest {
             );
             for (var ioOp : ioOps) {
                 AtomicReference<Exception> exception = new AtomicReference<>();
-                Runnable task = () -> exception.set(expectThrows(ISE, ioOp));
+                Runnable task = () -> exception.set(expectThrows(WTE, ioOp));
                 var t = new Thread(task);
                 t.start();
                 t.join();
