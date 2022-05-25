e9bddc18ab91c29d491b0e3bd145d641f6a62c5d
Jan Lahoda <jlahoda@openjdk.org>
2022-05-25 11:56:24 +0000

    8262889: Compiler implementation for Record Patterns
    Co-authored-by: Brian Goetz <briangoetz@openjdk.org>
    Co-authored-by: Jan Lahoda <jlahoda@openjdk.org>
    Co-authored-by: Aggelos Biboudis <abimpoudis@openjdk.org>
    Reviewed-by: mcimadamore, vromero

--- src/jdk.compiler/share/classes/com/sun/tools/javac/tree/TreeMaker.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/tree/TreeMaker.java
@@ -500,6 +500,13 @@ public class TreeMaker implements JCTree.Factory {
         return tree;
     }
 
+    public JCRecordPattern RecordPattern(JCExpression deconstructor, List<JCPattern> nested,
+                                         JCVariableDecl var) {
+        JCRecordPattern tree = new JCRecordPattern(deconstructor, nested, var);
+        tree.pos = pos;
+        return tree;
+    }
+
     public JCArrayAccess Indexed(JCExpression indexed, JCExpression index) {
         JCArrayAccess tree = new JCArrayAccess(indexed, index);
         tree.pos = pos;
