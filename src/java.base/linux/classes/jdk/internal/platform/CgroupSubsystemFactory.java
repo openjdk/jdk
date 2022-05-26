9e6c0f37974946ecd9f2bcf2623853931be6b92e
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2022-05-26 08:53:32 -0700

    Made requested changes

--- src/java.base/linux/classes/jdk/internal/platform/CgroupSubsystemFactory.java
+++ src/java.base/linux/classes/jdk/internal/platform/CgroupSubsystemFactory.java
@@ -36,6 +36,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -109,11 +110,11 @@ public class CgroupSubsystemFactory {
         if (result.isCgroupV2()) {
             // For unified it doesn't matter which controller we pick.
             CgroupInfo anyController = infos.values().iterator().next();
-            CgroupSubsystem subsystem = CgroupV2Subsystem.getInstance(anyController);
+            CgroupSubsystem subsystem = CgroupV2Subsystem.getInstance(Objects.requireNonNull(anyController));
             return new CgroupMetrics(subsystem);
         } else {
             CgroupV1Subsystem subsystem = CgroupV1Subsystem.getInstance(infos);
-            return new CgroupV1MetricsImpl(subsystem);
+            return subsystem != null ? new CgroupV1MetricsImpl(subsystem) : null;
         }
     }
 
