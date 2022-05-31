4186c03645fc03c3ad9befb1cc08b6d4b4cd4821
Zhengyu Gu <zgu@redhat.com>
2022-05-31 11:14:08 -0400

    NMethod::heal_nmethod() does not need to handle ref-update

--- src/hotspot/share/gc/shenandoah/shenandoahNMethod.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahNMethod.cpp
@@ -165,9 +165,6 @@ void ShenandoahNMethod::heal_nmethod(nmethod* nm) {
              (Continuations::enabled() && heap->is_evacuation_in_progress())) {
     ShenandoahEvacOOMScope evac_scope;
     heal_nmethod_metadata(data);
-  } else if (Continuations::enabled() && heap->is_update_refs_in_progress()) {
-    ShenandoahConcUpdateRefsClosure cl;
-    data->oops_do(&cl, true /*fix relocation*/);
   } else {
     // There is possibility that GC is cancelled when it arrives final mark.
     // In this case, concurrent root phase is skipped and degenerated GC should be
