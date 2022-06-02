8c0e18175686f5f612db82f930ca2cb63a9e38c4
Joe Darcy <joe.darcy@oracle.com>
2022-06-01 18:05:29 -0700

    JDK-8042981: Strip type annotations in Types' utility methods

--- src/java.compiler/share/classes/javax/lang/model/util/Types.java
+++ src/java.compiler/share/classes/javax/lang/model/util/Types.java
@@ -35,6 +35,10 @@ import javax.lang.model.type.*;
 /**
  * Utility methods for operating on types.
  *
+ * Where a method returns a type mirror or a collection of type
+ * mirrors, any type mirrors represent types with no type annotations,
+ * unless otherwise indicated.
+ *
  * <p><b>Compatibility Note:</b> Methods may be added to this interface
  * in future releases of the platform.
  *
@@ -216,6 +220,8 @@ public interface Types {
     /**
      * {@return an array type with the specified component type}
      *
+     * Annotations on the component type are preserved.
+     *
      * @param componentType  the component type
      * @throws IllegalArgumentException if the component type is not valid for
      *          an array
@@ -225,6 +231,8 @@ public interface Types {
     /**
      * {@return a new wildcard type}  Either of the wildcard's
      * bounds may be specified, or neither, but not both.
+     *
+     * Annotations on the bounds are preserved.
      *
      * @param extendsBound  the extends (upper) bound, or {@code null} if none
      * @param superBound    the super (lower) bound, or {@code null} if none
