6ed972510c748051d53501e1840f21b68aeb66b3
Christoph Langer <clanger@openjdk.org>
2022-06-03 13:09:52 +0000

    8287162: (zipfs) Performance regression related to support for POSIX file permissions
    Backport-of: a10c5597d93c4402bafdbb570437aac052b10027

--- src/jdk.zipfs/share/classes/jdk/nio/zipfs/ZipFileSystem.java
+++ src/jdk.zipfs/share/classes/jdk/nio/zipfs/ZipFileSystem.java
@@ -151,9 +151,9 @@ class ZipFileSystem extends FileSystem {
         this.forceEnd64 = isTrue(env, "forceZIP64End");
         this.defaultCompressionMethod = getDefaultCompressionMethod(env);
         this.supportPosix = isTrue(env, PROPERTY_POSIX);
-        this.defaultOwner = initOwner(zfpath, env);
-        this.defaultGroup = initGroup(zfpath, env);
-        this.defaultPermissions = initPermissions(env);
+        this.defaultOwner = supportPosix ? initOwner(zfpath, env) : null;
+        this.defaultGroup = supportPosix ? initGroup(zfpath, env) : null;
+        this.defaultPermissions = supportPosix ? initPermissions(env) : null;
         this.supportedFileAttributeViews = supportPosix ?
             Set.of("basic", "posix", "zip") : Set.of("basic", "zip");
         if (Files.notExists(zfpath)) {
