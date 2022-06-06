dcc0858f023c0c5496b7a83f57a915c17c3263b5
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-06-06 16:38:31 +0100

    Use RObot.createMultiResoutionCapture

--- test/jdk/javax/swing/text/CSSBorder/6796710/bug6796710.java
+++ test/jdk/javax/swing/text/CSSBorder/6796710/bug6796710.java
@@ -36,6 +36,7 @@ import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
+import java.awt.image.MultiResolutionImage;
 import java.io.File;
 
 import javax.imageio.ImageIO;
@@ -127,7 +128,7 @@ public class bug6796710 {
         Thread.sleep(1000);
 
         BufferedImage pnBottomImage = getPnBottomImage();
-        if (!compareBufferedImages(bufferedImage, pnBottomImage)) {
+        if (!Util.compareBufferedImages(bufferedImage, pnBottomImage)) {
             ImageIO.write(bufferedImage, "png", new File("bufferedImage.png"));
             ImageIO.write(pnBottomImage, "png", new File("pnBottomImage.png"));
             throw new RuntimeException("The test failed");
@@ -136,47 +137,13 @@ public class bug6796710 {
         System.out.println("The test bug6796710 passed.");
     }
 
-    public static boolean compareBufferedImages(BufferedImage bufferedImage0, BufferedImage bufferedImage1) {
-        int width = bufferedImage0.getWidth();
-        int height = bufferedImage0.getHeight();
-        int colorTolerance = 5;
-
-        if (width != bufferedImage1.getWidth() || height != bufferedImage1.getHeight()) {
-            return false;
-        }
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                Color bufCol0 = new Color(bufferedImage0.getRGB(x, y));
-                Color bufCol1 = new Color(bufferedImage1.getRGB(x, y));
-
-                int red1 = bufCol0.getRed();
-                int blue1 = bufCol0.getBlue();
-                int green1 = bufCol0.getGreen();
-
-                int red2 = bufCol1.getRed();
-                int blue2 = bufCol1.getBlue();
-                int green2 = bufCol1.getGreen();
-
-                if ((Math.abs(red1 - red2) > colorTolerance) ||
-                    (Math.abs(green1 - green2) > colorTolerance) ||
-                    (Math.abs(blue1 - blue2) > colorTolerance)) {
-                        System.out.println("x "+ x + " y " + y +
-                            " rgb1: " + Integer.toHexString(bufferedImage0.getRGB(x, y)) +
-                            " rgb2: " + Integer.toHexString(bufferedImage1.getRGB(x, y)));
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
     private static BufferedImage getPnBottomImage() {
         Rectangle rect = pnBottom.getBounds();
 
         Util.convertRectToScreen(rect, pnBottom.getParent());
 
-        return robot.createScreenCapture(rect);
+        MultiResolutionImage img = robot.createMultiResolutionScreenCapture(rect);
+        return (BufferedImage)img.getResolutionVariant(rect.width, rect.height);
+        //return robot.createScreenCapture(rect);
     }
 }
