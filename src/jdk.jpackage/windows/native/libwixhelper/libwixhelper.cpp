7be6376961a149d9062059a3bd2900882d9f04ed
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2022-06-07 21:46:22 -0400

    8283707: Support <major.minor.update.build> version format on Windows

--- src/jdk.jpackage/windows/native/libwixhelper/libwixhelper.cpp
+++ src/jdk.jpackage/windows/native/libwixhelper/libwixhelper.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,71 +23,134 @@
  * questions.
  */
 
-#include <Windows.h>
-#include <msiquery.h>
-#include <shlwapi.h>
 
-extern "C" {
+#include "MsiDb.h"
+#include "MsiCA.h"
+#include "Version.h"
+#include "FileUtils.h"
+#include "WinErrorHandling.h"
 
-#ifdef JP_EXPORT_FUNCTION
-#error Unexpected JP_EXPORT_FUNCTION define
-#endif
-#define JP_EXPORT_FUNCTION comment(linker, "/EXPORT:" __FUNCTION__ "=" __FUNCDNAME__)
 
-    BOOL WINAPI DllMain(HINSTANCE hInst, ULONG ulReason,
-            LPVOID lpvReserved) {
-        return TRUE;
+JP_CA(CheckInstallDir) {
+    const tstring installDir = ca.getProperty(_T("INSTALLDIR"));
+
+    bool canProceed = !FileUtils::isFileExists(installDir);
+    if (!canProceed && FileUtils::isDirectory(installDir)) {
+        canProceed = !FileUtils::isDirectoryNotEmpty(installDir);
     }
 
-    BOOL DirectoryExist(TCHAR *szValue) {
-        DWORD attr = GetFileAttributes(szValue);
-        if (attr == INVALID_FILE_ATTRIBUTES) {
-            return FALSE;
-        }
+    ca.setProperty(_T("INSTALLDIR_VALID"), canProceed ? _T("1") : _T("0"));
+}
 
-        if (attr & FILE_ATTRIBUTE_DIRECTORY) {
-            return TRUE;
-        }
 
-        return FALSE;
+namespace {
+
+typedef Version<
+    VersionDetails::Base<10, VersionDetails::Parser, 2>
+> DottedVersion;
+
+
+class ProductInfo {
+public:
+    explicit ProductInfo(const Guid& pc): productCode(pc),
+            version(msi::getProductInfo(pc, INSTALLPROPERTY_VERSIONSTRING)) {
     }
 
-    UINT __stdcall CheckInstallDir(MSIHANDLE hInstall) {
-        #pragma JP_EXPORT_FUNCTION
+    const DottedVersion& getVersion() const {
+        return version;
+    }
 
-        TCHAR *szValue = NULL;
-        DWORD cchSize = 0;
+    const Guid& getProductCode() const {
+        return productCode;
+    }
 
-        UINT result = MsiGetProperty(hInstall, TEXT("INSTALLDIR"),
-                (LPTSTR)TEXT(""), &cchSize);
-        if (result == ERROR_MORE_DATA) {
-            cchSize = cchSize + 1; // NULL termination
-            szValue = new TCHAR[cchSize];
-            if (szValue) {
-                result = MsiGetProperty(hInstall, TEXT("INSTALLDIR"),
-                        szValue, &cchSize);
+private:
+    Guid productCode;
+    DottedVersion version;
+};
+
+
+void findInstalledPackages(const Guid& upgradeCode,
+                                            std::vector<ProductInfo>& products) {
+    const LPCTSTR upgradeCodeStr = upgradeCode.toMsiString().c_str();
+    for (DWORD productCodeIdx = 0; true; ++productCodeIdx) {
+        TCHAR productCode[39 /* http://msdn.microsoft.com/en-us/library/aa370101(v=vs.85).aspx */];
+        const UINT status = MsiEnumRelatedProducts(upgradeCodeStr, 0,
+                                              productCodeIdx++, productCode);
+        if (ERROR_NO_MORE_ITEMS == status) {
+            break;
+        }
+
+        if (ERROR_SUCCESS != status) {
+            LOG_WARNING(tstrings::any()
+                        << "MsiEnumRelatedProducts("
+                        << upgradeCodeStr << ", "
+                        << productCodeIdx
+                        << ") failed with error=[" << status << "]");
+        }
+
+        JP_NO_THROW(products.push_back(ProductInfo(Guid(productCode))));
+    }
+}
+
+DottedVersion getDottedVersion(const msi::DatabaseRecord& record, UINT idx) {
+    if (!MsiRecordIsNull(record.getHandle(), idx)) {
+        JP_NO_THROW(return DottedVersion(record.getString(idx)));
+    }
+
+    return DottedVersion();
+}
+
+} // namespace
+
+JP_CA(FindRelatedProductsEx) {
+    const Guid upgradeCode = Guid(ca.getProperty(_T("UpgradeCode")));
+
+    std::vector<ProductInfo> installedProducts;
+    findInstalledPackages(upgradeCode, installedProducts);
+
+    const msi::Database db(ca);
+
+    msi::DatabaseView view(db, (tstrings::any()
+            << _T("SELECT `VersionMin`,`VersionMax`,`Language`,`Attributes`,`Remove`,`ActionProperty` FROM Upgrade WHERE `ActionProperty` NOT NULL And `UpgradeCode` = '")
+            << upgradeCode.toMsiString() << _T("'")).tstr());
+    msi::DatabaseRecord record;
+    while (!record.tryFetch(view).empty()) {
+        const DottedVersion versionMin = getDottedVersion(record, 1);
+        const DottedVersion versionMax = getDottedVersion(record, 2);
+
+        const int attrs = MsiRecordIsNull(
+                          record.getHandle(), 4) ? 0 : record.getInteger(4);
+
+        std::vector<ProductInfo>::const_iterator productIt =
+                                                installedProducts.begin();
+        std::vector<ProductInfo>::const_iterator productEnd =
+                                                installedProducts.end();
+        for (; productIt != productEnd; ++productIt) {
+            bool match = false;
+            if (!versionMin.source().empty()) {
+                if (attrs & msidbUpgradeAttributesVersionMinInclusive) {
+                    match = (versionMin <= productIt->getVersion());
+                } else {
+                    match = (versionMin < productIt->getVersion());
+                }
+            }
+
+            if (!versionMax.source().empty()) {
+                if (attrs & msidbUpgradeAttributesVersionMaxInclusive) {
+                    match = (productIt->getVersion() <= versionMax);
+                } else {
+                    match = (productIt->getVersion() < versionMax);
+                }
+            }
+
+            const tstring actionProperty = record.getString(6);
+            if (match) {
+                ca.setProperty(actionProperty,
+                                productIt->getProductCode().toMsiString());
             } else {
-                return ERROR_INSTALL_FAILURE;
+                ca.removeProperty(actionProperty);
             }
         }
-
-        if (result != ERROR_SUCCESS) {
-            delete [] szValue;
-            return ERROR_INSTALL_FAILURE;
-        }
-
-        if (DirectoryExist(szValue)) {
-            if (PathIsDirectoryEmpty(szValue)) {
-                MsiSetProperty(hInstall, TEXT("INSTALLDIR_VALID"), TEXT("1"));
-            } else {
-                MsiSetProperty(hInstall, TEXT("INSTALLDIR_VALID"), TEXT("0"));
-            }
-        } else {
-            MsiSetProperty(hInstall, TEXT("INSTALLDIR_VALID"), TEXT("1"));
-        }
-
-        delete [] szValue;
-
-        return ERROR_SUCCESS;
     }
 }
