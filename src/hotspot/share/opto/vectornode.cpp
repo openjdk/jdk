3419beec7fa646ab30f55ac27fdb47c4c1e1e764
wanghaomin <wanghaomin@loongson.cn>
2022-06-09 04:00:31 +0000

    8286847: Rotate vectors don't support byte or short
    Reviewed-by: eliu, xgong, jiefu, jbhateja

--- src/hotspot/share/opto/vectornode.cpp
+++ src/hotspot/share/opto/vectornode.cpp
@@ -385,7 +385,6 @@ bool VectorNode::is_vector_rotate_supported(int vopc, uint vlen, BasicType bt) {
              Matcher::match_rule_supported_vector(Op_LShiftVL,  vlen, bt) &&
              Matcher::match_rule_supported_vector(Op_URShiftVL, vlen, bt);
     default:
-      assert(false, "not supported: %s", type2name(bt));
       return false;
   }
 }
