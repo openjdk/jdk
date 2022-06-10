fa7b3b7b1b4fba507ae4e557ecbdca142ed726c5
MBaesken <matthias.baesken@sap.com>
2022-06-10 09:31:18 +0200

    dll_path on Linux - add NULL check, change special output to not available

--- src/hotspot/os/linux/os_linux.cpp
+++ src/hotspot/os/linux/os_linux.cpp
@@ -1788,10 +1788,12 @@ void * os::Linux::dll_load_in_vmthread(const char *filename, char *ebuf,
 
 const char* os::Linux::dll_path(void* lib) {
   struct link_map *lmap;
-  int res_dli = ::dlinfo(lib, RTLD_DI_LINKMAP, &lmap);
-  const char* l_path = "<none>";
-  if (res_dli == 0) {
-    l_path = lmap->l_name;
+  const char* l_path = "<not available>";
+  if (lib != NULL) {
+    int res_dli = ::dlinfo(lib, RTLD_DI_LINKMAP, &lmap);
+    if (res_dli == 0) {
+      l_path = lmap->l_name;
+    }
   }
   return l_path;
 }
