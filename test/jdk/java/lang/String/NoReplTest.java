e1afd207df14bc8075c99b2c57269e51ddd894e5
Naoto Sato <naoto.sato@oracle.com>
2022-06-17 17:24:01 -0700

    Added c2b test

--- test/jdk/java/lang/String/NoReplTest.java
+++ test/jdk/java/lang/String/NoReplTest.java
@@ -24,19 +24,32 @@
 /*
  * @test
  * @bug 8286287 8288589
- * @summary Verifies newStringNoRepl() throws a CharacterCodingException.
+ * @summary Tests for *NoRepl() shared secret methods.
+ * @run testng NoReplTest
+ * @modules jdk.charsets
  */
 
 import java.io.IOException;
 import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.HexFormat;
 import static java.nio.charset.StandardCharsets.UTF_16;
 
-public class NewStringNoRepl {
-    private final static byte[] MALFORMED_UTF16 = {(byte)0x00, (byte)0x20, (byte)0x00};
+import org.testng.annotations.Test;
 
-    public static void main(String... args) throws IOException {
+@Test
+public class NoReplTest {
+    private final static byte[] MALFORMED_UTF16 = {(byte)0x00, (byte)0x20, (byte)0x00};
+    private final static String MALFORMED_WINDOWS_1252 = "\u0080\u041e";
+    private final static Charset WINDOWS_1252 = Charset.forName("windows-1252");
+
+    @Test
+    /**
+     * Verifies newStringNoRepl() throws a CharacterCodingException.
+     * The method is invoked by `Files.readString()` method.
+     */
+    public void newStringNoReplTest() throws IOException {
         var f = Files.createTempFile(null, null);
         try (var fos = Files.newOutputStream(f)) {
             fos.write(MALFORMED_UTF16);
@@ -46,6 +59,23 @@ public class NewStringNoRepl {
                             .withPrefix("x")
                             .withUpperCase()
                             .formatHex(read.getBytes(UTF_16)));
+        } catch (CharacterCodingException cce) {
+            // success
+        } finally {
+            Files.delete(f);
+        }
+    }
+
+    @Test
+    /**
+     * Verifies getBytesNoRepl() throws a CharacterCodingException.
+     * The method is invoked by `Files.writeString()` method.
+     */
+    public void getBytesNoReplTest() throws IOException {
+        var f = Files.createTempFile(null, null);
+        try {
+            Files.writeString(f, MALFORMED_WINDOWS_1252, WINDOWS_1252);
+            throw new RuntimeException("Exception should be thrown");
         } catch (CharacterCodingException cce) {
             // success
         } finally {
