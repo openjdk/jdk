b7d808e96b68e1a648a74b5c1666304f58c347a0
Goetz Lindenmaier <goetz@openjdk.org>
2022-06-22 10:50:17 +0000

    8284367: JQuery UI upgrade from 1.12.1 to 1.13.1
    Reviewed-by: mdoerr
    Backport-of: ac41b78cfbb94f0b0e9a15cfcc50bdddc43811e9

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDoclet.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/formats/html/HtmlDoclet.java
@@ -312,19 +312,7 @@ public class HtmlDoclet extends AbstractDoclet {
         List<String> files = Arrays.asList(
                 "jquery-3.6.0.min.js",
                 "jquery-ui.min.js",
-                "jquery-ui.min.css",
-                "jquery-ui.structure.min.css",
-                "images/ui-bg_glass_65_dadada_1x400.png",
-                "images/ui-icons_454545_256x240.png",
-                "images/ui-bg_glass_95_fef1ec_1x400.png",
-                "images/ui-bg_glass_75_dadada_1x400.png",
-                "images/ui-bg_highlight-soft_75_cccccc_1x100.png",
-                "images/ui-icons_888888_256x240.png",
-                "images/ui-icons_2e83ff_256x240.png",
-                "images/ui-icons_cd0a0a_256x240.png",
-                "images/ui-bg_glass_55_fbf9ee_1x400.png",
-                "images/ui-icons_222222_256x240.png",
-                "images/ui-bg_glass_75_e6e6e6_1x400.png");
+                "jquery-ui.min.css");
         DocFile f;
         for (String file : files) {
             DocPath filePath = DocPaths.JQUERY_FILES.resolve(file);
