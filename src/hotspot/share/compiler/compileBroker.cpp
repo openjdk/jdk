3269cec1d49e858fd5bef7a5a7d6a41ccdb9a6bc
Goetz Lindenmaier <goetz@openjdk.org>
2022-06-24 08:03:51 +0000

    8286870: Memory leak with RepeatCompilation
    Backport-of: 69ff86a32088d9664e5e0dae12edddc0643e3fd3

--- src/hotspot/share/compiler/compileBroker.cpp
+++ src/hotspot/share/compiler/compileBroker.cpp
@@ -2319,8 +2319,9 @@ void CompileBroker::invoke_compiler_on_method(CompileTask* task) {
       /* Repeat compilation without installing code for profiling purposes */
       int repeat_compilation_count = directive->RepeatCompilationOption;
       while (repeat_compilation_count > 0) {
+        ResourceMark rm(thread);
         task->print_ul("NO CODE INSTALLED");
-        comp->compile_method(&ci_env, target, osr_bci, false , directive);
+        comp->compile_method(&ci_env, target, osr_bci, false, directive);
         repeat_compilation_count--;
       }
     }
