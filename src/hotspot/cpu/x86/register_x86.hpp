4878347d80b271ad838c20cdf044df3e9c30e875
Andrew Haley <aph-open@littlepinkcloud.com>
2022-06-24 08:57:37 +0100

    Update src/hotspot/cpu/x86/register_x86.hpp
    Co-authored-by: Aleksey Shipilëv <shade@redhat.com>

--- src/hotspot/cpu/x86/register_x86.hpp
+++ src/hotspot/cpu/x86/register_x86.hpp
@@ -67,7 +67,7 @@ public:
 
   // accessors
   int   raw_encoding() const                     { return this - first(); }
-  int   encoding() const                         { assert(is_valid(), "invalid register"); return (this - first()); }
+  int   encoding() const                         { assert(is_valid(), "invalid register"); return raw_encoding(); }
   bool  is_valid() const                         { return 0 <= raw_encoding() && raw_encoding() < number_of_registers; }
   bool  has_byte_register() const                { return 0 <= raw_encoding() && raw_encoding() < number_of_byte_registers; }
   const char *name() const;
