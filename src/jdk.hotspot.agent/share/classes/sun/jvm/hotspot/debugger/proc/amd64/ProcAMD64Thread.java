7905788e969727c81eea4397f0d9b918cdb5286a
Andrey Turbanov <aturbanov@openjdk.org>
2022-06-27 07:30:19 +0000

    8289126: Cleanup unnecessary null comparison before instanceof check in jdk.hotspot.agent
    Reviewed-by: ayang, cjplummer

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/proc/amd64/ProcAMD64Thread.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/proc/amd64/ProcAMD64Thread.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -73,11 +73,11 @@ public class ProcAMD64Thread implements ThreadProxy {
     }
 
     public boolean equals(Object obj) {
-        if ((obj == null) || !(obj instanceof ProcAMD64Thread)) {
+        if (!(obj instanceof ProcAMD64Thread other)) {
             return false;
         }
 
-        return (((ProcAMD64Thread) obj).id == id);
+        return (other.id == id);
     }
 
     public int hashCode() {
