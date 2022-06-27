1a2afd8c51fa120c2dcfbf46e9e1ce4ce81b27cf
Thejasvi Voniadka <thejasvi.v.voniadka@oracle.com>
2022-06-27 03:51:52 -0700

    8282036: [Test]: Update java/util/zip/ZipFile/DeleteTempJar.java to stop HttpServer cleanly in case of exceptions

--- test/jdk/java/util/zip/ZipFile/DeleteTempJar.java
+++ test/jdk/java/util/zip/ZipFile/DeleteTempJar.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -73,8 +73,8 @@ public class DeleteTempJar
                 }
             });
 
+        server.start();
         try {
-            server.start();
             URL url = new URL("jar:http://localhost:"
                           + new Integer(server.getAddress().getPort()).toString()
                           + "/deletetemp.jar!/");
