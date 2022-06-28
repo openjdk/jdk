f299e7c9638723fbbf2272547520f8eed468676d
TheShermanTanker <tanksherman27@gmail.com>
2022-06-28 14:51:30 +0800

    Remove __declspec(dllimport) from pointers

--- src/jdk.crypto.cryptoki/windows/native/libj2pkcs11/p11_md.h
+++ src/jdk.crypto.cryptoki/windows/native/libj2pkcs11/p11_md.h
@@ -69,7 +69,7 @@
 #define CK_PTR *
 #define CK_DEFINE_FUNCTION(returnType, name) returnType __declspec(dllexport) name
 #define CK_DECLARE_FUNCTION(returnType, name) returnType __declspec(dllimport) name
-#define CK_DECLARE_FUNCTION_POINTER(returnType, name) returnType __declspec(dllimport) (* name)
+#define CK_DECLARE_FUNCTION_POINTER(returnType, name) returnType (* name)
 #define CK_CALLBACK_FUNCTION(returnType, name) returnType (* name)
 #ifndef NULL_PTR
 #define NULL_PTR 0
