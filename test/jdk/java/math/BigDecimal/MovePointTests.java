9755bb120158f26f208f8ec08617fa9cfad2c605
Raffaello Giulietti <rgiulietti@openjdk.org>
2022-06-28 12:27:54 +0200

    8289260: BigDecimal movePointLeft() and movePointRight() do not follow their API spec

--- test/jdk/java/math/BigDecimal/MovePointTests.java
+++ test/jdk/java/math/BigDecimal/MovePointTests.java
@@ -37,17 +37,49 @@ public class MovePointTests {
         checkNotIdentical(bd, bd.movePointLeft(0));
         checkNotIdentical(bd, bd.movePointRight(0));
 
+        bd = BigDecimal.valueOf(1, 0);
+        checkIdentical(bd, bd.movePointLeft(0));
+        checkIdentical(bd, bd.movePointRight(0));
+
         bd = BigDecimal.valueOf(1, 3);
         checkIdentical(bd, bd.movePointLeft(0));
         checkIdentical(bd, bd.movePointRight(0));
 
         bd = BigDecimal.valueOf(1, -3);
         checkNotEquals(bd, bd.movePointLeft(1));
+        checkNotEquals(bd, bd.movePointLeft(-1));
         checkNotEquals(bd, bd.movePointRight(1));
+        checkNotEquals(bd, bd.movePointRight(-1));
+
+        bd = BigDecimal.valueOf(1, 0);
+        checkNotEquals(bd, bd.movePointLeft(1));
+        checkNotEquals(bd, bd.movePointLeft(-1));
+        checkNotEquals(bd, bd.movePointRight(1));
+        checkNotEquals(bd, bd.movePointRight(-1));
+
+        bd = BigDecimal.valueOf(1, 3);
+        checkNotEquals(bd, bd.movePointLeft(1));
+        checkNotEquals(bd, bd.movePointLeft(-1));
+        checkNotEquals(bd, bd.movePointRight(1));
+        checkNotEquals(bd, bd.movePointRight(-1));
 
         bd = BigDecimal.valueOf(1, -3);
-        checkNotEquals(bd, bd.movePointLeft(-1));
-        checkNotEquals(bd, bd.movePointRight(-1));
+        checkNotEquals(bd, bd.movePointLeft(10));
+        checkNotEquals(bd, bd.movePointLeft(-10));
+        checkNotEquals(bd, bd.movePointRight(10));
+        checkNotEquals(bd, bd.movePointRight(-10));
+
+        bd = BigDecimal.valueOf(1, 0);
+        checkNotEquals(bd, bd.movePointLeft(10));
+        checkNotEquals(bd, bd.movePointLeft(-10));
+        checkNotEquals(bd, bd.movePointRight(10));
+        checkNotEquals(bd, bd.movePointRight(-10));
+
+        bd = BigDecimal.valueOf(1, 3);
+        checkNotEquals(bd, bd.movePointLeft(10));
+        checkNotEquals(bd, bd.movePointLeft(-10));
+        checkNotEquals(bd, bd.movePointRight(10));
+        checkNotEquals(bd, bd.movePointRight(-10));
     }
 
     private static void checkIdentical(BigDecimal bd, BigDecimal res) {
