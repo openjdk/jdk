779b4e1d1959bc15a27492b7e2b951678e39cca8
Yuta Sato <satouyuzzb@oss.nttdata.com>
2022-06-29 01:17:28 +0000

    8287001: Add warning message when fail to load hsdis libraries
    Reviewed-by: kvn, ysuenaga

--- src/hotspot/share/compiler/disassembler.cpp
+++ src/hotspot/share/compiler/disassembler.cpp
@@ -837,6 +837,8 @@ bool Disassembler::load_library(outputStream* st) {
   if (_library != NULL) {
     _decode_instructions_virtual = CAST_TO_FN_PTR(Disassembler::decode_func_virtual,
                                           os::dll_lookup(_library, decode_instructions_virtual_name));
+  } else {
+    log_warning(os)("Loading hsdis library failed");
   }
   _tried_to_load_library = true;
   _library_usable        = _decode_instructions_virtual != NULL;
