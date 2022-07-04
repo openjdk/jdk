d28656f95d6044c61e41316733788615a4d21d56
Yasumasa Suenaga <suenaga@oss.nttdata.com>
2022-07-04 12:54:36 +0900

    8289646: configure script failed on WSL

--- make/scripts/fixpath.sh
+++ make/scripts/fixpath.sh
@@ -146,15 +146,11 @@ function import_path() {
   fi
 
   if [[ "$path" != "" ]]; then
-    # Store current unix path
-    unixpath="$path"
+    if [[ ! -e "$path" && -e "$path.exe" ]]; then
+      path="$path.exe"
+    fi
     # Now turn it into a windows path
     winpath="$($PATHTOOL -w "$path" 2>/dev/null)"
-    # If it fails, try again with an added .exe (needed on WSL)
-    if [[ $? -ne 0 ]]; then
-      unixpath="$unixpath.exe"
-      winpath="$($PATHTOOL -w "$unixpath" 2>/dev/null)"
-    fi
     if [[ $? -eq 0 ]]; then
       if [[ ! "$winpath" =~ ^"$ENVROOT"\\.*$ ]] ; then
         # If it is not in envroot, it's a generic windows path
@@ -163,25 +159,21 @@ function import_path() {
           # This monster of a command uses the %~s support from cmd.exe to
           # reliably convert to short paths on all winenvs.
           shortpath="$($CMD /q /c for %I in \( "$winpath" \) do echo %~sI 2>/dev/null | tr -d \\n\\r)"
-          unixpath="$($PATHTOOL -u "$shortpath")"
-          # unixpath is based on short name
+          path="$($PATHTOOL -u "$shortpath")"
+          # path is based on short name
         fi
         # Make it lower case
-        path="$(echo "$unixpath" | tr '[:upper:]' '[:lower:]')"
+        path="$(echo "$path" | tr '[:upper:]' '[:lower:]')"
       fi
     else
       # On WSL1, PATHTOOL will fail for files in envroot. If the unix path
       # exists, we assume that $path is a valid unix path.
 
       if [[ ! -e $path ]]; then
-        if [[ -e $path.exe ]]; then
-          path="$path.exe"
-        else
-          if [[ $QUIET != true ]]; then
-            echo fixpath: warning: Path "'"$path"'" does not exist >&2
-          fi
-          # This is not a fatal error, maybe the path will be created later on
+        if [[ $QUIET != true ]]; then
+          echo fixpath: warning: Path "'"$path"'" does not exist >&2
         fi
+        # This is not a fatal error, maybe the path will be created later on
       fi
     fi
   fi
