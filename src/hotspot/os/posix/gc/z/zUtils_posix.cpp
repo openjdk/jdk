688712f75cd54caa264494adbe4dfeefc079e1dd
Thomas Stuefe <stuefe@openjdk.org>
2022-07-05 04:26:45 +0000

    8289633: Forbid raw C-heap allocation functions in hotspot and fix findings
    Reviewed-by: kbarrett, dholmes

--- src/hotspot/os/posix/gc/z/zUtils_posix.cpp
+++ src/hotspot/os/posix/gc/z/zUtils_posix.cpp
@@ -24,13 +24,16 @@
 #include "precompiled.hpp"
 #include "gc/z/zUtils.hpp"
 #include "utilities/debug.hpp"
+#include "utilities/globalDefinitions.hpp"
 
 #include <stdlib.h>
 
 uintptr_t ZUtils::alloc_aligned(size_t alignment, size_t size) {
   void* res = NULL;
 
-  if (posix_memalign(&res, alignment, size) != 0) {
+  // Use raw posix_memalign as long as we have no wrapper for it
+  ALLOW_C_FUNCTION(::posix_memalign, int rc = posix_memalign(&res, alignment, size);)
+  if (rc != 0) {
     fatal("posix_memalign() failed");
   }
 
