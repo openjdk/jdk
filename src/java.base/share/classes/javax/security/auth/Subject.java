048f86d62b411e6c0b47b1a6330f5edb932460b2
RivenSun2 <riven.sun@zoom.us>
2022-07-06 20:22:07 +0800

    fix issue that Wrong order problem with Set returned by Subject.getXXXCredentials(Class)

--- src/java.base/share/classes/javax/security/auth/Subject.java
+++ src/java.base/share/classes/javax/security/auth/Subject.java
@@ -963,7 +963,7 @@ public final class Subject implements java.io.Serializable {
             Set<Principal> thatPrincipals;
             synchronized(that.principals) {
                 // avoid deadlock from dual locks
-                thatPrincipals = new HashSet<>(that.principals);
+                thatPrincipals = new LinkedHashSet<>(that.principals);
             }
             if (!principals.equals(thatPrincipals)) {
                 return false;
@@ -972,7 +972,7 @@ public final class Subject implements java.io.Serializable {
             Set<Object> thatPubCredentials;
             synchronized(that.pubCredentials) {
                 // avoid deadlock from dual locks
-                thatPubCredentials = new HashSet<>(that.pubCredentials);
+                thatPubCredentials = new LinkedHashSet<>(that.pubCredentials);
             }
             if (!pubCredentials.equals(thatPubCredentials)) {
                 return false;
@@ -981,7 +981,7 @@ public final class Subject implements java.io.Serializable {
             Set<Object> thatPrivCredentials;
             synchronized(that.privCredentials) {
                 // avoid deadlock from dual locks
-                thatPrivCredentials = new HashSet<>(that.privCredentials);
+                thatPrivCredentials = new LinkedHashSet<>(that.privCredentials);
             }
             return privCredentials.equals(thatPrivCredentials);
         }
@@ -1625,7 +1625,7 @@ public final class Subject implements java.io.Serializable {
         ClassSet(int which, Class<T> c) {
             this.which = which;
             this.c = c;
-            set = new HashSet<T>();
+            set = new LinkedHashSet<T>();
 
             switch (which) {
             case Subject.PRINCIPAL_SET:
