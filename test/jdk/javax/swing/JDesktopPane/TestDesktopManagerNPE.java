d4d7b1416d71dac5c3d2ec9e320b83652eafb2ff
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-07-07 17:36:36 +0530

    Fix

--- test/jdk/javax/swing/JDesktopPane/TestDesktopManagerNPE.java
+++ test/jdk/javax/swing/JDesktopPane/TestDesktopManagerNPE.java
@@ -22,49 +22,50 @@
  */
 
 /* @test
-   @bug 8170794
-   @summary Verifies iconifying internalframe after setting DesktopManager 
-            does not return NPE
-   @run main TestDesktopManagerNPE
+ * @bug 8170794
+ * @key headful
+ * @summary Verifies iconifying internalframe after setting DesktopManager
+ *          does not return NPE
+ *  @run main TestDesktopManagerNPE
  */
 
 import java.awt.Dimension;
 import java.awt.Robot;
 import java.awt.Toolkit;
-import javax.swing.DefaultDesktopManager; 
-import javax.swing.JInternalFrame; 
-import javax.swing.JDesktopPane; 
-import javax.swing.JFrame; 
-import javax.swing.JMenu; 
-import javax.swing.JMenuItem; 
-import javax.swing.JMenuBar; 
-import javax.swing.KeyStroke; 
+import javax.swing.DefaultDesktopManager;
+import javax.swing.JInternalFrame;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public class TestDesktopManagerNPE {
 
-    static JDesktopPane desktop; 
+    static JDesktopPane desktop;
     static JInternalFrame internalFrame;
     static JFrame frame;
 
-    //Create a new internal frame. 
-    protected static void createFrame() { 
-        internalFrame = new JInternalFrame(); 
+    //Create a new internal frame.
+    protected static void createFrame() {
+        internalFrame = new JInternalFrame();
         internalFrame.setSize(300, 300);
         internalFrame.setLocation(30, 30);
-        internalFrame.setVisible(true); //necessary as of 1.3 
-        desktop.add(internalFrame); 
-        try { 
-            internalFrame.setSelected(true); 
-        } catch (java.beans.PropertyVetoException e) {} 
-    } 
+        internalFrame.setVisible(true); //necessary as of 1.3
+        desktop.add(internalFrame);
+        try {
+            internalFrame.setSelected(true);
+        } catch (java.beans.PropertyVetoException e) {}
+    }
 
-    /** 
-     * Create the GUI and show it. For thread safety, 
-     * this method should be invoked from the 
-     * event-dispatching thread. 
-     */ 
-    private static void createAndShowGUI() { 
+    /**
+     * Create the GUI and show it. For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
         JFrame.setDefaultLookAndFeelDecorated(true);
 
         int inset = 50;
@@ -82,12 +83,11 @@ public class TestDesktopManagerNPE {
 
         createFrame(); //create first "window"
         frame.setContentPane(desktop);
-        //Display the window. 
-        frame.setVisible(true); 
+        //Display the window.
+        frame.setVisible(true);
+    }
 
-    } 
-
-    public static void main(String[] args) throws Exception { 
+    public static void main(String[] args) throws Exception {
         try {
             SwingUtilities.invokeAndWait(() -> createAndShowGUI());
             Robot robot = new Robot();
@@ -101,6 +101,5 @@ public class TestDesktopManagerNPE {
                 }
             });
         }
-    } 
- 
-} 
+    }
+}
