b2d2cf75c9d5d9cfc51ee7d44f7ea08d8734bd63
Lance Andersen <lance.andersen@oracle.com>
2022-07-08 14:08:00 -0400

    Fix for JDK-8289984

--- test/jdk/java/nio/file/Files/CheckPermissions.java
+++ test/jdk/java/nio/file/Files/CheckPermissions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -217,6 +217,14 @@ public class CheckPermissions {
 
             prepare();
             exists(file);
+            assertCheckRead(file);
+
+            prepare();
+            isDirectory(file);
+            assertCheckRead(file);
+
+            prepare();
+            isRegularFile(file);
             assertCheckRead(file);
 
             prepare();
