2496d081337168f1dc59953c1b4b0377319f2216
Albert Yang <albert.m.yang@oracle.com>
2022-07-13 09:55:18 +0200

    g1-inline-forward-block

--- src/hotspot/share/gc/g1/heapRegion.hpp
+++ src/hotspot/share/gc/g1/heapRegion.hpp
@@ -144,13 +144,6 @@ private:
   // This version synchronizes with other calls to par_allocate_impl().
   inline HeapWord* par_allocate_impl(size_t min_word_size, size_t desired_word_size, size_t* actual_word_size);
 
-  // Return the address of the beginning of the block that contains "addr".
-  // "q" is a block boundary that is <= "addr"; "n" is the address of the
-  // next block (or the end of the HeapRegion.)
-  inline HeapWord* forward_to_block_containing_addr(HeapWord* q, HeapWord* n,
-                                                    const void* addr,
-                                                    HeapWord* pb) const;
-
   static bool obj_is_filler(oop obj);
 
 public:
