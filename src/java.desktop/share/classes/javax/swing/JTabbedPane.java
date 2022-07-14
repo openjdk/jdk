fe9a41b8b560b65348550c74e6fd2a6ffcd7f53e
Tejesh R <tejesh.r@oracle.com>
2022-07-14 20:58:47 +0530

    Updated java document

--- src/java.desktop/share/classes/javax/swing/JTabbedPane.java
+++ src/java.desktop/share/classes/javax/swing/JTabbedPane.java
@@ -722,7 +722,7 @@ public class JTabbedPane extends JComponent
      * @param component the component to be displayed when this tab is clicked.
      * @param tip the tooltip to be displayed for this tab
      * @param index the position to insert this new tab
-     *       ({@code > 0 and <= getTabCount()})
+     *       ({@code >= 0 and <= getTabCount()})
      *
      * @throws IndexOutOfBoundsException if the index is out of range
      *         ({@code < 0 or > getTabCount()})
