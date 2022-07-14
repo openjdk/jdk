ccc4732eeb706980d2f7f92aec3835f60d146126
Martin Doerr <mdoerr@openjdk.org>
2022-07-14 09:58:36 +0000

    8290004: [PPC64] JfrGetCallTrace: assert(_pc != nullptr) failed: must have PC
    Reviewed-by: lucy, clanger
    Backport-of: cb6e9cb7286f609dec1fe1157bf95afc503870a9

--- src/hotspot/os_cpu/aix_ppc/thread_aix_ppc.cpp
+++ src/hotspot/os_cpu/aix_ppc/thread_aix_ppc.cpp
@@ -30,7 +30,7 @@
 frame JavaThread::pd_last_frame() {
   assert(has_last_Java_frame(), "must have last_Java_sp() when suspended");
 
-  intptr_t* sp = last_Java_sp();
+  intptr_t* sp = Atomic::load_acquire(&_anchor._last_Java_sp);
   address pc = _anchor.last_Java_pc();
 
   // Last_Java_pc ist not set, if we come here from compiled code.
