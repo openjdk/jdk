5b18c39c53d94d6d622da98198adc3c25a6fb41f
Sergey Tsypanov <sergei.tsypanov@yandex.ru>
2022-07-18 14:28:48 +0300

    8290300: Remove unused import

--- src/java.base/share/classes/java/lang/ProcessBuilder.java
+++ src/java.base/share/classes/java/lang/ProcessBuilder.java
@@ -34,7 +34,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 import jdk.internal.event.ProcessStartEvent;
 import sun.security.action.GetPropertyAction;
 
