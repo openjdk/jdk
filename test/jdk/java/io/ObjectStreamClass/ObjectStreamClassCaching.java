28426c23bbfeae8e25b9aca0b78c02402f2d0728
Aleksey Shipilev <shade@redhat.com>
2022-07-18 09:40:00 +0200

    Fix

--- test/jdk/java/io/ObjectStreamClass/ObjectStreamClassCaching.java
+++ test/jdk/java/io/ObjectStreamClass/ObjectStreamClassCaching.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,7 +41,6 @@ public class ObjectStreamClassCaching {
     @Test
     public void testCachingEffectiveness() throws Exception {
         var ref = lookupObjectStreamClass(TestClass.class);
-        System.gc();
         Thread.sleep(100L);
         // to trigger any ReferenceQueue processing...
         lookupObjectStreamClass(AnotherTestClass.class);
