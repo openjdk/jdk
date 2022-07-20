c98b075378a205ee9570934445921b27a318d54d
Erik Österlund <eosterlund@openjdk.org>
2022-07-20 16:30:17 +0000

    fixing 32 bit build again

--- src/hotspot/cpu/x86/sharedRuntime_x86_32.cpp
+++ src/hotspot/cpu/x86/sharedRuntime_x86_32.cpp
@@ -1518,7 +1518,7 @@ nmethod* SharedRuntime::generate_native_wrapper(MacroAssembler* masm,
 
 
   BarrierSetAssembler* bs = BarrierSet::barrier_set()->barrier_set_assembler();
-  bs->nmethod_entry_barrier(masm);
+  bs->nmethod_entry_barrier(masm, NULL /* slow_path */, NULL /* continuation */);
 
   // Frame is now completed as far as size and linkage.
   int frame_complete = ((intptr_t)__ pc()) - start;
