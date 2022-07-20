eeb5f82d2d9710ad2aaae93e3da9530fabecb6ff
Erik Österlund <eosterlund@openjdk.org>
2022-07-20 15:49:51 +0000

    fix 32 bit build again

--- src/hotspot/cpu/x86/gc/shared/barrierSetAssembler_x86.cpp
+++ src/hotspot/cpu/x86/gc/shared/barrierSetAssembler_x86.cpp
@@ -336,7 +336,7 @@ void BarrierSetAssembler::nmethod_entry_barrier(MacroAssembler* masm, Label* slo
   }
 }
 #else
-void BarrierSetAssembler::nmethod_entry_barrier(MacroAssembler* masm, Label* slow_path, Label* continuation) {
+void BarrierSetAssembler::nmethod_entry_barrier(MacroAssembler* masm, Label*, Label*) {
   BarrierSetNMethod* bs_nm = BarrierSet::barrier_set()->barrier_set_nmethod();
   if (bs_nm == NULL) {
     return;
