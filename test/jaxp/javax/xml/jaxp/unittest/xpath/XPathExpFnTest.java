0c9c43e7404edac05a2bf12342ab85e2d43dfa1e
Bill Huang <bill.w.huang@oracle.com>
2022-07-25 14:35:30 -0700

    Created XPathExpFnTest.java for XPath functions and implemented test cases for node set functions.

---
+++ test/jaxp/javax/xml/jaxp/unittest/xpath/XPathExpFnTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package xpath;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import javax.xml.xpath.*;
+
+/*
+ * @test
+ * @bug 8289948
+ * @library /javax/xml/jaxp/unittest
+ * @run testng xpath.XPathExpFnTest
+ * @summary Test for XPath functions
+ */
+public class XPathExpFnTest extends XPathTestBase {
+
+    private static final Document doc = getDtdDocument();
+
+    @DataProvider(name = "idExpTestCases")
+    public Object[][] getIdExp() {
+        return new Object[][]{
+                {"id('3')", "Customer_3"},
+                {"id('1 2 3')[3]", "Customer_3"},
+                {"id('1 | 2 | 3')[3]", "Customer_3"},
+                {"id('x')", "Email_x"},
+                {"id(//Customer[3]/@id)", "Customer_3"},
+                {"id(//*[.='123@xyz.com']/@id)", "Email_x"},
+        };
+    }
+
+    @DataProvider(name = "countExpTestCases")
+    public Object[][] getCountExp() {
+        return new Object[][]{
+                {"count(//Customer)", 3},
+                {"count(//@id)", 6},
+                {"count(//Customer/@id)", 3},
+                {"count(//@*)", 6},
+                {"count(//*)", 25},
+                {"count(//*[@id])", 6},
+                {"count(./*)", 1},
+                {"count(//Customer[1]/following::*)", 16},
+                {"count(//Customer[1]/following-sibling::*)", 2},
+                {"count(//Customer[3]/preceding::*)", 16},
+                {"count(//Customer[3]/preceding-sibling::*)", 2},
+                {"count(//Customer[1]/ancestor::*)", 1},
+                {"count(//Customer[1]/ancestor-or-self::*)", 2},
+                {"count(//Customer[1]/descendant::*)", 7},
+                {"count(//Customer[1]/descendant-or-self::*)", 8},
+                {"count(//Customer/node())", 27},
+        };
+    }
+
+    @DataProvider(name = "positionExpTestCases")
+    public Object[][] getPositionExp() {
+        return new Object[][]{
+                {"//Customer[position()=1]", "Customer_1"},
+                {"//Customer[position()=last()]", "Customer_3"},
+                {"//Customer[position()>1 and position()<last()]",
+                        "Customer_2"},
+                {"//Customer[position() mod 2 =0]", "Customer_2"},
+                {"//Customer[last()]", "Customer_3"},
+        };
+    }
+
+    @DataProvider(name = "nameExpTestCases")
+    public Object[][] getNameExp() {
+        return new Object[][]{
+                {"local-name(//Customer)", "Customer"},
+                {"local-name(//Customer/@id)", "id"},
+                {"local-name(//*[local-name()='Customer'])", "Customer"},
+                {"namespace-uri(//Customer)", ""},
+                {"name(//Customer)", "Customer"},
+                {"name(//Customer/@id)", "id"},
+                {"name(//*[name()='Customer'])", "Customer"},
+        };
+    }
+
+    @Test(dataProvider = "idExpTestCases")
+    void testIdFn(String exp, String expected) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        Node node = xPath.evaluateExpression(exp, doc, Node.class);
+        Node node2 = (Node) xPath.evaluate(exp, doc, XPathConstants.NODE);
+
+        Assert.assertEquals(node.getNodeName() + "_" +
+                        node.getAttributes().item(0).getNodeValue()
+                , expected);
+        Assert.assertEquals(node2, node);
+    }
+
+    @Test(dataProvider = "countExpTestCases")
+    void testCountFn(String exp, int expected) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        double num = xPath.evaluateExpression(exp, doc, Double.class);
+        double num2 = (double) xPath.evaluate(exp, doc, XPathConstants.NUMBER);
+
+        Assert.assertEquals(num, expected);
+        Assert.assertEquals(num2, num);
+    }
+
+    @Test(dataProvider = "positionExpTestCases")
+    void testPositionFn(String exp, String expected) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        Node node = xPath.evaluateExpression(exp, doc, Node.class);
+        Node node2 = (Node) xPath.evaluate(exp, doc, XPathConstants.NODE);
+
+        Assert.assertEquals(node.getNodeName() + "_" +
+                        node.getAttributes().item(0).getNodeValue()
+                , expected);
+        Assert.assertEquals(node2, node);
+    }
+
+    @Test(dataProvider = "nameExpTestCases")
+    void testNameFn(String exp, String expected) throws Exception {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+
+        String s = xPath.evaluateExpression(exp, doc, String.class);
+        String s2 = (String) xPath.evaluate(exp, doc, XPathConstants.STRING);
+
+        Assert.assertEquals(s, expected);
+        Assert.assertEquals(s2, s);
+    }
+}
--- test/jaxp/javax/xml/jaxp/unittest/xpath/XPathTestBase.java
+++ test/jaxp/javax/xml/jaxp/unittest/xpath/XPathTestBase.java
@@ -31,11 +31,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathFactory;
-import static javax.xml.xpath.XPathConstants.BOOLEAN;
-import static javax.xml.xpath.XPathConstants.NUMBER;
-import static javax.xml.xpath.XPathConstants.STRING;
-import static javax.xml.xpath.XPathConstants.NODE;
-import static javax.xml.xpath.XPathConstants.NODESET;
 import javax.xml.xpath.XPathNodes;
 import javax.xml.xpath.XPathEvaluationResult;
 
@@ -51,14 +46,32 @@ import org.w3c.dom.Node;
  * Base class for XPath test
  */
 class XPathTestBase {
+    static final String DECLARATION = "<?xml version=\"1.0\" " +
+            "encoding=\"UTF-8\" standalone=\"yes\"?>";
 
-    static final String rawXML
-            = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"
-            + "<Customers>"
+    static final String DTD = """
+            <!DOCTYPE Customers [
+               <!ELEMENT Customers (Customer*)>
+               <!ELEMENT Customer (Name, Phone, Email, Address)>
+               <!ELEMENT Name (#PCDATA)>
+               <!ELEMENT Phone (#PCDATA)>
+               <!ELEMENT Email (#PCDATA)>
+               <!ELEMENT Address (Street, City, State)>
+               <!ELEMENT Street (#PCDATA)>
+               <!ELEMENT City (#PCDATA)>
+               <!ELEMENT State (#PCDATA)>
+               <!ATTLIST Customer id ID #REQUIRED>
+               <!ATTLIST Email id ID #REQUIRED>
+            ]>
+                        
+            """;
+
+    static final String RAW_XML
+            = "<Customers>"
             + "    <Customer id=\"1\">"
             + "        <Name>name1</Name>"
             + "        <Phone>1111111111</Phone>"
-            + "        <Email>123@xyz.com</Email>"
+            + "        <Email id=\"x\">123@xyz.com</Email>"
             + "        <Address>"
             + "            <Street>1111 111st ave</Street>"
             + "            <City>The City</City>"
@@ -68,7 +81,7 @@ class XPathTestBase {
             + "    <Customer id=\"2\">"
             + "        <Name>name1</Name>"
             + "        <Phone>2222222222</Phone>"
-            + "        <Email>123@xyz.com</Email>"
+            + "        <Email id=\"y\">123@xyz.com</Email>"
             + "        <Address>"
             + "            <Street>2222 222nd ave</Street>"
             + "            <City>The City</City>"
@@ -78,7 +91,7 @@ class XPathTestBase {
             + "    <Customer id=\"3\">"
             + "        <Name>name1</Name>"
             + "        <Phone>3333333333</Phone>"
-            + "        <Email>123@xyz.com</Email>"
+            + "        <Email id=\"z\">123@xyz.com</Email>"
             + "        <Address>"
             + "            <Street>3333 333rd ave</Street>"
             + "            <City>The City</City>"
@@ -86,6 +99,27 @@ class XPathTestBase {
             + "        </Address>"
             + "    </Customer>"
             + "</Customers>";
+
+    public static Document getDtdDocument() throws RuntimeException {
+        return documentOf(DECLARATION + DTD + RAW_XML);
+    }
+
+    public static Document getDocument() throws RuntimeException {
+        return documentOf(DECLARATION + RAW_XML);
+    }
+
+    public static Document documentOf(String xml) throws RuntimeException {
+        try {
+            var dBF = DocumentBuilderFactory.newInstance();
+            dBF.setValidating(true);
+            dBF.setNamespaceAware(true);
+            return dBF.newDocumentBuilder().parse(
+                    new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        } catch (Exception e) {
+            System.out.println("Exception while initializing XML document");
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
     void verifyResult(XPathEvaluationResult<?> result, Object expected) {
         switch (result.type()) {
@@ -126,13 +160,13 @@ class XPathTestBase {
     public Object[][] getInvalidNumericTypes() {
         XPath xpath = XPathFactory.newInstance().newXPath();
         return new Object[][]{{xpath, AtomicInteger.class},
-            {xpath, AtomicInteger.class},
-            {xpath, AtomicLong.class},
-            {xpath, BigDecimal.class},
-            {xpath, BigInteger.class},
-            {xpath, Byte.class},
-            {xpath, Float.class},
-            {xpath, Short.class}
+                {xpath, AtomicInteger.class},
+                {xpath, AtomicLong.class},
+                {xpath, BigDecimal.class},
+                {xpath, BigInteger.class},
+                {xpath, Byte.class},
+                {xpath, Float.class},
+                {xpath, Short.class}
         };
     }
 
@@ -140,13 +174,8 @@ class XPathTestBase {
      * DataProvider: XPath and Document objects
      */
     @DataProvider(name = "document")
-    public Object[][] getDocument() throws Exception {
-        DocumentBuilderFactory dBF = DocumentBuilderFactory.newInstance();
-        dBF.setValidating(false);
-        dBF.setNamespaceAware(true);
-        Document doc = dBF.newDocumentBuilder().parse(
-                new ByteArrayInputStream(rawXML.getBytes("UTF-8")));
-
+    public Object[][] getDocuments() throws RuntimeException {
+        Document doc = getDocument();
         return new Object[][]{{XPathFactory.newInstance().newXPath(), doc}};
     }
 }
