7f45d2878175fdf4bf62a9de6bea3fff9fd2c5ae
Ramkumar Sunderbabu <ramkumar.sunderbabu@oracle.com>
2022-07-26 21:19:33 +0530

    8282651: ZGC: vmTestbase/gc/ArrayJuggle/ tests fails intermittently with exit code 97

--- test/hotspot/jtreg/vmTestbase/gc/ArrayJuggle/Juggle20/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/gc/ArrayJuggle/Juggle20/TestDescription.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,6 +31,8 @@
  *
  * @library /vmTestbase
  *          /test/lib
- * @run main/othervm -Xlog:gc=debug:gc.log gc.ArrayJuggle.Juggle01.Juggle01 -gp floatArr -ms medium
+ * @build jdk.test.whitebox.WhiteBox
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
+ * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xlog:gc=debug:gc.log gc.ArrayJuggle.Juggle01.Juggle01 -gp floatArr -ms medium
  */
 
