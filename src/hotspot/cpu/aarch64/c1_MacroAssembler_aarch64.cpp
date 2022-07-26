8159a1ab708d7571814bbb1a60893b4a7379a082
Aleksey Shipilev <shade@openjdk.org>
2022-07-26 17:19:10 +0000

    8290706: Remove the support for inline contiguous allocations
    Reviewed-by: eosterlund, aph, rrich, fyang, thartmann

--- src/hotspot/cpu/aarch64/c1_MacroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/c1_MacroAssembler_aarch64.cpp
@@ -157,7 +157,7 @@ void C1_MacroAssembler::try_allocate(Register obj, Register var_size_in_bytes, i
   if (UseTLAB) {
     tlab_allocate(obj, var_size_in_bytes, con_size_in_bytes, t1, t2, slow_case);
   } else {
-    eden_allocate(obj, var_size_in_bytes, con_size_in_bytes, t1, slow_case);
+    b(slow_case);
   }
 }
 
