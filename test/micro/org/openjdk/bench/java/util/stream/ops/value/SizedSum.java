b5ee06700b36d0d737bcd653f51bc61fb148b4e3
Eric Caspole <eric.caspole@oracle.com>
2022-08-03 12:23:51 -0400

    8291828: Reduce runtime of java.util.stream microbenchmarks

--- test/micro/org/openjdk/bench/java/util/stream/ops/value/SizedSum.java
+++ test/micro/org/openjdk/bench/java/util/stream/ops/value/SizedSum.java
@@ -26,6 +26,8 @@ import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Param;
