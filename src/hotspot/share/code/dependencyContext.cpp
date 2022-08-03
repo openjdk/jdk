0141b28d3b30745da31e12a4489ac71e14bd18e2
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2022-08-03 14:27:03 +0200

    Simplify and assert in remove_all_dependents

--- src/hotspot/share/code/dependencyContext.cpp
+++ src/hotspot/share/code/dependencyContext.cpp
@@ -215,15 +215,8 @@ nmethodBucket* DependencyContext::release_and_get_next_not_unloading(nmethodBuck
 // Invalidate all dependencies in the context
 void DependencyContext::remove_all_dependents() {
   nmethodBucket* b = dependencies_not_unloading();
-   set_dependencies(NULL);
-   int removed = 0;
-   while (b != NULL) {
-     b = release_and_get_next_not_unloading(b);
-     ++removed;
-   }
-   if (UsePerfData && removed > 0) {
-     _perf_total_buckets_deallocated_count->inc(removed);
-   }
+  set_dependencies(NULL);
+  assert(b == nullptr, "All dependents should be unloading");
 }
 
 int DependencyContext::remove_and_mark_for_deoptimization_all_dependents() {
