70606fd9da13b90c3fb54f682c4f6f944c31f480
Mark Powers <mark.powers@oracle.com>
2022-08-04 19:11:42 -0700

    just one more thing

--- src/java.base/share/classes/javax/security/auth/spi/LoginModule.java
+++ src/java.base/share/classes/javax/security/auth/spi/LoginModule.java
@@ -25,10 +25,10 @@
 
 package javax.security.auth.spi;
 
+import java.util.Map;
 import javax.security.auth.Subject;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.LoginException;
-import java.util.Map;
 
 /**
  * <p> Service-provider interface for authentication technology providers.
