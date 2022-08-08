7505e3f0acfd232de7f097d7a3f963f10ae0ce5a
Tejesh R <tejesh.r@oracle.com>
2022-08-08 09:35:21 +0530

    Updated based on review comments

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicFileChooserUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicFileChooserUI.java
@@ -38,8 +38,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
@@ -719,10 +717,9 @@ public class BasicFileChooserUI extends FileChooserUI {
                             for (Object object : objects) {
                                 File f = (File)object;
                                 boolean isDir = f.isDirectory();
-                                Path path = f.toPath();
                                 if ((chooser.isFileSelectionEnabled() && !isDir)
                                     || (chooser.isDirectorySelectionEnabled()
-                                        && (fsv.isFileSystem(f) || Files.isSymbolicLink(path))
+                                        && (fsv.isFileSystem(f) || Files.isSymbolicLink(f.toPath()))
                                         && isDir)) {
                                     fList.add(f);
                                 }
