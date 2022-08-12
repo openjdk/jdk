e9c7a51543be688ff03f28c795bad27457d97b67
Kevin Walls <kevin.walls@oracle.com>
2022-08-12 15:31:48 +0100

    remove unnecessary import.

--- test/jdk/javax/management/loading/GetMBeansFromURLTest.java
+++ test/jdk/javax/management/loading/GetMBeansFromURLTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2004, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,7 +34,6 @@
  * @run main  GetMBeansFromURLTest
  */
 
-import javax.management.MBeanRegistrationException;
 import javax.management.MBeanServer;
 import javax.management.MBeanServerFactory;
 import javax.management.ObjectName;
