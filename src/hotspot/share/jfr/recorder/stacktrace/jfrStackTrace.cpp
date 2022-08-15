88790a19712e1b73ff77e43b52325a2241f04777
Stefan Karlsson <stefank@openjdk.org>
2022-08-15 16:25:14 +0200

    Turn off walking of heap stack chunks for JFR events

--- src/hotspot/share/jfr/recorder/stacktrace/jfrStackTrace.cpp
+++ src/hotspot/share/jfr/recorder/stacktrace/jfrStackTrace.cpp
@@ -149,7 +149,15 @@ class JfrVframeStream : public vframeStreamCommon {
 };
 
 JfrVframeStream::JfrVframeStream(JavaThread* jt, const frame& fr, bool stop_at_java_call_stub, bool async_mode) :
-  vframeStreamCommon(RegisterMap(jt, false, false, true)), _cont_entry(JfrThreadLocal::is_vthread(jt) ? jt->last_continuation() : nullptr),
+  // NOTE: walk_cont is false, because of interactions with ZGC relocation and
+  //       load barriers. This code is run while generating stack traces for
+  //       the ZPage allocation event, even when ZGC is relocating  objects.
+  //       When ZGC is relocating, it is forbidden to run code that performes
+  //       load barriers. With walk_cont == true, we visit heap stack cunks
+  //       and could be using load barriers.
+  //
+  // There might be an opportunity here to only set walk_cont == false, when we execute within ZGC relocation.
+  vframeStreamCommon(RegisterMap(jt, false, false, false /* walk_cont */)), _cont_entry(JfrThreadLocal::is_vthread(jt) ? jt->last_continuation() : nullptr),
     _async_mode(async_mode), _vthread(JfrThreadLocal::is_vthread(jt)) {
   assert(!_vthread || _cont_entry != nullptr, "invariant");
   _reg_map.set_async(async_mode);
