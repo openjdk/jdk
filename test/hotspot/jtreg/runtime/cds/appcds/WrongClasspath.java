5af389655b6be77eefb587d4d5af1f14826f1bf5
Calvin Cheung <calvin.cheung@oracle.com>
2022-08-16 05:18:29 +0000

    comments from Ioi

--- test/hotspot/jtreg/runtime/cds/appcds/WrongClasspath.java
+++ test/hotspot/jtreg/runtime/cds/appcds/WrongClasspath.java
@@ -25,7 +25,7 @@
 /*
  * @test
  * @summary classpath mismatch between dump time and execution time
- * @requires vm.cds & vm.flagless
+ * @requires vm.cds
  * @library /test/lib
  * @compile test-classes/Hello.java
  * @compile test-classes/C2.java
@@ -75,7 +75,7 @@ public class WrongClasspath {
     // message should be there.
     output = TestCommon.execAuto(
         "-cp", jars, "Hello");
-    output.shouldMatch(".warning..cds. A jar file is not the one used while building the shared archive file:.*jar2.jar")
+    output.shouldMatch("A jar file is not the one used while building the shared archive file:.*jar2.jar")
           .shouldMatch(".warning..cds.*jar2.jar timestamp has changed.");
   }
 }
