09846f007628cf995c0d8f145d4f242b02d4f4c1
Naoto Sato <naoto.sato@oracle.com>
2022-08-21 13:42:26 -0700

    Modified the test per review suggestion

--- test/jdk/java/io/InputStreamReader/StatefulDecoderNearEOF.java
+++ test/jdk/java/io/InputStreamReader/StatefulDecoderNearEOF.java
@@ -29,29 +29,51 @@
  */
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 import java.nio.charset.CodingErrorAction;
 import java.nio.charset.MalformedInputException;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.IntStream;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertThrows;
 
 @Test
 public class StatefulDecoderNearEOF {
-    private static final byte[] INPUT = {
-            (byte) 0xff, (byte) 0xfe, // BOM (in UTF-16LE)
-            0, (byte) 0xd8, // High surrogate (in UTF-16LE)
-    };
 
-    public void testStatefulDecoderNearEOF() {
-        assertThrows(MalformedInputException.class, () -> {
-            try (var r = new InputStreamReader(
-                    new ByteArrayInputStream(INPUT),
-                    StandardCharsets.UTF_16.newDecoder().onMalformedInput(CodingErrorAction.REPORT))) {
-                System.out.printf("%04x%n", r.read()); // \u00d8 (wrong, uses UTF-16BE)
-                System.out.printf("%04x%n", r.read()); // EOF
-            }
-        });
+    @DataProvider
+    public Object[][] inputs() {
+        return new Object[][] {
+            // BOM, followed by High surrogate (in UTF-16LE).
+            // First read() should throw an exception.
+            {new byte[] {(byte)0xff, (byte)0xfe, 0, (byte)0xd8}, 0},
+
+            // BOM, followed by 'A', 'B', 'C', then by High surrogate (in UTF-16LE).
+            // Fourth read() should throw an exception.
+            {new byte[] {(byte)0xff, (byte)0xfe, (byte)0x41, 0, (byte)0x42, 0, (byte)0x43, 0, 0, (byte)0xd8}, 3},
+        };
+    }
+
+    @Test (dataProvider = "inputs")
+    public void testStatefulDecoderNearEOF(byte[] ba, int numSucessReads) throws IOException {
+        try (var r = new InputStreamReader(
+                new ByteArrayInputStream(ba),
+                StandardCharsets.UTF_16.newDecoder().onMalformedInput(CodingErrorAction.REPORT))) {
+            // Issue read() as many as numSucessReads which should not fail
+            IntStream.rangeClosed(1, numSucessReads).forEach(i -> {
+                try {
+                    assertEquals(r.read(), (int)ba[i * 2]);
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
+
+            // Final dangling high surrogate should throw an exception
+            assertThrows(MalformedInputException.class, () -> r.read());
+        }
     }
 }
