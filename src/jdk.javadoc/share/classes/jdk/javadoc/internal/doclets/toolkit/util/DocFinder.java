4ba658f098fadf649a99f23ecaff065caed98464
Pavel Rappo <prappo@openjdk.org>
2022-08-25 16:26:13 +0100

    refactor: support exceptions in DocFinder.search
    Provides a specialized version of Function, which can optionally throw
    a checked exception.

--- src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/DocFinder.java
+++ src/jdk.javadoc/share/classes/jdk/javadoc/internal/doclets/toolkit/util/DocFinder.java
@@ -38,6 +38,51 @@ import javax.lang.model.element.ExecutableElement;
 
 public class DocFinder {
 
+    /*
+     * A specialized function that accepts a method and either returns an
+     * optional result or throws a possibly checked exception, which
+     * terminates the search and transparently bubbles up the stack.
+     *
+     * If a method does not meet the criterion, returns an empty optional.
+     *
+     * In a rare case, if a criterion performs inner searches and the search
+     * client needs to disambiguate between the outer search exceptions and
+     * any inner search exceptions, the criterion needs to provide extra code
+     * (to wrap and/or unwrap exceptions). For example:
+     *
+     *     try {
+     *         r = finder.trySearch(method, m -> {
+     *             ...
+     *             try {
+     *                 r1 = finder.trySearch(...);
+     *             } catch (NoOverriddenMethodsFound e) {
+     *                 throw new MyInnerSearchException(e);
+     *             }
+     *             ...
+     *         });
+     *     } catch (NoOverriddenMethodsFound e) {
+     *         ...
+     *     } catch (MyInnerSearchException e) {
+     *         ...
+     *     }
+     *
+     * Since such a use case should be rare, this API does not account for it.
+     * This allows to reduce bloat and streamline a typical use case.
+     *
+     * Here are some examples of the API bloat avoided:
+     *
+     *   - unconditional unwrapping of a dedicated exception thrown by
+     *     a search method
+     *   - either unconditional handling of a dedicated exception thrown by
+     *     a search method, or having multiple (overloaded?) search methods
+     *     and types of criterion: a criterion that can throw and a criterion
+     *     that cannot throw an exception
+     */
+    @FunctionalInterface
+    public interface Criterion<T, X extends Throwable> {
+        Optional<T> apply(ExecutableElement method) throws X;
+    }
+
     private final Function<ExecutableElement, ExecutableElement> overriddenMethodLookup;
     private final BiFunction<ExecutableElement, ExecutableElement, Iterable<ExecutableElement>> implementedMethodsLookup;
 
@@ -50,36 +95,57 @@ public class DocFinder {
     public static final class NoOverriddenMethodsFound extends Exception {
         @java.io.Serial
         private static final long serialVersionUID = 1L;
+
+        // only DocFinder should instantiate this exception
+        private NoOverriddenMethodsFound() { }
     }
 
-    public <T> Optional<T> search(ExecutableElement method,
-                                  Function<? super ExecutableElement, Optional<T>> criteria) {
-        return search(method, true, criteria);
+    public <T, X extends Throwable> Optional<T> search(ExecutableElement method,
+                                                       Criterion<T, X> criterion)
+            throws X
+    {
+        return search(method, true, criterion);
     }
 
-    public <T> Optional<T> search(ExecutableElement method,
-                                  boolean includeMethod,
-                                  Function<? super ExecutableElement, Optional<T>> criteria) {
+    public <T, X extends Throwable> Optional<T> search(ExecutableElement method,
+                                                       boolean includeMethod,
+                                                       Criterion<T, X> criterion)
+            throws X
+    {
         try {
-            return search0(method, includeMethod, false, criteria);
+            return search0(method, includeMethod, false, criterion);
         } catch (NoOverriddenMethodsFound e) {
             // should not happen because the exception flag is unset
             throw new AssertionError(e);
         }
     }
 
-    public <T> Optional<T> trySearch(ExecutableElement method,
-                                     Function<? super ExecutableElement, Optional<T>> criteria)
-            throws NoOverriddenMethodsFound
+    public <T, X extends Throwable> Optional<T> trySearch(ExecutableElement method,
+                                                          Criterion<T, X> criterion)
+            throws NoOverriddenMethodsFound, X
     {
-        return search0(method, false, true, criteria);
+        return search0(method, false, true, criterion);
     }
 
-    private <T> Optional<T> search0(ExecutableElement method,
-                                    boolean includeMethodInSearch,
-                                    boolean throwExceptionIfDoesNotOverride,
-                                    Function<? super ExecutableElement, Optional<T>> criteria)
-            throws NoOverriddenMethodsFound
+    /*
+     * Overridden methods hierarchy search.
+     *
+     * Depending on how it is instructed, search starts either from the given
+     * method or the first method it overrides. The search then applies the
+     * given criterion to that method and methods up the hierarchy, in order,
+     * until either of the following happens:
+     *
+     *  - the criterion returns a non-empty optional
+     *  - the criterion throws an exception
+     *  - the hierarchy is exhausted
+     *  - the given method overrides no methods and
+     *    the search is instructed to detect that
+     */
+    private <T, X extends Throwable> Optional<T> search0(ExecutableElement method,
+                                                         boolean includeMethodInSearch,
+                                                         boolean throwExceptionIfDoesNotOverride,
+                                                         Criterion<T, X> criterion)
+            throws NoOverriddenMethodsFound, X
     {
         // if required, first check if the method overrides anything, so that
         // the result would not depend on whether the method itself is included
@@ -89,30 +155,37 @@ public class DocFinder {
             throw new NoOverriddenMethodsFound();
         }
         if (includeMethodInSearch) {
-            Optional<T> r = criteria.apply(method);
+            Optional<T> r = criterion.apply(method);
             if (r.isPresent())
                 return r;
         }
         while (overriddenMethods.hasNext()) {
             ExecutableElement m = overriddenMethods.next();
-            Optional<T> r = criteria.apply(m);
+            Optional<T> r = criterion.apply(m);
             if (r.isPresent())
                 return r;
         }
         return Optional.empty();
     }
 
+    /*
+     * An iterator over methods overridden by some method.
+     *
+     * The iteration order is as defined in the Documentation Comment
+     * Specification for the Standard Doclet.
+     *
+     * This iterator can be used to create a stream; for example:
+     *
+     *     var spliterator = Spliterators.spliteratorUnknownSize(iterator,
+     *             Spliterator.ORDERED | Spliterator.NONNULL
+     *                     | Spliterator.IMMUTABLE | Spliterator.DISTINCT);
+     *     var stream = StreamSupport.stream(spliterator, false);
+     */
     private class OverriddenMethodsHierarchy implements Iterator<ExecutableElement> {
 
         final Deque<LazilyAccessedImplementedMethods> path = new LinkedList<>();
         ExecutableElement next;
 
-        /*
-         * Constructs an iterator over methods overridden by the given method.
-         *
-         * The iteration order is as defined in the Documentation Comment
-         * Specification for the Standard Doclet.
-         */
         public OverriddenMethodsHierarchy(ExecutableElement method) {
             assert method.getKind() == ElementKind.METHOD : method.getKind();
             next = method;
