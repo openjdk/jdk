adde88d11e49efcd12182fadf6c5b9589ff906fa
Tejesh R <tejesh.r@oracle.com>
2022-08-25 08:51:03 +0530

    Updated based on review comments

--- src/java.desktop/share/classes/javax/swing/text/DefaultStyledDocument.java
+++ src/java.desktop/share/classes/javax/swing/text/DefaultStyledDocument.java
@@ -484,6 +484,18 @@ public class DefaultStyledDocument extends AbstractDocument implements StyledDoc
      * A write lock is held by this operation while changes
      * are being made, and a DocumentEvent is sent to the listeners
      * after the change has been successfully completed.
+     *
+     * <p>
+     * {@code offset} and {@code length} define the range of the text
+     * over which the attributes are set.
+     * If the length is &lt;= 0, then no action is taken  and the method
+     * just returns.
+     * If the offset is &lt;=0 or &gt; the length of the text then no
+     * action is taken, and the method just returns.
+     * Otherwise if {@code offset + length} will exceed the length of
+     * the  text then the affected range is truncated.
+     * </p>
+     *
      * <p>
      * This method is thread safe, although most Swing methods
      * are not. Please see
