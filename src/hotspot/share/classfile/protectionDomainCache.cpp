842b30ab3d7c24551f1a90d6a0bded087c6f3e6c
Coleen Phillimore <coleen.phillimore@oracle.com>
2022-08-26 17:03:03 +0000

    include mutexLocker.hpp for minimal build.

--- src/hotspot/share/classfile/protectionDomainCache.cpp
+++ src/hotspot/share/classfile/protectionDomainCache.cpp
@@ -35,6 +35,7 @@
 #include "oops/oop.inline.hpp"
 #include "oops/weakHandle.inline.hpp"
 #include "runtime/atomic.hpp"
+#include "runtime/mutexLocker.hpp"
 #include "utilities/growableArray.hpp"
 #include "utilities/resourceHash.hpp"
 
