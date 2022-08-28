98ead88d3676ec5ded2087166418538a10ba7861
Ramkumar Sunderbabu <ramkumar.sunderbabu@oracle.com>
2022-08-28 09:52:09 +0530

    8282651: ZGC: vmTestbase/gc/ArrayJuggle/ tests fails intermittently with exit code 97

--- test/hotspot/jtreg/vmTestbase/gc/ArrayJuggle/Juggle32/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/gc/ArrayJuggle/Juggle32/TestDescription.java
@@ -31,12 +31,8 @@
  *
  * @library /vmTestbase
  *          /test/lib
- * @build jdk.test.whitebox.WhiteBox
- * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
  * @run main/othervm
- *      -Xbootclasspath/a:.
- *      -XX:+UnlockDiagnosticVMOptions
- *      -XX:+WhiteBoxAPI
+ *      -XX:+HeapDumpOnOutOfMemoryError
  *      -Xlog:gc=debug:gc.log
  *      gc.ArrayJuggle.Juggle01.Juggle01
  *      -gp hashed(objectArr)
