6a0efae0f14d831e7c320dc7b40c923d88a8ce70
Daniel Skantz <daniel.skantz@oracle.com>
2022-09-01 17:02:39 +0200

    JDK-8283627 - Update MachineDescription.java
    JDK-8283627 - Update comment also in MachineDescription.java regarding LP64 (as noted by @plummercj, @sspitsyn)

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/MachineDescription.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/debugger/MachineDescription.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -54,6 +54,6 @@ public interface MachineDescription extends Serializable {
   public boolean isBigEndian();
 
   /** Indicates whether the underlying machine supports the LP64 data
-      model (currently only SPARC/64). */
+      model. */
   public boolean isLP64();
 }
