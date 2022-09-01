2d18dda3f2074a4f8b9a0c62ece9ac6d5284e93b
Joe Darcy <darcy@openjdk.org>
2022-09-01 16:43:50 +0000

    8173605: Remove support for source and target 1.7 option in javac
    Reviewed-by: vromero

--- test/langtools/tools/javac/analyzer/AnalyzersCheckSourceLevel.java
+++ test/langtools/tools/javac/analyzer/AnalyzersCheckSourceLevel.java
@@ -4,7 +4,7 @@
  * @summary Ensure that the lambda analyzer does not run when --release 7 is specified,
  *          even if explicitly requested
  * @compile/fail/ref=AnalyzersCheckSourceLevel.out -Werror -XDfind=lambda -XDrawDiagnostics AnalyzersCheckSourceLevel.java
- * @compile -Werror --release 7 -Xlint:-options -XDfind=lambda AnalyzersCheckSourceLevel.java
+ *
  */
 public class AnalyzersCheckSourceLevel {
     void t() {
