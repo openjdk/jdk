20d589e3238cd45defdc4cb2f6891148861b8e08
Vladimir Ivanov <vladimir.x.ivanov@oracle.com>
2022-09-02 10:41:53 -0700

    constants

--- src/hotspot/cpu/x86/stubRoutines_x86.hpp
+++ src/hotspot/cpu/x86/stubRoutines_x86.hpp
@@ -123,11 +123,6 @@ class x86 {
   static jint    _mxcsr_std;
 
   static address _verify_mxcsr_entry;
-  // shuffle mask for fixing up 128-bit words consisting of big-endian 32-bit integers
-  static address _key_shuffle_mask_addr;
-
-  //shuffle mask for big-endian 128-bit integers
-  static address _counter_shuffle_mask_addr;
 
   static address _method_entry_barrier;
 
@@ -145,11 +140,6 @@ class x86 {
 #endif // _LP64
   // table for CRC32C
   static juint* _crc32c_table;
-  // swap mask for ghash
-  static address _ghash_long_swap_mask_addr;
-  static address _ghash_byte_swap_mask_addr;
-  static address _ghash_poly_addr;
-  static address _ghash_shuffmask_addr;
 
   // upper word mask for sha1
   static address _upper_word_mask_addr;
@@ -190,7 +180,6 @@ class x86 {
   static address _k512_W_addr;
   // byte flip mask for sha512
   static address _pshuffle_byte_flip_mask_addr_sha512;
-  static address _counter_mask_addr;
   // Masks for base64
   static address _encoding_table_base64;
   static address _shuffle_base64;
@@ -206,7 +195,6 @@ class x86 {
   static address _join_1_2_base64;
   static address _join_2_3_base64;
   static address _decoding_table_base64;
-  static address _ghash_poly512_addr;
 #endif
   // byte flip mask for sha256
   static address _pshuffle_byte_flip_mask_addr;
@@ -214,20 +202,13 @@ class x86 {
  public:
   static address addr_mxcsr_std()        { return (address)&_mxcsr_std; }
   static address verify_mxcsr_entry()    { return _verify_mxcsr_entry; }
-  static address key_shuffle_mask_addr() { return _key_shuffle_mask_addr; }
-  static address counter_shuffle_mask_addr() { return _counter_shuffle_mask_addr; }
   static address crc_by128_masks_addr()  { return (address)_crc_by128_masks; }
 #ifdef _LP64
   static address crc_by128_masks_avx512_addr()  { return (address)_crc_by128_masks_avx512; }
   static address shuf_table_crc32_avx512_addr()  { return (address)_shuf_table_crc32_avx512; }
   static address crc_table_avx512_addr()  { return (address)_crc_table_avx512; }
   static address crc32c_table_avx512_addr()  { return (address)_crc32c_table_avx512; }
-  static address ghash_polynomial512_addr() { return _ghash_poly512_addr; }
 #endif // _LP64
-  static address ghash_long_swap_mask_addr() { return _ghash_long_swap_mask_addr; }
-  static address ghash_byte_swap_mask_addr() { return _ghash_byte_swap_mask_addr; }
-  static address ghash_shufflemask_addr() { return _ghash_shuffmask_addr; }
-  static address ghash_polynomial_addr() { return _ghash_poly_addr; }
   static address upper_word_mask_addr() { return _upper_word_mask_addr; }
   static address shuffle_byte_flip_mask_addr() { return _shuffle_byte_flip_mask_addr; }
   static address k256_addr()      { return _k256_adr; }
@@ -336,7 +317,6 @@ class x86 {
   static address base64_avx2_shuffle_addr() { return _avx2_shuffle_base64; }
   static address base64_avx2_input_mask_addr() { return _avx2_input_mask_base64; }
   static address base64_avx2_lut_addr() { return _avx2_lut_base64; }
-  static address counter_mask_addr() { return _counter_mask_addr; }
   static address base64_vbmi_lookup_lo_addr() { return _lookup_lo_base64; }
   static address base64_vbmi_lookup_hi_addr() { return _lookup_hi_base64; }
   static address base64_vbmi_lookup_lo_url_addr() { return _lookup_lo_base64url; }
