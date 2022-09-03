4ba53b452360a3a52f462f151aca769687a88b4c
Dean Long <17332032+dean-long@users.noreply.github.com>
2022-09-03 21:50:20 +0000

    remove support for pre-2013 replay files

--- src/hotspot/share/ci/ciReplay.cpp
+++ src/hotspot/share/ci/ciReplay.cpp
@@ -732,14 +732,7 @@ class CompileReplay : public StackObj {
     Method* method = parse_method(CHECK);
     if (had_error()) return;
     int entry_bci = parse_int("entry_bci");
-    const char* comp_level_label = "comp_level";
-    int comp_level = parse_int(comp_level_label);
-    // old version w/o comp_level
-    if (had_error() && (error_message() == comp_level_label)) {
-      // use highest available tier
-      comp_level = CompilationPolicy::highest_compile_level();
-      _error_message = NULL;
-    }
+    int comp_level = parse_int("comp_level");
     if (!is_valid_comp_level(comp_level)) {
       return;
     }
