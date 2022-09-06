b4d5004dd3299d084769309b505b19ed7dd3182e
Markus KARG <markus@headcrashing.eu>
2022-09-06 19:13:19 +0200

    Revert "HexPrinter::transferTo"
    This reverts commit 6f0e2a8f7be5706b39a8e9bc88a97f34d2819098.

--- test/lib/jdk/test/lib/hexdump/HexPrinter.java
+++ test/lib/jdk/test/lib/hexdump/HexPrinter.java
@@ -1185,12 +1185,5 @@ public final class HexPrinter {
             byteOffset += Math.max(size, 0);
             return size;
         }
-
-        @Override
-        public long transferTo(OutputStream out) throws IOException {
-            long size = super.transferTo(out);
-            byteOffset += size;
-            return size;
-        }
     }
 }
