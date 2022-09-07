ce915aadaa3525aff40a75c80a55543d3ed8c70e
Weijun Wang <weijun.wang@oracle.com>
2022-09-07 19:09:04 -0400

    remove comment

--- test/jdk/sun/security/ssl/X509TrustManagerImpl/BasicConstraints12.java
+++ test/jdk/sun/security/ssl/X509TrustManagerImpl/BasicConstraints12.java
@@ -21,11 +21,6 @@
  * questions.
  */
 
-//
-// SunJSSE does not support dynamic system properties, no way to re-use
-// system properties in samevm/agentvm mode.
-//
-
 /*
  * @test
  * @bug 8293489
