36d7a37bca95d975ca2779f221431de38c3eed3e
Jie Fu <jiefu@openjdk.org>
2022-09-14 17:39:55 +0800

    8293792: runtime/Dictionary/ProtectionDomainCacheTest.java fails with FileAlreadyExistsException: /tmp

--- test/lib/jdk/test/lib/util/JarUtils.java
+++ test/lib/jdk/test/lib/util/JarUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -68,7 +68,7 @@ public final class JarUtils {
     {
         // create the target directory
         Path parent = jarfile.getParent();
-        if (parent != null) {
+        if (parent != null && Files.notExists(parent)) {
             Files.createDirectories(parent);
         }
 
