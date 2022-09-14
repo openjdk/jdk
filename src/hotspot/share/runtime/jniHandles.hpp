41cf5090031ead0bf6d6e69752e4288b394ee8c0
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2022-09-14 14:52:23 +0200

    UPSTREAM: JNI Global Handle Change
    Co-authored-by: Stefan Karlsson <stefank@openjdk.org>

--- src/hotspot/share/runtime/jniHandles.hpp
+++ src/hotspot/share/runtime/jniHandles.hpp
@@ -45,8 +45,10 @@ class JNIHandles : AllStatic {
   static OopStorage* global_handles();
   static OopStorage* weak_global_handles();
 
-  inline static bool is_jweak(jobject handle);
-  inline static oop* jobject_ptr(jobject handle); // NOT jweak!
+  inline static bool is_jweak_tagged(jobject handle);
+  inline static bool is_global_tagged(jobject handle);
+  inline static oop* jobject_ptr(jobject handle); // NOT jweak or global!
+  inline static oop* global_ptr(jobject handle);
   inline static oop* jweak_ptr(jobject handle);
 
   template <DecoratorSet decorators, bool external_guard> inline static oop resolve_impl(jobject handle);
@@ -70,7 +72,15 @@ class JNIHandles : AllStatic {
   static const uintptr_t weak_tag_size = 1;
   static const uintptr_t weak_tag_alignment = (1u << weak_tag_size);
   static const uintptr_t weak_tag_mask = weak_tag_alignment - 1;
+  static const uintptr_t tag_size = 2;
+  static const uintptr_t tag_alignment = (1u << tag_size);
+  static const uintptr_t tag_mask = tag_alignment - 1;
   static const int weak_tag_value = 1;
+  // Low tag bit in jobject used to distinguish a global jobject.
+  // There are places where we need to be able to distinguish global
+  // values from other jobjects, and is_global_handle is unsuitable
+  // for performance reasons.
+  static const int global_tag_value = 2;
 
   // Resolve handle into oop
   inline static oop resolve(jobject handle);
