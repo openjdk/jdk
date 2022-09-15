aff5ff14b208b3c2be93d7b4fab8b07c5be12f3e
Adam Sotona <asotona@openjdk.org>
2022-09-15 10:10:23 +0000

    8244681: Add a warning for possibly lossy conversion in compound assignments 8293797: Release Note: Javac warns about type casts in compound assignments with possible lossy conversions
    Reviewed-by: erikj, prr

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -3941,6 +3941,7 @@ public class Attr extends JCTree.Visitor {
             chk.checkCastable(tree.rhs.pos(),
                               operator.type.getReturnType(),
                               owntype);
+            chk.checkLossOfPrecision(tree.rhs.pos(), operand, owntype);
         }
         result = check(tree, owntype, KindSelector.VAL, resultInfo);
     }
