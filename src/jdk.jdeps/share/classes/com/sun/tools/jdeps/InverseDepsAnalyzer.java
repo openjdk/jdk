f83f92880ee7aa2ff7a5239f34effd358ec0d33e
MBaesken <matthias.baesken@sap.com>
2022-09-16 10:24:10 +0200

    JDK-8293701

--- src/jdk.jdeps/share/classes/com/sun/tools/jdeps/InverseDepsAnalyzer.java
+++ src/jdk.jdeps/share/classes/com/sun/tools/jdeps/InverseDepsAnalyzer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -147,7 +147,7 @@ public class InverseDepsAnalyzer extends DepsAnalyzer {
                     m.descriptor().requires().stream()
                         .map(Requires::name)
                         .map(configuration::findModule)  // must be present
-                        .forEach(v -> builder.addEdge(v.get(), m));
+                        .forEach(v -> builder.addEdge(v.isPresent() ? v.get() : null, m));
                 });
 
             // add the dependences from the analysis
