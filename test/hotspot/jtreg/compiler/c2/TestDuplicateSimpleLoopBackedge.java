fdc07f6ceac4f044afb34a86b4e4272b7396aee9
casparcwang <casparcwang@tencent.com>
2022-09-20 19:54:35 +0800

    Fix code style

--- test/hotspot/jtreg/compiler/c2/TestDuplicateSimpleLoopBackedge.java
+++ test/hotspot/jtreg/compiler/c2/TestDuplicateSimpleLoopBackedge.java
@@ -24,7 +24,6 @@
  /**
   * @test
   * @bug 8293978
-  * @library /test/lib
   * @summary Crash in PhaseIdealLoop::verify_strip_mined_scheduling
   *
   * @run main/othervm -Xbatch TestDuplicateSimpleLoopBackedge
@@ -33,23 +32,26 @@
 
 public class TestDuplicateSimpleLoopBackedge {
     static void zero(Byte[] a) {
-        for (int e = 0; e < a.length; e++)
+        for (int e = 0; e < a.length; e++) {
             a[e] = 0;
+        }
     }
 
     int foo(int g) {
         Byte h[] = new Byte[500];
         zero(h);
         short i = 7;
-        while (i != 1)
+        while (i != 1) {
             i = (short)(i - 3);
+        }
         return 0;
     }
 
     void bar(String[] k) {
         try {
             int l = 5;
-            if (l < foo(l));
+            if (l < foo(l)) {
+            }
         } catch (Exception m) {
         }
     }
