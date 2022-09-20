356ee078f15a7fabc42f3a8fc46a9d4014b2fe81
lmesnik <leonid.mesnik@oracle.com>
2022-09-19 17:31:40 -0700

    revert changes

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/ThreadReference/interrupt/interrupt001/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/ThreadReference/interrupt/interrupt001/TestDescription.java
@@ -60,7 +60,7 @@
  *     4759463 TEST_BUG: tests against ThreadReference.interrupt() should be corrected
  *
  * @library /vmTestbase
- *          /test/lib /testlibrary
+ *          /test/lib
  * @build nsk.jdi.ThreadReference.interrupt.interrupt001
  *        nsk.jdi.ThreadReference.interrupt.interrupt001a
  * @run main/othervm
@@ -70,5 +70,5 @@
  *      -waittime=5
  *      -debugee.vmkind=java
  *      -transport.address=dynamic
- *      -debugee.vmkeys="${test.vm.opts} ${test.java.opts} -Djava.library.path=${test.nativepath}"
+ *      -debugee.vmkeys="${test.vm.opts} ${test.java.opts}"
  */
