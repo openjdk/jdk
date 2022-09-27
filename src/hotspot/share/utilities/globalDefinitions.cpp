28899c6df28103b634d9837cc3f9769b16d55f07
Johan Sjölén <johan.sjolen@oracle.com>
2022-09-27 13:18:30 +0200

    Change impl of type2name

--- src/hotspot/share/utilities/globalDefinitions.cpp
+++ src/hotspot/share/utilities/globalDefinitions.cpp
@@ -236,6 +236,17 @@ const char* type2name_tab[T_CONFLICT+1] = {
   "*narrowklass*",
   "*conflict*"
 };
+const char* type2name(BasicType t) {
+  if (t < (T_CONFLICT + 1)) {
+    return type2name_tab[t];
+  } else if (t == T_ILLEGAL) {
+    return "*illegal*";
+  } else {
+    fatal("invalid type %d", t);
+    return "invalid type";
+  }
+}
+
 
 
 BasicType name2type(const char* name) {
