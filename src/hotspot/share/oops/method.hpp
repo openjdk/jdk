c7ab1caafb4e92bbc2900a4d28ebfd27bdd29cf5
Vladimir Ivanov <vlivanov@openjdk.org>
2022-09-30 23:30:45 +0000

    8294609: C2: Improve inlining of methods with unloaded signature classes
    Reviewed-by: kvn, dlong

--- src/hotspot/share/oops/method.hpp
+++ src/hotspot/share/oops/method.hpp
@@ -965,9 +965,6 @@ public:
   // Resolve all classes in signature, return 'true' if successful
   static bool load_signature_classes(const methodHandle& m, TRAPS);
 
-  // Return if true if not all classes references in signature, including return type, has been loaded
-  static bool has_unloaded_classes_in_signature(const methodHandle& m, TRAPS);
-
   // Printing
   void print_short_name(outputStream* st = tty) const; // prints as klassname::methodname; Exposed so field engineers can debug VM
 #if INCLUDE_JVMTI
