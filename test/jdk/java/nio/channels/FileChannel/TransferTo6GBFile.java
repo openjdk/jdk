0f22fb5418c0af51029d7adddb2fefdfbded0ca3
Daniel Jelinski <daniel.jelinski@oracle.com>
2022-09-30 22:19:41 +0200

    Use try with resources

--- test/jdk/java/nio/channels/FileChannel/TransferTo6GBFile.java
+++ test/jdk/java/nio/channels/FileChannel/TransferTo6GBFile.java
@@ -60,97 +60,91 @@ public class TransferTo6GBFile {
 
         out.println("  Writing large file...");
         long t0 = System.nanoTime();
-        FileChannel fc = FileChannel.open(file, READ, WRITE);
-        try {
+        try (FileChannel fc = FileChannel.open(file, READ, WRITE)) {
             fc.write(ByteBuffer.wrap("0123456789012345".getBytes("UTF-8")), 6*G);
             long t1 = System.nanoTime();
             out.printf("  Wrote large file in %d ns (%d ms) %n",
             t1 - t0, TimeUnit.NANOSECONDS.toMillis(t1 - t0));
-        } catch (IOException x) {
-            err.println("  Unable to create test file:" + x);
-            fc.close();
-            return;
-        }
 
-        // Setup looback connection and echo server
+            // Setup looback connection and echo server
 
-        ServerSocketChannel ssc = ServerSocketChannel.open();
-        ssc.socket().bind(new InetSocketAddress(0));
+            ServerSocketChannel ssc = ServerSocketChannel.open();
+            ssc.socket().bind(new InetSocketAddress(0));
 
-        InetAddress lh = InetAddress.getLocalHost();
-        InetSocketAddress isa = new InetSocketAddress(lh, ssc.socket().getLocalPort());
-        SocketChannel source = SocketChannel.open(isa);
-        SocketChannel sink = ssc.accept();
+            InetAddress lh = InetAddress.getLocalHost();
+            InetSocketAddress isa = new InetSocketAddress(lh, ssc.socket().getLocalPort());
+            SocketChannel source = SocketChannel.open(isa);
+            SocketChannel sink = ssc.accept();
 
-        Thread thr = new Thread(new EchoServer(sink));
-        thr.start();
+            Thread thr = new Thread(new EchoServer(sink));
+            thr.start();
 
-        // Test data is array of positions and counts
+            // Test data is array of positions and counts
 
-        long testdata[][] = {
-            { 2*G-1,    1 },
-            { 2*G-1,    10 },       // across 2GB boundary
-            { 2*G,      1 },
-            { 2*G,      10 },
-            { 2*G+1,    1 },
-            { 4*G-1,    1 },
-            { 4*G-1,    10 },       // across 4GB boundary
-            { 4*G,      1 },
-            { 4*G,      10 },
-            { 4*G+1,    1 },
-            { 5*G-1,    1 },
-            { 5*G-1,    10 },
-            { 5*G,      1 },
-            { 5*G,      10 },
-            { 5*G+1,    1 },
-            { 6*G,      1 },
-        };
+            long testdata[][] = {
+                { 2*G-1,    1 },
+                { 2*G-1,    10 },       // across 2GB boundary
+                { 2*G,      1 },
+                { 2*G,      10 },
+                { 2*G+1,    1 },
+                { 4*G-1,    1 },
+                { 4*G-1,    10 },       // across 4GB boundary
+                { 4*G,      1 },
+                { 4*G,      10 },
+                { 4*G+1,    1 },
+                { 5*G-1,    1 },
+                { 5*G-1,    10 },
+                { 5*G,      1 },
+                { 5*G,      10 },
+                { 5*G+1,    1 },
+                { 6*G,      1 },
+            };
 
-        ByteBuffer sendbuf = ByteBuffer.allocateDirect(100);
-        ByteBuffer readbuf = ByteBuffer.allocateDirect(100);
+            ByteBuffer sendbuf = ByteBuffer.allocateDirect(100);
+            ByteBuffer readbuf = ByteBuffer.allocateDirect(100);
 
-        try {
-            byte value = 0;
-            for (int i=0; i<testdata.length; i++) {
-                long position = testdata[(int)i][0];
-                long count = testdata[(int)i][1];
+            try {
+                byte value = 0;
+                for (int i=0; i<testdata.length; i++) {
+                    long position = testdata[(int)i][0];
+                    long count = testdata[(int)i][1];
 
-                // generate bytes
-                for (long j=0; j<count; j++) {
-                    sendbuf.put(++value);
+                    // generate bytes
+                    for (long j=0; j<count; j++) {
+                        sendbuf.put(++value);
+                    }
+                    sendbuf.flip();
+
+                    // write to file and transfer to echo server
+                    fc.write(sendbuf, position);
+                    t0 = System.nanoTime();
+                    fc.transferTo(position, count, source);
+                    out.printf("  transferTo(%d, %2d, source): %d ns%n",
+                        position, count, System.nanoTime() - t0);
+
+                    // read from echo server
+                    long nread = 0;
+                    while (nread < count) {
+                        int n = source.read(readbuf);
+                        if (n < 0)
+                            throw new RuntimeException("Premature EOF!");
+                        nread += n;
+                    }
+
+                    // check reply from echo server
+                    readbuf.flip();
+                    sendbuf.flip();
+                    if (!readbuf.equals(sendbuf))
+                        throw new RuntimeException("Echoed bytes do not match!");
+                    readbuf.clear();
+                    sendbuf.clear();
                 }
-                sendbuf.flip();
-
-                // write to file and transfer to echo server
-                fc.write(sendbuf, position);
-                t0 = System.nanoTime();
-                fc.transferTo(position, count, source);
-                out.printf("  transferTo(%d, %2d, source): %d ns%n",
-                    position, count, System.nanoTime() - t0);
-
-                // read from echo server
-                long nread = 0;
-                while (nread < count) {
-                    int n = source.read(readbuf);
-                    if (n < 0)
-                        throw new RuntimeException("Premature EOF!");
-                    nread += n;
-                }
-
-                // check reply from echo server
-                readbuf.flip();
-                sendbuf.flip();
-                if (!readbuf.equals(sendbuf))
-                    throw new RuntimeException("Echoed bytes do not match!");
-                readbuf.clear();
-                sendbuf.clear();
+            } finally {
+                source.close();
+                ssc.close();
             }
-        } finally {
-            source.close();
-            ssc.close();
-            fc.close();
-            Files.delete(file);
         }
+        Files.delete(file);
     }
 
     /**
