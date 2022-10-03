429f5a87de4ee3e4f098b4ebaf08f114dc34949d
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2022-10-03 15:10:58 +0200

    Remove noisy assertion

--- src/hotspot/share/opto/loopnode.cpp
+++ src/hotspot/share/opto/loopnode.cpp
@@ -3901,17 +3901,6 @@ uint IdealLoopTree::est_loop_flow_merge_sz() const {
   return 0;
 }
 
-#ifdef ASSERT
-bool IdealLoopTree::has_reduction_nodes() const {
-  for (uint i = 0; i < _body.size(); i++) {
-    if (_body[i]->is_reduction()) {
-      return true;
-    }
-  }
-  return false;
-}
-#endif // ASSERT
-
 #ifndef PRODUCT
 //------------------------------dump_head--------------------------------------
 // Dump 1 liner for loop header info
--- src/hotspot/share/opto/loopnode.hpp
+++ src/hotspot/share/opto/loopnode.hpp
@@ -778,11 +778,6 @@ public:
 
   void remove_main_post_loops(CountedLoopNode *cl, PhaseIdealLoop *phase);
 
-#ifdef ASSERT
-  // Tell whether the body contains nodes marked as reductions.
-  bool has_reduction_nodes() const;
-#endif // ASSERT
-
 #ifndef PRODUCT
   void dump_head() const;       // Dump loop head only
   void dump() const;            // Dump this loop recursively
