0464eaf90bf3bcd7f2df979c9d0f8d9d406cb80c
Goetz Lindenmaier <goetz@openjdk.org>
2022-10-04 18:28:54 +0000

    8288651: CDS test HelloUnload.java should not use literal string as ClassLoader name
    Backport-of: e322e77e9535fc3f37b409a1c805e9f6b728377a

--- test/hotspot/jtreg/runtime/cds/appcds/customLoader/test-classes/HelloUnload.java
+++ test/hotspot/jtreg/runtime/cds/appcds/customLoader/test-classes/HelloUnload.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -63,7 +63,7 @@ public class HelloUnload {
         }
 
         URLClassLoader urlClassLoader =
-            new URLClassLoader("HelloClassLoader", urls, null);
+            new URLClassLoader("HelloClassLoader" + System.currentTimeMillis(), urls, null);
         Class c = Class.forName(className, true, urlClassLoader);
         System.out.println(c);
         System.out.println(c.getClassLoader());
