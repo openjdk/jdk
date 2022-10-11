d7a3a9e812d602e6604ca4c6483cb3b2a3ff5b44
Johannes Bechberger <johannes.bechberger@sap.com>
2022-10-11 07:36:02 +0000

    8284752: Zero does not build on Mac OS X due to missing os::current_thread_enable_wx implementation
    Backport-of: bc12e8616d1a28ab8504bcb6edc06783748cc797

--- src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
+++ src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
@@ -350,3 +350,9 @@ int os::extra_bang_size_in_bytes() {
   // Zero does not require an additional stack bang.
   return 0;
 }
+
+#if defined(AARCH64) && defined(__APPLE__)
+void os::current_thread_enable_wx(WXMode mode) {
+  pthread_jit_write_protect_np(mode == WXExec);
+}
+#endif
