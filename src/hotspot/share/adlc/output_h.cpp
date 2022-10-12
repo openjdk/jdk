e0972f81b352ae37b72fa54b0b675a9cb2a0776b
Quan Anh Mai <qamai@openjdk.org>
2022-10-12 00:31:04 +0000

    8283699: Improve the peephole mechanism of hotspot
    Reviewed-by: kvn, dlong

--- src/hotspot/share/adlc/output_h.cpp
+++ src/hotspot/share/adlc/output_h.cpp
@@ -1703,7 +1703,7 @@ void ArchDesc::declareClasses(FILE *fp) {
 
     // If there is an explicit peephole rule, build it
     if ( instr->peepholes() != NULL ) {
-      fprintf(fp,"  virtual MachNode      *peephole(Block *block, int block_index, PhaseRegAlloc *ra_, int &deleted);\n");
+      fprintf(fp,"  virtual int            peephole(Block* block, int block_index, PhaseCFG* cfg_, PhaseRegAlloc* ra_);\n");
     }
 
     // Output the declaration for number of relocation entries
