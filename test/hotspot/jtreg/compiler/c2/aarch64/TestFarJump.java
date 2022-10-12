4a492945bca6dfe9afc5e2e6d6bc3437a3584931
Evgeny Astigeevich <eastigeevich@openjdk.org>
2022-10-12 15:42:23 +0000

    8284681: compiler/c2/aarch64/TestFarJump.java fails with "RuntimeException: for CodeHeap < 250MB the far jump is expected to be encoded with a single branch instruction"
    Backport-of: 21de4e55b8fa2ba138338ec82c159897ab3d4233

--- test/hotspot/jtreg/compiler/c2/aarch64/TestFarJump.java
+++ test/hotspot/jtreg/compiler/c2/aarch64/TestFarJump.java
@@ -74,9 +74,9 @@ public class TestFarJump {
             }
             int dump = (int)Long.parseLong(match, 16);
             int encoding = Integer.reverseBytes(dump);
-            if (isADRP(encoding)) {
-                return true;
-            }
+            // Check the first instruction only. The raw pointer can be confused with the encoded adrp instruction:
+            // emit_exception_handler() = far_call() + should_not_reach_here() = ADRP + ADD + BLR + DCPS1 + raw_pointer
+            return isADRP(encoding);
         }
         return false;
     }
