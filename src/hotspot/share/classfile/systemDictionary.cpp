b871a4cbdccea3c37f9b4fbe59a6aa3ea5fdbcf0
Coleen Phillimore <coleen.phillimore@oracle.com>
2022-10-15 02:54:02 +0000

    Fix comments.

--- src/hotspot/share/classfile/systemDictionary.cpp
+++ src/hotspot/share/classfile/systemDictionary.cpp
@@ -631,7 +631,7 @@ InstanceKlass* SystemDictionary::resolve_instance_class_or_null(Symbol* name,
     //    These class loaders lock a per-class object lock when ClassLoader.loadClass()
     //    is called. A LOAD_INSTANCE placeholder isn't used for mutual exclusion.
     // case 3. traditional classloaders that rely on the classloader object lock
-    //    There is no need for need for LOAD_INSTANCE, since the loadClassInternal code
+    //    There is no need for need for LOAD_INSTANCE, since the SynchronizedLoader.loadClass
     //    will take out the placeholder equivalent in Java code.
     //    For these classloaders, we ensure that the first requestor
     //    completes the load and other requestors wait for completion.
