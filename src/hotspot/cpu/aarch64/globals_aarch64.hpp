75ce3744f5e1252110d9b98062b720c255336061
Goetz Lindenmaier <goetz@openjdk.org>
2022-10-18 13:49:41 +0000

    8272791: java -XX:BlockZeroingLowLimit=1 crashes after 8270947
    Backport-of: e17118aa8afb14dfca049f19fd98a1483b2b5c1b

--- src/hotspot/cpu/aarch64/globals_aarch64.hpp
+++ src/hotspot/cpu/aarch64/globals_aarch64.hpp
@@ -106,7 +106,7 @@ define_pd_global(intx, InlineSmallCode,          1000);
           "Use DC ZVA for block zeroing")                               \
   product(intx, BlockZeroingLowLimit, 256,                              \
           "Minimum size in bytes when block zeroing will be used")      \
-          range(1, max_jint)                                            \
+          range(wordSize, max_jint)                                     \
   product(bool, TraceTraps, false, "Trace all traps the signal handler")\
   product(int, SoftwarePrefetchHintDistance, -1,                        \
           "Use prfm hint with specified distance in compiled code."     \
--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -4885,7 +4885,7 @@ address MacroAssembler::zero_words(Register ptr, Register cnt)
 // r10, r11, rscratch1, and rscratch2 are clobbered.
 address MacroAssembler::zero_words(Register base, uint64_t cnt)
 {
-  guarantee(zero_words_block_size < BlockZeroingLowLimit,
+  assert(wordSize <= BlockZeroingLowLimit,
             "increase BlockZeroingLowLimit");
   address result = nullptr;
   if (cnt <= (uint64_t)BlockZeroingLowLimit / BytesPerWord) {
