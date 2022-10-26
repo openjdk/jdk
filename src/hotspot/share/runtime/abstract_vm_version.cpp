f2e3c7258cf50a4979b4336a0ced3fbeae5c7094
TheShermanTanker <32636402+TheShermanTanker@users.noreply.github.com>
2022-10-26 22:03:52 +0800

    Properly handle VERSION_BUILD

--- src/hotspot/share/runtime/abstract_vm_version.cpp
+++ src/hotspot/share/runtime/abstract_vm_version.cpp
@@ -80,17 +80,13 @@ VirtualizationType Abstract_VM_Version::_detected_virtualization = NoDetectedVir
 
 #define VM_RELEASE HOTSPOT_VERSION_STRING
 
-// Does not work for string literals or C++ syntax characters, but it should be enough
-// for what it's meant to do
-#define EMPTY(ARG) ~(~ARG + 0) == 0 && ~(~ARG + 1) == 1
-
 // HOTSPOT_VERSION_STRING equals the JDK VERSION_STRING (unless overridden
 // in a standalone build).
 int Abstract_VM_Version::_vm_major_version = VERSION_FEATURE;
 int Abstract_VM_Version::_vm_minor_version = VERSION_INTERIM;
 int Abstract_VM_Version::_vm_security_version = VERSION_UPDATE;
 int Abstract_VM_Version::_vm_patch_version = VERSION_PATCH;
-#if EMPTY(VERSION_BUILD)
+#if IS_EMPTY_VALUE(VERSION_BUILD)
 int Abstract_VM_Version::_vm_build_number = 0;
 #else
 int Abstract_VM_Version::_vm_build_number = VERSION_BUILD;
