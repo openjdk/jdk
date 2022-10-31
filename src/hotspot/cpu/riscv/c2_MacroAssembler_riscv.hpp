37e1616d00bfbb078317cc4d2587d4a0de528707
Fei Yang <yangfei@iscas.ac.cn>
2022-10-31 20:40:06 +0800

    8286301: JEP 425 to RISC-V

--- src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.hpp
+++ src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.hpp
@@ -29,6 +29,9 @@
 // C2_MacroAssembler contains high-level macros for C2
 
  private:
+  // Return true if the phase output is in the scratch emit size mode.
+  virtual bool in_scratch_emit_size() override;
+
   void element_compare(Register r1, Register r2,
                        Register result, Register cnt,
                        Register tmp1, Register tmp2,
--- src/hotspot/cpu/riscv/macroAssembler_riscv.hpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.hpp
@@ -55,6 +55,9 @@ class MacroAssembler: public Assembler {
     assert(is_aligned(pc, alignment), "bad alignment");
   }
 
+  // nop
+  void post_call_nop();
+
   // Stack frame creation/removal
   // Note that SP must be updated to the right place before saving/restoring RA and FP
   // because signal based thread suspend/resume could happen asynchronously.
@@ -384,6 +387,12 @@ class MacroAssembler: public Assembler {
   }
 
   static int patch_oop(address insn_addr, address o);
+
+  // Return whether code is emitted to a scratch blob.
+  virtual bool in_scratch_emit_size() {
+    return false;
+  }
+
   address emit_trampoline_stub(int insts_call_instruction_offset, address target);
   void emit_static_call_stub();
 
@@ -571,6 +580,9 @@ public:
 
   void push_CPU_state(bool save_vectors = false, int vector_size_in_bytes = 0);
   void pop_CPU_state(bool restore_vectors = false, int vector_size_in_bytes = 0);
+
+  void push_cont_fastpath(Register java_thread);
+  void pop_cont_fastpath(Register java_thread);
 
   // if heap base register is used - reinit it with the correct value
   void reinit_heapbase();
