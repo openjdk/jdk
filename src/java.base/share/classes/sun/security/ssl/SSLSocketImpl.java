eef3c9605e2d75fc5724118fd0140b7844daa6e8
Zdenek Zambersky <zzambers@redhat.com>
2022-11-02 14:50:24 +0000

    8282600: SSLSocketImpl should not use user_canceled workaround when not necessary
    Backport-of: 7e88ff8a82cefa2a20acc9d5a9e42858d60fe3a3

--- src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
+++ src/java.base/share/classes/sun/security/ssl/SSLSocketImpl.java
@@ -639,8 +639,12 @@ public final class SSLSocketImpl
             if (!conContext.protocolVersion.useTLS13PlusSpec()) {
                 hasCloseReceipt = true;
             } else {
-                // Use a user_canceled alert for TLS 1.3 duplex close.
-                useUserCanceled = true;
+                // Do not use user_canceled workaround if the other side has
+                // already half-closed the connection
+                if (!conContext.isInboundClosed()) {
+                    // Use a user_canceled alert for TLS 1.3 duplex close.
+                    useUserCanceled = true;
+                }
             }
         } else if (conContext.handshakeContext != null) {   // initial handshake
             // Use user_canceled alert regardless the protocol versions.
