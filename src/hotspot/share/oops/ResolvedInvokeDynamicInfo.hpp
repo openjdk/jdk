1fadce63491979ad7bab9c7289754d5ce2bffe6f
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2022-11-03 14:15:36 +0000

    fixed code formatting

--- src/hotspot/share/oops/ResolvedInvokeDynamicInfo.hpp
+++ src/hotspot/share/oops/ResolvedInvokeDynamicInfo.hpp
@@ -20,12 +20,12 @@ public:
         _return_type(0),
         _has_appendix(false) {}
     ResolvedInvokeDynamicInfo(u2 resolved_references_index, u2 cpool_index) :
-                _method(nullptr),
-                _resolved_references_index(resolved_references_index),
-                _cpool_index(cpool_index),
-                _number_of_parameters(0),
-                _return_type(0),
-                _has_appendix(false) {}
+        _method(nullptr),
+        _resolved_references_index(resolved_references_index),
+        _cpool_index(cpool_index),
+        _number_of_parameters(0),
+        _return_type(0),
+        _has_appendix(false) {}
     bool has_local_signature() const { return true; }
     bool is_final() const { return true; }
     bool is_resolved() const { return _method != nullptr; }
@@ -52,10 +52,10 @@ public:
     }
     void metaspace_pointers_do(MetaspaceClosure* it);
 
-    static ByteSize method_offset() { return byte_offset_of(ResolvedInvokeDynamicInfo, _method);                                       }
+    static ByteSize method_offset()                    { return byte_offset_of(ResolvedInvokeDynamicInfo, _method);                    }
     static ByteSize resolved_references_index_offset() { return byte_offset_of(ResolvedInvokeDynamicInfo, _resolved_references_index); }
-    static ByteSize result_type_offset() { return byte_offset_of(ResolvedInvokeDynamicInfo, _return_type);                             }
-    static ByteSize has_appendix_offset() { return byte_offset_of(ResolvedInvokeDynamicInfo, _has_appendix);                           }
+    static ByteSize result_type_offset()               { return byte_offset_of(ResolvedInvokeDynamicInfo, _return_type);               }
+    static ByteSize has_appendix_offset()              { return byte_offset_of(ResolvedInvokeDynamicInfo, _has_appendix);              }
 };
 
 #endif // SHARE_OOPS_RESOLVEDINVOKEDYNAMICINFO_HPP
