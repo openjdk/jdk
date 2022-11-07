42fbb1c8367d3050086fdf309b1147e97cdd4e5c
sendaoYan <yansendao.ysd@alibaba-inc.com>
2022-11-07 16:35:18 +0800

    8296384: [TESTBUG] sun/security/provider/SecureRandom/AbstractDrbg/SpecTest.java intermittently timeout
    Reviewed-by: Yi Yang
    Signed-off-by: sendaoYan <yansendao.ysd@alibaba-inc.com>

--- test/jdk/java/security/SecureRandom/NoSync.java
+++ test/jdk/java/security/SecureRandom/NoSync.java
@@ -30,7 +30,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 /*
  * @test
  * @bug 7004967
- * @run main/othervm NoSync
+ * @run main/othervm -Djava.security.egd=file:/dev/urandom NoSync
  * @summary SecureRandom should be more explicit about threading
  */
 public class NoSync {
