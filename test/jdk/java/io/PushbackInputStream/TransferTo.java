baaf1c6a1d729662247b238d79bf447abb35b61f
Markus Karg <markus@headcrashing.eu>
2022-11-09 07:07:21 +0000

    Removed unused imports

--- test/jdk/java/io/PushbackInputStream/TransferTo.java
+++ test/jdk/java/io/PushbackInputStream/TransferTo.java
@@ -21,28 +21,13 @@
  * questions.
  */
 
-import java.io.BufferedInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PushbackInputStream;
-import java.nio.ByteBuffer;
-import java.nio.channels.Channels;
-import java.nio.channels.FileChannel;
-import java.nio.channels.IllegalBlockingModeException;
-import java.nio.channels.Pipe;
-import java.nio.channels.ReadableByteChannel;
-import java.nio.channels.SelectableChannel;
-import java.nio.channels.WritableByteChannel;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
@@ -52,7 +37,6 @@ import org.testng.annotations.Test;
 import jdk.test.lib.RandomFactory;
 
 import static java.lang.String.format;
-import static java.nio.file.StandardOpenOption.*;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertThrows;
