19a0ba96b7f2dfce75655d7fd63c18051eec8ed6
Naveen Narayanan <naveen.n.narayanan@oracle.com>
2022-11-09 11:06:33 +0530

    8296275: Review comments fixed.

--- test/jdk/java/awt/Desktop/JMenuItemSetAcceleratorTest.java
+++ test/jdk/java/awt/Desktop/JMenuItemSetAcceleratorTest.java
@@ -1,3 +1,24 @@
+/* Copyright (c) 2016, 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 
 import java.awt.Desktop;
 import java.awt.Desktop.Action;
@@ -25,7 +46,8 @@ import javax.swing.JMenuItem;
 
 public class JMenuItemSetAcceleratorTest {
     private static JFrame frame = new JFrame();
-    private volatile static CountDownLatch actionPerformLatch;
+    private volatile static CountDownLatch actionPerformLatch =
+        new CountDownLatch(1);
     private static Robot robot;
 
     private static void createAndShow() {
@@ -42,7 +64,8 @@ public class JMenuItemSetAcceleratorTest {
         bar.add(menu);
 
         frame.setJMenuBar(bar);
-        frame.setBounds(200, 200, 200, 200);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
@@ -55,7 +78,6 @@ public class JMenuItemSetAcceleratorTest {
                 return;
             }
 
-            actionPerformLatch = new CountDownLatch(1);
             SwingUtilities
                 .invokeAndWait(JMenuItemSetAcceleratorTest::createAndShow);
 
@@ -83,7 +105,6 @@ public class JMenuItemSetAcceleratorTest {
     public static void disposeFrame() {
         if (frame != null) {
             frame.dispose();
-            frame = null;
         }
     }
 }
