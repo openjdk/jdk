b74d12158867e95077a007de73a1058f805be9fb
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2022-11-10 10:26:54 -0800

    Rename verify_edges() method and move it to Compile class

--- src/hotspot/share/opto/node.cpp
+++ src/hotspot/share/opto/node.cpp
@@ -2680,57 +2680,6 @@ void Node::dump_comp(const char* suffix, outputStream *st) const {
 }
 
 // VERIFICATION CODE
-// For each input edge to a node (ie - for each Use-Def edge), verify that
-// there is a corresponding Def-Use edge.
-//------------------------------verify_edges-----------------------------------
-void Node::verify_edges(Node* root, Unique_Node_List &visited, Node_List &nstack) {
-  nstack.push(root);
-
-  while (nstack.size() > 0) {
-    Node* next = nstack.pop();
-    if (visited.member(next)) {
-      continue;
-    }
-    visited.push(next);
-
-    // Walk over all input edges, checking for correspondence
-    uint length = next->len();
-    for (uint i = 0; i < length; i++) {
-      Node* n = next->in(i);
-      if (n != NULL && !visited.member(n)) {
-        nstack.push(n); // Put it on stack
-      }
-      if (n != NULL && !n->is_top()) {
-        // Count instances of `next`
-        int cnt = 0;
-        for (uint idx = 0; idx < n->_outcnt; idx++) {
-          if (n->_out[idx] == next) {
-            cnt++;
-          }
-        }
-        assert(cnt > 0, "Failed to find Def-Use edge.");
-        // Check for duplicate edges
-        // walk the input array downcounting the input edges to n
-        for(uint j = 0; j < length; j++) {
-          if (next->in(j) == n) {
-            cnt--;
-          }
-        }
-        assert(cnt == 0, "Mismatched edge count.");
-      } else if (n == NULL) {
-        assert(i >= next->req() || i == 0 ||
-               next->is_Region() || next->is_Phi() || next->is_ArrayCopy() ||
-               (next->is_Unlock() && i == (next->req() - 1)) ||
-               (next->is_MemBar() && i == 5), // the precedence edge to a membar can be removed during macro node expansion
-              "only region, phi, arraycopy, unlock or membar nodes have null data edges");
-      } else {
-        assert(n->is_top(), "sanity");
-        // Nothing to check.
-      }
-    }
-  }
-}
-
 // Verify all nodes if verify_depth is negative
 void Node::verify(int verify_depth, VectorSet& visited, Node_List& worklist) {
   assert(verify_depth != 0, "depth should not be 0");
