b986d35c471ec1691a4a6d6b0d4bb4c465aa33f6
kirillgarbar <st087492@student.spbu.ru>
2022-11-13 07:20:59 +0300

    Unused flags deleted

--- src/hotspot/share/gc/noop/noop_globals.hpp
+++ src/hotspot/share/gc/noop/noop_globals.hpp
@@ -38,28 +38,10 @@
                          range,                                             \
                          constraint)                                        \
                                                                             \
-  product(size_t, NoopPrintHeapSteps, 20, EXPERIMENTAL,                     \
-          "Print heap occupancy stats with this number of steps. "          \
-          "0 turns the printing off.")                                      \
-          range(0, max_intx)                                                \
-                                                                            \
-  product(size_t, NoopUpdateCountersStep, 1 * M, EXPERIMENTAL,              \
-          "Update heap occupancy counters after allocating this much "      \
-          "memory. Higher values would make allocations faster at "         \
-          "the expense of lower resolution in heap counters.")              \
-          range(1, max_intx)                                                \
-                                                                            \
   product(size_t, NoopMaxTLABSize, 4 * M, EXPERIMENTAL,                     \
           "Max TLAB size to use with Noop GC. Larger value improves "       \
           "performance at the expense of per-thread memory waste. This "    \
           "asks TLAB machinery to cap TLAB sizes at this value.")           \
-          range(1, max_intx)                                                \
-                                                                            \
-  product(size_t, NoopTLABDecayTime, 1000, EXPERIMENTAL,                    \
-          "TLAB sizing policy decays to initial size after thread had not " \
-          "allocated for this long. Time is in milliseconds. Lower value "  \
-          "improves memory footprint, but penalizes actively allocating "   \
-          "threads.")                                                       \
           range(1, max_intx)                                                \
                                                                             \
   product(size_t, NoopMinHeapExpand, 128 * M, EXPERIMENTAL,                 \
