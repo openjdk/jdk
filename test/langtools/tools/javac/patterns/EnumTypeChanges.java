1ccb11a5c0ab0a9e9ad9542591c392246dbdc01f
Jan Lahoda <jlahoda@openjdk.org>
2022-11-16 10:03:49 +0100

    8297118: Change IncompatibleClassChangeError to MatchException for exhaustive switch statements and switch expressions

--- test/langtools/tools/javac/patterns/EnumTypeChanges.java
+++ test/langtools/tools/javac/patterns/EnumTypeChanges.java
@@ -55,7 +55,7 @@ public class EnumTypeChanges {
         try {
             c.apply(EnumTypeChangesEnum.valueOf("C"));
             throw new AssertionError();
-        } catch (IncompatibleClassChangeError e) {
+        } catch (MatchException e) {
             //expected
         }
     }
