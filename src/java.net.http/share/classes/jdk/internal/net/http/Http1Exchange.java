8b1ff9e37efc42aeb05170463ec330c221ce1e4c
Daniel Fuchs <dfuchs@openjdk.org>
2022-11-16 13:13:54 +0000

    8297132: BACKOUT JDK-8296889: Race condition when cancelling a request
    Reviewed-by: djelinski, alanb

--- src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
@@ -209,11 +209,6 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
         }
 
         @Override
-        public void onSubscribed() {
-            exchange.registerResponseSubscriber(this);
-        }
-
-        @Override
         protected void complete(Throwable t) {
             try {
                 exchange.unregisterResponseSubscriber(this);
@@ -464,6 +459,7 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
         BodySubscriber<T> subscriber = handler.apply(response);
         Http1ResponseBodySubscriber<T> bs =
                 new Http1ResponseBodySubscriber<T>(subscriber, this);
+        registerResponseSubscriber(bs);
         return bs;
     }
 
