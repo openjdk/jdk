d02bfdf9d7de393b6c76d045b6cd079d7b62a89d
Tobias Holenstein <tholenstein@openjdk.org>
2022-11-17 13:04:07 +0000

    8297032: IGV: shortcut to center selected nodes
    Reviewed-by: chagedorn, rcastanedalo

--- src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/actions/CenterSelectedNodesAction.java
+++ src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/actions/CenterSelectedNodesAction.java
@@ -23,63 +23,51 @@
  */
 package com.sun.hotspot.igv.view.actions;
 
+import com.sun.hotspot.igv.view.DiagramViewModel;
 import com.sun.hotspot.igv.view.EditorTopComponent;
-import javax.swing.Action;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
-import org.openide.util.HelpCtx;
-import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
-import org.openide.util.actions.CallableSystemAction;
 
-@ActionID(category = "View", id = "com.sun.hotspot.igv.view.actions.ZoomResetAction")
-@ActionRegistration(displayName = "#CTL_ZoomResetAction")
+@ActionID(category = "View", id = "com.sun.hotspot.igv.view.actions.CenterSelectedNodesAction")
+@ActionRegistration(displayName = "#CTL_CenterSelectedNodesAction")
 @ActionReferences({
-        @ActionReference(path = "Menu/View", position = 550),
-        @ActionReference(path = "Shortcuts", name = "D-0")
+        @ActionReference(path = "Menu/View", position = 600),
+        @ActionReference(path = "Shortcuts", name = "D-9")
 })
 @NbBundle.Messages({
-        "CTL_ZoomResetAction=Reset zoom (100%)",
-        "HINT_ZoomResetAction=Reset the zoom level to 100%"
+        "CTL_CenterSelectedNodesAction=Center selected nodes",
+        "HINT_CenterSelectedNodesAction=Center the selected nodes"
 })
-public final class ZoomResetAction extends CallableSystemAction {
-
-    public ZoomResetAction() {
-        putValue(Action.SHORT_DESCRIPTION, getDescription());
-        putValue(Action.SMALL_ICON , ImageUtilities.loadImageIcon(iconResource(), true));
-    }
+public final class CenterSelectedNodesAction extends ModelAwareAction {
 
     @Override
-    public void performAction() {
-        EditorTopComponent editor = EditorTopComponent.getActive();
-        if (editor != null) {
-            editor.setZoomLevel(100);
-        }
+    protected String iconResource() {
+        return "com/sun/hotspot/igv/view/images/centerSelectedNodes.svg"; // NOI18N
     }
 
     @Override
     public String getName() {
-        return NbBundle.getMessage(NextDiagramAction.class, "CTL_ZoomResetAction");
-    }
-
-    private String getDescription() {
-        return NbBundle.getMessage(NextDiagramAction.class, "HINT_ZoomResetAction");
+        return NbBundle.getMessage(NextDiagramAction.class, "CTL_CenterSelectedNodesAction");
     }
 
     @Override
-    public HelpCtx getHelpCtx() {
-        return HelpCtx.DEFAULT_HELP;
+    protected String getDescription() {
+        return NbBundle.getMessage(NextDiagramAction.class, "HINT_CenterSelectedNodesAction");
     }
 
     @Override
-    protected boolean asynchronous() {
-        return false;
+    public void performAction(DiagramViewModel model) {
+        EditorTopComponent editor = EditorTopComponent.findEditorForGraph(model.getGraph());
+        if (editor != null) {
+            editor.centerSelectedNodes();
+        }
     }
 
     @Override
-    protected String iconResource() {
-        return "com/sun/hotspot/igv/view/images/zoomReset.svg"; // NOI18N
+    public boolean isEnabled(DiagramViewModel model) {
+        return !model.getSelectedNodes().isEmpty();
     }
 }
