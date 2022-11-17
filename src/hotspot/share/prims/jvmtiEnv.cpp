d0045d53c89f63622891e5517ea685a3126a39e5
Coleen Phillimore <coleenp@openjdk.org>
2022-11-17 11:58:00 +0000

    8296492: Remove ObjectLocker in JVMTI get_subgroups call
    Reviewed-by: dholmes, alanb, sspitsyn

--- src/hotspot/share/prims/jvmtiEnv.cpp
+++ src/hotspot/share/prims/jvmtiEnv.cpp
@@ -1670,7 +1670,7 @@ JvmtiEnv::GetThreadGroupChildren(jthreadGroup group, jint* thread_count_ptr, jth
   NULL_CHECK(group_obj, JVMTI_ERROR_INVALID_THREAD_GROUP);
 
   Handle *thread_objs = NULL;
-  Handle *group_objs  = NULL;
+  objArrayHandle group_objs;
   jint nthreads = 0;
   jint ngroups = 0;
   int hidden_threads = 0;
