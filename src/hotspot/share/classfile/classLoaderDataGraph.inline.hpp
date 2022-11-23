424e0f9d831279ba2d1986ebacb499f8d4a6c078
Stefan Johansson <stefan.johansson@oracle.com>
2022-11-23 16:09:47 +0100

    Missing include for minimal

--- src/hotspot/share/classfile/classLoaderDataGraph.inline.hpp
+++ src/hotspot/share/classfile/classLoaderDataGraph.inline.hpp
@@ -31,6 +31,7 @@
 #include "oops/oop.inline.hpp"
 #include "runtime/atomic.hpp"
 #include "runtime/handles.inline.hpp"
+#include "runtime/mutexLocker.hpp"
 #include "runtime/orderAccess.hpp"
 #include "runtime/safepoint.hpp"
 
