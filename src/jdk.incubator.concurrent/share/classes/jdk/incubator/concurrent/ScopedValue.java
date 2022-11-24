034b17fc0caf5ed98d89230a3b3e3e024aa88dde
Andrew Haley <aph-open@littlepinkcloud.com>
2022-11-24 10:31:08 +0000

    Update src/jdk.incubator.concurrent/share/classes/jdk/incubator/concurrent/ScopedValue.java
    Co-authored-by: Paul Sandoz <paul.d.sandoz@googlemail.com>

--- src/jdk.incubator.concurrent/share/classes/jdk/incubator/concurrent/ScopedValue.java
+++ src/jdk.incubator.concurrent/share/classes/jdk/incubator/concurrent/ScopedValue.java
@@ -461,7 +461,7 @@ public final class ScopedValue<T> {
     /**
      * Calls a value-returning operation with a {@code ScopedValue} bound to a value
      * in the current thread. When the operation completes (normally or with an
-     * exception), the {@code ScopedValue} will revert to being unbound, or rervert to
+     * exception), the {@code ScopedValue} will revert to being unbound, or revert to
      * its previous value when previously bound, in the current thread.
      *
      * <p> Scoped values are intended to be used in a <em>structured manner</em>.
