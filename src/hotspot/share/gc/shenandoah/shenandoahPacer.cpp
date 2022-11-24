d8d439f0b9118a486fc92796f40552188cb6fe06
Ashutosh Mehra <asmehra@redhat.com>
2022-11-24 16:05:04 -0500

    8297285: Shenandoah pacing causes assertion failure during VM initialization
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- src/hotspot/share/gc/shenandoah/shenandoahPacer.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahPacer.cpp
@@ -241,7 +241,13 @@ void ShenandoahPacer::pace_for_alloc(size_t words) {
   // Threads that are attaching should not block at all: they are not
   // fully initialized yet. Blocking them would be awkward.
   // This is probably the path that allocates the thread oop itself.
-  if (JavaThread::current()->is_attaching_via_jni()) {
+  //
+  // Thread which is not an active Java thread should also not block.
+  // This can happen during VM init when main thread is still not an
+  // active Java thread.
+  JavaThread* current = JavaThread::current();
+  if (current->is_attaching_via_jni() ||
+      current->is_active_Java_thread()) {
     return;
   }
 
