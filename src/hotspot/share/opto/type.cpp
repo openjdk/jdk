16495b45b3260ecc49543ea1d4b9921d9af5fd08
Roland Westrelin <roland@openjdk.org>
2022-11-28 12:27:14 +0000

    8297343: TestStress*.java fail with "got different traces for the same seed"
    Reviewed-by: thartmann, chagedorn

--- src/hotspot/share/opto/type.cpp
+++ src/hotspot/share/opto/type.cpp
@@ -3180,16 +3180,25 @@ void TypePtr::InterfaceSet::compute_hash() {
   _hash = hash;
 }
 
+static int compare_interfaces(ciKlass** k1, ciKlass** k2) {
+  return (int)((*k1)->ident() - (*k2)->ident());
+}
+
 void TypePtr::InterfaceSet::dump(outputStream *st) const {
   if (_list.length() == 0) {
     return;
   }
+  ResourceMark rm;
   st->print(" (");
-  for (int i = 0; i < _list.length(); i++) {
+  GrowableArray<ciKlass*> interfaces;
+  interfaces.appendAll(&_list);
+  // Sort the interfaces so they are listed in the same order from one run to the other of the same compilation
+  interfaces.sort(compare_interfaces);
+  for (int i = 0; i < interfaces.length(); i++) {
     if (i > 0) {
       st->print(",");
     }
-    ciKlass* k = _list.at(i);
+    ciKlass* k = interfaces.at(i);
     k->print_name_on(st);
   }
   st->print(")");
