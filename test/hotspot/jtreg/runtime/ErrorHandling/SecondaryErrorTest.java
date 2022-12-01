b24574034078807d2c416cbf2af0156f369c9eb3
tstuefe <thomas.stuefe@gmail.com>
2022-12-01 17:32:43 +0100

    Remove unused imports from test

--- test/hotspot/jtreg/runtime/ErrorHandling/SecondaryErrorTest.java
+++ test/hotspot/jtreg/runtime/ErrorHandling/SecondaryErrorTest.java
@@ -45,12 +45,8 @@
  * @run driver SecondaryErrorTest with_callstacks
  */
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.regex.Pattern;
 
 import jdk.test.lib.process.OutputAnalyzer;
