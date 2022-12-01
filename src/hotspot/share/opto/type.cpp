dda0a08c0ca392f934f5ff8422d7d6c2c336a0d9
rwestrel <rwestrel@redhat.com>
2022-12-01 11:31:30 +0100

    Revert "8297343: TestStress*.java fail with "got different traces for the same seed""
    This reverts commit eff4c039dab99aa946dbdde1be8901929ebbfc6f.

--- src/hotspot/share/opto/type.cpp
+++ src/hotspot/share/opto/type.cpp
@@ -3182,25 +3182,16 @@ void TypePtr::InterfaceSet::compute_hash() {
   _hash = hash;
 }
 
-static int compare_interfaces(ciKlass** k1, ciKlass** k2) {
-  return (int)((*k1)->ident() - (*k2)->ident());
-}
-
 void TypePtr::InterfaceSet::dump(outputStream *st) const {
   if (_list.length() == 0) {
     return;
   }
-  ResourceMark rm;
   st->print(" (");
-  GrowableArray<ciKlass*> interfaces;
-  interfaces.appendAll(&_list);
-  // Sort the interfaces so they are listed in the same order from one run to the other of the same compilation
-  interfaces.sort(compare_interfaces);
-  for (int i = 0; i < interfaces.length(); i++) {
+  for (int i = 0; i < _list.length(); i++) {
     if (i > 0) {
       st->print(",");
     }
-    ciKlass* k = interfaces.at(i);
+    ciKlass* k = _list.at(i);
     k->print_name_on(st);
   }
   st->print(")");
