d745f4371e4e2e90a6d49f551394426a7de383e6
Justin King <jcking@google.com>
2022-12-08 13:43:31 +0530

    Add comment to metaprogramming usage
    Signed-off-by: Justin King <jcking@google.com>

--- src/hotspot/share/memory/metadataFactory.hpp
+++ src/hotspot/share/memory/metadataFactory.hpp
@@ -72,11 +72,12 @@ class MetadataFactory : AllStatic {
       assert(!md->is_shared(), "cannot deallocate if in shared spaces");
       md->deallocate_contents(loader_data);
       bool is_klass = md->is_klass();
-      // If requested, call the destructor.
+      // If requested, call the destructor. This is currently used for MethodData which has a member
+      // that needs to be destructed to release resources. Most Metadata derived classes have noop
+      // destructors and/or cleanup using deallocate_contents.
       if (invoke_destructor) {
-        // This is currently used for MethodData which has a member that needs to be destructed to
-        // release resources. Most Metadata derived classes have noop destructors and/or cleanup
-        // using deallocate_contents.
+        // T is a potentially const or volatile qualified pointer. Remove the pointer and any const
+        // or volatile so we can call the destructor of the type T points to.
         using U = typename RemoveCV<typename RemovePointer<T>::type>::type;
         md->~U();
       }
