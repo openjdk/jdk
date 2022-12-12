42e66fdef7952ff570a95ca9303fead24810ffe8
Justin King <jcking@google.com>
2022-12-12 14:45:22 +0530

    Move __ubsan_default_options to be automagically included via SetupNativeCompilation
    Signed-off-by: Justin King <jcking@google.com>

---
+++ make/src/native/__ubsan_default_options.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Override weak symbol exposed by UBSan to override default options. This is called by UBSan
+// extremely early during library loading, before main is called. We need to override the default
+// options because by default UBSan only prints a warning for each occurrence. We want jtreg tests
+// to fail when undefined behavior is encountered. We also want a full stack trace for the offending
+// thread so it is easier to track down. You can override these options by setting the environment
+// variable UBSAN_OPTIONS.
+__attribute__((visibility("default"))) const char* __ubsan_default_options() {
+  return "halt_on_error=1,print_stacktrace=1";
+}
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
