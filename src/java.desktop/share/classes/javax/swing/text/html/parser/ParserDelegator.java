3a229786268eb7e9bc917a4514c742f0cd400e7c
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-12-12 03:30:18 +0000

    Remove space

--- src/java.desktop/share/classes/javax/swing/text/html/parser/ParserDelegator.java
+++ src/java.desktop/share/classes/javax/swing/text/html/parser/ParserDelegator.java
@@ -43,12 +43,12 @@ import java.security.PrivilegedAction;
  * each time its parse method is invoked. Stores a
  * reference to the dtd.
  * <strong>Warning:</strong>
- *  Serialized objects of this class will not be compatible with
- *  future Swing releases. The current serialization support is
- *  appropriate for short term storage or RMI between applications running
- *  the same version of Swing.  As of 1.4, support for long term storage
- *  of all JavaBeans
- *  has been added to the <code>java.beans</code> package
+ * Serialized objects of this class will not be compatible with
+ * future Swing releases. The current serialization support is
+ * appropriate for short term storage or RMI between applications running
+ * the same version of Swing.  As of 1.4, support for long term storage
+ * of all JavaBeans has been added to the <code>java.beans</code> package
+ *
  * @author  Sunita Mani
  */
 @SuppressWarnings("serial") // Same-version serialization only
