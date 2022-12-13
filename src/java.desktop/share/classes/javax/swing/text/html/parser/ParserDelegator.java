6ac67d2f751e86e1a77eecfbb373c3cf2a5ded36
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2022-12-13 03:28:39 +0000

    Add missing .

--- src/java.desktop/share/classes/javax/swing/text/html/parser/ParserDelegator.java
+++ src/java.desktop/share/classes/javax/swing/text/html/parser/ParserDelegator.java
@@ -47,7 +47,7 @@ import java.security.PrivilegedAction;
  * future Swing releases. The current serialization support is
  * appropriate for short term storage or RMI between applications running
  * the same version of Swing.  As of 1.4, support for long term storage
- * of all JavaBeans has been added to the <code>java.beans</code> package
+ * of all JavaBeans has been added to the <code>java.beans</code> package.
  *
  * @author  Sunita Mani
  */
