777d665a185f7557d3c172b6f67f9f8c55cc2289
Daniel Jelinski <daniel.jelinski@oracle.com>
2022-12-20 21:06:01 +0100

    Fix warnings

--- src/java.desktop/share/native/common/awt/debug/debug_mem.c
+++ src/java.desktop/share/native/common/awt/debug/debug_mem.c
@@ -68,10 +68,10 @@ typedef struct MemoryListLink {
 /**************************************************
  * Global Data structures
  */
-static DMemState                DMemGlobalState;
-extern const DMemState *        DMemStatePtr = &DMemGlobalState;
-static MemoryListLink           MemoryList = {NULL,NULL,FALSE};
-static dmutex_t                 DMemMutex = NULL;
+static DMemState         DMemGlobalState;
+const DMemState *        DMemStatePtr = &DMemGlobalState;
+static MemoryListLink    MemoryList = {NULL,NULL,FALSE};
+static dmutex_t          DMemMutex = NULL;
 
 /**************************************************/
 
@@ -271,10 +271,10 @@ Exit:
 }
 
 static void DMem_DumpHeader(MemoryBlockHeader * header) {
-    char        report[FILENAME_MAX+MAX_DECIMAL_DIGITS*3+1];
-    static const char * reportFormat =
+    char        report[FILENAME_MAX+MAX_DECIMAL_DIGITS*3+42];
+    static const char * const reportFormat =
         "file:  %s, line %d\n"
-        "size:  %d bytes\n"
+        "size:  %zd bytes\n"
         "order: %d\n"
         "-------";
 
