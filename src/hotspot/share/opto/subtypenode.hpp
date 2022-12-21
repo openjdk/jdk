0ed25bdd055d62714944582bbb5a04afe7ece189
rwestrel <rwestrel@redhat.com>
2022-12-21 15:40:06 +0100

    fix

--- src/hotspot/share/opto/subtypenode.hpp
+++ src/hotspot/share/opto/subtypenode.hpp
@@ -57,6 +57,8 @@ private:
 
   static bool is_oop(PhaseGVN* phase, Node* n);
 #endif // ASSERT
+
+  Node* load_klass(PhaseGVN* phase, Node* obj_or_subklass) const;
 };
 
 #endif // SHARE_OPTO_SUBTYPENODE_HPP
