33dc873ae0e4217d9f1e2fe1c8fae298108d0515
Justin King <jcking@google.com>
2023-01-03 04:09:25 +0530

    Remove include
    Signed-off-by: Justin King <jcking@google.com>

--- src/hotspot/share/runtime/atomic.hpp
+++ src/hotspot/share/runtime/atomic.hpp
@@ -33,7 +33,6 @@
 #include "metaprogramming/isSame.hpp"
 #include "metaprogramming/primitiveConversions.hpp"
 #include "metaprogramming/removeCV.hpp"
-#include "metaprogramming/removePointer.hpp"
 #include "runtime/orderAccess.hpp"
 #include "utilities/align.hpp"
 #include "utilities/bytes.hpp"
