b78389e8c1e23cb83338387ba20091bf705480e1
Alexey Ivanov <alexey.ivanov@oracle.com>
2023-01-03 19:40:38 +0000

    Drop the unused import: Component

--- test/jdk/javax/swing/border/LineBorder/ScaledLineBorderTest.java
+++ test/jdk/javax/swing/border/LineBorder/ScaledLineBorderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,6 @@
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
