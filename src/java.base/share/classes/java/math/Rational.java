d2cb7f45d3a5be883f12b1ca2197f2e96b6ab601
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2023-01-05 19:34:43 +0100

    Created Rational class

--- src/java.base/share/classes/java/math/Rational.java
+++ src/java.base/share/classes/java/math/Rational.java
@@ -22,3 +22,25 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
+ package java.math;
+
+public class Rational extends Number implements Comparable<Rational> {
+    /**
+     * The signum of this Rational: -1 for negative, 0 for zero, or
+     * 1 for positive.  Note that the Rational zero <em>must</em> have
+     * a signum of 0.  This is necessary to ensures that there is exactly one
+     * representation for each Rational value.
+     */
+    private final int signum;
+
+    /**
+     * The least non-negative numerator necessary to represent this Rational.
+     */
+    private final BigInteger numerator;
+
+    /**
+     * The least positive denominator necessary to represent this Rational.
+     */
+    private final BigInteger numerator;
+}
