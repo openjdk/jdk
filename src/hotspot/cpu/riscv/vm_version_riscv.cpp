271f6d691c66e3d06f0c68be9bcd48378c06d4d9
yadongw <yadonn.wang@huawei.com>
2023-01-06 17:31:51 +0800

    fix

--- src/hotspot/cpu/riscv/vm_version_riscv.cpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.cpp
@@ -59,6 +59,9 @@ void VM_Version::initialize() {
     if (FLAG_IS_DEFAULT(UseZbb)) {
       FLAG_SET_DEFAULT(UseZbb, true);
     }
+    if (FLAG_IS_DEFAULT(UseZbs)) {
+      FLAG_SET_DEFAULT(UseZbs, true);
+    }
     if (FLAG_IS_DEFAULT(UseZic64b)) {
       FLAG_SET_DEFAULT(UseZic64b, true);
     }
