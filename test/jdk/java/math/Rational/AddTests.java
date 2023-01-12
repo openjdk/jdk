729407cc2e506ceb8c35440f89169be284cc8184
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2023-01-13 00:52:46 +0100

    Update AddTests.java

--- test/jdk/java/math/Rational/AddTests.java
+++ test/jdk/java/math/Rational/AddTests.java
@@ -11,27 +11,17 @@ import java.util.EnumSet;
 
 public class AddTests {
 
-    private static Set<RoundingMode> nonExactRoundingModes =
-        EnumSet.complementOf(EnumSet.of(RoundingMode.UNNECESSARY));
-
     /**
-     * Test for some simple additions, particularly, it will test
-     * the overflow case.
+     * Test for some simple additions, particularly, it will test the overflow case.
      */
     private static int simpleTests() {
         int failures = 0;
 
-        Rational[] bd1 = {
-            new Rational("78124046.66936930160"),
-            new Rational("7812404.666936930160"),
-            new Rational("78124.04666936930160"),
-        };
+        Rational[] bd1 = { new Rational("78124046.66936930160"), new Rational("7812404.666936930160"),
+                new Rational("78124.04666936930160"), };
         Rational bd2 = new Rational("279000.0");
-        Rational[] expectedResult = {
-            new Rational("78403046.66936930160"),
-            new Rational("8091404.666936930160"),
-            new Rational("1060240.4666936930160"),
-        };
+        Rational[] expectedResult = { new Rational("78403046.66936930160"), new Rational("8091404.666936930160"),
+                new Rational("1060240.4666936930160"), };
         for (int i = 0; i < bd1.length; i++) {
             if (!bd1[i].add(bd2).equals(expectedResult[i]))
                 failures++;
@@ -40,250 +30,50 @@ public class AddTests {
     }
 
     /**
-     * Test for extreme value of scale and rounding precision that
-     * could cause integer overflow in right-shift-into-sticky-bit
-     * computations.
+     * Test for extreme value of scale that could cause
+     * integer overflow in right-shift-into-sticky-bit computations.
      */
     private static int extremaTests() {
         int failures = 0;
 
-        failures += addWithoutException(valueOf(1, -Integer.MAX_VALUE),
-                                        valueOf(2, Integer.MAX_VALUE), null);
-        failures += addWithoutException(valueOf(1, -Integer.MAX_VALUE),
-                                        valueOf(-2, Integer.MAX_VALUE), null);
+        failures += addWithoutException(new Rational(valueOf(1, -Integer.MAX_VALUE)),
+                new Rational(valueOf(2, Integer.MAX_VALUE)));
+        failures += addWithoutException(new Rational(valueOf(1, -Integer.MAX_VALUE)),
+                new Rational(valueOf(-2, Integer.MAX_VALUE)));
         return failures;
     }
 
     /**
-     * Print sum of b1 and b2; correct result will not throw an
-     * exception.
+     * Print sum of b1 and b2; correct result will not throw an exception.
      */
-    private static int addWithoutException(BigDecimal b1, BigDecimal b2, MathContext mc) {
-        if (mc == null)
-            mc = new MathContext(2, RoundingMode.DOWN);
-
+    private static int addWithoutException(Rational b1, Rational b2) {
         try {
-            BigDecimal sum = b1.add(b2, mc);
+            Rational sum = b1.add(b2);
             printAddition(b1, b2, sum.toString());
             return 0;
-        } catch(ArithmeticException ae) {
+        } catch (ArithmeticException ae) {
             printAddition(b1, b2, "Exception!");
             return 1;
         }
     }
 
-    /**
-     * Test combinations of operands that may meet the condensation
-     * criteria when rounded to different precisions.
-     */
-    private static int roundingGradationTests() {
-        int failures = 0;
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(   "1234e97"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(    "1234e96"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(     "1234e95"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(      "1234e94"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(       "1234e93"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal(        "1234e92"));
-
-        failures += roundAway(new BigDecimal("1234e100"),
-                              new BigDecimal("1234e50"));
-
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(   "1234e97"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(    "1234e96"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(     "1234e95"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(      "1234e94"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(       "1234e93"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal(        "1234e92"));
-
-        failures += roundAway(new BigDecimal("1000e100"),
-                              new BigDecimal("1234e50"));
-
-
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(   "1234e97"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(    "1234e96"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(     "1234e95"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(      "1234e94"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(       "1234e93"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal(        "1234e92"));
-
-        failures += roundAway(new BigDecimal("1999e100"),
-                              new BigDecimal("1234e50"));
-
-
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(   "1234e97"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(    "1234e96"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(     "1234e95"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(      "1234e94"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(       "1234e93"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal(        "1234e92"));
-
-        failures += roundAway(new BigDecimal("9999e100"),
-                              new BigDecimal("1234e50"));
-
-        return failures;
-    }
-
     private static void printAddition(BigDecimal b1, BigDecimal b2, String s) {
-        System.out.println("" + b1+ "\t+\t" + b2 + "\t=\t" + s);
+        System.out.println("" + b1 + "\t+\t" + b2 + "\t=\t" + s);
     }
 
-    private static int roundAway(BigDecimal b1, BigDecimal b2) {
+    private static int outOfMemoryErrorTest() {
         int failures = 0;
-
-        b1.precision();
-        b2.precision();
-
-        BigDecimal b1_negate = b1.negate();
-        BigDecimal b2_negate = b2.negate();
-
-        b1_negate.precision();
-        b2_negate.precision();
-
-        failures += roundAway1(b1,        b2);
-        failures += roundAway1(b1,        b2_negate);
-        failures += roundAway1(b1_negate, b2);
-        failures += roundAway1(b1_negate, b2_negate);
-
-        return failures;
-    }
-
-    private static int roundAway1(BigDecimal b1, BigDecimal b2) {
-        int failures = 0;
-        failures += roundAway0(b1, b2);
-        failures += roundAway0(b2, b1);
-        return failures;
-    }
-
-    /**
-     * Compare b1.add(b2, mc) with b1.add(b2).round(mc) for a variety
-     * of MathContexts.
-     */
-    private static int roundAway0(BigDecimal b1, BigDecimal b2) {
-        int failures = 0;
-        BigDecimal exactSum = b1.add(b2);
-
-        for(int precision = 1 ; precision < exactSum.precision()+2; precision++) {
-            for(RoundingMode rm : nonExactRoundingModes) {
-                MathContext mc = new MathContext(precision, rm);
-                BigDecimal roundedExactSum = exactSum.round(mc);
-
-                try {
-                    BigDecimal sum = b1.add(b2, mc);
-
-                    if (!roundedExactSum.equals(sum) ) {
-                        failures++;
-                        System.out.println("Exact sum " + exactSum +
-                                           "\trounded by " + mc +
-                                           "\texpected: " + roundedExactSum + " got: ");
-                        printAddition(b1, b2, sum.toString());
-                    }
-//                  else {
-//                      System.out.print(mc + "\t");
-//                      printAddition(b1, b2, sum.toString());
-//                  }
-
-                } catch (ArithmeticException ae) {
-                    printAddition(b1, b2, "Exception!");
-                    failures++;
-                }
-            }
-        }
-
-        return failures;
-    }
-
-    /**
-     * Verify calling the precision method should not change the
-     * computed result.
-     */
-    private static int precisionConsistencyTest() {
-        int failures = 0;
-        MathContext mc = new MathContext(1,RoundingMode.DOWN);
-        BigDecimal a = BigDecimal.valueOf(1999, -1); //value is equivalent to 19990
-
-        BigDecimal sum1 = a.add(BigDecimal.ONE, mc);
-        a.precision();
-        BigDecimal sum2 = a.add(BigDecimal.ONE, mc);
-
-        if (!sum1.equals(sum2)) {
-            failures ++;
-            System.out.println("Unequal sums after calling precision!");
-            System.out.print("Before:\t");
-            printAddition(a, BigDecimal.ONE, sum1.toString());
-
-            System.out.print("After:\t");
-            printAddition(a, BigDecimal.ONE, sum2.toString());
-        }
-
-        return failures;
-    }
-
-    private static int arithmeticExceptionTest() {
-        int failures = 0;
-        BigDecimal x;
+        Rational x;
         try {
             //
             // The string representation "1e2147483647", which is equivalent
-            // to 10^Integer.MAX_VALUE, is used to create an augend with an
-            // unscaled value of 1 and a scale of -Integer.MAX_VALUE. The
-            // addend "1" has an unscaled value of 1 with a scale of 0. The
-            // addition is performed exactly and is specified to have a
-            // preferred scale of max(-Integer.MAX_VALUE, 0). As the scale
-            // of the result is 0, a value with Integer.MAX_VALUE + 1 digits
-            // would need to be created. Therefore the next statement is
-            // expected to overflow with an ArithmeticException.
+            // to 10^Integer.MAX_VALUE, is used to create an augend too big
+            // to be represented into a Rational, so the program must throw
+            // an OutOfMemoryError
             //
-            x = new BigDecimal("1e2147483647").add(new BigDecimal(1));
+            x = new Rational("1e2147483647").add(new Rational(1));
             failures++;
-        } catch (ArithmeticException ae) {
+        } catch (OutOfMemoryError e) {
         }
         return failures;
     }
@@ -292,14 +82,11 @@ public class AddTests {
         int failures = 0;
 
         failures += simpleTests();
-//        failures += extremaTests();
-//        failures += roundingGradationTests();
-//        failures += precisionConsistencyTest();
-//        failures += arithmeticExceptionTest();
+        failures += extremaTests();
+        failures += outOfMemoryErrorTest();
 
         if (failures > 0) {
-            throw new RuntimeException("Incurred " + failures +
-                                       " failures while testing rounding add.");
+            throw new RuntimeException("Incurred " + failures + " failures while testing rounding add.");
         }
     }
 }
