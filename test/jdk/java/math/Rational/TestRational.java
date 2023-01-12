1210caed9881b1da9611e475ed7c2713f684f547
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2023-01-13 00:02:50 +0100

    Added test class for add

--- test/jdk/java/math/Rational/TestRational.java
+++
@@ -1,8 +0,0 @@
-import java.math.*;
-
-public class TestRational {
-    public static void main(String[] args) {
-        char[] in = {'1', '2', '3'};
-        //Rational x = new Rational(in, 0, in.length);
-    }
-}
