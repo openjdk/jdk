2fe819ba1fd929eaaa6cc31caee38e217006c3f0
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-01-12 11:37:18 -0800

    8299684: Miscellaneous cleanup of unit test

--- test/jdk/java/nio/jni/NewDirectByteBuffer.java
+++ test/jdk/java/nio/jni/NewDirectByteBuffer.java
@@ -23,16 +23,19 @@
 import java.nio.ByteBuffer;
 import java.util.concurrent.atomic.AtomicReference;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /*
  * @test
  * @bug 8299684
- * @summary Verify that JNI NewDirectByteBuffer throws
- * IllegalArgumentException if the capacity is negative or greater than
- * Integer::MAX_VALUE
+ * @summary Unit test for the JNI function NewDirectByteBuffer
  * @requires (sun.arch.data.model == "64" & os.maxMemory >= 8g)
  * @run junit/othervm/native NewDirectByteBuffer
  */
@@ -42,15 +45,53 @@ public class NewDirectByteBuffer {
     }
 
     private static final void checkBuffer(ByteBuffer buf, long capacity) {
-        Assertions.assertTrue(buf.isDirect(), "Buffer is not direct");
-        Assertions.assertEquals(capacity, getDirectByteBufferCapacity(buf),
+        // Verify that the JNI function returns the correct capacity
+        assertEquals(capacity, getDirectByteBufferCapacity(buf),
             "GetDirectBufferCapacity returned unexpected value");
-        Assertions.assertEquals(capacity, buf.capacity(),
+
+        // Verify that the initial state values are correct
+        assertTrue(buf.isDirect(), "Buffer is not direct");
+        assertFalse(buf.hasArray(), "Buffer has an array");
+        if (capacity > 0) {
+            assertTrue(buf.hasRemaining(), "Buffer has no remaining values");
+        }
+        assertFalse(buf.isReadOnly(), "Buffer s read-only");
+        assertEquals(capacity, buf.capacity(),
             "Buffer::capacity returned unexpected value");
-        Assertions.assertEquals(0L, buf.position(),
+        assertEquals(0L, buf.position(),
             "Buffer::position returned unexpected value");
-        Assertions.assertEquals(capacity, buf.limit(),
+        assertEquals(capacity, buf.limit(),
             "Buffer::limit returned unexpected value");
+
+        // Verify that the various state mutators work correctly
+        int halfPos = buf.capacity()/2;
+        buf.position(halfPos);
+        assertEquals(halfPos, buf.position(),
+            "Position not set to halfPos");
+        assertEquals(buf.capacity() - halfPos, buf.remaining(),
+            "Remaining not capacity - halfPos");
+
+        buf.mark();
+
+        int twoThirdsPos = 2*(buf.capacity()/3);
+        buf.position(twoThirdsPos);
+        assertEquals(twoThirdsPos, buf.position(),
+            "Position not set to twoThirdsPos");
+        assertEquals(buf.capacity() - twoThirdsPos, buf.remaining(),
+            "Remaining != capacity - twoThirdsPos");
+
+        buf.reset();
+        assertEquals(halfPos, buf.position(),
+            "Buffer not reset to halfPos");
+
+        buf.limit(twoThirdsPos);
+        assertEquals(twoThirdsPos, buf.limit(),
+            "Limit not set to twoThirdsPos");
+        assertEquals(twoThirdsPos - halfPos, buf.remaining(),
+            "Remaining != twoThirdsPos - halfPos");
+
+        buf.position(twoThirdsPos);
+        assertFalse(buf.hasRemaining(), "Buffer has remaining values");
     }
 
     @ParameterizedTest
@@ -58,20 +99,11 @@ public class NewDirectByteBuffer {
         (long)Integer.MAX_VALUE - 1, (long)Integer.MAX_VALUE})
     void legalCapacities(long capacity) {
         try {
-            final AtomicReference<ByteBuffer> bufHolder = new AtomicReference();
-            Assertions.assertDoesNotThrow(() -> {
-                try {
-                    bufHolder.set(newDirectByteBuffer(capacity));
-                } finally {
-                    ByteBuffer buf = bufHolder.get();
-                    if (buf != null) {
-                        freeDirectByteBufferMemory(buf);
-                    }
-                }
-            });
-            ByteBuffer buf = bufHolder.get();
-            if (buf != null) {
+            ByteBuffer buf = newDirectByteBuffer(capacity);
+            try {
                 checkBuffer(buf, capacity);
+            } finally {
+                freeDirectByteBufferMemory(buf);
             }
         } catch (OutOfMemoryError ignored) {
             // Ignore the error
@@ -84,7 +116,7 @@ public class NewDirectByteBuffer {
         Long.MAX_VALUE})
     void illegalCapacities(long capacity) {
         try {
-            Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            assertThrows(IllegalArgumentException.class, () -> {
                 ByteBuffer buf = newDirectByteBuffer(capacity);
                 if (buf != null) {
                     freeDirectByteBufferMemory(buf);
