eed9e967c3001b6b2a91be49aec2d8d55b36613e
MBaesken <matthias.baesken@sap.com>
2023-01-17 09:14:02 +0100

    adjust COPYRIGHT, shorten line

--- test/jdk/javax/swing/text/html/parser/Parser/8078268/bug8078268.java
+++ test/jdk/javax/swing/text/html/parser/Parser/8078268/bug8078268.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2016, 2022, Oracle and/or its affiliates. All rights reserved.
+* Copyright (c) 2016, 2023, Oracle and/or its affiliates. All rights reserved.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 *
 * This code is free software; you can redistribute it and/or modify it
@@ -38,8 +38,8 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
    @run main bug8078268
 */
 public class bug8078268 {
-    private static final float timeoutFactor = Float.parseFloat(System.getProperty("test.timeout.factor", "1.0"));
-    private static final long TIMEOUT = 10_000 * (long)timeoutFactor;
+    private static final float tf = Float.parseFloat(System.getProperty("test.timeout.factor", "1.0"));
+    private static final long TIMEOUT = 10_000 * (long)tf;
 
     private static final String FILENAME = "slowparse.html";
 
