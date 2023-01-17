366a15f3cb427e4434305fcfbd3ecf1ffe7930d4
Justin Lu <justin.lu@oracle.com>
2023-01-17 15:07:36 -0800

    Switch to snippet

--- src/java.base/share/classes/java/text/CollationElementIterator.java
+++ src/java.base/share/classes/java/text/CollationElementIterator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -76,8 +76,7 @@ import jdk.internal.icu.text.NormalizerBase;
  * <p>
  * Example of the iterator usage,
  * <blockquote>
- * <pre>
- *
+ * {@snippet lang=java :
  *  String testString = "This is a test";
  *  Collator col = Collator.getInstance();
  *  if (col instanceof RuleBasedCollator) {
@@ -86,7 +85,7 @@ import jdk.internal.icu.text.NormalizerBase;
  *      int primaryOrder = CollationElementIterator.primaryOrder(collationElementIterator.next());
  *          :
  *  }
- * </pre>
+ * }
  * </blockquote>
  *
  * <p>
