e7e371212163ba6a56a9a365c662da3fa1a0fe47
Naoto Sato <naoto@openjdk.org>
2023-01-17 17:25:44 +0000

    8300010: UnsatisfiedLinkError on calling System.console().readPassword() on Windows
    Reviewed-by: alanb

--- src/java.base/windows/native/libjava/JdkConsoleImpl_md.c
+++ src/java.base/windows/native/libjava/JdkConsoleImpl_md.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,40 +26,30 @@
 #include "jni.h"
 #include "jni_util.h"
 #include "jvm.h"
-#include "java_io_Console.h"
+#include "jdk_internal_io_JdkConsoleImpl.h"
 
 #include <stdlib.h>
 #include <Wincon.h>
 
 JNIEXPORT jboolean JNICALL
-Java_java_io_Console_istty(JNIEnv *env, jclass cls)
+Java_jdk_internal_io_JdkConsoleImpl_echo(JNIEnv *env, jclass cls, jboolean on)
 {
-    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD fdwMode;
+    jboolean old;
     HANDLE hStdIn = GetStdHandle(STD_INPUT_HANDLE);
 
-    if (hStdIn == INVALID_HANDLE_VALUE ||
-        hStdOut == INVALID_HANDLE_VALUE) {
-        return JNI_FALSE;
+    if (! GetConsoleMode(hStdIn, &fdwMode)) {
+        JNU_ThrowIOExceptionWithLastError(env, "GetConsoleMode failed");
+        return !on;
     }
-
-    if (GetFileType(hStdIn) != FILE_TYPE_CHAR ||
-        GetFileType(hStdOut) != FILE_TYPE_CHAR) {
-        return JNI_FALSE;
+    old = (fdwMode & ENABLE_ECHO_INPUT) != 0;
+    if (on) {
+        fdwMode |= ENABLE_ECHO_INPUT;
+    } else {
+        fdwMode &= ~ENABLE_ECHO_INPUT;
     }
-
-    return JNI_TRUE;
-}
-
-JNIEXPORT jstring JNICALL
-Java_java_io_Console_encoding(JNIEnv *env, jclass cls)
-{
-    char buf[64];
-    int cp = GetConsoleCP();
-    if (cp >= 874 && cp <= 950)
-        sprintf(buf, "ms%d", cp);
-    else if (cp == 65001)
-        sprintf(buf, "UTF-8");
-    else
-        sprintf(buf, "cp%d", cp);
-    return JNU_NewStringPlatform(env, buf);
+    if (! SetConsoleMode(hStdIn, fdwMode)) {
+        JNU_ThrowIOExceptionWithLastError(env, "SetConsoleMode failed");
+    }
+    return old;
 }
