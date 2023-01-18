0cd4cc4ae52ec218199a7470f09dc86374ab1997
Eirik Bjorsnos <eirbjo@gmail.com>
2023-01-18 10:57:18 +0100

    Renaming the test to IgnoreUnrelatedSignatureFiles to make it clear that unrelated files should be ignored.

--- test/jdk/java/util/jar/JarFile/IgnoreUnrelatedSignatureFiles.java
+++ test/jdk/java/util/jar/JarFile/IgnoreUnrelatedSignatureFiles.java
@@ -59,7 +59,7 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipOutputStream;
 
-public class VerifyUnrelatedSignatureFiles {
+public class IgnoreUnrelatedSignatureFiles {
 
     private static final JavaUtilZipFileAccess JUZA = SharedSecrets.getJavaUtilZipFileAccess();
 
