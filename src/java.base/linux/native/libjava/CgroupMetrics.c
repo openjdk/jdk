346ddc84f2184605a7f074a5f02177f44c3b6779
Severin Gehwolf <sgehwolf@redhat.com>
2023-01-19 17:41:39 +0100

    8299858: [Metrics] Swap memory limit reported incorrectly when too large

--- src/java.base/linux/native/libjava/CgroupMetrics.c
+++ src/java.base/linux/native/libjava/CgroupMetrics.c
@@ -23,6 +23,7 @@
  * questions.
  */
 #include <unistd.h>
+#include <sys/sysinfo.h>
 
 #include "jni.h"
 #include "jvm.h"
@@ -42,4 +43,13 @@ Java_jdk_internal_platform_CgroupMetrics_getTotalMemorySize0
     jlong pages = sysconf(_SC_PHYS_PAGES);
     jlong page_size = sysconf(_SC_PAGESIZE);
     return pages * page_size;
+}
+
+JNIEXPORT jlong JNICALL
+Java_jdk_internal_platform_CgroupMetrics_getTotalSwapSize0
+  (JNIEnv *env, jclass ignored)
+{
+    struct sysinfo si;
+    sysinfo(&si);
+    return (jlong)si.totalswap;
 }
