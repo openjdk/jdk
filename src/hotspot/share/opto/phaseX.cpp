e6ddee560ba780bf9223f5476aa0488eea1b7930
Emanuel Peter <emanuel.peter@oracle.com>
2023-01-19 17:06:57 +0100

    sanity assert from last commit was wrong - turned it into actual logic

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -1682,7 +1682,6 @@ void PhaseIterGVN::add_users_to_worklist( Node *n ) {
         Node* cmp = use;
         Node* in1 = cmp->in(1);
         Node* in2 = cmp->in(2);
-        assert(in1 != in2, "sanity");
         // Notify CmpI / If pattern from CastIINode::Value (left pattern).
         // Must also notify if in1 is modified and possibly turns into X (right pattern).
         //
@@ -1700,22 +1699,24 @@ void PhaseIterGVN::add_users_to_worklist( Node *n ) {
         //    |      |                   |      |
         //   CastIINode                 CastIINode
         //
-        if (in1 == n) {
-          // in1 modified -> could turn into X -> do traversal basen on right pattern.
-          for (uint i2 = 0; i2 < cmp->outcnt(); i2++) {
-            Node* bol = cmp->raw_out(i2); // For each Bool
-            if(bol != nullptr && bol->is_Bool()) {
-              for (uint i3 = 0; i3 < bol->outcnt(); i3++) {
-                Node* iff = bol->raw_out(i3); // For each If
-                if(iff != nullptr && iff->is_If()) {
-                  for (uint i4 = 0; i4 < iff->outcnt(); i4++) {
-                    Node* if_proj = iff->raw_out(i4); // For each IfProj
-                    if(if_proj != nullptr && (if_proj->is_IfFalse() || if_proj->is_IfTrue())) {
-                      for (uint i5 = 0; i5 < if_proj->outcnt(); i5++) {
-                        Node* castii = if_proj->raw_out(i5); // For each CastII
-                        if (castii->is_CastII() &&
-                            castii->as_CastII()->carry_dependency()) {
-                          _worklist.push(castii);
+        if (in1 != in2) { // if they are equal, the CmpI can fold them away
+          if (in1 == n) {
+            // in1 modified -> could turn into X -> do traversal basen on right pattern.
+            for (uint i2 = 0; i2 < cmp->outcnt(); i2++) {
+              Node* bol = cmp->raw_out(i2); // For each Bool
+              if(bol != nullptr && bol->is_Bool()) {
+                for (uint i3 = 0; i3 < bol->outcnt(); i3++) {
+                  Node* iff = bol->raw_out(i3); // For each If
+                  if(iff != nullptr && iff->is_If()) {
+                    for (uint i4 = 0; i4 < iff->outcnt(); i4++) {
+                      Node* if_proj = iff->raw_out(i4); // For each IfProj
+                      if(if_proj != nullptr && (if_proj->is_IfFalse() || if_proj->is_IfTrue())) {
+                        for (uint i5 = 0; i5 < if_proj->outcnt(); i5++) {
+                          Node* castii = if_proj->raw_out(i5); // For each CastII
+                          if (castii->is_CastII() &&
+                              castii->as_CastII()->carry_dependency()) {
+                            _worklist.push(castii);
+                          }
                         }
                       }
                     }
@@ -1723,20 +1724,20 @@ void PhaseIterGVN::add_users_to_worklist( Node *n ) {
                 }
               }
             }
-          }
-        } else {
-          // Only in2 modified -> can assume X == in2 (left pattern).
-          assert(n == in2, "only in2 modified");
-          // Find all CastII with input in1.
-          for (uint j = 0; j < in1->outcnt(); j++) {
-            Node* castii = in1->raw_out(j);
-            if (castii->is_CastII() && castii->as_CastII()->carry_dependency()) {
-              // Find If.
-              if (castii->in(0) != nullptr && castii->in(0)->in(0) != nullptr && castii->in(0)->in(0)->is_If()) {
-                Node* ifnode = castii->in(0)->in(0);
-                // Check that if connects to the cmp
-                if (ifnode->in(1) != nullptr && ifnode->in(1)->is_Bool() && ifnode->in(1)->in(1) == cmp) {
-                  _worklist.push(castii);
+          } else {
+            // Only in2 modified -> can assume X == in2 (left pattern).
+            assert(n == in2, "only in2 modified");
+            // Find all CastII with input in1.
+            for (uint j = 0; j < in1->outcnt(); j++) {
+              Node* castii = in1->raw_out(j);
+              if (castii->is_CastII() && castii->as_CastII()->carry_dependency()) {
+                // Find If.
+                if (castii->in(0) != nullptr && castii->in(0)->in(0) != nullptr && castii->in(0)->in(0)->is_If()) {
+                  Node* ifnode = castii->in(0)->in(0);
+                  // Check that if connects to the cmp
+                  if (ifnode->in(1) != nullptr && ifnode->in(1)->is_Bool() && ifnode->in(1)->in(1) == cmp) {
+                    _worklist.push(castii);
+                  }
                 }
               }
             }
