873938c8c0d94b21598850b163487adf10a496ea
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2023-01-23 07:11:20 +0000

    Fix Skynet.java test config after rebase

--- test/jdk/java/lang/Thread/virtual/stress/Skynet.java
+++ test/jdk/java/lang/Thread/virtual/stress/Skynet.java
@@ -35,7 +35,7 @@
  * @requires vm.gc.Z
  * @enablePreview
  * @run main/othervm/timeout=300 -XX:+UnlockDiagnosticVMOptions
- *     -XX:+ZVerifyViews -XX:ZCollectionInterval=0.01 -Xmx1g Skynet
+ *     -XX:+ZVerifyOops -XX:ZCollectionInterval=0.01 -Xmx1g Skynet
  */
 
 import java.util.concurrent.BlockingQueue;
