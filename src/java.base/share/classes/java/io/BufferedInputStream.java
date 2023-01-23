aa03cbc2be6dff7f8dbcfd440f72c9949f3b7e0e
Per Minborg <pminborg@gmail.com>
2023-01-23 15:16:14 +0100

    Declare fields final

--- src/java.base/share/classes/java/io/BufferedInputStream.java
+++ src/java.base/share/classes/java/io/BufferedInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -55,7 +55,7 @@ import jdk.internal.util.ArraysSupport;
  */
 public class BufferedInputStream extends FilterInputStream {
 
-    private static int DEFAULT_BUFFER_SIZE = 8192;
+    private static final int DEFAULT_BUFFER_SIZE = 8192;
 
     /**
      * As this class is used early during bootstrap, it's motivated to use
