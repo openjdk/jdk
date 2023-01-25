56e5c23545e16f72826aed929a6509b7cbd37079
Justin King <jcking@google.com>
2023-01-25 07:45:38 -0800

    Consolidate more byteswap implementations and upgrade bit reversal
    Signed-off-by: Justin King <jcking@google.com>

--- src/hotspot/share/utilities/moveBits.hpp
+++ src/hotspot/share/utilities/moveBits.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Google and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,74 +27,156 @@
 #define SHARE_UTILITIES_MOVEBITS_HPP
 
 #include "metaprogramming/enableIf.hpp"
+#include "utilities/byteswap.hpp"
+#include "utilities/debug.hpp"
 #include "utilities/globalDefinitions.hpp"
 
+#include <cstdint>
 #include <type_traits>
 
-template <typename T>
-class ReverseBitsImpl {
-  static const size_t NB = sizeof(T) * BitsPerByte;
+// T reverse_bits<T>(T)
+//
+// Reverses the bits in the integral value of type T.
 
-  static_assert((NB == 8) || (NB == 16) || (NB == 32) || (NB == 64),
-                "unsupported size");
+template <typename T>
+using CanReverseBitsImpl = std::integral_constant<bool, (std::is_integral<T>::value &&
+                                                         (sizeof(T) == 1 ||
+                                                          sizeof(T) == 2 ||
+                                                          sizeof(T) == 4 ||
+                                                          sizeof(T) == 8))>;
+
+template <typename T, size_t N = sizeof(T)>
+struct ReverseBitsImpl;
+
+template <typename T, ENABLE_IF(CanReverseBitsImpl<T>::value)>
+ALWAYSINLINE T reverse_bits(T x) {
+  return ReverseBitsImpl<T>{}(x);
+}
+
+/*****************************************************************************
+ * Fallback
+ *****************************************************************************/
+
+template <typename T>
+struct ReverseBitsFallbackImpl {
+ private:
+  static constexpr size_t NB = sizeof(T) * BitsPerByte;
 
   // The unsigned integral type for calculations.
   using I = std::conditional_t<NB <= 32, uint32_t, uint64_t>;
 
-  static const I rep_5555 = static_cast<I>(UCONST64(0x5555555555555555));
-  static const I rep_3333 = static_cast<I>(UCONST64(0x3333333333333333));
-  static const I rep_0F0F = static_cast<I>(UCONST64(0x0F0F0F0F0F0F0F0F));
-  static const I rep_00FF = static_cast<I>(UCONST64(0x00FF00FF00FF00FF));
-  static const I rep_FFFF = static_cast<I>(UCONST64(0x0000FFFF0000FFFF));
+  static constexpr I rep_5555 = static_cast<I>(UINT64_C(0x5555555555555555));
+  static constexpr I rep_3333 = static_cast<I>(UINT64_C(0x3333333333333333));
+  static constexpr I rep_0F0F = static_cast<I>(UINT64_C(0x0F0F0F0F0F0F0F0F));
 
-public:
+ public:
+  STATIC_ASSERT(CanReverseBitsImpl<T>::value);
 
-  static constexpr T reverse_bits_in_bytes(T v) {
+  ALWAYSINLINE T operator()(T v) const {
     // Based on Hacker's Delight Section 7-1
-    auto x = static_cast<I>(v);
+    I x = static_cast<I>(v);
     x = ((x & rep_5555) << 1) | ((x >> 1) & rep_5555);
     x = ((x & rep_3333) << 2) | ((x >> 2) & rep_3333);
     x = ((x & rep_0F0F) << 4) | ((x >> 4) & rep_0F0F);
-    return static_cast<T>(x);
-  }
-
-  static constexpr T reverse_bytes(T v) {
-    // Based on Hacker's Delight Section 7-1
-    // NB: Compilers are good at recognizing byte-swap code and transforming
-    // it into platform-specific instructions like x86 bswap.
-    auto x = static_cast<I>(v);
-    switch (NB) {
-    case 64:
-      // The use of NB/2 rather than 32 avoids a warning in dead code when
-      // I is uint32_t, because shifting a 32bit type by 32 is UB.
-      x = (x << (NB/2)) | (x >> (NB/2));
-    case 32:                    // fallthrough
-      x = ((x & rep_FFFF) << 16) | ((x >> 16) & rep_FFFF);
-    case 16:                    // fallthrough
-      x = ((x & rep_00FF) << 8)  | ((x >> 8)  & rep_00FF);
-    default:                    // fallthrough
-      return static_cast<T>(x);
-    }
+    return byteswap<T>(static_cast<T>(x));
   }
 };
 
-// Performs byte reversal of an integral type up to 64 bits.
-template <typename T, ENABLE_IF(std::is_integral<T>::value)>
-constexpr T reverse_bytes(T x) {
-  return ReverseBitsImpl<T>::reverse_bytes(x);
-}
 
-// Performs bytewise bit reversal of each byte of an integral
-// type up to 64 bits.
-template <typename T, ENABLE_IF(std::is_integral<T>::value)>
-constexpr T reverse_bits_in_bytes(T x) {
-  return ReverseBitsImpl<T>::reverse_bits_in_bytes(x);
-}
+/*****************************************************************************
+ * GCC and compatible (including Clang)
+ *****************************************************************************/
+#if defined(TARGET_COMPILER_gcc)
 
-// Performs full bit reversal an integral type up to 64 bits.
-template <typename T, ENABLE_IF(std::is_integral<T>::value)>
-constexpr T reverse_bits(T x) {
-  return reverse_bytes(reverse_bits_in_bytes(x));
-}
+// Default implementation for GCC-like compilers is the fallback. At the time of writing GCC does
+// not have intrinsics for bit reversal while Clang does.
+
+template <typename T, size_t N>
+struct ReverseBitsImpl final : public ReverseBitsFallbackImpl<T> {};
+
+#ifdef __has_builtin
+
+#if __has_builtin(__builtin_bitreverse8)
+
+template <typename T>
+struct ReverseBitsImpl<T, 1> final {
+  STATIC_ASSERT(CanReverseBitsImpl<T>::value);
+  STATIC_ASSERT(sizeof(T) == 1);
+
+  ALWAYSINLINE T operator()(T v) const {
+    return static_cast<T>(__builtin_bitreverse8(static_cast<uint8_t>(v)));
+  }
+};
+
+#endif // __has_builtin(__builtin_bitreverse8)
+
+#if __has_builtin(__builtin_bitreverse16)
+
+template <typename T>
+struct ReverseBitsImpl<T, 2> final {
+  STATIC_ASSERT(CanReverseBitsImpl<T>::value);
+  STATIC_ASSERT(sizeof(T) == 2);
+
+  ALWAYSINLINE T operator()(T v) const {
+    return static_cast<T>(__builtin_bitreverse16(static_cast<uint16_t>(v)));
+  }
+};
+
+#endif // __has_builtin(__builtin_bitreverse16)
+
+#if __has_builtin(__builtin_bitreverse32)
+
+template <typename T>
+struct ReverseBitsImpl<T, 4> final {
+  STATIC_ASSERT(CanReverseBitsImpl<T>::value);
+  STATIC_ASSERT(sizeof(T) == 4);
+
+  ALWAYSINLINE T operator()(T v) const {
+    return static_cast<T>(__builtin_bitreverse32(static_cast<uint32_t>(v)));
+  }
+};
+
+#endif // __has_builtin(__builtin_bitreverse32)
+
+#if __has_builtin(__builtin_bitreverse64)
+
+template <typename T>
+struct ReverseBitsImpl<T, 8> final {
+  STATIC_ASSERT(CanReverseBitsImpl<T>::value);
+  STATIC_ASSERT(sizeof(T) == 8);
+
+  ALWAYSINLINE T operator()(T v) const {
+    return static_cast<T>(__builtin_bitreverse64(static_cast<uint64_t>(v)));
+  }
+};
+
+#endif // __has_builtin(__builtin_bitreverse64)
+
+#endif // __has_builtin
+
+/*****************************************************************************
+ * Microsoft Visual Studio
+ *****************************************************************************/
+#elif defined(TARGET_COMPILER_visCPP)
+
+template <typename T, size_t N>
+struct ReverseBitsImpl final : public ReverseBitsFallbackImpl<T> {};
+
+/*****************************************************************************
+ * IBM XL C/C++
+ *****************************************************************************/
+#elif defined(TARGET_COMPILER_xlc)
+
+template <typename T, size_t N>
+struct ReverseBitsImpl final : public ReverseBitsFallbackImpl<T> {};
+
+/*****************************************************************************
+ * Unknown toolchain
+ *****************************************************************************/
+#else
+
+#error Unknown toolchain.
+
+#endif
 
 #endif // SHARE_UTILITIES_MOVEBITS_HPP
--- test/hotspot/gtest/opto/test_moveBits.cpp
+++ test/hotspot/gtest/opto/test_moveBits.cpp
@@ -23,45 +23,47 @@
  */
 
 #include "precompiled.hpp"
+#include "utilities/byteswap.hpp"
 #include "utilities/globalDefinitions.hpp"
 #include "utilities/moveBits.hpp"
 #include "unittest.hpp"
 
-template<typename T>
-inline void test_moveBits() {
-  const int  NBIT = sizeof(T) * 8;
-  const bool IS_U = (T)-1 > 0;
-  const int XOR_REV_BITS = (NBIT - 1);
-  const int XOR_REV_BITS_IN_BYTES = 7;  // only flip position in byte
-  const int XOR_REV_BYTES = XOR_REV_BITS ^ XOR_REV_BITS_IN_BYTES;
-  printf("testing %sint%d_t...\n", IS_U ? "u" : "", NBIT);
-  ASSERT_EQ(reverse_bits((T)0), (T)0);
-  ASSERT_EQ(reverse_bits((T)-1), (T)-1);
-  ASSERT_EQ(reverse_bytes((T)0), (T)0);
-  ASSERT_EQ(reverse_bytes((T)-1), (T)-1);
-  ASSERT_EQ(reverse_bits_in_bytes((T)0), (T)0);
-  ASSERT_EQ(reverse_bits_in_bytes((T)-1), (T)-1);
-  for (int i1 = 0; i1 < NBIT; i1++) {
-    T mask1 = (T)1 << i1;
-    T revm1 = (T)1 << (i1 ^ XOR_REV_BITS);
-    T rbym1 = (T)1 << (i1 ^ XOR_REV_BYTES);
-    T ribm1 = (T)1 << (i1 ^ XOR_REV_BITS_IN_BYTES);
-    for (int i2 = 0; i2 <= i1; i2++) {
-      T mask2 = (T)1 << i2;
-      T revm2 = (T)1 << (i2 ^ XOR_REV_BITS);
-      T rbym2 = (T)1 << (i2 ^ XOR_REV_BYTES);
-      T ribm2 = (T)1 << (i2 ^ XOR_REV_BITS_IN_BYTES);
-      T mask = mask1|mask2;
-#define STUFF (IS_U?"u":"s") << NBIT << "@" << i1 << "," << i2
-      ASSERT_EQ(reverse_bits(mask), revm1|revm2) << STUFF;
-      ASSERT_EQ((T)~reverse_bits((T)~mask), revm1|revm2) << STUFF;
-      ASSERT_EQ(reverse_bytes(mask), rbym1|rbym2) << STUFF;
-      ASSERT_EQ((T)~reverse_bytes((T)~mask), rbym1|rbym2) << STUFF;
-      ASSERT_EQ(reverse_bits_in_bytes(mask), ribm1|ribm2) << STUFF;
-      ASSERT_EQ((T)~reverse_bits_in_bytes((T)~mask), ribm1|ribm2) << STUFF;
+class MoveBitsTest final {
+ public:
+  template<typename T>
+  static inline void test_moveBits() {
+    using ReverseBitsImpl<T>::reverse_bytes;
+
+    const int  NBIT = sizeof(T) * 8;
+    const bool IS_U = (T)-1 > 0;
+    const int XOR_REV_BITS = (NBIT - 1);
+    const int XOR_REV_BITS_IN_BYTES = 7;  // only flip position in byte
+    const int XOR_REV_BYTES = XOR_REV_BITS ^ XOR_REV_BITS_IN_BYTES;
+    printf("testing %sint%d_t...\n", IS_U ? "u" : "", NBIT);
+    ASSERT_EQ(reverse_bits((T)0), (T)0);
+    ASSERT_EQ(reverse_bits((T)-1), (T)-1);
+    ASSERT_EQ(byteswap<T>((T)0), (T)0);
+    ASSERT_EQ(byteswap<T>((T)-1), (T)-1);
+    for (int i1 = 0; i1 < NBIT; i1++) {
+      T mask1 = (T)1 << i1;
+      T revm1 = (T)1 << (i1 ^ XOR_REV_BITS);
+      T rbym1 = (T)1 << (i1 ^ XOR_REV_BYTES);
+      T ribm1 = (T)1 << (i1 ^ XOR_REV_BITS_IN_BYTES);
+      for (int i2 = 0; i2 <= i1; i2++) {
+        T mask2 = (T)1 << i2;
+        T revm2 = (T)1 << (i2 ^ XOR_REV_BITS);
+        T rbym2 = (T)1 << (i2 ^ XOR_REV_BYTES);
+        T ribm2 = (T)1 << (i2 ^ XOR_REV_BITS_IN_BYTES);
+        T mask = mask1|mask2;
+  #define STUFF (IS_U?"u":"s") << NBIT << "@" << i1 << "," << i2
+        ASSERT_EQ(reverse_bits(mask), revm1|revm2) << STUFF;
+        ASSERT_EQ((T)~reverse_bits((T)~mask), revm1|revm2) << STUFF;
+        ASSERT_EQ(byteswap<T>(mask), rbym1|rbym2) << STUFF;
+        ASSERT_EQ((T)~byteswap<T>((T)~mask), rbym1|rbym2) << STUFF;
+      }
     }
   }
-}
+};
 
 TEST_VM(opto, moveBits) {
   test_moveBits<int64_t>();
@@ -93,11 +95,7 @@ int32_t code_quality_reverse_bits_32(int32_t x) {
 }
 
 int32_t code_quality_reverse_bytes_32(int32_t x) {
-  return reverse_bytes(x);
-}
-
-int32_t code_quality_reverse_bits_in_bytes_32(int32_t x) {
-  return reverse_bits_in_bytes(x);
+  return byteswap(x);
 }
 
 int64_t code_quality_reverse_bits_64(int64_t x) {
@@ -105,9 +103,5 @@ int64_t code_quality_reverse_bits_64(int64_t x) {
 }
 
 int64_t code_quality_reverse_bytes_64(int64_t x) {
-  return reverse_bytes(x);
-}
-
-int64_t code_quality_reverse_bits_in_bytes_64(int64_t x) {
-  return reverse_bits_in_bytes(x);
+  return byteswap(x);
 }
