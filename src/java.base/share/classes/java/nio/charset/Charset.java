5c37f5bca2cf9814d74e0bd6a4dc7adf90cc3be8
Naoto Sato <naoto.sato@oracle.com>
2023-01-25 09:47:25 -0800

    Use standardProvider to determine the default charset

--- src/java.base/share/classes/java/nio/charset/Charset.java
+++ src/java.base/share/classes/java/nio/charset/Charset.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -653,7 +653,8 @@ public abstract class Charset
             synchronized (Charset.class) {
                 String csn = GetPropertyAction
                         .privilegedGetProperty("file.encoding");
-                Charset cs = lookup(csn);
+                // do not look for providers other than the standard one
+                Charset cs = standardProvider.charsetForName(csn);
                 if (cs != null)
                     defaultCharset = cs;
                 else
