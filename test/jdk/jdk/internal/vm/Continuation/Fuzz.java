856c99c754e1e5b3fdb53a571344c88a5bf7a05e
MBaesken <matthias.baesken@sap.com>
2023-01-27 09:32:34 +0100

    JDK-8301163

--- test/jdk/jdk/internal/vm/Continuation/Fuzz.java
+++ test/jdk/jdk/internal/vm/Continuation/Fuzz.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+* Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
 * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 *
 * This code is free software; you can redistribute it and/or modify it
@@ -90,6 +90,9 @@ public class Fuzz implements Runnable {
         if (Platform.isSlowDebugBuild() && Platform.isOSX() && Platform.isAArch64()) {
             throw new SkippedException("Test is unstable with slowdebug bits "
                                        + "on macosx-aarch64");
+        }
+        if (Platform.isLinux() && Platform.isPPC()) {
+            COMPILATION_TIMEOUT = COMPILATION_TIMEOUT * 2;
         }
         warmup();
         for (int compileLevel : new int[]{4}) {
