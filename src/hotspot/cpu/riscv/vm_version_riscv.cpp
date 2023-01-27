8fa7d9992bc8d42e868d802e7c8105327b89c36a
jiangfeilong <jiangfeilong@huawei.com>
2023-01-27 17:33:10 +0800

    add more information when reporting unsuppoted satp modes

--- src/hotspot/cpu/riscv/vm_version_riscv.cpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.cpp
@@ -39,7 +39,9 @@ void VM_Version::initialize() {
 
   // check if satp.mode is supported, currently supports up to SV48(RV64)
   if (get_satp_mode() > VM_SV48) {
-    vm_exit_during_initialization(err_msg("Unsupported satp mode: %s", _vm_mode));
+    vm_exit_during_initialization(
+      err_msg("Unsupported satp mode: %s. Only satp modes up to sv48 are supported for now.",
+              _vm_mode));
   }
 
   // https://github.com/riscv/riscv-profiles/blob/main/profiles.adoc#rva20-profiles
