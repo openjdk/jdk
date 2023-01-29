5c626677cad25d7262abecab77a11e62a7264a4a
Tingjun Yuan <yuantj1999@outlook.com>
2023-01-29 08:24:40 +0800

    Update Collections.java

--- src/java.base/share/classes/java/util/Collections.java
+++ src/java.base/share/classes/java/util/Collections.java
@@ -36,6 +36,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.IntFunction;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.random.RandomGenerator;
 import java.util.stream.IntStream;
