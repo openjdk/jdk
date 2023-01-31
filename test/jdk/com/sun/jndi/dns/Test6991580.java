d274657f17c1f7999a62306ed4a7f7ec31583a1a
Bill Huang <bill.w.huang@oracle.com>
2023-01-31 11:05:22 -0800

    Update com/sun/jndi/dns/Test6991580.java manual test instruction

--- test/jdk/com/sun/jndi/dns/Test6991580.java
+++ test/jdk/com/sun/jndi/dns/Test6991580.java
@@ -62,8 +62,10 @@ public class Test6991580 {
          "Modify the /etc/resolv.conf file if needed. "
             + "Don't forget to save the original content of the file.",
          "3. Type \"cd " + System.getProperty("test.classes") + "\".",
-         "4. Type \"" + System.getProperty("java.home") +
-                "/bin/java IPv6NameserverPlatformParsingTest\".",
+         "4. Type \"" + System.getProperty("java.home") + "/bin/java"
+                 + " --add-modules java.desktop,jdk.naming.dns"
+                 + " --add-opens jdk.naming.dns/com.sun.jndi.dns=ALL-UNNAMED"
+                 + " IPv6NameserverPlatformParsingTest\".",
          "5. If you see",
          "\"PASS: Found IPv6 address and DnsClient parsed it correctly.\"",
          ", press PASS else press FAIL.",
@@ -317,7 +319,7 @@ class TestDialog extends Dialog implements ActionListener
 
                if( posOfSpace <= 0 ) posOfSpace = maxStringLength - 1;
 
-               printStr = remainingStr.substring( 0, posOfSpace + 1 );
+               printStr = remainingStr.substring( 0, posOfSpace + 1 ) + " \\";
                remainingStr = remainingStr.substring( posOfSpace + 1 );
              }
             //else just print
