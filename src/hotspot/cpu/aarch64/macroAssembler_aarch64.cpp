388161467dc9fc99616161d7643600d5f523b554
Roman Kennke <rkennke@openjdk.org>
2023-01-31 19:37:57 +0000

    8301545: [Lilliput] Fix aarch64 interpreter load_nklass()
    Reviewed-by: shade

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -4039,7 +4039,7 @@ void MacroAssembler::load_nklass(Register dst, Register src) {
   // Check if we can take the (common) fast path, if obj is unlocked.
   ldr(dst, Address(src, oopDesc::mark_offset_in_bytes()));
   tst(dst, markWord::monitor_value);
-  br(Assembler::NE, fast);
+  br(Assembler::EQ, fast);
 
   // Fetch displaced header
   ldr(dst, Address(dst, OM_OFFSET_NO_MONITOR_VALUE_TAG(header)));
