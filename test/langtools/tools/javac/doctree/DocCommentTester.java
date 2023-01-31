6b22d92f960446f64e3170dbc5ba56e6c10b85af
Jonathan Gibbons <jonathan.gibbons@oracle.com>
2023-01-31 15:56:35 -0800

    fix broken tests

--- test/langtools/tools/javac/doctree/DocCommentTester.java
+++ test/langtools/tools/javac/doctree/DocCommentTester.java
@@ -895,8 +895,13 @@ public class DocCommentTester {
 
         @Override
         void check(TreePath path, Name name) throws Exception {
+            var annos = (path.getLeaf() instanceof MethodTree m)
+                    ? m.getModifiers().getAnnotations().toString()
+                    : "";
+            boolean normalizeTags = !annos.equals("@NormalizeTags(false)");
+
             String raw = trees.getDocComment(path);
-            String normRaw = normalize(raw);
+            String normRaw = normalize(raw, normalizeTags);
 
             StringWriter out = new StringWriter();
             DocPretty dp = new DocPretty(out);
@@ -913,36 +918,44 @@ public class DocCommentTester {
         }
 
         /**
-         * Normalize white space in places where the tree does not preserve it.
-         * Maintain contents of at-code and at-literal inline tags.
+         * Normalize whitespace in places where the tree does not preserve it.
+         * Maintain contents of inline tags unless {@code normalizeTags} is
+         * {@code false}. This should normally be {@code true}, but should be
+         * set to {@code false} when there is syntactically invalid content
+         * that might resemble an inline tag, but which is not so.
+         *
+         * @param s the comment text to be normalized
+         * @param normalizeTags whether to normalize inline tags
+         * @return the normalized content
          */
-        String normalize(String s) {
+        String normalize(String s, boolean normalizeTags) {
             String s2 = s.trim().replaceFirst("\\.\\s*\\n *@", ".\n@");
             StringBuilder sb = new StringBuilder();
             Pattern p = Pattern.compile("(?i)\\{@([a-z][a-z0-9.:-]*)( )?");
             Matcher m = p.matcher(s2);
             int start = 0;
-            while (m.find(start)) {
-                sb.append(normalizeFragment(s2.substring(start, m.start())));
-                sb.append(m.group().trim());
-                start = copyLiteral(s2, m.end(), sb);
+            if (normalizeTags) {
+                while (m.find(start)) {
+                    sb.append(normalizeFragment(s2.substring(start, m.start())));
+                    sb.append(m.group().trim());
+                    start = copyLiteral(s2, m.end(), sb);
+                }
             }
             sb.append(normalizeFragment(s2.substring(start)));
-            return sb.toString();
+            return sb.toString()
+                    .replaceAll("(?i)\\{@([a-z][a-z0-9.:-]*)\\s+}", "{@$1}")
+                    .replaceAll("(\\{@value\\s+[^}]+)\\s+(})", "$1$2");
         }
 
         String normalizeFragment(String s) {
-            return s.replaceAll("\\{@docRoot\\s+}", "{@docRoot}")
-                    .replaceAll("\\{@inheritDoc\\s+}", "{@inheritDoc}")
-                    .replaceAll("(\\{@value\\s+[^}]+)\\s+(})", "$1$2")
-                    .replaceAll("\n[ \t]+@", "\n@");
+            return s.replaceAll("\n[ \t]+@", "\n@");
         }
 
         int copyLiteral(String s, int start, StringBuilder sb) {
             int depth = 0;
             for (int i = start; i < s.length(); i++) {
                 char ch = s.charAt(i);
-                if (i == start && !Character.isWhitespace(ch)) {
+                if (i == start && !Character.isWhitespace(ch) && ch != '}') {
                     sb.append(' ');
                 }
                 switch (ch) {
