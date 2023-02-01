45b39d126fcc2aea8121a850d37ee6160f027609
Varada M <mvarada2000@gmail.com>
2023-02-01 06:19:21 -0800

    Removed the fuctions pthread_attr_setdetachstate and pthread_attr_setguardsize

--- test/hotspot/jtreg/runtime/jni/daemonDestroy/exedaemonDestroy.c
+++ test/hotspot/jtreg/runtime/jni/daemonDestroy/exedaemonDestroy.c
@@ -125,8 +125,6 @@ int main(int argc, char *argv[]) {
    pthread_t id;
    pthread_attr_t attr;
    pthread_attr_init(&attr);
-   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-   pthread_attr_setguardsize(&attr, 0);
    pthread_attr_setstacksize(&attr, adjusted_stack_size);
    pthread_create(&id, &attr, run, (void *)&args);
    pthread_join(id, NULL);
