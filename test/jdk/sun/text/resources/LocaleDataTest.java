71f53adab839b2ced0d6855770ac2ea5b4dc6ee2
Goetz Lindenmaier <goetz@openjdk.org>
2023-02-01 09:14:01 +0000

    8286154: Fix 3rd party notices in test files
    Backport-of: 1277f5d84e9c2863595396a471a61d83f8a0298c

--- test/jdk/sun/text/resources/LocaleDataTest.java
+++ test/jdk/sun/text/resources/LocaleDataTest.java
@@ -50,6 +50,10 @@
  */
 
 /*
+ * This file is available under and governed by the GNU General Public
+ * License version 2 only, as published by the Free Software Foundation.
+ * However, the following notice accompanied the original version of this
+ * file and, per its terms, should not be removed:
  *
  * (C) Copyright Taligent, Inc. 1996, 1997 - All Rights Reserved
  * (C) Copyright IBM Corp. 1996 - 1998 - All Rights Reserved
