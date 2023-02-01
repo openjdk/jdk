49a68df26c23062a9f115a48024c77397781de1f
Varada M <mvarada2000@gmail.com>
2023-02-01 08:04:02 +0000

    Indentation Fixes

--- test/jdk/jni/nullCaller/exeNullCallerTest.cpp
+++ test/jdk/jni/nullCaller/exeNullCallerTest.cpp
@@ -196,8 +196,8 @@ int main(int argc, char *argv[]){
    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
    pthread_attr_setguardsize(&attr, 0);
    pthread_attr_setstacksize(&attr, adjusted_stack_size);
-   pthread_create (&id,&attr,run,(void *)argv);
-   pthread_join(id,NULL);
+   pthread_create(&id, &attr, run, (void *)argv);
+   pthread_join(id, NULL);
 #else
    run(&argv);
 #endif //AIX
