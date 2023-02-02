b5d3e074335e394cde704359f693d1675449a6e1
Varada M <mvarada2000@gmail.com>
2023-02-02 10:38:53 -0800

    Added error handler

--- test/hotspot/jtreg/runtime/jni/daemonDestroy/exedaemonDestroy.c
+++ test/hotspot/jtreg/runtime/jni/daemonDestroy/exedaemonDestroy.c
@@ -127,7 +127,7 @@ int main(int argc, char *argv[]) {
    pthread_attr_t attr;
    pthread_attr_init(&attr);
    pthread_attr_setstacksize(&attr, adjusted_stack_size);
-   pthread_create(&id, &attr, run, (void *)&args);
+   result = pthread_create(&id, &attr, run, (void *)&args);
    if (result != 0) {
      fprintf(stderr, "Error: pthread_create failed with error code %d \n", result);
      return -1;
