c641bb79b463fee83912d739fb253bbee25a1585
Joshua Cao <joshcao@amazon.com>
2023-02-03 00:26:27 +0100

    Makefile script to build java files and run tests

--- PEA/auto.sh
+++ PEA/auto.sh
@@ -1,17 +1,4 @@
 #!/bin/bash 
-#
-function jdk_select() {
-    if [ -e $1/bin/java ]; then
-        P=$(realpath $1)
-        export JAVA_HOME=$P
-        export PATH=$JAVA_HOME/bin:$PATH
-        echo "choose $P"
-        java -version
-    else
-        echo "can't find valid jdk!"
-    fi
-}
-jdk_select ../build/linux-x86_64-server-fastdebug/images/jdk
 
 set -x
 JVM_FLAGS="-XX:+PrintEscapeAnalysis -XX:+PrintEliminateAllocations -XX:+PrintOptoAssembly"
