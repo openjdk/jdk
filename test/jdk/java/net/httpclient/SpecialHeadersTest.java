52eb90556432469ed535c675d04e08dbd6f5b581
Daniel Fuchs <daniel.fuchs@oracle.com>
2023-02-03 15:12:03 +0000

    8301787

--- test/jdk/java/net/httpclient/SpecialHeadersTest.java
+++ test/jdk/java/net/httpclient/SpecialHeadersTest.java
@@ -30,7 +30,6 @@
  * @build jdk.httpclient.test.lib.common.HttpServerAdapters
  *        jdk.httpclient.test.lib.http2.Http2TestServer
  *        jdk.test.lib.net.SimpleSSLContext
- * @requests (vm.compMode != "Xcomp")
  * @run testng/othervm
  *       -Djdk.httpclient.HttpClient.log=requests,headers,errors
  *       SpecialHeadersTest
