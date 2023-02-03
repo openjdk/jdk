0499ec2068db03278fcd4b74f5e8a813c729b61b
Varada M <mvarada2000@gmail.com>
2023-02-03 05:02:50 +0000

    8300139 : [AIX] Use pthreads to avoid JNI_createVM call from primordial thread

--- test/jdk/java/lang/reflect/exeCallerAccessTest/exeCallerAccessTest.c
+++ test/jdk/java/lang/reflect/exeCallerAccessTest/exeCallerAccessTest.c
@@ -244,12 +244,15 @@ int main(int argc, char *argv[]){
 #ifdef AIX
    size_t adjusted_stack_size = 1024*1024;
    pthread_t id;
+   int result;
    pthread_attr_t attr;
    pthread_attr_init(&attr);
-   pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-   pthread_attr_setguardsize(&attr, 0);
    pthread_attr_setstacksize(&attr, adjusted_stack_size);
-   pthread_create(&id, &attr, run, (void *)&argv);
+   result = pthread_create(&id, &attr, run, (void *)&argv);
+   if (result != 0)  {
+     fprintf(stderr, "Error: pthread_create failed with error code %d \n", result);
+     return -1;
+   }
    pthread_join(id, NULL);
 #else
    run(&argv);
