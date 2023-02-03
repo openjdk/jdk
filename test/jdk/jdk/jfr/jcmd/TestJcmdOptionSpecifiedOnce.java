bf39504675a4b079240197b01ebb22d7dcc6254b
Chihiro Ito <chiroito107@gmail.com>
2023-02-03 14:12:11 +0900

    to simplify tests

--- test/jdk/jdk/jfr/jcmd/TestJcmdOptionSpecifiedOnce.java
+++ test/jdk/jdk/jfr/jcmd/TestJcmdOptionSpecifiedOnce.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,15 +22,9 @@
  */
 package jdk.jfr.jcmd;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 /**
  * @test
- * @summary The test verifies that options can only be specified once with jcmd JFR.*
+ * @summary The test verifies that options can only be specified once with jcmd JFR
  * @key jfr
  * @requires vm.hasJFR
  * @library /test/lib /test/jdk
@@ -41,122 +35,12 @@ public class TestJcmdOptionSpecifiedOnce {
 
     public static void main(String[] args) throws Exception {
 
-        testCheckingLog();
-        checkStarting();
-        checkStopping();
-        checkDumping();
-        checkChecking();
+        testJCmdConflict();
     }
 
-    private static void testCheckingLog() {
-
-        testSpecifiedOption("JFR.start", "name=abc name=def", "name" );
-        testSpecifiedOptions("JFR.start", "name=abc name=def disk=true disk=false", Set.of("name", "disk"));
-        testSpecifiedOptions("JFR.start", "name=abc name=def disk=true disk=false delay=1s delay=2s", Set.of("name", "disk", "delay"));
-        testSpecifiedOptions("JFR.start", "name=abc name=def disk=true disk=false delay=1s delay=2s maxage=1s maxage=2s", Set.of("name", "disk", "delay", "maxage"));
-    }
-
-    public static void checkStarting() {
-
-        testSpecifiedOption("JFR.start", "name=abc name=def", "name" );
-        testSpecifiedOption("JFR.start", "disk=true disk=false", "disk" );
-        testSpecifiedOption("JFR.start", "delay=1s delay=2s", "delay" );
-        testSpecifiedOption("JFR.start", "duration=1s duration=2s", "duration" );
-        testSpecifiedOption("JFR.start", "maxage=1s maxage=2s", "maxage" );
-        testSpecifiedOption("JFR.start", "maxsize=1m maxsize=2m", "maxsize" );
-        testSpecifiedOption("JFR.start", "flush-interval=1s flush-interval=2s", "flush-interval" );
-        testSpecifiedOption("JFR.start", "dumponexit=true dumponexit=false", "dumponexit" );
-        testSpecifiedOption("JFR.start", "filename=filename1 filename=filename2", "filename" );
-        testSpecifiedOption("JFR.start", "path-to-gc-roots=true path-to-gc-roots=false", "path-to-gc-roots" );
-
-        testMultipleOption("JFR.start", "settings=default settings=profile");
-    }
-
-    public static void checkStopping() {
-
-        testSpecifiedOption("JFR.stop", "filename=filename1 filename=filename2", "filename" );
-        testSpecifiedOption("JFR.stop", "name=abc name=def", "name" );
-    }
-
-    public static void checkDumping() {
-
-        testSpecifiedOption("JFR.dump", "begin=0:00 begin=0:01", "begin" );
-        testSpecifiedOption("JFR.dump", "end=1:00 end=1:01", "end" );
-        testSpecifiedOption("JFR.dump", "filename=filename1 filename=filename2", "filename" );
-        testSpecifiedOption("JFR.dump", "maxage=1s maxage=2s", "maxage" );
-        testSpecifiedOption("JFR.dump", "name=abc name=def", "name" );
-        testSpecifiedOption("JFR.dump", "path-to-gc-roots=true path-to-gc-roots=false", "path-to-gc-roots" );
-    }
-
-    public static void checkChecking() {
-
-        testSpecifiedOption("JFR.check", "name=abc name=def", "name" );
-        testSpecifiedOption("JFR.check", "verbose=true verbose=false", "verbose" );
-    }
-
-    private static void testSpecifiedOption(String command, String option, String expectOption){
-
-        String output = JcmdHelper.jcmd("%s %s".formatted(command, option)).getOutput();
-
-        try {
-            Matcher matcher = Pattern.compile("Option ([a-z-]+) can only be specified once").matcher(output);
-            matcher.find();
-            String outputtedOption = matcher.group(1);
-
-            if (!outputtedOption.equals(expectOption)){
-                throw new RuntimeException("expected option is %s, but actual is %s".formatted(expectOption, outputtedOption));
-            }
-
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
-    }
-
-    private static void testMultipleOption(String command, String option){
-
-        String output = JcmdHelper.jcmd("%s %s".formatted(command, option)).getOutput();
-        final String regex = "Option ([a-z-]+) can only be specified once with ([a-z]+) flight recording";
-
-        try {
-            Matcher matcher = Pattern.compile(regex).matcher(output);
-            if (matcher.find()){
-                throw new RuntimeException("found in output : \"%s\"".formatted(regex));
-            }
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
-    }
-
-
-    private static void testSpecifiedOptions(String command, String option, Set<String> expectOptions){
-
-        String output = JcmdHelper.jcmd("%s %s".formatted(command, option)).getOutput();
-
-        try {
-            Matcher matcher = Pattern.compile("Options ([a-z-]+)((?:, [a-z-]+)*) and ([a-z-]+) can only be specified once").matcher(output);
-            matcher.find();
-
-            Set<String> outputtedOptions = new HashSet<>();
-
-            // Mandatory First option
-            outputtedOptions.add(matcher.group(1));
-
-            // A collection of non-mandatory options, strings joined by ",".
-            Arrays.stream(matcher.group(2).split(", ")).filter(s->s.length() > 0).forEach(outputtedOptions::add);
-
-            // Mandatory last option
-            outputtedOptions.add(matcher.group(3));
-
-            if (!outputtedOptions.equals(expectOptions)){
-                throw new RuntimeException("expected options are %s, but actual are %s".formatted(expectOptions, outputtedOptions));
-            }
-
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
+    private static void testJCmdConflict() {
+        var output= JcmdHelper.jcmd("JFR.start name=hello name=greetings");
+        output.shouldContain("name can only be specified once");
     }
 }
 
--- test/jdk/jdk/jfr/startupargs/TestStartupOptionSpecifiedOnce.java
+++ test/jdk/jdk/jfr/startupargs/TestStartupOptionSpecifiedOnce.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,12 +22,7 @@
  */
 package jdk.jfr.startupargs;
 
-import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
-
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * @test The test verifies that options can only be specified once with --XX:StartFlightRecording
@@ -38,102 +33,24 @@ import java.util.regex.Pattern;
  */
 public class TestStartupOptionSpecifiedOnce {
 
-    public static class TestMessage {
-        public static void main(String[] args) throws Exception {
-        }
-    }
-
     public static void main(String[] args) throws Exception {
-
-        testSpecifiedOption("name=abc,name=def", "name" );
-        testSpecifiedOption("disk=true,disk=false", "disk" );
-        testSpecifiedOption("delay=1s,delay=2s", "delay" );
-        testSpecifiedOption("duration=1s,duration=2s", "duration" );
-        testSpecifiedOption("maxage=1s,maxage=2s", "maxage" );
-        testSpecifiedOption("maxsize=1m,maxsize=2m", "maxsize" );
-        testSpecifiedOption("flush-interval=1s,flush-interval=2s", "flush-interval" );
-        testSpecifiedOption("dumponexit=true,dumponexit=false", "dumponexit" );
-        testSpecifiedOption("filename=filename1,filename=filename2", "filename" );
-        testSpecifiedOption("path-to-gc-roots=true,path-to-gc-roots=false", "path-to-gc-roots" );
-
-        testMultipleOption("settings=default,settings=default");
-
-        testSpecifiedOptions("name=abc,name=def,disk=true,disk=false", Set.of("name", "disk"));
-        testSpecifiedOptions("name=abc,name=def,disk=true,disk=false,delay=1s,delay=2s", Set.of("name", "disk", "delay"));
-        testSpecifiedOptions("name=abc,name=def,disk=true,disk=false,delay=1s,delay=2s,maxage=1s,maxage=2s", Set.of("name", "disk", "delay", "maxage"));
+        testStartFlightRecordingConflict();
+        testConflictThreeOptions();
+        testAbleMultipleOption();
     }
 
-    private static OutputAnalyzer startJfrJvm(String addedOptions) throws Exception {
-        List<String> commands = new ArrayList<>(2);
-        commands.add("-XX:StartFlightRecording=" + addedOptions);
-        commands.add(TestMessage.class.getName());
-        ProcessBuilder pb = ProcessTools.createTestJvm(commands);
-        OutputAnalyzer out = ProcessTools.executeProcess(pb);
-        return out;
+    private static void testStartFlightRecordingConflict() throws Exception {
+        var output = ProcessTools.executeTestJava("-XX:StartFlightRecording:disk=true,disk=false,name=cat,name=dog");
+        output.shouldContain("disk and name can only be specified once.");
     }
 
-    private static void testSpecifiedOption(String option, String expectOption) throws Exception{
-
-        String output = startJfrJvm(option).getOutput();
-
-        try {
-            Matcher matcher = Pattern.compile("Option ([a-z-]+) can only be specified once").matcher(output);
-            matcher.find();
-            String outputtedOption = matcher.group(1);
-
-            if (!outputtedOption.equals(expectOption)){
-                throw new RuntimeException("expected option is %s, but actual is %s".formatted(expectOption, outputtedOption));
-            }
-
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
+    private static void testConflictThreeOptions() throws Exception {
+        var output = ProcessTools.executeTestJava("-XX:StartFlightRecording:name=abc,name=def,disk=true,disk=false,delay=1s,delay=2s");
+        output.shouldContain("name, disk and delay can only be specified once.");
     }
 
-    private static void testMultipleOption(String option) throws Exception{
-
-        String output = startJfrJvm(option).getOutput();
-        final String regex = "Option ([a-z-]+) can only be specified once";
-
-        try {
-            Matcher matcher = Pattern.compile(regex).matcher(output);
-            if (matcher.find()){
-                throw new RuntimeException("found in output : \"%s\"".formatted(regex));
-            }
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
-    }
-
-
-    private static void testSpecifiedOptions(String option, Set<String> expectOptions) throws Exception{
-
-        String output = startJfrJvm(option).getOutput();
-
-        try {
-            Matcher matcher = Pattern.compile("Options ([a-z-]+)((?:, [a-z-]+)*) and ([a-z-]+) can only be specified once").matcher(output);
-            matcher.find();
-
-            Set<String> outputtedOptions = new HashSet<>();
-
-            // Mandatory First option
-            outputtedOptions.add(matcher.group(1));
-
-            // A collection of non-mandatory options, strings joined by ",".
-            Arrays.stream(matcher.group(2).split(", ")).filter(s->s.length() > 0).forEach(outputtedOptions::add);
-
-            // Mandatory last option
-            outputtedOptions.add(matcher.group(3));
-
-            if (!outputtedOptions.equals(expectOptions)){
-                throw new RuntimeException("expected options are %s, but actual are %s".formatted(expectOptions, outputtedOptions));
-            }
-
-        } catch (Exception e){
-            System.err.println(output);
-            throw e;
-        }
+    private static void testAbleMultipleOption() throws Exception {
+        var output = ProcessTools.executeTestJava("-XX:StartFlightRecording:settings=default,settings=profile");
+        output.shouldNotContain("settings can only be specified once");
     }
 }
