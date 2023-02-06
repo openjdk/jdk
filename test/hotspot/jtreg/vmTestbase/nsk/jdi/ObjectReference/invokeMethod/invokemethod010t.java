c1bcaef5952f8f95333c6481983b290b8bc9990d
Chris Plummer <chris.plummer@oracle.com>
2023-02-06 21:50:47 +0000

    Get rid of Thread.sleep() in the invoked method can add warning comment.

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/ObjectReference/invokeMethod/invokemethod010t.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/ObjectReference/invokeMethod/invokemethod010t.java
@@ -181,11 +181,15 @@ class invokemethod010tDummyClass {
     volatile boolean isInvoked = false;
 
     long longMeth(long l) throws InterruptedException {
+        /*
+         * WARNING: Since this method is called using INVOKE_SINGLE_THREADED, we need to
+         * be careful not to do anything that might block on another thread. That includes
+         * calling Thread.sleep(), which can be a problem for virtual threads.
+         */
         invokemethod010t.log.display("invokemethod010tDummyClass: longMeth: going to loop");
         isInvoked = true;
         while(!doExit) {
             l--; l++;
-            Thread.currentThread().sleep(0);
         }
         invokemethod010t.log.display("invokemethod010tDummyClass: longMeth: exiting");
         isInvoked = false;
