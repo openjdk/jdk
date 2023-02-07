2ef27802b22281c499133560b3e1c97ab22366f3
Alan Bateman <alan.bateman@oracle.com>
2023-02-07 14:39:08 +0000

    Initial commit

--- src/hotspot/share/runtime/continuation.cpp
+++ src/hotspot/share/runtime/continuation.cpp
@@ -411,9 +411,8 @@ void Continuations::init() {
   Continuation::init();
 }
 
-// While virtual threads are in Preview, there are some VM mechanisms we disable if continuations aren't used
 bool Continuations::enabled() {
-  return VMContinuations && Arguments::enable_preview();
+  return VMContinuations;
 }
 
 #define CC (char*)  /*cast a literal from (const char*)*/
