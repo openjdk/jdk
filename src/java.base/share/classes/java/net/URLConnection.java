e2690ee1e5a1f8a560da2fdb33322c7d178fbbd6
Andrey Turbanov <turbanoff@gmail.com>
2023-02-08 17:08:59 +0300

    8301964: Expensive fillInStackTrace operation in HttpURLConnection.getLastModified when no last-modified in response
    update copyright year

--- src/java.base/share/classes/java/net/URLConnection.java
+++ src/java.base/share/classes/java/net/URLConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,7 +33,6 @@ import java.util.Hashtable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.Locale;
 import java.util.Objects;
 import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
--- src/java.base/share/classes/sun/net/www/protocol/https/HttpsURLConnectionImpl.java
+++ src/java.base/share/classes/sun/net/www/protocol/https/HttpsURLConnectionImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -38,7 +38,6 @@ import java.util.Map;
 import java.util.List;
 import java.util.Optional;
 import sun.net.util.IPAddressUtil;
-import sun.net.www.http.HttpClient;
 
 /**
  * A class to represent an HTTP connection to a remote object.
