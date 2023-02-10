3ef2a6d7d4a44e4ad2d022bd3ce61ba3ed196b01
Archie L. Cobbs <archie.cobbs@gmail.com>
2023-02-10 18:33:05 -0500

    Add parser unit tests for extra import semicolon diagnostics.

---
+++ test/langtools/tools/javac/parser/ExtraImportSemicolon.java
@@ -0,0 +1,13 @@
+/*
+ * @test /nodynamiccopyright/
+ * @bug 5059679
+ * @summary Verify proper error reporting of extra semicolon before import statement
+ * @compile/fail/ref=ExtraImportSemicolon.out1 -XDrawDiagnostics ExtraImportSemicolon.java
+ * @compile/ref=ExtraImportSemicolon.out2 -source 20 -XDrawDiagnostics ExtraImportSemicolon.java
+ */
+
+import java.util.Map;;      // NOTE: extra semicolon
+import java.util.Set;
+
+class ExtraImportSemicolon {
+}
