574b48c6925ebfb31345fc46c7d23aa4153f99b0
Daniel D. Daugherty <dcubed@openjdk.org>
2023-02-16 17:13:16 +0000

    8302678: validate_source fails after JDK-8293313
    Reviewed-by: bpb

--- src/hotspot/share/services/mallocTracker.hpp
+++ src/hotspot/share/services/mallocTracker.hpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2021, 2023 SAP SE. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
