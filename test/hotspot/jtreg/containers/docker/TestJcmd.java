099231de432d3d377d31e5cd749d675a98ea559a
Severin Gehwolf <sgehwolf@redhat.com>
2023-02-20 18:26:10 +0100

    8302888: containers/docker/TestJcmd.java fails when run as root under podman

--- test/hotspot/jtreg/containers/docker/TestJcmd.java
+++ test/hotspot/jtreg/containers/docker/TestJcmd.java
@@ -169,7 +169,8 @@ public class TestJcmd {
             .addDockerOpts("--name", CONTAINER_NAME)
             .addClassOptions("" + TIME_TO_RUN_CONTAINER_PROCESS);
 
-        if (IS_PODMAN) {
+        String uid = getId("-u");
+        if (IS_PODMAN && !ROOT_UID.equals(uid)) {
             // map the current userid to the one in the target namespace
             opts.addDockerOpts("--userns=keep-id");
         }
