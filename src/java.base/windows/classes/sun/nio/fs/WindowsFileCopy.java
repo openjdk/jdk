03accc262902d214edfcb33f2899d73987779e64
Christian Stein <sormuras@gmail.com>
2023-02-21 11:49:01 +0100

    Address feedback

--- src/java.base/windows/classes/sun/nio/fs/WindowsFileCopy.java
+++ src/java.base/windows/classes/sun/nio/fs/WindowsFileCopy.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -301,7 +301,7 @@ class WindowsFileCopy {
                 continue;
             }
             if (option == null) throw new NullPointerException();
-            throw new UnsupportedOperationException("Unsupported move option: " + option);
+            throw new UnsupportedOperationException("Unsupported option: " + option);
         }
 
         @SuppressWarnings("removal")
