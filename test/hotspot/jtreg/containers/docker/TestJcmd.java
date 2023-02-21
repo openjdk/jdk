9974114b90b4dba21eeedb028a10f911d25b1ca5
Severin Gehwolf <sgehwolf@redhat.com>
2023-02-21 10:31:07 +0100

    Only call getId() in the podman case

--- test/hotspot/jtreg/containers/docker/TestJcmd.java
+++ test/hotspot/jtreg/containers/docker/TestJcmd.java
@@ -169,8 +169,7 @@ public class TestJcmd {
             .addDockerOpts("--name", CONTAINER_NAME)
             .addClassOptions("" + TIME_TO_RUN_CONTAINER_PROCESS);
 
-        String uid = getId("-u");
-        if (IS_PODMAN && !ROOT_UID.equals(uid)) {
+        if (IS_PODMAN && !ROOT_UID.equals(getId("-u"))) {
             // map the current userid to the one in the target namespace
             opts.addDockerOpts("--userns=keep-id");
         }
