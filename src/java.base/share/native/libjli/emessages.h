d923ef45d7508f4732b1f7c20d42bf9677b41f69
TheShermanTanker <32636402+TheShermanTanker@users.noreply.github.com>
2023-02-23 01:39:11 +0800

    src/java.base/share/native/libjli/emessages.h
    Co-authored-by: Mandy Chung <mandy.chung@oracle.com>

--- src/java.base/share/native/libjli/emessages.h
+++ src/java.base/share/native/libjli/emessages.h
@@ -57,7 +57,7 @@
 #define ARG_ERROR15     "Error: Option %s is not allowed in this context"
 #define ARG_ERROR16     "Error: Option %s in %s is not allowed in this context"
 #define ARG_ERROR17     "Error: Cannot specify main class in this context"
-#define ARG_ERROR18     "Error: Could not read %s"
+#define ARG_ERROR18     "Error: Failed to read %s"
 
 #define JVM_ERROR1      "Error: Could not create the Java Virtual Machine.\n" GEN_ERROR
 #define JVM_ERROR2      "Error: Could not detach main thread.\n" JNI_ERROR
