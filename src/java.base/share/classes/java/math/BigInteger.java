99373b4b5594f0a1e8677f1f3e7ddb8900385b46
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2023-02-27 12:03:57 -0500

    Made random generation of BigIntegers faster

--- src/java.base/share/classes/java/math/BigInteger.java
+++ src/java.base/share/classes/java/math/BigInteger.java
@@ -693,11 +693,11 @@ public class BigInteger extends Number implements Comparable<BigInteger> {
      * @see #bitLength()
      */
     public BigInteger(int numBits, Random rnd) {
-        byte[] magnitude = randomBits(numBits, rnd);
+        int[] magnitude = randomBits(numBits, rnd);
 
         try {
-            // stripLeadingZeroBytes() returns a zero length array if len == 0
-            this.mag = stripLeadingZeroBytes(magnitude, 0, magnitude.length);
+            // trustedStripLeadingZeroInts() returns a zero length array if len == 0
+            this.mag = trustedStripLeadingZeroInts(magnitude);
 
             if (this.mag.length == 0) {
                 this.signum = 0;
@@ -708,22 +708,25 @@ public class BigInteger extends Number implements Comparable<BigInteger> {
                 checkRange();
             }
         } finally {
-            Arrays.fill(magnitude, (byte)0);
+            Arrays.fill(magnitude, 0);
         }
     }
 
-    private static byte[] randomBits(int numBits, Random rnd) {
+    private static int[] randomBits(int numBits, Random rnd) {
         if (numBits < 0)
             throw new IllegalArgumentException("numBits must be non-negative");
-        int numBytes = (int)(((long)numBits+7)/8); // avoid overflow
-        byte[] randomBits = new byte[numBytes];
 
-        // Generate random bytes and mask out any excess bits
-        if (numBytes > 0) {
-            rnd.nextBytes(randomBits);
-            int excessBits = 8*numBytes - numBits;
-            randomBits[0] &= (byte)((1 << (8-excessBits)) - 1);
-        }
+        if (numBits == 0)
+            return new int[0];
+
+        int[] randomBits = new int[((numBits - 1) >> 5) + 1];
+        // randomBits.length >= 1, since numBits > 0
+
+        // Generate random ints and mask out any excess bits
+        for (int i = 0; i < randomBits.length; i++)
+            randomBits[i] = rnd.nextInt();
+
+        randomBits[0] &= -1 >>> -numBits;
         return randomBits;
     }
 
