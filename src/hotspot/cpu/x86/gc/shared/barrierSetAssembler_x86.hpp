8d2b8f4143bb31978208f1efd533c74b15acfa11
Erik Österlund <eosterlund@openjdk.org>
2023-02-27 11:21:38 +0000

    Add comment

--- src/hotspot/cpu/x86/gc/shared/barrierSetAssembler_x86.hpp
+++ src/hotspot/cpu/x86/gc/shared/barrierSetAssembler_x86.hpp
@@ -49,6 +49,9 @@ public:
   virtual void store_at(MacroAssembler* masm, DecoratorSet decorators, BasicType type,
                         Address dst, Register val, Register tmp1, Register tmp2, Register tmp3);
 
+  // The copy_[load/store]_at functions are used by arraycopy stubs. Be careful to only use
+  // r10 (aka rscratch1) in a context where restore_arg_regs_using_thread has been used instead
+  // of the looser setup_arg_regs. Currently this is done when using type T_OBJECT.
   virtual void copy_load_at(MacroAssembler* masm,
                             DecoratorSet decorators,
                             BasicType type,
