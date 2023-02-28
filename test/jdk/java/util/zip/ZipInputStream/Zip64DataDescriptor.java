45d34ce6075c31f837f188202dcdd743c3dcda76
Eirik Bjorsnos <eirbjo@gmail.com>
2023-02-28 22:25:01 +0100

    Update test to use a Zip64 file produced using the zip command with the -fd flag

--- test/jdk/java/util/zip/ZipInputStream/Zip64DataDescriptor.java
+++ test/jdk/java/util/zip/ZipInputStream/Zip64DataDescriptor.java
@@ -57,44 +57,45 @@ public class Zip64DataDescriptor {
          * of a Zip64 extended information extra field and the
          * Data Descriptor having 8-byte values for csize and size.
          *
+         * The file was produced using the following command:
+         * <pre>echo hello | zip -fd > hello.zip</pre>
+         *
          * ------  Local File Header  ------
          * 000000  signature          0x04034b50
          * 000004  version            45
-         * 000006  flags              0x0808
-         * 000008  method             8
-         * 000010  time               0x7e00         15:48
-         * 000012  date               0x564c         2023-02-12
+         * 000006  flags              0x0008
+         * 000008  method             8              Deflated
+         * 000010  time               0xb180         22:12
+         * 000012  date               0x565c         2023-02-28
          * 000014  crc                0x00000000
          * 000018  csize              -1
          * 000022  size               -1
-         * 000026  nlen               5
+         * 000026  nlen               1
          * 000028  elen               20
-         * 000030  name               5 bytes        'entry'
-         * 000035  ext id             0x0001         Zip64 extended information extra field
-         * 000037  ext size           16
-         * 000039  z64 size           0
-         * 000047  z64 csize          0
+         * 000030  name               1 bytes        '-'
+         * 000031  ext id             0x0001         Zip64 extended information extra field
+         * 000033  ext size           16
+         * 000035  z64 size           0
+         * 000043  z64 csize          0
+         *
          * ------  File Data  ------
-         * 000055  ext data           7 bytes
-         * ------  Data Descriptor  ------
-         * 000062  signature          0x08074b50
-         * 000066  crc                0x3610a686
-         * 000070  csize              7
-         * 000078  size               5
-         * 000086  ...
+         * 000051  data               8 bytes
+         *
+         * ------  Data Desciptor  ------
+         * 000059  signature          0x08074b50
+         * 000063  crc                0x363a3020
+         * 000067  csize              8
+         * 000075  size               6
+         * 000083  ...
          */
 
         String hex = """
-                504b03042d0008080800007e4c5600000000ffffffffffffffff05001400
-                656e7472790100100000000000000000000000000000000000cb48cdc9c9
-                0700504b070886a6103607000000000000000500000000000000504b0102
-                1400140008080800007e4c5686a61036ffffffffffffffff050020000900
-                ffff000000000000ffffffff656e74727901001c00050000000000000007
-                000000000000000000000000000000000000004120636f6d6d656e74504b
-                06062c000000000000002d002d0000000000000000000100000000000000
-                01000000000000005c000000000000005600000000000000504b06070000
-                0000b20000000000000001000000504b050600000000010001005c000000
-                560000000000""";
+                504b03042d000800080080b15c5600000000ffffffffffffffff01001400
+                2d0100100000000000000000000000000000000000cb48cdc9c9e7020050
+                4b070820303a3608000000000000000600000000000000504b01021e032d
+                000800080080b15c5620303a360800000006000000010000000000000001
+                000000b011000000002d504b050600000000010001002f00000053000000
+                0000""";
 
         zip64File = HexFormat.of().parseHex(hex.replaceAll("\n", ""));
     }
@@ -120,7 +121,7 @@ public class Zip64DataDescriptor {
             readZipInputStream();
         });
 
-        assertEquals(ex.getMessage(), "invalid entry size (expected 0 but got 5 bytes)");
+        assertEquals(ex.getMessage(), "invalid entry size (expected 0 but got 6 bytes)");
     }
 
     /**
@@ -135,7 +136,7 @@ public class Zip64DataDescriptor {
             readZipInputStream();
         });
 
-        assertEquals(ex.getMessage(), "invalid entry size (expected 0 but got 5 bytes)");
+        assertEquals(ex.getMessage(), "invalid entry size (expected 0 but got 6 bytes)");
     }
 
     /**
@@ -144,7 +145,7 @@ public class Zip64DataDescriptor {
      * @param size the value to set in the 'data size' field.
      */
     private void setExtraSize(short size) {
-        int extSizeOffset = 37;
+        int extSizeOffset = 33;
         ByteBuffer.wrap(zip64File).order(ByteOrder.LITTLE_ENDIAN)
                 .putShort(extSizeOffset, size);
     }
