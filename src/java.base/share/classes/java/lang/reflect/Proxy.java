2682d88c4b61beb385435368392b3f8ccce07dfe
Ravali Yatham <rayatha1@in.ibm.com>
2023-03-01 12:31:17 +0530

    Use classloaderName if not Null otherwise Objects.toIdentityString(ld)

--- src/java.base/share/classes/java/lang/reflect/Proxy.java
+++ src/java.base/share/classes/java/lang/reflect/Proxy.java
@@ -877,8 +877,16 @@ public class Proxy implements java.io.Serializable {
             } catch (ClassNotFoundException e) {
             }
             if (type != c) {
+                /**
+                * If classloader has a name explicitly set then
+                *        <loader-name>@<id>
+                * If classloader has no name then
+                *        <qualified-class-name>@<id>
+                */
+                String nid = (ld.getName() != null) ? ld.getName() + "@" + Integer.toHexString(System.identityHashCode(ld))
+                                                    : Objects.toIdentityString(ld);
                 throw new IllegalArgumentException(c.getName() +
-                        " referenced from a method is not visible from class loader: " + ld);
+                        " referenced from a method is not visible from class loader: " + nid);
             }
         }
 
