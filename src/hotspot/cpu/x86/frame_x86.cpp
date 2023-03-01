f1541b2f6bba4285d4b461937097ff38819ef33e
Johannes Bechberger <johannes.bechberger@sap.com>
2023-03-01 13:15:58 +0100

    Don't check for completeness of runtime stubs

--- src/hotspot/cpu/x86/frame_x86.cpp
+++ src/hotspot/cpu/x86/frame_x86.cpp
@@ -90,12 +90,12 @@ bool frame::safe_for_sender(JavaThread *thread) {
   if (_cb != NULL ) {
 
     // First check if frame is complete and tester is reliable
-    // Unfortunately we can only check frame complete for runtime stubs and nmethod
+    // Unfortunately we can only check frame complete for compiled blobs,
     // other generic buffer blobs are more problematic so we just assume they are
     // ok. adapter blobs never have a frame complete and are never ok.
 
     if (!_cb->is_frame_complete_at(_pc)) {
-      if (_cb->is_compiled() || _cb->is_adapter_blob() || _cb->is_runtime_stub()) {
+      if (_cb->is_compiled() || _cb->is_adapter_blob()) {
         return false;
       }
     }
