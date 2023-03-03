80739e11b52a73d76525f9508e30f8809342e933
Doug Simon <dnsimon@openjdk.org>
2023-03-03 15:52:46 +0000

    8279619: [JVMCI] improve EncodedSpeculationReason
    Reviewed-by: never

--- src/jdk.internal.vm.ci/share/classes/jdk.vm.ci.meta/src/jdk/vm/ci/meta/EncodedSpeculationReason.java
+++ src/jdk.internal.vm.ci/share/classes/jdk.vm.ci.meta/src/jdk/vm/ci/meta/EncodedSpeculationReason.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,6 +61,7 @@ public class EncodedSpeculationReason implements SpeculationReason {
         if (encoding == null) {
             encoding = encodingSupplier.get();
             encoding.addInt(groupId);
+            encoding.addInt(groupName.hashCode());
             for (Object o : context) {
                 if (o == null) {
                     encoding.addInt(0);
