openjdk/panama-foreign

8302346: Lift upcall sharing mechanism to AbstractLinker
Reviewed-by: mcimadamore

src/java.base/share/classes/jdk/internal/foreign/abi/AbstractLinker.java
@@ -50,9 +50,13 @@ public abstract sealed class AbstractLinker implements Linker permits LinuxAArch
                                                                       Windowsx64Linker, LinuxRISCV64Linker,
                                                                       FallbackLinker {
 
+    public interface UpcallStubFactory {
+        MemorySegment makeStub(MethodHandle target, Arena arena);
+    }
 
     private record LinkRequest(FunctionDescriptor descriptor, LinkerOptions options) {}
     private final SoftReferenceCache<LinkRequest, MethodHandle> DOWNCALL_CACHE = new SoftReferenceCache<>();
+    private final SoftReferenceCache<LinkRequest, UpcallStubFactory> UPCALL_CACHE = new SoftReferenceCache<>();
 
     @Override
     public MethodHandle downcallHandle(FunctionDescriptor function, Option... options) {
@@ -84,12 +88,13 @@ public abstract sealed class AbstractLinker implements Linker permits LinuxAArch
         if (!type.equals(target.type())) {
             throw new IllegalArgumentException("Wrong method handle type: " + target.type());
         }
-        return arrangeUpcall(target, target.type(), function, arena, optionSet);
+
+        UpcallStubFactory factory = UPCALL_CACHE.get(new LinkRequest(function, optionSet), linkRequest ->
+            arrangeUpcall(type, linkRequest.descriptor(), linkRequest.options()));
+        return factory.makeStub(target, arena);
     }
 
-    protected abstract MemorySegment arrangeUpcall(MethodHandle target, MethodType targetType,
-                                                   FunctionDescriptor function, Arena arena,
-                                                   LinkerOptions options);
+    protected abstract UpcallStubFactory arrangeUpcall(MethodType targetType, FunctionDescriptor function, LinkerOptions options);
 
     @Override
     public SystemLookup defaultLookup() {

openjdk/jdk

src/java.base/share/classes/jdk/internal/foreign/abi/AbstractLinker.java
@@ -45,10 +45,16 @@ import java.lang.invoke.MethodType;
 import java.util.Objects;
 
 public abstract sealed class AbstractLinker implements Linker permits LinuxAArch64Linker, MacOsAArch64Linker,
-                                                                      SysVx64Linker, WindowsAArch64Linker, Windowsx64Linker, LinuxRISCV64Linker {
+                                                                      SysVx64Linker, WindowsAArch64Linker,
+                                                                      Windowsx64Linker, LinuxRISCV64Linker {
+
+    public interface UpcallStubFactory {
+        MemorySegment makeStub(MethodHandle target, SegmentScope arena);
+    }
 
     private record LinkRequest(FunctionDescriptor descriptor, LinkerOptions options) {}
     private final SoftReferenceCache<LinkRequest, MethodHandle> DOWNCALL_CACHE = new SoftReferenceCache<>();
+    private final SoftReferenceCache<FunctionDescriptor, UpcallStubFactory> UPCALL_CACHE = new SoftReferenceCache<>();
 
     @Override
     public MethodHandle downcallHandle(FunctionDescriptor function, Option... options) {
@@ -79,11 +85,12 @@ public abstract sealed class AbstractLinker implements Linker permits LinuxAArch
         if (!type.equals(target.type())) {
             throw new IllegalArgumentException("Wrong method handle type: " + target.type());
         }
-        return arrangeUpcall(target, target.type(), function, scope);
+
+        UpcallStubFactory factory = UPCALL_CACHE.get(function, f -> arrangeUpcall(type, f));
+        return factory.makeStub(target, scope);
     }
 
-    protected abstract MemorySegment arrangeUpcall(MethodHandle target, MethodType targetType,
-                                                   FunctionDescriptor function, SegmentScope scope);
+    protected abstract UpcallStubFactory arrangeUpcall(MethodType targetType, FunctionDescriptor function);
 
     @Override
     public SystemLookup defaultLookup() {