a145693bebc2c31c1515b085e308a2e96fde900b
Eirik Bjorsnos <eirbjo@gmail.com>
2023-03-09 21:01:52 +0100

    Update copyright years

--- test/jdk/java/util/zip/DataDescriptorSignatureMissing.java
+++ test/jdk/java/util/zip/DataDescriptorSignatureMissing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 Google, Inc.  All Rights Reserved.
+ * Copyright 2012, 2023 Google, Inc.  All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
