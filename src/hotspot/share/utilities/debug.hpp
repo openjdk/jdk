359bccc8c4585a071cce96de2bef6a1582759a69
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2023-03-09 19:50:00 +0800

    debug.hpp

--- src/hotspot/share/utilities/debug.hpp
+++ src/hotspot/share/utilities/debug.hpp
@@ -163,19 +163,29 @@ extern bool Debugging;
 
 // error reporting helper functions
 void report_vm_error(const char* file, int line, const char* error_msg);
+
+ATTRIBUTE_PRINTF(4, 5)
 void report_vm_error(const char* file, int line, const char* error_msg,
-                     const char* detail_fmt, ...) ATTRIBUTE_PRINTF(4, 5);
+                     const char* detail_fmt, ...);
+
+
 void report_vm_status_error(const char* file, int line, const char* error_msg,
                             int status, const char* detail);
-void report_fatal(VMErrorType error_type, const char* file, int line, const char* detail_fmt, ...) ATTRIBUTE_PRINTF(4, 5);
+
+ATTRIBUTE_PRINTF(4, 5)
+void report_fatal(VMErrorType error_type, const char* file, int line, const char* detail_fmt, ...);
+
+ATTRIBUTE_PRINTF(5, 6)
 void report_vm_out_of_memory(const char* file, int line, size_t size, VMErrorType vm_err_type,
-                             const char* detail_fmt, ...) ATTRIBUTE_PRINTF(5, 6);
+                             const char* detail_fmt, ...);
+
 void report_should_not_call(const char* file, int line);
 void report_should_not_reach_here(const char* file, int line);
 void report_unimplemented(const char* file, int line);
 void report_untested(const char* file, int line, const char* message);
 
-void warning(const char* format, ...) ATTRIBUTE_PRINTF(1, 2);
+ATTRIBUTE_PRINTF(1, 2)
+void warning(const char* format, ...);
 
 #define STATIC_ASSERT(Cond) static_assert((Cond), #Cond)
 
