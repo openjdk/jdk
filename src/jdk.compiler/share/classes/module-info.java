48fcda1afb65fb5b4af02442600fd5f93981359b
Christian Stein <sormuras@gmail.com>
2023-03-13 09:02:08 +0100

    JDK-8304036: Use CommandLine class from shared module

--- src/jdk.compiler/share/classes/module-info.java
+++ src/jdk.compiler/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -216,6 +216,7 @@ import javax.tools.StandardLocation;
  */
 module jdk.compiler {
     requires transitive java.compiler;
+    requires jdk.internal.opt;
     requires jdk.zipfs;
 
     exports com.sun.source.doctree;
--- src/jdk.javadoc/share/classes/module-info.java
+++ src/jdk.javadoc/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -62,6 +62,7 @@ module jdk.javadoc {
 
     requires transitive java.compiler;
     requires transitive jdk.compiler;
+    requires jdk.internal.opt;
 
     exports jdk.javadoc.doclet;
 
