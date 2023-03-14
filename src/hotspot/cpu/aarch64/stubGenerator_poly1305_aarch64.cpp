40231d0ddcbb04ccca23c5e0c0de963764c8ae84
Andrew Haley <aph@openjdk.org>
2023-03-14 06:51:23 -0400

    Test fixes

--- src/hotspot/cpu/aarch64/stubGenerator_poly1305_aarch64.cpp
+++ src/hotspot/cpu/aarch64/stubGenerator_poly1305_aarch64.cpp
@@ -120,6 +120,10 @@ address generate_poly1305_processBlocks2() {
     __ subsw(rscratch1, length, BLOCK_LENGTH * 4);
     __ br(Assembler::LT, DONE);
 
+    __ poly1305_step(S0, u0, input_start);
+    __ poly1305_multiply(u0, S0, R, RR2, regs);
+    __ poly1305_reduce(u0);
+
     // __ poly1305_step(S1, u1, input_start);
     // __ poly1305_multiply(u1, S1, R, RR2, regs);
     // __ poly1305_reduce(u1);
@@ -130,10 +134,6 @@ address generate_poly1305_processBlocks2() {
     __ poly1305_reduce_foo(v_u1, upper_bits, vregs.remaining());
 
     poo = __ pc();
-
-    __ poly1305_step(S0, u0, input_start);
-    __ poly1305_multiply(u0, S0, R, RR2, regs);
-    __ poly1305_reduce(u0);
 
     __ incrementw(Address(&trips), 1);
 
@@ -147,18 +147,18 @@ address generate_poly1305_processBlocks2() {
 
   // Last two parallel blocks
   {
-    __ poly1305_step(S1, u1, input_start);
-    __ poly1305_multiply(u1, S1, R, RR2, regs);
-    __ poly1305_reduce(u1);
+    __ poly1305_step(S0, u0, input_start);
+    __ poly1305_multiply(u0, S0, R, RR2, regs);
+    __ poly1305_reduce(u0);
 
+    // Load R**1
     __ pack_26(R[0], R[1], R[2], r_start);
     __ lsl(RR2, R[2], 26);
     __ add(RR2, RR2, RR2, __ LSL, 2);
 
-    // Load R**1
-    __ poly1305_step(S0, u0, input_start);
-    __ poly1305_multiply(u0, S0, R, RR2, regs);
-    __ poly1305_reduce(u0);
+    __ poly1305_step(S1, u1, input_start);
+    __ poly1305_multiply(u1, S1, R, RR2, regs);
+    __ poly1305_reduce(u1);
 
     __ subw(length, length, BLOCK_LENGTH * 2);
   }
