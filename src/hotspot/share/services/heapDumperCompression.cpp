bc79161e25c256d41650325f5e26e426c703ae6c
Man Cao <manc@openjdk.org>
2023-03-15 07:16:31 +0000

    8303937: Corrupted heap dumps due to missing retries for os::write()
    Reviewed-by: clanger
    Backport-of: bf16b5b9880eb89b283006db090dce4346aa877b

--- src/hotspot/share/services/heapDumperCompression.cpp
+++ src/hotspot/share/services/heapDumperCompression.cpp
@@ -54,10 +54,14 @@ char const* FileWriter::write_buf(char* buf, ssize_t size) {
   assert(_fd >= 0, "Must be open");
   assert(size > 0, "Must write at least one byte");
 
-  ssize_t n = (ssize_t) os::write(_fd, buf, (uint) size);
+  while (size > 0) {
+    ssize_t n = os::write(_fd, buf, (uint) size);
+    if (n <= 0) {
+      return os::strerror(errno);
+    }
 
-  if (n <= 0) {
-    return os::strerror(errno);
+    buf += n;
+    size -= n;
   }
 
   return NULL;
