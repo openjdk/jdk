1ae69e3e91e548da06b35b327ec7a6d47eb1acd5
Daniel Jeliński <djelinski@openjdk.org>
2023-03-15 20:51:22 +0000

    8304287: Problemlist java/net/SocketOption/OptionsTest.java
    Reviewed-by: dfuchs

--- test/jdk/java/net/SocketOption/OptionsTest.java
+++ test/jdk/java/net/SocketOption/OptionsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -106,7 +106,7 @@ public class OptionsTest {
             Enumeration<NetworkInterface> nifs = NetworkInterface.getNetworkInterfaces();
             while (nifs.hasMoreElements()) {
                 NetworkInterface ni = nifs.nextElement();
-                if (ni.supportsMulticast() && !ni.getInterfaceAddresses().isEmpty()) {
+                if (ni.supportsMulticast()) {
                     return ni;
                 }
             }
