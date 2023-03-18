6f455341247767dda2f506cc987d9b1746da1191
Leonid Mesnik <leonid.mesnik@gmail.com>
2023-03-18 19:48:40 +0000

    updated name

--- test/jdk/com/sun/jdi/CLETest.java
+++ test/jdk/com/sun/jdi/CLETest.java
@@ -35,11 +35,11 @@ import com.sun.jdi.event.*;
 import com.sun.jdi.request.*;
 import java.util.*;
 
-class cle1 {
+class CLEClass1 {
     public static void foo() {
     }
 }
-class cle2 {
+class CLEClass2 {
     public static void foo() {
     }
 }
@@ -72,10 +72,10 @@ class CLEDebugee {
     // verify that the generated MethodEntry events during class loading are not improperly
     // co-located as described the the CR.
     public static void test1() {
-        cle1.foo();  // BREAKPOINT_1
+        CLEClass1.foo();  // BREAKPOINT_1
     }
     public static void test2() {
-        cle2.foo();  // BREAKPOINT_2
+        CLEClass2.foo();  // BREAKPOINT_2
     }
 
     // Tests that MethodEntry, Step, and Breakpoint events that occur at the same
