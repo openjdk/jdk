def205cb123a7b7e026dcf829dcd913140d9fc42
Daniel Fuchs <67001856+dfuch@users.noreply.github.com>
2023-03-20 10:48:53 +0000

    Update test/jdk/java/net/httpclient/ShutdownNow.java
    Co-authored-by: Andrey Turbanov <turbanoff@gmail.com>

--- test/jdk/java/net/httpclient/ShutdownNow.java
+++ test/jdk/java/net/httpclient/ShutdownNow.java
@@ -142,7 +142,7 @@ public class ShutdownNow implements HttpServerAdapters {
         Throwable accepted = null;
         while (t != null) {
             out.println(what + ": checking " + t);
-            if (t instanceof  IOException io && hasExpectedMessage(io)) {
+            if (t instanceof IOException io && hasExpectedMessage(io)) {
                 out.println(what + ": Got expected message in cause: " + io);
                 return;
             } else if (t instanceof ClosedChannelException) {
