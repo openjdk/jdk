62930b59ecd42db90870135e1c422921b12d5708
Archie L. Cobbs <archie.cobbs@gmail.com>
2023-03-20 10:40:59 -0500

    Add the standard "This is NOT part of any supported API" warning.

--- src/jdk.compiler/share/classes/com/sun/tools/javac/util/InvalidUtfException.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/util/InvalidUtfException.java
@@ -28,6 +28,11 @@ package com.sun.tools.javac.util;
 /**
  * Exception thrown when invalid Modified UTF-8 is encountered.
  *
+ *  <p><b>This is NOT part of any supported API.
+ *  If you write code that depends on this, you do so at your own risk.
+ *  This code and its internal interfaces are subject to change or
+ *  deletion without notice.</b>
+ *
  * @see Convert#utf2chars
  * @see Convert#utfValidate
  */
