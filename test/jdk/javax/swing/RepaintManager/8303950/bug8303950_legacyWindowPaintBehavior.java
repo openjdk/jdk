bd81761a1a5a6cf702cd3e8c6d1951a66730ca6f
jeremy <jeremy.wood@mac.com>
2023-03-21 15:54:20 -0400

    8303950: adding unit test for legacy window behavior
    This test passes in the current JDK. The test criteria don't really reflect a rational expected behavior; they just reflect the current status quo.
    
    This currently fails in this JDK-8303950 branch (using the new proposed AWTPaintManager), which indicates we've changed behavior.
    
    In this case it's "window 4" that's failing. We used to get a mostly blue background, and now we're getting a background that's mostly red with a little blue. This isn't necessarily a bad thing; this failure is just documenting a change.

---
+++ test/jdk/javax/swing/RepaintManager/8303950/bug8303950_legacyWindowPaintBehavior.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2007, 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import java.awt.*;
+import java.awt.geom.RoundRectangle2D;
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Semaphore;
+
+import javax.swing.JComponent;
+import javax.swing.SwingUtilities;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.PanelUI;
+
+/* @test
+ * @bug 8303950
+ * @summary this codifies the preexisting behavior for Window/JRootPane backgrounds. I want to be sure the resolution
+ *          to 8303950 does not alter this behavior.
+ * @author Jeremy Wood
+ */
+public class bug8303950_legacyWindowPaintBehavior {
+
+    private static Color WINDOW_BACKGROUND = Color.red;
+    private static Color ROOTPANE_BACKGROUND = Color.blue;
+
+    public static void main(String[] args) throws Exception {
+        Semaphore semaphore = new Semaphore(1);
+        semaphore.acquireUninterruptibly();
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                UIManager.getDefaults().put("Panel.background", Color.green);
+
+                int x = 0;
+                int y = 0;
+
+                GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+                Insets i = Toolkit.getDefaultToolkit().getScreenInsets(gc);
+                x += i.left;
+                y += i.top;
+
+                Window w1 = createWindow( WINDOW_BACKGROUND, null, x, y, 400, 400, false, "window 1");
+                Window w2 = createWindow( WINDOW_BACKGROUND, ROOTPANE_BACKGROUND, x + 400, y, 400, 400, false, "window 2");
+                Window w3 = createWindow( WINDOW_BACKGROUND, null, x, y + 400, 400, 400, true, "window 3");
+                Window w4 =  createWindow( WINDOW_BACKGROUND, ROOTPANE_BACKGROUND, x + 400, y + 400, 400, 400, true, "window 4");
+
+                SwingUtilities.invokeLater(new Runnable() {
+                    int ctr = 0;
+                    boolean failed = false;
+                    @Override
+                    public void run() {
+                        while (ctr++ < 100_000) {
+                            SwingUtilities.invokeLater(this);
+                            return;
+                        }
+
+                        try {
+                            Robot robot = new Robot();
+                            testColor(robot, w1, Color.green);
+                            testColor(robot, w2, Color.green);
+                            testColor(robot, w3, Color.red);
+                            testColor(robot, w4, Color.blue);
+                        } catch (AWTException e) {
+                            throw new RuntimeException(e);
+                        } finally {
+                            if (failed)
+                                System.exit(1);
+                            semaphore.release();
+                        }
+                    }
+
+                    private void testColor(Robot robot, Window window, Color expectedColor) {
+                        Color actual = robot.getPixelColor(window.getLocationOnScreen().x + window.getWidth() / 2, window.getLocationOnScreen().y + window.getHeight() / 2);
+                        if (Math.abs(actual.getRed() - expectedColor.getRed()) > 150 ||
+                                Math.abs(actual.getGreen() - expectedColor.getGreen()) > 150 ||
+                                Math.abs(actual.getBlue() - expectedColor.getBlue()) > 150) {
+                            System.err.println("name = \"" + window.getName() + "\" expected = " + expectedColor + ", actual = " + actual);
+                            failed = true;
+                        }
+                    }
+                });
+            }
+        });
+        semaphore.acquireUninterruptibly();
+    }
+
+    private static JWindow createWindow(Color windowBackground, Color rootPaneBackground, int x, int y, int w, int h, boolean translucent, String name) {
+        JWindow window = new JWindow();
+        window.setName(name);
+        if (translucent) {
+            windowBackground = new Color(windowBackground.getRed(), windowBackground.getGreen(), windowBackground.getBlue(), 128);
+            if (rootPaneBackground != null)
+                rootPaneBackground = new Color(rootPaneBackground.getRed(), rootPaneBackground.getGreen(), rootPaneBackground.getBlue(), 128);
+        }
+        window.setBackground(windowBackground);
+        if (rootPaneBackground != null)
+            window.getRootPane().setBackground(rootPaneBackground);
+
+        window.getContentPane().setLayout(new BorderLayout());
+        JTextArea text = new JTextArea("translucent = " + translucent+"\nrootPaneBackground = " + rootPaneBackground);
+        text.setOpaque(false);
+        text.setEditable(false);
+        window.getContentPane().add(text, BorderLayout.NORTH);
+
+        window.pack();
+        window.setBounds(x,y,w,h);
+        window.setVisible(true);
+
+        return window;
+    }
+}
