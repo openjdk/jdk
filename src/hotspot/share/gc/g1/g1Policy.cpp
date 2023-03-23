8ad6e9349814795a3e2d51201102112c50d05b0a
Thomas Schatzl <thomas.schatzl@oracle.com>
2023-03-23 10:27:55 +0100

    ayang review, break formula a bit

--- src/hotspot/share/gc/g1/g1Policy.cpp
+++ src/hotspot/share/gc/g1/g1Policy.cpp
@@ -1400,8 +1400,9 @@ uint G1Policy::calc_max_old_cset_length() const {
   // as a percentage of the heap size. I.e., it should bound the
   // number of old regions added to the CSet irrespective of how many
   // of them are available.
+  double result = (double)_g1h->num_regions() * (size_t)G1OldCSetRegionThresholdPercent / 100;
   // Round up to be conservative.
-  return (uint)ceil((double)_g1h->num_regions() * (size_t)G1OldCSetRegionThresholdPercent / 100);
+  return (uint)ceil(result);
 }
 
 void G1Policy::calculate_old_collection_set_regions(G1CollectionSetCandidates* candidates,
