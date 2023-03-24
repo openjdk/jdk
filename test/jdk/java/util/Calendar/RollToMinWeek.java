fe2f92f0748031459663a6f518fd73075fbbbf1d
Justin Lu <justin.lu@oracle.com>
2023-03-24 11:00:56 -0700

    Test all possible min week and first day of week combos for bugged date

--- test/jdk/java/util/Calendar/RollToMinWeek.java
+++ test/jdk/java/util/Calendar/RollToMinWeek.java
@@ -24,86 +24,88 @@
 /*
  * @test
  * @bug 8225641
- * @summary Test the behavior of Calendar.roll(WEEK_OF_YEAR) when the week
- * is rolled into a minimal week 1
+ * @summary Test the behavior of Calendar.roll(WEEK_OF_YEAR) when the last week
+ * is rolled up into a minimal week 1 of the same year
  * @run junit RollToMinWeek
  */
 
-import java.util.Calendar;
-import java.util.Locale;
+
+import java.util.*;
 import java.util.stream.Stream;
+import static java.util.Calendar.*;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.Arguments;
 
+/**
+ * Test to validate the behavior of Calendar.roll(WEEK_OF_YEAR, +1)
+ * when rolling into a minimal week 1 from the max week. WEEK_OF_YEAR can
+ * not be rolled to a week with a non-existent DAY_OF_WEEK. This only
+ * test the implementation of the Gregorian Calendar roll
+ */
 public class RollToMinWeek {
-
-    /**
-     * Test to validate the behavior of Calendar.roll(WEEK_OF_YEAR)
-     * when rolling into a minimal week 1. Since other methods
-     * will call complete(), and rolling WEEK_OF_YEAR will cause field mask
-     * to compute the new time using WEEK_OF_YEAR and DAY_OF_WEEK, these values
-     * must represent a valid date. If the DAY_OF_WEEK does not exist in the
-     * minimum first week, WEEK_OF_YEAR should be incremented by 1 to provide a
-     * valid combination.
-     *
-     * For example, (Locale.US) rolling December 29th, 2019 by 1 week will
-     * produce WEEK_OF_YEAR = 1, and DAY_OF_WEEK = 1. However, there is no
-     * Sunday in the first week of 2019, and WEEK_OF_YEAR should be
-     * incremented by 1 to a value of 2.
-     */
     @ParameterizedTest
-    @MethodSource("calendarProvider")
-    public void testRollToMinWeek1(Calendar calendar, String expectedDate) {
-        String originalDate = longDateString(calendar);
-        calendar.roll(Calendar.WEEK_OF_YEAR, +1);
-        String rolledDate = longDateString(calendar);
-        if (!rolledDate.equals(expectedDate)) {
-            fail(String.format("""
-            {$$$ Failed: Rolled: "%s" by 1 week, expecting: "%s", but got: "%s"},
-            """, originalDate, expectedDate, rolledDate));
+    @MethodSource("rollUpCalProvider")
+    public void rollUpTest(Calendar calendar, String[] validDates){
+        if (calendar instanceof GregorianCalendar) {
+            testRoll(calendar, validDates, +1);
         } else {
-            System.out.printf("""
-            {$$$ Passed: Rolled: "%s" by 1 week and successfully got: "%s"},
-            """, originalDate, rolledDate);
+            fail(String.format("Calendar is not Gregorian: %s", calendar));
         }
     }
 
-    /**
-     * Data provider for testing firstWeek().
-     */
-    private static Stream<Arguments> calendarProvider() {
-        return Stream.of(
-                // Test a variety of rolls that previously produced incorrect results
-                Arguments.of(buildCalendar(27, 11, 2020, Locale.ENGLISH),
-                        "Sunday, 5 January 2020"),
-                Arguments.of(buildCalendar(28, 11, 2020, Locale.ENGLISH),
-                        "Monday, 6 January 2020"),
-                Arguments.of(buildCalendar(29, 11, 2020, Locale.ENGLISH),
-                        "Tuesday, 7 January 2020"),
-                Arguments.of(buildCalendar(29, 11, 2019, Locale.ENGLISH),
-                        "Sunday, 6 January 2019"),
-                Arguments.of(buildCalendar(30, 11, 2019, Locale.ENGLISH),
-                        "Monday, 7 January 2019"),
-                Arguments.of(buildCalendar(30, 11, 2019, Locale.FRANCE),
-                        "Monday, 7 January 2019")
-        );
+    private void testRoll(Calendar calendar, String[] validDates, int amount) {
+        String originalDate = longDateString(calendar);
+        calendar.roll(Calendar.WEEK_OF_YEAR, amount);
+        String rolledDate = longDateString(calendar);
+        if (!Arrays.asList(validDates).contains(rolledDate)) {
+            fail(String.format("""
+            {$$$ Failed: Rolled: "%s" by %s week, where the first day of the week
+            is: %s with a minimum week length of: %s and was expecting one of: "%s", but got: "%s"},
+            """, originalDate, amount, calendar.getFirstDayOfWeek(),
+                    calendar.getMinimalDaysInFirstWeek(), Arrays.toString(validDates), rolledDate));
+        } else {
+            System.out.printf("""
+            {$$$ Passed: Rolled: "%s" by %s week where the first day of the week
+            is: %s with a minimum week length of: %s and successfully got: "%s"},
+            """, originalDate, amount, calendar.getFirstDayOfWeek(),
+                    calendar.getMinimalDaysInFirstWeek(), rolledDate);
+        }
     }
 
-    private static Calendar buildCalendar(int day, int month, int year, Locale locale) {
-        Calendar calendar = Calendar.getInstance(locale);
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        return calendar;
+    // This implicitly tests the Iso8601 calendar as
+    // MinWeek = 4 and FirstDayOfWeek = Monday is included in the provider
+    private static Stream<Arguments> rollUpCalProvider() {
+        ArrayList<Arguments> calList = new ArrayList<Arguments>();
+        for (int weekLength = 1; weekLength <= 7; weekLength++) {
+            for (int firstDay = 1; firstDay <= 7; firstDay++) {
+                // Week 1, Week 2 are all potential dates to roll into
+                String[] validDates = {"Sunday, 6 January 2019", "Sunday, 13 January 2019"};
+                calList.add(Arguments.of(buildCalendar("gregory", firstDay, weekLength,
+                                29, 11, 2019), validDates)
+                        // Roll from week Max to week 1 with non-existent day of week
+                );
+            }
+        }
+        return calList.stream();
     }
 
-    private String longDateString(Calendar calendar) {
+    private static Calendar buildCalendar(String type, int firstDayOfWeek,
+                                 int minimumWeekLength, int dayOfMonth,
+                                 int month, int year) {
+        Calendar.Builder calBuilder = new Builder();
+        calBuilder.setCalendarType(type);
+        calBuilder.setWeekDefinition(firstDayOfWeek, minimumWeekLength);
+        calBuilder.setDate(year, month, dayOfMonth);
+        return calBuilder.build();
+    }
+
+    private static String longDateString(Calendar calendar) {
         return String.format("%s, %s %s %s",
                 calendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.ENGLISH),
                 calendar.get(Calendar.DAY_OF_MONTH),
                 calendar.getDisplayName(Calendar.MONTH, Calendar.LONG, Locale.ENGLISH),
-                calendar.get(Calendar.YEAR));
+                calendar.get(YEAR));
     }
 }
