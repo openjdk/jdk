6d61fc116f387779e483db58ed7127d4e3f3c6d1
Sergey Bylokhov <serb@openjdk.org>
2023-03-24 17:00:09 +0000

    8227257: javax/swing/JFileChooser/4847375/bug4847375.java fails with AssertionError
    Reviewed-by: phh
    Backport-of: 78b1686c150aeaa29c5d969b70c9c42c872246a2

--- src/java.desktop/windows/classes/sun/awt/shell/Win32ShellFolder2.java
+++ src/java.desktop/windows/classes/sun/awt/shell/Win32ShellFolder2.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -902,7 +902,7 @@ final class Win32ShellFolder2 extends ShellFolder {
                         location =
                                 Win32ShellFolderManager2.createShellFolderFromRelativePIDL(getDesktop(),
                                         linkLocationPIDL);
-                    } catch (InterruptedException e) {
+                    } catch (InterruptedException | FileNotFoundException e) {
                         // Return null
                     } catch (InternalError e) {
                         // Could be a link to a non-bindable object, such as a network connection
