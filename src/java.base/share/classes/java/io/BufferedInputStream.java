db4b1c93d13dbc645ef05cce71b32a0b5fd92b86
Сергей Цыпанов <sergei.tsypanov@yandex.ru>
2023-03-24 13:22:55 +0200

    8304745: Rework

--- src/java.base/share/classes/java/io/BufferedInputStream.java
+++ src/java.base/share/classes/java/io/BufferedInputStream.java
@@ -57,6 +57,8 @@ public class BufferedInputStream extends FilterInputStream {
 
     private static final int DEFAULT_BUFFER_SIZE = 8192;
 
+    private static final byte[] EMPTY = new byte[0];
+
     /**
      * As this class is used early during bootstrap, it's motivated to use
      * Unsafe.compareAndSetObject instead of AtomicReferenceFieldUpdater
@@ -67,10 +69,11 @@ public class BufferedInputStream extends FilterInputStream {
     private static final long BUF_OFFSET
             = U.objectFieldOffset(BufferedInputStream.class, "buf");
 
-    private static final byte[] EMPTY = new byte[0];
-
     // initialized to null when BufferedInputStream is sub-classed
     private final InternalLock lock;
+
+    // initial buffer size (DEFAULT_BUFFER_SIZE or size specified to constructor)
+    private final int initialSize;
 
     /**
      * The internal buffer array where the data is stored. When necessary,
@@ -156,8 +159,6 @@ public class BufferedInputStream extends FilterInputStream {
      */
     protected int marklimit;
 
-    private final int size;
-
     /**
      * Check to make sure that underlying input stream has not been
      * nulled out due to close; if not return it;
@@ -170,30 +171,40 @@ public class BufferedInputStream extends FilterInputStream {
     }
 
     /**
-     * Return the buffer if already allocated, or allocate one, or throw {@link IOException} if this stream is closed.
-     * If a caller simply wants to ensure whether this BufferedInputStream is open
-     * they should call {@link #getInIfOpen()} instead.
+     * Returns the internal buffer, optionally allocating it if empty.
+     * @param allocateIfEmpty true to allocate if empty
+     * @throws IOException if the stream is closed (buf is null)
+     */
+    private byte[] getBufIfOpen(boolean allocateIfEmpty) throws IOException {
+        byte[] buffer = buf;
+        if (allocateIfEmpty && buffer == EMPTY) {
+            buffer = new byte[initialSize];
+            if (!U.compareAndSetReference(this, BUF_OFFSET, EMPTY, buffer)) {
+                // re-read buf
+                buffer = buf;
+            }
+        }
+        if (buffer == null) {
+            throw new IOException("Stream closed");
+        }
+        return buffer;
+    }
+
+    /**
+     * Returns the internal buffer, allocating it if empty.
+     * @throws IOException if the stream is closed (buf is null)
      */
     private byte[] getBufIfOpen() throws IOException {
-        byte[] allocated = null;
-        do {
-            byte[] buffer = buf;
-            if (buffer == null) {
-                throw new IOException("Stream closed");
-            }
-            if (buffer == EMPTY) {
-                if (allocated == null) {
-                    allocated = new byte[size];
-                }
-                // defend against asynchronous close
-                if (U.compareAndSetReference(this, BUF_OFFSET, EMPTY, allocated)) {
-                    return allocated;
-                } else{
-                    continue;
-                }
-            }
-            return buffer;
-        } while (true);
+        return getBufIfOpen(true);
+    }
+
+    /**
+     * Throws IOException if the stream is closed (buf is null).
+     */
+    private void ensureOpen() throws IOException {
+        if (buf == null) {
+            throw new IOException("Stream closed");
+        }
     }
 
     /**
@@ -225,13 +236,13 @@ public class BufferedInputStream extends FilterInputStream {
         if (size <= 0) {
             throw new IllegalArgumentException("Buffer size <= 0");
         }
-        this.size = size;
-        // keep using monitors and initializing buf when BufferedInputStream is sub-classed
-        // for compatibility
+        initialSize = size;
         if (getClass() == BufferedInputStream.class) {
+            // use internal lock and lazily create buffer when not subclassed
             lock = InternalLock.newLockOrNull();
             buf = EMPTY;
         } else {
+            // use monitors and eagerly create buffer when subclassed
             lock = null;
             buf = new byte[size];
         }
@@ -329,6 +340,7 @@ public class BufferedInputStream extends FilterInputStream {
                if there is no mark/reset activity, do not bother to copy the
                bytes into the local buffer.  In this way buffered streams will
                cascade harmlessly. */
+            int size = Math.max(getBufIfOpen(false).length, initialSize);
             if (len >= size && markpos == -1) {
                 return getInIfOpen().read(b, off, len);
             }
@@ -396,7 +408,7 @@ public class BufferedInputStream extends FilterInputStream {
     }
 
     private int implRead(byte[] b, int off, int len) throws IOException {
-        getInIfOpen(); // Check for closed stream
+        ensureOpen();
         if ((off | len | (off + len) | (b.length - (off + len))) < 0) {
             throw new IndexOutOfBoundsException();
         } else if (len == 0) {
@@ -443,7 +455,7 @@ public class BufferedInputStream extends FilterInputStream {
     }
 
     private long implSkip(long n) throws IOException {
-        InputStream input = getInIfOpen();// Check for closed stream
+        ensureOpen();
         if (n <= 0) {
             return 0;
         }
@@ -452,7 +464,7 @@ public class BufferedInputStream extends FilterInputStream {
         if (avail <= 0) {
             // If no mark position set then don't keep in buffer
             if (markpos == -1)
-                return input.skip(n);
+                return getInIfOpen().skip(n);
 
             // Fill in buffer to save bytes for reset
             fill();
@@ -566,7 +578,7 @@ public class BufferedInputStream extends FilterInputStream {
     }
 
     private void implReset() throws IOException {
-        getInIfOpen(); // Cause exception if closed
+        ensureOpen();
         if (markpos < 0)
             throw new IOException("Resetting to invalid mark");
         pos = markpos;
