e55ed936aa60acb5ce5cdd7f2d4aa10dd20998a2
Сергей Цыпанов <sergei.tsypanov@yandex.ru>
2023-03-24 18:57:11 +0200

    8304745: Amend copyright

--- test/micro/org/openjdk/bench/java/io/BufferedInputStreamBenchmark.java
+++ test/micro/org/openjdk/bench/java/io/BufferedInputStreamBenchmark.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2022, Red Hat Inc. All rights reserved.
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,6 +39,7 @@ import java.util.concurrent.TimeUnit;
 public class BufferedInputStreamBenchmark {
     private final int size = 1024;
     private ByteArrayInputStream bais;
+
     @Setup(Level.Iteration)
     public void setup() {
         byte[] bytes = new byte[size];
