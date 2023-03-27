dfea46659baf8fb77fb1be6b0c79380dd96c6be5
jeremy <jeremy.wood@mac.com>
2023-03-26 23:55:12 -0400

    8303904: use Transparency.TRANSLUCENT for non-opaque windows

--- src/java.desktop/macosx/classes/sun/java2d/metal/MTLGraphicsConfig.java
+++ src/java.desktop/macosx/classes/sun/java2d/metal/MTLGraphicsConfig.java
@@ -50,6 +50,7 @@ import java.awt.Image;
 import java.awt.ImageCapabilities;
 import java.awt.Rectangle;
 import java.awt.Transparency;
+import java.awt.Window;
 
 import java.awt.color.ColorSpace;
 import java.awt.image.BufferedImage;
@@ -256,7 +257,10 @@ public final class MTLGraphicsConfig extends CGraphicsConfig
     public Image createAcceleratedImage(Component target,
                                         int width, int height)
     {
-        ColorModel model = getColorModel(Transparency.OPAQUE);
+        int transparency = Transparency.OPAQUE;
+        if (target instanceof Window window && !window.isOpaque())
+            transparency = Transparency.TRANSLUCENT;
+        ColorModel model = getColorModel(transparency);
         WritableRaster wr = model.createCompatibleWritableRaster(width, height);
         return new OffScreenImage(target, model, wr,
                 model.isAlphaPremultiplied());
