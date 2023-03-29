70b55f14c993c965c6d0ef84ad719ef656d2ff37
liach <liach@users.noreply.github.com>
2023-03-28 20:14:10 -0500

    Remove unused JavaLangReflectReflectAccess.invokeDefault

--- src/java.base/share/classes/java/lang/reflect/ReflectAccess.java
+++ src/java.base/share/classes/java/lang/reflect/ReflectAccess.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -126,10 +126,5 @@ class ReflectAccess implements jdk.internal.access.JavaLangReflectAccess {
         throws IllegalAccessException, InstantiationException, InvocationTargetException
     {
         return ctor.newInstanceWithCaller(args, true, caller);
-    }
-
-    public Object invokeDefault(Object proxy, Method method, Object[] args, Class<?> caller)
-            throws Throwable {
-        return Proxy.invokeDefault(proxy, method, args, caller);
     }
 }
