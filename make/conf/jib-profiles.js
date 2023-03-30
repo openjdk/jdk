69f6a00fd27f3c5087d9d3079194409bbffbf455
Christian Stein <christian.s.stein@oracle.com>
2023-03-30 10:32:42 +0200

    JDK-8304896: Back to use build number 1
    [skip ci]

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -1151,7 +1151,7 @@ var getJibProfilesDependencies = function (input, common) {
             server: "jpg",
             product: "jtreg",
             version: "7.2",
-            build_number: "ci/13",
+            build_number: "1",
             file: "bundles/jtreg-7.2+1.zip",
             environment_name: "JT_HOME",
             environment_path: input.get("jtreg", "home_path") + "/bin",
