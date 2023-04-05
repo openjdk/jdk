fdf6b08a50a0b90e0df496046c984a24d965cf28
Emanuel Peter <emanuel.peter@oracle.com>
2023-04-05 11:50:41 +0200

    Renamed regression test, removed old test2

--- test/hotspot/jtreg/compiler/loopopts/TestUnrollLimitPreciseType.java
+++ test/hotspot/jtreg/compiler/loopopts/TestUnrollLimitPreciseType.java
@@ -28,12 +28,12 @@
  *          This lead to dead data but zero-trip-guard control did not die -> "malformed control flow".
  * @requires vm.compiler2.enabled
  * @run main/othervm
- *      -XX:CompileCommand=compileonly,compiler.loopopts.TestUnrollSubINoUnderflow::test1
- *      -XX:CompileCommand=dontinline,compiler.loopopts.TestUnrollSubINoUnderflow::*
+ *      -XX:CompileCommand=compileonly,compiler.loopopts.TestUnrollLimitPreciseType::test1
+ *      -XX:CompileCommand=dontinline,compiler.loopopts.TestUnrollLimitPreciseType::*
  *      -XX:MaxVectorSize=64
  *      -Xcomp
  *      -XX:+UnlockExperimentalVMOptions -XX:PerMethodSpecTrapLimit=0 -XX:PerMethodTrapLimit=0
- *      compiler.loopopts.TestUnrollSubINoUnderflow test1
+ *      compiler.loopopts.TestUnrollLimitPreciseType test1
  */
 
 /*
@@ -43,34 +43,16 @@
  *          This lead to dead data but zero-trip-guard control did not die -> "malformed control flow".
  * @requires vm.compiler2.enabled
  * @run main/othervm
- *      -XX:CompileCommand=compileonly,compiler.loopopts.TestUnrollSubINoUnderflow::*
+ *      -XX:CompileCommand=compileonly,compiler.loopopts.TestUnrollLimitPreciseType::*
  *      -Xcomp
- *      compiler.loopopts.TestUnrollSubINoUnderflow test2
- */
-
-/*
- * @test id=test3
- * @bug 8298935
- * @summary CMoveI for underflow protection of the limit did not compute a type that was precise enough.
- *          This lead to dead data but zero-trip-guard control did not die -> "malformed control flow".
- * @requires vm.compiler2.enabled
- * @run main/othervm
- *      -XX:CompileCommand=compileonly,compiler.loopopts.TestUnrollSubINoUnderflow::*
- *      -Xcomp
- *      compiler.loopopts.TestUnrollSubINoUnderflow test3
+ *      compiler.loopopts.TestUnrollLimitPreciseType test2
  */
 
 
 package compiler.loopopts;
 
-public class TestUnrollSubINoUnderflow {
+public class TestUnrollLimitPreciseType {
     static final int RANGE = 512;
-
-    // for test2
-    static long count;
-    static float fval;
-    static float arr2[][];
-    static float arr[];
 
     public static void main(String args[]) {
         if (args.length != 1) {
@@ -81,8 +63,6 @@ public class TestUnrollSubINoUnderflow {
             test1(data);
         } else if (args[0].equals("test2")) {
             test2();
-        } else if (args[0].equals("test3")) {
-            test3();
         } else {
             throw new RuntimeException("Do not have: " + args[0]);
         }
@@ -98,20 +78,6 @@ public class TestUnrollSubINoUnderflow {
     }
 
     static void test2() {
-        // Loop is unrolled, but never SuperWord'ed.
-        // Also reproduces with avx2, but not with avx1.
-        int x = 1;
-        // Compiles, traps, and OSR-compiles.
-        for (int j = 0; j < 100_000; j++) {
-            for (int i = 1; i > x; --i) {
-                float v = arr[0] + fval;
-                arr2[i + 1][x] = v;
-                count += v;
-            }
-        }
-    }
-
-    static void test3() {
         // Loop is SuperWord'ed.
         // We unroll more afterwards, and so add vectorized post loop.
         // But it turns out that the vectorized post loop is never entered.
