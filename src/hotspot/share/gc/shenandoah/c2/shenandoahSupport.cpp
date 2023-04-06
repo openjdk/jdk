d6df935b0b280e7cde9a97a49f2578615a3812b1
Aleksey Shipilev <shade@openjdk.org>
2023-04-06 16:53:40 +0000

    Update copyright lines
    Reviewed-by: wkemper

--- src/hotspot/share/gc/shenandoah/c2/shenandoahSupport.cpp
+++ src/hotspot/share/gc/shenandoah/c2/shenandoahSupport.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2015, 2021, Red Hat, Inc. All rights reserved.
  * Copyright (C) 2022 THL A29 Limited, a Tencent company. All rights reserved.
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
--- src/hotspot/share/gc/shenandoah/shenandoahCardTable.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahCardTable.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2021, Amazon.com, Inc. and/or its affiliates. All rights reserved.
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
