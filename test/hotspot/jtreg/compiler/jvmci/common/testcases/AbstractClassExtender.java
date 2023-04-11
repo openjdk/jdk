386af9f864e21feab5e553690a72076d8930e964
Afshin Zafari <Afshin.zafari@oracle.com>
2023-04-11 09:39:06 +0200

    8305080: Remove finalize() from test/hotspot/jtreg/compiler/jvmci/common/testcases that used in compiler/jvmci/compilerToVM/ tests

--- test/hotspot/jtreg/compiler/jvmci/common/testcases/AbstractClassExtender.java
+++ test/hotspot/jtreg/compiler/jvmci/common/testcases/AbstractClassExtender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,10 +27,5 @@ public class AbstractClassExtender extends AbstractClass {
     @Override
     public void abstractMethod() {
         // empty
-    }
-
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
     }
 }
--- test/hotspot/jtreg/compiler/jvmci/common/testcases/MultipleImplementer1.java
+++ test/hotspot/jtreg/compiler/jvmci/common/testcases/MultipleImplementer1.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,9 +33,5 @@ public class MultipleImplementer1 implements MultipleImplementersInterface {
     @Override
     public void testMethod() {
         // empty
-    }
-    @Override
-    public void finalize() throws Throwable {
-        super.finalize();
     }
 }
