b26235005087cb43fc0871efc777e09881a8f07e
Aleksey Shipilev <shade@openjdk.org>
2023-04-13 08:39:32 +0000

    Reduce the number of new product flags
    Reviewed-by: ysr, rkennke

--- src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoah_globals.hpp
@@ -78,7 +78,7 @@
           " compact - run GC more frequently and with deeper targets to "   \
           "free up more memory.")                                           \
                                                                             \
-  product(ccstr, ShenandoahOldGCHeuristics, "adaptive",                     \
+  product(ccstr, ShenandoahOldGCHeuristics, "adaptive", EXPERIMENTAL,       \
           "Similar to ShenandoahGCHeuristics, but applied to the old "      \
           "generation. This configuration is only used to trigger old "     \
           "collections and does not change how regions are selected "       \
@@ -205,13 +205,13 @@
           "time from active application. Time is in milliseconds. "         \
           "Setting this to 0 disables the feature.")                        \
                                                                             \
-  product(uintx, ShenandoahGuaranteedOldGCInterval, 10*60*1000,  EXPERIMENTAL,  \
+  product(uintx, ShenandoahGuaranteedOldGCInterval, 10*60*1000, EXPERIMENTAL, \
           "Run a collection of the old generation at least this often. "    \
           "Heuristics may trigger collections more frequently. Time is in " \
           "milliseconds. Setting this to 0 disables the feature.")          \
                                                                             \
-  product(uintx, ShenandoahGuaranteedYoungGCInterval, 5*60*1000,  EXPERIMENTAL,  \
-          "Run a collection of the young generation at least this often. "    \
+  product(uintx, ShenandoahGuaranteedYoungGCInterval, 5*60*1000, EXPERIMENTAL, \
+          "Run a collection of the young generation at least this often. "  \
           "Heuristics may trigger collections more frequently. Time is in " \
           "milliseconds. Setting this to 0 disables the feature.")          \
                                                                             \
@@ -371,13 +371,13 @@
           "events.")                                                        \
           range(0,100)                                                      \
                                                                             \
-  product(uintx, ShenandoahMinYoungPercentage, 20,                                \
+  product(uintx, ShenandoahMinYoungPercentage, 20, EXPERIMENTAL,            \
           "The minimum percentage of the heap to use for the young "        \
           "generation. Heuristics will not adjust the young generation "    \
           "to be less than this.")                                          \
           range(0, 100)                                                     \
                                                                             \
-  product(uintx, ShenandoahMaxYoungPercentage, 80,                                \
+  product(uintx, ShenandoahMaxYoungPercentage, 80, EXPERIMENTAL,            \
           "The maximum percentage of the heap to use for the young "        \
           "generation. Heuristics will not adjust the young generation "    \
           "to be more than this.")                                          \
