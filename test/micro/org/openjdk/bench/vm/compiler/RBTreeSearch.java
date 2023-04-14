4cd22b606243642e0bb42ad8779682af9fb4e613
Kirill A. Korinsky <kirill@korins.ky>
2023-04-14 21:09:35 +0200

    Rename benchmark and add header

--- test/micro/org/openjdk/bench/vm/compiler/RBTreeSearch.java
+++ test/micro/org/openjdk/bench/vm/compiler/RBTreeSearch.java
@@ -1,8 +1,37 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
 package org.openjdk.bench.vm.compiler;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.util.concurrent.TimeUnit;
 
 /*
  * This benchmark is used as easy reproducer of JDK-8305995
@@ -13,8 +42,12 @@ import org.openjdk.jmh.annotations.State;
  * At the end it contains a tree serialized as lines, and
  * maxPattern which is used to search in this tree.
  */
-@State(Scope.Benchmark)
-public class JDK8305995 {
+@State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 4, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 4, time = 2, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 3)
+public class RBTreeSearch {
 
     private final Tree pattern;
 
@@ -25,7 +58,7 @@ public class JDK8305995 {
     private final int[] idxStack;
     private final Tree[] objStack;
 
-    public JDK8305995() {
+    public RBTreeSearch() {
         idxStack = new int[maxPattern];
 
         objStack = new Tree[maxPattern];
