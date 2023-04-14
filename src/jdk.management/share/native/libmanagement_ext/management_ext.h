9d98f728ce5462544d43dd60c5a3f1cc81c63f99
Jiangli Zhou <jianglizhou@google.com>
2023-04-14 10:08:18 -0700

    Add comment suggested by dholmes-ora.

--- src/jdk.management/share/native/libmanagement_ext/management_ext.h
+++ src/jdk.management/share/native/libmanagement_ext/management_ext.h
@@ -31,6 +31,9 @@
 #ifndef _MANAGEMENT_EXT_H_
 #define _MANAGEMENT_EXT_H_
 
+// These symbols are global in this library but need to be uniquely named to
+// avoid conflicts with same-named symbols in other native libraries, when
+// statically linking.
 extern const JmmInterface* jmm_interface_management_ext;
 extern jint jmm_version_management_ext;
 extern void throw_internal_error(JNIEnv* env, const char* msg);
