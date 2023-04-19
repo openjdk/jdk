624d0758d52e02e0773b41636e8cf3ebffc0da71
liach <liach@users.noreply.github.com>
2023-04-19 11:40:15 -0500

    8306457: Classfile API components implementations should not be exposed

--- src/java.base/share/classes/jdk/internal/classfile/components/CodeLocalsShifter.java
+++ src/java.base/share/classes/jdk/internal/classfile/components/CodeLocalsShifter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -19,25 +19,15 @@
  * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
  * or visit www.oracle.com if you need additional information or have any
  * questions.
- *
  */
 package jdk.internal.classfile.components;
 
 import java.lang.constant.MethodTypeDesc;
-import java.util.Arrays;
-
 import java.lang.reflect.AccessFlag;
 import jdk.internal.classfile.AccessFlags;
-import jdk.internal.classfile.CodeBuilder;
-import jdk.internal.classfile.CodeElement;
 import jdk.internal.classfile.CodeTransform;
-import jdk.internal.classfile.Signature;
-import jdk.internal.classfile.instruction.IncrementInstruction;
-import jdk.internal.classfile.instruction.LoadInstruction;
-import jdk.internal.classfile.instruction.LocalVariable;
-import jdk.internal.classfile.instruction.StoreInstruction;
 import jdk.internal.classfile.TypeKind;
-import jdk.internal.classfile.instruction.LocalVariableType;
+import jdk.internal.classfile.impl.CodeLocalsShifterImpl;
 
 /**
  * {@link CodeLocalsShifter} is a {@link CodeTransform} shifting locals to
@@ -45,11 +35,11 @@ import jdk.internal.classfile.instruction.LocalVariableType;
  * Locals pointing to the receiver or to method arguments slots are never shifted.
  * All locals pointing beyond the method arguments are re-indexed in order of appearance.
  */
-public sealed interface CodeLocalsShifter extends CodeTransform {
+public sealed interface CodeLocalsShifter extends CodeTransform permits CodeLocalsShifterImpl {
 
     /**
      * Creates a new instance of {@link CodeLocalsShifter}
-     * with fixed local slots calculated from provided method information
+     * with fixed local slots calculated from provided method information.
      * @param methodFlags flags of the method to construct {@link CodeLocalsShifter} for
      * @param methodDescriptor descriptor of the method to construct {@link CodeLocalsShifter} for
      * @return new instance of {@link CodeLocalsShifter}
@@ -59,66 +49,5 @@ public sealed interface CodeLocalsShifter extends CodeTransform {
         for (var param : methodDescriptor.parameterList())
             fixed += TypeKind.from(param).slotSize();
         return new CodeLocalsShifterImpl(fixed);
-    }
-
-    final static class CodeLocalsShifterImpl implements CodeLocalsShifter {
-
-        private int[] locals = new int[0];
-        private final int fixed;
-
-        private CodeLocalsShifterImpl(int fixed) {
-            this.fixed = fixed;
-        }
-
-        @Override
-        public void accept(CodeBuilder cob, CodeElement coe) {
-            switch (coe) {
-                case LoadInstruction li ->
-                    cob.loadInstruction(
-                            li.typeKind(),
-                            shift(cob, li.slot(), li.typeKind()));
-                case StoreInstruction si ->
-                    cob.storeInstruction(
-                            si.typeKind(),
-                            shift(cob, si.slot(), si.typeKind()));
-                case IncrementInstruction ii ->
-                    cob.incrementInstruction(
-                            shift(cob, ii.slot(), TypeKind.IntType),
-                            ii.constant());
-                case LocalVariable lv ->
-                    cob.localVariable(
-                            shift(cob, lv.slot(), TypeKind.fromDescriptor(lv.type().stringValue())),
-                            lv.name(),
-                            lv.type(),
-                            lv.startScope(),
-                            lv.endScope());
-                case LocalVariableType lvt ->
-                    cob.localVariableType(
-                            shift(cob, lvt.slot(),
-                                    (lvt.signatureSymbol() instanceof Signature.BaseTypeSig bsig)
-                                            ? TypeKind.fromDescriptor(bsig.signatureString())
-                                            : TypeKind.ReferenceType),
-                            lvt.name(),
-                            lvt.signature(),
-                            lvt.startScope(),
-                            lvt.endScope());
-                default -> cob.with(coe);
-            }
-        }
-
-        private int shift(CodeBuilder cob, int slot, TypeKind tk) {
-            if (tk == TypeKind.VoidType)  throw new IllegalArgumentException("Illegal local void type");
-            if (slot >= fixed) {
-                int key = 2*slot - fixed + tk.slotSize() - 1;
-                if (key >= locals.length) locals = Arrays.copyOf(locals, key + 20);
-                slot = locals[key] - 1;
-                if (slot < 0) {
-                    slot = cob.allocateLocal(tk);
-                    locals[key] = slot + 1;
-                    if (tk.slotSize() == 2) locals[key - 1] = slot + 1;
-                }
-            }
-            return slot;
-        }
     }
 }
