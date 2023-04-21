64de0729b9ce8a6dd36e0890e2dcf1ca5e254b1f
bchristi <brent.christian@oracle.com>
2023-04-21 16:47:20 -0700

    tweak

--- src/java.base/share/classes/java/lang/ref/Reference.java
+++ src/java.base/share/classes/java/lang/ref/Reference.java
@@ -485,7 +485,7 @@ public abstract sealed class Reference<T>
      * @apiNote
      * An unsuccessful {@code enqueue()} can occur if the GC collects the referent
      * before the {@code enqueue()} call. {@link #reachabilityFence(Object)} can
-     * prevent this, by keeping the referent strongly-reachable.
+     * prevent this, if used to keep the referent strongly-reachable.
      *
      * @return   {@code true} if this reference object was successfully
      *           enqueued; {@code false} if it was already enqueued or if
