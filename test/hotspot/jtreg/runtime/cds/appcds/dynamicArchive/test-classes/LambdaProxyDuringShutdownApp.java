16652c9851781a151659edea302ddc3920834a21
Calvin Cheung <calvin.cheung@oracle.com>
2023-04-21 17:27:31 +0000

    @iklam comments

--- test/hotspot/jtreg/runtime/cds/appcds/dynamicArchive/test-classes/LambdaProxyDuringShutdownApp.java
+++ test/hotspot/jtreg/runtime/cds/appcds/dynamicArchive/test-classes/LambdaProxyDuringShutdownApp.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,7 +34,7 @@ class Outer{
     }
 }
 
-class ShutdownHelper extends Thread {
+class MyShutdown extends Thread {
     public void run() {
         Outer.Inner inner = new Outer.Inner();
     }
@@ -43,7 +43,7 @@ class ShutdownHelper extends Thread {
 public class LambdaProxyDuringShutdownApp {
   public static void main(String[] args) throws Exception {
       Runtime r = Runtime.getRuntime();
-      r.addShutdownHook(new ShutdownHelper());
+      r.addShutdownHook(new MyShutdown());
       System.exit(0);
   }
 }
