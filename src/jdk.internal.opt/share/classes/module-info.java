163fcbfea3f73948af50650e5cee5e7e7d1e4c88
Christian Stein <cstein@openjdk.org>
2023-04-22 13:36:21 +0200

    Improve formatting

--- src/jdk.internal.opt/share/classes/module-info.java
+++ src/jdk.internal.opt/share/classes/module-info.java
@@ -29,6 +29,13 @@
  * @since 9
  */
 module jdk.internal.opt {
-    exports jdk.internal.joptsimple to jdk.jlink, jdk.jshell;
-    exports jdk.internal.opt to jdk.compiler, jdk.jartool, jdk.javadoc, jdk.jlink, jdk.jpackage;
+    exports jdk.internal.joptsimple to
+        jdk.jlink,
+        jdk.jshell;
+    exports jdk.internal.opt to
+        jdk.compiler,
+        jdk.jartool,
+        jdk.javadoc,
+        jdk.jlink,
+        jdk.jpackage;
 }
