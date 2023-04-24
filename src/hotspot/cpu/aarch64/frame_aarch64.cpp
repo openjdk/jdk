b0c908887962be52acbc5258ccb4a79c578cd9c1
Johannes Bechberger <johannes.bechberger@sap.com>
2023-04-24 11:41:43 +0200

    Remove errorneously added check

--- src/hotspot/cpu/aarch64/frame_aarch64.cpp
+++ src/hotspot/cpu/aarch64/frame_aarch64.cpp
@@ -268,9 +268,9 @@ bool frame::safe_for_sender(JavaThread *thread) {
     return false;
   }
 
-  // Will the pc we fetch be non-zero (which we'll find at the oldest frame) and readable
+  // Will the pc we fetch be non-zero (which we'll find at the oldest frame)
 
-  if (SafeFetchN(this->fp() + return_addr_offset, 0) == 0) return false;
+  if ((address) this->fp()[return_addr_offset] == NULL) return false;
 
   // could try and do some more potential verification of native frame if we could think of some...
 
