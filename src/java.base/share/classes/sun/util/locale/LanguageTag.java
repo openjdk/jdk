0133268354dd8fccbce0e14298496d17042551ef
Justin Lu <justin.lu@oracle.com>
2023-04-26 11:27:29 -0700

    Adjust imports in LanguageTag.java

--- src/java.base/share/classes/sun/util/locale/LanguageTag.java
+++ src/java.base/share/classes/sun/util/locale/LanguageTag.java
@@ -31,7 +31,14 @@
  */
 package sun.util.locale;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.Locale;
 
 public class LanguageTag {
     //
