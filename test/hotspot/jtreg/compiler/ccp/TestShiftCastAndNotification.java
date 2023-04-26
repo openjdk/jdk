69c265e242dee66aa24d340710b7de5e5c5f2247
Emanuel Peter <emanuel.peter@oracle.com>
2023-04-26 10:54:33 +0200

    corrected java whitespaces

--- test/hotspot/jtreg/compiler/ccp/TestShiftCastAndNotification.java
+++ test/hotspot/jtreg/compiler/ccp/TestShiftCastAndNotification.java
@@ -34,29 +34,29 @@
 package compiler.ccp;
 
 public class TestShiftCastAndNotification {
-  static int N;
-  static int iArrFld[] = new int[1];
-  static int test() {
-    int x = 1;
-    int sval = 4;
-    long useless[] = new long[N];
-    for (double d1 = 63; d1 > 2; d1 -= 2) {
-      for (double d2 = 3; 1 < d2; d2--) {
-        x <<= sval; // The LShiftI
-      }
-      // CastII probably somewhere in the loop structure
-      x &= 3; // The AndI
-      for (int i = 1; i < 3; i++) {
-        try {
-          x = iArrFld[0];
-          sval = 0;
-        } catch (ArithmeticException a_e) {
+    static int N;
+    static int iArrFld[] = new int[1];
+    static int test() {
+        int x = 1;
+        int sval = 4;
+        long useless[] = new long[N];
+        for (double d1 = 63; d1 > 2; d1 -= 2) {
+            for (double d2 = 3; 1 < d2; d2--) {
+                x <<= sval; // The LShiftI
+            }
+            // CastII probably somewhere in the loop structure
+            x &= 3; // The AndI
+            for (int i = 1; i < 3; i++) {
+                try {
+                    x = iArrFld[0];
+                    sval = 0;
+                } catch (ArithmeticException a_e) {
+                }
+            }
         }
-      }
+        return x;
     }
-    return x;
-  }
-  public static void main(String[] args) {
-      test();
-  }
+    public static void main(String[] args) {
+        test();
+    }
 }
