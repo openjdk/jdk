f867ba2e2d7b47b750e521832e167899640b0057
Mahendra Chhipa <mahendra.chhipa@oracle.com>
2023-04-27 18:18:30 +0100

    Implemented the review comments.

--- test/jdk/javax/xml/jaxp/datatype/8033980/SerializationTest.java
+++ test/jdk/javax/xml/jaxp/datatype/8033980/SerializationTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,15 +28,13 @@
  * @run testng SerializationTest
  */
 
-import java.io.File;
-import java.io.FileOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.util.Base64;
+import java.util.Formatter;
 
 import javax.xml.datatype.DatatypeConfigurationException;
 import javax.xml.datatype.DatatypeFactory;
@@ -44,7 +42,6 @@ import javax.xml.datatype.Duration;
 import javax.xml.datatype.XMLGregorianCalendar;
 
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -55,122 +52,254 @@ import org.testng.annotations.Test;
  */
 public class SerializationTest {
 
-    final String FILENAME_CAL = "_XMLGregorianCalendar.ser";
-    final String FILENAME_DURATION = "_Duration.ser";
-    String filePath;
-
-    {
-        filePath = System.getProperty("test.src");
-        if (filePath == null) {
-            //current directory
-            filePath = System.getProperty("user.dir");
-        }
-        filePath += File.separator;
-    }
     final String EXPECTED_CAL = "0001-01-01T00:00:00.0000000-05:00";
     final String EXPECTED_DURATION = "P1Y1M1DT1H1M1S";
     static String[] JDK = {"JDK6", "JDK7", "JDK8", "JDK9"};
 
+    private GregorianCalendarAndDurationSerData[] gregorianCalendarAndDurationSerData = {null, new JDK6GregorianCalendarAndDurationSerData(),
+    new JDK7GregorianCalendarAndDurationSerData(), new JDK8GregorianCalendarAndDurationSerData(), new JDK9GregorianCalendarAndDurationSerData()};
+
     /**
-     * Create the Data files.
+     * Create the Serialization Bytes using JDK under test.
      * @throws DatatypeConfigurationException
      * @throws IOException
+     * @throws ClassNotFoundException
      */
     @BeforeClass
-    public void setup() throws DatatypeConfigurationException, IOException {
+    public void setup() throws DatatypeConfigurationException, IOException, ClassNotFoundException {
         DatatypeFactory dtf = DatatypeFactory.newInstance();
-        for(String jdkVersion : JDK) {
-            if(!Files.exists(Path.of(filePath, jdkVersion+FILENAME_CAL ))) {
-                XMLGregorianCalendar c = dtf.newXMLGregorianCalendar(EXPECTED_CAL);
-                toFile((Serializable) c, filePath + jdkVersion + FILENAME_CAL);
+        XMLGregorianCalendar xmlGregorianCalendar = dtf.newXMLGregorianCalendar(EXPECTED_CAL);
 
-            }
-            if(!Files.exists(Path.of(filePath, jdkVersion+"t"+FILENAME_DURATION ))) {
-                Duration d = dtf.newDuration(EXPECTED_DURATION);
-                toFile((Serializable) d, filePath + jdkVersion + FILENAME_DURATION);
+        //Serialize the given xmlGregorianCalendar
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(xmlGregorianCalendar);
+        oos.flush();
+        oos.close();
+
+        // Now get a Base64 string representation of the serialized bytes.
+        final String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
+
+        // Generates the Java Pseudo code for base64 encoded string that can be cut & pasted into the test
+        final StringBuilder sb = new StringBuilder();
+        sb.append("    /**").append('\n');
+        sb.append("     * Base64 encoded string for XMLGregorianCalendar object.").append('\n');
+        sb.append("     * Java version: ").append(System.getProperty("java.version")).append('\n');
+        sb.append("     **/").append('\n');
+        sb.append("    private final String gregorianCalendarBase64 = ").append("\n          ");
+        final int last = base64.length() - 1;
+        for (int i=0; i<base64.length();i++) {
+            if (i%64 == 0) sb.append("\"");
+            sb.append(base64.charAt(i));
+            if (i%64 == 63 || i == last) {
+                sb.append("\"");
+                if (i == last) sb.append(";\n");
+                else sb.append("\n        + ");
             }
         }
-    }
+        System.out.println(sb);
 
-    /**
-     *Delete the data files.
-     * @throws IOException
-     */
-    @AfterClass
-    public void cleanup() throws IOException {
-        for(String jdkVersion : JDK) {
-            Files.deleteIfExists(Path.of(filePath, jdkVersion+FILENAME_CAL ));
-            Files.deleteIfExists(Path.of(filePath, jdkVersion+FILENAME_DURATION ));
+        // Generates the Java Pseudo code for byte array that can be cut & pasted into the test
+        byte [] bytes = baos.toByteArray();
+        StringBuilder sb2 = new StringBuilder(bytes.length * 5);
+        Formatter fmt = new Formatter(sb2);
+        fmt.format("    private final byte[] %s = {", "gregorianCalendarBytes");
+        final int linelen = 8;
+        for (int i = 0; i <bytes.length; i++) {
+            if (i % linelen == 0) {
+                fmt.format("%n        ");
+            }
+            fmt.format(" (byte) 0x%x,", bytes[i] & 0xff);
         }
+        fmt.format("%n    };%n");
+
+        System.out.println(sb2);
+
+        Duration duration = dtf.newDuration(EXPECTED_DURATION);
+
+        //Serialize the given xml Duration
+
+        final ByteArrayOutputStream baos2 = new ByteArrayOutputStream();
+        final ObjectOutputStream oos2 = new ObjectOutputStream(baos2);
+        oos2.writeObject(duration);
+        oos2.flush();
+        oos2.close();
+
+        // Now get a Base64 string representation of the serialized bytes.
+        final String base64dur = Base64.getEncoder().encodeToString(baos2.toByteArray());
+
+        // Generates the Java Pseudo code for Duration serialized bytes  Base64 encoded string  that can be cut &
+        // pasted into the test
+
+        final StringBuilder sbdur = new StringBuilder();
+        sbdur.append("    /**").append('\n');
+        sbdur.append("     * Base64 encoded string for Duration object.").append('\n');
+        sbdur.append("     * Java version: ").append(System.getProperty("java.version")).append('\n');
+        sbdur.append("     **/").append('\n');
+        sbdur.append("    private final String durationBase64 = ").append("\n          ");
+        final int lastdur = base64dur.length() - 1;
+        for (int i=0; i<base64dur.length();i++) {
+            if (i%64 == 0) sbdur.append("\"");
+            sbdur.append(base64dur.charAt(i));
+            if (i%64 == 63 || i == lastdur) {
+                sbdur.append("\"");
+                if (i == lastdur) sbdur.append(";\n");
+                else sbdur.append("\n        + ");
+            }
+        }
+
+        System.out.println(sbdur);
+
+        // Generates the Java Pseudo code for Duration serialized byte array that can be cut & pasted into the test
+        byte [] bytesdur = baos2.toByteArray();
+        StringBuilder sbdur2 = new StringBuilder(bytesdur.length * 5);
+        Formatter fmt2 = new Formatter(sbdur2);
+        fmt2.format("    private final byte[] %s = {", "durationBytes");
+        final int linelen2 = 8;
+        for (int i = 0; i <bytesdur.length; i++) {
+            if (i % linelen2 == 0) {
+                fmt2.format("%n        ");
+            }
+            fmt2.format(" (byte) 0x%x,", bytesdur[i] & 0xff);
+        }
+        fmt2.format("%n    };%n");
+
+        System.out.println(sbdur2);
+
+        // Create the Data for JDK under test.
+        gregorianCalendarAndDurationSerData[0] = new GregorianCalendarAndDurationSerData() {
+            @Override
+            public byte[] getGregorianCalendarByteArray() {
+                return baos.toByteArray();
+            }
+
+            @Override
+            public String getGregorianCalendarBase64() {
+                return base64;
+            }
+
+            @Override
+            public byte[] getDurationBytes() {
+                return baos2.toByteArray();
+            }
+
+            @Override
+            public String getDurationBase64() {
+                return base64dur;
+            }
+        };
     }
 
-
     /**
-     *Provide data for JDK version and Gregorian Calender
+     *Provide data for JDK version and Gregorian Calendar serialized bytes
      */
-    @DataProvider(name = "GregorianCalendar")
-    public Object [][]gregorianCalendarData() {
-        return new Object [][] {{JDK[0], EXPECTED_CAL}, {JDK[1], EXPECTED_CAL},
-                {JDK[2], EXPECTED_CAL}, {JDK[3], EXPECTED_CAL}};
+    @DataProvider(name = "GregorianCalendarData")
+    public Object [][] gregorianCalendarDataBytes() {
+        return new Object [][] {{System.getProperty("java.version"), gregorianCalendarAndDurationSerData[0], EXPECTED_CAL},
+                {JDK[0], gregorianCalendarAndDurationSerData[1], EXPECTED_CAL},
+                {JDK[1], gregorianCalendarAndDurationSerData[2], EXPECTED_CAL},
+                {JDK[2], gregorianCalendarAndDurationSerData[3], EXPECTED_CAL},
+                {JDK[3], gregorianCalendarAndDurationSerData[4], EXPECTED_CAL}};
     }
 
     /**
-     * Provide data for JDK version and Duration
+     *Provide data for JDK version and serialized Gregorian Calendar Base64 encoded string
      */
-    @DataProvider(name = "GregorianDuration")
-    public Object [][]gregorianDurationData() {
-        return new Object [][] {{JDK[0], EXPECTED_DURATION}, {JDK[1], EXPECTED_DURATION},
-                {JDK[2], EXPECTED_DURATION}, {JDK[3], EXPECTED_DURATION}};
+    @DataProvider(name = "GregorianCalendarDataBase64")
+    public Object [][] gregorianCalendarDataBase64() {
+        return new Object [][] {{System.getProperty("java.version"), gregorianCalendarAndDurationSerData[0], EXPECTED_CAL},
+                {JDK[2], gregorianCalendarAndDurationSerData[3], EXPECTED_CAL},
+                {JDK[3], gregorianCalendarAndDurationSerData[4], EXPECTED_CAL}};
+    }
+
+    /**
+     *Provide data for JDK version and Duration serialized bytes
+     */
+    @DataProvider(name = "DurationData")
+    public Object [][] DurationData() {
+        return new Object [][] {{System.getProperty("java.version"), gregorianCalendarAndDurationSerData[0], EXPECTED_DURATION},
+                {JDK[0], gregorianCalendarAndDurationSerData[1], EXPECTED_DURATION},
+                {JDK[1], gregorianCalendarAndDurationSerData[2], EXPECTED_DURATION},
+                {JDK[2], gregorianCalendarAndDurationSerData[3], EXPECTED_DURATION},
+                {JDK[3], gregorianCalendarAndDurationSerData[4], EXPECTED_DURATION}};
+    }
+
+    /**
+     *Provide data for JDK version and Duration Base64 encode serialized bytes string
+     */
+    @DataProvider(name = "DurationDataBase64")
+    public Object [][] DurationDataBase64() {
+        return new Object [][] {{System.getProperty("java.version"), gregorianCalendarAndDurationSerData[0], EXPECTED_DURATION},
+                {JDK[2], gregorianCalendarAndDurationSerData[3], EXPECTED_DURATION},
+                {JDK[3], gregorianCalendarAndDurationSerData[4], EXPECTED_DURATION}};
     }
 
     /**
      * verify serialization compatibility for XMLGregorianCalendar
      * @param javaVersion
+     * @param gcsd
      * @param gregorianDate
      * @throws IOException
      * @throws ClassNotFoundException
      */
-    @Test(dataProvider="GregorianCalendar")
-    public void testReadCal(String javaVersion, String gregorianDate) throws IOException, ClassNotFoundException {
-        XMLGregorianCalendar d1 = (XMLGregorianCalendar) fromFile(javaVersion + FILENAME_CAL);
-        Assert.assertEquals(d1.toString(), gregorianDate);
+    @Test(dataProvider="GregorianCalendarData")
+    public void testReadCalBytes(String javaVersion, GregorianCalendarAndDurationSerData gcsd, String gregorianDate) throws IOException,
+            ClassNotFoundException {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(gcsd.getGregorianCalendarByteArray());
+        final ObjectInputStream ois = new ObjectInputStream(bais);
+        final XMLGregorianCalendar xgc = (XMLGregorianCalendar) ois.readObject();
+        Assert.assertEquals(xgc.toString(), gregorianDate);
+    }
+
+    /**
+     * verify serialization compatibility for XMLGregorianCalendar
+     * @param javaVersion
+     * @param gcsd
+     * @param gregorianDate
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
+    @Test(dataProvider="GregorianCalendarDataBase64")
+    public void testReadCalBase64(String javaVersion, GregorianCalendarAndDurationSerData gcsd, String gregorianDate) throws IOException,
+            ClassNotFoundException {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(gcsd.getGregorianCalendarBase64()));
+        final ObjectInputStream ois = new ObjectInputStream(bais);
+        final XMLGregorianCalendar xgc = (XMLGregorianCalendar) ois.readObject();
+        Assert.assertEquals(xgc.toString(), gregorianDate);
     }
 
     /**
      * verify serialization compatibility for Duration
      * @param javaVersion
-     * @param gregorianDuration
+     * @param gcsd
+     * @param duration
      * @throws IOException
      * @throws ClassNotFoundException
      */
-    @Test(dataProvider = "GregorianDuration")
-    public void testReadDuration(String javaVersion, String gregorianDuration) throws IOException, ClassNotFoundException {
-        Duration d1 = (Duration) fromFile(javaVersion + FILENAME_DURATION);
-        Assert.assertEquals(d1.toString().toUpperCase(), gregorianDuration);
-    }
-
-    /**
-     * Read the object from a file.
-     */
-    private static Object fromFile(String filePath) throws IOException,
+    @Test(dataProvider = "DurationData")
+    public void testReadDurationBytes(String javaVersion, GregorianCalendarAndDurationSerData gcsd, String duration) throws IOException,
             ClassNotFoundException {
-        InputStream streamIn = SerializationTest.class.getResourceAsStream(
-            filePath);
-        ObjectInputStream objectinputstream = new ObjectInputStream(streamIn);
-        Object o = objectinputstream.readObject();
-        objectinputstream.close();
-        streamIn.close();
-        return o;
+        final ByteArrayInputStream bais = new ByteArrayInputStream(gcsd.getDurationBytes());
+        final ObjectInputStream ois = new ObjectInputStream(bais);
+        final Duration d1 = (Duration) ois.readObject();
+        Assert.assertEquals(d1.toString().toUpperCase(), duration);
     }
 
     /**
-     * Write the object to a file.
+     * verify serialization compatibility for Duration
+     * @param javaVersion
+     * @param gcsd
+     * @param duration
+     * @throws IOException
+     * @throws ClassNotFoundException
      */
-    private static void toFile(Serializable o, String filePath) throws IOException {
-        FileOutputStream fout = new FileOutputStream(filePath, true);
-        ObjectOutputStream oos = new ObjectOutputStream(fout);
-        oos.writeObject(o);
-        oos.close();
-        fout.close();
+    @Test(dataProvider = "DurationDataBase64")
+    public void testReadDurationBase64(String javaVersion, GregorianCalendarAndDurationSerData gcsd, String duration) throws IOException,
+            ClassNotFoundException {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(Base64.getDecoder().decode(gcsd.getDurationBase64()));
+        final ObjectInputStream ois = new ObjectInputStream(bais);
+        final Duration d1 = (Duration) ois.readObject();
+        Assert.assertEquals(d1.toString().toUpperCase(), duration);
     }
 }
