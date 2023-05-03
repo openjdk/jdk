acd5ec0e4cd466852e9365dfba3aedf474f1d575
Afshin Zafari <Afshin.zafari@oracle.com>
2023-05-03 14:53:17 +0200

    8305082: Remove finalize() from test/hotspot/jtreg/runtime/linkResolver/InterfaceObjectTest.java

--- test/hotspot/jtreg/runtime/linkResolver/InterfaceObjectTest.java
+++ test/hotspot/jtreg/runtime/linkResolver/InterfaceObjectTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -42,7 +42,7 @@ public class InterfaceObjectTest implements ICloneExtend {
         System.out.println("In InterfaceObjectTest's clone() method\n");
         return null;
     }
-
+    @SuppressWarnings("removal")
     public void finalize() throws Throwable {
         try {
             System.out.println("In InterfaceObjectTest's finalize() method\n");
