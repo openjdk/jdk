430e2bc9162b305ae5bd710c7217a80a55e335f0
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2023-05-04 18:22:20 +0200

    doc changes

--- src/java.desktop/share/classes/java/awt/peer/RobotPeer.java
+++ src/java.desktop/share/classes/java/awt/peer/RobotPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -127,4 +127,10 @@ public interface RobotPeer
     default boolean useAbsoluteCoordinates() {
         return false;
     }
+
+    /**
+     * Resets the stored screen data capture permission for a set of screens.
+     * Is a no-op if not supported by the platform.
+     */
+    default void resetScreenCapturePermission() {}
 }
