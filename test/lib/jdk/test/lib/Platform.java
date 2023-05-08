8c541047b0bc8178c4ecffdaefa67cab5a76110b
Goetz Lindenmaier <goetz@openjdk.org>
2023-05-08 08:23:15 +0000

    8286191: misc tests fail due to JDK-8285987
    Reviewed-by: mbaesken
    Backport-of: de8f4d01b234f5224a687dae5db52ab31247c2da

--- test/lib/jdk/test/lib/Platform.java
+++ test/lib/jdk/test/lib/Platform.java
@@ -23,8 +23,10 @@
 
 package jdk.test.lib;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
