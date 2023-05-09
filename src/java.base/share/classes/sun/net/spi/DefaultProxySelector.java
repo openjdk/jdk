0a4af486d2bd1489338193b352dab72fc0f3265a
Darragh Clarke <Darragh.clarke@oracle.com>
2023-05-09 11:24:00 +0100

    updated copyright year and cleaned up some formatting of imports

--- src/java.base/share/classes/sun/net/spi/DefaultProxySelector.java
+++ src/java.base/share/classes/sun/net/spi/DefaultProxySelector.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -377,7 +377,7 @@ public class DefaultProxySelector extends ProxySelector {
             if (disjunct.isEmpty())
                 continue;
             disjunctionEmpty = false;
-            String regex = disjunctToRegex(disjunct.toLowerCase(Locale.ROOT));
+            String regex = disjunctToRegex(disjunct.toLowerCase());
             joiner.add(regex);
         }
         return disjunctionEmpty ? null : Pattern.compile(joiner.toString());
--- src/java.base/share/classes/sun/net/www/MimeTable.java
+++ src/java.base/share/classes/sun/net/www/MimeTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1994, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1994, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -34,7 +34,11 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.net.FileNameMap;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Hashtable;
+import java.util.Properties;
+import java.util.StringTokenizer;
 
 public class MimeTable implements FileNameMap {
     /** Hash mark introducing a URI fragment */
