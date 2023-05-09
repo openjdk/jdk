5af9f9c4a46e27879309f43978613e14dd7f5237
Adam Sotona <asotona@openjdk.org>
2023-05-09 18:03:09 +0200

    fixed StackMapGenerator

--- src/java.base/share/classes/jdk/internal/classfile/impl/StackMapGenerator.java
+++ src/java.base/share/classes/jdk/internal/classfile/impl/StackMapGenerator.java
@@ -743,7 +743,7 @@ public final class StackMapGenerator {
     }
 
     private void processFieldInstructions(RawBytecodeHelper bcs) {
-        var desc = Util.fieldTypeSymbol((MemberRefEntry)cp.entryByIndex(bcs.getIndexU2())).nameAndType());
+        var desc = Util.fieldTypeSymbol(((MemberRefEntry)cp.entryByIndex(bcs.getIndexU2())).nameAndType());
         switch (bcs.rawCode) {
             case GETSTATIC ->
                 currentFrame.pushStack(desc);
