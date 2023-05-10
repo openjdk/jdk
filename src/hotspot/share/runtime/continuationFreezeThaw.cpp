c073342fe154256a4d890a6a0628297149cd96b2
Tobias Holenstein <tobias.holenstein@oracle.com>
2023-05-10 13:43:56 +0200

    comment

--- src/hotspot/share/runtime/continuationFreezeThaw.cpp
+++ src/hotspot/share/runtime/continuationFreezeThaw.cpp
@@ -244,6 +244,8 @@ static JRT_LEAF(intptr_t*, thaw(JavaThread* thread, int kind))
   // vFrameStreamCommon allocates Handles in RegisterMap for continuations.
   // JRT_ENTRY instead?
   ResetNoHandleMark rnhm;
+
+  // we might modify the code cache via BarrierSetNMethod::nmethod_entry_barrier
   MACOS_AARCH64_ONLY(ThreadWXEnable __wx(WXWrite, thread));
   return ConfigT::thaw(thread, (Continuation::thaw_kind)kind);
 JRT_END
