d8d922425481c747a6b28bf4c864222ad49ff369
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-05-12 11:08:33 -0700

    8307976: Modify assertion message

--- test/jdk/java/nio/file/Files/CreateDirectories.java
+++ test/jdk/java/nio/file/Files/CreateDirectories.java
@@ -127,6 +127,6 @@ public class CreateDirectories {
         Files.deleteIfExists(temp.getParent());
         temp.toFile().deleteOnExit();
         Path a = Files.createDirectories(temp);
-        assertTrue(a == temp, a + " should not be absolute");
+        assertTrue(a == temp);
     }
 }
