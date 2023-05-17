f655ccd1c74bb18000ef2789d26952055c2deba1
Matthias Baesken <mbaesken@openjdk.org>
2023-05-17 07:07:52 +0000

    8304760: Add 2 Microsoft TLS roots
    Reviewed-by: mdoerr
    Backport-of: c7e1df832837c2f19629cf0d5a5d3e65142ac208

--- test/jdk/sun/security/lib/cacerts/VerifyCACerts.java
+++ test/jdk/sun/security/lib/cacerts/VerifyCACerts.java
@@ -28,7 +28,7 @@
  *      8209452 8209506 8210432 8195793 8216577 8222089 8222133 8222137 8222136
  *      8223499 8225392 8232019 8234245 8233223 8225068 8225069 8243321 8243320
  *      8243559 8225072 8258630 8259312 8256421 8225081 8225082 8225083 8245654
- *      8305975
+ *      8305975 8304760
  * @summary Check root CA entries in cacerts file
  */
 import java.io.ByteArrayInputStream;
@@ -47,12 +47,12 @@ public class VerifyCACerts {
             + File.separator + "security" + File.separator + "cacerts";
 
     // The numbers of certs now.
-    private static final int COUNT = 91;
+    private static final int COUNT = 93;
 
     // SHA-256 of cacerts, can be generated with
     // shasum -a 256 cacerts | sed -e 's/../&:/g' | tr '[:lower:]' '[:upper:]' | cut -c1-95
     private static final String CHECKSUM
-            = "D6:11:EB:91:CF:C2:38:18:99:B7:36:E7:87:23:35:7B:CA:A0:F8:CB:1A:31:3B:43:38:B3:24:5F:60:45:DC:BC";
+            = "B0:B4:38:03:49:F5:1B:80:CE:76:0D:96:ED:75:53:8B:11:06:C0:E0:03:4D:72:41:F5:AB:1D:40:EA:B7:1C:EF";
 
     // Hex formatter to upper case with ":" delimiter
     private static final HexFormat HEX = HexFormat.ofDelimiter(":").withUpperCase();
@@ -243,6 +243,10 @@ public class VerifyCACerts {
                     "E3:B6:A2:DB:2E:D7:CE:48:84:2F:7A:C5:32:41:C7:B7:1D:54:14:4B:FB:40:C1:1F:3F:1D:0B:42:F5:EE:A1:2D");
             put("twcaglobalrootca [jdk]",
                     "59:76:90:07:F7:68:5D:0F:CD:50:87:2F:9F:95:D5:75:5A:5B:2B:45:7D:81:F3:69:2B:61:0A:98:67:2F:0E:1B");
+            put("microsoftecc2017 [jdk]",
+                    "35:8D:F3:9D:76:4A:F9:E1:B7:66:E9:C9:72:DF:35:2E:E1:5C:FA:C2:27:AF:6A:D1:D7:0E:8E:4A:6E:DC:BA:02");
+            put("microsoftrsa2017 [jdk]",
+                    "C7:41:F7:0F:4B:2A:8D:88:BF:2E:71:C1:41:22:EF:53:EF:10:EB:A0:CF:A5:E6:4C:FA:20:F4:18:85:30:73:E0");
         }
     };
 
