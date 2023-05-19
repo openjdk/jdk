b75e16031a7fbda9441f68e83d0c4319b82b1df0
Adam Sotona <asotona@users.noreply.github.com>
2023-05-19 16:09:37 +0200

    Apply suggestions from code review
    Co-authored-by: liach <7806504+liach@users.noreply.github.com>

--- src/java.base/share/classes/jdk/internal/classfile/impl/CodeBuilderFinalizerImpl.java
+++ src/java.base/share/classes/jdk/internal/classfile/impl/CodeBuilderFinalizerImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,7 +43,7 @@ public final class CodeBuilderFinalizerImpl {
                                           boolean compactForm,
                                           Label... externalLabels) {
 
-        var mapping = new HashMap<Label, Label>(externalLabels.length + 1);
+        var mapping = HashMap.<Label, Label>newHashMap(externalLabels.length + 1);
         for (var extLabel : externalLabels) {
             mapping.put(extLabel, null);
         }
