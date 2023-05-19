3ad7bcd586b05fd917be3474eb363365c808f327
Justin Lu <justin.lu@oracle.com>
2023-05-19 16:06:05 -0700

    Add test for new definition

---
+++ test/jdk/java/text/Format/DecimalFormat/MantissaDigits.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8159023
+ * @summary Confirm behavior of mantissa for scientific notation in Decimal Format
+ * @run junit MantissaDigits
+ */
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MantissaDigits {
+    private static final double[] numbers = {
+            1.1, 12.1, 123.1, 1234.1, 12345.1, 123456.1,
+            -1.1, -12.1, -123.1, -1234.1, -12345.1, -123456.1,
+            1, 12, 123, 1234, 12345, 123456, 1234567,
+            -1, -12, -123, -1234, -12345, -123456, -1234567,
+            1.1234, 1.1111, 1.412, 222.333, -771.2222
+
+            // The following numbers when formatted, may produce
+            // results that have leading or trailing 0s (which ultimately,
+            // are ignored, but were "included" as significant digits)
+            // 1.101013, 1.10011, 0.00101, 0.100, -1.00130, -0.00131
+            // ie: 0.00101 formatted with #0.000##E0 gives 10.10E-4
+            // which implies there was only "4" significant digits even
+            // if the mathematical expression produced 6 significant digits.
+            // This is because the leading (or trailing) (00)10.10E-4 was
+            // stripped, but when un-stripping them, the # of significant digits
+            // still matches that of the mathematical expression. Thus, these
+            // values cannot be tested via this test, but a manual comparison
+            // of the results while keeping in mind the stripping of leading
+            // or trailing zeroes still aligns to the mathematical expression.
+            };
+
+    @ParameterizedTest
+    @MethodSource("patterns")
+    public void testMantissaDefinition(String pattern) {
+        Locale.setDefault(Locale.ENGLISH);
+        DecimalFormat df = new DecimalFormat(pattern);
+        for (double number : numbers) {
+            // Count the significant digits in the pre-formatted number
+            int originalNumDigits = String.valueOf(number)
+                    .replace(".", "")
+                    .replace("-", "")
+                    .length();
+
+            if (wholeNumber(number)) {
+                // Trailing 0 should not be counted
+                originalNumDigits--;
+            }
+
+            // Format the number, then grab the significant
+            // digits inside the mantissa
+            String formattedNum = df.format(number);
+            int mantissaDigits = formattedNum
+                    .replace(".", "")
+                    .replace("E", "")
+                    .replace("-", "")
+                    .length() - 1;
+
+            // Count the zeroes and hash symbols in the
+            // pattern, to be used in the mathematical expression
+            int zeroes = (int) (pattern.chars()
+                    // ignore exponent zero
+                    .filter(ch -> ch == '0').count() - 1);
+            int hashes = (int) pattern.chars()
+                    .filter(ch -> ch == '#').count();
+
+
+            // Test the new definition of the Mantissa
+            Integer calculatedDigits = Math
+                    .min(Math.max(zeroes, originalNumDigits), (hashes+zeroes));
+            assertEquals(mantissaDigits, calculatedDigits,
+                    String.format("%s formatted with %s gives %s, and significant " +
+                            "digit count was %s, but the formula provided %s%n",
+                            number, pattern, formattedNum, mantissaDigits, calculatedDigits));
+        }
+    }
+
+    private static Boolean wholeNumber(double number) {
+        return (int) number == number;
+    }
+
+    private static Stream<Arguments> patterns() {
+        return Stream.of(
+                Arguments.of("#0.0##E0"),
+                Arguments.of("#00.00##E0"),
+                Arguments.of("#0.000##E0"),
+                Arguments.of("#00.000##E0"),
+                Arguments.of("#000.0##E0"),
+                Arguments.of("#000.00##E0"),
+                Arguments.of("#000.000##E0"),
+                Arguments.of("000.000E0"),
+                Arguments.of("#.##E0"),
+                Arguments.of("######.######E0"),
+                Arguments.of("####00.00######E0")
+        );
+    }
+}
