faa32ec6a959e082d070a0da6466ba1b1de51fa6
Justin Lu <justin.lu@oracle.com>
2023-05-22 15:57:40 -0700

    Review: Use Static dfs, error message, and exponent digit. Hard code min and max digits instead of calculating at runtime

--- test/jdk/java/text/Format/DecimalFormat/MantissaDigits.java
+++ test/jdk/java/text/Format/DecimalFormat/MantissaDigits.java
@@ -31,6 +31,7 @@
  */
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import java.util.stream.Stream;
 
@@ -48,12 +49,16 @@ public class MantissaDigits {
             -1, -12, -123, -1234, -12345, -123456, -1234567,
             1.1234, 1.1111, 1.412, 222.333, -771.2222
             };
+    private static final DecimalFormatSymbols dfs = new DecimalFormatSymbols(Locale.US);
+    private static final String ERRMSG = "%s formatted with %s gives %s, and " +
+            "significant digit count was %s, but the formula provided %s%n";
+    // Hard coded as 1, since all test patterns only have 1 exponent digit
+    private static final int EXPONENTDIGITS = 1;
 
     @ParameterizedTest
     @MethodSource("patterns")
-    public void testMantissaDefinition(String pattern) {
-        Locale.setDefault(Locale.ENGLISH);
-        DecimalFormat df = new DecimalFormat(pattern);
+    public void testMantissaDefinition(String pattern, int minDigits, int maxDigits) {
+        DecimalFormat df = new DecimalFormat(pattern, dfs);
         for (double number : numbers) {
             // Count the significant digits in the pre-formatted number
             int originalNumDigits = String.valueOf(number)
@@ -73,24 +78,13 @@ public class MantissaDigits {
                     .replace(".", "")
                     .replace("E", "")
                     .replace("-", "")
-                    .length() - 1;
-
-            // Count the zeroes and hash symbols in the
-            // pattern, to be used in the mathematical expression
-            int zeroes = (int) (pattern.chars()
-                    // ignore exponent zero
-                    .filter(ch -> ch == '0').count() - 1);
-            int hashes = (int) pattern.chars()
-                    .filter(ch -> ch == '#').count();
-
+                    .length() - EXPONENTDIGITS;
 
             // Test the new definition of the Mantissa
             Integer calculatedDigits = Math
-                    .min(Math.max(zeroes, originalNumDigits), (hashes+zeroes));
-            assertEquals(mantissaDigits, calculatedDigits,
-                    String.format("%s formatted with %s gives %s, and significant " +
-                            "digit count was %s, but the formula provided %s%n",
-                            number, pattern, formattedNum, mantissaDigits, calculatedDigits));
+                    .min(Math.max(minDigits, originalNumDigits), maxDigits);
+            assertEquals(mantissaDigits, calculatedDigits, String.format(ERRMSG,
+                    number, pattern, formattedNum, mantissaDigits, calculatedDigits));
         }
     }
 
@@ -100,17 +94,17 @@ public class MantissaDigits {
 
     private static Stream<Arguments> patterns() {
         return Stream.of(
-                Arguments.of("#0.0##E0"),
-                Arguments.of("#00.00##E0"),
-                Arguments.of("#0.000##E0"),
-                Arguments.of("#00.000##E0"),
-                Arguments.of("#000.0##E0"),
-                Arguments.of("#000.00##E0"),
-                Arguments.of("#000.000##E0"),
-                Arguments.of("000.000E0"),
-                Arguments.of("#.##E0"),
-                Arguments.of("######.######E0"),
-                Arguments.of("####00.00######E0")
+                Arguments.of("#0.0##E0", 2, 5),
+                Arguments.of("#00.00##E0", 4, 7),
+                Arguments.of("#0.000##E0", 4, 7),
+                Arguments.of("#00.000##E0", 5, 8),
+                Arguments.of("#000.0##E0", 4, 7),
+                Arguments.of("#000.00##E0", 5, 8),
+                Arguments.of("#000.000##E0", 6, 9),
+                Arguments.of("000.000E0", 6, 6),
+                Arguments.of("#.##E0", 0, 3),
+                Arguments.of("######.######E0", 0, 12),
+                Arguments.of("####00.00######E0", 4, 14)
         );
     }
 }
