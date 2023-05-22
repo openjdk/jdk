b912155b56ea3f1a99e81a304cb69e1643db590a
TheRealMDoerr <martin.doerr@sap.com>
2023-05-23 00:23:28 +0200

    Parameter Save Area is the correct name.

--- src/hotspot/cpu/ppc/downcallLinker_ppc.cpp
+++ src/hotspot/cpu/ppc/downcallLinker_ppc.cpp
@@ -158,9 +158,11 @@ void DowncallStubGenerator::generate() {
   // This may be a bit more than needed when HFA is used (see CallArranger.java).
   // (native_abi_reg_args is native_abi_minframe plus space for 8 argument register spill slots)
   assert(_abi._shadow_space_bytes == frame::native_abi_minframe_size, "expected space according to ABI");
-  // Note: For ABIv2, we only need (_input_registers.length() > 8) ? _input_registers.length() : 0
-  int register_save_area_slots = MAX2(_input_registers.length(), 8);
-  int allocated_frame_size = frame::native_abi_minframe_size + register_save_area_slots * BytesPerWord;
+  // The Parameter Save Area needs to be at least 8 slots for ABIv1.
+  // ABIv2 allows omitting it when all parameters can get passed in registers. We currently don't optimize this.
+  // For ABIv2, we only need (_input_registers.length() > 8) ? _input_registers.length() : 0
+  int parameter_save_area_slots = MAX2(_input_registers.length(), 8);
+  int allocated_frame_size = frame::native_abi_minframe_size + parameter_save_area_slots * BytesPerWord;
 
   bool should_save_return_value = !_needs_return_buffer && _needs_transition;
   RegSpiller out_reg_spiller(_output_registers);
