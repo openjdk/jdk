cb399ea88f54236f9b9f5cab068f5c730452d644
Angelos Bimpoudis <angelos.bimpoudis@oracle.com>
2023-05-22 14:38:29 +0200

    Introduce check in Flow

--- test/langtools/tools/javac/T8305582.java
+++ test/langtools/tools/javac/T8305582.java
@@ -25,8 +25,7 @@
  * @test
  * @bug 8305582
  * @summary Compiler crash when compiling record patterns with var
- * @enablePreview
- * @compile/fail/ref=T8305582.out -XDrawDiagnostics --enable-preview -source ${jdk.version} -XDshould-stop.at=FLOW T8305582.java
+ * @compile/fail/ref=T8305582.out -XDrawDiagnostics -XDshould-stop.at=FLOW T8305582.java
  */
 
 public class T8305582 {
