6b65e5754cc96c812892077881fc069e02fedc62
Andrey Turbanov <aturbanov@openjdk.org>
2023-05-22 10:27:13 +0000

    8305785: Avoid redundant HashMap.containsKey call in java.util.regex
    Reviewed-by: stsypanov, jpai

--- src/java.base/share/classes/java/util/regex/Pattern.java
+++ src/java.base/share/classes/java/util/regex/Pattern.java
@@ -2712,14 +2712,15 @@ loop:   for(int x=0, offset=0; x<nCodePoints; x++, offset+=len) {
             if (read() != '<')
                 throw error("\\k is not followed by '<' for named capturing group");
             String name = groupname(read());
-            if (!namedGroupsMap().containsKey(name))
+            Integer number = namedGroupsMap().get(name);
+            if (number == null)
                 throw error("named capturing group <" + name + "> does not exist");
             if (create) {
                 hasGroupRef = true;
                 if (has(CASE_INSENSITIVE))
-                    root = new CIBackRef(namedGroupsMap().get(name), has(UNICODE_CASE));
+                    root = new CIBackRef(number, has(UNICODE_CASE));
                 else
-                    root = new BackRef(namedGroupsMap().get(name));
+                    root = new BackRef(number);
             }
             return -1;
         case 'l':
