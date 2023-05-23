7088b1a9df33ab6af0d1b24cbb32a2d6692d4b6a
Toby Holenstein <tobias.holenstein@oracle.com>
2023-05-23 11:01:16 +0200

    Update src/hotspot/share/compiler/compilerOracle.cpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/compiler/compilerOracle.cpp
+++ src/hotspot/share/compiler/compilerOracle.cpp
@@ -1020,7 +1020,9 @@ void CompilerOracle::parse_compile_only(char* line) {
   LineCopy original(line);
   char* method_pattern;
   do {
-    if (line[0] == '\0') break;
+    if (line[0] == '\0') {
+      break;
+    }
     method_pattern = strtok_r(line, ",", &line);
     if (method_pattern != nullptr) {
       TypedMethodOptionMatcher* matcher = TypedMethodOptionMatcher::parse_method_pattern(method_pattern, error_buf, sizeof(error_buf));
