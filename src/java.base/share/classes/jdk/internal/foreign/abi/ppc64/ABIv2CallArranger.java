20f15352a3014042aa69f7cbfb67de0f7fdddb40
Martin Doerr <mdoerr@openjdk.org>
2023-05-24 08:38:34 +0000

    8303040: linux PPC64le: Implementation of Foreign Function & Memory API (Preview)
    Reviewed-by: jvernee, rrich

--- src/java.base/share/classes/jdk/internal/foreign/abi/ppc64/ABIv2CallArranger.java
+++ src/java.base/share/classes/jdk/internal/foreign/abi/ppc64/ABIv2CallArranger.java
@@ -1,11 +1,13 @@
 /*
- * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2023 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -21,30 +23,13 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+package jdk.internal.foreign.abi.ppc64;
 
-#ifndef CPU_PPC_VM_FOREIGN_GLOBALS_PPC_HPP
-#define CPU_PPC_VM_FOREIGN_GLOBALS_PPC_HPP
+import jdk.internal.foreign.abi.ppc64.CallArranger;
 
-#include "asm/macroAssembler.hpp"
-#include "utilities/growableArray.hpp"
-
-struct ABIDescriptor {
-  GrowableArray<Register> _integer_argument_registers;
-  GrowableArray<Register> _integer_return_registers;
-  GrowableArray<FloatRegister> _float_argument_registers;
-  GrowableArray<FloatRegister> _float_return_registers;
-
-  GrowableArray<Register> _integer_additional_volatile_registers;
-  GrowableArray<FloatRegister> _float_additional_volatile_registers;
-
-  int32_t _stack_alignment_bytes;
-  int32_t _shadow_space_bytes;
-
-  VMStorage _scratch1;
-  VMStorage _scratch2;
-
-  bool is_volatile_reg(Register reg) const;
-  bool is_volatile_reg(FloatRegister reg) const;
-};
-
-#endif // CPU_PPC_VM_FOREIGN_GLOBALS_PPC_HPP
+/**
+ * PPC64 CallArranger specialized for ABI v2.
+ */
+public class ABIv2CallArranger extends CallArranger {
+    // Currently no specific content, but CallArranger detects usage of ABIv2 for this class.
+}
