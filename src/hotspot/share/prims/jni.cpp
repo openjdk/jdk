3d32a20e8921540f148adb9f8b7bd6a46c07d08f
Aleksey Shipilev <shade@openjdk.org>
2023-05-26 10:52:43 +0000

    8286331: jni_GetStringUTFChars() uses wrong heap allocator
    Backport-of: b0d2b0a3553cbb2db23d543b98ab1401b3dbfa91

--- src/hotspot/share/prims/jni.cpp
+++ src/hotspot/share/prims/jni.cpp
@@ -2234,7 +2234,7 @@ JNI_ENTRY(const char*, jni_GetStringUTFChars(JNIEnv *env, jstring string, jboole
   if (s_value != NULL) {
     size_t length = java_lang_String::utf8_length(java_string, s_value);
     /* JNI Specification states return NULL on OOM */
-    result = AllocateHeap(length + 1, mtInternal, 0, AllocFailStrategy::RETURN_NULL);
+    result = AllocateHeap(length + 1, mtInternal, AllocFailStrategy::RETURN_NULL);
     if (result != NULL) {
       java_lang_String::as_utf8_string(java_string, s_value, result, (int) length + 1);
       if (isCopy != NULL) {
