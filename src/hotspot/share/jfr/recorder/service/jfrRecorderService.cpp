9a74bd327b031662a8c10a9669190f14cdc84326
mgronlun <markus.gronlund@oracle.com>
2023-05-26 12:59:29 +0200

    variable rename

--- src/hotspot/share/jfr/recorder/service/jfrRecorderService.cpp
+++ src/hotspot/share/jfr/recorder/service/jfrRecorderService.cpp
@@ -132,18 +132,18 @@ class ClearObjectAllocationSampling : public ThreadClosure {
 };
 
 template <typename Iterator>
-static inline void iterate(Iterator& it, ClearObjectAllocationSampling& robs) {
+static inline void iterate(Iterator& it, ClearObjectAllocationSampling& coas) {
   while (it.has_next()) {
-    robs.do_thread(it.next());
+    coas.do_thread(it.next());
   }
 }
 
 static void clear_object_allocation_sampling() {
-  ClearObjectAllocationSampling robs;
+  ClearObjectAllocationSampling coas;
   JfrJavaThreadIterator jit;
-  iterate(jit, robs);
+  iterate(jit, coas);
   JfrNonJavaThreadIterator njit;
-  iterate(njit, robs);
+  iterate(njit, coas);
 }
 
 template <typename Instance, size_t(Instance::*func)()>
