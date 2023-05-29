c51b50d33de9434d7a2711583e2e416011c24553
Adam Sotona <asotona@users.noreply.github.com>
2023-05-29 09:59:41 +0200

    Update src/java.base/share/classes/jdk/internal/classfile/impl/EntryMap.java
    Co-authored-by: Andrey Turbanov <turbanoff@gmail.com>

--- src/java.base/share/classes/jdk/internal/classfile/impl/EntryMap.java
+++ src/java.base/share/classes/jdk/internal/classfile/impl/EntryMap.java
@@ -174,7 +174,7 @@ public abstract class EntryMap<T> {
     }
 
     public static long nextPowerOfTwo( long x ) {
-        return 1l << -Long.numberOfLeadingZeros(x - 1);
+        return 1L << -Long.numberOfLeadingZeros(x - 1);
     }
 
     public static int arraySize( final int expected, final float f ) {
