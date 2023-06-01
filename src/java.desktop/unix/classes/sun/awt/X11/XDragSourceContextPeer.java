e9b0cb55ae7ebcbe802b8c8adbbf4e9d24959892
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2023-06-01 16:58:57 +0200

    initial

--- src/java.desktop/unix/classes/sun/awt/X11/XDragSourceContextPeer.java
+++ src/java.desktop/unix/classes/sun/awt/X11/XDragSourceContextPeer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,6 +27,7 @@ package sun.awt.X11;
 
 import java.awt.Component;
 import java.awt.Cursor;
+import java.awt.Toolkit;
 import java.awt.Window;
 
 import java.awt.datatransfer.DataFlavor;
@@ -393,6 +394,42 @@ public final class XDragSourceContextPeer
     }
 
     /**
+     * Our X11 code expects the drop target window to be a top level window
+     * and to have the XA_WM_STATE property.
+     * This is not true when performing drag and drop from XWayland
+     * to a native Wayland application.
+     * In this case XWayland creates a dummy window with only one property,
+     * XdndAware.
+     *
+     * @param window to test
+     * @return true if window has XdndAware property when running under Wayland
+     * property
+     */
+    private static boolean isXWaylandDndAwareWindow(long window) {
+        if (Toolkit.getDefaultToolkit() instanceof SunToolkit sunToolkit
+                && !sunToolkit.isRunningOnWayland()) {
+            return false;
+        }
+
+        WindowPropertyGetter wpg =
+                new WindowPropertyGetter(window,
+                        XDnDConstants.XA_XdndAware,
+                        0, 1, false,
+                        XConstants.AnyPropertyType);
+
+        try {
+            int status = wpg.execute(XErrorHandler
+                    .IgnoreBadWindowHandler.getInstance());
+
+            return status == XConstants.Success
+                    && wpg.getData() != 0
+                    && wpg.getActualType() == XAtom.XA_ATOM;
+        } finally {
+            wpg.dispose();
+        }
+    }
+
+    /**
      * Returns the client window under the specified root subwindow.
      */
     private static long findClientWindow(long window) {
@@ -406,6 +443,10 @@ public final class XDragSourceContextPeer
             if (win != 0) {
                 return win;
             }
+        }
+
+        if (isXWaylandDndAwareWindow(window)) {
+            return window;
         }
 
         return 0;
