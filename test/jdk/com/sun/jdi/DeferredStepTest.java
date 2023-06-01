24dfa505cf23702268461ad1a58d3556e6f37e7d
Chris Plummer <chris.plummer@oracle.com>
2023-06-01 23:00:15 +0000

    Use join() to make sure test threads have exited

--- test/jdk/com/sun/jdi/DeferredStepTest.java
+++ test/jdk/com/sun/jdi/DeferredStepTest.java
@@ -74,8 +74,17 @@ class DeferredStepTestTarg {
 
         jj1 obj1 = new jj1();
         jj2 obj2 = new jj2();
-        TestScaffold.newThread(obj1, "jj1").start();
-        TestScaffold.newThread(obj2, "jj2").start();
+        Thread jj1 = TestScaffold.newThread(obj1, "jj1");
+        Thread jj2 = TestScaffold.newThread(obj2, "jj2");
+        jj1.start();
+        jj2.start();
+        // Threads might be deamon threads, so wait here for them to complete.
+        try {
+            jj1.join();
+            jj2.join();
+        } catch (InterruptedException ie) {
+            throw new RuntimeException(ie);
+        }
     }
 }
 
