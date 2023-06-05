cf12a0ba4cae5bf19e8aa734e434a592abc92dd6
Serguei Spitsyn <serguei.spitsyn@oracle.com>
2023-06-05 18:36:10 +0000

    review: add real java agent to TestJcmdNoAgentLoad

--- test/jdk/sun/tools/jcmd/TestJcmdNoAgentLoad.java
+++ test/jdk/sun/tools/jcmd/TestJcmdNoAgentLoad.java
@@ -28,18 +28,23 @@ import java.lang.management.ManagementFactory;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 import java.util.List;
 
 import com.sun.management.HotSpotDiagnosticMXBean;
 import com.sun.management.VMOption;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.Utils;
+import jdk.test.lib.util.JarUtils;
 
 /*
  * @test id=default
  * @bug 8304438
  * @summary JVMTI.agent_load should obey EnableDynamicAgentLoading (by default)
+ * @modules jdk.attach jdk.jcmd
  * @library /test/lib
+ * @build JavaAgent
  * @run main/othervm TestJcmdNoAgentLoad
  */
 
@@ -47,7 +52,9 @@ import jdk.test.lib.Utils;
  * @test id=disabled
  * @bug 8304438
  * @summary JVMTI.agent_load should obey EnableDynamicAgentLoading (disabled)
+ * @modules jdk.attach jdk.jcmd
  * @library /test/lib
+ * @build JavaAgent
  * @run main/othervm -XX:-EnableDynamicAgentLoading TestJcmdNoAgentLoad
  */
 
@@ -55,7 +62,9 @@ import jdk.test.lib.Utils;
  * @test id=enabled
  * @bug 8304438
  * @summary JVMTI.agent_load should obey EnableDynamicAgentLoading (enabled)
+ * @modules jdk.attach jdk.jcmd
  * @library /test/lib
+ * @build JavaAgent
  * @run main/othervm -XX:+EnableDynamicAgentLoading TestJcmdNoAgentLoad
  */
 
@@ -65,23 +74,39 @@ public class TestJcmdNoAgentLoad {
     // is expected before an attempt to load the Agent.jar.
     private static final String[] CMD = new String[] { "JVMTI.agent_load", "Agent.jar" };
     private static final String PTRN = "Dynamic agent loading is not enabled";
-    private static boolean enableDynLoad = true;
+    private static boolean dynamicLoadongEnabled = true;
+    private static final String TEST_CLASSES = System.getProperty("test.classes");
+    private static String javaAgent;
 
     static {
+        // get VM option EnableDynamicAgentLoading value
         HotSpotDiagnosticMXBean bean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class);
-        VMOption enableDynLoadOpt = bean.getVMOption("EnableDynamicAgentLoading");
-        enableDynLoad = enableDynLoadOpt.getValue().equals("true");
+        VMOption dynamicLoadongEnabledOpt = bean.getVMOption("EnableDynamicAgentLoading");
+        dynamicLoadongEnabled = dynamicLoadongEnabledOpt.getValue().equals("true");
     }
 
     public static void main(String[] args) throws Exception {
-        testNoAgentLoad(CMD);
+        setup();
+        testNoAgentLoad(new String[] { "JVMTI.agent_load", javaAgent });
+    }
+
+    private static void setup() throws Exception {
+        // create JAR file with Java agent
+        Manifest man = new Manifest();
+        Attributes attrs = man.getMainAttributes();
+        attrs.put(Attributes.Name.MANIFEST_VERSION, "1.0");
+        attrs.put(new Attributes.Name("Agent-Class"), "JavaAgent");
+        Path jarfile = Path.of("javaagent.jar");
+        Path classes = Path.of(TEST_CLASSES);
+        JarUtils.createJarFile(jarfile, man, classes, Path.of("JavaAgent.class"));
+        javaAgent = jarfile.toString();
     }
 
     private static void testNoAgentLoad(String... jcmdArgs) throws Exception {
         OutputAnalyzer output = JcmdBase.jcmd(jcmdArgs);
 
         output.shouldHaveExitValue(0);
-        if (enableDynLoad) {
+        if (dynamicLoadongEnabled) {
             output.shouldNotContain(PTRN);
         } else {
             output.shouldContain(PTRN);
