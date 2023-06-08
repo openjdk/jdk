6d233e90416ee7bcd908851309e482a5f759ccf7
Guoxiong Li <lgxbslgx@gmail.com>
2023-06-08 14:30:48 +0800

    Remove Parallel GC in 'WB_FullGC'

--- src/hotspot/share/prims/whitebox.cpp
+++ src/hotspot/share/prims/whitebox.cpp
@@ -1445,12 +1445,12 @@ WB_END
 WB_ENTRY(void, WB_FullGC(JNIEnv* env, jobject o))
   Universe::heap()->soft_ref_policy()->set_should_clear_all_soft_refs(true);
   Universe::heap()->collect(GCCause::_wb_full_gc);
-#if defined(INCLUDE_G1GC) || defined(INCLUDE_PARALLELGC) || defined(INCLUDE_SERIALGC)
-  if (UseG1GC || UseParallelGC || UseSerialGC) {
-    // Needs to be cleared explicitly for G1, Parallel and Serial GC.
+#if defined(INCLUDE_G1GC) || defined(INCLUDE_SERIALGC)
+  if (UseG1GC || UseSerialGC) {
+    // Needs to be cleared explicitly for G1 and Serial GC.
     Universe::heap()->soft_ref_policy()->set_should_clear_all_soft_refs(false);
   }
-#endif // INCLUDE_G1GC || INCLUDE_PARALLELGC || INCLUDE_SERIALGC
+#endif // INCLUDE_G1GC || INCLUDE_SERIALGC
 WB_END
 
 WB_ENTRY(void, WB_YoungGC(JNIEnv* env, jobject o))
