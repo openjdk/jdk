6592c9c4a36e59439d66a809a108f02fd05d8ea4
Toby Holenstein <tobias.holenstein@oracle.com>
2023-06-08 16:20:44 +0200

    Update TestStableFloat.java

--- test/hotspot/jtreg/compiler/stable/TestStableFloat.java
+++ test/hotspot/jtreg/compiler/stable/TestStableFloat.java
@@ -30,23 +30,23 @@
  * @build jdk.test.whitebox.WhiteBox
  *
  * @run main/bootclasspath/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xcomp
- *                                 -XX:CompileOnly=*TestStableFloat::get*
+ *                                 -XX:CompileOnly=*TestStableFloat*::get*
  *                                 -XX:-TieredCompilation
  *                                 -XX:+FoldStableValues
  *                                 compiler.stable.TestStableFloat
  * @run main/bootclasspath/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xcomp
- *                                 -XX:CompileOnly=*TestStableFloat::get*
+ *                                 -XX:CompileOnly=*TestStableFloat*::get*
  *                                 -XX:-TieredCompilation
  *                                 -XX:-FoldStableValues
  *                                 compiler.stable.TestStableFloat
  *
  * @run main/bootclasspath/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xcomp
- *                                 -XX:CompileOnly=*TestStableFloat::get*
+ *                                 -XX:CompileOnly=*TestStableFloat*::get*
  *                                 -XX:+TieredCompilation -XX:TieredStopAtLevel=1
  *                                 -XX:+FoldStableValues
  *                                 compiler.stable.TestStableFloat
  * @run main/bootclasspath/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xcomp
- *                                 -XX:CompileOnly=*TestStableFloat::get*
+ *                                 -XX:CompileOnly=*TestStableFloat*::get*
  *                                 -XX:+TieredCompilation -XX:TieredStopAtLevel=1
  *                                 -XX:-FoldStableValues
  *                                 compiler.stable.TestStableFloat
