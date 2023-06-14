cb2f86dedc2b425d2b1193ff3dd98e09b4f7e81c
Alexey Ivanov <alexey.ivanov@oracle.com>
2023-06-14 20:57:41 +0100

    8297923: Add the bugid to the test

--- test/jdk/java/awt/ScrollPane/ScrollPaneExtraScrollBar.java
+++ test/jdk/java/awt/ScrollPane/ScrollPaneExtraScrollBar.java
@@ -23,7 +23,7 @@
 
 /*
   @test
-  @bug 4152524
+  @bug 4152524 8310054
   @requires os.family=="windows"
   @summary Test that scroll pane doesn't have scroll bars visible when it is
   shown for the first time with SCROLLBARS_AS_NEEDED style
