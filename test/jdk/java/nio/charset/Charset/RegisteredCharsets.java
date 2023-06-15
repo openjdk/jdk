cd9ddb76dcb171928f734a101c85b372a61dc694
Justin Lu <justin.lu@oracle.com>
2023-06-15 10:53:30 -0700

    Remove unusued JUnit import in RegisteredCharsets.java

--- test/jdk/java/nio/charset/Charset/RegisteredCharsets.java
+++ test/jdk/java/nio/charset/Charset/RegisteredCharsets.java
@@ -33,7 +33,6 @@
 import java.nio.charset.Charset;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
