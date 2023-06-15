cbe0111f505244ecc02943413f0bf770b119a79e
Damon Fenacci <damon.fenacci@oracle.com>
2023-06-15 12:35:04 +0200

    JDK-8310020: remove unneeded mr ppc instruction

--- src/hotspot/cpu/ppc/c1_Runtime1_ppc.cpp
+++ src/hotspot/cpu/ppc/c1_Runtime1_ppc.cpp
@@ -553,7 +553,6 @@ OopMapSet* Runtime1::generate_code_for(StubID id, StubAssembler* sasm) {
         __ set_info("unwind_exception", dont_gc_arguments);
 
         if (AbortVMOnException) {
-          __ mr(R11_scratch1, Rexception);
           save_live_registers(sasm);
           __ call_VM_leaf(CAST_FROM_FN_PTR(address, check_abort_on_vm_exception), Rexception);
           restore_live_registers(sasm, noreg, noreg);
