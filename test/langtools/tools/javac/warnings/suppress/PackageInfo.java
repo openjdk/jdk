01369a616b815e3804bf04c2763c1e6eb1d75d25
Goetz Lindenmaier <goetz@openjdk.org>
2023-06-19 07:04:05 +0000

    8285756: clean up use of bad arguments for `@clean` in langtools tests
    Backport-of: 3312d8c620003125a88540fd5b2bacff34732ade

--- test/langtools/tools/javac/warnings/suppress/PackageInfo.java
+++ test/langtools/tools/javac/warnings/suppress/PackageInfo.java
@@ -25,6 +25,6 @@
  * @test
  * @bug 8021112
  * @summary Verify that deprecated warnings are printed correctly for package-info.java
- * @clean pack.package-info pack.DeprecatedClass
+ * @clean pack.*
  * @compile/ref=PackageInfo.out -source 8 -XDrawDiagnostics -Xlint:deprecation,-options pack/package-info.java pack/DeprecatedClass.java
  */
