e0c96436f634f871103a806577079558b66d9dc1
Martin Doerr <mdoerr@openjdk.org>
2023-06-23 13:08:06 +0000

    8306281: function isWsl() returns false on WSL2
    Backport-of: 9b0baa10b9140045aa3a49b03e685dcd3ea7c623

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -1477,7 +1477,7 @@ var getVersionNumbers = function () {
 var isWsl = function (input) {
     return ( input.build_osenv == "wsl"
              || (input.build_os == "linux"
-                 && java.lang.System.getProperty("os.version").contains("Microsoft")));
+                 && java.lang.System.getProperty("os.version").toLowerCase().contains("microsoft")));
 }
 
 var error = function (s) {
