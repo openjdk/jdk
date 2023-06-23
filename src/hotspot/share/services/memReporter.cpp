52fb263024bb6af547a05cb3ef6438742e0f6c3b
Martin Doerr <mdoerr@openjdk.org>
2023-06-23 13:09:10 +0000

    8306428: RunThese30M.java crashed with assert(early->flag() == current->flag() || early->flag() == mtNone)
    Reviewed-by: mbaesken
    Backport-of: ca54f4e007ab0f13bec9aaf995d34c0ab3ba6452

--- src/hotspot/share/services/memReporter.cpp
+++ src/hotspot/share/services/memReporter.cpp
@@ -731,6 +731,13 @@ void MemDetailDiffReporter::diff_virtual_memory_sites() const {
       } else if (compVal > 0) {
         old_virtual_memory_site(early_site);
         early_site = early_itr.next();
+      } else if (early_site->flag() != current_site->flag()) {
+        // This site was originally allocated with one flag, then released,
+        // then re-allocated at the same site (as far as we can tell) with a different flag.
+        old_virtual_memory_site(early_site);
+        early_site = early_itr.next();
+        new_virtual_memory_site(current_site);
+        current_site = current_itr.next();
       } else {
         diff_virtual_memory_site(early_site, current_site);
         early_site   = early_itr.next();
@@ -793,7 +800,6 @@ void MemDetailDiffReporter::old_virtual_memory_site(const VirtualMemoryAllocatio
 
 void MemDetailDiffReporter::diff_virtual_memory_site(const VirtualMemoryAllocationSite* early,
   const VirtualMemoryAllocationSite* current) const {
-  assert(early->flag() == current->flag(), "Should be the same");
   diff_virtual_memory_site(current->call_stack(), current->reserved(), current->committed(),
     early->reserved(), early->committed(), current->flag());
 }
