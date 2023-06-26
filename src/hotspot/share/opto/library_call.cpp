5763fde8430431593b29cd6b8045c89c245d1bd8
Eric Nothum <eric.nothum@oracle.com>
2023-06-26 09:30:49 +0200

    8307625: moved declaration of t inside the assert to avoid dead code in the product version.

--- src/hotspot/share/opto/library_call.cpp
+++ src/hotspot/share/opto/library_call.cpp
@@ -4274,8 +4274,7 @@ LibraryCallKit::generate_method_call(vmIntrinsics::ID method_id, bool is_virtual
     slow_call = new CallStaticJavaNode(C, tf,
                            SharedRuntime::get_resolve_static_call_stub(), method);
   } else if (is_virtual) {
-    const Type* t = gvn().type(argument(0));
-    assert(!t->maybe_null(), "should not be null");
+    assert(!gvn().type(argument(0))->maybe_null(), "should not be null");
     int vtable_index = Method::invalid_vtable_index;
     if (UseInlineCaches) {
       // Suppress the vtable call
@@ -4291,8 +4290,7 @@ LibraryCallKit::generate_method_call(vmIntrinsics::ID method_id, bool is_virtual
                           SharedRuntime::get_resolve_virtual_call_stub(),
                           method, vtable_index);
   } else {  // neither virtual nor static:  opt_virtual
-    const Type* t = gvn().type(argument(0));
-    assert(!t->maybe_null(), "should not be null");
+    assert(!gvn().type(argument(0))->maybe_null(), "should not be null");
     slow_call = new CallStaticJavaNode(C, tf,
                                 SharedRuntime::get_resolve_opt_virtual_call_stub(), method);
     slow_call->set_optimized_virtual(true);
