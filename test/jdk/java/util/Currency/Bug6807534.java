337b8ae628aa493cbfed24d7f6eb701470633918
Justin Lu <justin.lu@oracle.com>
2023-06-26 17:12:17 -0700

    Refactor Bug6807534.java

--- test/jdk/java/util/Currency/Bug6807534.java
+++ test/jdk/java/util/Currency/Bug6807534.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,53 +20,70 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 /*
  * @test
  * @bug 6807534
  * @summary check whether the default implementation of
  *    CurrencNameProvider.getDisplayName(String, Locale) throws appropriate
  *    exceptions when necessary.
+ * @run junit Bug6807534
  */
 
 import java.util.Locale;
 import java.util.spi.CurrencyNameProvider;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class Bug6807534 {
 
     static final CurrencyNameProvider cnp = new CurrencyNameProviderImpl();
 
-    public static void main(String[] args) throws Exception {
-        // test for NullPointerException (currencyCode)
-        try {
-            cnp.getDisplayName(null, Locale.US);
-            throwException("NPE was not thrown with null currencyCode");
-        } catch (NullPointerException npe) {}
-
-        // test for NullPointerException (locale)
-        try {
-            cnp.getDisplayName("USD", null);
-            throwException("NPE was not thrown with null locale");
-        } catch (NullPointerException npe) {}
-
-        // test for IllegalArgumentException (illegal currencyCode)
-        try {
-            cnp.getDisplayName("INVALID", Locale.US);
-            throwException("IllegalArgumentException was not thrown with invalid currency code");
-        } catch (IllegalArgumentException iae) {}
-        try {
-            cnp.getDisplayName("inv", Locale.US);
-            throwException("IllegalArgumentException was not thrown with invalid currency code");
-        } catch (IllegalArgumentException iae) {}
-
-        // test for IllegalArgumentException (non-supported locale)
-        try {
-            cnp.getDisplayName("USD", Locale.JAPAN);
-            throwException("IllegalArgumentException was not thrown with non-supported locale");
-        } catch (IllegalArgumentException iae) {}
+    /**
+     * Tests that the currency name provider throws a NullPointerException
+     * under the expected circumstances.
+     */
+    @ParameterizedTest
+    @MethodSource("nullArgProvider")
+    public void NPETest(String currencyCode, Locale locale, String err) {
+        assertThrows(NullPointerException.class,
+                () -> cnp.getDisplayName(currencyCode, locale), err);
     }
 
-    static void throwException(String msg) {
-        throw new RuntimeException("test failed. "+msg);
+    /**
+     * Tests that the currency name provider throws a IllegalArgumentException
+     * under the expected circumstances.
+     */
+    @ParameterizedTest
+    @MethodSource("illegalArgProvider")
+    public void IAETest(String currencyCode, Locale locale, String err) {
+        assertThrows(IllegalArgumentException.class,
+                () -> cnp.getDisplayName(currencyCode, locale), err);
+    }
+
+    private static Stream<Arguments> nullArgProvider() {
+        return Stream.of(
+                Arguments.of(null, Locale.US,
+                        "NPE was not thrown with null currencyCode"),
+                Arguments.of("USD", null,
+                        "NPE was not thrown with null locale")
+        );
+    }
+
+    private static Stream<Arguments> illegalArgProvider() {
+        return Stream.of(
+                Arguments.of("INVALID", Locale.US,
+                        "IAE was not thrown with invalid currency code"),
+                Arguments.of("inv", Locale.US,
+                        "IAE was not thrown with invalid currency code"),
+                Arguments.of("USD", Locale.JAPAN,
+                        "IllegalArgumentException was not thrown with non-supported locale")
+        );
     }
 
     static class CurrencyNameProviderImpl extends CurrencyNameProvider {
