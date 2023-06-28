9325d759bb7c110550177db898b2fbe942cbb205
Jatin Bhateja <jatin.bhateja@intel.com>
2023-06-28 22:51:40 +0530

    8309531: Incorrect result with unwrapped iotaShuffle.

--- test/hotspot/jtreg/compiler/vectorapi/TestVectorShuffleIota.java
+++ test/hotspot/jtreg/compiler/vectorapi/TestVectorShuffleIota.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +24,9 @@
 
 package compiler.vectorapi;
 
+import jdk.incubator.vector.ByteVector;
 import jdk.incubator.vector.IntVector;
+import jdk.incubator.vector.ShortVector;
 import jdk.incubator.vector.VectorSpecies;
 import jdk.incubator.vector.VectorShuffle;
 
@@ -36,10 +39,63 @@ import jdk.incubator.vector.VectorShuffle;
 
 public class TestVectorShuffleIota {
     static final VectorSpecies<Integer> SPECIESi = IntVector.SPECIES_128;
+    static final VectorSpecies<Short> SPECIESs = ShortVector.SPECIES_128;
+    static final VectorSpecies<Byte> SPECIESb = ByteVector.SPECIES_128;
 
-    static final int INVOC_COUNT = 50000;
+    static final int INVOC_COUNT = 5000;
 
     static int[] ai = {87, 65, 78, 71};
+
+    interface compute_kernel {
+        long apply(int start, int step, boolean wrap);
+    }
+
+    static void validateTests(compute_kernel agen, compute_kernel egen, int start, int step, boolean wrap) {
+        long actual   = agen.apply(start, step, wrap);
+        long expected = egen.apply(start, step, wrap);
+        if (actual != expected) {
+            throw new AssertionError("Result Mismatch!, actual = " + actual + " expected = " + expected);
+        }
+    }
+
+    static void testShuffleIota (VectorSpecies<?> SPECIES, int start, int step, boolean wrap) {
+        compute_kernel sobj = new compute_kernel()  {
+            public long apply(int start, int step, boolean wrap) {
+                long res = 0;
+                int lanesM1 = SPECIES.length() - 1;
+                if (wrap) {
+                    for (int i = 0; i < 1024; i++) {
+                        start += i;
+                        res += (lanesM1 & (start + step * lanesM1)) * i;
+                    }
+                } else {
+                    for (int i = 0; i < 1024; i++) {
+                        start += i;
+                        int effective_index = start + step * lanesM1;
+                        int wrapped_effective_index = effective_index & lanesM1;
+                        res += (effective_index == wrapped_effective_index ?
+                                 wrapped_effective_index :
+                                 -SPECIES.length() + wrapped_effective_index) * i;
+                    }
+                }
+                return res;
+            }
+        };
+
+        compute_kernel vobj = new compute_kernel()  {
+            public long apply(int start, int step, boolean wrap) {
+                long res = 0;
+                for (int i = 0; i < 1024; i++) {
+                    start += i;
+                    res += SPECIES.iotaShuffle(start, step, wrap)
+                                  .laneSource(SPECIES.length()-1) * i;
+                }
+                return res;
+            }
+        };
+
+        validateTests(vobj, sobj, start, step, wrap);
+    }
 
     static void testShuffleI() {
         IntVector iv = (IntVector) VectorShuffle.iota(SPECIESi, 0, 2, false).toVector();
@@ -49,6 +105,27 @@ public class TestVectorShuffleIota {
     public static void main(String[] args) {
         for (int i = 0; i < INVOC_COUNT; i++) {
             testShuffleI();
+
+            testShuffleIota(SPECIESi, 128, 1, true);
+            testShuffleIota(SPECIESi, 128, 1, false);
+            testShuffleIota(SPECIESi, -128, 1, true);
+            testShuffleIota(SPECIESi, -128, 1, false);
+            testShuffleIota(SPECIESi, 1, 1, true);
+            testShuffleIota(SPECIESi, 1, 1, false);
+
+            testShuffleIota(SPECIESs, 128, 1, true);
+            testShuffleIota(SPECIESs, 128, 1, false);
+            testShuffleIota(SPECIESs, -128, 1, true);
+            testShuffleIota(SPECIESs, -128, 1, false);
+            testShuffleIota(SPECIESs, 1, 1, true);
+            testShuffleIota(SPECIESs, 1, 1, false);
+
+            testShuffleIota(SPECIESb, 128, 1, true);
+            testShuffleIota(SPECIESb, 128, 1, false);
+            testShuffleIota(SPECIESb, -128, 1, true);
+            testShuffleIota(SPECIESb, -128, 1, false);
+            testShuffleIota(SPECIESb, 1, 1, true);
+            testShuffleIota(SPECIESb, 1, 1, false);
         }
         for (int i = 0; i < ai.length; i++) {
             System.out.print(ai[i] + ", ");
