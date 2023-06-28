9998c07bd9348b5850a68cd8d8b6bbcfed5cc5fc
Tobias Hartmann <thartmann@openjdk.org>
2023-06-28 05:19:09 +0000

    8310459: [BACKOUT] 8304450: [vectorapi] Refactor VectorShuffle implementation
    Reviewed-by: kvn
    Backport-of: ff9a7541097bd853306a8594c97774f36877a0f9

--- src/hotspot/share/opto/c2compiler.cpp
+++ src/hotspot/share/opto/c2compiler.cpp
@@ -754,6 +754,8 @@ bool C2Compiler::is_intrinsic_supported(const methodHandle& method) {
   case vmIntrinsics::_VectorBinaryOp:
   case vmIntrinsics::_VectorTernaryOp:
   case vmIntrinsics::_VectorFromBitsCoerced:
+  case vmIntrinsics::_VectorShuffleIota:
+  case vmIntrinsics::_VectorShuffleToVector:
   case vmIntrinsics::_VectorLoadOp:
   case vmIntrinsics::_VectorLoadMaskedOp:
   case vmIntrinsics::_VectorStoreOp:
