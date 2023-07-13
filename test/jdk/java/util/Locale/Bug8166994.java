713101b15ebb0b9e40796c3c8f76d4e6971661cb
Justin Lu <justin.lu@oracle.com>
2023-07-13 11:41:24 -0700

    Missing import in Bug8166994.java

--- test/jdk/java/util/Locale/Bug8166994.java
+++ test/jdk/java/util/Locale/Bug8166994.java
@@ -41,6 +41,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Bug8166994 {
 
     /*
