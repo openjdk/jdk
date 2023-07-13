933c0312ed2fa0f40a750e950c166b7e820df7ab
Justin Lu <justin.lu@oracle.com>
2023-07-13 13:07:54 -0700

    Same as prev commit

--- test/jdk/java/util/Locale/Bug8135061.java
+++ test/jdk/java/util/Locale/Bug8135061.java
@@ -64,13 +64,9 @@ public class Bug8135061 {
     public void lookupReturnValueTest() {
         List<LanguageRange> ranges = LanguageRange.parse("i-navajo");
         Collection<Locale> locales = Collections.singleton(Locale.of("nv"));
-        try {
-            Locale match = Locale.lookup(ranges, locales);
-            assertEquals(match.toLanguageTag(), "nv");
-        } catch (Exception ex) {
-            throw new RuntimeException("[Locale.lookup failed on language"
-                    + " range: " + ranges + " and language tags "
-                    + locales + "]", ex);
-        }
+        Locale match = Locale.lookup(ranges, locales);
+        assertEquals(match.toLanguageTag(), "nv", "[Locale.lookup failed on language"
+                + " range: " + ranges + " and language tags "
+                + locales + "]");
     }
 }
