469be625ecabfbf65db989466dabd1ccc2885a86
Yi Yang <qingfeng.yy@alibaba-inc.com>
2023-07-19 20:34:29 +0800

    test failure on Windows

--- src/hotspot/share/services/heapDumper.cpp
+++ src/hotspot/share/services/heapDumper.cpp
@@ -1556,7 +1556,7 @@ void DumpMerger::merge_file(char* path) {
   assert(!SafepointSynchronize::is_at_safepoint(), "merging happens outside safepoint");
   TraceTime timer("Merge segmented heap file", TRACETIME_LOG(Info, heapdump));
 
-  fileStream segment_fs(path, "r");
+  fileStream segment_fs(path, "rb");
   if (!segment_fs.is_open()) {
     log_error(heapdump)("Can not open segmented heap file %s during merging", path);
     _writer->set_error("Can not open segmented heap file during merging");
@@ -1574,7 +1574,8 @@ void DumpMerger::merge_file(char* path) {
 
   _writer->flush();
   if (segment_fs.fileSize() != total) {
-    log_error(heapdump)("Merged heap dump %s is incomplete", path);
+    log_error(heapdump)("Merged heap dump %s is incomplete, expect %ld but read " JLONG_FORMAT " bytes",
+                        path, segment_fs.fileSize(), total);
     _writer->set_error("Merged heap dump is incomplete");
     _has_error = true;
   }
