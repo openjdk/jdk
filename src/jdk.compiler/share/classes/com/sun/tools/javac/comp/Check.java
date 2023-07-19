48760d7a044560dea6eeaca675ed27b5096cadd8
Aggelos Biboudis <abimpoudis@openjdk.org>
2023-07-19 07:37:21 +0000

    8312163: Crash in dominance check when compiling unnamed patterns
    Reviewed-by: jlahoda
    Backport-of: 1fc726a8b34fcd41dae12a6d7c63232f9ccef3f4

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Check.java
@@ -4733,10 +4733,13 @@ public class Check {
                     return false;
                 }
             }
-            if (currentPattern instanceof JCBindingPattern) {
-                return existingPattern instanceof JCBindingPattern;
+            if (currentPattern instanceof JCBindingPattern ||
+                currentPattern instanceof JCAnyPattern) {
+                return existingPattern instanceof JCBindingPattern ||
+                       existingPattern instanceof JCAnyPattern;
             } else if (currentPattern instanceof JCRecordPattern currentRecordPattern) {
-                if (existingPattern instanceof JCBindingPattern) {
+                if (existingPattern instanceof JCBindingPattern ||
+                    existingPattern instanceof JCAnyPattern) {
                     return true;
                 } else if (existingPattern instanceof JCRecordPattern existingRecordPattern) {
                     List<JCPattern> existingNested = existingRecordPattern.nested;
