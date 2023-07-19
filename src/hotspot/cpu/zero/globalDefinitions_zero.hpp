ec8ed70bd444b47e0cedcfa35cf351693635b50b
Sidraya <sidraya.jayagond@ibm.com>
2023-07-19 13:36:16 +0000

    Add AIX pd_segfault_address to -1 on zero build

--- src/hotspot/cpu/zero/globalDefinitions_zero.hpp
+++ src/hotspot/cpu/zero/globalDefinitions_zero.hpp
@@ -37,6 +37,12 @@
 // Indicates whether the C calling conventions require that
 // 32-bit integer argument values are extended to 64 bits.
 const bool CCallingConventionRequiresIntsAsLongs = false;
-const size_t pd_segfault_address = S390_ONLY(4096) NOT_S390(1024);
+#if defined(AIX)
+const size_t pd_segfault_address = -1;
+#elif defined(S390)
+const size_t pd_segfault_address = 4096;
+#else
+const size_t pd_segfault_address = 1024;
+#endif
 
 #endif // CPU_ZERO_GLOBALDEFINITIONS_ZERO_HPP
