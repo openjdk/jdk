dba2d33425e5618ca8f767096396102b389436b3
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-07-21 08:35:53 -0700

    4800398: Revise verbiage update per Reviewer suggestion

--- src/java.base/share/classes/java/nio/channels/Channels.java
+++ src/java.base/share/classes/java/nio/channels/Channels.java
@@ -259,14 +259,15 @@ public final class Channels {
     /**
      * Constructs a channel that reads bytes from the given stream.
      *
-     * <p> The resulting channel will be in blocking mode and will not be
-     * buffered; it will simply redirect its I/O operations to the given stream.
-     * Closing the channel will in turn cause the stream to be closed.  </p>
+     * <p> The resulting channel will not be buffered; it will simply redirect
+     * its I/O operations to the given stream. Reading from the resulting
+     * channel will read from the input stream hence might block. Closing the
+     * channel will in turn cause the stream to be closed.  </p>
      *
      * @param  in
      *         The stream from which bytes are to be read
      *
-     * @return  A new readable byte channel in blocking mode
+     * @return  A new readable byte channel
      */
     public static ReadableByteChannel newChannel(InputStream in) {
         Objects.requireNonNull(in, "in");
