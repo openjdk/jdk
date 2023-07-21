47b1d16bd8c1d1aecfe248cf3576b6d5b948261e
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2023-07-21 23:48:47 +0800

    macroAssembler_aarch64.cpp

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -179,8 +179,7 @@ public:
   virtual int immediate(address insn_addr, address &target) = 0;
   virtual void verify(address insn_addr, address &target) = 0;
 
-  ALWAYSINLINE
-  int run(address insn_addr, address &target) {
+  ALWAYSINLINE int run(address insn_addr, address &target) {
     int instructions = 1;
 
     uint32_t dispatch = Instruction_aarch64::extract(_insn, 30, 25);
