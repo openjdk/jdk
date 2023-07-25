c0bc6b9010c34a5bacac81eb098592a2714e60f5
Joshua Cao <joshcao@amazon.com>
2023-07-25 18:57:05 +0200

    change Amazon license to Oracle

--- test/micro/org/openjdk/bench/vm/compiler/pea/Blender.java
+++ test/micro/org/openjdk/bench/vm/compiler/pea/Blender.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,6 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package org.openjdk.bench.vm.compiler.pea;
 
 import org.openjdk.jmh.annotations.Benchmark;
