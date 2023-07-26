3ce65eb6b0dd141e6724842571922d13115ae3c4
Jenny Shivayogi <kspeeyu@amazon.com>
2023-07-26 10:46:52 -0700

    changing comments in TestParallelGCThreads

--- test/hotspot/jtreg/gc/arguments/TestParallelGCThreads.java
+++ test/hotspot/jtreg/gc/arguments/TestParallelGCThreads.java
@@ -112,8 +112,8 @@ public class TestParallelGCThreads {
         Asserts.assertEQ(count, i, "Specifying ParallelGCThreads=" + i + " for " + gc + "GC does not set the thread count properly!");
       }
     }
-
-    // 2147483647 == (signed int) -1
+    
+    // Test the max value for ParallelGCThreads
     // So setting ParallelGCThreads=2147483647 should give back 2147483647
     long count = getParallelGCThreadCount(
         "-XX:+UseSerialGC",
