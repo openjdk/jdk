33adaaa2f00841638cf23ed92f45d4152993c1e5
Kevin Walls <kevin.walls@oracle.com>
2023-07-26 11:15:14 +0100

    8313174: Create fewer predictable port clashes in management tests

--- test/jdk/javax/management/remote/mandatory/subjectDelegation/SubjectDelegation2Test.java
+++ test/jdk/javax/management/remote/mandatory/subjectDelegation/SubjectDelegation2Test.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -72,7 +72,7 @@ public class SubjectDelegation2Test {
             //
             System.out.println("Start RMI registry...");
             Registry reg = null;
-            int port = 5800;
+            int port = 5880;
             while (port++ < 6000) {
                 try {
                     reg = LocateRegistry.createRegistry(port);
