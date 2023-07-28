a5a6e425cc6a90334d67ab056ca099e599bad991
Joshua Cao <joshcao@amazon.com>
2023-07-28 16:09:56 -0700

    [PEA] Fix typo in run_load_self_into_field.sh (#67)

--- PEA/run_load_self_into_field.sh
+++ PEA/run_load_self_into_field.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-java -ea -Xcomp -Xms32M -Xmx32M -XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:-UseOnStackReplacement -XX:+UseTLAB -XX:CompileCommand='compileOnly,LoadIntoSelf.test*' $* LoadSelfIntoField
+java -ea -Xcomp -Xms32M -Xmx32M -XX:+AlwaysPreTouch -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:-UseOnStackReplacement -XX:+UseTLAB -XX:CompileCommand='compileOnly,LoadSelfIntoField.test*' $* LoadSelfIntoField
