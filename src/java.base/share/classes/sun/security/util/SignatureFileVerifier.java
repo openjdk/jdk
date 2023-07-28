03f0d31c25c48f69464d61585b216bfbe427a498
Hai-May Chao <hai-may.chao@oracle.com>
2023-07-28 14:37:07 -0700

    Variable tmp declaration update

--- src/java.base/share/classes/sun/security/util/SignatureFileVerifier.java
+++ src/java.base/share/classes/sun/security/util/SignatureFileVerifier.java
@@ -847,7 +847,7 @@ public class SignatureFileVerifier {
          * the maximum allowed number of bytes for the signature-related files
          * in a JAR file.
          */
-        Integer tmp = GetIntegerAction.privilegedGetProperty(
+        int tmp = GetIntegerAction.privilegedGetProperty(
                 "jdk.jar.maxSignatureFileSize", 16000000);
         if (tmp < 0 || tmp > MAX_ARRAY_SIZE) {
             if (debug != null) {
