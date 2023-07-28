eaca173c4a031650f8ab7e8035e7cab67d4bf5cc
Aleksey Shipilev <shipilev@amazon.de>
2023-07-28 19:06:44 +0200

    Change to Locale.US

--- test/jdk/java/util/Formatter/Padding.java
+++ test/jdk/java/util/Formatter/Padding.java
@@ -309,7 +309,7 @@ public class Padding {
     @ParameterizedTest
     @MethodSource
     void padding(String expected, String format, Object value) {
-        assertEquals(expected, String.format(Locale.ROOT, format, value));
+        assertEquals(expected, String.format(Locale.US, format, value));
     }
 
 }
