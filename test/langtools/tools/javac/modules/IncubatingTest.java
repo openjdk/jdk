ae3e86cff5ffb7c6b774d73afa106f19f2a425f4
Qing Xiao <qing.xiao@oracle.com>
2023-08-01 13:55:17 -0700

    Modified all tests under test/langtools/tools/javac/modules

--- test/langtools/tools/javac/modules/IncubatingTest.java
+++ test/langtools/tools/javac/modules/IncubatingTest.java
@@ -28,7 +28,13 @@
  * @library /tools/lib
  * @modules jdk.compiler/com.sun.tools.javac.api
  *          jdk.compiler/com.sun.tools.javac.main
- *          jdk.jdeps/com.sun.tools.classfile
+ *          java.base/jdk.internal.classfile
+ *          java.base/jdk.internal.classfile.attribute
+ *          java.base/jdk.internal.classfile.constantpool
+ *          java.base/jdk.internal.classfile.instruction
+ *          java.base/jdk.internal.classfile.components
+ *          java.base/jdk.internal.classfile.impl
+ *          java.base/jdk.internal.module
  *          jdk.jdeps/com.sun.tools.javap
  * @build toolbox.ToolBox toolbox.JarTask toolbox.JavacTask toolbox.JavapTask ModuleTestBase
  * @run main IncubatingTest
@@ -47,14 +53,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import com.sun.tools.classfile.Attribute;
-import com.sun.tools.classfile.Attributes;
-import com.sun.tools.classfile.ClassFile;
-import com.sun.tools.classfile.ClassWriter;
-import com.sun.tools.classfile.ConstantPool;
-import com.sun.tools.classfile.ConstantPool.CONSTANT_Utf8_info;
-import com.sun.tools.classfile.ConstantPool.CPInfo;
-import com.sun.tools.classfile.ModuleResolution_attribute;
+import jdk.internal.classfile.Attributes;
+import jdk.internal.classfile.ClassElement;
+import jdk.internal.classfile.ClassModel;
+import jdk.internal.classfile.Classfile;
+import jdk.internal.classfile.attribute.ModuleResolutionAttribute;
+import static jdk.internal.module.ClassFileConstants.DO_NOT_RESOLVE_BY_DEFAULT;
+import static jdk.internal.module.ClassFileConstants.WARN_INCUBATING;
+
+import jdk.internal.classfile.constantpool.ConstantPool;
+import jdk.internal.classfile.constantpool.ConstantPoolBuilder;
+import jdk.internal.classfile.constantpool.PoolEntry;
 import toolbox.JavacTask;
 import toolbox.Task;
 import toolbox.Task.Expect;
@@ -85,7 +94,7 @@ public class IncubatingTest extends ModuleTestBase {
         copyJavaBase(classes);
 
         Path jdkIModuleInfo = iClasses.resolve("module-info.class");
-        addModuleResolutionAttribute(jdkIModuleInfo, ModuleResolution_attribute.DO_NOT_RESOLVE_BY_DEFAULT);
+        addModuleResolutionAttribute(jdkIModuleInfo, DO_NOT_RESOLVE_BY_DEFAULT);
 
         Path testSrc = base.resolve("test-src");
         tb.writeJavaFiles(testSrc,
@@ -175,7 +184,7 @@ public class IncubatingTest extends ModuleTestBase {
                 .writeAll();
 
         Path jdkIModuleInfo = iClasses.resolve("module-info.class");
-        addModuleResolutionAttribute(jdkIModuleInfo, ModuleResolution_attribute.WARN_INCUBATING);
+        addModuleResolutionAttribute(jdkIModuleInfo, WARN_INCUBATING);
 
         Path testSrc = base.resolve("test-src");
         tb.writeJavaFiles(testSrc,
@@ -255,34 +264,15 @@ public class IncubatingTest extends ModuleTestBase {
     }
 
     private void addModuleResolutionAttribute(Path classfile, int resolution_flags) throws Exception {
-        ClassFile cf = ClassFile.read(classfile);
-        Attributes attrs = cf.attributes;
-        List<CPInfo> cpData = new ArrayList<>();
-        cpData.add(null);
-        for (CPInfo info : cf.constant_pool.entries()) {
-            cpData.add(info);
-            if (info.size() == 2)
-                cpData.add(null);
-        }
-        cpData.add(new CONSTANT_Utf8_info(Attribute.ModuleResolution));
-        ConstantPool newCP = new ConstantPool(cpData.toArray(new CPInfo[0]));
-        ModuleResolution_attribute res = new ModuleResolution_attribute(newCP, resolution_flags);
-        Map<String, Attribute> newAttributeMap = new HashMap<>(attrs.map);
-        newAttributeMap.put(Attribute.ModuleResolution, res);
-        Attributes newAttrs = new Attributes(newAttributeMap);
-        ClassFile newCF = new ClassFile(cf.magic,
-                                        cf.minor_version,
-                                        cf.major_version,
-                                        newCP,
-                                        cf.access_flags,
-                                        cf.this_class,
-                                        cf.super_class,
-                                        cf.interfaces,
-                                        cf.fields,
-                                        cf.methods,
-                                        newAttrs);
-        try (OutputStream out = Files.newOutputStream(classfile)) {
-            new ClassWriter().write(newCF, out);
-        }
+        ClassModel cm = Classfile.of().parse(classfile);
+        ModuleResolutionAttribute res = ModuleResolutionAttribute.of(resolution_flags);
+
+        Classfile.of().buildTo(classfile, cm.thisClass().asSymbol(), cb -> {
+            for (ClassElement ce : cm.elementList()) {
+                if (!(ce instanceof ModuleResolutionAttribute))
+                    cb.with(ce);
+            }
+            cb.with(res);
+        });
     }
 }
