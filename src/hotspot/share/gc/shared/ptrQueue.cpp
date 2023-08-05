eaf191298244979884052e3462ddd4ff287d80c7
Kim Barrett <kim.barrett@oracle.com>
2023-08-05 16:19:07 +0000

    use queue/node size/empty/capacity

--- src/hotspot/share/gc/shared/ptrQueue.cpp
+++ src/hotspot/share/gc/shared/ptrQueue.cpp
@@ -92,9 +92,7 @@ PtrQueueSet::PtrQueueSet(BufferNode::Allocator* allocator) :
 PtrQueueSet::~PtrQueueSet() {}
 
 void PtrQueueSet::reset_queue(PtrQueue& queue) {
-  if (queue.buffer() != nullptr) {
-    queue.set_index(buffer_capacity());
-  }
+  queue.set_index(queue.current_capacity());
 }
 
 void PtrQueueSet::flush_queue(PtrQueue& queue) {
