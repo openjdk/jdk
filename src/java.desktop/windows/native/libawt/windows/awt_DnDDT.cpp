2bdb89f3d53f18f0f7b91ca5607288008310c9ea
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2023-08-06 23:14:25 +0800

    Likewise for awt_DnDDT.cpp

--- src/java.desktop/windows/native/libawt/windows/awt_DnDDT.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_DnDDT.cpp
@@ -27,11 +27,11 @@
 #include <shellapi.h>
 #include <memory.h>
 
+#include "awt_ole.h"
 #include "awt_DataTransferer.h"
 #include "java_awt_dnd_DnDConstants.h"
 #include "sun_awt_windows_WDropTargetContextPeer.h"
 #include "awt_Container.h"
-#include "awt_ole.h"
 #include "awt_Toolkit.h"
 #include "awt_DnDDT.h"
 #include "awt_DnDDS.h"
