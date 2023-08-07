acd70219dfed5197520674d4058f9ce7d9d5bda8
Per Minborg <pminborg@openjdk.org>
2023-08-07 10:59:49 +0000

    8313238: Improve the performance of strlen for null-terminated strings
    Reviewed-by: jvernee

--- src/java.base/share/classes/jdk/internal/foreign/StringSupport.java
+++ src/java.base/share/classes/jdk/internal/foreign/StringSupport.java
@@ -25,18 +25,43 @@
 
 package jdk.internal.foreign;
 
+import jdk.internal.util.ArraysSupport;
+
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
+import java.lang.invoke.MethodHandle;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
+import static java.lang.foreign.ValueLayout.ADDRESS;
 import static java.lang.foreign.ValueLayout.JAVA_BYTE;
-import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
+import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 
 /**
  * Miscellaneous functions to read and write strings, in various charsets.
  */
 public class StringSupport {
+
+    // Maximum segment byte size for which a trivial method will be invoked.
+    private static final long MAX_TRIVIAL_SIZE = 1024L;
+    private static final MethodHandle STRNLEN_TRIVIAL;
+    private static final MethodHandle STRNLEN;
+    private static final boolean SIZE_T_IS_INT;
+
+    static {
+        var size_t = Objects.requireNonNull(Linker.nativeLinker().canonicalLayouts().get("size_t"));
+        Linker linker = Linker.nativeLinker();
+        var strnlen = linker.defaultLookup().find("strnlen").orElseThrow();
+        var description = FunctionDescriptor.of(size_t, ADDRESS, size_t);
+
+        STRNLEN_TRIVIAL = linker.downcallHandle(strnlen, description, Linker.Option.isTrivial());
+        STRNLEN = linker.downcallHandle(strnlen, description);
+        SIZE_T_IS_INT = (size_t.byteSize() == Integer.BYTES);
+    }
+
     public static String read(MemorySegment segment, long offset, Charset charset) {
         return switch (CharsetKind.of(charset)) {
             case SINGLE_BYTE -> readFast_byte(segment, offset, charset);
@@ -53,7 +78,7 @@ public class StringSupport {
         }
     }
     private static String readFast_byte(MemorySegment segment, long offset, Charset charset) {
-        long len = strlen_byte(segment, offset);
+        long len = native_strlen_byte(segment, offset);
         byte[] bytes = new byte[(int)len];
         MemorySegment.copy(segment, JAVA_BYTE, offset, bytes, 0, (int)len);
         return new String(bytes, charset);
@@ -91,15 +116,83 @@ public class StringSupport {
         segment.set(JAVA_INT, offset + bytes.length, 0);
     }
 
-    private static int strlen_byte(MemorySegment segment, long start) {
+    private static int native_strlen_byte(MemorySegment segment, long start) {
+        if (start > 0) {
+            segment = segment.asSlice(start);
+        }
+        long segmentSize = segment.byteSize();
+        final long len;
+        if (SIZE_T_IS_INT) {
+            if (segmentSize < MAX_TRIVIAL_SIZE) {
+                len = strnlen_int_trivial(segment, segmentSize);
+            } else if (segmentSize < Integer.MAX_VALUE * 2L) { // size_t is unsigned
+                len = strnlen_int(segment, segmentSize);
+            } else {
+                // There is no way to express the max size in the native method using an int so, revert
+                // to a Java method. It is possible to use a reduction of several STRNLEN invocations
+                // in a future optimization.
+                len = strlen_byte(segment);
+            }
+        } else {
+            len = segmentSize < MAX_TRIVIAL_SIZE
+                    ? strnlen_long_trivial(segment, segmentSize)
+                    : strnlen_long(segment, segmentSize);
+        }
+        if (len > ArraysSupport.SOFT_MAX_ARRAY_LENGTH) {
+            throw newIaeStringTooLarge();
+        }
+        return (int)len;
+    }
+
+    static long strnlen_int_trivial(MemorySegment segment, long size) {
+        try {
+            return Integer.toUnsignedLong((int)STRNLEN_TRIVIAL.invokeExact(segment, (int)size));
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_int(MemorySegment segment, long size) {
+        try {
+            return Integer.toUnsignedLong((int)STRNLEN.invokeExact(segment, (int)size));
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_long_trivial(MemorySegment segment, long size) {
+        try {
+            return (long)STRNLEN_TRIVIAL.invokeExact(segment, size);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_long(MemorySegment segment, long size) {
+        try {
+            return (long)STRNLEN.invokeExact(segment, size);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static int strlen_byte(MemorySegment segment) {
         // iterate until overflow (String can only hold a byte[], whose length can be expressed as an int)
-        for (int offset = 0; offset >= 0; offset++) {
-            byte curr = segment.get(JAVA_BYTE, start + offset);
+        for (int offset = 0; offset >= 0; offset += 2) {
+            short curr = segment.get(JAVA_SHORT, offset);
             if (curr == 0) {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     private static int strlen_short(MemorySegment segment, long start) {
@@ -110,7 +203,7 @@ public class StringSupport {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     private static int strlen_int(MemorySegment segment, long start) {
@@ -121,7 +214,7 @@ public class StringSupport {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     public enum CharsetKind {
@@ -151,4 +244,9 @@ public class StringSupport {
             }
         }
     }
+
+    private static IllegalArgumentException newIaeStringTooLarge() {
+        return new IllegalArgumentException("String too large");
+    }
+
 }
--- test/jdk/java/foreign/TestStringEncoding.java
+++ test/jdk/java/foreign/TestStringEncoding.java
@@ -22,13 +22,32 @@
  *
  */
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.lang.foreign.Arena;
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
+import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
+import java.lang.invoke.MethodHandle;
 import java.lang.reflect.Field;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
 
 import org.testng.annotations.*;
+
+import static java.lang.foreign.ValueLayout.ADDRESS;
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 import static org.testng.Assert.*;
 
 /*
@@ -39,7 +58,7 @@ import static org.testng.Assert.*;
 public class TestStringEncoding {
 
     @Test(dataProvider = "strings")
-    public void testStrings(String testString) throws ReflectiveOperationException {
+    public void testStrings(String testString) {
         for (Charset charset : Charset.availableCharsets().values()) {
             if (isStandard(charset)) {
                 try (Arena arena = Arena.ofConfined()) {
@@ -70,25 +89,253 @@ public class TestStringEncoding {
         }
     }
 
+    @Test(dataProvider = "strings")
+    public void unboundedSegment(String testString) {
+        testModifyingSegment(testString,
+                standardCharsets(),
+                s -> s.reinterpret(Long.MAX_VALUE),
+                UnaryOperator.identity());
+    }
+
+    @Test(dataProvider = "strings")
+    public void unalignedSegmentSingleByte(String testString) {
+        testModifyingSegment(testString,
+                singleByteCharsets(),
+                s -> s.byteSize() > 1 ? s.asSlice(1) : s,
+                s -> s.length() > 0 ? s.substring(1) : s);
+    }
+
+    @Test(dataProvider = "strings")
+    public void expandedSegment(String testString) {
+        try (var arena = Arena.ofConfined()) {
+            for (int i = 0; i < Long.BYTES; i++) {
+                int extra = i;
+                testModifyingSegment(testString,
+                        // Single byte charsets
+                        standardCharsets(),
+                        s -> {
+                            var s2 = arena.allocate(s.byteSize() + extra);
+                            MemorySegment.copy(s, 0, s2, 0, s.byteSize());
+                            return s2;
+                        },
+                        UnaryOperator.identity());
+            }
+        }
+    }
+
+    public void testModifyingSegment(String testString,
+                                     List<Charset> charsets,
+                                     UnaryOperator<MemorySegment> segmentMapper,
+                                     UnaryOperator<String> stringMapper) {
+        for (var charset : charsets) {
+            try (Arena arena = Arena.ofConfined()) {
+                MemorySegment text = arena.allocateFrom(testString, charset);
+                text = segmentMapper.apply(text);
+                String roundTrip = text.getString(0, charset);
+                String expected = stringMapper.apply(testString);
+                if (charset.newEncoder().canEncode(testString)) {
+                    assertEquals(roundTrip, expected);
+                }
+            }
+        }
+    }
+
+    @Test()
+    public void testPeculiarContentSingleByte() {
+        Random random = new Random(42);
+        for (int len = 7; len < 71; len++) {
+            try (var arena = Arena.ofConfined()) {
+                var segment = arena.allocate(len, 4);
+                var arr = new byte[len];
+                random.nextBytes(arr);
+                segment.copyFrom(MemorySegment.ofArray(arr));
+                int terminatorIndex = random.nextInt(len);
+                segment.set(ValueLayout.JAVA_BYTE, terminatorIndex, (byte) 0);
+                for (Charset charset : singleByteCharsets()) {
+                    var s = segment.getString(0, charset);
+                    var ref = referenceImpl(segment, 0, charset);
+                    assertEquals(s, ref);
+                }
+            }
+        }
+    }
+
+
+    @Test(dataProvider = "strings")
+    public void testOffset(String testString) {
+        if (testString.length() < 3 || !containsOnlyRegularCharacters(testString)) {
+            return;
+        }
+        for(var charset:singleByteCharsets()) {
+            try (Arena arena = Arena.ofConfined()) {
+                MemorySegment inSegment = arena.allocateFrom(testString, charset);
+                for (int i = 0; i < 3; i++) {
+                    String actual = inSegment.getString(i, charset);
+                    assertEquals(actual, testString.substring(i));
+                }
+            }
+        }
+    }
+
+    @Test()
+    public void testJumboSegment() {
+        testWithJumboSegment("testJumboSegment", segment -> {
+            segment.fill((byte) 1);
+            segment.set(JAVA_BYTE, Integer.MAX_VALUE + 10L, (byte) 0);
+            String big = segment.getString(100);
+        });
+    }
+
+    @Test()
+    public void testStringLargerThanMaxInt() {
+        testWithJumboSegment("testStringLargerThanMaxInt", segment -> {
+            segment.fill((byte) 1);
+            segment.set(JAVA_BYTE, Integer.MAX_VALUE + 10L, (byte) 0);
+            assertThrows(IllegalArgumentException.class, () -> {
+                segment.getString(0);
+            });
+        });
+    }
+
+    private static void testWithJumboSegment(String testName, Consumer<MemorySegment> tester) {
+        Path path = Paths.get("mapped_file");
+        try {
+            // Relly try to make sure the file is deleted after use
+            path.toFile().deleteOnExit();
+            deleteIfExistsOrThrow(path);
+            try (RandomAccessFile raf = new RandomAccessFile(path.toFile(), "rw")) {
+                FileChannel fc = raf.getChannel();
+                try (Arena arena = Arena.ofConfined()) {
+                    var segment = fc.map(FileChannel.MapMode.READ_WRITE, 0L, (long) Integer.MAX_VALUE + 100, arena);
+                    tester.accept(segment);
+                }
+            }
+        } catch (Exception e) {
+            throw new AssertionError(e);
+        } catch (OutOfMemoryError oome) {
+           // Unfortunately, we run out of memory and cannot run this test in this configuration
+            System.out.println("Skipping test because of insufficient memory: " + testName);
+        } finally {
+            deleteIfExistsOrThrow(path);
+        }
+    }
+
+    private static void deleteIfExistsOrThrow(Path file) {
+        try {
+            Files.deleteIfExists(file);
+        } catch (IOException ioe) {
+            throw new AssertionError("Unable to delete mapped file: " + file);
+        }
+    }
+
+    private static final MemoryLayout CHAR_POINTER = ADDRESS
+            .withTargetLayout(MemoryLayout.sequenceLayout(Long.MAX_VALUE, JAVA_BYTE));
+    private static final Linker LINKER = Linker.nativeLinker();
+    private static final MethodHandle STRCAT = LINKER.downcallHandle(
+            LINKER.defaultLookup().find("strcat").orElseThrow(),
+            FunctionDescriptor.of(CHAR_POINTER, CHAR_POINTER, CHAR_POINTER));
+
+    @Test(dataProvider = "strings")
+    public void nativeSegFromNativeCall(String testString) {
+        String addition = "123";
+        try (var arena = Arena.ofConfined()) {
+            try {
+                MemorySegment concatenation = (MemorySegment) STRCAT.invokeExact(arena.allocateFrom(testString), arena.allocateFrom(addition));
+                var actual = concatenation.getString(0);
+                assertEquals(actual, testString + addition);
+            } catch (Throwable t) {
+                throw new AssertionError(t);
+            }
+        }
+    }
+
+    @Test
+    public void segmentationFault() {
+        for (int i = 1; i < 18; i++) {
+            var size = 1 << i;
+            try (var arena = Arena.ofConfined()) {
+                var seg = arena.allocate(size, size);
+                seg.fill((byte)1);
+                try {
+                    var s = seg.getString(0);
+                    System.out.println("s.length() = " + s.length());
+                } catch (IndexOutOfBoundsException e) {
+                    // we will end up here if strlen finds a zero outside the MS
+                }
+            }
+        }
+    }
+
     @DataProvider
     public static Object[][] strings() {
-        return new Object[][] {
-            { "testing" },
-            { "" },
-            { "X" },
-            { "12345" },
-            { "yen \u00A5" },
-            { "snowman \u26C4" },
-            { "rainbow \uD83C\uDF08" }
+        return new Object[][]{
+                {"testing"},
+                {""},
+                {"X"},
+                {"12345"},
+                {"yen \u00A5"},
+                {"snowman \u26C4"},
+                {"rainbow \uD83C\uDF08"},
+                {"0"},
+                {"01"},
+                {"012"},
+                {"0123"},
+                {"01234"},
+                {"012345"},
+                {"0123456"},
+                {"01234567"},
+                {"012345678"},
+                {"0123456789"}
         };
     }
 
-    boolean isStandard(Charset charset) throws ReflectiveOperationException {
+    public static boolean containsOnlyRegularCharacters(String s) {
+        return s.chars()
+                .allMatch(c -> Character.isLetterOrDigit((char) c));
+    }
+
+    boolean isStandard(Charset charset) {
         for (Field standardCharset : StandardCharsets.class.getDeclaredFields()) {
-            if (standardCharset.get(null) == charset) {
-                return true;
+            try {
+                if (standardCharset.get(null) == charset) {
+                    return true;
+                }
+            } catch (ReflectiveOperationException e) {
+                throw new AssertionError(e);
             }
         }
         return false;
     }
+
+    List<Charset> standardCharsets() {
+        return Charset.availableCharsets().values().stream()
+                .filter(this::isStandard)
+                .toList();
+    }
+
+    List<Charset> singleByteCharsets() {
+        return Arrays.asList(StandardCharsets.UTF_8, StandardCharsets.ISO_8859_1, StandardCharsets.US_ASCII);
+    }
+
+
+    static String referenceImpl(MemorySegment segment, long offset, Charset charset) {
+        long len = strlen_byte(segment, offset);
+        byte[] bytes = new byte[(int) len];
+        MemorySegment.copy(segment, JAVA_BYTE, offset, bytes, 0, (int) len);
+        return new String(bytes, charset);
+    }
+
+
+    // Reference implementation
+    private static int strlen_byte(MemorySegment segment, long start) {
+        // iterate until overflow (String can only hold a byte[], whose length can be expressed as an int)
+        for (int offset = 0; offset >= 0; offset++) {
+            byte curr = segment.get(JAVA_BYTE, start + offset);
+            if (curr == 0) {
+                return offset;
+            }
+        }
+        throw new IllegalArgumentException("String too large");
+    }
+
 }
