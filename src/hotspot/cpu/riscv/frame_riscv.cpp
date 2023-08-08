04d550fe17c9f3f61a631d4eef4cd2f9df070c1f
Richard Startin <richard.startin@datadoghq.com>
2023-08-08 19:39:04 +0100

    revert unnecessary change

--- src/hotspot/cpu/riscv/frame_riscv.cpp
+++ src/hotspot/cpu/riscv/frame_riscv.cpp
@@ -481,7 +481,9 @@ bool frame::is_interpreted_frame_valid(JavaThread* thread) const {
   Method* m = safe_interpreter_frame_method();
 
   // validate the method we'd find in this potential sender
-  if (!Method::is_valid_method(m)) return false;
+  if (!Method::is_valid_method(m)) {
+      return false;
+  }
 
   // stack frames shouldn't be much larger than max_stack elements
   // this test requires the use of unextended_sp which is the sp as seen by
