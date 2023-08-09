2f5a9527fee0f380b515d41200dbf6db421183a5
Martin Doerr <mdoerr@openjdk.org>
2023-08-09 13:19:12 +0000

    8195589: T6587786.java failed after JDK-8189997
    Backport-of: 21b62fef335aa981a239415532fa8b804fb5676d

--- test/langtools/tools/javap/T6587786.java
+++ test/langtools/tools/javap/T6587786.java
@@ -36,18 +36,21 @@ public class T6587786 {
     }
 
     public void run() throws IOException {
-        javap("com.sun.javadoc.Doc", "com.sun.crypto.provider.ai");
-        javap("com.sun.crypto.provider.ai", "com.sun.javadoc.ClassDoc");
+        javap("jdk.javadoc.doclet.Doclet", "java.util.List");
+        javap("java.util.List", "jdk.javadoc.doclet.StandardDoclet");
     }
 
     void javap(String... args) {
         StringWriter sw = new StringWriter();
         PrintWriter out = new PrintWriter(sw);
         //sun.tools.javap.Main.entry(args);
-        int rc = com.sun.tools.javap.Main.run(args, out);
-        if (rc != 0)
-            throw new Error("javap failed. rc=" + rc);
-        out.close();
-        System.out.println(sw.toString());
+        try {
+            int rc = com.sun.tools.javap.Main.run(args, out);
+            if (rc != 0)
+                throw new Error("javap failed. rc=" + rc);
+        } finally {
+            out.close();
+            System.out.println(sw.toString());
+        }
     }
 }
