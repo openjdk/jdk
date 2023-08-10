4b825b8607ead505c8c89700ac3a9a0d757bedc5
Tejesh R <tejesh.r@oracle.com>
2023-08-10 12:39:21 +0530

    Test update

--- test/jdk/javax/swing/JFileChooser/NewFolderLocale/FileChooserNewFolderLocaleTest.java
+++ test/jdk/javax/swing/JFileChooser/NewFolderLocale/FileChooserNewFolderLocaleTest.java
@@ -22,75 +22,30 @@
  */
 
 import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-import javax.swing.WindowConstants;
 
-import java.awt.BorderLayout;
-
+import java.io.File;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 /*
  * @test
  * @bug 8312075
- * @key headful
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
  * @summary Test to check if created newFolder is updated with
  *          changing Locale.
- * @run main/manual FileChooserNewFolderLocaleTest
+ * @run main FileChooserNewFolderLocaleTest
  */
 
 public class FileChooserNewFolderLocaleTest {
-    static JFrame frame;
-    static JFileChooser jfc;
-    static PassFailJFrame passFailJFrame;
     static ResourceBundle res;
+    static String newFolderKey;
+    static String newFolderSubKey;
 
     public static void main(String[] args) throws Exception {
-        SwingUtilities.invokeAndWait(new Runnable() {
-            public void run() {
-                try {
-                    initialize();
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-        passFailJFrame.awaitAndCheck();
-    }
-
-    private static void initialize() throws Exception {
-        setLocale("fr");
-        //Initialize the components
-        final String INSTRUCTIONS = """
-                Instructions to Test:
-                1. Click on New Folder button to create new Folder.
-                2. If the created New Folder name is "Nouveau dossier"
-                   (set from bundle_fr properties file) test is PASS.
-                3. created New Folder name is "New Folder" then test is FAIL.
-                """;
-        frame = new JFrame("JFileChooser NewFolder Locale test");
-        jfc = new JFileChooser();
-
-        passFailJFrame = new PassFailJFrame("Test Instructions",
-                INSTRUCTIONS, 5L, 8, 40);
-        PassFailJFrame.addTestWindow(frame);
-        PassFailJFrame.positionTestWindow(frame, PassFailJFrame.Position.HORIZONTAL);
-
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.add(jfc, BorderLayout.CENTER);
-        frame.pack();
-        frame.setVisible(true);
-    }
-
-    private static void setLocale(String locale) {
-        boolean IS_WINDOWS = System.getProperty("os.name").toLowerCase().contains("windows");
-        String newFolderKey;
-        String newFolderSubKey;
-
+        File newFolderEnglish = null;
+        File newFolderFrench = null;
+        boolean IS_WINDOWS =
+                System.getProperty("os.name").toLowerCase().contains("windows");
         if (IS_WINDOWS) {
             newFolderKey = "FileChooser.win32.newFolder";
             newFolderSubKey = "FileChooser.win32.newFolder.subsequent";
@@ -99,18 +54,63 @@ public class FileChooserNewFolderLocaleTest {
             newFolderSubKey = "FileChooser.other.newFolder.subsequent";
         }
 
-        res = ResourceBundle.getBundle("bundle", new Locale(locale));
-        UIManager.put("FileChooser.acceptAllFileFilterText", res.getString("accept_all"));
-        UIManager.put("FileChooser.directoryOpenButtonText", res.getString("open_text"));
-        UIManager.put("FileChooser.openButtonText", res.getString("open_text"));
-        UIManager.put("FileChooser.saveButtonText", res.getString("save_text"));
-        UIManager.put("FileChooser.cancelButtonText", res.getString("cancel_text"));
-        UIManager.put("FileChooser.lookInLabelText", res.getString("file_look"));
-        UIManager.put("FileChooser.saveInLabelText", res.getString("file_save"));
-        UIManager.put("FileChooser.fileNameLabelText", res.getString("file_name"));
-        UIManager.put("FileChooser.filesOfTypeLabelText", res.getString("file_type"));
-        UIManager.put(newFolderKey, res.getString("new_folder"));
-        UIManager.put(newFolderSubKey, res.getString("new_folder")
-                + " ({0})");
+        try {
+            JFileChooser fileChooser = new JFileChooser();
+            File currentDir = new File(".");
+            newFolderEnglish =
+                    fileChooser.getFileSystemView().createNewFolder(currentDir);
+
+            setLocale("fr");
+            fileChooser = new JFileChooser();
+            newFolderFrench =
+                    fileChooser.getFileSystemView().createNewFolder(currentDir);
+            if( !newFolderFrench.getName().contains
+                    (UIManager.getString(newFolderKey))) {
+                throw new RuntimeException("Failed to update new Locale");
+            }
+        } finally {
+            if (newFolderEnglish != null && newFolderEnglish.delete()) {
+                System.out.println("Deleted the file: " +
+                        newFolderEnglish.getName());
+            } else {
+                System.out.println("Failed to delete file : " +
+                        newFolderEnglish.getName());
+            }
+            if (newFolderFrench != null && newFolderFrench.delete()) {
+                System.out.println("Deleted the file: " +
+                        newFolderFrench.getName());
+            } else {
+                System.out.println("Failed to delete file : " +
+                        newFolderFrench.getName());
+            }
+        }
+        System.out.println("Test Passed!");
+    }
+
+    private static void setLocale(String locale) {
+        res = ResourceBundle.getBundle("bundle",
+                new Locale(locale));
+        UIManager.put("FileChooser.acceptAllFileFilterText",
+                res.getString("accept_all"));
+        UIManager.put("FileChooser.directoryOpenButtonText",
+                res.getString("open_text"));
+        UIManager.put("FileChooser.openButtonText",
+                res.getString("open_text"));
+        UIManager.put("FileChooser.saveButtonText",
+                res.getString("save_text"));
+        UIManager.put("FileChooser.cancelButtonText",
+                res.getString("cancel_text"));
+        UIManager.put("FileChooser.lookInLabelText",
+                res.getString("file_look"));
+        UIManager.put("FileChooser.saveInLabelText",
+                res.getString("file_save"));
+        UIManager.put("FileChooser.fileNameLabelText",
+                res.getString("file_name"));
+        UIManager.put("FileChooser.filesOfTypeLabelText",
+                res.getString("file_type"));
+        UIManager.put(newFolderKey,
+                res.getString("new_folder"));
+        UIManager.put(newFolderSubKey,
+                res.getString("new_folder") + " ({0})");
     }
 }
