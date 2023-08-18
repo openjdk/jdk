900ac074476fb6d85392b0b5b1dd057b9761752c
Emmy <emmyy@kth.se>
2023-08-18 17:52:27 +0200

    Fixes

--- src/utils/IdealGraphVisualizer/HierarchicalLayout/src/main/java/com/sun/hotspot/igv/hierarchicallayout/HierarchicalStableLayoutManager.java
+++ src/utils/IdealGraphVisualizer/HierarchicalLayout/src/main/java/com/sun/hotspot/igv/hierarchicallayout/HierarchicalStableLayoutManager.java
@@ -128,7 +128,6 @@ public class HierarchicalStableLayoutManager {
      * Adjust the X-coordinates of the nodes in the given layer, as a new node has
      * been inserted at that layer
      *
-     * @param newNode
      * @param layer
      */
     private void adjustXCoordinates(int layer) {
@@ -1539,11 +1538,8 @@ public class HierarchicalStableLayoutManager {
          * @return true if point q lies on line segment 'pr'
          */
         private boolean onSegment(Point p, Point q, Point r) {
-            if (q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
-                    q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y))
-                return true;
-
-            return false;
+            return q.x <= Math.max(p.x, r.x) && q.x >= Math.min(p.x, r.x) &&
+                    q.y <= Math.max(p.y, r.y) && q.y >= Math.min(p.y, r.y);
         }
 
         /**
@@ -1557,8 +1553,9 @@ public class HierarchicalStableLayoutManager {
             int val = (q.y - p.y) * (r.x - q.x) -
                     (q.x - p.x) * (r.y - q.y);
 
-            if (val == 0)
+            if (val == 0) {
                 return 0; // collinear
+            }
 
             return (val > 0) ? 1 : 2; // clock or counterclock wise
         }
@@ -1582,9 +1579,10 @@ public class HierarchicalStableLayoutManager {
             int o4 = orientation(p2, q2, q1);
 
             // General case
-            if (o1 != o2 && o3 != o4)
+            if (o1 != o2 && o3 != o4) {
                 return true;
-
+            }
+            
             // Special Cases
             // p1, q1 and p2 are collinear and p2 lies on segment p1q1
             if (o1 == 0 && onSegment(p1, p2, q1)) {
@@ -1707,8 +1705,6 @@ public class HierarchicalStableLayoutManager {
         /**
          * Computes how much the bends in an edge deviates from being straight, on
          * average
-         *
-         * @return
          */
         private float averageEdgeBendDegrees() {
             float totDegree = 0;
