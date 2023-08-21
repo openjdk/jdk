b88dc1149a3ec3ff4c8c77162b227c63d8692716
Alisen Chung <alisen.chung@oracle.com>
2023-08-21 11:12:42 -0700

    test update

--- test/jdk/javax/swing/JFrame/MaximizeWindowTest.java
+++ test/jdk/javax/swing/JFrame/MaximizeWindowTest.java
@@ -14,40 +14,47 @@ import java.lang.reflect.InvocationTargetException;
  * @run main MaximizeWindowTest
  */
 @SuppressWarnings("serial")
-public class MaximizeWindowTest extends JFrame
-{
+public class MaximizeWindowTest extends JFrame {
+    private static JFrame frame;
+    private static final Dimension ORIGINAL_SIZE = new Dimension(200, 200);
+
     public static void main(String[] arguments) throws Exception {
         Robot robot = new Robot();
+        try {
+            SwingUtilities.invokeAndWait(new Runnable() {
+                @Override
+                public void run() {
+                    JSplitPane splitPane = new JSplitPane();
 
-        SwingUtilities.invokeAndWait(new Runnable() {
-            @Override
-            public void run() {
-                JSplitPane splitPane = new JSplitPane();
+                    frame = new JFrame();
+                    frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+                    frame.setSize(200, 200);
+                    frame.setLocation(400, 400);
+                    frame.add(splitPane);
+                    frame.setExtendedState(MAXIMIZED_BOTH);
 
-                JFrame frame = new JFrame();
-                frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-                frame.setSize(200, 200);
-                frame.setLocation(400, 400);
-                frame.add(splitPane);
-                frame.setExtendedState(MAXIMIZED_BOTH);
-
-                frame.addComponentListener(new ComponentAdapter() {
-                    @Override
-                    public void componentResized(ComponentEvent e) {
-                        System.out.println("Component size: " + e.getComponent().getSize());
-                        if (e.getComponent().getSize().equals(new Dimension(200, 200))) {
-                            throw new RuntimeException("Test Failed! " +
-                                    "Frame was visible at original size before maximizing");
+                    frame.addComponentListener(new ComponentAdapter() {
+                        @Override
+                        public void componentResized(ComponentEvent e) {
+                            System.out.println("Component size: " + e.getComponent().getSize());
+                            if (e.getComponent().getSize().equals(new Dimension(200, 200))) {
+                                throw new RuntimeException("Test Failed! " +
+                                        "Frame was visible at original size before maximizing");
+                            }
                         }
-                    }
-                });
+                    });
 
-                splitPane.setDividerLocation(1000);
+                    splitPane.setDividerLocation(1000);
 
-                frame.setVisible(true);
+                    frame.setVisible(true);
 
-                robot.delay(1000);
+                }
+            });
+            robot.delay(1000);
+        } finally {
+            if (frame != null) {
+                SwingUtilities.invokeAndWait(() -> frame.dispose());
             }
-        });
+        }
     }
 }
