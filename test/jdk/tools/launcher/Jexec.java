a2e8a04592bdb7e2dc7c97bf3c534e2644ad96cb
Vladimir Petko <vladimir.petko@canonical.com>
2023-08-22 08:27:22 +1200

    remove unused imports

--- test/jdk/tools/launcher/Jexec.java
+++ test/jdk/tools/launcher/Jexec.java
@@ -30,18 +30,9 @@
  * @run main Jexec
  */
 
-import java.beans.Transient;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.HashMap;
 
 public class Jexec extends TestHelper {
     private final File testJar;
