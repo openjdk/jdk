7d0ad24350661b24de939516cdc7254f1f3d54b4
rwestrel <rwestrel@redhat.com>
2023-08-23 10:59:31 +0200

    comments

--- test/hotspot/jtreg/compiler/loopopts/TestNodeSunkFromPreLoop.java
+++ test/hotspot/jtreg/compiler/loopopts/TestNodeSunkFromPreLoop.java
@@ -41,16 +41,21 @@ public class TestNodeSunkFromPreLoop {
 
     private static int test(A object, int stop, int inv) {
         int res = 0;
+        // pre/main/post loops created for this loop
         for (int i = 0; i < stop; i++) {
+            // Optimized out. Delay transformation of loop above.
             for (int j = 0; j < 10; j++) {
                 for (int k = 0; k < 10; k++) {
                 }
             }
+            // null check in pre loop so field load also in pre loop
             int v = object.field;
             int v2 = (v + inv);
             if (i > 1000) {
+                // never taken. v + inv has a use out of loop at an unc.
                 unusedField = v2;
             }
+            // transformed in the main loop to i + (v + inv)
             int v3 = (v + (i + inv));
             if (v3 > 1000) {
                 break;
