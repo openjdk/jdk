d35a896350979d402af70c625a3b550178c10019
Aleksey Shipilev <shade@openjdk.org>
2023-08-23 06:30:50 +0000

    8293851: hs_err should print more stack in hex dump
    Backport-of: cbd0688b321ff88a405be4f7929d6862e543ab50

--- src/hotspot/share/runtime/os.hpp
+++ src/hotspot/share/runtime/os.hpp
@@ -694,12 +694,13 @@ class os: AllStatic {
   static void print_environment_variables(outputStream* st, const char** env_list);
   static void print_context(outputStream* st, const void* context);
   static void print_tos_pc(outputStream* st, const void* context);
+  static void print_tos(outputStream* st, address sp);
+  static void print_instructions(outputStream* st, address pc, int unitsize);
   static void print_register_info(outputStream* st, const void* context);
   static bool signal_sent_by_kill(const void* siginfo);
   static void print_siginfo(outputStream* st, const void* siginfo);
   static void print_signal_handlers(outputStream* st, char* buf, size_t buflen);
   static void print_date_and_time(outputStream* st, char* buf, size_t buflen);
-  static void print_instructions(outputStream* st, address pc, int unitsize);
 
   // helper for output of seconds in days , hours and months
   static void print_dhm(outputStream* st, const char* startStr, long sec);
