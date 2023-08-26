185c9f6f5aacb09c41ea3d6269675ace06600d61
jiangfeilong <jiangfeilong@huawei.com>
2023-08-26 19:57:11 +0800

    update copyright

--- src/hotspot/cpu/riscv/vtableStubs_riscv.cpp
+++ src/hotspot/cpu/riscv/vtableStubs_riscv.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2003, 2023, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2014, Red Hat Inc. All rights reserved.
- * Copyright (c) 2020, 2022, Huawei Technologies Co., Ltd. All rights reserved.
+ * Copyright (c) 2020, 2023, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
