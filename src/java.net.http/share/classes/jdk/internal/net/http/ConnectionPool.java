6a7c7e275deedcd01752afb0b98b165b2ab7d48e
Daniel Jelinski <daniel.jelinski@oracle.com>
2023-08-28 17:17:55 +0200

    remove reverse DNS query

--- src/java.net.http/share/classes/jdk/internal/net/http/ConnectionPool.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/ConnectionPool.java
@@ -108,14 +108,9 @@ final class ConnectionPool {
                 return false;
             }
             if (secure && destination != null) {
-                if (destination.getHostName() != null) {
-                    if (!destination.getHostName().equalsIgnoreCase(
-                            other.destination.getHostName())) {
-                        return false;
-                    }
-                } else {
-                    if (other.destination.getHostName() != null)
-                        return false;
+                if (!destination.getHostString().equalsIgnoreCase(
+                        other.destination.getHostString())) {
+                    return false;
                 }
             }
             return true;
