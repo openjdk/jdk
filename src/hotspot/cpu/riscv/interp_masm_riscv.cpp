7fbad4cd5e2b6107410e82ae774aa8b5c05f6054
Amit Kumar <amitkumar@openjdk.org>
2023-08-28 03:34:20 +0000

    8310596: Utilize existing method frame::interpreter_frame_monitor_size_in_bytes()
    Reviewed-by: dholmes, stefank

--- src/hotspot/cpu/riscv/interp_masm_riscv.cpp
+++ src/hotspot/cpu/riscv/interp_masm_riscv.cpp
@@ -694,7 +694,7 @@ void InterpreterMacroAssembler::remove_activation(
   // Check that all monitors are unlocked
   {
     Label loop, exception, entry, restart;
-    const int entry_size = frame::interpreter_frame_monitor_size() * wordSize;
+    const int entry_size = frame::interpreter_frame_monitor_size_in_bytes();
     const Address monitor_block_top(
       fp, frame::interpreter_frame_monitor_block_top_offset * wordSize);
     const Address monitor_block_bot(
