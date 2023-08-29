c91fb3f7a51461e24fee53187543333b1455accb
Oli Gillespie <ogillespie@openjdk.org>
2023-08-29 15:08:37 +0000

    8313874: JNI NewWeakGlobalRef throws exception for null arg
    Reviewed-by: phh
    Backport-of: 028b3ae1b162bd8f7c340bfa6e9487ca83697955

--- src/hotspot/share/prims/jni.cpp
+++ src/hotspot/share/prims/jni.cpp
@@ -2918,7 +2918,7 @@ JNI_ENTRY(jweak, jni_NewWeakGlobalRef(JNIEnv *env, jobject ref))
   HOTSPOT_JNI_NEWWEAKGLOBALREF_ENTRY(env, ref);
   Handle ref_handle(thread, JNIHandles::resolve(ref));
   jweak ret = JNIHandles::make_weak_global(ref_handle, AllocFailStrategy::RETURN_NULL);
-  if (ret == NULL) {
+  if (ret == NULL && ref_handle.not_null()) {
     THROW_OOP_(Universe::out_of_memory_error_c_heap(), NULL);
   }
   HOTSPOT_JNI_NEWWEAKGLOBALREF_RETURN(ret);
