f09ba7996581df64cc194b80a50f0da6b50c5636
Aleksey Shipilev <shade@openjdk.org>
2023-08-30 05:46:00 +0000

    8314020: Print instruction blocks in byte units
    Reviewed-by: phh
    Backport-of: a602624ef46908456052146d50467c60efa636c3

--- src/hotspot/share/runtime/os.hpp
+++ src/hotspot/share/runtime/os.hpp
@@ -695,7 +695,7 @@ class os: AllStatic {
   static void print_context(outputStream* st, const void* context);
   static void print_tos_pc(outputStream* st, const void* context);
   static void print_tos(outputStream* st, address sp);
-  static void print_instructions(outputStream* st, address pc, int unitsize);
+  static void print_instructions(outputStream* st, address pc, int unitsize = 1);
   static void print_register_info(outputStream* st, const void* context);
   static bool signal_sent_by_kill(const void* siginfo);
   static void print_siginfo(outputStream* st, const void* siginfo);
