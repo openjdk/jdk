5944fe0d8f873ec2fdaf0a3a4584a70cd95666ba
Rajat Mahajan <rajat.m.mahajan@oracle.com>
2023-09-01 13:36:02 -0700

    Update instructions for clarity

--- test/jdk/javax/swing/JMenuItem/8031573/bug8031573.java
+++ test/jdk/javax/swing/JMenuItem/8031573/bug8031573.java
@@ -43,6 +43,7 @@ import javax.swing.text.JTextComponent;
  * @bug 8031573 8040279 8143064 8294427
  * @summary  Checkmarks of JCheckBoxMenuItems aren't rendered
  *           in high resolution on Retina
+ * @requires (os.family != "linux")
  * @run main/manual bug8031573
  */
 
@@ -53,17 +54,19 @@ public class bug8031573 {
     private static final CountDownLatch latch = new CountDownLatch(1);
 
     public static final String INSTRUCTIONS = "INSTRUCTIONS:\n\n"
-            + "Verify that high resolution system icons are used for\n"
-            + "JCheckBoxMenuItem and JRadioButtonMenuItem on HiDPI displays.\n"
-            + "If the display does not support HiDPI mode press PASS.\n"
-            + "1. Run the test on HiDPI Display.\n"
-            + "2. Open the Menu.\n"
-            + "3. Check that the icon on the JCheckBoxMenuItem is smooth.\n"
-            + "4. Check that the icon on the JRadioButtonMenuItem is smooth.\n"
-            + "5. Test the markers are still crisp after changing the scale in Windows settings.\n"
-            + "   This could be done on same monitor by changing scale or multi-monitor setup\n"
-            + "   with different scales.\n"
-            + "6. If so, press PASS, else press FAIL.\n";
+            + "Verify that the check and radio-check icons are rendered smoothly\n"
+            + "for both JCheckBoxMenuItem and JRadioButtonMenuItem.\n"
+            + "1. Open the Menu.\n"
+            + "2. Check that the icon on the JCheckBoxMenuItem is smooth.\n"
+            + "3. Check that the icon on the JRadioButtonMenuItem is smooth.\n"
+            + "4. If you're on Windows:\n"
+            + "   Test the markers are still crisp after changing the scale in Windows settings.\n"
+            + "   This could be done on same monitor by changing its scale or\n"
+            + "   by moving the window to a secondary monitor with a different scale.\n"
+            + "   Then go to step 6.\n"
+            + "5. If you're on Mac OS:\n"
+            + "   If you tested on a Retina display, go to step 6.\n"
+            + "6. If both icons render smoothly, press PASS, otherwise press FAIL.\n";
 
     public static void main(String[] args) throws Exception {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
