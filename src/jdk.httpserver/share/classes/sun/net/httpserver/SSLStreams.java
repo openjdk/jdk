42f5b9e7aacd69cad8b104ed49337cc64fc7ce7e
Daniel Jeliński <djelinski@openjdk.org>
2023-09-01 10:19:53 +0000

    8315436: HttpsServer does not send TLS alerts
    Reviewed-by: dfuchs, michaelm

--- src/jdk.httpserver/share/classes/sun/net/httpserver/SSLStreams.java
+++ src/jdk.httpserver/share/classes/sun/net/httpserver/SSLStreams.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -286,7 +286,6 @@ class SSLStreams {
                 } while (status == Status.BUFFER_OVERFLOW);
                 if (status == Status.CLOSED && !ignoreClose) {
                     closed = true;
-                    return r;
                 }
                 if (r.result.bytesProduced() > 0) {
                     wrap_dst.flip();
