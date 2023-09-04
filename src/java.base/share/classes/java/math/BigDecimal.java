9846ff90043aa0b89c256f37cc8ebe818c08697c
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-09-04 10:39:10 +0800

    update copyright info

--- src/java.base/share/classes/java/math/BigDecimal.java
+++ src/java.base/share/classes/java/math/BigDecimal.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Alibaba Group Holding Limited. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
