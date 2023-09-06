5e439da86582a1d6477a5ab2b81cf4d8ccb8073a
Jorn Vernee <JornVernee@users.noreply.github.com>
2023-09-06 11:56:21 +0200

    Fix typo in doc
    Co-authored-by: Paul Sandoz <paul.d.sandoz@googlemail.com>

--- src/java.base/share/classes/java/lang/foreign/MemorySegment.java
+++ src/java.base/share/classes/java/lang/foreign/MemorySegment.java
@@ -151,7 +151,7 @@ import jdk.internal.vm.annotation.ForceInline;
  *     ValueLayout.JAVA_INT.withName("size"),
  *     MemoryLayout.sequenceLayout(4, ValueLayout.JAVA_INT).withName("data") // array of 4 elements
  * );
- * VarHandle intHandle = segmentLayout.varHandle(MemoryLayout.PathElemenet.groupElement("data"),
+ * VarHandle intHandle = segmentLayout.varHandle(MemoryLayout.PathElement.groupElement("data"),
  *                                               MemoryLayout.PathElement.sequenceElement());
  * int value = (int) intHandle.get(segment, 0L, 3L); // get int element at offset 0 + offsetof(data) + 3 * 4 = 12
  * }
