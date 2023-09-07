5f80c7ca355598904500ce2e9801101510522c9f
Ludovic Henry <git@ludovic.dev>
2023-09-07 11:54:36 +0000

    fixup! 8315841: RISC-V: Check for hardware TSO support

--- src/hotspot/cpu/riscv/vm_version_riscv.cpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.cpp
@@ -210,7 +210,7 @@ void VM_Version::initialize() {
       unaligned_access.value() == MISALIGNED_FAST);
   }
 
-#if TARGET_ZTSO
+#if defined(TARGET_ZTSO) && TARGET_ZTSO
   // Hotspot is compiled with TSO support, it will only run on hardware which
   // supports Ztso
   if (FLAG_IS_DEFAULT(UseZtso)) {
