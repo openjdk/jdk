5721f39b753320895fd045685ec06362ee6dd8ec
Ian Myers <ianrichr@amazon.com>
2023-09-07 13:36:25 +0000

    Updated Copyright and remove excess new line

--- test/jdk/sun/security/util/math/TestIntegerModuloP.java
+++ test/jdk/sun/security/util/math/TestIntegerModuloP.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -99,7 +99,6 @@
  * @build BigIntegerModuloP
  * @run main TestIntegerModuloP sun.security.util.math.intpoly.Curve448OrderField 56 12
  */
-
 
 import sun.security.util.math.*;
 import sun.security.util.math.intpoly.*;
