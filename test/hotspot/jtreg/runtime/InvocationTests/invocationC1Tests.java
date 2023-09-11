be34f0b28ea8bdcbe55320f665960aad95f352f0
Aleksey Shipilev <shade@openjdk.org>
2023-09-11 10:47:29 +0000

    8271886: mark hotspot runtime/InvocationTests tests which ignore external VM flags
    Backport-of: ff7431eafea8bd763250f929aa35edcaae2fbc58

--- test/hotspot/jtreg/runtime/InvocationTests/invocationC1Tests.java
+++ test/hotspot/jtreg/runtime/InvocationTests/invocationC1Tests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,7 @@
  * @test id=special
  * @bug 8226956
  * @summary Run invocation tests against C1 compiler
+ * @requires vm.flagless
  * @library /test/lib
  * @modules java.base/jdk.internal.org.objectweb.asm
  *          java.base/jdk.internal.misc
