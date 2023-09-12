0f3698a5dd3f40a4aaf261ad6e16290ac8b91fcf
Justin Lu <justin.lu@oracle.com>
2023-09-12 14:18:21 -0700

    Update header / copyright for CurrencyFormat

--- test/jdk/java/text/Format/NumberFormat/CurrencyFormat.java
+++ test/jdk/java/text/Format/NumberFormat/CurrencyFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 4290801 4942982 5102005 8008577 8021121 8210153 8227313
+ * @bug 4290801 4942982 5102005 8008577 8021121 8210153 8227313 8301991
  * @summary Basic tests for currency formatting.
  * @modules jdk.localedata
  * @run main/othervm -Djava.locale.providers=COMPAT CurrencyFormat COMPAT
