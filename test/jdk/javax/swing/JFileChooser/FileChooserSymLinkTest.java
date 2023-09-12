a8339019e5994181c7ce3579b9733c1a350cb238
Alexey Ivanov <alexey.ivanov@oracle.com>
2023-09-12 16:21:45 +0100

    Amend indentation of nested <li> elements

--- test/jdk/javax/swing/JFileChooser/FileChooserSymLinkTest.java
+++ test/jdk/javax/swing/JFileChooser/FileChooserSymLinkTest.java
@@ -91,32 +91,32 @@ public class FileChooserSymLinkTest {
                     <ul style="margin-bottom: 0px">
                     <li><strong>Single-selection:</strong>
                         <ol>
-                            <li>Ensure <b>Enable multi-selection</b> is cleared
-                                (the default state).
-                            <li>Click <code>link</code> directory,
-                                the absolute path of the symbolic
-                                link should be displayed.<br>
-                                If it's <code>null</code>, click <b>Fail</b>.
-                            <li>Click <code>target</code> directory,
-                                its absolute path should be displayed.
+                        <li>Ensure <b>Enable multi-selection</b> is cleared
+                            (the default state).
+                        <li>Click <code>link</code> directory,
+                            the absolute path of the symbolic
+                            link should be displayed.<br>
+                            If it's <code>null</code>, click <b>Fail</b>.
+                        <li>Click <code>target</code> directory,
+                            its absolute path should be displayed.
                         </ol>
                     <li><strong>Multi-selection:</strong>
                         <ol>
-                            <li>Select <b>Enable multi-selection</b>.
-                            <li>Click <code>link</code>,
-                            <li>Press <kbd>Ctrl</kbd> and
-                                then click <code>target</code>.
-                            <li>Both should be selected and
-                                their absolute paths should be displayed.
-                            <li>If <code>link</code> can't be selected or
-                                if its absolute path is <code>null</code>,
-                                click <b>Fail</b>.
+                        <li>Select <b>Enable multi-selection</b>.
+                        <li>Click <code>link</code>,
+                        <li>Press <kbd>Ctrl</kbd> and
+                            then click <code>target</code>.
+                        <li>Both should be selected and
+                            their absolute paths should be displayed.
+                        <li>If <code>link</code> can't be selected or
+                            if its absolute path is <code>null</code>,
+                            click <b>Fail</b>.
                         </ol>
                     </ul>
                     <p>If <code>link</code> can be selected in both
                     single- and multi-selection modes, click <b>Pass</b>.</p>
                 <li>When done with testing, paste the following commands to
-                   remove the <code>FileChooserTest</code> directory:
+                    remove the <code>FileChooserTest</code> directory:
                 <pre><code>cd \\
                 rmdir /s /q C:\\FileChooserTest</code></pre>
 
