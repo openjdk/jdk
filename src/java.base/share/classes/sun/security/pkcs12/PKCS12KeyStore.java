4ac28fc1407e80457b4d2b003e1b23d0158f8da9
Chad Rakoczy <chadrako@amazon.com>
2023-09-13 21:03:53 +0000

    8285516: clearPassword should be called in a finally try block
    Backport-of: 36e4df9d66134ef160bbba0e59d0e3dbb183ba4b

--- src/java.base/share/classes/sun/security/pkcs12/PKCS12KeyStore.java
+++ src/java.base/share/classes/sun/security/pkcs12/PKCS12KeyStore.java
@@ -864,14 +864,14 @@ public final class PKCS12KeyStore extends KeyStoreSpi {
     {
         SecretKey skey = null;
 
+        PBEKeySpec keySpec = new PBEKeySpec(password);
         try {
-            PBEKeySpec keySpec = new PBEKeySpec(password);
             SecretKeyFactory skFac = SecretKeyFactory.getInstance("PBE");
             skey = skFac.generateSecret(keySpec);
-            keySpec.clearPassword();
         } catch (Exception e) {
-           throw new IOException("getSecretKey failed: " +
-                                 e.getMessage(), e);
+            throw new IOException("getSecretKey failed: " + e.getMessage(), e);
+        } finally {
+            keySpec.clearPassword();
         }
         return skey;
     }
