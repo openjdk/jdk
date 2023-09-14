c336780dff983944bf740f75d86b00a8df37423d
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-09-14 10:21:43 -0700

    8316156: Convert test to JUnit 5 and separate read/write testing

--- test/jdk/java/nio/channels/Channels/ChannelStreamsIO.java
+++ test/jdk/java/nio/channels/Channels/ChannelStreamsIO.java
@@ -24,9 +24,11 @@
 /* @test
  * @bug 8316156
  * @summary Ensure Channel{In,Out}putStreams do not overrun MaxDirectMemorySize
- * @run main/othervm -XX:MaxDirectMemorySize=5M ChannelStreamsIO
+ * @run junit/othervm -XX:MaxDirectMemorySize=5M ChannelStreamsIO
  */
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -39,34 +41,63 @@ import java.util.Random;
 
 import static java.nio.file.StandardOpenOption.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class ChannelStreamsIO {
     // this value must exceed MaxDirectMemorySize
     private static final int SIZE = 10*1024*1024;
 
-    public static void main(String[] args) throws IOException {
-        byte[] x = new byte[SIZE];
-        Random rnd = new Random(System.nanoTime());
-        rnd.nextBytes(x);
+    static byte[] src;
+    static Path path;
 
-        Path file = Files.createTempFile("sna", "fu");
-        try {
-            try (FileChannel fc = FileChannel.open(file, CREATE, WRITE)) {
-                try (OutputStream out = Channels.newOutputStream(fc)) {
-                    out.write(x);
-                }
-            }
-            try (FileChannel fc = FileChannel.open(file, READ)) {
-                try (InputStream in = Channels.newInputStream(fc)) {
-                    byte[] y = new byte[SIZE];
-                    int n = -1;
-                    if ((n = in.read(y)) != SIZE)
-                        throw new RuntimeException("n " + n + " != " + SIZE);
-                    if (!Arrays.equals(x, y))
-                        throw new RuntimeException("Arrays are not equal");
-                }
-            }
-        } finally {
-            Files.delete(file);
+    @BeforeAll
+    public static void before() throws IOException {
+        src = new byte[SIZE];
+        Random rnd = new Random(System.nanoTime());
+        rnd.nextBytes(src);
+        path = Files.createTempFile("SNA", "FU");
+    }
+
+    @AfterEach
+    public void after() throws IOException {
+        Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void write() throws IOException {
+        try (FileChannel fc = FileChannel.open(path, CREATE, WRITE);
+             OutputStream out = Channels.newOutputStream(fc)) {
+            out.write(src);
+        } catch (OutOfMemoryError oome) {
+            throw new RuntimeException(oome);
+        }
+
+        try (InputStream in = new FileInputStream(path.toFile())) {
+            byte[] dst = new byte[SIZE];
+            int n = -1;
+            if ((n = in.read(dst)) != SIZE)
+                throw new RuntimeException(n + " != " + SIZE);
+            if (!Arrays.equals(src, dst))
+                throw new RuntimeException("Arrays are not equal");
+        }
+    }
+
+    @Test
+    public void read() throws IOException {
+        try (OutputStream out = new FileOutputStream(path.toFile());
+             FileChannel fc = FileChannel.open(path, READ);
+             InputStream in = Channels.newInputStream(fc)) {
+            out.write(src);
+            byte[] dst = new byte[SIZE];
+            int n = -1;
+            if ((n = in.read(dst)) != SIZE)
+                throw new RuntimeException(n + " != " + SIZE);
+            if (!Arrays.equals(src, dst))
+                throw new RuntimeException("Arrays are not equal");
+        } catch (OutOfMemoryError oome) {
+            throw new RuntimeException(oome);
         }
     }
 }
