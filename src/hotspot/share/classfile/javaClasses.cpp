35cf71fcef1d7a49990d4c47ead9564c0b363913
Goetz Lindenmaier <goetz@openjdk.org>
2023-09-15 14:08:05 +0000

    8301065: Handle control characters in java_lang_String::print
    Backport-of: 41d6be4d807921a91339029ae96e8dc14561bea6

--- src/hotspot/share/classfile/javaClasses.cpp
+++ src/hotspot/share/classfile/javaClasses.cpp
@@ -819,8 +819,13 @@ void java_lang_String::print(oop java_string, outputStream* st) {
 
   st->print("\"");
   for (int index = 0; index < length; index++) {
-    st->print("%c", (!is_latin1) ?  value->char_at(index) :
-                           ((jchar) value->byte_at(index)) & 0xff );
+    jchar c = (!is_latin1) ?  value->char_at(index) :
+                             ((jchar) value->byte_at(index)) & 0xff;
+    if (c < ' ') {
+      st->print("\\x%02X", c); // print control characters e.g. \x0A
+    } else {
+      st->print("%c", c);
+    }
   }
   st->print("\"");
 }
