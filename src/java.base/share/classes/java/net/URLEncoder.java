cbb7628a595d7b5c8b1d98c9bf7888c546cf9d59
shaojin.wensj <shaojin.wensj@alibaba-inc.com>
2023-09-18 06:28:11 +0800

    optimize encodeUTF8

--- src/java.base/share/classes/java/net/URLEncoder.java
+++ src/java.base/share/classes/java/net/URLEncoder.java
@@ -250,38 +250,86 @@ public class URLEncoder {
 
         int spaceCount = 0;
         int needEncodingCount = 0;
-        boolean ascii = true;
-        for (int i = 0; i < s.length(); ++i) {
-            int c = s.charAt(i);
-            if (c == ' ') {
-                spaceCount++;
-            }
-            if (!dotNeedEncoding(c)) {
+        boolean utf8 = charset == StandardCharsets.UTF_8;
+        int ut8Length = 0;
+        boolean surrogateError = false;
+        for (int i = 0, length = s.length(); i < length; ++i) {
+            char c = s.charAt(i);
+            if (c < 0x80) {
+                if (c == ' ') {
+                    spaceCount++;
+                }
+                if (!dotNeedEncoding(c)) {
+                    needEncodingCount++;
+                }
+            } else {
                 needEncodingCount++;
-                if (ascii & c > 0x7f) {
-                    ascii = false;
+                if (utf8) {
+                    if (c < 0x800) {
+                        // 2 bytes
+                        ut8Length += 3;
+                    } else if (Character.isSurrogate(c)) {
+                        // Have a surrogate pair
+                        if (Character.isHighSurrogate(c) && i + 1 < length) {
+                            char d = s.charAt(i + 1);
+                            if (Character.isLowSurrogate(d)) {
+                                ut8Length += 8;
+                                i++;
+                                continue;
+                            }
+                        }
+                        surrogateError = true;
+                        ut8Length += 3;
+                    } else {
+                        // 3 bytes
+                        ut8Length += 6;
+                    }
                 }
             }
         }
 
         if (needEncodingCount == 0 && spaceCount == 0) {
+            // not need change
             return s;
         }
 
-        if (!ascii) {
-            return encodeSlow(s, charset);
+        if (utf8 & !surrogateError) {
+            return encodeUTF8(s, needEncodingCount, ut8Length);
         }
 
-        byte[] buf = new byte[s.length() + needEncodingCount * 2];
+        return encodeSlow(s, charset);
+    }
+
+    private static String encodeUTF8(String s, int needEncodingCount, int ut8Length) {
+        byte[] buf = new byte[s.length() + needEncodingCount * 2 + ut8Length];
 
         int off = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            int c = s.charAt(i);
-            if (dotNeedEncoding(c)) {
-                buf[off++] = (byte) (c == ' ' ? '+' : c);
+        for (int i = 0, length = s.length(); i < length; ++i) {
+            char c = s.charAt(i);
+            if (c < 0x80) {
+                if (dotNeedEncoding(c)) {
+                    buf[off++] = (byte) (c == ' ' ? '+' : c);
+                } else {
+                    putEncoded(buf, off, c);
+                    off += 3;
+                }
+            } else if (c < 0x800) {
+                // 2 bytes, 11 bits
+                utf8Put2(buf, off, c);
+                off += 6;
+            } else if (Character.isSurrogate(c)) {
+                // Have a surrogate pair and not handle surrogate error
+                char d = s.charAt(i + 1);
+                utf8PutSurrogatePair(
+                        buf,
+                        off,
+                        Character.toCodePoint(c, d));
+                off += 12;
+                i++;
             } else {
-                putHex(buf, off, c);
-                off += 3;
+                // 3 bytes
+                utf8Put3(buf, off, c);
+                off += 9;
             }
         }
 
@@ -292,7 +340,25 @@ public class URLEncoder {
         }
     }
 
-    private static void putHex(byte[] buf, int off, int c) {
+    private static void utf8PutSurrogatePair(byte[] buf, int off, int uc) {
+        putEncoded(buf, off, 0xf0 | ((uc >> 18)));
+        putEncoded(buf, off + 3, 0x80 | ((uc >> 12) & 0x3f));
+        putEncoded(buf, off + 6, 0x80 | ((uc >> 6) & 0x3f));
+        putEncoded(buf, off + 9, 0x80 | (uc & 0x3f));
+    }
+
+    private static void utf8Put2(byte[] buf, int off, int c) {
+        putEncoded(buf, off, 0xC0 | ((c >> 6) & 0x1F));
+        putEncoded(buf, off + 3, 0x80 | (c & 0x3F));
+    }
+
+    private static void utf8Put3(byte[] buf, int off, int c) {
+        putEncoded(buf, off, 0xE0 | ((c >> 12) & 0x0F));
+        putEncoded(buf, off + 3, 0x80 | ((c >> 6) & 0x3F));
+        putEncoded(buf, off + 6, 0x80 | (c & 0x3F));
+    }
+
+    private static void putEncoded(byte[] buf, int off, int c) {
         buf[off] = '%';
         int n0 = (c >> 4) & 0xf;
         buf[off + 1] = (byte) (n0 + (n0 < 10 ? '0' : 'A' - 10));
