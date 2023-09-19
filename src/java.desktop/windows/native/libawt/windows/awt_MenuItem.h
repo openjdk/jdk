0c972468e7d72c991f28943f602232e0317ea21d
Harshitha Onkar <honkar@openjdk.org>
2023-09-19 17:37:49 +0000

    8311113: Remove invalid pointer cast and clean up setLabel() in awt_MenuItem.cpp
    Reviewed-by: aivanov, serb

--- src/java.desktop/windows/native/libawt/windows/awt_MenuItem.h
+++ src/java.desktop/windows/native/libawt/windows/awt_MenuItem.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -133,7 +133,7 @@ public:
     virtual BOOL IsTopMenu();
     void DrawCheck(HDC hDC, RECT rect);
 
-    void SetLabel(LPCTSTR sb);
+    void SetLabel();
     virtual void Enable(BOOL isEnabled);
     virtual void UpdateContainerLayout();
     virtual void RedrawMenuBar();
