cdbce9c49dae3e7176362f1f954bf771af6fac92
Goetz Lindenmaier <goetz@openjdk.org>
2023-09-27 08:10:03 +0000

    8249826: 5 javax/net/ssl/SSLEngine tests use @ignore w/o bug-id
    Backport-of: bf9a8ce0bb975a3d50e92148f92850ef930d64b0

--- test/jdk/javax/net/ssl/SSLEngine/Basics.java
+++ test/jdk/javax/net/ssl/SSLEngine/Basics.java
@@ -26,8 +26,6 @@
  * @bug 4495742
  * @summary Add non-blocking SSL/TLS functionality, usable with any
  *      I/O abstraction
- * @ignore JSSE supported cipher suites are changed with CR 6916074,
- *     need to update this test case in JDK 7 soon
  *
  * This is intended to test many of the basic API calls to the SSLEngine
  * interface.  This doesn't really exercise much of the SSL code.
