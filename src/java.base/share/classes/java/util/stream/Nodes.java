3ab7af5bd7d50d9aef70f0c77fdca92e5509570d
Mourad Abbay <mourad.abbay@oracle.com>
2023-09-27 18:44:18 +0100

    Remove unused imports.

--- src/java.base/share/classes/java/util/stream/Nodes.java
+++ src/java.base/share/classes/java/util/stream/Nodes.java
@@ -28,7 +28,6 @@ import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Deque;
-import java.util.List;
 import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
--- src/java.base/share/classes/java/util/stream/package-info.java
+++ src/java.base/share/classes/java/util/stream/package-info.java
@@ -752,5 +752,4 @@
  */
 package java.util.stream;
 
-import java.util.function.BinaryOperator;
 import java.util.function.UnaryOperator;
