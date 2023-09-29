5ee4d17fdbe9e04cba6dabe7d415745e29278075
Angelos Bimpoudis <angelos.bimpoudis@oracle.com>
2023-09-29 12:06:37 +0200

    8317048: VerifyError with unnamed pattern variable and more than one components

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/TransPatterns.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/TransPatterns.java
@@ -907,6 +907,7 @@ public class TransPatterns extends TreeTranslator {
                 }
             } else if (currentBinding != null &&
                        commonBinding.type.tsym == currentBinding.type.tsym &&
+                       commonBinding.isUnnamedVariable() == currentBinding.isUnnamedVariable() &&
                        !previousNullable &&
                        new TreeDiffer(List.of(commonBinding), List.of(currentBinding))
                                .scan(commonNestedExpression, currentNestedExpression)) {
--- test/langtools/tools/javac/patterns/T8317048.java
+++ test/langtools/tools/javac/patterns/T8317048.java
@@ -35,7 +35,7 @@ public class T8317048 {
     record R2<T>(Integer value) implements R<T> {}
     sealed interface R<T> {}
 
-    static <T1 extends Comparable, T2 extends Comparable> int compareTo(Tuple<R<T1>, R<T2>> o) {
+    static <T1 extends Comparable, T2 extends Comparable> int meth1(Tuple<R<T1>, R<T2>> o) {
         return switch (o) {
             case Tuple<R<T1>, R<T2>>(R1<T1> _, R1<T2> _) -> -1;
             case Tuple<R<T1>, R<T2>>(R1<T1> _, R2<T2> _) -> -1;
@@ -44,7 +44,45 @@ public class T8317048 {
         };
     }
 
+    static <T1 extends Comparable, T2 extends Comparable> int meth2(Tuple<R<T1>, R<T2>> o) {
+        return switch (o) {
+            case Tuple<R<T1>, R<T2>>(R1<T1> _, R1<T2> _) -> -1;
+            case Tuple<R<T1>, R<T2>>(R1<T1> fst, R2<T2> snd) -> fst.value().compareTo(snd.value());
+            case Tuple<R<T1>, R<T2>>(R2<T1> _, R1<T2> _) -> -1;
+            case Tuple<R<T1>, R<T2>>(R2<T1> fst, R2<T2> snd) -> fst.value().compareTo(snd.value());
+        };
+    }
+
+    static <T1 extends Comparable, T2 extends Comparable> int meth3(Tuple<R<T1>, R<T2>> o) {
+        return switch (o) {
+            case Tuple<R<T1>, R<T2>>(R1<T1> fst, R1<T2> _) -> fst.value();
+            case Tuple<R<T1>, R<T2>>(R1<T1> _, R2<T2> snd) -> snd.value();
+            case Tuple<R<T1>, R<T2>>(R2<T1> _, R1<T2> _) -> -1;
+            case Tuple<R<T1>, R<T2>>(R2<T1> fst, R2<T2> snd) -> fst.value().compareTo(snd.value());
+        };
+    }
+
+    static <T1 extends Comparable, T2 extends Comparable> int meth4(Tuple<R<T1>, R<T2>> o) {
+        return switch (o) {
+            case Tuple<R<T1>, R<T2>>(R1<T1> _, R1<T2> _) -> -1;
+            case Tuple<R<T1>, R<T2>>(R1<T1> _, R2<T2> _) -> -1;
+            case Tuple<R<T1>, R<T2>>(R2<T1> fst, R2<T2> snd) -> fst.value().compareTo(snd.value());
+            case Tuple<R<T1>, R<T2>>(R2<T1> _, R1<T2> _) -> -1;
+        };
+    }
+
     public static void main(String[] args) {
-        compareTo(new Tuple<R<Integer>, R<Integer>>(new R2<>(1), new R2<>(2)));
+        assertEquals(1, meth1(new Tuple<R<Integer>, R<Integer>>(new R2<>(2), new R2<>(1))));
+        assertEquals(1, meth2(new Tuple<R<Integer>, R<Integer>>(new R1<>(2), new R2<>(1))));
+        assertEquals(0, meth2(new Tuple<R<Integer>, R<Integer>>(new R2<>(1), new R2<>(1))));
+        assertEquals(2, meth3(new Tuple<R<Integer>, R<Integer>>(new R1<>(2), new R1<>(1))));
+        assertEquals(3, meth3(new Tuple<R<Integer>, R<Integer>>(new R1<>(2), new R2<>(3))));
+        assertEquals(1, meth4(new Tuple<R<Integer>, R<Integer>>(new R2<>(2), new R2<>(1))));
+    }
+
+    static void assertEquals(int expected, int actual) {
+        if (expected != actual) {
+            throw new AssertionError("Expected: " + expected + ", but got: " + actual);
+        }
     }
 }
