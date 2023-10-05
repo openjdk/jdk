ced89d4a7fd9a2d88ea722921642b6e054a4b69d
Justin Lu <justin.lu@oracle.com>
2023-10-05 13:02:19 -0700

    init

--- src/java.base/share/classes/java/text/ChoiceFormat.java
+++ src/java.base/share/classes/java/text/ChoiceFormat.java
@@ -231,6 +231,9 @@ public class ChoiceFormat extends NumberFormat {
      * for the ChoiceFormat pattern can be seen in the {@linkplain ##patterns
      * Patterns} section.
      *
+     * @implSpec {@link ChoiceFormat#ChoiceFormat(String)} invokes this method.
+     * Therefore, any changes made to the applyPattern method in subclasses
+     * will be reflected in the constructor as well.
      * @param newPattern a pattern string
      * @throws    NullPointerException if {@code newPattern}
      *            is {@code null}
