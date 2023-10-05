bd10afa83e97d8128f853e71d24e58491053dd3a
Justin Lu <justin.lu@oracle.com>
2023-10-05 13:35:23 -0700

    init

--- test/jdk/java/text/Format/ChoiceFormat/Bug8001209.java
+++ test/jdk/java/text/Format/ChoiceFormat/Bug8001209.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,36 +21,47 @@
  * questions.
  */
 
-/**
+/*
  * @test
  * @bug 8001209
  * @summary Confirm that the values set by setChoices() are not mutable.
+ * @run junit Bug8001209
  */
-import java.text.*;
+
+import java.text.ChoiceFormat;
+import java.text.ParsePosition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Bug8001209 {
 
-    public static void main(String[] args) throws Exception {
-        boolean err = false;
-
-        // Borrow an example in API doc
-        double[] limits = {1,2,3,4,5,6,7};
-        String[] dayOfWeekNames = {"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    /*
+     * Ensure that mutating the arrays passed to setChoices and setLimits as well
+     * as the arrays returned by getChoices and getLimits does not affect
+     * the internal representation of the original ChoiceFormat.
+     */
+    @Test
+    public void immutableChoicesAndFormatsTest() {
+        double[] limits = {1, 2, 3, 4, 5, 6, 7};
+        String[] dayOfWeekNames = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
         ChoiceFormat form = new ChoiceFormat(limits, dayOfWeekNames);
         ParsePosition status = new ParsePosition(0);
 
+        // Build the expected results of formatting with the original ChoiceFormat
         StringBuilder before = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
             status.setIndex(0);
             String s = form.format(i);
             before.append(" ");
             before.append(s);
-            before.append(form.parse(form.format(i),status));
+            before.append(form.parse(form.format(i), status));
         }
         String original = before.toString();
 
-        double[] newLimits = form.getLimits();
-        String[] newFormats = (String[])form.getFormats();
+        // Modify the array returned by getFormats() -> newFormats
+        String[] newFormats = (String[]) form.getFormats();
         newFormats[6] = "Doyoubi";
         StringBuilder after = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
@@ -58,14 +69,13 @@ public class Bug8001209 {
             String s = form.format(i);
             after.append(" ");
             after.append(s);
-            after.append(form.parse(form.format(i),status));
+            after.append(form.parse(form.format(i), status));
         }
-        if (!original.equals(after.toString())) {
-            err = true;
-            System.err.println("  Expected:" + before
-                               + "\n  Got:     " + after);
-        }
+        // Compare the expected results with the new formatted results
+        assertEquals(after.toString(), original,
+                "Mutating array returned from getter changed internals of ChoiceFormat");
 
+        // Modify the array passed to setFormats() -> dayOfWeekNames
         dayOfWeekNames[6] = "Saturday";
         after = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
@@ -73,18 +83,10 @@ public class Bug8001209 {
             String s = form.format(i);
             after.append(" ");
             after.append(s);
-            after.append(form.parse(form.format(i),status));
+            after.append(form.parse(form.format(i), status));
         }
-        if (!original.equals(after.toString())) {
-            err = true;
-            System.err.println("  Expected:" + before
-                               + "\n  Got:     " + after);
-        }
-
-        if (err) {
-            throw new RuntimeException("Failed.");
-        } else {
-            System.out.println("Passed.");
-        }
+        // Compare the expected results with the new formatted results
+        assertEquals(after.toString(), original,
+                "Mutating array passed to setter changed internals of ChoiceFormat");
     }
 }
