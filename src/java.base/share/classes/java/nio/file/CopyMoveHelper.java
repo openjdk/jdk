e6028854e1074bfeb5f68f566b8a7ea1cfecf331
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-10-04 18:12:29 -0700

    8317128: Move read check outside replaceExisting c conditional

--- src/java.base/share/classes/java/nio/file/CopyMoveHelper.java
+++ src/java.base/share/classes/java/nio/file/CopyMoveHelper.java
@@ -133,14 +133,14 @@ class CopyMoveHelper {
         if (sourceAttrs.isSymbolicLink())
             throw new IOException("Copying of symbolic links not supported");
 
-        // delete target if it exists and REPLACE_EXISTING is specified
-        if (opts.replaceExisting) {
-            // ensure source can be copied
-            FileSystemProvider provider = source.getFileSystem().provider();
-            provider.checkAccess(source, AccessMode.READ);
+        // ensure source can be copied
+        FileSystemProvider provider = source.getFileSystem().provider();
+        provider.checkAccess(source, AccessMode.READ);
 
+        // delete target if it exists and REPLACE_EXISTING is specified
+        if (opts.replaceExisting)
             Files.deleteIfExists(target);
-        } else if (Files.exists(target))
+        else if (Files.exists(target))
             throw new FileAlreadyExistsException(target.toString());
 
         // create directory or copy file
