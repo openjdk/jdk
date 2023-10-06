b90d18c4d351c43dce615b5243bd85c309fbb15a
Joe Darcy <joe.darcy@oracle.com>
2023-10-06 13:32:55 -0700

    Implement review feedback.

--- test/langtools/tools/javac/6341866/T6341866.java
+++ test/langtools/tools/javac/6341866/T6341866.java
@@ -67,7 +67,6 @@ public class T6341866 {
 
     enum AnnoType {
         NONE,           // no annotation processing
-     // SERVICE,        // implicit annotation processing, via ServiceLoader
         SPECIFY         // explicit annotation processing
     };
 
@@ -107,9 +106,6 @@ public class T6341866 {
             opts.add(implicitType.opt);
 
         switch (annoType) {
-//         case SERVICE:
-//             createProcessorServices(Anno.class.getName());
-//             break;
         case SPECIFY:
             opts.addAll(Arrays.asList("-processor", Anno.class.getName()));
             break;
@@ -148,9 +144,6 @@ public class T6341866 {
             String expectKey = null;
             if (implicitType == ImplicitType.OPT_UNSET) {
                 switch (annoType) {
-//                 case SERVICE:
-//                      expectKey = "compiler.warn.proc.use.proc.or.implicit";
-//                     break;
                 case SPECIFY:
                     expectKey = "compiler.warn.proc.use.implicit";
                     break;
