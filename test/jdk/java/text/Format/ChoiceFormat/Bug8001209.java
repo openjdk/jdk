fcd8ebfa9f81f128fd8738f65a327f31ca43adf6
Justin Lu <justin.lu@oracle.com>
2023-10-06 10:43:10 -0700

    Review: separate into two tests

--- test/jdk/java/text/Format/ChoiceFormat/Bug8001209.java
+++ test/jdk/java/text/Format/ChoiceFormat/Bug8001209.java
@@ -31,62 +31,80 @@
 import java.text.ChoiceFormat;
 import java.text.ParsePosition;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Bug8001209 {
 
-    /*
-     * Ensure that mutating the arrays passed to setChoices and setLimits as well
-     * as the arrays returned by getChoices and getLimits does not affect
-     * the internal representation of the original ChoiceFormat.
-     */
-    @Test
-    public void immutableChoicesAndFormatsTest() {
+    // Represents the expected output of formatting the ChoiceFormat
+    private static String expectedFormattedOutput;
+    private static ChoiceFormat cFmt;
+    private static ParsePosition status;
+    private static String[] originalSetterArray;
+
+    // Build the original ChoiceFormat to test if it can be mutated
+    @BeforeAll
+    static void setUpChoiceFormatAndOutput() {
         double[] limits = {1, 2, 3, 4, 5, 6, 7};
-        String[] dayOfWeekNames = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
-        ChoiceFormat form = new ChoiceFormat(limits, dayOfWeekNames);
-        ParsePosition status = new ParsePosition(0);
+        originalSetterArray = new String[]{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+        // Constructor calls setChoices
+        cFmt = new ChoiceFormat(limits, originalSetterArray);
+        status = new ParsePosition(0);
 
         // Build the expected results of formatting with the original ChoiceFormat
         StringBuilder before = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
             status.setIndex(0);
-            String s = form.format(i);
+            String s = cFmt.format(i);
             before.append(" ");
             before.append(s);
-            before.append(form.parse(form.format(i), status));
+            before.append(cFmt.parse(cFmt.format(i), status));
         }
-        String original = before.toString();
+        expectedFormattedOutput = before.toString();
+    }
 
+    /*
+     * Ensure that mutating he arrays returned by getChoices and getLimits does
+     * not affect the internal representation of the ChoiceFormat.
+     */
+    @Test
+    public void immutableArraysFromGetters() {
         // Modify the array returned by getFormats() -> newFormats
-        String[] newFormats = (String[]) form.getFormats();
+        String[] newFormats = (String[]) cFmt.getFormats();
         newFormats[6] = "Doyoubi";
         StringBuilder after = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
             status.setIndex(0);
-            String s = form.format(i);
+            String s = cFmt.format(i);
             after.append(" ");
             after.append(s);
-            after.append(form.parse(form.format(i), status));
+            after.append(cFmt.parse(cFmt.format(i), status));
         }
         // Compare the expected results with the new formatted results
-        assertEquals(after.toString(), original,
+        assertEquals(after.toString(), expectedFormattedOutput,
                 "Mutating array returned from getter changed internals of ChoiceFormat");
+    }
 
+    /*
+     * Ensure that mutating the arrays passed to setChoices/constructor does
+     * not affect the internal representation of the ChoiceFormat.
+     */
+    @Test
+    public void immutableArraysFromSetter() {
         // Modify the array passed to setFormats() -> dayOfWeekNames
-        dayOfWeekNames[6] = "Saturday";
-        after = new StringBuilder();
+        originalSetterArray[6] = "Saturday";
+        StringBuilder after = new StringBuilder();
         for (double i = 1.0; i <= 7.0; ++i) {
             status.setIndex(0);
-            String s = form.format(i);
+            String s = cFmt.format(i);
             after.append(" ");
             after.append(s);
-            after.append(form.parse(form.format(i), status));
+            after.append(cFmt.parse(cFmt.format(i), status));
         }
         // Compare the expected results with the new formatted results
-        assertEquals(after.toString(), original,
+        assertEquals(after.toString(), expectedFormattedOutput,
                 "Mutating array passed to setter changed internals of ChoiceFormat");
     }
 }
