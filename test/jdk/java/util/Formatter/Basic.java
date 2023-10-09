bce554a84237298dc1d1bae6428db594a00a4df9
wenshao <shaojin.wensj@alibaba-inc.com>
2023-10-10 07:29:23 +0800

    move testcases to Basic.java

--- test/jdk/java/util/Formatter/Basic.java
+++ test/jdk/java/util/Formatter/Basic.java
@@ -24,6 +24,7 @@
 import java.io.*;
 import java.util.Formatter;
 import java.util.Locale;
+import java.util.UnknownFormatConversionException;
 
 public class Basic {
 
@@ -168,6 +169,8 @@ public class Basic {
     }
 
     public static void main(String[] args) {
+        common();
+
         BasicBoolean.test();
         BasicBooleanObject.test();
         BasicByte.test();
@@ -196,5 +199,13 @@ public class Basic {
         } else {
             System.out.printf("All %d tests passed", pass);
         }
+    }
+
+    private static void common() {
+        // non-conversion
+        tryCatch("%12", UnknownFormatConversionException.class);
+        tryCatch("% ", UnknownFormatConversionException.class);
+        tryCatch("%,", UnknownFormatConversionException.class);
+        tryCatch("%03.2", UnknownFormatConversionException.class);
     }
 }
