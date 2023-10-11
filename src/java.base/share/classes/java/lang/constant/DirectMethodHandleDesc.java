dd6d4103a1fd5c49b6b0a9a10a30c79251a62fa8
Per Minborg <pminborg@gmail.com>
2023-10-11 09:43:27 +0200

    Add @sealedGraph to MethodHandleDesc and descendants

--- src/java.base/share/classes/java/lang/constant/DirectMethodHandleDesc.java
+++ src/java.base/share/classes/java/lang/constant/DirectMethodHandleDesc.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -46,6 +46,7 @@ import static java.lang.invoke.MethodHandleInfo.REF_putStatic;
  * {@link MethodHandle}.  A {@linkplain DirectMethodHandleDesc} corresponds to
  * a {@code Constant_MethodHandle_info} entry in the constant pool of a classfile.
  *
+ * @sealedGraph
  * @since 12
  */
 public sealed interface DirectMethodHandleDesc
