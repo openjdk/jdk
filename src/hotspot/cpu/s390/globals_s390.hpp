5806af4a370986e92a884e6cc46caf8976fa3efd
Aleksey Shipilev <shipilev@amazon.de>
2023-10-12 16:23:51 +0200

    S390 implementation

--- src/hotspot/cpu/s390/globals_s390.hpp
+++ src/hotspot/cpu/s390/globals_s390.hpp
@@ -78,8 +78,7 @@ define_pd_global(bool, CompactStrings, true);
 // 8146801 (Short Array Allocation): No performance work done here yet.
 define_pd_global(intx, InitArrayShortSize, 1*BytesPerLong);
 
-// Not implemented yet
-define_pd_global(uint, SecondarySuperMissBackoff, 0);
+define_pd_global(uint, SecondarySuperMissBackoff, 1000);
 
 #define ARCH_FLAGS(develop,                                                   \
                    product,                                                   \
