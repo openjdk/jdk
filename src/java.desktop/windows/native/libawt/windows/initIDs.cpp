e66a9ffdda6e451c4298f59786adbd434dd1adb5
Daniel Jelinski <daniel.jelinski@oracle.com>
2023-10-13 09:37:46 +0200

    Remove FileDialog_initIDs

--- src/java.desktop/windows/native/libawt/windows/initIDs.cpp
+++ src/java.desktop/windows/native/libawt/windows/initIDs.cpp
@@ -28,25 +28,6 @@
 #include <jni_util.h>
 
 /************************************************************************
- * FileDialog initIDs
- */
-
-extern "C" {
-
-/*
- * Class:     java_awt_FileDialog
- * Method:    initIDs
- * Signature: ()V
- */
-JNIEXPORT void JNICALL
-Java_java_awt_FileDialog_initIDs(JNIEnv *env, jclass cls)
-{
-  /* This stub is needed, because the Solaris code needs this method. */
-}
-
-} /* extern "C" */
-
-/************************************************************************
  * TextField initIDs
  */
 
