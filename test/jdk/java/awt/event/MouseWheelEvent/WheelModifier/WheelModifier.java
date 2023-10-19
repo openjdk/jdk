72690f76b8bbbee5c15ad4399d6154e3a700df4a
Renjith Kannath Pariyangad <renjith.kannath.pariyangad@oracle.com>
2023-10-19 19:49:20 +0530

    Added frame dispose

--- test/jdk/java/awt/event/MouseWheelEvent/WheelModifier/WheelModifier.java
+++ test/jdk/java/awt/event/MouseWheelEvent/WheelModifier/WheelModifier.java
@@ -164,17 +164,17 @@ public class WheelModifier {
             throw new RuntimeException("Mouse is not released");
         }
         System.out.println("# Released!");
-
-        if (f != null) {
-            f.dispose();
-        }
     }
 
     public static void main(String[] args) throws Exception {
         WheelModifier test = new WheelModifier();
 
-        SwingUtilities.invokeAndWait(test::createGui);
-        test.run();
+        try {
+            SwingUtilities.invokeAndWait(test::createGui);
+            test.run();
+        } finally {
+            SwingUtilities.invokeAndWait(test.f::dispose);
+        }
 
         System.out.println("Done.");
     }
