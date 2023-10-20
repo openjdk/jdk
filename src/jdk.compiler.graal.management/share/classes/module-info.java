cc0474b2b4adcfa069a624c7473bab6db3945d06
Doug Simon <doug.simon@oracle.com>
2023-10-20 17:27:07 +0200

    fix copyright dates and @since tags to reflect history

--- src/jdk.compiler.graal.management/share/classes/module-info.java
+++ src/jdk.compiler.graal.management/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -33,7 +33,7 @@
  * module descriptor.
  *
  * @moduleGraph
- * @since 22
+ * @since 10
  */
 module jdk.compiler.graal.management {
     requires jdk.internal.vm.ci;
