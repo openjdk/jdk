2a60ac989d410491a91aaa68047d5ef3cbf76902
Jorn Vernee <jorn.vernee@oracle.com>
2023-10-24 10:41:59 +0200

    remove unused import

--- test/jdk/java/foreign/UpcallTestHelper.java
+++ test/jdk/java/foreign/UpcallTestHelper.java
@@ -29,7 +29,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
