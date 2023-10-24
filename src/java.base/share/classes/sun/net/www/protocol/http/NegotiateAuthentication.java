d7afd4cae989f1c1af791c6a35c8ecfcb5cf4ae4
Michael-Mc-Mahon <michael.x.mcmahon@oracle.com>
2023-10-24 17:27:25 +0100

    removed comment

--- src/java.base/share/classes/sun/net/www/protocol/http/NegotiateAuthentication.java
+++ src/java.base/share/classes/sun/net/www/protocol/http/NegotiateAuthentication.java
@@ -152,7 +152,6 @@ class NegotiateAuthentication extends AuthenticationInfo {
 
     @Override
     protected boolean useAuthCache() {
-        //return super.useAuthCache() && cacheSPNEGO;
         return false;
     }
 
