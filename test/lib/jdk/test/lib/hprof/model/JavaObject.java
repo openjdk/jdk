d4fe30dd6bd724017b543abf7c3fd5a17137904a
Aleksey Shipilev <shade@openjdk.org>
2023-10-25 08:33:59 +0000

    8316778: test hprof lib: invalid array element type from JavaValueArray.elementSize
    Backport-of: f7deaf4bef21cb86fcc8a6c10454fbd6bbbba2d9

--- test/lib/jdk/test/lib/hprof/model/JavaObject.java
+++ test/lib/jdk/test/lib/hprof/model/JavaObject.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -192,7 +192,7 @@ public class JavaObject extends JavaLazyReadObject {
         if (getClazz().isString()) {
             JavaThing value = getField("value");
             if (value instanceof JavaValueArray) {
-                return ((JavaValueArray)value).valueString();
+                return ((JavaValueArray)value).valueAsString();
             } else {
                 return "null";
             }
