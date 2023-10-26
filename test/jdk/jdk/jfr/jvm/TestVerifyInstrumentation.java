48b371c82b34a9a316dd1d0db1dd13fcf2a77514
mgronlun <markus.gronlund@oracle.com>
2023-10-26 17:08:54 +0200

    whitespace

--- test/jdk/jdk/jfr/jvm/TestVerifyInstrumentation.java
+++ test/jdk/jdk/jfr/jvm/TestVerifyInstrumentation.java
@@ -21,43 +21,41 @@
  * questions.
  */
 
- package jdk.jfr.jvm;
- 
- import jdk.jfr.Recording;
- import jdk.test.lib.jfr.EventNames;
- 
- /**
-  * @test
-  * @bug 8316271
-  * @key jfr
-  * @requires vm.hasJFR
-  * @library /test/lib
-  * @run main/othervm -Xverify:all jdk.jfr.jvm.TestVerifyInstrumentation
-  */
- public class TestVerifyInstrumentation {
- 
-     private final static String EVENT_NAME = EventNames.ThreadSleep;
- 
-     public static void main(String[] args) {
-         // This thread sleep wil load the jdk.internal.event.ThreadSleepEvent
-         // before JFR has started recording. This will set the type of the static
-         // field EventConfiguration to become untyped, i.e. java.lang.Object.
-         // Before issuing an invokevirtual instructions for this receiver, we must perform
-         // the proper type conversion, i.e. a downcast to type EventConfiguration using
-         // a conditional checkcast. -Xverify:all asserts this is ok.
-         //
-         // If not ok, the following exception is thrown as part of retransformation:
-         //
-         // java.lang.RuntimeException: JfrJvmtiAgent::retransformClasses failed: JVMTI_ERROR_FAILS_VERIFICATION.
-         //
-         try {
-             Thread.sleep(1);
-         } catch (InterruptedException ie) {}
-         try (Recording recording = new Recording()) {
-             recording.enable(EVENT_NAME).withoutThreshold().withStackTrace();
-             recording.start();
-             recording.stop();
-         }
-     }
- }
- 
+package jdk.jfr.jvm;
+
+import jdk.jfr.Recording;
+import jdk.test.lib.jfr.EventNames;
+
+/**
+ * @test
+ * @bug 8316271
+ * @key jfr
+ * @requires vm.hasJFR
+ * @library /test/lib
+ * @run main/othervm -Xverify:all jdk.jfr.jvm.TestVerifyInstrumentation
+ */
+public class TestVerifyInstrumentation {
+    private final static String EVENT_NAME = EventNames.ThreadSleep;
+
+    public static void main(String[] args) {
+        // This thread sleep wil load the jdk.internal.event.ThreadSleepEvent
+        // before JFR has started recording. This will set the type of the static
+        // field EventConfiguration to become untyped, i.e. java.lang.Object.
+        // Before issuing an invokevirtual instructions for this receiver, we must perform
+        // the proper type conversion, i.e. a downcast to type EventConfiguration using
+        // a conditional checkcast. -Xverify:all asserts this is ok.
+        //
+        // If not ok, the following exception is thrown as part of retransformation:
+        //
+        // java.lang.RuntimeException: JfrJvmtiAgent::retransformClasses failed: JVMTI_ERROR_FAILS_VERIFICATION.
+        //
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException ie) {}
+        try (Recording recording = new Recording()) {
+            recording.enable(EVENT_NAME).withoutThreshold().withStackTrace();
+            recording.start();
+            recording.stop();
+        }
+    }
+}
