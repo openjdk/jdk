fad0da2e6b952d47537c91687f2c0fdb002a36e5
Eirik Bjorsnos <eirbjo@gmail.com>
2023-10-28 14:12:27 +0200

    Remove excessive comment

--- src/java.base/share/classes/java/util/zip/ZipInputStream.java
+++ src/java.base/share/classes/java/util/zip/ZipInputStream.java
@@ -579,9 +579,9 @@ public class ZipInputStream extends InflaterInputStream implements ZipConstants 
         }
         if ((flag & 8) == 8) {
             /* "Data Descriptor" present */
-            if (hasZip64Extra(e) // LOC indicates Zip64 format, expect 8-byte sizes
-                    || inf.getBytesWritten() > ZIP64_MAGICVAL
-                    || inf.getBytesRead() > ZIP64_MAGICVAL) {
+            if (hasZip64Extra(e) ||
+                inf.getBytesWritten() > ZIP64_MAGICVAL ||
+                inf.getBytesRead() > ZIP64_MAGICVAL) {
                 // ZIP64 format
                 readFully(tmpbuf, 0, ZIP64_EXTHDR);
                 long sig = get32(tmpbuf, 0);
