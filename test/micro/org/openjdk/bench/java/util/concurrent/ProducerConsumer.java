f7a12af9757d18ce6d6346f501f0594fc11cfdf6
Viktor Klang <viktor.klang@oracle.com>
2023-10-30 16:04:30 +0100

    Updating copyright year and adjusting whitespace

--- test/micro/org/openjdk/bench/java/util/concurrent/ProducerConsumer.java
+++ test/micro/org/openjdk/bench/java/util/concurrent/ProducerConsumer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -67,7 +67,6 @@ public class ProducerConsumer {
 
     @Setup
     public void prepare(ThreadParams params) {
-
         capacity = Math.max(params.getThreadCount(), capacity);
 
         switch (type) {
