8d3a9aa378adcf2479e1a4e54bf5878a17e6286a
Jorn Vernee <jorn.vernee@oracle.com>
2023-10-31 17:39:59 +0100

    Revert "add exception handler pruning stress flags"
    This reverts commit e223d95b0b06a3369a3cce4963a3c7ea8907abb7.

--- src/hotspot/share/opto/c2_globals.hpp
+++ src/hotspot/share/opto/c2_globals.hpp
@@ -779,9 +779,6 @@
                                                                             \
   product(bool, PruneDeadExceptionHandlers, true,                           \
           "Prune exception handlers that are never entered")                \
-                                                                            \
-  develop(bool, StressPrunedExceptionHandlers, false,                       \
-          "Stress pruning of unentered exception handlers")                 \
 
 // end of C2_FLAGS
 
