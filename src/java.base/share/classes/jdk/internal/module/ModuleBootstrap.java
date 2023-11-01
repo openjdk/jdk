b214fa7c6d0bb32ff5211fa3c7c9b17afd1bb313
Calvin Cheung <calvin.cheung@oracle.com>
2023-11-01 04:29:31 +0000

    more comments from Alan and Ioi; copyright year update

--- src/java.base/share/classes/jdk/internal/module/ModuleBootstrap.java
+++ src/java.base/share/classes/jdk/internal/module/ModuleBootstrap.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -227,7 +227,8 @@ public final class ModuleBootstrap {
                 systemModules = SystemModuleFinders.systemModules(mainModule);
                 if (systemModules != null && !isPatched) {
                     needResolution = (traceOutput != null);
-                    canArchive = true;
+                    if (CDS.isDumpingStaticArchive())
+                        canArchive = true;
                 }
             }
             if (systemModules == null) {
