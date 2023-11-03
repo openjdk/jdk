4db0df9f225b107e2ae767e608d5c232bd0b40c4
Roman Kennke <rkennke@amazon.de>
2023-11-03 13:40:36 +0100

    8319376: Parallel: Forwarded objects during heap inspection

--- src/hotspot/share/memory/heapInspection.cpp
+++ src/hotspot/share/memory/heapInspection.cpp
@@ -213,6 +213,10 @@ KlassInfoEntry* KlassInfoTable::lookup(Klass* k) {
 // Return false if the entry could not be recorded on account
 // of running out of space required to create a new entry.
 bool KlassInfoTable::record_instance(const oop obj) {
+  if (obj->is_forwarded()) {
+    // We will see the forwardee instead, don't count this object as missed.
+    return true;
+  }
   Klass*        k = obj->klass();
   KlassInfoEntry* elt = lookup(k);
   // elt may be null if it's a new klass for which we
