340a389a7ee544fbc796df96069d2d5c561466d3
Goetz Lindenmaier <goetz@openjdk.org>
2023-11-06 07:15:48 +0000

    8309778: java/nio/file/Files/CopyAndMove.java fails when using second test directory
    Backport-of: cfae6ef2f61f0a6611de2f66e6e773c547ba7878

--- test/jdk/java/nio/file/Files/CopyAndMove.java
+++ test/jdk/java/nio/file/Files/CopyAndMove.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -892,7 +892,7 @@ public class CopyAndMove {
         if (supportsLinks) {
             source = createSourceFile(dir1);
             link = dir1.resolve("link");
-            createSymbolicLink(link, source);
+            createSymbolicLink(link, source.getFileName());
             target = getTargetFile(dir2);
             copyAndVerify(link, target);
             delete(link);
