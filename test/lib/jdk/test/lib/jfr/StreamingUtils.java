e3d52aa778cebac92ebf495127b70e30b8b20348
Goetz Lindenmaier <goetz@openjdk.org>
2023-11-06 07:11:31 +0000

    8269425: 2 jdk/jfr/api/consumer/streaming tests failed to attach
    Backport-of: fffa73c1ef377eb28371fc4094eea8725850de7d

--- test/lib/jdk/test/lib/jfr/StreamingUtils.java
+++ test/lib/jdk/test/lib/jfr/StreamingUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -51,8 +51,8 @@ public class StreamingUtils {
             try {
                 VirtualMachine vm = VirtualMachine.attach(String.valueOf(process.pid()));
                 String repo = vm.getSystemProperties().getProperty("jdk.jfr.repository");
+                vm.detach();
                 if (repo != null) {
-                    vm.detach();
                     System.out.println("JFR repository: " + repo);
                     return Paths.get(repo);
                 }
