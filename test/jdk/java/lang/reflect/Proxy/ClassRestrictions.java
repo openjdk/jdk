4be40b5d9d206261059919ad85a7fbfc16e2624d
Mandy Chung <mandy.chung@oracle.com>
2023-11-06 12:19:18 -0800

    Review feedback

--- test/jdk/java/lang/reflect/Proxy/ClassRestrictions.java
+++ test/jdk/java/lang/reflect/Proxy/ClassRestrictions.java
@@ -37,12 +37,13 @@ import java.lang.reflect.Proxy;
 import java.net.URLClassLoader;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClassRestrictions {
 
@@ -58,37 +59,26 @@ public class ClassRestrictions {
         void foo();
     }
 
-    static Stream<Arguments> proxyInterfaces() {
+    private static final String TEST_CLASSES = System.getProperty("test.classes", ".");
+    private static final ClassLoader LOADER = ClassRestrictions.class.getClassLoader();
+
+    static Stream<List<Class<?>>> badProxyInterfaces() {
         return Stream.of(
-                /*
-                 * All of the Class objects in the interfaces array must represent
-                 * interfaces, not classes or primitive types.
-                 */
-                Arguments.of(new Class<?>[] { Object.class }, "proxy class created with java.lang.Object as interface"),
-                Arguments.of(new Class<?>[] { Integer.TYPE }, "proxy class created with int.class as interface"),
-                Arguments.of(new Class<?>[] { Bar.class, Bar.class }, "proxy class created with repeated interfaces"),
-                /*
-                 * No two interfaces may each have a method with the same name and
-                 * parameter signature but different return type.
-                 */
-                Arguments.of(new Class<?>[] { Bar.class, Baz.class }, "proxy class created with conflicting methods")
+                List.of(Object.class),          // proxy interface cannot be a class
+                List.of(int.class),             // proxy interface can't be primitive type
+                List.of(Bar.class, Bar.class),  // cannot have repeated interfaces
+                // two proxy interfaces have the method of same method name but different return type
+                List.of(Bar.class, Baz.class)
         );
     }
 
     /*
-     * Test valid interfaces
+     * Test cases for illegal proxy interfaces
      */
     @ParameterizedTest
-    @MethodSource("proxyInterfaces")
-    void testForName(Class<?>[] interfaces, String message) {
-        ClassLoader loader = ClassRestrictions.class.getClassLoader();
-        try {
-            var proxyClass = Proxy.getProxyClass(loader, interfaces);
-            throw new Error(message);
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            // assume exception is for intended failure
-        }
+    @MethodSource("badProxyInterfaces")
+    void testForName(List<Class<?>> interfaces) {
+        assertThrows(IllegalArgumentException.class, () -> Proxy.getProxyClass(LOADER, interfaces.toArray(Class[]::new)));
     }
 
     private static final String nonPublicIntrfaceName = "java.util.zip.ZipConstants";
@@ -98,31 +88,21 @@ public class ClassRestrictions {
      */
     @Test
     void testNonPublicIntfs() throws Exception {
-        Class<?> nonPublic1 = Bashful.class;
-        Class<?> nonPublic2 = Class.forName(nonPublicIntrfaceName);
-        if (Modifier.isPublic(nonPublic2.getModifiers())) {
-            throw new Error("Interface " + nonPublicIntrfaceName +
-                            " is public and need to be changed!");
-        }
-        try {
-            ClassLoader loader = ClassRestrictions.class.getClassLoader();
-            var interfaces = new Class<?>[] { nonPublic1, nonPublic2 };
-            var proxyClass = Proxy.getProxyClass(loader, interfaces);
-            throw new Error("proxy class created with two non-public interfaces " +
-                            "in different packages");
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
+        var nonPublic1 = Bashful.class;
+        var nonPublic2 = Class.forName(nonPublicIntrfaceName);
+        assertFalse(Modifier.isPublic(nonPublic2.getModifiers()),
+            "Interface " + nonPublicIntrfaceName + " is public and need to be changed!");
+        var interfaces = new Class<?>[] { nonPublic1, nonPublic2 };
+        assertThrows(IllegalArgumentException.class, () -> Proxy.getProxyClass(LOADER, interfaces));
     }
 
     static Stream<ClassLoader> loaders() {
         return Stream.of(null,
                          ClassLoader.getPlatformClassLoader(),
-                         ClassLoader.getSystemClassLoader());
+                         ClassLoader.getSystemClassLoader(),
+                         LOADER);
     }
 
-    private static final String[] CPATHS = System.getProperty("test.classes", ".")
-                                                 .split(File.pathSeparator);
     /*
      * All of the interfaces types must be visible by name though the
      * specified class loader.
@@ -130,18 +110,14 @@ public class ClassRestrictions {
     @ParameterizedTest
     @MethodSource("loaders")
     void testNonVisibleInterface(ClassLoader loader) throws Exception {
-        URL[] urls = new URL[CPATHS.length];
-        for (int i = 0; i < CPATHS.length; i++) {
-            urls[i] = Paths.get(CPATHS[i]).toUri().toURL();
+        String[] cpaths = TEST_CLASSES.split(File.pathSeparator);
+        URL[] urls = new URL[cpaths.length];
+        for (int i = 0; i < cpaths.length; i++) {
+            urls[i] = Paths.get(cpaths[i]).toUri().toURL();
         }
-        ClassLoader altLoader = new URLClassLoader(urls, null);
-        Class<?> altBarClass = Class.forName(Bar.class.getName(), false, altLoader);
-        try {
-            var interfaces = new Class<?>[]{ altBarClass };
-            var proxyClass = Proxy.getProxyClass(loader, interfaces);
-            throw new Error("proxy class created with interface not visible to class loader");
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-        }
+        var altLoader = new URLClassLoader(urls, null);
+        var altBarClass = Class.forName(Bar.class.getName(), false, altLoader);
+        var interfaces = new Class<?>[]{ altBarClass };
+        assertThrows(IllegalArgumentException.class, () -> Proxy.getProxyClass(loader, interfaces));
     }
 }
