305b0567eb19a4eb0adccc6c6ea37d7f979f91b8
Stefan Karlsson <stefank@openjdk.org>
2023-11-07 08:22:06 +0100

    Punctuation

--- src/hotspot/share/runtime/synchronizer.cpp
+++ src/hotspot/share/runtime/synchronizer.cpp
@@ -1060,7 +1060,7 @@ JavaThread* ObjectSynchronizer::get_lock_owner(ThreadsList * t_list, Handle h_ob
 
 // Visitors ...
 
-// Iterate owned ObjectMonitors where the owner `filter` returns true
+// Iterate owned ObjectMonitors where the owner `filter` returns true.
 template <typename OwnerFilter>
 void ObjectSynchronizer::monitors_iterate_filtered(MonitorClosure* closure, OwnerFilter filter) {
   MonitorList::Iterator iter = _in_use_list.iterator();
@@ -1089,7 +1089,7 @@ void ObjectSynchronizer::monitors_iterate(MonitorClosure* closure, JavaThread* t
   return monitors_iterate_filtered(closure, thread_filter);
 }
 
-// Iterate owned ObjectMonitors
+// Iterate owned ObjectMonitors.
 void ObjectSynchronizer::monitors_iterate(MonitorClosure* closure) {
   auto all_filter = [&](void* owner) { return true; };
   return monitors_iterate_filtered(closure, all_filter);
