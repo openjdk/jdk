0d13a5a58667f52adfda8407dc6db3ea03a45371
JoeWang-Java <huizhe.wang@oracle.com>
2023-11-07 05:38:22 +0000

    remove commented out block in ExsltDatetime

--- src/java.xml/share/classes/com/sun/org/apache/xalan/internal/lib/ExsltDatetime.java
+++ src/java.xml/share/classes/com/sun/org/apache/xalan/internal/lib/ExsltDatetime.java
@@ -79,29 +79,6 @@ public class ExsltDatetime
      */
     public static String dateTime()
     {
-      /*Calendar cal = Calendar.getInstance();
-      Date datetime = cal.getTime();
-      // Format for date and time.
-      SimpleDateFormat dateFormat = new SimpleDateFormat(dt);
-
-      StringBuffer buff = new StringBuffer(dateFormat.format(datetime));
-      // Must also include offset from UTF.
-      // Get the offset (in milliseconds).
-      int offset = cal.get(Calendar.ZONE_OFFSET) + cal.get(Calendar.DST_OFFSET);
-      // If there is no offset, we have "Coordinated
-      // Universal Time."
-      if (offset == 0)
-        buff.append("Z");
-      else
-      {
-        // Convert milliseconds to hours and minutes
-        int hrs = offset/(60*60*1000);
-        // In a few cases, the time zone may be +/-hh:30.
-        int min = offset%(60*60*1000);
-        char posneg = hrs < 0? '-': '+';
-        buff.append(posneg).append(formatDigits(hrs)).append(':').append(formatDigits(min));
-      }
-      return buff.toString();*/
       String resultStr = "";
       try {
          GregorianCalendar cal = new GregorianCalendar();
