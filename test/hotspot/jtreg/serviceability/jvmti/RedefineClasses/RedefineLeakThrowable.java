5d61cda63cad215168763bb5017a67b47250adfb
MBaesken <matthias.baesken@sap.com>
2023-11-08 09:32:02 +0100

    Adjust start section of the test

--- test/hotspot/jtreg/serviceability/jvmti/RedefineClasses/RedefineLeakThrowable.java
+++ test/hotspot/jtreg/serviceability/jvmti/RedefineClasses/RedefineLeakThrowable.java
@@ -23,7 +23,7 @@
 
 /*
  * @test
- # @bug 8308762
+ * @bug 8308762
  * @library /test/lib
  * @summary Test that redefinition of class containing Throwable refs does not leak constant pool
  * @requires vm.jvmti
@@ -32,7 +32,7 @@
  * @modules java.instrument
  *          java.compiler
  * @run main RedefineClassHelper
- * @run main/othervm/timeout=6000 -javaagent:redefineagent.jar -XX:MetaspaceSize=23m  -XX:MaxMetaspaceSize=23m RedefineLeakThrowable
+ * @run main/othervm/timeout=6000 -javaagent:redefineagent.jar -XX:MetaspaceSize=23m -XX:MaxMetaspaceSize=23m RedefineLeakThrowable
  */
 
 class Tester {
