b9565b2788c5bff2527f5993ba425b7ba74d8b7d
Jan Lahoda <jan.lahoda@oracle.com>
2023-11-13 15:28:39 +0100

    Fixing tests.

--- test/langtools/jdk/jshell/Presets.java
+++ test/langtools/jdk/jshell/Presets.java
@@ -26,19 +26,22 @@ import java.util.*;
 
 public class Presets {
     public static final String TEST_DEFAULT_EXECUTION;
+    public static final String TEST_STANDARD_EXECUTION;
 
     static {
         String loopback = InetAddress.getLoopbackAddress().getHostAddress();
 
         TEST_DEFAULT_EXECUTION = "failover:0(jdi:hostname(" + loopback + "))," +
                                  "1(jdi:launch(true)), 2(jdi), 3(local)";
+        TEST_STANDARD_EXECUTION = "failover:0(jdi:hostname(" + loopback + "))," +
+                                  "1(jdi:launch(true)), 2(jdi)";
     }
 
     public static String[] addExecutionIfMissing(String[] args) {
-        if (Arrays.stream(args).noneMatch(a -> "--execution".equals(a))) {
+        if (Arrays.stream(args).noneMatch(Presets::remoteRelatedOption)) {
             List<String> augmentedArgs = new ArrayList<>();
 
-            augmentedArgs.add("--exection");
+            augmentedArgs.add("--execution");
             augmentedArgs.add(Presets.TEST_DEFAULT_EXECUTION);
             augmentedArgs.addAll(List.of(args));
 
@@ -46,5 +49,11 @@ public class Presets {
         }
 
         return args;
+    }
+
+    private static boolean remoteRelatedOption(String option) {
+        return "--execution".equals(option) ||
+               "--add-modules".equals(option) ||
+               option.startsWith("-R");
     }
 }
