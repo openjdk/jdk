fa795543ecd11bc0ef3a84a85d6a1a0ceb9821f1
Justin Lu <justin.lu@oracle.com>
2023-11-13 13:28:56 -0800

    reflect review, one -> any

--- src/java.base/share/classes/java/text/DateFormat.java
+++ src/java.base/share/classes/java/text/DateFormat.java
@@ -65,7 +65,7 @@ import sun.util.locale.provider.LocaleServiceProviderPool;
  * formatters based on the default or a given locale and a number of formatting
  * styles. The formatting styles include {@link #FULL}, {@link #LONG}, {@link #MEDIUM}, and {@link #SHORT}.
  * For any of the factory methods with the parameter <i>style</i>, an {@code
- * IllegalArgumentException} will be thrown if <i>style</i> is not equal to one
+ * IllegalArgumentException} will be thrown if <i>style</i> is not equal to any
  * of the defined formatting styles. More detail and examples of using these styles are provided in the method
  * descriptions.
  *
