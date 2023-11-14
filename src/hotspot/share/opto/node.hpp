c6896c2d401275ba1e8e516e964e96371873397d
Daniel Lundén <daniel.lunden@oracle.com>
2023-11-14 09:42:31 +0100

    Add curly braces around loop and conditional bodies

--- src/hotspot/share/opto/node.hpp
+++ src/hotspot/share/opto/node.hpp
@@ -1643,15 +1643,16 @@ void Node::visit_uses(Callback callback, Check is_boundary) const {
   Node_List worklist;
 
   // The initial worklist consists of the direct uses
-  for (DUIterator_Fast kmax, k = fast_outs(kmax); k < kmax; k++)
+  for (DUIterator_Fast kmax, k = fast_outs(kmax); k < kmax; k++) {
     worklist.push(fast_out(k));
+  }
 
   while (worklist.size() > 0) {
     Node* use = worklist.pop();
     // Skip already visited nodes
-    if (visited.test_set(use->_idx)) continue;
+    if (visited.test_set(use->_idx)) { continue; }
     // Apply callback on boundary nodes
-    if (is_boundary(use)) callback(use);
+    if (is_boundary(use)) { callback(use); }
     else {
       // Not a boundary node, continue search
       for (DUIterator_Fast kmax, k = use->fast_outs(kmax); k < kmax; k++)
