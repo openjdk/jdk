a6343c0b7b14563f9d219506ba431f96befd5401
Ioi Lam <iklam@openjdk.org>
2023-11-15 05:09:10 +0000

    8319999: Refactor MetaspaceShared::use_full_module_graph()
    Reviewed-by: dholmes, ccheung

--- src/hotspot/share/cds/cdsConfig.hpp
+++ src/hotspot/share/cds/cdsConfig.hpp
@@ -31,6 +31,8 @@
 class CDSConfig : public AllStatic {
 #if INCLUDE_CDS
   static bool _is_dumping_dynamic_archive;
+  static bool _dumping_full_module_graph_disabled;
+  static bool _loading_full_module_graph_disabled;
 #endif
 
 public:
@@ -43,6 +45,11 @@ public:
 
   // CDS archived heap
   static bool      is_dumping_heap()                         NOT_CDS_JAVA_HEAP_RETURN_(false);
+  static void disable_dumping_full_module_graph(const char* reason = nullptr) NOT_CDS_JAVA_HEAP_RETURN;
+  static bool      is_dumping_full_module_graph()            NOT_CDS_JAVA_HEAP_RETURN_(false);
+  static void disable_loading_full_module_graph(const char* reason = nullptr) NOT_CDS_JAVA_HEAP_RETURN;
+  static bool      is_loading_full_module_graph()            NOT_CDS_JAVA_HEAP_RETURN_(false);
+
 };
 
 #endif // SHARE_CDS_CDSCONFIG_HPP
