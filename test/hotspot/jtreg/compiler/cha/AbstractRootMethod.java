e85dd02f90d5786970732bb1c3669014e15f266e
Tobias Hartmann <tobias.hartmann@oracle.com>
2023-11-16 11:24:27 +0100

    Reverted test changes

--- test/hotspot/jtreg/compiler/cha/AbstractRootMethod.java
+++ test/hotspot/jtreg/compiler/cha/AbstractRootMethod.java
@@ -38,7 +38,7 @@
  *                   -XX:CompileCommand=compileonly,*::test -XX:CompileCommand=dontinline,*::test
  *                   -Xbatch -Xmixed -XX:+WhiteBoxAPI
  *                   -XX:-TieredCompilation
- *                   -XX:-StressMethodHandleLinkerInlining -XX:-StressIncrementalInlining
+ *                   -XX:-StressMethodHandleLinkerInlining
  *                      compiler.cha.AbstractRootMethod
  *
  * @run main/othervm -Xbootclasspath/a:. -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions
@@ -47,7 +47,7 @@
  *                   -XX:CompileCommand=compileonly,*::test -XX:CompileCommand=dontinline,*::test
  *                   -Xbatch -Xmixed -XX:+WhiteBoxAPI
  *                   -XX:+TieredCompilation -XX:TieredStopAtLevel=1
- *                   -XX:-StressMethodHandleLinkerInlining -XX:-StressIncrementalInlining
+ *                   -XX:-StressMethodHandleLinkerInlining
  *                      compiler.cha.AbstractRootMethod
  */
 package compiler.cha;
