f00d6747defab5b243d1587621f63f3b2501b0f3
Alan Bateman <alan.bateman@oracle.com>
2023-11-20 16:07:22 +0000

    Initial commit

--- src/java.base/aix/classes/sun/nio/ch/PollsetPoller.java
+++ src/java.base/aix/classes/sun/nio/ch/PollsetPoller.java
@@ -66,7 +66,7 @@ class PollsetPoller extends Poller {
     }
 
     @Override
-    void implDeregister(int fd) {
+    void implDeregister(int fdVal, boolean polled) {
         int ret = Pollset.pollsetCtl(setid, Pollset.PS_DELETE, fd, 0);
         assert ret == 0;
     }
