874582b7320d5a39432d5e63ff7a2ceaa5ff9f5d
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2023-11-22 08:41:54 +0000

    8320057: [macos14] javax/swing/JToolTip/4846413/bug4846413.java: Tooltip has not been found!

--- test/jdk/javax/swing/JToolTip/4846413/bug4846413.java
+++ test/jdk/javax/swing/JToolTip/4846413/bug4846413.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,71 +28,94 @@
  * @summary Checks if No tooltip modification when no KeyStroke modifier
  * @library ../../regtesthelpers
  * @build Util
- * @author Konstantin Eremin
  * @run main bug4846413
  */
+
+import java.io.File;
 import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
-import javax.swing.*;
-import java.awt.event.*;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.awt.event.KeyEvent;
+import java.awt.image.BufferedImage;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JToolTip;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.imageio.ImageIO;
 import javax.swing.plaf.metal.MetalToolTipUI;
 
 public class bug4846413 {
 
     private static volatile boolean isTooltipAdded;
     private static JButton button;
+    private static JFrame frame;
+    private static Robot robot;
 
     public static void main(String[] args) throws Exception {
+        try {
+            robot = new Robot();
+            robot.setAutoDelay(100);
 
-        Robot robot = new Robot();
-        robot.setAutoDelay(50);
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 
-        UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+            SwingUtilities.invokeAndWait(() -> createAndShowGUI());
 
-        javax.swing.SwingUtilities.invokeAndWait(new Runnable() {
+            robot.waitForIdle();
+	    robot.delay(1000);
 
-            public void run() {
-                createAndShowGUI();
+            Point movePoint = getButtonPoint();
+            robot.mouseMove(movePoint.x, movePoint.y);
+	    if (System.getProperty("os.name").contains("OS X")) {
+                String version = System.getProperty("os.version", "");
+                if (version.startsWith("14.")) {
+                    robot.mouseMove(movePoint.x, movePoint.y);
+                }
             }
-        });
+            robot.waitForIdle();
 
-        robot.waitForIdle();
+            long timeout = System.currentTimeMillis() + 9000;
+            while (!isTooltipAdded && (System.currentTimeMillis() < timeout)) {
+                try {Thread.sleep(500);} catch (Exception e) {}
+            }
 
-        Point movePoint = getButtonPoint();
-        robot.mouseMove(movePoint.x, movePoint.y);
-        robot.waitForIdle();
-
-        long timeout = System.currentTimeMillis() + 9000;
-        while (!isTooltipAdded && (System.currentTimeMillis() < timeout)) {
-            try {Thread.sleep(500);} catch (Exception e) {}
+            SwingUtilities.invokeAndWait(() -> checkToolTip());
+	} finally {
+            SwingUtilities.invokeAndWait(() -> {
+                if (frame != null) {
+                    frame.dispose();
+                }
+            });
         }
-
-        checkToolTip();
     }
 
-    private static void checkToolTip() throws Exception {
-        SwingUtilities.invokeAndWait(new Runnable() {
+    private static void checkToolTip() {
+        JToolTip tooltip = (JToolTip) Util.findSubComponent(JFrame.getFrames()[0], "JToolTip");
 
-            @Override
-            public void run() {
-                JToolTip tooltip = (JToolTip) Util.findSubComponent(
-                        JFrame.getFrames()[0], "JToolTip");
+        if (tooltip == null) {
+            try {
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                BufferedImage img = robot.createScreenCapture(
+                            new Rectangle(0, 0, (int)screenSize.getWidth(),
+                                                (int)screenSize.getHeight()));
+                ImageIO.write(img, "png", new File("screen.png"));
+	    } catch (Exception e) {}
+            throw new RuntimeException("Tooltip has not been found!");
+        }
 
-                if (tooltip == null) {
-                    throw new RuntimeException("Tooltip has not been found!");
-                }
+        MetalToolTipUI tooltipUI = (MetalToolTipUI) MetalToolTipUI.createUI(tooltip);
+        tooltipUI.installUI(tooltip);
 
-                MetalToolTipUI tooltipUI = (MetalToolTipUI) MetalToolTipUI.createUI(tooltip);
-                tooltipUI.installUI(tooltip);
-
-                if (!"-Insert".equals(tooltipUI.getAcceleratorString())) {
-                    throw new RuntimeException("Tooltip acceleration is not properly set!");
-                }
-
-            }
-        });
+        if (!"-Insert".equals(tooltipUI.getAcceleratorString())) {
+            throw new RuntimeException("Tooltip acceleration is not properly set!");
+        }
     }
 
     private static Point getButtonPoint() throws Exception {
@@ -111,7 +134,7 @@ public class bug4846413 {
     }
 
     private static void createAndShowGUI() {
-        JFrame frame = new JFrame("Test");
+        frame = new JFrame("Test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(200, 200);
         frame.setLocationRelativeTo(null);
