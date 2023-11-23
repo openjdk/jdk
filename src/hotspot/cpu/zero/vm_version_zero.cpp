06d957fd7c1ff59f314315969a545a8f4a5137be
Aleksey Shipilev <shade@openjdk.org>
2023-11-23 08:20:38 +0000

    8320582: Zero: Misplaced CX8 enablement flag
    Reviewed-by: dholmes

--- src/hotspot/cpu/zero/vm_version_zero.cpp
+++ src/hotspot/cpu/zero/vm_version_zero.cpp
@@ -137,6 +137,12 @@ void VM_Version::initialize() {
 #ifdef ASSERT
   UNSUPPORTED_OPTION(CountCompiledCalls);
 #endif
+
+  // Supports 8-byte cmpxchg with compiler built-ins.
+  // These built-ins are supposed to be implemented on
+  // all platforms (even if not natively), so we claim
+  // the support unconditionally.
+  _supports_cx8 = true;
 }
 
 void VM_Version::initialize_cpu_information(void) {
@@ -144,12 +150,6 @@ void VM_Version::initialize_cpu_information(void) {
   if (_initialized) {
     return;
   }
-
-  // Supports 8-byte cmpxchg with compiler built-ins.
-  // These built-ins are supposed to be implemented on
-  // all platforms (even if not natively), so we claim
-  // the support unconditionally.
-  _supports_cx8 = true;
 
   _no_of_cores  = os::processor_count();
   _no_of_threads = _no_of_cores;
