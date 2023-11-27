12e983a72e72ed56741ddc45e47d337716a8da65
Archie Cobbs <acobbs@openjdk.org>
2023-11-27 17:26:52 +0000

    8194743: Compiler implementation for Statements before super()
    Reviewed-by: vromero, jwaters, mcimadamore

--- test/langtools/tools/javac/diags/examples/RedundantSuperclassInit.java
+++ test/langtools/tools/javac/diags/examples/RedundantSuperclassInit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,8 +21,11 @@
  * questions.
  */
 
-// key: compiler.err.non.canonical.constructor.invoke.another.constructor
+// key: compiler.err.redundant.superclass.init
 
-record R(int x) {
-    public R(int x, int y) { this.x = x; }
+class RedundantSuperclassInit {
+    RedundantSuperclassInit() {
+        super();
+        super();
+    }
 }
