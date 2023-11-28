cb3df4fb7131d0c21e1920a1da3dc70211fff50b
JoKern65 <Joachim.kern@sap.com>
2023-11-28 12:32:48 +0100

    JDK-8320830

--- src/hotspot/os/aix/libodm_aix.cpp
+++ src/hotspot/os/aix/libodm_aix.cpp
@@ -48,14 +48,14 @@ dynamicOdm::dynamicOdm() {
   _odm_terminate   = (fun_odm_terminate  )dlsym(_libhandle, "odm_terminate"  );
   if (!_odm_initialize || !_odm_set_path || !_odm_mount_class || !_odm_get_obj || !_odm_terminate) {
     trcVerbose("Couldn't find all required odm symbols from %s", libodmname);
-    dlclose(_libhandle);
+    os::dll_unload(_libhandle);
     _libhandle = nullptr;
     return;
   }
 }
 
 dynamicOdm::~dynamicOdm() {
-  if (_libhandle) { dlclose(_libhandle); }
+  if (_libhandle) { os::dll_unload(_libhandle); }
 }
 
 
