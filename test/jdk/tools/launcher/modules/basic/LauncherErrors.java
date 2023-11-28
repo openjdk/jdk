0d01c488c2550889c3190b77347efe9f73412fbe
JimLaskey <james.laskey@oracle.com>
2023-11-28 16:00:51 -0400

    Requested changes

--- test/jdk/tools/launcher/modules/basic/LauncherErrors.java
+++ test/jdk/tools/launcher/modules/basic/LauncherErrors.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -93,7 +93,7 @@ public class LauncherErrors {
                     .outputTo(System.out)
                     .errorTo(System.out)
                     .shouldContain("Error: Unable to load main class " + MAIN_CLASS + " in module " + TEST_MODULE)
-                    .shouldContain("java.security.AccessControlException")
+                    .shouldContain("Caused by: java.security.AccessControlException")
                     .shouldNotHaveExitValue(0);
 
     }
