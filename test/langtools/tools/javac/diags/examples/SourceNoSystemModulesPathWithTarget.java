65dfcae6d6c11e8dcb807a2559278fb082e095b9
Jan Lahoda <jlahoda@openjdk.org>
2023-11-29 12:27:54 +0000

    8308399: Recommend --release when -source and -target are misused
    Reviewed-by: vromero

--- test/langtools/tools/javac/diags/examples/SourceNoSystemModulesPathWithTarget.java
+++ test/langtools/tools/javac/diags/examples/SourceNoSystemModulesPathWithTarget.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,7 +21,8 @@
  * questions.
  */
 
-// key: compiler.err.expected3
-// options: --release 15
+// key: compiler.warn.source.no.system.modules.path
+// key: compiler.misc.source.no.system.modules.path.with.target
+// options: -source 9 -target 9
 
-int Expected3;
+class SourceNoSystemModulesPath { }
