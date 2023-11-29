8bf03b93ea702a5a0eb20e13b389364630d150e5
Martin Doerr <mdoerr@openjdk.org>
2023-11-29 10:01:55 +0000

    8320363: ppc64 TypeEntries::type_unknown logic looks wrong, missed optimization opportunity
    Backport-of: 6aa197667ad05bd93adf3afc7b06adbfb2b18a22

--- src/hotspot/cpu/ppc/interp_masm_ppc_64.cpp
+++ src/hotspot/cpu/ppc/interp_masm_ppc_64.cpp
@@ -1734,7 +1734,7 @@ void InterpreterMacroAssembler::profile_obj_type(Register obj, Register mdo_addr
   // Klass seen before, nothing to do (regardless of unknown bit).
   //beq(CCR1, do_nothing);
 
-  andi_(R0, klass, TypeEntries::type_unknown);
+  andi_(R0, tmp, TypeEntries::type_unknown);
   // Already unknown. Nothing to do anymore.
   //bne(CCR0, do_nothing);
   crorc(CCR0, Assembler::equal, CCR1, Assembler::equal); // cr0 eq = cr1 eq or cr0 ne
