14e681234d4557170937fe7d2f4306cfd59b8242
David Alvarez <alvdavi@amazon.com>
2023-11-29 17:38:17 +0000

    8317960: [17u] Excessive CPU usage on AbstractQueuedSynchronized.isEnqueued
    Reviewed-by: phh

--- src/java.base/share/classes/java/util/concurrent/locks/AbstractQueuedSynchronizer.java
+++ src/java.base/share/classes/java/util/concurrent/locks/AbstractQueuedSynchronizer.java
@@ -1524,7 +1524,9 @@ public abstract class AbstractQueuedSynchronizer
          */
         private boolean canReacquire(ConditionNode node) {
             // check links, not status to avoid enqueue race
-            return node != null && node.prev != null && isEnqueued(node);
+            Node p; // traverse unless known to be bidirectionally linked
+            return node != null && (p = node.prev) != null &&
+                (p.next == node || isEnqueued(node));
         }
 
         /**
