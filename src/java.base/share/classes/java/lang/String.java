e0007d2fdd2b22352d4f82ed258bd717a303a610
Francesco Nigro <nigro.fra@gmail.com>
2023-12-02 17:52:25 +0100

    Reuse StringLatin1::equals in regionMatches

--- src/java.base/share/classes/java/lang/String.java
+++ src/java.base/share/classes/java/lang/String.java
@@ -2177,10 +2177,14 @@ public final class String
         if (len <= 0) {
            return true;
         }
+        byte coder = coder();
+        byte otherCoder = other.coder();
         byte[] tv = value;
         byte[] ov = other.value;
-        byte coder = coder();
-        if (coder == other.coder()) {
+        if (coder == otherCoder) {
+            if (ooffset == 0 && toffset == 0 && len == (tv.length >> coder) && ov.length == tv.length) {
+                return StringLatin1.equals(tv, ov);
+            }
             if (coder == UTF16) {
                 toffset <<= UTF16;
                 ooffset <<= UTF16;
