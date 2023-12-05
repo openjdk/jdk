b0d4cb96dd546eb409d5f5c13f6b80bf5a034045
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-12-05 09:44:26 -0800

    8321053: s/tmp/tmpbuf/

--- src/java.base/share/classes/java/io/ByteArrayInputStream.java
+++ src/java.base/share/classes/java/io/ByteArrayInputStream.java
@@ -208,22 +208,22 @@ public class ByteArrayInputStream extends InputStream {
         int len = count - pos;
         if (len > 0) {
             // 'tmp' is null if and only if 'out' is trusted
-            byte[] tmp;
+            byte[] tmpbuf;
             Class<?> outClass = out.getClass();
             if (outClass == ByteArrayOutputStream.class ||
                 outClass == FileOutputStream.class ||
                 outClass == PipedOutputStream.class)
-                tmp = null;
+                tmpbuf = null;
             else
-                tmp = new byte[Integer.min(len, MAX_TRANSFER_SIZE)];
+                tmpbuf = new byte[Integer.min(len, MAX_TRANSFER_SIZE)];
 
             int nwritten = 0;
             while (nwritten < len) {
                 int nbyte = Integer.min(len - nwritten, MAX_TRANSFER_SIZE);
                 // if 'out' is not trusted, transfer via a temporary buffer
-                if (tmp != null) {
-                    System.arraycopy(buf, pos, tmp, 0, nbyte);
-                    out.write(tmp, 0, nbyte);
+                if (tmpbuf != null) {
+                    System.arraycopy(buf, pos, tmpbuf, 0, nbyte);
+                    out.write(tmpbuf, 0, nbyte);
                 } else
                     out.write(buf, pos, nbyte);
                 pos += nbyte;
