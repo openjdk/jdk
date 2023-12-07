955abea4ca59be2c678d3ef0997a66bdc0f3f127
Brian Burkhalter <brian.burkhalter@oracle.com>
2023-12-06 16:07:00 -0800

    8321429: (fc) FileChannel.lock creates a FileKey containing two long index values, they could be stored as int values

--- src/java.base/windows/classes/sun/nio/ch/FileKey.java
+++ src/java.base/windows/classes/sun/nio/ch/FileKey.java
@@ -33,9 +33,9 @@ import java.io.IOException;
  */
 public class FileKey {
 
-    private long dwVolumeSerialNumber;
-    private long nFileIndexHigh;
-    private long nFileIndexLow;
+    private int dwVolumeSerialNumber;
+    private int nFileIndexHigh;
+    private int nFileIndexLow;
 
     private FileKey() { }
 
@@ -47,9 +47,7 @@ public class FileKey {
 
     @Override
     public int hashCode() {
-        return (int)(dwVolumeSerialNumber ^ (dwVolumeSerialNumber >>> 32)) +
-               (int)(nFileIndexHigh ^ (nFileIndexHigh >>> 32)) +
-               (int)(nFileIndexLow ^ (nFileIndexLow >>> 32));
+        return dwVolumeSerialNumber + nFileIndexHigh + nFileIndexLow; 
     }
 
     @Override
