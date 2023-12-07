9e710392a16df757ed07da4e75bff45605d66a28
Daniel Lundén <daniel.lunden@gmail.com>
2023-12-07 14:58:13 +0100

    Update src/hotspot/share/opto/loopopts.cpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/loopopts.cpp
+++ src/hotspot/share/opto/loopopts.cpp
@@ -1448,7 +1448,7 @@ void PhaseIdealLoop::split_if_with_blocks_post(Node *n) {
     // Now split the IF
     C->print_method(PHASE_BEFORE_SPLIT_IF, 4, iff);
     if ((PrintOpto && VerifyLoopOptimizations) || TraceLoopOpts) {
-      tty->print_cr("Split-if");
+      tty->print_cr("Split-If");
     }
     do_split_if(iff);
     C->print_method(PHASE_AFTER_SPLIT_IF, 4, iff);
