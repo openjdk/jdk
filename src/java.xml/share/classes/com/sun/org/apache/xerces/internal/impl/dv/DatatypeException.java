bf2a1d35f8903982beca97f551addf80522552c1
Sonia Zaldana Calles <szaldana@openjdk.org>
2023-12-08 15:11:20 +0000

    8283994: Make Xerces DatatypeException stackless
    Reviewed-by: phh
    Backport-of: 85f8d14edf0128e94bfc8102619a6ddbc37ead70

--- src/java.xml/share/classes/com/sun/org/apache/xerces/internal/impl/dv/DatatypeException.java
+++ src/java.xml/share/classes/com/sun/org/apache/xerces/internal/impl/dv/DatatypeException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
  */
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -35,7 +35,7 @@ import jdk.xml.internal.SecuritySupport;
  *
  * @author Sandy Gao, IBM
  *
- * @LastModified: Sep 2017
+ * @LastModified: Mar 2022
  */
 public class DatatypeException extends Exception {
 
@@ -106,5 +106,11 @@ public class DatatypeException extends Exception {
         }
 
         return msg;
+    }
+
+    @Override
+    public Throwable fillInStackTrace() {
+        // This is an internal exception; the stack trace is irrelevant.
+        return this;
     }
 }
