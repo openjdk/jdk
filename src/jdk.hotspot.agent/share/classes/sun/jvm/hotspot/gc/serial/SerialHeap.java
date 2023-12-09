6d8381cbb83c6745e4fe84550ca87cb1b8fb6a4e
Lei Zaakjyu <lizbing07734@icloud.com>
2023-12-09 09:32:58 +0800

    fix import statement

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/serial/SerialHeap.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/gc/serial/SerialHeap.java
@@ -26,6 +26,7 @@ package sun.jvm.hotspot.gc.serial;
 
 import sun.jvm.hotspot.debugger.*;
 import sun.jvm.hotspot.gc.shared.*;
+import sun.jvm.hotspot.runtime.*;
 import sun.jvm.hotspot.types.*;
 import sun.jvm.hotspot.utilities.*;
 
