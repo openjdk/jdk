5ed19267f63e0dde4f8ea41f4832237b980b3480
Aleksey Shipilev <shade@openjdk.org>
2023-12-19 11:30:24 +0000

    8317507: C2 compilation fails with "Exceeded _node_regs array"
    Backport-of: a5818972c16bd883d768ff2fb23a8aa9e0142c65

--- src/hotspot/share/adlc/output_c.cpp
+++ src/hotspot/share/adlc/output_c.cpp
@@ -3034,6 +3034,9 @@ static void define_fill_new_machnode(bool used, FILE *fp_cpp) {
     fprintf(fp_cpp, "    if( i != cisc_operand() ) \n");
     fprintf(fp_cpp, "      to[i] = _opnds[i]->clone();\n");
     fprintf(fp_cpp, "  }\n");
+    fprintf(fp_cpp, "  // Do not increment node index counter, since node reuses my index\n");
+    fprintf(fp_cpp, "  Compile* C = Compile::current();\n");
+    fprintf(fp_cpp, "  C->set_unique(C->unique() - 1);\n");
     fprintf(fp_cpp, "}\n");
   }
   fprintf(fp_cpp, "\n");
