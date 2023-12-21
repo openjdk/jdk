4f56afb7c721425c05981a2063054a27c41f1dd3
Olga Mikhaltsova <omikhaltcova@gmail.com>
2023-12-22 01:57:03 +0300

    Moved the code up + comments

--- src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -4239,6 +4239,48 @@ void MacroAssembler::zero_dcache_blocks(Register base, Register cnt, Register tm
   bge(cnt, tmp1, loop);
 }
 
+// java.math.round(float a)
+// Returns the closest int to the argument, with ties rounding to positive infinity.
+void MacroAssembler::java_round_float(Register dst, FloatRegister src, FloatRegister ftmp) {
+  // this instructions calling sequence provides performance improvement on all tested devices;
+  // don't change it without re-verification
+  Label done;
+  mv(t0, jint_cast(0.5f));
+  fmv_w_x(ftmp, t0);
+
+  // dst = 0 if NaN
+  feq_s(t0, src, src); // replacing fclass with feq as performance optimization for SiFive
+  mv(dst, zr);
+  beqz(t0, done);
+
+  // dst = (src + 0.5f) rounded down towards negative infinity
+  fadd_s(ftmp, src, ftmp, RoundingMode::rdn); // RDN is required here otherwise some inputs produce incorrect results
+  fcvt_w_s(dst, ftmp, RoundingMode::rdn);
+
+  bind(done);
+}
+
+// java.math.round(double a)
+// Returns the closest long to the argument, with ties rounding to positive infinity.
+void MacroAssembler::java_round_double(Register dst, FloatRegister src, FloatRegister ftmp) {
+  // this instructions calling sequence provides performance improvement on all tested devices;
+  // don't change it without re-verification
+  Label done;
+  mv(t0, julong_cast(0.5));
+  fmv_d_x(ftmp, t0);
+
+  // dst = 0 if NaN
+  feq_d(t0, src, src); // replacing fclass with feq as performance optimization for SiFive
+  mv(dst, zr);
+  beqz(t0, done);
+
+  // dst = (src + 0.5) rounded down towards negative infinity
+  fadd_d(ftmp, src, ftmp, RoundingMode::rdn); // RDN is required here otherwise some inputs produce incorrect results
+  fcvt_l_d(dst, ftmp, RoundingMode::rdn);
+
+  bind(done);
+}
+
 #define FCVT_SAFE(FLOATCVT, FLOATSIG)                                                     \
 void MacroAssembler::FLOATCVT##_safe(Register dst, FloatRegister src, Register tmp) {     \
   Label done;                                                                             \
@@ -4258,44 +4300,6 @@ FCVT_SAFE(fcvt_w_d, d);
 FCVT_SAFE(fcvt_l_d, d);
 
 #undef FCVT_SAFE
-
-void MacroAssembler::java_round_float(Register dst, FloatRegister src, FloatRegister ftmp) {
-  /* this instructions calling sequence provides performance improvement on all tested devices;
-     don't change it without re-verification */
-  Label done;
-  mv(t0, jint_cast(0.5f));
-  fmv_w_x(ftmp, t0);
-
-  /* dst = 0 if NaN */
-  feq_s(t0, src, src); /* replacing fclass with feq as performance optimization for SiFive */
-  mv(dst, zr);
-  beqz(t0, done);
-
-  /* dst = (src + 0.5f) rounded down towards negative infinity */
-  fadd_s(ftmp, src, ftmp, RoundingMode::rdn); /* RDN is required here otherwise some inputs produce incorrect results */
-  fcvt_w_s(dst, ftmp, RoundingMode::rdn);
-
-  bind(done);
-}
-
-void MacroAssembler::java_round_double(Register dst, FloatRegister src, FloatRegister ftmp) {
-  /* this instructions calling sequence provides performance improvement on all tested devices;
-     don't change it without re-verification */
-  Label done;
-  mv(t0, julong_cast(0.5));
-  fmv_d_x(ftmp, t0);
-
-  /* dst = 0 if NaN */
-  feq_d(t0, src, src); /* replacing fclass with feq as performance optimization for SiFive */
-  mv(dst, zr);
-  beqz(t0, done);
-
-  /* dst = (src + 0.5) rounded down towards negative infinity */
-  fadd_d(ftmp, src, ftmp, RoundingMode::rdn); /* RDN is required here otherwise some inputs produce incorrect results */
-  fcvt_l_d(dst, ftmp, RoundingMode::rdn);
-
-  bind(done);
-}
 
 #define FCMP(FLOATTYPE, FLOATSIG)                                                       \
 void MacroAssembler::FLOATTYPE##_compare(Register result, FloatRegister Rs1,            \
