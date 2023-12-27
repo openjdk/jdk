c5e37b433e3a63f23a420051d315ba06f261c455
Hamlin-Li <leeming1509@outlook.com>
2023-12-27 18:35:57 +0000

    RotateLeftV/RotateRightV: Initial Commit

--- src/hotspot/cpu/riscv/assembler_riscv.hpp
+++ src/hotspot/cpu/riscv/assembler_riscv.hpp
@@ -1918,6 +1918,51 @@ enum Nf {
 
 #undef INSN
 
+
+// ========================================
+// RISC-V Crypto Instructions Extension
+// ========================================
+
+#define patch_VArith(op, Reg, funct3, Reg_or_Imm5, Vs2, vm, funct6)            \
+    unsigned insn = 0;                                                         \
+    patch((address)&insn, 6, 0, op);                                           \
+    patch((address)&insn, 14, 12, funct3);                                     \
+    patch((address)&insn, 19, 15, Reg_or_Imm5);                                \
+    patch((address)&insn, 25, vm);                                             \
+    patch((address)&insn, 31, 26, funct6);                                     \
+    patch_reg((address)&insn, 7, Reg);                                         \
+    patch_reg((address)&insn, 20, Vs2);                                        \
+    emit(insn)
+
+#define INSN(NAME, op, funct3, funct6)                                                             \
+  void NAME(VectorRegister Vd, VectorRegister Vs2, uint32_t imm, VectorMask vm = unmasked) {       \
+    guarantee(is_uimm5(imm), "imm is invalid");                                                    \
+    patch_VArith(op, Vd, funct3, (uint32_t)(imm & 0x1f), Vs2, vm, funct6);                         \
+  }
+
+   INSN(vror_vi, 0b1010111, 0b011, 0b010100);
+#undef INSN
+
+#define INSN(NAME, op, funct3, funct6)                                                             \
+  void NAME(VectorRegister Vd, VectorRegister Vs2, Register Rs1, VectorMask vm = unmasked) {       \
+    patch_VArith(op, Vd, funct3, Rs1->raw_encoding(), Vs2, vm, funct6);                            \
+  }
+
+   INSN(vror_vx, 0b1010111, 0b100, 0b010100);
+   INSN(vrol_vx, 0b1010111, 0b100, 0b010101);
+#undef INSN
+
+#define INSN(NAME, op, funct3, funct6)                                                             \
+  void NAME(VectorRegister Vd, VectorRegister Vs2, VectorRegister Vs1, VectorMask vm = unmasked) { \
+    patch_VArith(op, Vd, funct3, Vs1->raw_encoding(), Vs2, vm, funct6);                            \
+  }
+
+   INSN(vror_vv, 0b1010111, 0b000, 0b010100);
+   INSN(vrol_vv, 0b1010111, 0b000, 0b010101);
+#undef INSN
+
+#undef patch_VArith
+
 // ========================================
 // RISC-V Compressed Instructions Extension
 // ========================================
