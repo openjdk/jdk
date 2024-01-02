a37238010c9ce7ba257405fb6e8d23cc4697f4ba
Kim Barrett <kim.barrett@oracle.com>
2024-01-02 07:22:54 +0000

    fix subexpression grouping in patch_vtype guarantee

--- src/hotspot/cpu/riscv/assembler_riscv.hpp
+++ src/hotspot/cpu/riscv/assembler_riscv.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2014, 2020, Red Hat Inc. All rights reserved.
  * Copyright (c) 2020, 2023, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -1157,7 +1157,7 @@ static Assembler::SEW elemtype_to_sew(BasicType etype) {
 
 #define patch_vtype(hsb, lsb, vlmul, vsew, vta, vma, vill)   \
     if (vill == 1) {                                         \
-      guarantee((vlmul | vsew | vta | vma == 0),             \
+      guarantee((vlmul | vsew | vta | vma) == 0,             \
                 "the other bits in vtype shall be zero");    \
     }                                                        \
     patch((address)&insn, lsb + 2, lsb, vlmul);              \
