f888a56d629e155260fee660832d546b585163ec
Tobias Hartmann <tobias.hartmann@oracle.com>
2024-01-04 15:16:50 +0100

    Adjusted according to review

--- src/hotspot/cpu/aarch64/c1_LIRAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/c1_LIRAssembler_aarch64.cpp
@@ -282,10 +282,10 @@ void LIR_Assembler::osr_entry() {
         __ bind(L);
       }
 #endif
-      __ ldr(r19, Address(OSR_buf, slot_offset + 0));
+      __ ldr(r19, Address(OSR_buf, slot_offset));
+      __ ldr(r20, Address(OSR_buf, slot_offset + BytesPerWord));
       __ str(r19, frame_map()->address_for_monitor_lock(i));
-      __ ldr(r19, Address(OSR_buf, slot_offset + 1*BytesPerWord));
-      __ str(r19, frame_map()->address_for_monitor_object(i));
+      __ str(r20, frame_map()->address_for_monitor_object(i));
     }
   }
 }
