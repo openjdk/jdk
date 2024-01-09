db80bd4aef88aebb534c4375cf64b346f3fdb368
Quan Anh Mai <anhmdq99@gmail.com>
2024-01-10 01:20:30 +0800

    update include order and license year

--- test/hotspot/gtest/opto/test_constant_division.cpp
+++ test/hotspot/gtest/opto/test_constant_division.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,9 +26,9 @@
 #include "opto/divconstants.hpp"
 #include "runtime/os.hpp"
 #include "utilities/growableArray.hpp"
+#include "unittest.hpp"
 
 #include <type_traits>
-#include "unittest.hpp"
 
 // Generate a random positive integer of type T in a way that biases
 // towards smaller values
