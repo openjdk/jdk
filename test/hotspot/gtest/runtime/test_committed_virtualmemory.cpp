968a64b132df9aee0f5867241676da33c6589ac5
Varada M <mvarada2000@gmail.com>
2024-01-11 11:48:55 +0000

    JDK-8301466: [AIX] Revisit CommittedVirtualMemoryTest

--- test/hotspot/gtest/runtime/test_committed_virtualmemory.cpp
+++ test/hotspot/gtest/runtime/test_committed_virtualmemory.cpp
@@ -32,7 +32,6 @@
 class CommittedVirtualMemoryTest {
 public:
   static void test() {
-#ifndef _AIX
     // See JDK-8202772: temporarily disabled.
     Thread* thr = Thread::current();
     address stack_end = thr->stack_end();
@@ -77,7 +76,6 @@ public:
     ASSERT_TRUE(i >= 1);
     ASSERT_TRUE(found_stack_top);
     ASSERT_TRUE(found_i_addr);
-#endif // !_AIX
   }
 
   static void check_covered_pages(address addr, size_t size, address base, size_t touch_pages, int* page_num) {
