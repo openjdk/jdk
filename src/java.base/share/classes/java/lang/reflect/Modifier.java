cc67f321ee1f5dd4f7f1c9153a2b21b5a0783dcb
Joe Darcy <joe.darcy@oracle.com>
2024-01-11 23:32:16 -0800

    Refine wording per review feedback.

--- src/java.base/share/classes/java/lang/reflect/Modifier.java
+++ src/java.base/share/classes/java/lang/reflect/Modifier.java
@@ -235,7 +235,7 @@ public class Modifier {
      * To make a high-fidelity representation of the Java source
      * modifiers of a class or member, source-level modifiers that do
      * <em>not</em> have a constant in this class should be
-     * included and ordered consistent with the full recommended
+     * included and appear in an order consistent with the full recommended
      * ordering for that kind of declaration as given in <cite>The
      * Java Language Specification</cite>. For example, for a
      * {@linkplain Method#toGenericString() method} the "{@link
