32128744252d75104e0d19f5eb701ffdc7b3d417
Christoph Langer <christoph.langer@sap.com>
2024-01-12 17:40:41 +0100

    JDK-8323664

--- src/java.desktop/windows/native/libawt/windows/awt_Win32GraphicsEnv.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Win32GraphicsEnv.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -124,9 +124,10 @@ BOOL DWMIsCompositionEnabled() {
     }
 
     dwmIsCompositionEnabled = bRes;
+    jboolean checkException;
 
     JNIEnv *env = (JNIEnv *)JNU_GetEnv(jvm, JNI_VERSION_1_2);
-    JNU_CallStaticMethodByName(env, NULL,
+    JNU_CallStaticMethodByName(env, &checkException,
                               "sun/awt/Win32GraphicsEnvironment",
                               "dwmCompositionChanged", "(Z)V", (jboolean)bRes);
     return bRes;
