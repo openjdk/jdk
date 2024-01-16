91be4c2d8036c5ee50e48ee4523a76bad448c6f4
Doug Simon <doug.simon@oracle.com>
2024-01-16 09:33:43 +0100

    use vm_exit_during_initialization instead of vm_exit

--- src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
+++ src/hotspot/share/jvmci/jvmciCompilerToVM.cpp
@@ -3040,8 +3040,8 @@ C2V_VMENTRY(void, callSystemExit, (JNIEnv* env, jobject, jint status))
   if (!JVMCIENV->is_hotspot()) {
     // It's generally not safe to call Java code before the module system is initialized
     if (!Universe::is_module_initialized()) {
-      JVMCI_event_1("callSystemExit before Universe::is_module_initialized() -> direct VM exit");
-      vm_exit(status);
+      JVMCI_event_1("callSystemExit(%d) before Universe::is_module_initialized() -> direct VM exit", status);
+      vm_exit_during_initialization();
     }
   }
   CompilerThreadCanCallJava canCallJava(thread, true);
