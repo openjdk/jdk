9a35fd8e61e6f92a5d8655c33f12c070aaa4c853
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2024-01-19 21:23:30 +0800

    compilerWarnings_visCPP.hpp

--- src/hotspot/share/utilities/compilerWarnings_visCPP.hpp
+++ src/hotspot/share/utilities/compilerWarnings_visCPP.hpp
@@ -30,39 +30,16 @@
 #define PRAGMA_DIAG_PUSH _Pragma("warning(push)")
 #define PRAGMA_DIAG_POP  _Pragma("warning(pop)")
 
-// The Visual Studio implementation of FORBID_C_FUNCTION explicitly does
-// nothing, because there doesn't seem to be a way to implement it for Visual
-// Studio.  What seems the most likely approach is to use deprecation warnings,
-// but that runs into problems.
-//
-// (1) Declaring the function deprecated (using either __declspec(deprecated)
-// or the C++14 [[deprecated]] attribute) fails with warnings like this:
-//   warning C4273: 'exit': inconsistent dll linkage
-// It seems attributes are not simply additive with this compiler.
-//
-// (2) Additionally adding __declspec(dllimport) to deal with (1) fails with
-// warnings like this:
-//   error C2375: 'vsnprintf': redefinition; different linkage
-// It seems some functions in the set of interest have different linkage than
-// others ("exit" is marked imported while "vsnprintf" is not, for example).
-// That makes it difficult to provide a generic macro.
-//
-// (3) Using __pragma(deprecated(name)) fails with
-//   warning C4995: 'frobnicate': name was marked as #pragma deprecated
-// for a *declaration* (not a use) of a 'frobnicate' function.
-//
 // ALLOW_C_FUNCTIONS disables deprecation warnings over the statement scope.
 // Some of the functions we're interested in allowing are conditionally
 // deprecated on Windows, under the control of various preprocessor defines
 // such as _CRT_SECURE_NO_WARNINGS.  Annotating vetted uses allows those
 // warnings to catch unchecked uses.
 
-#define FORBID_C_FUNCTION(signature, alternative)
-
 #define ALLOW_C_FUNCTION(name, ...)             \
   PRAGMA_DIAG_PUSH                              \
   PRAGMA_DISABLE_MSVC_WARNING(4996)             \
-  __VA_ARGS__                                   \
+  ::name(__VA_ARGS__)                           \
   PRAGMA_DIAG_POP
 
 #endif // SHARE_UTILITIES_COMPILERWARNINGS_VISCPP_HPP
