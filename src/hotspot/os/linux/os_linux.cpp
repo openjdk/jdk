5a66616f2a5e4e3e7a0a11dd34169b869268ce6e
Sam James <sam@gentoo.org>
2024-01-19 10:33:07 +0000

    sendfile64 -> sendfile
    Signed-off-by: Sam James <sam@gentoo.org>

--- src/hotspot/os/linux/os_linux.cpp
+++ src/hotspot/os/linux/os_linux.cpp
@@ -4249,7 +4249,7 @@ jlong os::Linux::fast_thread_cpu_time(clockid_t clockid) {
 // the number of bytes written to out_fd is returned if transfer was successful
 // otherwise, returns -1 that implies an error
 jlong os::Linux::sendfile(int out_fd, int in_fd, jlong* offset, jlong count) {
-  return ::sendfile64(out_fd, in_fd, (off_t*)offset, (size_t)count);
+  return ::sendfile(out_fd, in_fd, (off_t*)offset, (size_t)count);
 }
 
 // Determine if the vmid is the parent pid for a child in a PID namespace.
