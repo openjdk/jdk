e1c8e01175637deed2732c9ae46cada4f9fee558
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2024-01-19 21:18:12 +0800

    zUtils_posix.cpp

--- src/hotspot/os/posix/gc/z/zUtils_posix.cpp
+++ src/hotspot/os/posix/gc/z/zUtils_posix.cpp
@@ -32,7 +32,7 @@ uintptr_t ZUtils::alloc_aligned(size_t alignment, size_t size) {
   void* res = nullptr;
 
   // Use raw posix_memalign as long as we have no wrapper for it
-  ALLOW_C_FUNCTION(::posix_memalign, int rc = posix_memalign(&res, alignment, size);)
+  int rc = ALLOW_C_FUNCTION(posix_memalign, &res, alignment, size);
   if (rc != 0) {
     fatal("posix_memalign() failed");
   }
