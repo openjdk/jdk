25556c8fd625c5133ad425712125bac80e286cbe
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2024-01-23 13:44:58 +0000

    Add try_recursive_enter precondition documentation

--- src/hotspot/share/runtime/lockStack.hpp
+++ src/hotspot/share/runtime/lockStack.hpp
@@ -98,6 +98,7 @@ public:
   inline bool is_recursive(oop o) const;
 
   // Try recursive enter.
+  // Precondition: This lock-stack must no be full.
   inline bool try_recursive_enter(oop o);
 
   // Try recursive exit.
