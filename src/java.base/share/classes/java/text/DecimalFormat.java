96607df7f055a80d56ea4c19f3f4fcb32838b1f8
Justin Lu <jlu@openjdk.org>
2024-01-23 22:22:16 +0000

    8321545: Override toString() for Format subclasses
    Reviewed-by: naoto, rriggs

--- src/java.base/share/classes/java/text/DecimalFormat.java
+++ src/java.base/share/classes/java/text/DecimalFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2994,6 +2994,17 @@ public class DecimalFormat extends NumberFormat {
     public int hashCode() {
         return super.hashCode() * 37 + positivePrefix.hashCode();
         // just enough fields for a reasonable distribution
+    }
+
+    /**
+     * {@return a string identifying this {@code DecimalFormat}, for debugging}
+     */
+    @Override
+    public String toString() {
+        return
+            """
+            DecimalFormat [locale: "%s", pattern: "%s"]
+            """.formatted(symbols.getLocale().getDisplayName(), toPattern());
     }
 
     /**
