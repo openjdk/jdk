554ac1c8129da858d5de1268ab566c46a92cb61d
Emanuel Peter <emanuel.peter@oracle.com>
2024-01-23 11:20:03 +0100

    8323727

--- src/hotspot/share/opto/subtypenode.cpp
+++ src/hotspot/share/opto/subtypenode.cpp
@@ -43,6 +43,18 @@ const Type* SubTypeCheckNode::sub(const Type* sub_t, const Type* super_t) const 
     if (!superklass->is_interface() && superklass->is_abstract() &&
         !superklass->as_instance_klass()->has_subklass()) {
       Compile::current()->dependencies()->assert_leaf_type(superklass);
+      if (subk->is_same_java_type_as(superk) && !sub_t->maybe_null()) {
+        // The super_t has no subclasses, and sub_t has the same type and is not null,
+        // hence the check should always evaluate to EQ. However, this is an impossible
+        // situation since super_t is also abstract, and hence sub_t cannot have the
+        // same type and be non-null.
+        // Still, if the non-static method of an abstract class without subclasses is
+        // force-compiled, the Param0 has the self/this pointer with NotNull. This
+        // method would now never be called, because of the leaf-type dependency. Hence,
+        // just for consistency with verification, we return EQ.
+        return TypeInt::CC_EQ;
+      }
+      // subk is either a supertype of superk, or null. In either case, superk is a subtype.
       return TypeInt::CC_GT;
     }
   }
