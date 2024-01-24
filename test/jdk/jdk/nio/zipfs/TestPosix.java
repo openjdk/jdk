dabe0fb7e781d035c8f57bcfa13814f5972ee55f
Eirik Bjorsnos <eirbjo@gmail.com>
2024-01-24 15:20:04 +0100

    If the entry currently has no permissions set, don't try to merge permission bits with existing -1 bits

--- test/jdk/jdk/nio/zipfs/TestPosix.java
+++ test/jdk/jdk/nio/zipfs/TestPosix.java
@@ -61,7 +61,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 /**
  * @test
- * @bug 8213031 8273935
+ * @bug 8213031 8273935 8324635
  * @summary Test POSIX ZIP file operations.
  * @modules jdk.zipfs
  *          jdk.jartool
@@ -719,7 +719,7 @@ public class TestPosix {
     }
 
     /**
-     * Verify that calling Files.setPosixPermissions with the current
+     * Verify that calling Files.setPosixFilePermissions with the current
      * permission set does not change the 'external file attributes' field.
      *
      * @throws IOException if an unexpected IOException occurs
@@ -731,6 +731,33 @@ public class TestPosix {
             // Set permissions to their current value
             Files.setPosixFilePermissions(path, Files.getPosixFilePermissions(path));
         });
+    }
+
+    /**
+     * Verify that calling Files.setPosixFilePermissions on an MS-DOS entry
+     * results in only the expected permission bits being set
+     *
+     * @throws IOException if an unexpected IOException occurs
+     */
+    @Test
+    public void setPermissionsShouldConvertToUnix() throws IOException {
+        // The default environment creates MS-DOS entries, with zero 'external file attributes'
+        createEmptyZipFile(ZIP_FILE, ENV_DEFAULT);
+        try (FileSystem fs = FileSystems.newFileSystem(ZIP_FILE, ENV_DEFAULT)) {
+            Path path = fs.getPath("hello.txt");
+            Files.createFile(path);
+        }
+
+        // Sanity check that all 'external file attributes' bits are zero
+        verifyExternalFileAttribute(Files.readAllBytes(ZIP_FILE), "0");
+
+        // Convert to a UNIX entry by calling Files.setPosixFilePermissions
+        try (FileSystem fs = FileSystems.newFileSystem(ZIP_FILE, ENV_POSIX)) {
+            Path path = fs.getPath("hello.txt");
+            Files.setPosixFilePermissions(path, EnumSet.of(OWNER_READ));
+        }
+        // The first of the nine trailing permission bits should be set
+        verifyExternalFileAttribute(Files.readAllBytes(ZIP_FILE), "100000000");
     }
 
     /**
