30a584041f4aec532b163a4245f77bdc96b7de61
Kangcheng Xu <kxu@redhat.com>
2024-01-29 10:07:51 -0500

    add TestShenandoahBarrierExpansion

---
+++ test/hotspot/jtreg/compiler/gcbarriers/TestShenandoahBarrierExpansion.java
@@ -0,0 +1,69 @@
+package compiler.gcbarriers;
+
+import compiler.lib.ir_framework.CompilePhase;
+import compiler.lib.ir_framework.IR;
+import compiler.lib.ir_framework.IRNode;
+import compiler.lib.ir_framework.Test;
+import compiler.lib.ir_framework.TestFramework;
+
+/**
+ * @test
+ * @bug 8231569
+ * @summary Test that Shenandoah barriers are expanded correctly
+ * @library /test/lib /
+ * @requires vm.gc.Shenandoah
+ * @run driver compiler.gcbarriers.TestShenandoahBarrierExpansion
+ */
+public class TestShenandoahBarrierExpansion {
+    public static void main(String[] args) {
+        TestFramework test = new TestFramework(TestShenandoahBarrierExpansion.class);
+        test.addFlags(
+                "-XX:+UseShenandoahGC",
+                "-XX:-BackgroundCompilation",
+                "-XX:-UseOnStackReplacement",
+                "-XX:-UseCompressedOops",
+                "-XX:CompileCommand=dontinline," + TestShenandoahBarrierExpansion.class.getName() + "::notInlined");
+        test.start();
+    }
+
+    private static Object staticField;
+    @Test
+    @IR(failOn = IRNode.IF, phase = CompilePhase.MACRO_EXPANSION)
+    @IR(counts = { IRNode.IF, "2" }, phase = CompilePhase.BARRIER_EXPANSION)
+    public Object testLoadFieldObject() {
+        return staticField;
+    }
+
+    private static A staticField2 = new A();
+    @Test
+    @IR(counts = { IRNode.IF, "1" }, phase = CompilePhase.MACRO_EXPANSION)
+    @IR(counts = { IRNode.IF, "3" }, phase = CompilePhase.BARRIER_EXPANSION)
+    private static int testLoadObjectFieldWithNullCheck() {
+        return staticField2.intField;
+    }
+
+    private static A staticField3 = new A();
+    @Test
+    @IR(counts = { IRNode.IF, "2" }, phase = CompilePhase.MACRO_EXPANSION)
+    @IR(counts = { IRNode.IF, "6" }, phase = CompilePhase.BARRIER_EXPANSION)
+    private static int testLoadTwoObjectFieldsWithNullCheck() {
+        return staticField2.intField + staticField3.intField;
+    }
+
+    @Test
+    @IR(failOn = IRNode.IF, phase = CompilePhase.MACRO_EXPANSION)
+    @IR(counts = { IRNode.IF, "3" }, phase = CompilePhase.BARRIER_EXPANSION)
+    private static void testLoadTwoFieldObjectAndEscape() {
+        final A field2 = staticField2;
+        final A field3 = staticField3;
+        notInlined(field2, field3);
+    }
+
+    private static void notInlined(A field2, A field3) {
+        // noop
+    }
+
+    private static class A {
+        public int intField;
+    }
+}
