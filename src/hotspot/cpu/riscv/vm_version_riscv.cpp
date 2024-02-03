dd9605b074506d76f5169c922ba0fc22f087ff83
hamlin <hamlin@hamlin.eu.rivosinc.com>
2024-02-03 06:54:01 +0000

    clean code

--- src/hotspot/cpu/riscv/vm_version_riscv.cpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.cpp
@@ -101,10 +101,6 @@ void VM_Version::initialize() {
     }
   }
 
-  if (FLAG_IS_DEFAULT(UseZvfh)) {
-    FLAG_SET_DEFAULT(UseZvfh, true);
-  }
-
   // Enable vendor specific features
 
   if (mvendorid.enabled()) {
