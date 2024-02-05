8ed22523990cc460d7c59aea1398b61f81ca5d32
Daniel Lundén <daniel.lunden@oracle.com>
2024-02-05 16:35:16 +0100

    Remove flag from some test cases

--- test/hotspot/jtreg/compiler/c2/TestModDivTopInput.java
+++ test/hotspot/jtreg/compiler/c2/TestModDivTopInput.java
@@ -26,9 +26,9 @@
  * @test
  * @bug 8283451
  * @summary C2: assert(_base == Long) failed: Not a Long
- * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+StressLCM -XX:+StressGCM -XX:+StressCCP -XX:+StressIGVN -XX:+StressMacroExpansion
+ * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+StressLCM -XX:+StressGCM -XX:+StressCCP -XX:+StressIGVN
  *                   -Xcomp -XX:CompileOnly=TestModDivTopInput::* -XX:-TieredCompilation -XX:StressSeed=87628618 TestModDivTopInput
- * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+StressLCM -XX:+StressGCM -XX:+StressCCP -XX:+StressIGVN -XX:+StressMacroExpansion
+ * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+StressLCM -XX:+StressGCM -XX:+StressCCP -XX:+StressIGVN
  *                   -Xcomp -XX:CompileOnly=TestModDivTopInput::* -XX:-TieredCompilation TestModDivTopInput
  */
 
