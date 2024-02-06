4acc75550a5b323c4d55f093a05b6338d95c3dfd
tstuefe <thomas.stuefe@gmail.com>
2024-02-06 09:15:40 +0100

    adjust copyrights

--- src/hotspot/os/linux/hugepages.hpp
+++ src/hotspot/os/linux/hugepages.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2011, 2023, Red Hat Inc. All rights reserved.
+ * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2024, Red Hat Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
