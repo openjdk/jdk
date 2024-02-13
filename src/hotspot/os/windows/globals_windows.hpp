microsoft/openjdk-jdk

Add product flag for enabling all processor groups

src/hotspot/os/windows/globals_windows.hpp
@@ -36,6 +36,9 @@
                          range,                                           \
                          constraint)                                      \
                                                                           \
+product(bool, UseAllProcessorGroups, false,                               \
+        "Use all processor groups on supported Windows versions")         \
+                                                                          \
 product(bool, UseOSErrorReporting, false,                                 \
         "Let VM fatal error propagate to the OS (ie. WER on Windows)")
 

openjdk/jdk

src/hotspot/os/windows/globals_windows.hpp
@@ -36,6 +36,9 @@
                          range,                                           \
                          constraint)                                      \
                                                                           \
+product(bool, UseAllWindowsProcessorGroups, false,                        \
+        "Use all processor groups on supported Windows versions")         \
+                                                                          \
 product(bool, UseOSErrorReporting, false,                                 \
         "Let VM fatal error propagate to the OS (ie. WER on Windows)")
 