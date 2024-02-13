a305e8b9790686f10f6f0e8599f6e1c62c013fea
Stefan Johansson <stefan.johansson@oracle.com>
2024-02-13 14:39:33 +0100

    Add AlwaysPreTouch to jvmArgsAppend

--- test/micro/org/openjdk/bench/vm/gc/system_gc/HalfDeadFirstPart.java
+++ test/micro/org/openjdk/bench/vm/gc/system_gc/HalfDeadFirstPart.java
@@ -36,7 +36,7 @@ import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.SingleShotTime)
-@Fork(value=25, jvmArgsAppend={"-Xmx5g", "-Xms5g", "-Xmn3g"})
+@Fork(value=25, jvmArgsAppend={"-Xmx5g", "-Xms5g", "-Xmn3g", "-XX:+AlwaysPreTouch"})
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
 public class HalfDeadFirstPart {
