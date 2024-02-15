35f06d62dd3583829f31ed05b4961e21dc63e6ac
Justin Lu <justin.lu@oracle.com>
2024-02-14 16:27:02 -0800

    improve method name as suggested, remove unusued clutter

--- test/jdk/java/text/Format/NumberFormat/DFSExponential.java
+++ test/jdk/java/text/Format/NumberFormat/DFSExponential.java
@@ -41,26 +41,21 @@ import static org.junit.jupiter.api.Assertions.fail;
 public class DFSExponential {
 
     @Test
-    public void DFSExponenTest() throws Exception {
+    public void TestDFSExponential() {
         DecimalFormatSymbols sym = new DecimalFormatSymbols(Locale.US);
-        String pat[] = { "0.####E0", "00.000E00", "##0.####E000", "0.###E0;[0.###E0]"  };
-        double val[] = { 0.01234, 123456789, 1.23e300, -3.141592653e-271 };
-        long lval[] = { 0, -1, 1, 123456789 };
-        String valFormat[][] = {
+        String[] pat = { "0.####E0", "00.000E00", "##0.####E000", "0.###E0;[0.###E0]"};
+        double[] val = { 0.01234, 123456789, 1.23e300, -3.141592653e-271};
+        String[][] valFormat = {
                 {"1.234x10^-2", "1.2346x10^8", "1.23x10^300", "-3.1416x10^-271"},
                 {"12.340x10^-03", "12.346x10^07", "12.300x10^299", "-31.416x10^-272"},
                 {"12.34x10^-003", "123.4568x10^006", "1.23x10^300", "-314.1593x10^-273"},
                 {"1.234x10^-2", "1.235x10^8", "1.23x10^300", "[3.142x10^-271]"},
         };
-
-
-        int ival = 0, ilval = 0;
         System.out.println("Default exponent separator: "+sym.getExponentSeparator());
         try {
             sym.setExponentSeparator("x10^");
         } catch (NullPointerException e){
             fail("null String was passed to set an exponent separator symbol");
-            throw new RuntimeException("Test Malfunction: null String was passed to set an exponent separator symbol" );
         }
         System.out.println("Current exponent separator: "+sym.getExponentSeparator());
 
@@ -69,19 +64,15 @@ public class DFSExponential {
             System.out.println("     Pattern: " + fmt.toPattern());
             String locPattern = fmt.toLocalizedPattern();
             System.out.println("     Localized pattern: "+locPattern);
-            //fmt.applyLocalizedPattern(locPattern);
-            //System.out.println("      fmt.applyLocalizedPattern(): "+fmt.toLocalizedPattern());
-
             for (int v=0; v<val.length; ++v) {
                 String s = fmt.format(val[v]);
                 System.out.println("         " + val[v]+" --> "+s);
-                if(valFormat[p][v].equals(s)){
+                if (valFormat[p][v].equals(s)){
                     System.out.println(": Passed");
-                }else{
+                } else{
                    fail(" Failed: Should be formatted as "+valFormat[p][v]+ "but got "+s);
-                   throw new RuntimeException(" Failed: Should be formatted as "+valFormat[p][v]+ "but got "+s);
                 }
            }
-         } //end of the first for loop
-   }
+         }
+    }
 }
