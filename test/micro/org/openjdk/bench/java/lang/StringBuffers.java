d2590c69b4efe5aa2b48b08070e0dbafb04ef202
Claes Redestad <redestad@openjdk.org>
2024-02-20 20:28:55 +0000

    8325730: StringBuilder.toString allocation for the empty String
    Reviewed-by: jlaskey, shade

--- test/micro/org/openjdk/bench/java/lang/StringBuffers.java
+++ test/micro/org/openjdk/bench/java/lang/StringBuffers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -80,4 +80,10 @@ public class StringBuffers {
         return blaha.substring(30, 35);
     }
 
+    StringBuffer sb = new StringBuffer();
+
+    @Benchmark
+    public String emptyToString() {
+        return sb.toString();
+    }
 }
