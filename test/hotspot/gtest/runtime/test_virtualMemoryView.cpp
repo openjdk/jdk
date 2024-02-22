afab181f6ec083d63952593f5f5df334bde7d199
Johan Sjölén <johan.sjolen@oracle.com>
2024-02-22 23:17:28 +0100

    Changes

--- test/hotspot/gtest/runtime/test_virtualMemoryView.cpp
+++ test/hotspot/gtest/runtime/test_virtualMemoryView.cpp
@@ -8,29 +8,35 @@
 #include "unittest.hpp"
 
 #include <stdio.h>
-/*
-class VirtualMemoryViewTest {
-  using vmv = VirtualMemoryView;
+
+class VirtualMemoryViewTest : public testing::Test {
+  using VMV = VirtualMemoryView;
 public:
+  static VirtualMemoryView vmv;
+  static VMV::PhysicalMemorySpace space;
+
+  VirtualMemoryViewTest() {
+    space = vmv.register_space("hi");
+  }
   static address addr(size_t x) {
     return (address)x;
   }
   static void sort_n_merge() {
-    for (vmv::Id space_id = 0; space_id < vmv::PhysicalMemorySpace::unique_id; space_id++) {
-      auto& reserved_ranges = vmv::_virt_mem->reserved_regions.at(space_id);
-      auto& mapped_ranges = vmv::_virt_mem->mapped_regions.at(space_id);
-      auto& committed_ranges = vmv::_virt_mem->committed_regions.at(space_id);
-      vmv::sort_regions(reserved_ranges);
-      vmv::sort_regions(mapped_ranges);
-      vmv::sort_regions(committed_ranges);
-      vmv::merge_memregions(reserved_ranges);
-      vmv::merge_memregions(committed_ranges);
-      vmv::merge_mapped(mapped_ranges);
+    for (VMV::Id space_id = 0; space_id < VMV::PhysicalMemorySpace::unique_id; space_id++) {
+      auto& reserved_ranges = vmv._virt_mem.reserved_regions.at(space_id);
+      auto& mapped_ranges = vmv._virt_mem.mapped_regions.at(space_id);
+      auto& committed_ranges = vmv._virt_mem.committed_regions.at(space_id);
+      vmv.sort_regions(reserved_ranges);
+      vmv.sort_regions(mapped_ranges);
+      vmv.sort_regions(committed_ranges);
+      vmv.merge_memregions(reserved_ranges);
+      vmv.merge_memregions(committed_ranges);
+      vmv.merge_mapped(mapped_ranges);
     }
   }
 
   static void test_merging() {
-    auto& reserved_ranges = vmv::_virt_mem->reserved_regions.at(vmv::heap.id);
+    auto& reserved_ranges = vmv._virt_mem.reserved_regions.at(space.id);
     auto range_has = [&](int idx, address addr, size_t sz, MEMFLAGS flag = mtNone) {
       auto& rng = reserved_ranges.at(idx);
       ASSERT_TRUE(rng.size == sz);
@@ -40,13 +46,13 @@ public:
       ASSERT_TRUE(rng.start == addr);
     };
     // Two adjacent ranges should be merged after sort+merge
-    vmv::reserve_memory(addr(0), 100, MEMFLAGS::mtTest, CURRENT_PC);
-    vmv::reserve_memory(addr(100), 100, MEMFLAGS::mtTest, CURRENT_PC);
+    vmv.reserve_memory(addr(0), 100, MEMFLAGS::mtTest, CURRENT_PC);
+    vmv.reserve_memory(addr(100), 100, MEMFLAGS::mtTest, CURRENT_PC);
     sort_n_merge();
     ASSERT_TRUE(reserved_ranges.length() == 1);
     range_has(0, 0, 200, mtTest);
     // Two identical ranges but with differing memflags are both kept
-    vmv::reserve_memory(addr(0), 200, MEMFLAGS::mtArguments, CURRENT_PC);
+    vmv.reserve_memory(addr(0), 200, MEMFLAGS::mtArguments, CURRENT_PC);
     sort_n_merge();
     ASSERT_TRUE(reserved_ranges.length() == 2);
     range_has(0, 0, 200);
@@ -55,4 +61,9 @@ public:
                 || (reserved_ranges.at(0).flag == mtArguments && reserved_ranges.at(1).flag == mtTest));
   }
 };
-*/
+VirtualMemoryView VirtualMemoryViewTest::vmv{true /*is_detailed_mode*/};
+VirtualMemoryView::PhysicalMemorySpace VirtualMemoryViewTest::space{0}; // Doesn't matter, will make new one during construction
+
+TEST_VM_F(VirtualMemoryViewTest, TestOne) {
+  VirtualMemoryViewTest::test_merging();
+}
