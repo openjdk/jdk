f4fd2cbfa67760ac155c1f5aeaaadf7e022d7631
Goetz Lindenmaier <goetz@openjdk.org>
2024-02-23 08:46:49 +0000

    8290041: ModuleDescriptor.hashCode is inconsistent
    Backport-of: 4cc6cb9d9ddbcc540baac7b81398f2af83f93340

--- src/java.base/share/classes/java/lang/module/ModuleDescriptor.java
+++ src/java.base/share/classes/java/lang/module/ModuleDescriptor.java
@@ -2553,7 +2553,7 @@ public class ModuleDescriptor
     private static int modsHashCode(Iterable<? extends Enum<?>> enums) {
         int h = 0;
         for (Enum<?> e : enums) {
-            h = h * 43 + Objects.hashCode(e.name());
+            h += e.name().hashCode();
         }
         return h;
     }
