da14aa463b8fcd56ba1f1a4cdb3e3f7b19f78964
Magnus Ihse Bursie <ihse@openjdk.org>
2024-02-27 10:26:55 +0000

    8017234: Hotspot should stop using mapfiles
    Reviewed-by: djelinski, erikj, dholmes

--- src/hotspot/share/jvmci/vmStructs_jvmci.cpp
+++ src/hotspot/share/jvmci/vmStructs_jvmci.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -45,6 +45,11 @@
 #include "gc/g1/g1CardTable.hpp"
 #include "gc/g1/g1HeapRegion.hpp"
 #include "gc/g1/g1ThreadLocalData.hpp"
+#endif
+
+#if defined(TARGET_COMPILER_gcc)
+#undef JNIEXPORT
+#define JNIEXPORT
 #endif
 
 #define VM_STRUCTS(nonstatic_field, static_field, unchecked_nonstatic_field, volatile_nonstatic_field) \
