310a085593584b4815f81e928f1114f1fbc1a52d
Alexey Ivanov <alexey.ivanov@oracle.com>
2024-02-29 15:09:03 +0000

    8286759: TextComponentPrintable: consequent -> consecutive positions

--- src/java.desktop/share/classes/sun/swing/text/TextComponentPrintable.java
+++ src/java.desktop/share/classes/sun/swing/text/TextComponentPrintable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -781,7 +781,7 @@ public class TextComponentPrintable implements CountingPrintable {
                             && (y != previousY || height != previousHeight)) {
                         /*
                          * we do not store the same value as previous. in our
-                         * documents it is often for consequent positions to have
+                         * documents it is often for consecutive positions to have
                          * the same modelToView y and height.
                          */
                         previousY = y;
