6c6e41261cdf6048043410a7b571ec61193fad46
Raju Gupta <raju.gupta@workday.com>
2024-03-03 05:58:58 +0000

    The ternanry operation is redundant.

--- src/java.sql/share/classes/java/sql/DriverManager.java
+++ src/java.sql/share/classes/java/sql/DriverManager.java
@@ -561,7 +561,7 @@ public class DriverManager {
                 result = false;
             }
 
-             result = ( aClass == driver.getClass() ) ? true : false;
+             result = ( aClass == driver.getClass() );
         }
 
         return result;
