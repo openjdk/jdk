ae0e0acc72bcf1bba81dc4218a64766eb2f2549a
iklam <ilam3388@gmail.com>
2024-03-06 14:16:24 -0800

    fixed alignments

--- src/hotspot/share/cds/cdsConfig.hpp
+++ src/hotspot/share/cds/cdsConfig.hpp
@@ -37,9 +37,9 @@ class CDSConfig : public AllStatic {
   static bool _is_dumping_full_module_graph;
   static bool _is_using_full_module_graph;
 
-  static char*  _default_archive_path;
-  static char*  _static_archive_path;
-  static char*  _dynamic_archive_path;
+  static char* _default_archive_path;
+  static char* _static_archive_path;
+  static char* _dynamic_archive_path;
 #endif
 
   static void extract_shared_archive_paths(const char* archive_path,
@@ -62,36 +62,46 @@ public:
   static void check_unsupported_dumping_properties() NOT_CDS_RETURN;
   static bool check_vm_args_consistency(bool patch_mod_javabase,  bool mode_flag_cmd_line) NOT_CDS_RETURN_(true);
 
-  // Basic CDS features
-  static bool      is_dumping_archive()                      { return is_dumping_static_archive() || is_dumping_dynamic_archive(); }
-  static bool      is_dumping_static_archive()               { return CDS_ONLY(_is_dumping_static_archive) NOT_CDS(false); }
-  static void  enable_dumping_static_archive()               { CDS_ONLY(_is_dumping_static_archive = true); }
-  static bool      is_dumping_dynamic_archive()              { return CDS_ONLY(_is_dumping_dynamic_archive) NOT_CDS(false); }
-  static void  enable_dumping_dynamic_archive()              { CDS_ONLY(_is_dumping_dynamic_archive = true); }
-  static void disable_dumping_dynamic_archive()              { CDS_ONLY(_is_dumping_dynamic_archive = false); }
-  static bool      is_using_archive()                        NOT_CDS_RETURN_(false);
+  // --- Basic CDS features
 
-  // Archive paths
+  // archive(s) in general
+  static bool is_dumping_archive()                           { return is_dumping_static_archive() || is_dumping_dynamic_archive(); }
+  static bool is_using_archive()                             NOT_CDS_RETURN_(false);
+  static int num_archives(const char* archive_path)          NOT_CDS_RETURN_(0);
+
+  // static_archive
+  static bool is_dumping_static_archive()                    { return CDS_ONLY(_is_dumping_static_archive) NOT_CDS(false); }
+  static void enable_dumping_static_archive()                { CDS_ONLY(_is_dumping_static_archive = true); }
+
+  // dynamic_archive
+  static bool is_dumping_dynamic_archive()                   { return CDS_ONLY(_is_dumping_dynamic_archive) NOT_CDS(false); }
+  static void enable_dumping_dynamic_archive()               { CDS_ONLY(_is_dumping_dynamic_archive = true); }
+  static void disable_dumping_dynamic_archive()              { CDS_ONLY(_is_dumping_dynamic_archive = false); }
+
+  // optimized_module_handling -- can we skip some expensive operations related to modules?
+  static bool is_using_optimized_module_handling()           { return CDS_ONLY(_is_using_optimized_module_handling) NOT_CDS(false); }
+  static void stop_using_optimized_module_handling()         NOT_CDS_RETURN;
+
+  static bool is_logging_lambda_form_invokers()              NOT_CDS_RETURN_(false);
+
+  // archive_path
+
   // Points to the classes.jsa in $JAVA_HOME
-  static char* default_archive_path()                         NOT_CDS_RETURN_(nullptr);
+  static char* default_archive_path()                        NOT_CDS_RETURN_(nullptr);
   // The actual static archive  (if any) selected at runtime
   static const char* static_archive_path()                   { return CDS_ONLY(_static_archive_path) NOT_CDS(nullptr); }
   // The actual dynamic archive  (if any) selected at runtime
   static const char* dynamic_archive_path()                  { return CDS_ONLY(_dynamic_archive_path) NOT_CDS(nullptr); }
 
-  static int num_archives(const char* archive_path)          NOT_CDS_RETURN_(0);
-  static bool is_logging_lambda_form_invokers()              NOT_CDS_RETURN_(false);
+  // --- Archived java objects
 
-  // Can we skip some expensive operations related to modules?
-  static bool   is_using_optimized_module_handling()         { return CDS_ONLY(_is_using_optimized_module_handling) NOT_CDS(false); }
-  static void stop_using_optimized_module_handling()         NOT_CDS_RETURN;
-
-  // CDS archived heap
   static bool   is_dumping_heap()                            NOT_CDS_JAVA_HEAP_RETURN_(false);
+
+  // full_module_graph (requires optimized_module_handling)
   static void stop_dumping_full_module_graph(const char* reason = nullptr) NOT_CDS_JAVA_HEAP_RETURN;
-  static bool   is_dumping_full_module_graph()               { return CDS_ONLY(_is_dumping_full_module_graph) NOT_CDS(false); }
-  static void   stop_using_full_module_graph(const char* reason = nullptr) NOT_CDS_JAVA_HEAP_RETURN;
-  static bool     is_using_full_module_graph()                NOT_CDS_JAVA_HEAP_RETURN_(false);
+  static bool is_dumping_full_module_graph()                 { return CDS_ONLY(_is_dumping_full_module_graph) NOT_CDS(false); }
+  static void stop_using_full_module_graph(const char* reason = nullptr) NOT_CDS_JAVA_HEAP_RETURN;
+  static bool is_using_full_module_graph()                   NOT_CDS_JAVA_HEAP_RETURN_(false);
 };
 
 #endif // SHARE_CDS_CDSCONFIG_HPP
