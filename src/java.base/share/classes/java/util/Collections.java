aac03cfc21b3b89a7023bc1c997ba71398ea3480
Korov <korov9@163.com>
2024-03-07 02:27:03 +0800

    8292955: Collections.checkedMap Map.merge does not properly check key and value

--- src/java.base/share/classes/java/util/Collections.java
+++ src/java.base/share/classes/java/util/Collections.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -3921,7 +3921,6 @@ public class Collections {
             Objects.requireNonNull(func);
             return (k, v) -> {
                 V newValue = func.apply(k, v);
-                System.out.println("key:" + k + "value:" + v);
                 typeCheck(k, newValue);
                 return newValue;
             };
