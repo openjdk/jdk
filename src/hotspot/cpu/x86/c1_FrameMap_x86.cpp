3114c8e708ea0a8040cd758c71aee806c558ccd2
Denghui Dong <denghui.ddh@alibaba-inc.com>
2024-03-08 22:36:25 +0800

    fix failure and update header

--- src/hotspot/cpu/x86/c1_FrameMap_x86.cpp
+++ src/hotspot/cpu/x86/c1_FrameMap_x86.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -297,6 +297,9 @@ void FrameMap::initialize() {
   rcx_metadata_opr = as_metadata_opr(rcx);
 
   rsp_opr = as_pointer_opr(rsp);
+#ifndef _LP64
+  rbp_opr = as_pointer_opr(rbp);
+#endif
 
 #ifdef _LP64
   r8_oop_opr = as_oop_opr(r8);
