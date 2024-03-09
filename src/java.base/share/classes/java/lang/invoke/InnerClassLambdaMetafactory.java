761ed250ec4b0d92d091a0c316b6d5028986a019
Ioi Lam <iklam@openjdk.org>
2024-03-09 03:48:38 +0000

    8327138: Clean up status management in cdsConfig.hpp and CDS.java
    Reviewed-by: ccheung, matsaave

--- src/java.base/share/classes/java/lang/invoke/InnerClassLambdaMetafactory.java
+++ src/java.base/share/classes/java/lang/invoke/InnerClassLambdaMetafactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -255,7 +255,7 @@ import static jdk.internal.org.objectweb.asm.Opcodes.*;
     private Class<?> spinInnerClass() throws LambdaConversionException {
         // CDS does not handle disableEagerInitialization or useImplMethodHandle
         if (!disableEagerInitialization && !useImplMethodHandle) {
-            if (CDS.isSharingEnabled()) {
+            if (CDS.isUsingArchive()) {
                 // load from CDS archive if present
                 Class<?> innerClass = LambdaProxyClassArchive.find(targetClass,
                                                                    interfaceMethodName,
