1fa89aa44b90d38fb12447e6da8f82ad23906242
Guoxiong Li <lgxbslgx@gmail.com>
2024-03-09 19:36:21 +0800

    Remove unnecessary code.

--- src/hotspot/share/oops/markWord.cpp
+++ src/hotspot/share/oops/markWord.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,7 +41,6 @@ markWord markWord::displaced_mark_helper() const {
   }
   // This should never happen:
   fatal("bad header=" INTPTR_FORMAT, value());
-  return markWord(value());
 }
 
 void markWord::set_displaced_mark_helper(markWord m) const {
