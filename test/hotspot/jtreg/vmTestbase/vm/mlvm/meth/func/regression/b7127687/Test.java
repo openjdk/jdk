74c14dd4ee40b465b73a7352375ebd54fa3eaafe
Oussama Louati <louati.oussama@ine.inpt.ma>
2024-03-11 21:52:42 +0000

    Converted regression Test for "MethodType leaks memory to use Classfile API

--- test/hotspot/jtreg/vmTestbase/vm/mlvm/meth/func/regression/b7127687/Test.java
+++ test/hotspot/jtreg/vmTestbase/vm/mlvm/meth/func/regression/b7127687/Test.java
@@ -23,18 +23,16 @@
 
 package vm.mlvm.mixed.func.regression.b7127687;
 
-import nsk.share.Consts;
+
 import vm.mlvm.share.MlvmTest;
 import vm.mlvm.share.Env;
-import vm.mlvm.share.CustomClassLoaders;
 
 import java.lang.invoke.MethodType;
+import java.lang.classfile.ClassFile;
+import java.lang.constant.ClassDesc;
 
 import java.util.List;
 import java.util.ArrayList;
-
-import jdk.internal.org.objectweb.asm.ClassWriter;
-import static jdk.internal.org.objectweb.asm.Opcodes.*;
 
 public class Test extends MlvmTest {
 
@@ -44,17 +42,17 @@ public class Test extends MlvmTest {
 
     @Override
     public boolean run() throws Throwable {
-        List<Class> classes = new ArrayList<Class>();
+        List<ClassDesc> classes = new ArrayList<>();
 
         //generating list of unique classes
         for (int i = 0; i < CLASSES_COUNT; i++) {
             classes.add(generateClass("Class" + i));
         }
 
-        for (Class a : classes) {
-            for (Class b : classes) {
+        for (ClassDesc a : classes) {
+            for (ClassDesc b : classes) {
                 Env.traceNormal("Perform call MethodType.methodType(" + a + ", " + b + ")");
-                MethodType.methodType(a, b);
+                MethodType.methodType(a.getClass(), b.getClass());
             }
         }
 
@@ -62,10 +60,13 @@ public class Test extends MlvmTest {
     }
 
 
-    private static Class generateClass(String name) throws ClassNotFoundException{
-        ClassWriter cw = new ClassWriter(0);
-        cw.visit(V1_1, ACC_PUBLIC, name, null, "java/lang/Object", null);
-        return CustomClassLoaders.makeClassBytesLoader(cw.toByteArray(), name).loadClass(name);
+    private static ClassDesc generateClass(String name) throws ClassNotFoundException{
+        byte[] bytes = ClassFile.of().build(ClassDesc.of(name),
+                ClassBuilder -> ClassBuilder
+                        .withFlags(ClassFile.ACC_PUBLIC)
+                        .withSuperclass(ClassDesc.ofInternalName("java/lang/Object")));
+
+        return ClassFile.of().parse(bytes).thisClass().asSymbol();
     }
 
 }
