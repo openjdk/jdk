244437cd583945c06f836aade1adfe0640f32459
Tejesh R <tejesh.r@oracle.com>
2024-03-11 16:37:41 +0530

    Conversion from Applet to Main

--- test/jdk/javax/swing/border/Test4129681.java
+++ test/jdk/javax/swing/border/Test4129681.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,39 +21,65 @@
  * questions.
  */
 
+import java.awt.BorderLayout;
+import java.awt.event.ItemEvent;
+import javax.swing.BorderFactory;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
 /*
  * @test
  * @bug 4129681
  * @summary Tests enabling/disabling of titled border's caption
- * @author Sergey Malenkov
- * @run applet/manual=yesno Test4129681.html
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
+ * @run main/manual Test4129681
  */
 
-import java.awt.BorderLayout;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import javax.swing.BorderFactory;
-import javax.swing.JApplet;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
+public class Test4129681 {
+    private static JLabel label;
+    private static JFrame frame;
 
-public class Test4129681 extends JApplet implements ItemListener {
-    private JLabel label;
+    public static void main(String[] args) throws Exception {
+        String testInstructions = """
+                When applet starts, you'll see a checkbox
+                and a label with a titled border.
+                Turn on the checkbox to disable the label.
+                The test passes if the title of the border
+                is disabled as well as the label.
+                """;
+        PassFailJFrame passFailJFrame = new PassFailJFrame.Builder()
+                .title("JInternalFrame Instructions")
+                .instructions(testInstructions)
+                .testTimeOut(5)
+                .rows(6)
+                .columns(35)
+                .build();
 
-    @Override
-    public void init() {
-        JCheckBox check = new JCheckBox("disable");
-        check.addItemListener(this);
-
-        this.label = new JLabel("message");
-        this.label.setBorder(BorderFactory.createTitledBorder("label"));
-        this.label.setEnabled(!check.isSelected());
-
-        add(BorderLayout.NORTH, check);
-        add(BorderLayout.CENTER, this.label);
+        SwingUtilities.invokeAndWait(() -> {
+            init();
+        });
+        passFailJFrame.awaitAndCheck();
     }
 
-    public void itemStateChanged(ItemEvent event) {
-        this.label.setEnabled(ItemEvent.DESELECTED == event.getStateChange());
+    public static void init() {
+        JCheckBox check = new JCheckBox("Enable/Disable");
+        frame = new JFrame("Test Border Enable/Disable");
+        check.addItemListener(event ->
+                label.setEnabled(ItemEvent.DESELECTED == event.getStateChange()));
+
+        label = new JLabel("message");
+        label.setBorder(BorderFactory.createTitledBorder("label"));
+        label.setEnabled(!check.isSelected());
+
+        frame.add(BorderLayout.NORTH, check);
+        frame.add(BorderLayout.CENTER, label);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        PassFailJFrame.addTestWindow(frame);
+        PassFailJFrame.positionTestWindow(frame, PassFailJFrame.Position.TOP_LEFT_CORNER);
+        frame.setVisible(true);
     }
 }
