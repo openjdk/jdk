4479bd13f5a4e196f49511db0256faae26c048ac
prrace <philip.race@oracle.com>
2024-03-12 15:53:21 -0700

    8328021

--- test/jdk/java/awt/List/SetFontTest/SetFontTest.java
+++ test/jdk/java/awt/List/SetFontTest/SetFontTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,38 +22,58 @@
  */
 
 /*
-  test
+  @test
   @bug 5010944 6248072
   @summary List's rows overlap one another
-  @author Dmitry Cherepanov area=awt-list
-  @run applet/manual=yesno SetFontTest.html
+  @library /java/awt/regtesthelpers
+  @run main/manual SetFontTest
 */
 
-import java.applet.Applet;
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.List;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SetFontTest extends Applet
-{
-    List list = new List(8, false);
-    Button button1 = new Button("Enlarge font");
-    Button button2 = new Button("Change mode");
+public class SetFontTest {
 
-    public void init()
-    {
+    static final String INSTRUCTIONS =
+        "1) Click on the 'Enlarge font' button to enlarge font of the list.\n" +
+        "2) If you see that the rows of the list overlap one another\n" +
+        "then the test failed. Otherwise, go to step 3.\n" +
+        "3) Click on the 'Change mode' button to set multiple-selection mode.\n" +
+        "4) If you see that the rows of the list overlap one another\n" +
+        "then the test failed. Otherwise, the test passed.";
+
+    public static void main(String[] args) throws Exception {
+        PassFailJFrame.builder()
+                      .title("AWT List Font Test")
+                       .instructions(INSTRUCTIONS)
+                       .rows(10)
+                       .columns(60)
+                       .testUI(SetFontTest::createFontTest)
+                       .build()
+                       .awaitAndCheck();
+    }
+
+    static Frame createFontTest() { 
+
+        Frame frame = new Frame("List Font Test");
+        List list = new List(8, false);
+        Button button1 = new Button("Enlarge font");
+        Button button2 = new Button("Change mode");
+
         list.add("111");
         list.add("222");
         list.add("333");
         list.add("444");
-        this.add(list);
-
-        this.add(button1);
-        this.add(button2);
 
         button1.addActionListener(
             new ActionListener(){
-                public void actionPerformed(ActionEvent ae){
-                    list.setFont( new Font( "SansSerif", Font.PLAIN, 30 ) );
+                public void actionPerformed(ActionEvent ae) {
+                    list.setFont(new Font("SansSerif", Font.PLAIN, 30));
                     list.repaint();
                 }
             });
@@ -65,170 +85,11 @@ public class SetFontTest extends Applet
                 }
             });
 
-        this.setLayout (new FlowLayout ());
-
-        String[] instructions =
-        {
-            "1) Click on the 'Enlarge font' button to enlarge font of the list.",
-            "2) If you see that the rows of the list overlap one another "+
-            "then the test failed. Otherwise, goto to the step 3.",
-            "3) Click on the 'Change mode' button to set multiple-selection mode.",
-            "4) If you see that the rows of the list overlap one another "+
-            "then the test failed. Otherwise, the test passed."
-        };
-        Sysout.createDialogWithInstructions( instructions );
-
-    }//End  init()
-
-    public void start ()
-    {
-
-        setSize (200,200);
-        setVisible(true);
-        validate();
-
-    }// start()
+        frame.setLayout(new FlowLayout());
+        frame.add(list);
+        frame.add(button1);
+        frame.add(button2);
+        frame.setSize(200,250);
+        return frame;
+    }
 }
-
-/* Place other classes related to the test after this line */
-
-
-
-
-
-/****************************************************
- Standard Test Machinery
- DO NOT modify anything below -- it's a standard
-  chunk of code whose purpose is to make user
-  interaction uniform, and thereby make it simpler
-  to read and understand someone else's test.
- ****************************************************/
-
-/**
- This is part of the standard test machinery.
- It creates a dialog (with the instructions), and is the interface
-  for sending text messages to the user.
- To print the instructions, send an array of strings to Sysout.createDialog
-  WithInstructions method.  Put one line of instructions per array entry.
- To display a message for the tester to see, simply call Sysout.println
-  with the string to be displayed.
- This mimics System.out.println but works within the test harness as well
-  as standalone.
- */
-
-class Sysout
-{
-    private static TestDialog dialog;
-
-    public static void createDialogWithInstructions( String[] instructions )
-    {
-        dialog = new TestDialog( new Frame(), "Instructions" );
-        dialog.printInstructions( instructions );
-        dialog.setVisible(true);
-        println( "Any messages for the tester will display here." );
-    }
-
-    public static void createDialog( )
-    {
-        dialog = new TestDialog( new Frame(), "Instructions" );
-        String[] defInstr = { "Instructions will appear here. ", "" } ;
-        dialog.printInstructions( defInstr );
-        dialog.setVisible(true);
-        println( "Any messages for the tester will display here." );
-    }
-
-
-    public static void printInstructions( String[] instructions )
-    {
-        dialog.printInstructions( instructions );
-    }
-
-
-    public static void println( String messageIn )
-    {
-        dialog.displayMessage( messageIn );
-    }
-
-}// Sysout  class
-
-/**
-  This is part of the standard test machinery.  It provides a place for the
-   test instructions to be displayed, and a place for interactive messages
-   to the user to be displayed.
-  To have the test instructions displayed, see Sysout.
-  To have a message to the user be displayed, see Sysout.
-  Do not call anything in this dialog directly.
-  */
-class TestDialog extends Dialog
-{
-
-    TextArea instructionsText;
-    TextArea messageText;
-    int maxStringLength = 80;
-
-    //DO NOT call this directly, go through Sysout
-    public TestDialog( Frame frame, String name )
-    {
-        super( frame, name );
-        int scrollBoth = TextArea.SCROLLBARS_BOTH;
-        instructionsText = new TextArea( "", 15, maxStringLength, scrollBoth );
-        add( "North", instructionsText );
-
-        messageText = new TextArea( "", 5, maxStringLength, scrollBoth );
-        add("Center", messageText);
-
-        pack();
-
-        setVisible(true);
-    }// TestDialog()
-
-    //DO NOT call this directly, go through Sysout
-    public void printInstructions( String[] instructions )
-    {
-        //Clear out any current instructions
-        instructionsText.setText( "" );
-
-        //Go down array of instruction strings
-
-        String printStr, remainingStr;
-        for( int i=0; i < instructions.length; i++ )
-        {
-            //chop up each into pieces maxSringLength long
-            remainingStr = instructions[ i ];
-            while( remainingStr.length() > 0 )
-            {
-                //if longer than max then chop off first max chars to print
-                if( remainingStr.length() >= maxStringLength )
-                {
-                    //Try to chop on a word boundary
-                    int posOfSpace = remainingStr.
-                        lastIndexOf( ' ', maxStringLength - 1 );
-
-                    if( posOfSpace <= 0 ) posOfSpace = maxStringLength - 1;
-
-                    printStr = remainingStr.substring( 0, posOfSpace + 1 );
-                    remainingStr = remainingStr.substring( posOfSpace + 1 );
-                }
-                //else just print
-                else
-                {
-                    printStr = remainingStr;
-                    remainingStr = "";
-                }
-
-                instructionsText.append( printStr + "\n" );
-
-            }// while
-
-        }// for
-
-    }//printInstructions()
-
-    //DO NOT call this directly, go through Sysout
-    public void displayMessage( String messageIn )
-    {
-        messageText.append( messageIn + "\n" );
-        System.out.println(messageIn);
-    }
-
-}// TestDialog  class
