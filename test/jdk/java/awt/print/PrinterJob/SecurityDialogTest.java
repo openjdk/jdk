fb1b47bfcdb67a4202e18abb46a3afe32774beab
Renjith Kannath Pariyangad <renjith.kannath.pariyangad@oracle.com>
2024-03-13 16:27:06 +0530

    JDK-8320675 : PrinterJob/SecurityDialogTest.java hangs

--- test/jdk/java/awt/print/PrinterJob/SecurityDialogTest.java
+++ test/jdk/java/awt/print/PrinterJob/SecurityDialogTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,210 +21,100 @@
  * questions.
  */
 
-/**
+import java.awt.Frame;
+import java.awt.print.PageFormat;
+import java.awt.print.PrinterJob;
+
+import javax.print.PrintService;
+import javax.print.attribute.HashPrintRequestAttributeSet;
+import javax.print.attribute.PrintRequestAttributeSet;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+/*
  * @test
  * @bug 4937672 5100706 6252456
  * @key printer
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
  * @run main/othervm/manual -Djava.security.manager=allow SecurityDialogTest
  */
+public class SecurityDialogTest extends Frame {
+    private static final String INSTRUCTIONS =
+            "This test brings up a native and cross-platform page and print dialogs.\n" +
+                    "\n" +
+                    "The dialogs should be displayed even when " +
+                    "there is no queuePrintJob permission.\n" +
+                    "If the dialog has an option to save to file, the option ought " +
+                    "to be disabled if there is no read/write file permission.\n" +
+                    "You should test this by trying different policy files.";
+    private static final JTextArea msg = new JTextArea("");
 
-import java.awt.* ;
-import java.awt.print.* ;
-import java.io.*;
-import java.security.*;
-import javax.print.*;
-import javax.print.attribute.*;
-
-public class SecurityDialogTest {
-
-
-    public static void main ( String args[] ) {
-
-        String[] instructions =
-           {
-            "You must have a printer available to perform this test.",
-            "This test brings up a native and cross-platform page and",
-            "print dialogs.",
-            "The dialogs should be displayed even when ",
-            "there is no queuePrintJob permission.",
-            "If the dialog has an option to save to file, the option ought",
-            "to be disabled if there is no read/write file permission.",
-            "You should test this by trying different policy files."
-          };
-
-         Sysout.createDialog( );
-         Sysout.printInstructions( instructions );
-
-        SecurityDialogTest pjc = new SecurityDialogTest() ;
-    }
-
-
-  public SecurityDialogTest() {
-
-      PrinterJob pj = PrinterJob.getPrinterJob() ;
-
-      // Install a security manager which does not allow reading and
-      // writing of files.
-      //PrintTestSecurityManager ptsm = new PrintTestSecurityManager();
-      SecurityManager ptsm = new SecurityManager();
-
-      try {
-          System.setSecurityManager(ptsm);
-      } catch (SecurityException e) {
-          System.out.println("Could not run test - security exception");
-      }
-
-      try {
-          PrintJob pjob = Toolkit.getDefaultToolkit().getPrintJob(new Frame(), "Printing", null, null);
-          Sysout.println("If the value of pjob is null, the test fails.\n");
-          Sysout.println("        pjob = "+pjob);
-      } catch (SecurityException e) {
-      }
-
-      PrintService[] services = PrinterJob.lookupPrintServices();
-      for (int i=0; i<services.length; i++) {
-          System.out.println("SecurityDialogTest service "+i+" : "+services[i]);
-      }
-
-      PrintService defservice = pj.getPrintService();
-      System.out.println("SecurityDialogTest default service : "+defservice);
-
-      System.out.println("SecurityDialogTest native PageDialog ");
-      PageFormat pf1 = pj.pageDialog(new PageFormat());
-
-      System.out.println("SecurityDialogTest swing PageDialog ");
-      PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
-      PageFormat pf2 = pj.pageDialog(attributes);
-
-      // With the security manager installed, save to file should now
-      // be denied.
-      System.out.println("SecurityDialogTest native printDialog ");
-      pj.printDialog();
-
-      System.out.println("SecurityDialogTest swing printDialog ");
-      pj.printDialog(attributes);
-  }
-
-
-    class PrintTestSecurityManager extends SecurityManager {
-        public void checkPackageAccess(String pkg) {
-        }
-        public void checkPropertyAccess(String key) {
+    public static void main(String[] args) throws Exception {
+        if (PrinterJob.lookupPrintServices().length == 0) {
+            throw new RuntimeException("Printer not configured or available.");
         }
 
+        PassFailJFrame passFailJFrame = new PassFailJFrame.Builder()
+                .instructions(INSTRUCTIONS)
+                .splitUIBottom(SecurityDialogTest::createTestUI)
+                .rows((int) INSTRUCTIONS.lines().count() + 1)
+                .columns(45)
+                .build();
+
+        new SecurityDialogTest();
+        passFailJFrame.awaitAndCheck();
     }
 
+    private static JComponent createTestUI()
+    {
+        Box main = Box.createVerticalBox();
+        JLabel title = new JLabel("Current Dialog:");
+        msg.setEditable(false);
+        main.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        main.add(Box.createVerticalGlue());
+        main.add(title);
+        main.add(Box.createVerticalStrut(4));
+        main.add(msg);
+        main.add(Box.createVerticalGlue());
+        return main;
+    }
+
+    public SecurityDialogTest() {
+        PrinterJob pj = PrinterJob.getPrinterJob();
+
+        // Install a security manager which does not allow reading and
+        // writing of files.
+        //PrintTestSecurityManager ptsm = new PrintTestSecurityManager();
+        SecurityManager ptsm = new SecurityManager();
+        System.setSecurityManager(ptsm);
+
+        PrintService[] services = PrinterJob.lookupPrintServices();
+        for (int i = 0; i < services.length; i++) {
+            System.out.println("SecurityDialogTest service " + i + " : " + services[i]);
+        }
+
+        PrintService defservice = pj.getPrintService();
+        System.out.println("SecurityDialogTest default service : " + defservice);
+
+        msg.setText("Native Page Dialog");
+        pj.pageDialog(new PageFormat());
+
+        msg.setText("Swing Page Dialog ");
+        PrintRequestAttributeSet attributes = new HashPrintRequestAttributeSet();
+        pj.pageDialog(attributes);
+
+        // With the security manager installed, save to file should now
+        // be denied.
+        msg.setText("Native Print Dialog ");
+        pj.printDialog();
+
+        msg.setText("Swing Print Dialog ");
+        pj.printDialog(attributes);
+
+        msg.setText("Test completed");
+    }
 }
-class Sysout {
-   private static TestDialog dialog;
-
-   public static void createDialogWithInstructions( String[] instructions )
-    {
-      dialog = new TestDialog( new Frame(), "Instructions" );
-      dialog.printInstructions( instructions );
-      dialog.show();
-      println( "Any messages for the tester will display here." );
-    }
-
-   public static void createDialog( )
-    {
-      dialog = new TestDialog( new Frame(), "Instructions" );
-      String[] defInstr = { "Instructions will appear here. ", "" } ;
-      dialog.printInstructions( defInstr );
-      dialog.show();
-      println( "Any messages for the tester will display here." );
-    }
-
-
-   public static void printInstructions( String[] instructions )
-    {
-      dialog.printInstructions( instructions );
-    }
-
-
-   public static void println( String messageIn )
-    {
-      dialog.displayMessage( messageIn );
-    }
-
-}// Sysout  class
-
-/**
-  This is part of the standard test machinery.  It provides a place for the
-   test instructions to be displayed, and a place for interactive messages
-   to the user to be displayed.
-  To have the test instructions displayed, see Sysout.
-  To have a message to the user be displayed, see Sysout.
-  Do not call anything in this dialog directly.
-  */
-class TestDialog extends Dialog {
-
-   TextArea instructionsText;
-   TextArea messageText;
-   int maxStringLength = 80;
-
-   //DO NOT call this directly, go through Sysout
-   public TestDialog( Frame frame, String name )
-    {
-      super( frame, name );
-      int scrollBoth = TextArea.SCROLLBARS_BOTH;
-      instructionsText = new TextArea( "", 15, maxStringLength, scrollBoth );
-      add( "North", instructionsText );
-
-      messageText = new TextArea( "", 5, maxStringLength, scrollBoth );
-      add("Center", messageText);
-
-      pack();
-
-      show();
-    }// TestDialog()
-
-   //DO NOT call this directly, go through Sysout
-   public void printInstructions( String[] instructions )
-    {
-      //Clear out any current instructions
-      instructionsText.setText( "" );
-
-      //Go down array of instruction strings
-
-      String printStr, remainingStr;
-      for( int i=0; i < instructions.length; i++ )
-       {
-         //chop up each into pieces maxSringLength long
-         remainingStr = instructions[ i ];
-         while( remainingStr.length() > 0 )
-          {
-            //if longer than max then chop off first max chars to print
-            if( remainingStr.length() >= maxStringLength )
-             {
-               //Try to chop on a word boundary
-               int posOfSpace = remainingStr.
-                  lastIndexOf( ' ', maxStringLength - 1 );
-
-               if( posOfSpace <= 0 ) posOfSpace = maxStringLength - 1;
-
-               printStr = remainingStr.substring( 0, posOfSpace + 1 );
-               remainingStr = remainingStr.substring( posOfSpace + 1 );
-             }
-            //else just print
-            else
-             {
-               printStr = remainingStr;
-               remainingStr = "";
-             }
-
-            instructionsText.append( printStr + "\n" );
-
-          }// while
-
-       }// for
-
-    }//printInstructions()
-
-   //DO NOT call this directly, go through Sysout
-   public void displayMessage( String messageIn )
-    {
-      messageText.append( messageIn + "\n" );
-    }
-
- }// TestDialog  class
