5c99942d049f5b4042d4230fe3ce63091b8a7b53
Guoxiong Li <lgxbslgx@gmail.com>
2024-03-15 12:34:59 +0800

    Adjust test.

--- test/hotspot/jtreg/gc/epsilon/TestEnoughUnusedSpace.java
+++ test/hotspot/jtreg/gc/epsilon/TestEnoughUnusedSpace.java
@@ -28,16 +28,16 @@ package gc.epsilon;
  * @test TestEnoughUnusedSpace
  * @requires vm.gc.Epsilon
  * @summary Epsilon should allocates object successfully if it has enough space.
- * @run main/othervm -XX:InitialHeapSize=64M -Xmx128M -XX:+UnlockExperimentalVMOptions
+ * @run main/othervm -Xms64M -Xmx128M -XX:+UnlockExperimentalVMOptions
  *                   -XX:+UseEpsilonGC gc.epsilon.TestEnoughUnusedSpace
  */
 
 public class TestEnoughUnusedSpace {
+    static volatile Object arr;
+
     public static void main(String[] args) {
         // Create an array about 90M. It should be created successfully
         // instead of throwing OOME, because 90M is smaller than 128M.
-        byte[] arr = new byte[90 * 1024 * 1024];
-        // Just prevent the compiler optimization.
-        System.out.println(arr.length);
+        arr = new byte[90 * 1024 * 1024];
     }
 }
