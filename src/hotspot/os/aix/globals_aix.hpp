261b7fccbf1a2b886ff75920d14b7d2dee5480e8
JoKern65 <Joachim.kern@sap.com>
2024-03-16 14:50:54 +0100

    JDK-8328272

--- src/hotspot/os/aix/globals_aix.hpp
+++ src/hotspot/os/aix/globals_aix.hpp
@@ -45,7 +45,7 @@
   /* via shmctl). */                                                                \
   /* Per default we quit with an error if that variable is found; for certain */    \
   /* customer scenarios, we may want to be able to run despite that variable. */    \
-  product(bool, AllowExtshm, false,                                                 \
+  product(bool, AllowExtshm, false, DIAGNOSTIC,                                     \
           "Allow VM to run with EXTSHM=ON.")                                        \
                                                                                     \
   /*  Maximum expected size of the data segment. That correlates with the      */   \
@@ -55,21 +55,21 @@
   /*  is too small, we might run into resource issues creating many native     */   \
   /*  threads, if it is too large, we reduce our chance of getting a low heap  */   \
   /*  address (needed for compressed Oops).                                    */   \
-  product(uintx, MaxExpectedDataSegmentSize, 8*G,                                   \
+  product(uintx, MaxExpectedDataSegmentSize, 8*G, DIAGNOSTIC,                       \
           "Maximum expected Data Segment Size.")                                    \
                                                                                     \
   /* Use optimized addresses for the polling page.                             */   \
-  product(bool, OptimizePollingPageLocation, true,                                  \
+  product(bool, OptimizePollingPageLocation, true, DIAGNOSTIC,                      \
              "Optimize the location of the polling page used for Safepoints")       \
                                                                                     \
   /* Use 64K pages for virtual memory (shmat). */                                   \
-  product(bool, Use64KPages, true,                                                  \
+  product(bool, Use64KPages, true, DIAGNOSTIC,                                      \
           "Use 64K pages if available.")                                            \
                                                                                     \
   /* Normally AIX commits memory on touch, but sometimes it is helpful to have */   \
   /* explicit commit behaviour. This flag, if true, causes the VM to touch     */   \
   /* memory on os::commit_memory() (which normally is a noop).                 */   \
-  product(bool, UseExplicitCommit, false,                                           \
+  product(bool, UseExplicitCommit, false, DIAGNOSTIC,                               \
           "Explicit commit for virtual memory.")
 
 // end of RUNTIME_OS_FLAGS
