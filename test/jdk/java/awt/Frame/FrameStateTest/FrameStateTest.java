813a4974949cd7677e34dee96cf8ed0beb1e320e
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2024-03-18 21:48:51 +0100

    Get rid of unnecessary ItemListener

--- test/jdk/java/awt/Frame/FrameStateTest/FrameStateTest.java
+++ test/jdk/java/awt/Frame/FrameStateTest/FrameStateTest.java
@@ -40,8 +40,6 @@ import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.util.ArrayDeque;
@@ -62,7 +60,7 @@ import javax.swing.Timer;
  * @run main/manual FrameStateTest
  */
 
-public class FrameStateTest implements ActionListener, ItemListener {
+public class FrameStateTest implements ActionListener {
 
     private static final String INSTRUCTIONS = """
         <html><body><p>
@@ -108,13 +106,11 @@ public class FrameStateTest implements ActionListener, ItemListener {
     Button btnDispose = new Button("Dispose Frame");
     CheckboxGroup cbgState = new CheckboxGroup();
     CheckboxGroup cbgResize = new CheckboxGroup();
-    Checkbox cbIconState = new Checkbox("Frame state ICONIFIED", cbgState, false);
-    Checkbox cbNormState = new Checkbox("Frame state NORMAL", cbgState, true);
+    Checkbox cbIconState = new Checkbox("Frame state ICONIFIED", cbgState, true);
+    Checkbox cbNormState = new Checkbox("Frame state NORMAL", cbgState, false);
     Checkbox cbNonResize = new Checkbox("Frame non-resizable", cbgResize, false);
     Checkbox cbResize = new Checkbox("Frame Resizable", cbgResize, true);
 
-    int iState = 0;
-    boolean bResize = true;
     CreateFrame icontst;
 
     public static void main(String[] args) throws Exception {
@@ -143,13 +139,8 @@ public class FrameStateTest implements ActionListener, ItemListener {
         panel.add(cbNonResize);
         panel.add(btnDispose);
 
-        // Add Listeners
         btnDispose.addActionListener(this);
         btnCreate.addActionListener(this);
-        cbNormState.addItemListener(this);
-        cbResize.addItemListener(this);
-        cbIconState.addItemListener(this);
-        cbNonResize.addItemListener(this);
 
         return panel;
     }
@@ -158,7 +149,7 @@ public class FrameStateTest implements ActionListener, ItemListener {
         if (evt.getSource() == btnCreate) {
             btnCreate.setEnabled(false);
             btnDispose.setEnabled(true);
-            icontst = new CreateFrame(iState, bResize);
+            icontst =new CreateFrame(cbIconState.getState(), cbResize.getState());
             icontst.setVisible(true);
         } else if (evt.getSource() == btnDispose) {
             btnCreate.setEnabled(true);
@@ -167,36 +158,21 @@ public class FrameStateTest implements ActionListener, ItemListener {
         }
     }
 
-    public void itemStateChanged(ItemEvent evt) {
-        if (cbNormState.getState()) iState = 0;
-        if (cbIconState.getState()) iState = 1;
-        if (cbResize.getState()) bResize = true;
-        if (cbNonResize.getState()) bResize = false;
-    }
-
     static class CreateFrame extends Frame
             implements ActionListener, WindowListener {
 
         Button b1, b2, b3, b4, b5, b6, b7;
-        boolean resizable = true;
-        boolean iconic = false;
+        boolean isResizable;
         String name = "Frame State Test";
 
-        CreateFrame(int iFrameState, boolean bFrameResizable) {
-
+        CreateFrame(boolean iconified, boolean resizable) {
             setTitle("Frame State Test (Window 2)");
 
-            if (iFrameState == 1) {
-                iconic = true;
-            }
-
-            if (!(bFrameResizable)) {
-                resizable = false;
-            }
+            isResizable = resizable;
 
             PassFailJFrame.log("CREATING FRAME - Initially " +
-                    ((iconic) ? "ICONIFIED" : "NORMAL (NON-ICONIFIED)") + " and " +
-                    ((resizable) ? "RESIZABLE" : "NON-RESIZABLE"));
+                    ((iconified) ? "ICONIFIED" : "NORMAL (NON-ICONIFIED)") + " and " +
+                    ((isResizable) ? "RESIZABLE" : "NON-RESIZABLE"));
 
             setLayout(new FlowLayout());
             add(b1 = new Button("resizable"));
@@ -216,8 +192,8 @@ public class FrameStateTest implements ActionListener, ItemListener {
             addWindowListener(this);
 
             setBounds(100, 2, 200, 200);
-            setState(iconic ? Frame.ICONIFIED : Frame.NORMAL);
-            setResizable(resizable);
+            setState(iconified ? Frame.ICONIFIED : Frame.NORMAL);
+            setResizable(isResizable);
             pack();
             setVisible(true);
         }
@@ -249,9 +225,9 @@ public class FrameStateTest implements ActionListener, ItemListener {
                 setBounds(r);
                 validate();
             } else if (e.getSource() == b1) {
-                resizable = !resizable;
-                stateLog(" - button pressed - setting Resizable to: " + resizable);
-                ((Frame) (b1.getParent())).setResizable(resizable);
+                isResizable = !isResizable;
+                stateLog(" - button pressed - setting Resizable to: " + isResizable);
+                ((Frame) (b1.getParent())).setResizable(isResizable);
             } else if (e.getSource() == b3) {
                 stateLog(" - button pressed - setting Iconic: ");
                 ((Frame) (b1.getParent())).setState(Frame.ICONIFIED);
