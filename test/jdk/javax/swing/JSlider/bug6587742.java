86f17447362483162a5983c270e0b530d8d99354
Abhishek Kumar <abhiscxk@openjdk.org>
2024-03-18 11:49:49 +0000

    8328248: Convert javax/swing/JSlider/6587742/bug6587742.java applet test to main
    Reviewed-by: azvegint, tr

--- test/jdk/javax/swing/JSlider/bug6587742.java
+++ test/jdk/javax/swing/JSlider/bug6587742.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,14 +21,18 @@
  * questions.
  */
 
-/* @test
- * @bug 6587742
- * @summary filling half of a JSlider's track is no longer optional
- * @author Pavel Porvatov
- * @run applet/manual=done bug6587742.html
- */
-
-import javax.swing.*;
+import javax.swing.BoxLayout;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JTabbedPane;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.plaf.metal.DefaultMetalTheme;
 import javax.swing.plaf.metal.MetalLookAndFeel;
 import javax.swing.plaf.metal.MetalTheme;
@@ -36,14 +40,46 @@ import javax.swing.plaf.metal.OceanTheme;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-public class bug6587742 extends JApplet {
-    public void init() {
-        TestPanel panel = new TestPanel();
+/* @test
+ * @bug 6587742
+ * @summary filling half of a JSlider's track is no longer optional
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
+ * @run main/manual bug6587742
+ */
 
-        setContentPane(panel);
+public class bug6587742 {
+    private static final String INSTRUCTIONS = """
+            Select every theme and check that all sliders looks good.
+            Note that every slider has a tooltip text with information about
+            slider configuration.
+            There is a small difference in sliders with property "filled = null" (it's
+            default behaviour when property JSlider.isFilled is not setted)
+            for themes:
+            1. OceanTheme - sliders look like filled
+            2. DefaultMetalTheme - sliders look like NOT filled\s""";
+    private static JFrame frame;
+
+    public static void main(String[] args) throws Exception {
+        PassFailJFrame.builder()
+                .title("JSlider Instructions")
+                .instructions(INSTRUCTIONS)
+                .rows(10)
+                .columns(40)
+                .testUI(bug6587742::createAndShowUI)
+                .build()
+                .awaitAndCheck();
     }
 
-    private class TestPanel extends JPanel {
+    public static JFrame createAndShowUI() {
+        frame = new JFrame("Test Sliders");
+        TestPanel panel = new TestPanel();
+        frame.setSize(800, 600);
+        frame.getContentPane().add(panel);
+        return frame;
+    }
+
+    private static class TestPanel extends JPanel {
         private final JComboBox cbThemes = new JComboBox();
 
         private TestPanel() {
@@ -68,30 +104,30 @@ public class bug6587742 extends JApplet {
                             return;
                         }
 
-                        SwingUtilities.updateComponentTreeUI(bug6587742.this);
+                        SwingUtilities.updateComponentTreeUI(frame);
                     }
                 }
             });
 
             JPanel pnVertical = new JPanel();
 
-            pnVertical.setLayout(new BoxLayout(pnVertical, BoxLayout.Y_AXIS));
+            pnVertical.setLayout(new BoxLayout(pnVertical, BoxLayout.X_AXIS));
 
             for (int i = 0; i < 12; i++) {
                 int filled = i >> 2;
 
-                pnVertical.add(createSlider(false, filled > 1 ? null : Boolean.valueOf(filled == 1), (i & 2) == 0,
+                pnVertical.add(createSlider(true, filled > 1 ? null : Boolean.valueOf(filled == 1), (i & 2) == 0,
                         (i & 1) != 0));
             }
 
             JPanel pnHorizontal = new JPanel();
 
-            pnHorizontal.setLayout(new BoxLayout(pnHorizontal, BoxLayout.X_AXIS));
+            pnHorizontal.setLayout(new BoxLayout(pnHorizontal, BoxLayout.Y_AXIS));
 
             for (int i = 0; i < 12; i++) {
                 int filled = i >> 2;
 
-                pnHorizontal.add(createSlider(true, filled > 1 ? null : Boolean.valueOf(filled == 1), (i & 2) == 0,
+                pnHorizontal.add(createSlider(false, filled > 1 ? null : Boolean.valueOf(filled == 1), (i & 2) == 0,
                         (i & 1) != 0));
             }
 
