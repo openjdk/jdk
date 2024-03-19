cc6d560d8ddd252eaa8b8cd523c393eb7a335ea1
Renjith Kannath Pariyangad <renjith.kannath.pariyangad@oracle.com>
2024-03-19 14:23:50 +0530

    Removed extra space

--- test/jdk/java/awt/print/PrinterJob/SecurityDialogTest.java
+++ test/jdk/java/awt/print/PrinterJob/SecurityDialogTest.java
@@ -39,8 +39,8 @@ import javax.swing.SwingUtilities;
  * @test
  * @bug 4937672 5100706 6252456
  * @key printer
- * @summary tests native and cross-platform page and print dialog
- *          when security manager is installed
+ * @summary Verifies "Print to file" option is disable if reading/writing files
+ *          is not allowed by Security Manager.
  * @library /java/awt/regtesthelpers
  * @build PassFailJFrame
  * @run main/manual/othervm -Djava.security.manager=allow SecurityDialogTest
@@ -66,7 +66,7 @@ public class SecurityDialogTest {
             throw new RuntimeException("Printer not configured or available.");
         }
 
-        PassFailJFrame passFailJFrame =  PassFailJFrame.builder()
+        PassFailJFrame passFailJFrame = PassFailJFrame.builder()
                 .instructions(INSTRUCTIONS)
                 .splitUIBottom(SecurityDialogTest::createTestUI)
                 .rows((int) INSTRUCTIONS.lines().count() + 1)
