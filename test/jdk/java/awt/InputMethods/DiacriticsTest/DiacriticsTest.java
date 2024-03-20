799f663c7de00ce47352eb511cd5d79f67cedb47
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2024-03-20 02:19:44 +0100

    initial

--- test/jdk/java/awt/InputMethods/DiacriticsTest/DiacriticsTest.java
+++ test/jdk/java/awt/InputMethods/DiacriticsTest/DiacriticsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,29 +22,60 @@
  */
 
 
-/*
-  @test
-  @bug 8000423 7197619 8025649
-  @summary Check if diacritical signs could be typed for TextArea and TextField
-  @run applet/manual=yesno DiacriticsTest.html
-*/
-
-
-import java.applet.Applet;
-import java.awt.*;
+import java.awt.GridLayout;
+import java.awt.TextArea;
+import java.awt.TextField;
 import javax.swing.JPanel;
 
+/*
+ * @test
+ * @bug 8000423 7197619 8025649
+ * @summary Check if diacritical signs could be typed for TextArea and TextField
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
+ * @run main/manual DiacriticsTest
+*/
 
-public class DiacriticsTest extends Applet {
+public class DiacriticsTest {
 
-    public void init() {
-        this.setLayout(new BorderLayout());
+    //TODO instructions per platform
+    private static final String INSTRUCTIONS = """
+    Test run requires the following keyboard layouts to be installed:
+    Linux OS: English (US, alternative international)
+    Windows OS: Hungarian
+    A keyboard layout having compose function or compose-like key. Programmer
+    Dvorak (http://www.kaufmann.no/roland/dvorak/) is suggested to use.
+
+    To test JDK-8000423 fix (Linux only!):
+    please switch to US alternative international layout and try to type diacritics
+    (using the following combinations: `+e; `+u; etc.)
+
+    To test JDK-7197619 fix (Windows only!):
+    please switch to Hungarian keyboard layout and try to type diacritics
+    (Ctrl+Alt+2 e; Ctrl+Alt+2 E)
+
+    To test JDK-8139189 fix:
+    please switch to Programmer Dvorak keyboard layout try to type diacritics
+    using compose combinations (Compose+z+d, Compose+z+Shift+d). The Compose key
+    in Programmer Dvorak layout is OEM102 the key which is located between
+    Left Shift and Z keys on the standard 102-key keyboard.
+    
+    If you can do that then the test is passed; otherwise failed.
+    """;
+
+    public static void main(String[] args) throws Exception {
+        PassFailJFrame
+                .builder()
+                .title("DiacriticsTest Instructions")
+                .instructions(INSTRUCTIONS)
+                .splitUIBottom(DiacriticsTest::createPanel)
+                .rows(15)
+                .columns(40)
+                .build()
+                .awaitAndCheck();
     }
 
-    public void start() {
-
-        setSize(350, 200);
-
+    public static JPanel createPanel() {
         JPanel panel = new JPanel();
         panel.setLayout(new GridLayout(2, 1));
 
@@ -54,10 +85,7 @@ public class DiacriticsTest extends Applet {
         TextField txtField = new TextField();
         panel.add(txtField);
 
-        add(panel, BorderLayout.CENTER);
-
-        validate();
-        setVisible(true);
+        return panel;
     }
 }
 
