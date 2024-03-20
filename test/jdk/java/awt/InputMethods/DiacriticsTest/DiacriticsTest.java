73d2d47da92a5b3e82f61468d1dc37eeee94718f
Aleksandr Zvegintsev <alexander.zvegintsev@oracle.com>
2024-03-20 03:59:20 +0100

    split instructions for Windows and Linux

--- test/jdk/java/awt/InputMethods/DiacriticsTest/DiacriticsTest.java
+++ test/jdk/java/awt/InputMethods/DiacriticsTest/DiacriticsTest.java
@@ -22,6 +22,7 @@
  */
 
 
+import jdk.test.lib.Platform;
 import java.awt.GridLayout;
 import java.awt.TextArea;
 import java.awt.TextField;
@@ -31,46 +32,79 @@ import javax.swing.JPanel;
  * @test
  * @bug 8000423 7197619 8025649
  * @summary Check if diacritical signs could be typed for TextArea and TextField
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
+ * @requires (os.family == "windows" | os.family == "linux")
+ * @library /java/awt/regtesthelpers /test/lib
+ * @build PassFailJFrame jdk.test.lib.Platform
  * @run main/manual DiacriticsTest
 */
 
 public class DiacriticsTest {
 
-    //TODO instructions per platform
-    private static final String INSTRUCTIONS = """
+    private static final String INSTRUCTIONS_WIN = """
     Test run requires the following keyboard layouts to be installed:
-    Linux OS: English (US, alternative international)
-    Windows OS: Hungarian
-    A keyboard layout having compose function or compose-like key. Programmer
+    - Hungarian
+    - A keyboard layout having compose function or compose-like key. Programmer
     Dvorak (http://www.kaufmann.no/roland/dvorak/) is suggested to use.
 
-    To test JDK-8000423 fix (Linux only!):
-    please switch to US alternative international layout and try to type diacritics
-    (using the following combinations: `+e; `+u; etc.)
+    To the right are a text area and a text field, you should check the behavior
+    for both of them.
 
-    To test JDK-7197619 fix (Windows only!):
-    please switch to Hungarian keyboard layout and try to type diacritics
+    To test the JDK-7197619 fix:
+    Please switch to Hungarian keyboard layout and try to type diacritics
     (Ctrl+Alt+2 e; Ctrl+Alt+2 E)
 
-    To test JDK-8139189 fix:
-    please switch to Programmer Dvorak keyboard layout try to type diacritics
-    using compose combinations (Compose+z+d, Compose+z+Shift+d). The Compose key
-    in Programmer Dvorak layout is OEM102 the key which is located between
-    Left Shift and Z keys on the standard 102-key keyboard.
-    
+    To test the JDK-8139189 fix:
+    Please switch to Programmer Dvorak keyboard layout try to type diacritics
+    using compose combinations (Compose+z+d, Compose+z+Shift+d).
+
+    The Compose key in the Programmer Dvorak layout is OEM102, the key located
+    between the and Z keys on a standard 102-key keyboard.
+    If you do not have this key on your keyboard, you can skip this part of the test.
+
+    If you can do that then the test is passed; otherwise failed.
+    """;
+
+    private static final String INSTRUCTIONS_LIN = """
+    Test run requires the following keyboard layouts to be installed:
+    - English (US, alternative international), aka English (US, alt. intl.)
+    - A keyboard layout having compose function or compose-like key. Programmer
+    Dvorak (http://www.kaufmann.no/roland/dvorak/) is suggested to use.
+
+    To the right are a text area and a text field, you should check the behavior
+    for both of them.
+
+    To test the JDK-8000423 fix:
+    Please switch to US alternative international layout and try to type diacritics
+    (using the following combinations: `+e; `+u; etc.)
+
+    To test the JDK-8139189 fix:
+    Please switch to Programmer Dvorak keyboard layout try to type diacritics
+    using compose combinations (Compose+z+d, Compose+z+Shift+d)..
+
+    The Compose key in the Programmer Dvorak layout is OEM102, the key located
+    between the and Z keys on a standard 102-key keyboard.
+
+    If the above key does not work in the Gnome shell,
+    it can be overridden in the system preferences:
+    System > Keyboard > Special character entry > Compose key
+    and set it to another key(e.g. menu key or scroll lock.)
+
     If you can do that then the test is passed; otherwise failed.
     """;
 
     public static void main(String[] args) throws Exception {
+        String instructions = Platform.isWindows()
+                ? INSTRUCTIONS_WIN
+                : INSTRUCTIONS_LIN;
+
         PassFailJFrame
                 .builder()
                 .title("DiacriticsTest Instructions")
-                .instructions(INSTRUCTIONS)
-                .splitUIBottom(DiacriticsTest::createPanel)
-                .rows(15)
-                .columns(40)
+                .instructions(instructions)
+                .splitUIRight(DiacriticsTest::createPanel)
+                .testTimeOut(10)
+                .rows((int) instructions.lines().count() + 2)
+                .columns(50)
                 .build()
                 .awaitAndCheck();
     }
@@ -88,4 +122,3 @@ public class DiacriticsTest {
         return panel;
     }
 }
-
