a849655e42bfaf1f5e923a7e74ec08efea9492a3
Alisen Chung <alisen.chung@oracle.com>
2024-03-20 12:12:10 -0700

    init commit

--- test/jdk/javax/swing/JFileChooser/4150029/bug4150029.java
+++ test/jdk/javax/swing/JFileChooser/4150029/bug4150029.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,73 +21,99 @@
  * questions.
  */
 
-/*
-   bug 4150029 8006087
-   summary BackSpace keyboard button does not lead to parent directory
-   author Oleg Mokhovikov
-*/
+import java.io.File;
+import java.io.IOException;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.UIManager;
 
 import jdk.test.lib.Platform;
 
-import javax.swing.*;
-import java.io.File;
-import java.io.IOException;
+/*
+ * @test
+ * @bug 4150029 8006087
+ * @summary BackSpace keyboard button does not lead to parent directory
+ * @library /test/lib /java/awt/regtesthelpers
+ * @build jdk.test.lib.Platform PassFailJFrame
+ * @run main/manual bug4150029
+ */
 
-public class bug4150029 extends JApplet {
-    private boolean res;
+public class bug4150029 {
+    private static boolean res;
 
-    public void init() {
-        if (Platform.isOSX()) {
-            try {
-                UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+    public static void main(String[] args) throws Exception {
+        String instructions = """
+                Follow the instructions below.
+                1.Go into 'subDir' folder.
+                2.Press BACKSPACE key.
+                3.Push OPEN button.
+                4.Push PASS button.""";
+
+        PassFailJFrame pfframe = PassFailJFrame.builder()
+                .title("bug4150029")
+                .instructions(instructions)
+                .rows(5)
+                .columns(40)
+                .testTimeOut(10)
+                .build();
+
+        try {
+            if (Platform.isOSX()) {
+                try {
+                    UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
 
-        String tmpDir = System.getProperty("java.io.tmpdir");
+            String tmpDir = System.getProperty("java.io.tmpdir");
 
-        if (tmpDir.length() == 0) {//'java.io.tmpdir' isn't guaranteed to be defined
-            tmpDir = System.getProperty("user.home");
-        }
+            //'java.io.tmpdir' isn't guaranteed to be defined
+            if (tmpDir.length() == 0) {
+                tmpDir = System.getProperty("user.home");
+            }
 
-        System.out.println("Temp directory: " + tmpDir);
+            System.out.println("Temp directory: " + tmpDir);
 
-        File testDir = new File(tmpDir, "testDir");
+            File testDir = new File(tmpDir, "testDir");
 
-        testDir.mkdir();
+            testDir.mkdir();
 
-        File subDir = new File(testDir, "subDir");
+            File subDir = new File(testDir, "subDir");
 
-        subDir.mkdir();
+            subDir.mkdir();
 
-        System.out.println("Created directory: " + testDir);
-        System.out.println("Created sub-directory: " + subDir);
+            System.out.println("Created directory: " + testDir);
+            System.out.println("Created sub-directory: " + subDir);
 
-        JFileChooser fileChooser = new JFileChooser(testDir);
+            JFileChooser fileChooser = new JFileChooser(testDir);
 
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-        try {
-            res = fileChooser.showOpenDialog(this) != JFileChooser.APPROVE_OPTION ||
-                    testDir.getCanonicalPath().equals(fileChooser.getSelectedFile().getCanonicalPath());
-        } catch (IOException e) {
-            res = false;
+            JDialog dialog = new JDialog();
+            PassFailJFrame.addTestWindow(dialog);
 
-            e.printStackTrace();
-        }
+            try {
+                res = fileChooser.showOpenDialog(dialog) != JFileChooser.APPROVE_OPTION ||
+                        testDir.getCanonicalPath().equals(fileChooser.getSelectedFile().getCanonicalPath());
+            } catch (IOException e) {
+                res = false;
 
-        try {
-            subDir.delete();
-            testDir.delete();
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        }
-    }
+                e.printStackTrace();
+            }
 
-    public void destroy() {
-        if (!res) {
-            throw new RuntimeException("BackSpace keyboard button does not lead to parent directory");
+            try {
+                subDir.delete();
+                testDir.delete();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+
+            pfframe.awaitAndCheck();
+        } finally {
+            if (!res) {
+                throw new RuntimeException("BackSpace keyboard button does not lead to parent directory");
+            }
         }
     }
 }
