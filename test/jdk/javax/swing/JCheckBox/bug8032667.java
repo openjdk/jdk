ef2bd5721316f208a458fecfffa02ec2c0ea7fe0
Damon Nguyen <dnguyen@openjdk.org>
2024-03-21 16:12:47 +0000

    8328558: Convert javax/swing/JCheckBox/8032667/bug8032667.java applet test to main
    Reviewed-by: abhiscxk, psadhukhan, aivanov

--- test/jdk/javax/swing/JCheckBox/bug8032667.java
+++ test/jdk/javax/swing/JCheckBox/bug8032667.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -20,24 +20,28 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 import java.awt.BorderLayout;
-import java.awt.Canvas;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
-import javax.swing.JApplet;
+
 import javax.swing.JCheckBox;
 import javax.swing.JComponent;
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /* @test
  * @bug 8032667
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
  * @summary [macosx] Components cannot be rendered in HiDPI to BufferedImage
- * @run applet/manual=yesno bug8032667.html
+ * @run main/manual bug8032667
  */
-public class bug8032667 extends JApplet {
+
+public class bug8032667 {
 
     static final int scale = 2;
     static final int width = 130;
@@ -45,30 +49,43 @@ public class bug8032667 extends JApplet {
     static final int scaledWidth = scale * width;
     static final int scaledHeight = scale * height;
 
-    @Override
-    public void init() {
-        SwingUtilities.invokeLater(new Runnable() {
+    private static final String INSTRUCTIONS = """
+            Verify that scaled components are rendered smoothly to image.
 
+            1. Run the test.
+            2. Check that Selected and Deselected JCheckBox icons are drawn smoothly.
+            If so, press PASS, else press FAIL.
+            """;
+
+    public static void main(String[] args) throws Exception {
+        PassFailJFrame.builder()
+                .title("bug8032667 Test Instructions")
+                .instructions(INSTRUCTIONS)
+                .rows((int) INSTRUCTIONS.lines().count() + 2)
+                .columns(40)
+                .testUI(bug8032667::createAndShowGUI)
+                .build()
+                .awaitAndCheck();
+    }
+
+    public static JFrame createAndShowGUI() {
+        JFrame frame = new JFrame("bug8032667 HiDPI Component Test");
+        frame.setLayout(new BorderLayout());
+        frame.setBounds(0, 400, 400, 400);
+
+        final Image image1 = getImage(getCheckBox("Deselected", false));
+        final Image image2 = getImage(getCheckBox("Selected", true));
+        JPanel panel = new JPanel() {
             @Override
-            public void run() {
-
-                final Image image1 = getImage(getCheckBox("Deselected", false));
-                final Image image2 = getImage(getCheckBox("Selected", true));
-
-                Canvas canvas = new Canvas() {
-
-                    @Override
-                    public void paint(Graphics g) {
-                        super.paint(g);
-                        g.drawImage(image1, 0, 0, scaledWidth, scaledHeight, this);
-                        g.drawImage(image2, 0, scaledHeight + 5,
-                                scaledWidth, scaledHeight, this);
-                    }
-                };
-
-                getContentPane().add(canvas, BorderLayout.CENTER);
+            public void paint(Graphics g) {
+                super.paint(g);
+                g.drawImage(image1, 0, 0, scaledWidth, scaledHeight, this);
+                g.drawImage(image2, 0, scaledHeight + 5,
+                        scaledWidth, scaledHeight, this);
             }
-        });
+        };
+        frame.add(panel, BorderLayout.CENTER);
+        return frame;
     }
 
     static JCheckBox getCheckBox(String text, boolean selected) {
