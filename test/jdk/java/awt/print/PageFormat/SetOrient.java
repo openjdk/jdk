85f44a0d59a8dca0c5567f36c7ff63dd26742d58
Damon Nguyen <damon.nguyen@oracle.com>
2024-03-25 12:56:46 -0700

    Add error msg. Add print button instead of auto print on exec.

--- test/jdk/java/awt/print/PageFormat/SetOrient.java
+++ test/jdk/java/awt/print/PageFormat/SetOrient.java
@@ -42,6 +42,8 @@ import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 
+import javax.swing.JButton;
+
 public class SetOrient {
     static PrinterJob pjob;
     private static final String INSTRUCTIONS =
@@ -56,54 +58,60 @@ public class SetOrient {
             """;
 
     public static void main(String[] args) throws Exception {
-        init();
         PassFailJFrame
                 .builder()
                 .title("SetOrient Test Instructions")
                 .instructions(INSTRUCTIONS)
                 .rows((int) INSTRUCTIONS.lines().count() + 2)
                 .columns(40)
+                .splitUIBottom(SetOrient::createAndShowGUI)
                 .build()
                 .awaitAndCheck();
     }
 
-    public static void init() {
-        pjob = PrinterJob.getPrinterJob();
+    public static JButton createAndShowGUI() {
+        JButton btn = new JButton("PRINT");
+        btn.addActionListener(e -> {
+            pjob = PrinterJob.getPrinterJob();
 
-        Printable p = new Printable() {
-            public int print(Graphics g, PageFormat pf, int pageIndex) {
-                Graphics2D g2d = (Graphics2D)g;
-                drawGraphics(g2d, pf);
-                return Printable.PAGE_EXISTS;
+            Printable p = new Printable() {
+                public int print(Graphics g, PageFormat pf, int pageIndex) {
+                    Graphics2D g2d = (Graphics2D)g;
+                    drawGraphics(g2d, pf);
+                    return Printable.PAGE_EXISTS;
+                }
+
+                void drawGraphics(Graphics2D g, PageFormat pf) {
+                    double ix = pf.getImageableX();
+                    double iy = pf.getImageableY();
+                    double iw = pf.getImageableWidth();
+                    double ih = pf.getImageableHeight();
+
+                    g.setColor(Color.black);
+                    g.drawString(((pf.getOrientation() == PageFormat.PORTRAIT)
+                                    ? "Portrait" : "Landscape"),
+                            (int) (ix + iw / 2), (int) (iy + ih / 2));
+                    g.draw(new Ellipse2D.Double(ix, iy, iw, ih));
+                }
+            };
+
+            Book book = new Book();
+            PageFormat pf = pjob.defaultPage();
+            pf.setOrientation(PageFormat.PORTRAIT);
+            book.append(p, pf);
+            pf = pjob.defaultPage();
+            pf.setOrientation(PageFormat.LANDSCAPE);
+            book.append(p, pf);
+            pjob.setPageable(book);
+
+            try {
+                pjob.print();
+            } catch (PrinterException ex) {
+                ex.printStackTrace();
+                String msg = "PrinterException: " + ex.getMessage();
+                PassFailJFrame.forceFail(msg);
             }
-
-            void drawGraphics(Graphics2D g, PageFormat pf) {
-                double ix = pf.getImageableX();
-                double iy = pf.getImageableY();
-                double iw = pf.getImageableWidth();
-                double ih = pf.getImageableHeight();
-
-                g.setColor(Color.black);
-                g.drawString(((pf.getOrientation() == PageFormat.PORTRAIT)
-                                ? "Portrait" : "Landscape"),
-                        (int) (ix + iw / 2), (int) (iy + ih / 2));
-                g.draw(new Ellipse2D.Double(ix, iy, iw, ih));
-            }
-        };
-
-        Book book = new Book();
-        PageFormat pf = pjob.defaultPage();
-        pf.setOrientation(PageFormat.PORTRAIT);
-        book.append(p, pf);
-        pf = pjob.defaultPage();
-        pf.setOrientation(PageFormat.LANDSCAPE);
-        book.append(p, pf);
-        pjob.setPageable(book);
-
-        try {
-            pjob.print();
-        } catch (PrinterException e) {
-            throw new RuntimeException(e.getMessage());
-        }
+        });
+        return btn;
     }
 }
