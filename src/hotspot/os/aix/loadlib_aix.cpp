b761ba52414f900bbb119f16e97972f93b6b4029
MBaesken <matthias.baesken@sap.com>
2024-03-27 16:40:14 +0100

    adjust COPYRIGHT years

--- src/hotspot/os/aix/loadlib_aix.cpp
+++ src/hotspot/os/aix/loadlib_aix.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017, 2023, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2012, 2019 SAP SE. All rights reserved.
+ * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2024 SAP SE. All rights reserved.
  * Copyright (c) 2022, IBM Corp.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
