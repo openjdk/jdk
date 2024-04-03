51d6f16ae163eed310b8acd1832d4afb27d78838
Andrew Haley <aph@openjdk.org>
2024-04-03 13:11:22 +0200

    JDK-8180450: secondary_super_cache does not scale well

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.hpp
@@ -1008,7 +1008,8 @@ public:
                                      Register temp3,
                                      Register temp4,
                                      FloatRegister vtemp,
-                                     Register result);
+                                     Register result,
+                                     int hash_slot = -1);
 
   void verify_klass_subtype_slow_path(Register sub_klass,
                                       Klass *super_klass,
