4794071bdff7cf6587d12beb7ca9dbcf7f56c50b
Evemose <124714317+Evemose@users.noreply.github.com>
2024-04-08 10:53:22 +0300

    Also moved to List interface
    Co-authored-by: ExE Boss <3889017+ExE-Boss@users.noreply.github.com>

--- src/java.base/share/classes/java/util/AbstractList.java
+++ src/java.base/share/classes/java/util/AbstractList.java
@@ -202,27 +202,6 @@ public abstract class AbstractList<E> extends AbstractCollection<E> implements L
      * {@inheritDoc}
      *
      * @implSpec
-     * This implementation first gets a list iterator (with
-     * {@code listIterator()}).  Then, it iterates over the list until a
-     * matching element is found or the beginning of the list is reached.
-     *
-     * @throws NullPointerException {@inheritDoc}
-     */
-    public int indexOf(Predicate<? super E> filter) {
-        ListIterator<E> it = listIterator();
-        while (it.hasNext()) {
-            E e = it.next();
-            if (filter.test(e)) {
-                return it.previousIndex();
-            }
-        }
-        return -1;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @implSpec
      * This implementation first gets a list iterator that points to the end
      * of the list (with {@code listIterator(size())}).  Then, it iterates
      * backwards over the list until the specified element is found, or the
