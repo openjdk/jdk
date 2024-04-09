bfccf6d7ab6f0f3bef772fed5986ac3610acb287
Johan Sjölén <johan.sjolen@oracle.com>
2024-04-09 16:35:31 +0200

    Save the errno and use

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -611,11 +611,12 @@ void os::print_jni_name_suffix_on(outputStream* st, int args_size) {
 bool os::get_host_name(char* buf, size_t buflen) {
   struct utsname name;
   int retcode = uname(&name);
+  int saved_errno = errno;
   if (retcode != -1) {
     jio_snprintf(buf, buflen, "%s", name.nodename);
     return true;
   }
-  const char* errmsg = os::strerror(retcode);
+  const char* errmsg = os::strerror(saved_errno);
   log_warning(os)("Failed to get host name, error message: %s", errmsg);
   return false;
 }
