bbe05c97ca75351550aa952b9f0fad9635ec4bb6
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2024-04-11 11:12:30 +0100

    Drop spurious jshell test change

--- test/langtools/jdk/jshell/CompletionSuggestionTest.java
+++ test/langtools/jdk/jshell/CompletionSuggestionTest.java
@@ -324,8 +324,7 @@ public class CompletionSuggestionTest extends KullaTesting {
                 "String(char[], int, int)");
         assertSignature("String.format(|",
                 "String String.format(String, Object...)",
-                "String String.format(java.util.Locale, String, Object...)"
-                );
+                "String String.format(java.util.Locale, String, Object...)");
         assertSignature("\"\".getBytes(\"\"|", "void String.getBytes(int, int, byte[], int)",
                                                     "byte[] String.getBytes(String) throws java.io.UnsupportedEncodingException",
                                                     "byte[] String.getBytes(java.nio.charset.Charset)");
