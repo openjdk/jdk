39559793eaaef3aa6b69fe0c979efceb3ea0c6b5
Fabian Meumertzheim <fabian@meumertzhe.im>
2024-04-17 08:45:55 +0200

    Move `@implNote`

--- src/java.base/share/classes/java/nio/file/WatchService.java
+++ src/java.base/share/classes/java/nio/file/WatchService.java
@@ -98,6 +98,16 @@ import java.util.concurrent.TimeUnit;
  * it is not required that changes to files carried out on remote systems be
  * detected.
  *
+ * @implNote
+ * In the reference implementation, the maximum size of the list of events
+ * returned by a key's {@link WatchKey#pollEvents() pollEvents} method is
+ * controlled by the system property {@code
+ * jdk.nio.file.WatchService.maxEventsPerPoll}. If the property is not set or
+ * is not an integer, the maximum size will be set to 512. The maximum size is
+ * always at least 1. If more events occur than the maximum size, the pending
+ * events are cleared and replaced with a single {@link
+ * StandardWatchEventKinds#OVERFLOW OVERFLOW} event.
+ *
  * @since 1.7
  *
  * @see FileSystem#newWatchService
