893e7bc894e2828f8d02db06302485d02f2cfa5a
Weijun Wang <weijun@openjdk.org>
2024-04-17 22:38:46 +0000

    8328726: Better Kerberos support
    Reviewed-by: ahgross, rhalade, valeriep, coffeys

--- src/java.security.jgss/share/classes/sun/security/jgss/krb5/Krb5Context.java
+++ src/java.security.jgss/share/classes/sun/security/jgss/krb5/Krb5Context.java
@@ -901,15 +901,11 @@ class Krb5Context implements GSSContextSpi {
 
     public final byte[] wrap(byte[] inBuf, int offset, int len,
                              MessageProp msgProp) throws GSSException {
-        if (DEBUG != null) {
-            DEBUG.println("Krb5Context.wrap: data=["
-                               + getHexBytes(inBuf, offset, len)
-                               + "]");
-        }
 
-        if (state != STATE_DONE)
-        throw new GSSException(GSSException.NO_CONTEXT, -1,
-                               "Wrap called in invalid state!");
+        if (state != STATE_DONE) {
+            throw new GSSException(GSSException.NO_CONTEXT, -1,
+                    "Wrap called in invalid state!");
+        }
 
         byte[] encToken = null;
         try {
@@ -1050,12 +1046,6 @@ class Krb5Context implements GSSContextSpi {
                         new WrapToken_v2(this, inBuf, offset, len, msgProp);
                 data = token.getData();
                 setSequencingAndReplayProps(token, msgProp);
-            }
-
-            if (DEBUG != null) {
-                DEBUG.println("Krb5Context.unwrap: data=["
-                                   + getHexBytes(data, 0, data.length)
-                                   + "]");
             }
 
             return data;
@@ -1407,8 +1397,8 @@ class Krb5Context implements GSSContextSpi {
 
         @Override
         public String toString() {
-            return "Kerberos session key: etype: " + key.getEType() + "\n" +
-                    new HexDumpEncoder().encodeBuffer(key.getBytes());
+            return "Kerberos session key: etype=" + key.getEType()
+                    + ", " + Krb5Util.keyInfo(key.getBytes());
         }
 
         /**
