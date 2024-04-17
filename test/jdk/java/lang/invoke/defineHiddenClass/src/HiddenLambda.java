8b877851374a82ca08a04d56ab3f2e2d2cc8742b
Adam Sotona <adam.sotona@oracle.com>
2024-04-17 18:03:21 +0200

    applied suggested changes

--- test/jdk/java/lang/invoke/defineHiddenClass/src/HiddenLambda.java
+++ test/jdk/java/lang/invoke/defineHiddenClass/src/HiddenLambda.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,9 +23,9 @@
 
 import java.util.function.Function;
 
-public class Lambda implements HiddenTest {
+public class HiddenLambda implements HiddenTest {
      public void test() {
-         Function<Object, String> f = Object::toString;
+         Function<Object, String> f = o -> o.toString();
          String s = f.apply(this);
          throw new Error("thrown by " + s);
      }
