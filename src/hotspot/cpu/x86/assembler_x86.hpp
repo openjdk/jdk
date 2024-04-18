455f70626f5b9b920ae6b6d97cdc1b7aaff0f622
Martin Balao <mbalao@redhat.com>
2024-04-18 19:43:07 -0400

    8330611: AES-CTR vector intrinsic may read out of bounds (x86_64, AVX-512)
    Co-authored-by: Francisco Ferrari Bihurriet <fferrari@redhat.com>
    Co-authored-by: Martin Balao <mbalao@openjdk.org>

--- src/hotspot/cpu/x86/assembler_x86.hpp
+++ src/hotspot/cpu/x86/assembler_x86.hpp
@@ -2253,6 +2253,7 @@ private:
   void xorb(Address dst, Register src);
   void xorb(Register dst, Address src);
   void xorw(Register dst, Register src);
+  void xorw(Register dst, Address src);
 
   void xorq(Register dst, Address src);
   void xorq(Address dst, int32_t imm32);
