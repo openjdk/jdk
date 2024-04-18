1a2a103ddb8aff5a32ad4a69b6cd0be84b4a6eab
Stefan Johansson <stefan.johansson@oracle.com>
2024-04-18 15:11:10 +0200

    8330626: ZGC: Windows address space placeholders not managed correctly

--- src/hotspot/os/windows/gc/z/zVirtualMemory_windows.cpp
+++ src/hotspot/os/windows/gc/z/zVirtualMemory_windows.cpp
@@ -40,21 +40,36 @@ public:
 };
 
 // Implements small pages (paged) support using placeholder reservation.
+// When a memory area is free (kept by the virtual memory manager) a
+// single placeholder is covering that memory area. When memory is
+// allocated from the manager the placeholder is split into granule
+// sized placeholders to allow mapping operations on that granulairty.
 class ZVirtualMemoryManagerSmallPages : public ZVirtualMemoryManagerImpl {
 private:
   class PlaceholderCallbacks : public AllStatic {
   public:
+    // Split an existing placeholder to create a new placeholder
+    // for the requested memory area.
     static void split_placeholder(zoffset start, size_t size) {
       ZMapper::split_placeholder(ZOffset::address_unsafe(start), size);
     }
 
+    // Coalesce all placeholders covering the given memory area.
     static void coalesce_placeholders(zoffset start, size_t size) {
       ZMapper::coalesce_placeholders(ZOffset::address_unsafe(start), size);
     }
 
-    static void split_into_placeholder_granules(zoffset start, size_t size) {
-      for (uintptr_t addr = untype(start); addr < untype(start) + size; addr += ZGranuleSize) {
-        split_placeholder(to_zoffset(addr), ZGranuleSize);
+    // Turn the single placeholder covering a memroy area into granule
+    // sized placeholders. This is done by splitting granule sized placeholders
+    // from the covering placeholder until the whole area is handled.
+    static void create_granule_sized_placeholders(zoffset start, size_t size) {
+      size_t current_placeholder_size = size;
+      zoffset current_placeholder_offset = start;
+      // Split the current placeholder as long as it is larger than a granule
+      while (current_placeholder_size > ZGranuleSize) {
+        split_placeholder(current_placeholder_offset, ZGranuleSize);
+        current_placeholder_offset += ZGranuleSize;
+        current_placeholder_size -= ZGranuleSize;
       }
     }
 
@@ -66,36 +81,66 @@ private:
       }
     }
 
+    // Called when a memory area is returned to the memory manager but can't
+    // be merged with an already existing area. Make sure this area is covered
+    // by a single placeholder.
     static void create_callback(const ZMemory* area) {
       assert(is_aligned(area->size(), ZGranuleSize), "Must be granule aligned");
       coalesce_into_one_placeholder(area->start(), area->size());
     }
 
+    // Called when a complete memory area in the memory manager is allocated.
+    // Create granule sized placeholder for the entier area.
     static void destroy_callback(const ZMemory* area) {
       assert(is_aligned(area->size(), ZGranuleSize), "Must be granule aligned");
-      // Don't try split the last granule - VirtualFree will fail
-      split_into_placeholder_granules(area->start(), area->size() - ZGranuleSize);
+      create_granule_sized_placeholders(area->start(), area->size());
     }
 
+    // Called when a memory area is allocated at the front of an exising memory area.
+    // Turn the first part of the memory area into granule sized placeholders.
     static void shrink_from_front_callback(const ZMemory* area, size_t size) {
+      assert(area->size() > size, "Must be larger than what we try to split out");
       assert(is_aligned(size, ZGranuleSize), "Must be granule aligned");
-      split_into_placeholder_granules(area->start(), size);
+
+      // Before creating the granule sized placeholders we need to make sure
+      // there is a placeholder covering that exact area. Otherwise splitting
+      // of a single granule sized won't work.
+      split_placeholder(area->start(), size);
+      create_granule_sized_placeholders(area->start(), size);
     }
 
+    // Called when a memory area is allocated at the end of an existing memory area.
+    // Turn the second part of the memory area into granule sized placeholders.
     static void shrink_from_back_callback(const ZMemory* area, size_t size) {
+      assert(area->size() > size, "Must be larger than what we try to split out");
       assert(is_aligned(size, ZGranuleSize), "Must be granule aligned");
-      // Don't try split the last granule - VirtualFree will fail
-      split_into_placeholder_granules(to_zoffset(untype(area->end()) - size), size - ZGranuleSize);
+
+      // Before creating the granule sized placeholders we need to make sure
+      // there is a placeholder covering that exact area. Otherwise splitting
+      // of a single granule sized won't work.
+      zoffset placeholder_start = to_zoffset(untype(area->end()) - size);
+
+      split_placeholder(placeholder_start, size);
+      create_granule_sized_placeholders(placeholder_start, size);
     }
 
+    // Called when freeing a memory area and it can be merged at the start of an
+    // existing area. Coalesce the underlying placeholders into one.
     static void grow_from_front_callback(const ZMemory* area, size_t size) {
       assert(is_aligned(area->size(), ZGranuleSize), "Must be granule aligned");
-      coalesce_into_one_placeholder(to_zoffset(untype(area->start()) - size), area->size() + size);
+
+      size_t placeholder_size = area->size() + size;
+      zoffset placeholder_start = to_zoffset(untype(area->start()) - size);
+      coalesce_into_one_placeholder(placeholder_start, placeholder_size);
     }
 
+    // Called when freeing a memory area and it can be merged at the end of an
+    // existing area. Coalesce the underlying placeholders into one.
     static void grow_from_back_callback(const ZMemory* area, size_t size) {
       assert(is_aligned(area->size(), ZGranuleSize), "Must be granule aligned");
-      coalesce_into_one_placeholder(area->start(), area->size() + size);
+
+      size_t placeholder_size = area->size() + size;
+      coalesce_into_one_placeholder(area->start(), placeholder_size);
     }
 
     static void register_with(ZMemoryManager* manager) {
