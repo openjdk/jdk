e917f20b913b7a564eb902457d8c79d18b825d16
Emanuel Peter <emanuel.peter@oracle.com>
2024-04-19 13:23:27 +0200

    some F/I and D/L tests

--- test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java
+++ test/hotspot/jtreg/compiler/loopopts/superword/TestCompatibleUseDefTypeSize.java
@@ -56,6 +56,10 @@ public class TestCompatibleUseDefTypeSize {
     int[] bI;
     long[] aL;
     long[] bL;
+    float[] aF;
+    float[] bF;
+    double[] aD;
+    double[] bD;
 
     // List of tests
     Map<String,TestFunction> tests = new HashMap<String,TestFunction>();
@@ -83,13 +87,23 @@ public class TestCompatibleUseDefTypeSize {
         bI = generateI();
         aL = generateL();
         bL = generateL();
+        aF = generateF();
+        bF = generateF();
+        aD = generateD();
+        bD = generateD();
 
         // Add all tests to list
         tests.put("test0",       () -> { return test0(aB.clone(), bC.clone()); });
         tests.put("test1",       () -> { return test1(aB.clone(), bC.clone()); });
         tests.put("test2",       () -> { return test2(aB.clone(), bC.clone()); });
         tests.put("test3",       () -> { return test3(aI.clone(), bI.clone()); });
-        tests.put("test3",       () -> { return test4(aI.clone(), bI.clone()); });
+        tests.put("test4",       () -> { return test4(aI.clone(), bI.clone()); });
+        tests.put("test5",       () -> { return test5(aI.clone(), bF.clone()); });
+        tests.put("test6",       () -> { return test6(aI.clone(), bF.clone()); });
+        tests.put("test7",       () -> { return test7(aI.clone(), bF.clone()); });
+        tests.put("test8",       () -> { return test8(aL.clone(), bD.clone()); });
+        tests.put("test9",       () -> { return test9(aL.clone(), bD.clone()); });
+        tests.put("test10",      () -> { return test10(aL.clone(), bD.clone()); });
 
         // Compute gold value for all test methods before compilation
         for (Map.Entry<String,TestFunction> entry : tests.entrySet()) {
@@ -105,7 +119,13 @@ public class TestCompatibleUseDefTypeSize {
                  "test1",
                  "test2",
                  "test3",
-                 "test4"})
+                 "test4",
+                 "test5",
+                 "test6",
+                 "test7",
+                 "test8",
+                 "test9",
+                 "test10"})
     public void runTests() {
         for (Map.Entry<String,TestFunction> entry : tests.entrySet()) {
             String name = entry.getKey();
@@ -159,6 +179,22 @@ public class TestCompatibleUseDefTypeSize {
         return a;
     }
 
+    static float[] generateF() {
+        float[] a = new float[RANGE];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Float.intBitsToFloat(RANDOM.nextInt());
+        }
+        return a;
+    }
+
+    static double[] generateD() {
+        double[] a = new double[RANGE];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Double.longBitsToDouble(RANDOM.nextLong());
+        }
+        return a;
+    }
+
     static void verify(String name, Object[] gold, Object[] result) {
         if (gold.length != result.length) {
             throw new RuntimeException("verify " + name + ": not the same number of outputs: gold.length = " +
@@ -192,6 +228,10 @@ public class TestCompatibleUseDefTypeSize {
                 verifyI(name, i, (int[])g, (int[])r);
             } else if (c == long.class) {
                 verifyL(name, i, (long[])g, (long[])r);
+            } else if (c == float.class) {
+                verifyF(name, i, (float[])g, (float[])r);
+            } else if (c == double.class) {
+                verifyD(name, i, (double[])g, (double[])r);
             } else {
                 throw new RuntimeException("verify " + name + ": array type not supported for verify:" +
                                        " gold[" + i + "].getClass() = " + g.getClass().getSimpleName() +
@@ -243,6 +283,26 @@ public class TestCompatibleUseDefTypeSize {
     static void verifyL(String name, int i, long[] g, long[] r) {
         for (int j = 0; j < g.length; j++) {
             if (g[j] != r[j]) {
+                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
+                                           " gold[" + i + "][" + j + "] = " + g[j] +
+                                           " result[" + i + "][" + j + "] = " + r[j]);
+            }
+        }
+    }
+
+    static void verifyF(String name, int i, float[] g, float[] r) {
+        for (int j = 0; j < g.length; j++) {
+            if (Float.floatToIntBits(g[j]) != Float.floatToIntBits(r[j])) {
+                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
+                                           " gold[" + i + "][" + j + "] = " + g[j] +
+                                           " result[" + i + "][" + j + "] = " + r[j]);
+            }
+        }
+    }
+
+    static void verifyD(String name, int i, double[] g, double[] r) {
+        for (int j = 0; j < g.length; j++) {
+            if (Double.doubleToLongBits(g[j]) != Double.doubleToLongBits(r[j])) {
                 throw new RuntimeException("verify " + name + ": arrays must have same content:" +
                                            " gold[" + i + "][" + j + "] = " + g[j] +
                                            " result[" + i + "][" + j + "] = " + r[j]);
@@ -320,6 +380,81 @@ public class TestCompatibleUseDefTypeSize {
         for (int i = 0; i < a.length-2; i+=2) {
             a[i+0] = (int)(b[i+0] + a[i+1]);
             a[i+1] = (int)(b[i+1] + a[i+2]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // In theory, one would expect this to be a simple 4byte -> 4byte conversion.
+    // But there is a CmpF and CMove here because we check for isNaN. Plus a MoveF2I.
+    //
+    // Would be nice to vectorize: Missing support for CmpF, CMove and MoveF2I.
+    static Object[] test5(int[] a, float[] b) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Float.floatToIntBits(b[i]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // Missing support for MoveF2I
+    static Object[] test6(int[] a, float[] b) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Float.floatToRawIntBits(b[i]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // Missing support for MoveI2F
+    static Object[] test7(int[] a, float[] b) {
+        for (int i = 0; i < a.length; i++) {
+            b[i] = Float.intBitsToFloat(a[i]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // Missing support for Needs CmpD, CMove and MoveD2L
+    static Object[] test8(long[] a, double[] b) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Double.doubleToLongBits(b[i]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // Missing support for MoveD2L
+    static Object[] test9(long[] a, double[] b) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = Double.doubleToRawLongBits(b[i]);
+        }
+        return new Object[]{ a, b };
+    }
+
+    @Test
+    @IR(counts = {IRNode.STORE_VECTOR, "= 0"},
+        applyIfPlatform = {"64-bit", "true"},
+        applyIfCPUFeatureOr = {"sse4.1", "true", "asimd", "true"})
+    // Missing support for MoveL2D
+    static Object[] test10(long[] a, double[] b) {
+        for (int i = 0; i < a.length; i++) {
+            b[i] = Double.longBitsToDouble(a[i]);
         }
         return new Object[]{ a, b };
     }
