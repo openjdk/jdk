6226f1b1b2f1d87efad4c2212f2b5fc371728aa3
redestad <claes.redestad@oracle.com>
2024-04-22 10:53:19 +0200

    Revert changes, splitting these out to a separate PR

--- src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
+++ src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
@@ -704,6 +704,11 @@ public class SwitchBootstraps {
 
         public static Map<TypePairs, String> initialize() {
             Map<TypePairs, String> typePairToName = new HashMap<>();
+            typePairToName.put(new TypePairs(byte.class,   char.class),   "isIntToCharExact");      // redirected
+            typePairToName.put(new TypePairs(short.class,  byte.class),   "isIntToByteExact");      // redirected
+            typePairToName.put(new TypePairs(short.class,  char.class),   "isIntToCharExact");      // redirected
+            typePairToName.put(new TypePairs(char.class,   byte.class),   "isIntToByteExact");      // redirected
+            typePairToName.put(new TypePairs(char.class,   short.class),  "isIntToShortExact");     // redirected
             typePairToName.put(new TypePairs(int.class,    byte.class),   "isIntToByteExact");
             typePairToName.put(new TypePairs(int.class,    short.class),  "isIntToShortExact");
             typePairToName.put(new TypePairs(int.class,    char.class),   "isIntToCharExact");
@@ -725,7 +730,7 @@ public class SwitchBootstraps {
             typePairToName.put(new TypePairs(double.class, int.class),    "isDoubleToIntExact");
             typePairToName.put(new TypePairs(double.class, long.class),   "isDoubleToLongExact");
             typePairToName.put(new TypePairs(double.class, float.class),  "isDoubleToFloatExact");
-            return Map.copyOf(typePairToName);
+            return typePairToName;
         }
     }
 }
