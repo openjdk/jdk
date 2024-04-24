1d06170758bd76a0ea32e5bb7d4a017e829ae710
Joshua Cao <joshcao@amazon.com>
2024-04-24 17:06:45 +0000

    8300148: Consider using a StoreStore barrier instead of Release barrier on ctor exit
    Reviewed-by: shade, kvn, dlong

--- src/hotspot/share/opto/memnode.cpp
+++ src/hotspot/share/opto/memnode.cpp
@@ -4125,7 +4125,7 @@ Node *MemBarNode::Ideal(PhaseGVN *phase, bool can_reshape) {
           eliminate = true;
         }
       }
-    } else if (opc == Op_MemBarRelease) {
+    } else if (opc == Op_MemBarRelease || (UseStoreStoreForCtor && opc == Op_MemBarStoreStore)) {
       // Final field stores.
       Node* alloc = AllocateNode::Ideal_allocation(in(MemBarNode::Precedent));
       if ((alloc != nullptr) && alloc->is_Allocate() &&
