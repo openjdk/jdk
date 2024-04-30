eb78413e7e4b4e72f478f1d7c88e07fa92a03f42
Thomas Schatzl <tschatzl@openjdk.org>
2024-04-30 10:35:52 +0200

    8331392
    Use different descriptions for different reclamation reasons

--- src/hotspot/share/gc/g1/g1ConcurrentMark.cpp
+++ src/hotspot/share/gc/g1/g1ConcurrentMark.cpp
@@ -1317,7 +1317,7 @@ public:
     if (!_cleanup_list.is_empty()) {
       log_debug(gc)("Reclaimed %u empty regions", _cleanup_list.length());
       // Now print the empty regions list.
-      _g1h->hr_printer()->cleanup(&_cleanup_list);
+      _g1h->hr_printer()->mark_reclaim(&_cleanup_list);
       // And actually make them available.
       _g1h->prepend_to_freelist(&_cleanup_list);
     }
