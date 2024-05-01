d1ebb1fe50b076e6d9fb086d8278ae1fa2340351
Nizar Benalla <nizar.benalla@oracle.com>
2024-05-01 01:25:45 +0100

    When Type-Import-on-Demand fails, log the position of the first PackageOrTypeName

--- test/langtools/jdk/jshell/ImportTest.java
+++ test/langtools/jdk/jshell/ImportTest.java
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8141415
+ * @bug 8141415 8129418
  * @summary Test imports
  * @modules jdk.compiler/com.sun.tools.javac.api
  *          jdk.compiler/com.sun.tools.javac.main
@@ -76,12 +76,11 @@ public class ImportTest extends KullaTesting {
         assertEval("abs(cos(PI / 2)) < 0.00001;", "true");
     }
 
-    @Test(enabled = false) // TODO 8129418
     public void testUnknownPackage() {
         assertDeclareFail("import unknown.qqq;",
                 new ExpectedDiagnostic("compiler.err.doesnt.exist", 7, 18, 14, -1, -1, Diagnostic.Kind.ERROR));
         assertDeclareFail("import unknown.*;",
-                new ExpectedDiagnostic("compiler.err.doesnt.exist", 7, 15, 7, -1, -1, Diagnostic.Kind.ERROR));
+                new ExpectedDiagnostic("compiler.err.doesnt.exist", 7, 7, 7, -1, -1, Diagnostic.Kind.ERROR));
     }
 
     public void testBogusImportIgnoredInFuture() {
