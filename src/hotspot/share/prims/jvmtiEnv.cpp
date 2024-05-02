b19e83b0bb1146e0911fb02221d6366567fd821f
Alex Menkov <69548902+alexmenkov@users.noreply.github.com>
2024-05-02 16:58:02 -0700

    Revert "renamed current_thread tp current"
    This reverts commit 4602632221044aa754a1bc8d11e7a3e9a0092590.

--- src/hotspot/share/prims/jvmtiEnv.cpp
+++ src/hotspot/share/prims/jvmtiEnv.cpp
@@ -197,7 +197,7 @@ JvmtiEnv::GetThreadLocalStorage(jthread thread, void** data_ptr) {
     // other than the current thread is required we need to transition
     // from native so as to resolve the jthread.
 
-    MACOS_AARCH64_ONLY(ThreadWXEnable __wx(WXWrite, current));
+    MACOS_AARCH64_ONLY(ThreadWXEnable __wx(WXWrite, current_thread));
     ThreadInVMfromNative __tiv(current);
     VM_ENTRY_BASE(jvmtiError, JvmtiEnv::GetThreadLocalStorage , current)
     debug_only(VMNativeEntryWrapper __vew;)
