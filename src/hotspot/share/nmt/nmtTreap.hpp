8d94096852b84d287343a16777c3518eb9d92704
Johan Sjölén <johan.sjolen@oracle.com>
2024-05-03 14:00:16 +0200

    Move out

--- src/hotspot/share/nmt/nmtTreap.hpp
+++ src/hotspot/share/nmt/nmtTreap.hpp
@@ -167,7 +167,37 @@ private:
     }
   }
 
-  bool verify_self();
+  bool verify_self() {
+    double expected_maximum_depth = log(this->_node_count) * 3;
+    // Find the maximum depth through DFS.
+    int maximum_depth_found = 0;
+
+    struct DFS {
+      int depth;
+      uint64_t parent_prio;
+      TreapNode* n;
+    };
+    GrowableArrayCHeap<TreapNode*, mtNMT> to_visit;
+    uint64_t positive_infinity = 0xFFFFFFFFFFFFFFFF;
+
+    to_visit.push({0, positive_infinity, this->_root});
+    while (!to_visit.is_empty()) {
+      DFS head = to_visit.pop();
+      if (head.n == nullptr) continue;
+      if (maximum_depth_found < head.depth) {
+        maximum_depth_found = head.depth;
+      }
+      if (head.parent_prio < head.n->_priority) {
+        return false;
+      }
+      to_visit.push({head.depth + 1, head.n->_priority, head.n->left()});
+      to_visit.push({head.depth + 1, head.n->_priority, head.n->right()});
+    }
+    if (maximum_depth_found > (int)expected_maximum_depth) {
+      return false;
+    }
+    return true;
+  }
 
 public:
   Treap(uint64_t seed = static_cast<uint64_t>(os::random()))
