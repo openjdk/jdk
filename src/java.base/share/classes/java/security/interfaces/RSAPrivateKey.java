48fc2b9bf0d5c3820770b76f6df0c14efd1fa488
Nizar Benalla <nizar.benalla@oracle.com>
2024-05-06 16:19:35 +0100

    Brought back changes to security interfaces after discussion

--- src/java.base/share/classes/java/security/interfaces/RSAPrivateKey.java
+++ src/java.base/share/classes/java/security/interfaces/RSAPrivateKey.java
@@ -68,7 +68,7 @@ public interface RSAPrivateKey extends java.security.PrivateKey, RSAKey
      * The default implementation returns {@code null}.
      *
      * @return {@inheritDoc java.security.AsymmetricKey}
-     * @since 22
+     * @since 11
      */
     @Override
     default AlgorithmParameterSpec getParams() {
