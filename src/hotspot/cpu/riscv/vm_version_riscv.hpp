1aebab780c5b84a85b6f10884d05bb29bae3c3bf
Hamlin Li <mli@openjdk.org>
2024-05-08 09:37:42 +0000

    8320995: RISC-V: C2 PopCountVI 8320996: RISC-V: C2 PopCountVL
    Reviewed-by: luhenry, fyang

--- src/hotspot/cpu/riscv/vm_version_riscv.hpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.hpp
@@ -161,6 +161,7 @@ class VM_Version : public Abstract_VM_Version {
   decl(ext_Ztso        , "Ztso"        , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZtso))        \
   decl(ext_Zihintpause , "Zihintpause" , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZihintpause)) \
   decl(ext_Zacas       , "Zacas"       , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZacas))       \
+  decl(ext_Zvbb        , "Zvbb"        , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZvbb))        \
   decl(ext_Zvfh        , "Zvfh"        , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZvfh))        \
   decl(ext_Zvkn        , "Zvkn"        , RV_NO_FLAG_BIT, true , UPDATE_DEFAULT(UseZvkn))        \
   decl(mvendorid       , "VendorId"    , RV_NO_FLAG_BIT, false, NO_UPDATE_DEFAULT)              \
