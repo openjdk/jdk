f6ba2e31fbf64da00ad96c82f92a6600d572b9b6
Serhiy Sachkov <serhiy.sachkov@oracle.com>
2024-05-09 10:06:49 +0100

    JDK-8331646 updating impl according to review requests

--- test/jdk/java/util/Calendar/CalendarLeapYearAddTest.java
+++ test/jdk/java/util/Calendar/CalendarLeapYearAddTest.java
@@ -34,11 +34,22 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.Locale;
 import java.util.stream.Stream;
 
-import static java.util.Calendar.*;
+import static java.util.Calendar.APRIL;
+import static java.util.Calendar.DATE;
+import static java.util.Calendar.DAY_OF_MONTH;
+import static java.util.Calendar.DAY_OF_WEEK;
+import static java.util.Calendar.DAY_OF_WEEK_IN_MONTH;
+import static java.util.Calendar.DAY_OF_YEAR;
+import static java.util.Calendar.FEBRUARY;
+import static java.util.Calendar.LONG;
+import static java.util.Calendar.MARCH;
 import static java.util.Calendar.MONTH;
+import static java.util.Calendar.WEEK_OF_YEAR;
+import static java.util.Calendar.YEAR;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalendarLeapYearAddTest {
@@ -51,8 +62,7 @@ public class CalendarLeapYearAddTest {
     public void testAddLeapYear(String testName, int calendarDate, int calendarMonth, int calendarYear,
                                 int value, int calendarField, int expectedDate, int expectedMonth,
                                 int expectedYear) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendarYear, calendarMonth, calendarDate);
+        Calendar calendar = new GregorianCalendar(calendarYear, calendarMonth, calendarDate);
         calendar.add(calendarField, value);
         assertEquals(expectedDate, calendar.get(DATE), testName
                 + " Expected " + expectedDate + " of " + expectedMonth + expectedYear + " but got " + calendar.getTime());
@@ -65,11 +75,10 @@ public class CalendarLeapYearAddTest {
      */
     @Test
     public void testMonthYearAddSubtractNonLeapYear() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(2024, 1, 29, 15, 0, 0);
-        calendar.add(Calendar.MONTH, 1);
+        Calendar calendar = new GregorianCalendar(2024, FEBRUARY, 29);
+        calendar.add(MONTH, 1);
         calendar.add(YEAR, -1);
-        calendar.add(Calendar.MONTH, -1);
+        calendar.add(MONTH, -1);
         /* When month added date jumps to 29th of March 2024, after year subtracted date jumps to 29th of March 2023
            after month subtracted date jumps to 28th of Feb 2023 as non leap year
          */
@@ -87,8 +96,7 @@ public class CalendarLeapYearAddTest {
     public void testAddSubtractLeapYear(String testName, int calendarDate, int calendarMonth, int calendarYear,
                                         int firstValue, int secondValue, int calendarField, int expectedDate,
                                         int expectedMonth, int expectedYear) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(calendarYear, calendarMonth, calendarDate);
+        Calendar calendar = new GregorianCalendar(calendarYear, calendarMonth, calendarDate);
         calendar.add(calendarField, firstValue);
         calendar.add(calendarField, secondValue);
         assertEquals(expectedDate, calendar.get(DATE), testName
@@ -99,23 +107,23 @@ public class CalendarLeapYearAddTest {
 
     private static Stream<Arguments> calendarAddSubtractSource() {
         return Stream.of(
-                Arguments.of("testMonthAddSubtractLeapYearReversed", 31, 2, 2024, -1, 1, MONTH, 29, MARCH, 2024),
-                Arguments.of("testMonthAddSubtractLeapYear", 29, 1, 2024, 1, -1, MONTH, 29, FEBRUARY, 2024),
-                Arguments.of("testYearAddSubtractLeapYear", 29, 1, 2024, 1, -1, YEAR, 28, FEBRUARY, 2024),
-                Arguments.of("testDayOfYearAddSubtractLeapYear", 29, 1, 2024, 365, -365, DAY_OF_YEAR, 29, FEBRUARY, 2024),
-                Arguments.of("testDateAddSubtractLeapYear", 29, 1, 2024, 365, -365, DATE, 29, FEBRUARY, 2024),
-                Arguments.of("testWeekOfYearAddSubtractLeapYear", 29, 1, 2024, 52, -52, WEEK_OF_YEAR, 29, FEBRUARY, 2024),
-                Arguments.of("testDateOfMonthAddSubtractLeapYear", 29, 1, 2024, 31, -31, DAY_OF_MONTH, 29, FEBRUARY, 2024),
-                Arguments.of("testDayOfWeekInMonthAddSubtractLeapYear", 29, 1, 2024, 6, -6, DAY_OF_WEEK_IN_MONTH, 29, FEBRUARY, 2024),
-                Arguments.of("testDayOfWeekAddSubtractLeapYear", 29, 1, 2024, 6, -6, DAY_OF_WEEK, 29, FEBRUARY, 2024)
+                Arguments.of("testMonthAddSubtractLeapYearReversed", 31, MARCH, 2024, -1, 1, MONTH, 29, MARCH, 2024),
+                Arguments.of("testMonthAddSubtractLeapYear", 29, FEBRUARY, 2024, 1, -1, MONTH, 29, FEBRUARY, 2024),
+                Arguments.of("testYearAddSubtractLeapYear", 29, FEBRUARY, 2024, 1, -1, YEAR, 28, FEBRUARY, 2024),
+                Arguments.of("testDayOfYearAddSubtractLeapYear", 29, FEBRUARY, 2024, 365, -365, DAY_OF_YEAR, 29, FEBRUARY, 2024),
+                Arguments.of("testDateAddSubtractLeapYear", 29, FEBRUARY, 2024, 365, -365, DATE, 29, FEBRUARY, 2024),
+                Arguments.of("testWeekOfYearAddSubtractLeapYear", 29, FEBRUARY, 2024, 52, -52, WEEK_OF_YEAR, 29, FEBRUARY, 2024),
+                Arguments.of("testDateOfMonthAddSubtractLeapYear", 29, FEBRUARY, 2024, 31, -31, DAY_OF_MONTH, 29, FEBRUARY, 2024),
+                Arguments.of("testDayOfWeekInMonthAddSubtractLeapYear", 29, FEBRUARY, 2024, 6, -6, DAY_OF_WEEK_IN_MONTH, 29, FEBRUARY, 2024),
+                Arguments.of("testDayOfWeekAddSubtractLeapYear", 29, FEBRUARY, 2024, 6, -6, DAY_OF_WEEK, 29, FEBRUARY, 2024)
         );
     }
 
     private static Stream<Arguments> calendarAddSource() {
         return Stream.of(
-                Arguments.of("testMonthAddLeapYear", 29, 1, 2024, 1, MONTH, 29, MARCH, 2024),
-                Arguments.of("testOneMonthSubtractLeapYear", 31, 2, 2024, -1, MONTH, 29, FEBRUARY, 2024),
-                Arguments.of("testTwoMonthSubtractLeapYear", 30, 3, 2024, -2, MONTH, 29, FEBRUARY, 2024)
+                Arguments.of("testMonthAddLeapYear", 29, FEBRUARY, 2024, 1, MONTH, 29, MARCH, 2024),
+                Arguments.of("testOneMonthSubtractLeapYear", 31, MARCH, 2024, -1, MONTH, 29, FEBRUARY, 2024),
+                Arguments.of("testTwoMonthSubtractLeapYear", 30, APRIL, 2024, -2, MONTH, 29, FEBRUARY, 2024)
         );
     }
 }
