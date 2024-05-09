9c38914afe1bf5ed2408bc2802a40a2ada8fe2c7
Hamlin-Li <leeming1509@outlook.com>
2024-05-09 08:35:57 +0000

    typo

--- src/hotspot/cpu/riscv/assembler_riscv.hpp
+++ src/hotspot/cpu/riscv/assembler_riscv.hpp
@@ -1892,7 +1892,7 @@ enum Nf {
   INSN(vrev8_v,  0b1010111, 0b010, 0b01001, 0b010010); // reverse bytes in every elememt
 
   INSN(vclz_v,  0b1010111, 0b010, 0b01100, 0b010010); // count leading zeros
-  INSN(vctz_v,  0b1010111, 0b010, 0b01101, 0b010010); // ount trailing zeros
+  INSN(vctz_v,  0b1010111, 0b010, 0b01101, 0b010010); // count trailing zeros
 
 #undef INSN
 
