c8d04635d4fe1b03e51b6bcea9b73fd92bec711b
Nizar Benalla <nizar.benalla@oracle.com>
2024-05-10 02:57:55 +0100

    add `@since` tags

--- src/jdk.httpserver/share/classes/com/sun/net/httpserver/spi/package-info.java
+++ src/jdk.httpserver/share/classes/com/sun/net/httpserver/spi/package-info.java
@@ -26,5 +26,6 @@
 /**
  * Provides a pluggable service provider interface, which allows the HTTP server
  * implementation to be replaced with other implementations.
+ * @since 1.6
  */
 package com.sun.net.httpserver.spi;
