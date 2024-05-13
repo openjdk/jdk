e85a1b478a85f798431213e5283e8e75048b5c4b
Kevin Driver <kevin.driver@oracle.com>
2024-05-13 18:06:10 -0500

    javadoc improvements

--- src/java.base/share/classes/javax/crypto/KDF.java
+++ src/java.base/share/classes/javax/crypto/KDF.java
@@ -47,9 +47,9 @@ import java.util.Objects;
  * {@code KDF} objects are instantiated with the {@code getInstance} family of
  * methods. KDF algorithm names follow a naming convention of
  * <em>Algorithm</em>With<em>PRF</em>. For instance, a KDF implementation of
- * HKDF using HMAC-SHA256 has an algorithm name of "HKDFWithHmacSHA256".
- * In some cases the PRF portion of the algorithm field may be omitted if
- * the KDF algorithm has a fixed or default PRF.
+ * HKDF using HMAC-SHA256 has an algorithm name of "HKDFWithHmacSHA256". In some
+ * cases the PRF portion of the algorithm field may be omitted if the KDF
+ * algorithm has a fixed or default PRF.
  * <p>
  * Example:
  * {@snippet lang = java:
@@ -377,7 +377,7 @@ public final class KDF {
     }
 
     /**
-     * Derive a key, returned as a {@code SecretKey}.
+     * Derives a key, returned as a {@code SecretKey}.
      * <p>
      * The {@code deriveKey} method may be called multiple times on a particular
      * {@code KDF} instance.
@@ -395,8 +395,7 @@ public final class KDF {
      *
      * @throws InvalidParameterSpecException
      *     if the information contained within the {@code KDFParameterSpec} is
-     *     invalid or incorrect for the type of key to be derived, or specifies
-     *     a type of output that is not a key (e.g. raw data)
+     *     invalid or incorrect for the type of key to be derived
      * @throws NullPointerException
      *     if {@code alg} is null
      */
@@ -451,7 +450,7 @@ public final class KDF {
     }
 
     /**
-     * Obtain raw data from a key derivation function.
+     * Obtains raw data from a key derivation function.
      * <p>
      * The {@code deriveData} method may be called multiple times on a
      * particular {@code KDF} instance.
@@ -462,9 +461,9 @@ public final class KDF {
      * @param kdfParameterSpec
      *     derivation parameters
      *
-     * @return a byte array whose length matches the length field in the
-     *     processed {@code KDFParameterSpec} and containing the next bytes of
-     *     output from the key derivation function
+     * @return a byte array whose length matches the specified length in the
+     *     processed {@code KDFParameterSpec} and containing the output from the
+     *     key derivation function
      *
      * @throws InvalidParameterSpecException
      *     if the information contained within the {@code KDFParameterSpec} is
