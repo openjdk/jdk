c4c47a0cb7b80bad34caeb47b23d9c7fb222b322
nanxt <nxtchongchong@163.com>
2024-05-14 11:03:08 +0800

    Code in GenFullCP is still disabled after JDK-8079697 was fixed

--- test/hotspot/jtreg/vmTestbase/vm/mlvm/cp/share/GenFullCP.java
+++ test/hotspot/jtreg/vmTestbase/vm/mlvm/cp/share/GenFullCP.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -237,8 +237,7 @@ public abstract class GenFullCP extends ClassfileGenerator {
     @Override
     public Klass[] generateBytecodes() {
 
-        // COMPUTE_FRAMES were disabled due to JDK-8079697
-        ClassWriterExt cw = new ClassWriterExt(/*ClassWriter.COMPUTE_FRAMES |*/ ClassWriter.COMPUTE_MAXS);
+        ClassWriterExt cw = new ClassWriterExt(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
 
         String[] interfaces = new String[1];
         interfaces[0] = getDummyInterfaceClassName();
