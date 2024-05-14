9dacde8bc15a05f706041d11160ce4344d102766
Jan Lahoda <jan.lahoda@oracle.com>
2024-05-14 13:20:33 +0200

    Enhancing test as suggested.

--- test/langtools/tools/javac/patterns/UninitializedThisException.java
+++ test/langtools/tools/javac/patterns/UninitializedThisException.java
@@ -35,17 +35,16 @@ import java.io.StringWriter;
 import java.util.Objects;
 import java.util.function.Supplier;
 
-public class UninitializedThisException {
+public class UninitializedThisException extends Base {
 
     public UninitializedThisException(String s1, String s2) {
-        Objects.requireNonNull(s1);
-        Objects.requireNonNull(s2);
+        super(s1, s2);
     }
 
     public UninitializedThisException(R o1, R o2, R o3) {
         out.println("-pre(" + o1.fail() + ")" +
-                           "-nest(" + o2.fail() + ")" +
-                           "-post(" + o3.fail() + ")");
+                    "-nest(" + o2.fail() + ")" +
+                    "-post(" + o3.fail() + ")");
         String val1 = o1 instanceof R(String s, _) ? s : null;
         out.println("check1");
         this(val1, o2 instanceof R(String s, _) ? s : null);
@@ -57,7 +56,7 @@ public class UninitializedThisException {
 
     public UninitializedThisException(String o1, R o2, R o3) {
         out.println("-nest(" + o2.fail() + ")" +
-                           "-post(" + o3.fail() + ")");
+                    "-post(" + o3.fail() + ")");
         String val1 = o1;
         out.println("check1");
         this(val1, o2 instanceof R(String s, _) ? s : null);
@@ -69,7 +68,7 @@ public class UninitializedThisException {
 
     public UninitializedThisException(R o1, String o2, R o3) {
         out.println("-pre(" + o1.fail() + ")" +
-                           "-post(" + o3.fail() + ")");
+                    "-post(" + o3.fail() + ")");
         String val1 = o1 instanceof R(String s, _) ? s : null;
         out.println("check1");
         this(val1, o2);
@@ -81,7 +80,7 @@ public class UninitializedThisException {
 
     public UninitializedThisException(R o1, R o2, String o3) {
         out.println("-pre(" + o1.fail() + ")" +
-                           "-nest(" + o2.fail() + ")");
+                    "-nest(" + o2.fail() + ")");
         String val1 = o1 instanceof R(String s, _) ? s : null;
         out.println("check1");
         this(val1, o2 instanceof R(String s, _) ? s : null);
@@ -118,6 +117,95 @@ public class UninitializedThisException {
         String val1 = o1;
         out.println("check1");
         this(val1, o2);
+        out.println("check2");
+        String val2 = o3 instanceof R(String s, _) ? s : null;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(R o1, R o2, R o3, boolean superMarker) {
+        out.println("-pre(" + o1.fail() + ")" +
+                    "-nest(" + o2.fail() + ")" +
+                    "-post(" + o3.fail() + ")" +
+                    "-super");
+        String val1 = o1 instanceof R(String s, _) ? s : null;
+        out.println("check1");
+        super(val1, o2 instanceof R(String s, _) ? s : null);
+        out.println("check2");
+        String val2 = o3 instanceof R(String s, _) ? s : null;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(String o1, R o2, R o3, boolean superMarker) {
+        out.println("-nest(" + o2.fail() + ")" +
+                    "-post(" + o3.fail() + ")" +
+                    "-super");
+        String val1 = o1;
+        out.println("check1");
+        super(val1, o2 instanceof R(String s, _) ? s : null);
+        out.println("check2");
+        String val2 = o3 instanceof R(String s, _) ? s : null;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(R o1, String o2, R o3, boolean superMarker) {
+        out.println("-pre(" + o1.fail() + ")" +
+                    "-post(" + o3.fail() + ")" +
+                    "-super");
+        String val1 = o1 instanceof R(String s, _) ? s : null;
+        out.println("check1");
+        super(val1, o2);
+        out.println("check2");
+        String val2 = o3 instanceof R(String s, _) ? s : null;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(R o1, R o2, String o3, boolean superMarker) {
+        out.println("-pre(" + o1.fail() + ")" +
+                    "-nest(" + o2.fail() + ")" +
+                    "-super");
+        String val1 = o1 instanceof R(String s, _) ? s : null;
+        out.println("check1");
+        super(val1, o2 instanceof R(String s, _) ? s : null);
+        out.println("check2");
+        String val2 = o3;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(R o1, String o2, String o3, boolean superMarker) {
+        out.println("-pre(" + o1.fail() + ")" +
+                    "-super");
+        String val1 = o1 instanceof R(String s, _) ? s : null;
+        out.println("check1");
+        super(val1, o2);
+        out.println("check2");
+        String val2 = o3;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(String o1, R o2, String o3, boolean superMarker) {
+        out.println("-nest(" + o2.fail() + ")" +
+                    "-super");
+        String val1 = o1;
+        out.println("check1");
+        super(val1, o2 instanceof R(String s, _) ? s : null);
+        out.println("check2");
+        String val2 = o3;
+        out.println("check3");
+        Objects.requireNonNull(val2);
+    }
+
+    public UninitializedThisException(String o1, String o2, R o3, boolean superMarker) {
+        out.println("-post(" + o3.fail() + ")" +
+                    "-super");
+        String val1 = o1;
+        out.println("check1");
+        super(val1, o2);
         out.println("check2");
         String val2 = o3 instanceof R(String s, _) ? s : null;
         out.println("check3");
@@ -163,71 +251,111 @@ public class UninitializedThisException {
         runAndCatch(() -> new UninitializedThisException("", "", new R("", true)));
         new UninitializedThisException("", "", new R("", false));
 
-        String actualLog = log.toString().replaceAll("\\R", "\n");
+        runAndCatch(() -> new UninitializedThisException(new R("", true), new R("", false), new R("", false), true));
+        runAndCatch(() -> new UninitializedThisException(new R("", false), new R("", true), new R("", false), true));
+        runAndCatch(() -> new UninitializedThisException(new R("", false), new R("", false), new R("", true), true));
+        new UninitializedThisException(new R("", false), new R("", false), new R("", false), true);
 
-        if (!Objects.equals(actualLog, EXPECTED_LOG)) {
-            throw new AssertionError("Expected log:\n" + EXPECTED_LOG +
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException("", new R("", true), new R("", false), true));
+        runAndCatch(() -> new UninitializedThisException("", new R("", false), new R("", true), true));
+        new UninitializedThisException("", new R("", false), new R("", false), true);
+
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException(new R("", true), "", new R("", false), true));
+        runAndCatch(() -> new UninitializedThisException(new R("", false), "", new R("", true), true));
+        new UninitializedThisException(new R("", false), "", new R("", false), true);
+
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException(new R("", true), new R("", false), "", true));
+        runAndCatch(() -> new UninitializedThisException(new R("", false), new R("", true), "", true));
+        new UninitializedThisException(new R("", false), new R("", false), "", true);
+
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException(new R("", true), "", "", true));
+        new UninitializedThisException(new R("", false), "", "", true);
+
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException("", new R("", true), "", true));
+        new UninitializedThisException("", new R("", false), "", true);
+
+        out.println();
+
+        runAndCatch(() -> new UninitializedThisException("", "", new R("", true), true));
+        new UninitializedThisException("", "", new R("", false), true);
+
+        String actualLog = log.toString().replaceAll("\\R", "\n");
+        String expectedLog = EXPECTED_LOG_PATTERN.replace("${super}", "") +
+                             EXPECTED_LOG_PATTERN.replace("${super}", "-super");
+
+        if (!Objects.equals(actualLog, expectedLog)) {
+            throw new AssertionError("Expected log:\n" + expectedLog +
                                      ", but got: " + actualLog);
         }
     }
 
-    static final String EXPECTED_LOG =
+    static final String EXPECTED_LOG_PATTERN =
             """
-            -pre(true)-nest(false)-post(false)
-            -pre(false)-nest(true)-post(false)
+            -pre(true)-nest(false)-post(false)${super}
+            -pre(false)-nest(true)-post(false)${super}
             check1
-            -pre(false)-nest(false)-post(true)
-            check1
-            check2
-            -pre(false)-nest(false)-post(false)
+            -pre(false)-nest(false)-post(true)${super}
             check1
             check2
-            check3
-
-            -nest(true)-post(false)
-            check1
-            -nest(false)-post(true)
-            check1
-            check2
-            -nest(false)-post(false)
+            -pre(false)-nest(false)-post(false)${super}
             check1
             check2
             check3
 
-            -pre(true)-post(false)
-            -pre(false)-post(true)
+            -nest(true)-post(false)${super}
+            check1
+            -nest(false)-post(true)${super}
             check1
             check2
-            -pre(false)-post(false)
-            check1
-            check2
-            check3
-
-            -pre(true)-nest(false)
-            -pre(false)-nest(true)
-            check1
-            -pre(false)-nest(false)
+            -nest(false)-post(false)${super}
             check1
             check2
             check3
 
-            -pre(true)
-            -pre(false)
+            -pre(true)-post(false)${super}
+            -pre(false)-post(true)${super}
+            check1
+            check2
+            -pre(false)-post(false)${super}
             check1
             check2
             check3
 
-            -nest(true)
+            -pre(true)-nest(false)${super}
+            -pre(false)-nest(true)${super}
             check1
-            -nest(false)
+            -pre(false)-nest(false)${super}
             check1
             check2
             check3
 
-            -post(true)
+            -pre(true)${super}
+            -pre(false)${super}
             check1
             check2
-            -post(false)
+            check3
+
+            -nest(true)${super}
+            check1
+            -nest(false)${super}
+            check1
+            check2
+            check3
+
+            -post(true)${super}
+            check1
+            check2
+            -post(false)${super}
             check1
             check2
             check3
@@ -252,5 +380,11 @@ public class UninitializedThisException {
                 return s;
             }
         }
+    }
+}
+class Base {
+    public Base(String s1, String s2) {
+        Objects.requireNonNull(s1);
+        Objects.requireNonNull(s2);
     }
 }
