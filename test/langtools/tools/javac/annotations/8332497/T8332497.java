7b554e0f716a06c5fb0ccfc5b8d64edf0d7c809f
Evemose <rotan.olexandr@lll.kpi.ua>
2024-05-20 11:05:45 +0300

    Added --enable-preview param

--- test/langtools/tools/javac/annotations/8332497/T8332497.java
+++ test/langtools/tools/javac/annotations/8332497/T8332497.java
@@ -1,8 +1,13 @@
 import toolbox.*;
 
-import javax.annotation.processing.Processor;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Set;
 
 /**
  * @test
@@ -16,11 +21,14 @@ import java.nio.file.Paths;
  */
 public class T8332497 extends TestRunner {
     final toolbox.ToolBox tb = new ToolBox();
-    final String processorName = "AP";
     final Path base = Paths.get(".");
     final String processedSource = """
             import module java.base;
             import java.util.List;
+            import java.lang.annotation.ElementType;
+            import java.lang.annotation.Retention;
+            import java.lang.annotation.RetentionPolicy;
+            import java.lang.annotation.Target;
             public class Main {
               public static void main(String[] args) {
                 List.of();
@@ -29,38 +37,10 @@ public class T8332497 extends TestRunner {
               private void test() {
                 List.of();
               }
-            }
-            """;
-
-    final String annotationSource = """
-            import java.lang.annotation.ElementType;
-            import java.lang.annotation.Retention;
-            import java.lang.annotation.RetentionPolicy;
-            import java.lang.annotation.Target;
-            @Retention(RetentionPolicy.RUNTIME)
-            @Target(ElementType.METHOD)
-            public @interface Ann {
-            }
-            """;
-
-    final String processorSource = """
-            import module java.base;
-            import javax.annotation.processing.AbstractProcessor;
-            import javax.annotation.processing.RoundEnvironment;
-            import javax.annotation.processing.SupportedAnnotationTypes;
-            import javax.annotation.processing.SupportedSourceVersion;
-            import javax.lang.model.element.TypeElement;
-            import javax.lang.model.SourceVersion;
-            @SupportedAnnotationTypes("*")
-            public class AP extends AbstractProcessor {
-                @Override
-                public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-                    return false;
-                }
-                @Override
-                public SourceVersion getSupportedSourceVersion() {
-                    return SourceVersion.latestSupported();
-                }
+              @Retention(RetentionPolicy.RUNTIME)
+              @Target(ElementType.METHOD)
+              public @interface Ann {
+              }
             }
             """;
 
@@ -74,14 +54,11 @@ public class T8332497 extends TestRunner {
     }
 
     public void test() throws Exception {
-        var procJarPath = createProcessorJarFile();
         tb.writeJavaFiles(base, processedSource);
-        tb.writeJavaFiles(base, annotationSource);
-
         new toolbox.JavacTask(tb)
-                .classpath(procJarPath)
                 .options(
-                        "-processorpath", procJarPath.toString()
+                        "-processor", AP.class.getName(),
+                        "--enable-preview"
                 )
                 .outdir(base.toString())
                 .files(base.resolve("Main.java"), base.resolve("Ann.java"))
@@ -90,29 +67,18 @@ public class T8332497 extends TestRunner {
                 .getOutputLines(Task.OutputKind.DIRECT);
     }
 
-    private Path createProcessorJarFile() throws Exception {
-        var apDir = base;
+    @SupportedAnnotationTypes("*")
+    public static final class AP extends AbstractProcessor {
 
-        toolbox.JarTask jarTask = new toolbox.JarTask(tb, processorName + ".jar");
+        @Override
+        public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+            return false;
+        }
 
-        // write out META-INF/services file for the processor
-        var servicesFile =
-                apDir.resolve("META-INF")
-                        .resolve("services")
-                        .resolve(Processor.class.getCanonicalName());
-        tb.writeFile(servicesFile, processorName);
+        @Override
+        public SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.latest();
+        }
 
-        // write out processor source file
-        tb.writeJavaFiles(apDir, processorSource);
-
-        // Compile the processor
-        new toolbox.JavacTask(tb)
-                .files(processorName + ".java")
-                .run(Task.Expect.SUCCESS)
-                .writeAll();
-
-        // Create jar file
-        jarTask.files(servicesFile.toString(), apDir.resolve(processorName + ".class").toString()).run();
-        return base.resolve(processorName + ".jar");
     }
 }
