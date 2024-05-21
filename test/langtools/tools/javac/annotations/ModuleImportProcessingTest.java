a38583afb66f685c78c88809ae6f5185e6e62240
Evemose <rotan.olexandr@lll.kpi.ua>
2024-05-21 12:45:29 +0300

    Added legal header and renamed test

--- test/langtools/tools/javac/annotations/ModuleImportProcessingTest.java
+++ test/langtools/tools/javac/annotations/ModuleImportProcessingTest.java
@@ -1,3 +1,29 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+
+package tools.javac.annotations;
+
 import toolbox.*;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -16,10 +42,10 @@ import java.util.Set;
  * @library /tools/lib
  * @modules jdk.compiler/com.sun.tools.javac.api
  * jdk.compiler/com.sun.tools.javac.main
- * @build toolbox.JavacTask toolbox.TestRunner toolbox.ToolBox toolbox.Assert toolbox.Task
- * @run main T8332497
+ * @build toolbox.JavacTask toolbox.ToolBox toolbox.Assert toolbox.Task
+ * @run main tools.javac.annotations.ModuleImportProcessingTest
  */
-public class T8332497 extends TestRunner {
+public class ModuleImportProcessingTest {
     final toolbox.ToolBox tb = new ToolBox();
     final Path base = Paths.get(".");
     final String processedSource = """
@@ -44,12 +70,8 @@ public class T8332497 extends TestRunner {
             }
             """;
 
-    public T8332497() {
-        super(System.err);
-    }
-
     public static void main(String[] args) throws Exception {
-        var t = new T8332497();
+        var t = new ModuleImportProcessingTest();
         t.test();
     }
 
