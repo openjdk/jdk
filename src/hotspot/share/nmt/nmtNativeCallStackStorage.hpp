549a9393f2a9a4b4f8334970aa3a67d486d05bf1
Johan Sjölén <johan.sjolen@oracle.com>
2024-05-21 14:40:24 +0200

    Fix copyright

--- src/hotspot/share/nmt/nmtNativeCallStackStorage.hpp
+++ src/hotspot/share/nmt/nmtNativeCallStackStorage.hpp
@@ -10,7 +10,7 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
  * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).    EXPECT_EQ(dev->_summary.by_type(mtTest)->reserved(), 0);
+ * accompanied this code).
  *
  * You should have received a copy of the GNU General Public License version
  * 2 along with this work; if not, write to the Free Software Foundation,
