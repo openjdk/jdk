db1d400eec56d01243f1be8c1f71aac9467ec217
Vicente Romero <vicente.romero@oracle.com>
2024-05-23 18:46:54 -0400

    more review comments

--- src/java.base/share/classes/java/lang/reflect/Executable.java
+++ src/java.base/share/classes/java/lang/reflect/Executable.java
@@ -255,9 +255,9 @@ public abstract sealed class Executable extends AccessibleObject
      * represented by this object.  Returns an array of length
      * 0 if the underlying executable takes no parameters.
      * Note that the constructors of some inner classes
-     * may have an implicitly ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) declared parameter in addition to
+     * may have an ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) parameter in addition to
      * explicitly declared ones.  Also note that compact constructors
-     * of a record class may have implicitly ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) declared parameters.
+     * of a record class may have ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) parameters.
      *
      * @return the parameter types for the executable this object
      * represents
@@ -294,7 +294,7 @@ public abstract sealed class Executable extends AccessibleObject
      * of a record class will be returned.
      * Note that parameters of compact constructors of a record class are a special case,
      * as they are not explicitly present in the source, and its type will be returned
-     * regardless of the parameters being ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) or not.
+     * regardless of the parameters being ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) or not.
      * If generic information is not present, implicit and synthetic parameters may be
      * returned as well.
      *
@@ -302,7 +302,7 @@ public abstract sealed class Executable extends AccessibleObject
      * the {@code Type} object returned for it must accurately reflect
      * the actual type arguments used in the source code. This assertion also
      * applies to the parameters of compact constructors of a record class, independently
-     * of them being ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) or not.
+     * of them being ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) or not.
      *
      * <p>If a formal parameter type is a type variable or a parameterized
      * type, it is created. Otherwise, it is resolved.
@@ -772,9 +772,9 @@ public abstract sealed class Executable extends AccessibleObject
      * Returns an array of length 0 if the method/constructor declares no
      * parameters.
      * Note that the constructors of some inner classes
-     * may have an implicitly ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) declared parameter in addition to
+     * may have an ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) parameter in addition to
      * explicitly declared ones.  Also note that compact constructors of a
-     * record class may have implicitly ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED mandated}) declared parameters.
+     * record class may have ({@linkplain java.compiler/javax.lang.model.util.Elements.Origin#MANDATED implicitly declared}) parameters.
      *
      * @return an array of objects representing the types of the
      * formal parameters of the method or constructor represented by this
