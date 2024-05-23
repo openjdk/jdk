b03ff0c5bc1ee4d53db06386e94f9792a4279618
MBaesken <matthias.baesken@sap.com>
2024-05-23 09:23:03 +0200

    remarks Roger Riggs

--- src/java.base/unix/native/libjava/ProcessImpl_md.c
+++ src/java.base/unix/native/libjava/ProcessImpl_md.c
@@ -559,10 +559,10 @@ spawnChild(JNIEnv *env, jobject process, ChildStuff *c, const char *helperpath) 
     }
     offset = copystrings(buf, 0, &c->argv[0]);
     offset = copystrings(buf, offset, &c->envv[0]);
-    if (c->pdir != NULL) {
+    if (sp.dirlen > 0 && c->pdir != NULL) {
         memcpy(buf+offset, c->pdir, sp.dirlen);
+        offset += sp.dirlen;
     }
-    offset += sp.dirlen;
     offset = copystrings(buf, offset, parentPathv);
     assert(offset == bufsize);
 
