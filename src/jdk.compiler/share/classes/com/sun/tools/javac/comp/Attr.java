0ef59ec9b581bd8e9c63f6a366d1b327d8d74f01
Jan Lahoda <jan.lahoda@oracle.com>
2024-05-23 12:57:01 +0200

    8332230: jshell throws AssertionError when processing annotations

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Attr.java
@@ -5255,6 +5255,8 @@ public class Attr extends JCTree.Visitor {
             default:
                 attribClass(env.tree.pos(), env.enclClass.sym);
         }
+
+        annotate.flush();
     }
 
     public void attribPackage(DiagnosticPosition pos, PackageSymbol p) {
