abe4de70896c37a5f763f821a3800b99411333aa
Jayathirth Rao D V <jayathirth.d.v@oracle.com>
2024-05-24 13:47:36 +0530

    8332866: Crash in ImageIO JPEG decoding when MEM_STATS in enabled

--- src/java.desktop/share/native/libjavajpeg/imageioJPEG.c
+++ src/java.desktop/share/native/libjavajpeg/imageioJPEG.c
@@ -666,8 +666,6 @@ static void imageio_reset(JNIEnv *env,
 static void imageio_dispose(j_common_ptr info) {
 
     if (info != NULL) {
-        free(info->err);
-        info->err = NULL;
         if (info->is_decompressor) {
             j_decompress_ptr dinfo = (j_decompress_ptr) info;
             free(dinfo->src);
@@ -678,6 +676,8 @@ static void imageio_dispose(j_common_ptr info) {
             cinfo->dest = NULL;
         }
         jpeg_destroy(info);
+        free(info->err);
+        info->err = NULL;
         free(info);
     }
 }
