d77d67988cf7ac080a98631d76ea9516067db78f
Leonid Mesnik <leonid.mesnik@gmail.com>
2024-05-28 13:50:51 -0700

    fixed check

--- test/hotspot/jtreg/vmTestbase/nsk/share/jdb/JdbTest.java
+++ test/hotspot/jtreg/vmTestbase/nsk/share/jdb/JdbTest.java
@@ -218,7 +218,7 @@ public abstract class JdbTest {
                     }
                 }
 
-                if (debuggee != null && debuggee.terminated()) {
+                if (debuggee != null && !debuggee.terminated()) {
                     log.complain("debuggee is still running, check for exception in the logs.");
                     debuggee.killDebuggee();
                 }
