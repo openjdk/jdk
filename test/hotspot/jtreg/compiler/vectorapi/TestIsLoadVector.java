fd7aaac6dbcaa98ece350217b376ba90c3a24a47
Damon Fenacci <damon.fenacci@oracle.com>
2024-05-29 11:21:16 +0000

    JDK-8333099: fix indentation

--- test/hotspot/jtreg/compiler/vectorapi/TestIsLoadVector.java
+++ test/hotspot/jtreg/compiler/vectorapi/TestIsLoadVector.java
@@ -31,33 +31,33 @@ package compiler.vectorapi;
  */
 
 public class TestIsLoadVector {
-  static int[] iArrFld = new int[400];
+    static int[] iArrFld = new int[400];
 
-  static void test() {
-      int i13, i16 = 3;
-      short s = 50;
-      for (int i = 0; i < 4; i++) {
-          for (int i12 = 0; i12 < 8; ++i12) {
-              for (int i14 = 2; 82 > i14; i14++) {
-                  s <<= 90;
-                  do {
-                      try {
-                          i13 = 0;
-                      } catch (ArithmeticException a_e) {
-                      }
-                  } while (++i16 < 2);
-              }
-          }
-      }
-      int i18 = 1;
-      while (++i18 < 41) {
-          iArrFld[i18] >>= s;
-      }
-  }
+    static void test() {
+        int i13, i16 = 3;
+        short s = 50;
+        for (int i = 0; i < 4; i++) {
+            for (int i12 = 0; i12 < 8; ++i12) {
+                for (int i14 = 2; 82 > i14; i14++) {
+                    s <<= 90;
+                    do {
+                        try {
+                            i13 = 0;
+                        } catch (ArithmeticException a_e) {
+                        }
+                    } while (++i16 < 2);
+                }
+            }
+        }
+        int i18 = 1;
+        while (++i18 < 41) {
+            iArrFld[i18] >>= s;
+        }
+    }
 
-  public static void main(String[] strArr) {
-      for (int i = 0; i < 100; i++) {
-          test();
-      }
-  }
+    public static void main(String[] strArr) {
+        for (int i = 0; i < 100; i++) {
+            test();
+        }
+    }
 }
