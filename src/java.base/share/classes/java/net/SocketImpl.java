0e3c59f8d0004e2cb5b08849a417af788be486a3
Jaikiran Pai <jaikiran.pai@gmail.com>
2024-05-29 11:17:31 +0530

    Alan's suggestion to add a note for SocketImpl.create(boolean stream)

--- src/java.base/share/classes/java/net/SocketImpl.java
+++ src/java.base/share/classes/java/net/SocketImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -79,6 +79,10 @@ public abstract class SocketImpl implements SocketOptions {
 
     /**
      * Creates either a stream or a datagram socket.
+     *
+     * @apiNote
+     * {@code SocketImpl} should not be used for datagram sockets.
+     * Creating a datagram socket using this method is deprecated.
      *
      * @param      stream   if {@code true}, create a stream socket;
      *                      otherwise, create a datagram socket.
