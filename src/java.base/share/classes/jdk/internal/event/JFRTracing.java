4a20691e9b0276e2dc5e7eb6a4d05393d6b4c99c
Erik Gahlin <egahlin@openjdk.org>
2024-05-30 13:32:57 +0000

    8331876: JFR: Move file read and write events to java.base
    Reviewed-by: mgronlun, alanb

--- src/java.base/share/classes/jdk/internal/event/JFRTracing.java
+++ src/java.base/share/classes/jdk/internal/event/JFRTracing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,35 +22,31 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+package jdk.internal.event;
 
-package jdk.jfr.events;
+import sun.nio.ch.FileChannelImpl;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.RandomAccessFile;
+import java.lang.Throwable;
+import java.lang.reflect.Field;
 
-import jdk.jfr.Category;
-import jdk.jfr.Description;
-import jdk.jfr.Label;
-import jdk.jfr.DataAmount;
-import jdk.jfr.Name;
-import jdk.jfr.internal.Type;
-import jdk.jfr.internal.MirrorEvent;
+/**
+ * Helper class to enable JFR tracing.
+ */
+public final class JFRTracing {
 
-@Name(Type.EVENT_NAME_PREFIX + "FileRead")
-@Label("File Read")
-@Category("Java Application")
-@Description("Reading data from a file")
-@StackFilter({"java.io.FileInputStream", "java.io.RandomAccessFile", "sun.nio.ch.FileChannelImpl"})
-public final class FileReadEvent extends MirrorEvent {
+  public static void enable() throws NoSuchFieldException, IllegalAccessException {
+      enable(Throwable.class);
+      enable(FileInputStream.class);
+      enable(FileOutputStream.class);
+      enable(FileChannelImpl.class);
+      enable(RandomAccessFile.class);
+  }
 
-    @Label("Path")
-    @Description("Full path of the file, or N/A if a file descriptor was used to create the stream, for example System.in")
-    public String path;
-
-    @Label("Bytes Read")
-    @Description("Number of bytes read from the file (possibly 0)")
-    @DataAmount
-    public long bytesRead;
-
-    @Label("End of File")
-    @Description("If end of file was reached")
-    public boolean endOfFile;
-
+  private static void enable(Class<?> clazz) throws NoSuchFieldException, IllegalAccessException {
+      Field field = clazz.getDeclaredField("jfrTracing");
+      field.setAccessible(true);
+      field.setBoolean(null, true);
+  }
 }
