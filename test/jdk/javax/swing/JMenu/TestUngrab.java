4b22432bdbe5cff58553ac29d8aa123081f1489f
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-05-30 18:30:21 +0530

    Copyright

--- test/jdk/javax/swing/JMenu/TestUngrab.java
+++ test/jdk/javax/swing/JMenu/TestUngrab.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -69,7 +69,7 @@ public class TestUngrab {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
-    
+
     public static void main(String[] args) throws Exception {
         try {
             Robot robot = new Robot();
@@ -101,5 +101,5 @@ public class TestUngrab {
             SwingUtilities.invokeAndWait(() -> frame.dispose());
         }
     }
-    
+
 }
