b0d7942f6334af40da07bfee041bab569077fcdc
John Engebretson <jengebr@amazon.com>
2024-05-31 17:48:27 +0000

    Fixing copyright message, returning values from benchmark

--- test/micro/org/openjdk/bench/java/lang/reflect/ConstructorBenchmark.java
+++ test/micro/org/openjdk/bench/java/lang/reflect/ConstructorBenchmark.java
@@ -1,5 +1,24 @@
 /*
  * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
  */
 package org.openjdk.bench.java.lang.reflect;
 
@@ -46,22 +65,22 @@ public class ConstructorBenchmark {
     }
 
     @Benchmark
-    public void getExceptionTypes() throws Exception {
-        oneExceptionConstructor.getExceptionTypes();
+    public Object[] getExceptionTypes() throws Exception {
+        return oneExceptionConstructor.getExceptionTypes();
     }
 
     @Benchmark
-    public void getExceptionTypesEmpty() throws Exception {
-        emptyExceptionsConstructor.getExceptionTypes();
+    public Object[] getExceptionTypesEmpty() throws Exception {
+        return emptyExceptionsConstructor.getExceptionTypes();
     }
 
     @Benchmark
-    public void getParameterTypesEmpty() throws Exception {
-        emptyParametersConstructor.getParameterTypes();
+    public Object[] getParameterTypesEmpty() throws Exception {
+        return emptyParametersConstructor.getParameterTypes();
     }
 
     @Benchmark
-    public void getParameterTypes() throws Exception {
-        oneParameterConstructor.getParameterTypes();
+    public Object[] getParameterTypes() throws Exception {
+        return oneParameterConstructor.getParameterTypes();
     }
 }
