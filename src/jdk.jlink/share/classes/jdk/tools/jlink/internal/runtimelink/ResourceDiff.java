2b96a05a250315e5ad128e36834ad2f4060ce3a2
Severin Gehwolf <sgehwolf@redhat.com>
2024-06-05 18:53:49 +0200

    Remove printStackTrace()

--- src/jdk.jlink/share/classes/jdk/tools/jlink/internal/runtimelink/ResourceDiff.java
+++ src/jdk.jlink/share/classes/jdk/tools/jlink/internal/runtimelink/ResourceDiff.java
@@ -212,7 +212,6 @@ public class ResourceDiff implements Comparable<ResourceDiff> {
                 }
             }
         } catch (IOException e) {
-            e.printStackTrace();
             throw e;
         }
     }
