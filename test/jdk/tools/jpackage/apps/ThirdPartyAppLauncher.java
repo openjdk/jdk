8805c8cb39737128427a72e3e5e497ee363adecc
Vanitha <vanitha.b.p@oracle.com>
2024-06-05 09:05:27 +0530

    8325525: Create jtreg test case for JDK-8325203

--- test/jdk/tools/jpackage/apps/ThirdPartyAppLauncher.java
+++ test/jdk/tools/jpackage/apps/ThirdPartyAppLauncher.java
@@ -30,38 +30,40 @@ import java.nio.file.Paths;
 import java.util.logging.Logger;
 
 public class ThirdPartyAppLauncher {
-	private static final Logger logger = Logger.getLogger(ThirdPartyAppLauncher.class.getName());
-	private static final String FS = File.separator;
-	private static final Path CWD = Paths.get(".");
-	private static final String processIdFilePath = CWD + FS + "process.tmp";
+    private static final Logger logger = Logger
+	    .getLogger(ThirdPartyAppLauncher.class.getName());
+    private static final String FS = File.separator;
+    private static final Path CWD = Paths.get(".");
+    private static final String processIdFilePath = CWD + FS + "process.tmp";
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		ProcessBuilder processBuilder = new ProcessBuilder("regedit");
-		Process process = processBuilder.start();
-		logger.info("RegEdit id=" + process.pid());
+	ProcessBuilder processBuilder = new ProcessBuilder("regedit");
+	Process process = processBuilder.start();
+	logger.info("RegEdit id=" + process.pid());
 
-		File file = new File(processIdFilePath);
-		BufferedWriter writer = null;
-		try {
-			if (file.createNewFile()) {
-				logger.info(file.getAbsolutePath().toString());
-				writer = new BufferedWriter(new FileWriter(file));
-				writer.write("" + process.pid());
-				writer.close();
-				logger.info("Successfully written processid to file " + processIdFilePath);
-			}
-		} catch (IOException e) {
-			throw new IOException(e);
-		} finally {
-			try {
-				if (writer != null) {
-					writer.close();
-				}
-			} catch (IOException ex) {
-				throw new IOException(ex);
-			}
+	File file = new File(processIdFilePath);
+	BufferedWriter writer = null;
+	try {
+	    if (file.createNewFile()) {
+		logger.info(file.getAbsolutePath().toString());
+		writer = new BufferedWriter(new FileWriter(file));
+		writer.write("" + process.pid());
+		writer.close();
+		logger.info("Successfully written processid to file "
+			+ processIdFilePath);
+	    }
+	} catch (IOException e) {
+	    throw new IOException(e);
+	} finally {
+	    try {
+		if (writer != null) {
+		    writer.close();
 		}
-		System.exit(0);
+	    } catch (IOException ex) {
+		throw new IOException(ex);
+	    }
 	}
+	System.exit(0);
+    }
 }
--- test/jdk/tools/jpackage/windows/JpackageTest.java
+++ test/jdk/tools/jpackage/windows/JpackageTest.java
@@ -21,7 +21,6 @@
  * questions.
  */
 
-
 /* @test
  * @bug 8325203
  * @library ../helpers
@@ -33,129 +32,141 @@
  * @summary Test that Jpackage windows executable application kills the launched 3rd party application
  *          when System.exit(0) is invoked along with terminating java program. 
  */
- 
 
-import java.io.*;
+import java.io.File;
 import java.nio.file.Files;
-import java.util.Optional;
-
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Optional;
 import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 import java.util.logging.Logger;
 
 import jdk.jpackage.test.TKit;
 import jdk.test.lib.compiler.CompilerUtils;
-import jdk.test.lib.util.JarUtils;
 import jdk.test.lib.process.ProcessTools;
+import jdk.test.lib.util.JarUtils;
 
 public class JpackageTest {
-    private static final Logger logger = Logger.getLogger(JpackageTest.class.getName());
+    private static final Logger logger = Logger
+	    .getLogger(JpackageTest.class.getName());
     private static final String APPLICATION_NAME = "ThirdPartyAppLauncher";
     private static final String FS = File.separator;
-    private static final String SRC_FILE = TKit.TEST_SRC_ROOT.resolve("apps" + FS + APPLICATION_NAME +".java").toString();
+    private static final String SRC_FILE = TKit.TEST_SRC_ROOT
+	    .resolve("apps" + FS + APPLICATION_NAME + ".java").toString();
     private static final String CLASS_FILE = APPLICATION_NAME + ".class";
     private static final String JAR_FILE = APPLICATION_NAME + ".jar";
     private static final String INSTALLER = APPLICATION_NAME + "-1.0.exe";
     private static final String JAVA_HOME = System.getProperty("test.jdk");
-    private static final String JPACKAGE = JAVA_HOME + FS + "bin" + FS + "jpackage";
-    private static final String APPLICATION_PATH = "C:" + FS + "Program Files" + FS + APPLICATION_NAME + FS;
-    private static final String EXECUTABLE_APPLICATION = APPLICATION_PATH + APPLICATION_NAME + ".exe";
+    private static final String JPACKAGE = JAVA_HOME + FS + "bin" + FS
+	    + "jpackage";
+    private static final String APPLICATION_PATH = "C:" + FS + "Program Files"
+	    + FS + APPLICATION_NAME + FS;
+    private static final String EXECUTABLE_APPLICATION = APPLICATION_PATH
+	    + APPLICATION_NAME + ".exe";
     private static final Path CWD = Paths.get(".");
-    private static final String processIdFilePath =  CWD.toString() + FS + "process.tmp";
-    private static final Path srcDir = Paths.get(System.getProperty("test.src", "."));
-    private static final Path classesDir = Paths.get(System.getProperty("test.classes", "."));
+    private static final String processIdFilePath = CWD.toString() + FS
+	    + "process.tmp";
+    private static final Path srcDir = Paths
+	    .get(System.getProperty("test.src", "."));
+    private static final Path classesDir = Paths
+	    .get(System.getProperty("test.classes", "."));
 
     public static void main(String[] args) throws Throwable {
-    	JpackageTest test = new JpackageTest();
-        try {
-            test.closeThirdPartyApplication();
-            test.setUp();
-            test.run();
-        } finally{
-            test.cleanUp();
-        }
+	JpackageTest test = new JpackageTest();
+	try {
+	    test.closeThirdPartyApplication();
+	    test.setUp();
+	    test.run();
+	} finally {
+	    test.cleanUp();
+	}
     }
 
     /**
-     * Sets up the prerequisites for executing the test
-     *  - Compile the third party application launcher source
-     *  - Create a jar of third party application launcher
-     *  - Create a windows installer package of third party application launcher
+     * Sets up the prerequisites for executing the test - Compile the third
+     * party application launcher source - Create a jar of third party
+     * application launcher - Create a windows installer package of third party
+     * application launcher
      */
     private void setUp() throws Throwable {
-		
-    	CompilerUtils.compile(srcDir.resolve(SRC_FILE), classesDir);
-		logger.info("Compiled Successfully");
-		
-		Manifest manifest = new Manifest();
-		manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION, "1.0");
-		manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS, APPLICATION_NAME);
-		
-		JarUtils.createJarFile(Paths.get(JAR_FILE), manifest, classesDir, classesDir.resolve(CLASS_FILE));
-		logger.info("Jar Created Successfully");
-		
-		ProcessTools
-		        .executeCommand(JPACKAGE, "--input", CWD.toString(), "--name", APPLICATION_NAME, "--main-jar", JAR_FILE, "--main-class", APPLICATION_NAME,
-		                "--type", "exe").shouldHaveExitValue(0);
-		logger.info("Package Successfully");
-		
-		ProcessTools
-		        .executeCommand(INSTALLER).shouldHaveExitValue(0);
-		logger.info("Installed Successfully");
+
+	CompilerUtils.compile(srcDir.resolve(SRC_FILE), classesDir);
+	logger.info("Compiled Successfully");
+
+	Manifest manifest = new Manifest();
+	manifest.getMainAttributes().put(Attributes.Name.MANIFEST_VERSION,
+		"1.0");
+	manifest.getMainAttributes().put(Attributes.Name.MAIN_CLASS,
+		APPLICATION_NAME);
+
+	JarUtils.createJarFile(Paths.get(JAR_FILE), manifest, classesDir,
+		classesDir.resolve(CLASS_FILE));
+	logger.info("Jar Created Successfully");
+
+	ProcessTools
+		.executeCommand(JPACKAGE, "--input", CWD.toString(), "--name",
+			APPLICATION_NAME, "--main-jar", JAR_FILE,
+			"--main-class", APPLICATION_NAME, "--type", "exe")
+		.shouldHaveExitValue(0);
+	logger.info("Package Successfully");
+
+	ProcessTools.executeCommand(INSTALLER).shouldHaveExitValue(0);
+	logger.info("Installed Successfully");
     }
 
     /**
-     * Execute the test 
-     * - Start the third party application launcher
-     * - Check whether the termination of third party application launcher also terminating the launched third party application
-     * - If third party application is not terminated the test is successful else failure
+     * Execute the test - Start the third party application launcher - Check
+     * whether the termination of third party application launcher also
+     * terminating the launched third party application - If third party
+     * application is not terminated the test is successful else failure
      */
-    private void run() throws Throwable{
-		ProcessTools
-		        .executeCommand(EXECUTABLE_APPLICATION).shouldHaveExitValue(0);
-		logger.info("Launched Successfully");
-		
-		long processId = readProcessId();
-		logger.info("Successfully read processid from file " + processIdFilePath);
-		logger.info("Process Id is " + processId);
-		
-		Optional<ProcessHandle> processHandle = ProcessHandle.of(processId);
-		boolean isAlive = processHandle.isPresent() && processHandle.get().isAlive();
-		if (isAlive) {
-		    logger.info("Test Successful");
-		} else {
-		    logger.info("Test failed");
-		    throw new RuntimeException("Test failed: Third party software is terminated");
-		}
+    private void run() throws Throwable {
+	ProcessTools.executeCommand(EXECUTABLE_APPLICATION)
+		.shouldHaveExitValue(0);
+	logger.info("Launched Successfully");
+
+	long processId = readProcessId();
+	logger.info(
+		"Successfully read processid from file " + processIdFilePath);
+	logger.info("Process Id is " + processId);
+
+	Optional<ProcessHandle> processHandle = ProcessHandle.of(processId);
+	boolean isAlive = processHandle.isPresent()
+		&& processHandle.get().isAlive();
+	if (isAlive) {
+	    logger.info("Test Successful");
+	} else {
+	    logger.info("Test failed");
+	    throw new RuntimeException(
+		    "Test failed: Third party software is terminated");
+	}
     }
 
-	/**
-	 * Helper function to read processId from file
-	 */
-    private long readProcessId() throws Throwable{
-		 
-        Path path = Paths.get(processIdFilePath);
-		long processId = Long.parseLong(Files.readAllLines(path).get(0));
-		return processId;
-	}
+    /**
+     * Helper function to read processId from file
+     */
+    private long readProcessId() throws Throwable {
 
-	/**
-	 * Kill if any regedit.exe is running before launching
-	 */
-    private void closeThirdPartyApplication() throws Throwable{
-		Runtime.getRuntime().exec("taskkill /F /IM regedit.exe");
-	}
+	Path path = Paths.get(processIdFilePath);
+	long processId = Long.parseLong(Files.readAllLines(path).get(0));
+	return processId;
+    }
 
-	/**
-	 * Uninstalls the application and terminates the third party application
-	 */
-    private void cleanUp() throws Throwable{
-		ProcessTools
-		        .executeCommand(INSTALLER, "/q", "REMOVE=ALL").shouldHaveExitValue(0);
-		closeThirdPartyApplication();
-		logger.info("Cleanup Successful");
-	}
+    /**
+     * Kill if any regedit.exe is running before launching
+     */
+    private void closeThirdPartyApplication() throws Throwable {
+	Runtime.getRuntime().exec("taskkill /F /IM regedit.exe");
+    }
+
+    /**
+     * Uninstalls the application and terminates the third party application
+     */
+    private void cleanUp() throws Throwable {
+	ProcessTools.executeCommand(INSTALLER, "/q", "REMOVE=ALL")
+		.shouldHaveExitValue(0);
+	closeThirdPartyApplication();
+	logger.info("Cleanup Successful");
+    }
 }
-
