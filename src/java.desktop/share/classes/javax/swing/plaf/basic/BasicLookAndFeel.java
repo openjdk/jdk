98399ec844ffce9a1c0133e547acf365bc13b311
Alisen Chung <alisen.chung@oracle.com>
2024-06-06 08:20:32 -0700

    fix broken test, add MOUSE_RELEASED

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicLookAndFeel.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicLookAndFeel.java
@@ -2230,7 +2230,9 @@ public abstract class BasicLookAndFeel extends LookAndFeel implements Serializab
                     }
                     if(src != null && src.isEnabled()) {
                         JPopupMenu componentPopupMenu = src.getComponentPopupMenu();
-                        if(componentPopupMenu != null && (eventID == MouseEvent.MOUSE_PRESSED)) {
+                        if(componentPopupMenu != null && (
+                                (eventID == MouseEvent.MOUSE_PRESSED) ||
+                                eventID == MouseEvent.MOUSE_RELEASED)) {
                             Point pt = src.getPopupLocation(me);
                             if(pt == null) {
                                 pt = me.getPoint();
