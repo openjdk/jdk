ab3b9e3816a691465b91ee781e3d4ce962bad4e2
Joe Darcy <joe.darcy@oracle.com>
2024-06-07 13:58:43 -0700

    JDK-8333828: Use value javadoc tag in java.lang.{Float, Double}

--- src/java.base/share/classes/java/lang/Float.java
+++ src/java.base/share/classes/java/lang/Float.java
@@ -127,14 +127,14 @@ public final class Float extends Number
     public static final float MIN_VALUE = 0x0.000002P-126f; // 1.4e-45f
 
     /**
-     * The number of bits used to represent a {@code float} value.
+     * The number of bits used to represent a {@code float} value, {@value #SIZE}.
      *
      * @since 1.5
      */
     public static final int SIZE = 32;
 
     /**
-     * The number of bits in the significand of a {@code float} value.
+     * The number of bits in the significand of a {@code float} value, {@value #PRECISION}.
      * This is the parameter N in section {@jls 4.2.3} of
      * <cite>The Java Language Specification</cite>.
      *
@@ -143,7 +143,7 @@ public final class Float extends Number
     public static final int PRECISION = 24;
 
     /**
-     * Maximum exponent a finite {@code float} variable may have.  It
+     * Maximum exponent a finite {@code float} variable may have, {@value #MAX_EXPONENT}.  It
      * is equal to the value returned by {@code
      * Math.getExponent(Float.MAX_VALUE)}.
      *
@@ -152,7 +152,7 @@ public final class Float extends Number
     public static final int MAX_EXPONENT = (1 << (SIZE - PRECISION - 1)) - 1; // 127
 
     /**
-     * Minimum exponent a normalized {@code float} variable may have.
+     * Minimum exponent a normalized {@code float} variable may have, {@value #MIN_EXPONENT}.
      * It is equal to the value returned by {@code
      * Math.getExponent(Float.MIN_NORMAL)}.
      *
@@ -161,7 +161,7 @@ public final class Float extends Number
     public static final int MIN_EXPONENT = 1 - MAX_EXPONENT; // -126
 
     /**
-     * The number of bytes used to represent a {@code float} value.
+     * The number of bytes used to represent a {@code float} value, {@value #BYTES}.
      *
      * @since 1.8
      */
