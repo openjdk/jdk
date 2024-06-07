2df9dadf8d5458e35d37d54a5efa0c6cf385b28b
MBaesken <matthias.baesken@sap.com>
2024-06-07 14:06:48 +0200

    add comment suggested by Severin

--- src/hotspot/os/linux/os_linux.cpp
+++ src/hotspot/os/linux/os_linux.cpp
@@ -314,6 +314,8 @@ static jlong host_free_swap() {
 }
 
 jlong os::free_swap_space() {
+  // os::total_swap_space() might return the containerized limit which might be
+  // less than host_free_swap(). The upper bound of free swap needs to be the lower of the two.
   jlong host_free_swap_val = MIN2(os::total_swap_space(), host_free_swap());
   assert(host_free_swap_val >= 0, "sysinfo failed?");
   if (OSContainer::is_containerized()) {
