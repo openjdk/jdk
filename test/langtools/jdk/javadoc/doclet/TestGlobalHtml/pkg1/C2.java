a12924966a1a27ab05a8fcb181ad3ae368fcbbc0
Nizar Benalla <nizar.benalla@oracle.com>
2024-06-11 14:21:29 +0100

    Remove classpath exception

--- test/langtools/jdk/javadoc/doclet/TestGlobalHtml/pkg1/C2.java
+++ test/langtools/jdk/javadoc/doclet/TestGlobalHtml/pkg1/C2.java
@@ -4,9 +4,7 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -112,7 +110,6 @@ public class C2 extends C1 {
      * @see #calculateResult(int, int)
      */
     public boolean performAction(String action) {
-        // Placeholder implementation
         return true;
     }
 
@@ -135,7 +132,6 @@ public class C2 extends C1 {
      * @throws ArithmeticException if a division by zero occurs
      */
     public int calculateResult(int a, int b) throws ArithmeticException {
-        // Placeholder implementation
         return a + b;
     }
 
@@ -150,7 +146,6 @@ public class C2 extends C1 {
      */
     @Override
     public void readObject() throws IOException {
-        // Placeholder implementation
         super.readObject();
     }
 }
