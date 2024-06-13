7abe6171e4e62df660185de8856f200498385a12
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-06-13 16:00:06 -0500

    Disallow early assignments without FLEXIBLE_CONSTRUCTORS feature.

---
+++ test/langtools/tools/javac/SuperInit/EarlyAssignmentNoPreview1.java
@@ -0,0 +1,21 @@
+/*
+ * @test /nodynamiccopyright/
+ * @bug 8334258
+ * @summary Disallow early assignment if FLEXIBLE_CONSTRUCTORS preview feature is not enabled
+ * @compile/fail/ref=EarlyAssignmentNoPreview1.out -XDrawDiagnostics EarlyAssignmentNoPreview1.java
+ */
+public class EarlyAssignmentNoPreview1 {
+
+    Runnable r;
+
+    public EarlyAssignmentNoPreview1() {
+        this(r = () -> System.out.println("hello"));
+    }
+
+    public EarlyAssignmentNoPreview1(Runnable r) {
+    }
+
+    public static void main(String[] args) {
+        new EarlyAssignmentNoPreview1();
+    }
+}
