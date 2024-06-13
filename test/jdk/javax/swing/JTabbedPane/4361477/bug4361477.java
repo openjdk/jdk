1182f00953998543c56561b18bcb80d37518cfc8
Jayathirth Rao D V <jayathirth.d.v@oracle.com>
2024-06-13 20:45:55 +0530

    Test update

--- test/jdk/javax/swing/JTabbedPane/4361477/bug4361477.java
+++ test/jdk/javax/swing/JTabbedPane/4361477/bug4361477.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,6 @@ import javax.swing.event.*;
  * @key headful
  * @bug 4361477
  * @summary JTabbedPane throws ArrayIndexOutOfBoundsException
- * @author Oleg Mokhovikov
  * @run main bug4361477
  */
 public class bug4361477 {
