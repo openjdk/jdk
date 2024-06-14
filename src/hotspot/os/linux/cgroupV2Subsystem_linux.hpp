2c067ddaf2805fc7149f9de88c6c35894f48b303
Severin Gehwolf <sgehwolf@redhat.com>
2024-06-14 16:41:33 +0200

    Style nit

--- src/hotspot/os/linux/cgroupV2Subsystem_linux.hpp
+++ src/hotspot/os/linux/cgroupV2Subsystem_linux.hpp
@@ -86,7 +86,7 @@ class CgroupV2Subsystem: public CgroupSubsystem {
     CachingCgroupController<CgroupCpuController*>* _cpu = nullptr;
 
   public:
-    CgroupV2Subsystem(CgroupV2MemoryController * memory,
+    CgroupV2Subsystem(CgroupV2MemoryController* memory,
                       CgroupV2CpuController* cpu,
                       CgroupV2Controller* unified) {
       _unified = unified;
