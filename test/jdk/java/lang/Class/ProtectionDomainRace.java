4ce5bc2482bc7e80420b64a587dbb00139370194
Weijun Wang <weijun.wang@oracle.com>
2024-06-17 11:20:53 -0400

    explain why the test is related to the fix

--- test/jdk/java/lang/Class/ProtectionDomainRace.java
+++ test/jdk/java/lang/Class/ProtectionDomainRace.java
@@ -30,6 +30,31 @@
 import javax.security.auth.Subject;
 import java.security.PrivilegedAction;
 
+/**
+ * Without the code fix, this test would fail with
+ * java.lang.AssertionError: sun.security.util.ResourcesMgr (PD)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller.checkInjectedInvoker(MethodHandleImpl.java:1209)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller.makeInjectedInvoker(MethodHandleImpl.java:1110)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller$1.computeValue(MethodHandleImpl.java:1117)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller$1.computeValue(MethodHandleImpl.java:1114)
+ *         at java.base/java.lang.ClassValue.getFromHashMap(ClassValue.java:229)
+ *         at java.base/java.lang.ClassValue.getFromBackup(ClassValue.java:211)
+ *         at java.base/java.lang.ClassValue.get(ClassValue.java:117)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller.bindCallerWithInjectedInvoker(MethodHandleImpl.java:1089)
+ *         at java.base/java.lang.invoke.MethodHandleImpl$BindCaller.bindCaller(MethodHandleImpl.java:1077)
+ *         at java.base/java.lang.invoke.MethodHandleImpl.bindCaller(MethodHandleImpl.java:1032)
+ *         at java.base/java.lang.invoke.MethodHandles$Lookup.maybeBindCaller(MethodHandles.java:4149)
+ *         at java.base/java.lang.invoke.MethodHandles$Lookup.getDirectMethodCommon(MethodHandles.java:4133)
+ *         at java.base/java.lang.invoke.MethodHandles$Lookup.getDirectMethodNoSecurityManager(MethodHandles.java:4077)
+ *         at java.base/java.lang.invoke.MethodHandles$Lookup.getDirectMethodForConstant(MethodHandles.java:4326)
+ *         at java.base/java.lang.invoke.MethodHandles$Lookup.linkMethodHandleConstant(MethodHandles.java:4274)
+ *         at java.base/java.lang.invoke.MethodHandleNatives.linkMethodHandleConstant(MethodHandleNatives.java:628)
+ *         at java.base/sun.security.util.ResourcesMgr.getBundle(ResourcesMgr.java:54)
+ *         at java.base/sun.security.util.ResourcesMgr.getString(ResourcesMgr.java:40)
+ *         at java.base/javax.security.auth.Subject.doAs(Subject.java:517)
+ *         ...
+ * as the Class::protectionDomain might assign different objects to the (original) allPermDomain field.
+ */
 public class ProtectionDomainRace {
     private static volatile Throwable failed = null;
     public static void main(String[] args) throws Throwable {
