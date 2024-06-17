76b8f1022dc3be0294d8a55439fd3b78933faedd
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-06-17 12:02:21 -0500

    Disallow attempts to access a free variable proxy field from a static method.

--- src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Lower.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/comp/Lower.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1653,6 +1653,13 @@ public class Lower extends TreeTranslator {
     }
 //where
         JCExpression loadFreevar(DiagnosticPosition pos, VarSymbol v) {
+
+            // Verify we are not trying to access a proxy field from a static method
+            Symbol proxy = proxies.get(v);
+            if (proxy != null && proxy.owner == currentClass && currentMethodSym.isStatic())
+                log.error(pos, Errors.NonStaticCantBeRef(Kinds.kindName(v), v));
+
+            // Access symbol
             return access(v, make.at(pos).Ident(v), null, false);
         }
 
