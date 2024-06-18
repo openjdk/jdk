231a8a91eece41e07af535e49b08195b2a03111c
tstuefe <thomas.stuefe@gmail.com>
2024-06-18 16:42:25 +0200

    copyrights

--- test/hotspot/jtreg/serviceability/dcmd/vm/SystemMapTest.java
+++ test/hotspot/jtreg/serviceability/dcmd/vm/SystemMapTest.java
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2023, Red Hat, Inc. and/or its affiliates.
+ * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2024, Red Hat, Inc. and/or its affiliates.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
