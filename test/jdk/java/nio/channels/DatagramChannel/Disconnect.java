2866ce4f66a2302f0c4ebda91a786d3328f0741f
Daniel Fuchs <daniel.fuchs@oracle.com>
2024-06-25 12:59:30 +0100

    Add some more debug traces

--- test/jdk/java/nio/channels/DatagramChannel/Disconnect.java
+++ test/jdk/java/nio/channels/DatagramChannel/Disconnect.java
@@ -35,6 +35,7 @@ import java.nio.*;
 import java.nio.channels.*;
 import java.io.IOException;
 
+import jdk.test.lib.Platform;
 import jdk.test.lib.net.IPSupport;
 
 public class Disconnect {
@@ -43,23 +44,29 @@ public class Disconnect {
 
         // test with default protocol family
         try (DatagramChannel dc = DatagramChannel.open()) {
-            test(dc, InetAddress.getLoopbackAddress());
-            test(dc, InetAddress.getLoopbackAddress());
+            InetAddress lo = InetAddress.getLoopbackAddress();
+            System.out.println("Testing with default family and " + lo);
+            test(dc, lo);
+            test(dc, lo);
         }
 
         if (IPSupport.hasIPv4()) {
             // test with IPv4 only
             try (DatagramChannel dc = DatagramChannel.open(StandardProtocolFamily.INET)) {
-                test(dc, InetAddress.ofLiteral("127.0.0.1"));
-                test(dc, InetAddress.ofLiteral("127.0.0.1"));
+                InetAddress lo4 = InetAddress.ofLiteral("127.0.0.1");
+                System.out.println("Testing with INET family and " + lo4);
+                test(dc, lo4);
+                test(dc, lo4);
             }
         }
 
         if (IPSupport.hasIPv6()) {
             // test with IPv6 only
             try (DatagramChannel dc = DatagramChannel.open(StandardProtocolFamily.INET6)) {
-                test(dc, InetAddress.ofLiteral("::1"));
-                test(dc, InetAddress.ofLiteral("::1"));
+                InetAddress lo6 = InetAddress.ofLiteral("::1");
+                System.out.println("Testing with INET6 family and " + lo6);
+                test(dc, lo6);
+                test(dc, lo6);
             }
         }
     }
@@ -74,6 +81,8 @@ public class Disconnect {
             server.bind(new InetSocketAddress(l0, 0));
 
             dc.connect(new InetSocketAddress(l0, server.socket().getLocalPort()));
+            System.out.println("dc connected from " +
+                    dc.getLocalAddress() + " to " + dc.getRemoteAddress());
 
             dc.write(ByteBuffer.wrap("hello".getBytes()));
 
