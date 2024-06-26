fb40557910387af784aace41dd74d4c294792562
Daniel Fuchs <daniel.fuchs@oracle.com>
2024-06-26 19:38:51 +0100

    Minor doc fix

--- test/jdk/java/nio/channels/TestServers.java
+++ test/jdk/java/nio/channels/TestServers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -514,6 +514,7 @@ public class TestServers {
         }
 
         /**
+         * Creates a new abstract UDP server.
          *
          * @param linger the amount of time the server should wait before
          *          responding to requests.
