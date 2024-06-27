d9073c3e0f74921af7494c0c687a2f05b61164c5
Xiaolong Peng <xlpeng@amazon.com>
2024-06-26 23:34:34 -0700

    Make log_status_with_heap_locked public

--- src/hotspot/share/gc/shenandoah/shenandoahFreeSet.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahFreeSet.hpp
@@ -318,8 +318,6 @@ private:
 
   void finish_rebuild(size_t cset_regions);
 
-  void log_status_with_heap_locked();
-
 public:
   ShenandoahFreeSet(ShenandoahHeap* heap, size_t max_regions);
 
@@ -343,6 +341,7 @@ public:
 
   void recycle_trash();
   void log_status();
+  void log_status_with_heap_locked();
 
   inline size_t capacity()  const { return _partitions.capacity_of(ShenandoahFreeSetPartitionId::Mutator); }
   inline size_t used()      const { return _partitions.used_by(ShenandoahFreeSetPartitionId::Mutator);     }
