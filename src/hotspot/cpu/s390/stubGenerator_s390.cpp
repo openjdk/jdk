8c7f5509c25d6fce7ab624b5b29000ab2d9c04b4
Amit Kumar <amitlocham09@icloud.com>
2024-06-28 20:35:05 +0530

    extra whitespace

--- src/hotspot/cpu/s390/stubGenerator_s390.cpp
+++ src/hotspot/cpu/s390/stubGenerator_s390.cpp
@@ -3293,7 +3293,7 @@ class StubGenerator: public StubCodeGenerator {
     }
     if (UseSecondarySupersTable) {
       StubRoutines::_lookup_secondary_supers_table_slow_path_stub = generate_lookup_secondary_supers_table_slow_path_stub();
-      if (! InlineSecondarySupersTest) {
+      if (!InlineSecondarySupersTest) {
         for (int slot = 0; slot < Klass::SECONDARY_SUPERS_TABLE_SIZE; slot++) {
           StubRoutines::_lookup_secondary_supers_table_stubs[slot] = generate_lookup_secondary_supers_table_stub(slot);
         }
