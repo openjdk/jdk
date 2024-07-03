65b8c7c6ce5336792637a3e4818316ff61d2d0a2
Albert Yang <albert.m.yang@oracle.com>
2024-07-03 11:56:27 +0200

    trivial

--- src/hotspot/share/gc/serial/defNewGeneration.hpp
+++ src/hotspot/share/gc/serial/defNewGeneration.hpp
@@ -228,13 +228,6 @@ class DefNewGeneration: public Generation {
   // GC support
   void compute_new_size();
 
-  // Returns true if the collection is likely to be safely
-  // completed. Even if this method returns true, a collection
-  // may not be guaranteed to succeed, and the system should be
-  // able to safely unwind and recover from that failure, albeit
-  // at some additional cost.
-  bool collection_attempt_is_safe();
-
   bool collect(bool clear_all_soft_refs);
 
   HeapWord* expand_and_allocate(size_t size, bool is_tlab);
