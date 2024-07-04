55432d39ca75408fa36f41f68b459da0c948d2d8
Johan Sjölén <johan.sjolen@oracle.com>
2024-07-04 17:43:04 +0200

    Apparently this(!)

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/HotSpotTypeDataBase.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/HotSpotTypeDataBase.java
@@ -117,7 +117,7 @@ public class HotSpotTypeDataBase extends BasicTypeDataBase {
         BasicType basicTargetType = createBasicType(cTypeName, false, false, false);
 
         // transfer fields from GrowableArrayBase to template instance
-        BasicType generic = lookupOrFail("GrowableArrayBase");
+        BasicType generic = lookupOrFail("GrowableArrayBase<int>");
         BasicType specific = lookupOrFail("GrowableArray<int>");
         basicTargetType.setSize(specific.getSize());
         Iterator fields = generic.getFields();
