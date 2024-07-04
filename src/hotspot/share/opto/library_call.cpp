5df1873d1b86b7c1c508fee39790ff72ddb70827
Tobias Holenstein <tobias.holenstein@oracle.com>
2024-07-04 13:59:31 +0200

    JDK-8320308: C2 compilation crashes in LibraryCallKit::inline_unsafe_access

--- src/hotspot/share/opto/library_call.cpp
+++ src/hotspot/share/opto/library_call.cpp
@@ -2360,6 +2360,9 @@ bool LibraryCallKit::inline_unsafe_access(bool is_store, const BasicType type, c
 
   Node* adr = make_unsafe_address(base, offset, type, kind == Relaxed);
 
+  //if (stopped()) {
+    //return true;
+  //}
   if (_gvn.type(base)->isa_ptr() == TypePtr::NULL_PTR) {
     if (type != T_OBJECT) {
       decorators |= IN_NATIVE; // off-heap primitive access
