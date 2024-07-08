55fd1ed228ea3c42aaf92579e5dcb818fe14351d
Jatin Bhateja <jbhateja@openjdk.org>
2024-07-08 06:42:46 +0000

    8333890: Fatal error in auto-vectorizer with float16 kernel.
    Reviewed-by: kvn

--- src/hotspot/share/opto/superword.cpp
+++ src/hotspot/share/opto/superword.cpp
@@ -2586,6 +2586,12 @@ const Type* VLoopTypes::container_type(Node* n) const {
   }
   const Type* t = _vloop.phase()->igvn().type(n);
   if (t->basic_type() == T_INT) {
+    // Float to half float conversion may be succeeded by a conversion from
+    // half float to float, in such a case back propagation of narrow type (SHORT)
+    // may not be possible.
+    if (n->Opcode() == Op_ConvF2HF) {
+      return TypeInt::SHORT;
+    }
     // A narrow type of arithmetic operations will be determined by
     // propagating the type of memory operations.
     return TypeInt::INT;
