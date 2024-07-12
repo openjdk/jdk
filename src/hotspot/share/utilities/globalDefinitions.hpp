ed2797febe6d770a84463cc5d33429617cd18ae5
Henry Lin <hny@amazon.com>
2024-07-12 11:29:55 -0700

    8332697: fix ubsan:shenandoahSimpleBitMap.inline.hpp runtime integer overflow

--- src/hotspot/share/utilities/globalDefinitions.hpp
+++ src/hotspot/share/utilities/globalDefinitions.hpp
@@ -1066,7 +1066,7 @@ const intptr_t OneBit     =  1; // only right_most bit set in a word
 // get a word with the n.th or the right-most or left-most n bits set
 // (note: #define used only so that they can be used in enum constant definitions)
 #define nth_bit(n)        (((n) >= BitsPerWord) ? 0 : (OneBit << (n)))
-#define right_n_bits(n)   (nth_bit(n) - 1)
+#define right_n_bits(n)   ((uintptr_t) nth_bit(n) - 1)
 
 // bit-operations using a mask m
 inline void   set_bits    (intptr_t& x, intptr_t m) { x |= m; }
