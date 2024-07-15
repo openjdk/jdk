1fad7dff7fafed0f9754151187b2d60f26fec557
Alan Bateman <alan.bateman@oracle.com>
2024-07-15 12:33:50 +0100

    Drop JLA updates for this update

--- src/java.base/share/classes/jdk/internal/access/JavaLangAccess.java
+++ src/java.base/share/classes/jdk/internal/access/JavaLangAccess.java
@@ -43,9 +43,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
 import java.util.concurrent.RejectedExecutionException;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Stream;
 
 import jdk.internal.misc.CarrierThreadLocal;
@@ -583,16 +581,6 @@ public interface JavaLangAccess {
      * @throws RejectedExecutionException if the scheduler cannot accept a task
      */
     void unparkVirtualThread(Thread thread);
-
-    /**
-     * Returns the virtual thread default scheduler.
-     */
-    Executor virtualThreadDefaultScheduler();
-
-    /**
-     * Returns a stream of the delayed task schedulers used for virtual threads.
-     */
-    Stream<ScheduledExecutorService> virtualThreadDelayedTaskSchedulers();
 
     /**
      * Creates a new StackWalker
