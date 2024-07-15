a441f3801668d6044cd6273d1fbf47a06a63a6f5
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-07-15 10:14:37 +0100

    Prevent AIOBE

--- src/java.desktop/share/classes/javax/swing/JTable.java
+++ src/java.desktop/share/classes/javax/swing/JTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1267,8 +1267,10 @@ public class JTable extends JComponent implements TableModelListener, Scrollable
             resizeAndRepaint();
             if (tableHeader != null) {
                 if (mode == JTable.AUTO_RESIZE_LAST_COLUMN) {
-                    tableHeader.setResizingColumn(
-                        columnModel.getColumn(columnModel.getColumnCount() - 1));
+                    if (columnModel.getColumnCount() > 0) {
+                        tableHeader.setResizingColumn(
+                                columnModel.getColumn(columnModel.getColumnCount() - 1));
+                    }
                 }
                 tableHeader.resizeAndRepaint();
             }
