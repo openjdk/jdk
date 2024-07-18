aae841d89a89993780ee14a4f0ce1a76db9ab653
liach <liach@openjdk.org>
2024-07-18 17:15:42 -0500

    8336777: BufferedMethodBuilder not initialized with static flag

--- src/java.base/share/classes/jdk/internal/classfile/impl/DirectMethodBuilder.java
+++ src/java.base/share/classes/jdk/internal/classfile/impl/DirectMethodBuilder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -42,7 +42,7 @@ import java.lang.classfile.constantpool.Utf8Entry;
 
 public final class DirectMethodBuilder
         extends AbstractDirectBuilder<MethodModel>
-        implements TerminalMethodBuilder, WritableElement<MethodModel>, MethodInfo {
+        implements TerminalMethodBuilder, WritableElement<MethodModel> {
 
     final Utf8Entry name;
     final Utf8Entry desc;
