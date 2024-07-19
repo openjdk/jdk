484f25eb1f3647b1852a87d8643a444b823d4099
SoniaZaldana <zaldanasonia@gmail.com>
2024-07-19 10:42:39 -0400

    8327054: DiagnosticCommand Compiler.perfmap does not log on output()

--- test/hotspot/jtreg/serviceability/dcmd/compiler/PerfMapTest.java
+++ test/hotspot/jtreg/serviceability/dcmd/compiler/PerfMapTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2024, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020, 2023, Arm Limited. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -111,6 +111,16 @@ public class PerfMapTest {
         String test_dir = System.getProperty("test.dir", ".");
         Path path = Paths.get("/tmp/perf-<pid>.map");
         run(new JMXExecutor(), "Compiler.perfmap " + path.toString(), path);
+        Files.deleteIfExists(path);
+    }
+
+    @Test
+    public void logErrorsDcmdOutputStream() throws IOException {
+        String test_dir = System.getProperty("test.dir", ".");
+        Path path = Paths.get("nonexistent", test_dir);
+        OutputAnalyzer output = new JMXExecutor().execute("Compiler.perfmap %s".formatted(path));
+        output.shouldContain("Failed to create nonexistent/%s for perf map".formatted(test_dir));
+        output.shouldNotHaveExitValue(0);
         Files.deleteIfExists(path);
     }
 }
