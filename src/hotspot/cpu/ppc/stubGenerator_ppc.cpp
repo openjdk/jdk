209f2e55cf606f3f254a6a98eb3779aad2543fbd
suchismith1993 <suchismith1993@gmail.com>
2024-07-19 09:40:11 -0500

    add instructions for gcm shift

--- src/hotspot/cpu/ppc/stubGenerator_ppc.cpp
+++ src/hotspot/cpu/ppc/stubGenerator_ppc.cpp
@@ -630,8 +630,66 @@ class StubGenerator: public StubCodeGenerator {
     return start;
   }
 address generate_ghash_processBlocks() {
-  StubCodeMark mark(this, "StubRoutines", name);
+  StubCodeMark mark(this, "StubRoutines", "ghash");
   address start = __ function_entry();
+  Register data    = R3_ARG1;  // byte[] data
+    Register ofs     = R4_ARG2;  // int ofs
+    Register state   = R5_ARG3;  // long[] st
+    Register subkeyH = R6_ARG4;  // long[] subH
+
+  // Temporary registers
+    Register temp1  = R8;
+    Register temp2  = R9;
+    Register temp3  = R10;
+    Register temp4  = R11;
+
+VectorRegister vH        = VR0;
+    VectorRegister vX        = VR1;
+    VectorRegister vH_shift  = VR2;
+    VectorRegister vTmp1     = VR3;
+    VectorRegister vTmp2     = VR4;
+    VectorRegister vTmp3     = VR5;
+    VectorRegister vTmp4     = VR6;
+    VectorRegister vResult   = VR7;
+    VectorRegister vCarry    = VR8;
+    VectorRegister vPerm     = VR9;
+
+  //  const unsigned char constC2[16] = {0xc2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    // Load the address of the constant array into a register
+
+
+    __ li(temp1, 0xc2);
+    __ sldi(temp1, temp1, 56);
+    // Load the vector from memory into vConstC2
+    __ lvx(vX,temp1);
+     __ vxor(vTmp1, vTmp1, vTmp1);
+
+    // Load H into vector registers
+    __ lxvd2x(32 + 1, 0, subkeyH);  // Load H
+    __ vspltisb(vTmp2, 1);
+    __ vspltisb(vTmp3, 7);
+    __ vsldoi(vTmp4, vTmp1, vTmp2, 1);
+    __ vor(vPerm, vCarry, vTmp4);
+    __ vsplt(vTmp1, 0, vH);
+    __ vsl(vH_shift, vH, vTmp2);
+    __ vsrab(vTmp1, vTmp1, vTmp3);
+    __ vand(vTmp1, vTmp1, vPerm);
+    __ vxor(vTmp2, vH_shift, vTmp1);
+    
+    __ vsldoi(vCarry, vTmp1, vPerm, 8);
+    __ vsldoi(vTmp3, vTmp2, vTmp2, 8);
+    __ vsldoi(vTmp1, vTmp1, vTmp3, 8);
+    __ vsldoi(vTmp4, vTmp3, vTmp1, 8);
+
+    // Store shifted H
+    __ stvx(vCarry, 0, temp2);
+    __ stvx(vTmp1, 16, temp2);
+    __ stvx(vTmp3, 32, temp2);
+    __ stvx(vTmp4, 48, temp2);
+      // if(UseNewCode){
+  //   __ unimplemented("ghash");
+  // }
 
   return start;
 
