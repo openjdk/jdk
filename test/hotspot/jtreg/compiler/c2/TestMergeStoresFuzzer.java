6769cd6f974734511b46bd4594a3c7a469c29c8d
Emanuel Peter <emanuel.peter@oracle.com>
2024-07-22 08:44:40 +0200

    making it work

--- test/hotspot/jtreg/compiler/c2/TestMergeStoresFuzzer.java
+++ test/hotspot/jtreg/compiler/c2/TestMergeStoresFuzzer.java
@@ -31,13 +31,7 @@
  * @run driver TestMergeStoresFuzzer
  */
 
-import javax.tools.Diagnostic;
-import javax.tools.DiagnosticCollector;
-import javax.tools.JavaCompiler;
-import javax.tools.JavaCompiler.CompilationTask;
-import javax.tools.JavaFileObject;
-import javax.tools.SimpleJavaFileObject;
-import javax.tools.ToolProvider;
+import compiler.lib.ir_framework.*;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -46,99 +40,114 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import compiler.lib.ir_framework.*;
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 
 public class TestMergeStoresFuzzer {
-  public static void main(String args[]) throws IOException {
-    JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-    DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 
-    StringWriter writer = new StringWriter();
-    PrintWriter out = new PrintWriter(writer);
-    out.println("import compiler.lib.ir_framework.*;");
-    out.println("");
-    out.println("public class HelloWorld {");
-    out.println("    public static void main(String args[]) {");
-    out.println("        System.out.println(\"This is in another java file\");");
-    out.println("        ");
-    out.println("        ");
-    out.println("        System.out.println(Test.class.getName());");
-    out.println("        System.out.println(Test.class);");
-    out.println("        TestFramework.run(HelloWorld.class);");
-    out.println("        System.out.println(\"Done with IR framework.\");");
-    out.println("    }");
-    out.println("");
-    out.println("    @Test"); // TODO: somehow does not find this... why?
-    out.println("    static void test() {");
-    out.println("        throw new RuntimeException(\"xyz\");");
-    out.println("    }");
-    out.println("}");
-    out.close();
-    JavaFileObject file = new JavaSourceFromString("HelloWorld", writer.toString());
-
-    Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
-    List<String> optionList = new ArrayList<String>();
-    optionList.add("-classpath");
-    optionList.add(System.getProperty("java.class.path"));
-    CompilationTask task = compiler.getTask(null, null, diagnostics, optionList, null, compilationUnits);
-
-    System.out.println(Test.class.getName());
-    System.out.println(Test.class);
-    System.out.println("classpath: " + System.getProperty("java.class.path"));
-
-    boolean success = task.call();
-    for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
-      System.out.println(diagnostic.getCode());
-      System.out.println(diagnostic.getKind());
-      System.out.println(diagnostic.getPosition());
-      System.out.println(diagnostic.getStartPosition());
-      System.out.println(diagnostic.getEndPosition());
-      System.out.println(diagnostic.getSource());
-      System.out.println(diagnostic.getMessage(null));
+    public static String generate() throws IOException {
+        StringWriter writer = new StringWriter();
+        PrintWriter out = new PrintWriter(writer);
+        out.println("import compiler.lib.ir_framework.*;");
+        out.println("");
+        out.println("public class HelloWorld {");
+        out.println("    public static void main(String args[]) {");
+        out.println("        System.out.println(\"This is in another java file\");");
+        out.println("        ");
+        out.println("        ");
+        out.println("        System.out.println(Test.class.getName());");
+        out.println("        System.out.println(Test.class);");
+        out.println("        TestFramework.run(HelloWorld.class);");
+        out.println("        System.out.println(\"Done with IR framework.\");");
+        out.println("    }");
+        out.println("");
+        out.println("    @Test");
+        out.println("    static void test() {");
+        out.println("        throw new RuntimeException(\"xyz\");");
+        out.println("    }");
+        out.println("}");
+        out.close();
+        return writer.toString();
     }
 
-    if (success) {
-      System.out.println("Compilation successfull, invoking test...");
-      try {
-          ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
-          // Classpath for all included classes (e.g. IR Framework).
-          URL[] urls = new URL[] { new File("").toURI().toURL(),
-                                   new File(System.getProperty("test.classes")).toURI().toURL()};
-          URLClassLoader classLoader = URLClassLoader.newInstance(urls, sysLoader);
-          Class.forName("HelloWorld", true, classLoader).getDeclaredMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { null });
+    public static void main(String args[]) throws IOException {
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
 
-      } catch (ClassNotFoundException e) {
-        throw new RuntimeException("Class not found:", e);
-      } catch (NoSuchMethodException e) {
-        throw new RuntimeException("No such method:", e);
-      } catch (IllegalAccessException e) {
-        throw new RuntimeException("Illegal access:", e);
-      } catch (InvocationTargetException e) {
-        throw new RuntimeException("Invocation target:", e);
-      }
+        String src = generate();
+        System.out.println(src);
+        JavaFileObject file = new JavaSourceFromString("HelloWorld", src);
 
-      System.out.println("Invocation successful.");
-    } else {
-      System.out.println("Compilation failed.");
-      throw new RuntimeException("Compilation failed.");
+        Iterable<? extends JavaFileObject> compilationUnits = Arrays.asList(file);
+        List<String> optionList = new ArrayList<String>();
+        optionList.add("-classpath");
+        optionList.add(System.getProperty("java.class.path"));
+        optionList.add("-d");
+        optionList.add(System.getProperty("test.classes"));
+        CompilationTask task = compiler.getTask(null, null, diagnostics, optionList, null, compilationUnits);
+
+        System.out.println(Test.class.getName());
+        System.out.println(Test.class);
+        System.out.println("classpath: " + System.getProperty("java.class.path"));
+
+        boolean success = task.call();
+        for (Diagnostic diagnostic : diagnostics.getDiagnostics()) {
+            System.out.println(diagnostic.getCode());
+            System.out.println(diagnostic.getKind());
+            System.out.println(diagnostic.getPosition());
+            System.out.println(diagnostic.getStartPosition());
+            System.out.println(diagnostic.getEndPosition());
+            System.out.println(diagnostic.getSource());
+            System.out.println(diagnostic.getMessage(null));
+        }
+
+        if (success) {
+            System.out.println("Compilation successfull, invoking test...");
+            try {
+                ClassLoader sysLoader = ClassLoader.getSystemClassLoader();
+                // Classpath for all included classes (e.g. IR Framework).
+                URL[] urls = new URL[] { new File("").toURI().toURL(),
+                                         new File(System.getProperty("test.classes")).toURI().toURL()};
+                URLClassLoader classLoader = URLClassLoader.newInstance(urls, sysLoader);
+                Class.forName("HelloWorld", true, classLoader).getDeclaredMethod("main", new Class[] { String[].class }).invoke(null, new Object[] { null });
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException("Class not found:", e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException("No such method:", e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException("Illegal access:", e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException("Invocation target:", e);
+            }
+
+            System.out.println("Invocation successful.");
+        } else {
+            System.out.println("Compilation failed.");
+            throw new RuntimeException("Compilation failed.");
+        }
     }
-  }
 }
 
 class JavaSourceFromString extends SimpleJavaFileObject {
-  final String code;
+    final String code;
 
-  JavaSourceFromString(String name, String code) {
-    super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension), Kind.SOURCE);
-    this.code = code;
-  }
+    JavaSourceFromString(String name, String code) {
+        super(URI.create("string:///" + name.replace('.','/') + Kind.SOURCE.extension), Kind.SOURCE);
+        this.code = code;
+    }
 
-  @Override
-  public CharSequence getCharContent(boolean ignoreEncodingErrors) {
-    return code;
-  }
+    @Override
+    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+        return code;
+    }
 }
