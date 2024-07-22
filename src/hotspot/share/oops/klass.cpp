c252efcb8c10adf3e0595c0d65a0054a2851d527
Andrew Haley <aph@openjdk.org>
2024-07-22 17:48:11 +0100

    Review comments

--- src/hotspot/share/oops/klass.cpp
+++ src/hotspot/share/oops/klass.cpp
@@ -281,7 +281,7 @@ Klass::Klass() : _kind(UnknownKlassKind) {
 // The constructor is also used from CppVtableCloner,
 // which doesn't zero out the memory before calling the constructor.
 Klass::Klass(KlassKind kind) : _kind(kind),
-                               _bitmap(SECONDARY_SUPERS_BITMAP_FULL),
+                               _bitmap(SECONDARY_SUPERS_BITMAP_EMPTY),
                                _shared_class_path_index(-1) {
   CDS_ONLY(_shared_class_flags = 0;)
   CDS_JAVA_HEAP_ONLY(_archived_mirror_index = -1;)
