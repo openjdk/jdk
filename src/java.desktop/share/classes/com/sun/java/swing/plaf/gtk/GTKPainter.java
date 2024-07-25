5c904d9d4d32b067b21ddf0939a5a9fcf4e5dad4
Abhihsek Kumar <abhishek.cx.kumar@oracle.com>
2024-07-25 17:59:25 +0530

    minor spacing and comment fix

--- src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKPainter.java
+++ src/java.desktop/share/classes/com/sun/java/swing/plaf/gtk/GTKPainter.java
@@ -610,10 +610,10 @@ class GTKPainter extends SynthPainter {
                 x + insets.left, y + insets.top, w - insets.left - insets.right,
                 h - insets.top - insets.bottom);
             BufferedImage img = ENGINE.finishPainting();
-            if(!isHW && img != null) {
+            if (!isHW && img != null) {
                 int border = img.getRGB(0, h / 2);
                 if (border == img.getRGB(w / 2, h / 2)) {
-                    // fix no menu borders in Adwaita theme
+                    // fix no menu borders
                     Graphics g2 = img.getGraphics();
                     Color c = new Color(border);
                     g2.setColor(new Color(Math.max((int) (c.getRed() * 0.8), 0),
