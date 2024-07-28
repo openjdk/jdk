f241462c4a1c59d8ff84299da8961618b5e669e4
Shaojin Wen <shaojin.wensj@alibaba-inc.com>
2024-07-28 14:00:53 +0800

    fix mix Integer null error

--- src/java.base/share/classes/java/lang/StringConcatHelper.java
+++ src/java.base/share/classes/java/lang/StringConcatHelper.java
@@ -119,7 +119,7 @@ final class StringConcatHelper {
      * @return            new length and coder
      */
     static long mix(long lengthCoder, Integer value) {
-        return checkOverflow(lengthCoder + DecimalDigits.stringSize(value));
+        return checkOverflow(lengthCoder + (value == null ? 4: DecimalDigits.stringSize(value.intValue())));
     }
 
     /**
