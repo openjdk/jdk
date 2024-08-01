ad15d0a0e81ac94d398e46480f59b2bc2ba64c46
Brian Burkhalter <brian.burkhalter@oracle.com>
2024-08-01 11:06:23 -0700

    8324048: (fc) Make FileKey fields final

--- src/java.base/unix/classes/sun/nio/ch/FileKey.java
+++ src/java.base/unix/classes/sun/nio/ch/FileKey.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,21 +27,29 @@ package sun.nio.ch;
 
 import java.io.FileDescriptor;
 import java.io.IOException;
+import jdk.internal.access.SharedSecrets;
+import jdk.internal.access.JavaIOFileDescriptorAccess;
 
 /*
  * Represents a key to a specific file on Solaris or Linux
  */
 public class FileKey {
 
-    private long st_dev;    // ID of device
-    private long st_ino;    // Inode number
+    private final long st_dev;    // ID of device
+    private final long st_ino;    // Inode number
 
-    private FileKey() { }
+    private FileKey(long st_dev, long st_ino) {
+        this.st_dev = st_dev;
+        this.st_ino = st_ino;
+    }
 
     public static FileKey create(FileDescriptor fd) throws IOException {
-        FileKey fk = new FileKey();
-        fk.init(fd);
-        return fk;
+        JavaIOFileDescriptorAccess access =
+            SharedSecrets.getJavaIOFileDescriptorAccess();
+        int fdVal = access.get(fd);
+        long devIno[] = new long[2];
+        getDevIno(fdVal, devIno);
+        return new FileKey(devIno[0], devIno[1]);
     }
 
     @Override
@@ -59,10 +67,6 @@ public class FileKey {
                 && (this.st_ino == other.st_ino);
     }
 
-    private native void init(FileDescriptor fd) throws IOException;
-    private static native void initIDs();
-
-    static {
-        initIDs();
-    }
+    private static native void getDevIno(int fd, long[] ids)
+        throws IOException;
 }
--- src/java.base/unix/native/libnio/ch/FileKey.c
+++ src/java.base/unix/native/libnio/ch/FileKey.c
@@ -30,29 +30,20 @@
 #include "nio_util.h"
 #include "sun_nio_ch_FileKey.h"
 
-static jfieldID key_st_dev;    /* id for FileKey.st_dev */
-static jfieldID key_st_ino;    /* id for FileKey.st_ino */
-
-
 JNIEXPORT void JNICALL
-Java_sun_nio_ch_FileKey_initIDs(JNIEnv *env, jclass clazz)
-{
-    CHECK_NULL(key_st_dev = (*env)->GetFieldID(env, clazz, "st_dev", "J"));
-    CHECK_NULL(key_st_ino = (*env)->GetFieldID(env, clazz, "st_ino", "J"));
-}
-
-
-JNIEXPORT void JNICALL
-Java_sun_nio_ch_FileKey_init(JNIEnv *env, jobject this, jobject fdo)
+Java_sun_nio_ch_FileKey_getDevIno(JNIEnv* env, jclass clazz, jint fdVal,
+    jlongArray devIno)
 {
     struct stat fbuf;
     int res;
+    long deviceAndInode[2];
 
-    RESTARTABLE(fstat(fdval(env, fdo), &fbuf), res);
+    RESTARTABLE(fstat(fdVal, &fbuf), res);
     if (res < 0) {
         JNU_ThrowIOExceptionWithLastError(env, "fstat failed");
     } else {
-        (*env)->SetLongField(env, this, key_st_dev, (jlong)fbuf.st_dev);
-        (*env)->SetLongField(env, this, key_st_ino, (jlong)fbuf.st_ino);
+        deviceAndInode[0] = (jlong)fbuf.st_dev;
+        deviceAndInode[1] = (jlong)fbuf.st_ino;
+        (*env)->SetLongArrayRegion(env, devIno, 0, 2, deviceAndInode);
     }
 }
