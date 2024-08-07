22c1697398e9781c3fa07ff13a94ca919c456b2f
Coleen Phillimore <coleen.phillimore@oracle.com>
2024-08-07 21:02:18 +0000

    See if GHA compilers like this.

--- src/hotspot/share/runtime/atomic.hpp
+++ src/hotspot/share/runtime/atomic.hpp
@@ -31,8 +31,8 @@
 #include "runtime/orderAccess.hpp"
 #include "utilities/align.hpp"
 #include "utilities/bytes.hpp"
+#include "utilities/checkedCast.hpp"
 #include "utilities/macros.hpp"
-#include "utilities/globalDefinitions.hpp"
 
 #include <type_traits>
 
@@ -1119,7 +1119,7 @@ inline T Atomic::CmpxchgByteUsingInt::operator()(T volatile* dest,
   uint8_t canon_compare_value = compare_value;
   volatile uint32_t* aligned_dest
     = reinterpret_cast<volatile uint32_t*>(align_down(dest, sizeof(uint32_t)));
-  uint32_t offset = pointer_delta_as_int(dest, aligned_dest);
+  uint32_t offset = checked_cast<uint32_t>(pointer_delta(dest, aligned_dest, 1));
 
   uint32_t idx = (Endian::NATIVE == Endian::BIG)
                    ? (sizeof(uint32_t) - 1 - offset)
