d054dc550cfc6cd1c7faf400590db3d088dd7571
Matthias Baesken <mbaesken@openjdk.org>
2024-08-09 06:26:22 +0000

    8338058: map_or_reserve_memory_aligned Windows enhance remap assertion
    Reviewed-by: mdoerr, clanger

--- src/hotspot/os/windows/os_windows.cpp
+++ src/hotspot/os/windows/os_windows.cpp
@@ -3463,6 +3463,7 @@ static char* map_or_reserve_memory_aligned(size_t size, size_t alignment, int fi
   }
 
   assert(aligned_base != nullptr, "Did not manage to re-map after %d attempts?", max_attempts);
+  assert(aligned_base != nullptr, "Did not manage to re-map after %d attempts (size %zu, alignment %zu, file descriptor %d)", max_attempts, size, alignment, file_desc);
 
   return aligned_base;
 }
