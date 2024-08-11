0675d4349c99861f01e6965339b7ba279015ec9c
Chris Hennick <hennickc@amazon.com>
2024-08-10 22:46:08 -0700

    Revert "Fix?"
    This reverts commit 5e29b6f924269410b800c4f5a367d7bc259be5b2.

--- src/java.base/share/classes/jdk/internal/util/random/RandomSupport.java
+++ src/java.base/share/classes/jdk/internal/util/random/RandomSupport.java
@@ -1168,11 +1168,11 @@ public class RandomSupport {
         }
         // We didn't use the upper part of U1 after all.  We'll probably be able to use it later.
         final long maxExtraMinus1;
-        if (maxValue >= MAX_EXPONENTIAL) {
+        if (maxValue >= Math.nextDown(MAX_EXPONENTIAL)) {
             maxExtraMinus1 = Long.MAX_VALUE;
         } else {
-            // Conversion to long rounds toward zero
-            maxExtraMinus1 = (long) (maxValue / DoubleZigguratTables.exponentialX0);
+            // Add 1 because casting to long rounds down, and we want to round up
+            maxExtraMinus1 = ((long) (maxValue / DoubleZigguratTables.exponentialX0)) + 1;
         }
         for (long extra = 0; ; ) {
             // Use Walker's alias method to sample an (unsigned) integer j from a discrete
