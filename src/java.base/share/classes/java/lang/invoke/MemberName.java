779d8be9ecd282821f1611c87dbf52355da6bdf4
Shaojin Wen <shaojin.wensj@alibaba-inc.com>
2024-08-13 05:14:28 +0800

    revert MemberName

--- src/java.base/share/classes/java/lang/invoke/MemberName.java
+++ src/java.base/share/classes/java/lang/invoke/MemberName.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -958,9 +958,7 @@ final class MemberName implements Member, Cloneable {
                 if (m == null && speculativeResolve) {
                     return null;
                 }
-                if (allowedModes != LM_TRUSTED) {
-                    m.ensureTypeVisible(m.getDeclaringClass());
-                }
+                m.ensureTypeVisible(m.getDeclaringClass());
                 m.resolution = null;
             } catch (ClassNotFoundException | LinkageError ex) {
                 // JVM reports that the "bytecode behavior" would get an error
