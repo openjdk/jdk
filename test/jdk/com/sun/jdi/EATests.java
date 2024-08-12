c61943b8cbcd1eb7686c7023393e6b2f377d8fce
Coleen Phillimore <coleen.phillimore@oracle.com>
2024-08-12 19:54:20 +0000

    Update SAP copyright.

--- test/jdk/com/sun/jdi/EATests.java
+++ test/jdk/com/sun/jdi/EATests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020 SAP SE. All rights reserved.
+ * Copyright (c) 2020, 2024 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
