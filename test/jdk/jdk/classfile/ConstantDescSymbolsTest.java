4fb131b7c2cfa03a104a7d320729f14874bfc712
Chen Liang <liach@openjdk.org>
2024-08-15 17:50:34 +0000

    8338406: BytecodeHelpers using wrong bootstrap method descriptor for condy
    Reviewed-by: asotona

--- test/jdk/jdk/classfile/ConstantDescSymbolsTest.java
+++ test/jdk/jdk/classfile/ConstantDescSymbolsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,12 +23,14 @@
 
 /*
  * @test
- * @bug 8304031
- * @summary Testing that primitive class descs are encoded properly as loadable constants.
- * @run junit PrimitiveClassConstantTest
+ * @bug 8304031 8338406
+ * @summary Testing handling of various constant descriptors in ClassFile API.
+ * @run junit ConstantDescSymbolsTest
  */
 
 import java.lang.constant.ClassDesc;
+import java.lang.constant.DynamicConstantDesc;
+import java.lang.constant.MethodHandleDesc;
 import java.lang.constant.MethodTypeDesc;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
@@ -37,18 +39,16 @@ import java.lang.classfile.ClassFile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static java.lang.constant.ConstantDescs.CD_Class;
-import static java.lang.constant.ConstantDescs.CD_Object;
-import static java.lang.constant.ConstantDescs.CD_int;
-import static java.lang.constant.ConstantDescs.CD_long;
-import static java.lang.constant.ConstantDescs.INIT_NAME;
-import static java.lang.constant.ConstantDescs.MTD_void;
 import static java.lang.classfile.ClassFile.ACC_PUBLIC;
+import static java.lang.constant.ConstantDescs.*;
 
-public final class PrimitiveClassConstantTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+final class ConstantDescSymbolsTest {
+
+    // Testing that primitive class descs are encoded properly as loadable constants.
     @Test
-    public void test() throws Throwable {
+    void testPrimitiveClassDesc() throws Throwable {
         ClassDesc ape = ClassDesc.of("Ape");
         var lookup = MethodHandles.lookup();
         Class<?> a = lookup.defineClass(ClassFile.of().build(ape, clb -> {
@@ -73,7 +73,33 @@ public final class PrimitiveClassConstantTest {
         Supplier<?> t = (Supplier<?>) lookup.findConstructor(a, MethodType.methodType(void.class))
                 .asType(MethodType.methodType(Supplier.class))
                 .invokeExact();
-        Assertions.assertSame(int.class, t.get());
+        assertSame(int.class, t.get());
     }
 
+    // Tests that condy symbols with non-static-method bootstraps are using the right lookup descriptor.
+    @Test
+    void testConstantDynamicNonStaticBootstrapMethod() throws Throwable {
+        record CondyBoot(MethodHandles.Lookup lookup, String name, Class<?> type) {}
+        var bootClass = CondyBoot.class.describeConstable().orElseThrow();
+        var bootMhDesc = MethodHandleDesc.ofConstructor(bootClass, CD_MethodHandles_Lookup, CD_String, CD_Class);
+        var condyDesc = DynamicConstantDesc.of(bootMhDesc);
+
+        var targetCd = ClassDesc.of("Bat");
+        var lookup = MethodHandles.lookup();
+        Class<?> a = lookup.defineClass(ClassFile.of().build(targetCd, clb -> {
+            clb.withInterfaceSymbols(Supplier.class.describeConstable().orElseThrow())
+                    .withMethodBody(INIT_NAME, MTD_void, ACC_PUBLIC, cob -> cob
+                            .aload(0).invokespecial(CD_Object, INIT_NAME, MTD_void).return_())
+                    .withMethodBody("get", MethodTypeDesc.of(CD_Object), ACC_PUBLIC, cob -> cob
+                            .loadConstant(condyDesc).areturn());
+        }));
+        @SuppressWarnings("unchecked")
+        Supplier<CondyBoot> t = (Supplier<CondyBoot>) lookup.findConstructor(a, MethodType.methodType(void.class))
+                .asType(MethodType.methodType(Supplier.class)).invokeExact();
+        var cb = t.get();
+        assertEquals(MethodHandles.Lookup.ORIGINAL, cb.lookup.lookupModes() & MethodHandles.Lookup.ORIGINAL);
+        assertSame(a, cb.lookup.lookupClass());
+        assertEquals(DEFAULT_NAME, cb.name);
+        assertEquals(CondyBoot.class, cb.type);
+    }
 }
