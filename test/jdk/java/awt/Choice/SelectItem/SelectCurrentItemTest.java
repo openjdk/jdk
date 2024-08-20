1d6956c64d4d626d31f5fc0ff1ee12285aa5e156
Alexey Ivanov <alexey.ivanov@oracle.com>
2024-08-20 14:25:47 +0100

    Refactor Select{New,Current}ItemTest
    Encapsulate the common logic in SelectCurrentItemTest.
    Provide overridable checkXXX() methods to modify
      conditions.
    Provide an overridable method which defines where
      to click in the choice popup to select an item.

--- test/jdk/java/awt/Choice/SelectItem/SelectCurrentItemTest.java
+++ test/jdk/java/awt/Choice/SelectItem/SelectCurrentItemTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,7 @@ import java.awt.BorderLayout;
 import java.awt.Choice;
 import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.event.InputEvent;
@@ -35,8 +36,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 /*
@@ -45,8 +48,8 @@ import java.util.concurrent.atomic.AtomicReference;
  * @summary Test that selecting the current item does not send an ItemEvent
  * @key headful
  * @run main SelectCurrentItemTest
- */
-public final class SelectCurrentItemTest
+*/
+public class SelectCurrentItemTest
         extends WindowAdapter
         implements ItemListener {
     private static Frame frame;
@@ -57,15 +60,15 @@ public final class SelectCurrentItemTest
     private final CountDownLatch windowOpened = new CountDownLatch(1);
     private final CountDownLatch mouseClicked = new CountDownLatch(1);
 
-    private volatile boolean itemStateChanged;
+    protected final CountDownLatch itemStateChanged = new CountDownLatch(1);
 
-    private SelectCurrentItemTest() throws AWTException {
+    protected SelectCurrentItemTest() throws AWTException {
         robot = new Robot();
-        robot.setAutoDelay(500);
+        robot.setAutoDelay(250);
     }
 
     private void createUI() {
-        frame = new Frame("SelectCurrentItemTest");
+        frame = new Frame(getClass().getName());
         frame.setLayout(new BorderLayout());
 
         choice = new Choice();
@@ -91,7 +94,17 @@ public final class SelectCurrentItemTest
         frame.setVisible(true);
     }
 
-    private void runTest() throws Exception {
+    protected final void runTest()
+            throws InterruptedException, InvocationTargetException {
+        try {
+           doTest();
+        } finally {
+            EventQueue.invokeAndWait(this::dispose);
+        }
+    }
+
+    private void doTest()
+            throws InterruptedException, InvocationTargetException {
         EventQueue.invokeAndWait(this::createUI);
 
         if (!windowOpened.await(2, TimeUnit.SECONDS)) {
@@ -99,30 +112,69 @@ public final class SelectCurrentItemTest
         }
         robot.waitForIdle();
 
-        Rectangle choiceRect = getChoiceRect();
+        final int initialIndex = getSelectedIndex();
+
+        final Rectangle choiceRect = getChoiceRect();
+
+        // Open the choice popup
         robot.mouseMove(choiceRect.x + choiceRect.width - 10,
                         choiceRect.y + choiceRect.height / 2);
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-        if (!mouseClicked.await(2, TimeUnit.SECONDS)) {
+        if (!mouseClicked.await(500, TimeUnit.MILLISECONDS)) {
             throw new RuntimeException("Mouse is not clicked in time");
         }
         robot.waitForIdle();
 
-        robot.mouseMove(choiceRect.x + choiceRect.width / 2,
-                        choiceRect.y + choiceRect.height + 2);
+        // Click an item in the choice popup
+        final Point pt = getClickLocation(choiceRect);
+        robot.mouseMove(pt.x, pt.y);
         robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
         robot.waitForIdle();
 
-        if (itemStateChanged) {
-            throw new RuntimeException("Test failed: itemStateChanged is called");
+        checkItemStateChanged();
+
+        final int currentIndex = getSelectedIndex();
+        System.out.println("initialIndex = " + initialIndex);
+        System.out.println("currentIndex = " + currentIndex);
+        checkSelectedIndex(initialIndex, currentIndex);
+    }
+
+    protected void checkItemStateChanged() throws InterruptedException {
+        if (itemStateChanged.await(500, TimeUnit.MILLISECONDS)) {
+            throw new RuntimeException("ItemEvent is received but unexpected");
         }
     }
 
-    private Rectangle getChoiceRect() throws Exception {
+    protected void checkSelectedIndex(final int initialIndex,
+                                      final int currentIndex) {
+        if (initialIndex != currentIndex) {
+            throw new RuntimeException("Selected index in Choice should not change");
+        }
+    }
+
+    /**
+     * {@return the location for clicking choice popup to select an item}
+     * @param choiceRect the bounds of the Choice component
+     */
+    protected Point getClickLocation(final Rectangle choiceRect) {
+        // Click on the first item in the popup, it's the selected item
+        return new Point(choiceRect.x + choiceRect.width / 2,
+                         choiceRect.y + choiceRect.height + 3);
+    }
+
+    private int getSelectedIndex()
+            throws InterruptedException, InvocationTargetException {
+        AtomicInteger index = new AtomicInteger();
+        EventQueue.invokeAndWait(() -> index.set(choice.getSelectedIndex()));
+        return index.get();
+    }
+
+    private Rectangle getChoiceRect()
+            throws InterruptedException, InvocationTargetException {
         AtomicReference<Rectangle> rect = new AtomicReference<>();
         EventQueue.invokeAndWait(
                 () -> rect.set(new Rectangle(choice.getLocationOnScreen(),
@@ -131,27 +183,23 @@ public final class SelectCurrentItemTest
     }
 
     public static void main(String... args) throws Exception {
-        try {
-            new SelectCurrentItemTest().runTest();
-        } finally {
-            EventQueue.invokeAndWait(SelectCurrentItemTest::dispose);
-        }
+        new SelectCurrentItemTest().runTest();
     }
 
-    private static void dispose() {
+    private void dispose() {
         if (frame != null) {
             frame.dispose();
         }
     }
 
     @Override
-    public void itemStateChanged(ItemEvent e) {
-        System.out.println("ItemEvent received.  Test fails");
-        itemStateChanged = true;
+    public final void itemStateChanged(ItemEvent e) {
+        System.out.println("itemStateChanged: " + e);
+        itemStateChanged.countDown();
     }
 
     @Override
-    public void windowOpened(WindowEvent e) {
+    public final void windowOpened(WindowEvent e) {
         System.out.println("windowActivated()");
         windowOpened.countDown();
     }
--- test/jdk/java/awt/Choice/SelectItem/SelectNewItemTest.java
+++ test/jdk/java/awt/Choice/SelectItem/SelectNewItemTest.java
@@ -22,24 +22,9 @@
  */
 
 import java.awt.AWTException;
-import java.awt.BorderLayout;
-import java.awt.Choice;
-import java.awt.EventQueue;
-import java.awt.Frame;
+import java.awt.Point;
 import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicReference;
 
 /*
  * @test
@@ -49,124 +34,36 @@ import java.util.concurrent.atomic.AtomicReference;
  * @run main SelectNewItemTest
 */
 public final class SelectNewItemTest
-        extends WindowAdapter
-        implements ItemListener {
-    private static Frame frame;
-    private static Choice choice;
-
-    private final Robot robot;
-
-    private final CountDownLatch windowOpened = new CountDownLatch(1);
-    private final CountDownLatch mouseClicked = new CountDownLatch(1);
-    private final CountDownLatch itemStateChanged = new CountDownLatch(1);
+        extends SelectCurrentItemTest {
 
     private SelectNewItemTest() throws AWTException {
-        robot = new Robot();
-        robot.setAutoDelay(500);
+        super();
     }
 
-    private void createUI() {
-        frame = new Frame("SelectNewItemTest");
-        frame.setLayout(new BorderLayout());
-
-        choice = new Choice();
-        for (int i = 0; i < 10; i++) {
-            choice.add("Choice Item " + i);
+    @Override
+    protected void checkItemStateChanged() throws InterruptedException {
+        if (!itemStateChanged.await(500, TimeUnit.MILLISECONDS)) {
+            throw new RuntimeException("ItemEvent is not received");
         }
-        choice.addItemListener(this);
-        choice.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                System.out.println("mouseClicked()");
-                mouseClicked.countDown();
-            }
-        });
-
-        frame.add(choice, BorderLayout.CENTER);
-
-        frame.addWindowListener(this);
-
-        frame.setLocationRelativeTo(null);
-        frame.setResizable(false);
-        frame.pack();
-        frame.setVisible(true);
     }
 
-    private void runTest() throws Exception {
-        EventQueue.invokeAndWait(this::createUI);
-
-        if (!windowOpened.await(2, TimeUnit.SECONDS)) {
-            throw new RuntimeException("Frame is not open in time");
-        }
-        robot.waitForIdle();
-
-        final int selectedIndex = getSelectedIndex();
-
-        final Rectangle choiceRect = getChoiceRect();
-        robot.mouseMove(choiceRect.x + choiceRect.width - 10,
-                        choiceRect.y + choiceRect.height / 2);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-        if (!mouseClicked.await(2, TimeUnit.SECONDS)) {
-            throw new RuntimeException("Mouse is not clicked in time");
-        }
-        robot.waitForIdle();
-
-        robot.mouseMove(choiceRect.x + choiceRect.width / 2,
-                        choiceRect.y + choiceRect.height * 3);
-        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-
-        if (!itemStateChanged.await(2, TimeUnit.SECONDS)) {
-            throw new RuntimeException("ItemEvent is received");
-        }
-
-        if (selectedIndex == getSelectedIndex()) {
+    @Override
+    protected void checkSelectedIndex(final int initialIndex,
+                                      final int currentIndex) {
+        if (initialIndex == currentIndex) {
             throw new RuntimeException("Selected index in Choice should've changed");
         }
     }
 
-    private int getSelectedIndex()
-            throws InterruptedException, InvocationTargetException {
-        AtomicInteger index = new AtomicInteger();
-        EventQueue.invokeAndWait(() -> index.set(choice.getSelectedIndex()));
-        return index.get();
-    }
-
-    private Rectangle getChoiceRect()
-            throws InterruptedException, InvocationTargetException {
-        AtomicReference<Rectangle> rect = new AtomicReference<>();
-        EventQueue.invokeAndWait(
-                () -> rect.set(new Rectangle(choice.getLocationOnScreen(),
-                                             choice.getSize())));
-        return rect.get();
+    @Override
+    protected Point getClickLocation(final Rectangle choiceRect) {
+        // Click a different item the popup, not the first one
+        return new Point(choiceRect.x + choiceRect.width / 2,
+                         choiceRect.y + choiceRect.height * 3);
     }
 
     public static void main(String... args) throws Exception {
-        try {
-            new SelectNewItemTest().runTest();
-        } finally {
-            EventQueue.invokeAndWait(SelectNewItemTest::dispose);
-        }
-    }
-
-    private static void dispose() {
-        if (frame != null) {
-            frame.dispose();
-        }
-    }
-
-    @Override
-    public void itemStateChanged(ItemEvent e) {
-        System.out.println("itemStateChanged: " + e);
-        itemStateChanged.countDown();
-    }
-
-    @Override
-    public void windowOpened(WindowEvent e) {
-        System.out.println("windowActivated()");
-        windowOpened.countDown();
+        new SelectNewItemTest().runTest();
     }
 
 }
