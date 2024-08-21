c50ea137ecb9c41d2115b8b957819cf247001e76
Simon Tooke <stooke@redhat.com>
2024-08-21 08:30:16 -0400

    changes from tstuefe review

--- src/hotspot/os/windows/memMapPrinter_windows.cpp
+++ src/hotspot/os/windows/memMapPrinter_windows.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2023, 2024, Red Hat, Inc. and/or its affiliates.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Red Hat, Inc. and/or its affiliates.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,139 +28,122 @@
 #include "nmt/memMapPrinter.hpp"
 #include "runtime/os.hpp"
 
-#include <limits>
-#include <iostream>
-
+#include <limits.h>
 #include <winnt.h>
 #include <memoryapi.h>
 #include <psapi.h>
 
+/* maximum number of mapping records returned */
+static const int MAX_REGIONS_RETURNED = 1000000;
+
 class MappingInfo {
-    static const int MAX_STR_LEN = 20;
 public:
-    char apBuffer[MAX_STR_LEN];
-    char stateBuffer[MAX_STR_LEN];
-    char protectBuffer[MAX_STR_LEN];
-    char typeBuffer[MAX_STR_LEN];
-    char fileName[MAX_PATH];
+  stringStream _ap_buffer;
+  stringStream _state_buffer;
+  stringStream _protect_buffer;
+  stringStream _type_buffer;
+  char _file_name[MAX_PATH];
 
-    MappingInfo() {}
+  MappingInfo() {}
 
-    void process(MEMORY_BASIC_INFORMATION& mInfo) {
-        getProtectString(apBuffer, sizeof(apBuffer), mInfo.AllocationProtect);
-        getStateString(stateBuffer, sizeof(stateBuffer), mInfo);
-        getProtectString(protectBuffer, sizeof(protectBuffer), mInfo.Protect);
-        getTypeString(typeBuffer, sizeof(typeBuffer), mInfo);
-        fileName[0] = 0;
-        if (mInfo.Type == MEM_IMAGE) {
-            HMODULE hModule = 0;
-            if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, static_cast<LPCSTR>(mInfo.AllocationBase), &hModule)) {
-                GetModuleFileName(hModule, fileName, sizeof(fileName));
-            }
-        }
+  void process(MEMORY_BASIC_INFORMATION& mem_info) {
+    _ap_buffer.reset();
+    _state_buffer.reset();
+    _protect_buffer.reset();
+    _type_buffer.reset();
+    get_protect_string(_ap_buffer, mem_info.AllocationProtect);
+    get_state_string(_state_buffer, mem_info);
+    get_protect_string(_protect_buffer,  mem_info.Protect);
+    get_type_string(_type_buffer, mem_info);
+    _file_name[0] = 0;
+    if (mem_info.Type == MEM_IMAGE) {
+      HMODULE hModule = 0;
+      if (GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS, static_cast<LPCSTR>(mem_info.AllocationBase), &hModule)) {
+        GetModuleFileName(hModule, _file_name, sizeof(_file_name));
+      }
     }
+  }
 
-    const char* getProtectString(char* buffer, size_t bufsiz, DWORD prot) {
-        const int IR = 0;
-        const int IW = 1;
-        const int IX = 2;
-        const int IP = 3;
-        int idx = 4;
-        strncpy_s(buffer, bufsiz, "----  ", 7);
-        if (prot & PAGE_EXECUTE) {
-            buffer[IX] = 'x';
-        } else if (prot & PAGE_EXECUTE_READ) {
-            buffer[IR] = 'r';
-            buffer[IX] = 'x';
-        } else if (prot & PAGE_EXECUTE_READWRITE) {
-            buffer[IR] = 'r';
-            buffer[IW] = 'w';
-            buffer[IX] = 'x';
-        } else if (prot & PAGE_EXECUTE_WRITECOPY) {
-            buffer[IW] = 'w';
-            buffer[IX] = 'x';
-            buffer[idx++] = 'c';
-        } else if (prot & PAGE_NOACCESS) {
-            strncpy_s(buffer, bufsiz, "(NA)", 5);
-        } else if (prot & PAGE_READONLY) {
-            buffer[IR] = 'r';
-        } else if (prot & PAGE_READWRITE) {
-            buffer[IR] = 'r';
-            buffer[IW] = 'w';
-        } else if (prot & PAGE_WRITECOPY) {
-            buffer[IW] = 'w';
-            buffer[idx++] = 'c';
-        } else if (prot & PAGE_TARGETS_INVALID) {
-            buffer[idx++] = 'i';
-        } else if (prot & PAGE_TARGETS_NO_UPDATE) {
-            buffer[idx++] = 'n';
-        } else if (prot != 0) {
-            snprintf(buffer, bufsiz, "(0x%x)", prot);
-            idx = static_cast<int>(strlen(buffer));
-        }
+  void get_protect_string(outputStream& out, DWORD prot) {
+    const char read_c = prot & (PAGE_READONLY | PAGE_READWRITE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_WRITECOPY) ? 'r' : '-';
+    const char write_c = prot & (PAGE_READWRITE | PAGE_WRITECOPY | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY) ? 'w' : '-';
+    const char execute_c = prot & (PAGE_EXECUTE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE | PAGE_EXECUTE_WRITECOPY) ? 'x' : '-';
+    out.print("%c%c%c", read_c, write_c, execute_c);
+    if (prot & (PAGE_WRITECOPY | PAGE_EXECUTE_WRITECOPY)) {
+      out.put('c');
+    }
+    if (prot & PAGE_GUARD) {
+      out.put('g');
+    }
+    if (prot & PAGE_NOCACHE) {
+      out.put('n');
+    }
+    if (prot & PAGE_WRITECOMBINE) {
+      out.put('W');
+    }
+    const DWORD bits = PAGE_READONLY | PAGE_READWRITE | PAGE_EXECUTE_READ | PAGE_EXECUTE_READWRITE 
+                        | PAGE_WRITECOPY | PAGE_EXECUTE_WRITECOPY | PAGE_EXECUTE 
+                        | PAGE_GUARD | PAGE_NOCACHE | PAGE_WRITECOMBINE;
+    
+    assert((proc & bits) == proc, "Unknown Windows memory protection value");
+  }
 
-        if (prot & PAGE_GUARD) {
-            buffer[idx++] = 'G';
-        }
-        if (prot & PAGE_NOCACHE) {
-            buffer[idx++] = 'C';
-        }
-        if (prot & PAGE_WRITECOMBINE) {
-            buffer[idx++] = 'W';
-        }
-        buffer[idx] = 0;
-        return buffer;
+  void get_state_string(outputStream& out, MEMORY_BASIC_INFORMATION& mem_info) {
+    if (mem_info.State == MEM_COMMIT) {
+      out.put('c');
+    } else if (mem_info.State == MEM_FREE) {
+      out.put('f');
+    } else if (mem_info.State == MEM_RESERVE) {
+      out.put('r');
+    } else {
+      out.print("0x%x", mem_info.State);
     }
-    const char* getStateString(char* buffer, size_t bufsiz, MEMORY_BASIC_INFORMATION& mInfo) {
-        if (mInfo.State == MEM_COMMIT) {
-            buffer[0] = 'c'; buffer[1] = 0;
-        } else if (mInfo.State == MEM_FREE) {
-            buffer[0] = 'f'; buffer[1] = 0;
-        } else if (mInfo.State == MEM_RESERVE) {
-            buffer[0] = 'r'; buffer[1] = 0;
-        } else {
-            snprintf(buffer, bufsiz, "0x%x", mInfo.State);
-        }
-        return buffer;
-    }
+  }
 
-    const char* getTypeString(char* buffer, size_t bufsiz, MEMORY_BASIC_INFORMATION& mInfo) {
-        if (mInfo.Type == MEM_IMAGE) {
-            strncpy_s(buffer, bufsiz, "img", 4);
-        } else if (mInfo.Type == MEM_MAPPED) {
-            strncpy_s(buffer, bufsiz, "map", 4);
-        } else if (mInfo.Type == MEM_PRIVATE) {
-            strncpy_s(buffer, bufsiz, "pvt", 4);
-        } else {
-            snprintf(buffer, bufsiz, "0x%x", mInfo.Type);
-        }
-        return buffer;
+  void get_type_string(outputStream& out, MEMORY_BASIC_INFORMATION& mem_info) {
+    if (mem_info.Type == MEM_IMAGE) {
+      out.print("img");
+    } else if (mem_info.Type == MEM_MAPPED) {
+      out.print("map");
+    } else if (mem_info.Type == MEM_PRIVATE) {
+      out.print("pvt");
+    } else {
+      out.print("0x%x", mem_info.Type);
     }
+  }
 };
 
 class MappingInfoSummary {
   unsigned _num_mappings;
   size_t _total_region_size;  // combined resident set size
   size_t _total_committed;    // combined committed size
-  size_t _total_reserved;     // combined shared size
+  class WinOsInfo : public os::win32 {
+    public:
+      static void printOsInfo(outputStream* st) {
+        st->print("OS:");
+        os::win32::print_windows_version(st);
+        os::win32::print_uptime_info(st);
+        VM_Version::print_platform_virtualization_info(st);
+      }
+  };
 public:
   MappingInfoSummary() : _num_mappings(0),  _total_region_size(0),
-                      _total_committed(0), _total_reserved(0) {}
+                      _total_committed(0) {}
 
-  void add_mapping(const MEMORY_BASIC_INFORMATION& mInfo, const MappingInfo& info) {
-    if (mInfo.State != MEM_FREE) {
+  void add_mapping(const MEMORY_BASIC_INFORMATION& mem_info, const MappingInfo& mapping_info) {
+    if (mem_info.State != MEM_FREE) {
       _num_mappings++;
-      _total_region_size += mInfo.RegionSize;
-      _total_committed += mInfo.State == MEM_COMMIT ? mInfo.RegionSize : 0;
-      _total_reserved += mInfo.State == MEM_RESERVE ? mInfo.RegionSize : 0;
+      _total_region_size += mem_info.RegionSize;
+      _total_committed += mem_info.State == MEM_COMMIT ? mem_info.RegionSize : 0;
     }
   }
 
   void print_on(const MappingPrintSession& session) const {
     outputStream* st = session.out();
-    os::print_os_info(st);
+    WinOsInfo::printOsInfo(st);
     os::print_memory_info(st);
-    st->print_cr("current process reserved memory: " PROPERFMT, PROPERFMTARGS(_total_reserved));
+    st->print_cr("current process reserved memory: " PROPERFMT, PROPERFMTARGS(_total_region_size));
+    st->print_cr("current process committed memory: " PROPERFMT, PROPERFMTARGS(_total_committed));
   }
 };
 
@@ -171,26 +154,26 @@ public:
     _session(session)
   {}
 
-  void print_single_mapping(const MEMORY_BASIC_INFORMATION& mInfo, const MappingInfo& info) const {
+  void print_single_mapping(const MEMORY_BASIC_INFORMATION& mem_info, const MappingInfo& mapping_info) const {
     outputStream* st = _session.out();
 #define INDENT_BY(n)          \
   if (st->fill_to(n) == 0) {  \
     st->print(" ");           \
   }
-    st->print(PTR_FORMAT "-" PTR_FORMAT, mInfo.BaseAddress, static_cast<const char*>(mInfo.BaseAddress) + mInfo.RegionSize);
+    st->print(PTR_FORMAT "-" PTR_FORMAT, mem_info.BaseAddress, static_cast<const char*>(mem_info.BaseAddress) + mem_info.RegionSize);
     INDENT_BY(38);
-    st->print("%12zu", mInfo.RegionSize);
+    st->print("%12zu", mem_info.RegionSize);
     INDENT_BY(51);
-    st->print("%s", info.protectBuffer);
+    st->print("%s", mapping_info._protect_buffer.base());
     INDENT_BY(57);
-    st->print("%s-%s", info.stateBuffer, info.typeBuffer);
+    st->print("%s-%s", mapping_info._state_buffer.base(), mapping_info._type_buffer.base());
     INDENT_BY(60);
-    st->print("%#9llx", reinterpret_cast<const unsigned long long>(mInfo.BaseAddress) - reinterpret_cast<const unsigned long long>(mInfo.AllocationBase));
+    st->print("%#9llx", reinterpret_cast<const unsigned long long>(mem_info.BaseAddress) - reinterpret_cast<const unsigned long long>(mem_info.AllocationBase));
     INDENT_BY(72);
-    if (_session.print_nmt_info_for_region(mInfo.BaseAddress, static_cast<const char*>(mInfo.BaseAddress) + mInfo.RegionSize)) {
+    if (_session.print_nmt_info_for_region(mem_info.BaseAddress, static_cast<const char*>(mem_info.BaseAddress) + mem_info.RegionSize)) {
       st->print(" ");
     }
-    st->print_raw(info.fileName);
+    st->print_raw(mapping_info._file_name);
   #undef INDENT_BY
     st->cr();
   }
@@ -201,11 +184,9 @@ public:
     st->print_cr("prot:    protection:");
     st->print_cr("             rwx: read / write / execute");
     st->print_cr("             c: copy on write");
-    st->print_cr("             G: guard");
-    st->print_cr("             C: no cache");
+    st->print_cr("             g: guard");
+    st->print_cr("             n: no cache");
     st->print_cr("             W: write combine");
-    st->print_cr("             i: targets invalid");
-    st->print_cr("             n: targets noupdate");
     st->print_cr("state:   region state and type:");
     st->print_cr("             state: committed / reserved");
     st->print_cr("             type: image / mapped / private");
@@ -229,28 +210,37 @@ public:
 
 void MemMapPrinter::pd_print_all_mappings(const MappingPrintSession& session) {
 
-    HANDLE hProcess = GetCurrentProcess();
+  HANDLE hProcess = GetCurrentProcess();
 
-    MappingInfoPrinter printer(session);
-    MappingInfoSummary summary;
+  MappingInfoPrinter printer(session);
+  MappingInfoSummary summary;
 
-    outputStream* const st = session.out();
+  outputStream* const st = session.out();
 
-    printer.print_legend();
-    st->cr();
-    printer.print_header();
+  printer.print_legend();
+  st->cr();
+  printer.print_header();
 
-    MEMORY_BASIC_INFORMATION mInfo;
-    MappingInfo info;
+  MEMORY_BASIC_INFORMATION mem_info;
+  MappingInfo mapping_info;
 
-    for (char* ptr = 0; VirtualQueryEx(hProcess, ptr, &mInfo, sizeof(mInfo)) == sizeof(mInfo); ptr += mInfo.RegionSize) {
-      info.process(mInfo);
-      if (mInfo.State != MEM_FREE) {
-        printer.print_single_mapping(mInfo, info);
-        summary.add_mapping(mInfo, info);
-      }
+  int region_count = 0;
+  ::memset(&mem_info, 0, sizeof(mem_info));
+  for (char* ptr = 0; VirtualQueryEx(hProcess, ptr, &mem_info, sizeof(mem_info)) == sizeof(mem_info); ) {
+    assert(mem_info.RegionSize > 0);
+    if (++region_count > MAX_REGIONS_RETURNED) {
+      st->print_cr("limit of %d regions reached (results inaccurate)", region_count);
+      break;
     }
-    st->cr();
-    summary.print_on(session);
-    st->cr();
+    mapping_info.process(mem_info);
+    if (mem_info.State != MEM_FREE) {
+      printer.print_single_mapping(mem_info, mapping_info);
+      summary.add_mapping(mem_info, mapping_info);
+    }
+    ptr += mem_info.RegionSize;
+    ::memset(&mem_info, 0, sizeof(mem_info));
+  }
+  st->cr();
+  summary.print_on(session);
+  st->cr();
 }
