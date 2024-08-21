b6531578d39d7eb5d4109c5f511de00f4e0a849d
Hamlin-Li <leeming1509@outlook.com>
2024-08-21 07:30:10 +0000

    fix warning

--- src/hotspot/share/adlc/output_h.cpp
+++ src/hotspot/share/adlc/output_h.cpp
@@ -1556,7 +1556,7 @@ void ArchDesc::declareClasses(FILE *fp) {
     }
 
     fprintf(fp,"  MachOper *opnd_array(uint operand_index) const {\n");
-    fprintf(fp,"    assert(operand_index < (_num_opnds + _num_opnds_unexpanded), \"invalid _opnd_array index\");\n");
+    fprintf(fp,"    assert(operand_index < (uint)(_num_opnds + _num_opnds_unexpanded), \"invalid _opnd_array index\");\n");
     fprintf(fp,"    return _opnd_array[operand_index];\n");
     fprintf(fp,"  }\n");
     fprintf(fp,"  void      set_opnd_array(uint operand_index, MachOper *operand) {\n");
