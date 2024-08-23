678eb13ff122741edac133db0148d0ca795585f2
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-08-23 10:13:03 +0100

    8190329: [macos] Swing InterOp Platform.exit() crash

--- src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/AWTWindow.m
@@ -36,6 +36,8 @@
 #import "ThreadUtilities.h"
 #import "JNIUtilities.h"
 
+extern JavaVM *jvm;
+
 #define MASK(KEY) \
     (sun_lwawt_macosx_CPlatformWindow_ ## KEY)
 
@@ -631,6 +633,11 @@ AWT_ASSERT_APPKIT_THREAD;
 - (void) _deliverMoveResizeEvent {
 AWT_ASSERT_APPKIT_THREAD;
 
+    JNIEnv *pEnv = nil;
+    jint ret = (*jvm)->GetEnv(jvm, &pEnv, JNI_VERSION_1_4);
+    if (ret == JNI_EDETACHED) {
+        return;
+    }
     // deliver the event if this is a user-initiated live resize or as a side-effect
     // of a Java initiated resize, because AppKit can override the bounds and force
     // the bounds of the window to avoid the Dock or remain on screen.
