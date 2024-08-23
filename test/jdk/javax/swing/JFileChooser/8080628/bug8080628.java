892a84374f4cfaf58e78c039429e0139ec3dfd58
Abhihsek Kumar <abhishek.cx.kumar@oracle.com>
2024-08-23 14:48:38 +0530

    Review comment update

--- test/jdk/javax/swing/JFileChooser/8080628/bug8080628.java
+++ test/jdk/javax/swing/JFileChooser/8080628/bug8080628.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -114,4 +114,5 @@ public class bug8080628 {
             exception = e;
         }
     }
+
 }
