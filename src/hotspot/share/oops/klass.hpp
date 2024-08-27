94413cd111c82749738af044a30e0ac83bb2f3e4
Coleen Phillimore <coleen.phillimore@oracle.com>
2024-08-27 17:21:54 +0000

    With JDK-8338929 we don't need is_in_class_space().

--- src/hotspot/share/oops/klass.hpp
+++ src/hotspot/share/oops/klass.hpp
@@ -211,8 +211,6 @@ protected:
 
   virtual bool is_klass() const { return true; }
 
-  bool is_in_class_space() const { return !is_interface() && !is_abstract(); }
-
   // super() cannot be InstanceKlass* -- Java arrays are covariant, and _super is used
   // to implement that. NB: the _super of "[Ljava/lang/Integer;" is "[Ljava/lang/Number;"
   // If this is not what your code expects, you're probably looking for Klass::java_super().
