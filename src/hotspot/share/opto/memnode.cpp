c6a278b016a05ef921938803b6edf178990d784f
Coleen Phillimore <coleen.phillimore@oracle.com>
2024-08-27 17:36:11 +0000

    The test compiler/types/TestSubTypeCheckNewObjectNotConstant.java asserts because the opcode is Op_LoadUB.  No idea why.

--- src/hotspot/share/opto/memnode.cpp
+++ src/hotspot/share/opto/memnode.cpp
@@ -1949,7 +1949,7 @@ LoadNode::load_array_final_field(const TypeKlassPtr *tkls,
   if (tkls->offset() == in_bytes(Klass::misc_flags_offset())) {
     // The field is Klass::_misc_flags.  Return its (constant) value.
     // (Folds up the 2nd indirection in Reflection.getClassAccessFlags(aClassConstant).)
-    assert(this->Opcode() == Op_LoadB, "must load an byte from _misc_flags");
+    assert(this->Opcode() == Op_LoadB || this->Opcode() == Op_LoadUB, "must load a byte from _misc_flags");
     return TypeInt::make(klass->misc_flags());
   }
   if (tkls->offset() == in_bytes(Klass::layout_helper_offset())) {
