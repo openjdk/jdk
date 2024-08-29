acbabc91917925608c65e140ef5204c5d2dab475
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-08-29 07:20:40 +0100

    Flip default owner flag

--- src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
+++ src/java.desktop/macosx/native/libosxapp/ThreadUtilities.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2013, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,7 @@
 
 // The following must be named "jvm", as there are extern references to it in AWT
 JavaVM *jvm = NULL;
-static BOOL isNSApplicationOwner = YES;
+static BOOL isNSApplicationOwner = NO;
 static JNIEnv *appKitEnv = NULL;
 static jobject appkitThreadGroup = NULL;
 static NSString* JavaRunLoopMode = @"AWTRunLoopMode";
