a4ce2b832ef2e4cf3f4758f94a66e0bb5cc2b546
Eirik Bjorsnos <eirbjo@gmail.com>
2024-08-30 12:46:55 +0200

    Use {@code InputStream}

--- src/java.base/share/classes/java/util/zip/GZIPInputStream.java
+++ src/java.base/share/classes/java/util/zip/GZIPInputStream.java
@@ -38,8 +38,8 @@ import java.util.Objects;
  * the GZIP file format.
  *
  * <p>
- * The InputStream passed to the constructor of this class may represent a
- * single GZIP file or multiple consecutive GZIP files. When the end of a
+ * The {@code InputStream} passed to the constructor of this class may represent
+ * a single GZIP file or multiple consecutive GZIP files. When the end of a
  * GZIP file is immediately followed by a new GZIP file, this class continues
  * to decode compressed data into a single, concatenated stream of uncompressed
  * data. Otherwise, any additional trailing bytes following a GZIP file are
