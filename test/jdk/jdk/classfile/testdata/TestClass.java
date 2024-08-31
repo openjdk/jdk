2a26400e42feb2befb315e6eb94b42bc9c7b4d02
Nizar Benalla <nizar.benalla@oracle.com>
2024-08-31 17:44:13 +0100

    drop unused test file

--- test/jdk/jdk/classfile/testdata/TestClass.java
+++ test/jdk/jdk/classfile/testdata/TestClass.java
@@ -9,43 +9,6 @@ import java.lang.annotation.*;
 @interface FunctionalInterface {
 }
 
-@Deprecated
-public class TestClass extends BaseClass implements Serializable {
-
-    public static final int STATIC_FIELD = 42;
-
-    private String instanceField;
-
-    public TestClass(String instanceField) {
-        this.instanceField = instanceField;
-    }
-
-    public static void staticMethod() {
-        System.out.println(".");
-    }
-
-    @Override
-    public String toString() {
-        return "field:" + instanceField;
-    }
-
-    public void riskyMethod() throws Exception {
-        throw new Exception(".");
-    }
-
-    public enum Status {
-        UNSTARTED,
-        PASS,
-        FAIL;
-    }
-
-    public class InnerClass {
-        public void innerMethod() {
-            System.out.println(".");
-        }
-    }
-}
-
 class BaseClass {
     protected void baseMethod() {
         System.out.println(".");
