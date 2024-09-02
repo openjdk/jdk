206a67ba8ceb75d8c8a3b7d0a5addbf285448f22
Casper Norrbin <casper.norrbin@oracle.com>
2024-09-02 11:19:00 +0200

    align overflow check

--- test/hotspot/gtest/utilities/test_align.cpp
+++ test/hotspot/gtest/utilities/test_align.cpp
@@ -30,7 +30,7 @@
 #include <limits>
 
 // A few arbitrarily chosen values to test the align functions on.
-static constexpr uint64_t values[] = {1, 3, 10, 345, 1023, 1024, 1025, 23909034, INT_MAX, uint64_t(-1) / 2, uint64_t(-1) / 2 + 100, uint64_t(-1)};
+static constexpr uint64_t values[] = {1, 3, 10, 345, 1023, 1024, 1025, 23909034, INT_MAX, uint64_t(-1) / 2, uint64_t(-1) / 2 + 100, ~(uint64_t(1) << 62)};
 
 template <typename T>
 static constexpr T max_alignment() {
