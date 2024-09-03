73eead7fdc6e8516125b9e1755bd71db03fe7c73
suchismith1993 <suchismith1993@gmail.com>
2024-09-03 11:01:26 -0500

    cast

--- src/hotspot/cpu/ppc/macroAssembler_ppc.hpp
+++ src/hotspot/cpu/ppc/macroAssembler_ppc.hpp
@@ -364,13 +364,12 @@ class MacroAssembler: public Assembler {
   // Call a C function via a function descriptor and use full C
   // calling conventions. Updates and returns _last_calls_return_pc.
   address call_c(Register function_descriptor);
+    // For tail calls: only branch, don't link, so callee returns to caller of this function.
+  address call_c_and_return_to_caller(Register function_descriptor);
   address call_c(const FunctionDescriptor* function_descriptor, relocInfo::relocType rt);
   address call_c(address function_entry, relocInfo::relocType rt = relocInfo::none) {
     return call_c((const FunctionDescriptor*)function_entry, rt);
   }
-    // For tail calls: only branch, don't link, so callee returns to caller of this function.
-  address call_c_and_return_to_caller(Register function_descriptor);
-  address call_c(const FunctionDescriptor* function_descriptor, relocInfo::relocType rt);
   address call_c_using_toc(const FunctionDescriptor* function_descriptor, relocInfo::relocType rt,
                            Register toc);
 #endif
