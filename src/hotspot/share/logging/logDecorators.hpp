4ba0588f0af121130c96431efe9a3e96d2358ff6
Antón Seoane Ampudia <anton.seoane.ampudia@oracle.com>
2024-09-09 12:58:36 +0200

    Fix compilation issue in Windows/Linux

--- src/hotspot/share/logging/logDecorators.hpp
+++ src/hotspot/share/logging/logDecorators.hpp
@@ -133,8 +133,8 @@ class LogDecorators {
 
   template<typename... Decorators>
   static uint mask_from_decorators(LogDecorators::Decorator first, Decorators... rest) {
-    uint bitmask = 0;
-    for (const LogDecorators::Decorator& decorator : (LogDecorators::Decorator[]){first, rest...}) {
+    LogDecorators::Decorator decorators[1 + sizeof...(rest)] = { first, rest... };
+    for (const auto decorator : decorators) {
       if (decorator == NoDecorators) return 0;
       bitmask |= mask(decorator);
     }
