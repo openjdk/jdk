4c7f181de2d8b05a9c43c976e691f7886e9500c6
gerard ziemski <63425797+gerard-ziemski@users.noreply.github.com>
2024-09-12 10:17:44 -0500

    Johan's feedback

--- src/hotspot/share/gc/shenandoah/shenandoahTaskqueue.inline.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahTaskqueue.inline.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2024, Red Hat, Inc. All rights reserved.
+ * Copyright (c) 2016, 2019, Red Hat, Inc. All rights reserved.
  * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
