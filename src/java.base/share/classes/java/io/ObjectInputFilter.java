89c172ac47a9cc238739338417015bf912ad5424
Joe Darcy <darcy@openjdk.org>
2024-09-13 16:49:28 +0000

    8340082: Use inline return tag in java.base
    Reviewed-by: iris, prappo, lancea, djelinski, naoto, liach

--- src/java.base/share/classes/java/io/ObjectInputFilter.java
+++ src/java.base/share/classes/java/io/ObjectInputFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1218,8 +1218,7 @@ public interface ObjectInputFilter {
             }
 
             /**
-             * Returns the pattern used to create this filter.
-             * @return the pattern used to create this filter
+             * {@return the pattern used to create this filter}
              */
             @Override
             public String toString() {
