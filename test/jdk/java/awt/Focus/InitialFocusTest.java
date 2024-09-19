bc9b4804a50bc835ab04372723c936c0891562b8
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-09-19 06:12:31 +0100

    8339995: Open source several AWT focus tests - series 6

--- test/jdk/java/awt/Focus/InitialFocusTest.java
+++ test/jdk/java/awt/Focus/InitialFocusTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,123 +22,86 @@
  */
 
 /*
-  @test
-  @bug 4150021
-  @summary if user requests focus on some component, it must become a focus owner after activation
-  @key headful
-  @run main InitialFocusTest
+ * @test
+ * @bug 4688591
+ * @summary Tab key hangs in Native Print Dialog on win32
+ * @library /open/test/jdk/java/awt/regtesthelpers
+ * @build PassFailJFrame
+ * @run main/manual InitialFocusTest
 */
 
-import java.awt.AWTException;
-import java.awt.Button;
-import java.awt.DefaultKeyboardFocusManager;
-import java.awt.Dimension;
-import java.awt.EventQueue;
+import java.awt.BorderLayout;
+import java.awt.Dialog;
 import java.awt.FlowLayout;
 import java.awt.Frame;
-import java.awt.Insets;
-import java.awt.Point;
-import java.awt.Robot;
-import java.awt.event.InputEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.lang.reflect.InvocationTargetException;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.awt.JobAttributes;
+import java.awt.PageAttributes;
+import java.awt.PrintJob;
+import java.awt.Toolkit;
 
-public class InitialFocusTest implements PropertyChangeListener {
-    //Declare things used in the test, like buttons and labels here
-    final static String FOCUSED_WINDOW_PROP = "focusedWindow";
-    final static int ACTION_TIMEOUT = 2000;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    volatile Frame frame;
-    volatile Button btn1;
-    volatile Button btn2;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-    public void start() throws InterruptedException, InvocationTargetException {
-        DefaultKeyboardFocusManager.getCurrentKeyboardFocusManager().
-                addPropertyChangeListener(FOCUSED_WINDOW_PROP, this);
-        try {
-            EventQueue.invokeAndWait(() -> {
-                frame = new Frame("InitialFocusTest");
-                frame.setLayout(new FlowLayout());
-                btn1 = new Button("First Button");
-                frame.add(btn1);
-                btn2 = new Button("Second Button");
-                frame.add(btn2);
-                frame.setLocationRelativeTo(null);
-                frame.pack();
-                frame.setVisible(true);
-            });
-            try {
-                Robot robot = new Robot();
-                robot.delay(ACTION_TIMEOUT);
-                if (!activateFrame(frame, robot, ACTION_TIMEOUT)) {
-                    throw new RuntimeException("Frame was not activated.");
-                }
-                robot.delay(ACTION_TIMEOUT);
-                AtomicBoolean isFocused = new AtomicBoolean(false);
-                EventQueue.invokeAndWait(() -> {
-                    isFocused.set(frame.isFocused());
-                });
-                if (!isFocused.get()) {
-                    throw new RuntimeException("Frame didn't become focused.");
-                }
-                EventQueue.invokeAndWait(() -> {
-                    isFocused.set(btn2.isFocusOwner());
-                });
-                if (!isFocused.get()) {
-                    throw new RuntimeException("Btn2 didn't receive focus.");
-                }
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
-            System.out.printf("Test passed.");
-        } finally {
-            if (frame != null) {
-                EventQueue.invokeAndWait(frame::dispose);
-            }
-        }
+public class InitialFocusTest {
+
+    private static final String INSTRUCTIONS = """
+            After the tests starts you will see a frame titled "PrintTest".
+            Press the "Print" button and the print dialog should appear.
+            If you are able to transfer focus between components of the Print dialog
+            using the TAB key, then the test passes else the test fails.
+
+            Note: close the Print dialog before clicking on "Pass" or "Fail" buttons.""";
+
+
+    public static void main(String[] args) throws Exception {
+        PassFailJFrame.builder()
+                .title("InitialFocusTest Instructions")
+                .instructions(INSTRUCTIONS)
+                .rows((int) INSTRUCTIONS.lines().count() + 2)
+                .columns(35)
+                .testUI(InitialFocusTest::createTestUI)
+                .build()
+                .awaitAndCheck();
     }
 
-    public void propertyChange(PropertyChangeEvent pce) {
-        if (FOCUSED_WINDOW_PROP.equals(pce.getPropertyName())) {
-            if (pce.getNewValue() == frame) {
-                System.out.println("requesting focus on btn2");
-                btn2.requestFocusInWindow();
-            }
-        }
-    }
+    private static JFrame createTestUI() {
+        return new PrintTest();
 
-    boolean activateFrame(Frame frame, Robot robot, int timeout)
-            throws InterruptedException, InvocationTargetException {
-        AtomicBoolean isActive = new AtomicBoolean(false);
-        EventQueue.invokeAndWait(() -> {
-            isActive.set(frame.isActive());
-        });
-        if (!isActive.get()) {
-            int[] point = new int[2];
-            EventQueue.invokeAndWait(() -> {
-                Point origin = frame.getLocationOnScreen();
-                Dimension dim = frame.getSize();
-                Insets insets = frame.getInsets();
-                point[0] = origin.x + dim.width / 2;
-                point[1] = origin.y + insets.top / 2;
-            });
-            robot.mouseMove(point[0], point[1]);
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-            robot.waitForIdle();
-            robot.delay(timeout);
-            EventQueue.invokeAndWait(() -> {
-                isActive.set(frame.isActive());
-            });
-        }
-        return frame.isActive();
-    }
-
-    public static void main(String[] args) throws InterruptedException,
-            InvocationTargetException {
-        InitialFocusTest test = new InitialFocusTest();
-        test.start();
     }
 }
+
+class PrintTest extends JFrame implements ActionListener {
+
+    JButton b;
+    JobAttributes jbattrib;
+    Toolkit tk ;
+    PageAttributes pgattrib;
+
+    public PrintTest() {
+        setTitle("PrintTest");
+        setSize(500, 400);
+
+        b = new JButton("Print");
+        jbattrib = new JobAttributes();
+        tk = Toolkit.getDefaultToolkit();
+        pgattrib = new PageAttributes();
+        getContentPane().setLayout(new FlowLayout());
+        getContentPane().add(b);
+
+        b.addActionListener(this);
+
+    }
+
+    public void actionPerformed(ActionEvent ae) {
+        if(ae.getSource()==b)
+            jbattrib.setDialog(JobAttributes.DialogType.NATIVE);
+
+        PrintJob pjob = tk.getPrintJob(this, "Printing Test",
+                                       jbattrib, pgattrib);
+
+    }
+}
+
