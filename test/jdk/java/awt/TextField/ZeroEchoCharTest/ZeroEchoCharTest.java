fdc16a373459cb2311316448c765b1bee5c73694
Phil Race <prr@openjdk.org>
2024-09-19 22:20:05 +0000

    8340480: Bad copyright notices in changes from JDK-8339902
    Reviewed-by: kcr, bpb, kizune

--- test/jdk/java/awt/TextField/ZeroEchoCharTest/ZeroEchoCharTest.java
+++ test/jdk/java/awt/TextField/ZeroEchoCharTest/ZeroEchoCharTest.java
@@ -2,7 +2,6 @@
  * Copyright (c) 2000, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
- * summary:
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
  * published by the Free Software Foundation.
