7c31ea8a07218af566379b3959b87ff4b0c43ff1
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2024-09-20 04:57:37 +0100

    8340367: Opensource few AWT image tests

--- test/jdk/java/awt/image/ImageOffsetTest.java
+++ test/jdk/java/awt/image/ImageOffsetTest.java
@@ -1,3 +1,5 @@
+/* 
+ * Copyright (c) 2000, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +25,7 @@
  * @test
  * @bug 4259548
  * @summary tests that MemoryImageSource correctly handles images with offsets
- * @library /open/test/jdk/java/awt/regtesthelpers
+ * @library /java/awt/regtesthelpers
  * @build PassFailJFrame
  * @run main/manual ImageOffsetTest
  */
