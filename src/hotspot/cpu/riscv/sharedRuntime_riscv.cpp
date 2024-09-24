7719b5cfd24c630bbdfdb8411e88955154540eae
Ubuntu <ubuntu@ip-172-31-2-236.us-east-2.compute.internal>
2024-09-24 19:07:02 +0000

    use all arg v regs

--- src/hotspot/cpu/riscv/sharedRuntime_riscv.cpp
+++ src/hotspot/cpu/riscv/sharedRuntime_riscv.cpp
@@ -666,13 +666,13 @@ AdapterHandlerEntry* SharedRuntime::generate_i2c2i_adapters(MacroAssembler *masm
 int SharedRuntime::vector_calling_convention(VMRegPair *regs,
                                              uint num_bits,
                                              uint total_args_passed) {
-  // More than 8 argument inputs are not supported for now.
   assert(total_args_passed <= Argument::n_vector_register_parameters_c, "unsupported");
   assert(num_bits >= 64 && num_bits <= 2048 && is_power_of_2(num_bits), "unsupported");
 
   // check more info at https://github.com/riscv-non-isa/riscv-elf-psabi-doc/blob/master/riscv-cc.adoc
   static const VectorRegister VEC_ArgReg[Argument::n_vector_register_parameters_c] = {
-    v8, v9, v10, v11, v12, v13, v14, v15
+    v8, v9, v10, v11, v12, v13, v14, v15,
+    v16, v17, v18, v19, v20, v21, v22, v23
   };
 
   const int next_reg_val = 3;
