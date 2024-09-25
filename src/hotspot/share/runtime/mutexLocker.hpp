c087ff7f14add4cff64b2f5a500bc188cbae8f13
olivergillespie <62296743+olivergillespie@users.noreply.github.com>
2024-09-25 09:55:42 +0100

    Improve comment
    Co-authored-by: David Holmes <62092539+dholmes-ora@users.noreply.github.com>

--- src/hotspot/share/runtime/mutexLocker.hpp
+++ src/hotspot/share/runtime/mutexLocker.hpp
@@ -62,7 +62,7 @@ extern Monitor* CodeCache_lock;                  // a lock on the CodeCache
 extern Mutex*   TouchedMethodLog_lock;           // a lock on allocation of LogExecutedMethods info
 extern Mutex*   RetData_lock;                    // a lock on installation of RetData inside method data
 extern Monitor* VMOperation_lock;                // a lock on queue of vm_operations waiting to execute
-extern Monitor* ThreadsLockThrottle_lock;        // used by Thread start/stop to reduce competition for Threads_lock,
+extern Monitor* ThreadsLockThrottle_lock;        // used by Thread start/exit to reduce competition for Threads_lock,
                                                  // so a VM thread calling a safepoint is prioritized
 extern Monitor* Threads_lock;                    // a lock on the Threads table of active Java threads
                                                  // (also used by Safepoints too to block threads creation/destruction)
