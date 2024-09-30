a8017a8cccc9a01d34a7812e78cf4e35276a046a
Jan Lahoda <jan.lahoda@oracle.com>
2024-09-30 13:30:16 +0200

    Updating copyright years.

--- test/langtools/jdk/jshell/AnalyzeSnippetTest.java
+++ test/langtools/jdk/jshell/AnalyzeSnippetTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8182270
+ * @bug 8182270 8341176
  * @summary test non-eval Snippet analysis
  * @build KullaTesting TestingInputStream
  * @run testng AnalyzeSnippetTest
