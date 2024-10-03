e0ae1cc9f77131651d42ce6a1becbe494ecaac57
Attila Szegedi <szegedia@gmail.com>
2024-10-03 21:26:38 +0200

    - Rename test to more descriptive - Add a test case for whole list sorting

--- test/jdk/java/util/ArrayList/SortingModCount.java
+++ test/jdk/java/util/ArrayList/SortingModCount.java
@@ -23,6 +23,7 @@
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
 /**
@@ -31,12 +32,28 @@ import java.util.List;
  * @summary ConcurrentModificationException when sorting ArrayList sublists
  */
 
-public class Bug8340572 {
+public class SortingModCount {
     public static void main(String[] args) {
+        testSortingSubListsDoesNotIncrementModCount();
+        testSortingListDoesIncrementModCount();
+    }
+
+    private static void testSortingSubListsDoesNotIncrementModCount() {
         List<Integer> l = new ArrayList<>(List.of(1, 2, 3, 4));
         var a = l.subList(0, 2);
         var b = l.subList(2, 4);
         Collections.sort(a);
         Collections.sort(b);
+    }
+
+    private static void testSortingListDoesIncrementModCount() {
+        List<Integer> l = new ArrayList<>(List.of(1, 2, 3, 4));
+        var b = l.subList(2, 4);
+        Collections.sort(l);
+        try {
+            Collections.sort(b);
+            throw new Error("expected ConcurrentModificationException not thrown");
+        } catch (ConcurrentModificationException expected) {
+        }
     }
 }
