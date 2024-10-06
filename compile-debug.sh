feb0b227776b7521950d3ad714dff25cd534e2dc
viqueen <hasnae@viqueen.org>
2024-10-06 17:51:55 +0200

    set testng into the debug classpath

--- compile-debug.sh
+++ compile-debug.sh
@@ -3,5 +3,6 @@
 target=$(find build -name *fastdebug)
 
 ./"${target}"/images/jdk/bin/javac \
+  -classpath ../jtreg/build/deps/testng/testng-7.3.0.jar \
   -J-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 \
   ./test/jdk/java/util/Map/SyntaxSugarMapTest.java
