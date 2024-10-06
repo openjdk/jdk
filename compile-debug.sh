9b901940c83d13a2311e5ca327ede94c814e10dd
viqueen <hasnae@viqueen.org>
2024-10-06 16:06:24 +0200

    whitelabel to 2024

--- compile-debug.sh
+++ compile-debug.sh
@@ -2,6 +2,6 @@
 
 target=$(find build -name *fastdebug)
 
-./${target}/images/jdk/bin/javac \
+./"${target}"/images/jdk/bin/javac \
   -J-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005 \
   ./test/jdk/java/util/Map/SyntaxSugarMapTest.java
