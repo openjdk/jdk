8a2c9bd4c2c88e9d173c14d34f068aaa45d875a7
Kangcheng Xu <kxu@redhat.com>
2024-10-07 14:03:28 -0400

    verify results with custom test methods

--- test/hotspot/jtreg/compiler/c2/TestSerialAdditions.java
+++ test/hotspot/jtreg/compiler/c2/TestSerialAdditions.java
@@ -23,8 +23,12 @@
 
 package compiler.c2;
 
-import compiler.lib.ir_framework.*;
 import compiler.lib.ir_framework.Test;
+import compiler.lib.ir_framework.*;
+import jdk.test.lib.Asserts;
+import jdk.test.lib.Utils;
+
+import java.util.Random;
 
 /*
  * @test
@@ -34,229 +38,220 @@ import compiler.lib.ir_framework.Test;
  * @run driver compiler.c2.TestSerialAdditions
  */
 public class TestSerialAdditions {
+    private static final Random RNG = Utils.getRandomInstance();
+
     public static void main(String[] args) {
         TestFramework.run();
     }
 
-    @DontInline
-    private static void verifyResult(int base, int factor, int observed) {
-        int expected = base * factor; // compute expected result here while making sure not inlined in callers
-        if (expected != observed) {
-            throw new AssertionError("Expected " + expected + " but got " + observed);
+    @Run(test = { //
+            "addTo2", //
+            "addTo3", //
+            "addTo4", //
+            "shiftAndAddTo4", //
+            "mulAndAddTo4", //
+            "addTo5", //
+            "addTo6", //
+            "addTo7", //
+            "addTo8", //
+            "addTo16", //
+            "addAndShiftTo16", //
+            "addTo42", //
+            "mulAndAddTo42", //
+            "mulAndAddToMax", //
+            "mulAndAddToOverflow", //
+            "mulAndAddToZero", //
+            "mulAndAddToMinus1", //
+            "mulAndAddToMinus42" //
+    })
+    private void runIntTests() {
+        for (int a : new int[] { 0, 1, Integer.MIN_VALUE, Integer.MAX_VALUE, RNG.nextInt() }) {
+            Asserts.assertEQ(a * 2, addTo2(a));
+            Asserts.assertEQ(a * 3, addTo3(a));
+            Asserts.assertEQ(a * 4, addTo4(a));
+            Asserts.assertEQ(a * 4, shiftAndAddTo4(a));
+            Asserts.assertEQ(a * 4, mulAndAddTo4(a));
+            Asserts.assertEQ(a * 5, addTo5(a));
+            Asserts.assertEQ(a * 6, addTo6(a));
+            Asserts.assertEQ(a * 7, addTo7(a));
+            Asserts.assertEQ(a * 8, addTo8(a));
+            Asserts.assertEQ(a * 16, addTo16(a));
+            Asserts.assertEQ(a * 16, addAndShiftTo16(a));
+            Asserts.assertEQ(a * 42, addTo42(a));
+            Asserts.assertEQ(a * 42, mulAndAddTo42(a));
+            Asserts.assertEQ(a * Integer.MAX_VALUE, mulAndAddToMax(a));
+            Asserts.assertEQ(a * Integer.MIN_VALUE, mulAndAddToOverflow(a));
+            Asserts.assertEQ(0, mulAndAddToZero(a));
+            Asserts.assertEQ(a * -1, mulAndAddToMinus1(a));
+            Asserts.assertEQ(a * -42, mulAndAddToMinus42(a));
         }
     }
 
-    @DontInline
-    private static void verifyResult(long base, long factor, long observed) {
-        long expected = base * factor;
-        if (expected != observed) {
-            throw new AssertionError("Expected " + expected + " but got " + observed);
+    @Run(test = { //
+            "mulAndAddToIntOverflowL", //
+            "mulAndAddToMaxL", //
+            "mulAndAddToOverflowL" //
+    })
+    private void runLongTests() {
+        for (long a : new long[] { 0, 1, Long.MIN_VALUE, Long.MAX_VALUE, RNG.nextLong() }) {
+            Asserts.assertEQ(a * (Integer.MAX_VALUE + 1L), mulAndAddToIntOverflowL(a));
+            Asserts.assertEQ(a * Long.MAX_VALUE, mulAndAddToMaxL(a));
+            Asserts.assertEQ(a * Long.MIN_VALUE, mulAndAddToOverflowL(a));
         }
     }
 
     // ----- integer tests -----
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(counts = { IRNode.ADD_I, "1" })
-    @IR(failOn = {IRNode.LSHIFT_I})
-    private static void addTo2(int a) {
-        int sum = a + a; // Simple additions like a + a should be kept as-is
-        verifyResult(a, 2, sum);
+    @IR(failOn = IRNode.LSHIFT_I)
+    private static int addTo2(int a) {
+        return a + a; // Simple additions like a + a should be kept as-is
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
-    @IR(counts = { IRNode.ADD_I, "1" })
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addTo3(int a) {
-        int sum = a + a + a; // a*3 => (a<<1) + a
-        verifyResult(a, 3, sum);
+    @IR(counts = { IRNode.ADD_I, "1", IRNode.LSHIFT_I, "1" })
+    private static int addTo3(int a) {
+        return a + a + a; // a*3 => (a<<1) + a
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addTo4(int a) {
-        int sum = a + a + a + a; // a*4 => a<<2
-        verifyResult(a, 4, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int addTo4(int a) {
+        return a + a + a + a; // a*4 => a<<2
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void shiftAndAddTo4(int a) {
-        int sum = (a << 1) + a + a; // a*2 + a + a => a*3 + a => a*4 => a<<2
-        verifyResult(a, 4, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int shiftAndAddTo4(int a) {
+        return (a << 1) + a + a; // a*2 + a + a => a*3 + a => a*4 => a<<2
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void mulAndAddTo4(int a) {
-        int sum = a * 3 + a; // a*4 => a<<2
-        verifyResult(a, 4, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int mulAndAddTo4(int a) {
+        return a * 3 + a; // a*4 => a<<2
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
-    @IR(counts = { IRNode.ADD_I, "1" })
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addTo5L(int a) {
-        int sum = a + a + a + a + a; // a*5 => (a<<2) + a
-        verifyResult(a, 5, sum);
+    @IR(counts = { IRNode.ADD_I, "1", IRNode.LSHIFT_I, "1" })
+    private static int addTo5(int a) {
+        return a + a + a + a + a; // a*5 => (a<<2) + a
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
-    @IR(counts = { IRNode.ADD_I, "1" })
-    @IR(counts = {IRNode.LSHIFT_I, "2"})
-    private static void addTo6L(int a) {
-        int sum = a + a + a + a + a + a; // a*6 => (a<<1) + (a<<2)
-        verifyResult(a, 6, sum);
+    @IR(counts = { IRNode.ADD_I, "1", IRNode.LSHIFT_I, "2" })
+    private static int addTo6(int a) {
+        return a + a + a + a + a + a; // a*6 => (a<<1) + (a<<2)
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    @IR(counts = {IRNode.SUB_I, "1"})
-    private static void addTo7(int a) {
-        int sum = a + a + a + a + a + a + a; // a*7 => (a<<3) - a
-        verifyResult(a, 7, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1", IRNode.SUB_I, "1" })
+    private static int addTo7(int a) {
+        return a + a + a + a + a + a + a; // a*7 => (a<<3) - a
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addTo8(int a) {
-        int sum = a + a + a + a + a + a + a + a; // a*8 => a<<3
-        verifyResult(a, 8, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int addTo8(int a) {
+        return a + a + a + a + a + a + a + a; // a*8 => a<<3
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addTo16(int a) {
-        int sum = a + a + a + a + a + a + a + a + a + a
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int addTo16(int a) {
+        return a + a + a + a + a + a + a + a + a + a //
                 + a + a + a + a + a + a; // a*16 => a<<4
-        verifyResult(a, 16, sum);
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = {IRNode.LSHIFT_I, "1"})
-    private static void addAndShiftTo16(int a) {
-        int sum = (a + a) << 3; // a<<(3 + 1) => a<<4
-        verifyResult(a, 16, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1" })
+    private static int addAndShiftTo16(int a) {
+        return (a + a) << 3; // a<<(3 + 1) => a<<4
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
     @IR(counts = { IRNode.MUL_I, "1" })
-    private static void addTo42(int a) {
-        int sum = a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
-                + a + a + a + a + a + a + a + a + a + a
+    private static int addTo42(int a) {
+        return a + a + a + a + a + a + a + a + a + a //
+                + a + a + a + a + a + a + a + a + a + a //
+                + a + a + a + a + a + a + a + a + a + a //
+                + a + a + a + a + a + a + a + a + a + a //
                 + a + a; // a*42
-        verifyResult(a, 42, sum);
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
     @IR(counts = { IRNode.MUL_I, "1" })
-    private static void mulAndAddTo42(int a) {
-        int sum = a * 40 + a + a; // a*41 + a => a*42
-        verifyResult(a, 42, sum);
+    private static int mulAndAddTo42(int a) {
+        return a * 40 + a + a; // a*41 + a => a*42
     }
 
     private static final int INT_MAX_MINUS_ONE = Integer.MAX_VALUE - 1;
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = { IRNode.LSHIFT_I, "1" })
-    @IR(counts = {IRNode.SUB_I, "1"})
-    private static void mulAndAddToMax(int a) {
-        int sum = a * INT_MAX_MINUS_ONE + a; // a*MAX => a*(MIN-1) => a*MIN - 1 => (a<<31) - 1
-        verifyResult(a, Integer.MAX_VALUE, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1", IRNode.SUB_I, "1" })
+    private static int mulAndAddToMax(int a) {
+        return a * INT_MAX_MINUS_ONE + a; // a*MAX => a*(MIN-1) => a*MIN - a => (a<<31) - a
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
     @IR(counts = { IRNode.LSHIFT_I, "1" })
-    private static void mulAndAddToOverflow(int a) {
-        int sum = a * Integer.MAX_VALUE + a; // a*(MAX+1) => a*(MIN) => a<<31
-        verifyResult(a, Integer.MIN_VALUE, sum);
+    private static int mulAndAddToOverflow(int a) {
+        return a * Integer.MAX_VALUE + a; // a*(MAX+1) => a*(MIN) => a<<31
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
     @IR(counts = { IRNode.CON_I, "1" })
-    private static void mulAndAddToZero(int a) {
-        int sum = a*-1 + a; // 0
-        verifyResult(a, 0, sum);
+    private static int mulAndAddToZero(int a) {
+        return a * -1 + a; // 0
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
-    @IR(counts = { IRNode.LSHIFT_I, "1" })
-    @IR(counts = { IRNode.SUB_I, "1" })
-    private static void mulAndAddToMinus1(int a) {
-        int sum = a*-2 + a; // a*-1 => a - (a<<1)
-        verifyResult(a, -1, sum);
+    @IR(counts = { IRNode.LSHIFT_I, "1", IRNode.SUB_I, "1" })
+    private static int mulAndAddToMinus1(int a) {
+        return a * -2 + a; // a*-1 => a - (a<<1)
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_I)
     @IR(counts = { IRNode.MUL_I, "1" })
-    private static void mulAndAddToMinus42(int a) {
-        int sum = a*-43 + a; // a*-42
-        verifyResult(a, -42, sum);
+    private static int mulAndAddToMinus42(int a) {
+        return a * -43 + a; // a*-42
     }
 
     // --- long tests ---
-    private static final long INT_MAX_PLUS_ONE = (long) Integer.MAX_VALUE + 1;
-
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_L)
     @IR(counts = { IRNode.LSHIFT_L, "1" })
-    private static void mulAndAddToIntOverflowL(long a) {
-        long sum = a * Integer.MAX_VALUE + a; // a*(INT_MAX+1)
-        verifyResult(a, INT_MAX_PLUS_ONE, sum);
+    private static long mulAndAddToIntOverflowL(long a) {
+        return a * Integer.MAX_VALUE + a; // a*(INT_MAX+1)
     }
 
     private static final long LONG_MAX_MINUS_ONE = Long.MAX_VALUE - 1;
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_L)
-    @IR(counts = { IRNode.LSHIFT_L, "1" })
-    @IR(counts = {IRNode.SUB_L, "1"})
-    private static void mulAndAddToMaxL(long a) {
-        long sum = a * LONG_MAX_MINUS_ONE + a; // a*MAX => a*(MIN-1) => a*MIN - 1 => (a<<63) - 1
-        verifyResult(a, Long.MAX_VALUE, sum);
+    @IR(counts = { IRNode.LSHIFT_L, "1", IRNode.SUB_L, "1" })
+    private static long mulAndAddToMaxL(long a) {
+        return a * LONG_MAX_MINUS_ONE + a; // a*MAX => a*(MIN-1) => a*MIN - 1 => (a<<63) - 1
     }
 
     @Test
-    @Arguments(values = {Argument.RANDOM_EACH})
     @IR(failOn = IRNode.ADD_L)
     @IR(counts = { IRNode.LSHIFT_L, "1" })
-    private static void mulAndAddToOverflowL(long a) {
-        long sum = a * Long.MAX_VALUE + a; // a*(MAX+1) => a*(MIN) => a<<63
-        verifyResult(a, Long.MIN_VALUE, sum);
+    private static long mulAndAddToOverflowL(long a) {
+        return a * Long.MAX_VALUE + a; // a*(MAX+1) => a*(MIN) => a<<63
     }
 }
