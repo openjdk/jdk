1c7639858b4d5c35f66b2b2663d830d65ff3e421
Aleksey Shipilev <shipilev@amazon.de>
2024-10-08 16:58:54 +0200

    Attempt to share truncation code

--- .github/scripts/gen-build-failure-report.sh
+++ .github/scripts/gen-build-failure-report.sh
@@ -24,6 +24,9 @@
 # questions.
 #
 
+# Import common utils
+. report-utils.sh
+
 GITHUB_STEP_SUMMARY="$1"
 BUILD_DIR="$(ls -d build/*)"
 
@@ -66,16 +69,4 @@ hs_err_files=$(ls make/hs_err*.log 2> /dev/null || true)
   echo ':arrow_right: To see the entire test log, click the job in the list to the left. To download logs, see the `failure-logs` [artifact above](#artifacts).'
 ) >> $GITHUB_STEP_SUMMARY
 
-# With large hs_errs, the summary can easily exceed 1024 kB, the limit set by Github
-# Trim it down if so.
-summary_size=$(wc -c < $GITHUB_STEP_SUMMARY)
-if [[ $summary_size -gt 1000000 ]]; then
-  # Trim to below 1024 kB, and cut off after the last detail group
-  head -c 1000000 $GITHUB_STEP_SUMMARY | tac | sed -n -e '/<\/details>/,$ p' | tac > $GITHUB_STEP_SUMMARY.tmp
-  mv $GITHUB_STEP_SUMMARY.tmp $GITHUB_STEP_SUMMARY
-  (
-    echo ''
-    echo ':x: **WARNING: Summary is too large and has been truncated.**'
-    echo ''
-  )  >> $GITHUB_STEP_SUMMARY
-fi
+truncate_summary
