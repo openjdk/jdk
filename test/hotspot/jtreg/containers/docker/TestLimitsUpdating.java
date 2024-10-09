75001218022f43384f5c0f33a01305935920f037
Ramkumar Sunderbabu <ramkumar.sunderbabu@oracle.com>
2024-10-09 18:41:33 +0530

    corrected the copyright headers

--- test/hotspot/jtreg/containers/docker/TestLimitsUpdating.java
+++ test/hotspot/jtreg/containers/docker/TestLimitsUpdating.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2023, Red Hat, Inc.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
--- test/lib/jdk/test/lib/Container.java
+++ test/lib/jdk/test/lib/Container.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019, Red Hat Inc.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
