45333b945a7d65e286ce2eee6d5a1e9012a7040d
MBaesken <matthias.baesken@sap.com>
2024-10-10 10:47:11 +0200

    JDK-8341820

--- src/jdk.hotspot.agent/linux/native/libsaproc/symtab.c
+++ src/jdk.hotspot.agent/linux/native/libsaproc/symtab.c
@@ -389,9 +389,9 @@ static struct symtab* build_symtab_internal(int fd, const char *filename, bool t
         goto bad;
       }
 
-      // int rslt =
-      hcreate_r(htab_sz, symtab->hash_table);
-      // guarantee(rslt, "unexpected failure: hcreate_r");
+      if (hcreate_r(htab_sz, symtab->hash_table) == 0) {
+        goto bad;
+      }
 
       // shdr->sh_link points to the section that contains the actual strings
       // for symbol names. the st_name field in ELF_SYM is just the
