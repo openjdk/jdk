c9560127634aab56a3fb1cccf3ef1d272f8ae4c7
Galder Zamarreño <galder@openjdk.org>
2024-10-10 11:06:06 +0200

    Encapsulate benchmark state within an inner class

--- test/micro/org/openjdk/bench/java/lang/MinMaxLoopBench.java
+++ test/micro/org/openjdk/bench/java/lang/MinMaxLoopBench.java
@@ -22,157 +22,158 @@ import java.util.stream.LongStream;
 @Fork(2)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode(Mode.Throughput)
-@State(Scope.Thread)
 public class MinMaxLoopBench
 {
-    @Param({"100", "1000", "10000"})
-    int size;
+    @State(Scope.Thread)
+    public static final class LoopState {
+        @Param({"100", "1000", "10000"})
+        int size;
 
-    /**
-     * Probability of one of the min/max branches being taken.
-     * For max, this value represents the percentage of branches in which
-     * the value will be bigger or equal than the current max.
-     * For min, this value represents the percentage of branches in which
-     * the value will be smaller or equal than the current min.
-     */
-    @Param({"50", "80", "100"})
-    int probability;
+        /**
+         * Probability of one of the min/max branches being taken.
+         * For max, this value represents the percentage of branches in which
+         * the value will be bigger or equal than the current max.
+         * For min, this value represents the percentage of branches in which
+         * the value will be smaller or equal than the current min.
+         */
+        @Param({"50", "80", "100"})
+        int probability;
 
-    int[] minIntA;
-    int[] minIntB;
-    long[] minLongA;
-    long[] minLongB;
-    int[] maxIntA;
-    int[] maxIntB;
-    long[] maxLongA;
-    long[] maxLongB;
-    int[] resultIntArray;
-    long[] resultLongArray;
+        int[] minIntA;
+        int[] minIntB;
+        long[] minLongA;
+        long[] minLongB;
+        int[] maxIntA;
+        int[] maxIntB;
+        long[] maxLongA;
+        long[] maxLongB;
+        int[] resultIntArray;
+        long[] resultLongArray;
 
-    @Setup
-    public void setup() {
-        final long[][] longs = distributeLongRandomIncrement(size, probability);
-        maxLongA = longs[0];
-        maxLongB = longs[1];
-        maxIntA = toInts(maxLongA);
-        maxIntB = toInts(maxLongB);
-        minLongA = negate(maxLongA);
-        minLongB = negate(maxLongB);
-        minIntA = toInts(minLongA);
-        minIntB = toInts(minLongB);
-        resultIntArray = new int[size];
-        resultLongArray = new long[size];
-    }
+        @Setup
+        public void setup() {
+            final long[][] longs = distributeLongRandomIncrement(size, probability);
+            maxLongA = longs[0];
+            maxLongB = longs[1];
+            maxIntA = toInts(maxLongA);
+            maxIntB = toInts(maxLongB);
+            minLongA = negate(maxLongA);
+            minLongB = negate(maxLongB);
+            minIntA = toInts(minLongA);
+            minIntB = toInts(minLongB);
+            resultIntArray = new int[size];
+            resultLongArray = new long[size];
+        }
+        static long[] negate(long[] nums) {
+            return LongStream.of(nums).map(l -> -l).toArray();
+        }
 
-    static long[] negate(long[] nums) {
-        return LongStream.of(nums).map(l -> -l).toArray();
-    }
+        static int[] toInts(long[] nums) {
+            return Arrays.stream(nums).mapToInt(i -> (int) i).toArray();
+        }
 
-    static int[] toInts(long[] nums) {
-        return Arrays.stream(nums).mapToInt(i -> (int) i).toArray();
-    }
+        static long[][] distributeLongRandomIncrement(int size, int probability) {
+            long[][] result;
+            int aboveCount, abovePercent;
 
-    static long[][] distributeLongRandomIncrement(int size, int probability) {
-        long[][] result;
-        int aboveCount, abovePercent;
+            // Iterate until you find a set that matches the requirement probability
+            do {
+                long max = ThreadLocalRandom.current().nextLong(10);
+                result = new long[2][size];
+                result[0][0] = max;
+                result[1][0] = max - 1;
 
-        // Iterate until you find a set that matches the requirement probability
-        do {
-            long max = ThreadLocalRandom.current().nextLong(10);
-            result = new long[2][size];
-            result[0][0] = max;
-            result[1][0] = max - 1;
-
-            aboveCount = 0;
-            for (int i = 1; i < result[0].length; i++) {
-                long value;
-                if (ThreadLocalRandom.current().nextLong(101) <= probability) {
-                    long increment = ThreadLocalRandom.current().nextLong(10);
-                    value = max + increment;
-                    aboveCount++;
-                } else {
-                    // Decrement by at least 1
-                    long decrement = ThreadLocalRandom.current().nextLong(10) + 1;
-                    value = max - decrement;
+                aboveCount = 0;
+                for (int i = 1; i < result[0].length; i++) {
+                    long value;
+                    if (ThreadLocalRandom.current().nextLong(101) <= probability) {
+                        long increment = ThreadLocalRandom.current().nextLong(10);
+                        value = max + increment;
+                        aboveCount++;
+                    } else {
+                        // Decrement by at least 1
+                        long decrement = ThreadLocalRandom.current().nextLong(10) + 1;
+                        value = max - decrement;
+                    }
+                    result[0][i] = value;
+                    result[1][i] = max;
+                    max = Math.max(max, value);
                 }
-                result[0][i] = value;
-                result[1][i] = max;
-                max = Math.max(max, value);
-            }
 
-            abovePercent = ((aboveCount + 1) * 100) / size;
-        } while (abovePercent != probability);
+                abovePercent = ((aboveCount + 1) * 100) / size;
+            } while (abovePercent != probability);
 
-        return result;
-    }
-
-    @Benchmark
-    public int[] intLoopMin() {
-        for (int i = 0; i < size; i++) {
-            resultIntArray[i] = Math.min(minIntA[i], minIntB[i]);
+            return result;
         }
-        return resultIntArray;
     }
 
     @Benchmark
-    public int[] intLoopMax() {
-        for (int i = 0; i < size; i++) {
-            resultIntArray[i] = Math.max(maxIntA[i], maxIntB[i]);
+    public int[] intLoopMin(LoopState state) {
+        for (int i = 0; i < state.size; i++) {
+            state.resultIntArray[i] = Math.min(state.minIntA[i], state.minIntB[i]);
         }
-        return resultIntArray;
+        return state.resultIntArray;
     }
 
     @Benchmark
-    public int intReductionMin() {
+    public int[] intLoopMax(LoopState state) {
+        for (int i = 0; i < state.size; i++) {
+            state.resultIntArray[i] = Math.max(state.maxIntA[i], state.maxIntB[i]);
+        }
+        return state.resultIntArray;
+    }
+
+    @Benchmark
+    public int intReductionMin(LoopState state) {
         int result = 0;
-        for (int i = 0; i < size; i++) {
-            final int v = 11 * minIntA[i];
+        for (int i = 0; i < state.size; i++) {
+            final int v = 11 * state.minIntA[i];
             result = Math.min(result, v);
         }
         return result;
     }
 
     @Benchmark
-    public int intReductionMax() {
+    public int intReductionMax(LoopState state) {
         int result = 0;
-        for (int i = 0; i < size; i++) {
-            final int v = 11 * maxIntA[i];
+        for (int i = 0; i < state.size; i++) {
+            final int v = 11 * state.maxIntA[i];
             result = Math.max(result, v);
         }
         return result;
     }
 
     @Benchmark
-    public long[] longLoopMin() {
-        for (int i = 0; i < size; i++) {
-            resultLongArray[i] = Math.min(minLongA[i], minLongB[i]);
+    public long[] longLoopMin(LoopState state) {
+        for (int i = 0; i < state.size; i++) {
+            state.resultLongArray[i] = Math.min(state.minLongA[i], state.minLongB[i]);
         }
-        return resultLongArray;
+        return state.resultLongArray;
     }
 
     @Benchmark
-    public long[] longLoopMax() {
-        for (int i = 0; i < size; i++) {
-            resultLongArray[i] = Math.max(maxLongA[i], maxLongB[i]);
+    public long[] longLoopMax(LoopState state) {
+        for (int i = 0; i < state.size; i++) {
+            state.resultLongArray[i] = Math.max(state.maxLongA[i], state.maxLongB[i]);
         }
-        return resultLongArray;
+        return state.resultLongArray;
     }
 
     @Benchmark
-    public long longReductionMin() {
+    public long longReductionMin(LoopState state) {
         long result = 0;
-        for (int i = 0; i < size; i++) {
-            final long v = 11 * minLongA[i];
+        for (int i = 0; i < state.size; i++) {
+            final long v = 11 * state.minLongA[i];
             result = Math.min(result, v);
         }
         return result;
     }
 
     @Benchmark
-    public long longReductionMax() {
+    public long longReductionMax(LoopState state) {
         long result = 0;
-        for (int i = 0; i < size; i++) {
-            final long v = 11 * maxLongA[i];
+        for (int i = 0; i < state.size; i++) {
+            final long v = 11 * state.maxLongA[i];
             result = Math.max(result, v);
         }
         return result;
