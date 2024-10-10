80dbaf0af88650df50708dc0711e437e6d9f790c
zwd <978344408@qq.com>
2024-10-10 22:58:50 +0800

    8331467

--- src/java.base/aix/classes/sun/nio/fs/DefaultFileSystemProvider.java
+++ src/java.base/aix/classes/sun/nio/fs/DefaultFileSystemProvider.java
@@ -26,6 +26,7 @@
 package sun.nio.fs;
 
 import java.nio.file.FileSystem;
+import java.nio.file.spi.FileSystemProvider;
 
 /**
  * Creates this platform's default FileSystemProvider.
@@ -49,5 +50,13 @@ public class DefaultFileSystemProvider {
      */
     public static FileSystem theFileSystem() {
         return INSTANCE.theFileSystem();
+    }
+
+    /**
+     * This method is from java8 API to be compatible against runtime.
+     * Returns the default FileSystemProvider.
+     */
+    public static FileSystemProvider create() {
+        return INSTANCE;
     }
 }
