83589b5129a43f070f43c69b43dfe14dbd03419a
Eirik Bjorsnos <eirbjo@gmail.com>
2024-10-11 14:24:16 +0200

    Add tests for the cases where there is a CRC mismatch, but the class file data is valid

--- test/jdk/jdk/internal/loader/URLClassPath/InvalidCRCClassData.java
+++ test/jdk/jdk/internal/loader/URLClassPath/InvalidCRCClassData.java
@@ -26,6 +26,9 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.lang.classfile.ClassFile;
+import java.lang.constant.ClassDesc;
+import java.lang.constant.ConstantDescs;
 import java.lang.module.Configuration;
 import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
@@ -54,22 +57,23 @@ import static org.junit.jupiter.api.Assertions.*;
  *          defineClass fails with ClassFormatError
  * @run junit InvalidCRCData
  */
-public class InvalidCRCData {
+public class InvalidCRCClassData {
 
     String className = "foo.bar.Library";
 
     /**
      * Verify that URLClassLoader adds a suppressed exception when
-     * a class file is rejected with ClassFormatError and the class data
-     * file CRC32 checksum does not match the CRC32 stated in the JAR CEN header.
+     * a class file is rejected with ClassFormatError and the CRC32 checksum
+     * of the class data file does not match the CRC32 stated in the JAR CEN header.
      *
      * @throws IOException if an unexpected IO exception occurs
      * @throws ClassNotFoundException if a class is unexpectedly not found
      */
     @Test
-    public void verifyURLClassLoader() throws IOException, ClassNotFoundException {
+    public void invalidClassInvalidCRC_URLClassLoader() throws IOException, ClassNotFoundException {
 
-        Path jarFile = createJarFile();
+        // Create a JAR file with invalid class data, invalid CRC
+        Path jarFile = createJarFile(false, false);
 
         try (var cl = new CustomURLClassLoader(jarFile)) {
             // Expect ClassFormatError
@@ -83,18 +87,66 @@ public class InvalidCRCData {
     }
 
     /**
+     * A CRC mismatch itself does not cause exceptions for a valid class
+     *
+     * @throws IOException if an unexpected IO exception occurs
+     * @throws ClassNotFoundException if a class is unexpectedly not found
+     */
+    @Test
+    public void validClassInvalidCRC_URLClassLoader() throws IOException, ClassNotFoundException {
+
+        // Create a JAR file with valid class data, invalid CRC
+        Path jarFile = createJarFile(true, false);
+
+        try (var cl = new CustomURLClassLoader(jarFile)) {
+            assertEquals(className, cl.findClass(className).getName());
+        }
+    }
+
+    /**
      * Document that a JAR file with invalid class file data and a mismatching
-     * CRC32 checksums does not cause ClassFormatError to have suppressed
+     * CRC32 checksum does not cause ClassFormatError to have suppressed
      * exceptions when loaded using the module class loader.
      *
      * @throws IOException if an unexpected IO exception occurs
      * @throws ClassNotFoundException if a class is unexpectedly not found
      */
     @Test
-    public void verifyModule() throws IOException, ClassNotFoundException {
+    public void invalidClassCRCMismatchModule() throws IOException, ClassNotFoundException {
 
+        // Load a module with invalid class data and invalid CRC
+        Module m1 = loadModule(false, false);
+
+        // Expect ClassFormatError when loading class
+        ClassFormatError exception = assertThrows(ClassFormatError.class, () -> {
+            m1.getClassLoader().loadClass(className);
+        });
+
+        // Verify that jdk.internal.Loader does not check CRC32 on ClassFormatError
+        assertFalse(isCRC32Suppressed(exception));
+    }
+
+    /**
+     * Document that a JAR file with valid class file data and a mismatching
+     * CRC32 checksums does not cause exceptions
+     *
+     * @throws IOException if an unexpected IO exception occurs
+     * @throws ClassNotFoundException if a class is unexpectedly not found
+     */
+    @Test
+    public void validClassCRCMismatchModule() throws IOException, ClassNotFoundException {
+
+        // Load a module with valid class data, but invalid CRC
+        Module m1 = loadModule(true, false);
+
+        // Expect class to load, even with CRC mismatch
+        assertEquals(className, m1.getClassLoader().loadClass(className).getName());
+    }
+
+    // Load a module from a constructed JAR file
+    private Module loadModule(boolean validClass, boolean validCrc) throws IOException {
         // Create a module JAR file
-        Path jarFile = createJarFile();
+        Path jarFile = createJarFile(validClass, validCrc);
 
         // Load the module
         ModuleFinder moduleFinder = ModuleFinder.of(jarFile);
@@ -106,15 +158,7 @@ public class InvalidCRCData {
                 Collections.singletonList(ModuleLayer.boot()),
                 getClass().getClassLoader());
 
-        Module m1 = controller.layer().findModule("m1").orElseThrow();
-
-        // Expect ClassFormatError when loading class
-        ClassFormatError exception = assertThrows(ClassFormatError.class, () -> {
-            m1.getClassLoader().loadClass(className);
-        });
-
-        // Verify that jdk.internal.Loader does not check CRC32 on ClassFormatError
-        assertFalse(isCRC32Suppressed(exception));
+        return controller.layer().findModule("m1").orElseThrow();
     }
 
     // Return true iff CFE has a suppressed CRC32 mismatch error
@@ -128,8 +172,9 @@ public class InvalidCRCData {
         return false;
     }
 
-    // Create a JAR / module file for use in this test
-    private Path createJarFile() throws IOException {
+    // Create a JAR / module file for use in this test,
+    // optionally with invalid class file data or invalid CRC checksum
+    private Path createJarFile(boolean validClass, boolean validCrc) throws IOException {
         // Create a ZIP file with an invalid class file
         Path zipFile = Path.of("invalid-class-data.jar");
         ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -137,8 +182,11 @@ public class InvalidCRCData {
             // Write library class
             String entryName = className.replace('.', '/') +".class";
             zo.putNextEntry(new ZipEntry(entryName));
-            // Invalid class file data
-            zo.write("efac".getBytes(StandardCharsets.UTF_8));
+            if (!validClass) {
+                // Invalid class file data
+                zo.write("efac".getBytes(StandardCharsets.UTF_8));
+            }
+            zo.write(libraryClass());
 
             // Write module descriptor
             zo.putNextEntry(new ZipEntry("module-info.class"));
@@ -150,14 +198,22 @@ public class InvalidCRCData {
 
         byte[] bytes = out.toByteArray();
 
-        // Put an invalid CRC value in the CEN header for the class file entry
-        ByteBuffer buf = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN);
-        int cenOff = buf.getInt(bytes.length - ZipEntry.ENDHDR + ZipEntry.ENDOFF);
-        buf.putInt(cenOff + ZipEntry.CENCRC, 0x0);
+        if (!validCrc) {
+            // Put an invalid CRC value in the CEN header for the class file entry
+            ByteBuffer buf = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN);
+            int cenOff = buf.getInt(bytes.length - ZipEntry.ENDHDR + ZipEntry.ENDOFF);
+            buf.putInt(cenOff + ZipEntry.CENCRC, 0x0);
+        }
 
         // Write the file to disk
         Files.write(zipFile, bytes);
         return zipFile;
+    }
+
+    private byte[] libraryClass() {
+        return ClassFile.of().build(ClassDesc.of(className), cb -> {
+            cb.withSuperclass(ConstantDescs.CD_Object);
+        });
     }
 
     // URLClassLoader with access bridge to findClass
