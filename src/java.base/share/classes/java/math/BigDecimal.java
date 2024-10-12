0723858d8b6acee27b6acd25144dc4db4f919147
fabioromano1 <51378941+fabioromano1@users.noreply.github.com>
2024-10-12 19:34:08 +0200

    Minor change

--- src/java.base/share/classes/java/math/BigDecimal.java
+++ src/java.base/share/classes/java/math/BigDecimal.java
@@ -5289,7 +5289,7 @@ public class BigDecimal extends Number implements Comparable<BigDecimal> {
         }
 
         // bitLength(remainingZeros) == min{n >= 0 : 5^(2^n) > 5^remainingZeros}
-        // so, while the cycle's condition is true,
+        // so, while the loop condition is true,
         // the invariant i == max{n : 5^(2^n) <= 5^remainingZeros}
         // is preserved at the beginning of each iteration.
         // Note that the loop stops exactly when remainingZeros == 0.
