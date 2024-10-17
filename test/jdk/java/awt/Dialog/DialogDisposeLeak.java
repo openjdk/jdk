2964b6a3af3834d4e69ce43212f019670614bd19
Alisen Chung <alisen.chung@oracle.com>
2024-10-17 10:18:32 -0700

    revert test to manual

--- test/jdk/java/awt/Dialog/DialogDisposeLeak.java
+++ test/jdk/java/awt/Dialog/DialogDisposeLeak.java
@@ -33,9 +33,7 @@ import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Label;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 
