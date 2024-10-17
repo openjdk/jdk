09971b103efc3cca748b0b0ce2eaf0dabbff6c1b
Fernando Guallini <fernando.guallini@oracle.com>
2024-10-17 10:47:50 +0200

    initial commit

--- test/jdk/com/sun/crypto/provider/KeyAgreement/DHKeyAgreement3.java
+++ test/jdk/com/sun/crypto/provider/KeyAgreement/DHKeyAgreement3.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -61,27 +61,26 @@ public class DHKeyAgreement3 {
 
     private void run() throws Exception {
 
-        DHParameterSpec dhSkipParamSpec;
-
-        System.err.println("Using SKIP Diffie-Hellman parameters");
-        dhSkipParamSpec = new DHParameterSpec(skip1024Modulus, skip1024Base);
+        DHParameterSpec ffhde2048ParamSpec;
+        System.err.println("Using FFDHE2048 Diffie-Hellman parameters");
+        ffhde2048ParamSpec = new DHParameterSpec(ffhde2048Modulus, ffhde2048Base);
 
         // Alice creates her own DH key pair
         System.err.println("ALICE: Generate DH keypair ...");
         KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH", "SunJCE");
-        aliceKpairGen.initialize(dhSkipParamSpec);
+        aliceKpairGen.initialize(ffhde2048ParamSpec);
         KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
 
         // Bob creates his own DH key pair
         System.err.println("BOB: Generate DH keypair ...");
         KeyPairGenerator bobKpairGen = KeyPairGenerator.getInstance("DH", "SunJCE");
-        bobKpairGen.initialize(dhSkipParamSpec);
+        bobKpairGen.initialize(ffhde2048ParamSpec);
         KeyPair bobKpair = bobKpairGen.generateKeyPair();
 
         // Carol creates her own DH key pair
         System.err.println("CAROL: Generate DH keypair ...");
         KeyPairGenerator carolKpairGen = KeyPairGenerator.getInstance("DH", "SunJCE");
-        carolKpairGen.initialize(dhSkipParamSpec);
+        carolKpairGen.initialize(ffhde2048ParamSpec);
         KeyPair carolKpair = carolKpairGen.generateKeyPair();
 
 
@@ -179,46 +178,43 @@ public class DHKeyAgreement3 {
         System.err.println("[-gen]");
     }
 
-    // The 1024 bit Diffie-Hellman modulus values used by SKIP
-    private static final byte skip1024ModulusBytes[] = {
-        (byte)0xF4, (byte)0x88, (byte)0xFD, (byte)0x58,
-        (byte)0x4E, (byte)0x49, (byte)0xDB, (byte)0xCD,
-        (byte)0x20, (byte)0xB4, (byte)0x9D, (byte)0xE4,
-        (byte)0x91, (byte)0x07, (byte)0x36, (byte)0x6B,
-        (byte)0x33, (byte)0x6C, (byte)0x38, (byte)0x0D,
-        (byte)0x45, (byte)0x1D, (byte)0x0F, (byte)0x7C,
-        (byte)0x88, (byte)0xB3, (byte)0x1C, (byte)0x7C,
-        (byte)0x5B, (byte)0x2D, (byte)0x8E, (byte)0xF6,
-        (byte)0xF3, (byte)0xC9, (byte)0x23, (byte)0xC0,
-        (byte)0x43, (byte)0xF0, (byte)0xA5, (byte)0x5B,
-        (byte)0x18, (byte)0x8D, (byte)0x8E, (byte)0xBB,
-        (byte)0x55, (byte)0x8C, (byte)0xB8, (byte)0x5D,
-        (byte)0x38, (byte)0xD3, (byte)0x34, (byte)0xFD,
-        (byte)0x7C, (byte)0x17, (byte)0x57, (byte)0x43,
-        (byte)0xA3, (byte)0x1D, (byte)0x18, (byte)0x6C,
-        (byte)0xDE, (byte)0x33, (byte)0x21, (byte)0x2C,
-        (byte)0xB5, (byte)0x2A, (byte)0xFF, (byte)0x3C,
-        (byte)0xE1, (byte)0xB1, (byte)0x29, (byte)0x40,
-        (byte)0x18, (byte)0x11, (byte)0x8D, (byte)0x7C,
-        (byte)0x84, (byte)0xA7, (byte)0x0A, (byte)0x72,
-        (byte)0xD6, (byte)0x86, (byte)0xC4, (byte)0x03,
-        (byte)0x19, (byte)0xC8, (byte)0x07, (byte)0x29,
-        (byte)0x7A, (byte)0xCA, (byte)0x95, (byte)0x0C,
-        (byte)0xD9, (byte)0x96, (byte)0x9F, (byte)0xAB,
-        (byte)0xD0, (byte)0x0A, (byte)0x50, (byte)0x9B,
-        (byte)0x02, (byte)0x46, (byte)0xD3, (byte)0x08,
-        (byte)0x3D, (byte)0x66, (byte)0xA4, (byte)0x5D,
-        (byte)0x41, (byte)0x9F, (byte)0x9C, (byte)0x7C,
-        (byte)0xBD, (byte)0x89, (byte)0x4B, (byte)0x22,
-        (byte)0x19, (byte)0x26, (byte)0xBA, (byte)0xAB,
-        (byte)0xA2, (byte)0x5E, (byte)0xC3, (byte)0x55,
-        (byte)0xE9, (byte)0x2F, (byte)0x78, (byte)0xC7
+    /**
+     * RFC 7919 - ffdhe2048.
+     */
+    private static byte[] FFDHE2048PrimeBytes = {
+            (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
+            (byte) 0xAD, (byte) 0xF8, (byte) 0x54, (byte) 0x58, (byte) 0xA2, (byte) 0xBB, (byte) 0x4A, (byte) 0x9A,
+            (byte) 0xAF, (byte) 0xDC, (byte) 0x56, (byte) 0x20, (byte) 0x27, (byte) 0x3D, (byte) 0x3C, (byte) 0xF1,
+            (byte) 0xD8, (byte) 0xB9, (byte) 0xC5, (byte) 0x83, (byte) 0xCE, (byte) 0x2D, (byte) 0x36, (byte) 0x95,
+            (byte) 0xA9, (byte) 0xE1, (byte) 0x36, (byte) 0x41, (byte) 0x14, (byte) 0x64, (byte) 0x33, (byte) 0xFB,
+            (byte) 0xCC, (byte) 0x93, (byte) 0x9D, (byte) 0xCE, (byte) 0x24, (byte) 0x9B, (byte) 0x3E, (byte) 0xF9,
+            (byte) 0x7D, (byte) 0x2F, (byte) 0xE3, (byte) 0x63, (byte) 0x63, (byte) 0x0C, (byte) 0x75, (byte) 0xD8,
+            (byte) 0xF6, (byte) 0x81, (byte) 0xB2, (byte) 0x02, (byte) 0xAE, (byte) 0xC4, (byte) 0x61, (byte) 0x7A,
+            (byte) 0xD3, (byte) 0xDF, (byte) 0x1E, (byte) 0xD5, (byte) 0xD5, (byte) 0xFD, (byte) 0x65, (byte) 0x61,
+            (byte) 0x24, (byte) 0x33, (byte) 0xF5, (byte) 0x1F, (byte) 0x5F, (byte) 0x06, (byte) 0x6E, (byte) 0xD0,
+            (byte) 0x85, (byte) 0x63, (byte) 0x65, (byte) 0x55, (byte) 0x3D, (byte) 0xED, (byte) 0x1A, (byte) 0xF3,
+            (byte) 0xB5, (byte) 0x57, (byte) 0x13, (byte) 0x5E, (byte) 0x7F, (byte) 0x57, (byte) 0xC9, (byte) 0x35,
+            (byte) 0x98, (byte) 0x4F, (byte) 0x0C, (byte) 0x70, (byte) 0xE0, (byte) 0xE6, (byte) 0x8B, (byte) 0x77,
+            (byte) 0xE2, (byte) 0xA6, (byte) 0x89, (byte) 0xDA, (byte) 0xF3, (byte) 0xEF, (byte) 0xE8, (byte) 0x72,
+            (byte) 0x1D, (byte) 0xF1, (byte) 0x58, (byte) 0xA1, (byte) 0x36, (byte) 0xAD, (byte) 0xE7, (byte) 0x35,
+            (byte) 0x30, (byte) 0xAC, (byte) 0xCA, (byte) 0x4F, (byte) 0x48, (byte) 0x3A, (byte) 0x79, (byte) 0x7A,
+            (byte) 0xBC, (byte) 0x0A, (byte) 0xB1, (byte) 0x82, (byte) 0xB3, (byte) 0x24, (byte) 0xFB, (byte) 0x61,
+            (byte) 0xD1, (byte) 0x08, (byte) 0xA9, (byte) 0x4B, (byte) 0xB2, (byte) 0xC8, (byte) 0xE3, (byte) 0xFB,
+            (byte) 0xB9, (byte) 0x6A, (byte) 0xDA, (byte) 0xB7, (byte) 0x60, (byte) 0xD7, (byte) 0xF4, (byte) 0x68,
+            (byte) 0x1D, (byte) 0x4F, (byte) 0x42, (byte) 0xA3, (byte) 0xDE, (byte) 0x39, (byte) 0x4D, (byte) 0xF4,
+            (byte) 0xAE, (byte) 0x56, (byte) 0xED, (byte) 0xE7, (byte) 0x63, (byte) 0x72, (byte) 0xBB, (byte) 0x19,
+            (byte) 0x0B, (byte) 0x07, (byte) 0xA7, (byte) 0xC8, (byte) 0xEE, (byte) 0x0A, (byte) 0x6D, (byte) 0x70,
+            (byte) 0x9E, (byte) 0x02, (byte) 0xFC, (byte) 0xE1, (byte) 0xCD, (byte) 0xF7, (byte) 0xE2, (byte) 0xEC,
+            (byte) 0xC0, (byte) 0x34, (byte) 0x04, (byte) 0xCD, (byte) 0x28, (byte) 0x34, (byte) 0x2F, (byte) 0x61,
+            (byte) 0x91, (byte) 0x72, (byte) 0xFE, (byte) 0x9C, (byte) 0xE9, (byte) 0x85, (byte) 0x83, (byte) 0xFF,
+            (byte) 0x8E, (byte) 0x4F, (byte) 0x12, (byte) 0x32, (byte) 0xEE, (byte) 0xF2, (byte) 0x81, (byte) 0x83,
+            (byte) 0xC3, (byte) 0xFE, (byte) 0x3B, (byte) 0x1B, (byte) 0x4C, (byte) 0x6F, (byte) 0xAD, (byte) 0x73,
+            (byte) 0x3B, (byte) 0xB5, (byte) 0xFC, (byte) 0xBC, (byte) 0x2E, (byte) 0xC2, (byte) 0x20, (byte) 0x05,
+            (byte) 0xC5, (byte) 0x8E, (byte) 0xF1, (byte) 0x83, (byte) 0x7D, (byte) 0x16, (byte) 0x83, (byte) 0xB2,
+            (byte) 0xC6, (byte) 0xF3, (byte) 0x4A, (byte) 0x26, (byte) 0xC1, (byte) 0xB2, (byte) 0xEF, (byte) 0xFA,
+            (byte) 0x88, (byte) 0x6B, (byte) 0x42, (byte) 0x38, (byte) 0x61, (byte) 0x28, (byte) 0x5C, (byte) 0x97,
+            (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF
     };
-
-    // The SKIP 1024 bit modulus
-    private static final BigInteger skip1024Modulus
-    = new BigInteger(1, skip1024ModulusBytes);
-
-    // The base used with the SKIP 1024 bit modulus
-    private static final BigInteger skip1024Base = BigInteger.valueOf(2);
+    private static final BigInteger ffhde2048Modulus = new BigInteger(1, FFDHE2048PrimeBytes);
+    private static final BigInteger ffhde2048Base = BigInteger.valueOf(2);
 }
