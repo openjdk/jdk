d98129a0bf0cab2c4a90941f3852a0235601e172
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2024-10-21 11:32:35 -0400

    Add getter for PathGroup's keys

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/PathGroup.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/PathGroup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,6 +32,7 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -59,6 +60,13 @@ final class PathGroup {
         } else {
             entries.remove(id);
         }
+    }
+
+    /**
+     * All configured IDs.
+     */
+    Set<Object> keys() {
+        return entries.keySet();
     }
 
     /**
