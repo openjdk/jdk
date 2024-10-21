5c76442ac797c4e6d97ffd313e89473c10f2a79c
Patricio Chilano Mateo <patricio.chilano.mateo@oracle.com>
2024-10-21 11:25:43 -0400

    Adjust comment in JavaThread.hpp

--- src/hotspot/share/runtime/javaThread.hpp
+++ src/hotspot/share/runtime/javaThread.hpp
@@ -162,7 +162,7 @@ class JavaThread: public Thread {
 
   // ID used as owner for inflated monitors. Same as the j.l.Thread.tid of the
   // current _vthread object, except during creation of the primordial and JNI
-  // attached thread cases where this field can have a temporal value.
+  // attached thread cases where this field can have a temporary value.
   int64_t _lock_id;
 
  public:
