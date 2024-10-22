d196929fe87bd44f3b07ac44cf8ec088f6a0b2ff
iklam <ilam3388@gmail.com>
2024-10-22 11:23:36 -0700

    Added test case for using --module-path with -XX:+AOTClassLinking

--- test/hotspot/jtreg/runtime/cds/appcds/aotClassLinking/AOTClassLinkingVMOptions.java
+++ test/hotspot/jtreg/runtime/cds/appcds/aotClassLinking/AOTClassLinkingVMOptions.java
@@ -37,7 +37,13 @@
  * @run driver AOTClassLinkingVMOptions
  */
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import jdk.test.lib.cds.CDSTestUtils;
 import jdk.test.lib.helpers.ClassFileInstaller;
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.test.lib.process.ProcessTools;
 
 public class AOTClassLinkingVMOptions {
     static final String appJar = ClassFileInstaller.getJarPath("app.jar");
@@ -66,7 +72,6 @@ public class AOTClassLinkingVMOptions {
         TestCommon.run("-cp", appJar, "-Xlog:cds", "-m", "jdk.compiler/com.sun.tools.javac.Main")
             .assertAbnormalExit("CDS archive has aot-linked classes." +
                                 " It cannot be used when archived full module graph is not used.");
-
         testCase("Cannot use security manager");
         TestCommon.run("-cp", appJar, "-Xlog:cds", "-Djava.security.manager=allow")
             .assertAbnormalExit("CDS archive has aot-linked classes." +
@@ -77,5 +82,79 @@ public class AOTClassLinkingVMOptions {
 
         // NOTE: tests for ClassFileLoadHook + AOTClassLinking is in
         // ../jvmti/ClassFileLoadHookTest.java
+
+        modulePathTests();
+    }
+
+    static void modulePathTests() throws Exception {
+        CDSModulePathUtils.init();
+
+        testCase("Cannot use mis-matched module path");
+        String goodModulePath = CDSModulePathUtils.getModulesDir().toString();
+        TestCommon.testDump(null, CDSModulePathUtils.getAppClasses(),
+                            "--module-path", goodModulePath,
+                            "-XX:+AOTClassLinking",
+                            "-m", CDSModulePathUtils.MAIN_MODULE);
+        TestCommon.run("-Xlog:cds",
+                       "--module-path", goodModulePath,
+                       "-m", CDSModulePathUtils.MAIN_MODULE)
+            .assertNormalExit("Using AOT-linked classes: true");
+
+        TestCommon.run("-Xlog:cds",
+                       "--module-path", goodModulePath + "/bad",
+                       "-m", CDSModulePathUtils.MAIN_MODULE)
+            .assertAbnormalExit("CDS archive has aot-linked classes. It cannot be used when archived full module graph is not used.");
+    }
+}
+
+// TODO: enhance and move this class to jdk.test.lib.cds.CDSModulePathUtils
+
+class CDSModulePathUtils {
+    private static String TEST_SRC = System.getProperty("test.root");
+    private static Path USER_DIR = Paths.get(CDSTestUtils.getOutputDir());
+    private static Path SRC_DIR = Paths.get(TEST_SRC, "runtime/cds/appcds/jigsaw/modulepath/src");
+    private static Path MODS_DIR = Paths.get("mods");
+
+    public static String MAIN_MODULE = "com.bars";
+    public static String TEST_MODULE = "com.foos";
+
+    private static String MAIN_CLASS = "com.bars.Main";
+    private static String TEST_CLASS = "com.foos.Test";
+    private static String appClasses[] = {MAIN_CLASS, TEST_CLASS};
+
+    private static Path modulesDir;
+
+    // This directory contains all the modular jar files
+    //     $USER_DIR/modules/com.bars.jar
+    //     $USER_DIR/modules/com.foos.jar
+    static Path getModulesDir() {
+        return modulesDir;
+    }
+
+    static String[] getAppClasses() {
+        return appClasses;
+    }
+
+    static void init() throws Exception  {
+        JarBuilder.compileModule(SRC_DIR.resolve(TEST_MODULE),
+                                 MODS_DIR.resolve(TEST_MODULE),
+                                 null);
+        JarBuilder.compileModule(SRC_DIR.resolve(MAIN_MODULE),
+                                 MODS_DIR.resolve(MAIN_MODULE),
+                                 MODS_DIR.toString());
+
+        String PATH_LIBS = "modules";
+        modulesDir = Files.createTempDirectory(USER_DIR, PATH_LIBS);
+        Path mainJar = modulesDir.resolve(MAIN_MODULE + ".jar");
+        Path testJar = modulesDir.resolve(TEST_MODULE + ".jar");
+
+        // modylibs contains both modules com.foos.jar, com.bars.jar
+        // build com.foos.jar
+        String classes = MODS_DIR.resolve(TEST_MODULE).toString();
+        JarBuilder.createModularJar(testJar.toString(), classes, TEST_CLASS);
+
+        // build com.bars.jar
+        classes = MODS_DIR.resolve(MAIN_MODULE).toString();
+        JarBuilder.createModularJar(mainJar.toString(), classes, MAIN_CLASS);
     }
 }
