da78b148af8c823b5033cc1c9b6041e770a35c8a
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2024-10-22 19:27:20 +0000

    Enabled uncompressed oops on Epsilon and Shenandoah

--- src/hotspot/share/gc/epsilon/epsilonHeap.hpp
+++ src/hotspot/share/gc/epsilon/epsilonHeap.hpp
@@ -128,7 +128,7 @@ public:
   bool is_in_reserved(const void* addr) const { return _reserved.contains(addr); }
 
   // Support for loading objects from CDS archive into the heap
-  bool can_load_archived_objects() const override { return UseCompressedOops; }
+  bool can_load_archived_objects() const override { return true; }
   HeapWord* allocate_loaded_archive_space(size_t size) override;
 
   void print_on(outputStream* st) const override;
