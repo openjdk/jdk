ed7c10a26c3a2786994f72d4530faf16e2f2da3f
hanklo6 <hank.lo@intel.com>
2024-10-22 10:59:08 -0700

    set avx and evex in test

--- src/hotspot/cpu/x86/vm_version_x86.hpp
+++ src/hotspot/cpu/x86/vm_version_x86.hpp
@@ -640,8 +640,8 @@ public:
   LP64_ONLY(static void clear_apx_test_state());
 
   static void clean_cpuFeatures()   { _features = 0; }
-  static void set_avx_cpuFeatures() { _features = (CPU_SSE | CPU_SSE2 | CPU_AVX | CPU_VZEROUPPER ); }
-  static void set_evex_cpuFeatures() { _features = (CPU_AVX512F | CPU_SSE | CPU_SSE2 | CPU_VZEROUPPER ); }
+  static void set_avx_cpuFeatures() { _features |= (CPU_SSE | CPU_SSE2 | CPU_AVX | CPU_VZEROUPPER ); }
+  static void set_evex_cpuFeatures() { _features |= (CPU_AVX512F | CPU_SSE | CPU_SSE2 | CPU_VZEROUPPER ); }
   static void set_apx_cpuFeatures() { _features |= CPU_APX_F; }
   static void set_bmi1_cpuFeatures() { _features |= CPU_BMI1; }
 
