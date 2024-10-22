19d05e43ac2bf2ad3350ce3432f3ea658297d400
Roman <rkennke@amazon.de>
2024-10-22 14:21:12 +0000

    Update copyright headers

--- src/hotspot/share/gc/parallel/parallelArguments.cpp
+++ src/hotspot/share/gc/parallel/parallelArguments.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2017, Red Hat, Inc. and/or its affiliates.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
