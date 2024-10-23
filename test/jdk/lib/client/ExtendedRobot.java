514a813c5bc7cf3f33c2379729ede927eb6658a9
Alisen Chung <alisen.chung@oracle.com>
2024-10-23 13:34:48 -0700

    init commit

--- test/jdk/lib/client/ExtendedRobot.java
+++ test/jdk/lib/client/ExtendedRobot.java
@@ -108,6 +108,7 @@ public class ExtendedRobot extends Robot {
      */
     public ExtendedRobot(GraphicsDevice screen) throws AWTException {
         super(screen);
+//        throw new RuntimeException("ExtendedRobot");
     }
 
     /**
@@ -117,192 +118,12 @@ public class ExtendedRobot extends Robot {
      *
      * @see     #waitForIdle()
      */
-    public int getSyncDelay(){ return this.syncDelay; }
-
-    /**
-     * Clicks mouse button(s) by calling {@link java.awt.Robot#mousePress(int)} and
-     * {@link java.awt.Robot#mouseRelease(int)} methods
-     *
-     *
-     * @param   buttons The button mask; a combination of one or more mouse button masks.
-     * @throws  IllegalArgumentException if the {@code buttons} mask contains the mask for
-     *          extra mouse button and support for extended mouse buttons is
-     *          {@link Toolkit#areExtraMouseButtonsEnabled() disabled} by Java
-     * @throws  IllegalArgumentException if the {@code buttons} mask contains the mask for
-     *          extra mouse button that does not exist on the mouse and support for extended
-     *          mouse buttons is {@link Toolkit#areExtraMouseButtonsEnabled() enabled}
-     *          by Java
-     *
-     * @see     #mousePress(int)
-     * @see     #mouseRelease(int)
-     * @see     InputEvent#getMaskForButton(int)
-     * @see     Toolkit#areExtraMouseButtonsEnabled()
-     * @see     java.awt.event.MouseEvent
-     */
-    public void click(int buttons) {
-        mousePress(buttons);
-        waitForIdle(DEFAULT_SPEED);
-        mouseRelease(buttons);
-        waitForIdle();
+    public int getSyncDelay() {
+        throw new RuntimeException("getSyncDelay");
+//        return this.syncDelay;
     }
 
-    /**
-     * Clicks mouse button 1
-     *
-     * @throws  IllegalArgumentException if the {@code buttons} mask contains the mask for
-     *          extra mouse button and support for extended mouse buttons is
-     *          {@link Toolkit#areExtraMouseButtonsEnabled() disabled} by Java
-     * @throws  IllegalArgumentException if the {@code buttons} mask contains the mask for
-     *          extra mouse button that does not exist on the mouse and support for extended
-     *          mouse buttons is {@link Toolkit#areExtraMouseButtonsEnabled() enabled}
-     *          by Java
-     *
-     * @see     #click(int)
-     */
-    public void click() {
-        click(InputEvent.BUTTON1_DOWN_MASK);
-    }
 
-    /**
-     * Waits until all events currently on the event queue have been processed with given
-     * delay after syncing threads. It uses more advanced method of synchronizing threads
-     * unlike {@link java.awt.Robot#waitForIdle()}
-     *
-     * @param   delayValue  Additional delay length in milliseconds to wait until thread
-     *                      sync been completed
-     * @throws  sun.awt.SunToolkit.IllegalThreadException if called on the AWT event
-     *          dispatching thread
-     */
-    public synchronized void waitForIdle(int delayValue) {
-        super.waitForIdle();
-        delay(delayValue);
-    }
-
-    /**
-     * Waits until all events currently on the event queue have been processed with delay
-     * {@link #getSyncDelay()} after syncing threads. It uses more advanced method of
-     * synchronizing threads unlike {@link java.awt.Robot#waitForIdle()}
-     *
-     * @throws  sun.awt.SunToolkit.IllegalThreadException if called on the AWT event
-     *          dispatching thread
-     *
-     * @see     #waitForIdle(int)
-     */
-    @Override
-    public synchronized void waitForIdle() {
-        waitForIdle(syncDelay);
-    }
-
-    /**
-     * Move the mouse in multiple steps from where it is
-     * now to the destination coordinates.
-     *
-     * @param   x   Destination point x coordinate
-     * @param   y   Destination point y coordinate
-     *
-     * @see     #glide(int, int, int, int)
-     */
-    public void glide(int x, int y) {
-        Point p = MouseInfo.getPointerInfo().getLocation();
-        glide(p.x, p.y, x, y);
-    }
-
-    /**
-     * Move the mouse in multiple steps from where it is
-     * now to the destination point.
-     *
-     * @param   dest    Destination point
-     *
-     * @see     #glide(int, int)
-     */
-    public void glide(Point dest) {
-        glide(dest.x, dest.y);
-    }
-
-    /**
-     * Move the mouse in multiple steps from source coordinates
-     * to the destination coordinates.
-     *
-     * @param   fromX   Source point x coordinate
-     * @param   fromY   Source point y coordinate
-     * @param   toX     Destination point x coordinate
-     * @param   toY     Destination point y coordinate
-     *
-     * @see     #glide(int, int, int, int, int, int)
-     */
-    public void glide(int fromX, int fromY, int toX, int toY) {
-        glide(fromX, fromY, toX, toY, DEFAULT_STEP_LENGTH, DEFAULT_SPEED);
-    }
-
-    /**
-     * Move the mouse in multiple steps from source point to the
-     * destination point with default speed and step length.
-     *
-     * @param   src     Source point
-     * @param   dest    Destination point
-     *
-     * @see     #glide(int, int, int, int, int, int)
-     */
-    public void glide(Point src, Point dest) {
-        glide(src.x, src.y, dest.x, dest.y, DEFAULT_STEP_LENGTH, DEFAULT_SPEED);
-    }
-
-    /**
-     * Move the mouse in multiple steps from source point to the
-     * destination point with given speed and step length.
-     *
-     * @param   srcX        Source point x cordinate
-     * @param   srcY        Source point y cordinate
-     * @param   destX       Destination point x cordinate
-     * @param   destY       Destination point y cordinate
-     * @param   stepLength  Approximate length of one step
-     * @param   speed       Delay between steps.
-     *
-     * @see     #mouseMove(int, int)
-     * @see     #delay(int)
-     */
-     public void glide(int srcX, int srcY, int destX, int destY, int stepLength, int speed) {
-        int stepNum;
-        double tDx, tDy;
-        double dx, dy, ds;
-        double x, y;
-
-        dx = (destX - srcX);
-        dy = (destY - srcY);
-        ds = Math.sqrt(dx*dx + dy*dy);
-
-        tDx = dx / ds * stepLength;
-        tDy = dy / ds * stepLength;
-
-        int stepsCount = (int) ds / stepLength;
-
-        // Walk the mouse to the destination one step at a time
-        mouseMove(srcX, srcY);
-
-        for (x = srcX, y = srcY, stepNum = 0;
-             stepNum < stepsCount;
-             stepNum++) {
-            x += tDx;
-            y += tDy;
-            mouseMove((int)x, (int)y);
-            delay(speed);
-        }
-
-        // Ensure the mouse moves to the right destination.
-        // The steps may have led the mouse to a slightly wrong place.
-        mouseMove(destX, destY);
-    }
-
-    /**
-     * Moves mouse pointer to given screen coordinates.
-     *
-     * @param   position    Target position
-     *
-     * @see     java.awt.Robot#mouseMove(int, int)
-     */
-    public synchronized void mouseMove(Point position) {
-        mouseMove(position.x, position.y);
-    }
 
 
     /**
@@ -321,12 +142,13 @@ public class ExtendedRobot extends Robot {
      * @see     #glide(int, int, int, int)
      */
     public void dragAndDrop(int fromX, int fromY, int toX, int toY){
-        mouseMove(fromX, fromY);
-        mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        waitForIdle();
-        glide(toX, toY);
-        mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        waitForIdle();
+        throw new RuntimeException("dragAndDrop");
+//        mouseMove(fromX, fromY);
+//        mousePress(InputEvent.BUTTON1_DOWN_MASK);
+//        waitForIdle();
+//        glide(toX, toY);
+//        mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+//        waitForIdle();
     }
 
     /**
@@ -343,64 +165,7 @@ public class ExtendedRobot extends Robot {
      * @see     #dragAndDrop(int, int, int, int)
      */
     public void dragAndDrop(Point from, Point to){
-        dragAndDrop(from.x, from.y, to.x, to.y);
-    }
-
-    /**
-     * Successively presses and releases a given key.
-     * <p>
-     * Key codes that have more than one physical key associated with them
-     * (e.g. {@code KeyEvent.VK_SHIFT} could mean either the
-     * left or right shift key) will map to the left key.
-     *
-     * @param   keycode Key to press (e.g. {@code KeyEvent.VK_A})
-     * @throws  IllegalArgumentException if {@code keycode} is not
-     *          a valid key
-     *
-     * @see     java.awt.Robot#keyPress(int)
-     * @see     java.awt.Robot#keyRelease(int)
-     * @see     java.awt.event.KeyEvent
-     */
-    public void type(int keycode) {
-        keyPress(keycode);
-        waitForIdle(DEFAULT_SPEED);
-        keyRelease(keycode);
-        waitForIdle(DEFAULT_SPEED);
-    }
-
-    /**
-     * Types given character
-     *
-     * @param   c   Character to be typed (e.g. {@code 'a'})
-     *
-     * @see     #type(int)
-     * @see     java.awt.event.KeyEvent
-     */
-    public void type(char c) {
-        type(KeyEvent.getExtendedKeyCodeForChar(c));
-    }
-
-    /**
-     * Types given array of characters one by one
-     *
-     * @param   symbols Array of characters to be typed
-     *
-     * @see     #type(char)
-     */
-    public void type(char[] symbols) {
-        for (int i = 0; i < symbols.length; i++) {
-            type(symbols[i]);
-        }
-    }
-
-    /**
-     * Types given string
-     *
-     * @param   s   String to be typed
-     *
-     * @see     #type(char[])
-     */
-    public void type(String s) {
-        type(s.toCharArray());
+        throw new RuntimeException("dragAndDrop");
+//        dragAndDrop(from.x, from.y, to.x, to.y);
     }
 }
