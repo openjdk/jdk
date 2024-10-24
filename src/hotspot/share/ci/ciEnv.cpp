d28c366f9ed31560d6674e02d983e93ab853b3e1
MBaesken <matthias.baesken@sap.com>
2024-10-24 15:57:54 +0200

    JDK-8342823

--- src/hotspot/share/ci/ciEnv.cpp
+++ src/hotspot/share/ci/ciEnv.cpp
@@ -1611,7 +1611,9 @@ void ciEnv::dump_replay_data_helper(outputStream* out) {
 
   // The very first entry is the InstanceKlass of the root method of the current compilation in order to get the right
   // protection domain to load subsequent classes during replay compilation.
-  ciInstanceKlass::dump_replay_instanceKlass(out, task()->method()->method_holder());
+  if (this->task() != nullptr) {
+    ciInstanceKlass::dump_replay_instanceKlass(out, task()->method()->method_holder());
+  }
 
   for (int i = 0; i < objects->length(); i++) {
     objects->at(i)->dump_replay_data(out);
