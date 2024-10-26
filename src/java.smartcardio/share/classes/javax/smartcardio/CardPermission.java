7f86027d58820ffb153fb1b4daa89ccb1ac42a98
Sean Mullan <sean.mullan@oracle.com>
2024-10-26 11:19:26 -0400

    Remove apiNote. SM changes in smartcardio APIs should be done via JSR 268.

--- src/java.smartcardio/share/classes/javax/smartcardio/CardPermission.java
+++ src/java.smartcardio/share/classes/javax/smartcardio/CardPermission.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -65,10 +65,6 @@ import java.security.Permission;
  * {@linkplain Card#openLogicalChannel}
  *
  * </dl>
- *
- * @apiNote
- * This permission cannot be used for controlling access to resources
- * as the Security Manager is no longer supported.
  *
  * @since   1.6
  * @author  Andreas Sterbenz
