9eb275c4aaf9a88127c5c33e0bf7ca35125f29ea
Harshitha Onkar <harshitha.onkar@oracle.com>
2024-10-28 10:53:13 -0700

    clientlibs: Updated javax/swing/UIDefaults/6622002/bug6622002.java Removed createPrivateValue(), no longer used.

--- test/jdk/javax/swing/UIDefaults/6622002/bug6622002.java
+++ test/jdk/javax/swing/UIDefaults/6622002/bug6622002.java
@@ -26,7 +26,6 @@ import javax.swing.UIDefaults;
 /*
  * @test
  * @bug 6622002
- * @author Alexander Potochkin
  * @summary UIDefault.ProxyLazyValue has unsafe reflection usage
  * @run main bug6622002
  */
@@ -34,19 +33,10 @@ import javax.swing.UIDefaults;
 public class bug6622002 {
      public static void main(String[] args) {
 
-         if (createPrivateValue() == null) {
-             throw new RuntimeException("The private value unexpectedly wasn't created");
-         }
-
          if (createPublicValue() == null) {
              throw new RuntimeException("The public value unexpectedly wasn't created");
          }
      }
-
-    private static Object createPrivateValue() {
-        return new UIDefaults.ProxyLazyValue(
-            "javax.swing.MultiUIDefaults").createValue(null);
-    }
 
     private static Object createPublicValue() {
         return new UIDefaults.ProxyLazyValue(
