b35d2920c75f276ff5731a2f1789c9ce108e4f22
hanklo6 <hank.lo@intel.com>
2024-10-28 13:27:02 -0700

    Add ndd and nf tests

--- test/hotspot/gtest/x86/test_assemblerx86.cpp
+++ test/hotspot/gtest/x86/test_assemblerx86.cpp
@@ -66,6 +66,7 @@ static void asm_check(const uint8_t *insns, const uint8_t *insns1, const unsigne
 }
 
 TEST_VM(AssemblerX86, validate) {
+  UseAVX = 3;
   FlagSetting flag_change_apx(UseAPX, true);
   VM_Version::set_bmi_cpuFeatures();
   VM_Version::set_evex_cpuFeatures();
