0f03ca0b4ce541dfe4c3017924cf10112305aa2a
Fernando Guallini <fernando.guallini@oracle.com>
2024-10-29 16:53:00 +0100

    add more algos to TestExplicitKeyLength.java

--- test/jdk/com/sun/crypto/provider/KeyGenerator/TestExplicitKeyLength.java
+++ test/jdk/com/sun/crypto/provider/KeyGenerator/TestExplicitKeyLength.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,10 +35,10 @@ import java.util.*;
 
 public class TestExplicitKeyLength {
 
-    private static final String ALGOS[] = { "RC2", "ARCFOUR" };
+    private static final String ALGOS[] = { "RC2", "ARCFOUR", "AES", "AES", "AES" };
 
     private static final int KEY_SIZES[] =
-        { 64, 80 }; // in bits
+        { 64, 80, 128, 192, 256 }; // in bits
 
     public static void runTest(String algo, int keysize) throws Exception {
         KeyGenerator kg = KeyGenerator.getInstance(algo, "SunJCE");
