6c74ac766e2d7a9ffd1f481e0d8cac5074deca9c
TheShermanTanker <tanksherman27@gmail.com>
2024-10-31 13:38:10 +0800

    Remove the got local

--- src/java.base/windows/native/libjava/HostLocaleProviderAdapter_md.c
+++ src/java.base/windows/native/libjava/HostLocaleProviderAdapter_md.c
@@ -967,33 +967,36 @@ void getFixPart(const jchar * langtag, const jint numberStyle, BOOL positive, BO
     int style = numberStyle;
  // int got = 0;
 
-/*
     if (positive) {
         if (style == sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_CURRENCY) {
-            got = getLocaleInfoWrapper(langtag,
+         // got =
+                  getLocaleInfoWrapper(langtag,
                 LOCALE_ICURRENCY | LOCALE_RETURN_NUMBER,
                 (LPWSTR)&pattern, sizeof(pattern));
         } else if (style == sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_PERCENT) {
-            got = getLocaleInfoWrapper(langtag,
+         // got =
+                  getLocaleInfoWrapper(langtag,
                 LOCALE_IPOSITIVEPERCENT | LOCALE_RETURN_NUMBER,
                 (LPWSTR)&pattern, sizeof(pattern));
         }
     } else {
         if (style == sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_CURRENCY) {
-            got = getLocaleInfoWrapper(langtag,
+         // got =
+                  getLocaleInfoWrapper(langtag,
                 LOCALE_INEGCURR | LOCALE_RETURN_NUMBER,
                 (LPWSTR)&pattern, sizeof(pattern));
         } else if (style == sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_PERCENT) {
-            got = getLocaleInfoWrapper(langtag,
+         // got =
+                  getLocaleInfoWrapper(langtag,
                 LOCALE_INEGATIVEPERCENT | LOCALE_RETURN_NUMBER,
                 (LPWSTR)&pattern, sizeof(pattern));
         } else {
-            got = getLocaleInfoWrapper(langtag,
+         // got =
+                  getLocaleInfoWrapper(langtag,
                 LOCALE_INEGNUMBER | LOCALE_RETURN_NUMBER,
                 (LPWSTR)&pattern, sizeof(pattern));
         }
     }
-*/
 
     if (numberStyle == sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_INTEGER) {
         style = sun_util_locale_provider_HostLocaleProviderAdapterImpl_NF_NUMBER;
