5b5fd5f141f27950d21d1e2c889773f2c37c448e
Archie L. Cobbs <archie.cobbs@gmail.com>
2024-10-31 15:52:09 -0500

    Remove unnecessary @SuppressWarnings annotations.

--- src/java.base/share/classes/java/nio/channels/FileChannel.java
+++ src/java.base/share/classes/java/nio/channels/FileChannel.java
@@ -309,7 +309,7 @@ public abstract class FileChannel
         return provider.newFileChannel(path, options, attrs);
     }
 
-    @SuppressWarnings({"unchecked", "rawtypes"}) // generic array construction
+    @SuppressWarnings("rawtypes") // generic array construction
     private static final FileAttribute<?>[] NO_ATTRIBUTES = new FileAttribute[0];
 
     /**
