6057ff6cddd65ac2362f5865abe7dca89fcc8ff9
Johan Sjölén <johan.sjolen@oracle.com>
2024-11-04 10:09:24 +0100

    Typo

--- src/hotspot/share/runtime/mutexLocker.hpp
+++ src/hotspot/share/runtime/mutexLocker.hpp
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
- * DO NO ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
