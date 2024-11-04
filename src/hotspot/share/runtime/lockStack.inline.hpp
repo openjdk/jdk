7a1591e7dd20e8647b2218fca10d72e163cc84d9
Joel Sikström <joel.sikstrom@oracle.com>
2024-11-04 14:44:12 +0100

    Add comment on why pre verify is not called/performed

--- src/hotspot/share/runtime/lockStack.inline.hpp
+++ src/hotspot/share/runtime/lockStack.inline.hpp
@@ -216,6 +216,9 @@ inline bool LockStack::contains(oop o) const {
 }
 
 inline void LockStack::oops_do(OopClosure* cl) {
+  // We dont perform pre oops_do verify here because this function
+  // is used by the GC to fix the oops.
+
   int end = to_index(_top);
   for (int i = 0; i < end; i++) {
     cl->do_oop(&_base[i]);
