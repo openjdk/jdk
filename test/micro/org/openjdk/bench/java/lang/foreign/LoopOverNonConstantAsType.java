a56bc3a5d461f2f27ee77e169d7bc6b9e80247cb
Liam Miller-Cushon <cushon@google.com>
2024-11-04 14:19:09 -0800

    Update another reference to sun.misc.Unsafe

--- test/micro/org/openjdk/bench/java/lang/foreign/LoopOverNonConstantAsType.java
+++ test/micro/org/openjdk/bench/java/lang/foreign/LoopOverNonConstantAsType.java
@@ -37,7 +37,7 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Warmup;
-import sun.misc.Unsafe;
+import jdk.internal.misc.Unsafe;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
