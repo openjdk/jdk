4acd5fea6986e5627d358f1d146cbf521f85daca
rwestrel <rwestrel@redhat.com>
2024-11-06 16:10:45 +0100

    more

--- src/hotspot/share/opto/castnode.cpp
+++ src/hotspot/share/opto/castnode.cpp
@@ -303,29 +303,53 @@ const Type* CastLLNode::Value(PhaseGVN* phase) const {
   return widen_type(phase, res, T_LONG);
 }
 
+bool CastLLNode::inner_loop_backedge(Node* proj) {
+  if (proj != nullptr) {
+    Node* ctrl_use = proj->unique_ctrl_out_or_null();
+    if (ctrl_use != nullptr && ctrl_use->Opcode() == Op_Loop &&
+        ctrl_use->in(2) == proj &&
+        ctrl_use->as_Loop()->is_loop_nest_inner_loop()) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool CastLLNode::cmp_used_at_inner_loop_exit_test(Node* cmp) {
+  for (DUIterator_Fast imax, i = cmp->fast_outs(imax); i < imax; i++) {
+    Node* bol = cmp->fast_out(i);
+    if (bol->Opcode() == Op_Bool) {
+      for (DUIterator_Fast jmax, j = bol->fast_outs(jmax); j < jmax; j++) {
+        Node* iff = bol->fast_out(j);
+        if (iff->Opcode() == Op_If) {
+          Node* true_proj = iff->as_If()->proj_out_or_null(true);
+          Node* false_proj = iff->as_If()->proj_out_or_null(false);
+          if (inner_loop_backedge(true_proj) || inner_loop_backedge(false_proj)) {
+            return true;
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+
 bool CastLLNode::used_at_inner_loop_exit_test() {
   for (DUIterator_Fast imax, i = fast_outs(imax); i < imax; i++) {
     Node* convl2i = fast_out(i);
     if (convl2i->Opcode() == Op_ConvL2I) {
       for (DUIterator_Fast jmax, j = convl2i->fast_outs(jmax); j < jmax; j++) {
-        Node* cmp = convl2i->fast_out(j);
-        if (cmp->Opcode() == Op_CmpI) {
-          for (DUIterator_Fast kmax, k = cmp->fast_outs(kmax); k < kmax; k++) {
-            Node* bol = cmp->fast_out(k);
-            if (bol->Opcode() == Op_Bool) {
-              for (DUIterator_Fast lmax, l = bol->fast_outs(lmax); l < lmax; l++) {
-                Node* iff = bol->fast_out(l);
-                if (iff->Opcode() == Op_If) {
-                  Node* true_proj = iff->as_If()->proj_out_or_null(true);
-                  if (true_proj != nullptr) {
-                    Node* ctrl_use = true_proj->unique_ctrl_out_or_null();
-                    if (ctrl_use != nullptr && ctrl_use->Opcode() == Op_Loop &&
-                        ctrl_use->in(2) == true_proj &&
-                        ctrl_use->as_Loop()->is_loop_nest_inner_loop()) {
-                      return true;
-                    }
-                  }
-                }
+        Node* cmp_or_sub = convl2i->fast_out(j);
+        if (cmp_or_sub->Opcode() == Op_CmpI) {
+          if (cmp_used_at_inner_loop_exit_test(cmp_or_sub)) {
+            return true;
+          }
+        } else if (cmp_or_sub->Opcode() == Op_SubI && cmp_or_sub->in(1)->find_int_con(-1) == 0) {
+          for (DUIterator_Fast kmax, k = cmp_or_sub->fast_outs(kmax); k < kmax; k++) {
+            Node* cmp = cmp_or_sub->fast_out(k);
+            if (cmp->Opcode() == Op_CmpI) {
+              if (cmp_used_at_inner_loop_exit_test(cmp)) {
+                return true;
               }
             }
           }
