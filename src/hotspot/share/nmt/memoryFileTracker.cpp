8fd9b9d81dbe9df3c528283e1ecec1a3add7a25a
Johan Sjölén <johan.sjolen@oracle.com>
2024-11-06 17:37:49 +0100

    Revert "8304824: NMT should not use ThreadCritical"
    This reverts commit 0abfa3ba8f72538f62be838c1ebac8cfbdd14cdf.

--- src/hotspot/share/nmt/memoryFileTracker.cpp
+++ src/hotspot/share/nmt/memoryFileTracker.cpp
@@ -29,11 +29,13 @@
 #include "nmt/nmtCommon.hpp"
 #include "nmt/nmtNativeCallStackStorage.hpp"
 #include "nmt/vmatree.hpp"
+#include "runtime/mutex.hpp"
 #include "utilities/growableArray.hpp"
 #include "utilities/nativeCallStack.hpp"
 #include "utilities/ostream.hpp"
 
 MemoryFileTracker* MemoryFileTracker::Instance::_tracker = nullptr;
+PlatformMutex* MemoryFileTracker::Instance::_mutex = nullptr;
 
 MemoryFileTracker::MemoryFileTracker(bool is_detailed_mode)
   : _stack_storage(is_detailed_mode), _files() {}
@@ -130,6 +132,7 @@ bool MemoryFileTracker::Instance::initialize(NMT_TrackingLevel tracking_level) {
   _tracker = static_cast<MemoryFileTracker*>(os::malloc(sizeof(MemoryFileTracker), mtNMT));
   if (_tracker == nullptr) return false;
   new (_tracker) MemoryFileTracker(tracking_level == NMT_TrackingLevel::NMT_detail);
+  _mutex = new PlatformMutex();
   return true;
 }
 
@@ -189,4 +192,12 @@ void MemoryFileTracker::summary_snapshot(VirtualMemorySnapshot* snapshot) const 
 
 void MemoryFileTracker::Instance::summary_snapshot(VirtualMemorySnapshot* snapshot) {
   _tracker->summary_snapshot(snapshot);
+}
+
+MemoryFileTracker::Instance::Locker::Locker() {
+  MemoryFileTracker::Instance::_mutex->lock();
+}
+
+MemoryFileTracker::Instance::Locker::~Locker() {
+  MemoryFileTracker::Instance::_mutex->unlock();
 }
