0fd894fd2adcd91f3b4c50a0dcc0ffd16bee7b6d
Toby Holenstein <tobias.holenstein@oracle.com>
2024-11-06 13:36:24 +0100

    Update ColorAction.java

--- src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/actions/ColorAction.java
+++ src/utils/IdealGraphVisualizer/View/src/main/java/com/sun/hotspot/igv/view/actions/ColorAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -72,7 +72,6 @@ public final class ColorAction extends ModelAwareAction {
                 editor.colorSelectedFigures(selectedColor);
             }
         }
-        //model.showOnly(model.getSelectedNodes());
     }
 
     @Override
