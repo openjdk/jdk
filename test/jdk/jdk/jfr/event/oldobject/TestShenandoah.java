30a73e8a96d3cb8415a995ee584b7ef55cdf0aec
sendaoYan <yansendao.ysd@alibaba-inc.com>
2024-11-07 20:57:39 +0800

    Move Problemlist entry jdk/jfr/event/oldobject/TestShenandoah.java from test/jdk/ProblemList-shenandoah.txt to test/jdk/ProblemList.txt

--- test/jdk/jdk/jfr/event/oldobject/TestShenandoah.java
+++ test/jdk/jdk/jfr/event/oldobject/TestShenandoah.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2021, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,7 +39,7 @@ import jdk.test.lib.jfr.Events;
  * @summary Test leak profiler with Shenandoah
  * @library /test/lib /test/jdk
  * @modules jdk.jfr/jdk.jfr.internal.test
- * @run main/othervm -XX:TLABSize=2k jdk.jfr.event.oldobject.TestShenandoah
+ * @run main/othervm  -XX:TLABSize=2k -XX:+UseShenandoahGC jdk.jfr.event.oldobject.TestShenandoah
  */
 public class TestShenandoah {
 
