318b1811c6f2ceaddd60e107636c40f39e4a16bd
Daniel Fuchs <daniel.fuchs@oracle.com>
2024-11-08 17:45:35 +0000

    Add missing bug id

--- test/jdk/java/net/httpclient/http2/StreamFlowControlTest.java
+++ test/jdk/java/net/httpclient/http2/StreamFlowControlTest.java
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8342075
+ * @bug 8342075 8343855
  * @library /test/lib /test/jdk/java/net/httpclient/lib
  * @build jdk.httpclient.test.lib.http2.Http2TestServer jdk.test.lib.net.SimpleSSLContext
  * @run testng/othervm  -Djdk.internal.httpclient.debug=true
