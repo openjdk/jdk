eb40a88f4076360708402454a494907e8c0c845d
Fei Yang <fyang@openjdk.org>
2024-11-13 00:07:44 +0000

    8343430: RISC-V: C2: Remove old trampoline call
    Reviewed-by: mli, rehn

--- src/hotspot/cpu/riscv/globals_riscv.hpp
+++ src/hotspot/cpu/riscv/globals_riscv.hpp
@@ -119,8 +119,6 @@ define_pd_global(intx, InlineSmallCode,          1000);
           "Use Zvkn group extension, Zvkned, Zvknhb, Zvkb, Zvkt")                \
   product(bool, UseRVVForBigIntegerShiftIntrinsics, true,                        \
           "Use RVV instructions for left/right shift of BigInteger")             \
-  product(bool, UseTrampolines, false, EXPERIMENTAL,                             \
-          "Far calls uses jal to trampoline.")                                   \
   product(bool, UseCtxFencei, false, EXPERIMENTAL,                               \
           "Use PR_RISCV_CTX_SW_FENCEI_ON to avoid explicit icache flush")
 
