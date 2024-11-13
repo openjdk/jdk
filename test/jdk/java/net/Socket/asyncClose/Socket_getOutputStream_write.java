186fd9add66ce477678645ea787e2a27f9e07176
Jaikiran Pai <jaikiran.pai@gmail.com>
2024-11-13 20:31:41 +0530

    8343877: Test AsyncClose.java intermittent fails - Socket.getInputStream().read() wasn't preempted

--- test/jdk/java/net/Socket/asyncClose/Socket_getOutputStream_write.java
+++ test/jdk/java/net/Socket/asyncClose/Socket_getOutputStream_write.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -68,13 +68,14 @@ public class Socket_getOutputStream_write extends AsyncCloseTest implements Runn
             InetAddress lh = InetAddress.getLocalHost();
             ServerSocket ss = new ServerSocket(0, 0, lh);
             s.connect( new InetSocketAddress(lh, ss.getLocalPort()) );
-            Socket s2 = ss.accept();
-            Thread thr = new Thread(this);
-            thr.start();
-            latch.await();
-            Thread.sleep(1000);
-            s.close();
-            thr.join();
+            try (Socket s2 = ss.accept()) {
+                Thread thr = new Thread(this);
+                thr.start();
+                latch.await();
+                Thread.sleep(1000);
+                s.close();
+                thr.join();
+            }
 
             if (isClosed()) {
                 return passed();
