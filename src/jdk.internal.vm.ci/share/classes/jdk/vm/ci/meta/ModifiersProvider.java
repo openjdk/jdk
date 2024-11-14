7a56b6448e74353037fbc653c72642a16761f1a8
Yudi Zheng <yudi.zheng@oracle.com>
2024-11-14 17:29:17 +0100

    Override ModifiersProvider.isConcrete in ResolvedJavaType

--- src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/meta/ModifiersProvider.java
+++ src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/meta/ModifiersProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -137,9 +137,9 @@ public interface ModifiersProvider {
     }
 
     /**
-     * Checks that the method is concrete and not abstract.
+     * Checks that this element is concrete and not abstract.
      *
-     * @return whether the method is a concrete method
+     * @return whether this element is concrete
      */
     default boolean isConcrete() {
         return !isAbstract();
