7e17da7f97da9f0c17b78fb445a9f9ff1102abb7
Magnus Ihse Bursie <mag@icus.se>
2024-11-14 13:14:30 +0100

    Update build tools, data and IDE support

--- make/data/autoheaders/assemblyprefix.h
+++ make/data/autoheaders/assemblyprefix.h
@@ -1,12 +1,10 @@
 #
-# Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2022, 2024, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # This code is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License version 2 only, as
-# published by the Free Software Foundation.  Oracle designates this
-# particular file as subject to the "Classpath" exception as provided
-# by Oracle in the LICENSE file that accompanied this code.
+# published by the Free Software Foundation.
 #
 # This code is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -28,4 +26,3 @@
 // reproducible object file through a predictable value of the STT_FILE
 // symbol, and subsequently a reproducible .debuginfo.
 .file ASSEMBLY_SRC_FILE
-
