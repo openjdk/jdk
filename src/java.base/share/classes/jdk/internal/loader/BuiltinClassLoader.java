742fe89cc424099fa8e9ba32f7ffb53101c4e0f6
Alan Bateman <alan.bateman@oracle.com>
2024-11-14 11:20:43 +0000

    Change 2-arg findResource to use findResourceOnClassPath when finding resources on class path

--- src/java.base/share/classes/jdk/internal/loader/BuiltinClassLoader.java
+++ src/java.base/share/classes/jdk/internal/loader/BuiltinClassLoader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -293,7 +293,7 @@ public class BuiltinClassLoader
                 in = moduleReaderFor(mref).open(name).orElse(null);
             }
         } else {
-            URL url = findResource(name);
+            URL url = findResourceOnClassPath(name);
             if (url != null) {
                 in = url.openStream();
             }
