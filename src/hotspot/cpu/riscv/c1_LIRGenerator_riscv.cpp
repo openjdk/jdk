e8584412045158a69463c5d6022a25a9084a8be0
Amit Kumar <amitlocham09@icloud.com>
2024-11-15 15:22:13 +0530

    add safety net

--- src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
@@ -234,7 +234,7 @@ void LIRGenerator::cmp_reg_mem(LIR_Condition condition, LIR_Opr reg, LIR_Opr bas
 }
 
 bool LIRGenerator::strength_reduce_multiply(LIR_Opr left, jint c, LIR_Opr result, LIR_Opr tmp) {
-  if (tmp->is_valid() && c > 0 && c < max_jint) {
+  if (tmp->is_valid()) {
     if (is_power_of_2(c - 1)) {
       __ shift_left(left, exact_log2(c - 1), tmp);
       __ add(tmp, left, result);
