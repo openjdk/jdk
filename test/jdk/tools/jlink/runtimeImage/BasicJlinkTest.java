b6a5aa858615fe92721331b982cc6acfaaad3a64
MBaesken <matthias.baesken@sap.com>
2024-11-15 10:16:21 +0100

    JDK-8344036

--- test/jdk/tools/jlink/runtimeImage/BasicJlinkTest.java
+++ test/jdk/tools/jlink/runtimeImage/BasicJlinkTest.java
@@ -39,7 +39,7 @@ import tests.Helper;
  *          jdk.jlink/jdk.tools.jimage
  * @build tests.* jdk.test.lib.process.OutputAnalyzer
  *        jdk.test.lib.process.ProcessTools
- * @run main/othervm -Xmx1g BasicJlinkTest false
+ * @run main/othervm -Xmx2g BasicJlinkTest false
  */
 public class BasicJlinkTest extends AbstractLinkableRuntimeTest {
 
