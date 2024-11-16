05c7ea96e922a9bed5aeb46f408a7424a38609d2
Xiaolong Peng <xlpeng@amazon.com>
2024-11-15 17:48:53 -0800

    format

--- src/hotspot/share/gc/shenandoah/shenandoahPacer.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahPacer.cpp
@@ -256,15 +256,15 @@ void ShenandoahPacer::pace_for_alloc(size_t words) {
 
   if (!claimed) {
     jlong const start_time = os::elapsed_counter();
-    jlong const deadline = start_time + (jlong)((ShenandoahPacingMaxDelay / 1000.0) * (double) os::elapsed_frequency());
+    jlong const deadline = start_time + (jlong)(ShenandoahPacingMaxDelay / 1000.0 * (double) os::elapsed_frequency());
     bool timeout = false;
     while (Atomic::load(&_budget) < 0 &&
-          os::elapsed_counter() < deadline)  {
+           os::elapsed_counter() < deadline) {
       // We could instead assist GC, but this would suffice for now.
       timeout = wait(1);
       NOT_WINDOWS(if (!timeout) break;)
     }
-    ShenandoahThreadLocalData::add_paced_time(current, (double) (os::elapsed_counter() - start_time) / (double) os::elapsed_frequency());
+    ShenandoahThreadLocalData::add_paced_time(current, (double)(os::elapsed_counter() - start_time) / (double)os::elapsed_frequency());
   }
 }
 
