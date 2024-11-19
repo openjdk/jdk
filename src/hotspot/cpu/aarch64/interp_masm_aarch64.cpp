29e0d3ecbb582551a4f93f497e142c35a4c88794
sendaoYan <yansendao.ysd@alibaba-inc.com>
2024-11-19 18:02:13 +0800

    use cmp instead cmpw

--- src/hotspot/cpu/aarch64/interp_masm_aarch64.cpp
+++ src/hotspot/cpu/aarch64/interp_masm_aarch64.cpp
@@ -395,8 +395,8 @@ void InterpreterMacroAssembler::dispatch_base(TosState state,
   if (VerifyActivationFrameSize) {
     Label L;
     sub(rscratch2, rfp, esp);
-    int32_t min_frame_size = (frame::link_offset - frame::interpreter_frame_mirror_offset) * wordSize;
-    cmpw(rscratch2, min_frame_size);
+    unsigned char min_frame_size = (frame::link_offset - frame::interpreter_frame_mirror_offset) * wordSize;
+    cmp(rscratch2, min_frame_size);
     br(Assembler::GE, L);
     stop("broken stack frame");
     bind(L);
