7292c568cb65eaafb5e6429f0e2e33d7c9b22c41
Taizo Kurashige <taizousum11@gmail.com>
2024-11-19 22:58:47 +0900

    Fix how to execute chcp command

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/Executor.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/Executor.java
@@ -217,6 +217,11 @@ public final class Executor extends CommandArguments<Executor> {
                     "Can't change directory when using tool provider");
         }
 
+        if (toolProvider != null && winEnglishOutput) {
+            throw new IllegalArgumentException(
+                    "Can't change locale when using tool provider");
+        }
+
         return ThrowingSupplier.toSupplier(() -> {
             if (toolProvider != null) {
                 return runToolProvider();
@@ -334,19 +339,19 @@ public final class Executor extends CommandArguments<Executor> {
         return executable.toAbsolutePath();
     }
 
+    private List<String> prefixCommandLineArgs() {
+        if (winEnglishOutput) {
+            return List.of("cmd.exe", "/c", "chcp", "65001", ">nul", "2>&1", "&&");
+        } else {
+            return List.of();
+        }
+    }
+
     private Result runExecutable() throws IOException, InterruptedException {
         List<String> command = new ArrayList<>();
-
-        if (winEnglishOutput) {
-            // run chcp to change the code page to UTF-8 on Windows
-            command.add("cmd.exe");
-            command.add("/c");
-            command.add("chcp 65001 >nul 2>&1 && " + printCommandLine(executablePath().toString(), args));
-        } else {
-            command.add(executablePath().toString());
-            command.addAll(args);
-        }
-
+        command.addAll(prefixCommandLineArgs());
+        command.add(executablePath().toString());
+        command.addAll(args);
         ProcessBuilder builder = new ProcessBuilder(command);
         if (winTmpDir != null) {
             builder.environment().put("TMP", winTmpDir);
@@ -476,18 +481,17 @@ public final class Executor extends CommandArguments<Executor> {
             exec = executablePath().toString();
         }
 
-        String chcpCmdLine = winEnglishOutput ? "cmd.exe /c chcp 65001 && " : "";
-        int chcpTokenNum = chcpCmdLine.isEmpty() ? 0 : chcpCmdLine.split(" ").length;
+        var cmdline = Stream.of(prefixCommandLineArgs(), List.of(exec), args).flatMap(
+                List::stream).toList();
 
-        return String.format(format, chcpCmdLine + printCommandLine(exec, args),
-                chcpTokenNum + args.size() + 1);
+        return String.format(format, printCommandLine(cmdline), cmdline.size() + 1);
     }
 
-    private static String printCommandLine(String executable, List<String> args) {
+    private static String printCommandLine(List<String> cmdline) {
         // Want command line printed in a way it can be easily copy/pasted
         // to be executed manually
         Pattern regex = Pattern.compile("\\s");
-        return Stream.concat(Stream.of(executable), args.stream()).map(
+        return cmdline.stream().map(
                 v -> (v.isEmpty() || regex.matcher(v).find()) ? "\"" + v + "\"" : v).collect(
                         Collectors.joining(" "));
     }
