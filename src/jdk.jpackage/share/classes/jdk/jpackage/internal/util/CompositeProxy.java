345ccbfd2d699575f600684e4ac81f3ca9662a3d
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2024-11-19 14:28:11 -0500

    Merge CompositeProxy and CompositeProxySpec classes

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/util/CompositeProxy.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/util/CompositeProxy.java
@@ -26,9 +26,13 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
 import static java.util.stream.Collectors.toMap;
@@ -74,8 +78,18 @@ public final class CompositeProxy {
             BinaryOperator<Method> conflictResolver, InvokeTunnel invokeTunnel,
             Object... slices) {
 
-        final Map<Class<?>, Object> interfaceDispatch = CompositeProxySpec.create(
-                interfaceType, slices).getInterfaceDispatch();
+        validateTypeIsInterface(interfaceType);
+
+        final var interfaces = interfaceType.getInterfaces();
+        List.of(interfaces).forEach(CompositeProxy::validateTypeIsInterface);
+
+        if (interfaces.length != slices.length) {
+            throw new IllegalArgumentException(String.format(
+                    "type %s must extend %d interfaces", interfaceType.getName(),
+                    slices.length));
+        }
+
+        final Map<Class<?>, Object> interfaceDispatch = createInterfaceDispatch(interfaces, slices);
 
         final Map<Method, Handler> methodDispatch = getProxyableMethods(interfaceType).map(method -> {
             var handler = createHandler(interfaceType, method, interfaceDispatch,
@@ -90,9 +104,52 @@ public final class CompositeProxy {
         @SuppressWarnings("unchecked")
         T proxy = (T) Proxy.newProxyInstance(interfaceType.getClassLoader(),
                 new Class<?>[]{interfaceType},
-                new DynamicProxyInvocationHandler(methodDispatch));
+                new CompositeProxyInvocationHandler(methodDispatch));
 
         return proxy;
+    }
+
+    private static Map<Class<?>, Object> createInterfaceDispatch(
+            Class<?>[] interfaces, Object[] slices) {
+
+        final Map<Class<?>, Object> interfaceDispatch = Stream.of(interfaces).collect(toMap(x -> x, iface -> {
+            return Stream.of(slices).filter(obj -> {
+                return Set.of(obj.getClass().getInterfaces()).contains(iface);
+            }).reduce((a, b) -> {
+                throw new IllegalArgumentException(String.format(
+                        "both [%s] and [%s] slices implement %s", a, b, iface));
+            }).orElseThrow(() -> createInterfaceNotImplementedException(List.of(iface)));
+        }));
+
+        if (interfaceDispatch.size() != interfaces.length) {
+            final List<Class<?>> missingInterfaces = new ArrayList<>(Set.of(interfaces));
+            missingInterfaces.removeAll(interfaceDispatch.entrySet());
+            throw createInterfaceNotImplementedException(missingInterfaces);
+        }
+
+        return Stream.of(interfaces).flatMap(iface -> {
+            return unfoldInterface(iface).map(unfoldedIface -> {
+                return Map.entry(unfoldedIface, interfaceDispatch.get(iface));
+            });
+        }).collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private static Stream<Class<?>> unfoldInterface(Class<?> interfaceType) {
+        return Stream.concat(Stream.of(interfaceType), Stream.of(
+                interfaceType.getInterfaces()).flatMap(CompositeProxy::unfoldInterface));
+    }
+
+    private static IllegalArgumentException createInterfaceNotImplementedException(
+            Collection<Class<?>> missingInterfaces) {
+        return new IllegalArgumentException(String.format(
+                "none of the slices implement %s", missingInterfaces));
+    }
+
+    private static void validateTypeIsInterface(Class<?> type) {
+        if (!type.isInterface()) {
+            throw new IllegalArgumentException(String.format(
+                    "type %s must be an interface", type.getName()));
+        }
     }
 
     private static Handler createHandler(Class<?> interfaceType, Method method,
@@ -170,7 +227,7 @@ public final class CompositeProxy {
         return Objects.equals(a.getReturnType(), b.getReturnType());
     }
 
-    private record DynamicProxyInvocationHandler(Map<Method, Handler> dispatch) implements InvocationHandler {
+    private record CompositeProxyInvocationHandler(Map<Method, Handler> dispatch) implements InvocationHandler {
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
