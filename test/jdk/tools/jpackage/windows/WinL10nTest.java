92cf525d64a07ea9dd35116911626185fe3c3663
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2024-11-19 15:36:14 -0500

    Redundant change reverted

--- test/jdk/tools/jpackage/windows/WinL10nTest.java
+++ test/jdk/tools/jpackage/windows/WinL10nTest.java
@@ -34,7 +34,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import jdk.jpackage.internal.IOUtils;
 import jdk.jpackage.test.Executor;
 import static jdk.jpackage.test.WindowsHelper.WixType.WIX3;
 import static jdk.jpackage.test.WindowsHelper.getWixTypeFromVerboseJPackageOutput;
