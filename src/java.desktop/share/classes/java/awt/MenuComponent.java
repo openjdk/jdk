30152179feaaa49943d4c42d05f5c330cdd846b5
prrace <philip.race@oracle.com>
2024-11-21 13:21:37 -0800

    8344795

--- src/java.desktop/share/classes/java/awt/MenuComponent.java
+++ src/java.desktop/share/classes/java/awt/MenuComponent.java
@@ -30,8 +30,6 @@ import java.awt.peer.MenuComponentPeer;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serial;
-import java.security.AccessControlContext;
-import java.security.AccessController;
 
 import javax.accessibility.Accessible;
 import javax.accessibility.AccessibleComponent;
@@ -102,25 +100,6 @@ public abstract class MenuComponent implements java.io.Serializable {
      * @see #dispatchEvent(AWTEvent)
      */
     volatile boolean newEventsOnly;
-
-    /*
-     * The menu's AccessControlContext.
-     */
-    @SuppressWarnings("removal")
-    private transient volatile AccessControlContext acc =
-            AccessController.getContext();
-
-    /*
-     * Returns the acc this menu component was constructed with.
-     */
-    @SuppressWarnings("removal")
-    final AccessControlContext getAccessControlContext() {
-        if (acc == null) {
-            throw new SecurityException(
-                    "MenuComponent is missing AccessControlContext");
-        }
-        return acc;
-    }
 
     /*
      * Internal constants for serialization.
@@ -448,8 +427,6 @@ public abstract class MenuComponent implements java.io.Serializable {
         throws ClassNotFoundException, IOException, HeadlessException
     {
         GraphicsEnvironment.checkHeadless();
-
-        acc = AccessController.getContext();
 
         s.defaultReadObject();
 
--- src/java.desktop/share/classes/javax/swing/UIDefaults.java
+++ src/java.desktop/share/classes/javax/swing/UIDefaults.java
@@ -49,8 +49,6 @@ import java.awt.Color;
 import java.awt.Insets;
 import java.awt.Dimension;
 import java.beans.PropertyChangeListener;
-import java.security.AccessController;
-import java.security.AccessControlContext;
 
 import sun.reflect.misc.MethodUtil;
 import sun.reflect.misc.ReflectUtil;
@@ -1062,8 +1060,6 @@ public class UIDefaults extends Hashtable<Object,Object>
      * @since 1.3
      */
     public static class ProxyLazyValue implements LazyValue {
-        @SuppressWarnings("removal")
-        private AccessControlContext acc;
         private String className;
         private String methodName;
         private Object[] args;
@@ -1119,7 +1115,6 @@ public class UIDefaults extends Hashtable<Object,Object>
          */
         @SuppressWarnings("removal")
         public ProxyLazyValue(String c, String m, Object[] o) {
-            acc = AccessController.getContext();
             className = c;
             methodName = m;
             if (o != null) {
