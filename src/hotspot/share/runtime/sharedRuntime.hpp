b0083f8c4c1bcf705005f6a9d4a6a4b70c0acaa7
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2024-11-21 14:38:54 +0800

    Revise comment in sharedRuntime.hpp

--- src/hotspot/share/runtime/sharedRuntime.hpp
+++ src/hotspot/share/runtime/sharedRuntime.hpp
@@ -140,7 +140,12 @@ class SharedRuntime: AllStatic {
 
 
 #ifdef _WIN64
-  // Workaround for fmod issue in the Windows x64 CRT
+  // This used to be a workaround for the fmod issue
+  // in the Windows x64 CRT. Windows x64 now uses its
+  // own custom assembly, but during the review of
+  // 8342769 it was discovered that Windows ARM64
+  // now needs the workaround, so this is now used
+  // for Windows ARM64
   static double fmod_winx64(double x, double y);
 #endif
 
