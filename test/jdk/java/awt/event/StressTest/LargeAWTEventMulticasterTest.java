ed6a585a9685537c166fac73d4340d7dcd98cadd
jeremy <jeremy.wood@mac.com>
2024-11-25 17:24:44 -0500

    8342782: expanding wildcard imports
    This is in response to:
    https://github.com/openjdk/jdk/pull/21962#discussion_r1857389973

--- test/jdk/java/awt/event/StressTest/LargeAWTEventMulticasterTest.java
+++ test/jdk/java/awt/event/StressTest/LargeAWTEventMulticasterTest.java
@@ -21,8 +21,9 @@
  * questions.
  */
 
-import java.awt.event.*;
-import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.AWTEventMulticaster;
 
 /*
  * @test
