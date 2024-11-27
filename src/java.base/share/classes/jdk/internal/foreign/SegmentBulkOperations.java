57ee3ba7e1a2b436f6666b9465a4651f7baca7e3
Quan Anh Mai <qamai@openjdk.org>
2024-11-27 07:35:51 +0000

    8344912: Sharpen the return type of various internal methods in jdk.internal.foreign
    Reviewed-by: mcimadamore

--- src/java.base/share/classes/jdk/internal/foreign/SegmentBulkOperations.java
+++ src/java.base/share/classes/jdk/internal/foreign/SegmentBulkOperations.java
@@ -62,7 +62,7 @@ public final class SegmentBulkOperations {
         dst.checkReadOnly(false);
         if (dst.length == 0) {
             // Implicit state check
-            dst.checkValidState();
+            dst.sessionImpl().checkValidState();
         } else if (dst.length < NATIVE_THRESHOLD_FILL) {
             // 0 <= length < FILL_NATIVE_LIMIT : 0...0X...XXXX
 
