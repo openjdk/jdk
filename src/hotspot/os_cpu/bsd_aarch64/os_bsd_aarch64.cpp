7cb7f1d25f3babd9fc78d01e439acb046724f001
MBaesken <matthias.baesken@sap.com>
2024-11-28 08:50:35 +0100

    use assembler local labels

--- src/hotspot/os_cpu/bsd_aarch64/os_bsd_aarch64.cpp
+++ src/hotspot/os_cpu/bsd_aarch64/os_bsd_aarch64.cpp
@@ -502,7 +502,7 @@ static inline void atomic_copy64(const volatile void *src, volatile void *dst) {
 
 extern "C" {
   // wait function, should not be inlined; and causes trouble with linktime optimization
-  NOINLINE
+  // NOINLINE
   int SpinPause() {
     // We don't use StubRoutines::aarch64::spin_wait stub in order to
     // avoid a costly call to os::current_thread_enable_wx() on MacOS.
@@ -525,14 +525,14 @@ extern "C" {
                                       // to entry for case SpinWait::NOP
         "  add  %[d], %[d], %[o]  \n"
         "  br   %[d]              \n"
-        "  b    SpinPause_return  \n" // case SpinWait::NONE  (-1)
+        "  b    1f  \n" // case SpinWait::NONE  (-1)
         "  nop                    \n" // padding
         "  nop                    \n" // case SpinWait::NOP   ( 0)
-        "  b    SpinPause_return  \n"
+        "  b    1f  \n"
         "  isb                    \n" // case SpinWait::ISB   ( 1)
-        "  b    SpinPause_return  \n"
+        "  b    1f  \n"
         "  yield                  \n" // case SpinWait::YIELD ( 2)
-        "SpinPause_return:        \n"
+        "1:        \n"
         : [d]"=&r"(br_dst)
         : [o]"r"(off)
         : "memory");
