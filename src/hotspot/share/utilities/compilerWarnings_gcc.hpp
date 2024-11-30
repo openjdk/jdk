e725dba4a23de6b33892d7157008f8b50b989cd9
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2024-11-30 08:38:12 +0800

    Declaration fix in compilerWarnings_gcc.hpp

--- src/hotspot/share/utilities/compilerWarnings_gcc.hpp
+++ src/hotspot/share/utilities/compilerWarnings_gcc.hpp
@@ -86,7 +86,7 @@
 // so uses of functions that are both forbidden and fortified won't cause
 // forbidden warnings in such builds.
 #define FORBID_C_FUNCTION(signature, alternative) \
-  extern "C" __attribute__((__warning__(alternative))) signature;
+  [[gnu::warning(alternative)]] signature noexcept;
 
 // Disable warning attribute over the scope of the affected statement.
 // The name serves only to document the intended function.
