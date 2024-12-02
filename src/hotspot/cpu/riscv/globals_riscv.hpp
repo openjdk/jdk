c0e7d4f192a4fa38788a2c0e8454b0921616b137
hamlin <hamlin@hamlin.eu.rivosinc.com>
2024-12-02 09:45:25 +0000

    initial commit

--- src/hotspot/cpu/riscv/globals_riscv.hpp
+++ src/hotspot/cpu/riscv/globals_riscv.hpp
@@ -102,6 +102,7 @@ define_pd_global(intx, InlineSmallCode,          1000);
   product(bool, UseRVV, false, DIAGNOSTIC, "Use RVV instructions")               \
   product(bool, UseZba, false, DIAGNOSTIC, "Use Zba instructions")               \
   product(bool, UseZbb, false, DIAGNOSTIC, "Use Zbb instructions")               \
+  product(bool, UseZbc, false, EXPERIMENTAL, "Use Zbc instructions")             \
   product(bool, UseZbs, false, DIAGNOSTIC, "Use Zbs instructions")               \
   product(bool, UseZfh, false, DIAGNOSTIC, "Use Zfh instructions")               \
   product(bool, UseZacas, false, EXPERIMENTAL, "Use Zacas instructions")         \
