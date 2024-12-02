f2dc5704553a41b68de88c706f0eadab21a34715
Per Minborg <pminborg@gmail.com>
2024-12-02 15:05:33 +0100

    Correct docs for the MemorySegment::getString overloads

--- src/java.base/share/classes/java/lang/foreign/MemorySegment.java
+++ src/java.base/share/classes/java/lang/foreign/MemorySegment.java
@@ -1277,9 +1277,8 @@ public sealed interface MemorySegment permits AbstractMemorySegmentImpl {
      * @throws IllegalArgumentException if the size of the string is greater than the
      *         largest string supported by the platform
      * @throws IndexOutOfBoundsException if {@code offset < 0}
-     * @throws IndexOutOfBoundsException if {@code offset > byteSize() - (B + 1)}, where
-     *         {@code B} is the size, in bytes, of the string encoded using UTF-8 charset
-     *         {@code str.getBytes(StandardCharsets.UTF_8).length})
+     * @throws IndexOutOfBoundsException if no {@code '\0'} terminator is present in
+     *         this segment between the given {@code offset} to the end of this segment.
      * @throws IllegalStateException if the {@linkplain #scope() scope} associated with
      *         this segment is not {@linkplain Scope#isAlive() alive}
      * @throws WrongThreadException if this method is called from a thread {@code T},
@@ -1315,14 +1314,8 @@ public sealed interface MemorySegment permits AbstractMemorySegmentImpl {
      * @throws IllegalArgumentException  if the size of the string is greater than the
      *         largest string supported by the platform
      * @throws IndexOutOfBoundsException if {@code offset < 0}
-     * @throws IndexOutOfBoundsException if {@code offset > byteSize() - (B + N)}, where:
-     *         <ul>
-     *             <li>{@code B} is the size, in bytes, of the string encoded using the
-     *             provided charset (e.g. {@code str.getBytes(charset).length});</li>
-     *             <li>{@code N} is the size (in bytes) of the terminator char according
-     *             to the provided charset. For instance, this is 1 for
-     *             {@link StandardCharsets#US_ASCII} and 2 for {@link StandardCharsets#UTF_16}.</li>
-     *         </ul>
+     * @throws IndexOutOfBoundsException if no {@code '\0'} terminator is present in
+     *         this segment between the given {@code offset} to the end of this segment.
      * @throws IllegalStateException if the {@linkplain #scope() scope} associated with
      *         this segment is not {@linkplain Scope#isAlive() alive}
      * @throws WrongThreadException if this method is called from a thread {@code T},
