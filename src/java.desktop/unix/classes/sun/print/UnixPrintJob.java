328c48d2bd112c8265cddb98cb1c3054a76a8dc9
Phil Race <prr@openjdk.org>
2024-12-02 17:31:21 +0000

    8345142: Remove uses of SecurityManager in Printing related classes
    Reviewed-by: azvegint, psadhukhan

--- src/java.desktop/unix/classes/sun/print/UnixPrintJob.java
+++ src/java.desktop/unix/classes/sun/print/UnixPrintJob.java
@@ -707,12 +707,7 @@ public class UnixPrintJob implements CancelablePrintJob {
         }
 
         /* add the user name to the job */
-        String userName = "";
-        try {
-          userName = System.getProperty("user.name");
-        } catch (SecurityException se) {
-        }
-
+        String userName = System.getProperty("user.name");
         if (userName == null || userName.isEmpty()) {
             RequestingUserName ruName =
                 (RequestingUserName)reqSet.get(RequestingUserName.class);
@@ -790,17 +785,6 @@ public class UnixPrintJob implements CancelablePrintJob {
                         mDestination = (new File(uri)).getPath();
                     } catch (Exception e) {
                         throw new PrintException(e);
-                    }
-                    // check write access
-                    @SuppressWarnings("removal")
-                    SecurityManager security = System.getSecurityManager();
-                    if (security != null) {
-                      try {
-                        security.checkWrite(mDestination);
-                      } catch (SecurityException se) {
-                        notifyEvent(PrintJobEvent.JOB_FAILED);
-                        throw new PrintException(se);
-                      }
                     }
                 }
             } else if (category == JobSheets.class) {
