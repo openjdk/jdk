0f7e8d087cc14e6c39b9c16d5e0afe5646ec056d
Amit Kumar <amitlocham09@icloud.com>
2024-12-02 14:16:19 +0530

    z16 detection

--- src/hotspot/cpu/s390/vm_version_s390.hpp
+++ src/hotspot/cpu/s390/vm_version_s390.hpp
@@ -88,6 +88,9 @@ class VM_Version: public Abstract_VM_Version {
 // z13 end
 #define  MiscInstrExt2Mask              0x0000000000000020UL  // z14
 #define  MiscInstrExt3Mask              0x0000000000000004UL  // z15
+
+#define BEAREnhFacilityMask             0x4000000000000000UL  // z16, BEAR-enhancement facility, Bit: 193
+#define NNPAssistFacilityMask           0x0000000000040000UL  // z16, Neural-network-processing-assist facility, Bit: 165
 // ----------------------------------------------
 // --- FeatureBitString Bits  64..127 (DW[1]) ---
 // ----------------------------------------------
@@ -179,9 +182,10 @@ class VM_Version: public Abstract_VM_Version {
   static bool is_z10()  { return has_GnrlInstrExtensions()    && !has_DistinctOpnds(); }
   static bool is_z196() { return has_DistinctOpnds()          && !has_MiscInstrExt(); }
   static bool is_ec12() { return has_MiscInstrExt()           && !has_CryptoExt5(); }
-  static bool is_z13()  { return has_CryptoExt5()             && !has_MiscInstrExt2();}
-  static bool is_z14()  { return has_MiscInstrExt2()          && !has_MiscInstrExt3();}
-  static bool is_z15()  { return has_MiscInstrExt3();}
+  static bool is_z13()  { return has_CryptoExt5()             && !has_MiscInstrExt2(); }
+  static bool is_z14()  { return has_MiscInstrExt2()          && !has_MiscInstrExt3(); }
+  static bool is_z15()  { return has_MiscInstrExt3()          && !has_BEAR_Enh_Facility(); }
+  static bool is_z16()  { return has_BEAR_Enh_Facility(); }
 
   // Need to use nested class with unscoped enum.
   // C++11 declaration "enum class Cipher { ... } is not supported.
@@ -486,6 +490,9 @@ class VM_Version: public Abstract_VM_Version {
   static bool has_VectorPackedDecimal()       { return  (_features[2] & VectorPackedDecimalMask)       == VectorPackedDecimalMask; }
   static bool has_VectorPackedDecimalEnh()    { return  (_features[2] & VectorPackedDecimalEnhMask)    == VectorPackedDecimalEnhMask; }
 
+  static bool has_BEAR_Enh_Facility()         { return  (_features[3] & BEAREnhFacilityMask)           == BEAREnhFacilityMask; }
+  static bool has_NNP_Assist_Facility()       { return  (_features[2] & NNPAssistFacilityMask)         == NNPAssistFacilityMask; }
+
   // Crypto features query functions.
   static bool has_Crypto_AES_GCM128()         { return has_Crypto() && test_feature_bit(&_cipher_features_KMA[0], Cipher::_AES128, Cipher::_featureBits); }
   static bool has_Crypto_AES_GCM192()         { return has_Crypto() && test_feature_bit(&_cipher_features_KMA[0], Cipher::_AES192, Cipher::_featureBits); }
@@ -558,6 +565,8 @@ class VM_Version: public Abstract_VM_Version {
   static void set_has_VectorEnhancements2()       { _features[2] |= VectorEnhancements2Mask; }
   static void set_has_VectorPackedDecimal()       { _features[2] |= VectorPackedDecimalMask; }
   static void set_has_VectorPackedDecimalEnh()    { _features[2] |= VectorPackedDecimalEnhMask; }
+  static void set_has_BEAR_Enh_Facility()         { _features[3] |= BEAREnhFacilityMask;}
+  static void set_has_NNP_Assist_Facility()       { _features[2] |= NNPAssistFacilityMask;}
 
   static void reset_has_VectorFacility()          { _features[2] &= ~VectorFacilityMask; }
 
