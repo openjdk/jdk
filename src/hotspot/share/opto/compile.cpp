480b508cf2f6972691eea35f133cc8fb939ac30f
Daniel Skantz <daniel.skantz@oracle.com>
2024-12-09 16:29:56 +0000

    8345156: C2: Add bailouts next to a few asserts
    Reviewed-by: kvn, epeter

--- src/hotspot/share/opto/compile.cpp
+++ src/hotspot/share/opto/compile.cpp
@@ -2969,6 +2969,9 @@ void Compile::Code_Gen() {
     print_method(PHASE_GLOBAL_CODE_MOTION, 2);
     NOT_PRODUCT( verify_graph_edges(); )
     cfg.verify();
+    if (failing()) {
+      return;
+    }
   }
 
   PhaseChaitin regalloc(unique(), cfg, matcher, false);
@@ -5041,7 +5044,6 @@ bool Compile::fail_randomly() {
 }
 
 bool Compile::failure_is_artificial() {
-  assert(failing_internal(), "should be failing");
   return C->failure_reason_is("StressBailout");
 }
 #endif
