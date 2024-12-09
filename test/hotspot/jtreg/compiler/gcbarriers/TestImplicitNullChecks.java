efe2b79159983ac7569bd4256ca00814e2e2ec3a
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2024-12-09 13:40:20 +0000

    Complete test with stores and atomics

--- test/hotspot/jtreg/compiler/gcbarriers/TestImplicitNullChecks.java
+++ test/hotspot/jtreg/compiler/gcbarriers/TestImplicitNullChecks.java
@@ -24,8 +24,13 @@
 package compiler.gcbarriers;
 
 import compiler.lib.ir_framework.*;
+import java.lang.invoke.VarHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import jdk.test.lib.Asserts;
-
 
 /**
  * @test id=G1
@@ -56,7 +61,15 @@ public class TestImplicitNullChecks {
         volatile Object f;
     }
 
-    static final String ANY_BARRIER = ".*";
+    static final VarHandle fVarHandle;
+    static {
+        MethodHandles.Lookup l = MethodHandles.lookup();
+        try {
+            fVarHandle = l.findVarHandle(Outer.class, "f", Object.class);
+        } catch (Exception e) {
+            throw new Error(e);
+        }
+    }
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -65,7 +78,9 @@ public class TestImplicitNullChecks {
         if (!args[0].equals("G1") && !args[0].equals("Z")) {
             throw new IllegalArgumentException();
         }
-        TestFramework.runWithFlags("-XX:-TieredCompilation", "-XX:+Use" + args[0] + "GC");
+        TestFramework.runWithFlags("-XX:CompileCommand=inline,java.lang.ref.*::*",
+                                   "-XX:-TieredCompilation",
+                                   "-XX:+Use" + args[0] + "GC");
     }
 
     @Test
@@ -91,8 +106,6 @@ public class TestImplicitNullChecks {
     public static void runLoadTests() {
         {
             Outer o = new Outer();
-            Object o1 = new Object();
-            o.f = o1;
             // Trigger compilation with implicit null check.
             for (int i = 0; i < 10_000; i++) {
                 testLoad(o);
@@ -107,8 +120,6 @@ public class TestImplicitNullChecks {
         }
         {
             OuterWithVolatileField o = new OuterWithVolatileField();
-            Object o1 = new Object();
-            o.f = o1;
             // Trigger compilation with implicit null check.
             for (int i = 0; i < 10_000; i++) {
                 testLoadVolatile(o);
@@ -122,4 +133,108 @@ public class TestImplicitNullChecks {
             Asserts.assertTrue(nullPointerException);
         }
     }
+
+    @Test
+    // G1 and ZGC stores cannot be currently used to implement implicit null
+    // checks, because they expand into multiple memory access instructions that
+    // are not necessarily located at the initial instruction start address.
+    @IR(failOn = IRNode.NULL_CHECK,
+        phase = CompilePhase.FINAL_CODE)
+    public static void testStore(Outer o, Object o1) {
+        o.f = o1;
+    }
+
+    @Run(test = {"testStore"})
+    public static void runStoreTests() {
+        {
+            Outer o = new Outer();
+            Object o1 = new Object();
+            testStore(o, o1);
+        }
+    }
+
+    @Test
+    // G1 and ZGC compare-and-exchange operations cannot be currently used to
+    // implement implicit null checks, because they expand into multiple memory
+    // access instructions that are not necessarily located at the initial
+    // instruction start address. The same holds for testCompareAndSwap and
+    // testGetAndSet below.
+    @IR(failOn = IRNode.NULL_CHECK,
+        phase = CompilePhase.FINAL_CODE)
+    static Object testCompareAndExchange(Outer o, Object oldVal, Object newVal) {
+        return fVarHandle.compareAndExchange(o, oldVal, newVal);
+    }
+
+    @Test
+    @IR(failOn = IRNode.NULL_CHECK,
+        phase = CompilePhase.FINAL_CODE)
+    static boolean testCompareAndSwap(Outer o, Object oldVal, Object newVal) {
+        return fVarHandle.compareAndSet(o, oldVal, newVal);
+    }
+
+    @Test
+    @IR(failOn = IRNode.NULL_CHECK,
+        phase = CompilePhase.FINAL_CODE)
+    static Object testGetAndSet(Outer o, Object newVal) {
+        return fVarHandle.getAndSet(o, newVal);
+    }
+
+    @Run(test = {"testCompareAndExchange",
+                 "testCompareAndSwap",
+                 "testGetAndSet"})
+    public void runAtomicTests() {
+        {
+            Outer o = new Outer();
+            Object oldVal = new Object();
+            Object newVal = new Object();
+            testCompareAndExchange(o, oldVal, newVal);
+        }
+        {
+            Outer o = new Outer();
+            Object oldVal = new Object();
+            Object newVal = new Object();
+            testCompareAndSwap(o, oldVal, newVal);
+        }
+        {
+            Outer o = new Outer();
+            Object oldVal = new Object();
+            Object newVal = new Object();
+            testGetAndSet(o, newVal);
+        }
+    }
+
+    @Test
+    // G1 reference loads use indirect memory operands, which leads to a pattern
+    // that cannot be exploited by the current C2 analysis. The same holds for
+    // testLoadWeakReference.
+    @IR(applyIf = {"UseG1GC", "false"},
+        counts = {IRNode.NULL_CHECK, "1"},
+        phase = CompilePhase.FINAL_CODE)
+    static Object testLoadSoftReference(SoftReference<Object> ref) {
+        return ref.get();
+    }
+
+    @Test
+    @IR(applyIf = {"UseG1GC", "false"},
+        counts = {IRNode.NULL_CHECK, "1"},
+        phase = CompilePhase.FINAL_CODE)
+    static Object testLoadWeakReference(WeakReference<Object> ref) {
+        return ref.get();
+    }
+
+    @Run(test = {"testLoadSoftReference",
+                 "testLoadWeakReference"})
+    public void runReferenceTests() {
+        {
+            Object o1 = new Object();
+            SoftReference<Object> sref = new SoftReference<Object>(o1);
+            Object o2 = testLoadSoftReference(sref);
+        }
+        {
+            Object o1 = new Object();
+            WeakReference<Object> wref = new WeakReference<Object>(o1);
+            Object o2 = testLoadWeakReference(wref);
+        }
+    }
+
 }
