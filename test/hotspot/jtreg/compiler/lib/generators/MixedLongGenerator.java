37216a5913d14ae8dd3cbb08c59eeab92aea3428
Emanuel Peter <emanuel.peter@oracle.com>
2024-12-12 16:54:39 +0100

    JDK-8346107

---
+++ test/hotspot/jtreg/compiler/lib/generators/MixedLongGenerator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package compiler.lib.generators;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.HashSet;
+import jdk.test.lib.Utils;
+
+/**
+ * Mixed results between UniformLongGenerator and SpecialLongGenerator.
+ */
+public final class MixedLongGenerator extends LongGenerator {
+    private static final Random RANDOM = Utils.getRandomInstance();
+
+    private final UniformLongGenerator uniform;
+    private final SpecialLongGenerator special;
+    private final int weightUniform;
+    private final int weightSpecial;
+
+    public MixedLongGenerator(int weightUniform, int weightSpecial, int rangeSpecial) {
+        this.weightUniform = weightUniform;
+        this.weightSpecial = weightSpecial;
+        this.uniform = new UniformLongGenerator();
+        this.special = new SpecialLongGenerator(rangeSpecial);
+    }
+
+    @Override
+    public long nextLong(long lo, long hi) {
+        int r = RANDOM.nextInt(weightUniform + weightSpecial);
+        if (r < weightUniform) {
+            return uniform.nextLong(lo, hi);
+        } else {
+            return special.nextLong(lo, hi);
+        }
+    }
+}
