923688607efa34580f30a37cd290efd9296e0a8d
Emanuel Peter <emanuel.peter@oracle.com>
2024-12-12 12:33:47 +0100

    verify all array types and native memory segment

--- test/hotspot/jtreg/testlibrary_tests/verify/tests/TestVerify.java
+++ test/hotspot/jtreg/testlibrary_tests/verify/tests/TestVerify.java
@@ -34,18 +34,25 @@ package verify.tests;
 import java.lang.foreign.*;
 import java.util.Random;
 import jdk.test.lib.Utils;
+import java.nio.ByteBuffer;
+
 import compiler.lib.verify.*;
 
 public class TestVerify {
     private static final Random RANDOM = Utils.getRandomInstance();
-    // TODO remove random?
 
     public static void main(String[] args) {
-        testArray();
-        testMemorySegment();
+        testArrayByte();
+        testArrayChar();
+        testArrayShort();
+        testArrayInt();
+        testArrayLong();
+        testArrayFloat();
+        testArrayDouble();
+        testNativeMemorySegment();
     }
 
-    public static void testArray() {
+    public static void testArrayByte() {
         byte[] a = new byte[1000];
         byte[] b = new byte[1001];
         byte[] c = new byte[1000];
@@ -87,31 +94,286 @@ public class TestVerify {
         } catch (VerifyException e) {}
     }
 
-    public static void testMemorySegment() {
+
+    public static void testArrayShort() {
+        short[] a = new short[1000];
+        short[] b = new short[1001];
+        short[] c = new short[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
     }
 
-//        for (int i = 0; i < 10; i++) {
-//             int[] a = new int[1000];
-//             g.fill(a, Integer.MIN_VALUE, Integer.MAX_VALUE);
-//        }
-//        for (int i = 0; i < 100; i++) {
-//             int[] a = new int[1000];
-//             // hi in [min_int+1, max_int]
-//             // lo in [min_int, hi-1]
-//             int hi = RANDOM.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE) + 1;
-//             int lo = RANDOM.nextInt(Integer.MIN_VALUE, hi);
-//             g.fill(a, lo, hi);
-//             checkRange(a, lo, hi);
-//             MemorySegment ms = MemorySegment.ofArray(a);
-//             g.fill(ms, lo, hi);
-//             checkRange(ms, lo, hi);
-//        }
-//    }
-//
-//    public static void checkRange(MemorySegment ms, int lo, int hi) {
-//        for (long i = 0; i < ms.byteSize() / 4; i++ ) {
-//            int v = ms.get(ValueLayout.JAVA_INT_UNALIGNED, 4L * i);
-//            checkRange(v, lo, hi);
-//        }
-//    }
+
+    public static void testArrayChar() {
+        char[] a = new char[1000];
+        char[] b = new char[1001];
+        char[] c = new char[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
+
+
+    public static void testArrayInt() {
+        int[] a = new int[1000];
+        int[] b = new int[1001];
+        int[] c = new int[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
+
+
+    public static void testArrayLong() {
+        long[] a = new long[1000];
+        long[] b = new long[1001];
+        long[] c = new long[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
+
+
+    public static void testArrayFloat() {
+        float[] a = new float[1000];
+        float[] b = new float[1001];
+        float[] c = new float[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
+
+
+    public static void testArrayDouble() {
+        double[] a = new double[1000];
+        double[] b = new double[1001];
+        double[] c = new double[1000];
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(a));
+        Verify.checkEQ(MemorySegment.ofArray(b), MemorySegment.ofArray(b));
+        Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+        Verify.checkEQ(MemorySegment.ofArray(c), MemorySegment.ofArray(a));
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(b));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c[RANDOM.nextInt(c.length)] = 1;
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(MemorySegment.ofArray(a), MemorySegment.ofArray(c));
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
+
+    public static void testNativeMemorySegment() {
+        MemorySegment a = Arena.ofAuto().allocate(1000, 1);
+        MemorySegment b = Arena.ofAuto().allocate(1001, 1);
+        MemorySegment c = Arena.ofAuto().allocate(1000, 1);
+
+        Verify.checkEQ(a, a);
+        Verify.checkEQ(b, b);
+        Verify.checkEQ(a, c);
+        Verify.checkEQ(c, a);
+
+        // Size mismatch
+        try {
+            Verify.checkEQ(a, b);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+
+        c.set(ValueLayout.JAVA_BYTE, RANDOM.nextLong(c.byteSize()), (byte)1);
+
+        // Value mismatch
+        try {
+            Verify.checkEQ(a, c);
+            throw new RuntimeException("Should have thrown");
+        } catch (VerifyException e) {}
+    }
 }
