b5d4981f7a6200a2b15e20b6a8777f8dc8ae4073
Kevin Walls <kevin.walls@oracle.com>
2024-12-13 18:49:12 +0000

    (C)

--- test/jdk/sun/management/jdp/JdpTestCase.java
+++ test/jdk/sun/management/jdp/JdpTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -76,7 +76,7 @@ public abstract class JdpTestCase {
                 socket.receive(datagram);
                 onReceived(extractUDPpayload(datagram));
             } catch (SocketTimeoutException e) {
-                onSocketTimeout(e); // throws Exception on failure
+                onSocketTimeout(e);
             }
 
             if (!shouldContinue()) {
