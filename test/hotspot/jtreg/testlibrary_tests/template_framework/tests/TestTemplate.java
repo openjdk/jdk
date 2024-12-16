88699454b08a154d37b94790c2c555387b8df423
Emanuel Peter <emanuel.peter@oracle.com>
2024-12-16 17:33:19 +0100

    extend testing more

--- test/hotspot/jtreg/testlibrary_tests/template_framework/tests/TestTemplate.java
+++ test/hotspot/jtreg/testlibrary_tests/template_framework/tests/TestTemplate.java
@@ -31,6 +31,8 @@
 
 package template_framework.tests;
 
+import java.util.HashSet;
+
 import compiler.lib.template_framework.*;
 
 public class TestTemplate {
@@ -39,6 +41,7 @@ public class TestTemplate {
         test1();
         test2();
         test3();
+        test4();
     }
 
     public static void test1() {
@@ -66,13 +69,49 @@ public class TestTemplate {
     public static void test3() {
         Template template = new Template("my_template","start #{p1} #{p2} end");
         checkEQ(template.where("p1", "x").where("p2", "y").instantiate(), "start x y end");
-        checkEQ(template.where("p1", "a").where("p2", "b").instantiate(), "start x y end");
+        checkEQ(template.where("p1", "a").where("p2", "b").instantiate(), "start a b end");
         checkEQ(template.where("p1", "").where("p2", "").instantiate(), "start   end");
+    }
+
+    public static void test4() {
+        HashSet<CodeGenerator> codeGenerators = new HashSet<CodeGenerator>();
+
+        codeGenerators.add(new Template("my_generator_1", "proton"));
+
+        codeGenerators.add(new Template("my_generator_2",
+            """
+            electron #{param1}
+            neutron #{param2}
+            """
+        ));
+
+        codeGenerators.add(new Template("my_generator_3",
+            """
+            Universe @{:my_generator_1} {
+                #{:my_generator_2(param1=up,param2=down)}
+                #{:my_generator_2(param1=#param1,param2=#param2)}
+            }
+            """
+        ));
+        CodeGeneratorLibrary library = new CodeGeneratorLibrary(null, codeGenerators);
+
+        Template template = new Template("my_template",
+            """
+            #{:my_generator_3(param1=low,param2=high)}
+            {
+                #{:my_generator_3(param1=42,param2=24)}
+            }
+            """
+        );
+
+        // TODO instantiate with library and compare???
     }
 
     public static void checkEQ(String code, String expected) {
         if (!code.equals(expected)) {
-	    throw new RuntimeException("Template instantiation mismatch!");
+            System.out.println(code);
+            System.out.println(expected);
+            throw new RuntimeException("Template instantiation mismatch!");
         }
     }
 }
