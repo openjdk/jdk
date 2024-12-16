4d6eccd9fc87e4d4bf91c605b7c9578d5ba174b2
Ioi Lam <iklam@openjdk.org>
2024-12-16 06:17:11 +0000

    8346159: Disable CDS AOTClassLinking tests for JVMCI due to JDK-8345635
    Reviewed-by: ccheung
    Backport-of: c2f0ef5f4822e7e558fe2b4077d57634d380aa11

--- test/hotspot/jtreg/runtime/cds/appcds/resolvedConstants/AOTLinkedLambdas.java
+++ test/hotspot/jtreg/runtime/cds/appcds/resolvedConstants/AOTLinkedLambdas.java
@@ -28,6 +28,8 @@
  * @bug 8340836
  * @requires vm.cds
  * @requires vm.cds.supports.aot.class.linking
+ * @comment work around JDK-8345635
+ * @requires !vm.jvmci.enabled
  * @library /test/lib /test/hotspot/jtreg/runtime/cds/appcds/test-classes/
  * @build AOTLinkedLambdas
  * @run driver jdk.test.lib.helpers.ClassFileInstaller -jar app.jar
