b64a679d48e08316b159c144366981549893b84f
Artur Barashev <artur.barashev@oracle.com>
2024-12-19 11:34:46 -0500

    Cosmetic changes

--- test/jdk/sun/security/util/Resources/Usages.java
+++ test/jdk/sun/security/util/Resources/Usages.java
@@ -36,7 +36,12 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.ListResourceBundle;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
