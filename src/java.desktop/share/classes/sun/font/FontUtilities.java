f7a89cff4d511c086b6be82b75534d776d47d3af
Daniel Gredler <daniel.gredler@gmail.com>
2024-12-20 01:14:53 +0100

    Add more info about test fonts and default-ignorable chars

--- src/java.desktop/share/classes/sun/font/FontUtilities.java
+++ src/java.desktop/share/classes/sun/font/FontUtilities.java
@@ -309,10 +309,43 @@ public final class FontUtilities {
     }
 
     /**
-     * Checks whether or not the specified codepoint is ignorable by default, per the
+     * <p>Checks whether or not the specified codepoint is ignorable by default, per the
      * Unicode standard (with exceptions for HarfBuzz and Uniscribe compatibility).
      * The codepoints categorized as ignorable should remain in sync with HarfBuzz's
      * is_default_ignorable() in hb-unicode.hh.
+     *
+     * <p>See the original Unicode list of default-ignorable codepoints
+     * <a href="https://www.unicode.org/Public/16.0.0/ucd/DerivedCoreProperties.txt">here</a>:
+     *
+     * <pre>
+     * 00AD         # Cf       SOFT HYPHEN
+     * 034F         # Mn       COMBINING GRAPHEME JOINER
+     * 061C         # Cf       ARABIC LETTER MARK
+     * 115F..1160   # Lo   [2] HANGUL CHOSEONG FILLER..HANGUL JUNGSEONG FILLER
+     * 17B4..17B5   # Mn   [2] KHMER VOWEL INHERENT AQ..KHMER VOWEL INHERENT AA
+     * 180B..180D   # Mn   [3] MONGOLIAN FREE VARIATION SELECTOR ONE..MONGOLIAN FREE VARIATION SELECTOR THREE
+     * 180E         # Cf       MONGOLIAN VOWEL SEPARATOR
+     * 180F         # Mn       MONGOLIAN FREE VARIATION SELECTOR FOUR
+     * 200B..200F   # Cf   [5] ZERO WIDTH SPACE..RIGHT-TO-LEFT MARK
+     * 202A..202E   # Cf   [5] LEFT-TO-RIGHT EMBEDDING..RIGHT-TO-LEFT OVERRIDE
+     * 2060..2064   # Cf   [5] WORD JOINER..INVISIBLE PLUS
+     * 2065         # Cn       <reserved-2065>
+     * 2066..206F   # Cf  [10] LEFT-TO-RIGHT ISOLATE..NOMINAL DIGIT SHAPES
+     * 3164         # Lo       HANGUL FILLER
+     * FE00..FE0F   # Mn  [16] VARIATION SELECTOR-1..VARIATION SELECTOR-16
+     * FEFF         # Cf       ZERO WIDTH NO-BREAK SPACE
+     * FFA0         # Lo       HALFWIDTH HANGUL FILLER
+     * FFF0..FFF8   # Cn   [9] <reserved-FFF0>..<reserved-FFF8>
+     * 1BCA0..1BCA3 # Cf   [4] SHORTHAND FORMAT LETTER OVERLAP..SHORTHAND FORMAT UP STEP
+     * 1D173..1D17A # Cf   [8] MUSICAL SYMBOL BEGIN BEAM..MUSICAL SYMBOL END PHRASE
+     * E0000        # Cn       <reserved-E0000>
+     * E0001        # Cf       LANGUAGE TAG
+     * E0002..E001F # Cn  [30] <reserved-E0002>..<reserved-E001F>
+     * E0020..E007F # Cf  [96] TAG SPACE..CANCEL TAG
+     * E0080..E00FF # Cn [128] <reserved-E0080>..<reserved-E00FF>
+     * E0100..E01EF # Mn [240] VARIATION SELECTOR-17..VARIATION SELECTOR-256
+     * E01F0..E0FFF # Cn [3600] <reserved-E01F0>..<reserved-E0FFF>
+     * </pre>
      */
     public static boolean isDefaultIgnorable(int charCode) {
         if (charCode < 0x00AD) {
