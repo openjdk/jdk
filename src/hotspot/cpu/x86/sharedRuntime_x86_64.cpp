c09af501e3de1a96b36368ca0ca8e313f2e9fd02
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2024-12-23 20:36:57 +0800

    8346775

--- src/hotspot/cpu/x86/sharedRuntime_x86_64.cpp
+++ src/hotspot/cpu/x86/sharedRuntime_x86_64.cpp
@@ -3438,7 +3438,7 @@ do {                                                    \
 // in  A Cryptographic Library for the Motorola DSP56000,
 // Dusse and Kaliski, Proc. EUROCRYPT 90, pp. 230-237.
 
-static void NOINLINE
+NOINLINE static void
 montgomery_multiply(julong a[], julong b[], julong n[],
                     julong m[], julong inv, int len) {
   julong t0 = 0, t1 = 0, t2 = 0; // Triple-precision accumulator
@@ -3480,7 +3480,7 @@ montgomery_multiply(julong a[], julong b[], julong n[],
 // multiplication.  However, its loop control is more complex and it
 // may actually run slower on some machines.
 
-static void NOINLINE
+NOINLINE static void
 montgomery_square(julong a[], julong n[],
                   julong m[], julong inv, int len) {
   julong t0 = 0, t1 = 0, t2 = 0; // Triple-precision accumulator
