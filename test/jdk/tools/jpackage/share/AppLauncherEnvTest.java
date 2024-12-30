2f6dae40913ec0906370df1e3154fc69ff121e64
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2024-12-30 14:28:32 -0500

    - Don't exclude the "PATH" var from the environment when running app launchers in jpackage tests. Removal of the "PATH" var was a part of the JDK-8254920 fix. But this removal is redundant as JDK-8254920 was fixed by making jpackage app launcher not to look up for dll-s in the PATH. There are a few jpackage tests running app launchers without removing the PATH var from the environment - WinChildProcessTest, AppLauncherEnvTest. They proves that removal of the "PATH" var from the environment when testing app launchers is redundant.  - Add support to remove any var from the environment in Executor class.  - Add support for boolean property 'jpackage.test.clear-app-launcher-java-env-vars'. If set to true, "JAVA_TOOL_OPTIONS" and "_JAVA_OPTIONS" will be removed from the environment when running app launchers. This is needed to support local jpackage test configurations when JRE used to run jpackage tests relies on these variables, but they should not be applied to runtimes of app launchers.

--- test/jdk/tools/jpackage/share/AppLauncherEnvTest.java
+++ test/jdk/tools/jpackage/share/AppLauncherEnvTest.java
@@ -30,6 +30,7 @@ import java.util.stream.Stream;
 import jdk.jpackage.test.JPackageCommand;
 import jdk.jpackage.test.Annotations.Test;
 import jdk.jpackage.test.Executor;
+import static jdk.jpackage.test.HelloApp.configureEnvironment;
 import jdk.jpackage.test.TKit;
 
 /**
@@ -53,6 +54,7 @@ public class AppLauncherEnvTest {
 
         JPackageCommand cmd = JPackageCommand
                 .helloAppImage(TEST_APP_JAVA + "*Hello")
+                .ignoreFakeRuntime()
                 .addArguments("--java-options", "-D" + testAddDirProp
                         + "=$APPDIR");
 
@@ -62,7 +64,7 @@ public class AppLauncherEnvTest {
 
         final int attempts = 3;
         final int waitBetweenAttemptsSeconds = 5;
-        List<String> output = new Executor()
+        List<String> output = configureEnvironment(new Executor())
                 .saveOutput()
                 .setExecutable(cmd.appLauncherPath().toAbsolutePath())
                 .addArguments("--print-env-var=" + envVarName)
