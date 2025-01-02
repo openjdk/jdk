f216aa6f9a545bf9b05d1f2655d0ed330066c6ee
sendaoYan <yansendao.ysd@alibaba-inc.com>
2025-01-02 10:39:12 +0800

    Delete curly braces

--- src/hotspot/share/runtime/frame.cpp
+++ src/hotspot/share/runtime/frame.cpp
@@ -1162,9 +1162,8 @@ void frame::oops_do_internal(OopClosure* f, NMethodClosure* cf,
                              const RegisterMap* map, bool use_interpreter_oop_map_cache) const {
 #ifndef PRODUCT
   // simulate GC crash here to dump java thread in error report
-  if (CrashGCForDumpingJavaThread) {
+  if (CrashGCForDumpingJavaThread)
     guarantee(!CrashGCForDumpingJavaThread, "");
-  }
 #endif
   if (is_interpreted_frame()) {
     oops_interpreted_do(f, map, use_interpreter_oop_map_cache);
