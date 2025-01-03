b64f929591d93a417023c8d1b1c96c066e06e1b5
Emanuel Peter <emanuel.peter@oracle.com>
2025-01-03 09:24:25 +0100

    copyright 2025

--- src/hotspot/share/opto/vectorization.hpp
+++ src/hotspot/share/opto/vectorization.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2023, Arm Limited. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
