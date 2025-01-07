bb5074d7d25e9e5aaf12c9bee05e4a465a86dc92
theoweidmannoracle <theo.weidmann@oracle.com>
2025-01-07 16:58:22 +0100

    Improve phrasing

--- test/hotspot/jtreg/compiler/lib/generators/BoundGenerator.java
+++ test/hotspot/jtreg/compiler/lib/generators/BoundGenerator.java
@@ -24,10 +24,9 @@
 package compiler.lib.generators;
 
 /**
- * This is a common superclass for all generators that maintain a reference to the Generators class that created them.
+ * This is a common superclass for all generators that maintain a reference to the Generators object that created them.
  * This allows internally creating other generators or using the {@link RandomnessSource} provided in
  * {@link Generators#random}.
- * @param <T>
  */
 abstract class BoundGenerator<T> implements Generator<T> {
     Generators g;
