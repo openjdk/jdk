11e7769d5acbaeaf291de1aa239752f05b9bc31d
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-01-07 00:59:12 -0500

    More javadoc added

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/CustomLauncherIcon.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/CustomLauncherIcon.java
@@ -28,10 +28,26 @@ import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Custom application launcher icon.
+ * <p>
+ * Use {@link #create(Path)} method to create an instance of this type.
+ */
 public interface CustomLauncherIcon extends LauncherIcon {
 
+    /**
+     * Returns path to icon file.
+     * @return path to icon file
+     */
     Path path();
 
+    /**
+     * Returns the given icon as {@link CustomLauncherIcon} type or an empty {@link Optional} instance
+     * if the given icon object is not an instance of {@link CustomLauncherIcon} type.
+     *
+     * @param icon application launcher icon object or <code>null</null>
+     * @return the given icon as {@link CustomLauncherIcon} type or an empty {@link Optional} instance
+     */
     public static Optional<CustomLauncherIcon> fromLauncherIcon(LauncherIcon icon) {
         if (icon instanceof CustomLauncherIcon customIcon) {
             return Optional.of(customIcon);
@@ -40,11 +56,19 @@ public interface CustomLauncherIcon extends LauncherIcon {
         }
     }
 
+    /**
+     * Creates object of type {@link CustomLauncherIcon} from the path to icon file.
+     * @param path path to icon file
+     * @return {@link CustomLauncherIcon} instance
+     */
     public static CustomLauncherIcon create(Path path) {
         Objects.requireNonNull(path);
         return new Stub(path);
     }
 
+    /**
+     * Default implementation of {@link CustomLauncherIcon} type.
+     */
     record Stub(Path path) implements CustomLauncherIcon {
     }
 }
--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/DefaultLauncherIcon.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/DefaultLauncherIcon.java
@@ -26,8 +26,22 @@ package jdk.jpackage.internal.model;
 
 import java.util.Optional;
 
+/**
+ * Default application launcher icon.
+ * <p>
+ * Default icon is either loaded from the resources of {@link jdk.jpackage/} module or picked from the resource directory.
+ * <p>
+ * Use {@link #INSTANCE} field to get an instance of this type.
+ */
 public interface DefaultLauncherIcon extends LauncherIcon {
 
+    /**
+     * Returns the given icon as {@link DefaultLauncherIcon} type or an empty {@link Optional} instance
+     * if the given icon object is not an instance of {@link DefaultLauncherIcon} type.
+     *
+     * @param icon application launcher icon object or <code>null</null>
+     * @return the given icon as {@link DefaultLauncherIcon} type or an empty {@link Optional} instance
+     */
     public static Optional<DefaultLauncherIcon> fromLauncherIcon(LauncherIcon icon) {
         if (icon instanceof DefaultLauncherIcon defaultIcon) {
             return Optional.of(defaultIcon);
@@ -36,5 +50,8 @@ public interface DefaultLauncherIcon extends LauncherIcon {
         }
     }
 
+    /**
+     * Singleton.
+     */
     public static DefaultLauncherIcon INSTANCE = new DefaultLauncherIcon() {};
 }
