39046a72e12f65883c28e57b3f12f7384ceeed42
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-01-08 17:08:31 -0500

    Add javadoc to PackagerException but it is misleading because of its current implementation

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/PackagerException.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/model/PackagerException.java
@@ -24,6 +24,20 @@
  */
 package jdk.jpackage.internal.model;
 
+import jdk.jpackage.internal.util.StringBundle;
+
+/**
+ * Signals that error has occurred at packaging phase.
+ * <p>
+ * The preferred way to construct instances of this class is to use
+ * {@link jdk.jpackage.internal.util.LocalizedExceptionBuilder#buildLocalizedException(StringBundle)} methods
+ *
+ * {@snippet :
+ * StringBundle i18n = getStringBundle(); // Some way to obtain a string bundle with localized messages
+ *
+ * throw buildLocalizedException(i18n).message("error.no.name").create();
+ * }
+ */
 public class PackagerException extends Exception {
 
     private static final long serialVersionUID = 1L;
@@ -48,6 +62,17 @@ public class PackagerException extends Exception {
         super(I18N.format(key, arguments), cause);
     }
 
+    /**
+     * Throws the cause of the given {@link RuntimeException} exception
+     * as {@link PackagerException} if the cause is of this type or re-throws the given
+     * {@link RuntimeException} exception as-is otherwise.
+     * <p>
+     * Never return a value. It always throws some exception object.
+     *
+     * @param ex exception to re-throw
+     * @return doesn't return value
+     * @throws PackagerException
+     */
     public static RuntimeException rethrowPackagerException(RuntimeException ex) throws PackagerException {
         if (ex.getCause() instanceof PackagerException pkgEx) {
             throw pkgEx;
