c3e077b9ab6e32c47fcb0e829921b41d699edf67
Archie L. Cobbs <archie.cobbs@gmail.com>
2025-01-08 11:42:46 -0600

    Remove a few more unnecessary suppressions.

--- src/java.base/share/classes/sun/net/www/protocol/http/HttpURLConnection.java
+++ src/java.base/share/classes/sun/net/www/protocol/http/HttpURLConnection.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2069,7 +2069,6 @@ public class HttpURLConnection extends java.net.HttpURLConnection {
      * Gets the authentication for an HTTP proxy, and applies it to
      * the connection.
      */
-    @SuppressWarnings("fallthrough")
     private AuthenticationInfo getHttpProxyAuthentication(AuthenticationHeader authhdr)
         throws IOException {
 
