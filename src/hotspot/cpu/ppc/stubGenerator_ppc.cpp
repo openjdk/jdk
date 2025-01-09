eadd408c91d5f2268b4e21ce2a787e6daee33edd
suchismith1993 <suchismith1993@gmail.com>
2025-01-09 04:03:10 -0500

    restore

--- src/hotspot/cpu/ppc/stubGenerator_ppc.cpp
+++ src/hotspot/cpu/ppc/stubGenerator_ppc.cpp
@@ -1,4 +1,5 @@
 /*
+
  * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2012, 2024 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -23,6 +24,7 @@
  *
  */
 
+#include "precompiled.hpp"
 #include "asm/macroAssembler.inline.hpp"
 #include "compiler/oopMap.hpp"
 #include "gc/shared/barrierSet.hpp"
@@ -621,7 +623,7 @@ class StubGenerator: public StubCodeGenerator {
   // Don't generate, rather use C++ code.
   address generate_verify_oop() {
     // this is actually a `FunctionDescriptor*'.
-    address start = nullptr;
+    address start = 0;
 
 #if !defined(PRODUCT)
     start = CAST_FROM_FN_PTR(address, verify_oop_helper);
@@ -752,7 +754,7 @@ address generate_ghash_processBlocks() {
   __ blr();
   #ifdef ASSERT
     __ bind(L_error);
-    __ stop("Number of blocks must be positive");
+    __ stop("ghash_processBlocks : number of blocks must be positive");
   #endif
   return start;
 }
@@ -2164,8 +2166,7 @@ address generate_ghash_processBlocks() {
   void generate_type_check(Register sub_klass,
                            Register super_check_offset,
                            Register super_klass,
-                           Register temp1,
-                           Register temp2,
+                           Register temp,
                            Label& L_success) {
     assert_different_registers(sub_klass, super_check_offset, super_klass);
 
@@ -2173,9 +2174,9 @@ address generate_ghash_processBlocks() {
 
     Label L_miss;
 
-    __ check_klass_subtype_fast_path(sub_klass, super_klass, temp1, temp2, &L_success, &L_miss, nullptr,
+    __ check_klass_subtype_fast_path(sub_klass, super_klass, temp, R0, &L_success, &L_miss, nullptr,
                                      super_check_offset);
-    __ check_klass_subtype_slow_path(sub_klass, super_klass, temp1, temp2, &L_success);
+    __ check_klass_subtype_slow_path(sub_klass, super_klass, temp, R0, &L_success);
 
     // Fall through on failure!
     __ bind(L_miss);
@@ -2205,7 +2206,8 @@ address generate_ghash_processBlocks() {
     const Register R10_oop   = R10_ARG8;     // actual oop copied
     const Register R11_klass = R11_scratch1; // oop._klass
     const Register R12_tmp   = R12_scratch2;
-    const Register R2_tmp    = R2;
+
+    const Register R2_minus1 = R2;
 
     //__ align(CodeEntryAlignment);
     StubCodeMark mark(this, "StubRoutines", name);
@@ -2243,6 +2245,7 @@ address generate_ghash_processBlocks() {
     Label load_element, store_element, store_null, success, do_epilogue;
     __ or_(R9_remain, R5_count, R5_count); // Initialize loop index, and test it.
     __ li(R8_offset, 0);                   // Offset from start of arrays.
+    __ li(R2_minus1, -1);
     __ bne(CCR0, load_element);
 
     // Empty array: Nothing to do.
@@ -2270,7 +2273,7 @@ address generate_ghash_processBlocks() {
     }
 
     __ addi(R8_offset, R8_offset, heapOopSize);   // Step to next offset.
-    __ addic_(R9_remain, R9_remain, -1);          // Decrement the count.
+    __ add_(R9_remain, R2_minus1, R9_remain);     // Decrement the count.
     __ beq(CCR0, success);
 
     // ======== loop entry is here ========
@@ -2290,7 +2293,7 @@ address generate_ghash_processBlocks() {
 
     __ load_klass(R11_klass, R10_oop); // Query the object klass.
 
-    generate_type_check(R11_klass, R6_ckoff, R7_ckval, R12_tmp, R2_tmp,
+    generate_type_check(R11_klass, R6_ckoff, R7_ckval, R12_tmp,
                         // Branch to this on success:
                         store_element);
     // ======== end loop ========
@@ -2624,7 +2627,7 @@ address generate_ghash_processBlocks() {
       int sco_offset = in_bytes(Klass::super_check_offset_offset());
       __ lwz(sco_temp, sco_offset, dst_klass);
       generate_type_check(src_klass, sco_temp, dst_klass,
-                          temp, /* temp */ R10_ARG8, L_disjoint_plain_copy);
+                          temp, L_disjoint_plain_copy);
 
       // Fetch destination element klass from the ObjArrayKlass header.
       int ek_offset = in_bytes(ObjArrayKlass::element_klass_offset());
@@ -4582,9 +4585,9 @@ address generate_lookup_secondary_supers_table_stub(u1 super_klass_index) {
       r_bitmap       = R11_scratch1,
       result         = R8_ARG6;
 
-    __ lookup_secondary_supers_table_const(r_sub_klass, r_super_klass,
-                                           r_array_base, r_array_length, r_array_index,
-                                           r_bitmap, result, super_klass_index);
+    __ lookup_secondary_supers_table(r_sub_klass, r_super_klass,
+                                     r_array_base, r_array_length, r_array_index,
+                                     r_bitmap, result, super_klass_index);
     __ blr();
 
     return start;
