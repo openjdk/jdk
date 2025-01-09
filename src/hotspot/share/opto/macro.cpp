5f0abf218111d8f06b3cb6fa49a45de3ec55068c
theoweidmannoracle <theo.weidmann@oracle.com>
2025-01-09 12:45:41 +0100

    Update src/hotspot/share/opto/macro.cpp
    Co-authored-by: Emanuel Peter <emanuel.peter@oracle.com>

--- src/hotspot/share/opto/macro.cpp
+++ src/hotspot/share/opto/macro.cpp
@@ -2589,7 +2589,8 @@ bool PhaseMacroExpand::expand_macro_nodes() {
         bool is_drem = n->Opcode() == Op_ModD;
         CallNode* mod_macro = n->as_Call();
         CallNode* call = new CallLeafNode(mod_macro->tf(),
-                                          is_drem ? CAST_FROM_FN_PTR(address, SharedRuntime::drem) : CAST_FROM_FN_PTR(address, SharedRuntime::frem),
+                                          is_drem ? CAST_FROM_FN_PTR(address, SharedRuntime::drem)
+                                                  : CAST_FROM_FN_PTR(address, SharedRuntime::frem),
                                           is_drem ? "drem" : "frem", TypeRawPtr::BOTTOM);
         call->init_req(TypeFunc::Control, mod_macro->in(TypeFunc::Control));
         call->init_req(TypeFunc::I_O, mod_macro->in(TypeFunc::I_O));
