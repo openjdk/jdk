a1f3c723d63a4b31806ccae3bfefb446d215fb2a
Per Minborg <pminborg@gmail.com>
2025-01-13 10:01:11 +0100

    Remove thread test

--- test/jdk/java/foreign/TestCaptureStateUtil.java
+++ test/jdk/java/foreign/TestCaptureStateUtil.java
@@ -38,10 +38,6 @@ import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.locks.LockSupport;
-import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -119,107 +115,6 @@ final class TestCaptureStateUtil {
 
         assertThrows(NullPointerException.class, () -> CaptureStateUtil.adaptSystemCall(null, ERRNO_NAME));
         assertThrows(NullPointerException.class, () -> CaptureStateUtil.adaptSystemCall(noSegment, null));
-    }
-
-    private static final int THREAD_COUNT = 1 << 14;
-    private static final int LOOP_COUNT = 1 << 16;
-
-    // As TerminatingThreadLocal is carrier-local, this test tries to make sure that
-    // a VT can not see another VT's thread local memory segment despite extensive
-    // unmounting taking place.
-    @Test
-    void threadBarrage() {
-
-        // Create the work units
-        var works = IntStream.range(0, THREAD_COUNT)
-                .mapToObj(_ -> new Work())
-                .toList();
-
-        // Pre-create the VTs before starting them
-        var virtualThreads = works.stream()
-                .map(Thread.ofVirtual()::unstarted)
-                .toList();
-
-        // Kick off the VTs once all of them are create
-        virtualThreads.forEach(Thread::start);
-
-        // Rely on the scheduler to interrupt VTs with lower prio
-        var spawner = new HiPrioSpawner();
-        var spawnerThread = Thread.ofPlatform().start(spawner);
-
-        // Wait for all VTs to complete
-        virtualThreads.forEach(TestCaptureStateUtil::join);
-        spawner.shutdown();
-        join(spawnerThread);
-
-        // Count how many work items were ok
-        long ok = works.stream()
-                .filter(Work::isOk)
-                .count();
-
-        assertEquals(works.size(), ok, "Only " + ok + " of " + works.size() + " virtual threads made it!");
-    }
-
-    private static final class HiPrioSpawner implements Runnable {
-
-        private volatile boolean running = true;
-
-        private static final long INTERVAL_NS = 100_000; // Max 1_000_000_000/100_000 = 10_000 new threads per second
-
-        @Override
-        public void run() {
-            long next = System.nanoTime() + INTERVAL_NS;
-
-            while (running) {
-                //Thread t = Thread.ofVirtual().unstarted((() -> {}));
-                Thread t = Thread.ofPlatform().unstarted((() -> {}));
-                t.setPriority(Thread.MAX_PRIORITY);
-                t.start();
-                join(t);
-
-                // Throttle the number of new threads per second
-                while (System.nanoTime() < next) {
-                    LockSupport.parkNanos(INTERVAL_NS / 100);
-                }
-                next += INTERVAL_NS;
-            }
-        }
-
-        void shutdown() {
-            running = false;
-        }
-
-    }
-
-    private static final class Work implements Runnable {
-
-        private volatile boolean ok = true;
-
-        public void run() {
-            final int threadId = (int) Thread.currentThread().threadId() & Integer.MAX_VALUE;
-            for (int i = 0; i < LOOP_COUNT && ok; i++) {
-                try {
-                    long r = (long) ADAPTED_LONG.invoke(-1L, threadId);
-                    if (-threadId != r) {
-                        ok = false;
-                    }
-                } catch (Throwable t) {
-                    ok = false;
-                }
-            }
-        }
-
-        public boolean isOk() {
-            return ok;
-        }
-    }
-
-    static void join(Thread t) {
-        try {
-            t.join();
-        } catch (InterruptedException ie) {
-            throw new RuntimeException(ie);
-        }
     }
 
     // Dummy method that is just returning the provided parameters
