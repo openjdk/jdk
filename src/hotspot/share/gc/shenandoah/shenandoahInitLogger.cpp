31291599dce6eb1326332476eb38e26336117882
Coleen Phillimore <coleen.phillimore@oracle.com>
2025-01-14 13:59:00 +0000

    Add Oracle copyrights.

--- src/hotspot/share/gc/shenandoah/shenandoahInitLogger.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahInitLogger.cpp
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2020, Red Hat, Inc. All rights reserved.
  * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
