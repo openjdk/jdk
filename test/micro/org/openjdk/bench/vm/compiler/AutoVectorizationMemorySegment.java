a5b303fc0ee57a9c86b131d069c3452e4ca13117
Emanuel Peter <emanuel.peter@oracle.com>
2025-01-15 08:55:49 +0100

    more MemorySegment benchmarks

--- test/micro/org/openjdk/bench/vm/compiler/AutoVectorizationMemorySegment.java
+++ test/micro/org/openjdk/bench/vm/compiler/AutoVectorizationMemorySegment.java
@@ -64,6 +64,9 @@ public class AutoVectorizationMemorySegment {
     private MemorySegment bMS;
     private MemorySegment rMS;
 
+    private int  zeroInvarI = 0;
+    private long zeroInvarL = 0;
+
     @Setup
     public void init() {
         aMS = allocate();
@@ -88,19 +91,269 @@ public class AutoVectorizationMemorySegment {
     }
 
     @Benchmark
-    public void elementwiseByteIncr() {
-        for (long i = 0; i < aMS.byteSize(); i++) {
-            byte v = rMS.get(ValueLayout.JAVA_BYTE, i);
-            rMS.set(ValueLayout.JAVA_BYTE, i, (byte)(v + 1));
-        }
-    }
-
-    @Benchmark
-    public void elementwiseByteAdd() {
+    public void longLoopElementwiseByteAdd() {
         for (long i = 0; i < aMS.byteSize(); i++) {
             byte v1 = aMS.get(ValueLayout.JAVA_BYTE, i);
             byte v2 = bMS.get(ValueLayout.JAVA_BYTE, i);
             rMS.set(ValueLayout.JAVA_BYTE, i, (byte)(v1 + v2));
+        }
+    }
+
+    // see TestMemorySegment.testMemorySegmentBadExitCheck
+    @Benchmark
+    public void intLoopWithLongLimitElementByteIncrSameLongAdr() {
+        for (int i = 0; i < rMS.byteSize(); i++) {
+            long adr = i;
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_iv_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            long adr = i;
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_intInvar_sameAdr_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrWithIntInvarSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            long adr = (long)(i) + (long)(zeroInvarI);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_longInvar_sameAdr_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrWithLongInvarSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            long adr = (long)(i) + (long)(zeroInvarL);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_intInvar_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrWithIntInvarSeparateLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            long adr1 = (long)(i) + (long)(zeroInvarI);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr1);
+            long adr2 = (long)(i) + (long)(zeroInvarI);
+            rMS.set(ValueLayout.JAVA_BYTE, adr2, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_longInvar_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrWithLongInvarSeparateLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            long adr1 = (long)(i) + (long)(zeroInvarL);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr1);
+            long adr2 = (long)(i) + (long)(zeroInvarL);
+            rMS.set(ValueLayout.JAVA_BYTE, adr2, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_intIndex_intInvar_byte
+    @Benchmark
+    public void intLoopElementWiseByteIncrWithIntInvarSameIntIndex() {
+        for (int i = 0; i < (int)rMS.byteSize(); i++) {
+            int int_index = i + zeroInvarI;
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, int_index);
+            rMS.set(ValueLayout.JAVA_BYTE, int_index, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_iv_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++ ) {
+            long adr = 4L * i;
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_intInvar_sameAdr_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrWithIntInvarSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++) {
+            long adr = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_longInvar_sameAdr_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrWithLongInvarSameLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++) {
+            long adr = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_intInvar_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrWithIntInvarSeparateLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++) {
+            long adr1 = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr1);
+            long adr2 = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr2, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_longIndex_longInvar_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrWithLongInvarSeparateLongAdr() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++) {
+            long adr1 = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr1);
+            long adr2 = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr2, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testIntLoop_intIndex_intInvar_int
+    @Benchmark
+    public void intLoopElementWiseIntIncrWithIntInvarSameIntIndex() {
+        for (int i = 0; i < (int)rMS.byteSize()/4; i++) {
+            int int_index = i + zeroInvarI;
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, 4L * int_index);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, 4L * int_index, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_iv_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            long adr = i;
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_intInvar_sameAdr_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrWithIntInvarSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            long adr = (long)(i) + (long)(zeroInvarI);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_longInvar_sameAdr_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrWithLongInvarSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            long adr = (long)(i) + (long)(zeroInvarL);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr);
+            rMS.set(ValueLayout.JAVA_BYTE, adr, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_intInvar_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrWithIntInvarSeparateLongAdr() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            long adr1 = (long)(i) + (long)(zeroInvarI);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr1);
+            long adr2 = (long)(i) + (long)(zeroInvarI);
+            rMS.set(ValueLayout.JAVA_BYTE, adr2, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_longInvar_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrWithLongInvarSeparateLongAdr() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            long adr1 = (long)(i) + (long)(zeroInvarL);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, adr1);
+            long adr2 = (long)(i) + (long)(zeroInvarL);
+            rMS.set(ValueLayout.JAVA_BYTE, adr2, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_intIndex_intInvar_byte
+    @Benchmark
+    public void longLoopElementWiseByteIncrWithIntInvarSameIntIndex() {
+        for (long i = 0; i < rMS.byteSize(); i++) {
+            int int_index = (int)(i + zeroInvarI);
+            byte v = rMS.get(ValueLayout.JAVA_BYTE, int_index);
+            rMS.set(ValueLayout.JAVA_BYTE, int_index, (byte)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_iv_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize()/4; i++ ) {
+            long adr = 4L * i;
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_intInvar_sameAdr_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrWithIntInvarSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize()/4; i++) {
+            long adr = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_longInvar_sameAdr_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrWithLongInvarSameLongAdr() {
+        for (long i = 0; i < rMS.byteSize()/4; i++) {
+            long adr = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_intInvar_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrWithIntInvarSeparateLongAdr() {
+        for (long i = 0; i < rMS.byteSize()/4; i++) {
+            long adr1 = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr1);
+            long adr2 = 4L * (long)(i) + 4L * (long)(zeroInvarI);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr2, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_longIndex_longInvar_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrWithLongInvarSeparateLongAdr() {
+        for (long i = 0; i < rMS.byteSize()/4; i++) {
+            long adr1 = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, adr1);
+            long adr2 = 4L * (long)(i) + 4L * (long)(zeroInvarL);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, adr2, (int)(v + 1));
+        }
+    }
+
+    // see TestMemorySegment.testLongLoop_intIndex_intInvar_int
+    @Benchmark
+    public void longLoopElementWiseIntIncrWithIntInvarSameIntIndex() {
+        for (long i = 0; i < rMS.byteSize()/4; i++) {
+            int int_index = (int)(i + zeroInvarI);
+            int v = rMS.get(ValueLayout.JAVA_INT_UNALIGNED, 4L * int_index);
+            rMS.set(ValueLayout.JAVA_INT_UNALIGNED, 4L * int_index, (int)(v + 1));
         }
     }
 }
