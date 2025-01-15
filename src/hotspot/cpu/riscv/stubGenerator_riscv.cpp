04927290cd7c30f671193645ab2a1df0dd0c87c1
Andrew Dinn <adinn@redhat.com>
2025-01-15 12:46:20 +0000

    update copyrights

--- src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/stubGenerator_riscv.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2003, 2024, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2014, 2020, Red Hat Inc. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2025, Red Hat Inc. All rights reserved.
  * Copyright (c) 2020, 2023, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
