27e0f31cd1fc01affa8d7850e8f9e212a6c5c554
Serguei Spitsyn <serguei.spitsyn@oracle.com>
2025-01-16 03:49:43 +0000

    removed empty line with trailing spaces

--- src/hotspot/share/services/threadService.hpp
+++ src/hotspot/share/services/threadService.hpp
@@ -491,7 +491,6 @@ class JavaThreadInObjectWaitState : public JavaThreadStatusChanger {
     JavaThreadStatusChanger(java_thread,
                             interruptible ? (timed ? JavaThreadStatus::IN_OBJECT_WAIT_TIMED : JavaThreadStatus::IN_OBJECT_WAIT)
                                           : java_lang_Thread::get_thread_status(java_thread->threadObj())) {
-    
     if (is_alive() && interruptible) {
       _stat = java_thread->get_thread_stat();
       _active = ThreadService::is_thread_monitoring_contention();
