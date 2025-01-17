6e4811e025d93cffd8a0175fffabaa957531649c
Gennadiy Krivoshein <gennadiy.krivoshein@bell-sw.com>
2025-01-17 11:05:23 +0000

    Use PassFailJFrame

--- test/jdk/javax/print/CUPSPrinterImageableAreaTest.java
+++ test/jdk/javax/print/CUPSPrinterImageableAreaTest.java
@@ -32,31 +32,25 @@ import javax.print.attribute.standard.MediaPrintableArea;
 import javax.print.attribute.standard.MediaSize;
 import javax.print.attribute.standard.MediaSizeName;
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
 import java.awt.BasicStroke;
-import java.awt.BorderLayout;
-import java.awt.Dialog;
-import java.awt.FlowLayout;
-import java.awt.Frame;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Window;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.print.PageFormat;
 import java.awt.print.Paper;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
+import java.util.List;
 
 /*
  * @test
  * @bug 8344119
  * @key printer
  * @requires (os.family == "linux" | os.family == "mac")
+ * @library /java/awt/regtesthelpers
+ * @build PassFailJFrame
  * @summary CUPSPrinter imageable area
  * @run main/manual CUPSPrinterImageableAreaTest
  */
@@ -64,16 +58,78 @@ import java.awt.print.PrinterJob;
 public class CUPSPrinterImageableAreaTest {
 
     private static PrintService printService;
-    private static final long TIMEOUT = 5 * 60_000;
-    private static volatile boolean testPassed = false;
-    private static volatile boolean testFinished = false;
-    private static volatile boolean intime = false;
+    private static final List<MediaSizeName> ALLOWED_MEDIA_LIST = List.of(MediaSizeName.ISO_A4, MediaSizeName.NA_LETTER);
+    private static final double DPI = 72.0;
+    private static final double MM_PER_INCH = 2.54;
+    private static final String INSTRUCTIONS_FORMAT = """
+            <html>
+                <h3>The <b>'%s'</b> paper size should be used for this test.</h3>
+                <div>
+                The test checks that the media margins fetched from the printer's PPD file are correct.<br>
+                Press the '<b>Print sample</b>' button to print a test page.<br>
+                A passing test will print the page with a black rectangle along the printable area.
+                Ensure that all sides of the rectangle are printed.<br>
+                Expected margins: 
+                <ul>
+                    <li>left: %.1f mm.</li>
+                    <li>bottom: %.1f mm.</li>
+                    <li>right: %.1f mm.</li>
+                    <li>top: %.1f mm.</li>
+                </ul>
+                Click '<b>Pass</b>' button, or click '<b>Fail</b>' button if the test failed.
+                </div>
+            <html>
+            """;
+
 
     public static void main(String[] args) throws Exception {
         printService = PrintServiceLookup.lookupDefaultPrintService();
         if (printService == null) {
             throw new RuntimeException("Print service not found");
         }
+        MediaSizeName testMedia = getTestMediaSizeName();
+        PageFormat pageFormat = createTestPageFormat(testMedia);
+
+        // margins: left, bottom, right, top
+        double[] margins = new double[] {
+                pageFormat.getImageableX(),
+                pageFormat.getHeight() - pageFormat.getImageableHeight() - pageFormat.getImageableY(),
+                pageFormat.getWidth() - pageFormat.getImageableWidth() - pageFormat.getImageableX(),
+                pageFormat.getImageableY()
+        };
+
+        final String instructions = INSTRUCTIONS_FORMAT.formatted(testMedia.toString(),
+                inchesToMM(margins[0]), inchesToMM(margins[1]), inchesToMM(margins[2]), inchesToMM(margins[3]));
+
+        PassFailJFrame.builder()
+                .instructions(instructions)
+                .testUI(createTestUI(pageFormat))
+                .columns(55)
+                .build()
+                .awaitAndCheck();
+    }
+
+    private static JFrame createTestUI(PageFormat pageFormat) {
+        final JFrame f = new JFrame("CUPS Printer imageable area test");
+        JButton printButton = new JButton("Print sample");
+        printButton.setPreferredSize(new Dimension(200, 80));
+        printButton.addActionListener((e) -> {
+            printButton.setEnabled(false);
+            PrinterJob job = PrinterJob.getPrinterJob();
+            job.setPrintable(new RectPrintable(), pageFormat);
+            try {
+                job.print();
+            } catch (PrinterException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+        f.add(printButton);
+        f.pack();
+        return f;
+    }
+
+    private static MediaSizeName getTestMediaSizeName() {
+        //Use printer's default media or one of the alloed medias
         Media testMedia = (Media) printService.getDefaultAttributeValue(Media.class);
         if (testMedia == null) {
             Media[] medias = (Media[]) printService
@@ -82,7 +138,7 @@ public class CUPSPrinterImageableAreaTest {
                 throw new RuntimeException("Medias not found");
             }
             for (Media media: medias) {
-                if (media == MediaSizeName.ISO_A4 || media == MediaSizeName.NA_LETTER) {
+                if (ALLOWED_MEDIA_LIST.contains(media)) {
                     testMedia = media;
                     break;
                 }
@@ -91,138 +147,29 @@ public class CUPSPrinterImageableAreaTest {
         if (!(testMedia instanceof MediaSizeName)) {
             throw new RuntimeException("Test media not found");
         }
-        MediaSize ms = MediaSize.getMediaSizeForName((MediaSizeName)testMedia);
+        return (MediaSizeName)testMedia;
+    }
+
+    private static PageFormat createTestPageFormat(MediaSizeName testMedia) {
+        MediaSize ms = MediaSize.getMediaSizeForName(testMedia);
         if (ms == null) {
             throw new RuntimeException("Media size not defined");
         }
-        MediaPrintableArea mpa = getMaximumMediaPrintableArea((MediaSizeName)testMedia, ms);
+        MediaPrintableArea mpa = getMaximumMediaPrintableArea(testMedia, ms);
         if (mpa == null) {
             throw new RuntimeException("Media printable area not defined");
         }
 
-        final PageFormat pageFormat = new PageFormat();
+        PageFormat pageFormat = new PageFormat();
         pageFormat.setOrientation(PageFormat.PORTRAIT);
         Paper paper = new Paper();
-
-        paper.setSize(ms.getX(MediaSize.INCH) * 72, ms.getY(MediaSize.INCH) * 72);
-        paper.setImageableArea(mpa.getX(MediaPrintableArea.INCH) * 72,
-                mpa.getY(MediaPrintableArea.INCH) * 72,
-                mpa.getWidth(MediaPrintableArea.INCH) * 72,
-                mpa.getHeight(MediaPrintableArea.INCH) * 72);
+        paper.setSize(ms.getX(MediaSize.INCH) * DPI, ms.getY(MediaSize.INCH) * DPI);
+        paper.setImageableArea(mpa.getX(MediaPrintableArea.INCH) * DPI,
+                mpa.getY(MediaPrintableArea.INCH) * DPI,
+                mpa.getWidth(MediaPrintableArea.INCH) * DPI,
+                mpa.getHeight(MediaPrintableArea.INCH) * DPI);
         pageFormat.setPaper(paper);
-
-        // margins: left, bottom, right, top
-        final float[] margins = new float[] {
-                mpa.getX(MediaPrintableArea.MM),
-                ms.getY(MediaSize.MM) - mpa.getY(MediaPrintableArea.MM) - mpa.getHeight(MediaPrintableArea.MM),
-                ms.getX(MediaSize.MM) - mpa.getX(MediaPrintableArea.MM) - mpa.getWidth(MediaPrintableArea.MM),
-                mpa.getY(MediaPrintableArea.MM),
-        };
-
-        final String testMediaSizeName = testMedia.toString();
-
-        SwingUtilities.invokeLater(() -> {
-            try {
-                testPrint(pageFormat, testMediaSizeName, margins);
-            } catch (PrinterException e) {
-                fail();
-            }
-            testFinished = true;
-        });
-
-        long time = System.currentTimeMillis() + TIMEOUT;
-
-        while (intime = (System.currentTimeMillis() < time)) {
-            if (testFinished) {
-                break;
-            }
-            Thread.sleep(500);
-        }
-
-        closeDialogs();
-
-        if (!intime) {
-            throw new Exception("Timeout");
-        }
-
-        if (!testPassed) {
-            throw new Exception("Test failed!");
-        }
-    }
-
-    private static void testPrint(PageFormat pageFormat, String mediaName, float[] margins) throws PrinterException {
-        String[] instructions = {
-                "This test checks that the media margins ",
-                "fetched from the printer's PPD file are correct.",
-                "The '" + mediaName + "' will be used for the test.",
-                "Press the 'Start Test' button to print a test page.",
-                "A passing test will print the page with ",
-                "a black rectangle along the printable area.",
-                "Ensure that all sides of the rectangle are printed.",
-                String.format("Margin sizes: left %.1f mm., bottom %.1f mm., right %.1f mm., top %.1f mm.",
-                        margins[0], margins[1], margins[2], margins[3])
-        };
-
-        String title = "Media margins test";
-        final JDialog dialog = new JDialog((Frame) null, title, Dialog.ModalityType.DOCUMENT_MODAL);
-        JTextArea textArea = new JTextArea(String.join("\n", instructions));
-        textArea.setEditable(false);
-        final JButton testButton = new JButton("Start Test");
-        final JButton passButton = new JButton("PASS");
-        passButton.setEnabled(false);
-        passButton.addActionListener((e) -> {
-            pass();
-            dialog.dispose();
-        });
-        final JButton failButton = new JButton("FAIL");
-        failButton.setEnabled(false);
-        failButton.addActionListener((e) -> {
-            fail();
-            dialog.dispose();
-        });
-        testButton.addActionListener((e) -> {
-            testButton.setEnabled(false);
-            PrinterJob job = PrinterJob.getPrinterJob();
-            job.setPrintable(new RectPrintable(), pageFormat);
-            try {
-                job.print();
-            } catch (PrinterException ex) {
-                fail();
-            }
-            passButton.setEnabled(true);
-            failButton.setEnabled(true);
-        });
-
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.add(textArea, BorderLayout.CENTER);
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.add(testButton);
-        buttonPanel.add(passButton);
-        buttonPanel.add(failButton);
-        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
-        dialog.add(mainPanel);
-        dialog.pack();
-        dialog.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                fail();
-            }
-        });
-        dialog.setVisible(true);
-    }
-
-    private static void closeDialogs() {
-        for (Window w : Dialog.getWindows()) {
-            w.dispose();
-        }
-    }
-
-    private static void pass() {
-        testPassed = true;
-    }
-
-    private static void fail() {
-        testPassed = false;
+        return pageFormat;
     }
 
     private static MediaPrintableArea getMaximumMediaPrintableArea(MediaSizeName msn, MediaSize ms) {
@@ -250,6 +197,10 @@ public class CUPSPrinterImageableAreaTest {
             }
         }
         return mpa;
+    }
+
+    private static double inchesToMM(double inches) {
+        return inches / MM_PER_INCH;
     }
 
     private static class RectPrintable implements Printable {
