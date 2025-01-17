e1ceff3f93772318e2ccda4782eb0eb42d84161f
anass baya <anass.baya@oracle.com>
2025-01-17 21:39:22 +0100

    Test to verify that java.awt.Toolkit.getScreenInsets(GraphicsConfiguration) returns xcorrect values

--- test/jdk/java/awt/Multiscreen/MultiScreenInsetsTest/MultiScreenInsetsTest.java
+++ test/jdk/java/awt/Multiscreen/MultiScreenInsetsTest/MultiScreenInsetsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,9 +24,9 @@
 /*
   @test
   @key headful
-  @bug 8020443
-  @summary Frame is not created on the specified GraphicsDevice with two
-monitors
+  @bug 8020443 6899304
+  @summary Test to check if the frame is created on the specified GraphicsDevice 
+  and if getScreenInsets()returns the correct values across multiple monitors.
   @author Oleg Pekhovskiy
   @library /test/lib
   @build jdk.test.lib.Platform
@@ -47,12 +47,7 @@ public class MultiScreenInsetsTest {
     private static final int SIZE = 100;
 
     public static void main(String[] args) throws InterruptedException {
-        if (!Platform.isLinux()) {
-            System.out.println("This test is for Linux only..." +
-                               "skipping!");
-            return;
-        }
-
+  
         GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice[] gds = ge.getScreenDevices();
         if (gds.length < 2) {
@@ -70,7 +65,17 @@ public class MultiScreenInsetsTest {
             frame.setLocation(bounds.x + (bounds.width - SIZE) / 2,
                               bounds.y + (bounds.height - SIZE) / 2);
             frame.setSize(SIZE, SIZE);
-            frame.setUndecorated(true);
+
+            /* 
+             * On Windows, undecorated maximized frames are placed over the taskbar. 
+             * Use a decorated frame instead.
+             */
+            if(Platform.isWindows()) {
+                frame.setUndecorated(false);
+            } else {
+                frame.setUndecorated(true);
+            }
+ 
             frame.setVisible(true);
 
             // Maximize Frame to reach the struts
@@ -79,14 +84,42 @@ public class MultiScreenInsetsTest {
 
             Rectangle frameBounds = frame.getBounds();
             frame.dispose();
+
+            /* 
+             * On Windows, the top-left corner of an undecorated maximized frame may have negative coordinates (x, y).
+             * Adjust the frame bounds accordingly.
+             */
+            if (frameBounds.x < bounds.x)
+            {
+                frameBounds.width -= (bounds.x - frameBounds.x) * 2; 
+                frameBounds.x = bounds.x;
+            }
+            if (frameBounds.y < bounds.y)
+            {
+                frameBounds.height -= (bounds.y - frameBounds.y) * 2;
+                frameBounds.y = bounds.y;
+            }
+             
+             // Add a margin to compensate for the lost fractional parts when casting to an integer.       
+            int marginX = getMarginForScaleX(gc);
+            int marginY = getMarginForScaleY(gc);
+
             if (bounds.x + insets.left != frameBounds.x
                 || bounds.y + insets.top != frameBounds.y
-                || bounds.width - insets.right - insets.left != frameBounds.width
-                || bounds.height - insets.bottom - insets.top != frameBounds.height) {
+                || bounds.width - insets.right - insets.left + marginX!= frameBounds.width
+                || bounds.height - insets.bottom - insets.top + marginY!= frameBounds.height) {
                 throw new RuntimeException("Test FAILED! Wrong screen #" +
                                            screen + " insets: " + insets);
             }
         }
         System.out.println("Test PASSED!");
+    }
+    static int getMarginForScaleX(GraphicsConfiguration gc) {
+        float scaleFactorX = (float) gc.getDefaultTransform().getScaleX();
+        return (scaleFactorX % 1 == 0.5) ? 1 : 0;
+    }
+    static int getMarginForScaleY(GraphicsConfiguration gc) {
+        float scaleFactorY = (float) gc.getDefaultTransform().getScaleY();
+        return (scaleFactorY % 1 == 0.5) ? 1 : 0;
     }
 }
