5d1652b171ec2241e81d67579b0394fdf069f926
anass baya <anass.baya@oracle.com>
2025-01-17 19:19:44 +0100

    Remove extralines

--- src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
+++ src/java.desktop/windows/native/libawt/windows/awt_Toolkit.cpp
@@ -1936,7 +1936,6 @@ JNIEnv* AwtToolkit::GetEnv() {
 
 BOOL AwtToolkit::GetScreenInsets(int screenNum, RECT * rect)
 {
-
     MONITORINFO *miInfo = AwtWin32GraphicsDevice::GetMonitorInfo(screenNum);
     if (miInfo) {
         rect->top = miInfo->rcWork.top    - miInfo->rcMonitor.top;
@@ -1944,8 +1943,7 @@ BOOL AwtToolkit::GetScreenInsets(int screenNum, RECT * rect)
         rect->bottom = miInfo->rcMonitor.bottom - miInfo->rcWork.bottom;
         rect->right = miInfo->rcMonitor.right - miInfo->rcWork.right;
         return TRUE;
-    }
-    
+    }    
     return FALSE;
 }
 
