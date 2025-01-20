512941c00c4059a57f658c122dfeadf78c1e1d7d
Stefan Karlsson <stefank@openjdk.org>
2025-01-20 11:01:44 +0100

    Copyright

--- test/hotspot/gtest/utilities/test_bitMap_popcnt.cpp
+++ test/hotspot/gtest/utilities/test_bitMap_popcnt.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2020, SAP and/or its affiliates.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
