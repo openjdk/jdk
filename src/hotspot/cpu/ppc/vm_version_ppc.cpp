50ca470da8a96149b9634437ceb29b7daf00eac8
suchismith1993 <suchismith1993@gmail.com>
2025-01-23 22:13:28 +0530

    indentation

--- src/hotspot/cpu/ppc/vm_version_ppc.cpp
+++ src/hotspot/cpu/ppc/vm_version_ppc.cpp
@@ -281,7 +281,7 @@ void VM_Version::initialize() {
 
   // The AES intrinsic stubs require AES instruction support.
   if (has_vcipher()) {
-    if (FLAG_IS_DEFAULT(UseAES)) {
+   if (FLAG_IS_DEFAULT(UseAES)) {
       UseAES = true;
     }
   } else if (UseAES) {
@@ -306,14 +306,13 @@ void VM_Version::initialize() {
   }
 
   if (VM_Version::has_vsx()) {
-      if (FLAG_IS_DEFAULT(UseGHASHIntrinsics)) {
-          UseGHASHIntrinsics = true;
-      }
+    if (FLAG_IS_DEFAULT(UseGHASHIntrinsics)) {
+      UseGHASHIntrinsics = true;
+    }
   } else if (UseGHASHIntrinsics) {
-      if (!FLAG_IS_DEFAULT(UseGHASHIntrinsics)) {
-          warning("GHASH intrinsics are not available on this CPU");
-      }
-      FLAG_SET_DEFAULT(UseGHASHIntrinsics, false);
+    if (!FLAG_IS_DEFAULT(UseGHASHIntrinsics))
+      warning("GHASH intrinsics are not available on this CPU");
+    FLAG_SET_DEFAULT(UseGHASHIntrinsics, false);
   }
 
   if (FLAG_IS_DEFAULT(UseFMA)) {
