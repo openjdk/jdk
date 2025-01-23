679d49caac9a801e99f2a748f811d0ee67169c85
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2025-01-23 15:52:58 -0800

    8347997: assert(false) failed: EA: missing memory path

---
+++ test/hotspot/jtreg/compiler/intrinsics/TestContinuationPinningAndEA.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @bug 8347997
+ * @summary Test that Cintinuation.pin() and unpin() intrinsics work with EA.
+ * @run main/othervm --add-opens=java.base/jdk.internal.vm=ALL-UNNAMED
+ *      TestContinuationPinningAndEA
+ */
+
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+public class TestContinuationPinningAndEA {
+  public static final MethodHandle pin;
+  public static final MethodHandle unpin;
+
+  static {
+    try {
+      MethodHandles.Lookup lookup = MethodHandles.lookup();
+      Class<?> Continuation = lookup.findClass("jdk.internal.vm.Continuation");
+      pin = lookup.findStatic(Continuation, "pin", MethodType.methodType(void.class));
+      unpin = lookup.findStatic(Continuation, "unpin", MethodType.methodType(void.class));
+    } catch (Throwable e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  static class FailsEA {
+    final Object o;
+
+    public FailsEA() throws Throwable {
+      o = new Object();
+      pin.invokeExact();
+      unpin.invokeExact();
+    }
+  }
+
+  static class Crashes {
+    final Object o;
+
+    public Crashes() throws Throwable {
+      pin.invokeExact();
+      unpin.invokeExact();
+      o = new Object();
+    }
+  }
+
+  static void test_FailsEA() throws Throwable {
+    for (int i = 0; i < 10_000; ++i) {
+      new FailsEA();
+    }
+  }
+
+  static void test_Crashes() throws Throwable {
+    for (int i = 0; i < 10_000; ++i) {
+      new Crashes();
+    }  
+  }
+
+  public static void main(String[] args) throws Throwable {
+    int iterations = 100;
+    for (int i = 0; i < iterations; ++i) {
+      test_FailsEA();
+    }
+    for (int i = 0; i < iterations; ++i) {
+      test_Crashes();
+    }
+  }
+}
