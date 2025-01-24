0df9dcb6aa7c31511ca3137da246962faca818a3
Stefan Karlsson <stefank@openjdk.org>
2025-01-24 09:23:29 +0000

    8346572: Check is_reserved() before using ReservedSpace instances
    Reviewed-by: tschatzl, wkemper, ayang, ysr

--- src/hotspot/share/memory/virtualspace.cpp
+++ src/hotspot/share/memory/virtualspace.cpp
@@ -57,7 +57,7 @@ bool VirtualSpace::initialize(ReservedSpace rs, size_t committed_size) {
 }
 
 bool VirtualSpace::initialize_with_granularity(ReservedSpace rs, size_t committed_size, size_t max_commit_granularity) {
-  if(!rs.is_reserved()) return false;  // allocation failed.
+  assert(rs.is_reserved(), "ReservedSpace should have been initialized");
   assert(_low_boundary == nullptr, "VirtualSpace already initialized");
   assert(max_commit_granularity > 0, "Granularity must be non-zero.");
 
