a4302a39ec2fd57dac5d1f1f3ca87bb70fb7b793
Leonid Mesnik <leonid.mesnik@gmail.com>
2025-01-23 16:42:26 -0800

    8318098: Update jfr tests to replace keyword jfr with vm.flagless

--- test/jdk/jdk/jfr/jvm/TestChunkIntegrity.java
+++ test/jdk/jdk/jfr/jvm/TestChunkIntegrity.java
@@ -56,7 +56,7 @@ import jdk.test.lib.jfr.TestClassLoader;
 
 /**
  * @test
- * @key jfr
+ * @requires vm.flagless
  * @requires vm.hasJFR
  * @library /test/lib /test/jdk
  * @run main/othervm/timeout=300 jdk.jfr.jvm.TestChunkIntegrity
