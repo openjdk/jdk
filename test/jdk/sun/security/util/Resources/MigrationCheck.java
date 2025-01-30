43686286f49d8f140e26c151a6a045b32b354b8c
Artur Barashev <artur.barashev@oracle.com>
2025-01-30 09:34:12 -0500

    Adding security_fr

--- test/jdk/sun/security/util/Resources/MigrationCheck.java
+++ test/jdk/sun/security/util/Resources/MigrationCheck.java
@@ -26,8 +26,6 @@
  * @bug 8345940
  * @modules java.base/sun.security.util
  *          java.base/sun.security.util.resources
- *          java.base/sun.security.tools.keytool
- *          jdk.jartool/sun.security.tools.jarsigner
  * @summary Check migration correctness of resources files
  * @run testng/othervm MigrationCheck
  */
@@ -44,67 +42,67 @@ public class MigrationCheck {
     // Auth
 
     @Test
-    public void testAuth() throws Exception {
+    public void testAuth() {
         checkResourcesMigration(new sun.security.util.AuthResources(),
                                 new sun.security.util.resources.auth());
     }
 
     @Test
-    public void testAuth_de() throws Exception {
+    public void testAuth_de() {
         checkResourcesMigration(new sun.security.util.AuthResources_de(),
                                 new sun.security.util.resources.auth_de());
     }
 
     @Test
-    public void testAuth_es() throws Exception {
+    public void testAuth_es() {
         checkResourcesMigration(new sun.security.util.AuthResources_es(),
                                 new sun.security.util.resources.auth_es());
     }
 
     @Test
-    public void testAuth_fr() throws Exception {
+    public void testAuth_fr() {
         checkResourcesMigration(new sun.security.util.AuthResources_fr(),
                                 new sun.security.util.resources.auth_fr());
     }
 
     @Test
-    public void testAuth_it() throws Exception {
+    public void testAuth_it() {
         checkResourcesMigration(new sun.security.util.AuthResources_it(),
                                 new sun.security.util.resources.auth_it());
     }
 
     @Test
-    public void testAuth_ja() throws Exception {
+    public void testAuth_ja() {
         checkResourcesMigration(new sun.security.util.AuthResources_ja(),
                                 new sun.security.util.resources.auth_ja());
     }
 
     @Test
-    public void testAuth_ko() throws Exception {
+    public void testAuth_ko() {
         checkResourcesMigration(new sun.security.util.AuthResources_ko(),
                                 new sun.security.util.resources.auth_ko());
     }
 
     @Test
-    public void testAuth_sv() throws Exception {
+    public void testAuth_sv() {
         checkResourcesMigration(new sun.security.util.AuthResources_sv(),
                                 new sun.security.util.resources.auth_sv());
     }
 
     @Test
-    public void testAuth_pt_BR() throws Exception {
+    public void testAuth_pt_BR() {
         checkResourcesMigration(new sun.security.util.AuthResources_pt_BR(),
                                 new sun.security.util.resources.auth_pt_BR());
     }
 
     @Test
-    public void testAuth_zh_CN() throws Exception {
+    public void testAuth_zh_CN() {
         checkResourcesMigration(new sun.security.util.AuthResources_zh_CN(),
                                 new sun.security.util.resources.auth_zh_CN());
     }
 
     @Test
-    public void testAuth_zh_TW() throws Exception {
+    public void testAuth_zh_TW() {
         checkResourcesMigration(new sun.security.util.AuthResources_zh_TW(),
                                 new sun.security.util.resources.auth_zh_TW());
     }
@@ -112,32 +110,38 @@ public class MigrationCheck {
     // Security
 
     @Test
-    public void testSecurity() throws Exception {
+    public void testSecurity() {
         checkResourcesMigration(new sun.security.util.Resources(),
                 new sun.security.util.resources.security());
     }
 
     @Test
-    public void testSecurity_de() throws Exception {
+    public void testSecurity_de() {
         checkResourcesMigration(new sun.security.util.Resources_de(),
                 new sun.security.util.resources.security_de());
     }
 
     @Test
-    public void testSecurity_es() throws Exception {
+    public void testSecurity_es() {
         checkResourcesMigration(new sun.security.util.Resources_es(),
                 new sun.security.util.resources.security_es());
+    }
+
+    @Test
+    public void testSecurity_fr() {
+        checkResourcesMigration(new sun.security.util.Resources_fr(),
+                new sun.security.util.resources.security_fr());
     }
 
     // ResourcesMgr
 
     @Test
-    public void testResourcesMgrSecurityDefault() throws Exception {
+    public void testResourcesMgrSecurityDefault() {
         checkResourcesMgrSecurity(new sun.security.util.Resources());
     }
 
     @Test
-    public void testResourcesMgrAuthDefault() throws Exception {
+    public void testResourcesMgrAuthDefault() {
         checkResourcesMgrAuth(new sun.security.util.AuthResources());
     }
 
