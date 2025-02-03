b41b27b7a7de228bce1b52442c0e470248b462e1
Damon Nguyen <damon.nguyen@oracle.com>
2025-02-03 10:57:14 -0800

    Add platform conditionals to test instructions

--- test/jdk/java/awt/PopupMenu/PopupMenuVisuals.java
+++ test/jdk/java/awt/PopupMenu/PopupMenuVisuals.java
@@ -24,8 +24,8 @@
  * @test
  * @bug 6180413 6184485 6267144
  * @summary test for popup menu visual bugs in XAWT
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
+ * @library /java/awt/regtesthelpers /test/lib
+ * @build PassFailJFrame jdk.test.lib.Platform
  * @run main/manual PopupMenuVisuals
 */
 
@@ -40,16 +40,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import jdk.test.lib.Platform;
+
 public class PopupMenuVisuals {
     private static final String INSTRUCTIONS = """
          This test should show a button 'Popup'.
          Click on the button. A popup menu should be shown.
          If following conditions are met:
-          - Menu is disabled
-          - Menu has caption 'Popup menu' (only applicable for linux)
-          - Menu items don't show shortcuts (except on MacOS)
+          - Menu is disabled %s%s
 
-         Click Pass else click Fail.""";
+         Click Pass else click Fail."""
+            .formatted(
+                    Platform.isLinux() ? "\n - Menu has caption 'Popup menu'" : "",
+                    !Platform.isOSX() ? "\n - Menu items don't show shortcuts" : ""
+            );
 
     static PopupMenu pm;
     static Frame frame;
@@ -78,7 +82,7 @@ public class PopupMenuVisuals {
         CheckboxMenuItem mi3 = new CheckboxMenuItem("Item 3");
         Menu sm = new Menu("Submenu");
 
-        //Get things going.  Request focus, set size, et cetera
+        // Get things going.  Request focus, set size, et cetera
         frame = new Frame("PopupMenuVisuals");
         frame.setSize(200, 200);
         frame.validate();
