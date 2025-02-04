2f67c246182de025fd15665e12bd7e44ebc1f4d8
Shaojin Wen <shaojin.wensj@alibaba-inc.com>
2025-02-05 07:41:47 +0800

    Update src/java.base/share/classes/java/lang/Integer.java
    Co-authored-by: Raffaello Giulietti <raffaello.giulietti@oracle.com>

--- src/java.base/share/classes/java/lang/Integer.java
+++ src/java.base/share/classes/java/lang/Integer.java
@@ -522,6 +522,7 @@ public final class Integer extends Number
         if (s == null || radix != 10 || (len = (value = s.value()).length) == 0 || !s.isLatin1()) {
             return parseInt0(s, radix);
         }
+        /* Accumulating negatively avoids surprises near MAX_VALUE */
         int fc = value[0];
         int result = Integer.isDigitLatin1(fc)
                 ? '0' - fc
