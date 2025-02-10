b18bbdac5dce6c42ccea0cfbdc09188c3c1dd1de
Takakuri <k.takakuri@fujitsu.com>
2025-02-10 17:45:04 +0900

    8349288: runtime/os/windows/TestAvailableProcessors.java fails on localized Windows platform

--- test/hotspot/jtreg/runtime/os/windows/TestAvailableProcessors.java
+++ test/hotspot/jtreg/runtime/os/windows/TestAvailableProcessors.java
@@ -64,7 +64,7 @@ public class TestAvailableProcessors {
 
         List<String> command = new ArrayList<>();
         //Execution command to prevent garbled characters
-        command.addAll(List.of("cmd.exe", "/c", "set", "PATH=%PATH%;C:\\Windows\\System32", "&&"));
+        command.addAll(List.of("cmd.exe", "/c", "set", "PATH=%PATH%;C:\\Windows\\System32;C:\\Windows\\System32\\wbem", "&&"));
         command.addAll(List.of("chcp", "437", ">nul", "2>&1", "&&"));
         //Execute command to obtain OS Version
         command.add(systeminfoPath);
