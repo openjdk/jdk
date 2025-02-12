d0d42a4def056c035a6a7e7ea4e4c94d7c592743
Maxim Kartashev <maxim.kartashev@jetbrains.com>
2025-02-12 12:39:10 +0400

    Updated the copyright year and added the bugid to the test

--- test/jdk/java/nio/file/Files/SBC.java
+++ test/jdk/java/nio/file/Files/SBC.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,7 +22,7 @@
  */
 
 /* @test
- * @bug 4313887
+ * @bug 4313887 8349812
  * @summary Unit test for java.nio.file.Files.newByteChannel
  * @library ..
  * @modules jdk.unsupported
