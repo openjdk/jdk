1067f1a110931343131cde4cb8bad234dc73d03f
Koushik Thirupattur <koushik.thirupattur@oracle.com>
2025-02-14 11:08:20 -0800

    8328914: Document the java.security.debug property in javadoc

--- src/java.base/share/classes/java/security/package-info.java
+++ src/java.base/share/classes/java/security/package-info.java
@@ -71,6 +71,12 @@
  *
  * </ul>
  *
+ * <h2>Additional Specification</h2>
+ * <ul>
+ *   <li><a href="doc-files/security-properties.html">
+ *     Security Properties</a></li>
+ * </ul>
+ *
  * @spec security/standard-names.html Java Security Standard Algorithm Names
  * @since 1.1
  */
