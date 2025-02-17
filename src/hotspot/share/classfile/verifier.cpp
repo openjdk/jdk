4fc8fe81f3a82cedff00cb2c411eea9add0e5dc8
Afshin Zafari <Afshin.zafari@oracle.com>
2025-02-17 16:58:49 +0100

    comopared wit External Max instead.

--- src/hotspot/share/classfile/verifier.cpp
+++ src/hotspot/share/classfile/verifier.cpp
@@ -2038,8 +2038,8 @@ void ClassVerifier::verify_cp_type(
 
   verify_cp_index(bci, cp, index, CHECK_VERIFY(this));
   unsigned int tag = cp->tag_at(index).value();
-  // Resolution errors start with JVM_CONSTANT_InternalMin = 100, which is not valid for shift op
-  if (tag >= JVM_CONSTANT_InternalMin || (types & (1 << tag)) == 0) {
+  // tags up to JVM_CONSTANT_ExternalMax are verifiable and valid for shift op
+  if (tag > JVM_CONSTANT_ExternalMax || (types & (1 << tag)) == 0) {
     verify_error(ErrorContext::bad_cp_index(bci, index),
       "Illegal type at constant pool entry %d in class %s",
       index, cp->pool_holder()->external_name());
