189aa1e502d8404db5ab3f6a496945bdf8aec4ff
Kangcheng Xu <kxu@redhat.com>
2025-02-18 10:23:15 -0500

    use java_shift_left()

--- src/hotspot/share/opto/addnode.cpp
+++ src/hotspot/share/opto/addnode.cpp
@@ -457,8 +457,8 @@ Node* AddNode::find_simple_lshift_pattern(Node* n, BasicType bt, jlong* multipli
     }
 
     *multiplier = bt == T_INT
-                  ? (jlong) (1 << con->get_int()) // loss of precision is expected for int as it overflows
-                  : ((jlong) 1) << con->get_int();
+                    ? java_shift_left(1, con->get_int())
+                    : java_shift_left(1L, con->get_int());
     return n->in(1);
   }
 
@@ -521,8 +521,8 @@ Node* AddNode::find_power_of_two_addition_pattern(Node* n, BasicType bt, jlong* 
       }
 
       lhs_multiplier = bt == T_INT
-                       ? (jlong) (1 << con->get_int())
-                       : ((jlong) 1) << con->get_int();
+                       ? java_shift_left(1, con->get_int())
+                       : java_shift_left(1L, con->get_int());
 
     }
 
@@ -544,8 +544,8 @@ Node* AddNode::find_power_of_two_addition_pattern(Node* n, BasicType bt, jlong* 
         }
 
         *multiplier = lhs_multiplier + (bt == T_INT
-                      ? (jlong) (1 << con->get_int())
-                      : ((jlong) 1) << con->get_int());
+                      ? java_shift_left(1, con->get_int())
+                      : java_shift_left(1L, con->get_int()));
       }
 
       return lhs->in(1);
