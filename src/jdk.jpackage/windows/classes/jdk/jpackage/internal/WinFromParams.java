1117658794390e7e5655ab3b48d179f7b2b870b4
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-02-20 11:58:50 -0500

    Minor

--- src/jdk.jpackage/windows/classes/jdk/jpackage/internal/WinFromParams.java
+++ src/jdk.jpackage/windows/classes/jdk/jpackage/internal/WinFromParams.java
@@ -44,7 +44,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-
 import jdk.jpackage.internal.model.ConfigException;
 import jdk.jpackage.internal.model.WinApplication;
 import jdk.jpackage.internal.model.WinLauncher;
