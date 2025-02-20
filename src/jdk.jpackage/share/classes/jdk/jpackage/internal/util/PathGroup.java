2587c0bc44badd32bd0f126efdf28813725d79d7
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-02-20 11:59:17 -0500

    Remove redundant imports

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/util/PathGroup.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/util/PathGroup.java
@@ -37,9 +37,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /**
