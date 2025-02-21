73bf69ce4daa88eb8f186d2e7922b7dd68a5d0fe
Nicole Xu <yuyangx@nvidia.com>
2025-02-21 06:56:23 +0000

    Revert "8349944: [JMH] sun.misc.UnsafeOps cannot access class jdk.internal.misc.Unsafe"
    This reverts commit ebc32ae2c6e448075fedbdbb2b4879c43829c44b.

--- test/micro/org/openjdk/bench/sun/misc/UnsafeOps.java
+++ test/micro/org/openjdk/bench/sun/misc/UnsafeOps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,6 @@ import org.openjdk.jmh.annotations.*;
 @Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @State(org.openjdk.jmh.annotations.Scope.Thread)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Fork(value = 1, jvmArgs = {"--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED"})
 @SuppressWarnings("removal")
 public class UnsafeOps {
     static final Unsafe U;
