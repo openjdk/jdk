ec13274c51c9f9e2edf9e4980b5afcb4ce038cba
William Kemper <kemperw@amazon.com>
2025-02-24 14:44:04 -0800

    Use lock to protect in progress flag, remove unnecessary stop lock and flag

--- src/hotspot/share/gc/shenandoah/shenandoahUncommitThread.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahUncommitThread.hpp
@@ -38,17 +38,11 @@ class ShenandoahUncommitThread : public ConcurrentGCThread {
   // Indicates that an explicit gc has been requested
   ShenandoahSharedFlag _explicit_gc_requested;
 
-  // Indicates that the thread should stop and terminate
-  ShenandoahSharedFlag _stop_requested;
-
   // Indicates whether it is safe to uncommit regions
   ShenandoahSharedFlag _uncommit_allowed;
 
   // Indicates that regions are being actively uncommitted
   ShenandoahSharedFlag _uncommit_in_progress;
-
-  // This lock is used to coordinate stopping and terminating this thread
-  Monitor _stop_lock;
 
   // This lock is used to coordinate allowing or forbidding regions to be uncommitted
   Monitor _uncommit_lock;
@@ -65,6 +59,8 @@ class ShenandoahUncommitThread : public ConcurrentGCThread {
 
   // True if the control thread has allowed this thread to uncommit regions
   bool is_uncommit_allowed() const;
+
+  size_t do_uncommit_work(double shrink_before, size_t shrink_until) const;
 
 public:
   explicit ShenandoahUncommitThread(ShenandoahHeap* heap);
@@ -85,7 +81,7 @@ public:
   void allow_uncommit();
 
   // True if uncommit is in progress
-  bool is_uncommit_in_progress() {
+  bool is_uncommit_in_progress() const {
     return _uncommit_in_progress.is_set();
   }
 protected:
