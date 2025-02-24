d3429ada8fc6184bc5285b299c5a5c533ba6bd00
Prasanta Sadhukhan <psadhukhan@openjdk.org>
2025-02-24 06:50:42 +0000

    8349111: Enhance Swing supports
    Reviewed-by: rhalade, jdv, prr

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -495,7 +495,11 @@ public class BasicTabbedPaneUI extends TabbedPaneUI implements SwingConstants {
         }
         tabPane.addContainerListener(getHandler());
         if (tabPane.getTabCount()>0) {
-            htmlViews = createHTMLVector();
+            Boolean htmlDisabled = (Boolean)
+                                    tabPane.getClientProperty("html.disable");
+            if (!(Boolean.TRUE.equals(htmlDisabled))) {
+                htmlViews = createHTMLVector();
+            }
         }
     }
 
@@ -4065,8 +4069,10 @@ public class BasicTabbedPaneUI extends TabbedPaneUI implements SwingConstants {
 
         private void updateHtmlViews(int index, boolean inserted) {
             String title = tabPane.getTitleAt(index);
+            Boolean htmlDisabled = (Boolean)
+                                    tabPane.getClientProperty("html.disable");
             boolean isHTML = BasicHTML.isHTMLString(title);
-            if (isHTML) {
+            if (isHTML && !(Boolean.TRUE.equals(htmlDisabled))) {
                 if (htmlViews==null) {    // Initialize vector
                     htmlViews = createHTMLVector();
                 } else {                  // Vector already exists
