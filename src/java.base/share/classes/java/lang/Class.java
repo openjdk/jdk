591abdda6920617fa19aa4dc1106c32878b100f1
Coleen Phillimore <coleen.phillimore@oracle.com>
2025-02-24 19:26:36 +0000

    Add a comment about Class constructor.

--- src/java.base/share/classes/java/lang/Class.java
+++ src/java.base/share/classes/java/lang/Class.java
@@ -239,6 +239,7 @@ public final class Class<T> implements java.io.Serializable,
     private Class(ClassLoader loader, Class<?> arrayComponentType, char mods, ProtectionDomain pd, boolean isPrim) {
         // Initialize final field for classLoader.  The initialization value of non-null
         // prevents future JIT optimizations from assuming this final field is null.
+        // The following assignments are done directly by the VM without calling this constructor.
         classLoader = loader;
         componentType = arrayComponentType;
         modifiers = mods;
