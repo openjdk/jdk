3cc63fb27a720519391490609d522fae84a772e8
David Linus Briemann <david.briemann@sap.com>
2025-02-25 09:37:48 +0100

    finish CountBytecodesTest

--- test/hotspot/jtreg/runtime/interpreter/CountBytecodesTest.java
+++ test/hotspot/jtreg/runtime/interpreter/CountBytecodesTest.java
@@ -2,7 +2,8 @@
 /*
  * @test
  * @requires vm.debug & vm.bits == "64"
- * @summary this is a summary
+ * @summary Test the output for CountBytecodes and validate that the counter
+ *          does not overflow for more than 2^32 bytecodes counted.
  * @library /test/lib
  * @run main/othervm/timeout=300 -Xint -XX:+CountBytecodes CountBytecodesTest
  */
@@ -15,11 +16,10 @@ import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
 
 public class CountBytecodesTest {
-    private final static long iterations = 1L << 32; // Exceed 32 bit.
+    private final static long iterations = 1L << 32;
 
     public static void main(String args[]) throws Exception {
         if (args.length == 1 && args[0].equals("test")) {
-            System.out.println("### OUTPUT: " + iterations);
             for (long i = 0; i < iterations; i++) {
                 // Just iterating is enough to execute and count bytecodes.
             }
@@ -27,18 +27,13 @@ public class CountBytecodesTest {
             ProcessBuilder pb = ProcessTools.createTestJavaProcessBuilder("-Xint", "-XX:+CountBytecodes", "CountBytecodesTest", "test");
             OutputAnalyzer output = new OutputAnalyzer(pb.start());
             output.shouldHaveExitValue(0);
+
+            // Output format: [BytecodeCounter::counter_value = 38676232802]
             output.stdoutShouldContain("BytecodeCounter::counter_value");
-            Pattern counterRegex = Pattern.compile("BytecodeCounter::counter_value\s*=\s*\\d+");
-            Matcher counterMatcher = counterRegex.matcher(output.getStdout());
-            Asserts.assertTrue(counterMatcher.find());
-            String resultLine = counterMatcher.group();
+            String bytecodesStr = output.firstMatch("BytecodeCounter::counter_value\s*=\s*(\\d+)", 1);
+            long bytecodes = Long.parseLong(bytecodesStr);
 
-            // TODO: find right group and convert to number
-
-
-            // [BytecodeCounter::counter_value = 38676232802]
-            // System.out.println("stdout: " + output.getStdout());
-            // System.out.println("stderr: " + output.getStderr());
+            Asserts.assertGTE(bytecodes, 4294967296L);
         }
     }
 }
