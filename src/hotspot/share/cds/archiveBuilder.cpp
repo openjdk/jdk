b3f6916a3a2c885f9288dbec31e9266d393f5710
tstuefe <thomas.stuefe@gmail.com>
2025-02-26 13:19:06 +0100

    Revert "8330174: Protection zone for easier detection of accidental zero-nKlass use"
    This reverts commit a70eba8e4212c2c7125475f69b3952197e7a8ce3.

--- src/hotspot/share/cds/archiveBuilder.cpp
+++ src/hotspot/share/cds/archiveBuilder.cpp
@@ -153,6 +153,7 @@ void ArchiveBuilder::SourceObjList::relocate(int i, ArchiveBuilder* builder) {
 ArchiveBuilder::ArchiveBuilder() :
   _current_dump_region(nullptr),
   _buffer_bottom(nullptr),
+  _num_dump_regions_used(0),
   _requested_static_archive_bottom(nullptr),
   _requested_static_archive_top(nullptr),
   _requested_dynamic_archive_bottom(nullptr),
@@ -160,7 +161,6 @@ ArchiveBuilder::ArchiveBuilder() :
   _mapped_static_archive_bottom(nullptr),
   _mapped_static_archive_top(nullptr),
   _buffer_to_requested_delta(0),
-  _pz_region("pz", MAX_SHARED_DELTA), // protection zone -- used only during dumping; does NOT exist in cds archive.
   _rw_region("rw", MAX_SHARED_DELTA),
   _ro_region("ro", MAX_SHARED_DELTA),
   _ptrmap(mtClassShared),
@@ -323,14 +323,9 @@ address ArchiveBuilder::reserve_buffer() {
   _shared_rs = rs;
 
   _buffer_bottom = buffer_bottom;
-
-  if (CDSConfig::is_dumping_static_archive()) {
-    _current_dump_region = &_pz_region;
-    _current_dump_region->init(&_shared_rs, &_shared_vs);
-  } else {
-    _current_dump_region = &_rw_region;
-    _current_dump_region->init(&_shared_rs, &_shared_vs);
-  }
+  _current_dump_region = &_rw_region;
+  _num_dump_regions_used = 1;
+  _current_dump_region->init(&_shared_rs, &_shared_vs);
 
   ArchivePtrMarker::initialize(&_ptrmap, &_shared_vs);
 
@@ -371,8 +366,7 @@ address ArchiveBuilder::reserve_buffer() {
   if (CDSConfig::is_dumping_static_archive()) {
     // We don't want any valid object to be at the very bottom of the archive.
     // See ArchivePtrMarker::mark_pointer().
-    _pz_region.allocate(MetaspaceShared::protection_zone_size());
-    start_dump_region(&_rw_region);
+    rw_region()->allocate(16);
   }
 
   return buffer_bottom;
@@ -550,6 +544,7 @@ ArchiveBuilder::FollowMode ArchiveBuilder::get_follow_mode(MetaspaceClosure::Ref
 void ArchiveBuilder::start_dump_region(DumpRegion* next) {
   current_dump_region()->pack(next);
   _current_dump_region = next;
+  _num_dump_regions_used ++;
 }
 
 char* ArchiveBuilder::ro_strdup(const char* s) {
