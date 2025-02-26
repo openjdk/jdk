30647a9cfaa332a8dd498edbb5a1a552e7a1553d
Xiaolong Peng <xlpeng@amazon.com>
2025-02-25 23:46:58 -0800

    Fix test failure

--- src/hotspot/share/gc/shenandoah/shenandoahOldGC.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahOldGC.cpp
@@ -78,6 +78,11 @@ void ShenandoahOldGC::op_final_mark() {
     if (VerifyAfterGC) {
       Universe::verify();
     }
+    
+    {
+      ShenandoahGCPhase phase(ShenandoahPhaseTimings::final_mark_propagate_gc_state);
+      heap->propagate_gc_state_to_all_threads();
+    }
   }
 }
 
