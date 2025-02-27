95c299a8c688e87a684574eb29fa0114b4ec326d
Andrew Dinn <adinn@redhat.com>
2025-02-27 17:59:14 +0000

    use generated names when creating opto runtime blobs

--- src/hotspot/cpu/s390/runtime_s390.cpp
+++ src/hotspot/cpu/s390/runtime_s390.cpp
@@ -70,7 +70,8 @@ void OptoRuntime::generate_exception_blob() {
   // Allocate space for the code
   ResourceMark rm;
   // Setup code generation tools
-  CodeBuffer buffer("exception_blob", 2048, 1024);
+  const char* name = OptoRuntime::stub_name(OptoStubId::exception_id);
+  CodeBuffer buffer(name, 2048, 1024);
   MacroAssembler* masm = new MacroAssembler(&buffer);
 
   Register handle_exception = Z_ARG5;
