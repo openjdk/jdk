87817bc66199c04d0bf6a842bcbe1b37df0afd49
Hai-May Chao <hai-may.chao@oracle.com>
2025-02-28 00:20:33 -0800

    Revert changes to Resources.java

--- src/jdk.jartool/share/classes/sun/security/tools/jarsigner/Resources.java
+++ src/jdk.jartool/share/classes/sun/security/tools/jarsigner/Resources.java
@@ -324,9 +324,6 @@ public class Resources extends java.util.ListResourceBundle {
         {"Cannot.find.file.", "Cannot find file: "},
         {"event.ocsp.check", "Contacting OCSP server at %s ..."},
         {"event.crl.check", "Downloading CRL from %s ..."},
-        {"Different.content.observed.in.JarInputStream.and.JarFile.", "Different content observed in JarInputStream and JarFile."},
-        {".JarInputStream.only.", " (JarInputStream only)"},
-        {".JarFile.only.", " (JarFile only)"}
     };
 
     /**
