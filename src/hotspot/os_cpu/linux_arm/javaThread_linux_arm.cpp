0a540c79584f28fe90d128977f5121467f59626b
Cesar Soares <dcsl@amazon.com>
2025-03-06 11:42:02 -0800

    Fix build: no shenandoah on arm32.

--- src/hotspot/os_cpu/linux_arm/javaThread_linux_arm.cpp
+++ src/hotspot/os_cpu/linux_arm/javaThread_linux_arm.cpp
@@ -41,8 +41,8 @@ frame JavaThread::pd_last_frame() {
 
 void JavaThread::cache_global_variables() {
   BarrierSet* bs = BarrierSet::barrier_set();
-  if (bs->is_a(BarrierSet::CardTableBarrierSet) &&
-    !bs->is_a(BarrierSet::ShenandoahBarrierSet)) {
+
+  if (bs->is_a(BarrierSet::CardTableBarrierSet)) {
     _card_table_base = (address) (barrier_set_cast<CardTableBarrierSet>(bs)->card_table()->byte_map_base());
   } else {
     _card_table_base = nullptr;
