d5c3ee737a5d896f28e6f31cdfa10d53a17a7254
Weijun Wang <weijun.wang@oracle.com>
2025-03-06 10:09:40 -0500

    8351351: Pre-hash mode, context string, etc for ML-DSA

---
+++ test/jdk/sun/security/provider/ML-DSA/Parameters.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8351351
+ * @library /test/lib
+ */
+
+import jdk.test.lib.Asserts;
+import jdk.test.lib.Utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+import java.security.spec.SignatureParameterSpec;
+
+public class Parameters {
+    public static void main(String[] args) throws Exception {
+        paramsTest();
+        signTest();
+    }
+
+    static void signTest() throws Exception {
+        var g = KeyPairGenerator.getInstance("ML-DSA");
+        var kp = g.generateKeyPair();
+        var msg = "hello".getBytes(StandardCharsets.UTF_8);
+
+        var s = Signature.getInstance("ML-DSA");
+
+        // deterministic
+        s.setParameter(new SignatureParameterSpec(null, null, "deterministic"));
+        s.initSign(kp.getPrivate());
+        s.update(msg);
+        var sig1 = s.sign();
+        s.update(msg);
+        var sig2 = s.sign();
+        // non-deterministic, aka "hedged" as in FIPS 204
+        s.setParameter(new SignatureParameterSpec(null, null));
+        s.update(msg);
+        var sig3 = s.sign();
+        s.update(msg);
+        var sig4 = s.sign();
+
+        Asserts.assertEqualsByteArray(sig1, sig2);
+        Asserts.assertNotEqualsByteArray(sig3, sig4);
+
+        // null context is the same of empty context
+        s.setParameter(new SignatureParameterSpec(null, new byte[0], "deterministic"));
+        s.update(msg);
+        var sig5 = s.sign();
+        Asserts.assertEqualsByteArray(sig1, sig5);
+
+        // Unknown hash algorithm
+        Utils.runAndCheckException(
+                () -> s.setParameter(new SignatureParameterSpec("NOHASH", null)),
+                InvalidAlgorithmParameterException.class);
+        // Unknown feature
+        Utils.runAndCheckException(
+                () -> s.setParameter(new SignatureParameterSpec(null, null, "unknown")),
+                InvalidAlgorithmParameterException.class);
+        // externalMu without internal
+        Utils.runAndCheckException(
+                () -> s.setParameter(new SignatureParameterSpec(null, null, "externalMu")),
+                InvalidAlgorithmParameterException.class);
+    }
+
+    static void paramsTest() throws Exception {
+        new SignatureParameterSpec(null, null);
+        new SignatureParameterSpec("SHA-256", new byte[10], "hello", "good-bye");
+
+        // Just maximum allowed length
+        new SignatureParameterSpec(null, new byte[255]);
+        // context too long
+        Utils.runAndCheckException(
+                () -> new SignatureParameterSpec(null, new byte[256]),
+                IllegalArgumentException.class);
+        // features cannot be null
+        Utils.runAndCheckException(
+                () -> new SignatureParameterSpec(null, null, (String)null),
+                NullPointerException.class);
+        Utils.runAndCheckException(
+                () -> new SignatureParameterSpec(null, null, (String[])null),
+                NullPointerException.class);
+    }
+}
