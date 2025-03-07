cf25e97d4d6ecad951345f0249faca5cba68bbf6
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-03-06 21:49:46 -0500

    Fix bad merge

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PackageTest.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PackageTest.java
@@ -44,6 +44,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiConsumer;
