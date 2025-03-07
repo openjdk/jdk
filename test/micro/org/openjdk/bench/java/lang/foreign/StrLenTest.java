1f569a462be763bf25f4062003999514c5a54d2b
Vladimir <vladimir.a.ivanov@intel.com>
2025-03-07 07:20:14 -0800

    JDK-8350811 [JMH] test foreign.StrLenTest failed with StringIndexOutOfBoundsException for size=451

--- test/micro/org/openjdk/bench/java/lang/foreign/StrLenTest.java
+++ test/micro/org/openjdk/bench/java/lang/foreign/StrLenTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -57,7 +57,7 @@ public class StrLenTest extends CLayouts {
     SegmentAllocator arenaAllocator;
     SlicingPool pool;
 
-    @Param({"5", "20", "100"})
+    @Param({"5", "20", "100", "451"})
     public int size;
     public String str;
 
