29a96c57ac4302b7398e3927faf3ee4e9fccb733
Jiangli Zhou <jianglizhou@google.com>
2025-03-11 12:25:00 -0700

    Simplify get default classlist.

--- src/hotspot/share/cds/metaspaceShared.hpp
+++ src/hotspot/share/cds/metaspaceShared.hpp
@@ -195,6 +195,5 @@ private:
                                      ReservedSpace& class_space_rs);
   static MapArchiveResult map_archive(FileMapInfo* mapinfo, char* mapped_base_address, ReservedSpace rs);
   static void unmap_archive(FileMapInfo* mapinfo);
-  static void get_default_classlist(char* default_classlist, const size_t buf_size);
 };
 #endif // SHARE_CDS_METASPACESHARED_HPP
