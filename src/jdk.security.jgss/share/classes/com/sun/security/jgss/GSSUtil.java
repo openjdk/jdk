3aff705e86307c330febf38ccdaedf828178c02c
Sean Mullan <sean.mullan@oracle.com>
2025-03-11 08:41:04 -0400

    Restore part of second sentence. Move @return to end.

--- src/jdk.security.jgss/share/classes/com/sun/security/jgss/GSSUtil.java
+++ src/jdk.security.jgss/share/classes/com/sun/security/jgss/GSSUtil.java
@@ -43,7 +43,8 @@ public class GSSUtil {
 
     /**
      * Use this method to convert a {@code GSSName} and {@code GSSCredential}
-     * into a {@code Subject}.
+     * into a {@code Subject}. Typically this would be used by a server that
+     * wants to impersonate a client.
      *
      * The elements from the {@code GSSName} are placed in the principals set
      * of this {@code Subject} and those from the {@code GSSCredential} are
@@ -51,9 +52,6 @@ public class GSSUtil {
      * Kerberos specific elements that are added to the subject will be
      * instances of the standard Kerberos implementation classes defined in
      * {@link javax.security.auth.kerberos}.
-     *
-     * @return a {@code Subject} with the entries that contain elements from
-     * the given {@code GSSName} and {@code GSSCredential}.
      *
      * @param principals a {@code GSSName} containing one or more mechanism
      * specific representations of the same entity. These mechanism specific
@@ -65,11 +63,14 @@ public class GSSUtil {
      * specific credentials will be populated in the returned {@code Subject}'s
      * private credential set. Passing in a value of {@code null} will imply
      * that the private credential set should be left empty.
+     *
+     * @return a {@code Subject} with the entries that contain elements from
+     * the given {@code GSSName} and {@code GSSCredential}.
      */
     public static Subject createSubject(GSSName principals,
-                                     GSSCredential credentials) {
+                                        GSSCredential credentials) {
 
-        return  sun.security.jgss.GSSUtil.getSubject(principals,
-                                                     credentials);
+        return sun.security.jgss.GSSUtil.getSubject(principals,
+                                                    credentials);
     }
 }
