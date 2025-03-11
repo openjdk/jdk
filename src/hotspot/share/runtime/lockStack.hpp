0c9d4768fca68cc961b2c1324b3de7d60f28cbdb
Coleen Phillimore <coleen.phillimore@oracle.com>
2025-03-11 13:03:57 +0000

    Fix PRODUCT

--- src/hotspot/share/runtime/lockStack.hpp
+++ src/hotspot/share/runtime/lockStack.hpp
@@ -67,7 +67,7 @@ class LockStack {
   inline JavaThread* get_thread() const;
 
   // Tests if the calling thread is the thread that owns this lock-stack.
-  bool is_owning_thread(JavaThread* current) const PRODUCT_RETURN;
+  bool is_owning_thread(JavaThread* current) const PRODUCT_RETURN_({ return false; })
 
   // Verifies consistency of the lock-stack.
   void verify(const char* msg) const PRODUCT_RETURN;
