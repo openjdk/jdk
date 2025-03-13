e5517c7d14cdfe6571aa8170fffd2fcaf15550bc
dchuyko <dmitry.chuyko@bell-sw.com>
2025-03-13 17:28:48 +0500

    RC_MASK and TC_MASK instead of UMASK

--- src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
+++ src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
@@ -2077,7 +2077,7 @@ public class ForkJoinPool extends AbstractExecutorService {
         else if (deadline - System.currentTimeMillis() >= TIMEOUT_SLOP)
             stat = 0;                       // spurious wakeup
         else if (!compareAndSetCtl(
-                     c, nc = (w.stackPred & LMASK) | (UMASK & (c - TC_UNIT))))
+                     c, nc = (w.stackPred & LMASK) | (c & RC_MASK) | ((c - TC_UNIT) & TC_MASK)))
             stat = -1;                      // lost race to signaller
         else {
             stat = 1;
