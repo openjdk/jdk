3dbfbde692e9356ab30e790d9a355b24b4e11aa8
Renjith Kannath Pariyangad <renjith.kannath.pariyangad@oracle.com>
2025-03-14 16:57:12 +0530

    8334016: Make PrintNullString.java semi-automatic

--- test/jdk/java/awt/print/PrinterJob/PrintNullString.java
+++ test/jdk/java/awt/print/PrinterJob/PrintNullString.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,7 +21,6 @@
  * questions.
  */
 
-import java.awt.Button;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
@@ -47,53 +46,41 @@ import javax.swing.JOptionPane;
  */
 public class PrintNullString extends Frame {
     private static final String INSTRUCTIONS =
-            "This test should print a page which contains the same\n" +
-            "text messages as in the test window on the screen.\n" +
-            "\n" +
-            "The messages should contain only 'OK' and 'expected' messages.\n" +
-            "Press Pass if it's the case; otherwise press Fail.\n" +
-            "\n" +
-            "If the page fails to print, but there were no exceptions\n" +
-            "then the problem is likely elsewhere (i.e. your printer)";
+            "This test will automatically initiate a print\n\n" +
+            "A passing test will print 'OK' and 'expected' messages.\n" +
+            "Test failed if no exception thrown.";
 
     public static void main(String[] args) throws Exception {
         if (PrinterJob.lookupPrintServices().length == 0) {
             throw new RuntimeException("Printer not configured or available.");
         }
 
-        PassFailJFrame.builder()
+        PassFailJFrame passFailJFrame = PassFailJFrame.builder()
                 .instructions(INSTRUCTIONS)
-                .testUI(PrintNullString::new)
                 .rows((int) INSTRUCTIONS.lines().count() + 1)
                 .columns(45)
-                .build()
-                .awaitAndCheck();
+                .build();
+        new PrintNullString();
+        passFailJFrame.awaitAndCheck();
     }
 
     public PrintNullString() {
-        super("PrintNullString");
-
         TextCanvas c = new TextCanvas();
         add("Center", c);
-
-        Button b = new Button("Print");
-        add("South", b);
-        b.addActionListener(e -> {
-            PrinterJob pj = PrinterJob.getPrinterJob();
-            if (pj.printDialog()) {
-                pj.setPrintable(c);
-                try {
-                    pj.print();
-                } catch (PrinterException ex) {
-                    ex.printStackTrace();
-                    String msg = "PrinterException: " + ex.getMessage();
-                    JOptionPane.showMessageDialog(b, msg, "Error occurred",
-                            JOptionPane.ERROR_MESSAGE);
-                    PassFailJFrame.forceFail(msg);
-                }
-            }
-        });
         pack();
+        PrinterJob pj = PrinterJob.getPrinterJob();
+        if (pj.printDialog()) {
+            pj.setPrintable(c);
+            try {
+                pj.print();
+            } catch (PrinterException ex) {
+                ex.printStackTrace();
+                String msg = "PrinterException: " + ex.getMessage();
+                JOptionPane.showMessageDialog(c, msg, "Error occurred",
+                        JOptionPane.ERROR_MESSAGE);
+                PassFailJFrame.forceFail(msg);
+            }
+        }
     }
 
     private static class TextCanvas extends Panel implements Printable {
@@ -102,12 +89,6 @@ public class PrintNullString extends Frame {
         private final AttributedString emptyAttStr = new AttributedString(emptyStr);
         private final AttributedCharacterIterator nullIterator = null;
         private final AttributedCharacterIterator emptyIterator = emptyAttStr.getIterator();
-
-        @Override
-        public void paint(Graphics g) {
-            Graphics2D g2d = (Graphics2D) g;
-            paint(g2d);
-        }
 
         @Override
         public int print(Graphics g, PageFormat pgFmt, int pgIndex) {
@@ -126,7 +107,7 @@ public class PrintNullString extends Frame {
             // API 1: null & empty drawString(String, int, int);
             try {
                 g2d.drawString(nullStr, 20, 40);
-                g2d.drawString("FAILURE: No NPE for null String, int", 20, 40);
+                PassFailJFrame.forceFail("FAILURE: No NPE for null String, int");
             } catch (NullPointerException e) {
                 g2d.drawString("caught expected NPE for null String, int", 20, 40);
             }
@@ -137,7 +118,7 @@ public class PrintNullString extends Frame {
             // API 2: null & empty drawString(String, float, float);
             try {
                 g2d.drawString(nullStr, 20.0f, 80.0f);
-                g2d.drawString("FAILURE: No NPE for null String, float", 20, 80);
+                PassFailJFrame.forceFail("FAILURE: No NPE for null String, float");
             } catch (NullPointerException e) {
                 g2d.drawString("caught expected NPE for null String, float", 20, 80);
             }
@@ -148,14 +129,14 @@ public class PrintNullString extends Frame {
             // API 3: null & empty drawString(Iterator, int, int);
             try {
                 g2d.drawString(nullIterator, 20, 120);
-                g2d.drawString("FAILURE: No NPE for null iterator, int", 20, 120);
+                PassFailJFrame.forceFail("FAILURE: No NPE for null iterator, int");
             } catch (NullPointerException e) {
                 g2d.drawString("caught expected NPE for null iterator, int", 20, 120);
             }
 
             try {
                 g2d.drawString(emptyIterator, 20, 140);
-                g2d.drawString("FAILURE: No IAE for empty iterator, int", 20, 140);
+                PassFailJFrame.forceFail("FAILURE: No IAE for empty iterator, int");
             } catch (IllegalArgumentException e) {
                 g2d.drawString("caught expected IAE for empty iterator, int", 20, 140);
             }
@@ -163,17 +144,18 @@ public class PrintNullString extends Frame {
             // API 4: null & empty drawString(Iterator, float, int);
             try {
                 g2d.drawString(nullIterator, 20.0f, 160.0f);
-                g2d.drawString("FAILURE: No NPE for null iterator, float", 20, 160);
+                PassFailJFrame.forceFail("FAILURE: No NPE for null iterator, float");
             } catch (NullPointerException e) {
                 g2d.drawString("caught expected NPE for null iterator, float", 20, 160);
             }
 
             try {
                 g2d.drawString(emptyIterator, 20.0f, 180.0f);
-                g2d.drawString("FAILURE: No IAE for empty iterator, float", 20, 180);
+                PassFailJFrame.forceFail("FAILURE: No IAE for empty iterator, float");
             } catch (IllegalArgumentException e) {
                 g2d.drawString("caught expected IAE for empty iterator, float", 20, 180);
             }
+            PassFailJFrame.forcePass();
         }
 
         @Override
