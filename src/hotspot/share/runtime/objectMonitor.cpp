20f1bca0770b6b4d935b068e7f6a742cef4f5449
Aleksey Shipilev <shade@openjdk.org>
2025-03-18 10:17:54 +0000

    8351187: Add JFR monitor notification event
    Reviewed-by: dholmes, lmesnik, mgronlun

--- src/hotspot/share/runtime/objectMonitor.cpp
+++ src/hotspot/share/runtime/objectMonitor.cpp
@@ -1955,7 +1955,8 @@ void ObjectMonitor::wait(jlong millis, bool interruptible, TRAPS) {
 // then instead of transferring a thread from the wait_set to the entry_list
 // we might just dequeue a thread from the wait_set and directly unpark() it.
 
-void ObjectMonitor::notify_internal(JavaThread* current) {
+bool ObjectMonitor::notify_internal(JavaThread* current) {
+  bool did_notify = false;
   Thread::SpinAcquire(&_wait_set_lock, "wait_set - notify");
   ObjectWaiter* iterator = dequeue_waiter();
   if (iterator != nullptr) {
@@ -1980,6 +1981,7 @@ void ObjectMonitor::notify_internal(JavaThread* current) {
 
     iterator->_notified = true;
     iterator->_notifier_tid = JFR_THREAD_ID(current);
+    did_notify = true;
     add_to_entry_list(current, iterator);
 
     // _wait_set_lock protects the wait queue, not the entry_list.  We could
@@ -1995,6 +1997,25 @@ void ObjectMonitor::notify_internal(JavaThread* current) {
     }
   }
   Thread::SpinRelease(&_wait_set_lock);
+  return did_notify;
+}
+
+static void post_monitor_notify_event(EventJavaMonitorNotify* event,
+                                      ObjectMonitor* monitor,
+                                      int notified_count) {
+  assert(event != nullptr, "invariant");
+  assert(monitor != nullptr, "invariant");
+  const Klass* monitor_klass = monitor->object()->klass();
+  if (ObjectMonitor::is_jfr_excluded(monitor_klass)) {
+    return;
+  }
+  event->set_monitorClass(monitor_klass);
+  // Set an address that is 'unique enough', such that events close in
+  // time and with the same address are likely (but not guaranteed) to
+  // belong to the same object.
+  event->set_address((uintptr_t)monitor);
+  event->set_notifiedCount(notified_count);
+  event->commit();
 }
 
 // Consider: a not-uncommon synchronization bug is to use notify() when
@@ -2013,9 +2034,15 @@ void ObjectMonitor::notify(TRAPS) {
   if (_wait_set == nullptr) {
     return;
   }
+
+  EventJavaMonitorNotify event;
   DTRACE_MONITOR_PROBE(notify, this, object(), current);
-  notify_internal(current);
-  OM_PERFDATA_OP(Notifications, inc(1));
+  int tally = notify_internal(current) ? 1 : 0;
+  OM_PERFDATA_OP(Notifications, inc(tally));
+
+  if ((tally > 0) && event.should_commit()) {
+    post_monitor_notify_event(&event, this, /* notified_count = */ tally);
+  }
 }
 
 // notifyAll() transfers the waiters one-at-a-time from the waitset to
@@ -2031,14 +2058,20 @@ void ObjectMonitor::notifyAll(TRAPS) {
     return;
   }
 
+  EventJavaMonitorNotify event;
   DTRACE_MONITOR_PROBE(notifyAll, this, object(), current);
   int tally = 0;
   while (_wait_set != nullptr) {
-    tally++;
-    notify_internal(current);
+    if (notify_internal(current)) {
+      tally++;
+    }
   }
 
   OM_PERFDATA_OP(Notifications, inc(tally));
+
+  if ((tally > 0) && event.should_commit()) {
+    post_monitor_notify_event(&event, this, /* notified_count = */ tally);
+  }
 }
 
 void ObjectMonitor::vthread_wait(JavaThread* current, jlong millis) {
