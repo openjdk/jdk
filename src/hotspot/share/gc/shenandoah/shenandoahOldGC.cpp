8a1c85eaa902500d49ca82c67b6838d39cb5b24f
William Kemper <wkemper@openjdk.org>
2025-03-19 16:56:53 +0000

    8350898: Shenandoah: Eliminate final roots safepoint
    Reviewed-by: rkennke, kdnilsen, cslucas

--- src/hotspot/share/gc/shenandoah/shenandoahOldGC.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahOldGC.cpp
@@ -142,7 +142,7 @@ bool ShenandoahOldGC::collect(GCCause::Cause cause) {
   // return from here with weak roots in progress. This is not a valid gc state
   // for any young collections (or allocation failures) that interrupt the old
   // collection.
-  vmop_entry_final_roots();
+  heap->concurrent_final_roots();
 
   // We do not rebuild_free following increments of old marking because memory has not been reclaimed. However, we may
   // need to transfer memory to OLD in order to efficiently support the mixed evacuations that might immediately follow.
