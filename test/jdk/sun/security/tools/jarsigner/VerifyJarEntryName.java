0d53befdc5390cf57d106628373ba28023a5dea6
Hai-May Chao <hai-may.chao@oracle.com>
2025-03-20 11:26:00 -0700

    Convert new test to junit

--- test/jdk/sun/security/tools/jarsigner/VerifyJarEntryName.java
+++ test/jdk/sun/security/tools/jarsigner/VerifyJarEntryName.java
@@ -27,9 +27,13 @@
  * @summary Test that jarsigner -verify emits a warning when the filename of
  *     an entry in the LOC is changed
  * @library /test/lib
+ * @run junit VerifyJarEntryName
  */
 
-import java.io.ByteArrayOutputStream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -39,20 +43,21 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 import jdk.test.lib.SecurityTools;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class VerifyJarEntryName {
 
-    public static void main(String[] args) throws Exception {
-        ByteArrayOutputStream out = new ByteArrayOutputStream(1024);
-        try (ZipOutputStream zos = new ZipOutputStream(out)) {
+    private static final Path JAR_PATH = Path.of("test.jar");
+    private static final Path JAR_PATH1 = Path.of("test1.jar");
+
+    @BeforeAll
+    static void setup() throws Exception {
+        try (FileOutputStream fos = new FileOutputStream(JAR_PATH.toFile());
+             ZipOutputStream zos = new ZipOutputStream(fos)) {
             zos.putNextEntry(new ZipEntry(JarFile.MANIFEST_NAME));
             zos.write("Manifest-Version: 1.0\nCreated-By: Test\n".getBytes(StandardCharsets.UTF_8));
             zos.closeEntry();
         }
-
-        var template = out.toByteArray();
-        Path jarPath = Path.of("test.jar");
-        Files.write(jarPath, template);
 
         SecurityTools.keytool("-genkeypair -keystore ks -storepass changeit "
                 + "-alias mykey -keyalg rsa -dname CN=me ");
@@ -61,32 +66,41 @@ public class VerifyJarEntryName {
                         + "test.jar mykey")
                 .shouldHaveExitValue(0);
 
-        Path jarPath1 = Path.of("test1.jar");
-        Files.copy(jarPath, jarPath1, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(JAR_PATH, JAR_PATH1, StandardCopyOption.REPLACE_EXISTING);
+    }
 
-        // Modify a single byte in "MANIFEST.MF" filename in LOC
-        byte[] signedJar = Files.readAllBytes(jarPath);
-        var jarS = new String(signedJar, StandardCharsets.ISO_8859_1);
+    /*
+     * Modify a single byte in "MANIFEST.MF" filename in LOC, and
+     * validate that jarsigner -verify emits a warning message.
+     */
+    @Test
+    void verifyManifestEntryName() throws Exception {
+        byte[] signedJar = Files.readAllBytes(JAR_PATH);
+        var jarS = new String(signedJar, StandardCharsets.UTF_8);
 
         var manifestPos = jarS.indexOf("MANIFEST.MF");
-        if (manifestPos != -1) {
-            signedJar[manifestPos] = 'X';
-        }
-        Files.write(jarPath, signedJar);
+        assertTrue(manifestPos != -1, "Manifest entry is not present");
+        signedJar[manifestPos] = 'X';
+        Files.write(JAR_PATH, signedJar);
 
         SecurityTools.jarsigner("-verify -verbose test.jar")
                 .shouldContain("Manifest is missing when reading via JarInputStream")
                 .shouldHaveExitValue(0);
+    }
 
-        // Modify a single byte in "MYKEY.SF" filename in LOC
-        byte[] signedJar1 = Files.readAllBytes(jarPath1);
-        var jarS1 = new String(signedJar1, StandardCharsets.ISO_8859_1);
+    /*
+     * Modify a single byte in signature filename in LOC, and
+     * validate that jarsigner -verify emits a warning message.
+     */
+    @Test
+    void verifySignatureEntryName() throws Exception {
+        byte[] signedJar1 = Files.readAllBytes(JAR_PATH1);
+        var jarS1 = new String(signedJar1, StandardCharsets.UTF_8);
 
         var sfPos = jarS1.indexOf("MYKEY.SF");
-        if (sfPos != -1) {
-            signedJar1[sfPos] = 'X';
-        }
-        Files.write(jarPath1, signedJar1);
+        assertTrue(sfPos != -1, "Signature file is not present");
+        signedJar1[sfPos] = 'X';
+        Files.write(JAR_PATH1, signedJar1);
 
         SecurityTools.jarsigner("-verify -verbose test1.jar")
                 .shouldContain("Entries mismatch when comparing JarFile and JarInputStream")
