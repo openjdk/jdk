1622ef50668f6ef29d363d8ae129562c8e2135e7
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-03-21 12:18:58 +0100

    6734168: BasicButtonUI uses wrong FontMetrics to Layout JButtons text

--- src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsButtonUI.java
+++ src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsButtonUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -170,6 +170,10 @@ public class WindowsButtonUI extends BasicButtonUI
     public void paint(Graphics g, JComponent c) {
         if (XPStyle.getXP() != null) {
             WindowsButtonUI.paintXPButtonBackground(g, c);
+        }
+        AbstractButton b = (AbstractButton) c;
+        if (!(b.getFont().equals(g.getFont()))) {
+            b.setFont(g.getFont());
         }
         super.paint(g, c);
     }
