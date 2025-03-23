c04bf9fdf3b1d41b551a00607b5a888544f32559
gerard ziemski <63425797+gerard-ziemski@users.noreply.github.com>
2025-03-23 15:33:44 -0500

    test

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -707,57 +707,140 @@ void* os::realloc(void *memblock, size_t size, MemTag mem_tag) {
 }
 
 void* os::realloc(void *memblock, size_t size, MemTag mem_tag, const NativeCallStack& stack) {
+  //  if (memblock == nullptr) {
+  //    return os::malloc(size, mem_tag, stack);
+  //  }
+  //
+  //  void* rc = nullptr;
+  //  size_t outer_size = os::pre_alloc(&rc, memblock, size, false, mem_tag, stack);
+  //  if (rc != nullptr) {
+  //    return rc;
+  //  }
+  //  if (outer_size == 0) {
+  //    return nullptr;
+  //  }
+  //
+  //  size_t chunk = 0;
+  //  if (MemTracker::enabled()) {
+  //    // Perform integrity checks on and mark the old block as dead *before* calling the real realloc(3)
+  //    // since it may invalidate the old block, including its header.
+  //    MallocHeader* header = MallocHeader::resolve_checked(memblock);
+  //    MallocHeader::FreeInfo free_info = header->free_info();
+  //    if (free_info.size < size) {
+  //      chunk = free_info.size;
+  //      // Observe MallocLimit
+  //      if (MemTracker::check_exceeds_limit(size-free_info.size, mem_tag)) {
+  //        return nullptr;
+  //      }
+  //    }
+  //
+  //    // Perform integrity checks on and mark the old block as dead *before* calling the real realloc(3) since it may invalidate the old block, including its header.
+  //    assert(mem_tag == header->mem_tag(), "weird NMT type mismatch (new:\"%s\" != old:\"%s\")\n",
+  //           NMTUtil::tag_to_name(mem_tag), NMTUtil::tag_to_name(header->mem_tag()));
+  //
+  //    header->mark_block_as_dead();
+  //
+  //    // The real realloc
+  //    ALLOW_C_FUNCTION(::realloc, rc = ::realloc(header, outer_size);)
+  //    if (rc == nullptr) {
+  //      // realloc(3) failed and the block still exists.
+  //      // We have however marked it as dead, revert this change.
+  //      header->revive();
+  //      return nullptr;
+  //    }
+  //    MemTracker::deaccount(free_info);
+  //  } else {
+  //    // NMT disabled.
+  //    ALLOW_C_FUNCTION(::realloc, rc = ::realloc(memblock, size);)
+  //    if (rc == nullptr) {
+  //      return nullptr;
+  //    }
+  //  }
+  //
+  //  return os::post_alloc(rc, size, chunk, mem_tag, stack);
+
+  // Special handling for NMT preinit phase before arguments are parsed
+  void* rc = nullptr;
+  if (NMTPreInit::handle_realloc(&rc, memblock, size, mem_tag)) {
+    return rc;
+  }
+
   if (memblock == nullptr) {
     return os::malloc(size, mem_tag, stack);
   }
 
-  void* rc = nullptr;
-  size_t outer_size = os::pre_alloc(&rc, memblock, size, false, mem_tag, stack);
-  if (rc != nullptr) {
-    return rc;
-  }
-  if (outer_size == 0) {
-    return nullptr;
-  }
+  DEBUG_ONLY(check_crash_protection());
 
-  size_t chunk = 0;
+  // On realloc(p, 0), implementers of realloc(3) have the choice to return either
+  // null or a unique non-null pointer. To unify libc behavior across our platforms
+  // we chose the latter.
+  size = MAX2((size_t)1, size);
+
   if (MemTracker::enabled()) {
-    // Perform integrity checks on and mark the old block as dead *before* calling the real realloc(3)
-    // since it may invalidate the old block, including its header.
-    MallocHeader* header = MallocHeader::resolve_checked(memblock);
-    MallocHeader::FreeInfo free_info = header->free_info();
-    if (free_info.size < size) {
-      chunk = free_info.size;
-      // Observe MallocLimit
-      if (MemTracker::check_exceeds_limit(size-free_info.size, mem_tag)) {
-        return nullptr;
-      }
+    // NMT realloc handling
+
+    const size_t new_outer_size = size + MemTracker::overhead_per_malloc();
+
+    // Handle size overflow.
+    if (new_outer_size < size) {
+      return nullptr;
     }
 
-    // Perform integrity checks on and mark the old block as dead *before* calling the real realloc(3) since it may invalidate the old block, including its header.
+    const size_t old_size = MallocTracker::malloc_header(memblock)->size();
+
+    // Observe MallocLimit
+    if ((size > old_size) && MemTracker::check_exceeds_limit(size - old_size, mem_tag)) {
+      return nullptr;
+    }
+
+    // Perform integrity checks on and mark the old block as dead *before* calling the real realloc(3) since it
+    // may invalidate the old block, including its header.
+    MallocHeader* header = MallocHeader::resolve_checked(memblock);
     assert(mem_tag == header->mem_tag(), "weird NMT type mismatch (new:\"%s\" != old:\"%s\")\n",
            NMTUtil::tag_to_name(mem_tag), NMTUtil::tag_to_name(header->mem_tag()));
+    const MallocHeader::FreeInfo free_info = header->free_info();
 
     header->mark_block_as_dead();
 
-    // The real realloc
-    ALLOW_C_FUNCTION(::realloc, rc = ::realloc(header, outer_size);)
-    if (rc == nullptr) {
+    // the real realloc
+    ALLOW_C_FUNCTION(::realloc, void* const new_outer_ptr = ::realloc(header, new_outer_size);)
+
+    if (new_outer_ptr == nullptr) {
       // realloc(3) failed and the block still exists.
       // We have however marked it as dead, revert this change.
       header->revive();
       return nullptr;
     }
+    // realloc(3) succeeded, variable header now points to invalid memory and we need to deaccount the old block.
     MemTracker::deaccount(free_info);
+
+    // After a successful realloc(3), we account the resized block with its new size
+    // to NMT.
+    void* const new_inner_ptr = MemTracker::record_malloc(new_outer_ptr, size, mem_tag, stack);
+
+#ifdef ASSERT
+    assert(old_size == free_info.size, "Sanity");
+    if (old_size < size) {
+      // We also zap the newly extended region.
+      ::memset((char*)new_inner_ptr + old_size, uninitBlockPad, size - old_size);
+    }
+#endif
+
+    rc = new_inner_ptr;
+
   } else {
+
     // NMT disabled.
     ALLOW_C_FUNCTION(::realloc, rc = ::realloc(memblock, size);)
     if (rc == nullptr) {
       return nullptr;
     }
+    
   }
 
-  return os::post_alloc(rc, size, chunk, mem_tag, stack);
+  DEBUG_ONLY(break_if_ptr_caught(rc);)
+
+  return rc;
 }
 
 void os::free(void *memblock) {
