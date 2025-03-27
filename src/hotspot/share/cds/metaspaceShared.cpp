2eeda64632621c31c6b0a9257ddec7ce9bc5eaec
Calvin Cheung <ccheung@openjdk.org>
2025-03-27 15:34:56 +0000

    8325132: CDS: Make sure the ArchiveRelocationMode is always printed in the log
    Reviewed-by: shade, iklam

--- src/hotspot/share/cds/metaspaceShared.cpp
+++ src/hotspot/share/cds/metaspaceShared.cpp
@@ -1134,6 +1134,8 @@ void MetaspaceShared::initialize_runtime_shared_and_meta_spaces() {
     log_info(cds)("Core region alignment: %zu", static_mapinfo->core_region_alignment());
     dynamic_mapinfo = open_dynamic_archive();
 
+    log_info(cds)("ArchiveRelocationMode: %d", ArchiveRelocationMode);
+
     // First try to map at the requested address
     result = map_archives(static_mapinfo, dynamic_mapinfo, true);
     if (result == MAP_ARCHIVE_MMAP_FAILURE) {
