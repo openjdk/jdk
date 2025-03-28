40f9fbfa96d2fd1f4dfa407d9a91f2930064d4be
Saranya Natarajan <saranya.natarajan@oracle.com>
2025-03-28 14:00:44 +0100

    adding comment

--- src/hotspot/share/opto/memnode.hpp
+++ src/hotspot/share/opto/memnode.hpp
@@ -133,7 +133,10 @@ public:
   // (Return -1 if unknown.)
   virtual int store_Opcode() const { return -1; }
 
-  // What is the type of the value in memory?  (T_VOID mean "unspecified".)
+  // Return type is the property of the value that is loaded/stored 
+  // and not the memory that is accessed. For instance, in case of 
+  // mismatch a value of type 'short' can be stored into an array 
+  // with elements of type 'long'.
   virtual BasicType value_basic_type() const = 0;
   virtual int memory_size() const {
 #ifdef ASSERT
