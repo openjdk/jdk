cf4c59f4f9c8d80027d18ba90a8b8c2655b6339d
Vladimir <vladimir.a.ivanov@intel.com>
2025-03-28 16:17:19 -0700

    8346230: [perf] scalability issue for the specjvm2008::xml.transform workload

--- src/java.xml/share/classes/com/sun/org/apache/xml/internal/utils/XMLReaderManager.java
+++ src/java.xml/share/classes/com/sun/org/apache/xml/internal/utils/XMLReaderManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
  */
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
@@ -37,7 +37,7 @@ import org.xml.sax.XMLReader;
  * Creates XMLReader objects and caches them for re-use.
  * This class follows the singleton pattern.
  *
- * @LastModified: Nov 2024
+ * @LastModified: Mar 2025
  */
 public class XMLReaderManager {
 
@@ -109,7 +109,6 @@ public class XMLReaderManager {
         boolean threadHasReader = (rw != null);
         reader = threadHasReader ? rw.reader : null;
         String factory = System.getProperty(property);
-
         if (threadHasReader &&
                 (rw.overrideDefaultParser == m_overrideDefaultParser) &&
                 ( factory == null || reader.getClass().getName().equals(factory))) {
