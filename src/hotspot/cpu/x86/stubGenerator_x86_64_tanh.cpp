e563fd73c2f86c34a47130ab6946f69320c60439
Mohamed Issa <mohamed.issa@intel.com>
2025-03-27 17:15:03 -0700

    Change tanh intrinsic endpoint comparison value to match reference OpenJDK implementation

--- src/hotspot/cpu/x86/stubGenerator_x86_64_tanh.cpp
+++ src/hotspot/cpu/x86/stubGenerator_x86_64_tanh.cpp
@@ -46,7 +46,7 @@
 //                      for |x| in [23/64,3*2^7)
 // e^{-2*|x|}=2^{-k-f}*2^{-r} ~ 2^{-k}*(Tn+Dn)*(1+p)=(T0+D0)*(1+p)
 //
-// For |x| in [2^{-4},20):
+// For |x| in [2^{-4},22):
 //         2^{-r}-1 ~ p=c1*r+c2*r^2+..+c5*r^5
 //      Let R=1/(1+T0+p*T0), truncated to 35 significant bits
 //  R=1/(1+T0+D0+p*(T0+D0))*(1+eps), |eps|<2^{-33}
@@ -66,7 +66,7 @@
 //
 // For |x|<2^{-64}:  x is returned
 //
-// For |x|>=20: return +/-1
+// For |x|>=22: return +/-1
 //
 // Special cases:
 //  tanh(NaN) = quiet NaN, and raise invalid exception
@@ -328,8 +328,8 @@ address StubGenerator::generate_libmTanh() {
   __ movl(rdx, 32768);
   __ andl(rdx, rcx);
   __ andl(rcx, 32767);
-  __ cmpl(rcx, 16436);
-  __ jcc(Assembler::aboveEqual, L_2TAG_PACKET_2_0_1); // Branch only if |x| >= 20
+  __ cmpl(rcx, 16438);
+  __ jcc(Assembler::aboveEqual, L_2TAG_PACKET_2_0_1); // Branch only if |x| >= 22
   __ movsd(xmm3, ExternalAddress(HALFMASK), r11 /*rscratch*/);
   __ xorpd(xmm4, xmm4);
   __ movsd(xmm1, ExternalAddress(L2E), r11 /*rscratch*/);
@@ -341,8 +341,8 @@ address StubGenerator::generate_libmTanh() {
   __ andnpd(xmm4, xmm0);
   __ pshufd(xmm5, xmm4, 68);
   __ subl(rcx, 16304);
-  __ cmpl(rcx, 132);
-  __ jcc(Assembler::aboveEqual, L_2TAG_PACKET_0_0_1); // Branch only if |x| is not in [2^{-4},20)
+  __ cmpl(rcx, 134);
+  __ jcc(Assembler::aboveEqual, L_2TAG_PACKET_0_0_1); // Branch only if |x| is not in [2^{-4},22)
   __ subsd(xmm4, xmm3);
   __ mulsd(xmm3, xmm1);
   __ mulsd(xmm2, xmm5);
@@ -429,7 +429,7 @@ address StubGenerator::generate_libmTanh() {
 
   __ bind(L_2TAG_PACKET_0_0_1);
   __ addl(rcx, 960);
-  __ cmpl(rcx, 1092);
+  __ cmpl(rcx, 1094);
   __ jcc(Assembler::aboveEqual, L_2TAG_PACKET_1_0_1); // Branch only if |x| not in [2^{-64}, 2^{-4})
   __ movdqu(xmm2, ExternalAddress(pv), r11 /*rscratch*/);
   __ pshufd(xmm1, xmm0, 68);
