58cc444e69adead98c25a5823860812ac328081d
Artur Barashev <artur.barashev@oracle.com>
2025-03-31 09:59:10 -0400

    8272875: Change the default key manager to PKIX

--- src/java.base/share/classes/javax/net/ssl/KeyManagerFactory.java
+++ src/java.base/share/classes/javax/net/ssl/KeyManagerFactory.java
@@ -64,7 +64,7 @@ public class KeyManagerFactory {
     public static final String getDefaultAlgorithm() {
         String type = Security.getProperty("ssl.KeyManagerFactory.algorithm");
         if (type == null) {
-            type = "SunX509";
+            type = "NewSunX509";
         }
         return type;
     }
