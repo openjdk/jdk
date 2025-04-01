fc22cbfe2835ef510163d840b41b3036c126924a
Man Cao <manc@google.com>
2025-04-01 08:29:36 +0000

    Add two tests

---
+++ test/hotspot/jtreg/gc/g1/TestSoftMaxHeapSize.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2025, Google LLC. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package gc.g1;
+
+/*
+ * @test
+ * @bug 8236073
+ * @requires vm.gc.G1 & vm.opt.ExplicitGCInvokesConcurrent != true
+ * @library /test/lib
+ * @run main/othervm -Xmx200m -XX:MinHeapSize=4m -XX:MinHeapFreeRatio=99
+        -XX:MaxHeapFreeRatio=99 gc.g1.TestSoftMaxHeapSize
+ * @summary Test that SoftMaxHeapSize could limit G1's heap size when resizing.
+ */
+
+import java.lang.management.ManagementFactory;
+import java.util.LinkedList;
+import java.util.List;
+import jdk.test.lib.dcmd.PidJcmdExecutor;
+
+public class TestSoftMaxHeapSize {
+
+  private static final int OBJECT_SIZE = 1000;
+  private static final long ALLOCATED_BYTES = 20_000_000; // About 20M
+  private static final long MAX_HEAP_SIZE =
+      200 * 1024 * 1024; // 200MiB, must match -Xmx on command line.
+
+  private static final long SOFT_MAX_HEAP =
+      30 * 1024 * 1024; // 30MiB, leaving ~10MiB headroom above ALLOCATED_BYTES.
+
+  private static final List<byte[]> holder = new LinkedList<>();
+
+  private static long getCurrentHeapSize() {
+    return ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getCommitted();
+  }
+
+  public static void main(String[] args) throws Exception {
+
+    long count = ALLOCATED_BYTES / OBJECT_SIZE;
+    for (long i = 0; i < count; ++i) {
+      holder.add(new byte[OBJECT_SIZE]);
+    }
+
+    System.gc();
+    long heapSize = getCurrentHeapSize();
+    if (heapSize != MAX_HEAP_SIZE) {
+      throw new RuntimeException(
+          "Heap size did not fully expand to Xmx after full GC: heapSize = " + heapSize);
+    }
+
+    PidJcmdExecutor jcmd = new PidJcmdExecutor();
+    jcmd.execute("VM.set_flag SoftMaxHeapSize " + SOFT_MAX_HEAP, true);
+
+    System.gc();
+    heapSize = getCurrentHeapSize();
+    if (heapSize != SOFT_MAX_HEAP) {
+      throw new RuntimeException(
+          "Heap size did not shrink to SoftMaxHeapSize after full GC: heapSize = " + heapSize);
+    }
+  }
+}
---
+++ test/hotspot/jtreg/gc/g1/TestSoftMaxHeapSizeNoOOM.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2025, Google LLC. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+package gc.g1;
+
+/*
+ * @test id=normal
+ * @bug 8236073 8352765
+ * @requires vm.gc.G1
+ * @library /test/lib
+ * @run main/othervm -Xmx100m -XX:MinHeapSize=4m -XX:SoftMaxHeapSize=4m
+        gc.g1.TestSoftMaxHeapSizeNoOOM
+ * @summary Test that setting TestSoftMaxHeapSize to a small value won't
+ *          trigger OutOfMemoryError for normal allocations.
+ */
+
+/*
+ * @test id=humongous
+ * @bug 8236073 8352765
+ * @requires vm.gc.G1
+ * @library /test/lib
+ * @run main/othervm -Xmx100m -XX:MinHeapSize=4m -XX:SoftMaxHeapSize=4m
+        -Dhumongous=true gc.g1.TestSoftMaxHeapSizeNoOOM
+ * @summary Test that setting TestSoftMaxHeapSize to a small value won't
+ *          trigger OutOfMemoryError for humongous allocations.
+ */
+
+import java.util.ArrayList;
+
+public class TestSoftMaxHeapSizeNoOOM {
+
+  private static final long ALLOCATED_BYTES = 20_000_000; // About 20M
+  private static final int OBJECT_SIZE = 1000;
+  private static final int ITERATIONS = 100000;
+  private static final int HUMONGOUS_OBJECT_SIZE = 1_500_000; // About 1.5M
+  private static final int HUMONGOUS_ITERATIONS = 1000;
+
+  private static final ArrayList<byte[]> holder = new ArrayList<>();
+
+  private static void work(int objSize, int iterations) {
+    long count = ALLOCATED_BYTES / objSize;
+    for (long i = 0; i < count; ++i) {
+      holder.add(new byte[objSize]);
+    }
+    // Mutate old objects while allocating new objects.
+    // This is effective to trigger concurrent collections for G1,
+    // and is necessary to reproduce OutOfMemoryError in JDK-8352765.
+    for (long i = 0; i < iterations; ++i) {
+      holder.remove(0);
+      holder.add(new byte[objSize]);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (Boolean.getBoolean("humongous")) {
+      work(HUMONGOUS_OBJECT_SIZE, HUMONGOUS_ITERATIONS);
+    } else {
+      work(OBJECT_SIZE, ITERATIONS);
+    }
+  }
+}
