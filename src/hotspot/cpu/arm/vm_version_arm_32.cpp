9a8f62005e57d7103f2b76eaea9936fc29c548bc
Vladimir Ivanov <vladimir.x.ivanov@oracle.com>
2025-04-07 15:59:18 -0700

    features_string -> cpu_info_string

--- src/hotspot/cpu/arm/vm_version_arm_32.cpp
+++ src/hotspot/cpu/arm/vm_version_arm_32.cpp
@@ -295,7 +295,7 @@ void VM_Version::initialize() {
                (has_multiprocessing_extensions() ? ", mp_ext" : ""));
 
   // buf is started with ", " or is empty
-  _features_string = os::strdup(buf);
+  _cpu_info_string = os::strdup(buf);
 
   if (has_simd()) {
     if (FLAG_IS_DEFAULT(UsePopCountInstruction)) {
@@ -363,6 +363,6 @@ void VM_Version::initialize_cpu_information(void) {
   _no_of_threads = _no_of_cores;
   _no_of_sockets = _no_of_cores;
   snprintf(_cpu_name, CPU_TYPE_DESC_BUF_SIZE - 1, "ARM%d", _arm_arch);
-  snprintf(_cpu_desc, CPU_DETAILED_DESC_BUF_SIZE, "%s", _features_string);
+  snprintf(_cpu_desc, CPU_DETAILED_DESC_BUF_SIZE, "%s", _cpu_info_string);
   _initialized = true;
 }
