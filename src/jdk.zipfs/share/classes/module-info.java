4302846bdca7ede743c0570570ac8a8da368457e
David Beaumont <david.beaumont@oracle.com>
2025-04-09 13:54:12 +0200

    Rewording module info.
    * Rewording module info.

--- src/jdk.zipfs/share/classes/module-info.java
+++ src/jdk.zipfs/share/classes/module-info.java
@@ -266,40 +266,40 @@ import java.util.Set;
  *   </td>
  * </tr>
  * <tr>
- *   <th scope="row">accessPermission</th>
+ *   <th scope="row">accessMode</th>
  *   <td>{@link java.lang.String}</td>
  *   <td>null/unset</td>
  *   <td>
- *       A value defining the desired read-write access permission of the file
- *       system(either <em>read-write</em> or <em>read-only</em>).
+ *       A value defining the desired read/write access mode of the file system
+ *       (either <em>read-write</em> or <em>read-only</em>).
  *       <p>
  *       Even if a zip file system is writable ({@code fs.isReadOnly() == false}),
  *       this says nothing about whether individual files can be created or
  *       modified, simply that it might be possible.
  *       <ul>
  *           <li>
- *               If the value is {@code "default"}, or the property is not set,
- *               the file system will be created <em>read-write</em> if possible.
- *               Use {@link java.nio.file.FileSystem#isReadOnly() isReadOnly()}
- *               to determine the actual access permission.
+ *               If no value is set, the file system is created <em>read-write</em>
+ *               if possible. Use {@link java.nio.file.FileSystem#isReadOnly()
+ *               isReadOnly()} to determine the actual access mode.
  *           </li>
  *           <li>
- *               If the value is {@code "readOnly"}, the file system will be
+ *               If the value is {@code "readOnly"}, the file system is created
  *               <em>read-only</em>, and {@link java.nio.file.FileSystem#isReadOnly()
  *               isReadOnly()} will always return {@code true}.
  *           </li>
  *           <li>
- *               If the value is {@code "readWrite"}, the file system will be
+ *               If the value is {@code "readWrite"}, the file system is created
  *               <em>read-write</em>, and {@link java.nio.file.FileSystem#isReadOnly()
  *               isReadOnly()} will always return {@code false}. If a writable file
- *               system cannot be created for the source ZIP file, an {@code
- *               IllegalArgumentException} will be thrown.
+ *               system cannot be created, an {@code IOException} will be thrown.
  *           </li>
  *           <li>
  *               Any other values will cause an {@code IllegalArgumentException}
  *               to be thrown.
  *           </li>
  *       </ul>
+ *       The access mode has no effect on reported Posix file permissions (in cases
+ *       where Posix support is enabled).
  *   </td>
  * </tr>
  * </tbody>
