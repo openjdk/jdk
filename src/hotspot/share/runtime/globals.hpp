bcbd65e9bcb10a7c7f15d9ee042108d93d6761df
tstuefe <thomas.stuefe@gmail.com>
2025-04-10 07:12:24 +0200

    remove UseKLUT completely

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1400,9 +1400,6 @@ const int ObjectAlignmentInBytes = 8;
           "Force the class space to be allocated at this address or "       \
           "fails VM initialization (requires -Xshare=off.")                 \
                                                                             \
-  product(bool, UseKLUT, false, EXPERIMENTAL,                               \
-          "Only for coh")                                                   \
-                                                                            \
   product(bool, PrintKLUTStatistics, false,                                 \
           "Print KLUT statistics at exit")                                  \
                                                                             \
