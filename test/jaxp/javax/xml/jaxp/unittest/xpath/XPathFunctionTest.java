72c138f01b4a5ea5190367b18114ee06456f8f24
JoeWang-Java <huizhe.wang@oracle.com>
2025-04-12 00:04:24 +0000

    add copyright and docs

--- test/jaxp/javax/xml/jaxp/unittest/xpath/XPathFunctionTest.java
+++ test/jaxp/javax/xml/jaxp/unittest/xpath/XPathFunctionTest.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package xpath;
 
 import java.util.List;
@@ -60,7 +82,8 @@ public class XPathFunctionTest {
      * @param propertyValue the property value
      * @param expected the expected result
      * @param expectedType the expected throw type
-     * @throws Exception if the test fails other than the expected Exception
+     * @throws Exception if the test fails other than the expected Exception, which
+     * would indicate an issue in configuring the test
      */
     @ParameterizedTest
     @MethodSource("testData")
@@ -122,6 +145,7 @@ public class XPathFunctionTest {
         return null;
     }
 
+    // XPathFunctionResolver customized for the FindFunction
     class FunctionResolver implements XPathFunctionResolver {
         private final Document doc;
 
@@ -139,6 +163,7 @@ public class XPathFunctionTest {
         }
     }
 
+    // The Find function
     class FindFunction implements XPathFunction {
         private final Document doc;
 
