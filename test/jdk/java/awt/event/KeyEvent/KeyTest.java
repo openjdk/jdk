bb98276657abef0fe57019b96ac4b1bdc5ed0116
Alexander Zuev <alexander.zuev@oracle.com>
2025-04-12 21:37:08 -0700

    Correct library path.

--- test/jdk/java/awt/event/KeyEvent/KeyTest.java
+++ test/jdk/java/awt/event/KeyEvent/KeyTest.java
@@ -26,7 +26,7 @@
  * @bug 4151419, 4090870, 4169733
  * @summary Ensures that KeyEvent has right results for the following
  *          keys  -=\[];,./
- * @library /open/test/jdk/java/awt/regtesthelpers
+ * @library /java/awt/regtesthelpers
  * @build PassFailJFrame
  * @run main/manual KeyTest
  */
