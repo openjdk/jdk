4fa4ef905c7508444293b5d36f5836edbea27a5f
Matthias Bläsing <mblaesing@doppel-helix.eu>
2025-04-14 19:15:30 +0200

    Adress review comments: Update copyright years and fix whitespace in if condition

--- src/java.desktop/windows/classes/sun/awt/windows/WClipboard.java
+++ src/java.desktop/windows/classes/sun/awt/windows/WClipboard.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -129,7 +129,7 @@ final class WClipboard extends SunClipboard {
 
     @Override
     public void closeClipboard() {
-        if(clipboardLocked.isLocked()) {
+        if (clipboardLocked.isLocked()) {
             try {
                 closeClipboard0();
             } finally {
