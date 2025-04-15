87a4df05ba4c06726db2ef8aefc20b798d97ebca
Damon Nguyen <damon.nguyen@oracle.com>
2025-04-15 12:33:12 -0700

    Update to setPreferredSize

--- test/jdk/javax/swing/JSplitPane/bug4749792.java
+++ test/jdk/javax/swing/JSplitPane/bug4749792.java
@@ -49,17 +49,17 @@ public class bug4749792 {
         frame.setSize(450, 220);
         JPanel left = new JPanel();
         JPanel right = new JPanel();
-        left.setSize(200, 200);
-        right.setSize(200, 200);
+        left.setPreferredSize(new Dimension(200, 200));
+        right.setPreferredSize(new Dimension(200, 200));
         JSplitPane sp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, left, right);
         frame.add(sp);
 
         JPanel south = new JPanel();
-        south.setSize(new Dimension(20, 20));
+        south.setPreferredSize(new Dimension(20, 20));
         frame.add(south, BorderLayout.SOUTH);
 
         JPanel east = new JPanel();
-        east.setSize(new Dimension(20, 20));
+        east.setPreferredSize(new Dimension(20, 20));
         frame.add(east, BorderLayout.EAST);
 
         return frame;
