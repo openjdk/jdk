d41c32cdc9452bc4baa7aed6b3ee42cd84e1d1f3
Alex Menkov <alexey.menkov@oracle.com>
2025-04-15 16:26:22 -0700

    tests

--- test/hotspot/jtreg/runtime/NMT/MallocSiteTypeChange.java
+++ test/hotspot/jtreg/runtime/NMT/MallocSiteTypeChange.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2019, Red Hat, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -32,8 +33,6 @@
  * @run main/othervm -Xbootclasspath/a:. -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -XX:NativeMemoryTracking=detail MallocSiteTypeChange
  */
 
-import jdk.test.lib.JDKToolFinder;
-import jdk.test.lib.process.ProcessTools;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.whitebox.WhiteBox;
 
@@ -42,26 +41,19 @@ public class MallocSiteTypeChange {
         OutputAnalyzer output;
         WhiteBox wb = WhiteBox.getWhiteBox();
 
-        // Grab my own PID
-        String pid = Long.toString(ProcessTools.getProcessId());
-        ProcessBuilder pb = new ProcessBuilder();
-
         int pc = 1;
         long addr = wb.NMTMallocWithPseudoStack(4 * 1024, pc);
 
         // Verify that current tracking level is "detail"
-        pb.command(new String[] { JDKToolFinder.getJDKTool("jcmd"), pid, "VM.native_memory", "detail"});
-        output = new OutputAnalyzer(pb.start());
+        output = NMTTestUtils.startJcmdVMNativeMemory("detail");
         output.shouldContain("Test (reserved=4KB, committed=4KB)");
 
-        pb.command(new String[] { JDKToolFinder.getJDKTool("jcmd"), pid, "VM.native_memory", "baseline"});
-        output = new OutputAnalyzer(pb.start());
+        output = NMTTestUtils.startJcmdVMNativeMemory("baseline");
         output.shouldContain("Baseline taken");
 
         wb.NMTFree(addr);
         addr = wb.NMTMallocWithPseudoStackAndType(2 * 1024, pc, 9 /* mtInternal */ );
-        pb.command(new String[] { JDKToolFinder.getJDKTool("jcmd"), pid, "VM.native_memory", "detail.diff"});
-        output = new OutputAnalyzer(pb.start());
+        output = NMTTestUtils.startJcmdVMNativeMemory("detail.diff");
         output.shouldContain("(malloc=0KB type=Test -4KB)");
         output.shouldContain("(malloc=2KB type=Internal +2KB #1 +1)");
         output.shouldHaveExitValue(0);
