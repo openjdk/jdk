87e652bef7a2f621d471beff772c0d576dbde197
Johannes Graham <j3graham@gmail.com>
2025-04-16 11:53:51 -0400

    reflective test

--- test/jdk/java/text/Format/DecimalFormat/CloneTest.java
+++ test/jdk/java/text/Format/DecimalFormat/CloneTest.java
@@ -25,25 +25,89 @@
  * @test
  * @bug 8354522
  * @summary Check for cloning interference
- * @run junit CloneTest
+ * @run junit/othervm --add-opens=java.base/java.text=ALL-UNNAMED CloneTest
  */
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CloneTest {
+    private static Field DIGIT_LIST_FIELD;
+    private static Class<?> DIGIT_LIST_CLASS;
+
+    @BeforeAll
+    static void setup() throws Exception {
+        DIGIT_LIST_FIELD = DecimalFormat.class.getDeclaredField("digitList");
+        DIGIT_LIST_FIELD.setAccessible(true);
+
+        DecimalFormat df = new DecimalFormat();
+        Object digitList = DIGIT_LIST_FIELD.get(df);
+
+        DIGIT_LIST_CLASS = digitList.getClass();
+    }
 
     // Tests that when DecimalFormat is cloned after use with
     // a long double/BigDecimal, clones will be independent. This is not an
     // exhaustive test. This tests for the issue of the same DigitList.data
     // array being reused across clones of DecimalFormat.
+
+    @Test
+    public void testClone() throws Exception {
+        DecimalFormat df = new DecimalFormat("#");
+        assertCloneValidity(df);
+    }
+
+    @Test
+    public void testCloneAfterInit() throws Exception {
+        DecimalFormat df = new DecimalFormat("#");
+
+        // This initial use of the formatter initialises its internal state, which could
+        // subsequently be shared across clones. This is key to reproducing this specific
+        // issue.
+        String _ = df.format(Math.PI);
+        assertCloneValidity(df);
+    }
+
+    private static void assertCloneValidity(DecimalFormat df) throws Exception {
+        DecimalFormat dfClone = (DecimalFormat) df.clone();
+
+        Object digits = valFromDigitList(df, "digits");
+        assertNotSame(digits, valFromDigitList(dfClone, "digits"));
+
+
+        Object data = valFromDigitList(df, "data");
+        if (data != null) {
+            assertNotSame(data, valFromDigitList(dfClone, "data"));
+        }
+
+        Object tempBuilder = valFromDigitList(df, "tempBuilder");
+        if (tempBuilder != null) {
+            assertNotSame(data, valFromDigitList(dfClone, "data"));
+        }
+
+        assertEquals(DIGIT_LIST_FIELD.get(df), DIGIT_LIST_FIELD.get(dfClone));
+    }
+
+    private static Object valFromDigitList(DecimalFormat df, String fieldName) {
+        try {
+            Object digitList = DIGIT_LIST_FIELD.get(df);
+            Field field = DIGIT_LIST_CLASS.getDeclaredField(fieldName);
+            field.setAccessible(true);
+
+            return field.get(digitList);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Test
     public void testCloneIndependence() {
