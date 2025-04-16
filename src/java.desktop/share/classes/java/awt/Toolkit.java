194fc044dcc3fbcb18b2c2424d81a2b8f5d01723
Andrey Turbanov <turbanoff@gmail.com>
2025-04-16 17:45:47 +0300

    [PATCH] Avoid redundant WeakHashMap.get to obtain old value in Toolkit

--- src/java.desktop/share/classes/java/awt/Toolkit.java
+++ src/java.desktop/share/classes/java/awt/Toolkit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1472,8 +1472,7 @@ public abstract class Toolkit {
         Object oldValue;
 
         synchronized (this) {
-            oldValue = desktopProperties.get(name);
-            desktopProperties.put(name, newValue);
+            oldValue = desktopProperties.put(name, newValue);
         }
 
         // Don't fire change event if old and new values are null.
