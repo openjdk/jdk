e692363d1fedd32efcea2217d021bbba5972315a
erik_gahlin <erik.gahlin@oracle.com>
2025-04-17 16:06:28 +0200

    Initial

--- src/jdk.jfr/share/classes/jdk/jfr/internal/util/Bytecode.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/util/Bytecode.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -74,6 +74,8 @@ public final class Bytecode {
             MethodTypeDesc mtd = MethodTypeDesc.of(returnDesc, parameterDesc);
             return new MethodDesc(methodName, mtd);
         }
+    }
+    public record SettingDesc(ClassDesc paramType, String methodName) {
     }
 
     public static ClassDesc classDesc(ValueDescriptor v) {
