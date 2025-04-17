5e1df1a54238eae1e0cfdbcc7103e11ee852e851
Emanuel Peter <emanuel.peter@oracle.com>
2025-04-17 14:58:15 +0200

    fix up TestAliasing.java

--- test/hotspot/jtreg/compiler/loopopts/superword/TestAliasing.java
+++ test/hotspot/jtreg/compiler/loopopts/superword/TestAliasing.java
@@ -23,14 +23,15 @@
 
 package compiler.loopopts.superword;
 
-import compiler.lib.ir_framework.*;
 import jdk.test.lib.Utils;
-import jdk.test.whitebox.WhiteBox;
-import java.lang.reflect.Array;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Random;
-import java.nio.ByteOrder;
+
+import compiler.lib.ir_framework.*;
+import compiler.lib.verify.*;
+import static compiler.lib.generators.Generators.G;
+import compiler.lib.generators.Generator;
 
 /*
  * @test
@@ -48,32 +49,27 @@ import java.nio.ByteOrder;
  */
 
 public class TestAliasing {
-    static int RANGE = 1024*8;
-    static int RANGE_FINAL = 1024*8;
+    static int SIZE = 1024*8;
+    static int SIZE_FINAL = 1024*8;
     private static final Random RANDOM = Utils.getRandomInstance();
+    private static final Generator INT_GEN = G.ints();
 
-    // Inputs
-    byte[] aB;
-    byte[] bB;
-    byte mB = (byte)31;
-    short[] aS;
-    short[] bS;
-    short mS = (short)0xF0F0;
-    int[] aI;
-    int[] bI;
-    int mI = 0xF0F0F0F0;
-    long[] aL;
-    long[] bL;
-    long mL = 0xF0F0F0F0F0F0F0F0L;
+    // Original data.
+    public static int[] ORIG_AI = fillRandom(new int[SIZE]);
+    public static int[] ORIG_BI = fillRandom(new int[SIZE]);
+
+    // The data we use in the tests. It is initialized from ORIG_* every time.
+    public static int[] AI = new int[SIZE];
+    public static int[] BI = new int[SIZE];
 
     // List of tests
     Map<String,TestFunction> tests = new HashMap<String,TestFunction>();
 
     // List of gold, the results from the first run before compilation
-    Map<String,Object[]> golds = new HashMap<String,Object[]>();
+    Map<String,Object> golds = new HashMap<String,Object>();
 
     interface TestFunction {
-        Object[] run();
+        Object run();
     }
 
     public static void main(String[] args) {
@@ -93,30 +89,34 @@ public class TestAliasing {
     }
 
     public TestAliasing() {
-        // Generate input once
-        aB = generateB();
-        bB = generateB();
-        aS = generateS();
-        bS = generateS();
-        aI = generateI();
-        bI = generateI();
-        aL = generateL();
-        bL = generateL();
-
         // Add all tests to list
-        tests.put("test4a",      () -> { return test4a(aS.clone(), bS.clone()); });
-        tests.put("test4a_alias",() -> { short[] x = aS.clone(); return test4a_alias(x, x); });
+        tests.put("test4a",      () -> { return test4a(AI, BI); });
+        tests.put("test4a_alias",() -> { return test4a_alias(AI, AI); });
         // TODO: remove old tests, add new ones.
         //       Especially also the not-working one from the benchmark.
-        //       And maybe fix up generation and verification too!
 
         // Compute gold value for all test methods before compilation
         for (Map.Entry<String,TestFunction> entry : tests.entrySet()) {
             String name = entry.getKey();
             TestFunction test = entry.getValue();
-            Object[] gold = test.run();
+            init();
+            test.run();
+            Object gold = snapshotCopy();
             golds.put(name, gold);
         }
+    }
+
+    public static void init() {
+        System.arraycopy(ORIG_AI, 0, AI, 0, SIZE);
+        System.arraycopy(ORIG_BI, 0, BI, 0, SIZE);
+    }
+
+    public static Object snapshotCopy() {
+        return new Object[] { AI.clone(), BI.clone() };
+    }
+
+    public static Object snapshot() {
+        return new Object[] { AI, BI };
     }
 
     @Warmup(100)
@@ -127,123 +127,23 @@ public class TestAliasing {
             String name = entry.getKey();
             TestFunction test = entry.getValue();
             // Recall gold value from before compilation
-            Object[] gold = golds.get(name);
+            Object gold = golds.get(name);
             // Compute new result
-            Object[] result = test.run();
+            init();
+            test.run();
+            Object result = snapshot();
             // Compare gold and new result
-            verify(name, gold, result);
+            try {
+                Verify.checkEQ(gold, result);
+            } catch (VerifyException e) {
+                throw new RuntimeException("Verify failed for " + name, e);
+            }
         }
     }
 
-    static byte[] generateB() {
-        byte[] a = new byte[RANGE];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = (byte)RANDOM.nextInt();
-        }
+    static int[] fillRandom(int[] a) {
+        G.fill(INT_GEN, a);
         return a;
-    }
-
-    static short[] generateS() {
-        short[] a = new short[RANGE];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = (short)RANDOM.nextInt();
-        }
-        return a;
-    }
-
-    static int[] generateI() {
-        int[] a = new int[RANGE];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = RANDOM.nextInt();
-        }
-        return a;
-    }
-
-    static long[] generateL() {
-        long[] a = new long[RANGE];
-        for (int i = 0; i < a.length; i++) {
-            a[i] = RANDOM.nextLong();
-        }
-        return a;
-    }
-
-    static void verify(String name, Object[] gold, Object[] result) {
-        if (gold.length != result.length) {
-            throw new RuntimeException("verify " + name + ": not the same number of outputs: gold.length = " +
-                                       gold.length + ", result.length = " + result.length);
-        }
-        for (int i = 0; i < gold.length; i++) {
-            Object g = gold[i];
-            Object r = result[i];
-            if (g.getClass() != r.getClass() || !g.getClass().isArray() || !r.getClass().isArray()) {
-                throw new RuntimeException("verify " + name + ": must both be array of same type:" +
-                                           " gold[" + i + "].getClass() = " + g.getClass().getSimpleName() +
-                                           " result[" + i + "].getClass() = " + r.getClass().getSimpleName());
-            }
-            if (g == r) {
-                throw new RuntimeException("verify " + name + ": should be two separate arrays (with identical content):" +
-                                           " gold[" + i + "] == result[" + i + "]");
-            }
-            if (Array.getLength(g) != Array.getLength(r)) {
-                    throw new RuntimeException("verify " + name + ": arrays must have same length:" +
-                                           " gold[" + i + "].length = " + Array.getLength(g) +
-                                           " result[" + i + "].length = " + Array.getLength(r));
-            }
-            Class c = g.getClass().getComponentType();
-            if (c == byte.class) {
-                verifyB(name, i, (byte[])g, (byte[])r);
-            } else if (c == short.class) {
-                verifyS(name, i, (short[])g, (short[])r);
-            } else if (c == int.class) {
-                verifyI(name, i, (int[])g, (int[])r);
-            } else if (c == long.class) {
-                verifyL(name, i, (long[])g, (long[])r);
-            } else {
-                throw new RuntimeException("verify " + name + ": array type not supported for verify:" +
-                                       " gold[" + i + "].getClass() = " + g.getClass().getSimpleName() +
-                                       " result[" + i + "].getClass() = " + r.getClass().getSimpleName());
-            }
-        }
-    }
-
-    static void verifyB(String name, int i, byte[] g, byte[] r) {
-        for (int j = 0; j < g.length; j++) {
-            if (g[j] != r[j]) {
-                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
-                                           " gold[" + i + "][" + j + "] = " + g[j] +
-                                           " result[" + i + "][" + j + "] = " + r[j]);
-            }
-        }
-    }
-
-    static void verifyS(String name, int i, short[] g, short[] r) {
-        for (int j = 0; j < g.length; j++) {
-            if (g[j] != r[j]) {
-                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
-                                           " gold[" + i + "][" + j + "] = " + g[j] +
-                                           " result[" + i + "][" + j + "] = " + r[j]);
-            }
-        }
-    }
-
-    static void verifyI(String name, int i, int[] g, int[] r) {
-        for (int j = 0; j < g.length; j++) {
-            if (g[j] != r[j]) {
-                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
-                                           " gold[" + i + "][" + j + "] = " + g[j] +
-                                           " result[" + i + "][" + j + "] = " + r[j]);
-            }
-        }
-    }
-
-    static void verifyL(String name, int i, long[] g, long[] r) {
-        for (int j = 0; j < g.length; j++) {
-            if (g[j] != r[j]) {
-                throw new RuntimeException("verify " + name + ": arrays must have same content:" +
-                                           " gold[" + i + "][" + j + "] = " + g[j] +
-                                           " result[" + i + "][" + j + "] = " + r[j]);
-            }
-        }
     }
 
     @Test
@@ -263,8 +163,8 @@ public class TestAliasing {
         applyIfPlatform = {"64-bit", "true"},
         applyIfCPUFeatureOr = {"sse4.1", "true"})
     // Speculative aliasing check -> full vectorization.
-    static Object[] test4a(short[] a, short[] b) {
-        for (int i = 0; i < RANGE-64; i++) {
+    static Object[] test4a(int[] a, int[] b) {
+        for (int i = 0; i < SIZE-64; i++) {
           b[i+2] = a[i+0];
         }
         return new Object[]{ a, b };
@@ -288,8 +188,8 @@ public class TestAliasing {
         applyIfPlatform = {"64-bit", "true"},
         applyIfCPUFeatureOr = {"sse4.1", "true"})
     // Speculative aliasing check with multiversioning -> full vectorization & split packs.
-    static Object[] test4a_alias(short[] a, short[] b) {
-        for (int i = 0; i < RANGE-64; i++) {
+    static Object[] test4a_alias(int[] a, int[] b) {
+        for (int i = 0; i < SIZE-64; i++) {
           b[i+2] = a[i+0];
         }
         return new Object[]{ a, b };
