7ea000444932943baeff8a77fafb37be20d62fd1
Sergey Bylokhov <bylokhov@amazon.com>
2025-04-17 17:49:51 -0700

    Apply java.io.Serial annotations in java.compiler

--- src/java.compiler/share/classes/javax/lang/model/type/UnknownTypeException.java
+++ src/java.compiler/share/classes/javax/lang/model/type/UnknownTypeException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2005, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2005, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,8 @@
 
 package javax.lang.model.type;
 
+import java.io.Serial;
+
 import javax.lang.model.UnknownEntityException;
 
 /**
@@ -39,6 +41,7 @@ import javax.lang.model.UnknownEntityException;
  */
 public class UnknownTypeException extends UnknownEntityException {
 
+    @Serial
     private static final long serialVersionUID = 269L;
 
     private transient TypeMirror type;
