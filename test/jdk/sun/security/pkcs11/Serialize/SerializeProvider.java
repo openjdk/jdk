f0308bb152587cc91394bea009e8849fe18dc91c
Mikhail Yankelevich <myankelevich@openjdk.org>
2025-04-18 11:09:44 +0100

    JDK-8230016: re-visit test sun/security/pkcs11/Serialize/SerializeProvider.java
    test is fixed, provider is now added to the Security before the test

--- test/jdk/sun/security/pkcs11/Serialize/SerializeProvider.java
+++ test/jdk/sun/security/pkcs11/Serialize/SerializeProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,6 +30,8 @@
  * @modules jdk.crypto.cryptoki
  */
 
+import jtreg.SkippedException;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
@@ -42,8 +44,11 @@ public class SerializeProvider extends PKCS11Test {
 
     public void main(Provider p) throws Exception {
         if (Security.getProvider(p.getName()) != p) {
-            System.out.println("Provider not installed in Security, skipping");
-            return;
+            Security.addProvider(p);
+        }
+
+        if (Security.getProvider(p.getName()) != p) {
+            throw new SkippedException("Provider not installed in Security, skipping");
         }
 
         ByteArrayOutputStream out = new ByteArrayOutputStream();
