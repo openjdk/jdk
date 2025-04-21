e8adab3ce497390ad759a74634b707c3e363d7e8
liach <liach@openjdk.org>
2025-04-21 15:09:04 -0500

    Refine validation and defensive copying

--- src/java.base/share/classes/jdk/internal/vm/annotation/IntrinsicCandidate.java
+++ src/java.base/share/classes/jdk/internal/vm/annotation/IntrinsicCandidate.java
@@ -43,14 +43,15 @@ import java.lang.annotation.*;
  * should be done in Java.
  * <p>
  * In best practice, intrinsic methods are usually private, unsafe, and
- * static, and their encapsulating callers should perform:
- * <ul>
- * <li>Heap access, such as field reads into local variables;</li>
- * <li>Validation, such as null checks, range checks, type checks,
- * on local variables;</li>
- * <li>All validated values should be in local variables instead of re-read from
- * fields to avoid TOCTOU races.</li>
- * </ul>
+ * static, and their encapsulating callers should perform validation checks.
+ * Any validation must be done on values that are exclusively accessed by the
+ * current thread: shared fields must be read into local variables, and shared
+ * arrays must be copied to an exclusive copy, to ensure each shared location
+ * (a field or an array component) is accessed exactly once. If a shared
+ * location is read multiple times for check and for use, race conditions may
+ * cause two reads to produce distinct values, known as TOCTOU (time of check
+ * and time of use), and the read for use may produce an illegal value. Finally,
+ * the thread-exclusive validated values are passed to the intrinsic methods.
  * <p>
  * The HotSpot VM checks, when loading a class, the consistency of recognized
  * methods and {@code @IntrinsicCandidate} annotations, unless the {@code
