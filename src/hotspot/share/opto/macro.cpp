8581a24f103b47c563e1deeacb0ddbfbc9a19c62
Dhamoder Nalla <dhanalla@microsoft.com>
2025-04-22 17:56:23 +0100

    uncomment the fix

--- src/hotspot/share/opto/macro.cpp
+++ src/hotspot/share/opto/macro.cpp
@@ -819,7 +819,7 @@ SafePointScalarObjectNode* PhaseMacroExpand::create_scalarized_object_descriptio
 
     Node *field_val = value_from_mem(sfpt->memory(), sfpt->control(), basic_elem_type, field_type, field_addr_type, alloc);
     // If scalarize operation is adding too many nodes, bail out
-    /*if (C->live_nodes() + NodeLimitFudgeFactor > C->max_node_limit()) {
+    if (C->live_nodes() + NodeLimitFudgeFactor > C->max_node_limit()) {
       if (C->do_escape_analysis() == true && !C->failing()) {
         // Retry compilation without escape analysis.
         // If this is the first failure, the sentinel string will "stick"
@@ -827,7 +827,7 @@ SafePointScalarObjectNode* PhaseMacroExpand::create_scalarized_object_descriptio
         C->record_failure(C->congraph()->invocation() > 0 ? C2Compiler::retry_no_iterative_escape_analysis() : C2Compiler::retry_no_escape_analysis());
       }
       return nullptr;
-    }*/
+    }
 
     // We weren't able to find a value for this field,
     // give up on eliminating this allocation.
