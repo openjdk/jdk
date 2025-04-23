fb35852542b91c7c2b5e6f9d71e2571dd8d27043
Andrey Turbanov <turbanoff@gmail.com>
2025-04-23 12:25:35 +0300

    [PATCH] Remove excessive nested synchronized from getHeavyWeightPopupCache

--- src/java.desktop/share/classes/javax/swing/PopupFactory.java
+++ src/java.desktop/share/classes/javax/swing/PopupFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -449,17 +449,15 @@ public class PopupFactory {
          */
         @SuppressWarnings("unchecked")
         private static Map<Window, List<HeavyWeightPopup>> getHeavyWeightPopupCache() {
-            synchronized (HeavyWeightPopup.class) {
-                Map<Window, List<HeavyWeightPopup>> cache = (Map<Window, List<HeavyWeightPopup>>)SwingUtilities.appContextGet(
-                                  heavyWeightPopupCacheKey);
+            Map<Window, List<HeavyWeightPopup>> cache = (Map<Window, List<HeavyWeightPopup>>)SwingUtilities.appContextGet(
+                              heavyWeightPopupCacheKey);
 
-                if (cache == null) {
-                    cache = new HashMap<>(2);
-                    SwingUtilities.appContextPut(heavyWeightPopupCacheKey,
-                                                 cache);
-                }
-                return cache;
+            if (cache == null) {
+                cache = new HashMap<>(2);
+                SwingUtilities.appContextPut(heavyWeightPopupCacheKey,
+                                             cache);
             }
+            return cache;
         }
 
         /**
