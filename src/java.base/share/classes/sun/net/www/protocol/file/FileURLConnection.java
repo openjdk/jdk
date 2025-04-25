9aa9a848fbba924e0dd8022c26965ff9b3acc8af
Eirik Bjorsnos <eirbjo@gmail.com>
2025-04-25 20:32:39 +0200

    Use Boolean::getBoolean

--- src/java.base/share/classes/sun/net/www/protocol/file/FileURLConnection.java
+++ src/java.base/share/classes/sun/net/www/protocol/file/FileURLConnection.java
@@ -49,9 +49,8 @@ public class FileURLConnection extends URLConnection {
 
     // The feature of falling back to FTP for non-local file URLs is disabled
     // by default and can be re-enabled by setting a system property
-    private static boolean FTP_FALLBACK_ENABLED = "true".equalsIgnoreCase(
-            System.getProperty("jdk.net.file.ftpfallback", "false")
-    );
+    private static boolean FTP_FALLBACK_ENABLED =
+            Boolean.getBoolean("jdk.net.file.ftpfallback");
 
     private final File file;
     private InputStream is;
