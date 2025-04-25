e57fd710496b2ac8aa93eb3d4ff2234170fa2e37
Vladimir Ivanov <vlivanov@openjdk.org>
2025-04-25 21:22:37 +0000

    8353786: Migrate Vector API math library support to FFM API
    Reviewed-by: jbhateja, kvn, psandoz, xgong, jvernee, mli

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -3786,11 +3786,6 @@ jint Arguments::apply_ergo() {
       }
     }
     FLAG_SET_DEFAULT(EnableVectorAggressiveReboxing, false);
-
-    if (!FLAG_IS_DEFAULT(UseVectorStubs) && UseVectorStubs) {
-      warning("Disabling UseVectorStubs since EnableVectorSupport is turned off.");
-    }
-    FLAG_SET_DEFAULT(UseVectorStubs, false);
   }
 #endif // COMPILER2_OR_JVMCI
 
