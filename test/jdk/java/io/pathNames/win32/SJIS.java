76aac517daf38ec839b741bbf83184f07a031fdc
Naoto Sato <naoto.sato@oracle.com>
2025-04-25 12:35:46 -0700

    Expand wild card imports

--- test/jdk/java/io/pathNames/win32/SJIS.java
+++ test/jdk/java/io/pathNames/win32/SJIS.java
@@ -29,7 +29,11 @@
    @library /test/lib
 */
 
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import jtreg.SkippedException;
 
 
