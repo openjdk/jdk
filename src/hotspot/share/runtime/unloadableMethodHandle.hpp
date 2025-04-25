4db23b352719d252b17e6bcb3a6804c0b20f4102
Aleksey Shipilev <aleksey@shipilev.net>
2025-04-25 10:57:37 +0200

    Attempt at phasing doc

--- src/hotspot/share/runtime/unloadableMethodHandle.hpp
+++ src/hotspot/share/runtime/unloadableMethodHandle.hpp
@@ -25,56 +25,72 @@
 #ifndef SHARE_RUNTIME_UNLOADABLE_METHOD_HANDLE_HPP
 #define SHARE_RUNTIME_UNLOADABLE_METHOD_HANDLE_HPP
 
-#include "oops/klass.hpp"
 #include "oops/oopHandle.hpp"
 #include "oops/weakHandle.hpp"
 
 // Unloadable method handle.
 //
-// Useful when one needs to hold to Method* without delaying class unloading.
+// This handle allows holding to Method* safely without delaying class unloading
+// of its holder.
 //
-// This handle can be in 3 states:
-//  1. Initial weak state. Relevant Method* is only weakly-reachable, can be cleared
-//     by class unloading.
-//  2. Accessible strong state. Relevant Method* is strongly reachable, cannot be
-//     cleared by class unloading.
-//  3. Final released state. Relevant Method* is in unknown state, and cannot be
-//     accessed.
+// This handle can be in 4 states:
+//  1. Empty. There is no Method* inside. All methods are safe to call.
+//     This is a convenience state to allow easy initializations.
+//  2. Weak. Method* is present, but its holder is only weakly-reachable, and can
+//     be unloaded. Users need to check !is_unloaded() before calling method().
+//     method() is safe to call iff we have not crossed a safepoint since construction
+//     or last !is_unloaded() check. Calling block_unloading() after !is_unloaded() check
+//     moves handle to the strong state.
+//  3. Strong. Method* holder is strongly reachable, cannot be unloaded.
+//     Calling method() is always safe in this state.
+//  4. Released. Method* is in unknown state, and cannot be accessed.
+//     method() returns nullptr in this state.
 //
-// Users should call block_unloading() to reach accessible state before unwrapping
-// method().
+// The handle transitions are one-shot:
+//    weak   --(block_unloading) --> strong
+//    weak   ------(release) ------> released
+//    strong ------(release) ------> released
 //
+// Additionally, when handle is empty, it stays empty:
+//    empty  --(block_unloading) --> empty
+//    empty  ------(release) ------> empty
+//
+// Common usage pattern:
+//
+//   UnloadableMethodHandle mh;           // Initially empty.
+//   mh = UnloadableMethodHandle(method); // Now in weak state.
+//   mh.method()->print_on(tty);          // method() is good until the next safepoint.
+//   <safepoint>
+//   if (mh.is_unloaded()) {              // Can still use method()?
+//     mh.release();                      // No! Release the handle and exit.
+//     return;
+//   }
+//   mh.method()->print_on(tty);          // method() is good until the next safepoint.
+//   mh.block_unloading();                // Now in strong state.
+//   <safepoint>
+//   mh.method()->print_on(tty);          // method() is always good now.
+//   mh.release();                        // Release the handle.
+//
+
+class Method;
+
 class UnloadableMethodHandle {
   friend class VMStructs;
-
+private:
   Method* _method;
-  WeakHandle _weak_handle;   // oop that can be used to block unloading
-  OopHandle  _strong_handle; // oop that *is* used to block unloading
+  WeakHandle _weak_handle;
+  OopHandle _strong_handle;
 
   inline oop get_unload_blocker(Method* method);
 
 public:
-  UnloadableMethodHandle() : _method(nullptr) {}; // initialization
+  UnloadableMethodHandle() : _method(nullptr) {}
   UnloadableMethodHandle(Method* method);
-
-  /*
-   * Release the handle.
-   */
   inline void release();
 
-  /*
-   * Check if method holder is unloaded.
-   */
-  inline bool is_unloaded() const;
-
-  /*
-   * Return the method. Only safe when !is_unloaded().
-   */
   inline Method* method() const;
 
-  /*
-   * Block unloading, allow method() calls.
-   */
+  inline bool is_unloaded() const;
   void block_unloading();
 };
 
