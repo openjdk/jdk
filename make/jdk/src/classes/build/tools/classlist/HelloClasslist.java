5835a0d57402c0b559b872ee395a5588fd46a7a7
liach <liach@openjdk.org>
2025-04-28 09:21:01 -0500

    Use more simple function descriptor

--- make/jdk/src/classes/build/tools/classlist/HelloClasslist.java
+++ make/jdk/src/classes/build/tools/classlist/HelloClasslist.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -48,10 +48,8 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Optional;
 import java.text.DateFormat;
 
-import static java.lang.foreign.ValueLayout.*;
 import static java.util.stream.Collectors.*;
 
 /**
@@ -178,9 +176,7 @@ public class HelloClasslist {
         Class<?> striped64Class = Class.forName("java.util.concurrent.atomic.Striped64$Cell");
 
         // Initialize FFM linkers
-        var signature = FunctionDescriptor.of(ADDRESS,
-                ADDRESS, ADDRESS, ADDRESS,
-                JAVA_BOOLEAN, ADDRESS, ADDRESS);
+        var signature = FunctionDescriptor.ofVoid();
         Linker.nativeLinker().downcallHandle(signature);
     }
 
