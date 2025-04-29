eca12ce93e3abc722de7aa66dd282327f0d6183b
Manuel Hässig <manuel.hassig@oracle.com>
2025-04-29 12:16:19 +0200

    Add DeoptimizeALot and fix typo in test

--- test/hotspot/jtreg/compiler/print/TestPrintAssemblyDeoptRace.java
+++ test/hotspot/jtreg/compiler/print/TestPrintAssemblyDeoptRace.java
@@ -24,8 +24,8 @@
 /*
  * @test
  * @bug 8258229
- * @summary If a method is made not entrant while prinint the assembly, hotspot crashes due to mismatched relocation information.
- * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:-TieredCompilation -XX:CompileCommand=print,java/math/BitSieve.bit
+ * @summary If a method is made not entrant while printing the assembly, hotspot crashes due to mismatched relocation information.
+ * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:-TieredCompilation -XX:+DeoptimizeALot -XX:CompileCommand=print,java/math/BitSieve.bit
  *                   compiler.print.TestPrintAssemblyDeoptRace
  */
 
