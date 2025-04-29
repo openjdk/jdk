4976d80654291bd6218ab3a6063344e2e98e358d
suchismith1993 <suchismith1993@gmail.com>
2025-04-29 10:18:20 -0500

    trailing space

--- src/hotspot/cpu/ppc/c1_LIRGenerator_ppc.cpp
+++ src/hotspot/cpu/ppc/c1_LIRGenerator_ppc.cpp
@@ -733,7 +733,7 @@ void LIRGenerator::do_MathIntrinsic(Intrinsic* x) {
       switch (x->id()) {
         case vmIntrinsics::_dsqrt:
         case vmIntrinsics::_dsqrt_strict:
-	  /* run interpreted */    	
+	        /* run interpreted */
           break;
         case vmIntrinsics::_dsin:
           runtime_entry = CAST_FROM_FN_PTR(address, SharedRuntime::dsin);
