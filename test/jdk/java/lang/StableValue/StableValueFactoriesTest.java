fbc4691bfa11f31601fd89d05da63e689343e214
Per Minborg <pminborg@openjdk.org>
2025-04-30 16:03:25 +0000

    8351565: Implement JEP 502: Stable Values (Preview)
    Co-authored-by: Maurizio Cimadamore <mcimadamore@openjdk.org>
    Reviewed-by: vklang, jvernee, alanb, liach

--- test/jdk/java/lang/StableValue/StableValueFactoriesTest.java
+++ test/jdk/java/lang/StableValue/StableValueFactoriesTest.java
@@ -1,12 +1,10 @@
 /*
- * Copyright (c) 2020, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
+ * published by the Free Software Foundation.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -23,17 +21,23 @@
  * questions.
  */
 
-package jdk.internal.access;
+/* @test
+ * @summary Basic tests for StableValue factory implementations
+ * @modules java.base/jdk.internal.lang.stable
+ * @enablePreview
+ * @run junit StableValueFactoriesTest
+ */
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.IntFunction;
+import jdk.internal.lang.stable.StableUtil;
+import org.junit.jupiter.api.Test;
 
-public interface JavaUtilCollectionAccess {
-    <E> List<E> listFromTrustedArray(Object[] array);
-    <E> List<E> listFromTrustedArrayNullsAllowed(Object[] array);
-    <E> List<E> stableList(int size, IntFunction<? extends E> mapper);
-    <K, V> Map<K, V> stableMap(Set<K> keys, Function<? super K, ? extends V> mapper);
+import static org.junit.jupiter.api.Assertions.*;
+
+final class StableValueFactoriesTest {
+
+    @Test
+    void array() {
+        assertThrows(IllegalArgumentException.class, () -> StableUtil.array(-1));
+    }
+
 }
