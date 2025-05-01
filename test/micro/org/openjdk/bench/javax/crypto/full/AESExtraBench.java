c116b18b89950362d899448a36a4693f733067b1
Sergey Kuksenko <skuksenko@openjdk.org>
2025-05-01 18:45:20 +0000

    8355559: Benchmark modification/extension shouldn't affect the behavior of other benchmarks
    Reviewed-by: ecaspole

--- test/micro/org/openjdk/bench/javax/crypto/full/AESExtraBench.java
+++ test/micro/org/openjdk/bench/javax/crypto/full/AESExtraBench.java
@@ -23,24 +23,27 @@
 package org.openjdk.bench.javax.crypto.full;
 
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.infra.Blackhole;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.GeneralSecurityException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidParameterSpecException;
 
-public class AESBench extends CryptoBase {
+public class AESExtraBench extends AESBench {
 
-    public static final int SET_SIZE = 128;
+    public static final int SET_SIZE = 8;
 
-    @Param({"AES/ECB/NoPadding", "AES/ECB/PKCS5Padding", "AES/CBC/NoPadding", "AES/CBC/PKCS5Padding"})
+    @Param({"AES/CBC/NoPadding"})
     private String algorithm;
 
     @Param({"128", "192", "256"})
@@ -49,36 +52,27 @@ public class AESBench extends CryptoBase {
     @Param({"" + 16 * 1024})
     private int dataSize;
 
-    byte[][] data;
-    byte[][] encryptedData;
-    Cipher encryptCipher;
-    Cipher decryptCipher;
+    private byte[] outBuffer;
     int index = 0;
 
     @Setup
     public void setup() throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidParameterSpecException {
-        setupProvider();
-        byte[] keystring = fillSecureRandom(new byte[keyLength / 8]);
-        SecretKeySpec ks = new SecretKeySpec(keystring, "AES");
-        encryptCipher = makeCipher(prov, algorithm);
-        encryptCipher.init(Cipher.ENCRYPT_MODE, ks);
-        decryptCipher = makeCipher(prov, algorithm);
-        decryptCipher.init(Cipher.DECRYPT_MODE, ks, encryptCipher.getParameters());
-        data = fillRandom(new byte[SET_SIZE][dataSize]);
-        encryptedData = fillEncrypted(data, encryptCipher);
+        super.setup();
+        outBuffer = new byte[dataSize + 128]; // extra space for tag, etc
     }
 
     @Benchmark
-    public byte[] encrypt() throws BadPaddingException, IllegalBlockSizeException {
+    public int encrypt2() throws GeneralSecurityException {
         byte[] d = data[index];
         index = (index +1) % SET_SIZE;
-        return encryptCipher.doFinal(d);
+        return encryptCipher.doFinal(d, 0, d.length, outBuffer);
     }
 
     @Benchmark
-    public byte[] decrypt() throws BadPaddingException, IllegalBlockSizeException {
+    public int decrypt2() throws GeneralSecurityException {
         byte[] e = encryptedData[index];
         index = (index +1) % SET_SIZE;
-        return decryptCipher.doFinal(e);
+        return decryptCipher.doFinal(e, 0, e.length, outBuffer);
     }
+
 }
