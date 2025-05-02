83207d758ee20518efc8c329bb0521e1afdb2b17
Jan Lahoda <jan.lahoda@oracle.com>
2025-05-02 08:54:29 +0200

    8356057: PrintingProcessor (-Xprint) does not print type variable bounds and type annotations for Object supertypes

---
+++ test/langtools/tools/javac/processing/options/XprintTypeAnnotationsAndTypeVarBounds.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8356057
+ * @summary Verify annotated supertypes and type variable bounds are printed properly
+ * @compile/ref=XprintTypeAnnotationsAndTypeVarBounds.out -Xprint  XprintTypeAnnotationsAndTypeVarBounds.java
+ */
+
+import java.lang.annotation.*;
+
+class AnnotatedObjectSuperType extends @TA Object {
+}
+
+class UnannotatedObjectSuperType extends Object {
+}
+
+class TypeVariableWithAnnotation1<@TA T> {
+}
+
+class TypeVariableWithAnnotation2<@TA T extends Object> {
+}
+
+class TypeVariableWithBound1<T extends @TA Object> {
+}
+
+class TypeVariableWithBound2<T extends @TA CharSequence> {
+}
+
+class TypeVariableWithBound3<T extends @TA Object & CharSequence> {
+}
+
+class TypeVariableWithBound4<T extends Object & @TA CharSequence> {
+}
+
+@Target(ElementType.TYPE_USE)
+@interface TA {
+}
