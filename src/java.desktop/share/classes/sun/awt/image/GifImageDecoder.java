5dbf7a11e7910f3b59a69bb30af57ec57cd750e5
jeremy <jeremy.wood@mac.com>
2025-05-05 13:01:58 -0400

    8356137: Fixing non-zero transparent pixel with disposal method 1
    Also adding accompanying unit test.
    
    I think there's been a mix-up regarding the sample image attached to the original ticket. This ticket should refer to `clyde.gif` (originally based on a data structure identified here: https://free-gifs.org/gif/CC0-3D ).
    
    There is another similar looking (but different) ticket I filed last week for which `leo.gif` is the appropriate test case file. (See incident report 9218362. As far as I can see that hasn't made its way to the bug database yet.) I had to email both gifs to someone on the triage team, and I think they got mixed up.

--- src/java.desktop/share/classes/sun/awt/image/GifImageDecoder.java
+++ src/java.desktop/share/classes/sun/awt/image/GifImageDecoder.java
@@ -411,6 +411,15 @@ public class GifImageDecoder extends ImageDecoder {
                 // in order to avoid artefacts in case of animated images.
                 int runstart = -1;
                 int count = 1;
+
+                // the saved_image is initially filled with zeroes.
+                // If this is our first time updating it, and if zeroes
+                // are NOT the transparent pixel: we need to replace it
+                // with the appropriate transparent pixel.
+                boolean replaceTransPixelsInSavedImage = save &&
+                        saved_model == null &&
+                        trans_pixel > 0;
+
                 for (int i = rasbeg; i < rasend; i++, off++) {
                     byte pixel = rasline[i];
                     if ((pixel & 0xff) == trans_pixel) {
@@ -424,6 +433,9 @@ public class GifImageDecoder extends ImageDecoder {
                             }
                         }
                         runstart = -1;
+                        if (replaceTransPixelsInSavedImage) {
+                            saved_image[off] = pixel;
+                        }
                     } else {
                         if (runstart < 0) {
                             runstart = i;
