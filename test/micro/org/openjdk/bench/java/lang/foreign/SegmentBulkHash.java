2548d7fc96c5c5332b970012fa033246fcf3480b
Per Minborg <pminborg@gmail.com>
2025-05-06 11:19:47 +0200

    Add copyright header and revert unintended change

--- test/micro/org/openjdk/bench/java/lang/foreign/SegmentBulkHash.java
+++ test/micro/org/openjdk/bench/java/lang/foreign/SegmentBulkHash.java
@@ -46,7 +46,6 @@ import java.util.concurrent.TimeUnit;
 
 import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 
-
 @BenchmarkMode(Mode.AverageTime)
 @Warmup(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 @Measurement(iterations = 10, time = 500, timeUnit = TimeUnit.MILLISECONDS)
