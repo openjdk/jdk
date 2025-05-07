af03557b51a6cb7aa66b36968ecd522a25811e28
Manuel Hässig <manuel.hassig@oracle.com>
2025-05-07 09:51:54 +0200

    Removed bug number

--- test/hotspot/jtreg/compiler/print/TestPrintAssemblyDeoptRace.java
+++ test/hotspot/jtreg/compiler/print/TestPrintAssemblyDeoptRace.java
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8258229 8356310
+ * @bug 8258229
  * @summary If a method is made not entrant while printing the assembly, hotspot crashes due to mismatched relocation information.
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation -XX:+DeoptimizeALot
  *                   -XX:CompileCommand=print,java/math/BitSieve.bit compiler.print.TestPrintAssemblyDeoptRace
