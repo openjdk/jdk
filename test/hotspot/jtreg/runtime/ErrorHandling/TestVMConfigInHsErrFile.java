75e71d85305de671dcb5ef15f245649b5eac1ead
Roman Kennke <rkennke@amazon.de>
2025-05-07 10:49:36 +0200

    Add test case

---
+++ test/hotspot/jtreg/runtime/ErrorHandling/TestVMConfigInHsErrFile.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+
+/*
+ * @test
+ * @summary Test that we see VM configs reported correctly in hs_err file
+ * @library /test/lib
+ * @requires vm.flagless
+ * @requires vm.debug
+ * @modules java.base/jdk.internal.misc
+ *          java.management
+ * @run driver TestVMConfigInHsErrFile
+ */
+
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.test.lib.process.ProcessTools;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
+public class TestVMConfigInHsErrFile {
+
+  public static void main(String[] args) throws Exception {
+
+    ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(
+        "-XX:+UnlockDiagnosticVMOptions",
+        "-XX:+UnlockExperimentalVMOptions",
+        "-XX:+UseCompactObjectHeaders",
+        "-Xmx100M",
+        "-XX:-CreateCoredumpOnCrash",
+        "-XX:ErrorHandlerTest=14",
+        "-version");
+
+    OutputAnalyzer output = new OutputAnalyzer(pb.start());
+    output.shouldNotHaveExitValue(0);
+
+    // we should have crashed with a SIGSEGV
+    output.shouldMatch("#.+SIGSEGV.*");
+
+    // extract hs-err file
+    File f = HsErrFileUtils.openHsErrFileFromOutput(output);
+
+    ArrayList<Pattern> patterns = new ArrayList<>();
+    patterns.add(Pattern.compile("# A fatal error has been detected.*"));
+    patterns.add(Pattern.compile("# *SIGSEGV.*"));
+    patterns.add(Pattern.compile("# Java VM: .*compact obj headers.*"));
+    patterns.add(Pattern.compile("# *Problematic frame.*"));
+    patterns.add(Pattern.compile("# .*VMError::controlled_crash.*"));
+
+    HsErrFileUtils.checkHsErrFileContent(f, patterns.toArray(new Pattern[] {}), true);
+
+  }
+
+}
