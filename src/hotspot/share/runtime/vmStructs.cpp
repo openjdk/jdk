1a3bce937076e35f77324d23133f62d4d69464e1
Jatin Bhateja <jatin.bhateja@intel.com>
2025-05-08 19:03:54 +0530

    Code re-factoring from Vladimir

--- src/hotspot/share/runtime/vmStructs.cpp
+++ src/hotspot/share/runtime/vmStructs.cpp
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2000, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -708,10 +709,6 @@
      static_field(Abstract_VM_Version,         _vm_security_version,                          int)                                   \
      static_field(Abstract_VM_Version,         _vm_build_number,                              int)                                   \
                                                                                                                                      \
-     NOT_ZERO(X86_ONLY(   static_field(VM_Version,              _features,                      VM_Version::VM_Features)))           \
-     NOT_ZERO(X86_ONLY(nonstatic_field(VM_Version::VM_Features, _features_bitmap[0],            uint64_t)))                          \
-     NOT_ZERO(X86_ONLY(   static_field(VM_Version::VM_Features, _features_bitmap_size_in_bytes, int)))                               \
-                                                                                                                                     \
   /*************************/                                                                                                        \
   /* JVMTI */                                                                                                                        \
   /*************************/                                                                                                        \
@@ -1168,8 +1165,7 @@
   /********************/                                                  \
                                                                           \
   declare_toplevel_type(Abstract_VM_Version)                              \
-  NOT_ZERO(declare_toplevel_type(VM_Version))                             \
-  NOT_ZERO(X86_ONLY(declare_toplevel_type(VM_Version::VM_Features)))      \
+  declare_toplevel_type(VM_Version)                                       \
                                                                           \
   /*************/                                                         \
   /* Arguments */                                                         \
@@ -1722,7 +1718,6 @@
   /**********************/                                                \
   NOT_ZERO(PPC64_ONLY(declare_constant(frame::entry_frame_locals_size)))  \
                                                                           \
-  NOT_ZERO(X86_ONLY(declare_constant(frame::entry_frame_call_wrapper_offset)))      \
   declare_constant(frame::pc_return_offset)                               \
                                                                           \
   /*************/                                                         \
@@ -2152,3 +2147,4 @@ void vmStructs_init() {
   VMStructs::init();
 }
 #endif // ASSERT
+
