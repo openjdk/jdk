f07fcbff095ff2886781823d104e039e37d94acf
Aleksey Shipilev <aleksey@shipilev.net>
2025-05-09 13:08:56 +0200

    Fix

--- src/hotspot/share/oops/weakHandle.inline.hpp
+++ src/hotspot/share/oops/weakHandle.inline.hpp
@@ -40,7 +40,7 @@ inline oop WeakHandle::peek() const {
 }
 
 inline void WeakHandle::replace(oop with_obj) {
-  assert(!is_null(), "Cannot use replace on empty handle");
+  assert(!is_empty(), "Must not use replace on empty handle");
   NativeAccess<ON_PHANTOM_OOP_REF>::oop_store(_obj, with_obj);
 }
 
