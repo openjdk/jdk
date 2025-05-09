209a0eb9f4d1559d25a16f9dbe96bb28fa3dee47
Roland Westrelin <47032191+rwestrel@users.noreply.github.com>
2025-05-09 11:03:49 +0200

    Update src/hotspot/share/opto/castnode.hpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/castnode.hpp
+++ src/hotspot/share/opto/castnode.hpp
@@ -140,9 +140,7 @@ public:
   virtual const Type* Value(PhaseGVN* phase) const;
 
   bool inner_loop_backedge(Node* proj);
-
   bool cmp_used_at_inner_loop_exit_test(Node* cmp);
-
   bool used_at_inner_loop_exit_test();
 
   virtual Node* Ideal(PhaseGVN* phase, bool can_reshape);
