d7122ccbf3b03a3c43917656ad209624910f6230
Evgeny Nikitin <evgeny.nikitin@oracle.com>
2025-05-10 20:55:37 +0200

    Update modified wrappers

--- test/hotspot/jtreg/applications/ctw/modules/java_instrument.java
+++ test/hotspot/jtreg/applications/ctw/modules/java_instrument.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,7 +31,6 @@
  *          java.base/jdk.internal.misc
  *          java.base/jdk.internal.reflect
  * @modules java.instrument
- * @requires vm.jvmti
  *
  * @build jdk.test.whitebox.WhiteBox
  * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
