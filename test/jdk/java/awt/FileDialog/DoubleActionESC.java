1ef773d01338d7294c5209159c4c29da2649f2c7
anass baya <anass.baya@oracle.com>
2025-05-12 13:28:33 +0100

    The count of the latch has to be 2

--- test/jdk/java/awt/FileDialog/DoubleActionESC.java
+++ test/jdk/java/awt/FileDialog/DoubleActionESC.java
@@ -53,37 +53,34 @@ public class DoubleActionESC {
     private static final int REPEAT_COUNT = 2;
 
     public static void main(String[] args) throws Exception {
-        latch = new CountDownLatch(1);
+        latch = new CountDownLatch(REPEAT_COUNT);
 
         robot = new Robot();
-        robot.setAutoDelay(100);
+        robot.setAutoDelay(50);
         try {
             EventQueue.invokeAndWait(() -> {
                 createAndShowUI();
             });
 
+            robot.waitForIdle();
             robot.delay(1000);
+
             EventQueue.invokeAndWait(() -> {
                 p = showBtn.getLocationOnScreen();
                 d = showBtn.getSize();
             });
 
             for (int i = 0; i < REPEAT_COUNT; ++i) {
-                Thread thread = new Thread(() -> {
-                    robot.mouseMove(p.x + d.width / 2, p.y + d.height / 2);
-                    robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-                    robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                });
-                thread.start();
-                robot.delay(3000);
+                robot.mouseMove(p.x + d.width / 2, p.y + d.height / 2);
+                robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+                robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+                robot.waitForIdle();
+                robot.delay(1000);
 
-                Thread thread1 = new Thread(() -> {
-                    robot.keyPress(KeyEvent.VK_ESCAPE);
-                    robot.keyRelease(KeyEvent.VK_ESCAPE);
-                    robot.waitForIdle();
-                });
-                thread1.start();
-                robot.delay(3000);
+                robot.keyPress(KeyEvent.VK_ESCAPE);
+                robot.keyRelease(KeyEvent.VK_ESCAPE);
+                robot.waitForIdle();
+                robot.delay(1000);
             }
 
             latch.await();
