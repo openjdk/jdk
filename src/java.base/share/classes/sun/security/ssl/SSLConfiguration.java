9f7f9e190b124d569eae8bf1df5e6c076ad00a06
Jaikiran Pai <jaikiran.pai@oracle.com>
2025-05-13 10:16:38 +0530

    http3: remove unused method

--- src/java.base/share/classes/sun/security/ssl/SSLConfiguration.java
+++ src/java.base/share/classes/sun/security/ssl/SSLConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -527,10 +527,6 @@ final class SSLConfiguration implements Cloneable {
 
     public boolean isUseCompatibilityMode() {
         return useCompatibilityMode && !isQuic;
-    }
-
-    public boolean isQuic() {
-        return isQuic;
     }
 
     public void setQuic(boolean quic) {
