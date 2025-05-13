5759a146cf240e8ca3655fc33c24ef15ea2d5002
Marc Chevalier <marc.chevalier@oracle.com>
2025-05-13 13:47:17 +0200

    Use Platform.isWindows

--- test/hotspot/jtreg/compiler/lib/ir_framework/driver/TestVMProcess.java
+++ test/hotspot/jtreg/compiler/lib/ir_framework/driver/TestVMProcess.java
@@ -259,7 +259,7 @@ public class TestVMProcess {
         int exitCode = oa.getExitValue();
         String stdErr = oa.getStderr();
         String stdOut = "";
-        boolean osIsWindows = System.getProperty("os.name").toLowerCase().contains("windows");
+        boolean osIsWindows = Platform.isWindows();
         boolean JVMHadError = (!osIsWindows && exitCode == 134) || (osIsWindows && exitCode == -1);
         if (JVMHadError) {
             // Also dump the stdout if we experience a JVM error (e.g. to show hit assertions etc.).
