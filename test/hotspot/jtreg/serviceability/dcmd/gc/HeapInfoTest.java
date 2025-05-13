3a6c6a83ba291d8dff07fd8383b4c58ee80ad383
Joel Sikström <joel.sikstrom@oracle.com>
2025-05-13 18:10:51 +0200

    HeapInfoTest should check that GC.heap_info actually runs

--- test/hotspot/jtreg/serviceability/dcmd/gc/HeapInfoTest.java
+++ test/hotspot/jtreg/serviceability/dcmd/gc/HeapInfoTest.java
@@ -44,6 +44,8 @@ public class HeapInfoTest {
     public void run(CommandExecutor executor) {
         String cmd = "GC.heap_info";
         OutputAnalyzer output = executor.execute(cmd);
+        output.shouldNotContain("Unknown diagnostic command");
+        output.shouldHaveExitValue(0);
     }
 
     @Test
