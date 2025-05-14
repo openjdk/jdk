91f3d409151dd7e489c7754e8d856a3ae41827f4
Harshitha Onkar <harshitha.onkar@oracle.com>
2025-05-14 14:23:31 -0700

    DTRACE added

--- src/java.desktop/unix/native/libawt_xawt/xawt/XToolkit.c
+++ src/java.desktop/unix/native/libawt_xawt/xawt/XToolkit.c
@@ -693,10 +693,7 @@ static void wakeUp() {
     static char wakeUp_char = 'p';
     if (!isMainThread() && awt_pipe_inited) {
         if (write(AWT_WRITEPIPE, &wakeUp_char, 1) < 0) {
-            // if block is left empty to avoid adding unused-result to
-            // AwtLibraries.gmk disabled warning section on linux (gcc & clang)
-            // Additionally, throwing error in this case may introduce
-            // unexpected behavior change
+            DTRACE_PRINTLN("wakeUp(): write to AWT pipe failed");
         }
     }
 }
