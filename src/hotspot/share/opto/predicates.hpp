b031b7cdff881f084c0233bcce4687267ade8697
Roland Westrelin <47032191+rwestrel@users.noreply.github.com>
2025-05-15 16:42:24 +0200

    Update src/hotspot/share/opto/predicates.hpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/predicates.hpp
+++ src/hotspot/share/opto/predicates.hpp
@@ -74,7 +74,7 @@ class TemplateAssertionPredicate;
  *                           The predicate does not replace an actual check inside the loop. This predicate can only
  *                           be added once above the Loop Limit Check Parse Predicate for a loop.
  *     - Short Short:        This predicate is created when a long counted loop is transformed into an int counted
- *       Running Long        loop. In the general, that transformation requires an outer loop to guarantee that the new
+ *       Running Long        loop. In general, that transformation requires an outer loop to guarantee that the new
  *       Loop                loop nest iterates over the entire range of the loop before transformation. However, if the
  *       Predicate           loop is speculated to run for a small enough number of iterations, the outer loop is not
  *                           needed. This predicate is added to catch mis-speculation in this case. It also applies to
