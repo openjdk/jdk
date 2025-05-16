22058200df400b87690011bf0ce8e9b2d409e17f
rwestrel <rwestrel@redhat.com>
2025-05-16 16:11:19 +0200

    test

---
+++ test/hotspot/jtreg/compiler/c2/TestNullRegionInputAtPhiMakePathDead.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2025, Red Hat, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8355230
+ * @summary Crash in fuzzer tests: assert(n != nullptr) failed: must not be null
+ * @run main/othervm -XX:CompileCommand=compileonly,TestNullRegionInputAtPhiMakePathDead::* -Xcomp TestNullRegionInputAtPhiMakePathDead
+ */
+
+public class TestNullRegionInputAtPhiMakePathDead {
+
+    public static long instanceCount=-37082278491330812L;
+    public static float fFld=1.509F;
+    public static int iFld=89;
+
+    public static long vMeth_check_sum = 0;
+
+    public static void vMeth1(int i1) {
+        double d=-42.69476;
+        int i3=-119;
+
+        for (d = 5; d < 131; ++d) {
+            i3 = 12;
+            while (--i3 > 0) {
+                TestNullRegionInputAtPhiMakePathDead.fFld *= -31237;
+            }
+        }
+    }
+
+    public static double dMeth(long l) {
+        int i7=-76;
+
+        vMeth1(i7);
+        TestNullRegionInputAtPhiMakePathDead.fFld %= 16334;
+        return 0;
+    }
+
+    public static void vMeth() {
+        TestNullRegionInputAtPhiMakePathDead.instanceCount = (long)dMeth(TestNullRegionInputAtPhiMakePathDead.instanceCount);
+    }
+
+    public void mainTest(String[] strArr1) {
+
+        int i19=-9, i20=13736, i21=0, i24=5;
+        boolean b2=true;
+
+        vMeth();
+        for (i19 = 2; i19 < 281; i19++) {
+            try {
+                TestNullRegionInputAtPhiMakePathDead.iFld = (57 % i20);
+            } catch (ArithmeticException a_e) {}
+            i20 += (((i19 * TestNullRegionInputAtPhiMakePathDead.fFld) + TestNullRegionInputAtPhiMakePathDead.instanceCount) - i19);
+            if (b2) {
+                if (true) {
+                    for (i21 = i19; i21 < 90; i21++) {
+                        if (b2) {
+                        } else if (true) {
+                            switch ((((i21 >>> 1) % 4) * 5) + 115) {
+                            case 129:
+                                if (b2) break;
+                                break;
+                            case 135:
+                                i24 |= i24;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println("vMeth_check_sum: " + vMeth_check_sum);
+    }
+    public static void main(String[] strArr) {
+        try {
+            TestNullRegionInputAtPhiMakePathDead _instance = new TestNullRegionInputAtPhiMakePathDead();
+            for (int i = 0; i < 10; i++ ) {
+                _instance.mainTest(strArr);
+            }
+         } catch (Exception ex) {
+         }
+    }
+}
