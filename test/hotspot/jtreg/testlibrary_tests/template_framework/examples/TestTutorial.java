cc60064b1d786f30be43f8ffce69a28da770b995
Emanuel Peter <emanuel.peter@oracle.com>
2025-05-16 15:43:19 +0200

    improve tutorial for Names part1

--- test/hotspot/jtreg/testlibrary_tests/template_framework/examples/TestTutorial.java
+++ test/hotspot/jtreg/testlibrary_tests/template_framework/examples/TestTutorial.java
@@ -65,7 +65,8 @@ public class TestTutorial {
         comp.addJavaSourceCode("p.xyz.InnerTest4", generateWithCustomHooks());
         comp.addJavaSourceCode("p.xyz.InnerTest5", generateWithLibraryHooks());
         comp.addJavaSourceCode("p.xyz.InnerTest6", generateWithRecursionAndBindingsAndFuel());
-        comp.addJavaSourceCode("p.xyz.InnerTest7", generateWithNames());
+        comp.addJavaSourceCode("p.xyz.InnerTest7", generateWithNamesSimple());
+        comp.addJavaSourceCode("p.xyz.InnerTest8", generateWithNames());
 
         // Compile the source files.
         // Hint: if you want to see the generated source code, you can enable
@@ -81,6 +82,7 @@ public class TestTutorial {
         comp.invoke("p.xyz.InnerTest5", "main", new Object[] {});
         comp.invoke("p.xyz.InnerTest6", "main", new Object[] {});
         comp.invoke("p.xyz.InnerTest7", "main", new Object[] {});
+        comp.invoke("p.xyz.InnerTest8", "main", new Object[] {});
     }
 
     // This example shows the use of various Tokens.
@@ -421,39 +423,125 @@ public class TestTutorial {
         return templateClass.render();
     }
 
-    // In the example below ("generateWithNames"), we see the use of Names to add
-    // variables and fields to code scopes, and then sample from those variables
-    // and fields. Every Name has a Name.Type, which we now define for int and long.
+    // Below, we introduce the concept of "Names". Code generation often involves defining
+    // fields and variables, which are then available inside a defined scope, and can be
+    // sampled in any nested scope. To allow the use of names for multiple applications
+    // (e.g. fields, variables, methods, etc.), we define a Name, which captures the String
+    // representation to be used in code, as well as its type and if it is mutable.
+    //
+    // To get started, we show an example where all Names have the same type, and where
+    // all Names are mutable. For simplicity, our type represents the primitive int type.
+    private record MySimpleType() implements Name.Type {
+        // The type is only subtype of itself. This is relevant when sampling or weighing
+        // Names, because we do not just sample from the given type, but also its subtypes.
+        @Override
+        public boolean isSubtypeOf(Name.Type other) {
+            return other instanceof MySimpleType();
+        }
+
+        // The name of the type can later be accessed, and used in code. We are working
+        // with ints, so that is what we return.
+        @Override
+        public String name() { return "int"; }
+    }
+    private static final MySimpleType mySimpleType = new MySimpleType();
+
+    // In this Example, we generate 3 fields, and add their names to the
+    // current scope. In a nested Template, we can then sample one of these
+    // Names, which gives us one of the fields. We increment that randomly
+    // chosen field. At the end, we print all three fields.
+    public static String generateWithNamesSimple() {
+        var templateMain = Template.make(() -> body(
+            // Sample a random Name, i.e. field, and assign its name to
+            // the hashtag replacement "#f".
+            // We are picking a mutable Name, because we are not just
+            // reading but also writing to the field.
+            let("f", sampleName(mySimpleType, true).name()),
+            """
+            // Let us now sample a random field #f, and increment it.
+            #f += 42;
+            """
+        ));
+
+        var templateClass = Template.make(() -> body(
+            // Let us define the names for the three fields.
+            // We can then sample from these names in a nested Template.
+            // We make all Names mutable, and with the same weight of 1,
+            // so that they have equal probability of being sampled.
+            addName(new Name($("f1"), mySimpleType, true, 1)),
+            addName(new Name($("f2"), mySimpleType, true, 1)),
+            addName(new Name($("f3"), mySimpleType, true, 1)),
+            """
+            package p.xyz;
+
+            public class InnerTest7 {
+                // Let us define a some fields.
+                public static int $f1;
+                public static int $f2;
+                public static int $f3;
+
+                public static void main() {
+                    // Let us now call the nested template that samples
+                    // a random field and increments it.
+                    """,
+                    templateMain.asToken(),
+                    """
+                    // Now, we can print all three fields, and see which
+                    // one was incremented.
+                    System.out.println("f1: " + $f1);
+                    System.out.println("f2: " + $f2);
+                    System.out.println("f3: " + $f3);
+                    // We have two zeros, and one 42.
+                    if ($f1 + $f2 + $f3 != 42) { throw new RuntimeException("wrong result!"); }
+                }
+            }
+            """
+        ));
+
+        // Render templateClass to String.
+        return templateClass.render();
+    }
+
+    // In the example above, we could have easily kept track of the three fields ourselves,
+    // and would not have had to rely on the Template Framework's Names for this. However,
+    // with more complicated examples, this gets more and more difficult, if not impossible.
+    //
+    // In the example below, we make the scenario a little more realistic. We work with an
+    // int and a long type. In the main method, we add some fields and local variables, and
+    // register their Names. When sampling from the main method, we should be able to see
+    // both fields and variables that we just registered. But from another method, we should
+    // only see the fields, but the local variables from main should not be sampled.
+    //
+    // Let us now define the wrapper for primitive types such as int and long.
     private record MyPrimitive(String name) implements Name.Type {
         @Override
         public boolean isSubtypeOf(Name.Type other) {
             return other instanceof MyPrimitive(String n) && n == name();
         }
 
+        // Note: the name method is automatically overridden by the record
+        //       field accessor.
+        // But we would like to also directly use the type in the templates,
+        // hence we let "toString" return "int" or "long".
         @Override
         public String toString() { return name(); }
     }
     private static final MyPrimitive myInt = new MyPrimitive("int");
     private static final MyPrimitive myLong = new MyPrimitive("long");
 
-    // Example with names, i.e. addName, weighNames, and sampleName.
-    // These can be used to add variables and fields to code scopes, and then sample
-    // from the available variables and fields later.
     public static String generateWithNames() {
-        var templateSample = Template.make("type", (Name.Type type) -> body(
-            let("name", sampleName(type, false).name()),
-            """
-            System.out.println("Sampling type #type: #name = " + #name);
-            """
-        ));
-
+        // Define a static field.
         var templateStaticField = Template.make("type", (Name.Type type) -> body(
             addName(new Name($("field"), type, true, 1)),
+            // Note: since we have overridden MyPrimitive::toString, we can use
+            //       the type directly as "#type" in the template, which then
+            //       gets hashtag replaced with "int" or "long".
             """
             public static #type $field = 0;
             """
         ));
 
+        // Define a local variable.
         var templateLocalVariable = Template.make("type", (Name.Type type) -> body(
             addName(new Name($("var"), type, true, 1)),
             """
@@ -461,59 +549,89 @@ public class TestTutorial {
             """
         ));
 
+        // Sample a random field or variable, from those that are available at
+        // the current scope.
+        var templateSample = Template.make("type", (Name.Type type) -> body(
+            let("name", sampleName(type, true).name()),
+            """
+            System.out.println("Sampling type #type: #name = " + #name);
+            """
+        ));
+
+        // Check how many fields and variables are available at the current scope.
         var templateStatus = Template.make(() -> body(
-            let("ints", weighNames(myInt, false)),
-            let("longs", weighNames(myLong, false)),
+            let("ints", weighNames(myInt, true)),
+            let("longs", weighNames(myLong, true)),
             """
             System.out.println("Status: #ints ints, #longs longs.");
             """
         ));
 
+        // Definition of the main method body.
         var templateMain = Template.make(() -> body(
             """
             System.out.println("Starting inside main...");
             """,
+            // Check the initial status, there should be nothing available.
             templateStatus.asToken(),
+            // Define some local variables. We place them at the beginning of
+            // the method, by using the METHOD_HOOK.
             Hooks.METHOD_HOOK.insert(templateLocalVariable.asToken(myInt)),
             Hooks.METHOD_HOOK.insert(templateLocalVariable.asToken(myLong)),
+            // Define some static fields. We place them at the top of the class,
+            // by using the CLASS_HOOK.
             Hooks.CLASS_HOOK.insert(templateStaticField.asToken(myInt)),
             Hooks.CLASS_HOOK.insert(templateStaticField.asToken(myLong)),
+            // If we check the status now, we should see two int and two
+            // long names, corresponding to our two fields and variables.
             templateStatus.asToken(),
-            // We should see a mix of fields and variables sampled.
+            // Now, we sample 5 int and 5 long names. We should get a mix
+            // of fields and variables. We have access to the fields because
+            // we inserted them to the class scope. We have access to the
+            // variables because we inserted them to the current method
+            // body.
             Collections.nCopies(5, templateSample.asToken(myInt)),
             Collections.nCopies(5, templateSample.asToken(myLong)),
+            // The status should not have changed since we last checked.
             templateStatus.asToken(),
             """
             System.out.println("Finishing inside main.");
             """
         ));
 
+        // Definition of another method's body. It is in the same class
+        // as the main method, so it has access to the same static fields.
         var templateOther = Template.make(() -> body(
             """
             System.out.println("Starting inside other...");
             """,
+            // We should see the fields defined in the main body,
+            // one int and one long field.
             templateStatus.asToken(),
-            // We still have all the field definitions from main.
+            // Sampling 5 random int and 5 random long Names. We should
+            // only see the fields, and not the local variables from main.
             Collections.nCopies(5, templateSample.asToken(myInt)),
             Collections.nCopies(5, templateSample.asToken(myLong)),
+            // The status should not have changed since we last checked.
             templateStatus.asToken(),
             """
             System.out.println("Finishing inside other.");
             """
         ));
 
+        // Finally, we put it all together in a class.
         var templateClass = Template.make(() -> body(
             """
             package p.xyz;
 
-            public class InnerTest7 {
+            public class InnerTest8 {
             """,
             // Class Hook for fields.
             Hooks.CLASS_HOOK.set(
                 """
                 public static void main() {
                 """,
-                // Method Hook for local variables, and earlier computations.
+                // Method Hook for local variables.
                 Hooks.METHOD_HOOK.set(
                     """
                     // This is the beginning of the "main" method body.
@@ -530,8 +648,8 @@ public class TestTutorial {
 
                 private static void other() {
                 """,
-                // Have a separate method hook for other, so that it can insert
-                // its own local variables.
+                // Have a separate method hook for other, where it could insert
+                // its own local variables (but happens not to).
                 Hooks.METHOD_HOOK.set(
                     """
                     System.out.println("Welcome to other!");
