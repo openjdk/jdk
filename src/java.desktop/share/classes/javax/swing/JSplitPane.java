a1c8a60ed3c4623b21cf477462179a7d3c186790
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-05-19 07:02:15 +0100

    8356594: JSplitPane loses divider location when reopened via JOptionPane.createDialog()

--- src/java.desktop/share/classes/javax/swing/JSplitPane.java
+++ src/java.desktop/share/classes/javax/swing/JSplitPane.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -372,6 +372,7 @@ public class JSplitPane extends JComponent implements Accessible
         super.setComponentOrientation(orientation);
         Component leftComponent = this.getLeftComponent();
         Component rightComponent = this.getRightComponent();
+        this.setDividerLocation(this.getDividerLocation());
         if (!this.getComponentOrientation().isLeftToRight()) {
             if (rightComponent != null) {
                 setLeftComponent(rightComponent);
