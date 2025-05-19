25c68453b946fef2b01d79328321eceef21e0290
suchismith1993 <suchismith1993@gmail.com>
2025-05-19 08:02:15 -0500

    sha logic

--- src/hotspot/cpu/ppc/vm_version_ppc.cpp
+++ src/hotspot/cpu/ppc/vm_version_ppc.cpp
@@ -273,20 +273,22 @@ void VM_Version::initialize() {
     FLAG_SET_DEFAULT(UseSHA1Intrinsics, false);
   }
 
-  if (FLAG_IS_DEFAULT(UseSHA256Intrinsics)) {
-    if (UseSHA) {
+  if (UseSHA) {
+    if (FLAG_IS_DEFAULT(UseSHA256Intrinsics)) {
       FLAG_SET_DEFAULT(UseSHA256Intrinsics, true);
-    } else {
-      FLAG_SET_DEFAULT(UseSHA256Intrinsics, false);
     }
+  } else if (UseSHA256Intrinsics) {
+    warning("Intrinsics for SHA-224 and SHA-256 crypto hash functions not available on this CPU.");
+    FLAG_SET_DEFAULT(UseSHA256Intrinsics, false);
   }
 
-  if (FLAG_IS_DEFAULT(UseSHA512Intrinsics)) {
-    if (UseSHA) {
+  if (UseSHA) {
+    if (FLAG_IS_DEFAULT(UseSHA512Intrinsics)) {
       FLAG_SET_DEFAULT(UseSHA512Intrinsics, true);
-    } else {
-      FLAG_SET_DEFAULT(UseSHA512Intrinsics, false);
     }
+  } else if (UseSHA512Intrinsics) {
+    warning("Intrinsics for SHA-384 and SHA-512 crypto hash functions not available on this CPU.");
+    FLAG_SET_DEFAULT(UseSHA512Intrinsics, false);
   }
 
   if (UseSHA3Intrinsics) {
