00a9d43287544a22bbc0ff7b6381c813d9989134
tstuefe <thomas.stuefe@gmail.com>
2025-05-22 09:00:30 +0200

    add missing Oracle copyright

--- test/hotspot/jtreg/gc/TestAlwaysPreTouchBehavior.java
+++ test/hotspot/jtreg/gc/TestAlwaysPreTouchBehavior.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2014, 2024, Alibaba Group Holding Limited. All rights reserved.
  * Copyright (c) 2024, 2025, Red Hat Inc.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
