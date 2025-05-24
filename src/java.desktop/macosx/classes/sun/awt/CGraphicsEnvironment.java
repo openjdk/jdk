b3d4cedece43992e9ae086b2ac02158cf9aa6e17
Andrey Turbanov <turbanoff@gmail.com>
2025-05-24 19:33:57 +0300

    [PATCH] Remove unnecessary Map.containsKey from CGraphicsEnvironment.initDevices

--- src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
+++ src/java.desktop/macosx/classes/sun/awt/CGraphicsEnvironment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -175,8 +175,8 @@ public final class CGraphicsEnvironment extends SunGraphicsEnvironment {
             displayIDs = new int[]{mainDisplayID};
         }
         for (int id : displayIDs) {
-            devices.put(id, old.containsKey(id) ? old.remove(id)
-                                                : new CGraphicsDevice(id));
+            CGraphicsDevice d = old.remove(id);
+            devices.put(id, d != null ? d : new CGraphicsDevice(id));
         }
         // fetch the main display again, the old value might be outdated
         mainDisplayID = getMainDisplayID();
