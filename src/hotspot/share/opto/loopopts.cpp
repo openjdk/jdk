bebfec3fc050c810dea17f675abb944806a7c4c0
rwestrel <rwestrel@redhat.com>
2025-05-26 09:21:46 +0200

    review

--- src/hotspot/share/opto/loopopts.cpp
+++ src/hotspot/share/opto/loopopts.cpp
@@ -1689,7 +1689,8 @@ void PhaseIdealLoop::try_sink_out_of_loop(Node* n) {
       !n->is_OpaqueNotNull() &&
       !n->is_OpaqueInitializedAssertionPredicate() &&
       !n->is_OpaqueTemplateAssertionPredicate() &&
-      !is_raw_to_oop_cast // don't extend live ranges of raw oops
+      !is_raw_to_oop_cast &&// don't extend live ranges of raw oops
+      (KillPathsReachableByDeadTypeNode || !n->is_Type())
       ) {
     Node *n_ctrl = get_ctrl(n);
     IdealLoopTree *n_loop = get_loop(n_ctrl);
