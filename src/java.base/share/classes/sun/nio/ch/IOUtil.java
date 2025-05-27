d4b923d175b07e39ee8ee2c79f04457ea1cfbdd0
Per Minborg <pminborg@openjdk.org>
2025-05-27 19:11:12 +0000

    8357268: Use JavaNioAccess.getBufferAddress rather than DirectBuffer.address()
    Reviewed-by: alanb, valeriep

--- src/java.base/share/classes/sun/nio/ch/IOUtil.java
+++ src/java.base/share/classes/sun/nio/ch/IOUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -484,7 +484,7 @@ public final class IOUtil {
         NIO_ACCESS.acquireSession(bb);
     }
 
-    private static void releaseScope(ByteBuffer bb) {
+    static void releaseScope(ByteBuffer bb) {
         try {
             NIO_ACCESS.releaseSession(bb);
         } catch (Exception e) {
