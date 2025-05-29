ee8f784ea63b88edc977be06e1f9acdff0769b8f
Stuart Marks <stuart.marks@oracle.com>
2025-05-28 17:19:32 -0700

    Update @implSpecs of SequencedMap view methods.

--- src/java.base/share/classes/java/util/SequencedMap.java
+++ src/java.base/share/classes/java/util/SequencedMap.java
@@ -258,16 +258,20 @@ public interface SequencedMap<K, V> extends Map<K, V> {
         throw new UnsupportedOperationException();
     }
 
-    // XXX TODO need to update @implSpec for these three view classes to cover
-    // the behavior of add,get,remove/First,Last
-
     /**
      * Returns a {@code SequencedSet} view of this map's {@link #keySet keySet}.
      *
      * @implSpec
      * The implementation in this interface returns a {@code SequencedSet} instance
-     * that behaves as follows. Its {@link SequencedSet#add add} and {@link
-     * SequencedSet#addAll addAll} methods throw {@link UnsupportedOperationException}.
+     * that behaves as follows. Its {@link SequencedSet#add add}, {@link
+     * SequencedSet#addAll addAll}, {@link SequencedSet#addFirst addFirst}, and {@link
+     * SequencedSet#addLast addLast} methods throw {@link UnsupportedOperationException}.
+     * Its {@link SequencedSet#getFirst getFirst} and {@link SequencedSet#getLast getLast}
+     * methods are implemented in terms of the {@link #firstEntry firstEntry} and {@link
+     * #lastEntry lastEntry} methods of this class, respectively. Its {@link
+     * SequencedSet#removeFirst removeFirst} and {@link SequencedSet#removeLast removeLast}
+     * methods are implemented in terms of the {@link #pollFirstEntry pollFirstEntry} and
+     * {@link #pollLastEntry pollLastEntry} methods of this class, respectively.
      * Its {@link SequencedSet#reversed reversed} method returns the {@link
      * #sequencedKeySet sequencedKeySet} view of the {@link #reversed reversed} view of
      * this map. Each of its other methods calls the corresponding method of the {@link
@@ -308,8 +312,15 @@ public interface SequencedMap<K, V> extends Map<K, V> {
      *
      * @implSpec
      * The implementation in this interface returns a {@code SequencedCollection} instance
-     * that behaves as follows. Its {@link SequencedCollection#add add} and {@link
-     * SequencedCollection#addAll addAll} methods throw {@link UnsupportedOperationException}.
+     * that behaves as follows. Its {@link SequencedCollection#add add}, {@link
+     * SequencedCollection#addAll addAll}, {@link SequencedCollection#addFirst addFirst}, and {@link
+     * SequencedCollection#addLast addLast} methods throw {@link UnsupportedOperationException}.
+     * Its {@link SequencedCollection#getFirst getFirst} and {@link SequencedCollection#getLast getLast}
+     * methods are implemented in terms of the {@link #firstEntry firstEntry} and {@link
+     * #lastEntry lastEntry} methods of this class, respectively. Its {@link
+     * SequencedCollection#removeFirst removeFirst} and {@link SequencedCollection#removeLast removeLast}
+     * methods are implemented in terms of the {@link #pollFirstEntry pollFirstEntry} and
+     * {@link #pollLastEntry pollLastEntry} methods of this class, respectively.
      * Its {@link SequencedCollection#reversed reversed} method returns the {@link
      * #sequencedValues sequencedValues} view of the {@link #reversed reversed} view of
      * this map. Its {@link Object#equals equals} and {@link Object#hashCode hashCode} methods
@@ -345,8 +356,15 @@ public interface SequencedMap<K, V> extends Map<K, V> {
      *
      * @implSpec
      * The implementation in this interface returns a {@code SequencedSet} instance
-     * that behaves as follows. Its {@link SequencedSet#add add} and {@link
-     * SequencedSet#addAll addAll} methods throw {@link UnsupportedOperationException}.
+     * that behaves as follows. Its {@link SequencedSet#add add}, {@link
+     * SequencedSet#addAll addAll}, {@link SequencedSet#addFirst addFirst}, and {@link
+     * SequencedSet#addLast addLast} methods throw {@link UnsupportedOperationException}.
+     * Its {@link SequencedSet#getFirst getFirst} and {@link SequencedSet#getLast getLast}
+     * methods are implemented in terms of the {@link #firstEntry firstEntry} and {@link
+     * #lastEntry lastEntry} methods of this class, respectively. Its {@link
+     * SequencedSet#removeFirst removeFirst} and {@link SequencedSet#removeLast removeLast}
+     * methods are implemented in terms of the {@link #pollFirstEntry pollFirstEntry} and
+     * {@link #pollLastEntry pollLastEntry} methods of this class, respectively.
      * Its {@link SequencedSet#reversed reversed} method returns the {@link
      * #sequencedEntrySet sequencedEntrySet} view of the {@link #reversed reversed} view of
      * this map. Each of its other methods calls the corresponding method of the {@link
