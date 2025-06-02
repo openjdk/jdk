0c299471bb1eccccbca5f6c3a1800ac9dee9a14c
jeremy <jeremy.wood@mac.com>
2025-06-02 12:49:54 -0400

    8354646: remove redundant code in SynthPasswordFieldUI
    This is partially in response to:
    https://github.com/openjdk/jdk/pull/25443#issuecomment-2929752779

--- src/java.desktop/share/classes/javax/swing/plaf/synth/SynthPasswordFieldUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/synth/SynthPasswordFieldUI.java
@@ -108,19 +108,4 @@ public class SynthPasswordFieldUI extends SynthTextFieldUI {
                             int y, int w, int h) {
         context.getPainter().paintPasswordFieldBorder(context, g, x, y, w, h);
     }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    protected void installKeyboardActions() {
-        super.installKeyboardActions();
-        ActionMap map = SwingUtilities.getUIActionMap(getComponent());
-        if (map != null && map.get(DefaultEditorKit.selectWordAction) != null) {
-            Action a = map.get(DefaultEditorKit.selectLineAction);
-            if (a != null) {
-                map.put(DefaultEditorKit.selectWordAction, a);
-            }
-        }
-    }
 }
