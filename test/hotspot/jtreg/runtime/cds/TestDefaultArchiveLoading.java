9ef0863f66566633e0a8f34ccd31059814acc5e7
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2025-06-02 12:37:29 -0400

    Stefan comment

--- test/hotspot/jtreg/runtime/cds/TestDefaultArchiveLoading.java
+++ test/hotspot/jtreg/runtime/cds/TestDefaultArchiveLoading.java
@@ -26,7 +26,7 @@
  * @test id=nocoops_nocoh
  * @summary Test Loading of default archives in all configurations
  * @requires vm.cds
- * @requires vm.gc != "Z"
+ * @requires vm.cds.write.archived.java.heap
  * @requires vm.bits == 64
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
@@ -38,7 +38,7 @@
  * @test id=nocoops_coh
  * @summary Test Loading of default archives in all configurations (requires --enable-cds-archive-coh)
  * @requires vm.cds
- * @requires vm.gc != "Z"
+ * @requires vm.cds.write.archived.java.heap
  * @requires vm.bits == 64
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
@@ -50,7 +50,7 @@
  * @test id=coops_nocoh
  * @summary Test Loading of default archives in all configurations
  * @requires vm.cds
- * @requires vm.gc != "Z"
+ * @requires vm.cds.write.archived.java.heap
  * @requires vm.bits == 64
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
@@ -62,7 +62,7 @@
  * @test id=coops_coh
  * @summary Test Loading of default archives in all configurations (requires --enable-cds-archive-coh)
  * @requires vm.cds
- * @requires vm.gc != "Z"
+ * @requires vm.cds.write.archived.java.heap
  * @requires vm.bits == 64
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
