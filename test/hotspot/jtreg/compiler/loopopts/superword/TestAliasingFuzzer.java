8a2556254b83f43124f22181fd9a73e2b291c21b
Emanuel Peter <emanuel.peter@oracle.com>
2025-06-05 09:11:21 +0200

    more for fuzzer test

--- test/hotspot/jtreg/compiler/loopopts/superword/TestAliasingFuzzer.java
+++ test/hotspot/jtreg/compiler/loopopts/superword/TestAliasingFuzzer.java
@@ -39,6 +39,7 @@ import java.util.List;
 import java.util.ArrayList;
 
 import compiler.lib.compile_framework.*;
+import compiler.lib.generators.Generators;
 import compiler.lib.template_framework.Template;
 import compiler.lib.template_framework.TemplateToken;
 import static compiler.lib.template_framework.Template.body;
@@ -50,7 +51,19 @@ import compiler.lib.template_framework.library.TestFrameworkClass;
  * Simpler test cases can be found in {@link TestAliasing}.
  */
 public class TestAliasingFuzzer {
-
+    public record MyType(String name, int byteSize) {
+        @Override
+        public String toString() { return name(); }
+    }
+    public static final MyType myByte   = new MyType("byte", 1);
+    public static final MyType myChar   = new MyType("char", 2);
+    public static final MyType myShort  = new MyType("short", 2);
+    public static final MyType myInt    = new MyType("int", 4);
+    public static final MyType myLong   = new MyType("long", 8);
+    public static final MyType myFloat  = new MyType("float", 4);
+    public static final MyType myDouble = new MyType("double", 8);
+    public static final List<MyType> allTypes
+        = List.of(myByte, myChar, myShort, myInt, myLong, myFloat, myDouble);
 
     public static void main(String[] args) {
         // Create a new CompileFramework instance.
@@ -71,6 +84,16 @@ public class TestAliasingFuzzer {
         // Create a list to collect all tests.
         List<TemplateToken> testTemplateTokens = new ArrayList<>();
 
+        testTemplateTokens.addAll(allTypes.stream().map(t -> generateArray(t)).toList());
+
+        // TODO:
+        // - array, MemorySegment, Unsafe
+        // - various types - do I need them from the library?
+        // - various pointer shapes - different summands, count up vs down, strided etc.
+        // - aliasing
+        // - conversions on native
+        // Tricky: IR rules. May not vectorize in all cases.. how do we handle that?
+        // General strategy: one method compiled, one interpreted -> compare!
 
         // Create the test class, which runs all testTemplateTokens.
         return TestFrameworkClass.render(
@@ -83,5 +106,33 @@ public class TestAliasingFuzzer {
             comp.getEscapedClassPathOfCompiledClasses(),
             // The list of tests.
             testTemplateTokens);
+    }
+
+    public static TemplateToken generateArray(MyType type) {
+        var template = Template.make(() -> body(
+            let("size", Generators.G.safeRestrict(Generators.G.ints(), 10_000, 20_000).next()),
+            let("type", type),
+            """
+            // --- $test array start ---
+            // $test with size=#size and type=#type
+            private static #type[] $INPUT_A = new #type[#size];
+
+            @Run(test = "$test")
+            @Warmup(100)
+            public static void $run() {
+                $test($INPUT_A, $INPUT_A);
+            }
+
+            @Test
+            public static Object $test(#type[] a, #type[] b) {
+                for (int i = 0; i < a.length; i++) {
+                    a[i] = b[i];
+                }
+                return new Object[] {a, b};
+            }
+            // --- $test array end   ---
+            """
+        ));
+        return template.asToken();
     }
 }
