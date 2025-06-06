e443a1f2f3cfbf601c136d4d2a735ff787b63be6
Emanuel Peter <emanuel.peter@oracle.com>
2025-06-06 09:58:02 +0200

    CallDynamicJava

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -1613,6 +1613,25 @@ bool PhaseIterGVN::verify_node_Ideal(Node* n, bool can_reshape) {
     case Op_CallStaticJava:
       return false;
 
+    // CallDynamicJavaNode::Ideal
+    // During late inlining it can call CallJavaNode::register_for_late_inline
+    // That means we do more rounds of late inlining, but might fail.
+    // Then we do IGVN again, and register the node again for late inlining.
+    // This creates an endless cycle. Everytime we try late inlining, we
+    // are also creating more nodes, especially SafePoint and MergeMem.
+    // These nodes are immediately rejected when the inlining fails in the
+    // do_late_inline_check, but they still grow the memory, until we hit
+    // the MemLimit and crash.
+    // The assumption here seems that CallDynamicJavaNode::Ideal does not get
+    // called repeatedly, and eventually we terminate. I fear this is not
+    // a great assumption to make. We should investigate more.
+    //
+    // Found with:
+    //   compiler/loopopts/superword/TestDependencyOffsets.java#vanilla-U
+    //   -XX:+IgnoreUnrecognizedVMOptions -XX:VerifyIterativeGVN=1110
+    case Op_CallDynamicJava:
+      return false;
+
     // LShiftLNode::Ideal
     // Looks at pattern: "(x + x) << c0", converts it to "x << (c0 + 1)"
     // Probably a notification issue.
@@ -1936,7 +1955,6 @@ bool PhaseIterGVN::verify_node_Identity(Node* n) {
     // The observed issue was with AndVNode::Identity
     return false;
   }
-
 
   Node* i = n->Identity(this);
   // If we cannot find any other Identity, we are happy.
