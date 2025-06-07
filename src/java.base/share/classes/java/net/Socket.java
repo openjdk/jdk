909d7d27c78396d9051f00912ab6a20cda88c956
Jaikiran Pai <jaikiran.pai@gmail.com>
2025-06-07 10:26:40 +0530

    7116990: (spec) Socket.connect(addr,timeout) not clear if IOException because of TCP timeout

--- src/java.base/share/classes/java/net/Socket.java
+++ src/java.base/share/classes/java/net/Socket.java
@@ -621,6 +621,12 @@ public class Socket implements java.io.Closeable {
      *        {@code SocketException} with the interrupt status set.
      * </ol>
      *
+     * @apiNote Establishing a TCP/IP connection is subject to connection timeout settings
+     * in the operating system. The typical timeout is 60 seconds. If the operating system
+     * timeout expires before the {@code timeout} specified to this method then an
+     * {@code IOException} is thrown. The {@code timeout} specified to this method is typically
+     * a timeout value that is shorter than the operating system timeout.
+     *
      * @param   endpoint the {@code SocketAddress}
      * @param   timeout  the timeout value to be used in milliseconds.
      * @throws  IOException if an error occurs during the connection, the socket
