9321f4bd7175e570f4090a614940ae5c23639711
Taizo Kurashige <kurashige.taizo@fujitsu.com>
2025-06-09 16:37:42 +0900

    Improve hsdis load error message

--- src/hotspot/share/compiler/disassembler.cpp
+++ src/hotspot/share/compiler/disassembler.cpp
@@ -837,8 +837,6 @@ bool Disassembler::load_library(outputStream* st) {
   if (_library != nullptr) {
     _decode_instructions_virtual = CAST_TO_FN_PTR(Disassembler::decode_func_virtual,
                                           os::dll_lookup(_library, decode_instructions_virtual_name));
-  } else {
-    log_warning(os)("Loading hsdis library failed");
   }
   _tried_to_load_library = true;
   _library_usable        = _decode_instructions_virtual != nullptr;
