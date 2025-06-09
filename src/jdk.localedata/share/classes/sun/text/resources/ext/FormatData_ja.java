a9f7acc6864f9f51334633f18b8ba22dce206ae8
Magnus Ihse Bursie <mag@icus.se>
2025-06-09 17:46:30 +0200

    Fix comment indentation
    Co-authored-by: Alexey Ivanov <alexey.ivanov@oracle.com>

--- src/jdk.localedata/share/classes/sun/text/resources/ext/FormatData_ja.java
+++ src/jdk.localedata/share/classes/sun/text/resources/ext/FormatData_ja.java
@@ -87,7 +87,7 @@ public class FormatData_ja extends ParallelListResourceBundle {
         return new Object[][] {
             { "japanese.FirstYear",
                 new String[] {  // first year name
-                    "元",   // "Gan"-nen
+                    "元",       // "Gan"-nen
                 }
             },
         };
