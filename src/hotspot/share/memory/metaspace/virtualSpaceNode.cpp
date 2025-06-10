2eed16933ac5a6fb025cdd41fd41d08b298cf6a1
Coleen Phillimore <coleen.phillimore@oracle.com>
2025-06-10 12:22:59 +0000

    8357662: applications/runthese/RunThese8H.java Crash: 'Failed to uncommit metaspace'

--- src/hotspot/share/memory/metaspace/virtualSpaceNode.cpp
+++ src/hotspot/share/memory/metaspace/virtualSpaceNode.cpp
@@ -190,7 +190,8 @@ void VirtualSpaceNode::uncommit_range(MetaWord* p, size_t word_size) {
   // Uncommit...
   if (os::uncommit_memory((char*)p, word_size * BytesPerWord) == false) {
     // Note: this can actually happen, since uncommit may increase the number of mappings.
-    fatal("Failed to uncommit metaspace.");
+    UL(warning, "Failed to uncommit metaspace.");
+    return;
   }
 
   UL2(debug, "... uncommitted %zu words.", committed_words_in_range);
