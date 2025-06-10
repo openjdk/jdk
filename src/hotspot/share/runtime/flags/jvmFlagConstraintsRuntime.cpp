60179045084e7303bae6485ad53d70aca660d6c7
Casper Norrbin <casper.norrbin@oracle.com>
2025-06-10 14:30:27 +0200

    Remove PerfDataSamplingInterval constraint check

--- src/hotspot/share/runtime/flags/jvmFlagConstraintsRuntime.cpp
+++ src/hotspot/share/runtime/flags/jvmFlagConstraintsRuntime.cpp
@@ -99,18 +99,6 @@ JVMFlag::Error ContendedPaddingWidthConstraintFunc(int value, bool verbose) {
   }
 }
 
-JVMFlag::Error PerfDataSamplingIntervalFunc(int value, bool verbose) {
-  if ((value % PeriodicTask::interval_gran != 0)) {
-    JVMFlag::printError(verbose,
-                        "PerfDataSamplingInterval (%d) must be "
-                        "evenly divisible by PeriodicTask::interval_gran (%d)\n",
-                        value, PeriodicTask::interval_gran);
-    return JVMFlag::VIOLATES_CONSTRAINT;
-  } else {
-    return JVMFlag::SUCCESS;
-  }
-}
-
 JVMFlag::Error VMPageSizeConstraintFunc(uintx value, bool verbose) {
   uintx min = (uintx)os::vm_page_size();
   if (value < min) {
