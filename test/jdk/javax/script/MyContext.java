ca96e12be8ebc0e8ecfa59690bb1f65a2dff82c6
Volkan Yazıcı <volkan.yazici@oracle.com>
2025-06-11 12:39:10 +0200

    Revert superfluous test changes

--- test/jdk/javax/script/MyContext.java
+++ test/jdk/javax/script/MyContext.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2006, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2006, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -49,7 +49,7 @@ public class MyContext  implements ScriptContext {
         appScope = new SimpleBindings();
         engineScope = new SimpleBindings();
         globalScope = null;
-        reader = new InputStreamReader(System.in, System.getProperty("stdin.encoding"));
+        reader = new InputStreamReader(System.in);
         writer = new PrintWriter(System.out , true);
         errorWriter = new PrintWriter(System.err, true);
     }
