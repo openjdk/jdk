4eaa6e1f7efce73224bd7036d449d8cbc197e13b
Erik Österlund <eosterlund@openjdk.org>
2025-06-12 12:22:36 +0200

    Add elapsed_process_vtime

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -76,6 +76,7 @@
 #include <sys/resource.h>
 #include <sys/socket.h>
 #include <spawn.h>
+#include <sys/resource.h>
 #include <sys/time.h>
 #include <sys/times.h>
 #include <sys/types.h>
@@ -1510,6 +1511,17 @@ jlong os::elapsed_frequency() {
 }
 
 bool os::supports_vtime() { return true; }
+
+double os::elapsed_process_vtime() {
+  struct rusage usage;
+  int retval = getrusage(RUSAGE_SELF, &usage);
+  if (retval == 0) {
+    return usage.ru_utime.tv_sec + usage.ru_stime.tv_sec +
+         (usage.ru_utime.tv_usec + usage.ru_stime.tv_usec) / (1000.0 * 1000.0);
+  } else {
+    return -1;
+  }
+}
 
 // Return the real, user, and system times in seconds from an
 // arbitrary fixed point in the past.
