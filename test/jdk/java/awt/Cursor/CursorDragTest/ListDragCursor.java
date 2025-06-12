88d9cea1e33bd75f6dc3536793a5809361224061
Damon Nguyen <damon.nguyen@oracle.com>
2025-06-12 16:28:13 -0700

    Update test to avoid PFJ

--- test/jdk/java/awt/Cursor/CursorDragTest/ListDragCursor.java
+++ test/jdk/java/awt/Cursor/CursorDragTest/ListDragCursor.java
@@ -25,19 +25,63 @@
  * @test
  * @bug 4313052
  * @summary Test cursor changes after mouse dragging ends
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
  * @run main/manual ListDragCursor
  */
 
+import java.awt.Button;
 import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Frame;
+import java.awt.GridBagLayout;
 import java.awt.List;
 import java.awt.Panel;
 import java.awt.TextArea;
+import java.util.concurrent.CountDownLatch;
 
 public class ListDragCursor {
+    static Frame testFrame;
+    static Frame instructionsFrame;
+    static CountDownLatch countDownLatch;
+
+    public static CountDownLatch createCountDownLatch() {
+        return new CountDownLatch(1);
+    }
+
     public static void main(String[] args) throws Exception {
+        countDownLatch = createCountDownLatch();
+        EventQueue.invokeAndWait(() -> {
+            createTestFrame();
+            createInstructionsFrame();
+        });
+        countDownLatch.await();
+        System.out.println("Test Passed");
+    }
+
+    static void createTestFrame() {
+        Frame frame = new Frame("Cursor change after drag");
+        Panel panel = new Panel();
+
+        List list = new List(2);
+        list.add("List1");
+        list.add("List2");
+        list.add("List3");
+        list.add("List4");
+        list.setCursor(new Cursor(Cursor.HAND_CURSOR));
+
+        TextArea textArea = new TextArea(3, 5);
+        textArea.setCursor(new Cursor(Cursor.TEXT_CURSOR));
+
+        panel.add(textArea);
+        panel.add(list);
+
+        frame.add(panel);
+        frame.setSize(300, 150);
+        frame.setLocation(450, 400);
+        frame.setVisible(true);
+    }
+
+    static void createInstructionsFrame() {
         String INSTRUCTIONS = """
                 1. Move mouse to the TextArea.
                 2. Press the left mouse button.
@@ -56,35 +100,37 @@ public class ListDragCursor {
                 mouse button.
                 """;
 
-        PassFailJFrame.builder()
-                .title("Test Instructions")
-                .instructions(INSTRUCTIONS)
-                .rows((int) INSTRUCTIONS.lines().count() + 2)
-                .columns(35)
-                .testUI(ListDragCursor::createUI)
-                .build()
-                .awaitAndCheck();
+        TextArea textArea = new TextArea(INSTRUCTIONS,
+                15, 60, TextArea.SCROLLBARS_NONE);
+        Button passBtn = new Button("PASS");
+        Button failBtn = new Button("FAIL");
+        Panel btnPanel = new Panel(new GridBagLayout());
+        Panel panel = new Panel(new GridBagLayout());
+        instructionsFrame = new Frame("Test Instructions");
+        passBtn.setMaximumSize(new Dimension(100, 30));
+        failBtn.setMaximumSize(new Dimension(100, 30));
+        btnPanel.add(passBtn);
+        btnPanel.add(failBtn);
+        passBtn.addActionListener(e -> disposeFrames());
+        failBtn.addActionListener(e -> {
+            disposeFrames();
+            throw new RuntimeException("Test Failed");
+        });
+        panel.add(textArea);
+        panel.add(btnPanel);
+        instructionsFrame.add(panel);
+        instructionsFrame.pack();
+        instructionsFrame.setLocation(300, 100);
+        instructionsFrame.setVisible(true);
     }
 
-    public static Frame createUI() {
-        Frame frame = new Frame("Cursor change after drag");
-        Panel panel = new Panel();
-
-        List list = new List(2);
-        list.add("List1");
-        list.add("List2");
-        list.add("List3");
-        list.add("List4");
-        list.setCursor(new Cursor(Cursor.HAND_CURSOR));
-
-        TextArea textArea = new TextArea(3, 5);
-        textArea.setCursor(new Cursor(Cursor.TEXT_CURSOR));
-
-        panel.add(textArea);
-        panel.add(list);
-
-        frame.add(panel);
-        frame.setBounds(300, 100, 300, 150);
-        return frame;
+    static void disposeFrames() {
+        countDownLatch.countDown();
+        if (testFrame != null) {
+            testFrame.dispose();
+        }
+        if (instructionsFrame != null) {
+            instructionsFrame.dispose();
+        }
     }
 }
