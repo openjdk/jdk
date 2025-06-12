99085bfcad4a5c195de5790ea772aa87874abcfa
Andrew Dinn <adinn@redhat.com>
2025-06-12 18:57:43 +0100

    ensure fence stub is set as part of preuniverse init on zero

--- src/hotspot/cpu/zero/stubGenerator_zero.cpp
+++ src/hotspot/cpu/zero/stubGenerator_zero.cpp
@@ -179,7 +179,7 @@ class StubGenerator: public StubCodeGenerator {
   }
 
   void generate_preuniverse_stubs() {
-    // preuniverse stubs are not needed for zero
+    StubRoutines::_fence_entry               = ShouldNotCallThisStub();
   }
 
   void generate_initial_stubs() {
@@ -198,7 +198,6 @@ class StubGenerator: public StubCodeGenerator {
     StubRoutines::_atomic_cmpxchg_entry      = ShouldNotCallThisStub();
     StubRoutines::_atomic_cmpxchg_long_entry = ShouldNotCallThisStub();
     StubRoutines::_atomic_add_entry          = ShouldNotCallThisStub();
-    StubRoutines::_fence_entry               = ShouldNotCallThisStub();
   }
 
   void generate_continuation_stubs() {
