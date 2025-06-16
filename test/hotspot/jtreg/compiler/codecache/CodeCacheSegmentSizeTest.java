2db6f325e6d42dacfa793f02b0cfc79716d602d0
Amit Kumar <amitlocham09@icloud.com>
2025-06-16 11:41:33 +0000

    remove string check & update copyright header

--- test/hotspot/jtreg/compiler/codecache/CodeCacheSegmentSizeTest.java
+++ test/hotspot/jtreg/compiler/codecache/CodeCacheSegmentSizeTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 IBM Corporation. All rights reserved.
+ * Copyright (c) 2025 IBM Corporation. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -71,8 +71,6 @@ public class CodeCacheSegmentSizeTest {
 
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
 
-        output.shouldContain("openjdk version"); // typical first line
-        output.shouldContain("OpenJDK Runtime Environment");
         output.shouldHaveExitValue(0);
     }
 }
