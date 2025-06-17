85993dad4e88d7b4fbbec09c51539d5d0e2e7a59
Anjian-Wen <wenanjian@bytedance.com>
2025-06-17 16:34:09 +0800

    RISC-V: Simplify Interpreter::profile_taken_branch

--- src/hotspot/cpu/riscv/templateTable_riscv.cpp
+++ src/hotspot/cpu/riscv/templateTable_riscv.cpp
@@ -1608,7 +1608,7 @@ void TemplateTable::float_cmp(bool is_float, int unordered_result) {
 }
 
 void TemplateTable::branch(bool is_jsr, bool is_wide) {
-  __ profile_taken_branch(x10, x11);
+  __ profile_taken_branch(x10);
   const ByteSize be_offset = MethodCounters::backedge_counter_offset() +
                              InvocationCounter::counter_offset();
   const ByteSize inv_offset = MethodCounters::invocation_counter_offset() +
@@ -1657,7 +1657,6 @@ void TemplateTable::branch(bool is_jsr, bool is_wide) {
   if (UseLoopCounter) {
     // increment backedge counter for backward branches
     // x10: MDO
-    // x11: MDO bumped taken-count
     // x12: target offset
     __ bgtz(x12, dispatch); // count only if backward branch
 
