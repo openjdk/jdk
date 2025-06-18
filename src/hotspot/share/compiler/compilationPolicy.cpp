5e3089b5d1153635d4ed5b1ab9ac552f1eb90edd
Manuel Hässig <manuel.hassig@oracle.com>
2025-06-18 14:03:48 +0200

    Caclulate how many compiler buffers fit into NonNMethodCodeHeap

--- src/hotspot/share/compiler/compilationPolicy.cpp
+++ src/hotspot/share/compiler/compilationPolicy.cpp
@@ -572,7 +572,7 @@ void CompilationPolicy::initialize() {
       c2_size = C2Compiler::initial_code_buffer_size();
 #endif
       size_t buffer_size = c1_only ? c1_size : (c1_size/3 + 2*c2_size/3);
-      size_t max_count = (ReservedCodeCacheSize - (CodeCacheMinimumUseSpace DEBUG_ONLY(* 3))) / buffer_size;
+      size_t max_count = (NonNMethodCodeHeapSize - (CodeCacheMinimumUseSpace DEBUG_ONLY(* 3))) / buffer_size;
       if ((size_t)count > max_count) {
         // Lower the compiler count such that all buffers fit into the code cache
         count = MAX2((int)max_count, min_count);
