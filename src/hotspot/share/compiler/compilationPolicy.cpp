2cb4042865a370e7ec98c2dc2b22a6dc54957b8e
Manuel Hässig <manuel.hassig@oracle.com>
2025-06-18 14:11:31 +0200

    Calculate buffer size correctly for c2_only
    Co-authored-by: Aleksey Shipilev <shade@openjdk.org>

--- src/hotspot/share/compiler/compilationPolicy.cpp
+++ src/hotspot/share/compiler/compilationPolicy.cpp
@@ -571,7 +571,14 @@ void CompilationPolicy::initialize() {
 #ifdef COMPILER2
       c2_size = C2Compiler::initial_code_buffer_size();
 #endif
-      size_t buffer_size = c1_only ? c1_size : (c1_size/3 + 2*c2_size/3);
+      size_t buffer_size = 0;
+      if (c1_only) {
+        buffer_size = c1_size;
+      } else if (c2_only) {
+        buffer_size = c2_size;
+      } else {
+        buffer_size = c1_size / 3 + 2 * c2_size / 3;
+      }
       size_t max_count = (NonNMethodCodeHeapSize - (CodeCacheMinimumUseSpace DEBUG_ONLY(* 3))) / buffer_size;
       if ((size_t)count > max_count) {
         // Lower the compiler count such that all buffers fit into the code cache
