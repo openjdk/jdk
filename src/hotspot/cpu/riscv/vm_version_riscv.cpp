e963887f2049c40dc1062df93e66dcaf27040600
Anjian-Wen <wenanjian@bytedance.com>
2025-06-19 15:17:01 +0800

    delete some test change

--- src/hotspot/cpu/riscv/vm_version_riscv.cpp
+++ src/hotspot/cpu/riscv/vm_version_riscv.cpp
@@ -199,8 +199,7 @@ void VM_Version::common_initialize() {
       FLAG_SET_DEFAULT(UseRVV, false);
     } else {
       // read vector length from vector CSR vlenb
-      //_initial_vector_length = cpu_vector_length();
-      _initial_vector_length = 16;
+      _initial_vector_length = cpu_vector_length();
     }
   }
 
