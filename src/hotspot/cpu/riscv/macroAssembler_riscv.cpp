749350dac8c54b0ab55072bee3d2fffadae44714
hamlin <hamlin@hamlin.eu.rivosinc.com>
2025-06-19 08:58:44 +0000

    fix stop

--- src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/macroAssembler_riscv.cpp
@@ -3456,7 +3456,7 @@ void MacroAssembler::decode_klass_not_null(Register dst, Register src, Register 
   mv(xbase, (uintptr_t)CompressedKlassPointers::base());
 
   if (CompressedKlassPointers::shift() != 0) {
-    stop("CompressedKlassPointers::shift");
+    // stop("CompressedKlassPointers::shift");
     Register t = src == dst ? dst : t0;
     assert_different_registers(t, xbase);
     shadd(dst, src, xbase, t, CompressedKlassPointers::shift());
