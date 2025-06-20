4e8ce183d2cdedcfd9472b5e052f7be2318e83f4
Marc Chevalier <marc.chevalier@oracle.com>
2025-06-20 13:31:11 +0200

    Urgh

--- test/hotspot/jtreg/compiler/intrinsics/VectorIntoArrayInvalidControlFlow.java
+++ test/hotspot/jtreg/compiler/intrinsics/VectorIntoArrayInvalidControlFlow.java
@@ -28,7 +28,8 @@ package compiler.intrinsics;
  * @bug 8359344
  * @summary Intrinsic storeMasked can add some control flow before bailing out, leaving a malformed CFG.
  * @modules jdk.incubator.vector
- * @run main/othervm -XX:TypeProfileLevel=222 -Xbatch
+ * @run main/othervm -XX:+UnlockDiagnosticVMOptions
+ *                   -XX:TypeProfileLevel=222 -Xbatch
  *                   -XX:CompileCommand=compileonly,jdk.incubator.vector.Long*::intoArray0
  *                   -XX:+AbortVMOnCompilationFailure
  *                    compiler.intrinsics.VectorIntoArrayInvalidControlFlow
