5cdc5e11422b3a008e68d69c0c86876187185724
Coleen Phillimore <coleen.phillimore@oracle.com>
2025-06-20 13:26:47 +0000

    Put back include osthread.hpp

--- src/hotspot/share/interpreter/bytecodeTracer.cpp
+++ src/hotspot/share/interpreter/bytecodeTracer.cpp
@@ -36,7 +36,7 @@
 #include "runtime/atomic.hpp"
 #include "runtime/handles.inline.hpp"
 #include "runtime/mutexLocker.hpp"
-#include "runtime/osthread.hpp"
+#include "runtime/osThread.hpp"
 #include "utilities/align.hpp"
 
 // Prints the current bytecode and its attributes using bytecode-specific information.
