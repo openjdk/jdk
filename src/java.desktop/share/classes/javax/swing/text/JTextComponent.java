6e5eef637c02eceecdab3c0751fced30f4de14ea
Abhishek Kumar <abhishek.cx.kumar@oracle.com>
2025-06-23 23:05:23 +0530

    TextComponent's getBeforeIndex boundary value check fix

--- src/java.desktop/share/classes/javax/swing/text/JTextComponent.java
+++ src/java.desktop/share/classes/javax/swing/text/JTextComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2988,7 +2988,7 @@ public abstract class JTextComponent extends JComponent implements Scrollable, A
                 ((AbstractDocument)model).readLock();
             }
             try {
-                if (index < 0 || index >= model.getLength()) {
+                if (index < 0 || index > model.getLength()) {
                     return null;
                 }
                 switch (part) {
---
+++ test/jdk/javax/accessibility/TestAccessibleTextBoundaryValue.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+import javax.accessibility.AccessibleContext;
+import javax.accessibility.AccessibleText;
+
+import javax.swing.JTextField;
+
+/*
+ * @test
+ * @bug 8360070
+ * @summary Verifies that AccesibleText's getBeforeIndex method doesn't return
+ *          null for last character
+ * @run main TestAccessibleTextBoundaryValue
+ */
+
+public class TestAccessibleTextBoundaryValue {
+    public static void main(String[] args) throws Exception {
+        JTextField tf = new JTextField("Test1 Test2 Test3. Test4 Test5. Test6");
+        AccessibleContext ac = tf.getAccessibleContext();
+        AccessibleText at = ac.getAccessibleText();
+
+        if (at != null) {
+            String text = tf.getText();
+            int textLength = text.length();
+            String result = at.getBeforeIndex(AccessibleText.CHARACTER, textLength);
+
+            System.out.println("Text: \"" + text + "\"");
+            System.out.println("Text Length: " + textLength);
+            System.out.println("Call: getBeforeIndex(CHARACTER, " + textLength + ")");
+            System.out.println("Expected result: '6'");
+            System.out.println("Actual result: '" + result + "'");
+            if (!"6".equals(result)) {
+                throw new RuntimeException("The actual result does not match " +
+                                           "the expected result.");
+            }
+        }
+    }
+}
