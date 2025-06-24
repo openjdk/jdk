51600cdd219036906865909378135206d38c3886
Afshin Zafari <Afshin.zafari@oracle.com>
2025-06-24 16:47:17 +0200

    local VMT vs static one.

--- src/hotspot/share/nmt/memReporter.cpp
+++ src/hotspot/share/nmt/memReporter.cpp
@@ -422,7 +422,7 @@ void MemDetailReporter::report_virtual_memory_region(const ReservedMemoryRegion*
   outputStream* out = output();
   const char* scale = current_scale();
   const NativeCallStack*  stack = reserved_rgn->call_stack();
-  bool all_committed = reserved_rgn->size() == reserved_rgn->committed_size();
+  bool all_committed = reserved_rgn->size() == VirtualMemoryTracker::Instance::committed_size(reserved_rgn);
   const char* region_type = (all_committed ? "reserved and committed" : "reserved");
   out->cr();
   print_virtual_memory_region(region_type, reserved_rgn->base(), reserved_rgn->size());
