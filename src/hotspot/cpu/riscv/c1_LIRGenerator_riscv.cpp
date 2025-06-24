445f903be0852ae36ff4eee9d222723ab3bde052
jiangfeilong <jiangfeilong@huawei.com>
2025-06-24 22:30:55 +0800

    riscv: fix c1 primitive array clone intrinsic regression

--- src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
+++ src/hotspot/cpu/riscv/c1_LIRGenerator_riscv.cpp
@@ -772,7 +772,7 @@ void LIRGenerator::do_ArrayCopy(Intrinsic* x) {
   ciArrayKlass* expected_type = nullptr;
   arraycopy_helper(x, &flags, &expected_type);
   if (x->check_flag(Instruction::OmitChecksFlag)) {
-    flags = 0;
+    flags = AvoidUnalignedAccesses ? (flags & LIR_OpArrayCopy::unaligned) : 0;
   }
 
   __ arraycopy(src.result(), src_pos.result(), dst.result(), dst_pos.result(), length.result(), tmp,
