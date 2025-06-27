d14f5228cec87304ec39f018441ac73972df5324
Kevin Walls <kevin.walls@oracle.com>
2025-06-27 13:17:10 +0100

    Test requires: permit linux debug testing

--- test/jdk/com/sun/management/HotSpotDiagnosticMXBean/DumpThreadsWithEliminatedLock.java
+++ test/jdk/com/sun/management/HotSpotDiagnosticMXBean/DumpThreadsWithEliminatedLock.java
@@ -26,7 +26,8 @@
  * @bug 8356870
  * @summary Test HotSpotDiagnosticMXBean.dumpThreads with a thread owning a monitor for
  *     an object that is scalar replaced
- * @requires !vm.debug & (vm.compMode != "Xcomp")
+ * @requires vm.compMode != "Xcomp"
+ * @requires !vm.debug | (os.family == "linux")
  * @requires (vm.opt.TieredStopAtLevel == null | vm.opt.TieredStopAtLevel == 4)
  * @requires vm.jvmti
  * @modules jdk.management
