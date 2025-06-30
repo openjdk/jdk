5a9a0a45b2741b3dde6fe642b4fef5b212cacc33
hamlin <hamlin@hamlin.eu.rivosinc.com>
2025-06-30 18:23:05 +0000

    revert

--- src/hotspot/cpu/riscv/vtableStubs_riscv.cpp
+++ src/hotspot/cpu/riscv/vtableStubs_riscv.cpp
@@ -207,7 +207,7 @@ VtableStub* VtableStubs::create_itable_stub(int itable_index) {
                                   temp_reg, temp_reg2, itable_index, L_no_such_interface);
 
   // Reduce "estimate" such that "padding" does not drop below 8.
-  const ptrdiff_t estimate = AOTCodeCache::is_on_for_dump() ? 260 : 256;
+  const ptrdiff_t estimate = 256;
   const ptrdiff_t codesize = __ pc() - start_pc;
   slop_delta = (int)(estimate - codesize);
   slop_bytes += slop_delta;
