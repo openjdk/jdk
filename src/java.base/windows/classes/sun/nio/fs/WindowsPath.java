ea30eaf6bb1425d1a83894fab39d80ecaf262d28
Brian Burkhalter <brian.burkhalter@oracle.com>
2025-07-01 11:30:17 -0700

    8360028: (fs) Path.relativize throws StringIndexOutOfBoundsException (win)

--- src/java.base/windows/classes/sun/nio/fs/WindowsPath.java
+++ src/java.base/windows/classes/sun/nio/fs/WindowsPath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -521,6 +521,12 @@ class WindowsPath implements Path {
                     } else {
                         prevName = i;
                     }
+                    continue;
+                }
+
+                // skip empty names
+                if (name.isEmpty()) {
+                    ignore[i] = true;
                     continue;
                 }
 
