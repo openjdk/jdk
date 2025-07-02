a72f18346cd45a899af90c3267abc3f148e70d2e
kieran-farrell <kieran.farrell@oracle.com>
2025-07-02 11:43:52 +0100

    rm whitespace

--- src/java.base/share/classes/java/util/UUID.java
+++ src/java.base/share/classes/java/util/UUID.java
@@ -235,9 +235,9 @@ public final class UUID implements java.io.Serializable, Comparable<UUID> {
      *
      * @spec https://www.rfc-editor.org/rfc/rfc9562.html
      *       RFC 9562 Universally Unique IDentifiers (UUIDs)
-     *       
+     *
      * @since 26
-     */
+     * */
     public static UUID unixEpochTimeMillis(long timestamp) {
         if ((timestamp >> 48) != 0) {
             throw new IllegalArgumentException("Timestamp must be an unsigned 48-bit Unix Epoch time in milliseconds.");
