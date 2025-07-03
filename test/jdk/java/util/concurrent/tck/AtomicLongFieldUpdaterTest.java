c8fcf03cecad596857757588f2267087bd82a6da
liach <liach@openjdk.org>
2025-07-03 09:02:28 -0500

    Add tck coverage

--- test/jdk/java/util/concurrent/tck/AtomicLongFieldUpdaterTest.java
+++ test/jdk/java/util/concurrent/tck/AtomicLongFieldUpdaterTest.java
@@ -44,6 +44,7 @@ public class AtomicLongFieldUpdaterTest extends JSR166TestCase {
     private volatile long privateField;
     long w;
     float z;
+    static volatile long q;
     public static void main(String[] args) {
         main(suite(), args);
     }
@@ -84,6 +85,16 @@ public class AtomicLongFieldUpdaterTest extends JSR166TestCase {
     public void testConstructor3() {
         try {
             updaterFor("w");
+            shouldThrow();
+        } catch (IllegalArgumentException success) {}
+    }
+
+    /**
+     * construction with static field throws IllegalArgumentException
+     */
+    public void testConstructor4() {
+        try {
+            updaterFor("q");
             shouldThrow();
         } catch (IllegalArgumentException success) {}
     }
