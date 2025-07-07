270e1db4264c44ff6cfaac8f95fa06837305def6
Alex Menkov <alexey.menkov@oracle.com>
2025-07-07 16:47:38 -0700

    fix

--- test/hotspot/jtreg/vmTestbase/nsk/jvmti/RawMonitorEnter/rawmonenter003/TestDescription.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jvmti/RawMonitorEnter/rawmonenter003/TestDescription.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -37,6 +37,9 @@
  *         4431533: TEST_BUG: destroyed raw monitor can be occasionally valid
  *     Ported from JVMDI.
  *
+ * @comment the test intentionally passes bad argument to the function to verify the functionality,
+            it causes false positive from ASAN lib
+ * @requires !vm.asan
  * @library /vmTestbase
  *          /test/lib
  * @run main/othervm/native -agentlib:rawmonenter003 nsk.jvmti.RawMonitorEnter.rawmonenter003
