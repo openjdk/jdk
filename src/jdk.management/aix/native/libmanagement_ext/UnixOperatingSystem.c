19513b799bd6c2c8aeacbfda17117571dbce2658
suchismith1993 <suchismith1993@gmail.com>
2025-07-09 11:22:22 -0400

    system cpu load

--- src/jdk.management/aix/native/libmanagement_ext/UnixOperatingSystem.c
+++ src/jdk.management/aix/native/libmanagement_ext/UnixOperatingSystem.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2015, 2020 SAP SE. All rights reserved.
+ * Copyright (c) 2008, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,24 +28,68 @@
 /* Implement and update https://bugs.openjdk.org/browse/JDK-8030957 */
 
 #include <jni.h>
+#include <time.h>
 #include <stdlib.h>
 #include <libperfstat.h>
 #include "com_sun_management_internal_OperatingSystemImpl.h"
 perfstat_process_t prev_stats = {0};
 static unsigned long long prev_timebase = 0;
 static int initialized = 0;
- 
+
 #define HTIC2SEC(x) (((double)(x) * XINTFRAC) / 1000000000.0)
 
-
+static perfstat_cpu_total_t cpu_total_old;
+static time_t last_sample_time = 0;
+static double last_cpu_load = -1.0;
 JNIEXPORT jdouble JNICALL
 Java_com_sun_management_internal_OperatingSystemImpl_getCpuLoad0
 (JNIEnv *env, jobject dummy)
 {
-    return -1.0;
+    perfstat_cpu_total_t cpu_total;
+    int ret;
+
+    time_t now = time(NULL);
+    if (initialized && (now - last_sample_time < 5)) {
+        return last_cpu_load; // Return cached value if less than 5s
+    }
+
+    ret = perfstat_cpu_total(NULL, &cpu_total, sizeof(perfstat_cpu_total_t), 1);
+    if (ret < 0) {
+        return -1.0;
+    }
+
+    if (!initialized) {
+        cpu_total_old = cpu_total;
+        initialized = 1;
+        last_sample_time = now;
+        return -1.0; // Not enough data yet
+    }
+
+    long long user_diff = cpu_total.user - cpu_total_old.user;
+    long long sys_diff = cpu_total.sys - cpu_total_old.sys;
+    long long idle_diff = cpu_total.idle - cpu_total_old.idle;
+    long long wait_diff = cpu_total.wait - cpu_total_old.wait;
+    long long total = user_diff + sys_diff + idle_diff + wait_diff;
+
+    if (total == 0) {
+        return -1.0;
+    }
+
+    printf("User diff: %lld\n", user_diff);
+    printf("Sys diff: %lld\n", sys_diff);
+    printf("Idle diff: %lld\n", idle_diff);
+    printf("Wait diff: %lld\n", wait_diff);
+
+
+    double load = (double)(user_diff + sys_diff) / total;
+    printf("load:%.4f\n",load);    
+    fflush(stdout);
+    last_cpu_load = load;
+    last_sample_time = now;
+    cpu_total_old = cpu_total;
+
+    return load;
 }
-
-
 
 JNIEXPORT jdouble JNICALL
 Java_com_sun_management_internal_OperatingSystemImpl_getProcessCpuLoad0
@@ -55,13 +99,11 @@ Java_com_sun_management_internal_OperatingSystemImpl_getProcessCpuLoad0
     perfstat_id_t id;
     unsigned long long curr_timebase, timebase_diff;
     double user_diff, sys_diff, delta_time;
-    
 
-    if (perfstat_process(&id, &curr_stats, sizeof(perfstat_process_t), 1) == -1) {
-        return -1.0;  // Unable to get stats
+    if (perfstat_process(&id, &curr_stats, sizeof(perfstat_process_t), 1) < 0) {
+        return -1.0;  
     }
     if (!initialized) {
-        // First call: just store and return -1.0
         prev_stats = curr_stats;
         prev_timebase = curr_stats.last_timebase;
         initialized = 1;
