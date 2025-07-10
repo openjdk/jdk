0d826d75e0246118a8fbb1f1af6ca79cb23291ac
Darragh Clarke <Darragh.clarke@oracle.com>
2025-07-10 14:04:02 +0100

    adding salting and path verification to test

--- test/jdk/java/net/HttpURLConnection/HttpUrlConnectionExpectContinueResponseMessageTest.java
+++ test/jdk/java/net/HttpURLConnection/HttpUrlConnectionExpectContinueResponseMessageTest.java
@@ -43,13 +43,15 @@ import org.junit.jupiter.params.provider.MethodSource;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.URL;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -62,6 +64,7 @@ public class HttpUrlConnectionExpectContinueResponseMessageTest {
         volatile boolean stop = false;
         volatile String response = null;
         volatile Socket acceptingSocket = null;
+        volatile String testPath = null;
     }
 
     private Thread serverThread = null;
@@ -93,34 +96,28 @@ public class HttpUrlConnectionExpectContinueResponseMessageTest {
         Runnable runnable = () -> {
             while (!control.stop) {
                 try {
-                    control.acceptingSocket = control.serverSocket.accept();
-                    InputStream inputStream = control.acceptingSocket.getInputStream();
-                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    Socket socket = control.serverSocket.accept();
+                    String path = getPath(socket);
 
-                    StringBuilder stringBuilder = new StringBuilder();
+                    OutputStream outputStream;
+                    if (path.equals(control.testPath)) {
+                        control.acceptingSocket = socket;
+                        outputStream = control.acceptingSocket.getOutputStream();
 
-                    // Read initial request
-                    byte b;
-                    while (true) {
-                        b = (byte) inputStreamReader.read();
-                        stringBuilder.append((char) b);
-
-                        if (stringBuilder.length() >= 4) {
-                            char[] lastBytes = new char[4];
-                            stringBuilder.getChars(
-                                    stringBuilder.length() - 4,
-                                    stringBuilder.length(), lastBytes, 0);
-                            if (Arrays.equals(lastBytes, new char[]{'\r', '\n', '\r', '\n'})) {
-                                break;
-                            }
-                        }
+                        // send a wrong response and then shutdown
+                        outputStream.write(control.response.getBytes());
+                        outputStream.flush();
+                        control.acceptingSocket.shutdownOutput();
+                    } else {
+                        // stray request showed up, return 500 and close socket
+                        outputStream = socket.getOutputStream();
+                        outputStream.write("HTTP/1.1 500 Internal Server Error\r\n".getBytes());
+                        outputStream.write("Connection: close\r\n".getBytes());
+                        outputStream.write("Content-Length: 0\r\n".getBytes());
+                        outputStream.write("\r\n".getBytes());
+                        outputStream.flush();
+                        socket.close();
                     }
-                    OutputStream outputStream = control.acceptingSocket.getOutputStream();
-
-                    //send a wrong response
-                    outputStream.write(control.response.getBytes());
-                    outputStream.flush();
-                    control.acceptingSocket.shutdownOutput();
                 } catch (Exception e) {
                     // Any exceptions will be ignored
                 }
@@ -128,6 +125,22 @@ public class HttpUrlConnectionExpectContinueResponseMessageTest {
         };
         serverThread = new Thread(runnable);
         serverThread.start();
+    }
+
+    private static String getPath(Socket socket) throws IOException {
+        InputStream inputStream = socket.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        StringBuilder reqBuilder = new StringBuilder();
+        String line = null;
+        while (!(line = reader.readLine()).isEmpty()) {
+            reqBuilder.append(line + "\r\n");
+        }
+        String req = reqBuilder.toString();
+        StringTokenizer tokenizer = new StringTokenizer(req);
+        String method = tokenizer.nextToken();
+        String path = tokenizer.nextToken();
+        return path;
     }
 
     @AfterAll
@@ -146,8 +159,21 @@ public class HttpUrlConnectionExpectContinueResponseMessageTest {
         control.response = statusLine + "\r\n"
                 + "Content-Length: 0\r\n"
                 + "\r\n";
+        control.testPath = "/ContinueResponseMessageTest/" + expectedCode;
 
-        HttpURLConnection connection = createConnection();
+        URL url = URIBuilder.newBuilder()
+                .scheme("http")
+                .loopback()
+                .port(control.serverSocket.getLocalPort())
+                .path(control.testPath)
+                .toURL();
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Connection", "Close");
+        connection.setRequestProperty("Expect", "100-Continue");
+
         try {
             connection.setFixedLengthStreamingMode(body.getBytes().length);
             OutputStream outputStream = connection.getOutputStream();
@@ -166,22 +192,5 @@ public class HttpUrlConnectionExpectContinueResponseMessageTest {
                 String.format("Expected Response Message  %s, instead received %s",
                         expectedMessage, responseMessage));
         control.acceptingSocket.close();
-    }
-
-    // Creates a connection with all the common settings used in each test
-    private HttpURLConnection createConnection() throws Exception {
-        URL url = URIBuilder.newBuilder()
-                .scheme("http")
-                .loopback()
-                .port(control.serverSocket.getLocalPort())
-                .toURL();
-
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Connection", "Close");
-        connection.setRequestProperty("Expect", "100-Continue");
-
-        return connection;
     }
 }
