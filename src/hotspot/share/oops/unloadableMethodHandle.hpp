b27c0633163b3e599372caa3bbf8d3fb3422bf9e
Aleksey Shipilev <aleksey@shipilev.net>
2025-07-10 14:59:04 +0200

    Docs touchup

--- src/hotspot/share/oops/unloadableMethodHandle.hpp
+++ src/hotspot/share/oops/unloadableMethodHandle.hpp
@@ -35,16 +35,21 @@
 // of its holder.
 //
 // This handle can be in 2 states:
-//  1. Weak. Method* is present, but its holder is only weakly-reachable, and can
+//  1. Unsafe (weak). Method* is present, but its holder is only weakly-reachable, and can
 //     be unloaded. Users need to check is_safe() before calling method().
 //     method() is safe to call iff we have not crossed a safepoint since construction
 //     or last is_safe() check. Calling make_always_safe() after is_safe() check
 //     moves handle to the strong state.
-//  2. Strong. Method* holder is strongly reachable, cannot be unloaded.
+//  2. Safe (strong). Method* holder is strongly reachable, cannot be unloaded.
 //     Calling method() is always safe in this state.
 //
 // The handle transitions are one-shot:
-//    weak   --(make_always_safe) --> strong
+//    unsafe (weak) --(make_always_safe) --> safe (strong)
+//
+// There are internal shortcuts that bypass this mechanics when handle knows
+// the method holder is permanent and would not be unloaded. This is an implementation
+// detail, it does not change any external contract. Using this handle for permanent
+// method holders provides future safety.
 //
 // Common usage pattern:
 //
