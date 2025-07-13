dbf7edb0a34550c429e62d96fc3b5e793013bea1
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-07-13 10:09:19 +0100

    Document NPE behaviour and update test and add class-level statement

--- test/jdk/javax/swing/ImageIcon/ImageIconNullImageTest.java
+++ test/jdk/javax/swing/ImageIcon/ImageIconNullImageTest.java
@@ -28,13 +28,189 @@
  * @run main ImageIconNullImageTest
  */
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+import java.awt.image.BufferedImage;
 import java.awt.Image;
+import java.awt.Toolkit;
 import javax.swing.ImageIcon;
 
 public class ImageIconNullImageTest {
+    static File file;
 
     public static void main(String[] args) throws Exception {
+        testImageIconFileDesc(false);
+        testImageIconFile(false);
+        testImageIconURLDesc(false);
+        testImageIconURL(false);
+        testImageIconImageDesc(false);
+        testImageIconNull();
+        testImageIconByteDesc(false);
+        testImageIconByte(false);
         testSetImageNull();
+        testInvalid();
+    }
+
+    private static void testInvalid() throws Exception {
+        try {
+            String imgName = "invalid.gif";
+            byte[] invalidData = new byte[100];
+            new Random().nextBytes(invalidData);
+            try (FileOutputStream fos = new FileOutputStream(imgName)) {
+                fos.write(invalidData);
+            }
+            file = new File(System.getProperty("test.src", "."), imgName);
+            testImageIconFileDesc(true);
+            testImageIconFile(true);
+            testImageIconURLDesc(true);
+            testImageIconURL(true);
+            testImageIconImageDesc(true);
+            testImageIconByteDesc(true);
+            testImageIconByte(true);
+        } finally {
+            file.deleteOnExit();
+        }
+    }
+
+    private static void testImageIconFileDesc(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            try {
+                new ImageIcon((String)null, "gif");
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(FileName,Desc) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon(file.getName(), "gif");
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(FileName,Desc) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconFile(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            try {
+                new ImageIcon((String)null);
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(FileName) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon(file.getName());
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(FileName) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconURLDesc(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            try {
+                new ImageIcon((java.net.URL)null, "gif");
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(URL, Desc) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon("file://invalid.gif", "gif");
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(URL, Desc) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconURL(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            try {
+                new ImageIcon((java.net.URL)null);
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(URL) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon("file://invalid.gif");
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(URL) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconImageDesc(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            try {
+                new ImageIcon((Image) null, "gif");
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(Image, Desc) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon((Image)
+                        Toolkit.getDefaultToolkit().createImage(file.getName()), "gif");
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(Image, Desc) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconNull() {
+        // Passing null image shouldn't cause NPE
+        try {
+            new ImageIcon((Image) null);
+        } catch (NullPointerException e) {
+            System.out.println("null ImageIcon(image) throws NPE");
+        }
+    }
+
+    private static void testImageIconByteDesc(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            byte[] imageData = null;
+            try {
+                new ImageIcon(imageData, "gif");
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(byte[], desc) throws NPE");
+            }
+        } else {
+            try {
+               ImageIcon icon = new ImageIcon(new byte[0], "gif");
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(byte[], desc) throws NPE");
+            }
+        }
+    }
+
+    private static void testImageIconByte(boolean invalid) {
+        // Passing null image shouldn't cause NPE
+        if (!invalid) {
+            byte[] imageData = null;
+            try {
+                new ImageIcon(imageData);
+            } catch (NullPointerException e) {
+                System.out.println("null ImageIcon(byte[]) throws NPE");
+            }
+        } else {
+            try {
+                ImageIcon icon = new ImageIcon(new byte[0]);
+                System.out.println(icon.getImageLoadStatus());
+            } catch (NullPointerException e) {
+                System.out.println("invalid ImageIcon(byte[], desc) throws NPE");
+            }
+        }
     }
 
     private static void testSetImageNull() {
