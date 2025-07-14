25940a3afd3ba51eac354707f3e8600d81aabe84
Xiaolong Peng <xlpeng@amazon.com>
2025-07-14 08:22:27 -0700

    Don't eagerly steal allocate when GC state is not stable

--- src/hotspot/share/gc/shenandoah/shenandoahFreeSet.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahFreeSet.cpp
@@ -2211,7 +2211,7 @@ HeapWord* ShenandoahFreeSet::try_allocate_single_for_mutator(ShenandoahAllocRequ
     }
     uint steal_alloc_start_idx = (start_idx + max_probes) % ShenandoahDirectlyAllocatableRegionCount;
     uint steal_alloc_probes = ShenandoahDirectlyAllocatableRegionCount - max_probes;
-    if (!any_replacement_eligible && ShenandoahDirectAllocationStealingAllowed) {
+    if (!any_replacement_eligible && _heap->is_gc_state(ShenandoahHeap::GCState::STABLE)) {
       // After probing max_probes times with CAS alloc failure, if there is no region eligible for replacement,
       // Taking the lock and try to allocate direct allocation region will unlikely to successfully to replace any of them,
       // therefore, it tries steal space from other allocation regions first.
@@ -2227,7 +2227,7 @@ HeapWord* ShenandoahFreeSet::try_allocate_single_for_mutator(ShenandoahAllocRequ
     }
 
     if (!try_allocate_directly_allocatable_regions(start_idx, steal_alloc_done, req, obj, in_new_region)) {
-      if (obj == nullptr && ShenandoahDirectAllocationStealingAllowed) {
+      if (obj == nullptr) {
         obj = cas_allocate_single_for_mutator<IS_TLAB>(steal_alloc_start_idx,
                                                        steal_alloc_probes,
                                                        req,
