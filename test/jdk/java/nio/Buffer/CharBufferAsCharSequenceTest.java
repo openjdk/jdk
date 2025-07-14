fd92e8de9284c0173bf46905518282d1e9110e68
Brett Okken <brett.okken@oracle.com>
2025-07-14 07:37:29 -0500

    whitespace

--- test/jdk/java/nio/Buffer/CharBufferAsCharSequenceTest.java
+++ test/jdk/java/nio/Buffer/CharBufferAsCharSequenceTest.java
@@ -20,7 +20,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
- 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.CharBuffer;
