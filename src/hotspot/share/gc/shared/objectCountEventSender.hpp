a135b2ddfa061e478969194574ec8a7489f02404
Patrick Fontanilla <pfon@amazon.com>
2025-07-16 19:41:06 +0000

    Minimize code duplication by adding templates to ObjectCountEvent sender

--- src/hotspot/share/gc/shared/objectCountEventSender.hpp
+++ src/hotspot/share/gc/shared/objectCountEventSender.hpp
@@ -1,33 +1,9 @@
-/*
- * Copyright (c) 2013, 2022, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- *
- */
-
 #ifndef SHARE_GC_SHARED_OBJECTCOUNTEVENTSENDER_HPP
 #define SHARE_GC_SHARED_OBJECTCOUNTEVENTSENDER_HPP
 
-#include "gc/shared/gcTrace.hpp"
-#include "memory/allStatic.hpp"
-#include "utilities/globalDefinitions.hpp"
+#include "gc/shared/gcId.hpp"
+#include "jfr/jfrEvents.hpp"
+#include "memory/heapInspection.hpp"
 #include "utilities/macros.hpp"
 #include "utilities/ticks.hpp"
 
@@ -36,7 +12,8 @@
 class KlassInfoEntry;
 class Klass;
 
-class ObjectCountEventSender : public AllStatic {
+template <typename EventT>
+class ObjectCountEventSenderTemplate : public AllStatic {
   static bool _should_send_requestable_event;
 
   template <typename T>
@@ -50,6 +27,55 @@ class ObjectCountEventSender : public AllStatic {
   static bool should_send_event();
 };
 
+typedef ObjectCountEventSenderTemplate<EventObjectCount> ObjectCountEventSender;
+typedef ObjectCountEventSenderTemplate<EventObjectCountAfterGC> ObjectCountAfterGCEventSender;
+
+template <typename EventT>
+bool ObjectCountEventSenderTemplate<EventT>::should_send_event() {
+#if INCLUDE_JFR
+  return _should_send_requestable_event || EventT::is_enabled();
+#else
+  return false;
+#endif // INCLUDE_JFR
+}
+
+template <typename EventT>
+bool ObjectCountEventSenderTemplate<EventT>::_should_send_requestable_event = false;
+
+template <typename EventT>
+void ObjectCountEventSenderTemplate<EventT>::enable_requestable_event() {
+  _should_send_requestable_event = true;
+}
+
+
+template <typename EventT>
+void ObjectCountEventSenderTemplate<EventT>::disable_requestable_event() {
+  _should_send_requestable_event = false;
+}
+
+template <typename EventT>
+template <typename T>
+void ObjectCountEventSenderTemplate<EventT>::send_event_if_enabled(Klass* klass, jlong count, julong size, const Ticks& timestamp) {
+  T event(UNTIMED);
+  if (event.should_commit()) {
+    event.set_starttime(timestamp);
+    event.set_endtime(timestamp);
+    event.set_gcId(GCId::current());
+    event.set_objectClass(klass);
+    event.set_count(count);
+    event.set_totalSize(size);
+    event.commit();
+  }
+}
+
+template <typename EventT>
+void ObjectCountEventSenderTemplate<EventT>::send(const KlassInfoEntry* entry, const Ticks& timestamp) {
+  Klass* klass = entry->klass();
+  jlong count = entry->count();
+  julong total_size = entry->words() * BytesPerWord;
+  
+  send_event_if_enabled<EventT>(klass, count, total_size, timestamp);
+}
 #endif // INCLUDE_SERVICES
 
 #endif // SHARE_GC_SHARED_OBJECTCOUNTEVENTSENDER_HPP
