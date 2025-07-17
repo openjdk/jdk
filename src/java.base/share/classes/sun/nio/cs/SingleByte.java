7a6cd39d4367bd2ee0774e247dbcafc59e9b86bb
Volkan Yazıcı <volkan.yazici@oracle.com>
2025-07-17 15:01:08 +0200

    Fix out-of-bounds in `sun.nio.cs.SingleByte.Encoder::encodeArrayLoop`

--- src/java.base/share/classes/sun/nio/cs/SingleByte.java
+++ src/java.base/share/classes/sun/nio/cs/SingleByte.java
@@ -216,7 +216,7 @@ public class SingleByte
             int dl = dst.arrayOffset() + dst.limit();
             int len = Math.min(dl - dp, sl - sp);
 
-            if (isASCIICompatible) {
+            if (isASCIICompatible && dst.hasRemaining()) {
                 int n = JLA.encodeASCII(sa, sp, da, dp, len);
                 sp += n;
                 dp += n;
