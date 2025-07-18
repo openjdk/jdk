4bdf7d8cb22e958baec8f64b97b74887de92c1dd
tstuefe <thomas.stuefe@gmail.com>
2025-07-18 17:51:44 +0200

    wip

--- src/hotspot/share/utilities/vmError.cpp
+++ src/hotspot/share/utilities/vmError.cpp
@@ -60,6 +60,7 @@
 #include "runtime/vmOperations.hpp"
 #include "runtime/vmThread.hpp"
 #include "runtime/vm_version.hpp"
+#include "services/shorthist.hpp"
 #include "sanitizers/ub.hpp"
 #include "utilities/debug.hpp"
 #include "utilities/decoder.hpp"
@@ -1296,6 +1297,10 @@ void VMError::report(outputStream* st, bool _verbose) {
     NativeHeapTrimmer::print_state(st);
     st->cr();
 
+  STEP_IF("ShortHistory", _verbose)
+    ShortHistory::print(st, true);
+    st->cr();
+
   STEP_IF("printing system", _verbose)
     st->print_cr("---------------  S Y S T E M  ---------------");
     st->cr();
@@ -1495,6 +1500,9 @@ void VMError::print_vm_info(outputStream* st) {
   NativeHeapTrimmer::print_state(st);
   st->cr();
 
+  // STEP("ShortHistory")
+  ShortHistory::print(st, false);
+  st->cr();
 
   // STEP("printing system")
   st->print_cr("---------------  S Y S T E M  ---------------");
