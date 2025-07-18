f0888ceab15a2f9c94efa689d07cc727006d0d19
MBaesken <matthias.baesken@sap.com>
2025-07-18 10:17:47 +0200

    Simplify test coding

--- test/hotspot/gtest/gc/g1/test_freeRegionList.cpp
+++ test/hotspot/gtest/gc/g1/test_freeRegionList.cpp
@@ -44,9 +44,8 @@ TEST_OTHER_VM(G1FreeRegionList, length) {
 
   // Create a fake heap. It does not need to be valid, as the G1HeapRegion constructor
   // does not access it.
-  int val = 1;
-  HeapWord* ptr = reinterpret_cast<HeapWord*>(&val);
-  ptr = align_up(ptr, os::vm_page_size());
+  HeapWord val{};
+  HeapWord* ptr = align_up(&val, os::vm_page_size());
 
   MemRegion heap(ptr, num_regions_in_test * G1HeapRegion::GrainWords);
 
