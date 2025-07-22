27991b2029ad0d0b417ef14175b2d0cbdb7c5224
Chad Rakoczy <chadrako@amazon.com>
2025-07-22 00:08:01 +0000

    Rename method to nm

--- src/hotspot/share/code/codeBehaviours.cpp
+++ src/hotspot/share/code/codeBehaviours.cpp
@@ -29,18 +29,18 @@
 
 CompiledICProtectionBehaviour* CompiledICProtectionBehaviour::_current = nullptr;
 
-bool DefaultICProtectionBehaviour::lock(nmethod* method) {
-  if (is_safe(method)) {
+bool DefaultICProtectionBehaviour::lock(nmethod* nm) {
+  if (is_safe(nm)) {
     return false;
   }
   CompiledIC_lock->lock_without_safepoint_check();
   return true;
 }
 
-void DefaultICProtectionBehaviour::unlock(nmethod* method) {
+void DefaultICProtectionBehaviour::unlock(nmethod* nm) {
   CompiledIC_lock->unlock();
 }
 
-bool DefaultICProtectionBehaviour::is_safe(nmethod* method) {
-  return SafepointSynchronize::is_at_safepoint() || CompiledIC_lock->owned_by_self() || method->is_not_installed();
+bool DefaultICProtectionBehaviour::is_safe(nmethod* nm) {
+  return SafepointSynchronize::is_at_safepoint() || CompiledIC_lock->owned_by_self() || nm->is_not_installed();
 }
