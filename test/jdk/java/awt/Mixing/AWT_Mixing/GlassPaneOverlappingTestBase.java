09c742f078f1d9bf366fbe2d769f02929d9d2c15
Khalid Boulanouare <khalid.boulanouare@oracle.com>
2025-07-23 15:38:05 +0100

    Uses frame requestFocus instead of robot clicks for focus

--- test/jdk/java/awt/Mixing/AWT_Mixing/GlassPaneOverlappingTestBase.java
+++ test/jdk/java/awt/Mixing/AWT_Mixing/GlassPaneOverlappingTestBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -21,12 +21,21 @@
  * questions.
  */
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Point;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.InputEvent;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.FocusManager;
 import javax.swing.JFrame;
 import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
@@ -106,14 +115,35 @@ public abstract class GlassPaneOverlappingTestBase extends SimpleOverlappingTest
         if (!super.performTest()) {
             return false;
         }
+        final CountDownLatch latch = new CountDownLatch(1);
+        f.addFocusListener(new FocusAdapter() {
+            @Override public void focusGained(FocusEvent e) {
+                latch.countDown();
+            }
+        });
         if (testResize) {
             wasLWClicked = false;
             try {
                 SwingUtilities.invokeAndWait(new Runnable() {
 
                     public void run() {
-                        testedComponent.setBounds(0, 0, testedComponent.getPreferredSize().width, testedComponent.getPreferredSize().height + 20);
+                        testedComponent.setBounds(0, 0,
+                                testedComponent.getPreferredSize().width,
+                                testedComponent.getPreferredSize().height + 20);
                         ancestorLoc = f.getLocationOnScreen();
+                        System.out.println(
+                                "frame focusable? " + f.isFocusable());
+                        boolean focus = f.requestFocusInWindow();
+                        if (!focus) {
+                            Component focusOwner =
+                                    FocusManager.getCurrentManager()
+                                            .getFocusOwner();
+                            if (focusOwner == f) {
+                                //frame already had focus.
+                                latch.countDown();
+                            }
+                        }
+
                     }
                 });
             } catch (InterruptedException ex) {
@@ -123,20 +153,17 @@ public abstract class GlassPaneOverlappingTestBase extends SimpleOverlappingTest
             }
             Point lLoc = testedComponent.getLocationOnScreen();
             lLoc.translate(1, testedComponent.getPreferredSize().height + 1);
-
-            /* this is a workaround for certain jtreg(?) focus issue:
-               tests fail starting after failing mixing tests but always pass alone.
-             */
-            Util.waitForIdle(robot);
-            ancestorLoc.translate(isOel7orLater() ? 5 : f.getWidth() / 2 - 15, 2);
-            robot.mouseMove(ancestorLoc.x, ancestorLoc.y);
-            Util.waitForIdle(robot);
-            robot.mousePress(InputEvent.BUTTON1_MASK);
-            robot.delay(50);
-            robot.mouseRelease(InputEvent.BUTTON1_MASK);
-            Util.waitForIdle(robot);
-
-            clickAndBlink(robot, lLoc);
+            try {
+                boolean await = latch.await(50, TimeUnit.MILLISECONDS);
+                if (!await) {
+                    throw new RuntimeException(
+                            "Ancestor frame didn't receive " +
+                                    "focus");
+                }
+                clickAndBlink(robot, lLoc);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             return wasLWClicked;
         } else {
             return true;
