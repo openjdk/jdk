23a86e36c079e2eca13927c01d81f62484a25dcb
William Kemper <kemperw@amazon.com>
2025-07-23 16:13:11 -0700

    Fix merge error

--- src/hotspot/share/gc/shenandoah/shenandoahGenerationalHeap.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahGenerationalHeap.hpp
@@ -36,7 +36,6 @@ class ShenandoahGenerationalControlThread;
 class ShenandoahAgeCensus;
 
 class ShenandoahGenerationalHeap : public ShenandoahHeap {
-  void print_tracing_info() const override;
   void stop() override;
 
 public:
