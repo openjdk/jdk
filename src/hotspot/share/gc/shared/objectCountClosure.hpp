1c371b26d6fca5dbb42c63c9fefae542f1ed6740
Patrick Fontanilla <pfon@amazon.com>
2025-07-23 22:18:56 +0000

    Removed inheritance from objectCountClosure

--- src/hotspot/share/gc/shared/objectCountClosure.hpp
+++ src/hotspot/share/gc/shared/objectCountClosure.hpp
@@ -11,7 +11,7 @@
 class KlassInfoEntry;
 class Klass;
 
-class ObjectCountClosure : public ObjectCountEventSender {
+class ObjectCountClosure : public AllStatic {
     static KlassInfoTable cit;
 
 public:
@@ -19,6 +19,11 @@ public:
     static bool record_object(oop o);
     static KlassInfoTable* get_table();
     static void reset_table();
+
+    template <class Event>
+    static bool should_send_event() {
+        return ObjectCountEventSender::should_send_event<Event>();
+    }
 };
 
 #endif // SHARE_GC_SHARED_OBJECTCOUNTCLOSURE_HPP
