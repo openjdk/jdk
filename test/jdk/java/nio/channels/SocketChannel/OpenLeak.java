c9a05bd664946c4360a0ba037c00ee01f3491f6c
Michael-Mc-Mahon <michael.x.mcmahon@oracle.com>
2025-07-25 15:21:12 +0100

    revert test change

--- test/jdk/java/nio/channels/SocketChannel/OpenLeak.java
+++ test/jdk/java/nio/channels/SocketChannel/OpenLeak.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,7 +22,7 @@
  */
 
 /* @test
- * @bug 6548464 8362581
+ * @bug 6548464
  * @summary SocketChannel.open(SocketAddress) leaks file descriptor if
  *     connection cannot be established
  * @requires vm.flagless
