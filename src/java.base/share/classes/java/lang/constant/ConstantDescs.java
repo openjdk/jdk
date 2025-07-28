c7885d0f588ece9e132fb168841ae921d153d87e
Chen Liang <chen.l.liang@oracle.com>
2025-07-28 17:14:23 -0500

    Move AsTypeMethodHandleDesc to constant API impl package

--- src/java.base/share/classes/java/lang/constant/ConstantDescs.java
+++ src/java.base/share/classes/java/lang/constant/ConstantDescs.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,7 +43,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static java.lang.constant.DirectMethodHandleDesc.*;
 import static java.lang.constant.DirectMethodHandleDesc.Kind.STATIC;
 
 /**
@@ -336,9 +335,6 @@ public final class ConstantDescs {
      */
     public static final MethodTypeDesc MTD_void = MethodTypeDesc.of(CD_void);
 
-    static final DirectMethodHandleDesc MHD_METHODHANDLE_ASTYPE
-            = MethodHandleDesc.ofMethod(Kind.VIRTUAL, CD_MethodHandle, "asType",
-                                        MethodTypeDesc.of(CD_MethodHandle, CD_MethodType));
     /**
      * Returns a {@link MethodHandleDesc} corresponding to a bootstrap method for
      * an {@code invokedynamic} callsite, which is a static method whose leading
