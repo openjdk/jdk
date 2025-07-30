8e3ddcd7f7e7f05c6d1034b031a1fa084ba05db2
Lei Zhu <korov9.c@gmail.com>
2025-07-30 21:32:12 +0800

    8362533: Tests sun/management/jmxremote/bootstrap/* duplicate VM flags

--- test/jdk/sun/management/jmxremote/bootstrap/LocalManagementTest.java
+++ test/jdk/sun/management/jmxremote/bootstrap/LocalManagementTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,12 +24,10 @@
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import jdk.test.lib.process.ProcessTools;
-import jdk.test.lib.Utils;
 
 /**
  * @test
@@ -99,7 +97,6 @@ public class LocalManagementTest {
     private static boolean doTest(String testId, String arg) throws Exception {
         List<String> args = new ArrayList<>();
         args.add("-XX:+UsePerfData");
-        Collections.addAll(args, Utils.getTestJavaOpts());
         args.add("-cp");
         args.add(TEST_CLASSPATH);
 
@@ -108,7 +105,7 @@ public class LocalManagementTest {
         }
         args.add("TestApplication");
         ProcessBuilder server = ProcessTools.createTestJavaProcessBuilder(
-            args.toArray(new String[args.size()])
+            args.toArray(new String[0])
         );
 
         Process serverPrc = null, clientPrc = null;
