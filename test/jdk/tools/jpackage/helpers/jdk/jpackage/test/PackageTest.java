ff64379899c930bc229feee33667f27b9ef84f5e
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-08-04 00:45:23 -0400

    Use java.time.Duration and java.time.Instant in TKit.waitForFileCreated(). Make it public.

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PackageTest.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/PackageTest.java
@@ -36,6 +36,7 @@ import java.awt.GraphicsEnvironment;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -296,13 +297,9 @@ public final class PackageTest extends RunnablePackageTest {
                 Files.deleteIfExists(appOutput);
 
                 List<String> expectedArgs = testRun.openFiles(testFiles);
-                TKit.waitForFileCreated(appOutput, 7);
+                TKit.waitForFileCreated(appOutput, Duration.ofSeconds(7), Duration.ofSeconds(3));
 
-                // Wait a little bit after file has been created to
-                // make sure there are no pending writes into it.
-                Thread.sleep(3000);
-                HelloApp.verifyOutputFile(appOutput, expectedArgs,
-                        Collections.emptyMap());
+                HelloApp.verifyOutputFile(appOutput, expectedArgs, Map.of());
             });
 
             if (isOfType(cmd, WINDOWS)) {
