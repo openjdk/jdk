3bae7719825cf1c400d32ac14e2ed793ea19e58c
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-08-04 14:15:45 -0400

    ModelAsserter: adhoc comparison of model classes created from params and options; copy ObjectMapper from test/jdk/tools/jpackage/helpers/jdk/jpackage/test/ObjectMapper.java

--- src/jdk.jpackage/share/classes/jdk/jpackage/internal/DefaultBundlingEnvironment.java
+++ src/jdk.jpackage/share/classes/jdk/jpackage/internal/DefaultBundlingEnvironment.java
@@ -127,6 +127,10 @@ class DefaultBundlingEnvironment implements CliBundlingEnvironment {
 
         final var outputDir = OptionUtils.outputDir(optionValues).resolve(app.appImageDirName());
 
+        ModelAsserter.instance().notifyFromOptions(app);
+        ModelAsserter.instance().assertEquals(outputDir.getParent());
+
+/*
         IOUtils.writableOutputDir(outputDir.getParent());
 
         final var env = new BuildEnvFromOptions()
@@ -141,6 +145,7 @@ class DefaultBundlingEnvironment implements CliBundlingEnvironment {
 
         pipelineBuilder.excludeDirFromCopying(outputDir.getParent())
                 .create().execute(BuildEnv.withAppImageDir(env, outputDir), app);
+*/
     }
 
     static <T extends Package> void createNativePackage(Options optionValues,
@@ -167,11 +172,15 @@ class DefaultBundlingEnvironment implements CliBundlingEnvironment {
 
         var pkg = Objects.requireNonNull(createPackage.apply(optionValues));
 
+        ModelAsserter.instance().notifyFromOptions(pkg);
+        ModelAsserter.instance().assertEquals(OptionUtils.outputDir(optionValues));
+/*
         Packager.<T>build().pkg(pkg)
             .outputDir(OptionUtils.outputDir(optionValues))
             .env(Objects.requireNonNull(createBuildEnv.apply(optionValues, pkg)))
             .pipelineBuilderMutatorFactory(pipelineBuilderMutatorFactory)
             .execute(Objects.requireNonNull(createPipelineBuilder.apply(pkg)));
+*/
     }
 
     @Override
