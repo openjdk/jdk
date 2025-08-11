87f4700a27cc07ce0f73cfbeba468ad11e240ccb
Yudi Zheng <yudi.zheng@oracle.com>
2025-08-11 15:53:00 +0200

    [JVMCI] AArch64 CPU features are not computed correctly after 8364128

--- src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/hotspot/HotSpotJVMCIBackendFactory.java
+++ src/jdk.internal.vm.ci/share/classes/jdk/vm/ci/hotspot/HotSpotJVMCIBackendFactory.java
@@ -54,6 +54,47 @@ public interface HotSpotJVMCIBackendFactory {
      *             enum value
      * @return the set of converted values
      */
+    static <CPUFeatureType extends Enum<CPUFeatureType>> EnumSet<CPUFeatureType> convertFeaturesViaConstantMasks(
+            Class<CPUFeatureType> enumType,
+            Map<String, Long> constants,
+            long features,
+            Map<String, String> renaming) {
+        EnumSet<CPUFeatureType> outFeatures = EnumSet.noneOf(enumType);
+        List<String> missing = new ArrayList<>();
+        for (Entry<String, Long> e : constants.entrySet()) {
+            long bitMask = e.getValue();
+            String key = e.getKey();
+            if (key.startsWith("VM_Version::CPU_")) {
+                String name = key.substring("VM_Version::CPU_".length());
+                try {
+                    CPUFeatureType feature = Enum.valueOf(enumType, renaming.getOrDefault(name, name));
+                    if ((features & bitMask) != 0) {
+                        outFeatures.add(feature);
+                    }
+                } catch (IllegalArgumentException iae) {
+                    missing.add(name);
+                }
+            }
+        }
+        if (!missing.isEmpty()) {
+            throw new JVMCIError("Missing CPU feature constants: %s", missing);
+        }
+        return outFeatures;
+    }
+
+    /**
+     * Converts a bit mask of CPU features to enum constants.
+     *
+     * @param <CPUFeatureType> CPU feature enum type
+     * @param enumType the class of {@code CPUFeatureType}
+     * @param constants VM constants. Each entry whose key starts with {@code "VM_Version::CPU_"}
+     *            specifies a CPU feature and its value is a mask for a bit in {@code features}
+     * @param features bits specifying CPU features
+     * @param renaming maps from VM feature names to enum constant names where the two differ
+     * @throws IllegalArgumentException if any VM CPU feature constant cannot be converted to an
+     *             enum value
+     * @return the set of converted values
+     */
     static <CPUFeatureType extends Enum<CPUFeatureType>> EnumSet<CPUFeatureType> convertFeatures(
                     Class<CPUFeatureType> enumType,
                     Map<String, Long> constants,
@@ -62,7 +103,8 @@ public interface HotSpotJVMCIBackendFactory {
         EnumSet<CPUFeatureType> outFeatures = EnumSet.noneOf(enumType);
         List<String> missing = new ArrayList<>();
         for (Entry<String, Long> e : constants.entrySet()) {
-            long bitMask = e.getValue();
+            long bitIndex = e.getValue();
+            long bitMask = 1L << bitIndex;
             String key = e.getKey();
             if (key.startsWith("VM_Version::CPU_")) {
                 String name = key.substring("VM_Version::CPU_".length());
