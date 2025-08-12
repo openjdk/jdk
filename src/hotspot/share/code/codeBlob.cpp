5a994a7592222234ba78de4a52759064b9b98b1f
Aleksey Shipilev <aleksey@shipilev.net>
2025-08-12 16:01:30 +0200

    Unsigned overflow is not UB

--- src/hotspot/share/code/codeBlob.cpp
+++ src/hotspot/share/code/codeBlob.cpp
@@ -451,7 +451,7 @@ AdapterBlob::AdapterBlob(int size, CodeBuffer* cb, int entry_offset[AdapterBlob:
   assert(entry_offset[0] == 0, "sanity check");
   for (int i = 1; i < AdapterBlob::ENTRY_COUNT; i++) {
     assert((entry_offset[i] > 0 && entry_offset[i] < cb->insts()->size()) || // Within the adapter blob
-           (entry_offset[i] + p2i(cb->insts_begin()) == 0),                  // Or wraps back to nullptr
+           (p2u(cb->insts_begin()) + (uintptr_t)entry_offset[i] == 0),       // Or wraps back to nullptr
            "invalid entry offset[%d] = 0x%x", i, entry_offset[i]);
   }
   _c2i_offset = entry_offset[1];
