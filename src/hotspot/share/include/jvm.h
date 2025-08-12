0b1ec69c6091859895e6c3be7d2bd3c631035a57
Coleen Phillimore <coleenp@openjdk.org>
2025-08-12 16:30:09 +0000

    8364750: Remove unused declaration in jvm.h
    Reviewed-by: shade

--- src/hotspot/share/include/jvm.h
+++ src/hotspot/share/include/jvm.h
@@ -599,16 +599,6 @@ JVM_GetClassDeclaredFields(JNIEnv *env, jclass ofClass, jboolean publicOnly);
 JNIEXPORT jobjectArray JNICALL
 JVM_GetClassDeclaredConstructors(JNIEnv *env, jclass ofClass, jboolean publicOnly);
 
-
-/* Differs from JVM_GetClassModifiers in treatment of inner classes.
-   This returns the access flags for the class as specified in the
-   class file rather than searching the InnerClasses attribute (if
-   present) to find the source-level access flags. Only the values of
-   the low 13 bits (i.e., a mask of 0x1FFF) are guaranteed to be
-   valid. */
-JNIEXPORT jint JNICALL
-JVM_GetClassAccessFlags(JNIEnv *env, jclass cls);
-
 /* Nestmates - since JDK 11 */
 
 JNIEXPORT jboolean JNICALL
