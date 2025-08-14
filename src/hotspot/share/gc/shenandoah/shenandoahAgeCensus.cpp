c6a3467483eb208da6b5dc9835a2f5c891d4da51
William Kemper <kemperw@amazon.com>
2025-08-14 16:36:08 -0700

    Tone down some log messages, draw the tenuring threshold line above the first cohort that would be tenured

--- src/hotspot/share/gc/shenandoah/shenandoahAgeCensus.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahAgeCensus.cpp
@@ -352,13 +352,6 @@ void ShenandoahAgeCensus::print() {
     const size_t prev_pop = prev_pv->sizes[i-1];  // (i-1) OK because i >= 1
     const size_t cur_pop  = cur_pv->sizes[i];
     double mr = mortality_rate(prev_pop, cur_pop);
-    // Suppress printing when everything is zero
-    if (prev_pop + cur_pop > 0) {
-      log_info(gc, age)
-        (" - age %3u: prev %10zu bytes, curr %10zu bytes, mortality %.2f ",
-         i, prev_pop*oopSize, cur_pop*oopSize, mr);
-    }
-    total += cur_pop;
 
     if ( i >= tt) {
       total_tenurable += cur_pop;
@@ -367,6 +360,14 @@ void ShenandoahAgeCensus::print() {
         log_info(gc, age)("----------------------------------------------------------------------------");
       }
     }
+
+    // Suppress printing when everything is zero
+    if (prev_pop + cur_pop > 0) {
+      log_info(gc, age)
+        (" - age %3u: prev %10zu bytes, curr %10zu bytes, mortality %.2f ",
+         i, prev_pop*oopSize, cur_pop*oopSize, mr);
+    }
+    total += cur_pop;
   }
 
   log_info(gc, age)("%.3f of population meets tenuring threshold (%u). Total: (%zu), Tenurable: (%zu)",
--- src/hotspot/share/gc/shenandoah/shenandoahThreadLocalData.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahThreadLocalData.hpp
@@ -200,12 +200,12 @@ public:
   }
 
   static void enable_plab_promotions(Thread* thread) {
-    log_debug(gc, plab)("Enable PLAB promotions for thread: %d (java? %s)", thread->osthread()->thread_id(), BOOL_TO_STR(thread->is_Java_thread()));
+    log_develop_trace(gc, plab)("Enable PLAB promotions for thread: %d (java? %s)", thread->osthread()->thread_id(), BOOL_TO_STR(thread->is_Java_thread()));
     data(thread)->_plab_allows_promotion = true;
   }
 
   static void disable_plab_promotions(Thread* thread) {
-    log_debug(gc, plab)("Disable PLAB promotions for thread: %d", thread->osthread()->thread_id());
+    log_develop_trace(gc, plab)("Disable PLAB promotions for thread: %d", thread->osthread()->thread_id());
     data(thread)->_plab_allows_promotion = false;
   }
 
