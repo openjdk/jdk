c4201272bd044c401776a6bcedae1e98970fe58a
tstuefe <thomas.stuefe@gmail.com>
2025-08-15 17:17:23 +0200

    throttle unsafe.allocate too

--- src/hotspot/share/jfr/recorder/service/jfrEventThrottler.cpp
+++ src/hotspot/share/jfr/recorder/service/jfrEventThrottler.cpp
@@ -39,7 +39,8 @@ constexpr static const JfrSamplerParams _disabled_params = {
                                                            };
 
 constexpr static const JfrEventId throttleble_events[] = {
-    JfrCPUTimeSampleEvent, JfrObjectAllocationSampleEvent,  JfrSafepointLatencyEvent
+    JfrCPUTimeSampleEvent, JfrObjectAllocationSampleEvent,  JfrSafepointLatencyEvent,
+    JfrUnsafeAllocateMemoryEvent
 };
 constexpr static int num_throttled_events = sizeof(throttleble_events) / sizeof(throttleble_events[0]);
 
