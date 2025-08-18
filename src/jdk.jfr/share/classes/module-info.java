f713c352f1e8abfc00ffee88419a49376150ac7e
Jaikiran Pai <jpai@openjdk.org>
2025-08-18 13:40:42 +0000

    8365533: Remove outdated jdk.internal.javac package export to several modules from java.base
    Reviewed-by: alanb, liach

--- src/jdk.jfr/share/classes/module-info.java
+++ src/jdk.jfr/share/classes/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,15 +23,12 @@
  * questions.
  */
 
-import jdk.internal.javac.ParticipatesInPreview;
-
 /**
  * Defines the API for JDK Flight Recorder.
  *
  * @moduleGraph
  * @since 9
  */
-@ParticipatesInPreview
 module jdk.jfr {
     exports jdk.jfr;
     exports jdk.jfr.consumer;
