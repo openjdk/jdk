f6a21057bb653ed1e5ba8a68f36980237d565749
Jayathirth D V <jdv@openjdk.org>
2025-08-18 10:25:12 +0000

    8364214: Enhance polygon data support
    Reviewed-by: rhalade, psadhukhan, mschoene, prr

--- src/java.desktop/share/classes/sun/java2d/SunGraphics2D.java
+++ src/java.desktop/share/classes/sun/java2d/SunGraphics2D.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1996, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1996, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -1901,9 +1901,9 @@ public final class SunGraphics2D
         if (usrClip == null) {
             clipState = CLIP_DEVICE;
             clipRegion = devClip;
-        } else if (usrClip instanceof Rectangle2D) {
+        } else if (usrClip instanceof Rectangle2D clip) {
             clipState = CLIP_RECTANGULAR;
-            clipRegion = devClip.getIntersection((Rectangle2D) usrClip);
+            clipRegion = devClip.getIntersection(clip);
         } else {
             PathIterator cpi = usrClip.getPathIterator(null);
             int[] box = new int[4];
