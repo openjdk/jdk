2672c360e994e0f522cc01068a6e9ff6b409b208
Amit Kumar <amitlocham09@icloud.com>
2025-08-20 15:07:05 +0530

    adds comment for larger size requirement

--- test/hotspot/jtreg/compiler/startup/StartupOutput.java
+++ test/hotspot/jtreg/compiler/startup/StartupOutput.java
@@ -61,6 +61,8 @@ public class StartupOutput {
             throw new Exception("VM crashed with exit code " + exitCode);
         }
 
+        // On s390x, generated code is ~6x larger in fastdebug and ~1.4x in release builds vs. other archs,
+        // hence we require slightly more minimum space.
         int minInitialSize = 800 + (Platform.isS390x() ? 800 : 0);
         for (int i = 0; i < 200; i++) {
             int initialCodeCacheSizeInKb = minInitialSize + rand.nextInt(400);
