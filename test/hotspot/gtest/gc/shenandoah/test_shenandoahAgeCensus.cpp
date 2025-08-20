32f6b3bef0e648df4be05eb1794a3f5b49c8dcfb
William Kemper <kemperw@amazon.com>
2025-08-20 14:25:47 -0700

    Add test that simulates promotion above tenuring age

--- test/hotspot/gtest/gc/shenandoah/test_shenandoahAgeCensus.cpp
+++ test/hotspot/gtest/gc/shenandoah/test_shenandoahAgeCensus.cpp
@@ -28,27 +28,61 @@
 class ShenandoahAgeCensusTest : public ::testing::Test {
 protected:
   static constexpr size_t MinimumPopulationSize = 4*K;
-  static void build_mortality_rate_curve(ShenandoahAgeCensus& census, const double mortality_rates[], const size_t cohorts) {
-    constexpr size_t initial_population = MinimumPopulationSize * 1000;
+  static constexpr size_t InitialPopulationSize = MinimumPopulationSize * 1000;
 
-    size_t population = initial_population;
-    for (size_t i = 0; i < cohorts; i++) {
-      population = population * (1.0 - mortality_rates[i]);
-      census.add(i + 1, 0, 0, population, 0);
+  size_t _cohorts_count = ShenandoahAgeCensus::MAX_COHORTS;
+  double _mortality_rates[ShenandoahAgeCensus::MAX_COHORTS];
+  size_t _cohort_populations[ShenandoahAgeCensus::MAX_COHORTS];
+
+  ShenandoahAgeCensusTest()
+  : _mortality_rates{0.9, 0.7, 0.5, 0.3, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0}
+  {
+    get_cohort_populations(_mortality_rates, _cohort_populations, _cohorts_count);
+  }
+
+  void update(ShenandoahAgeCensus& census) const {
+    for (size_t i = 1; i < _cohorts_count; i++) {
+      census.add(i, 0, 0, _cohort_populations[i], 0);
     }
+    census.update_census(_cohort_populations[0]);
+  }
 
-    census.update_census(initial_population);
+  size_t get_total_population_older_than(const size_t min_cohort_age) const {
+    size_t total = 0;
+    for (size_t i = 0; i < _cohorts_count; i++) {
+      if (i >= min_cohort_age) {
+        total += _cohort_populations[i];
+      }
+    }
+    return total;
+  }
+
+  void promote_all_tenurable(const size_t tenuring_threshold) {
+    for (size_t i = 0; i < _cohorts_count; i++) {
+      if (i >= tenuring_threshold) {
+        _cohort_populations[i] = 0;
+      }
+    }
+  }
+
+  static void get_cohort_populations(const double mortality_rates[], size_t cohort_populations[], const size_t cohorts) {
+    size_t population = InitialPopulationSize;
+    cohort_populations[0] = population;
+    for (size_t i = 1; i < cohorts; i++) {
+      population = population * (1.0 - mortality_rates[i - 1]);
+      cohort_populations[i] = population;
+    }
   }
 };
 
 TEST_F(ShenandoahAgeCensusTest, initialize) {
-  ShenandoahAgeCensus census(4);
+  const ShenandoahAgeCensus census(1);
   EXPECT_EQ(census.tenuring_threshold(), ShenandoahAgeCensus::MAX_COHORTS);
 }
 
 TEST_F(ShenandoahAgeCensusTest, ignore_small_populations) {
   // Small populations are ignored so we do not return early before reaching the youngest cohort.
-  ShenandoahAgeCensus census(4);
+  ShenandoahAgeCensus census(1);
   census.add(1, 0, 0, 32, 0);
   census.add(1, 0, 0, 32, 0);
   census.update_census(64);
@@ -56,39 +90,57 @@ TEST_F(ShenandoahAgeCensusTest, ignore_small_populations) {
 }
 
 TEST_F(ShenandoahAgeCensusTest, find_high_mortality_rate) {
-  ShenandoahAgeCensus census(4);
-  constexpr double mortality_rates[] = {
-    0.9, 0.7, 0.5, 0.3, 0.1, 0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
-  };
-  size_t mortality_rates_count = sizeof(mortality_rates) / sizeof(double);
+  ShenandoahAgeCensus census(1);
 
   // Initial threshold, no data
   EXPECT_EQ(16u, census.tenuring_threshold());
 
   // No deaths in previous data, everybody seems to survive, set threshold to 1 (tenure everything).
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(1u, census.tenuring_threshold());
 
   // mr = 0.7 from 1 -> 2, above mr threshold of 0.1
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(2u, census.tenuring_threshold());
 
   // mr = 0.5 from 2 -> 3, above mr threshold of 0.1
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(3u, census.tenuring_threshold());
 
   // mr = 0.3 from 3 -> 4, above mr threshold of 0.1
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(4u, census.tenuring_threshold());
 
   // mr = 0.1 from 4 -> 5, not above mr threshold of 0.1, stay at 4?
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(5u, census.tenuring_threshold());
 
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(5u, census.tenuring_threshold());
 
-  build_mortality_rate_curve(census, mortality_rates, mortality_rates_count);
+  update(census);
   EXPECT_EQ(5u, census.tenuring_threshold());
+}
+
+TEST_F(ShenandoahAgeCensusTest, ignore_mortality_caused_by_promotions) {
+  ShenandoahAgeCensus census(1);
+
+  // Simulate a sequence of censuses with the same mortality rate. Each one will see a
+  // mortality rate above the tenuring threshold and raise the tenuring threshold by one.
+  update(census);
+  update(census);
+  update(census);
+
+  EXPECT_EQ(3u, census.tenuring_threshold());
+
+  // Simulate the effect of promoting all objects above the tenuring threshold
+  // out of the young generation. This will look like a very high (100%) mortality
+  // rate for these cohorts. However, we do _not_ want to raise the threshold in
+  // this case because these objects haven't really "died", they have just been
+  // tenured.
+  promote_all_tenurable(census.tenuring_threshold());
+  update(census);
+
+  // We want this to stay at 3 - the mortality in 4th cohort was caused by expected promotions.
+  EXPECT_EQ(4u, census.tenuring_threshold());
 }
