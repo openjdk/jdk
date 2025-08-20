eef20ae649ef9557e97c648754abe75b1c96c9ab
Tobias Hotz <tobias.hotz@hotmail.de>
2025-08-20 20:22:18 +0200

    Add a simple path for non-special-case corner calculation

--- src/hotspot/share/opto/divnode.cpp
+++ src/hotspot/share/opto/divnode.cpp
@@ -535,8 +535,7 @@ static const IntegerType* compute_generic_div_type(const IntegerType* i1, const 
   //   (i1->_lo, i2->_hi), (i1->_lo, i2->_lo), (i1->_hi, i2->_lo), (i1->_hi, i2->_hi).
   // We compute all four and take the min and max.
   // A special case handles overflow when dividing the most‐negative value by −1.
-  NativeType new_lo = max_jint;
-  NativeType new_hi = min_jint;
+
   // adjust i2 bounds to not include zero, as zero always throws
   NativeType i2_lo = i2->_lo == 0 ? 1 : i2->_lo;
   NativeType i2_hi = i2->_hi == 0 ? -1 : i2->_hi;
@@ -546,42 +545,57 @@ static const IntegerType* compute_generic_div_type(const IntegerType* i1, const 
   // Special overflow case: min_val / (-1) == min_val (cf. JVMS§6.5 idiv/ldiv)
   // We need to be careful that we never run min_val / (-1) in C++ code, as this overflow is UB there
   // We also must include min_val in the output if i1->_lo == min_val and i2->_hi.
-  if (i1->_lo == min_val && i2_hi == -1) {
-    new_lo = i1->_lo;
-    if (!i1->is_con()) {
-      // Also compute the "next" division result for a non‑constant range.
-      new_hi = MAX2(new_hi, i1->_lo + 1 / i2_hi);
+  if (i1->_lo == min_val) {
+    // Special case possible, check carefully
+    NativeType new_lo;
+    NativeType new_hi;
+    if (i2_hi == -1) {
+      // special overflow case as defined above
+      new_lo = min_val;
+      if (!i1->is_con()) {
+        // Also compute the "next" division result for a non‑constant range.
+        new_hi = i1->_lo + 1 / i2_hi;
+      } else {
+        new_hi = min_val;
+      }
+    } else {
+      // Normal corner: (i1->_lo, i2->_hi)
+      NativeType result = i1->_lo / i2_hi;
+      new_lo = result;
+      new_hi = result;
     }
-  } else {
-    // Normal corner: (i1->_lo, i2->_hi)
-    NativeType result = i1->_lo / i2_hi;
-    new_lo = MIN2(new_lo, result);
-    new_hi = MAX2(new_hi, result);
+
+    // If the divisor range is wider than a singleton, include (i1->_lo, i2->_lo).
+    // We cannot use is_con here, as a range of [-1, 0] will also result in i2_lo and i2_hi being -1
+    if (i2_lo != i2_hi) {
+      assert(i2_lo != -1, "Special case not possible here, as i2_lo has to be < i2_hi");
+      NativeType result = i1->_lo / i2_lo;
+      new_lo = MIN2(new_lo, result);
+      new_hi = MAX2(new_hi, result);
+    }
+
+    // If i1 is not a single constant, include the two corners with i1->_hi:
+    //   (i1->_hi, i2->_lo) and (i1->_hi, i2->_hi)
+    if (!i1->is_con()) {
+      assert(i2_hi > min_val, "Special case not possible here, as i1->_hi has to be > min");
+      NativeType result1 = i1->_hi / i2_lo;
+      NativeType result2 = i1->_hi / i2_hi;
+      new_lo = MIN3(new_lo, result1, result2);
+      new_hi = MAX3(new_hi, result1, result2);
+    }
+
+    return IntegerType::make(new_lo, new_hi, widen);
   }
 
-  // If the divisor range is wider than a singleton, include (i1->_lo, i2->_lo).
-  // We cannot use is_con here, as a range of [-1, 0] will also result in i2_lo and i2_hi being -1
-  if (i2_lo != i2_hi) {
-    // special case not possible here, _lo mus
-    assert(i2_lo != -1, "Special case not possible here");
-    NativeType result = i1->_lo / i2_lo;
-    new_lo = MIN2(new_lo, result);
-    new_hi = MAX2(new_hi, result);
-  }
+  // Special case not possible here, calculate all corners normally
+  assert(i1->_lo != min_val && i1->_hi != min_val, "Special has should have been filtered out");
+  NativeType corner1 = i1->_lo / i2_lo;
+  NativeType corner2 = i1->_lo / i2_hi;
+  NativeType corner3 = i1->_hi / i2_lo;
+  NativeType corner4 = i1->_hi / i2_hi;
 
-  // If i1 is not a single constant, include the two corners with i1->_hi:
-  //   (i1->_hi, i2->_lo) and (i1->_hi, i2->_hi)
-  if (!i1->is_con()) {
-    // Special case not possible here, as i1->_hi has to be > min
-    assert(i2_hi > min_val, "Special case not possible here");
-    NativeType result1 = i1->_hi / i2_lo;
-    NativeType result2 = i1->_hi / i2_hi;
-    new_lo = MIN2(new_lo, result1);
-    new_lo = MIN2(new_lo, result2);
-    new_hi = MAX2(new_hi, result1);
-    new_hi = MAX2(new_hi, result2);
-  }
-  assert(new_hi >= new_lo, "sanity");
+  NativeType new_lo = MIN4(corner1, corner2, corner3, corner4);
+  NativeType new_hi = MAX4(corner1, corner2, corner3, corner4);
   return IntegerType::make(new_lo, new_hi, widen);
 }
 
