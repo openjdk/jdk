d2a9f980c52b60071a9aef19a3fb7a61df6220d8
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-08-21 08:39:52 +0100

    Remove not used import

--- test/jdk/javax/swing/JInternalFrame/8160248/JInternalFrameDraggingTest.java
+++ test/jdk/javax/swing/JInternalFrame/8160248/JInternalFrameDraggingTest.java
@@ -25,7 +25,6 @@ import java.io.File;
 import java.awt.BorderLayout;
 import java.awt.image.BufferedImage;
 import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Robot;
