ba6f372d87765417f1bf05cd2b1a7e058c18350d
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-08-25 13:00:40 +0000

    move down

--- src/hotspot/share/precompiled/precompiled.hpp
+++ src/hotspot/share/precompiled/precompiled.hpp
@@ -30,15 +30,6 @@
 // compiling them.
 
 #include "classfile/javaClasses.inline.hpp"
-#if INCLUDE_SHENANDOAHGC
-#include "gc/shenandoah/shenandoahBarrierSet.inline.hpp"
-#include "gc/shenandoah/shenandoahHeap.inline.hpp"
-#endif
-#if INCLUDE_ZGC
-#include "gc/z/zBarrier.inline.hpp"
-#include "gc/z/zGeneration.inline.hpp"
-#include "gc/z/zHeap.inline.hpp"
-#endif
 #include "memory/allocation.hpp"
 #include "memory/iterator.inline.hpp"
 #include "oops/access.inline.hpp"
@@ -49,3 +40,12 @@
 #include "runtime/frame.inline.hpp"
 #include "runtime/javaThread.inline.hpp"
 #include "utilities/globalDefinitions.hpp"
+#if INCLUDE_SHENANDOAHGC
+#include "gc/shenandoah/shenandoahBarrierSet.inline.hpp"
+#include "gc/shenandoah/shenandoahHeap.inline.hpp"
+#endif
+#if INCLUDE_ZGC
+#include "gc/z/zBarrier.inline.hpp"
+#include "gc/z/zGeneration.inline.hpp"
+#include "gc/z/zHeap.inline.hpp"
+#endif
