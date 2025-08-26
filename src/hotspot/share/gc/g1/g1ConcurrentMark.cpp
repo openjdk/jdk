e7b2bc99e9cc08001f44f5920cf65227167eb046
Thomas Schatzl <tschatzl@openjdk.org>
2025-08-26 06:57:44 +0000

    8365656: [ubsan] G1CSetCandidateGroup::liveness() reports division by 0
    Reviewed-by: iwalulya, ayang

--- src/hotspot/share/gc/g1/g1ConcurrentMark.cpp
+++ src/hotspot/share/gc/g1/g1ConcurrentMark.cpp
@@ -3121,8 +3121,8 @@ void G1PrintRegionLivenessInfoClosure::log_cset_candidate_group_add_total(G1CSet
                           G1PPRL_TYPE_H_FORMAT,
                           group->group_id(),
                           group->length(),
-                          group->gc_efficiency(),
-                          group->liveness_percent(),
+                          group->length() > 0 ? group->gc_efficiency() : 0.0,
+                          group->length() > 0 ? group->liveness_percent() : 0.0,
                           group->card_set()->mem_size(),
                           type);
   _total_remset_bytes += group->card_set()->mem_size();
