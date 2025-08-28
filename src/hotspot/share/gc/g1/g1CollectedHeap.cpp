d06c66f7f5a6d3c649c0a10ad735f0cc7c673b2a
Thomas Schatzl <tschatzl@openjdk.org>
2025-08-28 09:21:26 +0000

    8365055: G1: Merge Heap Roots phase incorrectly clears young gen remembered set every time
    Reviewed-by: kbarrett, iwalulya

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -2441,6 +2441,12 @@ void G1CollectedHeap::update_perf_counter_cpu_time() {
 }
 
 void G1CollectedHeap::start_new_collection_set() {
+  // Clear current young cset group to allow adding.
+  // It is fine to clear it this late - evacuation does not add any remembered sets
+  // by itself, but only marks cards.
+  // The regions had their association to this group already removed earlier.
+  young_regions_cset_group()->clear();
+
   collection_set()->start_incremental_building();
 
   clear_region_attr();
