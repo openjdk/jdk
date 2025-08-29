4b33904ad288da9f5697e0d0b812c6abb232678c
Leo Korinth <leo.korinth@oracle.com>
2025-08-29 20:41:26 +0200

    update copyright

--- test/jdk/jdk/internal/platform/docker/TestLimitsUpdating.java
+++ test/jdk/jdk/internal/platform/docker/TestLimitsUpdating.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2023, Red Hat, Inc.
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  *
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
