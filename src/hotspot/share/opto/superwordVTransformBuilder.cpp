fcc09f4845328a16f8cf904848baecb65bd61cea
Emanuel Peter <epeter@openjdk.org>
2025-09-01 13:48:25 +0000

    8366427: C2 SuperWord: refactor VTransform scalar nodes
    Reviewed-by: mhaessig, chagedorn, kvn

--- src/hotspot/share/opto/superwordVTransformBuilder.cpp
+++ src/hotspot/share/opto/superwordVTransformBuilder.cpp
@@ -53,7 +53,19 @@ void SuperWordVTransformBuilder::build_scalar_vtnodes_for_non_packed_nodes() {
   for (int i = 0; i < _vloop_analyzer.body().body().length(); i++) {
     Node* n = _vloop_analyzer.body().body().at(i);
     if (_packset.get_pack(n) != nullptr) { continue; }
-    VTransformScalarNode* vtn = new (_vtransform.arena()) VTransformScalarNode(_vtransform, n);
+
+    VTransformNode* vtn = nullptr;
+    if (n->is_Load() || n->is_Store()) {
+      MemNode* mem = n->as_Mem();
+      const VPointer& mem_p = _vloop_analyzer.vpointers().vpointer(mem);
+      vtn = new (_vtransform.arena()) VTransformMemopScalarNode(_vtransform, mem, mem_p);
+    } else if (n->is_Phi()) {
+      vtn = new (_vtransform.arena()) VTransformLoopPhiNode(_vtransform, n->as_Phi());
+    } else if (n->is_CFG()) {
+      vtn = new (_vtransform.arena()) VTransformCFGNode(_vtransform, n);
+    } else {
+      vtn = new (_vtransform.arena()) VTransformDataScalarNode(_vtransform, n);
+    }
     map_node_to_vtnode(n, vtn);
   }
 }
@@ -108,8 +120,8 @@ void SuperWordVTransformBuilder::build_inputs_for_vector_vtnodes(VectorSet& vtn_
 void SuperWordVTransformBuilder::build_inputs_for_scalar_vtnodes(VectorSet& vtn_memory_dependencies) {
   for (int i = 0; i < _vloop_analyzer.body().body().length(); i++) {
     Node* n = _vloop_analyzer.body().body().at(i);
-    VTransformScalarNode* vtn = get_vtnode(n)->isa_Scalar();
-    if (vtn == nullptr) { continue; }
+    VTransformNode* vtn = get_vtnode(n);
+    if (vtn->isa_Vector() != nullptr) { continue; }
     vtn_memory_dependencies.clear(); // Add every dependency only once per vtn.
 
     if (n->is_Load()) {
@@ -178,7 +190,7 @@ VTransformVectorNode* SuperWordVTransformBuilder::make_vector_vtnode_for_pack(co
 }
 
 void SuperWordVTransformBuilder::init_req_with_scalar(Node* n, VTransformNode* vtn, const int index) {
-  VTransformNode* req = get_vtnode_or_wrap_as_input_scalar(n->in(index));
+  VTransformNode* req = get_vtnode_or_wrap_as_outer(n->in(index));
   vtn->init_req(index, req);
 }
 
@@ -210,7 +222,7 @@ VTransformNode* SuperWordVTransformBuilder::get_or_make_vtnode_vector_input_at_i
   Node* same_input = _packset.same_inputs_at_index_or_null(pack, index);
   if (same_input == nullptr && p0->in(index) == _vloop.iv()) {
     // PopulateIndex: [iv+0, iv+1, iv+2, ...]
-    VTransformNode* iv_vtn = get_vtnode_or_wrap_as_input_scalar(_vloop.iv());
+    VTransformNode* iv_vtn = get_vtnode_or_wrap_as_outer(_vloop.iv());
     BasicType p0_bt = _vloop_analyzer.types().velt_basic_type(p0);
     // If we have subword type, take that type directly. If p0 is some ConvI2L/F/D,
     // then the p0_bt can also be L/F/D but we need to produce ints for the input of
@@ -222,7 +234,7 @@ VTransformNode* SuperWordVTransformBuilder::get_or_make_vtnode_vector_input_at_i
   }
 
   if (same_input != nullptr) {
-    VTransformNode* same_input_vtn = get_vtnode_or_wrap_as_input_scalar(same_input);
+    VTransformNode* same_input_vtn = get_vtnode_or_wrap_as_outer(same_input);
     if (index == 2 && VectorNode::is_shift(p0)) {
       // Scalar shift count for vector shift operation: vec2 = shiftV(vec1, scalar_count)
       // Scalar shift operations masks the shift count, but the vector shift does not, so
@@ -264,12 +276,12 @@ VTransformNode* SuperWordVTransformBuilder::get_or_make_vtnode_vector_input_at_i
   ShouldNotReachHere();
 }
 
-VTransformNode* SuperWordVTransformBuilder::get_vtnode_or_wrap_as_input_scalar(Node* n) {
+VTransformNode* SuperWordVTransformBuilder::get_vtnode_or_wrap_as_outer(Node* n) {
   VTransformNode* vtn = get_vtnode_or_null(n);
   if (vtn != nullptr) { return vtn; }
 
   assert(!_vloop.in_bb(n), "only nodes outside the loop can be input nodes to the loop");
-  vtn = new (_vtransform.arena()) VTransformInputScalarNode(_vtransform, n);
+  vtn = new (_vtransform.arena()) VTransformOuterNode(_vtransform, n);
   map_node_to_vtnode(n, vtn);
   return vtn;
 }
