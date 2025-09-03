6abf25160e99419590523d438c5ae09d02261004
MBaesken <matthias.baesken@sap.com>
2025-09-03 10:25:08 +0200

    remove requires

--- test/hotspot/jtreg/runtime/cds/appcds/aotCache/AOTMapTest.java
+++ test/hotspot/jtreg/runtime/cds/appcds/aotCache/AOTMapTest.java
@@ -36,7 +36,6 @@
  * @test id=dynamic
  * @bug 8362566
  * @summary Test the contents of -Xlog:aot+map with AOT workflow
- * @requires vm.cds.default.archive.available
  * @requires vm.cds.supports.aot.class.linking
  * @library /test/lib /test/hotspot/jtreg/runtime/cds
  * @build jdk.test.whitebox.WhiteBox
