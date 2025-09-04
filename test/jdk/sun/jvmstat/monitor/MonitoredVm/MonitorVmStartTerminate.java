55b4d28f439380e4d9e929069f75fd41fc61dc71
SendaoYan <syan@openjdk.org>
2025-09-04 01:37:42 +0000

    8366695: Test sun/jvmstat/monitor/MonitoredVm/MonitorVmStartTerminate.java timed out
    Reviewed-by: lmesnik, kevinw

--- test/jdk/sun/jvmstat/monitor/MonitoredVm/MonitorVmStartTerminate.java
+++ test/jdk/sun/jvmstat/monitor/MonitoredVm/MonitorVmStartTerminate.java
@@ -70,7 +70,7 @@ import sun.jvmstat.monitor.event.VmStatusChangeEvent;
  * @modules java.management
  *          jdk.internal.jvmstat/sun.jvmstat.monitor
  *          jdk.internal.jvmstat/sun.jvmstat.monitor.event
- * @run main/othervm MonitorVmStartTerminate
+ * @run main/othervm/timeout=240 MonitorVmStartTerminate
  */
 public final class MonitorVmStartTerminate {
 
