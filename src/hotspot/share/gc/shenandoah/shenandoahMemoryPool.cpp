cd9522af38cefebe6f1f82aca0304baa155b541f
Kelvin Nilsen <kdnilsen@amazon.com>
2025-09-09 22:03:18 +0000

    another tweak to make GHA gtest happy

--- src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
@@ -24,7 +24,6 @@
  *
  */
 
-#include "gc/shenandoah/shenandoahHeap.hpp"
 #include "gc/shenandoah/shenandoahMemoryPool.hpp"
 #include "gc/shenandoah/shenandoahOldGeneration.hpp"
 #include "gc/shenandoah/shenandoahYoungGeneration.hpp"
@@ -63,10 +62,10 @@ MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   // the assert below, which would also fail in downstream code. To avoid that, adjust values
   // to make sense under the race. See JDK-8207200.
   committed = MAX2(used, committed);
-  assert(used <= committed, "used: %zu, committed: %zu", used,      committed);
-  assert(initial <= ShenandoahHeap::heap()->max_capacity(), "sanity");
-  assert(committed <= ShenandoahHeap::heap()->max_capacity(), "sanity");
-  assert(max <= ShenandoahHeap::heap()->max_capacity(), "sanity");
+  assert(used <= committed, "used: %zu, committed: %zu", used, committed);
+  assert(initial <= _heap->max_capacity(), "sanity");
+  assert(committed <= _heap->max_capacity(), "sanity");
+  assert(max <= _heap->max_capacity(), "sanity");
   return MemoryUsage(initial, used, committed, max);
 }
 
@@ -89,10 +88,10 @@ MemoryUsage ShenandoahGenerationalMemoryPool::get_memory_usage() {
   size_t used      = used_in_bytes();
   size_t committed = _generation->used_regions_size();
 
-  assert(initial <= ShenandoahHeap::heap()->max_capacity(), "sanity");
-  assert(used <= ShenandoahHeap::heap()->max_capacity(), "sanity");
-  assert(committed <= ShenandoahHeap::heap()->max_capacity(), "sanity");
-  assert(max <= ShenandoahHeap::heap()->max_capacity(), "sanity");
+  assert(initial <= _heap->max_capacity(), "sanity");
+  assert(used <= _heap->max_capacity(), "sanity");
+  assert(committed <= _heap->max_capacity(), "sanity");
+  assert(max <= _heap->max_capacity(), "sanity");
   return MemoryUsage(initial, used, committed, max);
 }
 
