0f098f2712f3a118da294eaab4b6a0d470027113
MBaesken <matthias.baesken@sap.com>
2025-09-12 09:40:07 +0200

    Add hint suggested by mdoerr

--- src/hotspot/share/cds/aotMetaspace.cpp
+++ src/hotspot/share/cds/aotMetaspace.cpp
@@ -1288,7 +1288,7 @@ void AOTMetaspace::unrecoverable_loading_error(const char* message) {
   } else if (CDSConfig::new_aot_flags_used()) {
     vm_exit_during_initialization("Unable to use AOT cache.", nullptr);
   } else {
-    vm_exit_during_initialization("Unable to use shared archive. Unrecoverable archive loading error", message);
+    vm_exit_during_initialization("Unable to use shared archive. Unrecoverable archive loading error (run with -Xlog:aot,cds for details)", message);
   }
 }
 
