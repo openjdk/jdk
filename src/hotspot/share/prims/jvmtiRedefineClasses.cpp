dacea30ae49411c1689a37b3ce9f21e4987034d1
Johan Sjölén <johan.sjolen@oracle.com>
2025-09-13 16:33:57 +0200

    Fix

--- src/hotspot/share/prims/jvmtiRedefineClasses.cpp
+++ src/hotspot/share/prims/jvmtiRedefineClasses.cpp
@@ -1621,7 +1621,7 @@ bool VM_RedefineClasses::merge_constant_pools(const constantPoolHandle& old_cp,
     } // end for each old_cp entry
 
     ConstantPool::copy_bsm_entries(old_cp, merge_cp_p, CHECK_false);
-    _bsmae_iter = merge_cp_p->extend_to_fit(scratch_cp, CHECK_false);
+    _bsmae_iter = merge_cp_p->start_extension(scratch_cp, CHECK_false);
 
     // We don't need to sanity check that *merge_cp_length_p is within
     // *merge_cp_p bounds since we have the minimum on-entry check above.
