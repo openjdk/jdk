1fca6d9a1dcc56040208e502702c723f51cfbaaa
Jan Lahoda <jan.lahoda@oracle.com>
2025-09-15 15:45:42 +0200

    8366582: Test jdk/jshell/ToolSimpleTest.java failed: provider not found

--- src/jdk.jshell/share/classes/jdk/jshell/SourceCodeAnalysisImpl.java
+++ src/jdk.jshell/share/classes/jdk/jshell/SourceCodeAnalysisImpl.java
@@ -154,6 +154,7 @@ import static jdk.jshell.TreeDissector.printType;
 import static java.util.stream.Collectors.joining;
 
 import javax.lang.model.type.IntersectionType;
+import jdk.internal.jshell.debug.InternalDebugControl;
 
 /**
  * The concrete implementation of SourceCodeAnalysis.
@@ -2303,7 +2304,8 @@ class SourceCodeAnalysisImpl extends SourceCodeAnalysis {
     //if an index exists for the given entry, the existing index is kept unless the timestamp is modified
     private ClassIndex indexForPath(Path path) {
         if (!Files.isDirectory(path)) {
-            if (Files.exists(path)) {
+            if (Files.exists(path) &&
+                !isJRTMarkerFile(path)) { //don't directly index lib/modules
                 return PATH_TO_INDEX.compute(path, (p, index) -> {
                     try {
                         long lastModified = Files.getLastModifiedTime(p).toMillis();
@@ -2332,6 +2334,10 @@ class SourceCodeAnalysisImpl extends SourceCodeAnalysis {
                 return index;
             });
         }
+    }
+
+    private static boolean isJRTMarkerFile(Path path) {
+        return path.equals(Paths.get(System.getProperty("java.home"), "lib", "modules"));
     }
 
     //create an index based on the content of the given dirs; the original JavaFileManager entry is originalPath.
