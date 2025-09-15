3aef6fa9128920edc30ab7af7358850b98424fee
Emanuel Peter <emanuel.peter@oracle.com>
2025-09-15 10:02:10 +0200

    empty test

---
+++ test/hotspot/jtreg/compiler/galery/NormalMapping.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test automatic
+ * @bug 8324751
+ * @summary Visual example of auto vectorization: normal mapping
+ * @library /test/lib /
+ * @run main compiler.galery.NormalMapping automatic
+ */
+// TODO: fix bug id above
+
+/*
+ * @test manual
+ * @library /test/lib /
+ * @run main/manual compiler.galery.NormalMapping manual
+ */
+
+package compiler.galery;
+
+public class NormalMapping {
+    public static void main(String[] args) {
+        System.out.println("mode: " + args[0]);
+    }
+}
