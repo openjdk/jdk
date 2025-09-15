28cd0d1121788026a4a41ab1af540e29825cd395
Weijun Wang <weijun.wang@oracle.com>
2025-09-15 11:38:02 -0400

    the fix

--- test/jdk/sun/security/krb5/auto/KvnoNA.java
+++ test/jdk/sun/security/krb5/auto/KvnoNA.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 7197159 8194486
+ * @bug 7197159 8194486 8367344
  * @summary accept different kvno if there no match
  * @library /test/lib
  * @compile -XDignore.symbol.file KvnoNA.java
@@ -31,6 +31,7 @@
  * @run main/othervm -Djdk.net.hosts.file=TestHosts KvnoNA
  */
 
+import jdk.test.lib.Asserts;
 import org.ietf.jgss.GSSException;
 import sun.security.jgss.GSSUtil;
 import sun.security.krb5.KrbException;
@@ -46,24 +47,51 @@ public class KvnoNA {
         OneKDC kdc = new OneKDC(null);
         kdc.writeJAASConf();
 
-        // In KDC, it's 2
-        char[] pass = "pass2".toCharArray();
-        kdc.addPrincipal(OneKDC.SERVER, pass);
+        // Use backend as server because its isInitiator is false,
+        // therefore no login failure. In KDC, kvno is 2.
+        kdc.addPrincipal(OneKDC.BACKEND, "pass2".toCharArray());
 
-        // In ktab, kvno is 1 or 3, 3 has the same password
+        // Rewrite a keytab which might contain different keys from KDC
         KeyTab ktab = KeyTab.create(OneKDC.KTAB);
         PrincipalName p = new PrincipalName(
-            OneKDC.SERVER+"@"+OneKDC.REALM, PrincipalName.KRB_NT_SRV_HST);
+                OneKDC.BACKEND + "@" + OneKDC.REALM, PrincipalName.KRB_NT_SRV_HST);
         ktab.addEntry(p, "pass1".toCharArray(), 1, true);
+
+        // Usual case, kvno 2 has the same password
+        ktab.addEntry(p, "pass2".toCharArray(), 2, true);
+        ktab.save();
+        check();
+
+        // Usual case, kvno 2 has a different password
+        ktab.deleteEntries(p, -1, 2);
+        ktab.addEntry(p, "pass3".toCharArray(), 2, true);
+        ktab.save();
+        Asserts.assertTrue(
+                Asserts.assertThrows(GSSException.class, KvnoNA::check)
+                                .getMessage().contains("Checksum failed"));
+
+        // 7197159: No kvno 2, kvno 3 has the same password
+        ktab.deleteEntries(p, -1, 2);
         ktab.addEntry(p, "pass2".toCharArray(), 3, true);
         ktab.save();
+        check();
 
+        // 8367344: No kvno 2, kvno 3 has a different password
+        ktab.deleteEntries(p, -1, 3);
+        ktab.addEntry(p, "pass3".toCharArray(), 3, true);
+        ktab.save();
+        Asserts.assertTrue(Asserts.assertThrows(GSSException.class, KvnoNA::check)
+                .getCause() instanceof KrbException ke
+                && ke.returnCode() == Krb5.KRB_AP_ERR_BADKEYVER);
+    }
+
+    static void check() throws Exception {
         Context c, s;
 
         c = Context.fromUserPass("dummy", "bogus".toCharArray(), false);
-        s = Context.fromJAAS("server");
+        s = Context.fromJAAS("backend");
 
-        c.startAsClient(OneKDC.SERVER, GSSUtil.GSS_KRB5_MECH_OID);
+        c.startAsClient(OneKDC.BACKEND, GSSUtil.GSS_KRB5_MECH_OID);
         s.startAsServer(GSSUtil.GSS_KRB5_MECH_OID);
 
         Context.handshake(c, s);
