93540e059dd36c101e4d61a06d379be7569db6fd
Man Cao <manc@google.com>
2025-09-16 14:54:45 -0700

    Switch to disable inlining for shortMethod

--- test/hotspot/jtreg/compiler/runtime/TestDontCompileHugeMethods.java
+++ test/hotspot/jtreg/compiler/runtime/TestDontCompileHugeMethods.java
@@ -23,10 +23,9 @@
 
 /**
  * @test
- * @bug 8366118
+ * @bug 8366118 8367613
  * @summary Check that a huge method is not compiled under -XX:+DontCompileHugeMethods.
  * @library /test/lib
- * @requires (vm.compMode != "Xcomp")
  * @run main compiler.runtime.TestDontCompileHugeMethods
  */
 package compiler.runtime;
@@ -100,6 +99,10 @@ public class TestDontCompileHugeMethods {
 
     private static void runTest(Path workDir, List jvmArgs) throws Exception {
         ArrayList<String> command = new ArrayList<>();
+        // Disable inlining for shortMethod().
+        // Otherwise under "-Xcomp -XX:TieredStopAtLevel=1", shortMethod() is inlined into main(),
+        // and is not directly executed or compiled.
+        command.add("-XX:CompileCommand=dontinline,HugeSwitch::shortMethod");
         command.add("-XX:+PrintCompilation");
         command.add("-Xbatch");
         command.addAll(jvmArgs);
