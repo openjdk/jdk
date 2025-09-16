d40fffdeb98339176e7d9ae04e7c3e087332564d
JohnTortugo <dcsl@amazon.com>
2025-09-16 02:29:15 +0000

    Merge consecutive ifs

--- src/hotspot/share/opto/escape.cpp
+++ src/hotspot/share/opto/escape.cpp
@@ -3131,13 +3131,11 @@ void ConnectionGraph::find_scalar_replaceable_allocs(GrowableArray<JavaObjectNod
           }
         } else if (use->is_LocalVar()) {
           Node* phi = use->ideal_node();
-          if (phi->Opcode() == Op_Phi && reducible_merges.member(phi)) {
-            if (!can_reduce_phi(phi->as_Phi())) {
-              set_not_scalar_replaceable(jobj NOT_PRODUCT(COMMA "is merged in a non-reducible phi"));
-              reducible_merges.yank(phi);
-              found_nsr_alloc = true;
-              break;
-            }
+          if (phi->Opcode() == Op_Phi && reducible_merges.member(phi) && !can_reduce_phi(phi->as_Phi())) {
+            set_not_scalar_replaceable(jobj NOT_PRODUCT(COMMA "is merged in a non-reducible phi"));
+            reducible_merges.yank(phi);
+            found_nsr_alloc = true;
+            break;
           }
         }
       }
