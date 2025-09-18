b73af018b8c2bf03422520453dcbe59b4b96dce6
Albert Mingkun Yang <ayang@openjdk.org>
2025-09-18 17:22:45 +0000

    8367626: Parallel: Remove ParallelCompactData::summarize_dense_prefix
    Reviewed-by: gli, fandreuzzi

--- src/hotspot/share/gc/parallel/psParallelCompact.hpp
+++ src/hotspot/share/gc/parallel/psParallelCompact.hpp
@@ -360,11 +360,6 @@ public:
   inline RegionData* region(size_t region_idx) const;
   inline size_t     region(const RegionData* const region_ptr) const;
 
-  // Fill in the regions covering [beg, end) so that no data moves; i.e., the
-  // destination of region n is simply the start of region n.  Both arguments
-  // beg and end must be region-aligned.
-  void summarize_dense_prefix(HeapWord* beg, HeapWord* end);
-
   HeapWord* summarize_split_space(size_t src_region, SplitInfo& split_info,
                                   HeapWord* destination, HeapWord* target_end,
                                   HeapWord** target_next);
