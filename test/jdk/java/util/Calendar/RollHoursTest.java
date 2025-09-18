9a3291fa42b62a21d0abdf2e43a97a4a8fa81ffc
Justin Lu <justin.lu@oracle.com>
2025-09-18 13:33:52 -0700

    DST test cases + compute expected value as method + improve err msg

--- test/jdk/java/util/Calendar/RollHoursTest.java
+++ test/jdk/java/util/Calendar/RollHoursTest.java
@@ -30,12 +30,16 @@
  * @run junit RollHoursTest
  */
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.FieldSource;
 
+import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.TimeZone;
 import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -45,43 +49,91 @@ public class RollHoursTest {
     // Should trigger multiple full HOUR/HOUR_OF_DAY cycles
     private static final List<Integer> hours =
             IntStream.rangeClosed(-55, 55).boxed().toList();
-    // Sample AM/PM Hours
-    private static final int AM_HOUR = 3;
-    private static final int PM_HOUR = 15;
+    // Various calendars to test against
+    private static final List<Calendar> calendars = List.of(
+            // GMT, daylight savings free
+            new GregorianCalendar(TimeZone.getTimeZone("GMT")),
+            // Daylight savings observing calendars
+            new GregorianCalendar(TimeZone.getTimeZone("America/Chicago")),
+            new GregorianCalendar(TimeZone.getTimeZone("America/Chicago")),
+            new GregorianCalendar(TimeZone.getTimeZone("America/Los_Angeles")),
+            new GregorianCalendar(TimeZone.getTimeZone("America/Los_Angeles"))
+    );
 
-    // Roll the HOUR_OF_DAY field. AM/PM specific calendar does not matter.
-    @ParameterizedTest
-    @FieldSource("hours")
-    void HourOfDayTest(int hour) {
-        var cal = new GregorianCalendar(2005, 8, 12, PM_HOUR, 30, 45);
-        // E.g. For hour +50 -> (50 + 15) % 24 = 17
-        //      For hour -50 -> (24 + (15 - 50) % 24) % 24
-        //                   -> (24 + - 11) % 24 = 13
-        var expectedHourOfDay = (hour >= 0 ? (PM_HOUR + hour) : (24 + (PM_HOUR + hour) % 24)) % 24 ;
-        cal.roll(Calendar.HOUR_OF_DAY, hour);
-        assertEquals(expectedHourOfDay, cal.get(Calendar.HOUR_OF_DAY),
-                cal.getTime() + " incorrectly rolled " + hour);
+    // Reset the times of each calendar. These calendars provide testing under
+    // daylight savings transitions (or the lack thereof) and different AM/PM hours.
+    @BeforeEach
+    void clear() {
+        // Reset all calendars each iteration for clean slate
+        calendars.forEach(Calendar::clear);
+
+        // Basic test, independent of daylight savings transitions
+        calendars.get(0).set(2005, 8, 20, 12, 10, 25);
+
+        // Transition to daylight savings time (CST/CDT) ---
+        // Day of transition: 03/13/2016 (Sunday)
+        //      Most interesting test case due to 2 AM skip
+        calendars.get(1).set(2016, 2, 13, 3, 30, 55);
+        // Day before transition: 03/12/2016 (Saturday)
+        calendars.get(2).set(2016, 2, 12, 15, 20, 45);
+
+        // Transition back to standard time (PST/PDT) ----
+        // Day of transition: 11/06/2016 (Sunday)
+        calendars.get(3).set(2016, 10, 6, 4, 15, 20);
+        // Day before transition: 11/05/2016 (Saturday)
+        calendars.get(4).set(2016, 10, 5, 12, 25, 30);
     }
 
-    // Roll the HOUR field for an AM calendar.
+    // Rolling the HOUR_OF_DAY field.
     @ParameterizedTest
     @FieldSource("hours")
-    void AMHourTest(int hour) {
-        var cal = new GregorianCalendar(2005, 8, 12, AM_HOUR, 30, 45);
-        var expectedAMHour = (hour >= 0 ? (AM_HOUR + hour) : (12 + (AM_HOUR + hour) % 12)) % 12;
-        cal.roll(Calendar.HOUR, hour);
-        assertEquals(expectedAMHour, cal.get(Calendar.HOUR),
-                cal.getTime() + " incorrectly rolled " + hour);
+    void HourOfDayTest(int hoursToRoll) {
+        for (var cal : calendars) {
+            var savedTime = cal.getTime();
+            var savedHour = cal.get(Calendar.HOUR_OF_DAY);
+            cal.roll(Calendar.HOUR_OF_DAY, hoursToRoll);
+            assertEquals(getExpectedHour(hoursToRoll, savedHour, 24, cal, savedTime),
+                    cal.get(Calendar.HOUR_OF_DAY),
+                    getMessage(cal.getTimeZone(), savedTime, hoursToRoll));
+        }
     }
 
-    // Roll the HOUR field for a PM calendar.
+    // Rolling the HOUR field.
     @ParameterizedTest
     @FieldSource("hours")
-    void PMHourTest(int hour) {
-        var cal = new GregorianCalendar(2005, 8, 12, PM_HOUR, 30, 45);
-        var expectedPMHour = (hour >= 0 ? (PM_HOUR + hour) : (12 + (PM_HOUR + hour) % 12)) % 12;
-        cal.roll(Calendar.HOUR, hour);
-        assertEquals(expectedPMHour, cal.get(Calendar.HOUR),
-                cal.getTime() + " incorrectly rolled " + hour);
+    void HourTest(int hoursToRoll) {
+        for (var cal : calendars) {
+            var savedTime = cal.getTime();
+            var savedHour = cal.get(Calendar.HOUR_OF_DAY);
+            cal.roll(Calendar.HOUR, hoursToRoll);
+            assertEquals(getExpectedHour(hoursToRoll, savedHour, 12, cal, savedTime),
+                    cal.get(Calendar.HOUR),
+                    getMessage(cal.getTimeZone(), savedTime, hoursToRoll));
+        }
+    }
+
+    // Gets the expected hour after rolling by X hours. Supports 12/24-hour cycle.
+    // Special handling for non-existent 2 AM case on transition day.
+    private static int getExpectedHour(int roll, int hour, int hourCycle, Calendar cal, Date oldDate) {
+        // For example with HOUR_OF_DAY at 15 and a 24-hour cycle
+        // For rolling forwards 50 hours -> (50 + 15) % 24 = 17
+        // For hour backwards 50 hours -> (24 + (15 - 50) % 24) % 24
+        //                             -> (24 + - 11) % 24 = 13
+        var result = (roll >= 0 ? (hour + roll) : (hourCycle + (hour + roll) % hourCycle)) % hourCycle;
+
+        // 2 AM does not exist on transition day. Calculate normalized value accordingly
+        if (cal.getTimeZone().inDaylightTime(oldDate) && cal.get(Calendar.MONTH) == Calendar.MARCH && result == 2) {
+            return roll > 0 ? result + 1 : result - 1;
+        } else {
+            // Normal return value
+            return result;
+        }
+    }
+
+    // Get a TimeZone adapted error message
+    private static String getMessage(TimeZone tz, Date date, int hoursToRoll) {
+        var fmt = DateFormat.getDateTimeInstance(DateFormat.FULL, DateFormat.FULL);
+        fmt.setTimeZone(tz);
+        return fmt.format(date) + " incorrectly rolled " + hoursToRoll;
     }
 }
