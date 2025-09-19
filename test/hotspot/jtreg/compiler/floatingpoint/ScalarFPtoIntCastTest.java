0acc719c44f2873c760eda74c4804fe184a8f16e
Mohamed Issa <mohamed.issa@intel.com>
2025-09-19 19:59:55 -0400

    Use compiler generator instead of standard Java streams

--- test/hotspot/jtreg/compiler/floatingpoint/ScalarFPtoIntCastTest.java
+++ test/hotspot/jtreg/compiler/floatingpoint/ScalarFPtoIntCastTest.java
@@ -33,46 +33,55 @@
 package compiler.floatingpoint;
 
 import compiler.lib.ir_framework.*;
+import compiler.lib.generators.Generator;
+import static compiler.lib.generators.Generators.G;
 import compiler.lib.verify.Verify;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.IntStream;
 
 public class ScalarFPtoIntCastTest {
     private static final int COUNT = 16;
+
     private float[] float_arr;
     private double[] double_arr;
-    private long[] long_arr;
-    private int[] int_arr;
-    private short[] short_arr;
-    private byte[] byte_arr;
+    private long[] long_float_arr;
+    private long[] long_double_arr;
+    private int[] int_float_arr;
+    private int[] int_double_arr;
+    private short[] short_float_arr;
+    private short[] short_double_arr;
+    private byte[] byte_float_arr;
+    private byte[] byte_double_arr;
+
+    private static final Generator<Float> genF = G.floats();
+    private static final Generator<Double> genD = G.doubles();
 
     public static void main(String[] args) {
         TestFramework testFramework = new TestFramework();
-        testFramework.setDefaultWarmup(5000).start();
+        testFramework.start();
     }
 
     public ScalarFPtoIntCastTest() {
-        Random ran = new Random(0);
-        int_arr = IntStream.range(0, COUNT)
-            .mapToObj(i -> ran.nextInt(Byte.MAX_VALUE))
-            .mapToInt(Integer::intValue)
-            .toArray();
-        long_arr = Arrays.stream(int_arr)
-            .mapToLong(i -> i)
-            .toArray();
-        short_arr = new short[COUNT];
-        byte_arr = new byte[COUNT];
-        double_arr = new double[COUNT];
+        long_float_arr = new long[COUNT];
+        long_double_arr = new long[COUNT];
+        int_float_arr = new int[COUNT];
+        int_double_arr = new int[COUNT];
+        short_float_arr = new short[COUNT];
+        short_double_arr = new short[COUNT];
+        byte_float_arr = new byte[COUNT];
+        byte_double_arr = new byte[COUNT];
         float_arr = new float[COUNT];
+        double_arr = new double[COUNT];
 
+        G.fill(genF, float_arr);
+        G.fill(genD, double_arr);
         for (int i = 0; i < COUNT; i++) {
-            int floor_val = int_arr[i];
-            int ceil_val = floor_val + 1;
-            short_arr[i] = (short) floor_val;
-            byte_arr[i] = (byte) floor_val;
-            double_arr[i] = ran.nextDouble(floor_val, ceil_val);
-            float_arr[i] = (float) double_arr[i];
+            long_float_arr[i] = (long) float_arr[i];
+            long_double_arr[i] = (long) double_arr[i];
+            int_float_arr[i] = (int) float_arr[i];
+            int_double_arr[i] = (int) double_arr[i];
+            short_float_arr[i] = (short) float_arr[i];
+            short_double_arr[i] = (short) double_arr[i];
+            byte_float_arr[i] = (byte) float_arr[i];
+            byte_double_arr[i] = (byte) double_arr[i];
         }
     }
 
@@ -86,7 +95,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             float float_val = float_arr[i];
             int computed = (int) float_val;
-            int expected = int_arr[i];
+            int expected = int_float_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -101,7 +110,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             float float_val = float_arr[i];
             long computed = (long) float_val;
-            long expected = long_arr[i];
+            long expected = long_float_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -116,7 +125,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             float float_val = float_arr[i];
             short computed = (short) float_val;
-            short expected = short_arr[i];
+            short expected = short_float_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -131,7 +140,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             float float_val = float_arr[i];
             byte computed = (byte) float_val;
-            byte expected = byte_arr[i];
+            byte expected = byte_float_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -146,7 +155,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             double double_val = double_arr[i];
             int computed = (int) double_val;
-            int expected = int_arr[i];
+            int expected = int_double_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -161,7 +170,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             double double_val = double_arr[i];
             long computed = (long) double_val;
-            long expected = long_arr[i];
+            long expected = long_double_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -176,7 +185,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             double double_val = double_arr[i];
             short computed = (short) double_val;
-            short expected = short_arr[i];
+            short expected = short_double_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
@@ -191,7 +200,7 @@ public class ScalarFPtoIntCastTest {
         for (int i = 0; i < COUNT; i++) {
             double double_val = double_arr[i];
             byte computed = (byte) double_val;
-            byte expected = byte_arr[i];
+            byte expected = byte_double_arr[i];
             Verify.checkEQ(computed, expected);
         }
     }
