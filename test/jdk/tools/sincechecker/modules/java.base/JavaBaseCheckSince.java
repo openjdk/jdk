ba42126ae22217629e9e2a1d2745a89051ddd6e0
Nizar Benalla <nizar.benalla@oracle.com>
2025-09-23 15:09:58 +0100

    (c)

--- test/jdk/tools/sincechecker/modules/java.base/JavaBaseCheckSince.java
+++ test/jdk/tools/sincechecker/modules/java.base/JavaBaseCheckSince.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, 2025 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8331051
+ * @bug 8331051 8367610
  * @summary Test for `@since` in java.base module
  * @library /test/lib /test/jdk/tools/sincechecker
  * @run main/timeout=480 SinceChecker java.base --exclude java.lang.classfile
