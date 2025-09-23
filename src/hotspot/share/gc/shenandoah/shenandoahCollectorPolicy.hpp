c962f1988427d3c37cdc44926504cba8e514211c
William Kemper <kemperw@amazon.com>
2025-09-23 14:04:10 -0700

    Fix typo in comment

--- src/hotspot/share/gc/shenandoah/shenandoahCollectorPolicy.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahCollectorPolicy.hpp
@@ -83,7 +83,7 @@ public:
   // Record that a degenerated cycle has been completed. Note that such a cycle may or
   // may not make "progress". We separately track the total number of degenerated cycles,
   // the number of consecutive degenerated cycles and the number of consecutive cycles that
-  // fail ot make good progress.
+  // fail to make good progress.
   void record_degenerated(bool is_young, bool is_abbreviated, bool progress);
   void record_success_full();
   void record_alloc_failure_to_degenerated(ShenandoahGC::ShenandoahDegenPoint point);
