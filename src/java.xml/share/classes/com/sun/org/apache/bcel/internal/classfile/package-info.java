0e813612caa1b7831b080fef061929e33149c743
JoeWang-Java <huizhe.wang@oracle.com>
2025-09-23 21:09:06 +0000

    8336695: Update Commons BCEL to Version 6.10.0

--- src/java.xml/share/classes/com/sun/org/apache/bcel/internal/classfile/package-info.java
+++ src/java.xml/share/classes/com/sun/org/apache/bcel/internal/classfile/package-info.java
@@ -12,19 +12,14 @@
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
-
-package com.sun.org.apache.bcel.internal.classfile;
 
 /**
- * Denote class to have an accept method();
+ * Classes that describe the structure of a Java class file and a class file parser.
  */
-public interface Node {
-
-    void accept(Visitor visitor);
-}
+package com.sun.org.apache.bcel.internal.classfile;
