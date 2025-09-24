106362e941d67377e52c178277c32740dc74a4b1
Guanqiang Han <yongheng_hgq@126.com>
2025-09-24 09:10:46 +0800

    Update g1PeriodicGCTask.cpp
    fix a small error

--- src/hotspot/share/gc/g1/g1PeriodicGCTask.cpp
+++ src/hotspot/share/gc/g1/g1PeriodicGCTask.cpp
@@ -57,7 +57,7 @@ bool G1PeriodicGCTask::should_start_periodic_gc(G1CollectedHeap* g1h,
   if (G1PeriodicGCSystemLoadThreshold > 0.0) {
     if (os::loadavg(&recent_load, 1) == -1) {
       G1PeriodicGCSystemLoadThreshold = 0.0;
-      log_warning(gc, periodic)("System loadavg not supported, G1PeriodicGCSystemLoadThreshold check is disabled");
+      log_warning(gc, periodic)("System loadavg not supported, G1PeriodicGCSystemLoadThreshold check is disabled.");
       return false;
     }
     if (recent_load > G1PeriodicGCSystemLoadThreshold) {
