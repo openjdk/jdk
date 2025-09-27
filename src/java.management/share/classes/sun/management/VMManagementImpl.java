ae29e031dcfee0f2ff01197995d831de8d973ad7
Jonas Norlinder <jonas.norlinder@oracle.com>
2025-09-27 18:29:51 +0200

    Remove extra new line

--- src/java.management/share/classes/sun/management/VMManagementImpl.java
+++ src/java.management/share/classes/sun/management/VMManagementImpl.java
@@ -33,7 +33,6 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.List;
-
 import java.util.Arrays;
 import java.util.Collections;
 
