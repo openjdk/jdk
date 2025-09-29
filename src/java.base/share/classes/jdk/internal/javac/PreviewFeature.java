a2b2dc7c01230cb5931a9faed18ee72b0a6a0374
Per Minborg <pminborg@gmail.com>
2025-09-29 09:28:49 +0200

    Update JEP number

--- src/java.base/share/classes/jdk/internal/javac/PreviewFeature.java
+++ src/java.base/share/classes/jdk/internal/javac/PreviewFeature.java
@@ -76,7 +76,7 @@ public @interface PreviewFeature {
         STREAM_GATHERERS,
         MODULE_IMPORTS, //remove when the boot JDK is JDK 25
         KEY_DERIVATION, //remove when the boot JDK is JDK 25
-        @JEP(number = 8359894, title = "Lazy Constants", status = "Second Preview")
+        @JEP(number = 526, title = "Lazy Constants", status = "Second Preview")
         LAZY_CONSTANTS,
         @JEP(number=470, title="PEM Encodings of Cryptographic Objects", status="Preview")
         PEM_API,
