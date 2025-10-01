bf9848381fcc7758b3389cf0dedca9092d3a3306
rwestrel <rwestrel@redhat.com>
2025-10-01 16:55:38 +0200

    test seed

--- test/hotspot/jtreg/compiler/c2/TestMismatchedAddPAfterMaxUnroll.java
+++ test/hotspot/jtreg/compiler/c2/TestMismatchedAddPAfterMaxUnroll.java
@@ -27,7 +27,7 @@
  * @summary C2 crash: assertion failed: Base pointers must match (addp 344)
  * @run main/othervm -XX:-BackgroundCompilation -XX:CompileOnly=TestMismatchedAddPAfterMaxUnroll::test1
  *                   -XX:+UnlockDiagnosticVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:-UseLoopPredicate
- *                   -XX:+StressIGVN -XX:StressSeed=1572080606 TestMismatchedAddPAfterMaxUnroll
+ *                   -XX:+StressIGVN -XX:StressSeed=383593806 TestMismatchedAddPAfterMaxUnroll
  * @run main/othervm -XX:-BackgroundCompilation -XX:CompileOnly=TestMismatchedAddPAfterMaxUnroll::test1
  *                   -XX:+UnlockDiagnosticVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:-UseLoopPredicate
  *                   -XX:+StressIGVN TestMismatchedAddPAfterMaxUnroll
