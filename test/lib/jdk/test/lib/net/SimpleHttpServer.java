15e71bba6f0a9fd4281c9d45c0a7000b202f779c
Mahendra Chhipa <mahendra.chhipa@oracle.com>
2025-10-02 14:08:45 +0100

    JDK-8367114: Update jdk.test.lib.net.SimpleHttpServer to use SimpleFileServer

--- test/lib/jdk/test/lib/net/SimpleHttpServer.java
+++ test/lib/jdk/test/lib/net/SimpleHttpServer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,47 +23,24 @@
 package jdk.test.lib.net;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.nio.file.FileSystemNotFoundException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-import com.sun.net.httpserver.Headers;
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.*;
 
 /**
  * A simple HTTP Server.
  **/
 public class SimpleHttpServer {
     private final HttpServer httpServer;
-    private ExecutorService executor;
     private String address;
-    private final String context;
-    private final String docRoot;
-    private final InetSocketAddress inetSocketAddress;
 
-    public SimpleHttpServer(final InetSocketAddress inetSocketAddress, final String context, final String docRoot)
-            throws IOException {
-        this.inetSocketAddress = inetSocketAddress;
-        this.context = context;
-        this.docRoot = docRoot;
-        httpServer = HttpServer.create();
+    public SimpleHttpServer(final InetSocketAddress inetSocketAddress, final String context, final String docRoot) {
+        httpServer = SimpleFileServer.createFileServer(inetSocketAddress, Path.of(docRoot), SimpleFileServer.OutputLevel.INFO);
     }
 
     public void start() throws IOException, URISyntaxException {
-        MyHttpHandler handler = new MyHttpHandler(docRoot);
-        httpServer.bind(inetSocketAddress, 0);
-        httpServer.createContext(context, handler);
-        executor = Executors.newCachedThreadPool();
-        httpServer.setExecutor(executor);
         httpServer.start();
         address = "http:" + URIBuilder.newBuilder().host(httpServer.getAddress().getAddress()).
                 port(httpServer.getAddress().getPort()).build().toString();
@@ -71,7 +48,6 @@ public class SimpleHttpServer {
 
     public void stop() {
         httpServer.stop(0);
-        executor.shutdown();
     }
 
     public String getAddress() {
@@ -80,73 +56,5 @@ public class SimpleHttpServer {
 
     public int getPort() {
         return httpServer.getAddress().getPort();
-    }
-
-    class MyHttpHandler implements HttpHandler {
-        private final URI rootUri;
-
-        MyHttpHandler(final String docroot) {
-            rootUri = Path.of(docroot).toUri().normalize();
-        }
-
-        public void handle(final HttpExchange t) throws IOException {
-            try (InputStream is = t.getRequestBody()) {
-                is.readAllBytes();
-                Headers rMap = t.getResponseHeaders();
-                try (OutputStream os = t.getResponseBody()) {
-                    URI uri = t.getRequestURI();
-                    String path = uri.getRawPath();
-                    assert path.isEmpty() || path.startsWith("/");
-                    Path fPath;
-                    try {
-                        uri = URI.create("file://" + rootUri.getRawPath() + path).normalize();
-                        fPath = Path.of(uri);
-                    } catch (IllegalArgumentException | FileSystemNotFoundException ex) {
-                        ex.printStackTrace();
-                        notfound(t, path);
-                        return;
-                    }
-                    byte[] bytes = Files.readAllBytes(fPath);
-                    String method = t.getRequestMethod();
-                    if (method.equals("HEAD")) {
-                        rMap.set("Content-Length", Long.toString(bytes.length));
-                        t.sendResponseHeaders(200, -1);
-                        t.close();
-                    } else if (!method.equals("GET")) {
-                        t.sendResponseHeaders(405, -1);
-                        t.close();
-                        return;
-                    }
-                    if (path.endsWith(".html") || path.endsWith(".htm")) {
-                        rMap.set("Content-Type", "text/html");
-                    } else {
-                        rMap.set("Content-Type", "text/plain");
-                    }
-                    t.sendResponseHeaders(200, bytes.length);
-                    os.write(bytes);
-                }
-            }
-        }
-        void moved(final HttpExchange t) throws IOException {
-            Headers req = t.getRequestHeaders();
-            Headers map = t.getResponseHeaders();
-            URI uri = t.getRequestURI();
-            String host = req.getFirst("Host");
-            String location = "http://" + host + uri.getPath() + "/";
-            map.set("Content-Type", "text/html");
-            map.set("Location", location);
-            t.sendResponseHeaders(301, -1);
-            t.close();
-        }
-        void notfound(final HttpExchange t, final String p) throws IOException {
-            t.getResponseHeaders().set("Content-Type", "text/html");
-            t.sendResponseHeaders(404, 0);
-            try (OutputStream os = t.getResponseBody()) {
-                String s = "<h2>File not found</h2>";
-                s = s + p + "<p>";
-                os.write(s.getBytes());
-            }
-            t.close();
-        }
     }
 }
