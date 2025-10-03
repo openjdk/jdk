4b353ef9246dbe4d224bdf3ce667835c7058877a
Hannes Wallnoefer <hannes.wallnoefer@oracle.com>
2025-10-03 12:50:09 +0200

    8276966: Improve diagnostic output for the mismatching parts of a hybrid snippet

--- test/langtools/jdk/javadoc/doclet/testSnippetTag/TestSnippetTag.java
+++ test/langtools/jdk/javadoc/doclet/testSnippetTag/TestSnippetTag.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -2166,6 +2166,7 @@ public class TestSnippetTag extends SnippetTester {
                         Hello, Snippet!
                         ----------------- external -----------------
                         Hello, Snippet!...more
+                        --------------------------------------------
                         </pre>
                         </details>
                         """);
@@ -2219,6 +2220,7 @@ public class TestSnippetTag extends SnippetTester {
                         ----------------- external -----------------
                         Hello, Snippet!
 
+                        --------------------------------------------
                         </pre>
                         </details>
                         """);
