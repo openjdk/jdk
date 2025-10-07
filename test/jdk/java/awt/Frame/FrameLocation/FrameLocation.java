60e5276f7719a5d20e40e3bc1147ce34875bf0e6
Tejesh R <tejesh.r@oracle.com>
2025-10-07 11:53:55 +0530

    Revert EDT and add screen capture

--- test/jdk/java/awt/Frame/FrameLocation/FrameLocation.java
+++ test/jdk/java/awt/Frame/FrameLocation/FrameLocation.java
@@ -30,7 +30,6 @@
  */
 
 import java.awt.Dimension;
-import java.awt.EventQueue;
 import java.awt.Frame;
 import java.awt.Point;
 import java.awt.Rectangle;
@@ -42,51 +41,39 @@ import javax.imageio.ImageIO;
 public class FrameLocation {
     private static final int X = 250;
     private static final int Y = 250;
-    private static Frame f;
-    private static volatile int xPos;
-    private static volatile int yPos;
 
     public static void main(String[] args) throws Exception {
         Robot robot = new Robot();
-        try {
-            EventQueue.invokeAndWait(() -> {
-                f = new Frame("Frame Location Test");
-                f.setBounds(X, Y, 250, 250); // the size doesn't matter
-                f.setVisible(true);
-            });
+        Frame f = new Frame("test");
+        f.setBounds(X, Y, 250, 250); // the size doesn't matter
+        f.setVisible(true);
 
-            for (int i = 0; i < 10; i++) {
-                // 2 seconds must be enough for the WM to show the window
-                robot.waitForIdle();
-                robot.delay(2000);
-
-                EventQueue.invokeAndWait(() -> {
-                    // Check the location
-                    xPos = f.getX();
-                    yPos = f.getY();
-                });
-
-                if (xPos != X || yPos != Y) {
-                    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-                    ImageIO.write(robot.createScreenCapture(
-                                    new Rectangle(new Point(), screenSize)),
-                            "png", new File("FailureImage.png"));
-                    throw new RuntimeException("The frame location is wrong! Current: " +
-                            xPos + ", " + yPos + ";  expected: " + X + ", " + Y);
-                }
-
-                // Emulate what happens when setGraphicsConfiguration() is called
-                synchronized (f.getTreeLock()) {
-                    f.removeNotify();
-                    f.addNotify();
-                }
+        for (int i = 0; i < 10; i++) {
+            // 2 seconds must be enough for the WM to show the window
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
             }
-        } finally {
-            EventQueue.invokeAndWait(() -> {
-                if (f != null) {
-                    f.dispose();
-                }
-            });
+
+            // Check the location
+            int x = f.getX();
+            int y = f.getY();
+
+            if (x != X || y != Y) {
+                Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+                ImageIO.write(robot.createScreenCapture(
+                                new Rectangle(new Point(), screenSize)),
+                        "png", new File("FailureImage.png"));
+                throw new RuntimeException("The frame location is wrong! Current: " + x + ", " + y + ";  expected: " + X + ", " + Y);
+            }
+
+            // Emulate what happens when setGraphicsConfiguration() is called
+            synchronized (f.getTreeLock()) {
+                f.removeNotify();
+                f.addNotify();
+            }
         }
+
+        f.dispose();
     }
 }
