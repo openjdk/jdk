d93815c9f57fa63ec2e8d8c911c1bae85dac60f0
jeremy <jeremy.wood@mac.com>
2025-10-07 13:24:03 -0400

    8367376: adding missing UIManager import
    This is in response to:
    https://git.openjdk.org/jdk/pull/27205#discussion_r2409647336

--- test/jdk/com/sun/java/swing/plaf/DesktopPropertyResetPendingFlagTest.java
+++ test/jdk/com/sun/java/swing/plaf/DesktopPropertyResetPendingFlagTest.java
@@ -34,6 +34,7 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import javax.swing.plaf.basic.BasicButtonUI;
 
 import sun.swing.plaf.DesktopProperty;
