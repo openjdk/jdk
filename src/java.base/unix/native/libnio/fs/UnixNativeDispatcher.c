d70adec10e564ece94a092518fce6a228b0f7cd2
Aleksei Voitylov <voitylov@gmail.com>
2025-10-09 13:47:23 +0500

    remove extra _

--- src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
+++ src/java.base/unix/native/libnio/fs/UnixNativeDispatcher.c
@@ -55,7 +55,7 @@
 #include <grp.h>
 
 #ifdef __linux__
-#include <stdint.h> // For uint_XX_t types used in statx support
+#include <stdint.h> // For uintXX_t types used in statx support
 #include <sys/syscall.h>
 #include <sys/sysmacros.h> // makedev macros
 #endif
