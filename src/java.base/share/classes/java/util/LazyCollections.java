1f72ef91d27a4e15cec2b541ce870914ef2d8cfa
Per-Ake Minborg <pminborg@gmail.com>
2025-10-10 15:18:08 +0200

    Update src/java.base/share/classes/java/util/LazyCollections.java
    Co-authored-by: Francesco Andreuzzi <andreuzzi.francesco@gmail.com>

--- src/java.base/share/classes/java/util/LazyCollections.java
+++ src/java.base/share/classes/java/util/LazyCollections.java
@@ -77,7 +77,7 @@ final class LazyCollections {
             super();
         }
 
-        @Override public boolean  isEmpty() { return size == 0;}
+        @Override public boolean  isEmpty() { return size == 0; }
         @Override public int      size() { return size; }
         @Override public Object[] toArray() { return copyInto(new Object[size]); }
 
