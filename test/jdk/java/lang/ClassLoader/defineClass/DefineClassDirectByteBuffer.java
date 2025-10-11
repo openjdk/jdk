43b61040f9fc61b56b6b9a670ba4d99d379b82cb
Xueming Shen <xueming.shen@oracle.com>
2025-10-10 18:21:48 -0700

    test case update

--- test/jdk/java/lang/ClassLoader/defineClass/DefineClassDirectByteBuffer.java
+++ test/jdk/java/lang/ClassLoader/defineClass/DefineClassDirectByteBuffer.java
@@ -37,6 +37,10 @@ import java.lang.reflect.Method;
 import java.nio.*;
 import java.nio.channels.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.security.ProtectionDomain;
 import java.util.HexFormat;
 import java.util.stream.Stream;
@@ -54,30 +58,43 @@ public class DefineClassDirectByteBuffer {
     // this is for the trusted/biltin classloader
     private static final String mode = System.getProperty("mode", "Direct");
 
-    public static final int ARRAY_BUFFER = 0;
-    public static final int ARRAY_BUFFER_READONLY = 1;
-    public static final int DIRECT_BUFFER = 2;
-    public static final int DIRECT_BUFFER_READONLY = 3;
-
+    static final int ARRAY_BUFFER = 0;
+    static final int WRAPPED_BUFFER = 1;
+    static final int DIRECT_BUFFER = 2;
+    static final int FOREIGN_BUFFER = 3;
+    static final int MAPPED_BUFFER = 4;
 
     // -------- untrusted path (custom loader) --------
     static Stream<Arguments> bufferTypes() {
         return Stream.of(
-                arguments(ARRAY_BUFFER, 0, false),
-                arguments(ARRAY_BUFFER_READONLY, 0, true),
-                arguments(DIRECT_BUFFER, 0, false),
-                arguments(DIRECT_BUFFER_READONLY, 0, false),
-                arguments(ARRAY_BUFFER, 16, false),
-                arguments(ARRAY_BUFFER_READONLY, 16, true),
-                arguments(DIRECT_BUFFER, 16, false),
-                arguments(DIRECT_BUFFER_READONLY, 16, false)
+                // type, readonly, pos, posToLimit
+                arguments(ARRAY_BUFFER, false, 0, false),
+                arguments(ARRAY_BUFFER, false, 16, false),
+                arguments(ARRAY_BUFFER, true, 0, false),
+                arguments(ARRAY_BUFFER, true, 16, false),
+
+                arguments(WRAPPED_BUFFER, false, 0, false),
+                arguments(WRAPPED_BUFFER, false, 16, false),
+                arguments(WRAPPED_BUFFER, true, 0, true),
+                arguments(WRAPPED_BUFFER, true, 16, true),
+
+                arguments(DIRECT_BUFFER, false, 0, false),
+                arguments(DIRECT_BUFFER, false, 16, false),
+                arguments(DIRECT_BUFFER, true, 0, false),
+                arguments(DIRECT_BUFFER, true, 16, false),
+
+                arguments(FOREIGN_BUFFER, false, 0, false),
+                arguments(FOREIGN_BUFFER, false, 16, false),
+                arguments(FOREIGN_BUFFER, true, 0, false),
+                arguments(FOREIGN_BUFFER, true, 16, false),
+
+                // MapMode.READ_ONLY from READ fc, the bb is readonly
+                arguments(MAPPED_BUFFER, false, 0, false),
+                arguments(MAPPED_BUFFER, false, 16, false)
         );
     }
 
-    @ParameterizedTest()
-    @MethodSource("bufferTypes")
-    void testDefineClassWithCustomLoaderByteBuffer(int type, int pos,  boolean posAtLimit) throws Exception {
-        CustomClassLoader loader = new CustomClassLoader();
+    static ByteBuffer getByteBufferWithTestClassBytes(int type, int pos) throws Exception {
         byte[] classBytes = getTestClassBytes();
         if (pos != 0) {
             byte[] newBytes = new byte[classBytes.length + pos];
@@ -86,43 +103,57 @@ public class DefineClassDirectByteBuffer {
         }
         switch (type) {
             case ARRAY_BUFFER -> {
-                var bb = ByteBuffer.wrap(classBytes).position(pos);
-                Class<?> clazz = loader.defineClassFromByteBuffer(bb);
-                assertInvocating(clazz);
-                assertEquals(posAtLimit, bb.position() == bb.limit());
+                return ByteBuffer.allocateDirect(classBytes.length)
+                        .put(classBytes)
+                        .flip()
+                        .position(pos);
             }
-            case ARRAY_BUFFER_READONLY -> {
-                var bb = ByteBuffer.wrap(classBytes).position(pos).asReadOnlyBuffer();
-                Class<?> clazz = loader.defineClassFromByteBuffer(bb);
-                assertInvocating(clazz);
-                assertEquals(posAtLimit, bb.position() == bb.limit());
+            case WRAPPED_BUFFER -> {
+                return ByteBuffer.wrap(classBytes).position(pos);
             }
             case DIRECT_BUFFER -> {
-                try (Arena arena = Arena.ofConfined()) {
-                    var bb = arena.allocate(classBytes.length)
-                            .asByteBuffer()
-                            .put(classBytes)
-                            .flip()
-                            .position(pos);
-                    Class<?> clazz = loader.defineClassFromByteBuffer(bb);
-                    assertInvocating(clazz);
-                    assertEquals(posAtLimit, bb.position() == bb.limit());
-                }
+                return ByteBuffer
+                        .allocateDirect(classBytes.length)
+                        .put(classBytes)
+                        .flip()
+                        .position(pos);
             }
-            case DIRECT_BUFFER_READONLY -> {
-                try (Arena arena = Arena.ofConfined()) {
-                    var bb = arena.allocate(classBytes.length)
-                            .asByteBuffer()
-                            .put(classBytes)
-                            .flip()
-                            .position(pos)
-                            .asReadOnlyBuffer();
-                    Class<?> clazz = loader.defineClassFromByteBuffer(bb);
-                    assertInvocating(clazz);
-                    assertEquals(posAtLimit, bb.position() == bb.limit());
+            case FOREIGN_BUFFER -> {
+                return Arena
+                        .ofConfined()
+                        .allocate(classBytes.length)
+                        .asByteBuffer()
+                        .put(classBytes)
+                        .flip()
+                        .position(pos);
+            }
+            case MAPPED_BUFFER -> {
+                Path tempDir = Paths.get(System.getProperty("test.classes", "."));
+                Files.createDirectories(tempDir);
+                Path tempClassFile = tempDir.resolve("DefineClassDirectByteBuffer_Greeting.class");
+                Files.write(tempClassFile, classBytes);
+                try (FileChannel fc = FileChannel.open(tempClassFile, StandardOpenOption.READ)) {
+                    return fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size())
+                            .position(pos);
                 }
             }
         }
+        return null;
+    }
+
+    @ParameterizedTest()
+    @MethodSource("bufferTypes")
+    void testDefineClassWithCustomLoaderByteBuffer(int type, boolean readonly, int pos, boolean posAtLimit)
+            throws Exception
+    {
+        ByteBuffer bb = getByteBufferWithTestClassBytes(type, pos);
+        if (readonly) {
+            bb = bb.asReadOnlyBuffer();
+        }
+        CustomClassLoader loader = new CustomClassLoader();
+        Class<?> clazz = loader.defineClassFromByteBuffer(bb);
+        assertInvocating(clazz);
+        assertEquals(posAtLimit, bb.position() == bb.limit());
     }
 
     // -------- trusted path (BuiltinClassLoader) --------
@@ -130,29 +161,16 @@ public class DefineClassDirectByteBuffer {
     void testDefineClassWithBuiltinLoaderByteBuffer() throws Exception {
         var classBytes = getTestClassBytes();
         var builtin = ClassLoader.getPlatformClassLoader();
-        if (mode.equals("Direct")) {
-            // ffm.arena
-            try (Arena arena = Arena.ofConfined()) {
-                var bb = arena.allocate(classBytes.length).asByteBuffer();
-                bb.put(classBytes).flip();
-                // reflectively call protected defineClass(String, ByteBuffer, ProtectionDomain)
-                Method m = ClassLoader.class.getDeclaredMethod(
-                        "defineClass", String.class, ByteBuffer.class, ProtectionDomain.class
-                );
-                m.setAccessible(true);
-                Class<?> clazz = (Class<?>) m.invoke(builtin, null, bb, null);
-                assertInvocating(clazz);
-            }
-        } else {
-            var bb = ByteBuffer.wrap(classBytes);
-            Method m = ClassLoader.class.getDeclaredMethod(
-                    "defineClass", String.class, ByteBuffer.class, ProtectionDomain.class
-            );
-            m.setAccessible(true);
-            Class<?> clazz = (Class<?>) m.invoke(builtin, null, bb, null);
-            assertInvocating(clazz);
-        }
-     }
+        var bb = getByteBufferWithTestClassBytes(
+                mode.equals("Direct") ? DIRECT_BUFFER : ARRAY_BUFFER, 0);
+        // reflectively call protected defineClass(String, ByteBuffer, ProtectionDomain)
+        Method m = ClassLoader.class.getDeclaredMethod(
+                "defineClass", String.class, ByteBuffer.class, ProtectionDomain.class
+        );
+        m.setAccessible(true);
+        Class<?> clazz = (Class<?>) m.invoke(builtin, null, bb, null);
+        assertInvocating(clazz);
+    }
 
     // -------- shared helpers --------
     private static void assertInvocating(Class<?> clazz) throws Exception {
@@ -169,25 +187,25 @@ public class DefineClassDirectByteBuffer {
 
     private static byte[] getTestClassBytes() throws Exception {
         final String source = """
-            public class Greeting {
-                public String hello() {
-                    return "Hello";
+                public class Greeting {
+                    public String hello() {
+                        return "Hello";
+                    }
                 }
-            }
-            """;
+                """;
         // (externally) compiled content of the above source, represented as hex
         final String classBytesHex = """
-            cafebabe0000004600110a000200030700040c000500060100106a617661
-            2f6c616e672f4f626a6563740100063c696e69743e010003282956080008
-            01000548656c6c6f07000a0100084772656574696e67010004436f646501
-            000f4c696e654e756d6265725461626c6501000568656c6c6f0100142829
-            4c6a6176612f6c616e672f537472696e673b01000a536f7572636546696c
-            6501000d4772656574696e672e6a61766100210009000200000000000200
-            01000500060001000b0000001d00010001000000052ab70001b100000001
-            000c000000060001000000010001000d000e0001000b0000001b00010001
-            000000031207b000000001000c000000060001000000030001000f000000
-            020010
-            """;
+                cafebabe0000004600110a000200030700040c000500060100106a617661
+                2f6c616e672f4f626a6563740100063c696e69743e010003282956080008
+                01000548656c6c6f07000a0100084772656574696e67010004436f646501
+                000f4c696e654e756d6265725461626c6501000568656c6c6f0100142829
+                4c6a6176612f6c616e672f537472696e673b01000a536f7572636546696c
+                6501000d4772656574696e672e6a61766100210009000200000000000200
+                01000500060001000b0000001d00010001000000052ab70001b100000001
+                000c000000060001000000010001000d000e0001000b0000001b00010001
+                000000031207b000000001000c000000060001000000030001000f000000
+                020010
+                """;
         return HexFormat.of().parseHex(classBytesHex.replaceAll("\n", ""));
     }
 }
