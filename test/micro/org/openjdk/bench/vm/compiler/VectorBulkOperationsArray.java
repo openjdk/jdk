4382e025ae7a9cc5bfe1233458592c6f30cf6622
Emanuel Peter <emanuel.peter@oracle.com>
2025-10-14 17:31:23 +0200

    clean up benchmark

--- test/micro/org/openjdk/bench/vm/compiler/VectorBulkOperationsArray.java
+++ test/micro/org/openjdk/bench/vm/compiler/VectorBulkOperationsArray.java
@@ -30,34 +30,26 @@ import java.util.Random;
 import java.util.Arrays;
 
 /**
- * TODO:
- * - Array, MemorySegment
- * - copy, fill, maybe more. (Array.fill, System.arraycopy)
- * - over various sizes
- * - control: alignment, 4k-aliasing
- * - OptimizeFill
- * - fill: with constant 0, and with some other arbitrary constant/non-constant.
- * - Impact of CompactObjectHeaders
- * - Object array? Which primitives?
- * - -XX:SuperWordAutomaticAlignment=0
- * - Limit unroll factor
- * - Investigate performance on various platforms.
- * - Compare to Graal?
+ * This benchmark is here to measure vectorized peformance for some simple bulk operations:
+ * - fill
+ * - copy
  *
- * TODO:: how to run the test:
- * make test TEST="micro:vm.compiler.VectorBulkOperationsArray.fill_var.*" CONF=linux-x64 TEST_VM_OPTS="-XX:+OptimizeFill -XX:UseAVX=2" MICRO="OPTIONS=-prof perfasm -p NUM_ACCESS_ELEMENTS=10000"
- * make test TEST="micro:vm.compiler.VectorBulkOperationsArray.copy_byte.*" CONF=linux-x64 TEST_VM_OPTS="-XX:+OptimizeFill -XX:UseAVX=2" MICRO="FORK=3;OPTIONS=-prof perfasm -p NUM_ACCESS_ELEMENTS=10000"
- * make test TEST="micro:vm.compiler.VectorBulkOperationsArray.copy_byte.*" CONF=linux-x64 TEST_VM_OPTS="-XX:+OptimizeFill -XX:UseAVX=3 -XX:LoopMaxUnroll=64" MICRO="FORK=3;OPTIONS=-prof perfasm -p NUM_ACCESS_ELEMENTS=10000"
+ * We may add more in the future, for example:
+ * - comparison
+ * - find index
+ * - filter
+ * - ...
  *
- * make test TEST="micro:VectorBulkOperationsArray.copy_.*_loop" CONF=linux-x64 TEST_VM_OPTS="-XX:+UnlockDiagnosticVMOptions -XX:AutoVectorizationOverrideProfitability=1" MICRO="OPTIONS=-p NUM_ACCESS_ELEMENTS=1,2,3,4,8,16,32,64"
+ * One important feature of this benchmark, is that we control for alignment and 4k-aliasing,
+ * something almost no benchmarks have considered up to now. But it is important to get more
+ * precise, clean and reliable results.
  *
- * TODO: observations
- * - Arrays.fill is sensitive to OptimizeFill, loop version not. That's a little strange, would have expected it to get replaced by stub.
- *   They do use different vector instructions though on AVX512, but same vector length. Maybe it is also the loops of auto-vec that
- *   are more complicated. And more unrolling, maybe too much? Ok, but then if I go to AVX2 it all looks way different, maybe just an artifact.
- * - Similar effect with copy_byte. Wow. There is a lot to be gained here probably.
- * - It also seems that -XX:LoopMaxUnroll=64 is not really repected. I think I've seen this before. It only seems to work before SuperWord, but not after.
- *   Right, but -XX:LoopMaxUnroll=32 is respected ... before SuperWord but then still super-unrolled. Fail.
+ * Note, you may want to play with "-XX:-OptimizeFill" for the fill benchmarks, so that we do
+ * not use the fill-intrinsic, but auto-vectorize. Though I'm currently not seeing a difference,
+ * maybe the loop is not recognized properly? TODO: investigate.
+ *
+ * Please also look at the companion benchmark:
+ *   VectorBulkOperationsMemorySegment.java
  */
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
@@ -66,46 +58,37 @@ import java.util.Arrays;
 @Measurement(iterations = 5, time = 1000, timeUnit = TimeUnit.MILLISECONDS)
 @Fork(value = 1)
 public class VectorBulkOperationsArray {
-    @Param({  "0", "8", "256", "10000" })
-    // TODO: replace with below
-    //// @Param({  "0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",
-    ////          "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
-    ////          "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
-    ////          "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
-    ////          "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
-    ////          "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
-    ////          "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
-    ////          "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
-    ////          "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
-    ////          "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
-    ////         "100","101","102","103","104","105","106","107","108","109",
-    ////         "110","111","112","113","114","115","116","117","118","119",
-    ////         "120","121","122","123","124","125","126","127","128","129",
-    ////         "130","131","132","133","134","135","136","137","138","139",
-    ////         "140","141","142","143","144","145","146","147","148","149",
-    ////         "150","151","152","153","154","155","156","157","158","159",
-    ////         "160","161","162","163","164","165","166","167","168","169",
-    ////         "170","171","172","173","174","175","176","177","178","179",
-    ////         "180","181","182","183","184","185","186","187","188","189",
-    ////         "190","191","192","193","194","195","196","197","198","199",
-    ////         "200","201","202","203","204","205","206","207","208","209",
-    ////         "210","211","212","213","214","215","216","217","218","219",
-    ////         "220","221","222","223","224","225","226","227","228","229",
-    ////         "230","231","232","233","234","235","236","237","238","239",
-    ////         "240","241","242","243","244","245","246","247","248","249",
-    ////         "250","251","252","253","254","255","256","257","258","259",
-    ////         "260","261","262","263","264","265","266","267","268","269",
-    ////         "270","271","272","273","274","275","276","277","278","279",
-    ////         "280","281","282","283","284","285","286","287","288","289",
-    ////         "290","291","292","293","294","295","296","297","298","299",
-    ////         "300",
-    ////         // Above, the "small loops".
-    ////         // Below, some "medium" to "large" loops.
-    ////         "1000", "3000", "10000"})
-    // Describes how many array elements are accessed, i.e. how many
-    // elements we loop over.
-    // TODO: add some really large sizes, to see what happens when we run out
-    // of L1 cache etc.
+    @Param({  "0",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",
+             "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+             "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+             "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+             "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+             "50", "51", "52", "53", "54", "55", "56", "57", "58", "59",
+             "60", "61", "62", "63", "64", "65", "66", "67", "68", "69",
+             "70", "71", "72", "73", "74", "75", "76", "77", "78", "79",
+             "80", "81", "82", "83", "84", "85", "86", "87", "88", "89",
+             "90", "91", "92", "93", "94", "95", "96", "97", "98", "99",
+            "100","101","102","103","104","105","106","107","108","109",
+            "110","111","112","113","114","115","116","117","118","119",
+            "120","121","122","123","124","125","126","127","128","129",
+            "130","131","132","133","134","135","136","137","138","139",
+            "140","141","142","143","144","145","146","147","148","149",
+            "150","151","152","153","154","155","156","157","158","159",
+            "160","161","162","163","164","165","166","167","168","169",
+            "170","171","172","173","174","175","176","177","178","179",
+            "180","181","182","183","184","185","186","187","188","189",
+            "190","191","192","193","194","195","196","197","198","199",
+            "200","201","202","203","204","205","206","207","208","209",
+            "210","211","212","213","214","215","216","217","218","219",
+            "220","221","222","223","224","225","226","227","228","229",
+            "230","231","232","233","234","235","236","237","238","239",
+            "240","241","242","243","244","245","246","247","248","249",
+            "250","251","252","253","254","255","256","257","258","259",
+            "260","261","262","263","264","265","266","267","268","269",
+            "270","271","272","273","274","275","276","277","278","279",
+            "280","281","282","283","284","285","286","287","288","289",
+            "290","291","292","293","294","295","296","297","298","299",
+            "300"})
     public static int NUM_ACCESS_ELEMENTS;
 
     // Every array has two regions:
