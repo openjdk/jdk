f2938a1b7e1058b9e2d08dc5c3789e0b1e43a709
Jaikiran Pai <jaikiran.pai@gmail.com>
2025-10-14 15:59:26 +0530

    Daniel's review, use a class level field for URLConnection

--- test/jdk/sun/net/www/protocol/file/FileURLConnStreamLeakTest.java
+++ test/jdk/sun/net/www/protocol/file/FileURLConnStreamLeakTest.java
@@ -21,18 +21,21 @@
  * questions.
  */
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.lang.ref.Reference;
 import java.net.URLConnection;
 import java.net.UnknownServiceException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -50,298 +53,128 @@ class FileURLConnStreamLeakTest {
     private static final String FILE_URLCONNECTION_CLASSNAME = "sun.net.www.protocol.file.FileURLConnection";
 
     private Path testFile;
+    // FileInputStream has a Cleaner which closes its underlying file descriptor.
+    // Here we keep reference to the URLConnection for the duration of each test method.
+    // This ensures that any FileInputStream this URLConnection may be retaining, won't be GCed
+    // until after the test method has checked for file descriptor leaks.
+    private URLConnection conn;
 
     @BeforeEach
     void beforeEach() throws Exception {
         final Path file = Files.createTempFile(Path.of("."), "8367561-", ".txt");
         Files.writeString(file, String.valueOf(System.currentTimeMillis()));
         this.testFile = file;
+        this.conn = this.testFile.toUri().toURL().openConnection();
+        assertNotNull(this.conn, "URLConnection for " + this.testFile + " is null");
+        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
+                "unexpected URLConnection type");
     }
 
     @AfterEach
     void afterEach() throws Exception {
+        this.conn = null;
         Files.deleteIfExists(this.testFile);
     }
 
-
-    @Test
-    void testGetContentEncoding() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getContentEncoding();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
+    static List<Consumer<URLConnection>> urlConnOperations() {
+        return List.of(
+                URLConnection::getContentEncoding,
+                URLConnection::getContentLength,
+                URLConnection::getContentLengthLong,
+                URLConnection::getContentType,
+                URLConnection::getDate,
+                URLConnection::getExpiration,
+                URLConnection::getLastModified
+        );
     }
 
-    @Test
-    void testGetContentLength() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getContentLength();
+    @MethodSource("urlConnOperations")
+    @ParameterizedTest
+    void testURLConnOps(final Consumer<URLConnection> connConsumer) throws IOException {
+        connConsumer.accept(this.conn);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
-    }
-
-    @Test
-    void testGetContentLengthLong() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getContentLengthLong();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
-    }
-
-    @Test
-    void testGetContentType() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getContentType();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
-    }
-
-    @Test
-    void testGetDate() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getDate();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
-    }
-
-    @Test
-    void testGetExpiration() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getExpiration();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderField() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getHeaderField(0);
+        final var _ = this.conn.getHeaderField(0);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFieldString() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final String val = conn.getHeaderField("foo");
+        final String val = this.conn.getHeaderField("foo");
         assertNull(val, "unexpected header field value: " + val);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFieldDate() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getHeaderFieldDate("bar", 42);
+        final var _ = this.conn.getHeaderFieldDate("bar", 42);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFieldInt() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final int val = conn.getHeaderFieldInt("hello", 42);
+        final int val = this.conn.getHeaderFieldInt("hello", 42);
         assertEquals(42, val, "unexpected header value");
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFieldKey() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final String val = conn.getHeaderFieldKey(42);
+        final String val = this.conn.getHeaderFieldKey(42);
         assertNull(val, "unexpected header value: " + val);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFieldLong() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final long val = conn.getHeaderFieldLong("foo", 42);
+        final long val = this.conn.getHeaderFieldLong("foo", 42);
         assertEquals(42, val, "unexpected header value");
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetHeaderFields() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final Map<String, List<String>> headers = conn.getHeaderFields();
+        final Map<String, List<String>> headers = this.conn.getHeaderFields();
         assertNotNull(headers, "null headers");
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
-    }
-
-    @Test
-    void testGetLastModified() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        final var _ = conn.getLastModified();
-        // verify that the URLConnection isn't holding on to any file descriptors
-        // of this test file.
-        Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetInputStream() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
-        try (final InputStream is = conn.getInputStream()) {
+        try (final InputStream is = this.conn.getInputStream()) {
             assertNotNull(is, "input stream is null");
         }
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 
     @Test
     void testGetOutputStream() throws Exception {
-        final URLConnection conn = this.testFile.toUri().toURL().openConnection();
-        assertNotNull(conn, "URLConnection for " + this.testFile + " is null");
-        assertEquals(FILE_URLCONNECTION_CLASSNAME, conn.getClass().getName(),
-                "unexpected URLConnection type");
         // FileURLConnection only supports reading
-        assertThrows(UnknownServiceException.class, conn::getOutputStream);
+        assertThrows(UnknownServiceException.class, this.conn::getOutputStream);
         // verify that the URLConnection isn't holding on to any file descriptors
         // of this test file.
         Files.delete(this.testFile); // must not fail
-        // FileInputStream has a Cleaner which closes its underlying file descriptor.
-        // Here we add a reachability fence to URLConnection. This ensures that any FileInputStream
-        // it may be retaining, won't be GCed until after we have checked for file descriptor
-        // leaks on the previous line.
-        Reference.reachabilityFence(conn);
     }
 }
 
