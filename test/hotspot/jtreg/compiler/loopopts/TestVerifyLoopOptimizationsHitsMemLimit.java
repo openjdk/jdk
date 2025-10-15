f9d737f1830cd3fc6ae8b1d1e9ec18abb78ffd59
Benoît Maillard <benoit.maillard@oracle.com>
2025-10-15 10:39:50 +0200

    Reorder flags

--- test/hotspot/jtreg/compiler/loopopts/TestVerifyLoopOptimizationsHitsMemLimit.java
+++ test/hotspot/jtreg/compiler/loopopts/TestVerifyLoopOptimizationsHitsMemLimit.java
@@ -35,8 +35,8 @@ package compiler.loopopts;
  * @run main/othervm -XX:+IgnoreUnrecognizedVMOptions -XX:+UnlockDiagnosticVMOptions
  *      -XX:CompileCommand=compileonly,compiler.loopopts.TestVerifyLoopOptimizationsHitsMemLimit::test
  *      -XX:CompileCommand=memlimit,compiler.loopopts.TestVerifyLoopOptimizationsHitsMemLimit::test,100M~crash
- *      -XX:-TieredCompilation -Xcomp
- *      -XX:+StressLoopPeeling -XX:PerMethodTrapLimit=0 -XX:+VerifyLoopOptimizations
+ *      -XX:-TieredCompilation -Xcomp -XX:PerMethodTrapLimit=0
+ *      -XX:+StressLoopPeeling -XX:+VerifyLoopOptimizations
  *      -XX:StressSeed=1870557292
  *      compiler.loopopts.TestVerifyLoopOptimizationsHitsMemLimit
  * @run main compiler.loopopts.TestVerifyLoopOptimizationsHitsMemLimit
