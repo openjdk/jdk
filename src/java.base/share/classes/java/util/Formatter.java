b3c6a01a7f971101af309f6535b57f6dc3abad30
Raffaello Giulietti <raffaello.giulietti@oracle.com>
2025-10-15 19:59:34 +0200

    8341914: The j.u.Formatter doc for TemporalAccessor should be enhanced

--- src/java.base/share/classes/java/util/Formatter.java
+++ src/java.base/share/classes/java/util/Formatter.java
@@ -438,6 +438,8 @@ import sun.util.locale.provider.ResourceBundleBasedAdapter;
  *     {@code long}, {@link Long}, and {@link Date} the time zone used is
  *     the {@linkplain TimeZone#getDefault() default time zone} for this
  *     instance of the Java virtual machine.
+ *     Not all subtypes of {@link TemporalAccessor} carry a time zone, though,
+ *     so using this conversion might throw an {@link IllegalFormatConversionException}.
  *
  * <tr><th scope="row" style="vertical-align:top">{@code 'Z'}
  *     <td> A string representing the abbreviation for the time zone.  This
@@ -446,6 +448,8 @@ import sun.util.locale.provider.ResourceBundleBasedAdapter;
  *     the {@linkplain TimeZone#getDefault() default time zone} for this
  *     instance of the Java virtual machine.  The Formatter's locale will
  *     supersede the locale of the argument (if any).
+ *     Not all subtypes of {@link TemporalAccessor} carry a time zone, though,
+ *     so using this conversion might throw an {@link IllegalFormatConversionException}.
  *
  * <tr><th scope="row" style="vertical-align:top">{@code 's'}
  *     <td> Seconds since the beginning of the epoch starting at 1 January 1970
@@ -1726,6 +1730,8 @@ import sun.util.locale.provider.ResourceBundleBasedAdapter;
  *     {@code long}, {@link Long}, and {@link Date} the time zone used is
  *     the {@linkplain TimeZone#getDefault() default time zone} for this
  *     instance of the Java virtual machine.
+ *     Not all subtypes of {@link TemporalAccessor} carry a time zone, though,
+ *     so using this conversion might throw an {@link IllegalFormatConversionException}.
  *
  * <tr><th scope="row" style="vertical-align:top">{@code 'Z'}
  *     <td style="vertical-align:top"> <code>'&#92;u005a'</code>
@@ -1735,6 +1741,8 @@ import sun.util.locale.provider.ResourceBundleBasedAdapter;
  *     the {@linkplain TimeZone#getDefault() default time zone} for this
  *     instance of the Java virtual machine.  The Formatter's locale will
  *     supersede the locale of the argument (if any).
+ *     Not all subtypes of {@link TemporalAccessor} carry a time zone, though,
+ *     so using this conversion might throw an {@link IllegalFormatConversionException}.
  *
  * <tr><th scope="row" style="vertical-align:top">{@code 's'}
  *     <td style="vertical-align:top"> <code>'&#92;u0073'</code>
