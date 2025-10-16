1363424e97335b8b7eb46b94a7f01f4e91d8f5de
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-10-16 22:49:17 +0000

    check jcmd exist

--- test/hotspot/jtreg/serviceability/EarlyDynamicLoad/libEarlyDynamicLoad.c
+++ test/hotspot/jtreg/serviceability/EarlyDynamicLoad/libEarlyDynamicLoad.c
@@ -36,8 +36,16 @@
 #endif // WINDOWS
 
 static void JNICALL VMStartJcmd(jvmtiEnv* jvmti, JNIEnv* env) {
+    const char* jcmd_path = getenv("JCMD_PATH");
+    FILE* file = fopen(jcmd_path, "r");
+    if (file == NULL) {
+        printf("%s does not exist", jcmd_path);
+        return;
+    }
+    fclose(file);
+
     char cmd[256];
-    snprintf(cmd, sizeof(cmd), "%s %d JVMTI.agent_load some.jar", getenv("JCMD_PATH"), PID());
+    snprintf(cmd, sizeof(cmd), "%s %d JVMTI.agent_load some.jar", jcmd_path, PID());
     printf("Running jcmd command: '%s'\n", cmd);
 
     int res = system(cmd);
