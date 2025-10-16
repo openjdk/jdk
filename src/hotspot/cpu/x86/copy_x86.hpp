333807896d13ffd7f99cb1ef27c613b314efe683
Antón Seoane Ampudia <anton.seoane.ampudia@oracle.com>
2025-10-16 12:57:05 +0200

    Fix build issues

--- src/hotspot/cpu/x86/copy_x86.hpp
+++ src/hotspot/cpu/x86/copy_x86.hpp
@@ -52,48 +52,7 @@ static void pd_zero_to_bytes(void* to, size_t count) {
 }
 
 static void pd_conjoint_words(const HeapWord* from, HeapWord* to, size_t count) {
-#ifdef _WINDOWS
   (void)memmove(to, from, count * HeapWordSize);
-#else
-  // Includes a zero-count check.
-  intx temp = 0;
-  __asm__ volatile("        testl   %6,%6         ;"
-                   "        jz      7f            ;"
-                   "        cmpl    %4,%5         ;"
-                   "        leal    -4(%4,%6,4),%3;"
-                   "        jbe     1f            ;"
-                   "        cmpl    %7,%5         ;"
-                   "        jbe     4f            ;"
-                   "1:      cmpl    $32,%6        ;"
-                   "        ja      3f            ;"
-                   "        subl    %4,%1         ;"
-                   "2:      movl    (%4),%3       ;"
-                   "        movl    %7,(%5,%4,1)  ;"
-                   "        addl    $4,%0         ;"
-                   "        subl    $1,%2          ;"
-                   "        jnz     2b            ;"
-                   "        jmp     7f            ;"
-                   "3:      rep;    smovl         ;"
-                   "        jmp     7f            ;"
-                   "4:      cmpl    $32,%2        ;"
-                   "        movl    %7,%0         ;"
-                   "        leal    -4(%5,%6,4),%1;"
-                   "        ja      6f            ;"
-                   "        subl    %4,%1         ;"
-                   "5:      movl    (%4),%3       ;"
-                   "        movl    %7,(%5,%4,1)  ;"
-                   "        subl    $4,%0         ;"
-                   "        subl    $1,%2          ;"
-                   "        jnz     5b            ;"
-                   "        jmp     7f            ;"
-                   "6:      std                   ;"
-                   "        rep;    smovl         ;"
-                   "        cld                   ;"
-                   "7:      nop                    "
-                   : "=S" (from), "=D" (to), "=c" (count), "=r" (temp)
-                   : "0"  (from), "1"  (to), "2"  (count), "3"  (temp)
-                   : "memory", "flags");
-#endif _WINDOWS
 }
 
 static void pd_disjoint_words(const HeapWord* from, HeapWord* to, size_t count) {
@@ -126,82 +85,7 @@ static void pd_aligned_disjoint_words(const HeapWord* from, HeapWord* to, size_t
 }
 
 static void pd_conjoint_bytes(const void* from, void* to, size_t count) {
-#if defined _WINDOWS
   (void)memmove(to, from, count);
-#else
-  // Includes a zero-count check.
-  intx temp = 0;
-  __asm__ volatile("        testl   %6,%6          ;"
-                   "        jz      13f            ;"
-                   "        cmpl    %4,%5          ;"
-                   "        leal    -1(%4,%6),%3   ;"
-                   "        jbe     1f             ;"
-                   "        cmpl    %7,%5          ;"
-                   "        jbe     8f             ;"
-                   "1:      cmpl    $3,%6          ;"
-                   "        jbe     6f             ;"
-                   "        movl    %6,%3          ;"
-                   "        movl    $4,%2          ;"
-                   "        subl    %4,%2          ;"
-                   "        andl    $3,%2          ;"
-                   "        jz      2f             ;"
-                   "        subl    %6,%3          ;"
-                   "        rep;    smovb          ;"
-                   "2:      movl    %7,%2          ;"
-                   "        shrl    $2,%2          ;"
-                   "        jz      5f             ;"
-                   "        cmpl    $32,%2         ;"
-                   "        ja      4f             ;"
-                   "        subl    %4,%1          ;"
-                   "3:      movl    (%4),%%edx     ;"
-                   "        movl    %%edx,(%5,%4,1);"
-                   "        addl    $4,%0          ;"
-                   "        subl    $1,%2           ;"
-                   "        jnz     3b             ;"
-                   "        addl    %4,%1          ;"
-                   "        jmp     5f             ;"
-                   "4:      rep;    smovl          ;"
-                   "5:      movl    %7,%2          ;"
-                   "        andl    $3,%2          ;"
-                   "        jz      13f            ;"
-                   "6:      xorl    %7,%3          ;"
-                   "7:      movb    (%4,%7,1),%%dl ;"
-                   "        movb    %%dl,(%5,%7,1) ;"
-                   "        addl    $1,%3          ;"
-                   "        subl    $1,%2           ;"
-                   "        jnz     7b             ;"
-                   "        jmp     13f            ;"
-                   "8:      std                    ;"
-                   "        cmpl    $12,%2         ;"
-                   "        ja      9f             ;"
-                   "        movl    %7,%0          ;"
-                   "        leal    -1(%6,%5),%1   ;"
-                   "        jmp     11f            ;"
-                   "9:      xchgl   %3,%2          ;"
-                   "        movl    %6,%0          ;"
-                   "        addl    $1,%2          ;"
-                   "        leal    -1(%7,%5),%1   ;"
-                   "        andl    $3,%2          ;"
-                   "        jz      10f            ;"
-                   "        subl    %6,%3          ;"
-                   "        rep;    smovb          ;"
-                   "10:     movl    %7,%2          ;"
-                   "        subl    $3,%0          ;"
-                   "        shrl    $2,%2          ;"
-                   "        subl    $3,%1          ;"
-                   "        rep;    smovl          ;"
-                   "        andl    $3,%3          ;"
-                   "        jz      12f            ;"
-                   "        movl    %7,%2          ;"
-                   "        addl    $3,%0          ;"
-                   "        addl    $3,%1          ;"
-                   "11:     rep;    smovb          ;"
-                   "12:     cld                    ;"
-                   "13:     nop                    ;"
-                   : "=S" (from), "=D" (to), "=c" (count), "=r" (temp)
-                   : "0"  (from), "1"  (to), "2"  (count), "3"  (temp)
-                   : "memory", "flags", "%edx");
-#endif // _WINDOWS
 }
 
 static void pd_conjoint_bytes_atomic(const void* from, void* to, size_t count) {
