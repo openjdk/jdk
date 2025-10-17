4d2e42aa5e2627c47efc7a94faa741e08a73303a
Seán Coffey <sean.coffey@oracle.com>
2025-10-17 09:59:44 +0000

    man page and better help messages

--- src/hotspot/share/services/diagnosticCommand.hpp
+++ src/hotspot/share/services/diagnosticCommand.hpp
@@ -356,7 +356,9 @@ public:
   ThreadDumpDCmd(outputStream* output, bool heap);
   static const char* name() { return "Thread.print"; }
   static const char* description() {
-    return "Print all platform threads with stacktraces.";
+    return "Print all platform threads with stacktraces. "
+           "Use Thread.dump_to_file command for extended threads "
+            "detail including virtual thread information.";
   }
   static const char* impact() {
     return "Medium: Depends on the number of threads.";
@@ -768,7 +770,8 @@ public:
     return "Thread.dump_to_file";
   }
   static const char *description() {
-    return "Dump threads, with stack traces, to a file in plain text or JSON format.";
+    return "Dump threads (including virtual), with stack traces, "
+           "to a file in plain text or JSON format.";
   }
   static const char* impact() {
     return "Medium: Depends on the number of threads.";
