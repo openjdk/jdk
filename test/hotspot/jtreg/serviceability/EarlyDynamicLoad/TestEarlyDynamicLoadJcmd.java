8bbedbcae0cdab7f93e3552d912110513e866a90
Francesco Andreuzzi <andreuzzi.francesco@gmail.com>
2025-10-17 10:09:07 +0000

    backslash

--- test/hotspot/jtreg/serviceability/EarlyDynamicLoad/TestEarlyDynamicLoadJcmd.java
+++ test/hotspot/jtreg/serviceability/EarlyDynamicLoad/TestEarlyDynamicLoadJcmd.java
@@ -43,7 +43,7 @@ public class TestEarlyDynamicLoadJcmd {
                 "-agentpath:" + Utils.TEST_NATIVE_PATH + File.separator + System.mapLibraryName("EarlyDynamicLoad"),
                 "-version");
         String jcmdPath = JDKToolFinder.getJDKTool("jcmd");
-        pb.environment().put("JCMD_PATH", jcmdPath);
+        pb.environment().put("JCMD_PATH", jcmdPath.replace("\\", "\\\\"));
 
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
         output.shouldHaveExitValue(0);
