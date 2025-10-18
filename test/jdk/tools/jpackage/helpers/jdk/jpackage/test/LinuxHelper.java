fb8123f15cc38097b735338106f16d5733b41423
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2025-10-17 22:59:10 -0400

    Fix bad merge

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/LinuxHelper.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/LinuxHelper.java
@@ -23,6 +23,9 @@
 package jdk.jpackage.test;
 
 import static java.util.Collections.unmodifiableSortedSet;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
