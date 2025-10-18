c292b2e58aad52b5254c0288b50a92951623ea12
Oli <omattmann@ethz.ch>
2025-10-18 21:03:29 +0200

    escape analysis hook

--- src/hotspot/share/opto/escape.cpp
+++ src/hotspot/share/opto/escape.cpp
@@ -118,6 +118,7 @@ void ConnectionGraph::do_analysis(Compile *C, PhaseIterGVN *igvn) {
   if (congraph->compute_escape()) {
     // There are non escaping objects.
     C->set_congraph(congraph);
+    C->record_optimization_event(OptEvent_EscapeAnalysis);
   }
   // Cleanup.
   if (oop_null->outcnt() == 0) {
