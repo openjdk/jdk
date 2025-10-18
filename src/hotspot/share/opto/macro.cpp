a359e36b7effd70c3decd17d6e97c1f797036535
Oli <omattmann@ethz.ch>
2025-10-18 21:01:10 +0200

    eliminate autobox hooks

--- src/hotspot/share/opto/macro.cpp
+++ src/hotspot/share/opto/macro.cpp
@@ -2423,6 +2423,9 @@ void PhaseMacroExpand::eliminate_macro_nodes() {
         break;
       case Node::Class_CallStaticJava:
         success = eliminate_boxing_node(n->as_CallStaticJava());
+        if (success) {
+          C->record_optimization_event(OptEvent_EliminateAutobox);
+        }
         break;
       case Node::Class_Lock:
       case Node::Class_Unlock:
