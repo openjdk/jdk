d192c098e2f14390da176b46e9d28668e8fe7fb2
Oli <omattmann@ethz.ch>
2025-10-18 21:00:37 +0200

    add canonicalization hook

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -2170,6 +2170,7 @@ Node *PhaseIterGVN::transform_old(Node* n) {
     Node* con = makecon(t);     // Make a constant
     add_users_to_worklist(k);
     subsume_node(k, con);       // Everybody using k now uses con
+    C->record_optimization_event(OptEvent_Canonicalization);
     return con;
   }
 
@@ -2179,6 +2180,7 @@ Node *PhaseIterGVN::transform_old(Node* n) {
     NOT_PRODUCT(set_progress();)
     add_users_to_worklist(k);
     subsume_node(k, i);       // Everybody using k now uses i
+    C->record_optimization_event(OptEvent_Canonicalization);
     return i;
   }
 
@@ -2189,10 +2191,12 @@ Node *PhaseIterGVN::transform_old(Node* n) {
     NOT_PRODUCT(set_progress();)
     add_users_to_worklist(k);
     subsume_node(k, i);       // Everybody using k now uses i
+    C->record_optimization_event(OptEvent_Canonicalization);
     return i;
   }
 
   // Return Idealized original
+  C->record_optimization_event(OptEvent_Canonicalization);
   return k;
 }
 
