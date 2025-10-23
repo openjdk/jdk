2ea05fb847d38061acd6d946aceb4355e36cdd94
Chad Rakoczy <chadrako@amazon.com>
2025-10-23 22:41:06 +0000

    Change CodeCacheExpansionSize back to size_t

--- src/hotspot/share/runtime/globals.hpp
+++ src/hotspot/share/runtime/globals.hpp
@@ -1546,7 +1546,7 @@ const int ObjectAlignmentInBytes = 8;
           "Size of code heap with predicted hot methods (in bytes)")        \
           constraint(VMPageSizeConstraintFunc, AtParse)                     \
                                                                             \
-  product_pd(uintx, CodeCacheExpansionSize,                                 \
+  product_pd(size_t, CodeCacheExpansionSize,                                \
           "Code cache expansion size (in bytes)")                           \
           range(32*K, SIZE_MAX)                                             \
                                                                             \
