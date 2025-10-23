f46dbcd2de3ab7d27c691f4097a60d2f2929d9a7
Brian Burkhalter <brian.burkhalter@oracle.com>
2025-10-23 12:37:01 -0700

    8370387: Revert changes to Piped*

--- src/java.base/share/classes/java/io/PipedInputStream.java
+++ src/java.base/share/classes/java/io/PipedInputStream.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1995, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1995, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -279,8 +279,7 @@ public class PipedInputStream extends InputStream {
             try {
                 wait(1000);
             } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-                throw new IOException(ex);
+                throw new java.io.InterruptedIOException();
             }
         }
     }
@@ -333,8 +332,7 @@ public class PipedInputStream extends InputStream {
             try {
                 wait(1000);
             } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-                throw new IOException(ex);
+                throw new java.io.InterruptedIOException();
             }
         }
         int ret = buffer[out++] & 0xFF;
