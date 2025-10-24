e6fc40589a7eb09df1ba15b507ff8176dd80da1f
Serguei Spitsyn <serguei.spitsyn@oracle.com>
2025-10-24 02:10:30 -0700

    review: call to invalidate_jvmti_stack for pure continuations only

--- src/hotspot/share/runtime/continuationFreezeThaw.cpp
+++ src/hotspot/share/runtime/continuationFreezeThaw.cpp
@@ -1626,11 +1626,14 @@ static void invalidate_jvmti_stack(JavaThread* thread) {
 }
 
 static void jvmti_yield_cleanup(JavaThread* thread, ContinuationWrapper& cont) {
-  if (!cont.entry()->is_virtual_thread() && JvmtiExport::has_frame_pops(thread)) {
-    int num_frames = num_java_frames(cont);
+  if (!cont.entry()->is_virtual_thread()) {
+    if (JvmtiExport::has_frame_pops(thread)) {
+      int num_frames = num_java_frames(cont);
 
-    ContinuationWrapper::SafepointOp so(Thread::current(), cont);
-    JvmtiExport::continuation_yield_cleanup(JavaThread::current(), num_frames);
+      ContinuationWrapper::SafepointOp so(Thread::current(), cont);
+      JvmtiExport::continuation_yield_cleanup(thread, num_frames);
+    }
+    invalidate_jvmti_stack(thread);
   }
 }
 
@@ -1685,7 +1688,6 @@ static inline freeze_result freeze_epilog(ContinuationWrapper& cont) {
 
   log_develop_debug(continuations)("=== End of freeze cont ### #" INTPTR_FORMAT, cont.hash());
 
-  JVMTI_ONLY(invalidate_jvmti_stack(JavaThread::current()));
   return freeze_ok;
 }
 
@@ -2311,7 +2313,7 @@ NOINLINE intptr_t* Thaw<ConfigT>::thaw_slow(stackChunkOop chunk, Continuation::t
 
   assert(_cont.chunk_invariant(), "");
 
-  JVMTI_ONLY(invalidate_jvmti_stack(_thread));
+  JVMTI_ONLY(if (!_cont.entry()->is_virtual_thread()) invalidate_jvmti_stack(_thread));
 
   _thread->set_cont_fastpath(_fastpath);
 
