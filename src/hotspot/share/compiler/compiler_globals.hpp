94c5d14e5f9c603c7d99668d00fd7a6f8c03556f
Igor Veresov <iveresov@openjdk.org>
2025-10-27 15:09:59 +0000

    8368321: Rethink compilation delay strategy for lukewarm methods
    Reviewed-by: kvn, vlivanov

--- src/hotspot/share/compiler/compiler_globals.hpp
+++ src/hotspot/share/compiler/compiler_globals.hpp
@@ -271,13 +271,6 @@
           "Maximum rate sampling interval (in milliseconds)")               \
           range(1, max_intx)                                                \
                                                                             \
-  product(double, Tier0ProfileDelayFactor, 100.0, DIAGNOSTIC,               \
-          "Delay profiling/compiling of methods that were "                 \
-          "observed to be lukewarm")                                        \
-                                                                            \
-  product(double, Tier2ProfileDelayFactor, 250.0, DIAGNOSTIC,               \
-          "Delay profiling of methods that were observed to be lukewarm")   \
-                                                                            \
   product(bool, SkipTier2IfPossible, false, DIAGNOSTIC,                     \
           "Compile at tier 4 instead of tier 2 in training replay "         \
           "mode if posssible")                                              \
