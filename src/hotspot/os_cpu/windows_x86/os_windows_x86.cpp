168006d8d699aa0d3784f679b49c6215a3adc6fb
Erik Österlund <eosterlund@openjdk.org>
2025-10-27 08:47:02 +0000

    Comment

--- src/hotspot/os_cpu/windows_x86/os_windows_x86.cpp
+++ src/hotspot/os_cpu/windows_x86/os_windows_x86.cpp
@@ -341,6 +341,12 @@ intptr_t* os::fetch_bcp_from_context(const void* ucVoid) {
 
 // Returns the current stack pointer. Accurate value needed for
 // os::verify_stack_alignment().
+// The function is intentionally not inlined. This way, the transfer of control
+// into this method must be made with a call instruction. The MSVC
+// _AddressOfReturnAddress() intrinsic returns the address of the return PC
+// saved by that call instruction. Therefore, the stack pointer of the caller
+// just before the call instruction, is acquired by skipping over the return PC
+// slot in the stack.
 __declspec(noinline)
 address os::current_stack_pointer() {
   return ((address)_AddressOfReturnAddress()) + sizeof(void*);
