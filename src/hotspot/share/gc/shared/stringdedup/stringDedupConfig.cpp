ceeb719b160a6c5b7cd927b89a2e109b78a8039b
Erik Österlund <eosterlund@openjdk.org>
2025-10-27 15:02:49 +0000

    Remove duplicate string dedup disabling when dumping

--- src/hotspot/share/gc/shared/stringdedup/stringDedupConfig.cpp
+++ src/hotspot/share/gc/shared/stringdedup/stringDedupConfig.cpp
@@ -126,12 +126,6 @@ bool StringDedup::Config::ergo_initialize() {
                              "not supported by selected GC");
     FLAG_SET_ERGO(UseStringDeduplication, false);
     return true;
-  } else if (CDSConfig::is_dumping_heap()) {
-    // String deduplication can mess with the string interning logic during
-    // heap dumping.
-    log_warning(stringdedup)("String Deduplication disabled: "
-                             "not supported when heap dumping");
-    FLAG_SET_ERGO(UseStringDeduplication, false);
   }
 
   // UseStringDeduplication is enabled.  Check parameters.  These checks are
