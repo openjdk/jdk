ca92f16e80e2c3803bb22e08c00b0672d4c10fee
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-10-27 10:49:05 +0100

    Automate test

--- test/jdk/javax/swing/text/GlyphView/TestGlyphBGHeight.java
+++ test/jdk/javax/swing/text/GlyphView/TestGlyphBGHeight.java
@@ -24,56 +24,82 @@
 /*
  * @test
  * @bug 8017266
+ * @key headful
  * @summary Verifies if Background is painted taller than needed for styled text.
- * @library /java/awt/regtesthelpers
- * @build PassFailJFrame
- * @run main/manual TestGlyphBGHeight
+ * @run main TestGlyphBGHeight
  */
 
+import java.io.File;
+import java.awt.Graphics2D;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Robot;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
+import javax.swing.SwingUtilities;
 
 public class TestGlyphBGHeight {
 
-    final static String INSTRUCTIONS = """
-        A StyledDocument with a text and yellow background will be shown.
-        If the yellow background is way taller than the text,
-        press FAIL else press PASS.""";
+    static JFrame frame;
 
     public static void main(String[] args) throws Exception {
-        PassFailJFrame.builder()
-                .title("TestGlyphBGHeight Test Instructions")
-                .instructions(INSTRUCTIONS)
-                .columns(30)
-                .testUI(TestGlyphBGHeight::createUI)
-                .build()
-                .awaitAndCheck();
-    }
-
-    private static JFrame createUI() {
-        JFrame frame = new JFrame("TestGlyphBGHeight");
-        frame.setSize(100, 100);
-        frame.getContentPane().setLayout(new BorderLayout());
-
-        final JTextPane comp = new JTextPane();
-        final StyledDocument doc = comp.getStyledDocument();
-
-        Style style = comp.addStyle("superscript", null);
-        StyleConstants.setSuperscript(style, true);
-        StyleConstants.setFontSize(style, 32);
-        StyleConstants.setBackground(style, Color.YELLOW);
         try {
-            doc.insertString(doc.getLength(), "hello", style);
-        } catch(Exception e) {}
+            Robot robot = new Robot();
+            SwingUtilities.invokeAndWait(() -> {
+                frame = new JFrame("TestGlyphBGHeight");
+                frame.setSize(100, 100);
+                frame.getContentPane().setLayout(new BorderLayout());
 
-        comp.setDocument(doc);
+                final JTextPane comp = new JTextPane();
+                final StyledDocument doc = comp.getStyledDocument();
 
-        frame.getContentPane().add(comp, BorderLayout.CENTER);
-        return frame;
+                Style style = comp.addStyle("superscript", null);
+                StyleConstants.setSuperscript(style, true);
+                StyleConstants.setFontSize(style, 32);
+                StyleConstants.setBackground(style, Color.YELLOW);
+                try {
+                    doc.insertString(doc.getLength(), "hello", style);
+                } catch (Exception e) {}
+
+                comp.setDocument(doc);
+                comp.setBackground(Color.RED);
+
+                frame.getContentPane().add(comp, BorderLayout.CENTER);
+
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            });
+            robot.waitForIdle();
+            robot.delay(1000);
+
+            BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = (Graphics2D) img.getGraphics();
+            frame.paint(g2d);
+            ImageIO.write(img, "png", new File("AppTest.png"));
+            g2d.dispose();
+
+            BufferedImage bimg = img.getSubimage(0, 80, 100, 1);
+            ImageIO.write(bimg, "png", new File("AppTest1.png"));
+            robot.waitForIdle();
+            robot.delay(1000);
+            for (int x = 10; x < 50; x++) {
+                Color col = new Color(bimg.getRGB(x, 0));
+                System.out.println(Integer.toHexString(bimg.getRGB(x, 0)));
+                if (col.equals(Color.YELLOW)) {
+                    throw new RuntimeException(" Background is painted taller than needed for styled text");
+                }
+            }
+        } finally {
+            SwingUtilities.invokeAndWait(() -> {
+                if (frame != null) {
+                    frame.dispose();
+                }
+            });
+        }
     }
 }
