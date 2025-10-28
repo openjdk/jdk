e132e73f71e94d6f941c7db04243d3bcd58682dd
Leonid Mesnik <leonid.mesnik@gmail.com>
2025-10-28 16:24:06 -0700

    8370851: Mark hotspot and jdk tests incompatible with test thread factory

--- test/jdk/com/sun/management/ThreadMXBean/VirtualThreads.java
+++ test/jdk/com/sun/management/ThreadMXBean/VirtualThreads.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,7 @@
  * @test id=default
  * @bug 8284161 8303242
  * @summary Test com.sun.management.ThreadMXBean with virtual threads
+ * @requires test.thread.factory != "Virtual"
  * @library /test/lib
  * @run junit/othervm VirtualThreads
  */
@@ -32,6 +33,7 @@
 /**
  * @test id=no-vmcontinuations
  * @requires vm.continuations
+ * @requires test.thread.factory != "Virtual"
  * @library /test/lib
  * @run junit/othervm -XX:+UnlockExperimentalVMOptions -XX:-VMContinuations VirtualThreads
  */
