6fad402094f3860bfc614dc8b962c02b17cfe3ca
Volkan Yazıcı <volkan.yazici@oracle.com>
2025-10-29 12:32:08 +0100

    Revert unnecessary changes to `AbstractThrowingSubscribers`

--- test/jdk/java/net/httpclient/AbstractThrowingSubscribers.java
+++ test/jdk/java/net/httpclient/AbstractThrowingSubscribers.java
@@ -679,8 +679,8 @@ public abstract class AbstractThrowingSubscribers implements HttpServerAdapters 
 
         @Override
         public boolean test(Throwable throwable) {
-            // `UncheckedIOException` is peeled off by `HttpClientImpl::translateSendAsyncExecFailure`
-            return throwable instanceof CustomIOException;
+            return UncheckedIOException.class.isInstance(throwable)
+                    && CustomIOException.class.isInstance(throwable.getCause());
         }
 
         @Override
