48b42140d32e7af7c5d66defddb6bdf451cfa7d6
Jaikiran Pai <jpai@openjdk.org>
2025-10-29 01:33:26 +0000

    8370775: ModulePatcher$JarResourceFinder.getByteBuffer() does not close the InputStream after reading the bytes
    Reviewed-by: alanb, fandreuzzi

--- src/java.base/share/classes/jdk/internal/module/ModulePatcher.java
+++ src/java.base/share/classes/jdk/internal/module/ModulePatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -467,8 +467,10 @@ public final class ModulePatcher {
                 }
                 @Override
                 public ByteBuffer getByteBuffer() throws IOException {
-                    byte[] bytes = getInputStream().readAllBytes();
-                    return ByteBuffer.wrap(bytes);
+                    try (InputStream in = getInputStream()) {
+                        byte[] bytes = in.readAllBytes();
+                        return ByteBuffer.wrap(bytes);
+                    }
                 }
                 @Override
                 public InputStream getInputStream() throws IOException {
