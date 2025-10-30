c8eeb2fd714b82a9449431d6d88374fa4c607081
Emanuel Peter <emanuel.peter@oracle.com>
2025-10-30 14:16:43 +0100

    x7

--- test/hotspot/jtreg/compiler/igvn/templated/TestJtreg.java
+++ test/hotspot/jtreg/compiler/igvn/templated/TestJtreg.java
@@ -28,8 +28,8 @@ public class TestJtreg {
         long arg5 = 2590086221109333669L;
         int arg6 = -16385;
 
-        boolean v0 = primitiveConTest_compiled(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
-        boolean v1 = primitiveConTest_reference(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
+        long v0 = primitiveConTest_compiled(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
+        long v1 = primitiveConTest_reference(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
         if (v0 != v1) {
             StringBuilder sb = new StringBuilder();
             sb.append("wrong value: " + v0 + " vs " + v1);
@@ -44,17 +44,15 @@ public class TestJtreg {
         }
     }
 
-    public static boolean primitiveConTest_compiled(int arg0, float arg1, float arg2, float arg3, long arg4, long arg5, int arg6) {
+    public static long primitiveConTest_compiled(int arg0, float arg1, float arg2, float arg3, long arg4, long arg5, int arg6) {
         arg5 = (long)Math.min(Math.max(arg5, 8796093022205L), -3297683376527800628L);
         arg5 = (long)((arg5 & 1541862882599486797L) | -4294967288L);
-        var val = (((!((arg0 == -16384)))?Float.min(arg1, (float)((char)40629)):(float)((true?(char)(Float.max(arg2, (-1.10794424E-7f * arg3))):Character.reverseBytes((char)((byte)((char)(arg4))))))) <= (float)(Long.compress(arg5, Integer.toUnsignedLong(arg6))));
-        return val;
+        return Long.compress(arg5, Integer.toUnsignedLong(arg6));
     }
 
-    public static boolean primitiveConTest_reference(int arg0, float arg1, float arg2, float arg3, long arg4, long arg5, int arg6) {
+    public static long primitiveConTest_reference(int arg0, float arg1, float arg2, float arg3, long arg4, long arg5, int arg6) {
         arg5 = (long)Math.min(Math.max(arg5, 8796093022205L), -3297683376527800628L);
         arg5 = (long)((arg5 & 1541862882599486797L) | -4294967288L);
-        var val = (((!((arg0 == -16384)))?Float.min(arg1, (float)((char)40629)):(float)((true?(char)(Float.max(arg2, (-1.10794424E-7f * arg3))):Character.reverseBytes((char)((byte)((char)(arg4))))))) <= (float)(Long.compress(arg5, Integer.toUnsignedLong(arg6))));
-        return val;
+        return Long.compress(arg5, Integer.toUnsignedLong(arg6));
     }
 }
