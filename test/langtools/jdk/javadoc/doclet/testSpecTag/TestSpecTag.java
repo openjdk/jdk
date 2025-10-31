5af884066453c22b12b93771baebbc12d3e68e2e
Hannes Wallnoefer <hannes.wallnoefer@oracle.com>
2025-10-31 11:28:21 +0100

    8370612: Simplify implementation of dark theme 8371021: Tab order in theme picker is broken

--- test/langtools/jdk/javadoc/doclet/testSpecTag/TestSpecTag.java
+++ test/langtools/jdk/javadoc/doclet/testSpecTag/TestSpecTag.java
@@ -126,10 +126,10 @@ public class TestSpecTag extends JavadocTester {
                     <li><a href="index-all.html">Index</a></li>
                     <li><a href="search.html">Search</a></li>
                     <li><a href="help-doc.html#external-specs">Help</a></li>
-                    <li><button id="theme-button" aria-label="Select Theme" title="Select Theme"><img src="re\
-                    source-files/sun.svg" alt="Select Theme" class="theme-light"><img src="resource-files/moo\
-                    n.svg" alt="Select Theme" class="theme-dark"></button></li>
-                    </ul>
+                    <li><button id="theme-button" aria-label="Select Theme" title="Select Theme"><im\
+                    g src="resource-files/sun.svg" alt="Select Theme" class="light-theme-icon"><img \
+                    src="resource-files/moon.svg" alt="Select Theme" class="dark-theme-icon"></butto\
+                    n></li>
                     """);
     }
 
