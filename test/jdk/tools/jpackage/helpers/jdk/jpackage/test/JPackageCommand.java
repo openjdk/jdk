7c900da1985c9508198ea7805e4955da4a7cac42
Alexey Semenyuk <asemenyuk@openjdk.org>
2025-11-02 02:19:11 +0000

    8371076: jpackage will wrongly overwrite the plist file in the embedded runtime when executed with the "--app-image" option
    Reviewed-by: almatvee

--- test/jdk/tools/jpackage/helpers/jdk/jpackage/test/JPackageCommand.java
+++ test/jdk/tools/jpackage/helpers/jdk/jpackage/test/JPackageCommand.java
@@ -376,7 +376,7 @@ public class JPackageCommand extends CommandArguments<JPackageCommand> {
         return cmd;
     }
 
-    public static Path createInputRuntimeImage() throws IOException {
+    public static Path createInputRuntimeImage() {
 
         final Path runtimeImageDir;
 
@@ -406,7 +406,7 @@ public class JPackageCommand extends CommandArguments<JPackageCommand> {
     }
 
     public JPackageCommand setDefaultAppName() {
-        return addArguments("--name", TKit.getCurrentDefaultAppName());
+        return setArgumentValue("--name", TKit.getCurrentDefaultAppName());
     }
 
     /**
