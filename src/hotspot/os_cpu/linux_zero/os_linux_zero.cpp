26c3f1183740698fb76719eb5b60b34dc172751d
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2025-11-05 20:40:15 +0800

    Revert changes in os_linux_zero.cpp

--- src/hotspot/os_cpu/linux_zero/os_linux_zero.cpp
+++ src/hotspot/os_cpu/linux_zero/os_linux_zero.cpp
@@ -51,10 +51,6 @@
 #include "utilities/events.hpp"
 #include "utilities/vmError.hpp"
 
-address currentStackPointer() {
-  return NOT_S390(static_cast<address>(__builtin_dwarf_cfa())) S390_ONLY(reinterpret_cast<address>(reinterpret_cast<uintptr_t>(__builtin_dwarf_cfa()) - 160));
-}
-
 frame os::get_sender_for_C_frame(frame* fr) {
   ShouldNotCallThis();
   return frame(nullptr, nullptr); // silence compile warning.
