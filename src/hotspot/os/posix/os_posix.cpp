93969d62182a09f9fcb164c0ddef03210ea57416
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2025-11-05 20:46:42 +0800

    Implement in os_posix.cpp

--- src/hotspot/os/posix/os_posix.cpp
+++ src/hotspot/os/posix/os_posix.cpp
@@ -1552,6 +1552,10 @@ void os::Posix::to_RTC_abstime(timespec* abstime, int64_t millis) {
              true  /* use real-time clock */);
 }
 
+address os::Posix::current_stack_pointer() {
+  return NOT_S390(static_cast<address>(__builtin_dwarf_cfa())) S390_ONLY(reinterpret_cast<address>(reinterpret_cast<uintptr_t>(__builtin_dwarf_cfa()) - 160));
+}
+
 // Common (partly) shared time functions
 
 jlong os::javaTimeMillis() {
