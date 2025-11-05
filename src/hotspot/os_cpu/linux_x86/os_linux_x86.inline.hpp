2284ebdd1edc7d428ab3858043fcd7af7b3422e0
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2025-11-05 20:41:58 +0800

    Remove from os_linux_x86.inline.hpp

--- src/hotspot/os_cpu/linux_x86/os_linux_x86.inline.hpp
+++ src/hotspot/os_cpu/linux_x86/os_linux_x86.inline.hpp
@@ -36,12 +36,4 @@ inline jlong os::rdtsc() {
   return (jlong)res;
 }
 
-inline address os::current_stack_pointer() {
-#if defined(__has_builtin) && __has_builtin(__builtin_stack_address)
-  return static_cast<address>(__builtin_stack_address());
-#else
-  return currentStackPointer();
-#endif
-}
-
 #endif // OS_CPU_LINUX_X86_OS_LINUX_X86_INLINE_HPP
