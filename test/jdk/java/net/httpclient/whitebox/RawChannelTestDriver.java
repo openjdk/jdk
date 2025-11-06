e7b8305b3892b82f370a3ff9f3b2b6f7f40f796d
Daniel Fuchs <daniel.fuchs@oracle.com>
2025-11-06 16:19:27 +0000

    8371366: java/net/httpclient/whitebox/RawChannelTestDriver.java fails intermittently in jtreg timeout

--- test/jdk/java/net/httpclient/whitebox/RawChannelTestDriver.java
+++ test/jdk/java/net/httpclient/whitebox/RawChannelTestDriver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,6 +25,6 @@
  * @test
  * @bug 8151299 8164704
  * @modules java.net.http/jdk.internal.net.http
- * @run testng java.net.http/jdk.internal.net.http.RawChannelTest
+ * @run testng/timeout=240 java.net.http/jdk.internal.net.http.RawChannelTest
  */
 //-Djdk.internal.httpclient.websocket.debug=true
