5fb0a8f5b84c9ee03df228c3b05bcc4e0a84a1d1
Benoît Maillard <bmaillard@openjdk.org>
2025-11-10 08:41:13 +0000

    8366990: C2: Compilation hits the memory limit when verifying loop opts in Split-If code
    Reviewed-by: chagedorn, dfenacci

--- src/hotspot/share/ci/ciInstanceKlass.cpp
+++ src/hotspot/share/ci/ciInstanceKlass.cpp
@@ -391,17 +391,21 @@ bool ciInstanceKlass::contains_field_offset(int offset) {
   return get_instanceKlass()->contains_field_offset(offset);
 }
 
+ciField* ciInstanceKlass::get_non_static_field_by_offset(const int field_offset) {
+  for (int i = 0, len = nof_nonstatic_fields(); i < len; i++) {
+    ciField* field = _nonstatic_fields->at(i);
+    int field_off = field->offset_in_bytes();
+    if (field_off == field_offset)
+      return field;
+  }
+  return nullptr;
+}
+
 // ------------------------------------------------------------------
 // ciInstanceKlass::get_field_by_offset
 ciField* ciInstanceKlass::get_field_by_offset(int field_offset, bool is_static) {
   if (!is_static) {
-    for (int i = 0, len = nof_nonstatic_fields(); i < len; i++) {
-      ciField* field = _nonstatic_fields->at(i);
-      int  field_off = field->offset_in_bytes();
-      if (field_off == field_offset)
-        return field;
-    }
-    return nullptr;
+    return get_non_static_field_by_offset(field_offset);
   }
   VM_ENTRY_MARK;
   InstanceKlass* k = get_instanceKlass();
@@ -427,6 +431,30 @@ ciField* ciInstanceKlass::get_field_by_name(ciSymbol* name, ciSymbol* signature,
   return field;
 }
 
+// This is essentially a shortcut for:
+//   get_field_by_offset(field_offset, is_static)->layout_type()
+// except this does not require allocating memory for a new ciField
+BasicType ciInstanceKlass::get_field_type_by_offset(const int field_offset, const bool is_static) {
+  if (!is_static) {
+    ciField* field = get_non_static_field_by_offset(field_offset);
+    return field != nullptr ? field->layout_type() : T_ILLEGAL;
+  }
+
+  // Avoid allocating a new ciField by obtaining the field type directly
+  VM_ENTRY_MARK;
+  InstanceKlass* k = get_instanceKlass();
+  fieldDescriptor fd;
+  if (!k->find_field_from_offset(field_offset, is_static, &fd)) {
+    return T_ILLEGAL;
+  }
+
+  // Reproduce the behavior of ciField::layout_type
+  BasicType field_type = fd.field_type();
+  if (is_reference_type(field_type)) {
+    return T_OBJECT;
+  }
+  return type2field[make(field_type)->basic_type()];
+}
 
 // ------------------------------------------------------------------
 // ciInstanceKlass::compute_nonstatic_fields
