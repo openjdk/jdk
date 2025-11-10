c8393cb48a82299d001cb0ffd56eb74796e5573d
Severin Gehwolf <sgehwolf@redhat.com>
2025-11-10 16:20:56 +0100

    Simplify os::used_memory()

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -2207,12 +2207,7 @@ static void assert_nonempty_range(const char* addr, size_t bytes) {
 bool os::used_memory(physical_memory_size_type& value) {
 #ifdef LINUX
   if (OSContainer::is_containerized()) {
-    physical_memory_size_type mem_usage = 0;
-    if (!OSContainer::memory_usage_in_bytes(mem_usage)) {
-      return false;
-    }
-    value = mem_usage;
-    return true;
+    return OSContainer::memory_usage_in_bytes(value);
   }
 #endif
   physical_memory_size_type avail_mem = 0;
