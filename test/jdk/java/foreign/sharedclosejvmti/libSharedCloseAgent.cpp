584179a3751662618860bb515a60c43fd9f4b5b7
Jorn Vernee <jvernee@openjdk.org>
2025-11-10 14:57:27 +0000

    8371474: Wrong object class or methodID passed to JNI call in TestSharedCloseJvmti with -Xshare:off
    Reviewed-by: alanb, mdoerr

--- test/jdk/java/foreign/sharedclosejvmti/libSharedCloseAgent.cpp
+++ test/jdk/java/foreign/sharedclosejvmti/libSharedCloseAgent.cpp
@@ -35,7 +35,7 @@ static const char* TARGET_METHOD_SIG = "()V";
 static const char* INTERCEPT_CLASS_NAME = "Ljdk/internal/foreign/MemorySessionImpl;";
 static const char* INTERCEPT_METHOD_NAME = "checkValidStateRaw";
 
-void start(jvmtiEnv *jvmti_env, JNIEnv* jni_env) {
+void start(jvmtiEnv*, JNIEnv* jni_env, jthread) {
 
   jclass cls = jni_env->FindClass(TARGET_CLASS_NAME);
   if (cls == nullptr) {
@@ -111,7 +111,7 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
   }
 
   jvmtiEventCallbacks callbacks;
-  callbacks.VMStart = start;
+  callbacks.VMInit = start;
   callbacks.MethodExit = method_exit;
 
   err = env->SetEventCallbacks(&callbacks, (jint) sizeof(callbacks));
@@ -124,7 +124,7 @@ Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
     return err;
   }
 
-  err = env->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_START, nullptr);
+  err = env->SetEventNotificationMode(JVMTI_ENABLE, JVMTI_EVENT_VM_INIT, nullptr);
   if (err != JVMTI_ERROR_NONE) {
     return err;
   }
