626fc490e93098fe9500ab641fbb49b2c31527b8
Afshin Zafari <Afshin.zafari@oracle.com>
2025-11-10 09:41:20 +0100

    order of includes corrected.

--- test/hotspot/gtest/nmt/test_nmt_buffer_overflow_detection.cpp
+++ test/hotspot/gtest/nmt/test_nmt_buffer_overflow_detection.cpp
@@ -27,10 +27,10 @@
 #include "nmt/memTracker.hpp"
 #include "runtime/os.hpp"
 #include "sanitizers/address.hpp"
-#include "testutils.hpp"
-#include "unittest.hpp"
 #include "utilities/debug.hpp"
 #include "utilities/ostream.hpp"
+#include "testutils.hpp"
+#include "unittest.hpp"
 
 #if !INCLUDE_ASAN
 
