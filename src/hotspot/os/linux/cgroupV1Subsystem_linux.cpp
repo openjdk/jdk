9e73674918254001047a9c796848225a1016f19f
Severin Gehwolf <sgehwolf@redhat.com>
2025-11-10 13:59:30 +0100

    Use better phrasing in trace logs

--- src/hotspot/os/linux/cgroupV1Subsystem_linux.cpp
+++ src/hotspot/os/linux/cgroupV1Subsystem_linux.cpp
@@ -165,7 +165,7 @@ bool CgroupV1MemoryController::read_memory_limit_in_bytes(physical_memory_size_t
     physical_memory_size_type hierlimit = 0;
     if (uses_mem_hierarchy() && read_hierarchical_memory_limit_val(hierlimit) &&
         hierlimit < upper_bound) {
-      log_trace(os, container)("Using hierarchical memory limit " PHYS_MEM_TYPE_FORMAT, hierlimit);
+      log_trace(os, container)("Memory Limit is:" PHYS_MEM_TYPE_FORMAT, hierlimit);
       result = hierlimit;
     } else {
       // Exceeding physical memory is treated as unlimited. This implementation
@@ -212,7 +212,7 @@ bool CgroupV1MemoryController::memory_and_swap_limit_in_bytes(physical_memory_si
     physical_memory_size_type hiermswlimit = 0;
     if (uses_mem_hierarchy() && read_hierarchical_mem_swap_val(hiermswlimit) &&
         hiermswlimit < total_mem_swap) {
-      log_trace(os, container)("Using hierarchical memory swap limit " PHYS_MEM_TYPE_FORMAT, hiermswlimit);
+      log_trace(os, container)("Memory and Swap Limit is: " PHYS_MEM_TYPE_FORMAT, hiermswlimit);
       memory_swap = hiermswlimit;
     } else {
       memory_swap = value_unlimited;
