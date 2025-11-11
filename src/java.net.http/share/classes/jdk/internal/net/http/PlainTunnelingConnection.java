e792a045a441156cc6566383b8e4d4060f2cca3f
EunHyunsu <ehs208@naver.com>
2025-11-11 20:28:17 +0900

    8328894: Read 407 response body in HTTPS CONNECT tunneling

--- src/java.net.http/share/classes/jdk/internal/net/http/PlainTunnelingConnection.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/PlainTunnelingConnection.java
@@ -36,13 +36,15 @@ import java.util.function.Function;
 
 import jdk.internal.net.http.common.FlowTube;
 import jdk.internal.net.http.common.MinimalFuture;
+
 import static java.net.http.HttpResponse.BodyHandlers.discarding;
+import static java.net.http.HttpResponse.BodyHandlers.ofByteArray;
 import static jdk.internal.net.http.common.Utils.ProxyHeaders;
 
 /**
  * A plain text socket tunnel through a proxy. Uses "CONNECT" but does not
- * encrypt. Used by WebSocket, as well as HTTP over SSL + Proxy.
- * Wrapped in SSLTunnelConnection or AsyncSSLTunnelConnection for encryption.
+ * encrypt. Used by WebSocket, as well as HTTP over SSL + Proxy. Wrapped in
+ * SSLTunnelConnection or AsyncSSLTunnelConnection for encryption.
  */
 final class PlainTunnelingConnection extends HttpConnection {
 
@@ -65,65 +67,84 @@ final class PlainTunnelingConnection extends HttpConnection {
 
     @Override
     public CompletableFuture<Void> connectAsync(Exchange<?> exchange) {
-        if (debug.on()) debug.log("Connecting plain connection");
+        if (debug.on()) {
+            debug.log("Connecting plain connection");
+        }
         return delegate.connectAsync(exchange)
-            .thenCompose(unused -> delegate.finishConnect())
-            .thenCompose((Void v) -> {
-                if (debug.on()) debug.log("sending HTTP/1.1 CONNECT");
-                HttpClientImpl client = client();
-                assert client != null;
-                HttpRequestImpl req = new HttpRequestImpl("CONNECT", address, proxyHeaders);
-                MultiExchange<Void> mulEx = new MultiExchange<>(null, req,
-                        client, discarding(), null);
-                Exchange<Void> connectExchange = mulEx.getExchange();
+                .thenCompose(unused -> delegate.finishConnect())
+                .thenCompose((Void v) -> {
+                    if (debug.on()) {
+                        debug.log("sending HTTP/1.1 CONNECT");
+                    }
+                    HttpClientImpl client = client();
+                    assert client != null;
+                    HttpRequestImpl req = new HttpRequestImpl(
+                            "CONNECT", address, proxyHeaders);
+                    MultiExchange<byte[]> mulEx = new MultiExchange<>(
+                            null, req, client, ofByteArray(), null);
+                    Exchange<byte[]> connectExchange = mulEx.getExchange();
 
-                return connectExchange
-                        .responseAsyncImpl(delegate)
-                        .thenCompose((Response resp) -> {
-                            CompletableFuture<Void> cf = new MinimalFuture<>();
-                            if (debug.on()) debug.log("got response: %d", resp.statusCode());
-                            if (resp.statusCode() == 407) {
-                                return connectExchange.ignoreBody().handle((r,t) -> {
-                                    // close delegate after reading body: we won't
-                                    // be reusing that connection anyway.
-                                    delegate.close();
-                                    ProxyAuthenticationRequired authenticationRequired =
-                                            new ProxyAuthenticationRequired(resp);
-                                    cf.completeExceptionally(authenticationRequired);
-                                    return cf;
-                                }).thenCompose(Function.identity());
-                            } else if (resp.statusCode() != 200) {
-                                delegate.close();
-                                cf.completeExceptionally(new IOException(
-                                        "Tunnel failed, got: "+ resp.statusCode()));
-                            } else {
-                                // get the initial/remaining bytes
-                                ByteBuffer b = ((Http1Exchange<?>)connectExchange.exchImpl).drainLeftOverBytes();
-                                int remaining = b.remaining();
-                                assert remaining == 0: "Unexpected remaining: " + remaining;
-                                cf.complete(null);
-                            }
-                            return cf;
-                        })
-                        .handle((result, ex) -> {
-                            if (ex == null) {
-                                return MinimalFuture.completedFuture(result);
-                            } else {
-                                if (debug.on())
-                                    debug.log("tunnel failed with \"%s\"", ex.toString());
-                                Throwable t = ex;
-                                if (t instanceof CompletionException)
-                                    t = t.getCause();
-                                if (t instanceof HttpTimeoutException) {
-                                    String msg = "proxy tunneling CONNECT request timed out";
-                                    t = new HttpTimeoutException(msg);
-                                    t.initCause(ex);
+                    return connectExchange
+                            .responseAsyncImpl(delegate)
+                            .thenCompose((Response resp) -> {
+                                CompletableFuture<Void> cf =
+                                        new MinimalFuture<>();
+                                if (debug.on()) {
+                                    debug.log("got response: %d",
+                                            resp.statusCode());
                                 }
-                                return MinimalFuture.<Void>failedFuture(t);
-                            }
-                        })
-                        .thenCompose(Function.identity());
-            });
+                                if (resp.statusCode() == 407) {
+                                    // Read the 407 body
+                                    return connectExchange.readBodyAsync(ofByteArray())
+                                            .handle((bodyBytes, t) -> {
+                                                // close delegate after reading body: we won't
+                                                // be reusing that connection anyway.
+                                                delegate.close();
+                                                ProxyAuthenticationRequired authenticationRequired =
+                                                        new ProxyAuthenticationRequired(resp, bodyBytes);
+                                                cf.completeExceptionally(authenticationRequired);
+                                                return cf;
+                                            }).thenCompose(Function.identity());
+                                } else if (resp.statusCode() != 200) {
+                                    delegate.close();
+                                    cf.completeExceptionally(new IOException(
+                                            "Tunnel failed, got: "
+                                                    + resp.statusCode()));
+                                } else {
+                                    // get the initial/remaining bytes
+                                    ByteBuffer b = ((Http1Exchange<?>) connectExchange.exchImpl)
+                                            .drainLeftOverBytes();
+                                    int remaining = b.remaining();
+                                    assert remaining == 0
+                                            : "Unexpected remaining: " + remaining;
+                                    cf.complete(null);
+                                }
+                                return cf;
+                            })
+                            .handle((result, ex) -> {
+                                if (ex == null) {
+                                    return MinimalFuture.completedFuture(
+                                            result);
+                                } else {
+                                    if (debug.on()) {
+                                        debug.log("tunnel failed with \"%s\"",
+                                                ex.toString());
+                                    }
+                                    Throwable t = ex;
+                                    if (t instanceof CompletionException) {
+                                        t = t.getCause();
+                                    }
+                                    if (t instanceof HttpTimeoutException) {
+                                        String msg =
+                                                "proxy tunneling CONNECT request timed out";
+                                        t = new HttpTimeoutException(msg);
+                                        t.initCause(ex);
+                                    }
+                                    return MinimalFuture.<Void>failedFuture(t);
+                                }
+                            })
+                            .thenCompose(Function.identity());
+                });
     }
 
     public CompletableFuture<Void> finishConnect() {
@@ -132,10 +153,14 @@ final class PlainTunnelingConnection extends HttpConnection {
     }
 
     @Override
-    boolean isTunnel() { return true; }
+    boolean isTunnel() {
+        return true;
+    }
 
     @Override
-    HttpPublisher publisher() { return delegate.publisher(); }
+    HttpPublisher publisher() {
+        return delegate.publisher();
+    }
 
     @Override
     boolean connected() {
