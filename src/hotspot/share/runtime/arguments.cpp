07c96057a2a77ee3e59c93cf47b102ebc0033464
Antón Seoane Ampudia <anton.seoane.ampudia@oracle.com>
2025-11-13 12:54:21 +0100

    Add deprecation notice to Xmaxjitcodesize

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -2463,6 +2463,9 @@ jint Arguments::parse_each_vm_init_arg(const JavaVMInitArgs* args, JVMFlagOrigin
       }
     } else if (match_option(option, "-Xmaxjitcodesize", &tail) ||
                match_option(option, "-XX:ReservedCodeCacheSize=", &tail)) {
+      if (match_option(option, "-Xmaxjitcodesize", &tail)) {
+        warning("Option -Xmaxjitcodesize was deprecated in JDK 26 and will likely be removed in a future release.");
+      }
       julong long_ReservedCodeCacheSize = 0;
 
       ArgsRange errcode = parse_memory_size(tail, &long_ReservedCodeCacheSize, 1);
