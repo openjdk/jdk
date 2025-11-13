bff7cb7408554232c13a57bba10b67a9fd19b811
Mat Carter <macarte@microsoft.com>
2025-11-13 09:48:50 -0800

    Adding test to validate using DiagnosticCommand MBean to invoke AOT.end_recording

---
+++ test/hotspot/jtreg/runtime/cds/appcds/aotCache/DiagnosticCommandMBeanTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) 2025, Microsoft, Inc. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+/*
+ * @test
+ * @summary Sanity test for DiagnosticCommand MBean ability to invoke AOT.end_recording
+ * @requires vm.cds.write.archived.java.heap
+ * @library /test/jdk/lib/testlibrary /test/lib
+ *          /test/hotspot/jtreg/runtime/cds/appcds/aotCache/test-classes
+ * @build DiagnosticCommandMBeanTest
+ * @run driver jdk.test.lib.helpers.ClassFileInstaller -jar app.jar DiagnosticCommandMBeanApp
+ * @run driver DiagnosticCommandMBeanTest
+ */
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import javax.management.InstanceNotFoundException;
+import javax.management.MBeanException;
+import javax.management.ReflectionException;
+import javax.management.MalformedObjectNameException;
+
+import jdk.test.lib.cds.CDSAppTester;
+import jdk.test.lib.helpers.ClassFileInstaller;
+import jdk.test.lib.process.OutputAnalyzer;
+
+public class DiagnosticCommandMBeanTest {
+    static final String appJar = ClassFileInstaller.getJarPath("app.jar");
+    static final String mainClass = "DiagnosticCommandMBeanApp";
+    public static void main(String[] args) throws Exception {
+        Tester tester = new Tester();
+        tester.runAOTWorkflow();
+    }
+
+    static class Tester extends CDSAppTester {
+        public Tester() {
+            super(mainClass);
+        }
+
+        @Override
+        public String classpath(RunMode runMode) {
+            return appJar;
+        }
+
+        @Override
+        public String[] vmArgs(RunMode runMode) {
+            return new String[] {
+               "-Xlog:cds+class=trace",
+                "--add-modules=jdk.management"
+            };
+        }
+
+        @Override
+        public String[] appCommandLine(RunMode runMode) {
+            return new String[] {
+                mainClass, runMode.name()
+            };
+        }
+
+        @Override
+        public void checkExecution(OutputAnalyzer out, RunMode runMode) {
+            var name = runMode.name();
+            if (runMode.isApplicationExecuted()) {
+                if(runMode == RunMode.TRAINING) {
+                    out.shouldContain("Hello Leyden " + name);
+                    out.shouldContain("AOT.end_recording invoked successfully");
+                    out.shouldContain("Successfully stopped recording");
+                } else if (runMode == RunMode.ASSEMBLY) {
+                    out.shouldNotContain("Hello Leyden ");
+                } else if (runMode == RunMode.PRODUCTION) {
+                    out.shouldContain("Hello Leyden " + name);
+                    out.shouldContain("AOT.end_recording invoked successfully");
+                    out.shouldContain("Failed to stop recording");
+                }
+                out.shouldNotContain("IOException occurred!");
+            }
+        }
+    }
+}
+
+class DiagnosticCommandMBeanApp {
+    public static void main(String[] args) {
+        System.out.println("Hello Leyden " + args[0]);
+        try {
+            MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+            ObjectName diagName = new ObjectName("com.sun.management:type=DiagnosticCommand");
+            Object[] params = { new String[0] };
+            String[] signature = { "[Ljava.lang.String;" };
+            String result = (String) server.invoke(diagName, "aotEndRecording", params, signature);
+            System.out.println("AOT.end_recording invoked successfully");
+            System.out.println("Result: #" + result + "#");
+            if (result.contains("Recording ended successfully")) {
+                System.out.println("Successfully stopped recording");
+            } else {
+                System.out.println("Failed to stop recording");
+            }
+        } catch (MBeanException e) {
+            System.out.println("MBeanException occurred!");
+        } catch (ReflectionException e) {
+            System.out.println("ReflectionException occurred!");
+        } catch (MalformedObjectNameException e) {
+            System.out.println("MalformedObjectNameException occurred!");
+        } catch (InstanceNotFoundException e) {
+            System.out.println("InstanceNotFoundException occurred!");
+        }
+    }
+}
