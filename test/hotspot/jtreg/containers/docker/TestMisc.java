9f3fe902e9f08cf379a387daf93a499f5b36ac84
Severin Gehwolf <sgehwolf@openjdk.org>
2025-11-14 13:13:09 +0000

    8370492: [Linux] Update cpu shares to cpu.weight mapping function
    Reviewed-by: cnorrbin, ayang, syan

--- test/hotspot/jtreg/containers/docker/TestMisc.java
+++ test/hotspot/jtreg/containers/docker/TestMisc.java
@@ -29,20 +29,24 @@
  * @requires !vm.asan
  * @library /test/lib
  * @modules java.base/jdk.internal.misc
+ *          java.base/jdk.internal.platform
  *          java.management
  *          jdk.jartool/sun.tools.jar
  * @build CheckContainerized jdk.test.whitebox.WhiteBox PrintContainerInfo
  * @run driver jdk.test.lib.helpers.ClassFileInstaller -jar whitebox.jar jdk.test.whitebox.WhiteBox
  * @run driver TestMisc
  */
+import jdk.internal.platform.Metrics;
 import jdk.test.lib.containers.docker.Common;
 import jdk.test.lib.containers.docker.DockerTestUtils;
 import jdk.test.lib.containers.docker.DockerRunOptions;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
+import jtreg.SkippedException;
 
 
 public class TestMisc {
+    private static final Metrics metrics = Metrics.systemMetrics();
     private static final String imageName = Common.imageName("misc");
 
     public static void main(String[] args) throws Exception {
@@ -58,6 +62,7 @@ public class TestMisc {
             testIsContainerized();
             testPrintContainerInfo();
             testPrintContainerInfoActiveProcessorCount();
+            testPrintContainerInfoCPUShares();
         } finally {
             DockerTestUtils.removeDockerImage(imageName);
         }
@@ -94,8 +99,53 @@ public class TestMisc {
         checkContainerInfo(Common.run(opts));
     }
 
+    // Test the mapping function on cgroups v2. Should also pass on cgroups v1 as it's
+    // a direct mapping there.
+    private static void testPrintContainerInfoCPUShares() throws Exception {
+        // Test won't work on cgv1 rootless podman since resource limits don't
+        // work there.
+        if ("cgroupv1".equals(metrics.getProvider()) &&
+            DockerTestUtils.isPodman() &&
+            DockerTestUtils.isRootless()) {
+            throw new SkippedException("Resource limits required for testPrintContainerInfoCPUShares(). " +
+                                       "This is cgv1 with podman in rootless mode. Test skipped.");
+        }
+        // Anything less than 1024 should return the back-mapped cpu-shares value without
+        // rounding to next multiple of 1024 (on cg v2). Only ensure that we get
+        // 'cpu_shares: <back-mapped-value>' over 'cpu_shares: no shares'.
+        printContainerInfo(512, 1024, false);
+        // Don't use 1024 exactly so as to avoid mapping to the unlimited/uset case.
+        // Use a value > 100 post-mapping so as to hit the non-default branch: 1052 => 103
+        printContainerInfo(1052, 1024, true);
+        // need at least 2 CPU cores for this test to work
+        if (Runtime.getRuntime().availableProcessors() >= 2) {
+            printContainerInfo(2048, 2048, true);
+        }
+    }
+
+    private static void printContainerInfo(int cpuShares, int expected, boolean numberMatch) throws Exception {
+        Common.logNewTestCase("Test print_container_info() - cpu shares - given: " + cpuShares + ", expected: " + expected);
+
+        DockerRunOptions opts = Common.newOpts(imageName, "PrintContainerInfo");
+        Common.addWhiteBoxOpts(opts);
+        opts.addDockerOpts("--cpu-shares", Integer.valueOf(cpuShares).toString());
+
+        OutputAnalyzer out = Common.run(opts);
+        String str = out.getOutput();
+        boolean isCgroupV2 = str.contains("cgroupv2");
+        // cg v1 maps cpu shares values verbatim. Only cg v2 uses the
+        // mapping function.
+        if (numberMatch) {
+          int valueExpected = isCgroupV2 ? expected : cpuShares;
+          out.shouldContain("cpu_shares: " + valueExpected);
+        } else {
+          // must not print "no shares"
+          out.shouldNotContain("cpu_shares: no shares");
+        }
+    }
+
     private static void testPrintContainerInfoActiveProcessorCount() throws Exception {
-        Common.logNewTestCase("Test print_container_info()");
+        Common.logNewTestCase("Test print_container_info() - ActiveProcessorCount");
 
         DockerRunOptions opts = Common.newOpts(imageName, "PrintContainerInfo").addJavaOpts("-XX:ActiveProcessorCount=2");
         Common.addWhiteBoxOpts(opts);
