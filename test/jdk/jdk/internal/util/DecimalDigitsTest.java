0070e7b3a33960a514a0e0e56f789d1196c4a47f
Shaojin Wen <shaojin.wensj@alibaba-inc.com>
2025-11-14 23:42:05 +0800

    add test and from @RogerRiggs

--- test/jdk/jdk/internal/util/DecimalDigitsTest.java
+++ test/jdk/jdk/internal/util/DecimalDigitsTest.java
@@ -23,8 +23,8 @@
 
 package test.jdk.internal.util;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 import jdk.internal.util.DecimalDigits;
 
@@ -33,7 +33,7 @@ import jdk.internal.util.DecimalDigits;
  * @bug 8366224
  * @summary Test DecimalDigits.appendPair method with LATIN1 and UTF16 encoding
  * @modules java.base/jdk.internal.util
- * @run junit test.jdk.internal.util.DecimalDigitsTest
+ * @run testng test.jdk.internal.util.DecimalDigitsTest
  */
 public class DecimalDigitsTest {
 
@@ -49,17 +49,43 @@ public class DecimalDigitsTest {
     }
 
     @Test
+    public void testAppendQuad() {
+        // Test values from 0 to 2000
+        for (int i = 0; i <= 2000; i += 5) {
+            StringBuilder sb = new StringBuilder();
+            DecimalDigits.appendQuad(sb, i);
+            String expected = String.format("%04d", i);
+            assertEquals(sb.toString(), expected, "Failed for value: " + i);
+        }
+    }
+
+    @Test
     public void testAppendPairWithUTF16Encoding() {
         // Test appendPair with UTF16 encoding
         StringBuilder sb = new StringBuilder();
-
-        // Force the StringBuilder to use UTF16 encoding by adding a non-LATIN1 character first
-        sb.append('€'); // Euro sign is not in LATIN1
-
+        char ch = '€';
         for (int i = 0; i <= 99; i++) {
             int currentLength = sb.length();
+            sb.append(ch); // Euro sign is not in LATIN1
             DecimalDigits.appendPair(sb, i);
-            String expected = String.format("%02d", i);
+            String expected = ch + String.format("%02d", i);
+
+            // Check that the pair was appended correctly
+            String appended = sb.substring(currentLength);
+            assertEquals(appended, expected, "Failed for value: " + i + " with UTF16 encoding");
+        }
+    }
+
+    @Test
+    public void testAppendQuadWithUTF16Encoding() {
+        // Test appendPair with UTF16 encoding
+        StringBuilder sb = new StringBuilder();
+        char ch = '€';
+        for (int i = 0; i <= 2000; i += 5) {
+            int currentLength = sb.length();
+            sb.append(ch); // Euro sign is not in LATIN1
+            DecimalDigits.appendQuad(sb, i);
+            String expected = ch + String.format("%04d", i);
 
             // Check that the pair was appended correctly
             String appended = sb.substring(currentLength);
