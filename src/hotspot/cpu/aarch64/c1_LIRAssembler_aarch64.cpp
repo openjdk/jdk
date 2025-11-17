1f7c2a7ae9c4b399b4cab9a55250a8d48b363f4c
tstuefe <thomas.stuefe@gmail.com>
2025-11-17 09:03:56 +0100

    Removed UseCCP from cpu/aarch64, cpu/arm

--- src/hotspot/cpu/aarch64/c1_LIRAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/c1_LIRAssembler_aarch64.cpp
@@ -1291,12 +1291,10 @@ void LIR_Assembler::emit_typecheck_helper(LIR_OpTypeCheck *op, Label* success, L
   } else if (obj == klass_RInfo) {
     klass_RInfo = dst;
   }
-  if (k->is_loaded() && !USE_COMPRESSED_CLASS_POINTERS_ALWAYS_TRUE) {
-    select_different_registers(obj, dst, k_RInfo, klass_RInfo);
-  } else {
-    Rtmp1 = op->tmp3()->as_register();
-    select_different_registers(obj, dst, k_RInfo, klass_RInfo, Rtmp1);
-  }
+
+  Rtmp1 = op->tmp3()->as_register();
+  select_different_registers(obj, dst, k_RInfo, klass_RInfo, Rtmp1);
+
 
   assert_different_registers(obj, k_RInfo, klass_RInfo);
 
