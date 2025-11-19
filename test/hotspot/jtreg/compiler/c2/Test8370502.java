e205f8871c3b829bffa4d85dec44b1ccdc6611fe
katkerem <katkerem@amazon.com>
2025-11-19 16:06:57 +0000

    8370502: C2: segfault while adding node to IGVN worklist

---
+++ test/hotspot/jtreg/compiler/c2/Test8370502.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2025 Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @bug 8370502
+ * @summary Do not segfault while adding node to IGVN worklist
+ *
+ * @run main/othervm -Xbatch compiler.c2.Test8370502
+ */
+
+package compiler.c2;
+
+public class Test8370502 {
+  public static void main(String[] args) {
+    int[] a = new int[0]; // test valid only when size is 0.
+    for (int i = 0; i < Integer.valueOf(10000); i++)
+      try {
+        test(a, 0);
+      } catch (ArrayIndexOutOfBoundsException e) {
+      }
+  }
+
+  static void test(int[] a, int invar) {
+    for (int i = 0; i < 1;) {
+      a[i] = 0;
+      synchronized (Test8370502.class) {
+      }
+      for (int j = 0; Integer.valueOf(j) < 1;)
+        j = 0;
+    }
+  }
+}
