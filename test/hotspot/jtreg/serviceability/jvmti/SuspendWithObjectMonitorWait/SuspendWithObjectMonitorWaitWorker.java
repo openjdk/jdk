d4f01852d88c98f032241a6f796d2e3302ab0428
Anton Artemov <anton.artemov@oracle.com>
2025-11-20 10:47:05 +0100

    8366659: Fixed tests.

--- test/hotspot/jtreg/serviceability/jvmti/SuspendWithObjectMonitorWait/SuspendWithObjectMonitorWaitWorker.java
+++ test/hotspot/jtreg/serviceability/jvmti/SuspendWithObjectMonitorWait/SuspendWithObjectMonitorWaitWorker.java
@@ -39,7 +39,7 @@ public class SuspendWithObjectMonitorWaitWorker extends Thread {
     native static int resumeThread(SuspendWithObjectMonitorWaitWorker thr);
 
     public void run() {
-        SuspendWithObjectMonitorWait1.logDebug("thread running");
+        SuspendWithObjectMonitorWaitBase.logDebug("thread running");
 
         //
         // Launch the waiter thread:
@@ -49,36 +49,36 @@ public class SuspendWithObjectMonitorWaitWorker extends Thread {
         //
         if (getName().equals("waiter")) {
             // grab threadLock before we tell main we are running
-            SuspendWithObjectMonitorWait1.logDebug("before enter threadLock");
-            synchronized(SuspendWithObjectMonitorWait1.threadLock) {
-                SuspendWithObjectMonitorWait1.logDebug("enter threadLock");
+            SuspendWithObjectMonitorWaitBase.logDebug("before enter threadLock");
+            synchronized(SuspendWithObjectMonitorWaitBase.threadLock) {
+                SuspendWithObjectMonitorWaitBase.logDebug("enter threadLock");
 
-                SuspendWithObjectMonitorWait1.checkTestState(SuspendWithObjectMonitorWait1.TS_INIT);
+                SuspendWithObjectMonitorWaitBase.checkTestState(SuspendWithObjectMonitorWaitBase.TS_INIT);
 
-                synchronized(SuspendWithObjectMonitorWait1.barrierLaunch) {
+                synchronized(SuspendWithObjectMonitorWaitBase.barrierLaunch) {
                     // tell main we are running
-                    SuspendWithObjectMonitorWait1.testState = SuspendWithObjectMonitorWait1.TS_WAITER_RUNNING;
-                    SuspendWithObjectMonitorWait1.barrierLaunch.notify();
+                    SuspendWithObjectMonitorWaitBase.testState = SuspendWithObjectMonitorWaitBase.TS_WAITER_RUNNING;
+                    SuspendWithObjectMonitorWaitBase.barrierLaunch.notify();
                 }
 
-                SuspendWithObjectMonitorWait1.logDebug("before wait");
+                SuspendWithObjectMonitorWaitBase.logDebug("before wait");
 
                 // TS_READY_TO_NOTIFY is set after the main thread has
                 // entered threadLock so a spurious wakeup can't get the
                 // waiter thread out of this threadLock.wait(0) call:
-                while (SuspendWithObjectMonitorWait1.testState <= SuspendWithObjectMonitorWait1.TS_READY_TO_NOTIFY) {
+                while (SuspendWithObjectMonitorWaitBase.testState <= SuspendWithObjectMonitorWaitBase.TS_READY_TO_NOTIFY) {
                     try {
-                        SuspendWithObjectMonitorWait1.threadLock.wait(waitTimeout);
+                        SuspendWithObjectMonitorWaitBase.threadLock.wait(waitTimeout);
                     } catch (InterruptedException ex) {
                     }
                 }
 
-                SuspendWithObjectMonitorWait1.logDebug("after wait");
+                SuspendWithObjectMonitorWaitBase.logDebug("after wait");
 
-                SuspendWithObjectMonitorWait1.checkTestState(SuspendWithObjectMonitorWait1.TS_CALL_RESUME);
-                SuspendWithObjectMonitorWait1.testState = SuspendWithObjectMonitorWait1.TS_WAITER_DONE;
+                SuspendWithObjectMonitorWaitBase.checkTestState(SuspendWithObjectMonitorWaitBase.TS_CALL_RESUME);
+                SuspendWithObjectMonitorWaitBase.testState = SuspendWithObjectMonitorWaitBase.TS_WAITER_DONE;
 
-                SuspendWithObjectMonitorWait1.logDebug("exit threadLock");
+                SuspendWithObjectMonitorWaitBase.logDebug("exit threadLock");
             }
         }
         //
@@ -89,39 +89,39 @@ public class SuspendWithObjectMonitorWaitWorker extends Thread {
         // - releases threadLock
         //
         else if (getName().equals("resumer")) {
-            synchronized(SuspendWithObjectMonitorWait1.barrierResumer) {
-                synchronized(SuspendWithObjectMonitorWait1.barrierLaunch) {
+            synchronized(SuspendWithObjectMonitorWaitBase.barrierResumer) {
+                synchronized(SuspendWithObjectMonitorWaitBase.barrierLaunch) {
                     // tell main we are running
-                    SuspendWithObjectMonitorWait1.testState = SuspendWithObjectMonitorWait1.TS_RESUMER_RUNNING;
-                    SuspendWithObjectMonitorWait1.barrierLaunch.notify();
+                    SuspendWithObjectMonitorWaitBase.testState = SuspendWithObjectMonitorWaitBase.TS_RESUMER_RUNNING;
+                    SuspendWithObjectMonitorWaitBase.barrierLaunch.notify();
                 }
-                SuspendWithObjectMonitorWait1.logDebug("thread waiting");
-                while (SuspendWithObjectMonitorWait1.testState != SuspendWithObjectMonitorWait1.TS_READY_TO_RESUME) {
+                SuspendWithObjectMonitorWaitBase.logDebug("thread waiting");
+                while (SuspendWithObjectMonitorWaitBase.testState != SuspendWithObjectMonitorWaitBase.TS_READY_TO_RESUME) {
                     try {
                         // wait for main to tell us when to continue
-                        SuspendWithObjectMonitorWait1.barrierResumer.wait(0);
+                        SuspendWithObjectMonitorWaitBase.barrierResumer.wait(0);
                     } catch (InterruptedException ex) {
                     }
                 }
             }
 
-            SuspendWithObjectMonitorWait1.logDebug("before enter threadLock");
-            synchronized(SuspendWithObjectMonitorWait1.threadLock) {
-                SuspendWithObjectMonitorWait1.logDebug("enter threadLock");
+            SuspendWithObjectMonitorWaitBase.logDebug("before enter threadLock");
+            synchronized(SuspendWithObjectMonitorWaitBase.threadLock) {
+                SuspendWithObjectMonitorWaitBase.logDebug("enter threadLock");
 
-                SuspendWithObjectMonitorWait1.checkTestState(SuspendWithObjectMonitorWait1.TS_READY_TO_RESUME);
-                SuspendWithObjectMonitorWait1.testState = SuspendWithObjectMonitorWait1.TS_CALL_RESUME;
+                SuspendWithObjectMonitorWaitBase.checkTestState(SuspendWithObjectMonitorWaitBase.TS_READY_TO_RESUME);
+                SuspendWithObjectMonitorWaitBase.testState = SuspendWithObjectMonitorWaitBase.TS_CALL_RESUME;
 
                 // resume the waiter thread so waiter.join() can work
-                SuspendWithObjectMonitorWait1.logDebug("before resume thread");
+                SuspendWithObjectMonitorWaitBase.logDebug("before resume thread");
                 int retCode = resumeThread(target);
                 if (retCode != 0) {
                     throw new RuntimeException("error in JVMTI ResumeThread: " +
                             "retCode=" + retCode);
                 }
-                SuspendWithObjectMonitorWait1.logDebug("resumed thread");
+                SuspendWithObjectMonitorWaitBase.logDebug("resumed thread");
 
-                SuspendWithObjectMonitorWait1.logDebug("exit threadLock");
+                SuspendWithObjectMonitorWaitBase.logDebug("exit threadLock");
             }
         }
     }
