d01e7d9850a57dd8e4849e2b7db6eacb37605647
Joel Sikström <jsikstro@openjdk.org>
2025-11-21 13:44:09 +0000

    8371783: Refactor GCCause in VM_CollectForMetadataAllocation
    Reviewed-by: kbarrett, ayang

--- src/hotspot/share/gc/shared/collectedHeap.cpp
+++ src/hotspot/share/gc/shared/collectedHeap.cpp
@@ -379,8 +379,7 @@ MetaWord* CollectedHeap::satisfy_failed_metadata_allocation(ClassLoaderData* loa
                                        word_size,
                                        mdtype,
                                        gc_count,
-                                       full_gc_count,
-                                       GCCause::_metadata_GC_threshold);
+                                       full_gc_count);
 
     VMThread::execute(&op);
 
