721b95b2715f0015df8327a62dffb4c1ae8b9757
prrace <philip.race@oracle.com>
2025-11-24 12:54:02 -0800

    6185110

--- src/java.desktop/share/classes/java/awt/image/SinglePixelPackedSampleModel.java
+++ src/java.desktop/share/classes/java/awt/image/SinglePixelPackedSampleModel.java
@@ -568,6 +568,7 @@ public class SinglePixelPackedSampleModel extends SampleModel
      * @param obj       A primitive array containing pixel data.
      * @param data      The DataBuffer containing the image data.
      * @see #getDataElements(int, int, Object, DataBuffer)
+     * @throws NullPointerException if {@code obj} is null.
      * @throws NullPointerException if {@code data} is null.
      * @throws ArrayIndexOutOfBoundsException if the coordinates are
      * not in bounds, or if obj is too small to hold the input.
