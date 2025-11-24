5c3305c1497365b2030321c4456e4aee948694d0
Liam Miller-Cushon <cushon@google.com>
2025-11-24 13:50:33 +0100

    Apply suggestions from code review
    Co-authored-by: ExE Boss <3889017+ExE-Boss@users.noreply.github.com>

--- src/java.base/share/classes/java/lang/String.java
+++ src/java.base/share/classes/java/lang/String.java
@@ -2120,6 +2120,10 @@ public final class String
         if (bytesCompatible(cs)) {
             return value.length;
         }
+        if (cs instanceof sun.nio.cs.UTF_16LE ||
+            cs instanceof sun.nio.cs.UTF_16BE) {
+            return value.length << (1 - coder());
+        }
         return getBytes(cs).length;
     }
 
