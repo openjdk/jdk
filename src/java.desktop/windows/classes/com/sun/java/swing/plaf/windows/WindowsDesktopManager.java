63216ab7ed2bc8ffb87b2665140bc1d90346b55e
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2025-11-26 06:50:58 +0000

    6681958: Maximization state of JInternalFrames is corrupted by WindowsDesktopManager

--- src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsDesktopManager.java
+++ src/java.desktop/windows/classes/com/sun/java/swing/plaf/windows/WindowsDesktopManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -80,8 +80,12 @@ public final class WindowsDesktopManager extends DefaultDesktopManager
                         if (f.isMaximizable()) {
                             if (!f.isMaximum()) {
                                 f.setMaximum(true);
-                            } else if (f.isMaximum() && f.isIcon()) {
-                                f.setIcon(false);
+                            } else if (f.isMaximum()) {
+                                if (f.isIcon()) {
+                                    f.setIcon(false);
+                                } else {
+                                    f.setMaximum(true);
+                                }
                             } else {
                                 f.setMaximum(false);
                             }
