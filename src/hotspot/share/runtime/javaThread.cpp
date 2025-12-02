ac6aab2f747f6613799f49d1557516e0fa3affc5
Leonid Mesnik <leonid.mesnik@gmail.com>
2025-12-02 13:03:28 -0800

    Update src/hotspot/share/runtime/javaThread.cpp
    Co-authored-by: Alex Menkov <alexey.menkov@oracle.com>

--- src/hotspot/share/runtime/javaThread.cpp
+++ src/hotspot/share/runtime/javaThread.cpp
@@ -451,7 +451,7 @@ JavaThread::JavaThread(MemTag mem_tag) :
   _is_in_VTMS_transition(false),
   _is_disable_suspend(false),
   _is_in_java_upcall(false),
-  _jvmti_events_disabled(false),
+  _jvmti_events_disabled(0),
   _VTMS_transition_mark(false),
   _on_monitor_waited_event(false),
   _contended_entered_monitor(nullptr),
