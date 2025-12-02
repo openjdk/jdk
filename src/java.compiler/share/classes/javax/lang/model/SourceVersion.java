b78e1e439434ed32d0b0880e3342910aef50c57d
Nizar Benalla <nbenalla@openjdk.org>
2025-12-02 20:51:52 +0000

    8372939: Update JDK 26 spec URLs
    Reviewed-by: liach

--- src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
+++ src/java.compiler/share/classes/javax/lang/model/SourceVersion.java
@@ -476,7 +476,7 @@ public enum SourceVersion {
      * @since 26
      *
      * @see <a
-     * href="https://docs.oracle.com/javase/specs/jls/se26/html/index.html">
+     * href="https://docs.oracle.com/en/java/javase/26/docs/specs/jls/index.html">
      * <cite>The Java Language Specification, Java SE 26 Edition</cite></a>
      */
     RELEASE_26,
