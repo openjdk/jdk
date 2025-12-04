2edfb4e895d66d46aec6c4050680563590fea300
Nizar Benalla <nbenalla@openjdk.org>
2025-12-04 17:01:41 +0000

    8370890: Start of release updates for JDK 27 8370893: Add SourceVersion.RELEASE_27 8370894: Add source 27 and target 27 to javac
    Reviewed-by: darcy, iris, liach, erikj, dholmes

--- src/java.compiler/share/classes/javax/lang/model/util/AbstractTypeVisitorPreview.java
+++ src/java.compiler/share/classes/javax/lang/model/util/AbstractTypeVisitorPreview.java
@@ -53,7 +53,7 @@ import static javax.lang.model.SourceVersion.*;
  * @see AbstractTypeVisitor14
  * @since 23
  */
-@SupportedSourceVersion(RELEASE_26)
+@SupportedSourceVersion(RELEASE_27)
 @PreviewFeature(feature=PreviewFeature.Feature.LANGUAGE_MODEL, reflective=true)
 public abstract class AbstractTypeVisitorPreview<R, P> extends AbstractTypeVisitor14<R, P> {
     /**
