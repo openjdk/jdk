991b9b994bff3bf6fb592d0c61975d5caec32779
Joe Darcy <joe.darcy@oracle.com>
2025-12-04 21:10:38 -0800

    JDK-8373125: Add defensive screening of modifiers for Field and Parameter toString() results

--- src/java.base/share/classes/java/lang/reflect/Field.java
+++ src/java.base/share/classes/java/lang/reflect/Field.java
@@ -367,7 +367,7 @@ class Field extends AccessibleObject implements Member {
      * @jls 8.3.1 Field Modifiers
      */
     public String toString() {
-        int mod = getModifiers();
+        int mod = getModifiers() & Modifier.fieldModifiers();
         return (((mod == 0) ? "" : (Modifier.toString(mod) + " "))
             + getType().getTypeName() + " "
             + getDeclaringClass().getTypeName() + "."
@@ -400,7 +400,7 @@ class Field extends AccessibleObject implements Member {
      * @jls 8.3.1 Field Modifiers
      */
     public String toGenericString() {
-        int mod = getModifiers();
+        int mod = getModifiers() & Modifier.fieldModifiers();
         Type fieldType = getGenericType();
         return (((mod == 0) ? "" : (Modifier.toString(mod) + " "))
             + fieldType.getTypeName() + " "
