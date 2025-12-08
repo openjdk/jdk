ec8de6c7276247e98228c4aafa83e69a2655e31b
Anton Artemov <aartemov@openjdk.org>
2025-12-08 16:07:01 +0000

    8366671: Refactor Thread::SpinAcquire and Thread::SpinRelease
    Co-authored-by: Axel Boldt-Christmas <aboldtch@openjdk.org>
    Reviewed-by: coleenp, kbarrett, dholmes, aboldtch

--- src/hotspot/share/runtime/park.cpp
+++ src/hotspot/share/runtime/park.cpp
@@ -25,6 +25,7 @@
 #include "memory/allocation.inline.hpp"
 #include "nmt/memTracker.hpp"
 #include "runtime/javaThread.hpp"
+#include "utilities/spinCriticalSection.hpp"
 
 // Lifecycle management for TSM ParkEvents.
 // ParkEvents are type-stable (TSM).
@@ -60,14 +61,13 @@ ParkEvent * ParkEvent::Allocate (Thread * t) {
   // Using a spin lock since we are part of the mutex impl.
   // 8028280: using concurrent free list without memory management can leak
   // pretty badly it turns out.
-  Thread::SpinAcquire(&ListLock);
   {
+    SpinCriticalSection scs(&ListLock);
     ev = FreeList;
     if (ev != nullptr) {
       FreeList = ev->FreeNext;
     }
   }
-  Thread::SpinRelease(&ListLock);
 
   if (ev != nullptr) {
     guarantee (ev->AssociatedWith == nullptr, "invariant") ;
@@ -88,12 +88,11 @@ void ParkEvent::Release (ParkEvent * ev) {
   ev->AssociatedWith = nullptr ;
   // Note that if we didn't have the TSM/immortal constraint, then
   // when reattaching we could trim the list.
-  Thread::SpinAcquire(&ListLock);
   {
+    SpinCriticalSection scs(&ListLock);
     ev->FreeNext = FreeList;
     FreeList = ev;
   }
-  Thread::SpinRelease(&ListLock);
 }
 
 // Override operator new and delete so we can ensure that the
