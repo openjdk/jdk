909faa51f8a336369bb8ae8e369a1b2973c87119
Hai-May Chao <hai-may.chao@oracle.com>
2025-12-09 09:40:41 -0800

    small tweak to long line

--- test/jdk/sun/security/pkcs11/tls/fips/FipsModeTLS.java
+++ test/jdk/sun/security/pkcs11/tls/fips/FipsModeTLS.java
@@ -37,8 +37,9 @@
  *   Stateless resumption doesn't currently work with NSS-FIPS, see JDK-8368669.
  *   NSS-FIPS does not support ML-KEM, so configures the list of named groups.
  * @run main/othervm/timeout=120
- *      -Djdk.tls.client.protocols=TLSv1.3 -Djdk.tls.namedGroups=x25519,secp256r1,secp384r1,secp521r1,x448,ffdhe2048,ffdhe3072,ffdhe4096,ffdhe6144,ffdhe8192
-*       FipsModeTLS
+ *      -Djdk.tls.client.protocols=TLSv1.3
+ *      -Djdk.tls.namedGroups=x25519,secp256r1,secp384r1,secp521r1,x448,ffdhe2048,ffdhe3072,ffdhe4096,ffdhe6144,ffdhe8192
+ *      FipsModeTLS
  */
 
 import java.io.File;
