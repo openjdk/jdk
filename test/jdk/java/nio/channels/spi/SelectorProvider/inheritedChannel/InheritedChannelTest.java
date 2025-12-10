7bf179de78f1ca97b28a2841ce3bd7c2618ad706
Jaikiran Pai <jpai@openjdk.org>
2025-12-10 02:04:12 +0000

    8255463: java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java failed with ThreadTimeoutException
    Reviewed-by: dfuchs, djelinski, bpb

--- test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java
+++ test/jdk/java/nio/channels/spi/SelectorProvider/inheritedChannel/InheritedChannelTest.java
@@ -40,7 +40,6 @@
  * @key intermittent
  */
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -77,7 +76,7 @@ public class InheritedChannelTest {
         };
     }
 
-    @Test(dataProvider = "testCases", timeOut=30000)
+    @Test(dataProvider = "testCases")
     public void test(String desc, List<String> opts) throws Throwable {
         String pathVar = Platform.sharedLibraryPathVariableName();
         System.out.println(pathVar + "=" + libraryPath);
