662b22982ce9a14f4311933751c4dcc399886946
rwestrel <rwestrel@redhat.com>
2025-12-12 13:46:36 +0100

    fix

--- src/hotspot/share/opto/cfgnode.cpp
+++ src/hotspot/share/opto/cfgnode.cpp
@@ -3114,6 +3114,8 @@ Node* CreateExNode::Identity(PhaseGVN* phase) {
   // If the CatchProj is optimized away, then we just carry the
   // exception oop through.
   CallNode *call = in(1)->in(0)->as_Call();
+  assert(in(0)->is_CatchProj() && in(0)->in(0)->is_Catch() &&
+          in(0)->in(0)->in(1) == in(1), "");
 
   return (in(0)->is_CatchProj() && in(0)->in(0)->is_Catch() &&
           in(0)->in(0)->in(1) == in(1)) ? this : call->in(TypeFunc::Parms);
