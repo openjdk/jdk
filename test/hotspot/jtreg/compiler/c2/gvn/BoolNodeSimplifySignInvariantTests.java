e007f6c9ce1b52252eb78b31dd0b2f49e7b2f053
SirYwell <hannesgreule@outlook.de>
2025-12-12 09:00:29 +0100

    more randomized tests

--- test/hotspot/jtreg/compiler/c2/gvn/BoolNodeSimplifySignInvariantTests.java
+++ test/hotspot/jtreg/compiler/c2/gvn/BoolNodeSimplifySignInvariantTests.java
@@ -22,9 +22,11 @@
  */
 package compiler.c2.gvn;
 
+import compiler.lib.generators.Generator;
 import compiler.lib.generators.Generators;
 import compiler.lib.generators.RestrictableGenerator;
 import compiler.lib.ir_framework.DontCompile;
+import compiler.lib.ir_framework.ForceInline;
 import compiler.lib.ir_framework.IR;
 import compiler.lib.ir_framework.IRNode;
 import compiler.lib.ir_framework.Run;
@@ -92,6 +94,12 @@ public class BoolNodeSimplifySignInvariantTests {
         "knownMinInputLong1", "knownMinInputLong2", "knownMinInputLong3", "knownMinInputLong4",
         "knownXorInputLong1", "knownXorInputLong2", "knownXorInputLong3", "knownXorInputLong4",
         "knownXorInputLong5", "knownXorInputLong6", "knownXorInputLong7", "knownXorInputLong8",
+
+        "testRandomRangeAndInt1", "testRandomRangeAndInt2", "testRandomRangeAndLong1", "testRandomRangeAndLong2",
+        "testRandomRangeOrInt1", "testRandomRangeOrInt2", "testRandomRangeOrLong1", "testRandomRangeOrLong2",
+        "testRandomRangeXorInt1", "testRandomRangeXorInt2", "testRandomRangeXorLong1", "testRandomRangeXorLong2",
+        "testRandomRangeMaxInt1", "testRandomRangeMaxInt2", "testRandomRangeMaxLong1", "testRandomRangeMaxLong2",
+        "testRandomRangeMinInt1", "testRandomRangeMinInt2", "testRandomRangeMinLong1", "testRandomRangeMinLong2",
     })
     public void runMethod() {
         assertResultI();
@@ -135,6 +143,21 @@ public class BoolNodeSimplifySignInvariantTests {
         Asserts.assertEQ((aPos ^ b) < 0, knownXorInputInt6(a, b));
         Asserts.assertEQ((a ^ bPos) >= 0, knownXorInputInt7(a, b));
         Asserts.assertEQ((aPos ^ b) >= 0, knownXorInputInt8(a, b));
+
+        for (int i = 0; i < 10; i++) {
+            int ia = GEN_INT.next();
+            int ib = GEN_INT.next();
+            Asserts.assertEQ(testRandomRangeAndInt1Interpreted(ia, ib), testRandomRangeAndInt1(ia, ib));
+            Asserts.assertEQ(testRandomRangeAndInt2Interpreted(ia, ib), testRandomRangeAndInt2(ia, ib));
+            Asserts.assertEQ(testRandomRangeOrInt1Interpreted(ia, ib), testRandomRangeOrInt1(ia, ib));
+            Asserts.assertEQ(testRandomRangeOrInt2Interpreted(ia, ib), testRandomRangeOrInt2(ia, ib));
+            Asserts.assertEQ(testRandomRangeXorInt1Interpreted(ia, ib), testRandomRangeXorInt1(ia, ib));
+            Asserts.assertEQ(testRandomRangeXorInt2Interpreted(ia, ib), testRandomRangeXorInt2(ia, ib));
+            Asserts.assertEQ(testRandomRangeMaxInt1Interpreted(ia, ib), testRandomRangeMaxInt1(ia, ib));
+            Asserts.assertEQ(testRandomRangeMaxInt2Interpreted(ia, ib), testRandomRangeMaxInt2(ia, ib));
+            Asserts.assertEQ(testRandomRangeMinInt1Interpreted(ia, ib), testRandomRangeMinInt1(ia, ib));
+            Asserts.assertEQ(testRandomRangeMinInt2Interpreted(ia, ib), testRandomRangeMinInt2(ia, ib));
+        }
     }
 
     @DontCompile
@@ -174,6 +197,21 @@ public class BoolNodeSimplifySignInvariantTests {
         Asserts.assertEQ((aPos ^ b) < 0, knownXorInputLong6(a, b));
         Asserts.assertEQ((a ^ bPos) >= 0, knownXorInputLong7(a, b));
         Asserts.assertEQ((aPos ^ b) >= 0, knownXorInputLong8(a, b));
+
+        for (int i = 0; i < 10; i++) {
+            long la = GEN_LONG.next();
+            long lb = GEN_LONG.next();
+            Asserts.assertEQ(testRandomRangeAndLong1Interpreted(la, lb), testRandomRangeAndLong1(la, lb));
+            Asserts.assertEQ(testRandomRangeAndLong2Interpreted(la, lb), testRandomRangeAndLong2(la, lb));
+            Asserts.assertEQ(testRandomRangeOrLong1Interpreted(la, lb), testRandomRangeOrLong1(la, lb));
+            Asserts.assertEQ(testRandomRangeOrLong2Interpreted(la, lb), testRandomRangeOrLong2(la, lb));
+            Asserts.assertEQ(testRandomRangeXorLong1Interpreted(la, lb), testRandomRangeXorLong1(la, lb));
+            Asserts.assertEQ(testRandomRangeXorLong2Interpreted(la, lb), testRandomRangeXorLong2(la, lb));
+            Asserts.assertEQ(testRandomRangeMaxLong1Interpreted(la, lb), testRandomRangeMaxLong1(la, lb));
+            Asserts.assertEQ(testRandomRangeMaxLong2Interpreted(la, lb), testRandomRangeMaxLong2(la, lb));
+            Asserts.assertEQ(testRandomRangeMinLong1Interpreted(la, lb), testRandomRangeMinLong1(la, lb));
+            Asserts.assertEQ(testRandomRangeMinLong2Interpreted(la, lb), testRandomRangeMinLong2(la, lb));
+        }
     }
 
     @Test
@@ -462,5 +500,258 @@ public class BoolNodeSimplifySignInvariantTests {
     @IR(failOn = {IRNode.XOR, IRNode.MAX, IRNode.MIN})
     public boolean knownXorInputLong8(long a, long b) {
         return (Math.min(HI_POS_L, Math.max(LO_POS_L, a)) ^ b) >= 0;
+    }
+
+    private static final IntRange INT_RANGE_1 = IntRange.generate(GEN_INT);
+    private static final IntRange INT_RANGE_2 = IntRange.generate(GEN_INT);
+
+    @Test
+    public boolean testRandomRangeAndInt1(int a, int b) {
+        return (INT_RANGE_1.clamp(a) & INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeAndInt1Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) & INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeAndInt2(int a, int b) {
+        return (INT_RANGE_1.clamp(a) & INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeAndInt2Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) & INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeOrInt1(int a, int b) {
+        return (INT_RANGE_1.clamp(a) | INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeOrInt1Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) | INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeOrInt2(int a, int b) {
+        return (INT_RANGE_1.clamp(a) | INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeOrInt2Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) | INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeXorInt1(int a, int b) {
+        return (INT_RANGE_1.clamp(a) ^ INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeXorInt1Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) ^ INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeXorInt2(int a, int b) {
+        return (INT_RANGE_1.clamp(a) ^ INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeXorInt2Interpreted(int a, int b) {
+        return (INT_RANGE_1.clamp(a) ^ INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMaxInt1(int a, int b) {
+        return Math.max(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMaxInt1Interpreted(int a, int b) {
+        return Math.max(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMaxInt2(int a, int b) {
+        return Math.max(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMaxInt2Interpreted(int a, int b) {
+        return Math.max(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMinInt1(int a, int b) {
+        return Math.min(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMinInt1Interpreted(int a, int b) {
+        return Math.min(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMinInt2(int a, int b) {
+        return Math.min(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMinInt2Interpreted(int a, int b) {
+        return Math.min(INT_RANGE_1.clamp(a), INT_RANGE_2.clamp(b)) >= 0;
+    }
+
+    private static final LongRange LONG_RANGE_1 = LongRange.generate(GEN_LONG);
+    private static final LongRange LONG_RANGE_2 = LongRange.generate(GEN_LONG);
+
+    @Test
+    public boolean testRandomRangeAndLong1(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) & LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeAndLong1Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) & LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeAndLong2(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) & LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeAndLong2Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) & LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeOrLong1(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) | LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeOrLong1Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) | LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeOrLong2(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) | LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeOrLong2Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) | LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeXorLong1(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) ^ LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeXorLong1Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) ^ LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeXorLong2(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) ^ LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeXorLong2Interpreted(long a, long b) {
+        return (LONG_RANGE_1.clamp(a) ^ LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMaxLong1(long a, long b) {
+        return Math.max(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMaxLong1Interpreted(long a, long b) {
+        return Math.max(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMaxLong2(long a, long b) {
+        return Math.max(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMaxLong2Interpreted(long a, long b) {
+        return Math.max(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMinLong1(long a, long b) {
+        return Math.min(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMinLong1Interpreted(long a, long b) {
+        return Math.min(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) < 0;
+    }
+
+    @Test
+    public boolean testRandomRangeMinLong2(long a, long b) {
+        return Math.min(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    @DontCompile
+    public boolean testRandomRangeMinLong2Interpreted(long a, long b) {
+        return Math.min(LONG_RANGE_1.clamp(a), LONG_RANGE_2.clamp(b)) >= 0;
+    }
+
+    record IntRange(int lo, int hi) {
+        IntRange {
+            if (lo > hi) {
+                throw new IllegalArgumentException("lo > hi");
+            }
+        }
+
+        @ForceInline
+        int clamp(int v) {
+            return Math.min(hi, Math.max(v, lo));
+        }
+
+        static IntRange generate(Generator<Integer> g) {
+            var a = g.next();
+            var b = g.next();
+            if (a > b) {
+                var tmp = a;
+                a = b;
+                b = tmp;
+            }
+            return new IntRange(a, b);
+        }
+    }
+    record LongRange(long lo, long hi) {
+        LongRange {
+            if (lo > hi) {
+                throw new IllegalArgumentException("lo > hi");
+            }
+        }
+
+        @ForceInline
+        long clamp(long v) {
+            return Math.min(hi, Math.max(v, lo));
+        }
+
+        static LongRange generate(Generator<Long> g) {
+            var a = g.next();
+            var b = g.next();
+            if (a > b) {
+                var tmp = a;
+                a = b;
+                b = tmp;
+            }
+            return new LongRange(a, b);
+        }
     }
 }
