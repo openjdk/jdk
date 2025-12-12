d1eec7c3533de4770da8ccb79b8edbbed832b72f
Bara' Hasheesh <bhashesh@amazon.com>
2025-12-12 12:10:19 +0000

    exception message

--- test/jdk/jdk/jfr/api/recording/deadlock/TestShutdownDeadLock.java
+++ test/jdk/jdk/jfr/api/recording/deadlock/TestShutdownDeadLock.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -52,7 +52,7 @@ public class TestShutdownDeadLock {
             }
             Asserts.assertNotNull(thrownException);
             Asserts.assertEquals(thrownException.getClass(), IllegalStateException.class);
-            Asserts.assertEquals(thrownException.getMessage(), "Shutdown in progress");
+            Asserts.assertEquals(thrownException.getMessage(), "Flight recorder is already shutdown");
         }));
 
         System.out.println("Exiting");
