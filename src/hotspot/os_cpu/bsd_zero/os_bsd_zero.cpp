060ae0357b84c065d651a8a22c0af26f20fb42f5
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2025-12-15 23:12:52 +0800

    Reimplement in os_bsd_zero.cpp

--- src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
+++ src/hotspot/os_cpu/bsd_zero/os_bsd_zero.cpp
@@ -55,6 +55,10 @@
 # include <pthread_np.h> /* For pthread_attr_get_np */
 #endif
 
+NOINLINE address os::current_stack_pointer() {
+  return static_cast<address>(__builtin_dwarf_cfa());
+}
+
 frame os::get_sender_for_C_frame(frame* fr) {
   ShouldNotCallThis();
   return frame();
