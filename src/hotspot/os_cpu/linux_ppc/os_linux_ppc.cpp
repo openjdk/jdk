0e342b0658bfd0532910b937768f3b433e5439fc
Julian Waters <32636402+TheShermanTanker@users.noreply.github.com>
2025-12-15 23:05:41 +0800

    Reimplement in os_linux_ppc.cpp

--- src/hotspot/os_cpu/linux_ppc/os_linux_ppc.cpp
+++ src/hotspot/os_cpu/linux_ppc/os_linux_ppc.cpp
@@ -77,6 +77,16 @@
 # include <ucontext.h>
 
 
+asm (R"(
+    .globl  _ZN2os21current_stack_pointerEv
+    .hidden _ZN2os21current_stack_pointerEv
+    .type   _ZN2os21current_stack_pointerEv, @function
+_ZN2os21current_stack_pointerEv:
+    mr 3, 1
+    blr     
+#   .long 0
+#   .byte 0, 9, 0, 0, 0, 0, 0, 0
+)");
 
 char* os::non_memory_address_word() {
   // Must never look like an address returned by reserve_memory,
