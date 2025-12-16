4176c9e437f5c1a09661c2cd116101a1d9d38121
rwestrel <rwestrel@redhat.com>
2025-12-16 11:51:58 +0100

    tests

--- test/hotspot/jtreg/compiler/loopopts/TestCreateExSunkOutOfLoop2.java
+++ test/hotspot/jtreg/compiler/loopopts/TestCreateExSunkOutOfLoop2.java
@@ -25,50 +25,61 @@
  * @test
  * @bug 8373508
  * @summary C2: sinking CreateEx out of loop breaks the graph
- * @library /test/lib
- * @run main/othervm -Xbatch ${test.main.class}
+ * @run main/othervm -Xbatch -XX:-TieredCompilation ${test.main.class}
  * @run main ${test.main.class}
  */
 
 package compiler.loopopts;
 
-import jdk.test.lib.Utils;
+public class TestCreateExSunkOutOfLoop2 {
+    boolean b;
 
-public class TestCreateExSunkOutOfLoop {
-    volatile boolean _mutatorToggle;
-
-    boolean _mutatorFlip() {
-        _mutatorToggle = !_mutatorToggle;
-        return _mutatorToggle;
+    boolean getB() {
+        return b;
     }
 
     void test() {
-        int idx = -845;
-        for (;;) {
+        int x = -845;
+
+        for (int i = 5; i > 1; i -= 2) {
             try {
                 try {
-                    for (Object temp = new byte[idx];;) {;}
+                    for (Object temp = new byte[x]; ; ) {
+                        // infinite loop
+                    }
                 } finally {
-                    boolean flag = _mutatorFlip();
-                    _mutatorFlip();
-                    for (;;) {
+                    int zeroLimit = 2;
+                    for (; zeroLimit < 4; zeroLimit *= 2) { }
+
+                    int zero = 34;
+                    for (int peel = 2; peel < zeroLimit; peel++) {
+                        zero = 0;
+                    }
+
+                    if (zero == 0) {
+                        // nop
+                    }
+
+                    int flagLimit = 2;
+                    for (; flagLimit < 4; flagLimit *= 2) { }
+
+                    boolean flag = getB();
+                    for (int peel = 2; peel < flagLimit; peel++) {
                         if (flag) {
                             break;
                         }
                     }
                 }
-            } catch (Throwable $) {;}
+            } catch (Throwable t) {
+                // ignored
+            }
         }
     }
 
-    public static void main(String[] strArr) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            TestCreateExSunkOutOfLoop t = new TestCreateExSunkOutOfLoop();
+    public static void main(String[] strArr) {
+        TestCreateExSunkOutOfLoop2 t = new TestCreateExSunkOutOfLoop2();
+        for (int i = 0; i < 10_000; ++i) {
             t.test();
-        });
-        thread.setDaemon(true);
-        thread.start();
-        Thread.sleep(Utils.adjustTimeout(4000));
+        }
     }
-
 }
