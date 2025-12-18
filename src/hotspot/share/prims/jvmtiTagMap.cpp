4c368b43a41ee5184eb27ec2e474f2bc65d31188
Leonid Mesnik <lmesnik@openjdk.org>
2025-12-18 04:27:18 +0000

    8373723: Deadlock with JvmtiTagMap::flush_object_free_events()
    Reviewed-by: dholmes, coleenp

--- src/hotspot/share/prims/jvmtiTagMap.cpp
+++ src/hotspot/share/prims/jvmtiTagMap.cpp
@@ -1204,8 +1204,10 @@ void JvmtiTagMap::flush_object_free_events() {
   assert_not_at_safepoint();
   if (env()->is_enabled(JVMTI_EVENT_OBJECT_FREE)) {
     {
+      // The other thread can block for safepoints during event callbacks, so ensure we
+      // are safepoint-safe while waiting.
+      ThreadBlockInVM tbivm(JavaThread::current());
       MonitorLocker ml(lock(), Mutex::_no_safepoint_check_flag);
-      // If another thread is posting events, let it finish
       while (_posting_events) {
         ml.wait();
       }
