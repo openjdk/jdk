9411a6e78c73d54f4fb927e2a44ef4445a19c37d
SendaoYan <yansendao@hygon.cn>
2025-12-26 10:08:22 +0800

    8374364: TestCgroupMetrics.java fails when docker service unavailable

--- test/jdk/jdk/internal/platform/cgroup/TestCgroupMetrics.java
+++ test/jdk/jdk/internal/platform/cgroup/TestCgroupMetrics.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -31,11 +31,13 @@
  */
 
 import jdk.test.lib.containers.cgroup.MetricsTester;
+import jdk.test.lib.containers.docker.DockerTestUtils;
 import jdk.internal.platform.Metrics;
 
 public class TestCgroupMetrics {
 
     public static void main(String[] args) throws Exception {
+        DockerTestUtils.checkCanTestDocker();
         MetricsTester.main(args);
     }
 
