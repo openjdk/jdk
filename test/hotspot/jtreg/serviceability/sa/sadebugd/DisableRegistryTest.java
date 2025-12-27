5559ab0f5fa878658a06c72b82afae6ffdf52424
Sergey Bylokhov <serb@openjdk.org>
2025-12-27 04:56:04 +0000

    8374358: Update copyright year to 2025 for test/hotspot in files where it was missed
    Reviewed-by: phh

--- test/hotspot/jtreg/serviceability/sa/sadebugd/DisableRegistryTest.java
+++ test/hotspot/jtreg/serviceability/sa/sadebugd/DisableRegistryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2025, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2021 NTT DATA.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
