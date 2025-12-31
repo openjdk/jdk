22b044eb943a01cb902c73b75059e77879edcf03
Sergey Bylokhov <serb@openjdk.org>
2025-12-31 17:13:17 +0000

    8374355: Update copyright year to 2025 for demo in files where it was missed
    Reviewed-by: aivanov

--- src/demo/share/jfc/J2Ddemo/java2d/Tools.java
+++ src/demo/share/jfc/J2Ddemo/java2d/Tools.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2007, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2025, Oracle and/or its affiliates. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
