4fa97fb26ab553b85c680866dfa0ed834b31a615
Alan Bateman <alan.bateman@oracle.com>
2025-12-31 07:41:00 +0000

    Remove unused import

--- test/jdk/java/nio/channels/AsynchronousFileChannel/BufferPositions.java
+++ test/jdk/java/nio/channels/AsynchronousFileChannel/BufferPositions.java
@@ -35,7 +35,6 @@ import java.nio.channels.CompletionHandler;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Objects;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Stream;
