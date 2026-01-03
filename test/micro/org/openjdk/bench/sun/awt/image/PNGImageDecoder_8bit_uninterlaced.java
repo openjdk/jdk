7e182f4f91125de79edd5d2c1b9b7c5f3db9e2f7
jeremy <jeremy.wood@mac.com>
2026-01-02 22:57:07 -0500

    8374377: adding imports
    This is in response to:
    https://github.com/openjdk/jdk/pull/29004#discussion_r2657043086

--- test/micro/org/openjdk/bench/sun/awt/image/PNGImageDecoder_8bit_uninterlaced.java
+++ test/micro/org/openjdk/bench/sun/awt/image/PNGImageDecoder_8bit_uninterlaced.java
@@ -23,11 +23,13 @@
 package org.openjdk.bench.sun.awt.image;
 
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
