2bafe7861a4844c5d85e5963b931cb81c43919ba
Weijun Wang <weijun.wang@oracle.com>
2026-01-05 17:10:49 -0500

    the fix

--- src/java.base/share/classes/sun/security/util/Password.java
+++ src/java.base/share/classes/sun/security/util/Password.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -65,7 +65,7 @@ public class Password {
                     }
                     consoleBytes = ConsoleHolder.convertToBytes(consoleEntered);
                     in = new ByteArrayInputStream(consoleBytes);
-                } else if (System.in.available() == 0) {
+                } else if (in == System.in && System.in.available() == 0) {
                     // This may be running in an IDE Run Window or in JShell,
                     // which acts like an interactive console and echoes the
                     // entered password. In this case, print a warning that
