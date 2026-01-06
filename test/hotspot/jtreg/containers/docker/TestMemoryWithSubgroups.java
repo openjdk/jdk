b1b2a482dd53a141d59d243af84142eda0a205f1
SendaoYan <yansendao@hygon.cn>
2026-01-06 17:11:24 +0800

    Revert the copyright change

--- test/hotspot/jtreg/containers/docker/TestMemoryWithSubgroups.java
+++ test/hotspot/jtreg/containers/docker/TestMemoryWithSubgroups.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2025, 2026, BELLSOFT. All rights reserved.
+ * Copyright (C) 2025, BELLSOFT. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
