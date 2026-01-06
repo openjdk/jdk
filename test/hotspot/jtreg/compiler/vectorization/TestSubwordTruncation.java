50bc132676e5a1276bdf2c236ae57873375a773d
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2026-01-06 18:48:00 -0500

    Use Xcomp test run instead of Warmup(0)

--- test/hotspot/jtreg/compiler/vectorization/TestSubwordTruncation.java
+++ test/hotspot/jtreg/compiler/vectorization/TestSubwordTruncation.java
@@ -481,7 +481,6 @@ public class TestSubwordTruncation {
 
     @Test
     @IR(counts = { IRNode.CAST_II, ">0" })
-    @Warmup(0)
     public Object[] testCastII() {
         byte[] bytes = new byte[400];
         intField = 6;
@@ -507,6 +506,7 @@ public class TestSubwordTruncation {
 
     public static void main(String[] args) {
         TestFramework.run();
+        TestFramework.runWithFlags("-Xcomp", "-XX:CompileCommand=compileonly,*TestSubwordTruncation::*");
     }
 }
 
