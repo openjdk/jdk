3f942c611509c66a0b665e46d3bf4325d968c83a
MBaesken <matthias.baesken@sap.com>
2026-01-08 14:43:18 +0100

    Add SAP copyright

--- test/jdk/build/CheckFiles.java
+++ test/jdk/build/CheckFiles.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 SAP SE. All rights reserved.
  * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
