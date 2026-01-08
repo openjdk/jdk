bcdec61f2422babf859d744126aecff30c7b2f26
Joe Darcy <joe.darcy@oracle.com>
2026-01-07 17:52:42 -0800

    JDK-8374752: Add more JLS links to javax.lang.model.element.*

--- src/java.compiler/share/classes/javax/lang/model/element/Parameterizable.java
+++ src/java.compiler/share/classes/javax/lang/model/element/Parameterizable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -30,6 +30,8 @@ import java.util.List;
 /**
  * A mixin interface for an element that has type parameters.
  *
+ * @jls 4.5 Parameterized Types
+
  * @since 1.7
  */
 public interface Parameterizable extends Element {
