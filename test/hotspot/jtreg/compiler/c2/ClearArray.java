4b526d8eba387ebbe7de5ecaf4c48c0f28238853
Volodymyr Paprotski <volodymyr.paprotski@intel.com>
2026-01-09 16:42:43 +0000

    testcase change

--- test/hotspot/jtreg/compiler/c2/ClearArray.java
+++ test/hotspot/jtreg/compiler/c2/ClearArray.java
@@ -24,7 +24,7 @@
 
 /*
  * @test ClearArray.java
- * @bug 8284883
+ * @bug 8284883 8374570
  * @compile ClearArray.java
  * @summary ClearArray instruction overflows scratch buffer
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:-TieredCompilation -Xbatch
@@ -33,8 +33,6 @@
  *   -XX:InitArrayShortSize=32768 -XX:-IdealizeClearArrayNode -XX:UseAVX=3 compiler.c2.ClearArray
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation -Xbatch
  *   -XX:InitArrayShortSize=32768 -XX:MaxVectorSize=8 -XX:-IdealizeClearArrayNode -XX:UseAVX=3 compiler.c2.ClearArray
- *
- * @bug 8374570
  * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:-TieredCompilation -Xbatch
  *   -XX:+EnableX86ECoreOpts -XX:MaxVectorSize=8 -XX:UseAVX=3 compiler.c2.ClearArray
  */
