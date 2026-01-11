9b8ed440ba9ee5fdb5c1660f45f4d9263bd626ba
mgronlun <markus.gronlund@oracle.com>
2026-01-11 13:17:44 +0100

    8373485

--- src/hotspot/share/jfr/periodic/sampling/jfrThreadSampler.cpp
+++ src/hotspot/share/jfr/periodic/sampling/jfrThreadSampler.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -351,15 +351,22 @@ bool JfrSamplerThread::sample_native_thread(JavaThread* jt) {
   //    outside the safepoint protocol.
 
   // OrderAccess::fence() as part of acquiring the lock prevents loads from floating up.
-  JfrMutexTryLock threads_lock(Threads_lock);
+  JfrMutexTryLock lock(Threads_lock);
 
-  if (!threads_lock.acquired() || !jt->has_last_Java_frame()) {
+  if (!lock.acquired()) {
     // Remove the native sample request and release the potentially waiting thread.
     JfrSampleMonitor jsm(tl);
     return false;
   }
 
-  if (jt->thread_state() != _thread_in_native) {
+  // If UseSystemMemoryBarrier, the OrderAccess::fence(), part of the try_lock attempt for the
+  // Threads_lock above, is inadequate to prevent the next load of the thread state from floating up.
+  // To prevent this, an explicit system memory barrier is needed.
+  if (UseSystemMemoryBarrier) {
+    SystemMemoryBarrier::emit();
+  }
+
+  if (jt->thread_state() != _thread_in_native || !jt->has_last_Java_frame()) {
     assert_lock_strong(Threads_lock);
     JfrSampleMonitor jsm(tl);
     if (jsm.is_waiting()) {
