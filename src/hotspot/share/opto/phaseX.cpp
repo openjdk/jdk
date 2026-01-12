438aeff326b1f055893c8b0206cd4ee5fdf1057c
Galder Zamarreño <galder@openjdk.org>
2026-01-12 09:57:30 +0100

    Revert "Remove exclude or Min/Max in verify identity"
    This reverts commit cf24abad55db9a320930379c4f0f3154791d26e2.

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -1971,6 +1971,27 @@ bool PhaseIterGVN::verify_Identity_for(Node* n) {
     case Op_ConvI2L:
       return false;
 
+    // MaxNode::find_identity_operation
+    //  Finds patterns like Max(A, Max(A, B)) -> Max(A, B)
+    //  This can be a 2-hop search, so maybe notification is not
+    //  good enough.
+    //
+    // Found with:
+    //   compiler/codegen/TestBooleanVect.java
+    //   -XX:VerifyIterativeGVN=1110
+    case Op_MaxL:
+    case Op_MinL:
+    case Op_MaxI:
+    case Op_MinI:
+    case Op_MaxF:
+    case Op_MinF:
+    case Op_MaxHF:
+    case Op_MinHF:
+    case Op_MaxD:
+    case Op_MinD:
+      return false;
+
+
     // AddINode::Identity
     // Converts (x-y)+y to x
     // Could be issue with notification
