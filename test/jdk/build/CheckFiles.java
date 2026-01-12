c7b8a193562ff7c9a75bc495a80a848d3735d847
MBaesken <matthias.baesken@sap.com>
2026-01-12 13:30:56 +0100

    JDK-8374990

--- test/jdk/build/CheckFiles.java
+++ test/jdk/build/CheckFiles.java
@@ -60,6 +60,7 @@ public class CheckFiles {
         System.out.println("Main directory to scan:" + mainDirToScan);
         Path binDir = mainDirToScan.resolve("bin");
         Path libDir = mainDirToScan.resolve("lib");
+        Path includeDir = mainDirToScan.resolve("include");
 
         System.out.println("Bin directory to scan:" + binDir);
         ArrayList<String> allowedEndingsBinDir = new ArrayList<>();
@@ -108,12 +109,29 @@ public class CheckFiles {
         }
         boolean libDirRes = scanFiles(libDir, allowedEndingsLibDir);
 
-        if (!binDirRes) {
+        if (binDirRes) {
+            System.out.println("Bin directory scan successful.");
+        } else {
             throw new Error("bin dir scan failed");
         }
 
-        if (!libDirRes) {
+        if (libDirRes) {
+            System.out.println("Lib directory scan successful.");
+        } else {
             throw new Error("lib dir scan failed");
+        }
+
+        if (Files.isDirectory(includeDir)) {
+            System.out.println("Include directory to scan:" + includeDir);
+            ArrayList<String> allowedEndingsIncludeDir = new ArrayList<>();
+            allowedEndingsIncludeDir.add(".h");
+            allowedEndingsIncludeDir.add(".hpp");
+            boolean includeDirRes = scanFiles(includeDir, allowedEndingsIncludeDir);
+            if (includeDirRes) {
+                System.out.println("Include directory scan successful.");
+            } else {
+                throw new Error("include dir scan failed");
+            }
         }
     }
 
