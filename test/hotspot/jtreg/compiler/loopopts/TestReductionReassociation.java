9eca8eb8ae16286008633744e9cd7abc236a4d62
Galder Zamarreño <galder@openjdk.org>
2026-01-12 18:45:25 +0100

    Expand to Float16

--- test/hotspot/jtreg/compiler/loopopts/TestReductionReassociation.java
+++ test/hotspot/jtreg/compiler/loopopts/TestReductionReassociation.java
@@ -31,24 +31,20 @@
 
 package compiler.loopopts;
 
-import compiler.igvn.TestMinMaxIdeal;
 import compiler.lib.compile_framework.CompileFramework;
-import compiler.lib.generators.Generator;
-import compiler.lib.ir_framework.*;
 import compiler.lib.template_framework.Template;
 import compiler.lib.template_framework.TemplateToken;
 import compiler.lib.template_framework.library.CodeGenerationDataNameType;
 import compiler.lib.template_framework.library.PrimitiveType;
 import compiler.lib.template_framework.library.TestFrameworkClass;
-import compiler.lib.verify.Verify;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
-import static compiler.lib.generators.Generators.G;
 import static compiler.lib.template_framework.Template.*;
 import static compiler.lib.template_framework.Template.let;
 
@@ -61,9 +57,12 @@ public class TestReductionReassociation {
         comp.addJavaSourceCode("compiler.loopopts.templated.ReductionReassociation", generate(comp));
 
         // Compile the source file.
-        comp.compile();
+        comp.compile("--add-modules=jdk.incubator.vector");
 
-        String[] flags = new String[] {"-XX:-UseSuperWord", "-XX:LoopMaxUnroll=0", "-XX:VerifyIterativeGVN=1000"};
+        String[] flags = new String[] {
+            "-XX:-UseSuperWord", "-XX:LoopMaxUnroll=0", "-XX:VerifyIterativeGVN=1000",
+            "--add-modules=jdk.incubator.vector", "--add-opens", "jdk.incubator.vector/jdk.incubator.vector=ALL-UNNAMED"
+        };
         comp.invoke("compiler.loopopts.templated.ReductionReassociation", "main", new Object[] {flags});
     }
 
@@ -87,7 +86,8 @@ public class TestReductionReassociation {
             // package and class name.
             "compiler.loopopts.templated", "ReductionReassociation",
             // List of imports.
-            Set.of("compiler.lib.generators.*",
+            Set.of("jdk.incubator.vector.Float16",
+                "compiler.lib.generators.*",
                 "compiler.lib.verify.*"),
             // classpath, so the Test VM has access to the compiled class files.
             comp.getEscapedClassPathOfCompiledClasses(),
@@ -98,6 +98,8 @@ public class TestReductionReassociation {
     enum AddOp {
         MIN_D(CodeGenerationDataNameType.doubles()),
         MAX_D(CodeGenerationDataNameType.doubles()),
+        MIN_HF(CodeGenerationDataNameType.float16()),
+        MAX_HF(CodeGenerationDataNameType.float16()),
         MIN_F(CodeGenerationDataNameType.floats()),
         MAX_F(CodeGenerationDataNameType.floats()),
         MIN_I(CodeGenerationDataNameType.ints()),
@@ -105,10 +107,14 @@ public class TestReductionReassociation {
         MIN_L(CodeGenerationDataNameType.longs()),
         MAX_L(CodeGenerationDataNameType.longs());
 
-        final PrimitiveType type;
+        final CodeGenerationDataNameType type;
 
-        AddOp(PrimitiveType type) {
+        AddOp(CodeGenerationDataNameType type) {
             this.type = type;
+        }
+
+        boolean isFloat16() {
+            return MIN_HF == this || MAX_HF == this;
         }
     }
 
@@ -163,6 +169,8 @@ public class TestReductionReassociation {
                 switch (add) {
                     case MIN_D -> "Double.min(#a, #b)";
                     case MAX_D -> "Double.max(#a, #b)";
+                    case MIN_HF -> "Float16.min(#a, #b)";
+                    case MAX_HF -> "Float16.max(#a, #b)";
                     case MIN_F -> "Float.min(#a, #b)";
                     case MAX_F -> "Float.max(#a, #b)";
                     case MIN_I -> "Integer.min(#a, #b)";
@@ -177,16 +185,24 @@ public class TestReductionReassociation {
         private TemplateToken generateResultInit(String resultName) {
             var template = Template.make(() -> scope(
                 let("resultName", resultName),
-                let("boxedType", add.type.boxedTypeName()),
+                let("boxedType", getBoxedTypeName()),
                 let("type", add.type.name()),
                 "#type ", resultName, " = #boxedType.",
                 switch (add) {
-                    case MIN_D, MIN_F, MIN_I, MIN_L -> "MAX_VALUE";
-                    case MAX_D, MAX_F, MAX_I, MAX_L -> "MIN_VALUE";
+                    case MIN_HF, MIN_D, MIN_F, MIN_I, MIN_L -> "MAX_VALUE";
+                    case MAX_HF, MAX_D, MAX_F, MAX_I, MAX_L -> "MIN_VALUE";
                 },
                 ";\n"
             ));
             return template.asToken();
+        }
+
+        private Object getBoxedTypeName() {
+            if (add.type instanceof PrimitiveType primitiveType) {
+                return primitiveType.boxedTypeName();
+            }
+
+            return add.type.name();
         }
 
         private TemplateToken generateTest(String input, String setup, String test) {
@@ -249,11 +265,28 @@ public class TestReductionReassociation {
                 let("size", size),
                 let("input", input),
                 let("type", add.type.name()),
-                let("gen", add.type.name() + "s"),
+                let("gen", add.type.name().toLowerCase(Locale.ROOT) + "s"),
                 """
                 private static #type[] #input = new #type[#size];
+                """,
+                add.isFloat16() ?
+                    """
+                    private static final Generator<Short> GEN_#input = Generators.G.float16s();
+
+                    static void fill_#input(Float16[] a) {
+                        for (int i = 0; i < a.length; i++) {
+                            a[i] = Float16.shortBitsToFloat16(GEN_#input.next());
+                        }
+                    }
+                    """
+                    : "",
+                """
                 static {
-                    Generators.G.fill(Generators.G.#gen(), #input);
+                """,
+                add.isFloat16() ?
+                    "fill_#input(#input);"
+                    : "Generators.G.fill(Generators.G.#gen(), #input);",
+                """
                 }
                 """
             ));
