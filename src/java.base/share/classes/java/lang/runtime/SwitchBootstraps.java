7b690f5899bbe7e22b0157a16a048f270539bba2
Jan Lahoda <jan.lahoda@oracle.com>
2026-01-13 08:56:00 +0100

    8375119: SwitchBoostraps.enumSwitch does not throw an NPE when lookup is null in some cases

--- src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
+++ src/java.base/share/classes/java/lang/runtime/SwitchBootstraps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -188,6 +188,9 @@ public final class SwitchBootstraps {
                                       String invocationName,
                                       MethodType invocationType,
                                       Object... labels) {
+        requireNonNull(lookup);
+        requireNonNull(invocationType);
+
         Class<?> selectorType = invocationType.parameterType(0);
         if (invocationType.parameterCount() != 2
             || (!invocationType.returnType().equals(int.class))
@@ -292,6 +295,9 @@ public final class SwitchBootstraps {
                                       String invocationName,
                                       MethodType invocationType,
                                       Object... labels) {
+        requireNonNull(lookup);
+        requireNonNull(invocationType);
+
         if (invocationType.parameterCount() != 2
             || (!invocationType.returnType().equals(int.class))
             || invocationType.parameterType(0).isPrimitive()
