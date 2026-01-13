c89cab2011f7098aa34953a6fc5d96a057ad7849
Alan Bateman <alan.bateman@oracle.com>
2026-01-13 20:30:41 +0000

    Change setParallelism to signalWork

--- src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
+++ src/java.base/share/classes/java/util/concurrent/ForkJoinPool.java
@@ -3310,11 +3310,14 @@ public class ForkJoinPool extends AbstractExecutorService
      * @since 19
      */
     public int setParallelism(int size) {
+        int prevSize;
         if (size < 1 || size > MAX_CAP)
             throw new IllegalArgumentException();
         if ((config & PRESET_SIZE) != 0)
             throw new UnsupportedOperationException("Cannot override System property");
-        return getAndSetParallelism(size);
+        if ((prevSize = getAndSetParallelism(size)) < size)
+            signalWork(); // trigger worker activation
+        return prevSize;
     }
 
     /**
