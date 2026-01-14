d8c3b0aa2dcba3a6e211f8f7e6b8150f562d7323
alexander_matveev <alexander.matveev@oracle.com>
2026-01-13 20:42:47 -0800

    8374215: [macos] Clean "lic_template.plist" [v2]

--- src/jdk.jpackage/macosx/classes/jdk/jpackage/internal/MacDmgPackager.java
+++ src/jdk.jpackage/macosx/classes/jdk/jpackage/internal/MacDmgPackager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -27,16 +27,20 @@ package jdk.jpackage.internal;
 
 import static jdk.jpackage.internal.util.PathUtils.normalizedAbsolutePathString;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.function.Consumer;
 import jdk.jpackage.internal.PackagingPipeline.PackageTaskID;
 import jdk.jpackage.internal.PackagingPipeline.TaskID;
@@ -163,6 +167,41 @@ record MacDmgPackager(BuildEnv env, MacDmgPackage pkg, Path outputDir,
                 .saveToFile(dmgSetup);
     }
 
+    private void writeSTRDataString(ByteArrayOutputStream bos,
+                String str, String charset) {
+        byte [] bytes = str.getBytes(Charset.forName(charset));
+        byte [] bytesLength = {(byte)bytes.length};
+        bos.writeBytes(bytesLength);
+        bos.writeBytes(bytes);
+    }
+
+    // Returns base64 decoded STR section data.
+    // Strings should be in following order:
+    // Language, message.dmg.license.button.agree,
+    // message.dmg.license.button.disagree, message.dmg.license.button.print
+    // message.dmg.license.button.save, message.dmg.license.message
+    // STR section data encoded:
+    // Number of strings in the list (unsigned 16-bit integer, big endian): 6
+    // A sequence of strings prefixed with string length (unsigned 8-bit integer)
+    // Note: Language should not be translated.
+    private String getSTRData(String language, Locale locale, String charset) {
+        ResourceBundle bundle = ResourceBundle.getBundle(
+                "jdk.jpackage.internal.resources.MacResources", locale);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        byte [] numberOfStrings = {0x00, 0x06}; // Always 6
+        bos.writeBytes(numberOfStrings);
+
+        writeSTRDataString(bos, language, charset);
+        writeSTRDataString(bos, bundle.getString("message.dmg.license.button.agree"), charset);
+        writeSTRDataString(bos, bundle.getString("message.dmg.license.button.disagree"), charset);
+        writeSTRDataString(bos, bundle.getString("message.dmg.license.button.print"), charset);
+        writeSTRDataString(bos, bundle.getString("message.dmg.license.button.save"), charset);
+        writeSTRDataString(bos, bundle.getString("message.dmg.license.message"), charset);
+
+        return Base64.getEncoder().encodeToString(bos.toByteArray());
+    }
+
     private void prepareLicense() throws IOException {
         final var licFile = pkg.licenseFile();
         if (licFile.isEmpty()) {
@@ -176,6 +215,14 @@ record MacDmgPackager(BuildEnv env, MacDmgPackage pkg, Path outputDir,
 
         Map<String, String> data = new HashMap<>();
         data.put("APPLICATION_LICENSE_TEXT", licenseInBase64);
+        data.put("STR_DATA_ENGLISH",
+                getSTRData("English", Locale.ENGLISH, "MacRoman"));
+        data.put("STR_DATA_GERMAN",
+                getSTRData("German", Locale.GERMAN, "MacRoman"));
+        data.put("STR_DATA_JAPANESE",
+                getSTRData("Japanese", Locale.JAPANESE, "Shift_JIS"));
+        data.put("STR_DATA_SIMPLIFIED_CHINESE",
+                getSTRData("Simplified Chinese", Locale.SIMPLIFIED_CHINESE, "GB2312"));
 
         env.createResource(DEFAULT_LICENSE_PLIST)
                 .setCategory(I18N.getString("resource.license-setup"))
