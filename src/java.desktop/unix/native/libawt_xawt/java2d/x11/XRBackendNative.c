1380681c45e6f4d9770f8756ed88d84eb97b95ee
SendaoYan <yansendao@hygon.cn>
2026-01-15 15:27:52 +0800

    8375370: XRBackendNative.c reported variable uninitialized by clang23

--- src/java.desktop/unix/native/libawt_xawt/java2d/x11/XRBackendNative.c
+++ src/java.desktop/unix/native/libawt_xawt/java2d/x11/XRBackendNative.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2020, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -340,6 +340,7 @@ JNIEXPORT jint JNICALL
 Java_sun_java2d_xr_XRBackendNative_createPictureNative
  (JNIEnv *env, jclass cls, jint drawable, jlong formatPtr) {
   XRenderPictureAttributes pict_attr;
+  pict_attr.repeat = RepeatNone;
   return XRenderCreatePicture(awt_display, (Drawable) drawable,
                               (XRenderPictFormat *) jlong_to_ptr(formatPtr),
                                0, &pict_attr);
