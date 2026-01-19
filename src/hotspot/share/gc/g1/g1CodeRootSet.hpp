b76b4df86a669cbcab5e0fe28331779255b55d43
Thomas Schatzl <thomas.schatzl@oracle.com>
2026-01-19 11:46:29 +0100

    8375622
    Hi all,
    
      please review this change to convert `G1CodeRootSet` to use `Atomic<T>` instead of `AtomicAccess`.
    
    Testing: gha,
    
    Thanks,
      Thomas

--- src/hotspot/share/gc/g1/g1CodeRootSet.hpp
+++ src/hotspot/share/gc/g1/g1CodeRootSet.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2014, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -26,6 +26,7 @@
 #define SHARE_GC_G1_G1CODEROOTSET_HPP
 
 #include "code/codeCache.hpp"
+#include "runtime/atomic.hpp"
 #include "utilities/globalDefinitions.hpp"
 
 class G1CodeRootSetHashTable;
