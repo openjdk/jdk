e539a64f46300c01a03cca5438373022aabcc535
Severin Gehwolf <sgehwolf@redhat.com>
2026-01-21 10:58:08 +0100

    Restore Bellsoft copyright

--- test/jdk/jdk/internal/platform/docker/TestDockerMemoryMetricsSubgroup.java
+++ test/jdk/jdk/internal/platform/docker/TestDockerMemoryMetricsSubgroup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, BELLSOFT. All rights reserved.
+ * Copyright (c) 2025, BELLSOFT. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
