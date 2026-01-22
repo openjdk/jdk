9e095e65a9ee5aa49457b33c54fd1eae9c671954
prrace <philip.race@oracle.com>
2026-01-22 12:56:24 -0800

    8375338

--- test/jdk/sun/awt/image/ImageRepresentation/LUTCompareTest.java
+++ test/jdk/sun/awt/image/ImageRepresentation/LUTCompareTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2007, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -28,7 +28,8 @@
  *          ImageRepresentation class does not produce extra transparent
  *          pixels.
  *
- * @run     main LUTCompareTest
+ * @run     main/othervm LUTCompareTest
+ * @run     main/othervm -Xcheck:jni LUTCompareTest
  */
 
 
