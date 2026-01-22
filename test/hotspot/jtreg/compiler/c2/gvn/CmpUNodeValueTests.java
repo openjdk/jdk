625bed1ccf98fe653aacd7c1f0913d3b28bcd1a4
Quan Anh Mai <anhmdq99@gmail.com>
2026-01-22 21:56:49 +0700

    Improve IR tests

--- test/hotspot/jtreg/compiler/c2/gvn/CmpUNodeValueTests.java
+++ test/hotspot/jtreg/compiler/c2/gvn/CmpUNodeValueTests.java
@@ -22,11 +22,9 @@
  */
 package compiler.c2.gvn;
 
+import compiler.lib.generators.Generators;
 import compiler.lib.ir_framework.*;
 import jdk.test.lib.Asserts;
-import jdk.test.lib.Utils;
-
-import java.util.Random;
 
 /*
  * @test
@@ -51,102 +49,161 @@ public class CmpUNodeValueTests {
         return 1;
     }
 
-    @Run(test = {"testInt1", "testInt2", "testInt3", "testInt4",
-                 "testLong1", "testLong2", "testLong3", "testLong4", "testLong5"})
+    @Run(test = {"testIntControl", "testIntLT", "testIntLE", "testIntGT", "testIntGE", "testIntEQ", "testIntNE",
+                 "testLongControl", "testLongLT", "testLongLE", "testLongGT", "testLongGE", "testLongEQ", "testLongNE"})
     public void run() {
-        Random r = Utils.getRandomInstance();
-        long x = r.nextLong();
-        long y = r.nextLong();
+        var stream = Generators.G.longs();
+        long x = stream.next();
+        long y = stream.next();
 
-        Asserts.assertEQ(0, testInt1(false, false));
-        Asserts.assertEQ(0, testInt1(false, true));
-        Asserts.assertEQ(0, testInt1(true, false));
-        Asserts.assertEQ(0, testInt1(true, true));
-        Asserts.assertEQ(0, testInt2(false, false));
-        Asserts.assertEQ(0, testInt2(false, true));
-        Asserts.assertEQ(0, testInt2(true, false));
-        Asserts.assertEQ(0, testInt2(true, true));
-        Asserts.assertEQ(0, testInt3((int) x, (int) y));
-        Asserts.assertEQ(0, testInt4());
-        Asserts.assertEQ(0, testLong1(false, false));
-        Asserts.assertEQ(0, testLong1(false, true));
-        Asserts.assertEQ(0, testLong1(true, false));
-        Asserts.assertEQ(0, testLong1(true, true));
-        Asserts.assertEQ(0, testLong2(false, false));
-        Asserts.assertEQ(0, testLong2(false, true));
-        Asserts.assertEQ(0, testLong2(true, false));
-        Asserts.assertEQ(0, testLong2(true, true));
-        Asserts.assertEQ(0, testLong3(x, y));
-        Asserts.assertEQ(0, testLong4());
-        Asserts.assertEQ(0, testLong5((int) x, false));
-        Asserts.assertEQ(0, testLong5((int) x, true));
+        Asserts.assertEQ(0, testIntControl(false, false));
+        Asserts.assertEQ(0, testIntControl(false, true));
+        Asserts.assertEQ(0, testIntControl(true, false));
+        Asserts.assertEQ(1, testIntControl(true, true));
+        Asserts.assertEQ(0, testIntLT((int) x));
+        Asserts.assertEQ(0, testIntLE(false, false));
+        Asserts.assertEQ(0, testIntLE(false, true));
+        Asserts.assertEQ(0, testIntLE(true, false));
+        Asserts.assertEQ(0, testIntLE(true, true));
+        Asserts.assertEQ(0, testIntGT(false, false));
+        Asserts.assertEQ(0, testIntGT(false, true));
+        Asserts.assertEQ(0, testIntGT(true, false));
+        Asserts.assertEQ(0, testIntGT(true, true));
+        Asserts.assertEQ(0, testIntGE((int) x, (int) y));
+        Asserts.assertEQ(0, testIntEQ());
+        Asserts.assertEQ(0, testIntNE(false, false));
+        Asserts.assertEQ(0, testIntNE(false, true));
+        Asserts.assertEQ(0, testIntNE(true, false));
+        Asserts.assertEQ(0, testIntNE(true, true));
+
+        Asserts.assertEQ(0, testLongControl(false, false));
+        Asserts.assertEQ(0, testLongControl(false, true));
+        Asserts.assertEQ(0, testLongControl(true, false));
+        Asserts.assertEQ(1, testLongControl(true, true));
+        Asserts.assertEQ(0, testLongLT((int) x, false));
+        Asserts.assertEQ(0, testLongLT((int) x, true));
+        Asserts.assertEQ(0, testLongLE(false, false));
+        Asserts.assertEQ(0, testLongLE(false, true));
+        Asserts.assertEQ(0, testLongLE(true, false));
+        Asserts.assertEQ(0, testLongLE(true, true));
+        Asserts.assertEQ(0, testLongGT(false, false));
+        Asserts.assertEQ(0, testLongGT(false, true));
+        Asserts.assertEQ(0, testLongGT(true, false));
+        Asserts.assertEQ(0, testLongGT(true, true));
+        Asserts.assertEQ(0, testLongGE((int) x, (int) y));
+        Asserts.assertEQ(0, testLongEQ());
+        Asserts.assertEQ(0, testLongNE(false, false));
+        Asserts.assertEQ(0, testLongNE(false, true));
+        Asserts.assertEQ(0, testLongNE(true, false));
+        Asserts.assertEQ(0, testLongNE(true, true));
+    }
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, counts = {IRNode.CMP_U, "1"})
+    int testIntControl(boolean b1, boolean b2) {
+        int v1 = b1 ? 1 : -1;
+        int v2 = b2 ? 1 : 0;
+        return Integer.compareUnsigned(v1, v2) > 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
-    int testInt1(boolean b1, boolean b2) {
-        int v1 = b1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
-        int v2 = b2 ? 0 : 2;
-        return Integer.compareUnsigned(v1, v2) <= 0 ? one() : 0;
+    int testIntLT(int x) {
+        int v1 = x & Integer.MAX_VALUE; // Unset the highest bit, make v1 non-negative
+        int v2 = Integer.MIN_VALUE;
+        return Integer.compareUnsigned(v1, v2) < 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
-    int testInt2(boolean b1, boolean b2) {
+    int testIntLE(boolean b1, boolean b2) {
         int v1 = b1 ? 2 : 0;
         int v2 = b2 ? -1 : 2;
-        return Integer.compareUnsigned(v1, v2) > 0 ? one() : 0;
+        return Integer.compareUnsigned(v1, v2) <= 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
-    int testInt3(int x, int y) {
+    int testIntGT(boolean b1, boolean b2) {
+        int v1 = b1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        int v2 = b2 ? 0 : 2;
+        return Integer.compareUnsigned(v1, v2) > 0 ? 0 : one();
+    }
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
+    int testIntGE(int x, int y) {
         int v1 = x | Integer.MIN_VALUE; // Set the highest bit, make v1 negative
         int v2 = y & Integer.MAX_VALUE; // Unset the highest bit, make v2 non-negative
-        return Integer.compareUnsigned(v1, v2) <= 0 ? one() : 0;
+        return Integer.compareUnsigned(v1, v2) >= 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
-    int testInt4() {
-        return Integer.compareUnsigned(oneInline(), 1) != 0 ? one() : 0;
+    int testIntEQ() {
+        return Integer.compareUnsigned(oneInline(), 1) == 0 ? 0 : one();
+    }
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_U, IRNode.CALL})
+    int testIntNE(boolean b1, boolean b2) {
+        int v1 = b1 ? 1 : -1;
+        int v2 = b2 ? 0 : 2;
+        return Integer.compareUnsigned(v1, v2) != 0 ? 0 : one();
+    }
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, counts = {IRNode.CMP_UL, "1"})
+    int testLongControl(boolean b1, boolean b2) {
+        long v1 = b1 ? 1 : -1;
+        long v2 = b2 ? 1 : 0;
+        return Long.compareUnsigned(v1, v2) > 0 ? 0 : one();
+    }
+
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
+    int testLongLT(int x, boolean b2) {
+        long v1 = Integer.toUnsignedLong(x);
+        long v2 = Integer.MIN_VALUE;
+        return Long.compareUnsigned(v1, v2) < 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
-    int testLong1(boolean b1, boolean b2) {
-        long v1 = b1 ? Long.MIN_VALUE : Long.MAX_VALUE;
-        long v2 = b2 ? 0 : 2;
-        return Long.compareUnsigned(v1, v2) <= 0 ? one() : 0;
-    }
-
-    @Test
-    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
-    int testLong2(boolean b1, boolean b2) {
+    int testLongLE(boolean b1, boolean b2) {
         long v1 = b1 ? 2 : 0;
         long v2 = b2 ? -1 : 2;
-        return Long.compareUnsigned(v1, v2) > 0 ? one() : 0;
+        return Long.compareUnsigned(v1, v2) <= 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
-    int testLong3(long x, long y) {
+    int testLongGT(boolean b1, boolean b2) {
+        long v1 = b1 ? Long.MIN_VALUE : Long.MAX_VALUE;
+        long v2 = b2 ? 0 : 2;
+        return Long.compareUnsigned(v1, v2) > 0 ? 0 : one();
+    }
+
+    @Test
+    @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
+    int testLongGE(int x, int y) {
         long v1 = x | Long.MIN_VALUE; // Set the highest bit, make v1 negative
         long v2 = y & Long.MAX_VALUE; // Unset the highest bit, make v2 non-negative
-        return Long.compareUnsigned(v1, v2) <= 0 ? one() : 0;
+        return Long.compareUnsigned(v1, v2) >= 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
-    int testLong4() {
-        return Long.compareUnsigned(oneInline(), 1L) != 0 ? one() : 0;
+    int testLongEQ() {
+        return Long.compareUnsigned(oneInline(), 1L) == 0 ? 0 : one();
     }
 
     @Test
     @IR(applyIfPlatformOr = {"x64", "true", "aarch64", "true"}, failOn = {IRNode.CMP_UL, IRNode.CALL})
-    int testLong5(int x, boolean b2) {
-        long v1 = Integer.toUnsignedLong(x);
-        long v2 = b2 ? Integer.toUnsignedLong(-1) : -1L;
-        return Long.compareUnsigned(v1, v2) > 0 ? one() : 0;
+    int testLongNE(boolean b1, boolean b2) {
+        long v1 = b1 ? 1 : -1;
+        long v2 = b2 ? 0 : 2;
+        return Long.compareUnsigned(v1, v2) != 0 ? 0 : one();
     }
 }
