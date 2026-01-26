838ed588c7aaaee73c9c38c3575abd10f0b52296
Adam Farley <adfarley@ibm.com>
2026-01-26 14:26:23 +0000

    Removing superfluous trailing whitespace
    Signed-off-by: Adam Farley <adfarley@ibm.com>

--- test/jdk/tools/jpackage/windows/Win8365790Test.java
+++ test/jdk/tools/jpackage/windows/Win8365790Test.java
@@ -44,7 +44,7 @@ import jdk.jpackage.test.TKit;
  * @summary Test case for JDK-8365790
  * @library /test/jdk/tools/jpackage/helpers
  * @library /test/lib
- * @build jtreg.SkippedException 
+ * @build jtreg.SkippedException
  * @build jdk.jpackage.test.*
  * @build Win8365790Test
  * @requires (os.family == "windows")
