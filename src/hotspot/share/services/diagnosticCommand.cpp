369b8c0297db3da9c1eec1cc1df110a4bd47460e
Larry Cable <larry.cable@yahoo.com>
2026-01-28 13:09:53 -0800

    JDK-8327246: updated VM.classes 'help' description

--- src/hotspot/share/services/diagnosticCommand.cpp
+++ src/hotspot/share/services/diagnosticCommand.cpp
@@ -952,13 +952,14 @@ ClassesDCmd::ClassesDCmd(outputStream* output, bool heap) :
                                      DCmdWithParser(output, heap),
   _verbose("-verbose",
            "Dump the detailed content of a Java class.\n"
-           "\t\tclasses are annotated with flags:\n"
-           "\t\tF = has, or inherits, a non-empty finalize method,\n"
-           "\t\tf = has final method,\n"
-           "\t\tW = methods rewritten,\n"
-           "\t\tC = marked with @Contended annotation,\n"
-           "\t\tR = has been redefined,\n"
-           "\t\tS = is an (App)CDS shared class (if -location is also specified, (either) 's' indicating static (or) 'd' indicating dynamic AOT cache locations, are appended)",
+           "\t\t   classes are annotated with flags:\n"
+           "\t\t   F = has, or inherits, a non-empty finalize method,\n"
+           "\t\t   f = has final method,\n"
+           "\t\t   W = methods rewritten,\n"
+           "\t\t   C = marked with @Contended annotation,\n"
+           "\t\t   R = has been redefined,\n"
+           "\t\t   S = is an (App)CDS shared class,\n"
+           "\t\t       (if -location is also specified, (either) 's' indicating static (or) 'd' indicating dynamic AOT cache locations, are appended)\n\t",
            "BOOLEAN", false, "false"),
   _location("-location", "Print class file location url (if available)", "BOOLEAN", false, "false") {
   _dcmdparser.add_dcmd_option(&_verbose);
