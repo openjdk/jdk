3e417ec04a56240d2ddcc79acbac7ca14cf436e1
SendaoYan <yansendao@hygon.cn>
2026-01-29 20:57:33 +0800

    8376700: java/nio/file/DirectoryStream/SecureDS.java fails AtomicMoveNotSupportedException

--- test/jdk/java/nio/file/DirectoryStream/SecureDS.java
+++ test/jdk/java/nio/file/DirectoryStream/SecureDS.java
@@ -21,7 +21,7 @@
  * questions.
  */
 
-/* @test
+/* @test id=tmp
  * @bug 4313887 6838333 8343020 8357425
  * @summary Unit test for java.nio.file.SecureDirectoryStream
  * @requires (os.family == "linux" | os.family == "mac" | os.family == "aix")
@@ -30,22 +30,34 @@
  * @run main SecureDS
  */
 
+/* @test id=cwd
+ * @requires (os.family == "linux" | os.family == "mac" | os.family == "aix")
+ * @library .. /test/lib
+ * @build jdk.test.lib.Platform
+ * @run main SecureDS cwd
+ */
+
 import java.nio.file.*;
 import static java.nio.file.Files.*;
 import static java.nio.file.StandardOpenOption.*;
 import static java.nio.file.LinkOption.*;
 import java.nio.file.attribute.*;
-import java.nio.channels.*;
 import java.io.IOException;
 import java.util.*;
 
 import jdk.test.lib.Platform;
+import jtreg.SkippedException;
 
 public class SecureDS {
     static boolean supportsSymbolicLinks;
 
     public static void main(String[] args) throws IOException {
-        Path dir = TestUtil.createTemporaryDirectory();
+        Path dir;
+        if (args.length > 0 && args[0].equals("cwd")) {
+            dir = TestUtil.createTemporaryDirectory(System.getProperty("user.dir"));
+        } else {
+            dir = TestUtil.createTemporaryDirectory();
+        }
         try {
             DirectoryStream<Path> stream = newDirectoryStream(dir);
             stream.close();
@@ -294,14 +306,18 @@ public class SecureDS {
 
         // Test: move to cwd
         final String TEXT = "Sous le pont Mirabeau coule la Seine";
-        Path file = Path.of("file");
+        Path file = Path.of("file-temp");
         Path filepath = dir.resolve(file);
         Path cwd = Path.of(System.getProperty("user.dir"));
         Path result = cwd.resolve(file);
         Files.writeString(filepath, TEXT);
         try (DirectoryStream<Path> ds = Files.newDirectoryStream(dir);) {
             if (ds instanceof SecureDirectoryStream<Path> sds) {
-                sds.move(file, null, file);
+                try {
+                    sds.move(file, null, file);
+                } catch (AtomicMoveNotSupportedException e) {
+                    throw new SkippedException("java.nio.file.AtomicMoveNotSupportedException");
+                }
                 if (!TEXT.equals(Files.readString(result)))
                     throw new RuntimeException(result + " content incorrect");
             } else {
@@ -311,11 +327,12 @@ public class SecureDS {
             boolean fileDeleted = Files.deleteIfExists(filepath);
             if (!fileDeleted)
                 Files.deleteIfExists(result);
+            // clean-up
+            delete(dir1);
+            delete(dir2);
+            stream1.close();
+            stream2.close();
         }
-
-        // clean-up
-        delete(dir1);
-        delete(dir2);
     }
 
     // null and ClosedDirectoryStreamException
