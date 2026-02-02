f22bc1cd518bc7f09dc49b78e40d06210226d2b7
Thomas Schatzl <tschatzl@openjdk.org>
2026-02-02 07:58:01 +0000

    8376131: Convert ContiguousSpace to use Atomic<T>
    Reviewed-by: dholmes, kbarrett

--- src/hotspot/share/runtime/vmStructs.cpp
+++ src/hotspot/share/runtime/vmStructs.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -901,6 +901,7 @@
   /*****************************/                                         \
                                                                           \
   declare_toplevel_type(void*)                                            \
+  declare_toplevel_type(Atomic<HeapWord*>)                                \
   declare_toplevel_type(int*)                                             \
   declare_toplevel_type(char*)                                            \
   declare_toplevel_type(char**)                                           \
