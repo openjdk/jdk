05860429f577fa688a506011dbcaac81420988b3
Andrew Haley <aph@openjdk.org>
2026-02-02 17:52:16 +0000

    Back out 37730e6aac899e1fbdcf4f201ac2ae1013201432

--- src/hotspot/share/code/nmethod.cpp
+++ src/hotspot/share/code/nmethod.cpp
@@ -2216,6 +2216,7 @@ void nmethod::verify_clean_inline_caches() {
 }
 
 void nmethod::mark_as_maybe_on_stack() {
+  MACOS_AARCH64_ONLY(os::thread_wx_enable_write());
   AtomicAccess::store(&_gc_epoch, CodeCache::gc_epoch());
 }
 
