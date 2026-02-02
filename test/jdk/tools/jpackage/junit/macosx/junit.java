28cf1de570bf8d7a90664f1d25fb0038ad393c44
Alexey Semenyuk <alexey.semenyuk@oracle.com>
2026-02-02 17:17:34 -0500

    Make ObjectFactory, ExecutorFactory, RetryExecutorFactory, and Executor accessible for creating jpackage mocks in tests outside the jdk.jpackage.internal package. Relocate MockUtils from jdk.jpackage.internal to jdk.jpackage.test.stdmock.JPackageMockUtils. The jdk.jpackage.test.stdmock package will contain jpackage-specific mock implementations.

--- test/jdk/tools/jpackage/junit/macosx/junit.java
+++ test/jdk/tools/jpackage/junit/macosx/junit.java
@@ -36,9 +36,9 @@
  * @requires (os.family == "mac")
  * @library /test/jdk/tools/jpackage/helpers
  * @build jdk.jpackage.test.mock.*
+ * @build jdk.jpackage.test.stdmock.*
  * @compile/module=jdk.jpackage -Xlint:all -Werror
  *    jdk/jpackage/internal/MacDmgSystemEnvironmentTest.java
- *    ../../share/jdk.jpackage/jdk/jpackage/internal/MockUtils.java
  * @run junit jdk.jpackage/jdk.jpackage.internal.MacDmgSystemEnvironmentTest
  */
 
@@ -47,8 +47,8 @@
  * @requires (os.family == "mac")
  * @library /test/jdk/tools/jpackage/helpers
  * @build jdk.jpackage.test.mock.*
+ * @build jdk.jpackage.test.stdmock.*
  * @compile/module=jdk.jpackage -Xlint:all -Werror
  *    jdk/jpackage/internal/MacDmgPackagerTest.java
- *    ../../share/jdk.jpackage/jdk/jpackage/internal/MockUtils.java
  * @run junit jdk.jpackage/jdk.jpackage.internal.MacDmgPackagerTest
  */
