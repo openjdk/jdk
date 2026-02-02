e7a1e76961eaad5a6f4be43fd54cd91305babc19
Marc Chevalier <mchevalier@openjdk.org>
2026-02-02 10:11:34 +0000

    8376325: [IR Framework] Detect and report overloads
    Reviewed-by: chagedorn, dfenacci

--- test/hotspot/jtreg/testlibrary_tests/ir_framework/tests/TestBadFormat.java
+++ test/hotspot/jtreg/testlibrary_tests/ir_framework/tests/TestBadFormat.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -276,9 +276,10 @@ class BadArgumentsAnnotation {
     }
 }
 
+// Since all the methods are failing, the class doesn't specify any @Test methods, which is another failure.
+@ClassFail
 class BadOverloadedMethod {
 
-    @FailCount(0) // Combined with both sameName() below
     @Test
     public void sameName() {}
 
