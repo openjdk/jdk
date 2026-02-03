22d8f91f1e431fdb42a8212ae3d686ce96bc937f
Yasumasa Suenaga <ysuenaga@openjdk.org>
2026-02-03 09:44:00 +0000

    8376284: New test serviceability/sa/TestJhsdbJstackMixedCore.java from JDK-8374482 fails on Linux Alpine
    Reviewed-by: cjplummer, mbaesken

--- test/hotspot/jtreg/serviceability/sa/TestJhsdbJstackMixedCore.java
+++ test/hotspot/jtreg/serviceability/sa/TestJhsdbJstackMixedCore.java
@@ -30,9 +30,11 @@ import jdk.test.lib.apps.LingeredApp;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.util.CoreUtils;
 
+import jtreg.SkippedException;
+
 /**
  * @test
- * @bug 8374482
+ * @bug 8374482 8376284
  * @requires (os.family == "linux") & (vm.hasSA)
  * @requires os.arch == "amd64"
  * @library /test/lib
@@ -59,11 +61,21 @@ public class TestJhsdbJstackMixedCore {
         System.out.println(out.getStdout());
         System.err.println(out.getStderr());
 
-        out.shouldContain("<signal handler called>");
+        out.shouldContain("__restore_rt <signal trampoline>");
         out.shouldContain("Java_jdk_test_lib_apps_LingeredApp_crash");
     }
 
     public static void main(String... args) throws Throwable {
+        // Check whether the symbol of signal trampoline is available.
+        var libc = SATestUtils.getLibCPath();
+
+        // SA distinguishes the frame is signal trampoline if the function
+        // is named "__restore_rt".
+        // SA cannot unwind problematic frame from it if the symbol not found.
+        if (!SATestUtils.isSymbolAvailable(libc, "__restore_rt")) {
+            throw new SkippedException("Signal trampoline (__restore_rt) not found in libc.");
+        }
+
         LingeredApp app = new LingeredApp();
         app.setForceCrash(true);
         LingeredApp.startApp(app, CoreUtils.getAlwaysPretouchArg(true));
