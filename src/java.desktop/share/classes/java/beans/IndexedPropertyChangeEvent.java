8850e2ba19a5eabc563ad1e4f5248bc277e78748
Sergey Bylokhov <bylokhov@amazon.com>
2026-02-03 17:58:11 -0800

    8377128: Add missing @Override annotations in "java.beans.*" packages

--- src/java.desktop/share/classes/java/beans/IndexedPropertyChangeEvent.java
+++ src/java.desktop/share/classes/java/beans/IndexedPropertyChangeEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -83,6 +83,7 @@ public class IndexedPropertyChangeEvent extends PropertyChangeEvent {
         return index;
     }
 
+    @Override
     void appendTo(StringBuilder sb) {
         sb.append("; index=").append(getIndex());
     }
--- src/java.desktop/share/classes/java/beans/XMLEncoder.java
+++ src/java.desktop/share/classes/java/beans/XMLEncoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -321,6 +321,7 @@ public class XMLEncoder extends Encoder implements AutoCloseable {
      *
      * @see XMLDecoder#readObject
      */
+    @Override
     public void writeObject(Object o) {
         if (internal) {
             super.writeObject(o);
@@ -391,6 +392,7 @@ public class XMLEncoder extends Encoder implements AutoCloseable {
      *               to the stream.
      * @see java.beans.PersistenceDelegate#initialize
      */
+    @Override
     public void writeStatement(Statement oldStm) {
         // System.out.println("XMLEncoder::writeStatement: " + oldStm);
         boolean internal = this.internal;
@@ -445,6 +447,7 @@ public class XMLEncoder extends Encoder implements AutoCloseable {
      *               to the stream.
      * @see java.beans.PersistenceDelegate#initialize
      */
+    @Override
     public void writeExpression(Expression oldExp) {
         boolean internal = this.internal;
         this.internal = true;
@@ -502,6 +505,7 @@ public class XMLEncoder extends Encoder implements AutoCloseable {
         clear();
     }
 
+    @Override
     void clear() {
         super.clear();
         nameGenerator.clear();
@@ -526,6 +530,7 @@ public class XMLEncoder extends Encoder implements AutoCloseable {
      * postamble and then closes the output stream associated
      * with this stream.
      */
+    @Override
     public void close() {
         flush();
         writeln("</java>");
