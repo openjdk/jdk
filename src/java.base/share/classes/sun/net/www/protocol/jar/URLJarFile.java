27bd238aaf4cd744910227e962166852637d24bc
Jaikiran Pai <jaikiran.pai@gmail.com>
2026-02-04 06:31:23 +0530

    URLJarFile$URLJarFileEntry need not clone the arrays returned by getCertificates() and getCodeSigners()

--- src/java.base/share/classes/sun/net/www/protocol/jar/URLJarFile.java
+++ src/java.base/share/classes/sun/net/www/protocol/jar/URLJarFile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2001, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2001, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -165,13 +165,12 @@ public class URLJarFile extends JarFile {
     }
 
     private class URLJarFileEntry extends JarEntry {
-        private final JarEntry je;
 
         URLJarFileEntry(JarEntry je) {
             super(je);
-            this.je = je;
         }
 
+        @Override
         public Attributes getAttributes() throws IOException {
             if (URLJarFile.this.isSuperMan()) {
                 Map<String, Attributes> e = URLJarFile.this.superEntries;
@@ -182,16 +181,6 @@ public class URLJarFile extends JarFile {
                 }
             }
             return null;
-        }
-
-        public java.security.cert.Certificate[] getCertificates() {
-            Certificate[] certs = je.getCertificates();
-            return certs == null? null: certs.clone();
-        }
-
-        public CodeSigner[] getCodeSigners() {
-            CodeSigner[] csg = je.getCodeSigners();
-            return csg == null? null: csg.clone();
         }
     }
 
