d85fa964f56ef06c78fdf54f7c1e8382a843592f
Anton Artemov <anton.artemov@oracle.com>
2026-02-05 16:10:42 +0100

    8377223: Port fdlibm atanh to java.

--- test/jdk/java/lang/StrictMath/FdlibmTranslit.java
+++ test/jdk/java/lang/StrictMath/FdlibmTranslit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -138,6 +138,10 @@ public class FdlibmTranslit {
 
     public static double tanh(double x) {
         return Tanh.compute(x);
+    }
+
+    public static double atanh(double x) {
+        return Atanh.compute(x);
     }
 
     public static double IEEEremainder(double f1, double f2) {
@@ -2750,6 +2754,53 @@ public class FdlibmTranslit {
                 x *= one;           /* create necessary signal */
             }
             return x;               /* exact output */
+        }
+    }
+
+    /*
+     *  Return the Inverse Hyperbolic Tangent of x
+     *
+     *  Method :
+     *    1.Reduced x to positive by atanh(-x) = -atanh(x)
+     *    2.For x>=0.5
+     *                1              2x                          x
+     *	  atanh(x) = --- * log(1 + -------) = 0.5 * log1p(2 * --------)
+     *                2             1 - x                      1 - x
+     *
+     * 	For x<0.5
+     *	  atanh(x) = 0.5*log1p(2x+2x*x/(1-x))
+     *
+     *  Special cases:
+     *	  atanh(x) is NaN if |x| > 1 with signal;
+     *	  atanh(NaN) is that NaN with no signal;
+     *	  atanh(+-1) is +-INF with signal.
+     *
+     */
+    private static final class Atanh {
+        private static final double zero = 0.0;
+        private static final double one = 1.0;
+        private static final double huge = 1.0e300;
+
+        static double compute(double x) {
+            double t;
+            int hx,ix;
+            /*unsigned*/ int lx;
+            hx = __HI(x);		/* high word */
+            lx = __LO(x);		/* low word */
+            ix = hx&0x7fffffff;
+            if ((ix|((lx|(-lx))>>31))>0x3ff00000) /* |x|>1 */
+                return (x-x)/(x-x);
+            if(ix==0x3ff00000)
+                return x/zero;
+            if(ix<0x3e300000&&(huge+x)>zero) return x;	/* x<2**-28 */
+            //__HI(x) = ix;		/* x <- |x| */
+            x = __HI(x, ix);
+            if(ix<0x3fe00000) {		/* x < 0.5 */
+                t = x+x;
+                t = 0.5*log1p(t+t*x/(one-x));
+            } else
+                t = 0.5*log1p((x+x)/(one-x));
+            if(hx>=0) return t; else return -t;
         }
     }
 }
