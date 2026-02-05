9e360240972f5ae5e103506c7b322db7eea9d59e
Andrew Dinn <adinn@redhat.com>
2026-02-05 13:39:44 +0000

    fix klass encode/decode for AOT code

--- src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/macroAssembler_aarch64.cpp
@@ -5140,7 +5140,8 @@ void MacroAssembler::cmp_klass(Register obj, Register klass, Register tmp) {
     if (CompressedKlassPointers::base() == nullptr) {
       cmp(klass, tmp, LSL, CompressedKlassPointers::shift());
       return;
-    } else if (((uint64_t)CompressedKlassPointers::base() & 0xffffffff) == 0
+    } else if (!AOTCodeCache::is_on_for_dump() &&
+               ((uint64_t)CompressedKlassPointers::base() & 0xffffffff) == 0
                && CompressedKlassPointers::shift() == 0) {
       // Only the bottom 32 bits matter
       cmpw(klass, tmp);
@@ -5399,7 +5400,7 @@ void MacroAssembler::encode_klass_not_null_for_aot(Register dst, Register src) {
 }
 
 void MacroAssembler::encode_klass_not_null(Register dst, Register src) {
-  if (AOTCodeCache::is_on_for_dump()) {
+  if (CompressedKlassPointers::base() != nullptr && AOTCodeCache::is_on_for_dump()) {
     encode_klass_not_null_for_aot(dst, src);
     return;
   }
@@ -5465,7 +5466,7 @@ void MacroAssembler::decode_klass_not_null_for_aot(Register dst, Register src) {
 void  MacroAssembler::decode_klass_not_null(Register dst, Register src) {
   assert (UseCompressedClassPointers, "should only be used for compressed headers");
 
-  if (AOTCodeCache::is_on_for_dump()) {
+  if (CompressedKlassPointers::base() != nullptr && AOTCodeCache::is_on_for_dump()) {
     decode_klass_not_null_for_aot(dst, src);
     return;
   }
