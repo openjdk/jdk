6a412777fcc5012a1f8b7b95d9a596efffe5b27f
David Linus Briemann <david.briemann@sap.com>
2026-02-06 12:36:27 +0100

    more copyright updates..

--- src/hotspot/cpu/ppc/matcher_ppc.hpp
+++ src/hotspot/cpu/ppc/matcher_ppc.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021, 2026, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
