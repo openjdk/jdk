de5fd62859a4b2f054d9576936e7ff7581fb0a74
SendaoYan <yansendao@hygon.cn>
2026-02-10 20:03:07 +0800

    Remove unnecessary explict gc()

--- test/jdk/jdk/jfr/event/gc/detailed/TestZAllocationStallEvent.java
+++ test/jdk/jdk/jfr/event/gc/detailed/TestZAllocationStallEvent.java
@@ -30,24 +30,17 @@ import jdk.jfr.Recording;
 import jdk.jfr.consumer.RecordedEvent;
 import jdk.test.lib.jfr.EventNames;
 import jdk.test.lib.jfr.Events;
-import jdk.test.whitebox.WhiteBox;
 
 /**
  * @test
  * @requires vm.hasJFR & vm.gc.Z
  * @requires vm.flagless
  * @library /test/lib /test/jdk /test/hotspot/jtreg
- * @build jdk.test.whitebox.WhiteBox
- * @run driver jdk.test.lib.helpers.ClassFileInstaller jdk.test.whitebox.WhiteBox
- * @run main/othervm -XX:+UnlockDiagnosticVMOptions -XX:+WhiteBoxAPI -Xbootclasspath/a:.
- *                   -XX:ActiveProcessorCount=4
- *                   -XX:+UseZGC -Xmx64M -Xlog:gc*:gc.log::filecount=0
+ * @run main/othervm -XX:+UseZGC -Xmx64M -Xlog:gc*:gc.log::filecount=0
  *                   jdk.jfr.event.gc.detailed.TestZAllocationStallEvent
  */
 
 public class TestZAllocationStallEvent {
-    static final WhiteBox whitebox = WhiteBox.getWhiteBox();
-
     public static void main(String[] args) throws Exception {
         try (Recording recording = new Recording()) {
             // Activate the event we are interested in and start recording
@@ -60,8 +53,6 @@ public class TestZAllocationStallEvent {
                     blackHole(new byte[4 * 1024 * 1024]);
                 }
             } catch (java.lang.OutOfMemoryError e) {
-                System.gc();
-                whitebox.fullGC();
             }
 
             recording.stop();
