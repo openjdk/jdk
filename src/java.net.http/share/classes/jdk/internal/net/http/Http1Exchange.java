70647fd8dff796142510dc14fdb035eb993f9d38
Jaikiran Pai <jaikiran.pai@gmail.com>
2026-02-11 18:10:37 +0530

    8377796: java.net.http.HttpClient.send() and sendAsync() never complete when BodyHandler.apply() returns null

--- src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
+++ src/java.net.http/share/classes/jdk/internal/net/http/Http1Exchange.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -474,6 +474,7 @@ class Http1Exchange<T> extends ExchangeImpl<T> {
     @Override
     Http1ResponseBodySubscriber<T> createResponseSubscriber(BodyHandler<T> handler, ResponseInfo response) {
         BodySubscriber<T> subscriber = handler.apply(response);
+        Objects.requireNonNull(subscriber, "BodyHandler returned a null BodySubscriber");
         var cancelTimerOnTermination =
                 cancelTimerOnResponseBodySubscriberTermination(
                         exchange.request().isWebSocket(), response.statusCode());
