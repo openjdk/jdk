13218df6eecc2780cc5b4d0e3b363e6ab86f253a
Anton Artemov <anton.artemov@oracle.com>
2026-02-12 12:58:24 +0100

    8376665: Fixed whitespaces.

--- src/java.base/share/classes/java/lang/FdLibm.java
+++ src/java.base/share/classes/java/lang/FdLibm.java
@@ -3588,20 +3588,20 @@ final class FdLibm {
             double t;
             int hx;
             hx = __HI(x);
-            if(hx < 0x3ff0_0000) {                           // x < 1 */
+            if (hx < 0x3ff0_0000) {                           // x < 1 */
                 return (x - x) / (x - x);
-            } else if (hx >= 0x41b0_0000) {                  // x > 2**28
-                if(hx >= 0x7ff0_0000) {                      // x is inf of NaN
+            } else if (hx >= 0x41b0_0000) {                   // x > 2**28
+                if (hx >= 0x7ff0_0000) {                      // x is inf of NaN
                     return x + x;
                 } else {
-                    return Log.compute(x) + ln2;            // acosh(huge) = log(2x)
+                    return Log.compute(x) + ln2;              // acosh(huge) = log(2x)
                 }
             } else if (((hx - 0x3ff0_0000) | __LO(x)) == 0) {
-                return 0.0;                                 // acosh(1) = 0
-            } else if (hx > 0x4000_0000) {                   // 2**28 > x > 2
+                return 0.0;                                   // acosh(1) = 0
+            } else if (hx > 0x4000_0000) {                    // 2**28 > x > 2
                 t = x * x;
                 return Log.compute(2.0 * x - 1.0 / (x + Sqrt.compute(t - 1.0)));
-            } else {                                        // 1< x <2
+            } else {                                          // 1< x <2
                 t = x - 1.0;
                 return Log1p.compute(t + Sqrt.compute(2.0 * t + t * t));
             }
