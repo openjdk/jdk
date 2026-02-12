03632e15de1956829727dafa4f3cd6d2dbc30c72
Yasumasa Suenaga <ysuenaga@openjdk.org>
2026-02-12 01:58:22 +0000

    8374577: Heap dump from core does not contain HPROF_GC_ROOT_JAVA_FRAME
    Reviewed-by: cjplummer, amenkov, kevinw, aturbanov

--- src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/ConstantPool.java
+++ src/jdk.hotspot.agent/share/classes/sun/jvm/hotspot/oops/ConstantPool.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -253,13 +253,16 @@ public class ConstantPool extends Metadata implements ClassConstants {
     return res;
   }
 
+  private int invokedynamicBootstrapRefIndexAt(int indyIndex) {
+    return getCache().getIndyEntryAt(indyIndex).getConstantPoolIndex();
+  }
+
   // Translate index, which could be CPCache index or Indy index, to a constant pool index
   public int to_cp_index(int index, int code) {
     Assert.that(getCache() != null, "'index' is a rewritten index so this class must have been rewritten");
     switch(code) {
       case Bytecodes._invokedynamic:
-        int poolIndex = getCache().getIndyEntryAt(index).getConstantPoolIndex();
-        return invokeDynamicNameAndTypeRefIndexAt(poolIndex);
+        return invokedynamicBootstrapRefIndexAt(index);
       case Bytecodes._getfield:
       case Bytecodes._getstatic:
       case Bytecodes._putfield:
