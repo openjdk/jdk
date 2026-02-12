e1006a9794f8bab7d1089c3ec0af9f3072c231bc
Vishva N <72214543+vishva238@users.noreply.github.com>
2026-02-13 00:13:15 +0530

    Adding access checking capability before changing default authenticator

---
+++ src/java.base/share/classes/java/net/AuthenticatorModifyAccessChecker.java
@@ -0,0 +1,11 @@
+package java.net;
+
+
+public class AuthenticatorModifyAccessChecker {
+    public AuthenticatorModifyAccessChecker() {
+    }
+
+    public boolean canModifyAuthenticator(Authenticator a) {
+        return true;
+    }
+}
