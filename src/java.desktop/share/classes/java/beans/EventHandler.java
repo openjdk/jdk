218ffb74f8bfd30bd2011f376e799beecc7efbce
Sergey Bylokhov <serb@openjdk.org>
2026-02-13 07:41:33 +0000

    8377128: Add missing @Override annotations in "java.beans.*" packages
    Reviewed-by: tr, prr

--- src/java.desktop/share/classes/java/beans/EventHandler.java
+++ src/java.desktop/share/classes/java/beans/EventHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2000, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2000, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -415,6 +415,7 @@ public class EventHandler implements InvocationHandler {
      *
      * @see EventHandler
      */
+    @Override
     public Object invoke(final Object proxy, final Method method, final Object[] arguments) {
         String methodName = method.getName();
         if (method.getDeclaringClass() == Object.class)  {
