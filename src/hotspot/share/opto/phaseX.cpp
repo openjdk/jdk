9583f0a4a69949e17802112f7aa7f42ead77334a
katkerem <katkerem@amazon.com>
2026-02-16 11:29:17 +0000

    push only VectorMaskToLong users

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -2731,7 +2731,12 @@ void PhaseIterGVN::add_users_of_use_to_worklist(Node* n, Node* use, Unique_Node_
   // VectorMaskToLongNode::Ideal_MaskAll looks through VectorStoreMask
   // to fold constant masks.
   if (use_op == Op_VectorStoreMask) {
-    add_users_to_worklist0(use, worklist);
+    for (DUIterator_Fast i2max, i2 = use->fast_outs(i2max); i2 < i2max; i2++) {
+      Node* u = use->fast_out(i2);
+      if (u->Opcode() == Op_VectorMaskToLong) {
+        worklist.push(u);
+      }
+    }
   }
 
   // From CastX2PNode::Ideal
