0cf94e07521a021b877a574bf2cbc1d0e9a3de91
Brian Burkhalter <brian.burkhalter@oracle.com>
2026-02-17 11:49:46 -0800

    8378115: (fs) sun.nio.fs.NativeBuffer no longer needs a cleaner

--- src/java.base/share/classes/sun/nio/fs/NativeBuffer.java
+++ src/java.base/share/classes/sun/nio/fs/NativeBuffer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2009, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,9 +25,7 @@
 
 package sun.nio.fs;
 
-import java.lang.ref.Cleaner.Cleanable;
 import jdk.internal.misc.Unsafe;
-import jdk.internal.ref.CleanerFactory;
 
 /**
  * A light-weight buffer in native memory.
@@ -38,27 +36,14 @@ class NativeBuffer implements AutoCloseable {
 
     private final long address;
     private final int size;
-    private final Cleanable cleanable;
 
     // optional "owner" to avoid copying
     // (only safe for use by thread-local caches)
     private Object owner;
 
-    private static class Deallocator implements Runnable {
-        private final long address;
-        Deallocator(long address) {
-            this.address = address;
-        }
-        public void run() {
-            unsafe.freeMemory(address);
-        }
-    }
-
     NativeBuffer(int size) {
         this.address = unsafe.allocateMemory(size);
         this.size = size;
-        this.cleanable = CleanerFactory.cleaner()
-                                       .register(this, new Deallocator(address));
     }
 
     @Override
@@ -79,7 +64,7 @@ class NativeBuffer implements AutoCloseable {
     }
 
     void free() {
-        cleanable.clean();
+        unsafe.freeMemory(address);
     }
 
     // not synchronized; only safe for use by thread-local caches
