microsoft/openjdk-jdk

8358560: Use `Unique_Node_List` in place of `GrowableArray<Node*>`
This patch is a minor cleanup of the code in escape.cpp and gcm.cpp.
Wherever the code calls `GrowableArray<Node*>::append_if_missing()` and
when the order of the insertions doesn't matter, this patch instead uses
`Unique_Node_List::push()`, since such insertions are asymptotically
faster than those due to `GrowableArray`, which has the scan the entire
array prior to insertion.

src/hotspot/share/opto/gcm.cpp
@@ -592,7 +592,7 @@ private:
   };
 
   GrowableArray<DefUsePair> _queue;
-  GrowableArray<MergeMemNode*> _worklist_visited; // visited mergemem nodes
+  Unique_Node_List _worklist_visited; // visited mergemem nodes
 
   bool already_enqueued(Node* def_mem, PhiNode* use_phi) const {
     // def_mem is one of the inputs of use_phi and at least one input of use_phi is
@@ -632,9 +632,12 @@ public:
   void push(Node* def_mem_state, Node* use_mem_state) {
     if (use_mem_state->is_MergeMem()) {
       // Be sure we don't get into combinatorial problems.
-      if (!_worklist_visited.append_if_missing(use_mem_state->as_MergeMem())) {
-        return; // already on work list; do not repeat
+      Node *mergeMem = use_mem_state->as_MergeMem();
+      if (_worklist_visited.member(mergeMem)) {
+        // already on work list; do not repeat
+        return;
       }
+      _worklist_visited.push(mergeMem);
     } else if (use_mem_state->is_Phi()) {
       // A Phi could have the same mem as input multiple times. If that's the case, we don't need to enqueue it
       // more than once. We otherwise allow phis to be repeated; they can merge two relevant states.

openjdk/jdk

src/hotspot/share/opto/gcm.cpp
@@ -592,7 +592,7 @@ private:
   };
 
   GrowableArray<DefUsePair> _queue;
-  GrowableArray<MergeMemNode*> _worklist_visited; // visited mergemem nodes
+  Unique_Node_List _worklist_visited; // visited mergemem nodes
 
   bool already_enqueued(Node* def_mem, PhiNode* use_phi) const {
     // def_mem is one of the inputs of use_phi and at least one input of use_phi is
@@ -632,9 +632,12 @@ public:
   void push(Node* def_mem_state, Node* use_mem_state) {
     if (use_mem_state->is_MergeMem()) {
       // Be sure we don't get into combinatorial problems.
-      if (!_worklist_visited.append_if_missing(use_mem_state->as_MergeMem())) {
-        return; // already on work list; do not repeat
+      Node *merge_mem = use_mem_state->as_MergeMem();
+      if (_worklist_visited.member(merge_mem)) {
+        // already on work list; do not repeat
+        return;
       }
+      _worklist_visited.push(merge_mem);
     } else if (use_mem_state->is_Phi()) {
       // A Phi could have the same mem as input multiple times. If that's the case, we don't need to enqueue it
       // more than once. We otherwise allow phis to be repeated; they can merge two relevant states.