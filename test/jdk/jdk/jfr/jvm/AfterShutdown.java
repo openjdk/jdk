e042c9907f3d8ea203f37f057f2fce9aee749057
erik_gahlin <erik.gahlin@oracle.com>
2026-02-18 17:38:59 +0100

    Update test

--- test/jdk/jdk/jfr/jvm/AfterShutdown.java
+++ test/jdk/jdk/jfr/jvm/AfterShutdown.java
@@ -25,60 +25,119 @@ package jdk.jfr.jvm;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 
 import jdk.jfr.Recording;
 import jdk.jfr.FlightRecorder;
 import jdk.jfr.RecordingState;
+import jdk.jfr.consumer.RecordingStream;
 
 /**
  * Test application that tries to interact with JFR after shutdown.
  */
+@SuppressWarnings("resource")
 public class AfterShutdown {
-    public static void main(String... args) {
-        Recording r = new Recording();
-        r.start();
+    public static void main(String... args) throws Exception {
+       Recording r1 = new Recording();
+        r1.start();
+        Recording r2 = new Recording();
+        r2.start();
+        Recording r3 = new Recording();
+        r3.start();
+        Recording r4 = new Recording();
+        r4.start();
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
                 awaitRepositoryRemoved();
-                try {
-                    r.dump(Path.of("file.jfr"));
-                    System.out.println("FAIL: Should not be able to dump after shutdown.");
-                    System.exit(1);
-                } catch (IOException ioe) {
-                    if (r.getState() != RecordingState.CLOSED) {
-                        System.out.println("FAIL: Expected recording to be closed.");
-                        System.out.println("Recording state: " + r.getState());
-                        System.exit(1);
-                    }
-                    List<Recording> recordings = FlightRecorder.getFlightRecorder().getRecordings();
-                    if (!recordings.isEmpty()) {
-                        System.out.println("FAIL: Expected no available recordings.");
-                        System.exit(1);
-                    }
-                }
-                try {
-                    new Recording();
-                    System.out.println("FAIL: Expected IllegalStateException if recording is created after repository removal.");
-                    System.exit(1);
-                } catch (IllegalStateException ise) {
-                    // As expected
-                }
-            }
-
-            private void awaitRepositoryRemoved() {
-                String path = System.getProperty("jdk.jfr.repository");
-                Path repository = Path.of(path);
-                while (Files.exists(repository)) {
-                    System.out.println("Repository still exist. Waiting 100 ms ...");
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
-                    }
-                }
-                System.out.println("Repository removed.");
+                testNew();
+                testDump(r1);
+                testStop(r2);
+                testClose(r3);
+                testCopy(r4);
+                testSnapshot();
             }
         });
+    }
+
+    private static void testNew() {
+        try {
+            new Recording();
+            fail("Expected IllegalStateException if recording is created after repository removal.");
+        } catch (IllegalStateException ise) {
+            // As expected
+        }
+        try {
+            new RecordingStream();
+            fail("Expected IllegalStateException if recording is created after repository removal.");
+        } catch (IllegalStateException ise) {
+            // As expected
+        }
+    }
+
+    private static void testDump(Recording r) {
+        try {
+            r.dump(Path.of("file.jfr"));
+            fail("Should not be able to dump recording after shutdown");
+        } catch (IOException ioe) {
+            assertClosed(r);
+        }
+    }
+
+    private static void testStop(Recording r) {
+        try {
+            r.stop();
+            fail("Should not be able to stop recording after shutdown.");
+        } catch (IllegalStateException ioe) {
+            assertClosed(r);
+        }
+    }
+
+    private static void testClose(Recording r) {
+        try {
+            r.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Should not be close recording after shutdown.");
+        }
+    }
+
+    private static void testCopy(Recording r) {
+        try {
+            Recording copy = r.copy(true);
+            assertClosed(copy);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Recording copy should not throw Exception.");
+        }
+    }
+
+    private static void testSnapshot() {
+        Recording r = FlightRecorder.getFlightRecorder().takeSnapshot();
+        assertClosed(r);
+    }
+
+    private static void awaitRepositoryRemoved() {
+        String path = System.getProperty("jdk.jfr.repository");
+        Path repository = Path.of(path);
+        while (Files.exists(repository)) {
+            System.out.println("Repository still exist. Waiting 100 ms ...");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        System.out.println("Repository removed.");
+    }
+
+    private static void assertClosed(Recording r) {
+        if (r.getState() != RecordingState.CLOSED) {
+            fail("Recording was in state " + r.getState() + " but expected it to be CLOSED");
+        }
+    }
+
+    private static void fail(String message) {
+        System.out.println("FAIL: " + message);
+        // This will typically hang the application because of shutdown lock.
+        System.exit(1);
     }
 }
