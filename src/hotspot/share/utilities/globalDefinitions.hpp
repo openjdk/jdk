839d70c8bf5350f86dccfca14a550e155ebed84b
Leo Korinth <leo.korinth@oracle.com>
2026-02-18 17:05:31 +0100

    add examples when it is good using sizeof/sizeof_auto

--- src/hotspot/share/utilities/globalDefinitions.hpp
+++ src/hotspot/share/utilities/globalDefinitions.hpp
@@ -179,6 +179,11 @@ constexpr auto sizeof_auto_impl() {
 }
 
 // Returns the size (in bytes) of the expression as an unsigned type that is as small as possible.
+//
+// Example of correct usage of sizeof/sizeof_auto:
+// size_t size = std::numeric_limits<uint32_t>::max() * sizeof(uint16_t); // this will wrap using sizeof_auto, use sizeof to ensure computation using size_t
+// uint32_t count = 42 / sizeof_auto(uint16_t);                           // implicit narrowing conversion or compiler warning/error using stricter compiler flags when using sizeof
+
 #define sizeof_auto(...) sizeof_auto_impl<sizeof(__VA_ARGS__)>()
 
 // Convert pointer to intptr_t, for use in printing pointers.
