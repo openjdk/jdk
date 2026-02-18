78ed9a4cdf041c00f103b3f4296d1e5c28ed4cca
Oli Gillespie <ogillesp@amazon.com>
2026-02-18 15:23:35 +0000

    8378110: Add -XX: prefix to settings-file flags in RuntimeMXBean.getInputArguments()

--- test/jdk/java/lang/management/RuntimeMXBean/InputArgument.java
+++ test/jdk/java/lang/management/RuntimeMXBean/InputArgument.java
@@ -62,41 +62,71 @@
  * @run main/othervm -Dprops=something InputArgument -Dprops=something
  */
 
+/*
+ * @test
+ * @bug     8378110
+ * @summary RuntimeMXBean.getInputArguments() handling of flags from settings file.
+ *
+ * @author  Oli Gillespie
+ *
+ * @run driver InputArgument generateFlagsFile
+ * @run main/othervm -XX:+UseFastJNIAccessors -XX:Flags=InputArgument.flags InputArgument
+ * -XX:+UseFastJNIAccessors -XX:-UseG1GC -XX:+UseParallelGC -XX:MaxHeapSize=100M
+ */
+
 import java.lang.management.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.ListIterator;
 
 public class InputArgument {
     private static RuntimeMXBean rm = ManagementFactory.getRuntimeMXBean();
-    private static String vmOption = null;
+    private static String[] vmOptions = null;
 
     public static void main(String args[]) throws Exception {
-        // set the expected input argument
-        if (args.length > 0) {
-            vmOption = args[0];
-        }
-
-        List<String> options = rm.getInputArguments();
-        if (vmOption == null) {
+        if (args.length > 0 && "generateFlagsFile".equals(args[0])) {
+            generateFlagsFile();
             return;
         }
 
-        boolean testFailed = true;
+        // set the expected input arguments
+        if (args.length > 0) {
+            vmOptions = args;
+        }
+
+        List<String> options = rm.getInputArguments();
+        if (vmOptions == null) {
+            return;
+        }
+
         System.out.println("Number of arguments = " + options.size());
         int i = 0;
         for (String arg : options) {
             System.out.println("Input arg " + i + " = " + arg);
             i++;
-            if (arg.equals(vmOption)) {
-                testFailed = false;
-                break;
-            }
         }
-        if (testFailed) {
-            throw new RuntimeException("TEST FAILED: " +
-                "VM option " + vmOption + " not found");
+
+        for (String expected : vmOptions) {
+            boolean found = false;
+            for (String arg : options) {
+                if (arg.equals(expected)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                throw new RuntimeException("TEST FAILED: " +
+                    "VM option " + expected + " not found");
+            }
         }
 
         System.out.println("Test passed.");
+    }
+
+    private static void generateFlagsFile() throws Exception {
+        // 3 types of flag; both boolean cases and 1 numeric
+        Files.writeString(Paths.get("", "InputArgument.flags"),
+            String.format("-UseG1GC%n+UseParallelGC%nMaxHeapSize=100M%n"));
     }
 }
