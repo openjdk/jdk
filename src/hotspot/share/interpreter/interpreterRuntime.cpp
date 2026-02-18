63bf3ffd48affb0be032231ee95f5b02e1ffab31
Paul Hübner <paul.hubner@oracle.com>
2026-02-18 14:15:21 +0100

    Make tty passing a lot more explicit.

--- src/hotspot/share/interpreter/interpreterRuntime.cpp
+++ src/hotspot/share/interpreter/interpreterRuntime.cpp
@@ -1522,7 +1522,7 @@ JRT_LEAF(intptr_t, InterpreterRuntime::trace_bytecode(JavaThread* current, intpt
   LastFrameAccessor last_frame(current);
   assert(last_frame.is_interpreted_frame(), "must be an interpreted frame");
   methodHandle mh(current, last_frame.method());
-  BytecodeTracer::trace_interpreter(mh, last_frame.bcp(), tos, tos2);
+  BytecodeTracer::trace_interpreter(mh, last_frame.bcp(), tos, tos2, tty);
   return preserve_this_value;
 JRT_END
 #endif // !PRODUCT
