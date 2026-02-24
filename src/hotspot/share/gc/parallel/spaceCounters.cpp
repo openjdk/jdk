86434a5afa89c3abe5833645f04a81d8d5d3d731
Thomas Schatzl <tschatzl@openjdk.org>
2026-02-24 08:14:38 +0000

    8378266: Update atomicAccess include after Atomic<T> changes
    Reviewed-by: ayang, kbarrett

--- src/hotspot/share/gc/parallel/spaceCounters.cpp
+++ src/hotspot/share/gc/parallel/spaceCounters.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,7 +25,6 @@
 #include "gc/parallel/spaceCounters.hpp"
 #include "memory/allocation.inline.hpp"
 #include "memory/resourceArea.hpp"
-#include "runtime/atomicAccess.hpp"
 #include "runtime/mutex.hpp"
 #include "runtime/mutexLocker.hpp"
 #include "utilities/debug.hpp"
