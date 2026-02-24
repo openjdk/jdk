6973e4b4cbe5a4f76c5cc3bbd50aab279ef89e16
Vishva N <72214543+vishva238@users.noreply.github.com>
2026-02-25 02:05:06 +0530

    Remove unnecessary whitespace in AuthenticatorModifyAccessChecker

--- src/java.base/share/classes/java/net/AuthenticatorModifyAccessChecker.java
+++ src/java.base/share/classes/java/net/AuthenticatorModifyAccessChecker.java
@@ -1,10 +1,9 @@
 package java.net;
 
-
 public class AuthenticatorModifyAccessChecker {
     public AuthenticatorModifyAccessChecker() {
     }
-
+    
     public boolean canModifyAuthenticator(Authenticator a) {
         return true;
     }
