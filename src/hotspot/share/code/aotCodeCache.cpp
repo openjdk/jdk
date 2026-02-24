a73de31b9f79a561bd9bc9f7ffabf7ef79d550d2
Andrew Dinn <adinn@redhat.com>
2026-02-24 17:20:11 +0000

    fix pointer deref

--- src/hotspot/share/code/aotCodeCache.cpp
+++ src/hotspot/share/code/aotCodeCache.cpp
@@ -1456,7 +1456,7 @@ void AOTCodeAddressTable::init_extrs() {
   // addresses of fields in AOT runtime constants area
   address* p = AOTRuntimeConstants::field_addresses_list();
   while (*p != nullptr) {
-    SET_ADDRESS(_extrs, p++);
+    SET_ADDRESS(_extrs, *p++);
   }
 
   _extrs_complete = true;
