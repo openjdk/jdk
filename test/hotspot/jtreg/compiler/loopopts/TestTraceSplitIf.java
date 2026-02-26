85440ae2eeb9c04d602e640ad4d559b51cc27320
Michael Reeves <michael.reeves077@gmail.com>
2026-02-26 18:33:45 +1100

    fix test whitespace

--- test/hotspot/jtreg/compiler/loopopts/TestTraceSplitIf.java
+++ test/hotspot/jtreg/compiler/loopopts/TestTraceSplitIf.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -43,9 +43,9 @@ public class TestTraceSplitIf {
         );
 
         OutputAnalyzer output = new OutputAnalyzer(pb.start());
-        
+
         output.shouldHaveExitValue(0);
-        output.shouldContain("Split-If"); 
+        output.shouldContain("Split-If");
     }
 
     public static class HelloWorld {
