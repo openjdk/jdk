4ac94728c9807947d26ee8a784c5b6a742fbb59a
Michael Reeves <michael.reeves077@gmail.com>
2026-02-26 18:26:16 +1100

    add test

---
+++ test/hotspot/jtreg/compiler/loopopts/TestTraceSplitIf.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8351847
+ * @summary Sanity test for -XX:+TraceSplitIf to ensure it doesn't crash and produces output.
+ * @library /test/lib
+ * @requires vm.compiler2.enabled & vm.debug
+ * @run driver TestTraceSplitIf
+ */
+
+import jdk.test.lib.process.OutputAnalyzer;
+import jdk.test.lib.process.ProcessTools;
+
+public class TestTraceSplitIf {
+    public static void main(String[] args) throws Exception {
+        ProcessBuilder pb = ProcessTools.createLimitedTestJavaProcessBuilder(
+            "-XX:+UnlockDiagnosticVMOptions",
+            "-XX:+TraceSplitIf",
+            "-Xcomp",
+            HelloWorld.class.getName()
+        );
+
+        OutputAnalyzer output = new OutputAnalyzer(pb.start());
+        
+        output.shouldHaveExitValue(0);
+        output.shouldContain("Split-If"); 
+    }
+
+    public static class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
