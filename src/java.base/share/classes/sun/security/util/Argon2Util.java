facd486e04b9f4a341db3e38a8dc959096d89dc5
Valerie Peng <valerie.peng@oracle.com>
2026-02-27 22:22:27 +0000

    update Argon2Util w/ more restrictions from the PHC spec and allow partial PHC strings.

--- src/java.base/share/classes/sun/security/util/Argon2Util.java
+++ src/java.base/share/classes/sun/security/util/Argon2Util.java
@@ -48,16 +48,74 @@ public final class Argon2Util {
                 (k.length != 0 ? ",keyid=%s" : "") +
                 (x.length != 0 ? ",data=%s" : "");
         switch (baseFormat.length()) {
-            case 14 ->{ return String.format(baseFormat, m, t, p); }
-            case 23 ->{ return String.format(baseFormat, m, t, p,
+            case 14 -> { return String.format(baseFormat, m, t, p); }
+            case 23 -> { return String.format(baseFormat, m, t, p,
                     enc.encodeToString(k)); }
-            case 22 ->{ return String.format(baseFormat, m, t, p,
+            case 22 -> { return String.format(baseFormat, m, t, p,
                     enc.encodeToString(x)); }
-            case 31 ->{ return String.format(baseFormat, m, t, p,
+            case 31 -> { return String.format(baseFormat, m, t, p,
                     enc.encodeToString(k), enc.encodeToString(x)); }
-            default ->{ throw new RuntimeException
+            default -> { throw new RuntimeException
                     ("Unsupported params format: " + baseFormat);
             }
+        }
+    }
+
+    private static int parseInt(String s, String varName, int max) {
+        String[] pair = s.split("=");
+        String name = pair[0];
+        if (!varName.equals(name)) {
+            throw new IllegalArgumentException("Expected " + varName +
+                    ", but got " + name);
+        }
+        int value = Integer.parseInt(pair[1]);
+        if (max != -1 && value > max) {
+            throw new IllegalArgumentException("Value exceeds max " + max +
+                    ": " + value);
+        }
+        return value;
+    }
+
+    // decode the Argon2 parameters in the order of m, t, p, keyid, data
+    private static void decodeAndSetParams(String paramStr,
+            Argon2ParameterSpec.Builder builder)
+            throws IllegalArgumentException {
+
+        String[] assignments = paramStr.split(",");
+        // must be in the order of m ,t, p, followed by optional keyid and data
+        if (assignments.length < 3 || assignments.length > 5) {
+            throw new IllegalArgumentException("Invalid params: " + paramStr);
+        }
+        builder.memoryKiB(parseInt(assignments[0], "m", -1));
+        builder.iterations(parseInt(assignments[1], "t" , -1));
+        builder.parallelism(parseInt(assignments[2], "p", 256));
+        int index = 3;
+        while (index < assignments.length) { // keyid or data
+            String[] nextPair = assignments[index++].split("=");
+            byte[] value = dec.decode(nextPair[1]);
+            if (nextPair[0].equals("keyid")) {
+                if (value.length > 8) {
+                    throw new IllegalArgumentException
+                            ("keyid length should not exceed 8: " +
+                            value.length);
+                }
+                builder.secret(value);
+            } else if (nextPair[0].equals("data")) {
+                if (value.length > 32) {
+                    throw new IllegalArgumentException
+                            ("data length should not exceed 32: " +
+                            value.length);
+                }
+                builder.ad(value);
+                break;
+            } else {
+                throw new IllegalArgumentException
+                                ("Unsupported param name " + nextPair[0]);
+            }
+        }
+        if (index != assignments.length) {
+            throw new IllegalArgumentException("Unparsed params" +
+                    assignments[index]);
         }
     }
 
@@ -83,50 +141,56 @@ public final class Argon2Util {
     // Used by javax.crypto.spec.Argon2ParameterSpec.Builder
     public static Argon2ParameterSpec.Builder decodeHash(String str)
             throws IllegalArgumentException {
-        // parse the encoded hash; format = $type$version$params$salt$hash
+        // parse the encoded hash: $type[$version][$params][$salt[$hash]]
         String[] values = str.split("\\$");
-        if (values.length != 6) {
-            throw new IllegalArgumentException("Invalid Argon2 encoded hash");
+        // requires type, the rest are optional
+        if (values.length < 2 || values.length > 6) {
+            throw new IllegalArgumentException("Invalid Argon2 PHC String: " +
+                    str);
+        }
+        if (values[0].length() != 0) { // nothing before the first $
+            throw new IllegalArgumentException("Illegal prefix " + values[0]);
         }
 
-        if (values[0].trim().length() != 0) {
-            throw new IllegalArgumentException("Illegal prefix " + values[0]);
-        } else {
-            Type type = Type.valueOf(values[1].toUpperCase());
-            Version ver = Version.get(values[2].split("=")[1]);
-            int mKiB = -1, passes = -1, p = -1;
-            byte[] k = null, x = null;
-            String[] assignments = values[3].split(",");
-            for (int i = 0; i < assignments.length; i++) {
-                String[] pair = assignments[i].split("=");
-                String varName = pair[0].trim();
-                String varValue = pair[1].trim();
-                switch (varName) {
-                    case "m"->{ mKiB = Integer.parseInt(varValue); }
-                    case "t"->{ passes = Integer.parseInt(varValue); }
-                    case "p"->{ p = Integer.parseInt(varValue); }
-                    case "keyid"->{ k = dec.decode(varValue); }
-                    case "data"->{ x = dec.decode(varValue); }
-                    default ->{ throw new IllegalArgumentException
-                            ("Unsupported parameter " + varName);
+        // parse type
+        Type type = Type.valueOf(values[1].toUpperCase());
+        Argon2ParameterSpec.Builder builder =
+                Argon2ParameterSpec.newBuilder(type);
+        int idx = 1;
+        while (++idx < values.length) {
+            switch (idx) {
+                case 2 -> {
+                    Version ver = Version.get(values[2].split("=")[1]);
+                    builder.version(ver);
+                }
+                case 3 -> {
+                    decodeAndSetParams(values[3], builder);
+                }
+                case 4 -> {
+                    byte[] salt = dec.decode(values[4]);
+                    if (salt.length < 8 || salt.length > 48) {
+                        throw new IllegalArgumentException
+                                ("salt length should be between 8 to 48 bytes: "
+                                + salt.length);
                     }
+                    builder.nonce(salt);
+                }
+                case 5 -> {
+                    // use the trailing hash to decide tagLen
+                    byte[] tag = dec.decode(values[5]);
+                    int tagLen = tag.length;
+                    if (tagLen < 12 || tagLen > 64) {
+                        throw new IllegalArgumentException
+                                ("hash output length should be between 12 to 64 bytes: "
+                                + tagLen);
+                    }
+                    builder.tagLen(tagLen);
+                }
+                default -> {
+                    throw new RuntimeException("should never happen");
                 }
             }
-            byte[] salt = dec.decode(values[4].trim());
-            // use the trailing hash to decide tagLen
-            byte[] tag = dec.decode(values[5].trim());
-            int tagLen = tag.length;
-            Argon2ParameterSpec.Builder builder =
-                    Argon2ParameterSpec.newBuilder(type).version(ver)
-                    .memoryKiB(mKiB).iterations(passes).parallelism(p)
-                    .nonce(salt).tagLen(tagLen);
-            if (k != null) {
-                builder.secret(k);
-            }
-            if (x != null) {
-                builder.ad(x);
-            }
-            return builder;
         }
+        return builder;
     }
 }
