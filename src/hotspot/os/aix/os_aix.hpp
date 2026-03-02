599e5ad0cff53ed2c50f882a26f0359c4cc581ca
David Linus Briemann <david.briemann@sap.com>
2026-03-02 09:43:34 +0100

    revert copyright header updates

--- src/hotspot/os/aix/os_aix.hpp
+++ src/hotspot/os/aix/os_aix.hpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1999, 2026, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2013, 2026 SAP SE. All rights reserved.
+ * Copyright (c) 1999, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2013, 2024 SAP SE. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -143,6 +143,7 @@ class os::Aix {
 
   static bool platform_print_native_stack(outputStream* st, const void* context, char *buf, int buf_size, address& lastpc);
   static void* resolve_function_descriptor(void* p);
+
 };
 
 #endif // OS_AIX_OS_AIX_HPP
