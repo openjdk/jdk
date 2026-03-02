8155ddc341ff2ca1c093ed44a07bba05510c5c3d
Jan Lahoda <jan.lahoda@oracle.com>
2026-03-02 11:53:39 +0100

    Improving test.

--- test/langtools/tools/javac/implicitCompile/APImplicitClassesWarnings.java
+++ test/langtools/tools/javac/implicitCompile/APImplicitClassesWarnings.java
@@ -31,41 +31,87 @@
  *      jdk.compiler/com.sun.tools.javac.api
  *      jdk.compiler/com.sun.tools.javac.main
  * @build toolbox.ToolBox toolbox.JavacTask
- * @run main APImplicitClassesWarnings
+ * @run junit APImplicitClassesWarnings
  */
 
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Set;
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 import toolbox.JavacTask;
+import toolbox.Task;
 import toolbox.ToolBox;
 
 public class APImplicitClassesWarnings {
 
-    public static void main(String... args) throws Exception {
-        new APImplicitClassesWarnings().run();
-    }
-
     ToolBox tb = new ToolBox();
+    Path base;
 
-    void run() throws Exception {
-        testCorrectSource(Paths.get("."));
-    }
-
-    void testCorrectSource(Path base) throws Exception {
+    @Test
+    public void testCorrectSource() throws Exception {
         Path src = base.resolve("src");
         Path classes = base.resolve("classes");
         tb.writeJavaFiles(src,
                           """
                           package test;
-                          
+
+                          @Deprecated(forRemoval=true)
+                          public class Depr {
+                          }
+                          """,
+                          """
+                          package test;
+                          public class Use {
+                              Implicit implicit;
+                              Depr depr;
+                          }
+                          """,
+                          """
+                          package test;
+                          public interface Implicit {}
+                          """);
+        Files.createDirectories(classes);
+
+        List<String> log = new JavacTask(tb)
+            .options("-d", classes.toString(),
+                     "-XDrawDiagnostics",
+                     "-implicit:class",
+                     "-sourcepath", src.toString())
+            .files(src.resolve("test").resolve("Depr.java"),
+                    src.resolve("test").resolve("Use.java"))
+            .processors(new ProcessorImpl())
+            .run()
+            .writeAll()
+            .getOutputLines(Task.OutputKind.DIRECT);
+
+        List<String> expected = List.of(
+            "Use.java:4:5: compiler.warn.has.been.deprecated.for.removal: test.Depr, test",
+            "Use.java:4:5: compiler.warn.has.been.deprecated.for.removal: test.Depr, test",
+            "Use.java:4:5: compiler.warn.has.been.deprecated.for.removal: test.Depr, test",
+            "3 warnings"
+        );
+
+        tb.checkEqual(expected, log);
+    }
+
+    @Test
+    public void testCorrectSuppress() throws Exception {
+        Path src = base.resolve("src");
+        Path classes = base.resolve("classes");
+        tb.writeJavaFiles(src,
+                          """
+                          package test;
+
                           @Deprecated(forRemoval=true)
                           public class Depr {
                           }
@@ -85,8 +131,12 @@ public class APImplicitClassesWarnings {
         Files.createDirectories(classes);
 
         new JavacTask(tb)
-                .options("-d", classes.toString(), "-Werror", "-sourcepath", src.toString(), "-implicit:class")
-                .files(src.resolve("test").resolve("Depr.java"), src.resolve("test").resolve("Use.java"))
+                .options("-d", classes.toString(),
+                         "-Werror",
+                         "-implicit:class",
+                         "-sourcepath", src.toString())
+                .files(src.resolve("test").resolve("Depr.java"),
+                       src.resolve("test").resolve("Use.java"))
                 .processors(new ProcessorImpl())
                 .run()
                 .writeAll();
@@ -102,5 +152,11 @@ public class APImplicitClassesWarnings {
         public SourceVersion getSupportedSourceVersion() {
             return SourceVersion.latest();
         }
+    }
+
+    @BeforeEach
+    public void setUp(TestInfo info) {
+        base = Paths.get(".")
+                    .resolve(info.getTestMethod().orElseThrow().getName());
     }
 }
