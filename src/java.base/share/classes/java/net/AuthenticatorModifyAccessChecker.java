d8e89dbe9d6685622cc055309047f8bd6339b947
Vishva N <72214543+vishva238@users.noreply.github.com>
2026-03-03 23:27:25 +0530

    Remove blank line in AuthenticatorModifyAccessChecker
    Removed unnecessary blank line in AuthenticatorModifyAccessChecker.

--- src/java.base/share/classes/java/net/AuthenticatorModifyAccessChecker.java
+++ src/java.base/share/classes/java/net/AuthenticatorModifyAccessChecker.java
@@ -28,7 +28,6 @@ package java.net;
 public class AuthenticatorModifyAccessChecker {
     public AuthenticatorModifyAccessChecker() {
     }
-    
     public boolean canModifyAuthenticator(Authenticator a) {
         return true;
     }
