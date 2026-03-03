afd1aec542d6963327780ecf7a19bb06716c27a7
Liam Miller-Cushon <cushon@google.com>
2026-03-03 09:40:43 +0100

    Remove discussion of allocation from apiNote

--- src/java.base/share/classes/java/lang/String.java
+++ src/java.base/share/classes/java/lang/String.java
@@ -2111,7 +2111,7 @@ public final class String
      * as {@link #getBytes(Charset) getBytes(cs).length}.
      *
      * @apiNote This method provides equivalent or better performance than {@link #getBytes(Charset)
-     *          getBytes(cs).length}. This method may allocate memory to compute the length for some charsets.
+     *          getBytes(cs).length}.
      *
      * @param cs The {@link Charset} used to the compute the length
      * @since 27
