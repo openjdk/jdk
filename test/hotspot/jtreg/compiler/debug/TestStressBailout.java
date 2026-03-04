9a7e3745a5b82ef9851864562f0bc162d3ad9b9a
Daniel Skantz <daniel.skantz@oracle.com>
2026-03-04 08:22:32 +0000

    Add new configuration to existing test

--- test/hotspot/jtreg/compiler/debug/TestStressBailout.java
+++ test/hotspot/jtreg/compiler/debug/TestStressBailout.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,6 +24,7 @@
 package compiler.debug;
 
 import java.util.Random;
+import java.util.stream.Stream;
 
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
@@ -36,24 +37,33 @@ import jdk.test.lib.Utils;
  * @requires vm.debug == true & vm.compiler2.enabled & (vm.opt.AbortVMOnCompilationFailure == "null" | !vm.opt.AbortVMOnCompilationFailure)
  * @summary Basic tests for bailout stress flag.
  * @library /test/lib /
- * @run driver compiler.debug.TestStressBailout
+ * @run main compiler.debug.TestStressBailout
+ */
+
+/*
+ * @test
+ * @key stress randomness
+ * @bug 8375905
+ * @requires vm.debug == true & vm.compiler2.enabled & (vm.opt.AbortVMOnCompilationFailure == "null" | !vm.opt.AbortVMOnCompilationFailure)
+ * @summary Additional crashes revealed by diagnostic code run during VerifyIterativeGVN
+ * @library /test/lib /
+ * @run main/othervm compiler.debug.TestStressBailout -XX:VerifyIterativeGVN=1111
  */
 
 public class TestStressBailout {
 
-    static void runTest(int invprob) throws Exception {
-        String[] procArgs = {"-Xcomp", "-XX:-TieredCompilation", "-XX:+StressBailout",
-                             "-XX:StressBailoutMean=" + invprob, "-version"};
-        ProcessBuilder pb  = ProcessTools.createTestJavaProcessBuilder(procArgs);
+    static void runTest(int invprob, Stream<String> vmArgs) throws Exception {
+        Stream<String> procArgs = Stream.of("-Xcomp", "-XX:-TieredCompilation", "-XX:+StressBailout", "-XX:StressBailoutMean=" + invprob, "-version");
+        ProcessBuilder pb  = ProcessTools.createTestJavaProcessBuilder(Stream.concat(vmArgs, procArgs).toArray(x -> new String[x]));
         OutputAnalyzer out = new OutputAnalyzer(pb.start());
         out.shouldHaveExitValue(0);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] vmArgs) throws Exception {
         Random r = Utils.getRandomInstance();
         // Likely bail out on -version, for some low Mean value.
-        runTest(r.nextInt(1, 10));
+        runTest(r.nextInt(1, 10), Stream.of(vmArgs));
         // Higher value
-        runTest(r.nextInt(10, 1_000_000));
+        runTest(r.nextInt(10, 1_000_000), Stream.of(vmArgs));
     }
 }
