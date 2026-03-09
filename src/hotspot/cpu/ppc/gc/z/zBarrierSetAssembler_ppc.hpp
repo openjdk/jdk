8421f9d12f66dc50b8898acd10bca454d9762f03
TheRealMDoerr <martin.doerr@sap.com>
2026-03-09 16:45:09 +0100

    Add missing volatile specifier.

--- src/hotspot/cpu/ppc/gc/z/zBarrierSetAssembler_ppc.hpp
+++ src/hotspot/cpu/ppc/gc/z/zBarrierSetAssembler_ppc.hpp
@@ -72,7 +72,7 @@ public:
   virtual void try_resolve_jobject_in_native(MacroAssembler* masm, Register dst, Register jni_env,
                                              Register obj, Register tmp, Label& slowpath);
 
-  void try_resolve_weak_handle(MacroAssembler* masm, Register obj, Register tmp, Label& slow_path);
+  virtual void try_resolve_weak_handle(MacroAssembler* masm, Register obj, Register tmp, Label& slow_path);
 
   virtual void check_oop(MacroAssembler *masm, Register obj, const char* msg);
 
