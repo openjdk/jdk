b262ab72a1b3648b2b24bc2fdc109cf58e5f3815
Anton Artemov <anton.artemov@oracle.com>
2026-03-11 13:50:04 +0100

    8294152: Added assert for x64.

--- src/hotspot/cpu/x86/frame_x86.inline.hpp
+++ src/hotspot/cpu/x86/frame_x86.inline.hpp
@@ -232,7 +232,7 @@ inline bool frame::equal(frame other) const {
 // Return unique id for this frame. The id must have a value where we can distinguish
 // identity and younger/older relationship. null represents an invalid (incomparable)
 // frame.
-inline intptr_t* frame::id(void) const { return unextended_sp(); }
+inline intptr_t* frame::id(void) const { return real_fp(); }
 
 // Return true if the frame is older (less recent activation) than the frame represented by id
 inline bool frame::is_older(intptr_t* id) const   { assert(this->id() != nullptr && id != nullptr, "null frame id");
@@ -396,6 +396,9 @@ inline frame frame::sender(RegisterMap* map) const {
   if (map->process_frames() && !map->in_cont()) {
     StackWatermarkSet::on_iteration(map->thread(), result);
   }
+
+  // Calling frame::id() is currently not supported for heap frames.
+  assert(result._on_heap || this->_on_heap || result.is_older(this->id()), "Must be");
 
   return result;
 }
