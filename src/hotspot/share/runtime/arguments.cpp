6d88cdf610feed0409ef666946cd8fa9053be6e4
Ashutosh Mehra <asmehra@redhat.com>
2026-03-17 10:49:00 -0400

    8380168: Deprecate unused flag UseNewLongLShift
    Signed-off-by: Ashutosh Mehra <asmehra@redhat.com>

--- src/hotspot/share/runtime/arguments.cpp
+++ src/hotspot/share/runtime/arguments.cpp
@@ -537,6 +537,7 @@ static SpecialFlag const special_jvm_flags[] = {
   { "UseCompressedClassPointers",   JDK_Version::jdk(25),  JDK_Version::jdk(27), JDK_Version::undefined() },
 #endif
   { "AggressiveHeap",               JDK_Version::jdk(26),  JDK_Version::jdk(27), JDK_Version::jdk(28) },
+  { "UseNewLongLShift",             JDK_Version::jdk(27),  JDK_Version::jdk(28), JDK_Version::jdk(29) },
   // --- Deprecated alias flags (see also aliased_jvm_flags) - sorted by obsolete_in then expired_in:
   { "CreateMinidumpOnCrash",        JDK_Version::jdk(9),  JDK_Version::undefined(), JDK_Version::undefined() },
 
