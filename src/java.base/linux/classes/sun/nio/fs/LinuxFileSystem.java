940203f7ebfd08c1a5e51dea361b36c9d577d313
Brian Burkhalter <brian.burkhalter@oracle.com>
2026-03-19 15:56:26 -0700

    8379583: (fs) Files.copy use of posix_fadvise is problematic on Linux

--- src/java.base/linux/classes/sun/nio/fs/LinuxFileSystem.java
+++ src/java.base/linux/classes/sun/nio/fs/LinuxFileSystem.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -134,15 +134,23 @@ class LinuxFileSystem extends UnixFileSystem {
 
     // --- file copying ---
 
+    private static void fadvise(int fd) throws UnixException {
+        // sequential data access
+        posix_fadvise(fd, 0, 0, POSIX_FADV_SEQUENTIAL);
+
+        // will access only once
+        posix_fadvise(fd, 0, 0, POSIX_FADV_NOREUSE);
+
+        // will access in near future
+        posix_fadvise(fd, 0, 0, POSIX_FADV_WILLNEED);
+    }
+
     @Override
     void bufferedCopy(int dst, int src, long address,
                       int size, long addressToPollForCancel)
         throws UnixException
     {
-        int advice = POSIX_FADV_SEQUENTIAL | // sequential data access
-                     POSIX_FADV_NOREUSE    | // will access only once
-                     POSIX_FADV_WILLNEED;    // will access in near future
-        posix_fadvise(src, 0, 0, advice);
+        fadvise(src);
 
         super.bufferedCopy(dst, src, address, size, addressToPollForCancel);
     }
@@ -151,10 +159,7 @@ class LinuxFileSystem extends UnixFileSystem {
     int directCopy(int dst, int src, long addressToPollForCancel)
         throws UnixException
     {
-        int advice = POSIX_FADV_SEQUENTIAL | // sequential data access
-                     POSIX_FADV_NOREUSE    | // will access only once
-                     POSIX_FADV_WILLNEED;    // will access in near future
-        posix_fadvise(src, 0, 0, advice);
+        fadvise(src);
 
         return directCopy0(dst, src, addressToPollForCancel);
     }
