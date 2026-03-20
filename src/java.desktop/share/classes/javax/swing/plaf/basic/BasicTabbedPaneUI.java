6a03078b9d9d2f4667de70d4266e4cc8e96c76b8
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2026-03-20 04:20:19 +0100

    Some more formatting fix

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
@@ -950,7 +950,7 @@ public class BasicTabbedPaneUI extends TabbedPaneUI implements SwingConstants {
         for (int i = runCount - 1; i >= 0; i--) {
             int start = tabRuns[i];
             int next = tabRuns[(i == runCount - 1) ? 0 : i + 1];
-            int end = (next != 0 ? next - 1: tabCount - 1);
+            int end = (next != 0 ? next - 1 : tabCount - 1);
             for (int j = start; j <= end; j++) {
                 if (j != selectedIndex && rects[j].intersects(clipRect)) {
                     paintTab(g, tabPlacement, rects, j, iconRect, textRect);
