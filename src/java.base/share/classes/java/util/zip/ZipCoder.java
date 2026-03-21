c55ff71045c615965e9aefa6e9922e57be113385
Eirik Bjorsnos <eirbjo@gmail.com>
2026-03-21 09:51:38 +0100

    Align ZipFS ZipCoder.toString and ZipCoder.getBytes implementations with that in java.util.zip

--- src/java.base/share/classes/java/util/zip/ZipCoder.java
+++ src/java.base/share/classes/java/util/zip/ZipCoder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2009, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -101,12 +101,10 @@ class ZipCoder {
     byte[] getBytes(String s) {
         try {
             ByteBuffer bb = encoder().encode(CharBuffer.wrap(s));
-            int pos = bb.position();
-            int limit = bb.limit();
-            if (bb.hasArray() && pos == 0 && limit == bb.capacity()) {
+            if (bb.hasArray() && bb.remaining() == bb.capacity()) {
                 return bb.array();
             }
-            byte[] bytes = new byte[bb.limit() - bb.position()];
+            byte[] bytes = new byte[bb.remaining()];
             bb.get(bytes);
             return bytes;
         } catch (CharacterCodingException x) {
