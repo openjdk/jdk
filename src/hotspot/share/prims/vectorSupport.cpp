034364977c8872020b4b84e2c78ee7ead36701f0
Jatin Bhateja <jatin.bhateja@amd.com>
2026-03-24 16:52:48 +0000

    Review comment resolution

--- src/hotspot/share/prims/vectorSupport.cpp
+++ src/hotspot/share/prims/vectorSupport.cpp
@@ -200,7 +200,6 @@ bool VectorSupport::is_unsigned_op(jint id) {
 }
 
 const char* VectorSupport::lanetype2name(LaneType lane_type) {
-  assert(lane_type >= LT_FLOAT && lane_type <= LT_LONG, "");
   const char* lanetype2name[] = {
     "float",
     "double",
@@ -212,6 +211,7 @@ const char* VectorSupport::lanetype2name(LaneType lane_type) {
   if (lane_type >= LT_FLOAT && lane_type <= LT_LONG) {
     return lanetype2name[lane_type];
   }
+  assert(false, "unknown lane type: %d", (int)lane_type);
   return "illegal";
 }
 
