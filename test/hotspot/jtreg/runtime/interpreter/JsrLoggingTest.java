b80ace9680cdadd6773261bf4bed51661e773e19
Coleen Phillimore <coleen.phillimore@oracle.com>
2026-03-24 15:02:14 -0400

    Fix test, rename is_linked to use_cp_cache.

--- test/hotspot/jtreg/runtime/interpreter/JsrLoggingTest.java
+++ test/hotspot/jtreg/runtime/interpreter/JsrLoggingTest.java
@@ -32,7 +32,7 @@
 
 public class JsrLoggingTest {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < 10; ++i) {
             JsrLogging.test();
         }
         System.out.println("PASSED");
