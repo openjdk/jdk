f1a817d925b7c7b955bc93785560a8b3d8cac5cf
Prasanta Sadhukhan <prasanta.sadhukhan@oracle.com>
2026-03-25 08:15:57 +0100

    Review comments

--- src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
+++ src/java.desktop/share/classes/javax/swing/plaf/basic/BasicTabbedPaneUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -4090,7 +4090,7 @@ public class BasicTabbedPaneUI extends TabbedPaneUI implements SwingConstants {
                     setHtmlView(v, inserted, index);
                 }
             } else {                             // Not HTML
-                if (htmlViews != null) {           // Add placeholder
+                if (htmlViews != null) {         // Add placeholder
                     setHtmlView(null, inserted, index);
                 }                                // else nada!
             }
