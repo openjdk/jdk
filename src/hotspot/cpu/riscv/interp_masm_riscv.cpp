a2c99d9a9d54c1a36c49ec8bbfdf08c39c56b776
Paul Hübner <phubner@openjdk.org>
2026-03-26 14:09:17 +0000

    8370504: InterpreterMacroAssembler::profile_virtual_call parameter 'receiver_can_be_null' is unused
    Reviewed-by: aseoane, azafari, mchevalier, fbredberg

--- src/hotspot/cpu/riscv/interp_masm_riscv.cpp
+++ src/hotspot/cpu/riscv/interp_masm_riscv.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2026, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2014, 2020, Red Hat Inc. All rights reserved.
  * Copyright (c) 2020, 2023, Huawei Technologies Co., Ltd. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
@@ -1040,26 +1040,15 @@ void InterpreterMacroAssembler::profile_final_call(Register mdp) {
 
 
 void InterpreterMacroAssembler::profile_virtual_call(Register receiver,
-                                                     Register mdp,
-                                                     bool receiver_can_be_null) {
+                                                     Register mdp) {
   if (ProfileInterpreter) {
     Label profile_continue;
 
     // If no method data exists, go to profile_continue.
     test_method_data_pointer(mdp, profile_continue);
 
-    Label skip_receiver_profile;
-    if (receiver_can_be_null) {
-      Label not_null;
-      // We are making a call.  Increment the count for null receiver.
-      increment_mdp_data_at(mdp, in_bytes(CounterData::count_offset()));
-      j(skip_receiver_profile);
-      bind(not_null);
-    }
-
     // Record the receiver type.
     profile_receiver_type(receiver, mdp, 0);
-    bind(skip_receiver_profile);
 
     // The method data pointer needs to be updated to reflect the new target.
 
