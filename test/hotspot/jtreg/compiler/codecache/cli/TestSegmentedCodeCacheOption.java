ead183bff2beae893af2f6ffb25c5b323cc68136
Chad Rakoczy <chadrako@amazon.com>
2026-03-26 16:25:13 +0000

    Remove the client emulation mode

--- test/hotspot/jtreg/compiler/codecache/cli/TestSegmentedCodeCacheOption.java
+++ test/hotspot/jtreg/compiler/codecache/cli/TestSegmentedCodeCacheOption.java
@@ -172,7 +172,7 @@ public class TestSegmentedCodeCacheOption {
         OPTION_VALUES_HOT {
             @Override
             public boolean isApplicable() {
-                return Platform.isServer() && !Platform.isEmulatedClient();
+                return Platform.isServer();
             }
 
             @Override
