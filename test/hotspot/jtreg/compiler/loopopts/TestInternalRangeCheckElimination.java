66b94a0dacc29b46c8dbc0b42b472d38aeb99fdd
Michael Reeves <michael.reeves077@gmail.com>
2026-03-30 22:25:29 +1100

    address reviewer feedback 2

--- test/hotspot/jtreg/compiler/loopopts/TestInternalRangeCheckElimination.java
+++ test/hotspot/jtreg/compiler/loopopts/TestInternalRangeCheckElimination.java
@@ -28,6 +28,7 @@ import compiler.lib.generators.RestrictableGenerator;
 import compiler.lib.ir_framework.*;
 import java.util.Arrays;
 import java.util.List;
+import jdk.test.lib.Asserts;
 import jdk.incubator.vector.IntVector;
 import jdk.incubator.vector.VectorMask;
 import jdk.incubator.vector.VectorSpecies;
@@ -44,38 +45,72 @@ public class TestInternalRangeCheckElimination {
     private static final int[] SRC = new int[1024];
     private static final int[] DST = new int[1024];
     private static final int SENTINEL = -999_999_937;
+    private static final int RANDOM_ITERATIONS = 200;
     private static final Generators RND = Generators.G;
+
+    private static final List<Integer> BASIC_LIMIT_SPECIALS = List.of(
+            0, 1, 2, 7, 8, 15, 16, 17,
+            31, 32, 33, 63, 64, 65, 127, 128, 129,
+            255, 256, 257, 319, 320, 321, 511, 512);
+
+    private static final List<Integer> LENGTH_SPECIALS = List.of(
+            0, 1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64);
 
     private static final RestrictableGenerator<Integer> BASIC_LIMIT_GEN =
         RND.mixed(
-            RND.orderedRandomElement(List.of(0, 1, 2, 7, 8, 15, 16, 17,
-                             31, 32, 33, 63, 64, 65, 127, 128, 129,
-                             255, 256, 257, 319, 320, 321, 511, 512)),
+            RND.orderedRandomElement(BASIC_LIMIT_SPECIALS),
             RND.safeRestrict(RND.ints(), 0, 512),
             3, 2);
 
     private static final RestrictableGenerator<Integer> LENGTH_GEN =
         RND.mixed(
-            RND.orderedRandomElement(List.of(0, 1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64)),
+            RND.orderedRandomElement(LENGTH_SPECIALS),
             RND.safeRestrict(RND.ints(), 0, 64),
             3, 2);
 
-    private static int lastBasicLimit;
-    private static int lastBasicOffset;
-    private static int lastFalseHotLimit;
-    private static int lastFalseHotOffset;
-    private static int lastIndexInRangeLikeLimit;
-    private static int lastIndexInRangeLikeLength;
-    private static int lastVectorLimit;
-    private static int lastVectorSpeciesLength;
+    private static final List<Integer> OFFSET_SPECIALS = List.of(
+            0, 1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64);
 
     static {
         RND.fill(RND.safeRestrict(RND.ints(), -100_000, 100_000), SRC);
     }
 
+    private static int nextOffset(int limit) {
+        int maxOffset = Math.min(limit, 64);
+        RestrictableGenerator<Integer> offsetGen = RND.mixed(
+                RND.orderedRandomElement(OFFSET_SPECIALS),
+                RND.safeRestrict(RND.ints(), 0, maxOffset),
+                3, 2);
+        return RND.safeRestrict(offsetGen, 0, maxOffset).next();
+    }
+
     public static void main(String[] args) {
         TestFramework.runWithFlags("-XX:LoopUnrollLimit=0",
                                    "--add-modules=jdk.incubator.vector");
+    }
+
+    private static List<Integer> vectorLimitSpecials(int len) {
+        return List.of(
+                0,
+                1,
+                Math.max(1, len - 1),
+                len,
+                len + 1,
+                Math.max(0, 2 * len - 1),
+                2 * len,
+                2 * len + 1,
+                255,
+                256,
+                257,
+                319
+        );
+    }
+
+    private static RestrictableGenerator<Integer> vectorLimitGen(int len) {
+        return RND.mixed(
+                RND.orderedRandomElement(vectorLimitSpecials(len)),
+                RND.safeRestrict(RND.ints(), 0, 320),
+                4, 1);
     }
 
     @DontCompile
@@ -117,23 +152,8 @@ public class TestInternalRangeCheckElimination {
         return expected;
     }
 
-    @Setup
-    public static Object[] setupBasic(SetupInfo info) {
-        int limit = BASIC_LIMIT_GEN.next();
-        int maxOffset = Math.min(limit, 64);
-        RestrictableGenerator<Integer> offsetGen = RND.mixed(
-                RND.orderedRandomElement(List.of(0, 1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64)),
-                RND.safeRestrict(RND.ints(), 0, maxOffset),
-                3, 2);
-        int offset = RND.safeRestrict(offsetGen, 0, maxOffset).next();
-        return new Object[] {limit, offset};
-    }
-
     @Test
-    @Arguments(setup = "setupBasic")
     public static int basicInternalIf(int limit, int offset) {
-        lastBasicLimit = limit;
-        lastBasicOffset = offset;
         int sum = 0;
         int bound = limit - offset;
         for (int i = 0; i < limit; i++) {
@@ -146,34 +166,32 @@ public class TestInternalRangeCheckElimination {
         return sum;
     }
 
-    @Check(test = "basicInternalIf")
-    public static void checkBasicInternalIf(int actual) {
-        int expected = basicInternalIfRef(lastBasicLimit, lastBasicOffset);
-        if (actual != expected) {
-            throw new RuntimeException("basicInternalIf mismatch: actual=" + actual
-                                       + " expected=" + expected
-                                       + " limit=" + lastBasicLimit
-                                       + " offset=" + lastBasicOffset);
+    private static void checkBasicInternalIf(int limit, int offset) {
+        int actual = basicInternalIf(limit, offset);
+        int expected = basicInternalIfRef(limit, offset);
+        Asserts.assertEQ(actual, expected,
+                "basicInternalIf mismatch: limit=" + limit + " offset=" + offset);
+    }
+
+    @Run(test = "basicInternalIf")
+    public void runBasicInternalIf() {
+        for (int limit : BASIC_LIMIT_SPECIALS) {
+            int maxOffset = Math.min(limit, 64);
+            for (int offset : OFFSET_SPECIALS) {
+                if (offset <= maxOffset) {
+                    checkBasicInternalIf(limit, offset);
+                }
+            }
+        }
+        for (int i = 0; i < RANDOM_ITERATIONS; i++) {
+            int limit = BASIC_LIMIT_GEN.next();
+            int offset = nextOffset(limit);
+            checkBasicInternalIf(limit, offset);
         }
     }
 
-    @Setup
-    public static Object[] setupFalseHot(SetupInfo info) {
-        int limit = BASIC_LIMIT_GEN.next();
-        int maxOffset = Math.min(limit, 64);
-        RestrictableGenerator<Integer> offsetGen = RND.mixed(
-                RND.orderedRandomElement(List.of(0, 1, 2, 3, 7, 8, 15, 16, 31, 32, 63, 64)),
-                RND.safeRestrict(RND.ints(), 0, maxOffset),
-                3, 2);
-        int offset = RND.safeRestrict(offsetGen, 0, maxOffset).next();
-        return new Object[] {limit, offset};
-    }
-
     @Test
-    @Arguments(setup = "setupFalseHot")
     public static int internalIfFalseHot(int limit, int offset) {
-        lastFalseHotLimit = limit;
-        lastFalseHotOffset = offset;
         int sum = 0;
         int bound = limit - offset;
         for (int i = 0; i < limit; i++) {
@@ -186,29 +204,32 @@ public class TestInternalRangeCheckElimination {
         return sum;
     }
 
-    @Check(test = "internalIfFalseHot")
-    public static void checkInternalIfFalseHot(int actual) {
-        int expected = internalIfFalseHotRef(lastFalseHotLimit, lastFalseHotOffset);
-        if (actual != expected) {
-            throw new RuntimeException("internalIfFalseHot mismatch: actual=" + actual
-                                       + " expected=" + expected
-                                       + " limit=" + lastFalseHotLimit
-                                       + " offset=" + lastFalseHotOffset);
+    private static void checkInternalIfFalseHot(int limit, int offset) {
+        int actual = internalIfFalseHot(limit, offset);
+        int expected = internalIfFalseHotRef(limit, offset);
+        Asserts.assertEQ(actual, expected,
+                "internalIfFalseHot mismatch: limit=" + limit + " offset=" + offset);
+    }
+
+    @Run(test = "internalIfFalseHot")
+    public void runInternalIfFalseHot() {
+        for (int limit : BASIC_LIMIT_SPECIALS) {
+            int maxOffset = Math.min(limit, 64);
+            for (int offset : OFFSET_SPECIALS) {
+                if (offset <= maxOffset) {
+                    checkInternalIfFalseHot(limit, offset);
+                }
+            }
+        }
+        for (int i = 0; i < RANDOM_ITERATIONS; i++) {
+            int limit = BASIC_LIMIT_GEN.next();
+            int offset = nextOffset(limit);
+            checkInternalIfFalseHot(limit, offset);
         }
     }
 
-    @Setup
-    public static Object[] setupIndexInRange(SetupInfo info) {
-        int limit = RND.safeRestrict(BASIC_LIMIT_GEN, 0, 512).next();
-        int length = LENGTH_GEN.next();
-        return new Object[] {limit, length};
-    }
-
     @Test
-    @Arguments(setup = "setupIndexInRange")
     public static int indexInRangeLike(int limit, int length) {
-        lastIndexInRangeLikeLimit = limit;
-        lastIndexInRangeLikeLength = length;
         int acc = 0;
         for (int i = 0; i < limit; i++) {
             boolean mask = (limit - i) >= length;
@@ -222,44 +243,28 @@ public class TestInternalRangeCheckElimination {
         return acc;
     }
 
-    @Check(test = "indexInRangeLike")
-    public static void checkIndexInRangeLike(int actual) {
-        int expected = indexInRangeLikeRef(lastIndexInRangeLikeLimit, lastIndexInRangeLikeLength);
-        if (actual != expected) {
-            throw new RuntimeException("indexInRangeLike mismatch: actual=" + actual
-                                       + " expected=" + expected
-                                       + " limit=" + lastIndexInRangeLikeLimit
-                                       + " length=" + lastIndexInRangeLikeLength);
+    private static void checkIndexInRangeLike(int limit, int length) {
+        int actual = indexInRangeLike(limit, length);
+        int expected = indexInRangeLikeRef(limit, length);
+        Asserts.assertEQ(actual, expected,
+                "indexInRangeLike mismatch: limit=" + limit + " length=" + length);
+    }
+
+    @Run(test = "indexInRangeLike")
+    public void runIndexInRangeLike() {
+        for (int limit : BASIC_LIMIT_SPECIALS) {
+            for (int length : LENGTH_SPECIALS) {
+                checkIndexInRangeLike(limit, length);
+            }
+        }
+        for (int i = 0; i < RANDOM_ITERATIONS; i++) {
+            int limit = RND.safeRestrict(BASIC_LIMIT_GEN, 0, 512).next();
+            int length = LENGTH_GEN.next();
+            checkIndexInRangeLike(limit, length);
         }
     }
 
-    @Setup
-    public static Object[] setupVectorIndexInRange(SetupInfo info) {
-        int len = IntVector.SPECIES_PREFERRED.length();
-        List<Integer> cases = List.of(
-                0,
-                1,
-                Math.max(1, len - 1),
-                len,
-                len + 1,
-                Math.max(0, 2 * len - 1),
-                2 * len,
-                2 * len + 1,
-                255,
-                256,
-                257,
-                319
-        );
-        RestrictableGenerator<Integer> vectorLimitGen = RND.mixed(
-            RND.orderedRandomElement(cases),
-            RND.safeRestrict(RND.ints(), 0, 320),
-            4, 1);
-        int limit = vectorLimitGen.next();
-        return new Object[] {limit};
-    }
-
     @Test
-    @Arguments(setup = "setupVectorIndexInRange")
     @IR(counts = {IRNode.LOAD_VECTOR_I, ">= 1",
                   IRNode.STORE_VECTOR, ">= 1",
                   IRNode.VECTOR_STORE_MASK, ">= 1"},
@@ -268,8 +273,6 @@ public class TestInternalRangeCheckElimination {
         Arrays.fill(DST, SENTINEL);
         int acc = 0;
         VectorSpecies<Integer> species = IntVector.SPECIES_PREFERRED;
-        lastVectorLimit = limit;
-        lastVectorSpeciesLength = species.length();
         for (int i = 0; i < limit; i += species.length()) {
             VectorMask<Integer> m = species.indexInRange(i, limit);
             IntVector v = IntVector.fromArray(species, SRC, i, m);
@@ -279,28 +282,32 @@ public class TestInternalRangeCheckElimination {
         return acc;
     }
 
-    @Check(test = "vectorIndexInRange")
-    public static void checkVectorIndexInRange(int actual) {
-        int expected = vectorIndexInRangeRef(lastVectorLimit, lastVectorSpeciesLength);
-        if (actual != expected) {
-            throw new RuntimeException("vectorIndexInRange mismatch: actual=" + actual
-                                       + " expected=" + expected
-                                       + " limit=" + lastVectorLimit
-                                       + " speciesLen=" + lastVectorSpeciesLength);
-        }
-        for (int i = 0; i < lastVectorLimit; i++) {
+    private static void checkVectorIndexInRange(int limit) {
+        int actual = vectorIndexInRange(limit);
+        int speciesLen = IntVector.SPECIES_PREFERRED.length();
+        int expected = vectorIndexInRangeRef(limit, speciesLen);
+        Asserts.assertEQ(actual, expected,
+                "vectorIndexInRange mismatch: limit=" + limit + " speciesLen=" + speciesLen);
+        for (int i = 0; i < limit; i++) {
             int expectedValue = SRC[i] + 1;
-            if (DST[i] != expectedValue) {
-                throw new RuntimeException("vectorIndexInRange wrong lane at index=" + i
-                                           + " got=" + DST[i]
-                                           + " expected=" + expectedValue);
-            }
+            Asserts.assertEQ(DST[i], expectedValue,
+                    "vectorIndexInRange wrong lane at index=" + i);
         }
-        for (int i = lastVectorLimit; i < DST.length; i++) {
-            if (DST[i] != SENTINEL) {
-                throw new RuntimeException("vectorIndexInRange wrote past limit at index=" + i
-                                           + " value=" + DST[i]);
-            }
+        for (int i = limit; i < DST.length; i++) {
+            Asserts.assertEQ(DST[i], SENTINEL,
+                    "vectorIndexInRange wrote past limit at index=" + i);
+        }
+    }
+
+    @Run(test = "vectorIndexInRange")
+    public void runVectorIndexInRange() {
+        int len = IntVector.SPECIES_PREFERRED.length();
+        for (int limit : vectorLimitSpecials(len)) {
+            checkVectorIndexInRange(limit);
+        }
+        RestrictableGenerator<Integer> vlimitGen = vectorLimitGen(len);
+        for (int i = 0; i < RANDOM_ITERATIONS; i++) {
+            checkVectorIndexInRange(vlimitGen.next());
         }
     }
 }
