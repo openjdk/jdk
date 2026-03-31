9cdda5659e58bd735e426771ed371c06ca0eb10e
Shiv Shah <shiv.shah@oracle.com>
2026-03-31 11:13:21 -0500

    8338594: Update nsk/aod tests to use driver instead of othervm

--- test/hotspot/jtreg/vmTestbase/nsk/share/aod/AODTestRunner.java
+++ test/hotspot/jtreg/vmTestbase/nsk/share/aod/AODTestRunner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2008, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -103,9 +103,11 @@ public class AODTestRunner {
             /*
              * Create target application id required by the Utils.findVMIdUsingJPS
              */
+            String classPath = System.getProperty("test.class.path");
             String targetAppCmd =
                     // path to java
                     argParser.getTestedJDK() + File.separator + "bin" + File.separator + "java " +
+                            (classPath != null ? "-classpath " + classPath + " " : "") +
                             // VM property to identify VM running target application
                             "-D" + appIdProperty + "=" + targetAppId +  " " +
                             // VM opts
