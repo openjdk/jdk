775978a0013175e1a96644ed32748d41f4648e2a
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2026-04-06 13:45:18 -0400

    Removed unused method declaration

--- src/hotspot/share/oops/method.hpp
+++ src/hotspot/share/oops/method.hpp
@@ -269,7 +269,6 @@ class Method : public Metadata {
   inline int interpreter_throwout_count() const;
 
   u2 size_of_parameters() const { return constMethod()->size_of_parameters(); }
-  u2 number_of_parameters() const;
 
   bool has_stackmap_table() const {
     return constMethod()->has_stackmap_table();
