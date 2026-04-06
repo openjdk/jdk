159e96604fa9314da42db4066d3adf5ce07dc6f2
shun <shunengineer2507@outlook.jp>
2026-04-06 22:39:33 +0900

    JDK-8200473: Use Objects.equals() instead of != for String comparison

--- src/java.base/share/classes/sun/security/validator/Validator.java
+++ src/java.base/share/classes/sun/security/validator/Validator.java
@@ -265,7 +265,7 @@ public abstract sealed class Validator permits PKIXValidator, SimpleValidator {
             // included in the PKIXParameters, so the extra checks would be
             // redundant.
             boolean checkUnresolvedCritExts =
-                    type != TYPE_PKIX;
+                    !Objects.equals(type, TYPE_PKIX);
             endEntityChecker.check(chain, parameter,
                                    checkUnresolvedCritExts);
         }
