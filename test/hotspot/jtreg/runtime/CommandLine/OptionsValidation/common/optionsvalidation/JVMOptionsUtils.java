ce685c6f31ce59edc62e17525a39c8773f037fb1
Manuel Hässig <manuel.hassig@oracle.com>
2026-04-08 13:19:26 +0200

    Delete the obvious files

--- test/hotspot/jtreg/runtime/CommandLine/OptionsValidation/common/optionsvalidation/JVMOptionsUtils.java
+++ test/hotspot/jtreg/runtime/CommandLine/OptionsValidation/common/optionsvalidation/JVMOptionsUtils.java
@@ -188,10 +188,6 @@ public class JVMOptionsUtils {
             option.addPrepend("-XX:+UseNUMA");
         }
 
-        if (name.contains("JVMCI")) {
-            option.addPrepend("-XX:+EnableJVMCI");
-        }
-
         switch (name) {
             case "MinHeapFreeRatio":
                 option.addPrepend("-XX:MaxHeapFreeRatio=100");
@@ -226,12 +222,6 @@ public class JVMOptionsUtils {
                 break;
             case "TLABWasteIncrement":
                 option.addPrepend("-XX:+UseParallelGC");
-                break;
-            case "BootstrapJVMCI":
-            case "PrintBootstrap":
-            case "JVMCIThreads":
-            case "JVMCIHostThreads":
-                option.addPrepend("-XX:+UseJVMCICompiler");
                 break;
             default:
                 /* Do nothing */
