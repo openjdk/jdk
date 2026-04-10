1e8ffb6cb947984501595d57b8722fb8e7c78dd9
Matthew Donovan <matthew.p.donovan@oracle.com>
2026-04-10 13:08:01 -0400

    8374454: Test sun/security/ssl/CipherSuite/DisabledCipherSuitesNotNegotiated.java from JDK-8356544 shows intermittent timeouts

--- test/jdk/sun/security/ssl/CipherSuite/DisabledCipherSuitesNotNegotiated.java
+++ test/jdk/sun/security/ssl/CipherSuite/DisabledCipherSuitesNotNegotiated.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -88,7 +88,7 @@ public class DisabledCipherSuitesNotNegotiated {
         SSLContext ctx = SSLContext.getInstance(TLS_PROTOCOL);
         ctx.init(null, null, null);
         SSLSocketFactory factory = ctx.getSocketFactory();
-        try(SSLSocket socket = (SSLSocket)factory.createSocket("localhost", portNumber)) {
+        try(SSLSocket socket = (SSLSocket)factory.createSocket(InetAddress.getLoopbackAddress(), portNumber)) {
             if (!disableInClient) {
                 socket.setEnabledCipherSuites(new String[]{DISABLED_CIPHERSUITE});
             }
