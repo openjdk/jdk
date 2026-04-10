277272a9d32f0874ce11f46bda103d3a62404944
Albert Yang <albert.m.yang@oracle.com>
2026-04-11 00:20:56 +0200

    g1-refine

--- src/hotspot/share/gc/g1/g1CollectedHeap.cpp
+++ src/hotspot/share/gc/g1/g1CollectedHeap.cpp
@@ -896,7 +896,7 @@ void G1CollectedHeap::abort_refinement() {
 
     // Record any available refinement statistics.
     policy()->record_refinement_stats(sweep_state.stats());
-    sweep_state.complete_work(false /* concurrent */, false /* print_log */);
+    sweep_state.reset_to_idle();
   }
   sweep_state.reset_stats();
 }
