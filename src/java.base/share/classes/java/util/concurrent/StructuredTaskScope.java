78603af93ddf06acbba16563e847194e4eb60549
Alan Bateman <alan.bateman@oracle.com>
2026-04-13 09:41:16 +0100

    Improve flow of operator description in 2-arg open method

--- src/java.base/share/classes/java/util/concurrent/StructuredTaskScope.java
+++ src/java.base/share/classes/java/util/concurrent/StructuredTaskScope.java
@@ -1126,18 +1126,23 @@ public sealed interface StructuredTaskScope<T, R, R_X extends Throwable>
      * method with the default configuration to produce the configuration for the new
      * scope:
      * <ul>
-     * <li> If the operator invokes {@link Configuration#withThreadFactory(ThreadFactory)
-     * Configuration.withThreadFactory(ThreadFactory)} to set a {@link ThreadFactory}
-     * then its {@link ThreadFactory#newThread(Runnable) newThread} method will be called
-     * to create threads when {@linkplain #fork(Callable) forking} subtasks in the scope.
-     * If a {@code ThreadFactory} is not set then forking subtasks will create an unnamed
-     * virtual thread for each subtask. </li>
-     * <li> If the operator invokes {@link Configuration#withTimeout(Duration)
-     * Configuration.withTimeout(Duration)} to set a timeout then it starts when the scope
-     * is opened. If the timeout expires before or while waiting in {@link #join()} then
-     * the scope is cancelled and {@code join()} wakes up with an outcome. If the outcome
-     * is an exception then it is thrown with {@link CancelledByTimeoutException
-     * CancelledByTimeoutException} as the cause. </li>
+     * <li> If the operator returns a {@code Configuration} with a {@link ThreadFactory},
+     * set using {@link Configuration#withThreadFactory(ThreadFactory)
+     * withThreadFactory(ThreadFactory)}, its {@link ThreadFactory#newThread(Runnable)
+     * newThread()} method will be invoked to create threads when {@linkplain #fork(Callable)
+     * forking} subtasks in the scope. If a {@code ThreadFactory} is not set then forking
+     * subtasks will create an unnamed virtual thread for each subtask. </li>
+     * <li> If the operator returns a {@code Configuration} with a timeout, set using
+     * {@link Configuration#withTimeout(Duration) withTimeout(Duration)}, the timeout will
+     * start when the scope is opened. If the timeout expires before or while waiting in
+     * {@link #join()} then the scope will be {@linkplain ##Cancellation cancelled}. It is
+     * {@link Joiner Joiner} specific as to whether the {@code join()} method returns a
+     * result or throws an exception when a timeout occurs. If the outcome is an exception
+     * then it will be thrown with a {@link CancelledByTimeoutException
+     * CancelledByTimeoutException} as the {@linkplain Throwable#getCause() cause}. </li>
+     * <li> If the operator returns a {@code Configuration} with a name, set using
+     * {@linkplain Configuration#withName(String) Configuration.withName(String)}, the name
+     * will be used for monitoring and management purposes. </li>
      * <li> If the operator completes with an exception or error then it is propagated by
      * this method. </li>
      * <li> If the operator returns {@code null} then {@code NullPointerException} is
