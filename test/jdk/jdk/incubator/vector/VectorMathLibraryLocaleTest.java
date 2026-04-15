847f8ccb5a4b62f5bd0210a72e41a0674869efd0
ChrisHegarty <chegar999@gmail.com>
2026-04-15 16:51:01 +0100

    8382267: VectorMathLibrary uses locale-sensitive String.format to construct native symbol names

---
+++ test/jdk/jdk/incubator/vector/VectorMathLibraryLocaleTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * @test
+ * @bug 8382267
+ * @summary VectorMathLibrary symbol names must not use locale-sensitive formatting
+ * @modules jdk.incubator.vector
+ * @run main/othervm -Duser.language=ar -Duser.country=SA VectorMathLibraryLocaleTest
+ */
+
+import java.util.Locale;
+import jdk.incubator.vector.FloatVector;
+import jdk.incubator.vector.VectorOperators;
+import jdk.incubator.vector.VectorSpecies;
+
+public class VectorMathLibraryLocaleTest {
+    public static void main(String[] args) {
+        Locale locale = Locale.getDefault();
+        System.out.println("Locale: " + locale);
+        System.out.println("String.format(\"%d\", 16) = " + String.format("%d", 16));
+
+        VectorSpecies<Float> species = FloatVector.SPECIES_PREFERRED;
+        FloatVector v = FloatVector.broadcast(species, 1.0f);
+
+        // EXP is SVML-supported at all vector sizes on x86,
+        // so it exercises the symbol name construction path.
+        FloatVector result = v.lanewise(VectorOperators.EXP);
+        System.out.println("exp(1.0) = " + result);
+    }
+}
