f65c9884ea457cccb77680d73af62abc76e1d968
Harshit <harshitdhiman1402@gmail.com>
2026-04-16 06:08:38 +0000

    remove declaration of load_mirror_from_const_method

--- src/hotspot/cpu/s390/macroAssembler_s390.hpp
+++ src/hotspot/cpu/s390/macroAssembler_s390.hpp
@@ -886,7 +886,6 @@ class MacroAssembler: public Assembler {
                    Register Rbase = Z_R1, int pow2_offset = -1);
 
   void resolve_oop_handle(Register result, Register tmp1, Register tmp2);
-  void load_mirror_from_const_method(Register mirror, Register const_method);
   void load_method_holder(Register holder, Register method);
 
   //--------------------------
