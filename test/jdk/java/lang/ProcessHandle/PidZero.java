c746c05493c91a330dcfe7963b0c9a3c4befce44
Michael-Mc-Mahon <michael.x.mcmahon@oracle.com>
2026-04-16 11:58:22 +0100

    remove imports not needed

--- test/jdk/java/lang/ProcessHandle/PidZero.java
+++ test/jdk/java/lang/ProcessHandle/PidZero.java
@@ -21,12 +21,6 @@
  * questions.
  */
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /*
