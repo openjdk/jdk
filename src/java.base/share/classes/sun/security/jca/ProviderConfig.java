d3049a989898017e4e92c3d81a4f086dd10b9565
Valerie Peng <valerie.peng@oracle.com>
2026-04-17 23:19:18 +0000

    8382442: Make SUN provider configurable so it support non-crypto services only

--- src/java.base/share/classes/sun/security/jca/ProviderConfig.java
+++ src/java.base/share/classes/sun/security/jca/ProviderConfig.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -213,6 +213,9 @@ final class ProviderConfig {
                     }
                 }
             };
+            if (hasArgument()) {
+                p = p.configure(argument);
+            }
             provider = p;
         }
         return p;
