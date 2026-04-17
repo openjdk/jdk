43a6ad258654b872890e0e88e8140b715659b1cc
Vladimir Kozlov <vladimir.kozlov@oracle.com>
2026-04-16 17:34:08 -0700

    8382359: Align 32-bit data in AOT Code Cache to sizeof(uint)

--- src/hotspot/share/code/aotCodeCache.hpp
+++ src/hotspot/share/code/aotCodeCache.hpp
@@ -478,6 +478,8 @@ private:
 
   bool set_write_position(uint pos);
   bool align_write();
+  bool align_write_int();
+  bool align_write_bytes(uint alignment);
   address reserve_bytes(uint nbytes);
   uint write_bytes(const void* buffer, uint nbytes);
   const char* addr(uint offset) const { return _load_buffer + offset; }
@@ -643,6 +645,7 @@ private:
   uint  _read_position;              // Position in _load_buffer
   uint  read_position() const { return _read_position; }
   void  set_read_position(uint pos);
+  uint  align_read_int();
   const char* addr(uint offset) const { return _load_buffer + offset; }
 
   bool _lookup_failed;       // Failed to lookup for info (skip only this code load)
