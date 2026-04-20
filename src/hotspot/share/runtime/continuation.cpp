3e726a9d4f42408d7522d7f80a3b55695fcd1700
Anton Artemov <anton.artemov@oracle.com>
2026-04-20 16:59:00 +0200

    V3

--- src/hotspot/share/runtime/continuation.cpp
+++ src/hotspot/share/runtime/continuation.cpp
@@ -222,19 +222,19 @@ bool Continuation::is_continuation_entry_frame(const frame& f, const RegisterMap
 // least on PPC64 unextended_sp < sp is possible as interpreted frames are trimmed
 // to the actual size of the expression stack before calls. The problem there is
 // that even unextended_sp < entry_sp < sp is possible for an interpreted frame.
-static inline bool is_sp_in_continuation(const ContinuationEntry* entry, intptr_t* const sp) {
+static inline bool is_sp_in_continuation(const ContinuationEntry* entry, const frame& f) {
   // entry_sp() returns the unextended_sp which is always greater or equal to the actual sp
-  return entry->entry_sp() > sp;
+  return entry->entry_sp() > f.sp();
 }
 
 bool Continuation::is_frame_in_continuation(const ContinuationEntry* entry, const frame& f) {
-  return is_sp_in_continuation(entry, f.sp());
+  return is_sp_in_continuation(entry, f);
 }
 
 ContinuationEntry* Continuation::get_continuation_entry_for_sp(JavaThread* thread, const frame& f) {
   assert(thread != nullptr, "");
   ContinuationEntry* entry = thread->last_continuation();
-  while (entry != nullptr && !is_sp_in_continuation(entry, f.sp())) {
+  while (entry != nullptr && !is_sp_in_continuation(entry, f)) {
     entry = entry->parent();
   }
   return entry;
@@ -419,7 +419,7 @@ void Continuation::notify_deopt(JavaThread* thread, const frame& f) {
     return;
   }
 
-  if (is_sp_in_continuation(entry, f.sp())) {
+  if (is_sp_in_continuation(entry, f)) {
     thread->push_cont_fastpath(f.sp());
     return;
   }
@@ -428,12 +428,12 @@ void Continuation::notify_deopt(JavaThread* thread, const frame& f) {
   do {
     prev = entry;
     entry = entry->parent();
-  } while (entry != nullptr && !is_sp_in_continuation(entry, f.sp()));
+  } while (entry != nullptr && !is_sp_in_continuation(entry, f));
 
   if (entry == nullptr) {
     return;
   }
-  assert(is_sp_in_continuation(entry, f.sp()), "");
+  assert(is_sp_in_continuation(entry, f), "");
   if (f.sp() > prev->parent_cont_fastpath()) {
     prev->set_parent_cont_fastpath(f.sp());
   }
