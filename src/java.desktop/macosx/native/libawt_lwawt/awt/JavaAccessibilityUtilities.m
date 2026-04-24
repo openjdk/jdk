a5cec360aa727f662b224d55a139773a362dcf97
jeremy <jeremy.wood@mac.com>
2026-04-24 16:43:51 -0400

    8377745: applying simpler link solution
    This is briefly alluded to here:
    https://github.com/openjdk/jdk/pull/30251#issuecomment-4307964833
    
    (Although at the time that was discussing a 2-line change, and this is a one-word change.)

--- src/java.desktop/macosx/native/libawt_lwawt/awt/JavaAccessibilityUtilities.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/JavaAccessibilityUtilities.m
@@ -464,7 +464,7 @@ void initializeRoles()
     [sRoles setObject:JavaAccessibilityIgnore forKey:@"frame"];
     [sRoles setObject:JavaAccessibilityIgnore forKey:@"glasspane"];
     [sRoles setObject:NSAccessibilityGroupRole forKey:@"groupbox"];
-    [sRoles setObject:NSAccessibilityStaticTextRole forKey:@"hyperlink"]; //maybe a group?
+    [sRoles setObject:NSAccessibilityLinkRole forKey:@"hyperlink"];
     [sRoles setObject:NSAccessibilityImageRole forKey:@"icon"];
     [sRoles setObject:NSAccessibilityGroupRole forKey:@"internalframe"];
     [sRoles setObject:NSAccessibilityStaticTextRole forKey:@"label"];
