401116646eaa2ffdd2c6c1279fda289d5f6a6803
minborg <pminborg@gmail.com>
2026-05-12 14:53:32 +0200

    Update copyright year

--- src/jdk.management/share/classes/com/sun/management/internal/PlatformMBeanProviderImpl.java
+++ src/jdk.management/share/classes/com/sun/management/internal/PlatformMBeanProviderImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -22,6 +22,7 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
+
 package com.sun.management.internal;
 
 import com.sun.management.DiagnosticCommandMBean;
