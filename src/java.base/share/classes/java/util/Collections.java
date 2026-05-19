732a8080fbe5a4ed279454736de72cd99d9254f4
Gleydson Fabricio <124312436+gleydsonfabricio@users.noreply.github.com>
2026-05-19 17:57:10 -0300

    refactor: UnmodifiableList to implement ReadableList
    Removed unsupported operations from UnmodifiableList class.

--- src/java.base/share/classes/java/util/Collections.java
+++ src/java.base/share/classes/java/util/Collections.java
@@ -1487,8 +1487,8 @@ public final class Collections {
     /**
      * @serial include
      */
-    static class UnmodifiableList<E> extends UnmodifiableCollection<E>
-                                  implements List<E> {
+    static class UnmodifiableList<E> implements ReadableList<E> {
+
         @java.io.Serial
         private static final long serialVersionUID = -283967356065247728L;
 
@@ -1505,29 +1505,8 @@ public final class Collections {
         public int hashCode()           {return list.hashCode();}
 
         public E get(int index) {return list.get(index);}
-        public E set(int index, E element) {
-            throw new UnsupportedOperationException();
-        }
-        public void add(int index, E element) {
-            throw new UnsupportedOperationException();
-        }
-        public E remove(int index) {
-            throw new UnsupportedOperationException();
-        }
         public int indexOf(Object o)            {return list.indexOf(o);}
         public int lastIndexOf(Object o)        {return list.lastIndexOf(o);}
-        public boolean addAll(int index, Collection<? extends E> c) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void replaceAll(UnaryOperator<E> operator) {
-            throw new UnsupportedOperationException();
-        }
-        @Override
-        public void sort(Comparator<? super E> c) {
-            throw new UnsupportedOperationException();
-        }
 
         public ListIterator<E> listIterator()   {return listIterator(0);}
 
@@ -1542,16 +1521,6 @@ public final class Collections {
                 public E previous()          {return i.previous();}
                 public int nextIndex()       {return i.nextIndex();}
                 public int previousIndex()   {return i.previousIndex();}
-
-                public void remove() {
-                    throw new UnsupportedOperationException();
-                }
-                public void set(E e) {
-                    throw new UnsupportedOperationException();
-                }
-                public void add(E e) {
-                    throw new UnsupportedOperationException();
-                }
 
                 @Override
                 public void forEachRemaining(Consumer<? super E> action) {
