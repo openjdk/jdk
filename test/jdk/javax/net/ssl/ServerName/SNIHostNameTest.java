b68e392b0abd3114222a71d3c9dd760bd511f66e
Volkan Yazıcı <volkan.yazici@oracle.com>
2026-05-19 10:42:28 +0200

    Combine tests a single file

--- test/jdk/javax/net/ssl/ServerName/SNIHostNameTest.java
+++ test/jdk/javax/net/ssl/ServerName/SNIHostNameTest.java
@@ -28,24 +28,31 @@ import javax.net.ssl.SNIHostName;
 import javax.net.ssl.StandardConstants;
 import java.net.IDN;
 import java.nio.charset.Charset;
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /*
  * @test
  * @bug 8020842 8261969 8378500
- * @summary Verifies `SNIHostName` works with valid input
+ * @summary Verifies `SNIHostName` constructors and static factory methods
  * @run junit ${test.main.class}
  */
 
 @SuppressWarnings("deprecation")
-class SNIHostNamePositiveTest {
+class SNIHostNameTest {
+
+    /**
+     * Strings whose punycoded representations translate to a `.` (dot) character.
+     */
+    private static final List<String> DOTS = List.of("\u002e", "\u3002", "\uff0e", "\uff61");
 
     private record Arg<T>(boolean failsOnlyOnStrictCheck, T hostName) {
 
@@ -75,15 +82,12 @@ class SNIHostNamePositiveTest {
                 new Arg<>(false, "1abc.com"),
                 new Arg<>(false, "123.com"),
                 new Arg<>(false, "a-b-c.com"),
-                new Arg<>(false, "\u00ebxample.com"),
-                // punycode("\u00ebxample.com") = "xn--xample-ova.com"
+                new Arg<>(false, "ëxample.com"),
+                // punycode("ëxample.com") = "xn--xample-ova.com"
                 new Arg<>(false, "xn--xample-ova.com"));
 
         // IDNs whose punycoded representations translate to a `.` (dot) character
-        var dotCases = "\u002e\u3002\uff0e\uff61".chars()
-                .boxed()
-                .map(Character::toString)
-                .map(s -> new Arg<>(false, "example" + s + "com"));
+        var dotCases = DOTS.stream().map(s -> new Arg<>(false, "example" + s + "com"));
 
         // Combine all
         return Stream.of(basicCases, dotCases).flatMap(Function.identity());
@@ -92,20 +96,88 @@ class SNIHostNamePositiveTest {
 
     @ParameterizedTest
     @MethodSource("validStringArgs")
-    void testNewString(Arg<String> arg) {
+    void positiveTestNewString(Arg<String> arg) {
         var sni = new SNIHostName(arg.hostName);
         verifySNIHostName(arg.hostName, US_ASCII, sni);
     }
 
     @ParameterizedTest
     @MethodSource("validStringArgs")
-    void testOfHostName(Arg<String> arg) {
+    void positiveTestOfHostName(Arg<String> arg) {
         if (arg.failsOnlyOnStrictCheck) {
             assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofHostName(arg.hostName));
         } else {
             var sni = SNIHostName.ofHostName(arg.hostName);
             verifySNIHostName(arg.hostName, US_ASCII, sni);
         }
+    }
+
+    static Stream<Arg<String>> invalidStringArgs() {
+
+        // Host names with illegal characters
+        var illegalChars = "\n\r\t !\"#$%&'()*+,/:;<=>?@[\\]^_`{|}~";
+        var illegalCharCases = illegalChars.chars()
+                .boxed()
+                // Next to testing a character `c`, also test its repetition: `cc`
+                .flatMap(i -> {
+                    var s = Character.toString(i);
+                    return Stream.of(s, s + s);
+                })
+                .flatMap((String s) -> Stream.of(
+                        new Arg<>(false, s),                        // Equals
+                        new Arg<>(false, "exa" + s + "mple.com"),   // Contains in between LDH
+                        new Arg<>(false, "example" + s + ".com"),   // Contains before dot
+                        new Arg<>(false, "example." + s + "com"),   // Contains after dot
+                        new Arg<>(false, s + "example.com"),        // Starts with
+                        new Arg<>(false, "example.com" + s)));      // Ends with
+
+        // IDNs whose punycoded representations translate to a `.` (dot) character
+        var dotCases = DOTS.stream().flatMap(s -> Stream.of(
+                // Empty labels
+                new Arg<>(false, s + "example.com"),
+                new Arg<>(false, "example" + s + s + "com"),
+                new Arg<>(false, "example" + s + ".com"),
+                new Arg<>(false, "example." + s + "com"),
+                new Arg<>(false, "example.com" + s)));
+
+        // Illegal host names with particular edge cases
+        var illegalPunycodeChar = '-';
+        var edgeCases = Stream.of(
+                new Arg<>(false, "example..com"),
+                new Arg<>(false, "com."),
+                new Arg<>(false, "."),
+                // Illegal hyphens
+                new Arg<>(false, "-abc.com"),
+                new Arg<>(false, "abc-.com"),
+                new Arg<>(false, "abc.-com"),
+                new Arg<>(false, "abc.com-"),
+                // punycode("ëxample.com") = "xn--xample-ova.com"
+                new Arg<>(false, "xn--xample-ova" + illegalPunycodeChar + ".com"),
+                new Arg<>(false, "xn--xample-ova.com" + illegalPunycodeChar),
+                // IP literal addresses
+                new Arg<>(true, "127.0.0.1"),
+                new Arg<>(false, "::1"),            // Fails also on non-strict check, because `:` is not LDH
+                new Arg<>(false, "::1%eth0"));      // Fails also on non-strict check, because `:` is not LDH
+
+        // Combine all
+        return Stream.of(illegalCharCases, dotCases, edgeCases).flatMap(Function.identity());
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidStringArgs")
+    void negativeTestNewString(Arg<String> arg) {
+        if (arg.failsOnlyOnStrictCheck) {
+            assertDoesNotThrow(() -> new SNIHostName(arg.hostName));
+        } else {
+            assertThrows(IllegalArgumentException.class, () -> new SNIHostName(arg.hostName));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidStringArgs")
+    void negativeTestOfHostName(Arg<String> arg) {
+        assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofHostName(arg.hostName));
     }
 
     static Stream<Arg<byte[]>> validAsciiArgs() {
@@ -120,14 +192,14 @@ class SNIHostNamePositiveTest {
 
     @ParameterizedTest
     @MethodSource("validAsciiArgs")
-    void testNewEncodedUsingAscii(Arg<byte[]> arg) {
+    void positiveTestNewEncodedUsingAscii(Arg<byte[]> arg) {
         var sni = new SNIHostName(arg.hostName);
         verifySNIHostName(new String(arg.hostName, US_ASCII), US_ASCII, sni);
     }
 
     @ParameterizedTest
     @MethodSource("validAsciiArgs")
-    void testOfEncodedUsingAscii(Arg<byte[]> arg) {
+    void positiveTestOfEncodedUsingAscii(Arg<byte[]> arg) {
         if (arg.failsOnlyOnStrictCheck) {
             assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofEncoded(arg.hostName));
         } else {
@@ -146,14 +218,14 @@ class SNIHostNamePositiveTest {
 
     @ParameterizedTest
     @MethodSource("validUtf8Args")
-    void testNewEncodedUsingUtf8(Arg<byte[]> arg) {
+    void positiveTestNewEncodedUsingUtf8(Arg<byte[]> arg) {
         var sni = new SNIHostName(arg.hostName);
         verifySNIHostName(new String(arg.hostName, UTF_8), UTF_8, sni);
     }
 
     @ParameterizedTest
     @MethodSource("validUtf8Args")
-    void testOfEncodedUsingUtf8(Arg<byte[]> arg) {
+    void positiveTestOfEncodedUsingUtf8(Arg<byte[]> arg) {
         if (arg.failsOnlyOnStrictCheck) {
             assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofEncoded(arg.hostName));
         } else {
@@ -184,6 +256,49 @@ class SNIHostNamePositiveTest {
         var asciiHostName = IDN.toASCII(hostName, IDN.USE_STD3_ASCII_RULES);
         assertEquals(asciiHostName, sni.getAsciiName());
         assertArrayEquals(encodedHostName, sni.getEncoded());
+    }
+
+    static Stream<Arg<byte[]>> invalidAsciiArgs() {
+        return invalidStringArgs()
+                .filter(arg -> isAscii(arg.hostName))
+                .map(arg -> new Arg<>(arg.failsOnlyOnStrictCheck, arg.hostName.getBytes(US_ASCII)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidAsciiArgs")
+    void negativeTestNewEncodedUsingAscii(Arg<byte[]> arg) {
+        if (arg.failsOnlyOnStrictCheck) {
+            assertDoesNotThrow(() -> new SNIHostName(arg.hostName));
+        } else {
+            assertThrows(IllegalArgumentException.class, () -> new SNIHostName(arg.hostName));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidAsciiArgs")
+    void negativeTestOfEncodedUsingAscii(Arg<byte[]> arg) {
+        assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofEncoded(arg.hostName));
+    }
+
+    static Stream<Arg<byte[]>> invalidUtf8Args() {
+        return invalidStringArgs()
+                .map(arg -> new Arg<>(arg.failsOnlyOnStrictCheck, arg.hostName.getBytes(UTF_8)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidUtf8Args")
+    void negativeTestNewEncodedUsingUtf8(Arg<byte[]> arg) {
+        if (arg.failsOnlyOnStrictCheck) {
+            assertDoesNotThrow(() -> new SNIHostName(arg.hostName));
+        } else {
+            assertThrows(IllegalArgumentException.class, () -> new SNIHostName(arg.hostName));
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidUtf8Args")
+    void negativeTestOfEncodedUsingUtf8(Arg<byte[]> arg) {
+        assertThrows(IllegalArgumentException.class, () -> SNIHostName.ofEncoded(arg.hostName));
     }
 
 }
