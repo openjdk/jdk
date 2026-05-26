a210cb4c23912cd46d8ecd7fadbaab011e765537
David Linus Briemann <david.briemann@sap.com>
2026-05-26 12:30:08 +0200

    revert function to have no param

--- src/hotspot/cpu/ppc/assembler_ppc.hpp
+++ src/hotspot/cpu/ppc/assembler_ppc.hpp
@@ -1358,8 +1358,8 @@ class Assembler : public AbstractAssembler {
     return (0 == addr % a);
   }
 
-  void publish_instructions(bool invalidate = true) {
-    AbstractAssembler::publish_instructions(invalidate);
+  void publish_instructions() {
+    AbstractAssembler::publish_instructions();
   }
 
   inline void emit_int32(int);  // shadows AbstractAssembler::emit_int32
