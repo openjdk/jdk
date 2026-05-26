c8cd2fc2970b4c39fbdbb0d95d3b090b923a002f
Jorn Vernee <jorn.vernee@oracle.com>
2026-05-26 15:15:01 +0000

    Add --add-reads test case

--- test/langtools/tools/javac/IncrementalComp/TestIncrementalComp.java
+++ test/langtools/tools/javac/IncrementalComp/TestIncrementalComp.java
@@ -35,12 +35,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.module.Configuration;
 import java.lang.module.ModuleFinder;
 import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.spi.ToolProvider;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -57,7 +62,11 @@ class TestIncrementalComp {
     static final ToolProvider JAVAC = ToolProvider.findFirst("javac")
             .orElseThrow();
 
-    record TestCase(String srcDir, Set<String> modules, String mainModule) {}
+    record TestCase(String srcDir, Set<String> modules, String mainModule, Map<String, String> addReadsEdges) {
+        TestCase(String srcDir, Set<String> modules, String mainModule) {
+            this(srcDir, modules, mainModule, Map.of());
+        }
+    }
 
     @ParameterizedTest
     @MethodSource("cases")
@@ -73,54 +82,73 @@ class TestIncrementalComp {
         Files.createDirectories(libPath.getParent());
         Files.copy(ALTS_DIR.resolve("Lib_int.java"), libPath);
 
-        String modulesArg = String.join(",", testCase.modules());
+        List<String> javacCommand = new ArrayList<>(List.of(
+            "-d", outDir.toString(),
+            "--module-source-path=" + localTestModules,
+            "--module", String.join(",", testCase.modules())
+        ));
+        for (Map.Entry<String, String> addReads : testCase.addReadsEdges().entrySet()) {
+            String reader = addReads.getKey();
+            String read = addReads.getValue();
+            javacCommand.add(String.format("--add-reads=%s=%s", reader, read));
+        }
         // compile both modules
-        compile(
-                "-d", outDir.toString(),
-                "--module-source-path=" + localTestModules,
-                "--module", modulesArg);
+        compile(javacCommand);
 
         String mainClass = testCase.mainModule() + ".app.Main";
-        invokeMainMethod(outDir, testCase.mainModule(), mainClass);
+        invokeMainMethod(outDir, testCase.mainModule(), mainClass, testCase.addReadsEdges());
 
         // modify sources. Dep is not modified
         Files.copy(ALTS_DIR.resolve("Lib_long.java"), libPath, REPLACE_EXISTING);
 
-        // recompile moda. Dep should be recompiled as well
-        compile(
-                "-d", outDir.toString(),
-                "--module-source-path=" + localTestModules,
-                "--module", modulesArg);
+        // recompile. Any dependency on the changed file should be recompiled as well
+        compile(javacCommand);
 
         // should work
-        invokeMainMethod(outDir, testCase.mainModule(), mainClass);
+        // if this fails because of incremental compilation issues, we can expect to see a NoSuchMethodError
+        invokeMainMethod(outDir, testCase.mainModule(), mainClass, testCase.addReadsEdges());
     }
 
     static Stream<TestCase> cases() {
         return Stream.of(
             new TestCase("single", Set.of("org.moda"), "org.moda"),
             new TestCase("multi", Set.of("org.moda", "org.modb"), "org.modb"),
-            new TestCase("transitive", Set.of("org.moda", "org.modb", "org.modc"), "org.modc")
+            new TestCase("transitive", Set.of("org.moda", "org.modb", "org.modc"), "org.modc"),
+            new TestCase("add_reads", Set.of("org.moda", "org.modb"), "org.modb", Map.of("org.modb", "org.moda"))
         );
     }
 
-    private static void invokeMainMethod(Path modulePath, String moduleName, String mainClassName, String... args)
+    private static void invokeMainMethod(Path modulePath, String moduleName, String mainClassName,
+                                         Map<String, String> addReadsEdges)
             throws ReflectiveOperationException {
+        // define module layer
+        // note that we need to explicitly add any read module to the set of roots
         ModuleLayer boot = ModuleLayer.boot();
-        ModuleLayer layer = boot.defineModulesWithOneLoader(
-                boot.configuration()
-                        .resolve(ModuleFinder.of(modulePath), ModuleFinder.of(), Set.of(moduleName)),
-                ClassLoader.getSystemClassLoader());
-        Class<?> main1 = layer.findLoader(moduleName).loadClass(mainClassName);
+        Set<String> allRoots = Stream.concat(Stream.of(moduleName), addReadsEdges.values().stream())
+                .collect(Collectors.toSet());
+        Configuration config = boot.configuration()
+                .resolve(ModuleFinder.of(modulePath), ModuleFinder.of(), allRoots);
+        ModuleLayer.Controller controller = ModuleLayer.defineModulesWithOneLoader(
+                config, List.of(boot), ClassLoader.getSystemClassLoader());
+
+        // add extra reads edges
+        for (Map.Entry<String, String> addReads : addReadsEdges.entrySet()) {
+            Module reader = controller.layer().findModule(addReads.getKey()).orElseThrow();
+            Module read = controller.layer().findModule(addReads.getValue()).orElseThrow();
+            controller.addReads(reader, read);
+        }
+
+        // invoke main
+        Class<?> main1 = controller.layer().findLoader(moduleName).loadClass(mainClassName);
         Method m = main1.getMethod("main", String[].class);
-        m.invoke(null, new Object[]{ args });
+        m.invoke(null, new Object[]{ new String[0] });
     }
 
-    private static void compile(String... args) {
-        System.err.println("compile: " + Arrays.asList(args));
+    private static void compile(List<String> args) {
+        System.err.println("compile: " + args);
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
-        int rc = JAVAC.run(pw, pw, args);
+        int rc = JAVAC.run(pw, pw, args.toArray(String[]::new));
         pw.close();
         System.err.println(sw);
         assertEquals(0, rc);
