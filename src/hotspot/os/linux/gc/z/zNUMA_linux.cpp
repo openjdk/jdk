9a6289fae6583f17e153c8fc6b9ede650eac4ac9
Joel Sikström <joel.sikstrom@oracle.com>
2026-05-26 22:02:16 +0200

    Small fixes

--- src/hotspot/os/linux/gc/z/zNUMA_linux.cpp
+++ src/hotspot/os/linux/gc/z/zNUMA_linux.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -32,7 +32,6 @@
 #include "runtime/java.hpp"
 #include "runtime/os.hpp"
 #include "utilities/debug.hpp"
-
 
 class ZNUMAConverter {
 private:
