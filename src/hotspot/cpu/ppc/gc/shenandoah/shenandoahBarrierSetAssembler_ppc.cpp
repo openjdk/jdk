bd1256479b2f705e388c6631da0c5087d1109e0c
David Linus Briemann <david.briemann@sap.com>
2026-06-02 11:52:05 +0200

    optimize out temp register (tmp3)

--- src/hotspot/cpu/ppc/gc/shenandoah/shenandoahBarrierSetAssembler_ppc.cpp
+++ src/hotspot/cpu/ppc/gc/shenandoah/shenandoahBarrierSetAssembler_ppc.cpp
@@ -1134,14 +1134,15 @@ void ShenandoahBarrierSetAssembler::store_c2(const MachNode* node, MacroAssemble
   ShenandoahBarrierStubC2::store_post(masm, node, Address(dst, disp), tmp1, tmp2);
 }
 
-void ShenandoahBarrierSetAssembler::compare_and_set_c2(const MachNode* node, MacroAssembler* masm, Register res, Register addr, Register oldval,
-      Register newval, Register tmp1, Register tmp2, Register tmp3, bool exchange, bool narrow, bool weak, bool acquire) {
+void ShenandoahBarrierSetAssembler::compare_and_set_c2(const MachNode* node, MacroAssembler* masm, Register res, Register addr,
+      Register oldval, Register newval, Register tmp1, Register tmp2, bool exchange, bool narrow, bool weak, bool acquire) {
 
-  ShenandoahBarrierStubC2::load_store_pre(masm, node, tmp1, addr, tmp2, tmp3, narrow);
+  Register dest_current = exchange ? res : tmp1,
+           other_tmp    = exchange ? tmp1 : res;
+  ShenandoahBarrierStubC2::load_store_pre(masm, node, dest_current, addr, tmp2, other_tmp, narrow);
 
-  Register dest_current = exchange ? res   : R0;
   Label no_update;
-  int semantics         = MacroAssembler::MemBarNone;
+  int semantics = MacroAssembler::MemBarNone;
 
   if (acquire) {
     semantics = support_IRIW_for_not_multiple_copy_atomic_cpu ?
@@ -1150,15 +1151,19 @@ void ShenandoahBarrierSetAssembler::compare_and_set_c2(const MachNode* node, Mac
 
   if (!exchange) { __ li(res, 0); }
   if (narrow) {
+    __ cmpw(CR0, dest_current, oldval); // loaded by pre-barrier
+    __ bne(CR0, no_update);
     // CmpxchgX sets CR0 to cmpX(src1, src2) and Rres to 'true'/'false'.
     __ cmpxchgw(CR0, dest_current, oldval, newval, addr,
                 semantics, MacroAssembler::cmpxchgx_hint_atomic_update(),
-                noreg, &no_update, true, weak);
+                noreg, &no_update, false, weak);
   } else {
+    __ cmpd(CR0, dest_current, oldval); // loaded by pre-barrier
+    __ bne(CR0, no_update);
     // CmpxchgX sets CR0 to cmpX(src1, src2) and Rres to 'true'/'false'.
     __ cmpxchgd(CR0, dest_current, oldval, newval, addr,
                 semantics, MacroAssembler::cmpxchgx_hint_atomic_update(),
-                noreg, &no_update, true, weak);
+                noreg, &no_update, false, weak);
   }
 
   ShenandoahBarrierStubC2::load_store_post(masm, node, Address(addr, 0), tmp1, tmp2);
@@ -1167,10 +1172,10 @@ void ShenandoahBarrierSetAssembler::compare_and_set_c2(const MachNode* node, Mac
   __ bind(no_update);
 }
 
-void ShenandoahBarrierSetAssembler::get_and_set_c2(const MachNode* node, MacroAssembler* masm, Register preval, Register newval, Register addr, Register tmp1, Register tmp2, Register tmp3) {
+void ShenandoahBarrierSetAssembler::get_and_set_c2(const MachNode* node, MacroAssembler* masm, Register preval, Register newval, Register addr, Register tmp1, Register tmp2) {
   bool is_narrow = node->bottom_type()->isa_narrowoop();
 
-  ShenandoahBarrierStubC2::load_store_pre(masm, node, tmp1, addr, tmp2, tmp3, is_narrow);
+  ShenandoahBarrierStubC2::load_store_pre(masm, node, preval, addr, tmp1, tmp2, is_narrow);
 
   if (is_narrow) {
     __ getandsetw(preval, newval, addr, MacroAssembler::cmpxchgx_hint_atomic_update());
