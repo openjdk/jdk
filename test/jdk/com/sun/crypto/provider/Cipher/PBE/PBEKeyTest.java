1f00cd93e8b25eccca2e388068a7716186d8d2ca
Matthew Donovan <matthew.p.donovan@oracle.com>
2026-06-02 09:17:10 -0400

    8376599: Update Cipher/PBE/PBEKeyTest.java for non-ASCII characters

--- test/jdk/com/sun/crypto/provider/Cipher/PBE/PBEKeyTest.java
+++ test/jdk/com/sun/crypto/provider/Cipher/PBE/PBEKeyTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1998, 2007, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1998, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -39,22 +39,21 @@ public class PBEKeyTest {
 
         // Valid password
         char[] pass = new char[] { 'p', 'a', 's', 's', 'w', 'o', 'r', 'd' };
+        testPassword(pass, fac, "ASCII password");
+
+        pass = new char[] { 'p', 'a', 's', 's', 'w', 'o', 'r', '\u0019' };
+        testPassword(pass, fac, "non-ASCII password");
+
+    }
+
+    private static void testPassword(char[] pass, SecretKeyFactory fac, String desc)
+            throws Exception {
         PBEKeySpec spec = new PBEKeySpec(pass);
         SecretKey skey = fac.generateSecret(spec);
         KeySpec spec1 = fac.getKeySpec(skey, PBEKeySpec.class);
         SecretKey skey1 = fac.generateSecret(spec1);
         if (!skey.equals(skey1))
-            throw new Exception("Equal keys not equal");
+            throw new Exception(desc + ": Equal keys not equal");
         System.out.println(new String(((PBEKeySpec)spec1).getPassword()));
-
-        // Invalid password
-        pass = new char[] { 'p', 'a', 's', 's', 'w', 'o', 'r', '\u0019' };
-        spec = new PBEKeySpec(pass);
-        try {
-            skey = fac.generateSecret(spec);
-            throw new Exception("Expected exception not thrown");
-        } catch (Exception e) {
-            System.out.println("Expected exception thrown");
-        }
     }
 }
