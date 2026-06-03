19f295b11ce1693126be539b7e3c4d29b7270d29
Sean Mullan <sean.mullan@oracle.com>
2026-06-03 08:50:33 -0400

    Initial revision.

--- src/jdk.jfr/share/classes/jdk/jfr/events/TLSHandshakeEvent.java
+++ src/jdk.jfr/share/classes/jdk/jfr/events/TLSHandshakeEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -57,4 +57,7 @@ public final class TLSHandshakeEvent extends MirrorEvent {
     @CertificateId
     @Unsigned
     public long certificateId;
+
+    @Label("Named Group")
+    public String namedGroup;
 }
