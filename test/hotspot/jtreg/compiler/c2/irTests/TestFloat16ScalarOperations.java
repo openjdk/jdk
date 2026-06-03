65c4a8455a8286871e4ef299dae32ab36c6f9353
Manuel Hässig <manuel.hassig@oracle.com>
2026-06-03 16:49:30 +0200

    Review comments

--- test/hotspot/jtreg/compiler/c2/irTests/TestFloat16ScalarOperations.java
+++ test/hotspot/jtreg/compiler/c2/irTests/TestFloat16ScalarOperations.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
  * Copyright (c) 2025, Arm Limited. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
@@ -35,7 +35,6 @@ import compiler.lib.ir_framework.*;
 import compiler.lib.verify.*;
 import jdk.incubator.vector.Float16;
 import static jdk.incubator.vector.Float16.*;
-import java.util.Random;
 
 import compiler.lib.generators.Generator;
 import static compiler.lib.generators.Generators.G;
