5a97f306cb8116e5f0a743878ce19d920a141cb9
Simon Tooke <stooke@redhat.com>
2026-06-05 13:45:54 -0400

    change test order and test forward bit

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -1343,8 +1343,8 @@ void os::print_location(outputStream* st, intptr_t x, bool verbose) {
   if (UseCompactObjectHeaders) {
     markWord mw = (markWord)(uintptr_t)(addr);
     static const uintptr_t valhalla_reserved_bits_in_place = right_n_bits(markWord::valhalla_reserved_bits) << markWord::valhalla_reserved_shift;
-    if (mw.has_no_hash() && Klass::is_valid(mw.klass_without_asserts())
-        && (mw.value() & valhalla_reserved_bits_in_place) == 0 && !mw.is_forwarded()) {
+    static const uintptr_t markbits_must_be_zero = valhalla_reserved_bits_in_place | markWord::self_fwd_bit_in_place;
+    if (mw.has_no_hash() && (mw.value() & markbits_must_be_zero) == 0 && Klass::is_valid(mw.klass_without_asserts())) {
       st->print(PTR_FORMAT " is a valid markword: ", p2i(addr));
       mw.print_on(st);
       st->print(" ");
