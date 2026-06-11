54801684cfbb6654e795fe1a5fe7a058fb5ba8e2
Albert Yang <albert.m.yang@oracle.com>
2026-06-11 10:14:33 +0200

    memtag-arg

--- src/hotspot/share/memory/memoryReserver.hpp
+++ src/hotspot/share/memory/memoryReserver.hpp
@@ -42,7 +42,8 @@ class MemoryReserver : AllStatic {
                                               size_t size,
                                               size_t alignment,
                                               size_t page_size,
-                                              bool exec);
+                                              bool exec,
+                                              MemTag mem_tag);
 
 public:
   // Final destination
