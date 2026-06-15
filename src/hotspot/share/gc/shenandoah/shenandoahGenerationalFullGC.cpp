eaea64e57e4eb327e268c1bc549b83b3f8f64761
duke <duke@openjdk.org>
2026-06-15 23:42:44 +0000

    Backport 1251526588314059c6c0bc132c1a052ef04c83a6

--- src/hotspot/share/gc/shenandoah/shenandoahGenerationalFullGC.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahGenerationalFullGC.cpp
@@ -144,7 +144,7 @@ void ShenandoahGenerationalFullGC::account_for_region(ShenandoahHeapRegion* r, s
 void ShenandoahGenerationalFullGC::maybe_coalesce_and_fill_region(ShenandoahHeapRegion* r) {
   if (r->is_pinned() && r->is_old() && r->is_active() && !r->is_humongous()) {
     r->begin_preemptible_coalesce_and_fill();
-    r->oop_coalesce_and_fill(false);
+    r->oop_coalesce_and_fill(/* cancellable = */ false, /* do_card_table_updates = */ false);
   }
 }
 
