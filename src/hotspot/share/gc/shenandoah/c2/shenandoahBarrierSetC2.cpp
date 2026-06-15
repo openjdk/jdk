002a837833c5f1551ebfac0bc890b49bbb095f90
David Linus Briemann <david.briemann@sap.com>
2026-06-15 13:52:13 +0200

    new invalidate_icache call not necessary

--- src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.cpp
+++ src/hotspot/share/gc/shenandoah/c2/shenandoahBarrierSetC2.cpp
@@ -880,7 +880,7 @@ void ShenandoahBarrierSetC2::emit_stubs(CodeBuffer& cb) const {
         skipped_after, skipped_before, skipped_after - skipped_before);
 #endif
 
-  masm.invalidate_icache();
+  // Code will be copied. No ICache sync required.
 }
 
 void ShenandoahBarrierStubC2::register_stub(ShenandoahBarrierStubC2* stub) {
