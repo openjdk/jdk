cf9b1febb0def0b9f93f23fbd2bcd20937db7152
Volodymyr Paprotski <volodymyr.paprotski@intel.com>
2026-06-29 00:12:44 +0000

    whitespace

--- src/java.base/share/classes/sun/security/provider/SHA3Parallel.java
+++ src/java.base/share/classes/sun/security/provider/SHA3Parallel.java
@@ -83,17 +83,17 @@ public class SHA3Parallel {
     public int squeezeBlock(int nr) throws InvalidAlgorithmParameterException {
         int retVal = 0;
         switch (nr) {
-            case 1: 
-                // until we enable single keccak intrinsic, use the better 
+            case 1:
+                // until we enable single keccak intrinsic, use the better
                 // doubleKeccak
             case 2:
                 retVal = doubleKeccak(lanesArr[0], lanesArr[1]);
                 break;
-            case 3:  
-                // until we enable single keccak intrinsic, use the better 
+            case 3:
+                // until we enable single keccak intrinsic, use the better
                 // doubleKeccak/quadKeccak
             case 4:
-                retVal = quadKeccak(lanesArr[0], lanesArr[1], lanesArr[2], 
+                retVal = quadKeccak(lanesArr[0], lanesArr[1], lanesArr[2],
                         lanesArr[3]);
                 break;
             default:
