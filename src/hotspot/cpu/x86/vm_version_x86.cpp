232606a4049ba1e33fa16b56e006f6b18493c4b1
Daniel Skantz <daniel.skantz@oracle.com>
2026-07-06 17:36:59 +0000

    fix

--- src/hotspot/cpu/x86/vm_version_x86.cpp
+++ src/hotspot/cpu/x86/vm_version_x86.cpp
@@ -1270,7 +1270,7 @@ void VM_Version::get_processor_features() {
 
   // Kyber Intrinsics
   // Currently we only have them for AVX512
-  if (supports_evex() && supports_avx512bw()) {
+  if (supports_avx512vlbw()) {
     if (FLAG_IS_DEFAULT(UseKyberIntrinsics)) {
       UseKyberIntrinsics = true;
     }
