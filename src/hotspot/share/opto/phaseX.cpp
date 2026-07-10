9107d8213d7d79bddb04343197a5c82785e7c9cf
Roland Westrelin <47032191+rwestrel@users.noreply.github.com>
2026-07-10 18:07:43 +0200

    Update src/hotspot/share/opto/phaseX.cpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -3365,7 +3365,7 @@ Node *PhaseCCP::transform( Node *n ) {
 
   if (KillPathsReachableByDeadDataNode) {
     for (uint i = 0; i < _maybe_top_type_or_div_mod_nodes.size(); ++i) {
-      Node* type_node = _maybe_top_type_or_div_mod_nodes.at(i);
+      Node* data_node = _maybe_top_type_or_div_mod_nodes.at(i);
       if (type(type_node) == Type::TOP) {
         ResourceMark rm;
         type_node->make_paths_from_here_dead(this, nullptr, "ccp");
