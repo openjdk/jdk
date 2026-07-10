2ff90230451fe29220a2b2daf6e4b869899a406e
William Kemper <kemperw@amazon.com>
2026-07-09 17:14:32 -0700

    Update comments references degenerated cycles

--- src/hotspot/share/gc/shenandoah/shenandoahMmuTracker.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahMmuTracker.hpp
@@ -85,12 +85,7 @@ public:
   void initialize();
 
   // At completion of each GC cycle (not including interrupted cycles), we invoke one of the following to record the
-  // GC utilization during this cycle.  Incremental efforts spent in an interrupted GC cycle will be accumulated into
-  // the CPU time reports for the subsequent completed [degenerated or full] GC cycle.
-  //
-  // We may redundantly record degen and full in the case that a degen upgrades to full.  When this happens, we will invoke
-  // both record_full() and record_degenerated() with the same value of gcid.  record_full() is called first and the log
-  // reports such a cycle as a FULL cycle.
+  // GC utilization during this cycle.
   void record_young(size_t gcid);
   void record_global(size_t gcid);
   void record_bootstrap(size_t gcid);
