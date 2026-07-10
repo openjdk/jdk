2470b926e232e1f98d5bc585bf16d3ef0bd9d6da
Roland Westrelin <47032191+rwestrel@users.noreply.github.com>
2026-07-10 18:02:09 +0200

    Update src/hotspot/share/opto/compile.hpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/compile.hpp
+++ src/hotspot/share/opto/compile.hpp
@@ -899,7 +899,11 @@ public:
   RootNode*    root() const                { return _root; }
   void         set_root(RootNode* r)       { _root = r; }
   DeadPathNode* dead_path() const          { return _dead_path; }
-  void         set_dead_path(DeadPathNode* dead_path) { assert(_dead_path == nullptr, ""); _dead_path = dead_path;}
+  
+  void set_dead_path(DeadPathNode* dead_path) {
+    assert(_dead_path == nullptr, "can only set once");
+    _dead_path = dead_path;
+  }
   StartNode*   start() const;              // (Derived from root.)
   void         verify_start(StartNode* s) const NOT_DEBUG_RETURN;
   Node*        immutable_memory();
