7bfb2431e613d7620666e2ee398066c7621bbc0e
Ruben Ayrapetyan <ruben.ayrapetyan@arm.com>
2026-07-13 10:14:32 +0000

    Address review comments and extend the test

--- test/hotspot/jtreg/compiler/vectorapi/TestSelectFromTwoVectorSameOperand.java
+++ test/hotspot/jtreg/compiler/vectorapi/TestSelectFromTwoVectorSameOperand.java
@@ -59,17 +59,20 @@ package compiler.vectorapi;
 
 import java.util.Random;
 import jdk.incubator.vector.*;
+import jdk.test.lib.Asserts;
 
 public class TestSelectFromTwoVectorSameOperand {
-    static final int SIZE = 16;
+    static final int SIZE = 8;
 
     static byte[] byte_input1 = new byte[SIZE];
     static byte[] byte_input2 = new byte[SIZE];
     static byte[] byte_output = new byte[SIZE];
+    static final byte byte_index_mask = 15;
 
     static short[] short_input1 = new short[SIZE / 2];
     static short[] short_input2 = new short[SIZE / 2];
     static short[] short_output = new short[SIZE / 2];
+    static final short short_index_mask = 7;
 
     static {
         Random r = new Random(42);
@@ -85,33 +88,55 @@ public class TestSelectFromTwoVectorSameOperand {
     public static void main(String[] args) {
         for (int i = 0; i < 100_000; ++i) {
             test_byte_src1();
+            verify_byte(byte_input1, byte_input2, byte_input1, byte_output);
             test_byte_src2();
+            verify_byte(byte_input1, byte_input2, byte_input2, byte_output);
             test_short_src1();
+            verify_short(short_input1, short_input2, short_input1, short_output);
             test_short_src2();
+            verify_short(short_input1, short_input2, short_input2, short_output);
         }
     }
 
     static void test_byte_src1() {
-        ByteVector src1 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input1, 0).and((byte) 15);
-        ByteVector src2 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input2, 0).and((byte) 15);
+        ByteVector src1 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input1, 0).and(byte_index_mask);
+        ByteVector src2 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input2, 0).and(byte_index_mask);
         src1.selectFrom(src1, src2).intoArray(byte_output, 0);
     }
 
     static void test_byte_src2() {
-        ByteVector src1 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input1, 0).and((byte) 15);
-        ByteVector src2 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input2, 0).and((byte) 15);
+        ByteVector src1 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input1, 0).and(byte_index_mask);
+        ByteVector src2 = ByteVector.fromArray(ByteVector.SPECIES_64, byte_input2, 0).and(byte_index_mask);
         src2.selectFrom(src1, src2).intoArray(byte_output, 0);
     }
 
     static void test_short_src1() {
-        ShortVector src1 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input1, 0).and((short) 7);
-        ShortVector src2 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input2, 0).and((short) 7);
+        ShortVector src1 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input1, 0).and(short_index_mask);
+        ShortVector src2 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input2, 0).and(short_index_mask);
         src1.selectFrom(src1, src2).intoArray(short_output, 0);
     }
 
     static void test_short_src2() {
-        ShortVector src1 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input1, 0).and((short) 7);
-        ShortVector src2 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input2, 0).and((short) 7);
+        ShortVector src1 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input1, 0).and(short_index_mask);
+        ShortVector src2 = ShortVector.fromArray(ShortVector.SPECIES_64, short_input2, 0).and(short_index_mask);
         src2.selectFrom(src1, src2).intoArray(short_output, 0);
+    }
+
+    static void verify_byte(byte[] src1, byte[] src2, byte[] index, byte[] output) {
+        for (int i = 0; i < SIZE; i++) {
+            int index_value = index[i] & byte_index_mask;
+            byte element_value = (index_value < SIZE) ? src1[index_value] : src2[index_value - SIZE];
+            byte masked_element_value = (byte) (element_value & byte_index_mask);
+            Asserts.assertEQ(masked_element_value, output[i]);
+        }
+    }
+
+    static void verify_short(short[] src1, short[] src2, short[] index, short[] output) {
+        for (int i = 0; i < SIZE / 2; i++) {
+            int index_value = index[i] & short_index_mask;
+            short element_value = (index_value < SIZE / 2) ? src1[index_value] : src2[index_value - SIZE / 2];
+            short masked_element_value = (short) (element_value & short_index_mask);
+            Asserts.assertEQ(masked_element_value, output[i]);
+        }
     }
 }
