c483d4c4b57960d4824ef1d0054f58e37273ad09
Vladimir V. Ivanov <vladimir.x.ivanov@oracle.com>
2026-07-13 10:50:54 -0700

    Update src/hotspot/share/opto/loopopts.cpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/loopopts.cpp
+++ src/hotspot/share/opto/loopopts.cpp
@@ -1414,7 +1414,7 @@ static Node* is_inner_of_stripmined_loop(const Node* out) {
 void PhaseIdealLoop::split_if_with_blocks_post(Node *n) {
   // Cloning Cmp through Phi's involves the split-if transform.
   if (n->is_Cmp()) {
-    assert(!n->is_FastLock(), "should not be materialized yet");
+    assert(!n->is_FastLock(), "should not be materialized, yet");
     Node *n_ctrl = get_ctrl(n);
     // Determine if the Node has inputs from some local Phi.
     // Returns the block to clone thru.
