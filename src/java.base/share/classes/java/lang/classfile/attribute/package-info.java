331357e635edaeeee1640182f3f20955bdd56125
by1361092 <by1361092@outlook.com>
2026-07-20 18:00:28 +0800

    Fix typo in package-info.java documentation

--- src/java.base/share/classes/java/lang/classfile/attribute/package-info.java
+++ src/java.base/share/classes/java/lang/classfile/attribute/package-info.java
@@ -61,7 +61,7 @@
  * <h2 id="writing">Writing Attributes</h2>
  * Most attributes implement at least one of {@link ClassElement}, {@link FieldElement}, {@link MethodElement}, or
  * {@link CodeElement}, so they can be sent to the respective {@link ClassFileBuilder} to be written as part of those
- * structure.  Attributes define if they can {@linkplain AttributeMapper#allowMultiple() appear multiple times} in one
+ * structures.  Attributes define if they can {@linkplain AttributeMapper#allowMultiple() appear multiple times} in one
  * structure; if they cannot, the last attribute instance supplied to the builder is the one written to the final
  * structure.  Some attributes, such as {@link BootstrapMethodsAttribute}, implement none of those interfaces.  They are
  * created through other means, specified in the modeling interface for each of the attributes.  Attributes for a {@link
@@ -69,7 +69,7 @@
  * <p>
  * The attribute factories generally have two sets of factory methods: one that accepts symbolic information
  * representing the uses, and another that accepts constant pool entries.  Most of time, the symbolic factories are
- * sufficent, but the constant pool entry ones can be used for fine-grained control over {@code class} file generation;
+ * sufficient, but the constant pool entry ones can be used for fine-grained control over {@code class} file generation;
  * see "{@linkplain java.lang.classfile.constantpool##writing Writing the constant pool entries}" for more details.
  * <p>
  * Many attributes can be bulk-copied if the data it depends on does not change; this information is exposed in {@link
