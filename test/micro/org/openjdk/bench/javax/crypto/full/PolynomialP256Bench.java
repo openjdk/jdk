de838850e5800500f5aa6f60b142ff76cecf7540
Shawn M Emery <shawn.emery@oracle.com>
2026-07-23 00:29:03 -0600

    8388706: PolynomialP256Bench::benchAssign regression after JDK-8355216

--- test/micro/org/openjdk/bench/javax/crypto/full/PolynomialP256Bench.java
+++ test/micro/org/openjdk/bench/javax/crypto/full/PolynomialP256Bench.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024, 2026,Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -35,6 +35,7 @@ import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.annotations.Benchmark;
 import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 import sun.security.util.math.intpoly.MontgomeryIntegerPolynomialP256;
 import sun.security.util.math.intpoly.IntegerPolynomialP256;
 import sun.security.util.math.MutableIntegerModuloP;
@@ -94,11 +95,13 @@ public class PolynomialP256Bench {
     public MutableIntegerModuloP benchAssign() {
         MutableIntegerModuloP test1 = X.mutable();
         MutableIntegerModuloP test2 = one.mutable();
+        long seed = 1234567890L;
+        Random rand = new Random(seed);
         for (int i = 0; i< 10000; i++) {
-            test1.conditionalSet(test2, 0);
-            test1.conditionalSet(test2, 1);
-            test2.conditionalSet(test1, 0);
-            test2.conditionalSet(test1, 1);
+            test1.conditionalSet(test2, rand.nextInt(2));
+            test1.conditionalSet(test2, rand.nextInt(2));
+            test2.conditionalSet(test1, rand.nextInt(2));
+            test2.conditionalSet(test1, rand.nextInt(2));
         }
         return test2;
     }
