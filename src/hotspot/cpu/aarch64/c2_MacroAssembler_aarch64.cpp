057985b5d147e52efed9ef40423dad7ff3a25e88
Fredrik Bredberg <fbredberg@openjdk.org>
2026-07-27 08:42:23 +0000

    8384807: Populate OMCache from C2
    Reviewed-by: aboldtch, coleenp, shade

--- src/hotspot/cpu/aarch64/c2_MacroAssembler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/c2_MacroAssembler_aarch64.cpp
@@ -219,6 +219,10 @@ void C2_MacroAssembler::fast_lock(Register obj, Register box, Register t1,
     bind(inflated);
 
     const Register t1_monitor = t1;
+    // Offsets into the current thread's object monitor cache (omc).
+    const ByteSize thr_omc_offset     = JavaThread::om_cache_offset();
+    const ByteSize omc_monitor_offset = OMCache::monitor_offset();
+    const ByteSize omc_obj_offset     = OMCache::obj_offset();
 
     if (!UseObjectMonitorTable) {
       assert(t1_monitor == t1_mark, "should be the same here");
@@ -229,18 +233,12 @@ void C2_MacroAssembler::fast_lock(Register obj, Register box, Register t1,
       // Save the mark, we might need it to extract the hash.
       mov(t3, t1_mark);
 
-      // Look for the monitor in the om_cache.
+      // Look for the monitor in the current thread's object monitor cache (omc).
 
-      ByteSize cache_offset   = JavaThread::om_cache_oops_offset();
-      ByteSize monitor_offset = OMCache::oop_to_monitor_difference();
-      const int num_unrolled  = OMCache::CAPACITY;
-      for (int i = 0; i < num_unrolled; i++) {
-        ldr(t1_monitor, Address(rthread, cache_offset + monitor_offset));
-        ldr(t2, Address(rthread, cache_offset));
-        cmp(obj, t2);
-        br(Assembler::EQ, monitor_found);
-        cache_offset = cache_offset + OMCache::oop_to_oop_difference();
-      }
+      ldr(t1_monitor, Address(rthread, thr_omc_offset + omc_monitor_offset));
+      ldr(t2, Address(rthread, thr_omc_offset + omc_obj_offset));
+      cmp(obj, t2);
+      br(Assembler::EQ, monitor_found);
 
       // Look for the monitor in the table.
 
@@ -267,6 +265,10 @@ void C2_MacroAssembler::fast_lock(Register obj, Register box, Register t1,
       bs_asm->try_peek_weak_handle_in_nmethod(this, t3, t3, t2, slow_path);
       cmp(t3, obj);
       br(Assembler::NE, slow_path);
+
+      // Store the monitor in the current thread's object monitor cache (omc).
+      str(t1_monitor, Address(rthread, thr_omc_offset + omc_monitor_offset));
+      str(obj, Address(rthread, thr_omc_offset + omc_obj_offset));
 
       bind(monitor_found);
     }
@@ -296,6 +298,7 @@ void C2_MacroAssembler::fast_lock(Register obj, Register box, Register t1,
 
     bind(monitor_locked);
     if (UseObjectMonitorTable) {
+      // Cache the monitor for unlock.
       str(t1_monitor, Address(box, BasicLock::object_monitor_cache_offset_in_bytes()));
     }
   }
