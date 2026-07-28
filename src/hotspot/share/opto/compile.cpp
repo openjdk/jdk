1eb687d436d9de87c2fa5d61e4265faaa791bc02
Guanqiang Han <hanguanqiang@kylinos.cn>
2026-07-28 08:51:39 +0800

    Add comments

--- src/hotspot/share/opto/compile.cpp
+++ src/hotspot/share/opto/compile.cpp
@@ -2373,6 +2373,8 @@ void Compile::Optimize() {
 
     if (failing())  return;
 
+    // inline_boxing_calls() may enqueue newly discovered calls matched by a
+    // CompileCommand=delayinline directive into _late_inlines.
     inline_incrementally(igvn);
 
     print_method(PHASE_INCREMENTAL_BOXING_INLINE, 2);
