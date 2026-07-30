51368303a31e85dc7a89793479ced13eed30f3ae
Ashay Rane <253344819+raneashay@users.noreply.github.com>
2026-07-30 18:10:57 -0500

    Address PR comments
    1. Uses zero or -1 for initialization so that either
       `is_valid_sve_arith_imm_pattern()` or (non-exclusively)
       `is_vector_bitwise_not_pattern()` is true.
    
    2. Moves the test to be under the compiler/vectorapi directory.
    
    3. Drops the SVE-only requirement.
    
    4. Reduces the loop trip count and `CompileThreshold` to make the test
       run slightly faster.

--- test/hotspot/jtreg/compiler/vectorapi/TestSveClone.java
+++ test/hotspot/jtreg/compiler/vectorapi/TestSveClone.java
@@ -27,32 +27,32 @@
  * @summary Ensure that users of replicate nodes are clone compatible before folding
  * @library /test/lib /
  * @modules jdk.incubator.vector
- * @requires vm.cpu.features ~= ".*sve.*"
  * @run main/othervm --add-modules=jdk.incubator.vector -XX:-TieredCompilation
- *                   -XX:CompileThreshold=100 -XX:-BackgroundCompilation
+ *                   -XX:CompileThreshold=10 -XX:-BackgroundCompilation
  *                   -XX:CompileCommand=compileonly,${test.main.class}::test
  *                   ${test.main.class}
  */
 
-package compiler.c2.aarch64;
+package compiler.vectorapi;
 
 import jdk.incubator.vector.*;
 
 public class TestSveClone {
     static final VectorSpecies<Byte> SB = ByteVector.SPECIES_64;
 
-    static long test() {
+    static long test(int init) {
         return ByteVector.broadcast(SB, (byte) -8)
             .lanewise(VectorOperators.XOR,
-                ByteVector.broadcast(SB, (byte) 0),
+                ByteVector.broadcast(SB, (byte) init),
                 ByteVector.fromArray(SB, new byte[8], 0)
-                    .compare(VectorOperators.EQ, (byte) 0))
+                .compare(VectorOperators.EQ, (byte) init))
             .reduceLanesToLong(VectorOperators.OR);
     }
 
     public static void main(String[] args) {
-        for (int i = 0; i < 20000; i++) {
-          test();
+        for (int i = 0; i < 100; i++) {
+            test(0);    // exercise is_valid_sve_arith_imm_pattern()
+            test(1);    // exercise is_vector_bitwise_not_pattern()
         }
         IO.println("done");
     }
