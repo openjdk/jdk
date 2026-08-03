27ab3b6082ed569cf24df2921a86f04f9b8a4eb0
Andrew Haley <aph@openjdk.org>
2026-08-03 17:11:31 +0100

    Review comments

--- src/hotspot/share/c1/c1_LIRGenerator.cpp
+++ src/hotspot/share/c1/c1_LIRGenerator.cpp
@@ -2930,7 +2930,7 @@ void LIRGenerator::do_Goto(Goto* x) {
     }
     LIR_Opr md_reg = new_register(T_METADATA);
 #ifdef RANDOMIZED_PROFILE_CAPTURE
-    LIR_Opr tmp = new_register(wordSize == 4 ? T_INT : T_LONG);
+    LIR_Opr tmp = new_pointer_register();
     LIR_Opr inc = LIR_OprFact::intConst(DataLayout::counter_increment);
     __ increment_counter(inc, tmp, md_reg, md->constant_encoding(), offset);
 #else
@@ -3913,7 +3913,7 @@ void LIRGenerator::increment_event_counter_impl(CodeEmitInfo* info,
     ShouldNotReachHere();
   }
 
-  LIR_Opr result = new_register(wordSize == 4 ? T_INT : T_LONG);
+  LIR_Opr result = new_pointer_register();
   if (notify && (!backedge || UseOnStackReplacement)) {
     int ratio_shift = exact_log2(ProfileCaptureRatio);
     LIR_Opr meth = LIR_OprFact::metadataConst(method->constant_encoding());
