b172389089b66e9a1898e38ebdd270cb8d9a770f
Patricio Chilano Mateo <patricio.chilano.mateo@oracle.com>
2026-08-02 23:32:24 -0400

    v1

--- src/hotspot/share/runtime/continuationFreezeThaw.cpp
+++ src/hotspot/share/runtime/continuationFreezeThaw.cpp
@@ -1292,10 +1292,6 @@ freeze_result FreezeBase::recurse_freeze_compiled_frame(frame& f, frame& caller,
     address return_pc = ContinuationHelper::CompiledFrame::return_pc(f);
     CodeBlob* caller_cb = CodeCache::find_blob_fast(return_pc);
     assert(nm->is_compiled_by_c2() || (caller_cb->is_nmethod() && caller_cb->as_nmethod()->is_compiled_by_c2()), "caller or callee should be c2 compiled");
-    assert((!caller_cb->is_nmethod() && nm->is_compiled_by_c2()) ||
-           (nm->compiler_type() != caller_cb->as_nmethod()->compiler_type()) ||
-           (nm->is_compiled_by_c2() && !method->is_static() && method->method_holder()->is_inline_klass()),
-           "frame should not be extended");
 #endif
   }
 
---
+++ test/hotspot/jtreg/compiler/valhalla/inlinetypes/TestVirtualThreadMethodHandle.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @bug 8388596
+ * @summary Test freezing a MethodHandle target frame with value class argument
+ * @enablePreview
+ * @requires vm.continuations
+ * @library /test/lib
+ * @run main/othervm -XX:-Inline TestVirtualThreadMethodHandle
+ */
+
+import java.lang.invoke.*;
+
+public class TestVirtualThreadMethodHandle {
+    static value class V { int a=0, b=0, c=0, d=0, e=0, f=0, g=0; }
+
+    static MethodHandle MH;
+
+    static void target(V v) { Thread.yield(); }
+
+    static void run() {
+        try {
+            for (int n = 0; n < 5_000; n++) MH.invokeExact(new V());
+        } catch (Throwable t) { }
+    }
+
+    public static void main(String[] args) throws Exception {
+        MH = MethodHandles.lookup().findStatic(TestVirtualThreadMethodHandle.class, "target", MethodType.methodType(void.class, V.class));
+        run();
+        Thread.startVirtualThread(TestVirtualThreadMethodHandle::run).join();
+    }
+}
