0bdfb3a2a4679ba9a3fdd9f728073b21eab3731f
Coleen Phillimore <coleenp@openjdk.org>
2026-08-04 12:13:31 +0000

    8388791: Refactor JavaClasses Throwable and StackTrace classes
    Reviewed-by: dholmes, sspitsyn, iklam

--- src/hotspot/share/services/threadService.cpp
+++ src/hotspot/share/services/threadService.cpp
@@ -23,6 +23,7 @@
  */
 
 #include "classfile/javaClasses.inline.hpp"
+#include "classfile/javaStackTraceClasses.hpp"
 #include "classfile/systemDictionary.hpp"
 #include "classfile/vmClasses.hpp"
 #include "classfile/vmSymbols.hpp"
