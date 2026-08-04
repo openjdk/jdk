82a96f90e22394ef5341552e8b097bd6010e7002
Guanqiang Han <hanguanqiang@kylinos.cn>
2026-08-04 23:18:19 +0800

    Handle null CRLs in getMatchingCRLs

--- src/java.base/share/classes/sun/security/provider/certpath/URICertStore.java
+++ src/java.base/share/classes/sun/security/provider/certpath/URICertStore.java
@@ -676,7 +676,7 @@ class URICertStore extends CertStoreSpi {
      */
     private static Collection<X509CRL> getMatchingCRLs
         (X509CRL crl, CRLSelector selector) {
-        if (selector == null || (crl != null && selector.match(crl))) {
+        if (crl != null && (selector == null || selector.match(crl))) {
             return Collections.singletonList(crl);
         } else {
             return Collections.emptyList();
