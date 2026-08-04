b19cc6c15d2d0593b055d661224587829d1f7e30
Frederic Parain <fparain@openjdk.org>
2026-08-04 19:47:11 +0000

    8388480: System.arraycopy fails to partially copy nullable flat arrays
    Reviewed-by: coleenp, matsaave

--- src/hotspot/share/oops/objArrayKlass.cpp
+++ src/hotspot/share/oops/objArrayKlass.cpp
@@ -279,6 +279,50 @@ void ObjArrayKlass::copy_array(arrayOop s, int src_pos, arrayOop d,
   ShouldNotReachHere();
 }
 
+void ObjArrayKlass::array_copy_offsets_and_range_check(arrayOop s, int src_pos,
+                                                       arrayOop d, int dst_pos, int length, TRAPS) {
+  // Check if all offsets and lengths are non negative
+  if (src_pos < 0 || dst_pos < 0 || length < 0) {
+    // Pass specific exception reason.
+    ResourceMark rm(THREAD);
+    stringStream ss;
+    if (src_pos < 0) {
+      ss.print("arraycopy: source index %d out of bounds for object array[%d]",
+               src_pos, s->length());
+    } else if (dst_pos < 0) {
+      ss.print(
+          "arraycopy: destination index %d out of bounds for object array[%d]",
+          dst_pos, d->length());
+    } else {
+      ss.print("arraycopy: length %d is negative", length);
+    }
+    THROW_MSG(vmSymbols::java_lang_ArrayIndexOutOfBoundsException(),
+              ss.as_string());
+  }
+
+  // Check if the ranges are valid
+  if ((((unsigned int)length + (unsigned int)src_pos) >
+       (unsigned int)s->length()) ||
+      (((unsigned int)length + (unsigned int)dst_pos) >
+       (unsigned int)d->length())) {
+    // Pass specific exception reason.
+    ResourceMark rm(THREAD);
+    stringStream ss;
+    if (((unsigned int)length + (unsigned int)src_pos) >
+        (unsigned int)s->length()) {
+      ss.print(
+          "arraycopy: last source index %u out of bounds for object array[%d]",
+          (unsigned int)length + (unsigned int)src_pos, s->length());
+    } else {
+      ss.print("arraycopy: last destination index %u out of bounds for object "
+                      "array[%d]",
+                      (unsigned int)length + (unsigned int)dst_pos, d->length());
+    }
+    THROW_MSG(vmSymbols::java_lang_ArrayIndexOutOfBoundsException(),
+              ss.as_string());
+  }
+}
+
 bool ObjArrayKlass::can_be_primary_super_slow() const {
   if (!bottom_klass()->can_be_primary_super())
     // array of interfaces
