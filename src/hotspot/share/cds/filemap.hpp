6449400f23f09a7c6137fb7c5be757861ff77578
Ioi Lam <iklam@openjdk.org>
2026-08-05 23:24:06 +0000

    8389189: Ignore different AOTClassLinking option between AOT training and assembly
    Reviewed-by: kvn, iveresov

--- src/hotspot/share/cds/filemap.hpp
+++ src/hotspot/share/cds/filemap.hpp
@@ -177,6 +177,7 @@ private:
   bool   _use_optimized_module_handling;// No module-relation VM options were specified, so we can skip
                                         // some expensive operations.
   bool   _has_aot_linked_classes;       // Was the CDS archive created with -XX:+AOTClassLinking
+  bool   _aot_class_linking_value;      // The value of the AOTClassLinking variable when this archive was created
   bool   _has_full_module_graph;        // Does this CDS archive contain the full archived module graph?
   bool   _has_valhalla_patched_classes; // Is this archived dumped with --enable-preview?
   CDSMustMatchFlags _must_match;        // These flags must be the same between dumptime and runtime
@@ -235,6 +236,7 @@ public:
   char* mapped_base_address()              const { return _mapped_base_address; }
   bool has_platform_or_app_classes()       const { return _has_platform_or_app_classes; }
   bool has_aot_linked_classes()            const { return _has_aot_linked_classes; }
+  bool aot_class_linking_value()           const { return _aot_class_linking_value; }
   bool compressed_oops()                   const { return _compressed_oops; }
   bool compatible_oop_compression()        const { return _compatible_oop_compression; }
   int narrow_klass_pointer_bits()          const { return _narrow_klass_pointer_bits; }
@@ -323,7 +325,7 @@ public:
   FileMapHeader *header() const       { return _header; }
   static bool get_base_archive_name_from_header(const char* archive_name,
                                                 const char** base_archive_name);
-  static bool is_preimage_static_archive(const char* file);
+  static void check_preimage_static_archive(const char* file);
 
   bool init_from_file(int fd);
 
