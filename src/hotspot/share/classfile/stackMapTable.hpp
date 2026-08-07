43afe8a5a0fa79cc887f336312aaa50ec4e2d2bc
Matias Saavedra Silva <matias.s.saavedra.silva@oracle.com>
2026-08-07 16:12:03 -0400

    Dan comments

--- src/hotspot/share/classfile/stackMapTable.hpp
+++ src/hotspot/share/classfile/stackMapTable.hpp
@@ -134,16 +134,17 @@ class StackMapReader : StackObj {
   // Check if reading first entry
   bool _first;
 
-  // An uninitializedThis in the locals array can be inherited by
-  // subsequent frames while uninitializedThis in the stack will be
-  // discarded as the stack is cleared between frames.
-  bool _uninit_in_locals;
+  // We track whether an uninitializedThis was in the previous frame's
+  // locals independently of the flags parameter as most StackMapTable
+  // frames reuse the same locals as the previous frame.  Chop and
+  // Full frames need to handle this specially.
+  bool _uninit_in_prev_frame_locals;
 
   StackMapFrame* next_helper(TRAPS);
   void check_offset(StackMapFrame* frame);
   void check_size(TRAPS);
   int32_t chop(VerificationType* locals, int32_t length, int32_t chops);
-  VerificationType parse_verification_type(u1* flags, bool local, TRAPS);
+  VerificationType parse_verification_type(u1* flags, bool parsing_locals, TRAPS);
   void check_verification_type_array_size(
       int32_t size, int32_t max_size, TRAPS) {
     if (size < 0 || size > max_size) {
