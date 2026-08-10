39286619ef641cbb7f56a2bc4f1b27be402c8bd7
Manuel Hässig <manuel.hassig@oracle.com>
2026-08-10 13:19:42 +0200

    Add stress.cpp file

--- src/hotspot/share/compiler/stress.hpp
+++ src/hotspot/share/compiler/stress.hpp
@@ -28,88 +28,17 @@
 #include "compiler/compileLog.hpp"
 #include "compiler/compilerDefinitions.hpp"
 #include "compiler/compilerDirectives.hpp"
-#include "compiler_globals.hpp"
 #include "memory/allocation.hpp"
-#include "opto/c2_globals.hpp"
-#include "runtime/globals_extension.hpp"
-#include "runtime/os.hpp"
-#include "utilities/ticks.hpp"
 
 class Stress : public StackObj {
  private:
   uint _stress_seed;
 
-  const uint RANDOMIZED_DOMAIN_POW  = 29;
-  const uint RANDOMIZED_DOMAIN      = 1 << RANDOMIZED_DOMAIN_POW;
-  const uint RANDOMIZED_DOMAIN_MASK = (1 << (RANDOMIZED_DOMAIN_POW + 1)) - 1;
-
-  bool should_initialize_stress_seed(CompilerType comp) {
-    switch (comp) {
-      case compiler_c1:
-        return false;
-      case compiler_c2:
-        return StressLCM || StressGCM || StressIGVN || StressCCP ||
-          StressIncrementalInlining || StressMacroExpansion ||
-          StressMacroElimination || StressUnstableIfTraps ||
-          StressBailout || StressLoopPeeling || StressCountedLoop ||
-          StressEliminateAllocations;
-      default:
-        assert(comp != compiler_none && comp != compiler_number_of_types, "expected valid compiler");
-        return false;
-    }
-  }
-
  public:
-  Stress(DirectiveSet* directives, CompileLog* log, CompilerType comp) {
-    if (!should_initialize_stress_seed(comp)) {
-      _stress_seed = 0;
-      return;
-    }
+  Stress(DirectiveSet* directives, CompileLog* log, CompilerType comp);
 
-    if (FLAG_IS_DEFAULT(StressSeed) || (FLAG_IS_ERGO(StressSeed) && directives->RepeatCompilationOption)) {
-      _stress_seed = static_cast<uint>(Ticks::now().nanoseconds());
-      FLAG_SET_ERGO(StressSeed, _stress_seed);
-    } else {
-      _stress_seed = StressSeed;
-    }
-    if (log != nullptr) {
-      log->elem("stress_test seed='%u'", _stress_seed);
-    }
-  }
-
-  uint random() {
-    _stress_seed = os::next_random(_stress_seed);
-    return _stress_seed;
-  }
-
-  // This method can be called an arbitrary number of times, with the current count
-  // as the argument. The logic allows for selecting a single candidate from the
-  // running list of candidates as follows:
-  //    int count = 0;
-  //    Cand* selected = null;
-  //    while(cand = cand->next()) {
-  //      if (randomized_select(++count)) {
-  //        selected = cand;
-  //      }
-  //    }
-  //
-  // Including the count equalizes the chances any candidate is "selected".
-  // This is useful when we don't have the complete list of candidates to choose
-  // from uniformly. In this case, we need to adjust the randomicity of the
-  // selection, or else we will end up biasing the selection towards the latter
-  // candidates.
-  //
-  // A quick back-of-the-envelope calculation shows that for the list of n candidates
-  // the equal probability for the candidate to persist as "best" as can be
-  // achieved by replacing it with "next" k-th candidate with the probability
-  // of 1/k. It can be easily shown that by the end of the run, the
-  // probability for any candidate has converged to 1/n, thus giving the
-  // uniform distribution among all the candidates.
-  //
-  // We don't care about the domain size as long as (RANDOMIZED_DOMAIN / count) is large.
-  bool randomized_select(uint count) {
-    return (random() & RANDOMIZED_DOMAIN_MASK) < (RANDOMIZED_DOMAIN / count);
-  }
+  uint random();
+  bool randomized_select(uint count);
 };
 
 #endif // SHARE_COMPILER_STRESS_HPP
