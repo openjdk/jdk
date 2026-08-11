3f4a3745c47855e62b9b6c99e61f19b429a9126f
Valerie Peng <valerie.peng@oracle.com>
2026-08-11 22:16:09 +0000

    removed unused import

--- test/jdk/sun/security/ec/NONEwithECDSAOffsetTest.java
+++ test/jdk/sun/security/ec/NONEwithECDSAOffsetTest.java
@@ -25,7 +25,6 @@ import java.nio.ByteBuffer;
 import java.security.KeyPairGenerator;
 import java.security.PrivateKey;
 import java.security.Provider;
-import java.security.SecureRandom;
 import java.security.Security;
 import java.security.Signature;
 import java.security.SignatureException;
