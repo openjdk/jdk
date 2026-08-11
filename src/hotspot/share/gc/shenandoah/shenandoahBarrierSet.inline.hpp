ad8ddaa5ff071ee098a04cf8c2b5ef91922a5de7
Aleksey Shipilev <aleksey@shipilev.net>
2026-08-11 16:06:03 +0200

    Revert "Only call post-barrier when CAS succeeded"
    This reverts commit bfd4463fbf7e732b23bc49ccba3bd3d087ccd7eb.

--- src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.inline.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahBarrierSet.inline.hpp
@@ -315,9 +315,7 @@ inline oop ShenandoahBarrierSet::AccessBarrier<decorators, BarrierSetT>::oop_ato
   ShenandoahBarrierSet* bs = barrier_set();
   bs->oop_cmpxchg_pre(decorators, addr, compare_value, new_value, /* in_heap = */ false);
   oop result = Raw::oop_atomic_cmpxchg_not_in_heap(addr, compare_value, new_value);
-  if (result == compare_value) {
-    bs->oop_store_post(decorators, addr, new_value, /* in_heap = */ false);
-  }
+  bs->oop_store_post(decorators, addr, new_value, /* in_heap = */ false);
   return result;
 }
 
@@ -327,9 +325,7 @@ inline oop ShenandoahBarrierSet::AccessBarrier<decorators, BarrierSetT>::oop_ato
   ShenandoahBarrierSet* bs = barrier_set();
   bs->oop_cmpxchg_pre(decorators, addr, compare_value, new_value, /* in_heap = */ true);
   oop result = Raw::oop_atomic_cmpxchg_in_heap(addr, compare_value, new_value);
-  if (result == compare_value) {
-    bs->oop_store_post(decorators, addr, new_value, /* in_heap = */ true);
-  }
+  bs->oop_store_post(decorators, addr, new_value, /* in_heap = */ true);
   return result;
 }
 
@@ -341,9 +337,7 @@ inline oop ShenandoahBarrierSet::AccessBarrier<decorators, BarrierSetT>::oop_ato
   ShenandoahBarrierSet* bs = barrier_set();
   bs->oop_cmpxchg_pre(resolved_decorators, addr, compare_value, new_value, /* in_heap = */ true);
   oop result = Raw::oop_atomic_cmpxchg_in_heap(addr, compare_value, new_value);
-  if (result == compare_value) {
-    bs->oop_store_post(resolved_decorators, addr, new_value, /* in_heap = */ true);
-  }
+  bs->oop_store_post(resolved_decorators, addr, new_value, /* in_heap = */ true);
   return result;
 }
 
