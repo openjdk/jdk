openjdk/jdk

commit 9802c795e724ad231a893921cf6b079cdc3bb25d

@ashu-mehra comment -- refactored "soft" check of module paths into AOTClassLocation::check_module_paths_exact_match()

src/hotspot/share/cds/aotClassLocation.cpp
@@ -98,6 +98,7 @@ public:
 
   int current() const { return _current; }
   bool is_empty() const { return _array.length() == 0; }
+  int size() const { return _array.length(); }
 };
 
 class BootCpClassLocationStream : public ClassLocationStream {
@@ -832,10 +833,12 @@ bool AOTClassLocationConfig::check_paths_existence(ClassLocationStream& runtime_
   return exist;
 }
 
-bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, int index_start, int index_end,
-                                                ClassLocationStream& runtime_css,
-                                                bool* module_paths_mismatch) const {
-  if (index_start >= index_end && runtime_css.is_empty()) { // nothing to check
+bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, bool has_full_module_graph,
+                                                ClassLocationStream& runtime_module_css) const {
+  const int index_start = module_path_start_index();
+  const int index_end = module_path_end_index();
+
+  if (index_start >= index_end && runtime_module_css.is_empty()) { // nothing to check
     return true;
   }
 
@@ -849,11 +852,13 @@ bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, int
     print_dumptime_classpath(ls, index_start, index_end, false, 0, nullptr, 0);
     ls.print_cr("'");
     ls.print("- actual   : '");
-    runtime_css.print(&ls);
+    runtime_module_css.print(&ls);
     ls.print_cr("'");
   }
 
-  // Make sure all the dumptime module paths exist and are unchanged
+  // All JAR files in the dumptime module paths must exist and must be unchanged,
+  // or else we have a "hard" failure, as we can no longer guarantee that archived
+  // classes from the module paths remain unchanged at runtime.
   for (int i = index_start; i < index_end; i++) {
     const AOTClassLocation* cs = class_location_at(i);
     const char* dumptime_path = cs->path();
@@ -866,37 +871,57 @@ bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, int
     }
   }
 
-  // We allow runtime_css to be a superset of the module paths specified in dumptime. E.g.,
-  // Dumptime:    A:C
-  // Runtime:     A:B:C
-  runtime_css.start();
-  for (int i = index_start; i < index_end; i++) {
-    const AOTClassLocation* cs = class_location_at(i);
-    const char* dumptime_path = cs->path();
-
-    while (true) {
-      if (!runtime_css.has_next()) {
-        aot_log_warning(aot)("module path has fewer elements than expected");
-        *module_paths_mismatch = true;
-        return true;
+  if (!check_module_paths_exact_match(runtime_module_css)) {
+    if (CDSConfig::new_aot_flags_used()) {
+      // New AOT workflow requires an exact match for --module-paths.
+      return false;
+    } else {
+      // For classical CDS, we have a "soft" failure if the runtime module paths are
+      // not an exact match with the dumptime module paths:
+      //   Classes from the module path will not be loaded if they are
+      //   rejected by SystemDictionary::is_shared_class_visible().
+      if (has_full_module_graph) {
+        CDSConfig::disable_full_module_graph();
+        AOTMetaspace::report_loading_error("full module graph: disabled because extra module path(s) are specified");
       }
-      // Both this->class_locations() and runtime_css are alphabetically sorted. Skip
-      // items in runtime_css until we see dumptime_path.
-      const char* runtime_path = runtime_css.get_next();
-      if (!os::same_files(dumptime_path, runtime_path)) {
-        aot_log_warning(aot)("the %d-th module path element is different: expected %s actual %s",
-                             i - index_start, dumptime_path, runtime_path);
-        *module_paths_mismatch = true;
-        return true;
-      } else {
-        break;
+
+      if (CDSConfig::is_dumping_dynamic_archive() && num_module_paths() > 0) {
+        CDSConfig::disable_dumping_dynamic_archive();
+        aot_log_warning(aot)("Dynamic archiving is disabled because base layer archive has a different module path");
       }
     }
   }
 
-  if (runtime_css.has_next()) {
-    aot_log_warning(aot)("module path has more elements than expected");
-    *module_paths_mismatch = true;
+  return true;
+}
+
+bool AOTClassLocationConfig::check_module_paths_exact_match(ClassLocationStream& runtime_module_css) const {
+  const int index_start = module_path_start_index();
+  const int index_end = module_path_end_index();
+
+  runtime_module_css.start();
+  for (int i = index_start; i < index_end; i++) {
+    const AOTClassLocation* cs = class_location_at(i);
+    const char* dumptime_path = cs->path();
+
+    if (!runtime_module_css.has_next()) {
+      aot_log_warning(aot)("module path has fewer elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
+      return false;
+    }
+    int n = runtime_module_css.current();
+    const char* runtime_path = runtime_module_css.get_next();
+    // Both dumptime and runtime module paths are alphabetically sorted, so we just need to
+    // compare each element at the same position.
+    if (!os::same_files(dumptime_path, runtime_path)) {
+      aot_log_warning(aot)("the %d-th module path element is different: expected %s actual %s",
+                           n, dumptime_path, runtime_path);
+      return false;
+    }
+  }
+
+  if (runtime_module_css.has_next()) {
+    aot_log_warning(aot)("module path has more elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
+    return false;
   }
 
   return true;
@@ -976,6 +1001,7 @@ bool AOTClassLocationConfig::validate(const char* cache_filename, bool has_aot_l
 
   log_locations(cache_filename, /*is_write=*/false);
 
+  // (1) Check JRT modules image
   const char* jrt = ClassLoader::get_jrt_entry()->name();
   log_info(class, path)("Checking [0] (modules image)");
   bool success = class_location_at(0)->check(jrt, has_aot_linked_classes);
@@ -984,13 +1010,8 @@ bool AOTClassLocationConfig::validate(const char* cache_filename, bool has_aot_l
     return false;
   }
 
-  bool module_paths_mismatch = false;
-
-  if (class_locations()->length() == 1) {
-    if ((module_path_start_index() >= module_path_end_index()) && Arguments::get_property("jdk.module.path") != nullptr) {
-      module_paths_mismatch = true;
-    }
-  } else {
+  {
+    // (2) Check boot/app classpath
     bool use_lcp_match = need_lcp_match(all_css);
     const char* runtime_lcp;
     size_t runtime_lcp_len;
@@ -1015,36 +1036,14 @@ bool AOTClassLocationConfig::validate(const char* cache_filename, bool has_aot_l
       log_info(class, path)("Archived app classpath validation: %s", success ? "passed" : "failed");
     }
 
+    // (3) Check module paths
     if (success) {
-      success = check_module_paths(has_aot_linked_classes, module_path_start_index(), module_path_end_index(),
-                                   all_css.module_path(), &module_paths_mismatch);
-      log_info(class, path)("Archived module path validation: %s%s", success ? "passed" : "failed",
-                            (module_paths_mismatch) ? " (module paths mismatch)" : "");
+      success = check_module_paths(has_aot_linked_classes, has_full_module_graph, all_css.module_path());
+      log_info(class, path)("Archived module path validation: %s", success ? "passed" : "failed");
     }
 
     if (runtime_lcp_len > 0) {
       os::free((void*)runtime_lcp);
-    }
-  }
-
-  if (success && module_paths_mismatch) {
-    if (CDSConfig::new_aot_flags_used()) {
-      // New AOT workflow requires --module-paths to be identical.
-      success = false;
-    } else {
-      // module_paths_mismatch is a "soft" failure for traditional CDS: archived
-      // classes from the module path will not be loaded if they are
-      // rejected by SystemDictionary::is_shared_class_visible().
-      if (has_full_module_graph) {
-        CDSConfig::disable_full_module_graph();
-        AOTMetaspace::report_loading_error("full module graph: disabled because extra module path(s) are specified");
-      }
-
-      if (CDSConfig::is_dumping_dynamic_archive() && num_module_paths() > 0 && module_paths_mismatch) {
-        CDSConfig::disable_dumping_dynamic_archive();
-        aot_log_warning(aot)(
-          "Dynamic archiving is disabled because base layer archive has a different module path");
-      }
     }
   }
 

src/hotspot/share/cds/aotClassLocation.hpp
@@ -164,8 +164,8 @@ class AOTClassLocationConfig : public CHeapObj<mtClassShared> {
   bool check_classpaths(bool is_boot_classpath, bool has_aot_linked_classes,
                         int index_start, int index_end, ClassLocationStream& runtime_css,
                         bool use_lcp_match, const char* runtime_lcp, size_t runtime_lcp_len) const;
-  bool check_module_paths(bool has_aot_linked_classes, int index_start, int index_end, ClassLocationStream& runtime_css,
-                          bool* module_paths_mismatch) const;
+  bool check_module_paths(bool has_aot_linked_classes, bool has_full_module_graph, ClassLocationStream& runtime_module_css) const;
+  bool check_module_paths_exact_match(ClassLocationStream& runtime_module_css) const;
   bool file_exists(const char* filename) const;
   bool check_paths_existence(ClassLocationStream& runtime_css) const;
 

test/hotspot/jtreg/runtime/cds/appcds/aotCache/InvalidModuleOptions.java
@@ -34,6 +34,7 @@
  * @run driver InvalidModuleOptions
  */
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import jdk.test.lib.cds.CDSModulePackager;
@@ -46,26 +47,32 @@ import jdk.test.lib.StringArrayUtils;
 public class InvalidModuleOptions {
     static final Path modulesSrc = Paths.get(System.getProperty("test.src")).resolve("modules");
     static final Path modulesSrc2 = Paths.get(System.getProperty("test.src")).resolve("modules2");
+    static final Path modulesSrc3 = Paths.get(System.getProperty("test.src")).resolve("modules3");
     static String appJar = ClassFileInstaller.getJarPath("hello.jar");
     static String aotConfigFile = "hello.aotconfig";
     static String aotCacheFile = "hello.aot";
     static String helloClass = "Hello";
-    static String modulePath;
+    static String modulePath1;
     static String modulePath2;
+    static String modulePath3;
 
     static String[][] testCases = {
         // --module-path
         {
-            "<mp>", null,
-            "module path has fewer elements than expected",
+            "<mp1>", null,
+            "module path has fewer elements (0) than expected (1)",
         },
         {
-            "<mp>", "<mp2>",
+            "<mp1>", "<mp2>",
             "the 0-th module path element is different",
         },
         {
-            null, "<mp>",
-            "module path has more elements than expected",
+            "<mp12>", "<mp13>",
+            "the 1-th module path element is different",
+        },
+        {
+            null, "<mp1>",
+            "module path has more elements (1) than expected (0)",
         },
 
         // --add-modules
@@ -97,13 +104,17 @@ public class InvalidModuleOptions {
     };
 
     public static void main(String[] args) throws Exception {
-        CDSModulePackager modulePackager = new CDSModulePackager(modulesSrc);
-        modulePackager.createModularJar("com.test");
-        modulePath = modulePackager.getOutputDir().toString();
+        CDSModulePackager modulePackager1 = new CDSModulePackager(modulesSrc, Paths.get("test-modules"));
+        modulePackager1.createModularJar("com.test");
+        modulePath1 = modulePackager1.getOutputDir().toString();
 
         CDSModulePackager modulePackager2 = new CDSModulePackager(modulesSrc2, Paths.get("test-modules2"));
         modulePackager2.createModularJar("com.moretest");
         modulePath2 = modulePackager2.getOutputDir().toString();
+
+        CDSModulePackager modulePackager3 = new CDSModulePackager(modulesSrc3, Paths.get("test-modules3"));
+        modulePackager3.createModularJar("com.evenmoretest");
+        modulePath3 = modulePackager3.getOutputDir().toString();
 
         for (int i = 0; i < testCases.length; i++) {
             String[] testSpec = testCases[i];
@@ -150,10 +161,14 @@ public class InvalidModuleOptions {
     }
 
     static String[] concat(String[] opts, String extra) {
-        if (extra.equals("<mp>")) {
-            return StringArrayUtils.concat(opts, "--module-path", modulePath);
+        if (extra.equals("<mp1>")) {
+            return StringArrayUtils.concat(opts, "--module-path", modulePath1);
         } else if (extra.equals("<mp2>")) {
             return StringArrayUtils.concat(opts, "--module-path", modulePath2);
+        } else if (extra.equals("<mp12>")) {
+            return StringArrayUtils.concat(opts, "--module-path", modulePath1 + File.pathSeparator + modulePath2);
+        } else if (extra.equals("<mp13>")) {
+            return StringArrayUtils.concat(opts, "--module-path", modulePath1 + File.pathSeparator + modulePath3);
         } else {
             return StringArrayUtils.concat(opts, extra);
         }

test/hotspot/jtreg/runtime/cds/appcds/aotCache/modules3/com.evenmoretest/com/evenmoretest/Baz.java
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+package com.evenmoretest;
+
+public class Baz {}

test/hotspot/jtreg/runtime/cds/appcds/aotCache/modules3/com.evenmoretest/module-info.java
@@ -1,0 +1,27 @@
+/*
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+module com.evenmoretest {
+  exports com.evenmoretest;
+}

commit 062382e5ef9c7cd556067e2a648df3d056ba4b4c

Fixed test errors introduced by 9802c795e724ad231a893921cf6b079cdc3bb25d

src/hotspot/share/cds/aotClassLocation.cpp
@@ -834,14 +834,9 @@ bool AOTClassLocationConfig::check_paths_existence(ClassLocationStream& runtime_
 }
 
 bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, bool has_full_module_graph,
-                                                ClassLocationStream& runtime_module_css) const {
+                                                ModulePathClassLocationStream& runtime_module_css) const {
   const int index_start = module_path_start_index();
   const int index_end = module_path_end_index();
-
-  if (index_start >= index_end && runtime_module_css.is_empty()) { // nothing to check
-    return true;
-  }
-
   ResourceMark rm;
 
   LogTarget(Info, class, path) lt;
@@ -895,7 +890,12 @@ bool AOTClassLocationConfig::check_module_paths(bool has_aot_linked_classes, boo
   return true;
 }
 
-bool AOTClassLocationConfig::check_module_paths_exact_match(ClassLocationStream& runtime_module_css) const {
+bool AOTClassLocationConfig::check_module_paths_exact_match(ModulePathClassLocationStream& runtime_module_css) const {
+  if (runtime_module_css.has_non_jar_modules()) {
+    AOTMetaspace::report_loading_error("module path contains sub-directories or non-JAR files (incompatible with full module graph)");
+    return false;
+  }
+
   const int index_start = module_path_start_index();
   const int index_end = module_path_end_index();
 
@@ -905,7 +905,7 @@ bool AOTClassLocationConfig::check_module_paths_exact_match(ClassLocationStream&
     const char* dumptime_path = cs->path();
 
     if (!runtime_module_css.has_next()) {
-      aot_log_warning(aot)("module path has fewer elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
+      AOTMetaspace::report_loading_error("module path has fewer elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
       return false;
     }
     int n = runtime_module_css.current();
@@ -913,14 +913,14 @@ bool AOTClassLocationConfig::check_module_paths_exact_match(ClassLocationStream&
     // Both dumptime and runtime module paths are alphabetically sorted, so we just need to
     // compare each element at the same position.
     if (!os::same_files(dumptime_path, runtime_path)) {
-      aot_log_warning(aot)("the %d-th module path element is different: expected %s actual %s",
-                           n, dumptime_path, runtime_path);
+      AOTMetaspace::report_loading_error("the %d-th module path element is different: expected %s actual %s",
+                                         n, dumptime_path, runtime_path);
       return false;
     }
   }
 
   if (runtime_module_css.has_next()) {
-    aot_log_warning(aot)("module path has more elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
+    AOTMetaspace::report_loading_error("module path has more elements (%d) than expected (%d)", runtime_module_css.size(), num_module_paths());
     return false;
   }
 

src/hotspot/share/cds/aotClassLocation.hpp
@@ -36,6 +36,7 @@
 class AllClassLocationStreams;
 class ClassLocationStream;
 class ClassPathZipEntry;
+class ModulePathClassLocationStream;
 class LogStream;
 
 // An AOTClassLocation is a location where the application is configured to load Java classes
@@ -164,8 +165,8 @@ class AOTClassLocationConfig : public CHeapObj<mtClassShared> {
   bool check_classpaths(bool is_boot_classpath, bool has_aot_linked_classes,
                         int index_start, int index_end, ClassLocationStream& runtime_css,
                         bool use_lcp_match, const char* runtime_lcp, size_t runtime_lcp_len) const;
-  bool check_module_paths(bool has_aot_linked_classes, bool has_full_module_graph, ClassLocationStream& runtime_module_css) const;
-  bool check_module_paths_exact_match(ClassLocationStream& runtime_module_css) const;
+  bool check_module_paths(bool has_aot_linked_classes, bool has_full_module_graph, ModulePathClassLocationStream& runtime_module_css) const;
+  bool check_module_paths_exact_match(ModulePathClassLocationStream& runtime_module_css) const;
   bool file_exists(const char* filename) const;
   bool check_paths_existence(ClassLocationStream& runtime_css) const;
 

test/hotspot/jtreg/runtime/cds/appcds/aotCache/InvalidModuleOptions.java
@@ -104,7 +104,7 @@ public class InvalidModuleOptions {
     };
 
     public static void main(String[] args) throws Exception {
-        CDSModulePackager modulePackager1 = new CDSModulePackager(modulesSrc, Paths.get("test-modules"));
+        CDSModulePackager modulePackager1 = new CDSModulePackager(modulesSrc, Paths.get("test-modules1"));
         modulePackager1.createModularJar("com.test");
         modulePath1 = modulePackager1.getOutputDir().toString();
 
@@ -136,7 +136,7 @@ public class InvalidModuleOptions {
                 "-XX:AOTMode=create",
                 "-XX:AOTConfiguration=" + aotConfigFile,
                 "-XX:AOTCache=" + aotCacheFile,
-                "-Xlog:cds,aot",
+                "-Xlog:cds,aot,class+path",
                 "-cp", appJar
             };
             if (assemblyOpt != null) {

test/hotspot/jtreg/runtime/cds/appcds/jigsaw/modulepath/ModulePathAndFMG.java
@@ -84,6 +84,7 @@ public class ModulePathAndFMG {
     private static String TEST_FROM_JAR = "class,load.*com.foos.Test.*[.]jar";
     private static String TEST_FROM_CDS = "class,load.*com.foos.Test.*shared objects file";
     private static String MAP_FAILED  = "Unable to use shared archive";
+    private static String NON_JAR_FILES = "module path contains sub-directories or non-JAR files";
     private static String PATH_SEPARATOR = File.pathSeparator;
     private static String appClasses[] = {MAIN_CLASS, TEST_CLASS};
     private static String prefix[] = {"-Djava.class.path=", "-Xlog:cds,class+load,class+path=info"};
@@ -116,9 +117,6 @@ public class ModulePathAndFMG {
         dupDir = Files.createTempDirectory(USER_DIR, DUP_LIBS);
         dupJar = dupDir.resolve(DUP_MODULE + ".jar");
         Files.copy(testJar, dupJar, StandardCopyOption.REPLACE_EXISTING);
-
-        badJar = libsDir.resolve(MAIN_MODULE + ".JAR");
-        Files.copy(mainJar, badJar, StandardCopyOption.REPLACE_EXISTING);
     }
 
     public static void buildJmod() throws Exception {
@@ -136,6 +134,9 @@ public class ModulePathAndFMG {
     public static void main(String... args) throws Exception {
         runWithModulePath();
         runWithExplodedModule();
+
+        badJar = libsDir.resolve(MAIN_MODULE + ".JAR");
+        Files.copy(mainJar, badJar, StandardCopyOption.REPLACE_EXISTING);
         runWithJmodAndBadJar();
     }
 
@@ -329,18 +330,22 @@ public class ModulePathAndFMG {
             .assertAbnormalExit(out -> {
                 out.shouldContain(FMG_DISABLED)
                    .shouldNotContain(FMG_ENABLED)
+                   .shouldContain(NON_JAR_FILES)
                    .shouldContain(FIND_EXCEPTION_MESSAGE);
             });
 
         runModulePath += PATH_SEPARATOR + testJar.toString();
+
+        // non-jar files in runtime --module is incompatible with FMG
         tty("12. run with CDS on, with module path com.bars.jar:com.foos.jmod:com.foos.jar");
         TestCommon.runWithModules(prefix,
                                  null,               // --upgrade-module-path
                                  runModulePath, // --module-path
                                  MAIN_MODULE)        // -m
             .assertNormalExit(out -> {
-                out.shouldNotContain(FMG_DISABLED)
-                   .shouldContain(FMG_ENABLED)
+                out.shouldContain(FMG_DISABLED)
+                   .shouldNotContain(FMG_ENABLED)
+                   .shouldContain(NON_JAR_FILES)
                    .shouldMatch(TEST_FROM_CDS)
                    .shouldMatch(MAIN_FROM_CDS)
                    .shouldContain(CLASS_FOUND_MESSAGE);
@@ -355,6 +360,7 @@ public class ModulePathAndFMG {
             .assertAbnormalExit(out -> {
                 out.shouldContain(FMG_DISABLED)
                    .shouldNotContain(FMG_ENABLED)
+                   .shouldContain(NON_JAR_FILES)
                    .shouldMatch(MODULE_NOT_RECOGNIZED);
             });
     }