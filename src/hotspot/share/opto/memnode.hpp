e2a12a228f9c1f6d9be2c8c588d8bdd6e9ef51c6
rwestrel <rwestrel@redhat.com>
2026-08-14 09:04:49 +0200

    fix

--- src/hotspot/share/opto/memnode.hpp
+++ src/hotspot/share/opto/memnode.hpp
@@ -906,7 +906,11 @@ public:
   uint8_t barrier_data() { return _barrier_data; }
   void set_barrier_data(uint8_t barrier_data) { _barrier_data = barrier_data; }
 
-#ifndef PRODUCT
+#ifdef ASSERT
+  void set_adr_type(const TypePtr* adr_type) { _adr_type = adr_type; }
+#endif
+
+  #ifndef PRODUCT
   virtual void dump_spec(outputStream *st) const;
 #endif
 
