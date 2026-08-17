74852451353874dee7fe29460e70463289e50583
Leonid Mesnik <leonid.mesnik@gmail.com>
2026-08-17 14:57:37 -0700

    fixed after feedback

--- src/hotspot/share/services/threadService.cpp
+++ src/hotspot/share/services/threadService.cpp
@@ -1503,11 +1503,12 @@ oop ThreadSnapshotFactory::get_thread_snapshot(jobject jthread, TRAPS) {
   if (cl._locks != nullptr && cl._locks->length() > 0) {
     Symbol* lock_sym = vmSymbols::jdk_internal_vm_ThreadLock();
     Klass* lock_k = SystemDictionary::resolve_or_fail(lock_sym, true, CHECK_NULL);
+    if (lock_k->should_be_initialized()) {
+      lock_k->initialize(CHECK_NULL);
+    }
+
     InstanceKlass* lock_klass = InstanceKlass::cast(lock_k);
     locks = oopFactory::new_refArray_handle(lock_klass, cl._locks->length(), CHECK_NULL);
-    if (lock_klass->should_be_initialized()) {
-      lock_klass->initialize(CHECK_NULL);
-    }
     for (int n = 0; n < cl._locks->length(); n++) {
       GetThreadSnapshotHandshakeClosure::OwnedLock* lock_info = cl._locks->adr_at(n);
 
