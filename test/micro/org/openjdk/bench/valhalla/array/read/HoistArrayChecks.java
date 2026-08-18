046c19ed1c440c43ee32f23c11819b680d5c3e55
Guoxiong Li <lgxbslgx@gmail.com>
2026-08-18 14:21:48 +0800

    JDK-8390501

--- test/micro/org/openjdk/bench/valhalla/array/read/HoistArrayChecks.java
+++ test/micro/org/openjdk/bench/valhalla/array/read/HoistArrayChecks.java
@@ -31,7 +31,7 @@ import org.openjdk.jmh.annotations.*;
 @State(Scope.Thread)
 @Warmup(iterations = 5, time = 1)
 @Measurement(iterations = 5, time = 1)
-@Fork(3)
+@Fork(value = 3, jvmArgsAppend = {"--add-opens", "java.base/jdk.internal.value=ALL-UNNAMED", "--enable-preview"})
 public class HoistArrayChecks {
     private static final int SIZE = 1_000_000;
 
