d045df81b0509a71a78b1bdfb8e3119bebdde375
Vicente Romero <vicente.romero@oracle.com>
2026-08-18 12:25:16 -0400

    addressing review comments

--- test/langtools/tools/javac/generics/typevars/TypeVarArrayInBoundTest.java
+++ test/langtools/tools/javac/generics/typevars/TypeVarArrayInBoundTest.java
@@ -4,9 +4,9 @@
  * @summary javac crashes with NullPointerException in Types.erasure when
  *          evaluating bounds of mutually dependent array type variables
  *          inside an intersection type definition
- * @build SigCyclic
- * @compile SigCyclicTest.java
+ * @build TypeVarArrayInBound
+ * @compile TypeVarArrayInBoundTest.java
  */
-class SigCyclicTest {
-    SigCyclic<?, ?> field;
+class TypeVarArrayInBoundTest {
+    TypeVarArrayInBound<?, ?> field;
 }
