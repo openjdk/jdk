711b1eeed2bb9c21ba55679e48975964883d6618
Jan Lahoda <jan.lahoda@oracle.com>
2026-08-21 14:04:59 +0200

    Cleanup.

--- test/langtools/tools/javac/modules/AutomaticModules.java
+++ test/langtools/tools/javac/modules/AutomaticModules.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2015, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /**
  * @test
- * @bug 8155026 8178011 8220702 8261625
+ * @bug 8155026 8178011 8220702 8261625 8390284
  * @summary Test automatic modules
  * @library /tools/lib
  * @modules
