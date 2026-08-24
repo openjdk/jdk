50d521c488557dae9878e466580a22f1504b77e5
Jatin Bhateja <jatin.bhateja@amd.com>
2026-08-24 12:07:03 +0000

    Review comments resolution

--- src/hotspot/cpu/x86/vm_version_x86.cpp
+++ src/hotspot/cpu/x86/vm_version_x86.cpp
@@ -1573,16 +1573,17 @@ void VM_Version::get_processor_features() {
       if (FLAG_IS_DEFAULT(UseUnalignedLoadStores)) {
         FLAG_SET_DEFAULT(UseUnalignedLoadStores, true);
       }
-#ifdef COMPILER2
-      // Enable UseFPUForSpilling on Zen1/Zen2 (family 0x17) and Hygon Dhyana (family 0x18).
-      // On Zen3 (family 0x19) and beyond it should be default off.
-      if (cpu_family() < 0x19) {
-        if (supports_sse4_2() && FLAG_IS_DEFAULT(UseFPUForSpilling)) {
-          FLAG_SET_DEFAULT(UseFPUForSpilling, true);
-        }
-      }
-#endif // COMPILER2
     }
+
+#ifdef COMPILER2
+    // Enable UseFPUForSpilling on Zen1/Zen2 (family 0x17) and Hygon Dhyana (family 0x18).
+    // On Zen3 (family 0x19) and beyond it should be default off.
+    if (cpu_family() >= 0x17 && cpu_family() < 0x19) {
+      if (supports_sse4_2() && FLAG_IS_DEFAULT(UseFPUForSpilling)) {
+        FLAG_SET_DEFAULT(UseFPUForSpilling, true);
+      }
+    }
+#endif // COMPILER2
 
   }
 
