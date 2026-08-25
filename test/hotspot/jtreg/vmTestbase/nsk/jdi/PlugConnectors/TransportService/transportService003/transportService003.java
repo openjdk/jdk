31b9dadce4eb25e3c2eb4d3073af4353f0fe320b
Shiv Shah <shiv.shah@oracle.com>
2026-08-25 02:49:22 +0000

    8390334: Remove deprecated println() method from nsk.share.Log
    Reviewed-by: cjplummer, coleenp, sspitsyn, dholmes

--- test/hotspot/jtreg/vmTestbase/nsk/jdi/PlugConnectors/TransportService/transportService003/transportService003.java
+++ test/hotspot/jtreg/vmTestbase/nsk/jdi/PlugConnectors/TransportService/transportService003/transportService003.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2003, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2003, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -111,7 +111,7 @@ public class transportService003 {
     }
 
     private static void logAlways(String message) {
-        logHandler.println(message);
+        logHandler.display(message);
     }
 
     public static void main (String argv[]) {
