9096e2917a410f0964c5e788ee393b946f5a23ab
David Briemann <dbriemann@openjdk.org>
2026-08-28 11:06:29 +0000

    8385696: Remove unnecessary ICache flush calls before code copy operations
    Reviewed-by: mdoerr, amitkumar, kvn, fyang

--- src/hotspot/cpu/riscv/downcallLinker_riscv.cpp
+++ src/hotspot/cpu/riscv/downcallLinker_riscv.cpp
@@ -383,5 +383,5 @@ void DowncallLinker::StubGenerator::generate() {
 
   //////////////////////////////////////////////////////////////////////////////
 
-  __ flush();
+  // Code will be copied. No ICache sync required.
 }
