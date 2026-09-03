07eaa9522f1636f67b4c46b8b1f5329276c9c7b7
Tobias Hartmann <thartmann@openjdk.org>
2026-09-03 11:18:30 +0000

    8391750: Disable StressVerifyMeetJoin in CTW until JDK-8391748 is fixed
    Reviewed-by: chagedorn, qamai

--- test/hotspot/jtreg/testlibrary/ctw/src/sun/hotspot/tools/ctw/CtwRunner.java
+++ test/hotspot/jtreg/testlibrary/ctw/src/sun/hotspot/tools/ctw/CtwRunner.java
@@ -323,7 +323,6 @@ public class CtwRunner {
                 "-XX:+StressMacroExpansion",
                 "-XX:+StressMacroElimination",
                 "-XX:+StressIncrementalInlining",
-                "-XX:+StressVerifyMeetJoin",
                 // StressSeed is uint
                 "-XX:StressSeed=" + rng.nextInt(Integer.MAX_VALUE),
                 // Do not fail on huge methods where StressGCM makes register
