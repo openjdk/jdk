3e80b57fdeb96c242468340f50e9213cf816018a
Patrick Fontanilla <pfon@amazon.com>
2026-09-04 00:41:54 +0000

    Try to be more deterministic

--- test/hotspot/jtreg/gc/shenandoah/TestHumongousAllocFailureDegeneration.java
+++ test/hotspot/jtreg/gc/shenandoah/TestHumongousAllocFailureDegeneration.java
@@ -25,86 +25,113 @@
 
 /*
  * @test id=generational
+ * @bug 8391591
  * @summary Collector degenerates on humongous allocation failure
  * @requires vm.gc.Shenandoah
+ * @requires vm.flagless
  * @library /test/lib
  * @run main/othervm TestHumongousAllocFailureDegeneration
  */
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import jdk.test.lib.Utils;
 import jdk.test.lib.process.OutputAnalyzer;
 import jdk.test.lib.process.ProcessTools;
 
 public class TestHumongousAllocFailureDegeneration {
     static final int KB = 1024;
     static final int MB = 1024 * 1024;
-    static final long HEAP_SIZE = MB * 128;
-    static final long TARGET = HEAP_SIZE - (HEAP_SIZE / 10);
+    static final int REGIONS = 128;
 
     public static void runTests(List<String> args) throws Exception {
         final String degenStartMsg = "Starting GC (degenerated): Humongous Allocation Failure";
-        final String degenCompletedMsg = "Completed GC (degenerated): Humongous Allocation Failure";
-        final String fullMsg = "Pause Full";
+        final String degenCompleteMsg = "Completed GC (degenerated): Humongous Allocation Failure";
+        final String fullStartMsg = "Starting GC (full): Humongous Allocation Failure";
+        final String fullCompleteMsg = "Completed GC (full): Humongous Allocation Failure";
         final String degenTest = DegenTest.class.getName();
         final String fullTest = FullTest.class.getName();
 
         OutputAnalyzer output = ProcessTools.executeLimitedTestJava(args);
         output.shouldHaveExitValue(0);
         output.shouldContain(degenStartMsg);
-        output.shouldContain(degenCompletedMsg);
+        output.shouldContain(degenCompleteMsg);
 
         if (args.get(args.size() - 1).equals(degenTest)) {
-            // No consecutive degenerated cycles
-             output.shouldNotContain(fullMsg);
+            // We should not have transitioned into a full GC.
+            output.shouldNotContain(fullStartMsg);
         } else {
             // Consecutive degenerated cycles triggers a full GC.
-            output.shouldContain(fullMsg);
+            output.shouldContain(fullStartMsg);
+            output.shouldContain(fullCompleteMsg);
         }
     }
 
     public static class DegenTest {
-        public static void main(String[] args) {
-            long estimated_allocated = 0;
-            for (int i = 0; i < 128; i++) {
-                // Allocate enough bytes to the point where the heap is ~90% full.
-                // This gives us a good chance that we run into humongous allocation failure.
-                // These small allocations shouldn't be enough where we run back-to-back
-                // degenerated cycles.
-                while (estimated_allocated <= TARGET) {
-                    byte[] regular = new byte[KB * 256];
-                    byte[] humongous = new byte[MB * 4];
-                    estimated_allocated += (KB * 256) + (MB * 4);
+        public static void main(String[] args) throws Exception {
+            final int regularAlloc = 512;
+            final int humongousMin = 1;
+            final int humongousMax = 8;
+            final int humongousIterations = 8;
+            final int regularIterations = 64;
+            final int humongousPressureRegions = REGIONS - (regularIterations / 2);
+            final int continueHumongousAllocation = 10;
+            final Random r = Utils.getRandomInstance();
+
+            for (int i = 0; i < 8; i++) {
+                System.gc();
+                List<Object> regular = new ArrayList<>();
+
+                // Allocates ~ 32 MB
+                for (int j = 0; j < regularIterations; j++) {
+                    regular.add(new byte[KB * regularAlloc]);
                 }
-                estimated_allocated = 0;
+
+                int humongousRegions = 0;
+                int retries = 0;
+                while (humongousRegions < humongousPressureRegions || retries < continueHumongousAllocation) {
+                    // We most likely hit humongous allocation failure here. The degenerated cycle should've
+                    // reclaimed all unreachable humongous objects. It's safe to allocate more humongous objects
+                    // because we have enough free regions to satisfy the humongous allocation. For example,
+                    // continueHumongousAllocation * humongousMax < humongousPressureRegions.
+                    if (humongousRegions >= humongousPressureRegions) {
+                        retries += 1;
+                    }
+
+                    int humongousRandomAlloc = r.nextInt((humongousMax - humongousMin) + 1) + humongousMin;
+                    byte[] humongous = new byte[MB * humongousRandomAlloc];
+                    humongousRegions += humongousRandomAlloc;
+                }
             }
         }
     }
 
     public static class FullTest {
         public static void main(String[] args) {
-            // Allocate a humongous object that takes up half the heap.
-            // There will be consecutive degenerated cycles that will
-            // transition into full GCs.
+            // Allocate a humongous object that takes up a large portion
+            // of the heap. There will be consecutive degenerated cycles
+            // that will transition into full GCs.
             for (int i = 0; i < 32; i++) {
-                byte[] obj1 = new byte[MB * 64];
+                byte[] humongous = new byte[MB * 96];
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
         ArrayList<String> flags = new ArrayList<>(List.of("-XX:+UnlockExperimentalVMOptions",
-                                                            "-XX:+UseShenandoahGC",
-                                                            "-XX:ShenandoahGCMode=generational",
-                                                            "-Xmx128m",
-                                                            "-Xms128m",
-                                                            "-XX:ShenandoahRegionSize=1m",
-                                                            "-XX:ShenandoahFullGCThreshold=3",
-                                                            "-Xlog:gc,gc+thread=debug",
-                                                            "-XX:ConcGCThreads=1", 
-                                                            "-XX:ParallelGCThreads=1"));
-
+                                                          "-XX:+UseShenandoahGC",
+                                                          "-XX:ShenandoahGCMode=generational",
+                                                          "-Xmx128m",
+                                                          "-Xms128m",
+                                                          "-XX:ShenandoahRegionSize=1m",
+                                                          "-XX:ShenandoahFullGCThreshold=3",
+                                                          "-XX:ShenandoahGuaranteedGCInterval=0",
+                                                          "-XX:+AlwaysPreTouch",
+                                                          "-Xlog:gc+thread=debug",
+                                                          "-XX:ConcGCThreads=1",
+                                                          "-XX:ParallelGCThreads=1"));
         flags.add(DegenTest.class.getName());
         runTests(flags);
         flags.removeLast();
