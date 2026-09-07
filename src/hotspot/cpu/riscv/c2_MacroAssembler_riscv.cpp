4e6d701ba9a0137c742456f1e650931bc506c80b
zifeihan <cg02554262@alibaba-inc.com>
2026-09-07 06:47:55 +0000

    RISC-V: C2 OverflowAdd/Sub/Mul I/L

--- src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
+++ src/hotspot/cpu/riscv/c2_MacroAssembler_riscv.cpp
@@ -2021,9 +2021,11 @@ void C2_MacroAssembler::enc_cmpUEqNeLeGt_imm0_branch(int cmpFlag, Register op1, 
 void C2_MacroAssembler::enc_cmpEqNe_imm0_branch(int cmpFlag, Register op1, Label& L, bool is_far) {
   switch (cmpFlag) {
     case BoolTest::eq:
+    case BoolTest::no_overflow:
       beqz(op1, L, is_far);
       break;
     case BoolTest::ne:
+    case BoolTest::overflow:
       bnez(op1, L, is_far);
       break;
     default:
