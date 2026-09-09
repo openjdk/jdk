e9a3adb76a5a04bc9d7abe9d09e4a1ad33f73974
Thomas Schatzl <thomas.schatzl@oracle.com>
2026-09-09 15:59:56 +0200

    * walulyai review

--- src/hotspot/share/gc/g1/g1CardTableClaimTable.cpp
+++ src/hotspot/share/gc/g1/g1CardTableClaimTable.cpp
@@ -64,12 +64,12 @@ void G1CardTableClaimTable::reset_all_to_claimed() {
 void G1CardTableClaimTable::heap_region_iterate_from_worker_offset(G1HeapRegionClosure* cl, uint worker_id, uint max_workers) {
   // Every worker will actually look at all regions, skipping over regions that
   // are completed.
-  const size_t num_regions = _max_reserved_regions;
-  const uint start_index = (uint)(worker_id * num_regions / max_workers);
+  const size_t max_num_regions = _max_reserved_regions;
+  const uint start_index = (uint)(worker_id * max_num_regions / max_workers);
 
-  for (uint count = 0; count < num_regions; count++) {
-    const uint index = (start_index + count) % num_regions;
-    assert(index < num_regions, "sanity");
+  for (uint count = 0; count < max_num_regions; count++) {
+    const uint index = (start_index + count) % max_num_regions;
+    assert(index < max_num_regions, "sanity");
     // Skip over fully processed regions
     if (!has_unclaimed_cards(index)) {
       continue;
