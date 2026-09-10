b483939ea8f0d5b7cd938363da8f79761909b144
Guanqiang Han <hanguanqiang@kylinos.cn>
2026-09-10 16:15:14 +0800

    Add \n

--- src/hotspot/share/opto/c2_globals.hpp
+++ src/hotspot/share/opto/c2_globals.hpp
@@ -744,13 +744,13 @@
                                                                             \
   develop(uint, VerifyIterativeGVN, 0,                                      \
           "Verify Iterative Global Value Numbering. Set the corresponding " \
-          "decimal place to 1 to enable a check, or 0 to disable it:"       \
-          "  100000: verify IGVN method return invariants"                  \
-          "   10000: verify node specific invariants"                       \
-          "    1000: verify Node::Identity did not miss opportunities"      \
-          "     100: verify Node::Ideal did not miss opportunities"         \
-          "      10: verify that type(n) == n->Value() after IGVN"          \
-          "       1: verify Def-Use modifications during IGVN. "            \
+          "decimal place to 1 to enable a check, or 0 to disable it:\n"     \
+          "100000: verify IGVN method return invariants\n"                  \
+          " 10000: verify node specific invariants\n"                       \
+          "  1000: verify Node::Identity did not miss opportunities\n"      \
+          "   100: verify Node::Ideal did not miss opportunities\n"         \
+          "    10: verify that type(n) == n->Value() after IGVN\n"          \
+          "     1: verify Def-Use modifications during IGVN\n"              \
           "Example: -XX:VerifyIterativeGVN=101 enables Node::Ideal and "    \
           "Def-Use checks.")                                                \
           constraint(VerifyIterativeGVNConstraintFunc, AtParse)             \
