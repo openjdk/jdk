f24c5e06951723f6ed149b46420b8554ff452aaf
Sean Mullan <sean.mullan@oracle.com>
2026-09-10 08:22:10 -0400

    Store properties as specified by Properties.store(OutputStream, String).

--- src/jdk.jlink/share/classes/jdk/tools/jlink/internal/plugins/SecurityPropertiesPlugin.java
+++ src/jdk.jlink/share/classes/jdk/tools/jlink/internal/plugins/SecurityPropertiesPlugin.java
@@ -35,6 +35,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HexFormat;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -116,36 +117,39 @@ public class SecurityPropertiesPlugin extends AbstractPlugin {
                 BufferedReader br = new BufferedReader(isr)) {
             String line = br.readLine();
             while (line != null) {
+                // add comments and blank lines as-is
                 if (isBlankOrComment(line)) {
                     lines.add(line);
                     line = br.readLine();
                     continue;
                 }
+
                 StringBuilder sb = new StringBuilder(line);
                 while (lineContinues(line)) {
-                    // multi-lined value
+                    // multi-lined value, add lines until end of value
                     line = br.readLine();
-                    sb.append("\n");
-                    sb.append(line);
+                    sb.append("\n").append(line);
                 }
 
                 // use Properties.load() to ensure property is parsed correctly
                 ByteArrayInputStream bais = new ByteArrayInputStream(
                     sb.toString().getBytes(StandardCharsets.ISO_8859_1));
-                Properties jsProps = new Properties();
-                jsProps.load(bais);
-                Set<String> propNames = jsProps.stringPropertyNames();
+                Properties props = new Properties();
+                props.load(bais);
+                Set<String> propNames = props.stringPropertyNames();
                 // should only be one property
                 if (propNames.size() != 1) {
                     throw new PluginException("Parsing error: " + propNames);
                 }
+
+                // check if property should be overridden
                 String propName = propNames.iterator().next();
                 String propValue = (String) extraProps.remove(propName);
                 if (propValue != null) {
                     // override value
-                    lines.add(propName + "=" + propValue);
+                    lines.add(line(propName, propValue));
                 } else {
-                    lines.add(sb.toString());
+                    lines.add(line(propName, props.getProperty(propName)));
                 }
                 line = br.readLine();
             }
@@ -157,12 +161,14 @@ public class SecurityPropertiesPlugin extends AbstractPlugin {
         String includeValue = (String) extraProps.remove("include");
 
         // add user-defined properties at end
-        extraProps.forEach((k, v) -> lines.add(k + "=" + v));
+        for (String propName : extraProps.stringPropertyNames()) {
+            lines.add(line(propName, extraProps.getProperty(propName)));
+        };
 
         // add include property at end if it has been specified and use
         // space character as delimiter
         if (includeValue != null) {
-            lines.add("include " + includeValue);
+            lines.add("include " + convert(includeValue, false));
         }
 
         // Write contents of list to byte array. Use ISO_8859_1 to be
@@ -175,6 +181,7 @@ public class SecurityPropertiesPlugin extends AbstractPlugin {
                 bw.append(line);
                 bw.newLine();
             }
+            bw.flush();
         } catch (Exception e) {
             throw new PluginException(e);
         }
@@ -182,7 +189,7 @@ public class SecurityPropertiesPlugin extends AbstractPlugin {
     }
 
     /**
-     * Returns true if line is empty, or only contains whitespace, or has an
+     * Returns true if line is empty, only contains whitespace, or has an
      * ASCII '#' or '!' as its first non-whitespace character. Whitespace
      * characters are as specified by java.util.Properties.
      */
@@ -216,5 +223,71 @@ public class SecurityPropertiesPlugin extends AbstractPlugin {
             }
         }
         return (backslashes % 2 == 0) ? false : true;
+    }
+
+    /**
+     * Create line to add to properties file.
+     */
+    private static String line(String name, String value) {
+        return convert(name, true) + "=" + convert(value, false);
+    }
+
+    /**
+     * Converts unicodes to encoded &#92;uxxxx and escapes
+     * special characters with a preceding slash.
+     *
+     * This method was copied from java.util.Properties.saveConvert().
+     */          
+    private static String convert(String theString,
+                                  boolean escapeSpace) {
+        int len = theString.length();
+        int bufLen = len * 2;
+        if (bufLen < 0) {
+            bufLen = Integer.MAX_VALUE;
+        }
+        StringBuilder outBuffer = new StringBuilder(bufLen);
+        HexFormat hex = HexFormat.of().withUpperCase();
+        for(int x=0; x<len; x++) {
+            char aChar = theString.charAt(x);
+            // Handle common case first, selecting largest block that
+            // avoids the specials below
+            if ((aChar > 61) && (aChar < 127)) {
+                if (aChar == '\\') {
+                    outBuffer.append('\\'); outBuffer.append('\\');
+                    continue;
+                }
+                outBuffer.append(aChar);
+                continue;
+            }
+            switch(aChar) {
+                case ' ':
+                    if (x == 0 || escapeSpace)
+                        outBuffer.append('\\');
+                    outBuffer.append(' ');
+                    break;
+                case '\t':outBuffer.append('\\'); outBuffer.append('t');
+                          break;
+                case '\n':outBuffer.append('\\'); outBuffer.append('n');
+                          break;
+                case '\r':outBuffer.append('\\'); outBuffer.append('r');
+                          break;
+                case '\f':outBuffer.append('\\'); outBuffer.append('f');
+                          break;
+                case '=': // Fall through
+                case ':': // Fall through
+                case '#': // Fall through
+                case '!':
+                    outBuffer.append('\\'); outBuffer.append(aChar);
+                    break;
+                default:
+                    if (((aChar < 0x0020) || (aChar > 0x007e))) {
+                        outBuffer.append("\\u");
+                        outBuffer.append(hex.toHexDigits(aChar));
+                    } else {
+                        outBuffer.append(aChar);
+                    }
+            }
+        }
+        return outBuffer.toString();
     }
 }
