8353b0df97b096edb8ca7eaa5b3df8eeb3d8b573
Jan Lahoda <jan.lahoda@oracle.com>
2026-09-14 16:54:11 +0200

    Cleanup.

--- test/langtools/tools/javac/annotations/typeAnnotations/TypeAnnosOnMemberReferenceTest.java
+++ test/langtools/tools/javac/annotations/typeAnnotations/TypeAnnosOnMemberReferenceTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2025, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -23,7 +23,7 @@
 
 /*
  * @test
- * @bug 8369489
+ * @bug 8369489 8391567
  * @summary Verify annotations on member references work reasonably.
  * @library /tools/lib /tools/javac/lib
  * @modules
