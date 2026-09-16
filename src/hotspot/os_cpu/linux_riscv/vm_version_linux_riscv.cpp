5583ab4891b01fff81de081874479a16545cba44
Guoxiong Li <guoxiong.lgx@alibaba-inc.com>
2026-09-16 12:13:46 +0000

    Fix extension location

--- src/hotspot/os_cpu/linux_riscv/vm_version_linux_riscv.cpp
+++ src/hotspot/os_cpu/linux_riscv/vm_version_linux_riscv.cpp
@@ -341,6 +341,7 @@ void VM_Version::xuantie_features() {
   }
 
   ext_v.enable_feature();
+  ext_Zabha.enable_feature();
   ext_Zba.enable_feature();
   ext_Zbb.enable_feature();
   ext_Zbc.enable_feature();
@@ -361,7 +362,6 @@ void VM_Version::xuantie_features() {
   ext_Zvfh.enable_feature();
   ext_Zvkn.enable_feature();
   ext_Zvkg.enable_feature();
-  ext_Zabha.enable_feature();
 
 #ifndef PRODUCT
   ext_Zacas.enable_feature();
