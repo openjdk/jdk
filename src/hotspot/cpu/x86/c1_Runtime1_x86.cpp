06cdcafb09ae9c2092b81da040f4dae34a06fd49
Manuel Hässig <manuel.hassig@oracle.com>
2026-09-17 12:15:53 +0200

    Improve scratch registers for valhalla c1 stubs

--- src/hotspot/cpu/x86/c1_Runtime1_x86.cpp
+++ src/hotspot/cpu/x86/c1_Runtime1_x86.cpp
@@ -1004,7 +1004,7 @@ OopMapSet* Runtime1::generate_code_for(StubId id, StubAssembler* sasm) {
 
         oop_maps = new OopMapSet();
         oop_maps->add_gc_map(call_offset, map);
-        restore_live_registers_except_rax(sasm);
+        restore_live_registers(sasm);
       }
       break;
 
