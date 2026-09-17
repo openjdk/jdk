53929c344bfd4d112a86c72d70ce10c9cff5cb42
Tobias Hartmann <tobias.hartmann@oracle.com>
2026-09-17 09:22:30 +0200

    8392533

--- src/hotspot/share/c1/c1_GraphBuilder.cpp
+++ src/hotspot/share/c1/c1_GraphBuilder.cpp
@@ -2138,6 +2138,10 @@ void GraphBuilder::access_field(Bytecodes::Code code) {
             bailout("store to an uninitialized nullable non-atomic flat field");
             return;
           }
+          if (value_klass->is_empty()) {
+            // Needs an explicit null check because below code does not perform any actual store if there are no fields
+            null_check(obj);
+          }
 
           // Store the subfields when field is a nullable non-atomic field
           Value object_null = append(new Constant(objectNull));
