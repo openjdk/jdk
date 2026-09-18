8a0bdd8dfac242b62e6b9095ee4e472db99ed06b
Patricio Chilano Mateo <patricio.chilano.mateo@oracle.com>
2026-09-18 01:07:20 -0400

    add has_scalarized_args check

--- src/hotspot/share/opto/output.cpp
+++ src/hotspot/share/opto/output.cpp
@@ -3431,8 +3431,13 @@ int PhaseOutput::frame_size_in_words() const {
 // in turn simplifies stack overflow handling.
 int PhaseOutput::bang_size_in_bytes() const {
   int extra_arg_size = 0;
-  if (C->needs_stack_repair()) {
+  // Methods retrieved from AOT archive can have needs_stack_repair
+  // set to true, but now we could be using nonscalarized convention,
+  // i.e. C->method()->get_sig_cc() returns null. Guard check with
+  // has_scalarized_args() which is cleared when archiving.
+  if (C->has_scalarized_args() && C->needs_stack_repair()) {
     // Account for required stack extension conservatively
+    assert(C->method()->get_sig_cc() != nullptr, "must have scalarized signature");
     extra_arg_size = MIN2(CompiledEntrySignature::max_stack_slots_cc() * VMRegImpl::stack_slot_size,
                           C->method()->get_sig_cc()->length() * wordSize);
     assert(extra_arg_size < (int)os::vm_page_size(), "assumption is it adds at most one page");
