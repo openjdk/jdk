0de203071fc5187c8f3723bcd97f0494b193e78f
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2026-09-18 10:31:46 +0000

    Extend and simplify tests

--- test/hotspot/jtreg/compiler/valhalla/valuetypes/TestValueArgEscapeState.java
+++ test/hotspot/jtreg/compiler/valhalla/valuetypes/TestValueArgEscapeState.java
@@ -28,8 +28,8 @@ import compiler.lib.ir_framework.*;
 /**
  * @test
  * @bug 8392283
- * @summary Test that C2 and the bytecode escape analyzer together compute the
- *          correct escape state for fields of value objects when these are
+ * @summary Test that C2 and bytecode escape analysis (BCEA) together compute
+ *          the correct escape state for fields of value objects when these are
  *          passed as scalar arguments.
  * @library /test/lib /
  * @enablePreview
@@ -69,88 +69,109 @@ class TestValueArgEscapeState {
         }
     }
 
-    static IdentityObject global = null;
+    static IdentityObject globalObj = null;
+    static ValueHolder globalHolder = null;
 
-    @DontInline
-    static void escape(ValueHolder holder) {
-        global = holder.obj;
-    }
-
-    @DontInline
-    static void escape(ValueHolderHolder holderHolder) {
-        global = holderHolder.holder.obj;
+    @ForceInline
+    static ValueHolder createValueHolderWithSynchronizedFieldWrite() {
+        IdentityObject obj = new IdentityObject();
+        ValueHolder holder = new ValueHolder(obj);
+        synchronized (obj) {
+            obj.val = 42;
+        }
+        return holder;
     }
 
     @DontInline
     static void dontUse(ValueHolder holder) {}
 
     @DontInline
-    static ValueHolder justReturn(ValueHolder holder) {
-        return holder;
+    static void dontUse(ValueHolderHolder holder) {}
+
+    @DontInline
+    static IdentityObject useButDontEscape(ValueHolder holder) {
+        return holder.obj;
     }
 
-    // The IdentityObject allocation referenced by holder.obj escapes within
-    // escape(), so C2 should classify it as globally escaping, and preserve the
-    // lock and unlock operations implementing the synchronized block.
-    @Test
-    @Arguments(values = {Argument.NUMBER_42})
-    @IR(counts = {IRNode.FAST_LOCK, "> 0",
-                  IRNode.FAST_UNLOCK, "> 0"})
-    static void testEscapingScalarizedValue(int val) {
-        IdentityObject obj = new IdentityObject();
-        ValueHolder holder = new ValueHolder(obj);
-        synchronized (obj) {
-            obj.val = val;
-        }
-        escape(holder);
+    @DontInline
+    static void escapeField(ValueHolder holder) {
+        globalObj = holder.obj;
     }
 
-    // Variant of the above test using a nested value object.
-    @Test
-    @Arguments(values = {Argument.NUMBER_42})
-    @IR(counts = {IRNode.FAST_LOCK, "> 0",
-                  IRNode.FAST_UNLOCK, "> 0"})
-    static void testEscapingNestedScalarizedValue(int val) {
-        IdentityObject obj = new IdentityObject();
-        ValueHolder holder = new ValueHolder(obj);
-        ValueHolderHolder holderHolder = new ValueHolderHolder(holder);
-        synchronized (obj) {
-            obj.val = val;
-        }
-        escape(holderHolder);
+    @DontInline
+    static void escapeField(ValueHolderHolder holderHolder) {
+        globalObj = holderHolder.holder.obj;
     }
 
-    // The IdentityObject allocation referenced by holder.obj is unused by the
-    // callee, so C2 should classify it as not globally escaping (exploiting the
-    // bytecode escape analyzer's ability to detect the argument as "local"),
-    // and optimize away the lock and unlock operations implementing the
-    // synchronized block.
+    @DontInline
+    static void escape(ValueHolder holder) {
+        globalHolder = holder;
+    }
+
+    // BCEA classifies the holder as a "local" object, which guarantees that
+    // none of its fields are accessed within the non-inlined callee. C2 should
+    // classify the IdentityObject field as not globally escaping and optimize
+    // away the synchronization operations.
     @Test
-    @Arguments(values = {Argument.NUMBER_42})
     @IR(failOn = {IRNode.FAST_LOCK, IRNode.FAST_UNLOCK})
-    static void testUnusedScalarizedValue(int val) {
-        IdentityObject obj = new IdentityObject();
-        ValueHolder holder = new ValueHolder(obj);
-        synchronized (obj) {
-            obj.val = val;
-        }
+    static void testPassLocalValue() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
         dontUse(holder);
     }
 
-    // The IdentityObject allocation referenced by holder.obj is just returned
-    // by the callee, so C2 should classify it as not globally escaping
-    // (exploiting the bytecode escape analyzer's ability to detect the argument
-    // as "local" and "returned"), and optimize away the lock and unlock
-    // operations implementing the synchronized block.
+    // Variant of the above test using a nested value object. The same outcome
+    // is expected.
     @Test
-    @Arguments(values = {Argument.NUMBER_42})
     @IR(failOn = {IRNode.FAST_LOCK, IRNode.FAST_UNLOCK})
-    static void testJustReturnedScalarizedValue(int val) {
-        IdentityObject obj = new IdentityObject();
-        ValueHolder holder = new ValueHolder(obj);
-        synchronized (obj) {
-            obj.val = val;
-        }
-        justReturn(holder);
+    static void testPassLocalNestedValue() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
+        ValueHolderHolder holderHolder = new ValueHolderHolder(holder);
+        dontUse(holderHolder);
+    }
+
+    // BCEA classifies the holder as a stack-allocatable object, but does not
+    // provide any guarantee about the escape state of its IdentityObject field,
+    // so C2 should classify the field as globally escaping, and preserve the
+    // synchronization operations.
+    // In this case, the field does not escape globally, but C2 acts
+    // conservatively in the absence of more precise BCEA information.
+    @Test
+    @IR(counts = {IRNode.FAST_LOCK, "> 0", IRNode.FAST_UNLOCK, "> 0"})
+    static void testPassStackValue() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
+        useButDontEscape(holder);
+    }
+
+    // BCEA reports the same result as in the above test for the holder: the
+    // holder itself is stack-allocatable, but nothing is known about the escape
+    // state of its fields. C2 should classify the field as globally escaping,
+    // and preserve the synchronization operations.
+    // In this case, this turns out to be critical, because the field does
+    // escape within the non-inlined callee.
+    @Test
+    @IR(counts = {IRNode.FAST_LOCK, "> 0", IRNode.FAST_UNLOCK, "> 0"})
+    static void testPassStackValueWithEscapingField() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
+        escapeField(holder);
+    }
+
+    // Variant of the above test using a nested value object. The same outcome
+    // is expected.
+    @Test
+    @IR(counts = {IRNode.FAST_LOCK, "> 0", IRNode.FAST_UNLOCK, "> 0"})
+    static void testPassStackNestedValueWithEscapingField() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
+        ValueHolderHolder holderHolder = new ValueHolderHolder(holder);
+        escapeField(holderHolder);
+    }
+
+    // BCEA classifies the holder as a globally escaping object, so C2 should
+    // classify its field as globally escaping, and preserve the synchronization
+    // operations.
+    @Test
+    @IR(counts = {IRNode.FAST_LOCK, "> 0", IRNode.FAST_UNLOCK, "> 0"})
+    static void testPassEscapingValue() {
+        ValueHolder holder = createValueHolderWithSynchronizedFieldWrite();
+        escape(holder);
     }
 }
