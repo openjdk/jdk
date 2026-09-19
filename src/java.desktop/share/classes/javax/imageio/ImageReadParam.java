dd557b93796176fcc2853b76d015e41ad3f716f0
Sergey Bylokhov <bylokhov@amazon.com>
2026-09-19 01:40:37 -0700

    8392733: Add missing @Override annotations in "javax.imageio" package

--- src/java.desktop/share/classes/javax/imageio/ImageReadParam.java
+++ src/java.desktop/share/classes/javax/imageio/ImageReadParam.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2014, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -197,6 +197,7 @@ public class ImageReadParam extends IIOParam {
     public ImageReadParam() {}
 
     // Comment inherited
+    @Override
     public void setDestinationType(ImageTypeSpecifier destinationType) {
         super.setDestinationType(destinationType);
         setDestination(null);
