53abeff9cb9680ac7188bcee17bc663e7d4e725b
Johan Sjölén <johan.sjolen@oracle.com>
2026-09-21 12:01:40 +0200

    Ref stack

--- src/hotspot/share/memory/metaspaceClosure.hpp
+++ src/hotspot/share/memory/metaspaceClosure.hpp
@@ -112,7 +112,6 @@ public:
     Writability _writability;
     address _enclosing_obj;
     Ref* _next;
-    NONCOPYABLE(Ref);
 
   protected:
     virtual void** mpp() const = 0;
@@ -123,6 +122,7 @@ public:
     virtual void metaspace_pointers_do(MetaspaceClosure *it) const = 0;
     virtual MetaspaceClosureType type() const = 0;
     virtual bool is_read_only_by_default() const = 0;
+    virtual Ref* clone() const = 0;
     virtual ~Ref() {}
 
     address obj() const {
@@ -198,6 +198,11 @@ private:
     virtual bool not_null()                const override { return dereference() != nullptr; }
     virtual int size()                     const override { return get_size(dereference()); }
     virtual MetaspaceClosureType type()    const override { return as_type(dereference()->type()); }
+    virtual Ref* clone()                   const override {
+      MSORef* cloned_obj = new MSORef(_mpp, writability());
+      cloned_obj->set_enclosing_obj(this->enclosing_obj());
+      return cloned_obj;
+    }
     virtual void metaspace_pointers_do(MetaspaceClosure *it) const override {
       dereference()->metaspace_pointers_do(it);
     }
@@ -226,6 +231,11 @@ private:
     virtual bool not_null()                const override { return dereference() != nullptr;  }
     virtual int size()                     const override { return (int)heap_word_size(sizeof(*dereference())); }
     virtual MetaspaceClosureType type()    const override { return MetaspaceClosureType::GrowableArrayType; }
+    virtual Ref* clone() const override {
+      GrowableArrayRef<T>* cloned_obj = new GrowableArrayRef<T>(_mpp, writability());
+      cloned_obj->set_enclosing_obj(this->enclosing_obj());
+      return cloned_obj;
+    }
     virtual void metaspace_pointers_do(MetaspaceClosure *it) const override {
       GrowableArray<T>* array = dereference();
       log_trace(aot)("Iter(GrowableArray): %p [%d]", array, array->length());
@@ -267,6 +277,11 @@ private:
     virtual bool not_null()                const override { precond(dereference() != nullptr); return true; }
     virtual int size()                     const override { return (int)heap_word_size(byte_size()); }
     virtual MetaspaceClosureType type()    const override { return MetaspaceClosureType::CArrayType; }
+    virtual Ref* clone()                   const override {
+      CArrayRef<T>* cloned_obj = new CArrayRef<T>(_mpp, _num_elems, writability());
+      cloned_obj->set_enclosing_obj(this->enclosing_obj());
+      return cloned_obj;
+    }
     virtual void metaspace_pointers_do(MetaspaceClosure *it) const override {
       metaspace_pointers_do_impl<T>(it, dereference());
     }
@@ -327,7 +342,8 @@ private:
   void push_with_ref(T** mpp, Writability w) {
     // We cannot make stack allocation because the Ref may need to be saved in
     // _pending_refs to avoid overflowing the C call stack
-    push_impl(new REF_TYPE(mpp, w));
+    REF_TYPE ref(mpp, w);
+    push_impl(&ref);
   }
 
 public:
@@ -363,7 +379,8 @@ public:
   // --- The buffer of GrowableArray<T>
   template <typename T>
   void push_c_array(T** mpp, int num_elems, Writability w = _default) {
-    push_impl(new CArrayRef<T>(mpp, num_elems, w));
+    CArrayRef<T> ref(mpp, num_elems, w);
+    push_impl(&ref);
   }
 };
 
