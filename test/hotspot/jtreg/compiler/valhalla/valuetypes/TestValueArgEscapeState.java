b56223f561f210c0242e1f10579cabd812cb4a46
Roberto Castañeda Lozano <roberto.castaneda.lozano@oracle.com>
2026-09-21 09:04:16 +0000

    Do not constrain test platforms

--- test/hotspot/jtreg/compiler/valhalla/valuetypes/TestValueArgEscapeState.java
+++ test/hotspot/jtreg/compiler/valhalla/valuetypes/TestValueArgEscapeState.java
@@ -33,7 +33,6 @@ import compiler.lib.ir_framework.*;
  *          passed as scalar arguments.
  * @library /test/lib /
  * @enablePreview
- * @requires (os.simpleArch == "x64" | os.simpleArch == "aarch64" | os.simpleArch == "riscv64")
  * @run driver ${test.main.class}
  */
 
