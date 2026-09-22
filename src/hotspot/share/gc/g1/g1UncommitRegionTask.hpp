b7a083c17f91e9758cc58c837ec03c9844423209
Thomas Schatzl <thomas.schatzl@oracle.com>
2026-09-22 14:29:56 +0200

    * shade review

--- src/hotspot/share/gc/g1/g1UncommitRegionTask.hpp
+++ src/hotspot/share/gc/g1/g1UncommitRegionTask.hpp
@@ -35,8 +35,8 @@ class G1UncommitRegionTask : public G1ServiceTask {
   // This limit is small enough to ensure that the duration of each invocation
   // is short, while still making reasonable progress.
   static const uint UncommitSizeLimit = 128 * M;
-  // Minimum number of regions to uncommit to get some reasonable progress.
-  static const uint MinRegionsToUncommit = 4;
+  // Minimum number of regions to uncommit.
+  static const uint MinRegionsToUncommit = 1;
   // The delay between two uncommit task executions.
   static const uint UncommitTaskDelayMs = 10;
 
