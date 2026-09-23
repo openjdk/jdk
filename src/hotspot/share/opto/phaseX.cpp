a39cbf522cf5908f78c83d03235c2db91385a66f
Marc Chevalier <marc.chevalier@oracle.com>
2026-09-23 10:14:25 +0200

    move

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -390,11 +390,6 @@ PhaseRemoveUseless::PhaseRemoveUseless(PhaseGVN* gvn, Unique_Node_List& worklist
 
   // Disconnect 'useless' nodes that are adjacent to useful nodes
   C->disconnect_useless_nodes(_useful, worklist);
-
-  // Useless nodes might be added to the worklist during parsing and in
-  // disconnect_useless_nodes. Let's remove them from the worklist now
-  // they are not in the graph anymore.
-  worklist.remove_useless_nodes(_useful.member_set());
 }
 
 //=============================================================================
