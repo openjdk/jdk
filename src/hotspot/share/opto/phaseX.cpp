2dc4f35b1fb8bf3af1fe9fb13c7baf6ae52ea26f
Roland Westrelin <47032191+rwestrel@users.noreply.github.com>
2026-09-24 17:32:07 +0200

    Update src/hotspot/share/opto/phaseX.cpp
    Co-authored-by: Christian Hagedorn <christian.hagedorn@oracle.com>

--- src/hotspot/share/opto/phaseX.cpp
+++ src/hotspot/share/opto/phaseX.cpp
@@ -2913,12 +2913,12 @@ void PhaseIterGVN::add_users_of_use_to_worklist(Node* n, Node* use, Unique_Node_
   // and is applied if there are EncodeP/DecodeN or casts between a Phi and InlineType nodes
   if (use->is_Phi() || use->is_EncodeP() || use->is_DecodeN() || use->is_ConstraintCast()) {
     auto is_boundary = [](Node* n){ return !n->is_EncodeP() && !n->is_DecodeN() && !n->is_ConstraintCast(); };
-    auto push_and_to_worklist = [&worklist](Node* n){
+    auto push_phi_to_worklist = [&worklist](Node* n){
       if (n->is_Phi()) {
         worklist.push(n);
       }
     };
-    use->visit_uses(push_and_to_worklist, is_boundary);
+    use->visit_uses(push_phi_to_worklist, is_boundary);
   }
 }
 
