79456340630a3d98ea021f6bdf8ce94a6dbb4d28
Serguei Spitsyn <serguei.spitsyn@oracle.com>
2026-09-24 01:38:06 -0700

    replace bug number 8389237 with 8392964

--- test/hotspot/jtreg/serviceability/jvmti/valhalla/SetTag/ValueTagSubstitutabilityTest.java
+++ test/hotspot/jtreg/serviceability/jvmti/valhalla/SetTag/ValueTagSubstitutabilityTest.java
@@ -24,7 +24,7 @@
 /*
  * @test
  * @summary Regression test for JVMTIs tag map value class instance substitutability test
- * @bug 8386824 8389237
+ * @bug 8386824 8392964
  * @requires vm.jvmti
  * @enablePreview
  * @run main/othervm/native -agentlib:ValueTagSubstitutabilityTest ValueTagSubstitutabilityTest
