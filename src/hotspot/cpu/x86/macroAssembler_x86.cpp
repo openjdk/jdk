d410fb94fb6f808327ca8a3c6ce1b16b709532d3
Andrew Haley <aph@openjdk.org>
2026-09-25 17:12:19 +0100

    Remove debug code

--- src/hotspot/cpu/x86/macroAssembler_x86.cpp
+++ src/hotspot/cpu/x86/macroAssembler_x86.cpp
@@ -4966,10 +4966,6 @@ Address MacroAssembler::argument_address(RegisterOrConstant arg_slot,
 // counter updates are not atomic.
 //
 
-void blah(Metadata* md) {
-  asm("nop");
-}
-
 void MacroAssembler::profile_receiver_type(Register recv, Register mdp, int mdp_offset,
                                            Register temp, addptr_32_insn_t increment) {
   int base_receiver_offset   = in_bytes(ReceiverTypeData::receiver_offset(0));
